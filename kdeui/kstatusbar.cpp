f956737d402860b4e5f552c7d98cca3f42ae790a
Bernd Wuebben <wuebben@kde.org>
1997-05-17 19:13:04 +0000

    Bernd: Added the following methods to kstatusbar: void setInsertOrder(); void setAlignment(); void setHeight(); void setBorderWidth(); I changed some geometry defaults to achieve a better look. Due to the new methods you will however be able to get the old look back if you are so inclined.
    svn path=/trunk/kdelibs/; revision=184

--- kdeui/kstatusbar.cpp
+++ kdeui/kstatusbar.cpp
@@ -1,22 +1,30 @@
 #include <kstatusbar.h>
 #include <ktopwidget.h>
-#include "kstatusbar.moc"
+#include <kstatusbar.moc>
 #include <qpainter.h>
 // Revision 1.10  1998/04/21 20:37:48  radej
-#define StatusBarWidth 25
+// Added insertWidget and some reorganisation - BINARY INCOMPATIBLE
+
+
+/*************************************************************************/
+// We want a statusbar-fields to be by this amount heigher than fm.height().
+
+// This does NOT include the border width which we set separately for the statusbar.
+
 
 KStatusBarItem::KStatusBarItem( char *text, int ID,
-		QWidget *parent, char *name) : QLabel( parent, name ) 
+		QWidget *parent, char *) : QLabel( parent ) 
 KStatusBarLabel::KStatusBarLabel( const QString& text, int _id,
     id = ID;
-    
+  QLabel( parent, name) 
     QFontMetrics fm = fontMetrics();
     w = fm.width( text )+8;
-    resize( w, 22 );
+    h = fm.height() + FONT_Y_DELTA;
+    resize( w, h );
   w = fm.width( text )+8;
     setText( text );
-	setFrameStyle( QFrame::Panel | QFrame::Sunken );
-	setAlignment( AlignLeft | AlignVCenter );
+    setFrameStyle( QFrame::Panel | QFrame::Sunken );
+    setAlignment( AlignLeft | AlignVCenter );
   if ( style() == MotifStyle )
       setFrameStyle( QFrame::Panel | QFrame::Sunken );
   setAlignment( AlignLeft | AlignVCenter );
@@ -32,14 +40,35 @@ KStatusBar::KStatusBar( QWidget *parent, char *name )
 	init();
   : QFrame( parent, name )
 {
+
   delete tmpTimer; // What do I have to notice!?
 };
 
 void KStatusBar::drawContents( QPainter * )
 {
 }
-	setFrameStyle( QFrame::NoFrame );
-	resize( width(), StatusBarWidth );
+
+
+void KStatusBar::init()
+{
+  borderwidth = DEFAULT_BORDERWIDTH;
+  fieldheight = fontMetrics().height() + FONT_Y_DELTA;
+
+  tmpTimer = new QTimer(this);
+  connect (tmpTimer, SIGNAL(timeout()), this, SLOT(clear()));
+
+}
+
+void KStatusBar::setHeight(int h){
+
+  fieldheight = h - 2* borderwidth;
+  resize( width(),h);
+  
+}
+
+
+void KStatusBar::setBorderWidth(int b){
+  
   borderwidth = b;
   resize( width(),height() + 2* borderwidth);
 KStatusBar::~KStatusBar()
@@ -50,18 +79,18 @@ KStatusBar::~KStatusBar()
 
 bool KStatusBar::enable( BarStatus stat )
 {
-	bool mystat = isVisible();
-	if ( (stat == Toggle && mystat) || stat == Hide )
-		hide();
-  	else
-    	show();
-    return ( isVisible() == mystat );
-}
+  bool mystat = isVisible();
+  if ( (stat == Toggle && mystat) || stat == Hide )
+    hide();
+  else if (tempWidget)
+    show();
+  return ( isVisible() == mystat );
+}	
 
 int KStatusBar::insertItem( char *text, int id )
 {
 	KStatusBarItem *label = new KStatusBarItem( text, id, this );
-    labels.append( label );	
+	labels.append( label );	
 	updateRects( TRUE );
 	return labels.at();
 }
@@ -70,22 +99,51 @@ void KStatusBar::resizeEvent( QResizeEvent * ) {
 	updateRects( );
                             width()-2*borderwidth, fieldheight);
   else
+    updateRects( ); // False? You wouldn't sell that to toolbar... (sven)
+}
+
+void KStatusBar::setInsertOrder(InsertOrder order){
+
+  insert_order = order;
 void KStatusBar::updateRects( bool res )
 {
-	int offset=0;
 
-  	for ( KStatusBarItem *b = labels.first(); b!=NULL; b=labels.next() ) {
-  		b->move( offset, 3 );	
-  		offset+=b->width()+2;
-  	}
-  	
-	if ( !res ) {
-		KStatusBarItem *l = labels.getLast();
-		if( l != NULL ) {
-			offset-=l->width()+2;
-			l->resize( width()-offset, 22 );
-		}
-	}
+  
+
+void KStatusBar::updateRects( bool res ){
+  
+    for ( KStatusBarItem *b = labels.first(); b!=NULL; b=labels.next() ) {
+
+    int offset= borderwidth;	
+    for ( KStatusBarItem *b = items.first(); b; b=items.next() ) {
+
+
+      b->setGeometry( offset, borderwidth, b->width(), fieldheight );	
+      offset+=b->width() + borderwidth;
+      KStatusBarItem *l = labels.getLast();
+      if( l != NULL ) {
+	offset-=l->width() + borderwidth;
+	l->setGeometry( offset ,borderwidth, width() - offset - borderwidth, fieldheight );
+      if( l ) {
+		offset-=l->width() + borderwidth;
+		l->setGeometry( offset ,borderwidth, width() - offset - borderwidth, fieldheight );
+      }
+    }
+  }
+    for ( KStatusBarItem *b = labels.first(); b!=NULL; b=labels.next() ) {
+    int offset = width();
+
+    for ( KStatusBarItem *b = items.first(); b; b=items.next() ) {
+      offset -=b->width() + borderwidth;
+      b->setGeometry(offset,borderwidth,b->width(),fieldheight );
+
+      KStatusBarItem *l = labels.getLast();
+      if( l != NULL ) {
+	offset+=l->width() - borderwidth;
+	l->setGeometry(borderwidth,borderwidth,offset,fieldheight);
+      if( l != 0L ) {
+		offset+=l->width() - borderwidth;
+		l->setGeometry(borderwidth,borderwidth,offset,fieldheight);
       }
     }
 void KStatusBar::changeItem( char *text, int id )
@@ -93,6 +151,14 @@ void KStatusBar::changeItem( char *text, int id )
 	for ( KStatusBarItem *b = labels.first(); b!=NULL; b=labels.next() ) 
       		if ( b->ID() == id )
 				b->setText( text );
+  for ( KStatusBarItem *b = items.first(); b; b=items.next() ) 
+	if ( b->ID() == id )
+	  ((KStatusBarLabel *)b->getItem())->setText( text );
+}
+  for ( KStatusBarItem *b = labels.first(); b!=NULL; b=labels.next() ) 
+void KStatusBar::setAlignment( int id, int align)
+	b->setAlignment( align | AlignVCenter );
+  for ( KStatusBarItem *b = items.first(); b; b=items.next() ) 
 void KStatusBar::slotReleased(int _id)
 
 //Eh!!!
--- kdeui/kstatusbar.h
+++ kdeui/kstatusbar.h
@@ -6,6 +6,7 @@
 #include <qlist.h>
 #include <qlabel.h> 
 
+
 class KStatusBarItem : public QLabel {
 	Q_OBJECT
 
@@ -13,7 +14,9 @@ public:
 	KStatusBarItem( char *text, int ID, QWidget *parent=NULL,
 		char *name=NULL );
 	int ID();
-	int w;
+
+	/// store width and height of the KStatusBarItem
+	int w,h;
   
 protected:
 
@@ -32,13 +35,15 @@ class KStatusBar : public QFrame {
 public:
 	enum BarStatus{ Toggle, Show, Hide };
 	enum Position{Top, Left, Bottom, Right, Floating};
-	
+	enum InsertOrder{LeftToRight, RightToLeft};
+
 	KStatusBar(QWidget *parent = NULL, char *name = NULL );
 	~KStatusBar();
 	
 	bool enable( BarStatus stat );
 	
 	/**
+
 		Insert field into the status bar. When inserting the item send the
 		longest text you expect to go into the field as the first argument.
 		The field is sized to accomodate this text. However, the last field
@@ -54,8 +59,34 @@ public:
 	
 	void changeItem( char *text, int id );
 
+	/** 
+	        If order is KStatusBar::LeftToRight the field are inserted from left
+                to right, in particular the last field ist streched to the right
+                border of the app. If order is KStatusBar::RightToLeft the fields
+		are inserted from the right.
+	**/
+
+	void setInsertOrder(InsertOrder order);
+
+	/**     Sets the alignment of a field. By default all fields are aligned left.
+	 */
+
+	void setAlignment(int id, int align);
+
+	/**     Sets the Height of the StatusBar
+	 */
+
+	void setHeight(int);
+
+	/**     Sets the border width of the status bar seperators and frame.
+	 */
+
+	void setBorderWidth(int);
+
 private:
 	QList <KStatusBarItem> labels;
+	InsertOrder insert_order;
+	int fieldheight, borderwidth;
 
 protected:
 	void drawContents ( QPainter * );
