a89603fcb868112cca1b41dac457eab807df47b5
Sirtaj Singh Kang <taj@kde.org>
1997-09-26 10:11:53 +0000

    Changed header documentation to javadoc
    svn path=/trunk/kdelibs/; revision=1452

--- kdeui/kwmmapp.h
+++ kdeui/kwmmapp.h
@@ -16,11 +16,15 @@
 #include "kwm.h"
 
 
-/* The class KWMModuleApplication is the base class for KDE
+/**
+ * The class KWMModuleApplication is the base class for KDE
  * windowmanager modules. It mainly informs a module about all
  * currently managed windows and changes to them (via Qt
  * signals). There are no methods to manipulate windows. These are
  * defined in the class KWM (see kwm.h).  
+ * @short Base class for KDE Window Manager modules.
+ * @author Matthias Ettrich (ettrich@kde.org)
+ * @version $Id$
  */
 class KWMModuleApplication:public KApplication {
   Q_OBJECT
@@ -30,19 +34,22 @@ public:
   KWMModuleApplication( int &argc, char *argv[]);
   virtual ~KWMModuleApplication(){};
 
-  /* Connect to KWM. This cannot be done in the constructor, since your
+  /**
+   * Connect to KWM. This cannot be done in the constructor, since your
    * application probably is not ready to recieve messages at this state.
    */
   void connectToKWM();
   
-  /* if you inherit KWMModuleApplication and overload x11EventFilter,
+  /**
+	if you inherit KWMModuleApplication and overload x11EventFilter,
      be sure to call its x11EventFilter in your x11EventFilter:
            if (KWMModuleApplication::x11EventFilter(XEvent *))
                return True;
   */
   virtual bool x11EventFilter( XEvent * );
 
-  /* A list of all toplevel windows currently managed by the
+  /**
+   * A list of all toplevel windows currently managed by the
    * windowmanger in the order of creation. Please do not rely on
    * indexes of this list: Whenever you enter Qt's eventloop in your
    * application it may happen, that entries are removed or added! So
@@ -51,43 +58,59 @@ public:
    */
   QList <Window> windows;
 
-  /* A list of all toplevel windows currently managed by the
+  /**
+   * A list of all toplevel windows currently managed by the
    * windowmanger in the current stacking order (from lower to
-   * higher). May be usefull for pagers.  
+   * higher). May be useful for pagers.  
    */
   QList <Window> windows_sorted;
 
-  /* Is <Window> still managed at present?
+  /**
+   * Is <Window> still managed at present?
    */
   bool hasWindow(Window);
 
 signals:
 
-  /* Note that an init() may also be emitted if the window manager is
+  /**
+   * Note that an init() may also be emitted if the window manager is
    * restarted. So your module MUST react on it by clearing all internal
    * data structures.
    */
   void init();
 
-  /* Switch to another virtual desktop
+  /**
+   * Switch to another virtual desktop
    */
   void desktopChange(int);
 
-  /* add/remove windows
+  /**
+   * Add a window
    */
   void windowAdd(Window);
+
+  /**
+   * Remove a window
+   */
   void windowRemove(Window);
 
-  /* a window has been changed (size, title, etc.)
+  /**
+   * A window has been changed (size, title, etc.)
    */
   void windowChange(Window);
 
-  /* raise/lower windows 
+  /**
+   * Raise a window
    */ 
   void windowRaise(Window);
+
+  /**
+   * Lower a window
+   */ 
   void windowLower(Window);
   
-  /* hint that <Window> is active (= has focus) now.
+  /**
+   * Hint that <Window> is active (= has focus) now.
    */
   void windowActivate(Window);
 
