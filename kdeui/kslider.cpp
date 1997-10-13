75c55ca564111b9c8bf8d13e28ddd902594fdabf
Christian Esken <esken@kde.org>
1997-10-13 21:45:02 +0000

    !!! BINARY INCOMPATIBLE CHANGE !!! New KSLider is there. It is a derivation of QSlider and now we have all its nifty features for free. For documentation see the QSlider docu- mentation ;-) KSlider has now 3 constructors, all being compatible to QSlider. This means, one constructor is semantically different than before. But no application I know has problems with this.
    svn path=/trunk/kdelibs/; revision=1969

--- kdeui/kslider.cpp
+++ kdeui/kslider.cpp
@@ -1,9 +1,15 @@
 //-----------------------------------------------------------------------------
-// Slider control
+// KSlider control V1.3
 //
 // Copyright (C) Martin R. Jones 1997
 //
-
+// KSlider now maintained by Christian Esken (chris@magicon.prima.ruhr.de)
+// Revision information.
+// 1.0 KSlider by Martin R. Jones
+// 1.1 All changes now by Christian Esken: sanity checks ( checkWidth() ).
+// 1.2 Implemented rangeChange()
+// 1.3 Reworked drawArrow(). For avoiding paint problems with some servers, I
+// am now painting the arrow with "precise" lines.
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
@@ -11,232 +17,200 @@
 
 #include <qpainter.h>
 #include <qdrawutl.h>
-#include <qevent.h>
 
 #include "kslider.h"
-
 #include "kslider.moc"
 
 #define ARROW_LENGTH	13
 
+KSlider::KSlider( QWidget *parent, const char *name )
+  : QSlider( parent, name )
+{
+  setBackgroundMode(PaletteBackground);
+}
+
 KSlider::KSlider( Orientation o, QWidget *parent, const char *name )
-  : QWidget( parent, name ), QRangeControl()
+  : QSlider( (QSlider::Orientation)o, parent, name )
 {
-  _orientation = o;
+  setBackgroundMode(PaletteBackground);
 }
 
-KSlider::KSlider( int _minValue, int _maxValue, int _lineStep, int _pageStep,
-				  Orientation o, QWidget *parent, const char *name )
-  : QWidget( parent, name ), QRangeControl()
+KSlider::KSlider( int _minValue, int _maxValue, int _Step, int _value,
+		  Orientation o, QWidget *parent, const char *name )
+  : QSlider( _minValue, _maxValue, _Step, _value, (QSlider::Orientation)o,
+             parent, name )
 {
-  _orientation = o;
-  setRange( _minValue, _maxValue );
-  setSteps( _lineStep, _pageStep );
+  setBackgroundMode(PaletteBackground);
 }
 
 
-void KSlider::rangeChange()
+
+void KSlider::drawShadeLine( QPainter *painter )
 {
-  // Necessary for range changes after creation time
-  valueChange();
-  repaint( true );
+  if ( orientation() == Vertical )
+    qDrawShadeLine(painter, 5, 3, 5, height()-3, colorGroup(), true, 1, 2);
+  else
+    qDrawShadeLine(painter, 3, 5, width()-3, 5, colorGroup(), true, 1, 2);
 }
 
-void KSlider::valueChange()
+
+void KSlider::paintSlider(QPainter *painter, const QRect &rect )
 {
-  QPainter painter;
   QPoint pos;
 
-  painter.begin( this );
-
   pos = calcArrowPos( prevValue() );
-  drawArrow( painter, FALSE, pos );
+  drawArrow( painter, false, pos );
 
-  if ( orientation() == Vertical )
-	{
-	  qDrawShadeLine(&painter, 5, 3, 5, height()-3, colorGroup(), TRUE, 1, 2);
-	}
-  else
-	{
-	  qDrawShadeLine(&painter, 3, 5, width()-3, 5, colorGroup(), TRUE, 1, 2);
-	}
+  drawShadeLine(painter);
 
   pos = calcArrowPos( value() );
-  drawArrow( painter, TRUE, pos );
-
-  painter.end();
+  drawArrow( painter, true, pos );
 }
 
-
-void KSlider::drawArrow( QPainter &painter, bool show, const QPoint &pos )
+void KSlider::drawArrow( QPainter *painter, bool show, const QPoint &pos )
 {
+  QPen        arrowPen;
   QPointArray array(5);
 
+  // Select Horizontal or Vertical Polygon Array
+  if ( orientation() == Vertical ) {
+    array.setPoint( 0, pos.x()+0, pos.y()+0 );
+    array.setPoint( 1, pos.x()-4, pos.y()+4 );
+    array.setPoint( 2, pos.x()-ARROW_LENGTH, pos.y()+4 );
+    array.setPoint( 3, pos.x()-ARROW_LENGTH, pos.y()-4 );
+    array.setPoint( 4, pos.x()-4, pos.y()-4 );
+  }
+  else {
+    array.setPoint( 0, pos.x()+0, pos.y()+0 );
+    array.setPoint( 1, pos.x()+4, pos.y()-4 );
+    array.setPoint( 2, pos.x()+4, pos.y()-ARROW_LENGTH );
+    array.setPoint( 3, pos.x()-4, pos.y()-ARROW_LENGTH );
+    array.setPoint( 4, pos.x()-4, pos.y()-4 );
+  }
+
+  // Select a base pen, then change parameters
   if ( show )
-	{
-	  painter.setPen( QPen( colorGroup().light() ) );
-	  painter.setBrush( backgroundColor() );
-	}
+    arrowPen = QPen( colorGroup().light() );
   else
-	{
-	  painter.setPen( QPen( backgroundColor() ) );
-	  painter.setBrush( backgroundColor() );
-	}
+    arrowPen = QPen( backgroundColor() );
 
-  if ( orientation() == Vertical )
-	{
-	  array.setPoint( 0, pos.x()+0, pos.y()+0 );
-	  array.setPoint( 1, pos.x()-4, pos.y()+4 );
-	  array.setPoint( 2, pos.x()-ARROW_LENGTH, pos.y()+4 );
-	  array.setPoint( 3, pos.x()-ARROW_LENGTH, pos.y()-4 );
-	  array.setPoint( 4, pos.x()-4, pos.y()-4 );
-	}
-  else
-	{
-	  array.setPoint( 0, pos.x()+0, pos.y()+0 );
-	  array.setPoint( 1, pos.x()+4, pos.y()-4 );
-	  array.setPoint( 2, pos.x()+4, pos.y()-ARROW_LENGTH );
-	  array.setPoint( 3, pos.x()-4, pos.y()-ARROW_LENGTH );
-	  array.setPoint( 4, pos.x()-4, pos.y()-4 );
-	}
+  arrowPen.setWidth(1);		// Yup, we REALLY want width 1, not 0 here !!
+  painter->setPen(arrowPen);
+  painter->setBrush( backgroundColor() );
+  // painter->setRasterOp ( CopyROP );
 
-  painter.drawPolygon( array );
+  painter->drawPolygon( array );
 
   if ( show )
-	{
-	  painter.setPen( QPen( colorGroup().dark() ) );
-	  painter.drawLine( array[0], array[1] );
-	  painter.drawLine( array[1], array[2] );
-	}
+    {
+      arrowPen = QPen( colorGroup().dark() );
+      arrowPen.setWidth(1);	// Yup, we REALLY want width 1, not 0, here !!
+      painter->setPen(arrowPen);
+      painter->drawPolyline( array, 0, 3);
+
+      // !!! This fixes a problem with a missing point! Qt Bug?
+      // !!! I will wait for Qt1.3 to see the results
+      //painter.drawPoint(array[1]);
+    }
 }
+
 
 QSize KSlider::sizeHint() const
 {
   QSize size;
 
-  if ( orientation() == Vertical )
-	{
-	  size.setWidth( ARROW_LENGTH + 5 + 1 );
-	  size.setHeight( 50 );
-	}
-  else
-	{
-	  size.setWidth( 50 );
-	  size.setHeight( ARROW_LENGTH + 5 + 1 );
-	}
-
+  if ( orientation() == Vertical ) {
+    size.setWidth( ARROW_LENGTH + 5 + 1 );
+    size.setHeight( -1 );
+  }
+  else {
+    size.setWidth( -1 );
+    size.setHeight( ARROW_LENGTH + 5 + 1 );
+  }
   return size;
 }
+
+
 
 QPoint KSlider::calcArrowPos( int val )
 {
   QPoint p;
-  int diffMaxMin = checkWidth();  
+  int diffMaxMin = checkWidth();	// sanity check, Christian Esken
 
-  if ( orientation() == Vertical )
-	{
-	  p.setY( height() - ( (height()-10) * ( val - minValue() )
-						   / diffMaxMin + 5 ) );
-	  p.setX( ARROW_LENGTH );
-	}
-  else
-	{
-	  p.setX( ( (width()-10) * ( val - minValue() )
-				/ diffMaxMin + 5 ) );
-	  p.setY( ARROW_LENGTH );
-	}
-
+  if ( orientation() == Vertical ) {
+    p.setY( height() - ( (height()-10) * ( val - minValue() )
+				  / diffMaxMin + 5 ) );
+    p.setX( ARROW_LENGTH );
+  }
+  else {
+    p.setX( ( (width()-10) * ( val - minValue() )
+	      / diffMaxMin + 5 ) );
+    p.setY( ARROW_LENGTH );
+  }
   return p;
 }
 
-void KSlider::moveArrow( const QPoint &pos )
+
+void KSlider::drawTickMarks(QPainter *painter)
 {
-  int val;
+  QPen tickPen = QPen( colorGroup().dark() );
+  tickPen.setWidth(1);	// Yup, we REALLY want width 1, not 0 here !!
+  painter->setPen(tickPen);
 
-  if ( orientation() == Vertical )
-	val = ( maxValue() - minValue() ) * (height()-pos.y()-3)
-	  / (height()-10) + minValue();
-  else
-	val = ( maxValue() - minValue() ) * (pos.x()-3)
-	  / (width()-10) + minValue();
+  int i;
+  int diffMaxMin = checkWidth();	// sanity check
+  if ( orientation() == Vertical ) {
+    // first clear the tickmark area
+    painter->fillRect(ARROW_LENGTH+1,0, ARROW_LENGTH + 6, height()-1, colorGroup().background()  );
 
-  if ( val > maxValue() )
-	val = maxValue();
-  if ( val < minValue() )
-	val = minValue();
-
-  emit valueChanged( val );
-  setValue( val );
+    // draw ruler marks
+    for ( i = 0; i <= maxValue() - minValue(); i += lineStep() ) {
+      int pos = (height()-10) * i / diffMaxMin + 5;
+      painter->drawLine( ARROW_LENGTH+1, pos, ARROW_LENGTH + 4, pos );
+    }
+    for ( i = 0; i <= maxValue() - minValue(); i += pageStep() ) {
+      int pos = (height()-10) * i / diffMaxMin + 5;
+      painter->drawLine( ARROW_LENGTH+1, pos, ARROW_LENGTH + 6, pos );
+    }
+  }
+  else {
+    // first clear the tickmark area
+    painter->fillRect(0, ARROW_LENGTH+1, width()-1, ARROW_LENGTH + 6, colorGroup().background() );
+    // draw ruler marks
+    for ( i = 0; i <= maxValue() - minValue(); i += lineStep() ) {
+      int pos = (width()-10) * i / diffMaxMin + 5;
+      painter->drawLine( pos, ARROW_LENGTH+1, pos, ARROW_LENGTH + 4 );
+    }
+    for ( i = 0; i <= maxValue() - minValue(); i += pageStep() ) {
+      int pos = (width()-10) * i / diffMaxMin + 5;
+      painter->drawLine( pos, ARROW_LENGTH+1, pos, ARROW_LENGTH + 6 );
+    }
+  }
 }
 
 void KSlider::paintEvent( QPaintEvent * )
 {
   QPainter painter;
-  int i = 0;
 
-  int diffMaxMin = checkWidth(); //sanity check
   painter.begin( this );
-
-  if ( orientation() == Vertical )
-	{
-	  qDrawShadeLine(&painter, 5, 3, 5, height()-3, colorGroup(), TRUE, 1, 2);
-
-	  // draw ruler marks
-	  for ( i = 0; i <= maxValue() - minValue(); i += lineStep() )
-		{
-		  int pos = (height()-10) * i / diffMaxMin + 5;
-		  painter.drawLine( ARROW_LENGTH+1, pos, ARROW_LENGTH + 4, pos );
-		}
-
-	  for ( i = 0; i <= maxValue() - minValue(); i += pageStep() )
-		{
-		  int pos = (height()-10) * i / diffMaxMin + 5;
-		  painter.drawLine( ARROW_LENGTH+1, pos, ARROW_LENGTH + 6, pos );
-		}
-	}
-  else
-	{
-	  qDrawShadeLine(&painter, 3, 5, width()-3, 5, colorGroup(), TRUE, 1, 2);
-
-	  // draw ruler marks
-	  for ( i = 0; i <= maxValue() - minValue(); i += lineStep() )
-		{
-		  int pos = (width()-10) * i / diffMaxMin + 5;
-		  painter.drawLine( pos, ARROW_LENGTH+1, pos, ARROW_LENGTH + 4 );
-		}
-
-	  for ( i = 0; i <= maxValue() - minValue(); i += pageStep() )
-		{
-		  int pos = (width()-10) * i / diffMaxMin + 5;
-		  painter.drawLine( pos, ARROW_LENGTH+1, pos, ARROW_LENGTH + 6 );
-		}
-	}
-
-  drawArrow( painter, TRUE, calcArrowPos( value() ) );
-
-
+  QRect rect(x(),y(),width(),height());
+  paintSlider(&painter, rect);
+  drawTickMarks(&painter);
   painter.end();
 }
 
-void KSlider::mousePressEvent( QMouseEvent *e )
+void KSlider::rangeChange()
 {
-  moveArrow( e->pos() );
-}
-
-void KSlider::mouseMoveEvent( QMouseEvent *e )
-{
-  moveArrow( e->pos() );
+  QSlider::rangeChange();
+  // when range changes, everything must be repainted  
+  paintEvent(NULL);
 }
 
 int KSlider::checkWidth()
 {
   int diff = maxValue() - minValue();
-  if( diff == 0 )
-	// If (max - min) has no "secure" value, set it to
-	// lineStep(). Christian Esken. 
-	diff = lineStep();
+  if ( diff == 0)
+    // If (max - min) has no "secure" value, set it to lineStep().
+    diff=lineStep();
   return diff;
 }
-
-
-
--- kdeui/kslider.h
+++ kdeui/kslider.h
@@ -1,75 +1,76 @@
 //-----------------------------------------------------------------------------
 // Slider control
 //
-// Copyright (C) Martin R. Jones 1997
-//
-
+// V1.0 Copyright (C) Martin R. Jones 1997
+// V1.1 and following: Now maintained by Christian Esken <chris@magicon.prima.ruhr.de>
 
 #ifndef __KSLIDER_H__
 #define __KSLIDER_H__
 
-#include <qrangect.h>
-#include <qwidget.h>
+// undef Above+Below because of Qt <-> X11 collision. Grr, I hate X11 headers
+#undef Above
+#undef Below
+#include <qslider.h>
 
 /// Control for selecting a value from using a sliding pointer.
 /**
-   KSlider is a control for selecting a value from using a sliding pointer.
-   It can be setup similarly to a scrollbar
-   Calling setSteps( line, page ) sets up the ruler marks along the slider.
-   e.g.
+KSlider is a control for selecting a value from using a sliding pointer.
+It can be setup similarly to a scrollbar
+Calling setSteps( line, page ) sets up the ruler marks along the slider.
+e.g.
 
-   KSlider slider( KSlider::Horizontal, parent );
+KSlider slider( KSlider::Horizontal, parent );
 
-   slider.setRange( 0, 20 );
-   slider.setSteps( 5, 10 );
+slider.setRange( 0, 20 );
+slider.setSteps( 5, 10 );
 
-   will create a horizontal slider with a range of 0 to 20 and small ruler
-   marks spaced 5 apart and long ruler marks placed 10 apart.
+will create a horizontal slider with a range of 0 to 20 and small ruler
+marks spaced 5 apart and long ruler marks placed 10 apart.
 
-   TODO
+TODO
 
-   Currently clicking any where in the control moves the slider to that position.
-   This should be changed so that the slider may be grabbed and moved, or an
-   area on the left or right of the control clicked to move the slider in
-   lineStep() increments.
+Currently clicking any where in the control moves the slider to that position.
+This should be changed so that the slider may be grabbed and moved, or an
+area on the left or right of the control clicked to move the slider in
+lineStep() increments.
 */
-class KSlider : public QWidget, public QRangeControl
+class KSlider : public QSlider
 {
   Q_OBJECT
-public:
+    public:
   enum Orientation { Horizontal, Vertical };
 
+  KSlider( QWidget *parent = NULL, const char *name = NULL );
   KSlider( Orientation o, QWidget *parent = NULL, const char *name = NULL );
-  KSlider( int _minValue, int _maxValue, int _lineStep, int _pageStep,
-		   Orientation o, QWidget *parent = NULL, const char *name = NULL );
+  KSlider( int _minValue, int _maxValue, int _Step, int _value,
+	   Orientation o, QWidget *parent = NULL, const char *name = NULL );
 
-  void setOrientation( Orientation o )
-	{	_orientation = o; }
-  Orientation orientation() const
-	{	return _orientation; }
-
-  signals:
-	void valueChanged( int value );
-
-protected:
-  virtual void valueChange();
-  virtual void rangeChange();
-  virtual void drawArrow( QPainter &painter, bool show, const QPoint &pos );
   virtual QSize sizeHint() const;
 
+signals:
+  void valueChanged( int value );
+  void sliderPressed();
+  void sliderMoved( int value );
+  void sliderReleased();
+
+protected:
+  //  virtual void valueChange();
+  virtual void rangeChange();
+  virtual void paintSlider(QPainter *painter, const QRect &rect );
 private:
   QPoint calcArrowPos( int val );
   void moveArrow( const QPoint &pos );
+  void drawArrow( QPainter *painter, bool show, const QPoint &pos );
+  void drawShadeLine( QPainter *painter );
+  void drawTickMarks( QPainter *painter );
 
   virtual void paintEvent( QPaintEvent * );
-  virtual void mousePressEvent( QMouseEvent *e );
-  virtual void mouseMoveEvent( QMouseEvent *e );
 
-protected:
-  Orientation _orientation;
+//  virtual void mousePressEvent( QMouseEvent *e );
+//  virtual void mouseMoveEvent( QMouseEvent *e );
 
-private:
   int checkWidth();
+//  int cur_slider_pos;
 };
 
 //-----------------------------------------------------------------------------
