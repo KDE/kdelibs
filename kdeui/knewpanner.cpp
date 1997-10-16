4c70349450494a6801c8272714c99b0fcdccc062
Richard J. Moore <rich@kde.org>
1997-10-16 10:00:57 +0000

    Added KNewPanner
    svn path=/trunk/kdelibs/; revision=2074

---
+++ kdeui/knewpanner.cpp
@@ -0,0 +1,287 @@
+// $Id$
+
+#include "knewpanner.h"
+#include <stdio.h>
+
+KNewPanner::KNewPanner(QWidget *parent= 0, const char *name= 0,
+			   Orientation orient= Vertical, Units units= Percent, int pos= 50)
+    : QWidget(parent, name)
+{
+    orientation= orient;
+    currentunits= units;
+    position= pos;
+    initialised= false;
+    showlabels= false;
+    startHeight= 0;
+
+    label0= 0;
+    label1= 0;
+}
+
+KNewPanner::~KNewPanner()
+{
+}
+
+void KNewPanner::activate(QWidget *c0, QWidget *c1)
+{
+    int minx, maxx, miny, maxy, pos;
+
+    child0= c0;
+    child1= c1;
+
+    // Set the minimum and maximum sizes
+    if (orientation == Horizontal) {
+	miny= c0->minimumSize().height() + c1->minimumSize().height()+4;
+	maxy= c0->maximumSize().height() + c1->maximumSize().height()+4;
+	minx= (c0->minimumSize().width() > c1->minimumSize().width())
+	    ? c0->minimumSize().width() : c1->minimumSize().width();
+	maxx= (c0->maximumSize().width() > c1->maximumSize().width())
+	    ? c0->maximumSize().width() : c1->maximumSize().width();
+
+	miny= (miny > 4) ? miny : 4;
+	maxy= (maxy < 2000) ? maxy : 2000;
+	minx= (minx > 2) ? minx : 2;
+	maxx= (maxx < 2000) ? maxx : 2000;
+
+	setMinimumSize(minx, miny);
+	setMaximumSize(maxx, maxy);
+    }
+    else {
+	minx= c0->minimumSize().width() + c1->minimumSize().width()+4;
+	maxx= c0->maximumSize().width() + c1->maximumSize().width()+4;
+	miny= (c0->minimumSize().height() > c1->minimumSize().height())
+	    ? c0->minimumSize().height() : c1->minimumSize().height();
+	maxy= (c0->maximumSize().height() > c1->maximumSize().height())
+	    ? c0->maximumSize().height() : c1->maximumSize().height();
+
+	minx= (minx > 4) ? minx : 4;
+	maxx= (maxx < 2000) ? maxx : 2000;
+	miny= (miny > 2) ? miny : 2;
+	maxy= (maxy < 2000) ? maxy : 2000;
+
+	setMinimumSize(minx, miny);
+	setMaximumSize(maxx, maxy);
+    }
+
+    divider= new QFrame(this, "pannerdivider");
+    divider->setFrameStyle(QFrame::Panel | QFrame::Raised);
+    divider->setLineWidth(1);
+
+    if (orientation == Horizontal)
+        divider->setCursor(QCursor(sizeVerCursor));
+    else
+        divider->setCursor(QCursor(sizeHorCursor));
+
+    divider->installEventFilter(this);
+
+    initialised= true;
+
+    pos= position;
+    position=0;
+
+    setSeperatorPos(pos);
+}
+
+void KNewPanner::setLabels(const char *text0, const char *text1)
+{
+   if (label0 == 0) {
+      label0= new QLabel(this, "label0");
+      label0->setFrameStyle(QFrame::Sunken | QFrame::Panel);
+   }
+   if (label1 == 0) {
+      label1= new QLabel(this, "label1");
+      label1->setFrameStyle(QFrame::Sunken | QFrame::Panel);
+   }
+
+   label0->setText(text0);
+   label1->setText(text1);
+   label0->adjustSize();
+   label1->adjustSize();
+
+   if (!showlabels) {
+      label0->hide();
+      label1->hide();
+   }
+      
+   if (showlabels && initialised) {
+      resizeEvent(0);
+   }
+}
+
+void KNewPanner::showLabels(bool newval)
+{
+  if (orientation == Vertical)
+    showlabels= newval;
+
+   if ((label0 == 0) || (label1 == 0))
+     setLabels("", "");
+
+   if (showlabels) {
+      label0->show();
+      label1->show();
+   }
+   else {
+      label0->hide();
+      label1->hide();
+   }
+
+  if (initialised && showlabels) {
+     label0->adjustSize();
+     label1->adjustSize();
+  }
+  if (initialised)
+     resizeEvent(0);
+}
+
+int KNewPanner::seperatorPos()
+{
+    int value;
+
+    if (currentunits == Percent)
+	value= (int) ((position / (double) width()) * 100);
+    else
+	value= position;
+	    
+    return value;
+}
+
+void KNewPanner::setSeperatorPos(int pos)
+{
+    if (currentunits == Percent)
+	setAbsSeperatorPos((int) (pos/100.0 * width()));
+    else
+	setAbsSeperatorPos(pos);
+}
+
+void KNewPanner::setAbsSeperatorPos(int pos)
+{
+    pos= checkValue(pos);
+
+    if (pos != position) {
+	position= pos;
+	resizeEvent(0);
+    }
+}
+
+int KNewPanner::absSeperatorPos()
+{
+    return position;
+}
+
+KNewPanner::Units KNewPanner::units()
+{
+    return currentunits;
+}
+
+void KNewPanner::setUnits(Units u)
+{
+    currentunits= u;
+}
+
+void KNewPanner::resizeEvent(QResizeEvent*)
+{
+  if (initialised) {
+     if (orientation == Horizontal) {
+      child0->setGeometry(0, 0, width(), position);
+      child1->setGeometry(0, position+4, width(), height()-position-4);
+      divider->setGeometry(0, position, width(), position+4);
+    }
+    else {
+      if (showlabels) {
+	label0->move(0,0);
+	label0->resize(position, label0->height());
+	label1->move(position+4, 0);
+	label1->resize(width()-position-4, label1->height());
+	startHeight= label0->height();
+      }
+      else {
+	 startHeight= 0;
+      }
+      child0->setGeometry(0, startHeight, position, (height())-startHeight);
+      child1->setGeometry(position+4, startHeight,
+			  (width())-(position+4), (height())-startHeight);
+      divider->setGeometry(position, startHeight, 4, (height())-startHeight);
+    }
+  }
+}
+
+int KNewPanner::checkValue(int pos)
+{
+   if (initialised) {
+    if (orientation == Vertical) {
+	if (pos < (child0->minimumSize().width()))
+	    pos= child0->minimumSize().width();
+	if ((width()-4-pos) < (child1->minimumSize().width()))
+	    pos= width() - (child1->minimumSize().width()) -4;
+    }
+    else {
+	if (pos < (child0->minimumSize().height()))
+	    pos= (child0->minimumSize().height());
+	if ((height()-4-pos) < (child1->minimumSize().height()))
+	    pos= height() - (child1->minimumSize().height()) -4;
+    }
+   }
+   
+    if (pos < 0) pos= 0;
+
+   if ((orientation == Vertical) && (pos > width()))
+     pos= width();
+   if ((orientation == Horizontal) && (pos > height()))
+     pos= height();
+
+    return pos;
+}
+
+bool KNewPanner::eventFilter(QObject *, QEvent *e)
+{
+    QMouseEvent *mev;
+    bool handled= false;
+
+    switch (e->type()) {
+    case Event_MouseMove:
+	mev= (QMouseEvent *)e;
+	child0->setUpdatesEnabled(false);
+	child1->setUpdatesEnabled(false);
+	if (orientation == Horizontal) {
+	    position= checkValue(position+mev->y());
+	    divider->setGeometry(0, position, width(), 4);
+	    divider->repaint(0);
+	}
+	else {
+	    position= checkValue(position+mev->x());
+	    divider->setGeometry(position, 0, 4, height());
+	    divider->repaint(0);
+	}
+	handled= true;
+	break;
+    case Event_MouseButtonRelease:
+	mev= (QMouseEvent *)e;
+
+	child0->setUpdatesEnabled(true);
+	child1->setUpdatesEnabled(true);
+
+	if (orientation == Horizontal) {
+	    setAbsSeperatorPos(absSeperatorPos());
+	    resizeEvent(0);
+	    child0->repaint(true);
+	    child1->repaint(true);
+	    divider->repaint(true);
+	}
+	else {
+	    setAbsSeperatorPos(absSeperatorPos());
+	    resizeEvent(0);
+	    child0->repaint(true);
+	    child1->repaint(true);
+	    divider->repaint(true);
+	}
+	handled= true;
+	break;
+    }
+
+    return handled;
+}
+
+#include "knewpanner.moc"
+
+
+
---
+++ kdeui/knewpanner.h
@@ -0,0 +1,164 @@
+// -*- c++ -*-
+
+#ifndef KNEWPANNER_H
+#define KNEWPANNER_H
+
+#include <qwidget.h>
+#include <qframe.h>
+#include <qlabel.h>
+
+/**
+ * KNewPanner is a simple widget for managing two children which
+ * are seperated by a draggable divider. The user can resize both
+ * children by moving the bar. You can also label the two children
+ * and the labels will be adjusted as the divider is moved.
+ *
+ * This widget is considerably easier to use than the old one, simply
+ * set the minimum and maximum sizes of the two children then call
+ * activate(). Note that the widgets to be panned should be children
+ * of the panner widget.
+ *
+ * This widget fixes a number of design
+ * problems in the original KPanner class which show up particularly
+ * if you try to use it under a layout manager (such as QBoxLayout).
+ *
+ * PLEASE NOTE: This panner is NOT source or binary compatable with
+ * the old one.
+ *
+ * @version $Id$
+ * @author Richard Moore rich@kde.org
+ */
+class KNewPanner : public QWidget
+{
+    Q_OBJECT
+
+public:
+    /**
+     * Constants used to specify the orientation.
+     */
+    enum Orientation { Vertical, Horizontal };
+
+    /**
+     * Constants used to choose between absolute (pixel) sizes and
+     * percentages.
+     */
+    enum Units { Percent, Absolute };
+
+    /**
+     * Construct a KNewPanner widget.
+     *
+     * @param parent  The parent widget
+     * @param name  The name of the panner widget.
+     * @param orient  The orientation of the panner, one of the constants
+     *   Vertical or Horizontal.
+     * @param units  The units in which you want to specify the seperator position. This must be one of the constants Percent or Absolute.
+     * @param pos  The initial seperator position in the selected units.
+     */
+    KNewPanner(QWidget *parent= 0, const char *name= 0,
+		 Orientation orient= Vertical, Units units= Percent, int pos= 50);
+
+    /**
+     * Clean up
+     */
+    virtual ~KNewPanner();
+
+    /**
+     * Begin managing these two widgets. If you want to set the minimum or
+     * maximum sizes of the children then you should do it before calling this
+     * method.
+     */
+    void activate(QWidget *c0, QWidget *c1);
+
+  /**
+   * Set the label texts for the two windows, note that they are only shown
+   * if you have called showLabels(true).
+   */
+  void setLabels(const char *text0, const char *text1);
+
+  /**
+   * Call this to show or hide the labels. Note that labels are only
+   * supported when the panner is oriented vertically. If it is not
+   * then calling this method has no effect.
+   */
+  void showLabels(bool);
+
+    /**
+     * This gets the current position of the seperator in the current units.
+     */
+    int seperatorPos();
+
+    /**
+     * This sets the position of the seperator to the specified position. The position
+     * is specified in the currently selected units.
+     */
+    void setSeperatorPos(int pos);
+
+    /**
+     * This gets the current position of the seperator in absolute 
+     * (pixel) units.
+     */
+    int absSeperatorPos();
+
+    /**
+     * This sets the position of the seperator to the specified position.
+     * The position is specified in absolute units (pixels) irrespective
+     * of the the currently selected units.
+     */
+    void setAbsSeperatorPos(int pos);
+
+    /**
+     * Get the current units.
+     */
+    Units units();
+
+    /**
+     * Set the current units.
+     */
+    void setUnits(Units);
+
+protected:
+    /**
+     * This returns the closest valid absolute seperator position to the
+     * position specified in the parameter.
+     */
+    int checkValue(int);
+
+    /**
+     * This method handles changes in the panner size, it will automatically resize
+     * the child widgets as appropriate.
+     */
+    void resizeEvent(QResizeEvent *);
+
+private:
+  // The managed children
+  QWidget *child0, *child1;
+
+  // Labels of the children
+  QLabel *label0, *label1;
+
+  // The height at which the children start
+  int startHeight;
+
+  // Have we started yet?
+  bool initialised;
+
+  // Should we show labels?
+  bool showlabels;
+
+    // The divider widget
+    QFrame *divider;
+
+    bool eventFilter(QObject *, QEvent *);
+
+    // The position in pixel units
+    int position;
+    Units currentunits;
+    Orientation orientation;
+};
+
+#endif
+
+
+
+
+
