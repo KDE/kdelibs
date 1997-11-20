2c69d93cccc8d7c78228df5a8d1d1adc56d2dd03
Stephan Kulow <coolo@kde.org>
1997-11-20 20:30:46 +0000

    burned the KDE FSSTND paths into config.h. The rest I mail Kalle as soon as it is finished.
    svn path=/trunk/kdelibs/; revision=2940

--- acconfig.h
+++ acconfig.h
@@ -35,3 +35,41 @@
 /* Define if you have usleep */
 #undef HAVE_USLEEP
 
+/* This is the prefix of the below paths. This may change in the future */
+#undef KDEDIR
+
+/* Where your docs should go to. */
+#undef KDE_HTMLDIR
+
+/* Where your application file (.kdelnk) should go to. */
+#undef KDE_APPSDIR
+
+/* Where your icon should go to. */
+#undef KDE_ICONDIR
+
+ /* Where your mini icon should go to. */
+#undef KDE_MINIDIR
+
+/* Where you install application data. */
+#undef KDE_DATADIR
+
+/* Where translation files should go to. */
+#undef KDE_LOCALE
+
+/* Where cgi-bin executables should go to. */
+#undef KDE_CGIDIR
+
+/* Where mimetypes should go to. */
+#undef KDE_MIMEDIR
+
+/* Where mimetypes should go to. */
+#undef KDE_TOOLBARDIR
+
+/* Where general wallpapers should go to. */
+#undef KDE_WALLPAPERDIR
+
+/* Where binaries should be installed. */
+#undef KDE_BINDIR
+
+/* Where KDE parts should be installed. */
+#undef KDE_PARTSDIR
