020412dc7680ebf6516ca440f9226ca5b96e9952
Stephan Kulow <coolo@kde.org>
1997-12-28 21:26:09 +0000

    added texts.cpp, where all the standard texts I added, are listed. It's easier to maintain. Kalle: if you feel to, go into kdelibs and "make messages" ;)
    svn path=/trunk/kdelibs/; revision=3684

---
+++ po/texts.cpp
@@ -0,0 +1,46 @@
+/* This file is part of the KDE libraries
+    Copyright (C) 1997 Stephan Kulow (coolo@kde.org)
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+    Boston, MA 02111-1307, USA.
+*/
+
+/* This file contains several often used phrases to avoid double
+   work. If you think, that a phrase belongs here, contact me 
+*/
+
+translate( "Error" );
+translate( "Malformed URL");
+translate( "Charset:");
+translate( "Yes");
+translate( "No");
+translate( "Warning");
+translate( "Open a file");
+translate( "Save a file");
+translate( "Contents");
+translate( "About");
+translate( "Open");
+translate( "Save");
+translate( "Save As");
+translate( "Close");
+translate( "Quit");
+translate( "Untitled");
+translate( "OK");
+translate( "Ok");
+translate( "Cancel");
+translate( "Apply");
+translate( "File");
+translate( "Help");
+translate( "Edit");
