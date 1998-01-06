3358995732564f842345e1993ba9b6cf257f36da
Stephan Kulow <coolo@kde.org>
1998-01-06 21:19:18 +0000

    added ksprintf, a function replaces QString::sprintf, if you're not sure about the length of your result. Currently it just looks for %s and adds space to the result
    svn path=/trunk/kdelibs/; revision=3953

--- kdecore/kstring.cpp
+++ kdecore/kstring.cpp
@@ -1,4 +1,6 @@
 #include "kstring.h"
+#include <stdio.h>  
+#include <stdarg.h>
 
 QString& operator<<( QString& _str, short _v )
 {
@@ -68,4 +70,40 @@ QString& operator<<( QString& _str, const char* _v )
 {
     _str += _v;
     return _str;
+}
+
+void ksprintf(QString *str, const char *fmt, ...)
+{
+    va_list ap;
+    va_start( ap, fmt );
+
+    int i = 0;
+    int len = 255; // default is the same as in QString::sprintf
+    int dummy; // this ugly hack is just to avoid warnings
+
+    while (fmt[i+1]) {
+
+	if (fmt[i] == '%' && fmt[i+1] != '%' ) { // argument
+	    if (fmt[i+1] == 's') {
+		const char *str = va_arg( ap, const char*);
+		len += strlen(str);
+	    } else
+		dummy = va_arg (ap, int); // take it out of the arg list
+	} else // jump over "%%"
+	    if (fmt[i] == '%' && fmt[i+1] == '%') i++;
+
+	i++; 
+    }
+
+    va_end( ap); // oh god, what I hope, that restarting is portable
+
+    char *tmp_data = new char[len]; // make enough space
+
+    va_start( ap, fmt ); // the rest does the libc for us
+    vsprintf( tmp_data, fmt, ap );
+    va_end( ap );
+
+    *str = tmp_data; // makes deep copy, that is also resized
+    
+    delete tmp_data; // clear the heap
 }
--- kdecore/kstring.h
+++ kdecore/kstring.h
@@ -13,4 +13,12 @@ QString& operator<<( QString&, float );
 QString& operator<<( QString&, double );
 QString& operator<<( QString&, const char* );
 
+/* replaces QString::sprintf, where it's not sure, that 256
+   bytes are enough for the resulting string.
+   
+   ksprintf adds to the 256 default bytes the len of every
+   used string. 
+   */
+void ksprintf(QString *str, const char *fmt, ...);
+
 #endif
