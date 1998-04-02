d5cf0173184a0b79db1fa013b6b2b4ccbb33706e
Stephan Kulow <coolo@kde.org>
1998-04-02 23:33:42 +0000

    first attempt at documentation ;(
    svn path=/trunk/kdelibs/; revision=6474

--- kfile/kfileinfocontents.h
+++ kfile/kfileinfocontents.h
@@ -27,55 +27,264 @@ class QSignal;
 #include <qwidget.h>
 #include "kdir.h"
 
+/**
+  * A base class for views of the KDE file selector
+  *
+  * This class defines an interface to all file views. It's intent is 
+  * to allow to switch the view of the files in the selector very easily.
+  * It defines some pure virtual functions, that must be implemented to
+  * make a file view working.
+  *
+  * Since this class is not a widget, but it's meant to be added to other 
+  * widgets, its most important function is @code widget. This should return
+  * a pointer to the implemented widget.
+  *
+  * @short A base class for views of the KDE file selector
+  * @author Stephan Kulow <coolo@kde.org>
+  * @version $Id$
+  **/
 class KFileInfoContents {
     
 public:
+    /** 
+      * @short Constructor. Defines some used flags and lists
+      * It loads the icons used to indicate files and folders in
+      * all views, initializes some used lists and variables 
+      *
+      * @param useSingleClick defines, if the view should react on
+      *        single click or double click
+      * @param sorting defines, how the list should be sorted. 
+      *        Currently Name, Size and Date are supported.
+      **/
     KFileInfoContents( bool useSingleClick, QDir::SortSpec sorting );
+
+    /** Destructor */
     virtual ~KFileInfoContents();
     
-    bool addItem(const KFileInfo *);
-    virtual const char *text(uint) const;
+    /** 
+      * Insert an item into the file view. The view will just maintain
+      * the pointer. It doesn't free the object.
+      *
+      * This functions inserts the file at the correct place in the
+      * sorted list
+      * @return true, if a repaint is necessary
+      * @param item the file to be inserted
+      **/
+    bool addItem(const KFileInfo *item);
+
+    /**
+      * @return the name of the file at position index. 
+      * 
+      * the meaning of index depends on the sorting and the other files
+      * in the view. So use this function with care.
+      **/
+    virtual const char *text(uint index) const;
+
+    /**
+      * a pure virtual function to get a QWidget, that can be added to
+      * other widgets. This function is needed to make it possible for
+      * derived classes to derive from other widgets.
+      **/
     virtual QWidget *widget() = 0;
-    virtual void setCurrentItem(const char *filename, const KFileInfo * i = 0);
-    virtual void setAutoUpdate(bool) = 0;
+
+    /**
+      * set the current item in the view.
+      * 
+      * the normal use of this function is to use the parameter filename to
+      * set the highlight on this file.
+      *
+      * the extend use of this function is to set filename to 0. In this
+      * case the @see KFileInfo is used to find out the position of the
+      * file. Since this is faster, this way is used in case a view has
+      * other child views.
+      **/
+    virtual void setCurrentItem(const char *filename, 
+				const KFileInfo * entry = 0);
+    
+    /**
+      * set the autoUpdate flag for the specific widget. 
+      * if set to false, no repaints should be done automaticly unless
+      * repaint is called.
+      *
+      * this function is pure virtual, so it must be implemented
+      **/
+    virtual void setAutoUpdate(bool f) = 0;
+
+    /**
+      * clears all lists and calls clearView 
+      */
     void clear();
+
+    /**
+      * does a repaint of the view. 
+      *
+      * The default implementation calls 
+      * <code>widget()->repaint(f)</code>
+      **/
     virtual void repaint(bool f = true);
+
+    /**
+      * specifies the sorting of the internal list. Newly added files
+      * are added through this sorting.
+      *
+      **/
     QDir::SortSpec sorting() { return mySorting; }
+
+    /**
+      * set the sorting of the view. If the sorting is the same as
+      * the current value and sortMode is Switching, it switches the
+      * order in the list. 
+      *
+      * Default is QDir::Name
+      **/
     void setSorting(QDir::SortSpec sort); // a little bit complexer
+    
+    /**
+      * returns the number of added files 
+      **/
     uint count() const { return itemsList->count(); }
 
+    /**
+      * to connect a slot to the internal signal, that is emited, if
+      * the user selects a directory.
+      * 
+      * Before this signal is emited, the value for @ref selectedDir is
+      * set to the correct value.
+      **/
     void connectDirSelected( QObject *receiver, const char *member);
+
+    /**
+      * to connect a slot to the internal signal, that is emited, if
+      * the user highlights a file.
+      * 
+      * Before this signal is emited, the value for @ref highlightedFile 
+      * is set to the correct value.
+      **/
     void connectFileHighlighted( QObject *receiver, const char *member);
+
+    /**
+      * to connect a slot to the internal signal, that is emited, if
+      * the user selects a file.
+      * 
+      * Before this signal is emited, the value for @ref selectedFile is
+      * set to the correct value.
+      **/
     void connectFileSelected( QObject *receiver, const char *member);
 
+    /**
+      * @return the last selected directory
+      **/
     KFileInfo *selectedDir()     { return lastSDir;  }
+    
+    /**
+      * @return the last selected file
+      **/
     KFileInfo *selectedFile()    { return lastSFile; }
+
+    /**
+      * @return the last highlighted file
+      **/
     KFileInfo *highlightedFile() { return lastHFile; }
 
+    /**
+      * the number of files. 
+      **/
     uint numFiles() const { return filesNumber; }
+    
+    /**
+      * the number of directories
+      **/
     uint numDirs() const { return dirsNumber; }
 
+    /**
+      * tries to find a completed filename, that starts with base
+      *
+      * if it fails, it returns a null string. 
+      * @param activateFound if true, the file should select the 
+      *        found file (or directory)
+      **/
     virtual QString findCompletion( const char *base, 
 				    bool activateFound = false );
     
+    /**
+      * a pure virtual function to indicate, if the view should list
+      * files. 
+      **/
     virtual bool acceptsFiles() = 0;
+
+    /**
+      * a pure virtual function to indicate, if the view should list
+      * directories.
+      **/
     virtual bool acceptsDirs() = 0;
 
 protected:
+    
+    /**
+      * this function is called after the class found out the correct
+      * index. The derived view must implement this function to add
+      * the file in the widget. 
+      *
+      * it's important, that the view keeps it's contents consistent
+      * with the indicies in the sorted list of the base class
+      * @return true, if a repaint is necessary
+      **/
     virtual bool insertItem(const KFileInfo *i, int index) = 0;
-    bool addItemInternal(const KFileInfo *);
-    enum SortMode { Increasing, Decreasing, Switching };
-    void setSortMode(SortMode mode) { mySortMode = mode; }
-    virtual void clearView() = 0;
-    virtual void highlightItem(unsigned int item) = 0;
 
+    /**
+      * this function does the actual sorting. It is called by 
+      * addItem and calls itself insertItem 
+      **/
+    bool addItemInternal(const KFileInfo *);
+
+    /**
+      * Increasing means greater indicies means bigger values
+      *
+      * Decrease means greater indicies means smaller values
+      *
+      * Switching means, that it should switch between Increasing and
+      * Decreasing
+      **/
+    enum SortMode { Increasing, Decreasing, Switching };
+
+    /**
+      * set the sorting mode. Default mode is Increasing
+      **/
+    void setSortMode(SortMode mode) { mySortMode = mode; }
+
+    /**
+      * pure virtual function, that should be implemented to clear
+      * the view. At this moment the list is already empty
+      **/
+    virtual void clearView() = 0;
+
+    /**
+      * set the highlighted item to index. This function must be implemented
+      * by the view
+      **/
+    virtual void highlightItem(unsigned int index) = 0;
+
+    /** the pixmap for normal files */ 
     static QPixmap *file_pixmap;
+    /** the pixmap for locked files */
     static QPixmap *locked_file;
+    /** the pixmap for normal folders */
     static QPixmap *folder_pixmap;
+    /** the pixmap for locked folders */
     static QPixmap *locked_folder;
     
+    /** 
+      * sets the value for the selected file and emits the correct
+      * signal (depending on the type of the entry)
+      **/
+    void select( KFileInfo *entry);
+
+    /**
+      * gets the entry at index and calls the other select function
+      * with it
+      **/
     void select( int index );
-    void select( KFileInfo *);
+
     void highlight( int index );
     void highlight( KFileInfo *);
     
