03cfad7c9713735ef3d441648a8671117c021b14
Kalle Dalheimer <kalle@kde.org>
1998-07-24 10:53:49 +0000

    KTreeList widget now LGPL
    svn path=/trunk/kdelibs/; revision=9264

--- kdeui/ktreelist.h
+++ kdeui/ktreelist.h
@@ -8,15 +8,15 @@ KTreeList class declaration
 Copyright (C) 1996 Keith Brown and KtSoft
 
 This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
+it under the terms of the GNU Library General Public License as published by
 the Free Software Foundation; either version 2 of the License, or
 (at your option) any later version.
 
 This program is distributed in the hope that it will be useful, but
 WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABLILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-General Public License for more details. You should have received a
-copy of the GNU General Public License along with this program; if
+Library General Public License for more details. You should have received a
+copy of the GNU Library General Public License along with this program; if
 not, write to the Free Software Foundation, Inc, 675 Mass Ave, 
 Cambridge, MA 02139, USA.
 
