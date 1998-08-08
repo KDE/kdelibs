a0a0919fd9bc99584905d8c8a395f5a759e6c76a
Kalle Dalheimer <kalle@kde.org>
1998-08-08 07:50:15 +0000

    Added kruler
    svn path=/trunk/kdelibs/; revision=9740

---
+++ kdeui/kruler.cpp
@@ -0,0 +1,569 @@
+/* This file is part of the KDE libraries
+    Copyright (C) 1998 Jörg Habenicht (j.habenicht@europemail.com)
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+    Boston, MA 02111-1307, USA.
+*/
+
+#include "kruler.moc"
+
+#include <qpainter.h>
+#include <qdatetm.h>
+#include <qfont.h>
+
+#define INIT_VALUE 0
+#define INIT_MIN_VALUE 0
+#define INIT_MAX_VALUE 100
+#define INIT_LITTLE_MARK_DISTANCE 5
+#define INIT_MIDDLE_MARK_DISTANCE (INIT_LITTLE_MARK_DISTANCE * 2)
+#define INIT_BIG_MARK_DISTANCE (INIT_LITTLE_MARK_DISTANCE * 10)
+#define INIT_SHOW_LITTLE_MARK true
+#define INIT_SHOW_MEDIUM_MARK true
+#define INIT_SHOW_BIG_MARK true
+#define INIT_SHOW_END_MARK true
+
+#define INIT_PIXEL_PER_MARK (double)10.0 /* distance between 2 base marks in pixel */
+#define INIT_OFFSET (-20)
+
+#define FIX_WIDTH 20 /* widget width in pixel */
+#define LINE_END (FIX_WIDTH - 3)
+#define END_MARK_LENGTH (FIX_WIDTH - 6)
+#define END_MARK_X2 LINE_END
+#define END_MARK_X1 (END_MARK_X2 - END_MARK_LENGTH)
+#define BIG_MARK_LENGTH (END_MARK_LENGTH*3/4)
+#define BIG_MARK_X2 LINE_END
+#define BIG_MARK_X1 (BIG_MARK_X2 - BIG_MARK_LENGTH)
+#define MIDDLE_MARK_LENGTH (END_MARK_LENGTH/2)
+#define MIDDLE_MARK_X2 LINE_END
+#define MIDDLE_MARK_X1 (MIDDLE_MARK_X2 - MIDDLE_MARK_LENGTH)
+#define LITTLE_MARK_LENGTH (MIDDLE_MARK_LENGTH/2)
+#define LITTLE_MARK_X2 LINE_END
+#define LITTLE_MARK_X1 (LITTLE_MARK_X2 - LITTLE_MARK_LENGTH)
+#define BASE_MARK_LENGTH (LITTLE_MARK_LENGTH/2)
+#define BASE_MARK_X2 LINE_END
+#define BASE_MARK_X1 (BASE_MARK_X2 - BASE_MARK_LENGTH)
+
+#define LABEL_SIZE 8
+#define END_LABEL_X 4
+#define END_LABEL_Y (END_LABEL_X + LABEL_SIZE - 2)
+
+#undef PROFILING
+
+
+KRuler::KRuler(direction dir, QWidget *parent, const char *name,
+	       WFlags f, bool allowLines)
+  : QFrame(parent,name,f,allowLines),
+    dir(dir),
+    lmDist(INIT_LITTLE_MARK_DISTANCE),
+    mmDist(INIT_MIDDLE_MARK_DISTANCE),
+    bmDist(INIT_BIG_MARK_DISTANCE),
+    offset(INIT_OFFSET),
+    showlm(INIT_SHOW_LITTLE_MARK),
+    showmm(INIT_SHOW_MEDIUM_MARK),
+    showbm(INIT_SHOW_BIG_MARK),
+    showem(INIT_SHOW_END_MARK),
+    ppm(INIT_PIXEL_PER_MARK),
+    range(INIT_MIN_VALUE, INIT_MAX_VALUE, 1, 10, INIT_VALUE)
+{
+  setFrameStyle(WinPanel | Raised);
+
+  if (horizontal == dir) {
+    setFixedHeight(FIX_WIDTH);
+  }
+  else {
+    setFixedWidth(FIX_WIDTH);
+  }
+
+}
+
+KRuler::KRuler(direction dir, int widgetWidth, QWidget *parent, const char *name, WFlags f, bool allowLines)
+  : QFrame(parent,name,f,allowLines),
+    dir(dir),
+    lmDist(INIT_LITTLE_MARK_DISTANCE),
+    mmDist(INIT_MIDDLE_MARK_DISTANCE),
+    bmDist(INIT_BIG_MARK_DISTANCE),
+    offset(INIT_OFFSET),
+    showlm(INIT_SHOW_LITTLE_MARK),
+    showmm(INIT_SHOW_MEDIUM_MARK),
+    showbm(INIT_SHOW_BIG_MARK),
+    showem(INIT_SHOW_END_MARK),
+    ppm(INIT_PIXEL_PER_MARK),
+    range(INIT_MIN_VALUE, INIT_MAX_VALUE, 1, 10, INIT_VALUE)
+{
+  setFrameStyle(WinPanel | Raised);
+
+  if (horizontal == dir) {
+    setFixedHeight(widgetWidth);
+  }
+  else {
+    setFixedWidth(widgetWidth);
+  }
+
+}
+
+KRuler::~KRuler()
+{
+}
+
+void 
+KRuler::setMinValue(int value)
+{
+  if (range.minValue() != value) {
+    range.setRange( value, range.maxValue() );
+    update(contentsRect());
+  }
+}
+
+void 
+KRuler::setMaxValue(int value)
+{
+  if (range.maxValue() != value) {
+    range.setRange( range.minValue(), value );
+    update(contentsRect());
+  }
+}
+
+void 
+KRuler::setRange(int min, int max)
+{// debug("set range from %i to %i", min, max);
+  if ((range.minValue() != min) || (range.maxValue() != max)) {
+    range.setRange( min, max );
+    update(contentsRect());
+  }
+}
+
+void 
+KRuler::setValue(int value)
+{
+  range.setValue(value);
+  update(contentsRect());
+}
+
+void 
+KRuler::setTinyMarkDistance(int dist)
+{
+  if (dist != tmDist) {
+    tmDist = dist;
+    update(contentsRect());
+  }
+}
+
+void 
+KRuler::setLittleMarkDistance(int dist)
+{
+  if (dist != lmDist) {
+    lmDist = dist;
+    update(contentsRect());
+  }
+}
+
+void 
+KRuler::setMediumMarkDistance(int dist)
+{
+  if (dist != mmDist) {
+    mmDist = dist;
+    valuemm = valuelm * mmDist;
+    update(contentsRect());
+  }
+}
+
+void 
+KRuler::setBigMarkDistance(int dist)
+{
+  if (dist != bmDist) {
+    bmDist = dist;
+    valuebm = valuelm * bmDist;
+    update(contentsRect());
+  }
+}
+
+void 
+KRuler::showTinyMarks(bool show)
+{
+  if (show != showtm) {
+    showtm = show;
+    update(contentsRect());
+  }
+}
+
+void 
+KRuler::showLittleMarks(bool show)
+{
+  if (show != showlm) {
+    showlm = show;
+    update(contentsRect());
+  }
+}
+
+void 
+KRuler::showMediumMarks(bool show)
+{
+  if (show != showmm) {
+    showmm = show;
+    update(contentsRect());
+  }
+}
+
+void 
+KRuler::showBigMarks(bool show)
+{
+  if (show != showbm) {
+    showbm = show;
+    update(contentsRect());
+  }
+}
+
+void 
+KRuler::showEndMarks(bool show)
+{
+  if (show != showem) {
+    showem = show;
+    update(contentsRect());
+  }
+}
+
+void 
+KRuler::setValuePerLittleMark(int value)
+{
+  if ((value != valuelm) && (!showValuelm)) {
+    showValuelm = TRUE;
+    valuelm = value;
+    showValuemm = TRUE;
+    valuemm = value * mmDist;
+    showValuebm = TRUE;
+    valuebm = value * bmDist;
+    update(contentsRect());
+  }
+}
+
+void 
+KRuler::setValuePerMediumMark(int value)
+{
+  if ((value != valuemm) && (!showValuemm)) {
+    showValuelm = FALSE;
+    showValuemm = TRUE;
+    valuemm = value;
+    showValuebm = TRUE;
+    valuebm = value * (bmDist/mmDist);
+    update(contentsRect());
+  }
+}
+
+void 
+KRuler::setValuePerBigMark(int value)
+{
+  if ((value != valuebm) && (!showValuebm)) {
+    showValuelm = FALSE;
+    showValuemm = FALSE;
+    showValuebm = TRUE;
+    valuebm = value;
+    update(contentsRect());
+  }
+}
+
+void 
+KRuler::setEndLabel(const char *label)
+{
+  endlabel = label;
+  update(contentsRect());
+}
+
+void 
+KRuler::setRulerStyle(metric_style style)
+{
+  switch (style) {
+  case pixel:
+    setLittleMarkDistance(1);
+    setMediumMarkDistance(5);
+    setBigMarkDistance(10);
+
+    showTinyMarks(false);
+    showLittleMarks(true);
+    showMediumMarks(true);
+    showBigMarks(true);
+    showEndMarks(true);
+
+    setValuePerMediumMark(50);
+    setPixelPerMark(10.0);
+
+    break;
+  case inch:
+    setTinyMarkDistance(1);
+    setLittleMarkDistance(2);
+    setMediumMarkDistance(4);
+    setBigMarkDistance(8);
+
+    showTinyMarks(true);
+    showLittleMarks(true);
+    showMediumMarks(true);
+    showBigMarks(true);
+    showEndMarks(true);
+
+    setValuePerBigMark(1);
+    setPixelPerMark(9.0);
+
+    break;
+  case millimetres:
+  case centimetres:
+  case metres:
+    setLittleMarkDistance(1);
+    setMediumMarkDistance(5);
+    setBigMarkDistance(10);
+
+    showTinyMarks(false);
+    showLittleMarks(true);
+    showMediumMarks(true);
+    showBigMarks(true);
+    showEndMarks(true);
+
+    setValuePerBigMark(10);
+    setPixelPerMark(3.0);
+  }
+  switch (style) {
+  case pixel:
+    setEndLabel("pixel");
+    break;
+  case inch:
+    setEndLabel("inch");
+    break;
+  case millimetres:
+    setEndLabel("mm");
+    break;
+  case centimetres:
+    setEndLabel("cm");
+    break;
+  case metres:
+    setEndLabel("m");
+  }
+  // if the style changes one of the values,
+  // update would have been called inside the methods
+  // -> no update() call needed here !
+}
+
+
+void 
+KRuler::setRulerStyle(paint_style);
+void 
+KRuler::setTickStyle(paint_style);
+
+/*
+void 
+KRuler::setPixelPerMark(int rate)
+{
+  ppm = (double)rate;
+  update(contentsRect());
+}
+*/
+
+void 
+KRuler::setPixelPerMark(double rate)
+{ // never compare floats against each other :)
+  ppm = rate;
+  update(contentsRect());
+}
+
+void
+KRuler::setOffset(int _offset)
+{// debug("set offset %i", _offset);
+  if (offset != _offset) {
+    offset = _offset;
+    update(contentsRect());
+  }
+}
+
+void 
+KRuler::slideup(int count = 1)
+{
+  if (count) {
+    offset += count;
+    update(contentsRect());
+  }
+}
+
+void 
+KRuler::slidedown(int count = 1)
+{
+  if (count) {
+    offset -= count;
+    update(contentsRect());
+  }
+}
+
+
+  /* the ruler may slide up and down "count" times*/
+/*
+void 
+KRuler::slideup(int count = 1);
+
+void 
+KRuler::slidedown(int count = 1);
+*/
+/* set ruler slide to "offset" from beginning */
+/*
+  void 
+KRuler::setOffset(int);
+*/
+
+void  
+KRuler::slotNewValue(int _value)
+{
+  int oldvalue = range.value();
+  if (oldvalue != _value) {
+    //    setValue(_value);
+    range.setValue(_value);
+    // get the rectangular of the old and the new ruler pointer
+    // and repaint only him
+    if (horizontal == dir) {
+      QRect oldrec(-5+oldvalue-offset,10, 11,6);
+      QRect newrec(-5+_value-offset,10, 11,6);
+      repaint( oldrec.unite(newrec) );
+    }
+    else {
+      QRect oldrec(10,-5+oldvalue-offset, 6,11);
+      QRect newrec(10,-5+_value-offset, 6,11);
+      repaint( oldrec.unite(newrec) );
+    }
+  }
+}
+
+void  
+KRuler::slotNewOffset(int _offset)
+{
+  if (offset != _offset) {
+    //setOffset(_offset);
+    offset = _offset;
+    repaint(contentsRect());
+  }
+}
+
+void  
+KRuler::drawContents(QPainter *p)
+{
+  //  debug ("KRuler::drawContents, %s",(horizontal==dir)?"horizontal":"vertical");
+
+#ifdef PROFILING
+  QTime time;
+  time.start();
+  for (int profile=0; profile<10; profile++) {
+#endif
+
+  int value  = range.value(),
+    minval = range.minValue(),
+    maxval = range.maxValue(),
+    ioffsetval = value-offset;
+    //    pixelpm = (int)ppm;
+  //    left  = clip.left(),
+  //    right = clip.right();
+  double f, fend,
+    offsetmin=(double)(minval-offset),
+    offsetmax=(double)(maxval-offset);
+
+  // draw labels
+  QFont font = p->font();
+  font.setPointSize(LABEL_SIZE);
+  p->setFont( font );
+  // draw littlemarklabel
+  
+  // draw mediummarklabel
+
+  // draw bigmarklabel
+
+  // draw endlabel
+  if (horizontal==dir) {
+    p->drawText( END_LABEL_X, END_LABEL_Y, (const char *)endlabel );
+  }
+  else { // rotate text +pi/2 and move down a bit
+    p->rotate(90.0);
+    p->translate( /*(float)LABEL_SIZE*/ 0,
+    		  -(float)(p->fontMetrics().boundingRect((const char *)endlabel).width()) );
+    p->drawText( END_LABEL_X, END_LABEL_Y, (const char *)endlabel );
+    p->resetXForm();
+  }
+  // draw the tiny marks
+  if (showtm) {
+    for ( f=offsetmin; f<offsetmax; f+=ppm ) {
+      if (horizontal==dir) {
+	p->drawLine((int)f, BASE_MARK_X1, (int)f, BASE_MARK_X2);
+      }
+      else {
+	p->drawLine(BASE_MARK_X1, (int)f, BASE_MARK_X2, (int)f);
+      }
+    }
+  }
+  if (showlm) {
+    // draw the little marks
+    fend = ppm*lmDist;
+    for ( f=offsetmin; f<offsetmax; f+=fend ) {
+      if (horizontal==dir) {
+	p->drawLine((int)f, LITTLE_MARK_X1, (int)f, LITTLE_MARK_X2);
+      }
+      else {
+	p->drawLine(LITTLE_MARK_X1, (int)f, LITTLE_MARK_X2, (int)f);
+      }
+    }
+  }
+  if (showmm) {
+    // draw medium marks
+    fend = ppm*mmDist;
+    for ( f=offsetmin; f<offsetmax; f+=fend ) {
+      if (horizontal==dir) {
+	p->drawLine((int)f, MIDDLE_MARK_X1, (int)f, MIDDLE_MARK_X2);
+      }
+      else {
+	p->drawLine(MIDDLE_MARK_X1, (int)f, MIDDLE_MARK_X2, (int)f);
+      }
+    }
+  }
+  if (showbm) {
+    // draw big marks
+    fend = ppm*bmDist;
+    for ( f=offsetmin; f<offsetmax; f+=fend ) {
+      if (horizontal==dir) {
+	p->drawLine((int)f, BIG_MARK_X1, (int)f, BIG_MARK_X2);
+      }
+      else {
+	p->drawLine(BIG_MARK_X1, (int)f, BIG_MARK_X2, (int)f);
+      }
+    }
+  }
+  if (showem) {
+    // draw end marks
+    if (horizontal==dir) {
+      p->drawLine(minval-offset, END_MARK_X1, minval-offset, END_MARK_X2);
+      p->drawLine(maxval-offset, END_MARK_X1, maxval-offset, END_MARK_X2);
+    }
+    else {
+      p->drawLine(END_MARK_X1, minval-offset, END_MARK_X2, minval-offset);
+      p->drawLine(END_MARK_X1, maxval-offset, END_MARK_X2, maxval-offset);    
+    }
+  }
+  // draw pointer
+  QPointArray pa(4);
+  if (horizontal==dir) {
+    pa.setPoints(3, -5+ioffsetval,10, 5+ioffsetval,10, 0+ioffsetval,15);
+  }
+  else {
+    pa.setPoints(3, 10,-5+ioffsetval, 10,5+ioffsetval, 15,0+ioffsetval);
+  }
+  p->setBrush( p->backgroundColor() );
+  p->drawPolygon( pa );
+
+#ifdef PROFILING
+  }
+  int elapsed = time.elapsed();
+  debug("paint time %i",elapsed);
+#endif
+  
+}
---
+++ kdeui/kruler.h
@@ -0,0 +1,346 @@
+/* -*- c++ -*- */
+/* This file is part of the KDE libraries
+    Copyright (C) 1998 Jörg Habenicht (j.habenicht@europemail.com)
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+    Boston, MA 02111-1307, USA.
+*/
+
+#ifndef KRULER_H
+#define KRULER_H
+
+#include <qframe.h>
+#include <qrangect.h>
+
+/**
+ * The vertical ruler looks similar to this:
+ *
+ *<pre>
+ *    meters                       inches
+ *
+ *    ------   <----end mark-----> ------
+ *        --                            -
+ *        --   <---little mark--->     --
+ *        --                            -
+ *        --                          ---
+ *       ---   <---medium mark          -
+ *        --                           --
+ *        --        tiny mark---->      -
+ *        --                         ----
+ *        --                            -
+ *      ----   <-----big mark          --
+ *        --                            -
+ *      |>--   <--ruler pointer-->   |>--
+ *
+ * </pre>
+ *
+ * To receive mouse clicks or mouse moves,
+ * the class has to be overloaded.
+ *
+ * For performance reasons, the public methods doesn't call repaint(),
+ * than update except the public slot methods as described.
+ * so all the changed settings will be painted once after leaving
+ * to the main event loop.
+ * For performance painting the slot methods should be used,
+ * they do a fast repaint(..) call after changing the values.
+ * For setting multiple values like minValue(), maxValue(), offset() etc.
+ * using the public methods is recommended,
+ * so the widget will be painted only once when entering the main event loop.
+ *
+ * @short A ruler widget
+ * @author Jörg Habenicht
+ */
+class KRuler : public QFrame
+{
+Q_OBJECT
+public:
+
+  /**
+   * direction of the ruler.
+   * Has to be defined when constructing the widget.
+   * see KRuler();
+   * @short ruler direction in window
+   */
+  enum direction{ horizontal, vertical };
+
+
+  enum metric_style{ pixel, inch, millimetres, centimetres, metres };
+  enum paint_style{ flat, raised, sunken };
+
+
+  /* create new ruler with "dir" direction
+   * ruler will be raised panel frame.
+   */
+
+  /**
+   * Constructor, requires the direction to paint to.
+   * parent, name, f and allowLines are handed through to QFrame.
+   * The default look is a raised widget,
+   * but may be changed with the QFrame methods.
+   *
+   * @param dir        direction of the ruler
+   * @param parent     will be handed over to QFrame
+   * @param name       will be handed over to QFrame
+   * @param f          will be handed over to QFrame
+   * @param allowLines will be handed over to QFrame
+   *
+   * @short Constructor
+   */
+KRuler(direction dir, QWidget *parent=0, const char *name=0, WFlags f=0, bool allowLines=TRUE);
+
+  /**
+   * Constructor, requires the direction to paint to and an initial width.
+   * The width sets the fixed width of the widget. This is usefull, if you
+   * want to draw the ruler bigger or smaller than the default size.
+   * Note: The size of the marks doesn't change.
+   * parent, name, f and allowLines are handed through to QFrame
+   *
+   * @param dir         direction of the ruler
+   * @param widgetWidth fixed width of the widget
+   * @param parent      will be handed over to QFrame
+   * @param name        will be handed over to QFrame
+   * @param f           will be handed over to QFrame
+   * @param allowLines  will be handed over to QFrame
+   *
+   * @short Constructor
+   */
+KRuler(direction dir, int widgetWidth, QWidget *parent=0, const char *name=0, WFlags f=0, bool allowLines=TRUE);
+
+  /**
+   * Destructor.
+   */
+~KRuler();
+
+  /** minimum value reachable, default: 0
+   * calls update(), so the widget is painted after leaving
+   * the main event loop
+   *
+   * @short set the minimal value of the ruler pointer
+   */
+  void setMinValue(int);
+
+  /**
+   * returns the minimal value of the ruler pointer
+   *
+   * @short get the minimal value of the ruler pointer
+   */
+  inline int getMinValue() const;
+
+  /** maximum value of the ruler pointer, default: 100 .
+   * calls update(), so the widget is painted after leaving
+   * the main event loop
+   *
+   * @short set the maximal value of the ruler pointer
+   */
+  void setMaxValue(int);
+  
+  /** returns the maximal value of the ruler pointer.
+   */
+  inline int getMaxValue() const;
+
+  /** sets minimal and maxmal values of the ruler pointer.
+   * calls update(), so the widget is painted after leaving
+   * to the main event loop.
+   */
+  void setRange(int min, int max);
+
+  /** set the value of the ruler pointer,
+   * the value is shown with ruler pointer painted.
+   * calls update(), the widget is painted after leaving
+   * to the main event loop.
+   */
+  void setValue(int);
+  inline int getValue() const;
+
+  /** distance of tiny marks.
+   * mostly used in inch system with distance of 1. 
+   */
+  void setTinyMarkDistance(int);
+  inline int getTinyMarkDistance() const;
+
+  /** set the paint distance of the little marks.
+   * value is 1 in metric system and 2 in inch system.
+   */
+  void setLittleMarkDistance(int);
+  inline int getLittleMarkDistance() const;
+
+  /** set the paint distance of the medium marks.
+   * for inch styles it is twice the little mark distance,
+   * for metric styles it is five times.
+   * default: 5 */
+  void setMediumMarkDistance(int);
+  inline int getMediumMarkDistance() const;
+
+  /** set the paint distance of the big marks.
+   * for inch or metric styles it is twice the medium mark distance.
+   * default: 10 */
+  void setBigMarkDistance(int);
+  inline int getBigMarkDistance() const;
+
+  /* base marks are shown constantly */
+  void showTinyMarks(bool);
+  void showLittleMarks(bool);
+  void showMediumMarks(bool);
+  void showBigMarks(bool);
+  void showEndMarks(bool);
+
+  /** sets the value that is shown per little mark.
+   * for metric systems this is 1 per mark.
+   * Note: If you set the value for little marks the values for 
+   * medium and big marks are updated accordingly in respect to
+   * the values set in "setXXMarkDistance(int)" */
+  void setValuePerLittleMark(int);
+
+  /** sets the value that is shown per little mark.
+   * for metric systems this is 5
+   * display of little marks is disabled,
+   * big marks are updated */
+  void setValuePerMediumMark(int);
+
+  /** sets the value that is shown per little mark.
+   * for metric systems this is 1
+   * display of little and medium marks marks is disabled,
+   */
+  void setValuePerBigMark(int);
+
+  /** paint number values of the little marks.
+   * default: false */
+  void showLittleMarkLabel(bool);
+
+  /** paint number values of the medium marks.
+   * default: false */
+  void showMediumMarkLabel(bool);
+
+  /** paint number values of the big marks.
+   * default: true */
+  void showBigMarkLabel(bool);
+
+  /** paint number values of the little marks.
+   * default: true */
+  void showEndLabel(bool);
+
+  /** label which is drawn at the beginning of the visible part
+   * of the ruler */
+  void setEndLabel(const char *);
+
+  /** convenience method:
+   * sets up the necessary tasks for the provided styles */
+  void setRulerStyle(metric_style);
+  inline metric_style getMetricRulerStyle() const;
+
+  void setRulerStyle(paint_style);
+  inline paint_style getPaintRulerStyle() const;
+  void setTickStyle(paint_style);
+  inline paint_style getTickStyle() const;
+
+  /** set the amount of pixel between two base marks.
+   * for pixel display the value is 10.0 marks per pixel ;-)
+   * for inches it is 9.0, and for centimetres ~2,835 -> 3.0 .
+   * If you like to magnify your part of display, you have to
+   * adjust HERE your markdistance.
+   * Notice: the floatingpoint type is only supported to give the possibility
+   *         of having some floatingpoint values.
+   *         It should be used with care, using values below 10.0
+   *         shows visible jumps of markpositions (e.g. 2.345).
+   *         Using whole numbers is highly recommended.
+   * To use int values use setPixelPerMark((int)your_int_value);
+   * default: 1 mark per 10 pixels 
+   */
+  void setPixelPerMark(double);
+  /*  void setPixelPerMark(int); */
+  inline double getPixelPerMark() const;
+
+  /** the ruler may slide up and down "count" pixels
+   * this doesn't affect the position of the ruler pointer,
+   * only the visual part of the ruler is moved */
+  void slideup(int count = 1);
+  void slidedown(int count = 1);
+
+  /** set ruler slide to "offset" from beginning.
+   * like "slideup" or "slidedown" with an absolute offset */
+  void setOffset(int);
+  inline int getOffset() const;
+
+public slots:
+
+  /** sets the pointer to a new position.
+   * the offset is NOT updated.
+   * repaint() is called afterwards */
+  void slotNewValue(int);
+
+  /** sets the ruler marks to a new position.
+   * the pointer is NOT updated.
+   * repaint() is called afterwards */
+  void slotNewOffset(int);
+
+protected:
+  virtual void drawContents(QPainter *);
+
+private:
+
+  direction dir;
+  int tmDist, lmDist, mmDist, bmDist, offset;
+  bool showtm, showlm, showmm, showbm, showem; /* show tiny, little, medium, big, endmarks */
+  int valuelm, valuemm, valuebm; /* value per little, medium, big mark */
+  bool showValuelm, showValuemm, showValuebm;
+  double ppm; /* pixel per mark */
+
+  QString endlabel;
+  QRangeControl range;
+};
+
+
+int
+KRuler::getMinValue() const
+{ return range.minValue(); }
+
+int 
+KRuler::getMaxValue() const
+{ return range.maxValue(); }
+
+int 
+KRuler::getValue() const
+{ return range.value(); }
+
+int 
+KRuler::getTinyMarkDistance() const
+{ return tmDist; }
+
+int 
+KRuler::getLittleMarkDistance() const
+{ return lmDist; }
+
+int 
+KRuler::getMediumMarkDistance() const
+{ return mmDist; }
+
+KRuler::metric_style 
+KRuler::getMetricRulerStyle() const;
+
+KRuler::paint_style 
+KRuler::getPaintRulerStyle() const;
+
+KRuler::paint_style 
+KRuler::getTickStyle() const;
+
+double 
+KRuler::getPixelPerMark() const
+{ return ppm; }
+
+int 
+KRuler::getOffset() const
+{ return offset; }
+
+#endif
