d8d4c92624faa1fda061cf420841d601157ac512
Hans Petter Bieker <bieker@kde.org>
1998-10-01 21:26:44 +0000

    This should work on both v2.2 and v3.0.
    svn path=/trunk/kdelibs/; revision=10937

--- bsd-port/kde-snapshot.mk
+++ bsd-port/kde-snapshot.mk
@@ -1,10 +1,8 @@
 #$Id$
 
-OSVERSION!=     sysctl -n kern.osreldate
-
 CONFIGURE_ENV=	CXXFLAGS="$(CFLAGS)" \
 		install_root=$(INSTALL_ROOT) \
-		INSTALL_SCRIPT="install -c -m 555"
+		INSTALL_SCRIPT="$(INSTALL_SCRIPT)"
 
 # Since there's nothing to fetch, we might as well use a dummy target
 do-fetch:
@@ -21,28 +19,15 @@ post-install:
 		${MAKE} PREFIX=${PREFIX} make-plist
 		cp -Rp ${INSTALL_ROOT}/* /
 		rm -rf ${INSTALL_ROOT}
-		ldconfig -m ${PREFIX}
+		ldconfig -m ${PREFIX}/lib
 
 # This should finally work somewhat decently now
 make-plist:
-.if ${OSVERSION} >= 300000
 		cd ${INSTALL_ROOT}/${PREFIX} && \
-			find ./ -type f|sed 's,^\./,,'|sort > $(PLIST)
+			find . -type f ! -name .|sed 's,^\./,,'|sort > $(PLIST)
 		cd ${INSTALL_ROOT}/${PREFIX} && \
-			find ./ -type l|sed 's,^\./,,'|sort >> $(PLIST)
-		@echo "@exec /sbin/ldconfig -m %B" >> $(PLIST)
+			find . -type l ! -name .|sed 's,^\./,,'|sort >> $(PLIST)
+		cd ${INSTALL_ROOT}/${PREFIX} && \
+			find . -type d ! -name .|sed 's,^\./,@dirrm ,'|sort -r>> $(PLIST)
 		@echo "@exec /sbin/ldconfig -R" >> $(PLIST)
-		cd ${INSTALL_ROOT}/${PREFIX} && \
-			find ./ -type d|sed 's,^\./,@dirrm ,'|sort -r>> $(PLIST)
 		@echo "@unexec /sbin/ldconfig -R" >> $(PLIST)
-.else
-		cd ${INSTALL_ROOT}/${PREFIX} && \
-			find ./ -type f|sed 's,^\.//,,'|sort > $(PLIST)
-		cd ${INSTALL_ROOT}/${PREFIX} && \
-			find ./ -type l|sed 's,^\.//,,'|sort >> $(PLIST)
-		@echo "@exec /sbin/ldconfig -m %B" >> $(PLIST)
-		@echo "@exec /sbin/ldconfig -R" >> $(PLIST)
-		cd ${INSTALL_ROOT}/${PREFIX} && \
-			find ./ -type d|sed 's,^\.//,@dirrm ,'|sort -r>> $(PLIST)
-		@echo "@unexec /sbin/ldconfig -R" >> $(PLIST)
-.endif
