5766b368eb18c31f6cc22b8650ebcc91b5be18be
Thomas Tanghus Olsen <tanghus@kde.org>
1998-10-27 08:10:21 +0000

    kimgio_png_write implemented. For now images are converted to 8 bits so alphachannels etc. are lost. Besides that it seems to work. Does anyone know how to make a check for libpng in admin/acinclude.m4.in?
    svn path=/trunk/kdelibs/; revision=11780

--- kimgio/pngr.cpp
+++ kimgio/pngr.cpp
@@ -16,6 +16,7 @@
 
 #include<stdio.h>
 #include<qimage.h>
+#include<qfile.h>
 
 extern "C" {
 #include<png.h>
@@ -138,10 +139,191 @@ void kimgio_png_read( QImageIO *io )
 	return;
 }
 
-void kimgio_png_write( QImageIO * )
+void kimgio_png_write( QImageIO *iio )
 {
-	fprintf( stderr, "Warning: PNG writing not yet implemented.\n" );
+	QIODevice *f = ( iio->ioDevice() );
+	FILE *fp = 0;
+	png_structp png_ptr;
+	png_infop info_ptr;
+        int colortype = 0;
+
+	const QImage& image = iio->image().convertDepth(8);
+	int w = image.width(), h = image.height();
+
+	if(image.hasAlphaBuffer())
+		colortype = PNG_COLOR_TYPE_RGB_ALPHA;
+	else
+		colortype = PNG_COLOR_TYPE_RGB;
+
+	// open the file
+	fp = fdopen(((QFile*)f)->handle(), "wb");
+	if (fp == 0) {
+		iio->setStatus( -1 );
+		return;
+	}
+
+	/* Create and initialize the png_struct with the desired error handler
+	 * functions.  If you want to use the default stderr and longjump method,
+	 * you can supply NULL for the last three parameters.  We also check that
+	 * the library version is compatible with the one used at compile time,
+	 * in case we are using dynamically linked libraries.  REQUIRED.
+	 */
+	png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, 0, 0, 0);
+
+	if (png_ptr == 0) {
+		fclose(fp);
+		iio->setStatus( -2 );
+		return;
+	}
+
+	/* Allocate/initialize the image information data.  REQUIRED */
+	info_ptr = png_create_info_struct(png_ptr);
+	if (info_ptr == 0) {
+		fclose(fp);
+		png_destroy_write_struct(&png_ptr,  (png_infopp)NULL);
+		iio->setStatus( -3 );
+		return;
+	}
+
+	info_ptr->channels = 3;
+
+	/* set up the output control if you are using standard C streams */
+	png_init_io(png_ptr, fp);
+
+	/* Set the image information here.  Width and height are up to 2^31,
+	* bit_depth is one of 1, 2, 4, 8, or 16, but valid values also depend on
+	* the color_type selected. color_type is one of PNG_COLOR_TYPE_GRAY,
+	* PNG_COLOR_TYPE_GRAY_ALPHA, PNG_COLOR_TYPE_PALETTE, PNG_COLOR_TYPE_RGB,
+	* or PNG_COLOR_TYPE_RGB_ALPHA.  interlace is either PNG_INTERLACE_NONE or
+	* PNG_INTERLACE_ADAM7, and the compression_type and filter_type MUST
+	* currently be PNG_COMPRESSION_TYPE_BASE and PNG_FILTER_TYPE_BASE. REQUIRED
+	*/
+	png_set_IHDR(png_ptr, info_ptr, w, h, 8, PNG_COLOR_TYPE_PALETTE,
+		PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_BASE, PNG_FILTER_TYPE_BASE);
+
+	/* set the palette if there is one.  REQUIRED for indexed-color images */
+	int numcolors = image.numColors();
+
+	info_ptr->palette = (png_colorp)png_malloc(png_ptr, numcolors * sizeof (png_color));
+	for(int i = 0; i < numcolors; i++) {
+		info_ptr->palette[i].red = qRed(image.color(i));
+		info_ptr->palette[i].blue = qBlue(image.color(i));
+		info_ptr->palette[i].green = qGreen(image.color(i));
+	}
+	if(numcolors > 0)
+		png_set_PLTE(png_ptr, info_ptr, info_ptr->palette, numcolors);
+
+	//optional significant bit chunk
+/*
+	info_ptr->sig_bit.red = 8;
+	info_ptr->sig_bit.green = 8;
+	info_ptr->sig_bit.blue = 8;
+*/
+	//if we are dealing with a grayscale image then
+	//sig_bit.gray = true_bit_depth;
+	//otherwise, if we are dealing with a color image then
+	//sig_bit.red = true_red_bit_depth;
+	//sig_bit.green = true_green_bit_depth;
+	//sig_bit.blue = true_blue_bit_depth;
+	//if the image has an alpha channel then
+	//sig_bit.alpha = true_alpha_bit_depth;
+	//png_set_sBIT(png_ptr, info_ptr, *info_ptr->sig_bit);
+
+  
+	// Optional gamma chunk is strongly suggested if you have any guess
+	// as to the correct gamma of the image.
+	//png_set_gAMA(png_ptr, info_ptr, gamma);
+
+	// Optionally write comments into the image
+	//text_ptr[0].key = "Title";
+	//text_ptr[0].text = "Mona Lisa";
+	//text_ptr[0].compression = PNG_TEXT_COMPRESSION_NONE;
+	//text_ptr[1].key = "Author";
+	//text_ptr[1].text = "Leonardo DaVinci";
+	//text_ptr[1].compression = PNG_TEXT_COMPRESSION_NONE;
+	//text_ptr[2].key = "Description";
+	//text_ptr[2].text = "<long text>";
+	//text_ptr[2].compression = PNG_TEXT_COMPRESSION_zTXt;
+	//png_set_text(png_ptr, info_ptr, text_ptr, 2);
+
+	// Write the file header information.  REQUIRED
+	png_write_info(png_ptr, info_ptr);
+
+	// Once we write out the header, the compression type on the text
+	// chunks gets changed to PNG_TEXT_COMPRESSION_NONE_WR or
+	// PNG_TEXT_COMPRESSION_zTXt_WR, so it doesn't get written out again
+	// at the end.
+
+	png_set_packing( png_ptr );
+	png_set_strip_16( png_ptr );
+
+	// pack pixels into bytes
+	//png_set_packing(png_ptr);
+
+	// swap location of alpha bytes from ARGB to RGBA 
+	//png_set_swap_alpha(png_ptr);
+
+	// Get rid of filler (OR ALPHA) bytes, pack XRGB/RGBX/ARGB/RGBA into
+	// RGB (4 channels -> 3 channels). The second parameter is not used.
+	//png_set_filler(png_ptr, 0, PNG_FILLER_BEFORE);
+
+	// flip BGR pixels to RGB
+	//png_set_bgr(png_ptr);
+
+	// swap bytes of 16-bit files to most significant byte first
+	//png_set_swap(png_ptr);
+
+	// swap bits of 1, 2, 4 bit packed pixel formats
+	//png_set_packswap(png_ptr);
+
+	// turn on interlace handling if you are not using png_write_image()
+	//if (interlacing)
+	//	number_passes = png_set_interlace_handling(png_ptr);
+	//else
+	//	number_passes = 1;
+
+	// The easiest way to write the image (you may have a different memory
+	// layout, however, so choose what fits your needs best).  You need to
+	// use the first method if you aren't handling interlacing yourself.
+
+#define entire
+	// One of the following output methods is REQUIRED 
+#ifdef entire // write out the entire image data in one call
+
+	png_byte **row_pointers = image.jumpTable();
+	png_write_image(png_ptr, row_pointers);
+
+#else no_entire // write out the image data by one or more scanlines
+	// If you are only writing one row at a time, this works
+
+	for (int y = 0; y < h; y++) {
+		png_bytep row_pointer = image.scanLine(y);
+		png_write_rows(png_ptr, row_pointer, 1);
+	}
+#endif no_entire // use only one output method
+
+	// You can write optional chunks like tEXt, zTXt, and tIME at the end as well.
+
+	// It is REQUIRED to call this to finish writing the rest of the file
+	png_write_end(png_ptr, info_ptr);
+
+	// if you malloced the palette, free it here
+	free(info_ptr->palette);
+
+	// if you allocated any text comments, free them here
+
+	// clean up after the write, and free any memory allocated
+	png_destroy_write_struct(&png_ptr, (png_infopp)NULL);
+
+	// close the file
+	fclose(fp);
+
+	iio->setStatus( 0 );
+
 	return;
+
+	//fprintf( stderr, "Warning: PNG writing not yet implemented.\n" );
+	//return;
 }
 
 #endif /* HAVE_LIBPNG */
