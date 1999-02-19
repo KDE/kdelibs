836d625c6365ed4c08c0f25d0f20528d19a693fb
Joerg Habenicht <j.habenicht@europemail.com>
1999-02-19 08:50:58 +0000

    xy=0 konstants removed from constructor
    svn path=/trunk/kdelibs/; revision=17076

--- kdetest/krulertest.cpp
+++ kdetest/krulertest.cpp
@@ -15,7 +15,7 @@ MyCheckBox::mouseReleaseEvent(QMouseEvent *e )
 }
 */
 
-MouseWidget::MouseWidget( QWidget *parent=0, const char *name=0, WFlags f=0 )
+MouseWidget::MouseWidget( QWidget *parent, const char *name, WFlags f )
   : QFrame(parent, name, f)
 {
 }
