1ab90051e2101a904d87f0d5601cec2cddd6c474
Stephan Kulow <coolo@kde.org>
1999-03-11 21:16:31 +0000

    removing HEAD libs from KDE 1.1 as suggested by David
    svn path=/branches/KDE_1_1_BRANCH/kdelibs/; revision=17935

--- khtml/khtmlembed.cpp
+++
@@ -1,9 +0,0 @@
-#include "khtmlembed.h"
-
-KHTMLEmbededWidget::KHTMLEmbededWidget( QWidget *_parent, int _frameborder, bool _allowresize ) : QWidget( _parent )
-{
-  iFrameBorder = _frameborder;
-  bAllowResize = _allowresize;
-}
-
-#include "khtmlembed.moc"
--- kio/kio_dlg.h
+++
@@ -1,6 +0,0 @@
-#ifndef __kio_dlg_h__
-#define __kio_dlg_h__
-
-bool open_CriticalDlg( const char *_titel, const char *_message, const char *_but1, const char *_but2 = 0L );
-
-#endif
