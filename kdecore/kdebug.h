4cca99160e3efdb8dab1e67b5fab3b59b28948c6
Alex Zepeda <zipzippy@sonic.net>
1999-04-07 23:25:42 +0000

    Move the kdebug and kdebug_null prototypes so that they come before the possible #defining of kdebug to kdebug_null.  Also, I tried to document kdebug's behavior a bit too.
    svn path=/trunk/kdelibs/; revision=19356

--- kdecore/kdebug.h
+++ kdecore/kdebug.h
@@ -20,15 +20,46 @@
 #ifndef _KDEBUG_H
 #define _KDEBUG_H
 
+/**
+  * kdebug provides and advanced mechanism for displaying debugging
+  * information to the user.  kdebug works like printf does but takes two
+  * extra arguments.
+  *
+  * The first argument required by kdebug is an "level".  A kdebug level
+  * determines how important the message being displayed is.  There are
+  * four available levels:
+  *     KDEBUG_INFO
+  *       for informal messages to the user such as: hey don't press that button
+  *     KDEBUG_WARN
+  *       for when something is just a little bit more significant happens.
+  *     KDEBUG_ERROR
+  *       for when a non fatal error has occured.
+  *     KDEBUG_FATAL
+  *       for when a horrific error has happened (1+1 != 2 for instance)
+  * By pressing SHIFT-CTRL-F12 (C-Sh-f12) a small configuration dialog box
+  * will pop up and let one assign actions to each debug level on an
+  * application by application basis.
+  *
+  * The second argument is a kdebug "area".  This "area" tells kdebug
+  * where the call to kdebug came from.  The "area" is an unsigned number
+  * specified in kdebug.areas ($KDEDIR/share/config/kdebug.areas).  If
+  * this number is zero, the application name (from KApplication) will be
+  * used, and if that is not available, unknown will be used instead.
+  *
+  * The third (and final) required argument is a format string identical
+  * to the kind used by printf.
+  */
+extern "C" {
+void kdebug( unsigned short level, unsigned short area, const char* fmt, ... );
+void kdebug_null (unsigned short level, unsigned short area, const char* fmt, ...);
+};
+
 #ifdef kdebug
 #undef kdebug
 #endif
 #ifdef NDEBUG
 #define kdebug kdebug_null
 #endif
-
-void kdebug( unsigned short level, unsigned short area, const char* fmt, ... );
-void kdebug_null (unsigned short level, unsigned short area, const char* fmt, ...);
 
 #ifndef NDEBUG
 #define KASSERT( Cond, Level, Area, String ) { if( !Cond ) kdebug( (Level), (Area), (String) ); }
