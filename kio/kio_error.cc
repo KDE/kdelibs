08b10b262c253509efbdd6febd1c51a0f77d7e42
Alex Zepeda <zipzippy@sonic.net>
1999-04-10 23:44:39 +0000

    Oops I forgot this too.
    svn path=/trunk/kdelibs/; revision=19532

--- kio/kio_error.cc
+++ kio/kio_error.cc
@@ -111,7 +111,10 @@ QString kioErrorString( int _errid, const char *_errortext )
       result = i18n( "Could not login into %1" ).arg ( _errortext );
       break;
     case  ERR_COULD_NOT_AUTHENTICATE:
-      result = i18n( " Athorization failed, %1 authentication not supported" ).arg( _errortext );
+      result = i18n( "Athorization failed, %1 authentication not supported" ).arg( _errortext );
+    case  ERR_WARNING:
+      result = i18n( "Warning: %1" ).arg( _errortext );
+      break;
     default:
       result = i18n( "Unknown error code %1\n%2\n\nPlease send a full bugreport at http://bugs.kde.org" ).arg(_errid ).arg(_errortext);
       break;
