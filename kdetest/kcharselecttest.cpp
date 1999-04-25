0807fde5ab8be481928759391ad8b8c7f49f208d
Reginald Stadlbauer <reggie@kde.org>
1999-04-25 21:47:16 +0000

    testprogram for KCharSelect
    svn path=/trunk/kdelibs/; revision=20329

---
+++ kdetest/kcharselecttest.cpp
@@ -0,0 +1,16 @@
+#include <kapp.h>
+#include <klocale.h>
+#include "kcharselect.h"
+
+int main (int argc,char **argv)
+{
+  KApplication app(argc,argv);
+
+  KCharSelect selector(0L,"char selector");
+  selector.resize(selector.sizeHint());
+  selector.show();
+  selector.setCaption(i18n("KCharSelect Test"));
+  
+  app.setMainWidget(&selector);
+  return app.exec();
+}
