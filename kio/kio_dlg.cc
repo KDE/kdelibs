a4fd1b9b33ab671ab0fe60980b8c529245045069
Matt Koss <koss@napri.sk>
1999-05-04 17:55:54 +0000

    Added creation of KApplication when needed - this fixes nasty bug in http slave
    svn path=/trunk/kdelibs/; revision=20813

--- kio/kio_dlg.cc
+++ kio/kio_dlg.cc
@@ -1,10 +1,18 @@
+#include <qmessagebox.h>
+#include <kapp.h>
+
 #include "kio_dlg.h"
 
-#include <qmessagebox.h>
-
-bool open_CriticalDlg( const char *_titel, const char *_message, const char *_but1, const char *_but2 )
+bool open_CriticalDlg( const char *_title, const char *_message, const char *_but1, const char *_but2 )
 {
-  if ( QMessageBox::critical( 0L, _titel, _message, _but1, _but2 ) )
+  if ( kapp == 0L )
+  {
+    char* a[2] = { "dummy", 0L };
+    int b = 1;
+    (void)new KApplication( b, a );
+  }
+
+  if ( QMessageBox::critical( 0L, _title, _message, _but1, _but2 ) )
     return true;
   return false;
 }
