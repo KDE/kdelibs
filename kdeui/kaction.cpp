23f4c29769436f55052f8419023be497b1f39f98
Sirtaj Singh Kang <taj@kde.org>
1999-05-07 16:55:03 +0000

    Test version of KAction and KUIActions. (not yet built by default, more coming)
    svn path=/trunk/kdelibs/; revision=21007

---
+++ kdeui/kaction.cpp
@@ -0,0 +1,267 @@
+/*
+* kuseraction.cpp -- Implementation of class KAction.
+* Author:	Sirtaj Singh Kang
+* Version:	$Id$
+* Generated:	Sat May  1 14:43:58 EST 1999
+*/
+
+
+#include<assert.h>
+#include<qiconset.h>
+#include<qmenudata.h>
+
+#include<kconfigbase.h>
+
+#include<kaction.h>
+//#include<kconfigbase.h>
+
+KAction::KAction( const char *desc, 
+			const QString& localDesc,
+			QObject *receiver, const char *member )
+	: _name( desc ),
+	_localName( localDesc ),
+	_icon( 0 ),
+	_accel ( 0 ),
+	_accelId( -1 ),
+
+	_triggers( 0 ),
+	_menus( 0 ),
+	_receiver( 0 ),
+	_member( QString::null )
+{
+	if( receiver && member ) {
+		connect( this, SIGNAL(activate()), receiver, member);
+		_receiver = receiver;
+		_member = member;
+	}
+
+	if( _localName.isNull() ) {
+		_localName = _name;
+	}
+
+	setEnabled( true );
+}
+
+KAction::~KAction()
+{
+	emit removeAccel( this );
+
+	delete _icon;		_icon = 0;
+	delete _triggers;	_triggers = 0;
+	delete _menus;		_menus = 0;
+}
+
+void KAction::setIcon( const QIconSet& icon )
+{
+	*_icon = icon;
+}
+
+const QIconSet& KAction::icon() const
+{
+	return *_icon;
+}
+
+void KAction::setAccel( int accel )
+{
+	_accel = accel;
+
+	if( _menus == 0 ) {
+		if ( _accel == 0 ) {
+			emit removeAccel( this );
+		}
+		else {
+			emit globalAccel( this, _accel );
+		}
+
+		return;
+	}
+ 
+ 	// set accel for all related menu items.
+
+	QListIterator<MenuTrigger> iter( *_menus );
+	QMenuData *d = 0;
+
+	for( MenuTrigger *t = iter.toFirst(); 
+			t != 0; t = ++iter ) {
+		d = dynamic_cast<QMenuData *>( t->menu );
+		assert( d != 0 );
+		d->setAccel( accel, t->id );
+	}
+
+	if( d != 0 && _accel != 0 ) {
+		removeAccel( this );
+	}
+}
+
+void KAction::addMenuItem( QObject *menu, int id )
+{
+	assert( menu != 0 );
+
+	MenuTrigger *t = new MenuTrigger( menu, id );
+
+	if( _menus == 0 ) {
+		_menus = new MenuItems;
+		_menus->setAutoDelete( true );
+	}
+
+	QListIterator<MenuTrigger> iter( *_menus );
+
+	// connect to menu's "destroyed" signal only once
+	for( iter.toFirst(); iter.current() && iter.current()->menu != menu; 
+			 ++iter ) {
+		// continue till found or end
+	}
+
+	if( !iter.current() ) {
+		connect( menu, SIGNAL(destroyed()), this, SLOT(menuDead()) );
+	}
+
+	_menus->append( t );
+
+	connect( menu, SIGNAL(activated(int)), 
+			this, SLOT(menuActivated(int)) );
+
+	QMenuData *menud = dynamic_cast<QMenuData *>(menu);
+	menud->setItemEnabled( id, enabled() );
+
+	if( _accel ) {
+		menud->setAccel( _accel, id );
+	}
+	
+	debug( "going to set global accel..." );
+
+	removeAccel( this );
+}
+
+void KAction::addTrigger( Trigger *uitrigger, const char *member,
+		bool allowDisable )
+{
+	assert( uitrigger != 0 );
+
+	if( _triggers == 0 ) {
+		_triggers = new Triggers;
+		_triggers->setAutoDelete( false );
+	}
+
+	if ( !allowDisable ) {
+		// TODO: implement this
+		warning( "KAction::addTrigger: Disallowing disable is not"
+			" yet implemented." );
+	}
+
+	_triggers->append( uitrigger );
+	connect( uitrigger, member, this, SIGNAL(activate()) );
+	connect( uitrigger, SIGNAL( destroyed() ), this, SLOT(senderDead()) );
+
+	uitrigger->setEnabled( enabled() );
+}
+
+void KAction::senderDead()
+{
+	const Trigger *sdr = dynamic_cast<const Trigger *>(sender());
+
+	if( sdr == 0 ) {
+		return;
+	}
+
+	for (Trigger *curr = _triggers->first(); curr != 0; ) {
+		if ( curr == sdr ) {
+			_triggers->remove();
+			curr = _triggers->current();
+			continue;
+		}
+
+		curr = _triggers->next();
+	}
+}
+
+void KAction::menuDead()
+{
+	const QObject *sdr = sender();
+
+	if( sdr == 0 || _menus == 0 ) {
+		return;
+	}
+
+	for (MenuTrigger *curr = _menus->first(); curr != 0; ) {
+		if ( curr->menu == sdr ) {
+			_menus->remove();
+			curr = _menus->current();
+			continue;
+		}
+
+		curr = _menus->next();
+	}
+
+	if ( _menus->count() == 0 && _accel != 0 ) {
+		emit globalAccel( this, _accel );
+	}
+}
+
+void KAction::menuActivated( int id )
+{
+	const QObject *sdr = sender();
+	
+	if( sdr == 0 || _menus == 0 ) {
+		return;
+	}
+
+	QListIterator<MenuTrigger> miter( *_menus );
+	const MenuTrigger *m = miter.toFirst();
+
+	for( ; m != 0; m = ++miter ) {
+		if ( m->menu == sdr && m->id == id ) {
+			debug( "%s: match! %d", _name.ascii(), m->id );
+			emit activate();
+			return;
+		}
+	}
+}
+
+void KAction::setEnabled( bool state )
+{
+	blockSignals( !state );
+
+	if( _triggers != 0 ) {
+		QListIterator<Trigger> titer ( *_triggers );
+
+		for( ; titer.current(); ++titer ) {
+			titer.current()->setEnabled( state );
+		}
+	}
+
+	if( _menus != 0 ) {
+		QListIterator<MenuTrigger> miter ( *_menus );
+		MenuTrigger *t = 0;
+
+		for( ; (t = miter.current()) != 0; ++miter ) {
+			QMenuData *menu = dynamic_cast<QMenuData *>(t->menu);
+			assert( menu != 0 );
+			menu->setItemEnabled( t->id, state );
+		}
+	}
+}
+
+
+bool KAction::readConfig( const KConfigBase& cfg )
+{
+	QString pfx = "Action" + desc;
+	QString key = pfx + "Accel";
+
+	if( cfg.hasKey( key ) ) {
+		setAccel( cfg.readNumEntry( key ) );
+	}
+	// TODO: read icon
+	return true;
+	
+}
+
+void KAction::writeConfig( KConfigBase& cfg )
+{
+	QString pfx = "Action" + desc;
+	QString key = pfx + "Accel";
+
+	cfg.writeEntry( key, accel() );
+}
+
+#include"kaction.moc"
---
+++ kdeui/kuiactions.cpp
@@ -0,0 +1,143 @@
+/*
+* kuiactions.cpp -- Implementation of class KUIActions.
+* Author:	Sirtaj Singh Kang
+* Version:	$Id$
+* Generated:	Thu May  6 01:08:20 EST 1999
+*/
+
+#include<assert.h>
+#include<qaccel.h>
+
+#include"kuiactions.h"
+
+KUIActions::KUIActions( QWidget *parent )
+	: QObject( parent ),
+	_actions ( new ActionDict ),
+	_accelContext( parent ),
+	_accel( new QAccel( parent ) )
+{
+	assert( parent != 0 );
+
+	_actions->setAutoDelete( true );
+}
+
+KUIActions::~KUIActions()
+{
+	delete _actions; _actions = 0;
+	delete _accel;	_accel = 0;
+}
+
+KAction *KUIActions::newAction( const char *name, 
+			QObject *receiver,
+			const char *member,
+			int accel)
+{
+	assert( name != 0 && _actions->find( name ) == 0 );
+
+	KAction *action = new KAction( name, QString::null, receiver, member );
+
+	if( action == 0 ) {
+		warning( "newAction:: out of memory" );
+		return  0;
+	}
+	_actions->insert( name, action );
+
+	connect( action, SIGNAL(globalAccel(KAction *, int)),
+		this, SLOT(setAccel(KAction *, int)) );
+	connect( action, SIGNAL(removeAccel(KAction *)),
+		this, SLOT(removeAccel(KAction *)) );
+
+	if( accel ) {
+		action->setAccel( accel );
+	}
+	
+	return action;
+}
+
+KAction *KUIActions::newAction( const char *name,
+			const QString& i18nName,
+			QObject *receiver,
+			const char *member,
+			int accel )
+{
+
+	KAction *a = newAction( name, receiver, member, accel );
+	assert( a != 0 );
+
+	a->setLocalDesc( i18nName );
+
+	return a;
+}
+
+KAction *KUIActions::newAction( KAction *action )
+{
+	assert( action != 0 && _actions->find( action->desc() ) == 0 );
+
+	connect( action, SIGNAL(accelChanged( const KAction *, int)),
+		this, SLOT(updateAccel( const KAction *, int)) );
+	
+	_actions->insert( action->desc(), action );
+
+	return action;
+}
+
+
+bool KUIActions::deleteAction( const char *action )
+{
+	KAction *a = _actions->find( action );
+	assert( a != 0 );
+
+	return _actions->remove( action );
+}
+
+bool KUIActions::enabled( const char *action )
+{
+	KAction *a = _actions->find( action );
+	assert( a != 0 );
+
+	return a->enabled();
+}
+
+void KUIActions::setEnabled( const char *action, bool enabled )
+{
+	KAction *a = _actions->find( action );
+	assert( a != 0 );
+
+	a->setEnabled( enabled );
+}
+
+void KUIActions::setAccel( KAction *action, int accel )
+{
+	assert( action != 0 );
+	debug( "accel: %s, %d", action->desc().ascii(), accel );
+
+	// enable the accel if it's valid, remove otherwise
+	int id = action->accelId();
+
+	if( id != -1 ) {
+		_accel->removeItem( id );
+		id = -1;
+	}
+
+	if( accel != 0 && action->receiver() != 0 
+			&& !action->member().isNull() ) {
+		id = _accel->insertItem( accel, id );
+		_accel->connectItem( id, action->receiver(), action->member() );
+	}
+	
+	action->setAccelId( id );
+}
+
+void KUIActions::removeAccel( KAction *action )
+{
+	int id = action->accelId();
+
+	if ( id == -1 ) {
+		return;
+	}
+
+	_accel->removeItem( id );
+	action->setAccelId( -1 );
+}
+
+#include "kuiactions.moc"
