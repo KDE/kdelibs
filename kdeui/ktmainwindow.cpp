d8b99de976c7468e03c4334cf4959885ede4f153
David Faure <faure@kde.org>
1999-05-11 23:14:05 +0000

    header included twice
    svn path=/trunk/kdelibs/; revision=21276

--- kdeui/ktmainwindow.cpp
+++ kdeui/ktmainwindow.cpp
@@ -18,8 +18,9 @@
     the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
     Boston, MA 02111-1307, USA.
 */
-#include <ktmainwindow.h>
-#include <ktmainwindow.h>
+
+#include "ktmainwindow.h"
+
 #include <kapp.h>
 #include <kdebug.h>
 #include <kconfig.h>
