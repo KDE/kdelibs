09b74528a20c3ab4ad0ccfedfc8abc0b9329694f
Reginald Stadlbauer <reggie@kde.org>
1999-05-12 12:06:13 +0000

    fix
    svn path=/trunk/kdelibs/; revision=21301

--- kimgio/kimgio.cpp
+++ kimgio/kimgio.cpp
@@ -1,4 +1,4 @@
- 
+
 /**
 * kimgio.h -- Implementation of interface to the KDE Image IO library.
 * Sirtaj Singh Kang <taj@kde.org>, 23 Sep 1998.
@@ -58,25 +58,25 @@ void kimgioRegister(void)
 	QDir dir(KApplication::kde_bindir() + "/../lib", "kimg_*.la");
 
 	for (uint index = 0; index < dir.count(); index++) {
-	    
+	
 	    QString libname = dir[index];
 	    if (libname.isNull())
 		break;
 
-	    libname = dir.path() + "/" + libname;
+	    libname = /*dir.path() + "/" +*/ libname;
 	    lt_dlhandle libhandle = lt_dlopen(libname.ascii());
 	    if (libhandle == 0) {
-		warning("couldn't dlopen %s (%s)", 
+		warning("couldn't dlopen %s (%s)",
 			libname.ascii(), lt_dlerror());
 	    }
-	    
+	
 	    libname.replace(QRegExp("^.*kimg_"), "kimgio_init_");
 	    libname.replace(QRegExp("\\.la$"), "");
-	    
+	
 	    lt_ptr_t init_func = lt_dlsym(libhandle, libname.ascii() );
 	
 	    if (init_func == NULL) {
-		warning("couldn't init module %s (%s)!", libname.ascii(), 
+		warning("couldn't init module %s (%s)!", libname.ascii(),
 			lt_dlerror());
 	    } else {
 		void (*func)();
@@ -96,5 +96,5 @@ void kimgioRegister(void)
 #ifdef LINKED_tiff
 	kimgio_init_tiff();
 #endif
-      
+
 }
