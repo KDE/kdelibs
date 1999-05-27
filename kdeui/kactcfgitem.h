128a5b79d889c8ab3ea35e410f0e0d686cc35a4e
Sirtaj Singh Kang <taj@kde.org>
1999-05-27 19:01:11 +0000

    Added KActionConfigWidget (hardly works)
    svn path=/trunk/kdelibs/; revision=22115

---
+++ kdeui/kactcfgitem.h
@@ -0,0 +1,41 @@
+
+#ifndef SSK_KACTCFGITEM_H
+#define SSK_KACTCFGITEM_H
+
+class QCheckBox;
+class KIconLoaderButton;
+
+/**
+ * An item in the action configuration widget.
+ * @internal
+ */
+class KActCfgListItem : public QListViewItem
+{
+public:
+	KActCfgListItem( QListView *view, KAction *action );
+	virtual ~KActCfgListItem();
+	void setAllText();
+
+	void loadFromAction();
+	void updateAction();
+
+	KAction *action;
+	bool dirty;
+
+	int accel;
+	QIconSet *icon;
+	QString iconPath;
+
+};
+
+class KActItemCfg : public QWidget
+{
+public:
+	KActItemCfg( QWidget *parent = 0 );
+	virtual ~KActItemCfg() {}
+
+	KIconLoaderButton *_iconBtn;
+	QCheckBox *_acNone, *_acDef, *_acCustom;
+};
+
+#endif
