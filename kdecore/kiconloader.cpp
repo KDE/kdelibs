69034165b83f604a8eb720e44449bfdd4f5fd5ab
Denis Perchine II <dyp@perchine.com>
1999-05-29 18:41:25 +0000

    File from 1.1.1 release restored. The old one was corrupted.
    svn path=/branches/KDE_1_1_BRANCH/kdelibs/; revision=22261

--- kdecore/kiconloader.cpp
+++ kdecore/kiconloader.cpp
@@ -20,6 +20,12 @@
    Boston, MA 02111-1307, USA.
    
    $Log$
+   Revision 1.26.4.2  1999/03/09 15:22:50  dfaure
+   Moved path-list initialisation to a private initPath().
+   Merged with Antonio's getIconPath(...) new method. (fixed missing .detach())
+    loadInternal() uses it.
+   Binary compatible. kfm will use it in a second :)
+
    Revision 1.26.4.1  1999/02/15 16:29:28  neerfeld
    reordered the searchpath: icons in ~/.kde/icons don't
    override application icons anymore
@@ -35,7 +41,7 @@
 
    Revision 1.24  1998/11/02 10:08:34  ettrich
    new reload method for kiconloader (Rene Beutler)
-}
+
    Revision 1.23  1998/10/04 11:05:30  neerfeld
    fixed a bug in loadInternal; fixes the crash of kmenuedit
 
@@ -45,34 +51,34 @@
 
    Revision 1.21  1998/08/26 18:37:47  neerfeld
    bug fix for loadInternal;  changed email address
-	config = KApplication::getKApplication()->getConfig();
+
    Revision 1.20  1998/08/17 10:34:02  konold
-	QStringList::Iterator it = list.begin();
+
    Martin K.: Fixed a typo
-	for ( ; it != list.end(); ++it ) {
+
    Revision 1.19  1998/06/15 12:49:32  kulow
    applied patch to replace .kde with localkdedir()
-		addPath( *it );
+
    Revision 1.18  1998/04/25 13:15:28  mark
    MD: Added KPixmap and changed KIconLoader to use it.
-	/*
+
    Revision 1.17  1998/03/08 18:49:03  wuebben
    Bernd: fixed up the kiconloader class -- it was completely busted
-	   printf("()in path:%s\n",c);
-}
+
+
 */
-	if (result.isNull() && !canReturnNull) {
+
 #include <qapplication.h>
 #include <qdir.h>
 #include <qpainter.h>
 #include <qwmatrix.h>
-		result = loadInternal("unknown.xpm", w, h);
+
 #include "kiconloader.h"
-	}
+
 #include <kpixmap.h>
 #include <klocale.h>
 #include <kapp.h>
-}
+
 void KIconLoader::initPath()
 {
   // order is important! -- Bernd
@@ -89,37 +95,37 @@ void KIconLoader::initPath()
   addPath( KApplication::kde_datadir() + "/" + kapp->appName() + "/pics" );
   addPath( KApplication::localkdedir() + "/share/apps/" + kapp->appName() + "/pics" ); 
 }
-QPixmap KIconLoader::reloadIcon ( const QString& name, int w, int h ){
+
 KIconLoader::KIconLoader( KConfig *conf, 
 			  const QString &app_name, const QString &var_name ){
-	flush( name );
+
   QStrList list;
-	return loadInternal( name, w, h );
+
   config = conf;
   config->setGroup(app_name);
   config->readListEntry( var_name, list, ':' );
 
   for (const char *it=list.first(); it; it = list.next())
     addPath(it);
-QPixmap KIconLoader::loadMiniIcon ( const QString& name, int w, int h ){
+
   initPath();
 
   name_list.setAutoDelete(TRUE);
   pixmap_dirs.setAutoDelete(TRUE);
   pixmap_list.setAutoDelete(TRUE);
-		result = loadInternal( (QString("mini/") + name).ascii(), w, h);
+
   /*
   for(char* c = pixmap_dirs.first(); c ; c = pixmap_dirs.next()){
     printf("in path:%s\n",pixmap_dirs.current());
   }
   */
-	}
+
 }
  
 KIconLoader::KIconLoader( ){
 
   QStrList list;
-Stephan: See above
+
   config = KApplication::getKApplication()->getConfig();
   config->setGroup("KDE Setup");
   config->readListEntry( "IconPath", list, ':' );
@@ -142,14 +148,14 @@ Stephan: See above
 }
  
 KIconLoader::~KIconLoader()
-
+{
   name_list.clear();
   pixmap_list.clear();
 }
 
 QPixmap KIconLoader::loadIcon ( const QString &name, int w, int h ){
   QPixmap result = loadInternal(name, w, h);
-	return result;
+
 /* Stephan: It's OK to know, how many icons are still missing, but
        we don't need to tell everybody ;) Perhaps this can be con-
        verted to a KDEBUG solution, that is more silent? Don't know.
@@ -158,61 +164,61 @@ QPixmap KIconLoader::loadIcon ( const QString &name, int w, int h ){
     warning(klocale->translate("ERROR: couldn't find icon: %s"), (const char *) name);
 
 */
-QPixmap KIconLoader::loadApplicationIcon ( const QString& name, int w, int h )
+
   return result;
 }
 
-	// this is trouble since you don't know whether the addPath was
+
 QPixmap KIconLoader::reloadIcon ( const QString &name, int w, int h ){
   flush( name );
-	// paths. I hope this will not give us too much of a performance
+
   return loadInternal( name, w, h );
-	// hit. Other wise I will have to break binary compatibiliy
-	// -- Bernd
+}
+
 
 QPixmap KIconLoader::loadMiniIcon ( const QString &name, int w, int h ){
 
   QPixmap result;
-	int w, int h )
+
   if (name.left(1)!='/'){
     result = loadInternal( "mini/" + name, w, h);
   }
 
   if (result.isNull())
     result = loadInternal(name, w, h);
-	// this is trouble since you don't know whether the addPath was
-	// paths. I hope this will not give us too much of a performance
+
+
 /* 
    Stephan: See above
    if (result.isNull())
     warning(klocale->translate("ERROR: couldn't find mini icon: %s"), 
     (const char *) name);
-	// -- Bernd
+
 */
 
   return result;
-			// Let's be recursive (but just once at most)
-			full_path = getIconPath( "unknown.xpm" , false); 
+}
+
 QPixmap KIconLoader::loadApplicationIcon ( const QString &name, int w, int h ){
-	return full_path;
+
   //  addPath(KApplication::kde_icondir());
   //  addPath(KApplication::localkdedir() + "/share/icons" );
-{
+
   QPixmap result = loadIcon(name, w, h);
-	KPixmap pix;
+
   // this is trouble since you don't know whether the addPath was successful
   // I simply added icon dir to the set of standard paths. I hope
   // this will not give us too much of a performance hit. Other wise
   // I will have to break binary compatibiliy -- Bernd
-	if ( QPixmapCache::find( cacheKey, pix ) == true ) {
+
   // pixmap_dirs.remove((unsigned int) 0);
   // pixmap_dirs.remove((unsigned int) 0);
-	pix.load( getIconPath(name), 0, KPixmap::LowColor );
+
   return result;
-	if ( pix.isNull() ) {
+
 }
 
-		QWMatrix m;
+
 QPixmap KIconLoader::loadApplicationMiniIcon ( const QString &name, int w, int h ){
 
   //  addPath(KApplication::kde_icondir());
@@ -224,14 +230,14 @@ QPixmap KIconLoader::loadApplicationMiniIcon ( const QString &name, int w, int h
   // I simply added icon dir to the set of standard paths. I hope
   // this will not give us too much of a performance hit. Other wise
   // I will have to break binary compatibiliy -- Bernd
-{ 
+
   //  pixmap_dirs.remove((unsigned int) 0);
   //  pixmap_dirs.remove((unsigned int) 0);
-	}
+
   return result;
 
 }
-	pixmap_dirs.insert( pixmap_dirs.at(index), dir_name ); 
+
 QString KIconLoader::getIconPath( const QString &name, bool always_valid)
 {
     QString full_path;
@@ -314,22 +320,17 @@ void KIconLoader::addPath(QString path){
   else{
     //    fprintf(stderr,"Path %s doesn't exist\n",path.data());
   }
-		pixmap_dirs.insert( pixmap_dirs.begin(), path );
-	}
-	else{
+
+}
+
 void KIconLoader::flush( const QString& name  )
-	}
+{
   int index;
 
   if( (index = name_list.find(name)) >= 0 ) {
      name_list.remove( index );
      pixmap_list.remove( index );
   }
-
-void KIconLoader::flush( const QString& )
-{
-	// This is a NO-OP
-	warning( "KIconLoader::flush is deprecated." );
 }
 
 #include "kiconloader.moc"
