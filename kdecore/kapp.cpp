b5ea4fb463ec377231a7fb0f01aef3c459d38386
Glen Parker <glenebob@nwlink.com>
1999-06-08 19:23:44 +0000

    wrong type for font() - *font() -> font().
    svn path=/trunk/kdelibs/; revision=23080

--- kdecore/kapp.cpp
+++ kdecore/kapp.cpp
@@ -1090,7 +1090,7 @@ void KApplication::kdisplaySetStyleAndFont()
   //  QApplication::setStyle( applicationStyle );
   // 	setStyle() works pretty well but may not change the style of combo
   //	boxes.
-    if ( *font() != generalFont_)
+    if ( font() != generalFont_)
 	QApplication::setFont( generalFont_, TRUE );
   applyGUIStyle(applicationStyle_);
 
