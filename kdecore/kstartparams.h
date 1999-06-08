a9b5b53820d6991dad02f99248551b06cd7086fb
Michael Koch <koch@kde.org>
1999-06-08 18:06:40 +0000

    Email-address changed-
    svn path=/trunk/kdelibs/; revision=23074

--- kdecore/kstartparams.h
+++ kdecore/kstartparams.h
@@ -1,5 +1,5 @@
 /* This file is part of the KDE project
-   Copyright (C) 1999 Michael Koch <mkoch@bigfoot.de>
+   Copyright (C) 1999 Michael Koch <m_koch@bigfoot.de>
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
