3b89344f5f104b9e7e437ab1c7974eb0bbb298ec
Stephan Kulow <coolo@kde.org>
1999-06-15 20:50:55 +0000

    wow - kspread compiles without warnings with kautoarray!
    svn path=/trunk/kdelibs/; revision=23644

---
+++ kdecore/kautoarray.h
@@ -0,0 +1,21 @@
+
+template <class X>
+class kauto_array {
+  X *ptr;
+ 
+public:
+  typedef X element_type;
+  explicit kauto_array(size_t n) { ptr = new X[n]; }
+  ~kauto_array() { delete [] ptr; }
+
+  X& operator[](int n) { return ptr[n]; }
+  operator X*() { return ptr; }
+  X* operator +(int offset) { return ptr + offset; }
+
+private:
+  kauto_array& operator=(const kauto_array& a);
+  kauto_array(const kauto_array& a);
+
+};
+
+
