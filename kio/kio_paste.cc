1ee82962c1fde597c5b94a39be70a3bf7e62de7d
Alex Zepeda <zipzippy@sonic.net>
1999-06-16 08:45:18 +0000

    QStringListIfy
    svn path=/trunk/kdelibs/; revision=23674

--- kio/kio_paste.cc
+++ kio/kio_paste.cc
@@ -20,46 +20,42 @@
 
 bool isClipboardEmpty()
 {
-  QStrList urls;
-  if ( KClipboard::self()->urlList( urls ) )
-  {
+  QStringList urls;
+  if ( KClipboard::self()->urlList( urls ) ){
     if ( urls.count() == 0 )
       return true;
     return false;
   }
-  
+
   return true;
 }
 
 void pasteClipboard( const char *_dest_url )
 {
-  if ( KURL::split( _dest_url ).isEmpty() )
-  {
+  if ( KURL::split( _dest_url ).isEmpty() ) {
     kioErrorDialog( ERR_MALFORMED_URL, _dest_url );
     return;
   }
   
-  QStrList urls;
-  if ( KClipboard::self()->urlList( urls ) )
-  {
-    if ( urls.count() == 0 )
-    {
+  QStringList urls;
+  if ( KClipboard::self()->urlList( urls ) ) {
+    if ( urls.count() == 0 ) {
       QMessageBox::critical( 0L, i18n("Error"), i18n("The clipboard is empty"), i18n("OK") );
       return;
     }
 
     KIOJob* job = new KIOJob;
     job->copy( urls, _dest_url );
-    
+
     return;
   }
-  
+
   if ( KClipboard::self()->size() == 0 )
   {
     QMessageBox::critical( 0L, i18n("Error"), i18n("The clipboard is empty"), i18n("OK") );
     return;
   }
-  
+
   QByteArray ba = KClipboard::self()->octetStream();
 
   pasteData( _dest_url, ba );
@@ -68,8 +64,7 @@ void pasteClipboard( const char *_dest_url )
 void pasteData( const char *_dest_url, QByteArray _data )
 {
   KURL u( _dest_url );
-  if ( !u.isLocalFile() )
-  {
+  if ( !u.isLocalFile() ) {
     // TODO: Use KIO put command here for writing the data.
     QMessageBox::critical( 0L, i18n("Error"), i18n("Pasting clipboard data is only supported on the local hard disk currently"),
 			   i18n("OK") );
@@ -78,11 +73,9 @@ void pasteData( const char *_dest_url, QByteArray _data )
 
   KLineEditDlg l( i18n("Filename for clipboard content:"), "", 0L, false );
   int x = l.exec();
-  if ( x )
-  {
+  if ( x ) {
     QString url = l.text();
-    if ( url.isEmpty() )
-    {
+    if ( url.isEmpty() ) {
       QMessageBox::critical( 0L, i18n("Error"), i18n("You did not enter a filename"), i18n("OK") );
       return;
     }
@@ -90,20 +83,18 @@ void pasteData( const char *_dest_url, QByteArray _data )
     u.addPath( l.text() );
     
     struct stat buff;
-    if ( stat( u.path().ascii(), &buff ) == 0 )
-    {
+    if ( stat( u.path().ascii(), &buff ) == 0 ) {
       QString tmp = i18n("The file %1 does already exist. Do you really want to overwrite it ?" ).arg( u.path() );
       if ( QMessageBox::critical( 0L, i18n("Warning"), tmp,i18n("Yes"), i18n("No") ) == 1 )
 	return;
     }
     
     FILE *f = fopen( u.path().ascii(), "wb" );
-    if ( f == 0L )
-    {
+    if ( f == 0L ) {
       kioErrorDialog( ERR_WRITE_ACCESS_DENIED, u.path().ascii() );
       return;
     }
-    
+
     fwrite( _data.data(), 1, _data.size(), f );
     fclose( f );
   }
