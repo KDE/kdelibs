ef02ae1d2e43d4f6acedd2d44a30029ccbaa251d
Robert Williams <rwilliams@jrc-utah.com>
1999-06-16 23:38:15 +0000

    getLast() -> last()
    svn path=/trunk/kdelibs/; revision=23741

--- kdecore/kclipboard.cpp
+++ kdecore/kclipboard.cpp
@@ -361,7 +361,7 @@ void KClipboard::setURLList( QStringList& _urls )
   QStringList::Iterator iterator = _urls.begin();
   for( ; iterator != _urls.end(); iterator++ )
   {
-    if ( (*iterator) == _urls.getLast() )
+    if ( (*iterator) == _urls.last() )
       writeBlock( *iterator, (*iterator).length() );
     else
       writeBlock( *iterator, (*iterator).length() + 1 );
