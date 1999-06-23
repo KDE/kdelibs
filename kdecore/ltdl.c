15559e17dd8ac70bb20cd8a7c6352c2983a190e4
Stephan Kulow <coolo@kde.org>
1999-06-23 16:05:31 +0000

    the bugs we fixed here are fixed in libtool CVS, so it's time to sync
    svn path=/trunk/kdelibs/; revision=24328

--- kdecore/ltdl.c
+++ kdecore/ltdl.c
@@ -165,7 +165,7 @@ strchr(str, ch)
 {
 	const char *p;
 
-	for (p = str; *p != (char)ch && p != '\0'; p++)
+	for (p = str; *p != (char)ch && *p != '\0'; p++)
 		/*NOWORK*/;
 
 	return (*p == (char)ch) ? p : 0;
@@ -514,16 +514,56 @@ sys_wll_exit LTDL_PARAMS((void))
 	return 0;
 }
 
+/* Forward declaration; required to implement handle search below. */
+static lt_dlhandle handles;
+
 static int
 sys_wll_open (handle, filename)
 	lt_dlhandle handle;
 	const char *filename;
 {
-	handle->handle = LoadLibrary(filename);
-	if (!handle->handle) {
+	lt_dlhandle cur;
+	char *searchname = NULL;
+	char *ext = strrchr(filename, '.');
+
+	if (ext) {
+		/* FILENAME already has an extension. */
+		searchname = strdup(filename);
+	} else {
+		/* Append a `.' to stop Windows from adding an
+		   implicit `.dll' extension. */
+		searchname = (char*)lt_dlmalloc(2+ strlen(filename));
+		strcpy(searchname, filename);
+		strcat(searchname, ".");
+	}
+    
+	handle->handle = LoadLibrary(searchname);
+	lt_dlfree(searchname);
+	
+	/* libltdl expects this function to fail if it is unable
+	   to physically load the library.  Sadly, LoadLibrary
+	   will search the loaded libraries for a match and return
+	   one of them if the path search load fails.
+
+	   We check whether LoadLibrary is returning a handle to
+	   an already loaded module, and simulate failure if we
+	   find one. */
+	cur = handles;
+	while (cur) {
+		if (!cur->handle) {
+			cur = 0;
+			break;
+		}
+		if (cur->handle == handle->handle)
+			break;
+		cur = cur->next;
+	}
+
+	if (cur || !handle->handle) {
 		last_error = cannot_open_error;
 		return 1;
 	}
+
 	return 0;
 }
 
@@ -531,7 +571,7 @@ static int
 sys_wll_close (handle)
 	lt_dlhandle handle;
 {
-	if (FreeLibrary(handle->handle) != 0) {
+	if (FreeLibrary(handle->handle) == 0) {
 		last_error = cannot_close_error;
 		return 1;
 	}
