5d9b23f4d1d429669698e43f9474c548b385e458
Pietro Iglio <iglio@kde.org>
1999-07-13 16:02:07 +0000

    Added KDesktopFile to manage .desktop/.kdelnk files
    svn path=/trunk/kdelibs/; revision=25455

---
+++ kdecore/kdesktopfile.cpp
@@ -0,0 +1,92 @@
+/*
+  This file is part of the KDE libraries
+  Copyright (c) 1999 Pietro Iglio <iglio@kde.org>
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Library General Public
+  License as published by the Free Software Foundation; either
+  version 2 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Library General Public License for more details.
+
+  You should have received a copy of the GNU Library General Public License
+  along with this library; see the file COPYING.LIB.  If not, write to
+  the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+  Boston, MA 02111-1307, USA.
+*/
+
+// $Id$
+
+#include "kdesktopfile.h"
+#include "kdesktopfile.moc"
+
+KDesktopFile::KDesktopFile(const QString& pFileName, bool bReadOnly = false )
+  : KSimpleConfig(pFileName, bReadOnly)
+{
+  setDesktopGroup();
+}
+
+KDesktopFile::~KDesktopFile()
+{
+  // Nothing to be done
+}
+
+bool KDesktopFile::isDesktopFile(const QString& path)
+{
+  int len = path.length();
+
+  if(len > 8 && path.right(8) == ".desktop")
+    return true;
+  else if(len > 7 && path.right(7) == ".kdelnk")
+    return true;
+  else
+    return false;
+}
+
+QString KDesktopFile::readType()
+{
+  return readEntry("Type");
+}
+
+QString KDesktopFile::readIcon()
+{
+  return readEntry("Icon");
+}
+
+QString KDesktopFile::readMiniIcon()
+{
+  return readEntry("MiniIcon");
+}
+
+QString KDesktopFile::readName()
+{
+  return readEntry("Name");
+}
+
+QString KDesktopFile::readComment()
+{
+  return readEntry("Comment");
+}
+
+QString KDesktopFile::readPath()
+{
+  return readEntry("Path");
+}
+
+bool KDesktopFile::hasURLType()
+{
+  return readEntry("Type") == "URL";
+}
+
+bool KDesktopFile::hasApplicationType()
+{
+  return readEntry("Type") == "Application";
+}
+
+bool KDesktopFile::hasDeviceType()
+{
+  return readEntry("Type") == "FSDev";
+}
---
+++ kdecore/kdesktopfile.h
@@ -0,0 +1,122 @@
+/* This file is part of the KDE libraries
+   Copyright (c) 1999 Pietro Iglio <iglio@kde.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+   
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+   
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+// $Id$
+
+#ifndef _KDESKTOPFILE_H
+#define _KDESKTOPFILE_H
+
+#include "ksimpleconfig.h"
+
+/** 
+ * KDE Desktop File Management
+ *
+ * @author Pietro Iglio <iglio@kde.org>
+ * @version $Id$
+ * @see KConfigBase KConfig
+ * @short KDE Desktop File Management class
+ */
+class KDesktopFile : public KSimpleConfig
+{
+  Q_OBJECT 
+
+public:
+  /** 
+   * Construct a KDesktopFile object and make it either read-write
+   * or read-only.  
+   *
+   * @param pFileName The file used for saving the data. The
+   *                  full path must be specified.
+   * @param bReadOnly Whether the object should be read-only.
+   */
+  KDesktopFile( const QString& pFileName, bool bReadOnly = false );
+
+  /** 
+   * Destructor. 
+   *
+   * Writes back any dirty configuration entries.
+   */
+  virtual ~KDesktopFile();
+
+  /**
+   * Returns true if the file appears to be a desktop file.
+   * The check is performed looking at the file extension (the file is not
+   * open).
+   * Currently, valid extensions are ".kdelnk" and ".desktop".
+   * 
+   */
+  static bool isDesktopFile(const QString& path);
+
+  /**
+   * Returns the value of the "Type=" entry.
+   */
+  QString readType();
+
+  /**
+   * Returns the value of the "Icon=" entry.
+   */
+  QString readIcon();
+
+  /**
+   * Returns the value of the "MiniIcon=" entry.
+   */
+  QString readMiniIcon();
+
+  /**
+   * Returns the value of the "Name=" entry.
+   */
+  QString readName();
+
+  /**
+   * Returns the value of the "Comment=" entry.
+   */
+  QString readComment();
+
+  /**
+   * Returns the value of the "Path=" entry.
+   */
+  QString readPath();
+
+
+  /**
+   * Returns true if there is an entry "Type=URL".
+   */
+  bool hasURLType();
+
+  /**
+   * Returns true if there is an entry "Type=Application".
+   */
+  bool hasApplicationType();
+
+  /**
+   * Returns true if there is an entry "Type=FSDev".
+   */
+  bool hasDeviceType();
+
+
+private:
+
+  // copy-construction and assignment are not allowed
+  KDesktopFile( const KDesktopFile& );
+  KDesktopFile& operator= ( const KDesktopFile& );
+
+};
+
+  
+#endif
+
