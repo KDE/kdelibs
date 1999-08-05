677383841a2f7516498a5a2cf41f3b7b9d5329bd
Daniel M. Duley <daniel.duley@verizon.net>
1999-08-05 13:53:28 +0000

    Added a test applet for KDualColorButton
    svn path=/trunk/kdelibs/; revision=27158

---
+++ kdetest/kdualcolortest.cpp
@@ -0,0 +1,63 @@
+#include "kdualcolortest.h"
+#include <kdualcolorbtn.h>
+#include <kapp.h>
+#include <klocale.h>
+#include <qlayout.h>
+#include <qpalette.h>
+
+KDualColorWidget::KDualColorWidget(QWidget *parent, const char *name)
+    : QWidget(parent, name)
+{
+    lbl = new QLabel(i18n("Testing, testing, 1, 2, 3..."), this);
+    KDualColorButton *colorBtn =
+        new KDualColorButton(lbl->colorGroup().text(),
+                             lbl->colorGroup().background(), this);
+    connect(colorBtn, SIGNAL(fgChanged(const QColor &)),
+            SLOT(slotFgChanged(const QColor &)));
+    connect(colorBtn, SIGNAL(bgChanged(const QColor &)),
+            SLOT(slotBgChanged(const QColor &)));
+    connect(colorBtn, SIGNAL(currentChanged(KDualColorButton::DualColor)),
+            SLOT(slotCurrentChanged(KDualColorButton::DualColor)));
+    
+    QHBoxLayout *layout = new QHBoxLayout(this, 5);
+    layout->addWidget(colorBtn, 0);
+    layout->addWidget(lbl, 1);
+    layout->activate();
+    resize(sizeHint());
+}
+
+void KDualColorWidget::slotFgChanged(const QColor &c)
+{
+    QPalette p = lbl->palette();
+    p.setColor(QColorGroup::Text, c);
+    lbl->setPalette(p);
+}
+
+void KDualColorWidget::slotBgChanged(const QColor &c)
+{
+    QPalette p = lbl->palette();
+    QBrush b(c, SolidPattern);
+    p.setBrush(QColorGroup::Background, b);
+    setPalette(p);
+}
+
+void KDualColorWidget::slotCurrentChanged(KDualColorButton::DualColor current)
+{
+    if(current == KDualColorButton::Foreground)
+        warning("Foreground Button Selected.");
+    else
+        warning("Background Button Selected.");
+}
+
+int main(int argc, char **argv)
+{
+    KApplication *app = new KApplication(argc, argv);
+    KDualColorWidget w;
+    app->setMainWidget(&w);
+    w.show();
+    return(app->exec());
+}
+
+#include "kdualcolortest.moc"
+
+
