a70ddced52012032be4303a4b65958f97c8172c4
Matthias Ettrich <ettrich@troll.no>
1999-08-16 20:31:08 +0000

    Easier access to the window manager with KWinModule. This makes the former KWMModuleApplication obsolete.
    svn path=/trunk/kdelibs/; revision=27717

---
+++ kdecore/kwinmodule.cpp
@@ -0,0 +1,289 @@
+/*
+
+    $Id$
+
+    This file is part of the KDE libraries
+    Copyright (C) 1999 Matthias Ettrich (ettrich@kde.org)
+
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+    Boston, MA 02111-1307, USA.
+
+    $Log$
+    
+*/
+
+
+#include "kwinmodule.h"
+#include "kwm.h"
+
+class KWinModulePrivate : public QWidget
+{
+public:
+    KWinModulePrivate( KWinModule* m  )
+	: QWidget(0,0)
+    {
+	module = m;
+    }
+    ~KWinModulePrivate()
+    {
+    }
+    
+    KWinModule* module;
+    
+    QValueList<WId> windows;
+    QValueList<WId> windowsSorted;
+    QValueList<WId> dockWindows;
+    
+    bool x11Event( XEvent * ev );
+    
+};
+
+
+KWinModule::KWinModule( QObject* parent, bool dockModule )
+    : QObject( parent, "kwin_module" )
+{
+    d = new KWinModulePrivate( this );
+    if (!dockModule)
+	KWM::setKWMModule( d->winId() );
+    else
+	KWM::setKWMDockModule( d->winId() );
+}
+
+KWinModule::~KWinModule()
+{
+    delete d;
+}
+
+
+const QValueList<WId>& KWinModule::windows() const
+{
+    return d->windows;
+}
+
+const QValueList<WId>& KWinModule::windowsSorted() const
+{
+    return d->windowsSorted;
+}
+
+
+bool KWinModule::hasWId(WId w) const
+{
+    return d->windows.contains( w );
+}
+
+const QValueList<WId>& KWinModule::dockWindows() const
+{
+    return d->dockWindows;
+}
+
+bool KWinModulePrivate::x11Event( XEvent * ev )
+{
+    static bool atoms = FALSE;
+
+    static Atom module_init;
+    static Atom module_initialized;
+    static Atom module_desktop_change;
+    static Atom module_win_add;
+    static Atom module_dialog_win_add;
+    static Atom module_win_remove;
+    static Atom module_win_change;
+    static Atom module_win_raise;
+    static Atom module_win_lower;
+    static Atom module_win_activate;
+    static Atom module_win_icon_change;
+    static Atom module_desktop_name_change;
+    static Atom module_desktop_number_change;
+    static Atom kwm_command;
+    static Atom module_dockwin_add;
+    static Atom module_dockwin_remove;
+    static Atom sound;
+    static Atom register_sound;
+    static Atom unregister_sound;
+    static Atom kwm_window_region_changed;
+
+
+    if ( ev->type != ClientMessage )
+	return FALSE; //not interested
+
+    if (!atoms){
+
+	module_init = XInternAtom(qt_xdisplay(),
+				  "KWM_MODULE_INIT", False);
+	module_initialized = XInternAtom(qt_xdisplay(),
+					 "KWM_MODULE_INITIALIZED", False);
+	module_desktop_change = XInternAtom(qt_xdisplay(),
+					    "KWM_MODULE_DESKTOP_CHANGE", False);
+	module_desktop_name_change = XInternAtom(qt_xdisplay(),
+						 "KWM_MODULE_DESKTOP_NAME_CHANGE", False);
+	module_desktop_number_change = XInternAtom(qt_xdisplay(),
+						   "KWM_MODULE_DESKTOP_NUMBER_CHANGE", False);
+
+	module_win_add = XInternAtom(qt_xdisplay(),
+				     "KWM_MODULE_WIN_ADD", False);
+	module_dialog_win_add = XInternAtom(qt_xdisplay(),
+					    "KWM_MODULE_DIALOG_WIN_ADD", False);
+	module_win_remove = XInternAtom(qt_xdisplay(),
+					"KWM_MODULE_WIN_REMOVE", False);
+	module_win_change = XInternAtom(qt_xdisplay(),
+					"KWM_MODULE_WIN_CHANGE", False);
+	module_win_raise = XInternAtom(qt_xdisplay(),
+				       "KWM_MODULE_WIN_RAISE", False);
+	module_win_lower = XInternAtom(qt_xdisplay(), "KWM_MODULE_WIN_LOWER", False);
+	module_win_activate = XInternAtom(qt_xdisplay(),
+					  "KWM_MODULE_WIN_ACTIVATE", False);
+	module_win_icon_change = XInternAtom(qt_xdisplay(),
+					     "KWM_MODULE_WIN_ICON_CHANGE", False);
+	kwm_command = XInternAtom(qt_xdisplay(),
+				  "KWM_COMMAND", False);
+
+	module_dockwin_add = XInternAtom(qt_xdisplay(),
+					 "KWM_MODULE_DOCKWIN_ADD", False);
+	module_dockwin_remove = XInternAtom(qt_xdisplay(),
+					    "KWM_MODULE_DOCKWIN_REMOVE", False);
+	sound = XInternAtom(qt_xdisplay(),
+			    "KDE_SOUND_EVENT", False);
+	register_sound = XInternAtom(qt_xdisplay(),
+				     "KDE_REGISTER_SOUND_EVENT", False);
+	unregister_sound = XInternAtom(qt_xdisplay(),
+				       "KDE_UNREGISTER_SOUND_EVENT", False);
+
+	kwm_window_region_changed = XInternAtom(qt_xdisplay(), "KWM_WINDOW_REGION_CHANGED", False);
+
+	atoms = true;
+    }
+
+
+    Atom a = ev->xclient.message_type;
+    WId w = (WId) (ev->xclient.data.l[0]);
+    if ( a ==  module_init) {
+	windows.clear();
+	windowsSorted.clear();
+	dockWindows.clear();
+	emit module->init();
+    }
+    else if ( a == module_initialized) {
+	emit module->initialized();
+    }
+    else if ( a == module_desktop_change) {
+	int d = (int) w;
+	emit module->desktopChange(d);
+    }
+    else if (a == module_desktop_name_change){
+	int d = (int) w;
+	emit module->desktopNameChange(d, KWM::getDesktopName(d));
+    }
+    else if (a == module_desktop_number_change){
+	int d = (int) w;
+	emit module->desktopNumberChange(d);
+    }
+    else if (a == module_win_add){
+
+	windows.append(w);
+	windowsSorted.append(w);
+	emit module->windowAdd(w);
+    }
+    else if (a == module_dialog_win_add){
+	emit module->dialogWindowAdd(w);
+    }
+
+    else if (a == module_win_remove){
+	windows.remove( w );
+	windowsSorted.remove( w );
+	emit module->windowRemove(w);
+    }
+
+    else if (a == module_win_change){
+	emit module->windowChange(w);
+    }
+
+    else if (a == module_win_raise){
+	windowsSorted.remove(w);
+	windowsSorted.append(w);
+	emit module->windowRaise(w);
+    }
+
+    else if (a == module_win_lower){
+	windowsSorted.remove(w);
+	windowsSorted.insert(0, w);
+	emit module->windowLower(w);
+    }
+
+    else if (a == module_win_activate){
+	emit module->windowActivate(w);
+    }
+
+    else if (a == module_win_icon_change){
+	emit module->windowIconChanged(w);
+    }
+
+    else if (a == kwm_command){
+	char c[21];
+	int i;
+	for (i=0;i<20;i++)
+	    c[i] = ev->xclient.data.b[i];
+	c[i] = '\0';
+	QString com = c;
+	emit module->commandReceived(com);
+    }
+
+    else if (a == module_dockwin_add){
+	dockWindows.append(w);
+	emit module->dockWindowAdd(w);
+    }
+
+    else if (a == module_dockwin_remove){
+	dockWindows.remove(w);
+	emit module->dockWindowRemove(w);
+    }
+
+    else if (a == sound){
+	char c[21];
+	int i;
+	for (i=0;i<20;i++)
+	    c[i] = ev->xclient.data.b[i];
+	c[i] = '\0';
+	QString com = c;
+	emit module->playSound(com);
+    }
+
+    else if (a == register_sound){
+	char c[21];
+	int i;
+	for (i=0;i<20;i++)
+	    c[i] = ev->xclient.data.b[i];
+	c[i] = '\0';
+	QString com = c;
+	emit module->registerSound(com);
+    }
+
+    else if (a == unregister_sound){
+	char c[21];
+	int i;
+	for (i=0;i<20;i++)
+	    c[i] = ev->xclient.data.b[i];
+	c[i] = '\0';
+	QString com = c;
+	emit module->unregisterSound(com);
+    }
+
+    else if (a == kwm_window_region_changed){
+	emit module->windowRegionChange();
+    }
+
+    return TRUE;
+}
+
+#include "kwinmodule.moc"
---
+++ kdecore/kwinmodule.h
@@ -0,0 +1,211 @@
+/* This file is part of the KDE libraries
+    Copyright (C) 1999 Matthias Ettrich (ettrich@kde.org)
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+    Boston, MA 02111-1307, USA.
+*/
+/*
+ * kwinmodule.h. Part of the KDE project.
+ */
+
+#ifndef KWINMODULE_H
+#define KWINMODULE_H
+
+#include <qobject.h>
+#include <qvaluelist.h>
+
+class KWinModulePrivate;
+
+/**
+   
+   THIS API ISN'T FINISHED YET: IT'S STILL VERY COMPATIBLE WITH THE OLD KWMMODULEAPPLICATION.
+   BE AWARE THAT IT MIGHT CHANGE A BIT BEFORE KDE-2.0 FREEZE
+   
+ *   The class KWinModule provides information about the window manager required by
+ * windowmanager modules. It mainly informs a module about all
+ * currently managed windows and changes to them (via Qt
+ * signals). There are no methods to manipulate windows. These are
+ * defined in the class KWM (see kwm.h).
+ * @short Base class for KDE Window Manager modules.
+ * @author Matthias Ettrich (ettrich@kde.org)
+ * @version $Id$
+ */
+class KWinModule : public QObject 
+{
+    Q_OBJECT
+    
+public:
+
+    /**
+       Creates a win module object and connects to the window
+       manager. If @param dockModule is TRUE, the module will also
+       serve as docking module.
+     */
+    KWinModule( QObject* parent = 0, bool dockModule = FALSE );
+    
+    /**
+       Destructor. Internal cleanup, nothing fancy.
+    */
+    ~KWinModule();
+    
+  /**
+   * A list of all toplevel windows currently managed by the
+   * windowmanger in the order of creation. Please do not rely on
+   * indexes of this list: Whenever you enter Qt's eventloop in your
+   * application it may happen, that entries are removed or added.
+   * your module should perhaps work on a copy of this list and verify a
+   * window with hasWindow() before any operations.
+   *
+   * Iterateration over this list can be done easily with
+   QValueList<WId>::ConstIterator it;
+   for ( it = module->windows().begin(); it != modules->windows().end(); ++it ) {
+         ... do something here,  (*it) is the current WId.
+   }
+   *
+   */
+    const QValueList<WId>& windows() const;
+
+  /**
+   * A list of all toplevel windows currently managed by the
+   * windowmanger in the current stacking order (from lower to
+   * higher). May be useful for pagers.
+   */
+    const QValueList<WId>& windowsSorted() const;
+
+    /**
+   * Is <WId> still managed at present?
+   */
+    bool hasWId(WId) const;
+
+
+    /**
+    * The dock windows. Only valid if you are succesfully connected as
+    * docking module
+    */
+    const QValueList<WId>& dockWindows() const;
+
+signals:
+
+  /**
+   * Note that an init() may also be emitted if the window manager is
+   * restarted. So your module MUST react on it by clearing all internal
+   * data structures.
+   */
+    void init();
+
+
+    /**
+   *    This signal is emitted when a connect is complete, i.e. when
+   *    all existing windows or soundevents have been transfered to
+   *    the module
+   */
+    void initialized();
+
+    /**
+   * Switch to another virtual desktop
+   */
+    void desktopChange(int);
+
+    /**
+   * Add a window
+   */
+    void windowAdd(WId);
+
+    /**
+     Add a dialog window. Note that dialog windows are also included
+     in the set of windows you recieve with windowAdd (see above).
+     The dialogs are reported extra (before the windowAdd) to allow a
+     taskbar to exclude them. The dialogWindowAdd signal is guaranteed
+     to be emitted before the correspondinging windowAdd signal.
+   */
+    void dialogWindowAdd(WId);
+
+    /**
+   * Remove a window
+   */
+    void windowRemove(WId);
+
+    /**
+   * A window has been changed (size, title, etc.)
+   */
+    void windowChange(WId);
+
+    /**
+   * Raise a window
+   */
+    void windowRaise(WId);
+
+    /**
+   * Lower a window
+   */
+    void windowLower(WId);
+
+    /**
+   * Hint that <Window> is active (= has focus) now.
+   */
+    void windowActivate(WId);
+
+    /**
+    * A command kwm did not understand. Maybe it is for
+    * your module.
+    */
+    void commandReceived(QString);
+
+
+    /**
+    * This is not integrated into windowChange since reading
+    * icons is somewhat expensive via the KWM class.
+    */
+    void windowIconChanged(WId);
+
+    /**
+    * The specified desktop got a new name
+    */
+    void desktopNameChange(int, QString);
+
+    /**
+    * The number of desktops changed
+    */
+    void desktopNumberChange(int);
+
+    /**
+   * Add a dock window
+   */
+    void dockWindowAdd(WId);
+
+    /**
+   * Remove a dock window
+   */
+    void dockWindowRemove(WId);
+
+    /**
+   * The window region has changed
+   */
+    void windowRegionChange();
+
+    /**
+    * Play/register/unregister a sound
+    */
+    void playSound(QString);
+    void registerSound(QString);
+    void unregisterSound(QString);
+    
+private:
+    KWinModulePrivate* d;
+    
+    friend KWinModulePrivate;
+};
+
+#endif
