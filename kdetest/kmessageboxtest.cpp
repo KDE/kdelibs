6d04ce70a6091e58a10ce436dcb90ad3baec3f4d
Waldo Bastian <bastian@kde.org>
1999-08-24 13:17:14 +0000

    WABA: Added test for kmessagebox
    svn path=/trunk/kdelibs/; revision=28109

---
+++ kdetest/kmessageboxtest.cpp
@@ -0,0 +1,67 @@
+#include "kmessagebox.h"
+
+#include <stdlib.h>
+
+#include <kstatusbar.h>
+#include <kapp.h>
+
+#include <kglobal.h>
+
+
+
+
+int main( int argc, char *argv[] )
+{
+    int i, test;
+    KApplication *myApp = new KApplication( argc, argv );
+
+for( test = 1; true; test++)
+{
+for( int count = 0; count < 5; count++)
+{
+switch(test) {
+case 1:
+    i = KMessageBox::questionYesNo(0, "1. Is this a question?");
+    
+    printf("%d. returned %d ", test, i);
+    switch( i) {
+    case KMessageBox::Ok : printf("(%s)\n", "Ok"); break;
+    case KMessageBox::Cancel : printf("(%s)\n", "Cancel"); break;
+    case KMessageBox::Yes : printf("(%s)\n", "Yes"); break;
+    case KMessageBox::No : printf("(%s)\n", "No"); break;
+    default: printf("(%s)\n", "ERROR!"); exit(1);
+    }
+    break;
+
+case 2:
+    i = KMessageBox::warningYesNo(0, "2. Is this a question?");
+    
+    printf("%d. returned %d ", test, i);
+    switch( i) {
+    case KMessageBox::Ok : printf("(%s)\n", "Ok"); break;
+    case KMessageBox::Cancel : printf("(%s)\n", "Cancel"); break;
+    case KMessageBox::Yes : printf("(%s)\n", "Yes"); break;
+    case KMessageBox::No : printf("(%s)\n", "No"); break;
+    default: printf("(%s)\n", "ERROR!"); exit(1);
+    }
+    break;
+case 3:
+    i = KMessageBox::warningYesNoCancel(0, "3. Is this a question?");
+    
+    printf("%d. returned %d ", test, i);
+    switch( i) {
+    case KMessageBox::Ok : printf("(%s)\n", "Ok"); break;
+    case KMessageBox::Cancel : printf("(%s)\n", "Cancel"); break;
+    case KMessageBox::Yes : printf("(%s)\n", "Yes"); break;
+    case KMessageBox::No : printf("(%s)\n", "No"); break;
+    default: printf("(%s)\n", "ERROR!"); exit(1);
+    }
+    break;
+
+default:
+    return 0;
+}
+}
+}
+}
+
