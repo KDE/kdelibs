99976eb8400008031940ed1f53227fc5f4759e1d
David Faure <faure@kde.org>
1999-09-09 22:11:15 +0000

    The additionnal test which made me fix that typo bug.
    svn path=/trunk/kdelibs/; revision=28940

--- kdetest/ktartest.cpp
+++ kdetest/ktartest.cpp
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /*
- * Usage : if you define TORBEN_TEST, run with ./ktartest /tmp/whatever.tar.gz
+ * Usage : if you define TORBEN_TEST, run with ./ktartest /tmp/whatever.tar.gz 
  * It will create the tar.gz, then close and reopen it
  *
  * if you don't, run with ./ktartest /path/to/existing_file.tar.gz
@@ -40,10 +40,24 @@ int main( int argc, char** argv )
   tar.writeFile( "test1", "weis", "users", 5, "Hallo" );
   tar.writeFile( "test2", "weis", "users", 8, "Hallo Du" );
   tar.writeFile( "mydir/test3", "weis", "users", 13, "Noch so einer" );
-  tar.writeFile( "my/dir/test3", "dfaure", "hackers", 13, "I don't speak German (David)" );
+  tar.writeFile( "my/dir/test3", "dfaure", "hackers", 29, "I don't speak German (David)" );
   // This last one should, in theory, create /my and /my/dir, but only creates /my/dir
   // It doesn't really matter since we handle that when reopening the file
   // See note near findOrCreate in ktar.h
+
+#define SIZE1 100
+  // Now a medium file : 100 null bytes
+  char medium[ SIZE1 ];
+  bzero( medium, SIZE1 );
+  tar.writeFile( "mediumfile", "user", "group", SIZE1, medium );
+  tar.writeFile( "mediumfile2", "user", "group", SIZE1, medium );
+
+  // Now a huge file : 20000 null bytes
+  int n = 20000;
+  char * huge = new char[ n ];
+  bzero( huge, n );
+  tar.writeFile( "hugefile", "user", "group", n, huge );
+  delete [] huge;
 
   tar.close();
 
