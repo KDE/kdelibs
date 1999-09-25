bf98acd06bb8492ffe486a0bf5819bab42d8d5ce
Mirko Boehm <mirko@kde.org>
1999-09-25 12:46:58 +0000

    The kdatepicktestdlg widget is no more needed (it was unused for a long time, anyway). The kdatepicktest example program has been rewritten along with the new implementation of the KDatePicker.
    svn path=/trunk/kdelibs/; revision=29639

--- kdetest/kdatepicktest.cpp
+++ kdetest/kdatepicktest.cpp
@@ -1,37 +1,17 @@
-/* This file is part of the KDE libraries
-    Copyright (C) 1997 Tim D. Gilman (tdgilman@best.org)
-
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Library General Public
-    License as published by the Free Software Foundation; either
-    version 2 of the License, or (at your option) any later version.
-
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Library General Public License for more details.
-
-    You should have received a copy of the GNU Library General Public License
-    along with this library; see the file COPYING.LIB.  If not, write to
-    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-    Boston, MA 02111-1307, USA.
-*/
-#include <kapp.h>
-#include <qpixmap.h>
-#include "kdatepicktestdlg.h"
 #include "kdatepik.h"
+#include <qlineedit.h>
+#include <kapp.h>
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
-   KApplication a(argc, argv);
-//   a.setStyle(WindowsStyle); // comment out for Motif
-   
-//   CMainDlg *w = new CMainDlg;
-   KDatePicker *w = new KDatePicker;
-
-   a.setMainWidget(w);
-   w->resize(220,200);
-   w->show();
-   return a.exec();
+  KApplication *app=new KApplication(argc, argv);
+  KDatePicker picker;
+  app->setMainWidget(&picker);
+  picker.resize(picker.sizeHint().width(), 
+		 picker.sizeHint().height());
+  picker.setMinimumSize(picker.size());
+  picker.show();
+  // picker.setEnabled(false);
+  return app->exec();
 }
-   
+
