d9060d3a72b9f1f9bf41a92320ae100461f3c1ff
Bernhard Rosenkraenzer <bero@arklinux.org>
1999-10-08 19:48:56 +0000

    Good-bye, KButton...
    svn path=/trunk/kdelibs/; revision=30298

--- kdeui/kbutton.cpp
+++ kdeui/kbutton.cpp
@@ -17,6 +17,7 @@
     the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
     Boston, MA 02111-1307, USA.
 */
+#error This file can be safely removed.
 #include "kbutton.h"
 
 #include <qpainter.h>
--- kdeui/kbutton.h
+++ kdeui/kbutton.h
@@ -17,6 +17,10 @@
     the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
     Boston, MA 02111-1307, USA.
 */
+
+#error Use QToolButton instead of KButton.
+#error This file can be safely removed.
+
 #ifndef KBUTTON_H
 #define KBUTTON_H
 
