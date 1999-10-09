25da96fc1b17efe0a5a55aab82913a0635c643ce
Simon Hausmann <hausmann@kde.org>
1999-10-09 01:54:58 +0000

    - adding (untested ;) - but compiling ) K2Action, derived from QAction, as we need that for Canossa
    svn path=/trunk/kdelibs/; revision=30356

---
+++ kdeui/k2action.h
@@ -0,0 +1,26 @@
+#ifndef __k2action_h__
+#define __k2action_h__ $Id$
+
+#include <qaction.h>
+
+class K2Action : public QAction
+{
+  Q_OBJECT
+public:
+    K2Action( const QString& text, int accel = 0, QObject* parent = 0, const char* name = 0 );
+    K2Action( const QString& text, int accel,
+	     QObject* receiver, const char* slot, QObject* parent, const char* name = 0 );
+    K2Action( const QString& text, const QIconSet& pix, int accel = 0,
+	     QObject* parent = 0, const char* name = 0 );
+    K2Action( const QString& text, const QIconSet& pix, int accel,
+	     QObject* receiver, const char* slot, QObject* parent, const char* name = 0 );
+    K2Action( QObject* parent = 0, const char* name = 0 );
+
+    virtual int plug( QWidget *w );
+    virtual void unplug( QWidget *w );
+
+    virtual void setEnabled( bool b );
+
+};
+
+#endif
