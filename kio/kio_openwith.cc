b059d334fcb5b3f8a3f88d4857eecac07a5106e0
Daniel M. Duley <daniel.duley@verizon.net>
1999-10-09 00:03:52 +0000

    Revert last change. Bero - update the Qt CVS and read my post to kde-core-devel.
    svn path=/trunk/kdelibs/; revision=30344

--- kio/kio_openwith.cc
+++ kio/kio_openwith.cc
@@ -301,7 +301,7 @@ void KApplicationTree::slotSelectionChanged(QListViewItem* i)
 void KApplicationTree::resizeEvent( QResizeEvent * e)
 {
 #if QT_VERSION == 210
-    setColumnWidth(0, width()-QApplication::style().scrollBarExtent());
+    setColumnWidth(0, width()-QApplication::style().scrollBarExtent().width());
 #else
     setColumnWidth(0, width()-20);
 #endif
