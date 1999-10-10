bd2e00d7961e89d1253f6dfca5165e6ce6452886
Mirko Boehm <mirko@kde.org>
1999-10-10 13:35:57 +0000

    Merged the functionality with KLedLamp. Not functional yet.
    svn path=/trunk/kdelibs/; revision=30533

--- kdeui/kled.h
+++ kdeui/kled.h
@@ -25,37 +25,38 @@
 #define LED_H
 
 #include <qwidget.h>
-#include <qcolor.h>
 
+class QColor;
 /** 
-* KLed displays a round light emmiting diode.
+* KLed displays a round  or rectangular light emmitting diode.
 *
-* It is configurable to 5 colors and the 2 on/off states.
+* It is configurable to 5 colors, the 2 on/off states and .
 *
 * It may display itself in a performant flat view, a round view with
 * light spot or a round view sunken in the screen.
 *
 * @short A round led widget
-* @author Jörg Habenicht
+* @author Joerg Habenicht, Richard J. Moore (rich@kde.org) 1998, 1999
 */
+
 class KLed : public QWidget
 {
-Q_OBJECT
+  Q_OBJECT
 
-public:
-
-  /**
-   * color of the led
-   * @short led color
-   */
-  enum Color { yellow, orange, red, green, blue };
+    public:
 
   /**
    * Status of the light is on/off
    * @short led on/off
    */
   enum State { Off=0, On=1 };
-
+  
+  /** 
+   * Shades of the lamp. 
+   * @short shade
+   */ 
+  enum Shape { NoShape, Rectangular, Circular };
+  
   /**
    * Displays a flat, round or round-sunken led.
    * Displaying the led flat is less time and color consuming,
@@ -86,7 +87,7 @@ public:
    * @param name     will be handed over to QWidget
    * @short Constructor
    */
-KLed( KLed::Color ledcolor, QWidget *parent = 0, const char *name = 0 );
+  KLed(const QColor &col=Qt::green, QWidget *parent=0, const char *name=0);
 
   /**
    * Constructor with the ledcolor, ledstate, ledlook,
@@ -101,36 +102,29 @@ KLed( KLed::Color ledcolor, QWidget *parent = 0, const char *name = 0 );
    * @param name     will be handed over to QWidget
    * @short Constructor
    */
-KLed( KLed::Color ledcolor, KLed::State state, KLed::Look look, QWidget *parent = 0, const char *name = 0 );
+  KLed(const QColor& col, KLed::State st, KLed::Look look, KLed::Shape shape, 
+       QWidget *parent=0, const char *name=0);
 
   /**
    * Hands back the current state of the widget (on/off)
    * see enum State
    * @short returns led state
    */
-  inline State getState() const;
+  State state() const;
 
   /**
    * Hands back the color of the widget
    * see enum Color
    * @short returns led color
    */
-  inline Color getColor() const;
-
-  /**
-   * Hands back the color of the widget in RGB value.
-   * The value depends on the state of the the led (on, off)
-   * see QRgb
-   * @short returns led color in RGB value
-   */
-  inline QRgb getRgbColor() const;
+  const QColor color() const;
 
   /**
    * Hands back the look of the widget
    * see enum Look
    * @short returns led look
    */
-  inline  Look getLook() const;
+   Look look() const;
 
   /**
    * sets the state of the widget to On or Off.
@@ -140,15 +134,17 @@ KLed( KLed::Color ledcolor, KLed::State state, KLed::Look look, QWidget *parent 
    * @param state the led state on or off
    * @short set led state
    */
-  inline void setState( State state );
-
+  void setState( State state );
+  
+  /** Set the shape. */
+  void setShape(Shape s);
   /**
    * toggles the state of the led from Off to On and vice versa.
    * The widget will be repainted when returning to the main
    * event loop.
    * @short toggles led on->off / off->on
    */
-  inline void toggleState();
+  void toggleState();
 
   /**
    * Sets the color of the widget.
@@ -160,7 +156,7 @@ KLed( KLed::Color ledcolor, KLed::State state, KLed::Look look, QWidget *parent 
    * @param color new Color of the led
    * @short sets the led color
    */
-  inline void setColor( KLed::Color color );
+  void setColor(const QColor& color);
 
   /**
    * Sets the look of the widget.
@@ -173,109 +169,43 @@ KLed( KLed::Color ledcolor, KLed::State state, KLed::Look look, QWidget *parent 
    * @param look new look of the led
    * @short sets led look
    */
-  inline void setLook( Look look );
+  void setLook( Look look );
 
-public slots:
+  public slots:
 
-  /**
-   * toggles the state of the led from Off to On an vice versa.
-   * The widget repaints itself immediately.
-   */
-  inline void toggle();
+    /**
+     * toggles the state of the led from Off to On an vice versa.
+     * The widget repaints itself immediately.
+     */
+    void toggle();
 
   /**
    * sets the state of the widget to On.
    * The widget will be painted immediately.
    * see off(), toggle(), toggleState(), setState()
    */  
-  inline void on();
+  void on();
 
   /**
    * sets the state of the widget to Off.
    * The widget will be painted immediately.
    * see also: on(), toggle(), toggleState(), setState()
    */
-  inline void off();
+  void off();
 
-protected:
+ protected:
   void paintEvent( QPaintEvent * );
-
-private:
+  
   void paintflat();
   void paintround();
   void paintsunken();
+  void paintrect();
 
-  static const QRgb lightcolor[];
-  static const QRgb darkcolor[];
   State led_state;
-  Color led_color;
+  QColor current_color;
+  QColor led_color;
   Look  led_look;
-  QRgb current_color;
+  Shape led_shape;
 };
-
-KLed::State
-KLed::getState() const
-{ return led_state; }
-
-KLed::Color
-KLed::getColor() const
-{ return led_color; }
-
-QRgb
-KLed::getRgbColor() const
-{ return current_color; }
-
-KLed::Look
-KLed::getLook() const
-{ return led_look; }
-
-void
-KLed::setState( State state )
-{
-  if (led_state != state) {
-    led_state = state;
-    current_color=led_state ? lightcolor[led_color] : darkcolor[led_color];
-    update();
-  }
-}
-
-void
-KLed::toggleState()
-{
-  led_state = (State)!led_state;
-  current_color = led_state ? lightcolor[led_color] : darkcolor[led_color];
-  update();
- }
-
-void
-KLed::setColor( KLed::Color color)
-{ 
-  if (led_color != color) {
-    led_color = color;
-    current_color = led_state ? lightcolor[led_color] : darkcolor[led_color];
-    update(); 
-  }
-}
-
-void
-KLed::setLook( Look look )
-{
-  if (led_look != look) {
-    led_look = look;
-    update();
-  }
-}
-
-void
-KLed::toggle()
-{ toggleState(); };
-
-void
-KLed::on()
-{ setState(On); };
-
-void
-KLed::off()
-{ setState(Off); };
 
 #endif
