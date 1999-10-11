98d59707a6902259d10a89bc8c362ce8c03ba3ca
Torben Weis <weis@kde.org>
1999-10-11 11:11:07 +0000

    Addes KSpell test for modal spell checking
    svn path=/trunk/kdelibs/; revision=30592

---
+++ kdetest/kspelltest.cpp
@@ -0,0 +1,22 @@
+#include "kspell.h"
+#include "kapp.h"
+
+#include <qstring.h>
+#include <qlabel.h>
+
+int main(int argc, char **argv)
+{
+    KApplication *app = new KApplication(argc, argv);
+
+    QString text( "I have noo idee of how to wride englisch or englisch" );
+    
+    KSpell::modalCheck( text );
+    
+    qDebug("Returned %s", text.latin1() );
+    
+    QLabel* l = new QLabel( text, (QWidget*)0 );
+    l->show();
+    
+    return(app->exec());
+}
+
