54941558394c2cf233d68187fea9dd25baa79146
Espen Sand <espen@kde.org>
1999-10-17 14:27:55 +0000

    1. Added void KIconLoaderCanvas::keyPressEvent(QKeyEvent *e) so that Key_Escape is ignored. This is useful when KIconLoaderCanvas is used in KIconLoaderDialog.
    2. KIconLoaderDialog is now based on KDialogBase (gaining correct captions
    underlining, spacings etc). I have removed the label that displays the name
    of the selected icon and the progressbar is no longer in the button area.
    The progressbar will only be displayed if there are more than ten items
    to be loaded.
    
    3. The KIconLoaderButton is now the parent of the dialog. This ensures
    better centering of the dialog.
    
    svn path=/trunk/kdelibs/; revision=31046

--- kdeui/kiconloaderdialog.cpp
+++ kdeui/kiconloaderdialog.cpp
@@ -1,30 +1,37 @@
-// -*- C++ -*-
-
-//
-//  kiconloaderdialog
-//
-//  Copyright (C) 1997 Christoph Neerfeld
-//  email:  Christoph.Neerfeld@home.ivm.de or chris@kde.org
-//
-//  This program is free software; you can redistribute it and/or modify
-//  it under the terms of the GNU Library General Public License as published by
-//  the Free Software Foundation; either version 2 of the License, or
-//  (at your option) any later version.
-//
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU Library General Public License for more details.
-//
-//  You should have received a copy of the GNU Library General Public License
-//  along with this program; if not, write to the Free Software
-//  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-//
+/*
+ *  -*- C++ -*-
+ *
+ *  kiconloaderdialog.
+ *
+ *  Copyright (C) 1997 Christoph Neerfeld
+ *  email:  Christoph.Neerfeld@home.ivm.de or chris@kde.org
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Library General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Library General Public License
+ *  along with this library; see the file COPYING.LIB.  If not, write to
+ *  the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ *  Boston, MA 02111-1307, USA.
+ *
+ */
 
 #include <qapplication.h>
 #include <qdir.h>
 #include <qpainter.h>
 #include <qwmatrix.h>
+
+#include <qlabel.h>
+#include <qcombobox.h>
+#include <qtimer.h>
+#include <qprogressbar.h>
 
 #include "kiconloaderdialog.h"
 
@@ -34,9 +41,11 @@
 #include <kglobal.h>
 #include <kstddirs.h>
 
+//
 //----------------------------------------------------------------------
 //---------------  KICONLOADERCANVAS   ---------------------------------
 //----------------------------------------------------------------------
+//
 KIconLoaderCanvas::KIconLoaderCanvas (QWidget *parent, const char *name )
     : QIconView( parent, name )
 {
@@ -107,197 +116,268 @@ void KIconLoaderCanvas::slotLoadDir()
     }
 }
 
+
+void KIconLoaderCanvas::slotCurrentChanged( QIconViewItem *item ) 
+{
+  emit nameChanged( (item != 0 ? item->text() : QString::null) );
+}
+
+QString KIconLoaderCanvas::getCurrent( void )
+{
+  return( !currentItem() ? QString::null : currentItem()->key() );
+}
+
+QString KIconLoaderCanvas::currentDir( void )
+{
+  return dir_name;
+}
+
+
+//
+// 1999-10-17 Espen Sand
+// Added this one so that the KIconLoaderDialog can be closed by pressing
+// Key_Escape when KIconLoaderCanvas has focus.
+//
+void KIconLoaderCanvas::keyPressEvent(QKeyEvent *e)
+{
+  if( e->key() == Key_Escape )
+  {
+    e->ignore();
+  }
+  else
+  {
+    QIconView::keyPressEvent(e);
+  }
+}
+
+
+
+
+//
 //----------------------------------------------------------------------
 //---------------  KICONLOADERDIALOG   ---------------------------------
 //----------------------------------------------------------------------
-
-void KIconLoaderDialog::init()
-{
-    setCaption(i18n("Select Icon"));
-    //---
-    i_filter = new KLineEdit(this);
-    i_filter->setGeometry(310, 8, 150, 24);
-    //---
-    l_filter = new QLabel( i18n("Filter:"), this );
-    l_filter->setGeometry( 310 - 50, 8, 40, 24 );
-    //---
-    canvas = new KIconLoaderCanvas(this);
-    canvas->setGeometry(10, 38, 450, 124);
-    //---
-    l_name = new QLabel("", this);
-    l_name->setGeometry(10, 165, 110, 24);
-    //---
-    cb_dirs = new QComboBox(FALSE, this);
-    cb_dirs->setGeometry(10, 8, 230, 24);
-    //---
-    ok = new QPushButton( i18n("OK"), this );
-    cancel = new QPushButton( i18n("Cancel"), this );
-    int w = QMAX( ok->sizeHint().width(), cancel->sizeHint().width() );
-    ok->resize( w, ok->sizeHint().height() );
-    cancel->resize( w, cancel->sizeHint().height() );
-
-    cancel->move( width() - cancel->width() - 10, canvas->y() + canvas->height() + 10 );
-    ok->move( width() - cancel->width() -  ok->width() - 15, cancel->y() );
-
-    progressBar = new QProgressBar( this );
-    progressBar->move( 10, cancel->y() );
-    progressBar->resize( width() - 20 - ok->x(), progressBar->sizeHint().height() );
-
-    connect( ok, SIGNAL(clicked()), this, SLOT(accept()) );
-    connect( cancel, SIGNAL(clicked()), this, SLOT(reject()) );
-    connect( canvas, SIGNAL(nameChanged(const QString&)), l_name, SLOT(setText(const QString&)) );
-    connect( canvas, SIGNAL(doubleClicked()), this, SLOT(accept()) );
-    connect( canvas, SIGNAL(interrupted()), this, SLOT(needReload()) );
-    connect( i_filter, SIGNAL(returnPressed()), this, SLOT(filterChanged()) );
-    connect( cb_dirs, SIGNAL(activated(const QString&)), this, SLOT(dirChanged(const QString&)) );
-    changeDirs(KGlobal::dirs()->resourceDirs("toolbar"));
-    connect( canvas, SIGNAL( startLoading( int ) ),
-	     this, SLOT( initProgressBar( int ) ) );
-    connect( canvas, SIGNAL( progress( int ) ),
-	     this, SLOT( progress( int ) ) );
-    connect( canvas, SIGNAL( finished () ),
-	     this, SLOT( hideProgressBar() ) );
-
-    resize( 470, 350 );
-    setMinimumSize( 470, 250 );
-}
-
+//
 KIconLoaderDialog::KIconLoaderDialog ( QWidget *parent, const char *name )
-    : QDialog( parent, name, TRUE )
+  : KDialogBase( parent, name, TRUE, i18n("Select Icon"), Help|Ok|Cancel, Ok )
 {
-    icon_loader = KGlobal::iconLoader();
-    init();
+  icon_loader = KGlobal::iconLoader();
+  init();
 }
 
-KIconLoaderDialog::KIconLoaderDialog ( KIconLoader *loader, QWidget *parent, const char *name )
-    : QDialog( parent, name, TRUE )
+KIconLoaderDialog::KIconLoaderDialog ( KIconLoader *loader, QWidget *parent, 
+				       const char *name )
+  : KDialogBase( parent, name, TRUE, i18n("Select Icon"), Help|Ok|Cancel, Ok )
 {
-    icon_loader = loader;
-    init();
+  icon_loader = loader;
+  init();
 }
+
+void KIconLoaderDialog::init( void )
+{
+  QWidget *page = new QWidget( this );
+  setMainWidget( page );
+  setInitialSizeStep( QSize(0,100) );
+  
+  QVBoxLayout *topLayout = new QVBoxLayout( page, 0, spacingHint() );
+  QHBoxLayout *hbox = new QHBoxLayout();
+  topLayout->addLayout(hbox);
+
+  cb_dirs = new QComboBox(FALSE, page);
+  hbox->addWidget( cb_dirs, 10 );
+  
+  i_filter = new KLineEdit(page);
+  l_filter = new QLabel( i_filter, i18n("&Filter:"), page );
+  hbox->addWidget( l_filter );
+  hbox->addWidget( i_filter );
+
+  canvas = new KIconLoaderCanvas(page);
+  canvas->setMinimumSize( 450, 125 );
+  topLayout->addWidget( canvas );
+
+  progressBar = new QProgressBar( page );
+  topLayout->addWidget( progressBar );
+
+  connect( canvas, SIGNAL(doubleClicked()), this, SLOT(accept()) );
+  connect( canvas, SIGNAL(interrupted()), this, SLOT(needReload()) );
+  connect( i_filter, SIGNAL(returnPressed()), this, SLOT(filterChanged()) );
+  connect( cb_dirs, SIGNAL(activated(const QString&)), this, 
+	   SLOT(dirChanged(const QString&)) );
+  changeDirs(KGlobal::dirs()->resourceDirs("toolbar"));
+  connect( canvas, SIGNAL( startLoading( int ) ),
+	   this, SLOT( initProgressBar( int ) ) );
+  connect( canvas, SIGNAL( progress( int ) ),
+	   this, SLOT( progress( int ) ) );
+  connect( canvas, SIGNAL( finished () ),
+	   this, SLOT( hideProgressBar() ) );
+
+  topLayout->activate();
+}
+
 
 KIconLoaderDialog::~KIconLoaderDialog()
 {
-    disconnect( ok );
-    disconnect( cancel );
-    disconnect( canvas );
-}
-
-void KIconLoaderDialog::reject()
-{
-    QDialog::reject();
 }
 
 void KIconLoaderDialog::needReload()
 {
-    i_filter->setText("");
+  i_filter->setText("");
+}
+
+void KIconLoaderDialog::initProgressBar( int steps ) 
+{
+  //
+  // 1999-10-17 Espen Sand
+  // If we are going to display a small number of new icons then it
+  // looks quite stupid to display a progressbar that is only going
+  // to be visible in a fraction of a second. The number below used as
+  // a threshold is of course a compromise.
+  //
+  if( steps < 10 )
+  {
+    hideProgressBar();
+  }
+  else
+  {
+    progressBar->setTotalSteps( steps );
+    progressBar->reset();
+    progressBar->show();
+  }
+}
+
+void KIconLoaderDialog::progress( int p ) 
+{
+  progressBar->setProgress( p );
+}
+
+void KIconLoaderDialog::hideProgressBar( void ) 
+{
+  //
+  // 1999-10-17 Espen Sand
+  // Sometimes the progressbar is not hidden even if I tell it to
+  // do so. This happens both with the old version and with this one
+  // (which relies on Qt layouts). The 'processOneEvent()' seems to kill
+  // this "feature".
+  //
+  for( int i=0; i<100 && progressBar->isVisible() == true; i++ )
+  {
+    progressBar->hide();
+    kapp->processOneEvent();
+  }
+}
+
+void KIconLoaderDialog::changeDirs( const QStringList &l ) 
+{ 
+  cb_dirs->clear(); 
+  cb_dirs->insertStringList(l); 
 }
 
 int KIconLoaderDialog::exec(QString filter)
 {
-    setResult( 0 );
-//     if( filter != i_filter->text() )
-//     {
-	canvas->loadDir( cb_dirs->currentText(), filter );
-	i_filter->setText( filter );
-//     } else
-// 	canvas->orderItemsInGrid();
-    show();
-    return result();
-}
-
-void KIconLoaderDialog::resizeEvent( QResizeEvent * )
-{
-    int w = width();
-    int h = height();
-    canvas->resize( w - 20, h - i_filter->height() - ok->height() - 30 );
-    l_name->resize( canvas->width(), 24 );
-    l_name->move( 10, 38 + canvas->height() );
-    i_filter->move( w - 160, 8 );
-    l_filter->move( w - 200, 8 );
-    cb_dirs->resize( canvas->width() - i_filter->width() - l_filter->width() - 18, 24 );
-    cancel->move( width() - cancel->width() - 10, canvas->y() + canvas->height() + 10 );
-    ok->move( width() - cancel->width() -  ok->width() - 15, cancel->y() );
-    progressBar->move( 10, cancel->y() );
-    progressBar->resize( width() - 20 - ( width() - ok->x() ), progressBar->sizeHint().height() );
+  setResult( 0 );
+  //if( filter != i_filter->text() )
+  //{
+  canvas->loadDir( cb_dirs->currentText(), filter );
+  i_filter->setText( filter );
+  //} else
+  //canvas->orderItemsInGrid();
+  show();
+  return result();
 }
 
 void KIconLoaderDialog::filterChanged()
 {
-    canvas->loadDir( cb_dirs->currentText(), i_filter->text() );
+  canvas->loadDir( cb_dirs->currentText(), i_filter->text() );
 }
 
 void KIconLoaderDialog::dirChanged(const QString& dir)
 {
-    canvas->loadDir( dir, i_filter->text() );
+  canvas->loadDir( dir, i_filter->text() );
 }
 
 QPixmap KIconLoaderDialog::selectIcon( QString &name, const QString &filter)
 {
-    QPixmap pixmap;
-    QString pix_name, old_filter;
-    old_filter = i_filter->text();
-    if( old_filter.isEmpty() )
-	old_filter = filter;
-    if( exec(old_filter) )
-    {
-	if( !(pix_name = canvas->getCurrent()).isNull() )
-	    // David : give a full path to loadIcon
-	    pixmap = icon_loader->loadIcon( canvas->currentDir() + "/" + pix_name );
-    }
-    name = pix_name;
-    return pixmap;
+  QPixmap pixmap;
+  QString pix_name, old_filter;
+  old_filter = i_filter->text();
+  if( old_filter.isEmpty() )
+    old_filter = filter;
+  if( exec(old_filter) )
+  {
+    if( !(pix_name = canvas->getCurrent()).isNull() )
+      // David : give a full path to loadIcon
+      pixmap = icon_loader->loadIcon( canvas->currentDir() + "/" + pix_name );
+  }
+  name = pix_name;
+  return pixmap;
 }
 
-KIconLoaderButton::KIconLoaderButton( QWidget *_parent ) : QPushButton( _parent )
+
+//
+//----------------------------------------------------------------------
+//---------------  KICONLOADERBUTTON   ---------------------------------
+//----------------------------------------------------------------------
+//
+KIconLoaderButton::KIconLoaderButton( QWidget *_parent ) 
+  : QPushButton( _parent )
 {
-    iconStr = "";
-    resType = "toolbar";
-    connect( this, SIGNAL( clicked() ), this, SLOT( slotChangeIcon() ) );
-    iconLoader = KGlobal::iconLoader();
-    loaderDialog = new KIconLoaderDialog();
+  iconStr = "";
+  resType = "toolbar";
+  connect( this, SIGNAL( clicked() ), this, SLOT( slotChangeIcon() ) );
+  iconLoader = KGlobal::iconLoader();
+  loaderDialog = new KIconLoaderDialog( this );
 }
 
-KIconLoaderButton::KIconLoaderButton( KIconLoader *_icon_loader, QWidget *_parent ) : QPushButton( _parent )
+KIconLoaderButton::KIconLoaderButton( KIconLoader *_icon_loader, 
+				      QWidget *_parent ) 
+  : QPushButton( _parent )
 {
-    iconStr = "";
-    resType = "toolbar";
-    connect( this, SIGNAL( clicked() ), this, SLOT( slotChangeIcon() ) );
-    loaderDialog = new KIconLoaderDialog( _icon_loader );
-    iconLoader = _icon_loader;
+  iconStr = "";
+  resType = "toolbar";
+  connect( this, SIGNAL( clicked() ), this, SLOT( slotChangeIcon() ) );
+  loaderDialog = new KIconLoaderDialog( _icon_loader, this );
+  iconLoader = _icon_loader;
 }
 
 void KIconLoaderButton::setIconType(const QString& _resType)
 {
-    resType = _resType;
-    loaderDialog->changeDirs(KGlobal::dirs()->resourceDirs(resType));
+  resType = _resType;
+  loaderDialog->changeDirs(KGlobal::dirs()->resourceDirs(resType));
 
-    // Reload icon (might differ in new resource type)
-    if(!iconStr.isEmpty())
-	setIcon(iconStr);
+  // Reload icon (might differ in new resource type)
+  if(!iconStr.isEmpty())
+    setIcon(iconStr);
 }
 
 void KIconLoaderButton::slotChangeIcon()
 {
-    QString name;
-    QPixmap pix = loaderDialog->selectIcon( name, "*" );
-    if( !pix.isNull() )
-    {
-	setPixmap(pix);
-	iconStr = name;
-	emit iconChanged( iconStr );
-    }
+  QString name;
+  QPixmap pix = loaderDialog->selectIcon( name, "*" );
+  if( !pix.isNull() )
+  {
+    setPixmap(pix);
+    iconStr = name;
+    emit iconChanged( iconStr );
+  }
 }
 
 void KIconLoaderButton::setIcon(const QString& _icon)
 {
-    iconStr = _icon;
-    setPixmap( locate(resType, iconStr) );
+  iconStr = _icon;
+  setPixmap( locate(resType, iconStr) );
 }
 
 KIconLoaderButton::~KIconLoaderButton()
 {
-    delete loaderDialog;
+  delete loaderDialog;
 }
 #include "kiconloaderdialog.moc"
+
+
+
+
+
+
+
+
 
--- kdeui/kiconloaderdialog.h
+++ kdeui/kiconloaderdialog.h
@@ -1,50 +1,50 @@
-// -*- C++ -*-
-
-//
-//  kiconloaderdialog
-//
-//  Copyright (C) 1997 Christoph Neerfeld
-//  email:  Christoph.Neerfeld@home.ivm.de or chris@kde.org
-//
-//  This program is free software; you can redistribute it and/or modify
-//  it under the terms of the GNU Library General Public License as published by
-//  the Free Software Foundation; either version 2 of the License, or
-//  (at your option) any later version.
-//
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU Library General Public License for more details.
-//
-//  You should have received a copy of the GNU Library General Public License
-//  along with this program; if not, write to the Free Software
-//  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-//
+/*
+ *  -*- C++ -*-
+ *
+ *  kiconloaderdialog.
+ *
+ *  Copyright (C) 1997 Christoph Neerfeld
+ *  email:  Christoph.Neerfeld@home.ivm.de or chris@kde.org
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Library General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Library General Public License
+ *  along with this library; see the file COPYING.LIB.  If not, write to
+ *  the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ *  Boston, MA 02111-1307, USA.
+ *
+ */
 
 // CHANGES
 // Torben, added KIconLoaderButton
 
+
+
 #ifndef KICONLOADERDIALOG_H
 #define KICONLOADERDIALOG_H
 
-#include <qapplication.h>
-#include <qlist.h>
 #include <qpixmap.h>
-#include <qstrlist.h>
 #include <qstring.h>
-#include <qtableview.h>
-#include <qdialog.h>
-#include <qlabel.h>
 #include <qpushbutton.h>
-#include <qcombobox.h>
-#include <qtimer.h>
-#include <qprogressbar.h>
 #include <qiconview.h>
 
 #include <kapp.h>
+#include <kdialogbase.h>
 #include <kiconloader.h>
 
+class QComboBox;
+class QLabel;
 class QLineEdit;
+class QProgressBar;
+class QTimer;
 
 /**
 * Internal display class for @ref KIconLoaderDialog
@@ -54,16 +54,17 @@ class QLineEdit;
 */
 class KIconLoaderCanvas : public QIconView
 {
-    Q_OBJECT
-public:
+  Q_OBJECT
+
+  public:
     KIconLoaderCanvas (QWidget *parent=0, const char* name=0);
     virtual ~KIconLoaderCanvas();
 
     void loadDir(QString dirname, QString filter);
-    QString getCurrent() { if ( !currentItem() ) return QString::null; return currentItem()->key(); }
-    QString currentDir() { return dir_name; }
+    QString getCurrent( void );
+    QString currentDir( void );
 
-signals:
+  signals:
     void nameChanged( const QString& );
     void doubleClicked();
     void interrupted();
@@ -71,106 +72,100 @@ signals:
     void progress( int p );
     void finished();
 
-private slots:
-    void slotLoadDir();
-    void slotCurrentChanged( QIconViewItem *item ) {
-	if ( item )
-	    emit nameChanged( item->text() );
-	else
-	    emit nameChanged( QString::null );
-    }
+  protected:
+    /**
+     * Makes sure Key_Escape is ignored
+     */
+    virtual void keyPressEvent(QKeyEvent *e);
 
-private:
+  private slots:
+    void slotLoadDir();
+    void slotCurrentChanged( QIconViewItem *item );
+
+  private:
     QString dir_name, filter;
     QTimer *loadTimer;
 
 };
 
+
 /**
-* Dialog for interactive selection of icons.
-*
-* KIconLoaderDialog is a derived class from QDialog.
-* It provides one function selectIcon() which displays a dialog.
-* This dialog lets you select the icons within the IconPath by image.
-*/
-class KIconLoaderDialog : public QDialog
+ * Dialog for interactive selection of icons.
+ *
+ * KIconLoaderDialog is a derived class from @ref KDialogBase.
+ * It provides one function selectIcon() which displays a dialog.
+ * This dialog lets you select the icons within the IconPath by image.
+ */
+class KIconLoaderDialog : public KDialogBase
 {
-    Q_OBJECT
-public:
+  Q_OBJECT
+
+  public:
     /**
-       The KIconLoaderDialog is a modal dialog; i.e. it has its own eventloop
-       and the normal program will stop after a call to selectIcon() until
-       selectIcon() returns.
-       This constructor creates a KIconLoaderDialog that will call
-       KApplication::getKApplication()->getIconLoader() to load any icons.
-       Note that it will not use this KIconLoader to display the icons, but
-       the QPixmap that it returns will be know to this KIconLoader.
-       KIconLoaderDialog caches all icons it has loaded as long as they are in the
-       same directory between two calls to selectIcon(). So it is a good idea to
-       delete the KIconLoaderDialog when it is not needed anymore.
-    */
+     * The KIconLoaderDialog is a modal dialog; i.e. it has its own eventloop
+     * and the normal program will stop after a call to selectIcon() until
+     * selectIcon() returns.
+     * This constructor creates a KIconLoaderDialog that will call
+     * KApplication::getKApplication()->getIconLoader() to load any icons.
+     * Note that it will not use this KIconLoader to display the icons, but
+     * the QPixmap that it returns will be know to this KIconLoader.
+     * KIconLoaderDialog caches all icons it has loaded as long as they 
+     * are in the same directory between two calls to selectIcon(). So it 
+     * is a good idea to delete the KIconLoaderDialog when it is not 
+     * needed anymore.
+     */
     KIconLoaderDialog ( QWidget *parent=0, const char *name=0 );
 
     /**
-       If you want to use another KIconLoader you can create the KIconLoaderDialog
-       with this constructor which accepts a pointer to a KIconLoader.
-       Make sure that this pointer is valid.
-    */
-    KIconLoaderDialog ( KIconLoader *loader, QWidget *parent=0, const char *name=0  );
+     * If you want to use another KIconLoader you can create the 
+     * KIconLoaderDialog with this constructor which accepts a pointer to a 
+     * KIconLoader. Make sure that this pointer is valid.
+     */
+    KIconLoaderDialog ( KIconLoader *loader, QWidget *parent=0, 
+			const char *name=0  );
 
     /**
-       Destructor
-    */
+     * Destructor
+     */
     ~KIconLoaderDialog ();
 
     /**
-       Select an icon from a modal choose dialog.
-
-       This function pops up a modal dialog and lets you select an icon by its
-       picture not name. The function returns a QPixmap object and the icons
-       name in 'name'
-       if the user has selected an icon, or null if the user has pressed the
-       cancel button. So check the result before taking any action.
-       The argument filter specifies a filter for the names of the icons to
-       display. For example "*" displays all icons and "mini*" displays only
-       those icons which names start with 'mini'.
-    */
+     * Get an icon from a modal selector dialog.
+     * This method pops up a modal dialog and lets you select an icon by its
+     * picture not name. The function returns a QPixmap object and the icons
+     * name in 'name'.
+     * if the user has selected an icon, or null if the user has pressed the
+     * cancel button. So check the result before taking any action.
+     * The argument filter specifies a filter for the names of the icons to
+     * display. For example "*" displays all icons and "mini*" displays only
+     * those icons which names start with 'mini'.
+     */
     QPixmap selectIcon( QString &name, const QString &filter);
 
     /**
      * sets the directories to choose from. By default these are
      * all directories that keep toolbar icons
      */
-    void changeDirs( const QStringList &l ) { cb_dirs->clear(); cb_dirs->insertStringList(l); }
+    void changeDirs( const QStringList &l );
     int exec(QString filter);
 
-protected slots:
+  protected:
+    void init();
+
+  protected slots:
     void filterChanged();
     void dirChanged(const QString&);
-    void reject();
     void needReload();
-    void initProgressBar( int steps ) {
-	progressBar->setTotalSteps( steps );
-	progressBar->reset();
-	progressBar->show();
-    }
-    void progress( int p ) {
-	progressBar->setProgress( p );
-    }
-    void hideProgressBar() {
-	progressBar->hide();
-    }
 
-protected:
-    void init();
-    virtual void resizeEvent( QResizeEvent *e );
+    void initProgressBar( int steps );
+    void progress( int p );
+    void hideProgressBar( void );
 
+  protected:
     KIconLoaderCanvas *canvas;
     QLabel	      *l_name;
     QLineEdit	      *i_filter;
     QLabel	      *l_filter;
-    QPushButton	      *ok;
-    QPushButton	      *cancel;
     QLabel	      *text;
     QComboBox	      *cb_dirs;
     KIconLoader	      *icon_loader;
