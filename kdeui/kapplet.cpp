fb03f74c9f72c0e086d005afb31462aa1e42f1bf
Matthias Ettrich <ettrich@troll.no>
1999-10-18 22:53:25 +0000

    reworked applets: they use DCOP instead of X-hacks now.
    Unfortunately, the API is incompatible (it's the same API but I renamed
    the two functions). Sorry, applet authors, but it's more logical as
    it is now.
    
    And one more thing: do _not_ call show() in your main function. All
    you should do is calling KApplet::setup(argc,argv). If the applet
    cannot dock into kicker, it will show itself.
    
    svn path=/trunk/kdelibs/; revision=31181

--- kdeui/kapplet.cpp
+++ kdeui/kapplet.cpp
@@ -1,16 +1,10 @@
 #include "qxembed.h"
 #include "kapplet.h"
-#include <X11/Xlib.h>
-#include <X11/Xatom.h>
 #include <unistd.h>
-
-static Atom kde_applet = 0;
-static Atom kde_applet_command = 0;
-
-//this has to be the same in kicker!
-#define KAPPLET_NEW 0 
-#define KAPPLET_MOVE 1
-#define KAPPLET_REMOVE 2
+#include <dcopclient.h>
+#include <kapp.h>
+#include <qcstring.h>
+#include <qdatastream.h>
 
 class KAppletData
 {
@@ -18,25 +12,19 @@ public:
     KAppletData()
     {
 	orientation = Qt::Horizontal;
-	applet_panner = 0;
     };
     ~KAppletData()
     {
     }
-    
+
     Qt::Orientation orientation;
-    WId applet_panner;
 };
 
 
 KApplet::KApplet( QWidget* parent, const char* name  )
-    : QWidget( parent, name )
+    : QWidget( parent, name ), DCOPObject()
 {
     d = new KAppletData;
-    if ( !kde_applet ) {
-	kde_applet =  XInternAtom(qt_xdisplay(), "KDE_APPLET", FALSE);
-	kde_applet_command = XInternAtom(qt_xdisplay(), "KDE_APPLET_COMMAND", FALSE);
-    }
 }
 
 KApplet::~KApplet()
@@ -44,146 +32,83 @@ KApplet::~KApplet()
     delete d;
 }
 
-void KApplet::setup( int& argc, char ** argv )
+void KApplet::init( int& argc, char ** argv )
 {
-    int actual_format;
-    Atom actual_type;
-    unsigned long nitems, bytesafter;
-    unsigned long *data;
-
-    Orientation orientation = Horizontal;
-    d->applet_panner = 0;
     int ideal_width = 42;
     int ideal_height = 42;
-    
-    XChangeProperty( qt_xdisplay(), winId(),
-		     kde_applet_command, XA_STRING, 8, PropModeReplace,
-		     (unsigned char *)argv[0], qstrlen( argv[0] ) );
-    
-    if(XGetWindowProperty (qt_xdisplay(), qt_xrootwin(), kde_applet, 0L, 4L,
-			   False, kde_applet, &actual_type,
-			   &actual_format, &nitems,
-			   &bytesafter, (unsigned char **)&data)==Success) {
-	d->applet_panner = data[0];
-	ideal_width = data[1];
-	ideal_height = data[2];
-	orientation = (Orientation) data[3];
-	XFree(( unsigned char* ) data );
+
+    DCOPClient* dcop = kapp->dcopClient();
+
+    if (!dcop->isAttached() ) {
+	if ( !dcop->attach() )
+	    goto error;
     }
 
+    // tell kicker that we are here and want be docked
+    {
+	QCString replyType;
+	QByteArray data, replyData;
+	QDataStream dataStream( data, IO_WriteOnly );
+	dataStream << objId();
 
-    init ( orientation, ideal_width, ideal_height );
-    
-    if ( d->applet_panner != 0 ) {
-	XEvent ev;
-	long mask;
-	memset(&ev, 0, sizeof(ev));
-	ev.xclient.type = ClientMessage;
-	ev.xclient.window = d->applet_panner;
-	ev.xclient.message_type = kde_applet;
-	ev.xclient.format = 32;
-	ev.xclient.data.l[0] = winId();
-	ev.xclient.data.l[1] = KAPPLET_NEW;
-	ev.xclient.data.l[2] = 0;
-	ev.xclient.data.l[3] = 0;
-	ev.xclient.data.l[4] = 0;
-	mask = 0L;
-	XSendEvent(qt_xdisplay(), d->applet_panner, False, mask, &ev);
-	XFlush( qt_xdisplay() );
-	int count = 0;
-	while ( !XCheckTypedWindowEvent( qt_xdisplay(), 
-					winId(), 
-					ReparentNotify,
-					 &ev ) ) {
-	    count++;
-	    if ( count > 100 ) {
-		exit(1);
-	    }
-	    usleep( 50000 );
-	}
-	XPutBackEvent( qt_xdisplay(), &ev );
+	// we use "call" to know whether it was really sucessful
+	if ( !dcop->call("kicker", "appletArea", "dockMe(QCString)", data, replyType, replyData ) )
+	    goto error;
     }
-    
-    
-    /*
 
-    int myargc = argc;
-    int i, j;
+    return;
 
-    j = 1;
-    for ( i=1; i<myargc; i++ ) {
-	if ( argv[i] && *argv[i] != '-' ) {
-	    argv[j++] = argv[i];
-	    continue;
-	}
-	QCString arg = argv[i];
-	if ( strcmp(arg,"-horizontal") == 0 ) {
-	    orientation = Horizontal;
-	    i++;
-	} else if ( strcmp(arg,"-vertical") == 0 ) {
-	    orientation = Vertical;
-	    i++;
-	} else if ( strcmp(arg,"-width") == 0 && i < myargc-1 ) {
-	    QCString s = argv[++i];
-	    ideal_width = s.toInt();
-	} else if ( strcmp(arg,"-height") == 0 && i < myargc-1 ) {
-	    QCString s = argv[++i];
-	    ideal_height = s.toInt();
-	} else
-	    argv[j++] = argv[i];
-    }
-    argc = j;
-
-    init ( orientation, ideal_width, ideal_height );
-    QXEmbed::processClientCmdline( this, argc, argv );
-    
-    */
+error:
+    // do something, at least
+    setupGeometry ( orientation(), ideal_width, ideal_height );
+    show();
 }
 
 
 void KApplet::removeRequest()
 {
-    if ( d->applet_panner != 0 ) {
-	XEvent ev;
-	long mask;
-	memset(&ev, 0, sizeof(ev));
-	ev.xclient.type = ClientMessage;
-	ev.xclient.window = d->applet_panner;
-	ev.xclient.message_type = kde_applet;
-	ev.xclient.format = 32;
-	ev.xclient.data.l[0] = winId();
-	ev.xclient.data.l[1] = KAPPLET_REMOVE;
-	ev.xclient.data.l[2] = 0;
-	ev.xclient.data.l[3] = 0;
-	ev.xclient.data.l[4] = 0;
-	mask = 0L;
-	XSendEvent(qt_xdisplay(), d->applet_panner, False, mask, &ev);
-    }
+    QByteArray data;
+    QDataStream dataStream( data, IO_WriteOnly );
+    dataStream << objId();
+    kapp->dcopClient()->send("kicker", "appletArea", "removeMe(QCString)", data);
 }
 
 void KApplet::moveRequest()
 {
-    if ( d->applet_panner != 0 ) {
-	XEvent ev;
-	long mask;
-	memset(&ev, 0, sizeof(ev));
-	ev.xclient.type = ClientMessage;
-	ev.xclient.window = d->applet_panner;
-	ev.xclient.message_type = kde_applet;
-	ev.xclient.format = 32;
-	ev.xclient.data.l[0] = winId();
-	ev.xclient.data.l[1] = KAPPLET_MOVE;
-	ev.xclient.data.l[2] = 0;
-	ev.xclient.data.l[3] = 0;
-	ev.xclient.data.l[4] = 0;
-	mask = 0L;
-	XSendEvent(qt_xdisplay(), d->applet_panner, False, mask, &ev);
-    }
+    QByteArray data;
+    QDataStream dataStream( data, IO_WriteOnly );
+    dataStream << objId();
+    kapp->dcopClient()->send("kicker", "appletArea", "moveMe(QCString)", data);
 }
 
 
-void KApplet::init( Orientation orientation , int width, int height  )
+bool KApplet::process(const QCString &fun, const QByteArray &data,
+	     QCString& replyType, QByteArray &replyData)
 {
+    if ( fun == "setupGeometry(int,int,int)" ) {
+	QDataStream dataStream( data, IO_ReadOnly );
+	int orient;
+	int width, height;
+	dataStream >> orient  >> width >> height;
+	setupGeometry( Orientation(orient), width, height );
+	return TRUE;
+    } else if ( fun == "winId()" ) {
+	QDataStream reply( replyData, IO_WriteOnly );
+	reply << winId();
+	replyType = "WId";
+	return TRUE;
+    } else if ( fun == "restartCommand()" ) {
+	QDataStream reply( replyData, IO_WriteOnly );
+	reply << QCString( kapp->argv()[0] );
+	replyType = "QCString";
+	return TRUE;
+    }
+    return FALSE;
+}
+
+void KApplet::setupGeometry( Orientation orientation , int width, int height  )
+{
+    QApplication::flushX();
     d->orientation = orientation;
     resize(width, height );
 }
@@ -198,6 +123,5 @@ Qt::Orientation KApplet::orientation() const
 {
     return d->orientation;
 }
-
 
 #include "kapplet.moc"
--- kdeui/kapplet.h
+++ kdeui/kapplet.h
@@ -2,7 +2,7 @@
 #define KAPPLET_H
 
 #include <qwidget.h>
-
+#include <dcopobject.h>
 
 class KAppletData;
 
@@ -14,7 +14,7 @@ class KAppletData;
 * @author Matthias Ettrich <ettrich@kde.org>
 * @short KDE Panel Applet class
 */
-class KApplet : public QWidget
+class KApplet : public QWidget, DCOPObject
 {
     Q_OBJECT
 public:
@@ -30,41 +30,57 @@ public:
 
     /**
        Initializes the applet according to the passed command line
-       parameters.
+       parameters
 
-       Will evalutate the command line arguments and call init()
-       respectively.
+       Evalutates some command line arguments, docks into the
+       respective applet container and eventually call
+       setupGeometry().
      */
-    void setup( int& argc, char ** argv );
+    void init( int& argc, char ** argv );
 
     /**
-       Initializes the applet. This function needs to be reimplemented
-       by subclasses.
+       Sets up the applets geometry. This function needs to be
+       reimplemented by subclasses.
 
-       @param orientation is the applets orientation, either Qt::Horizontal or Qt::Vertical
-       @param width and @param height define the size of the applet. 
+       @param orientation is the applets orientation, either Qt::Horizontal 
+       or Qt::Vertical
+       @param width and 
+       @param height define the size of the applet.
+
+       The size parameter is meant as a hint in case an applet
+       supports different look&feels depending on the target size. Of
+       course, an applet could do these things in resizeEvent(), but
+       this way it's more convinient.
+
+       The applet container that will embed this applet will resize it
+       to the standard size of its applets. If your applet needs more
+       space, ensure to set a proper minimum size with
+       QWidget::setMinimumWidth() or QWidget::setMinimumHeight()
+       depending on the applets orientation.
        
-       The size parameter is  meant as a hint in case an applet supports different look&feels 
-       depending on the target size. Of course, an applet could do these things in resizeEvent(),
-       but this way it's more convinient.
-
-       The applet container that will embed this applet will resize it to the
-       standard size of its applets. If your applet needs more space, ensure to set a proper
-       minimum size with QWidget::setMinimumWidth() or QWidget::setMinimumHeight() depending
-       on the applets orientation.
+       Keep in mind that setupGeometry() may be called several times
+       during a life-cycle of your applet, for example when the applet
+       container is resized, moved or changes orientation.
      */
-    virtual void init( Orientation orientation, int width, int height );
+    virtual void setupGeometry( Orientation orientation, int width, int height );
+
 
     QSize sizeHint() const;
-    
-    
+
+
     /**
        Returns  the current orientation set by the last KApplet::init() call.
     */
     Orientation orientation() const;
+
     
+    
+    // dcop internal
+    bool process(const QCString &fun, const QByteArray &data,
+		 QCString& replyType, QByteArray &replyData);
+
 public slots:
-    
+
     /*!
       Tells the applet container that the applet wants to be
       removed. Subclasses should provide a context menu with a
@@ -78,6 +94,8 @@ public slots:
       "Move" item connected to this slot.
      */
     void moveRequest();
+
+    
 
 private:
     KAppletData* d;
