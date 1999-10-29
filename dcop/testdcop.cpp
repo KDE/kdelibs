ce308f33a2d658715f5b8ea9534239fce5b1d740
Kurt Granroth <granroth@kde.org>
1999-10-29 20:21:46 +0000

    appears nobody has compiled this in awhile.. it was missing an end */
    svn path=/trunk/kdelibs/; revision=32200

--- dcop/testdcop.cpp
+++ dcop/testdcop.cpp
@@ -21,6 +21,7 @@ AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  
 ******************************************************************
+*/
 
 #include <kapp.h>
 #include <qbitarray.h>
