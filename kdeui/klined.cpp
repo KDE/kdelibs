ebbd4bf52e39a529de26ed703aeceffd0d13feb4
Reginald Stadlbauer <reggie@kde.org>
1999-10-31 19:55:27 +0000

    There is a bug in Qt´s child deletion which made KLineEdit´s constructor crashing. As this bug is not easy to fix I added a workaround here until it is fixed, so that KLineEdit´s dtor doesn´t crash.
    Anyway, I have just added a similar RMB menu to QLineEdit, so we don´t
    need KLineEdit for that task anymore, we only need it for completion.
    
    Should I remove the RMB stuff from here now?
    
    svn path=/trunk/kdelibs/; revision=32427

--- kdeui/klined.cpp
+++ kdeui/klined.cpp
@@ -3,17 +3,17 @@
    Copyright (C) 1997 Sven Radej (sven.radej@iname.com)
    Copyright (c) 1999 Patrick Ward <PAT_WARD@HP-USA-om5.om.hp.com>
    Copyright (c) 1999 Preston Brown <pbrown@kde.org>
-   
+
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
    License as published by the Free Software Foundation; either
    version 2 of the License, or (at your option) any later version.
-   
+
    This library is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
    Library General Public License for more details.
-   
+
    You should have received a copy of the GNU Library General Public License
    along with this library; see the file COPYING.LIB.  If not, write to
    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
@@ -32,74 +32,76 @@
 #endif
 
 KLineEdit::KLineEdit(const QString &string, QWidget *parent, const char *name)
-  : QLineEdit(string, parent, name)
+    : QLineEdit(string, parent, name)
 {
-  pop = new QPopupMenu( this );
-  pmid[0] = pop->insertItem( "Cut", this, SLOT(doCut()) );
-  pmid[1] = pop->insertItem( "Copy", this, SLOT(doCopy()) );
-  pmid[2] = pop->insertItem( "Paste", this, SLOT(doPaste()) );
-  pmid[3] = pop->insertItem( "Clear", this, SLOT(doClear()) );
-  pop->insertSeparator();
-  pmid[4] = pop->insertItem( "Select All", this, SLOT(doSelect()) );
-  
-  installEventFilter (this);
+    pop = new QPopupMenu( this );
+    pmid[0] = pop->insertItem( "Cut", this, SLOT(doCut()) );
+    pmid[1] = pop->insertItem( "Copy", this, SLOT(doCopy()) );
+    pmid[2] = pop->insertItem( "Paste", this, SLOT(doPaste()) );
+    pmid[3] = pop->insertItem( "Clear", this, SLOT(doClear()) );
+    pop->insertSeparator();
+    pmid[4] = pop->insertItem( "Select All", this, SLOT(doSelect()) );
+
+    installEventFilter (this);
 }
 
 KLineEdit::KLineEdit (QWidget *parent, const char *name)
-  : QLineEdit (parent, name)
+    : QLineEdit (parent, name)
 {
-  pop = new QPopupMenu( this );
-  pmid[0] = pop->insertItem( "Cut", this, SLOT(doCut()) );
-  pmid[1] = pop->insertItem( "Copy", this, SLOT(doCopy()) );
-  pmid[2] = pop->insertItem( "Paste", this, SLOT(doPaste()) );
-  pmid[3] = pop->insertItem( "Clear", this, SLOT(doClear()) );
-  pop->insertSeparator();
-  pmid[4] = pop->insertItem( "Select All", this, SLOT(doSelect()) );
-  
-  installEventFilter (this);
+    pop = new QPopupMenu( this );
+    pmid[0] = pop->insertItem( "Cut", this, SLOT(doCut()) );
+    pmid[1] = pop->insertItem( "Copy", this, SLOT(doCopy()) );
+    pmid[2] = pop->insertItem( "Paste", this, SLOT(doPaste()) );
+    pmid[3] = pop->insertItem( "Clear", this, SLOT(doClear()) );
+    pop->insertSeparator();
+    pmid[4] = pop->insertItem( "Select All", this, SLOT(doSelect()) );
+
+    installEventFilter (this);
 }
 
 KLineEdit::~KLineEdit ()
 {
-  removeEventFilter (this);
+    removeEventFilter (this);
+    delete pop;
+    pop = 0;
 }
 
 void KLineEdit::cursorAtEnd ()
 {
-  QKeyEvent ev( QEvent::KeyPress, Key_End, 0, 0 );
-  QLineEdit::keyPressEvent( &ev );
+    QKeyEvent ev( QEvent::KeyPress, Key_End, 0, 0 );
+    QLineEdit::keyPressEvent( &ev );
 }
 
 bool KLineEdit::eventFilter (QObject *, QEvent *e)
 {
-  if (e->type() == QEvent::KeyPress)
+    if (e->type() == QEvent::KeyPress)
     {
-      QKeyEvent *k = (QKeyEvent *) e;
-      
-      if ( ((k->state() == ControlButton) && (k->key() == Key_S || k->key() == Key_D))
-	   || k->key() == Key_Tab) {   
-	      if (k->key() == Key_Tab || k->key() == Key_S)
-		      emit completion ();
-	      else
-		      emit rotation ();
-	      cursorAtEnd();
-	      return TRUE;
-      }
+	QKeyEvent *k = (QKeyEvent *) e;
+
+	if ( ((k->state() == ControlButton) && (k->key() == Key_S || k->key() == Key_D))
+	     || k->key() == Key_Tab) {
+	    if (k->key() == Key_Tab || k->key() == Key_S)
+		emit completion ();
+	    else
+		emit rotation ();
+	    cursorAtEnd();
+	    return TRUE;
+	}
     }
-  return FALSE;
+    return FALSE;
 }
 
 void KLineEdit::mousePressEvent( QMouseEvent *e )
 {
-  if(e->button() != RightButton) {
-    QLineEdit::mousePressEvent( e );
-    return;
-  }
+    if(e->button() != RightButton) {
+	QLineEdit::mousePressEvent( e );
+	return;
+    }
 
-  int n = text().length();
-  pop->setItemEnabled( pmid[0], hasMarkedText() );
-  pop->setItemEnabled( pmid[1], hasMarkedText() );
-  pop->setItemEnabled( pmid[3], (n > 0) ? true : false );
-  pop->popup( e->globalPos() );
-}                                                                               
+    int n = text().length();
+    pop->setItemEnabled( pmid[0], hasMarkedText() );
+    pop->setItemEnabled( pmid[1], hasMarkedText() );
+    pop->setItemEnabled( pmid[3], (n > 0) ? true : false );
+    pop->popup( e->globalPos() );
+}
 
