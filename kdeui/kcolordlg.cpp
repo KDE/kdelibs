7c0bc3d1748d83d5c4fa94d8ab84e69b7ef7de7b
Reginald Stadlbauer <reggie@kde.org>
1999-10-31 11:49:24 +0000

    fix
    svn path=/trunk/kdelibs/; revision=32372

--- kdeui/kcolordlg.cpp
+++ kdeui/kcolordlg.cpp
@@ -377,9 +377,10 @@ void KColorPatch::dropEvent( QDropEvent *event)
 
 KColorDialog::KColorDialog( QWidget *parent, const char *name, bool modal )
   :KDialogBase( parent, name, modal, i18n("Select Color"), Help|Ok|Cancel,
-		Ok, true )
+		0, true )
 {
-  setHelp( "kcolordialog.html", QString::null, QString::null );
+    
+    setHelp( "kcolordialog.html", QString::null, QString::null );
   setResizeMode( ResizeFixed ); // Comment if resizeable
   connect( this, SIGNAL(okClicked(void)),this,SLOT(slotWriteSettings(void)));
 
