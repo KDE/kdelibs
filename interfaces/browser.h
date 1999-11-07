a97c8b067a5f66bc1ffcd029842dfb4177f1efb1
David Faure <faure@kde.org>
1999-11-07 14:58:36 +0000

    Added virtual void configure() with empty default implementation.
    svn path=/trunk/kdelibs/; revision=33114

--- interfaces/browser.h
+++ interfaces/browser.h
@@ -92,6 +92,11 @@ public:
   virtual int xOffset() = 0;
   virtual int yOffset() = 0;
   virtual void stop() = 0;
+  /**
+   * Re-read and apply configuration.
+   * Currently only for built-in views.
+   */
+  virtual void configure() { }
 
   virtual void saveState( QDataStream &stream )
   { stream << url() << (Q_INT32)xOffset() << (Q_INT32)yOffset(); }
