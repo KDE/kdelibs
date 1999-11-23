41edf31074e683ee7b61970aa6cf9fe7f95ab9be
Waldo Bastian <bastian@kde.org>
1999-11-23 19:16:44 +0000

    WABA: Added testcase for KUniqueApplication
    svn path=/trunk/kdelibs/; revision=34835

---
+++ kdecore/tests/kuniqueapptest.cpp
@@ -0,0 +1,68 @@
+/* This file is part of the KDE libraries
+    Copyright (c) 1999 Waldo Bastian <bastian@kde.org>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License version 2 as published by the Free Software Foundation.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+    Boston, MA 02111-1307, USA.
+*/
+
+#include "kuniqueapp.h"
+
+#include <unistd.h>
+#include <stdio.h>
+
+class TestApp : public KUniqueApplication
+{
+public:
+   TestApp(int argc, char *argv[])
+	: KUniqueApplication(argc, argv, "TestApp") { }
+   int newInstance( QValueList<QCString> params );
+};
+
+
+int
+TestApp::newInstance( QValueList<QCString> params )
+{
+   printf("%s PID = %d params = %d\n", name(), getpid(), params.count());
+   QValueList<QCString>::Iterator it = params.begin(); 
+   int i = 0;
+   for(;it != params.end(); it++, i++)
+   {
+      printf("arg %d: %s\n", i, (*it).data());
+   }
+   if ((params.count() != 2) || (params[1] == "--help"))
+   {
+       printf("Usage: %s [ --help | ok | exit ]\n", params[0].data());
+       return -1;
+   }
+   if (params[1] == "ok")
+   {
+       printf("OK\n");
+       return 1;
+   }
+   if (params[1] == "exit")
+   {
+       exit(0);
+   }
+   return 0;
+}
+
+int
+main(int argc, char *argv[])
+{
+   TestApp a(argc, argv);
+
+   printf("Running.\n");
+   kapp->exec();   
+   printf("Terminating.\n");
+}
