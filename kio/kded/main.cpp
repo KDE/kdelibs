14030f964977e8e71b28736cd7748e853e02a829
Waldo Bastian <bastian@kde.org>
1999-11-26 17:12:28 +0000

    WABA: Let KUniqueApp do the forking.
    svn path=/trunk/kdelibs/; revision=35109

--- kio/kded/main.cpp
+++ kio/kded/main.cpp
@@ -26,29 +26,14 @@
 
 int main(int argc, char *argv[])
 {
+     if (!KUniqueApplication::start(argc, argv, "kded"))
+     {
+        printf("KDED already running!\n");
+        exit(0);
+     }
      KUniqueApplication k(argc,argv, "kded", false /* not GUI */);
 
      KBuildSycoca *sycoca= new KBuildSycoca; // Build data base
      sycoca->recreate();
-
-#ifndef NOFORK     // define NOFORK to debug kded in gdb
-     QApplication::flushX();	
-     switch(fork()) {
-     case -1:
-	  fprintf(stderr, "kded: fork() failed!\n");
-	  break;
-     case 0:
-	  // ignore in child
-	  break;
-     default:
-	  // parent: exit immediatly
-          fprintf(stdout, "kded now running in the background\n");
-	  _exit(0);
-     }
-#endif
-
-     kapp->dcopClient()->attach();
-     kapp->dcopClient()->registerAs( kapp->name() );
-
      return k.exec(); // keep running
 }
