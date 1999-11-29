385a077182d8d97c7e4d106b8c2506df90b0005f
Waldo Bastian <bastian@kde.org>
1999-11-29 11:47:57 +0000

    WABA: Reverted previous patch. Behaviour is intented. Parent only returns after child has handled "newInstance()" event from event loop (both for first as well as for later invocations)
    svn path=/trunk/kdelibs/; revision=35412

--- kdecore/kuniqueapp.cpp
+++ kdecore/kuniqueapp.cpp
@@ -94,7 +94,7 @@ KUniqueApplication::start(int& argc, char** argv,
      }
      
      s_DCOPClient = dc;
-     result = 1;
+     result = 0;
      ::write(fd[1], &result, 1);
      ::close(fd[1]);
      return true; // Finished.
@@ -120,7 +120,7 @@ KUniqueApplication::start(int& argc, char** argv,
      ::close(fd[0]);
 
      if (result != 0)
-        ::exit(result); // Error occured in child or app not already running
+        ::exit(result); // Error occured in child.
 
      dc = new DCOPClient();
      if (!dc->attach())
