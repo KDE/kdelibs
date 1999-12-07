a818010b1d4acd84bec2e1be7b826aa15caf720d
David Faure <faure@kde.org>
1999-12-07 15:55:26 +0000

    Moved plugin-action docu to kplugin.h
    svn path=/trunk/kdelibs/; revision=36042

--- kparts/kplugin.h
+++ kparts/kplugin.h
@@ -4,6 +4,14 @@
 #include <qobject.h>
 #include <qaction.h>
 
+/**
+ * (TODO what is a plugin)
+ *
+ * If you want this plugin to be used by a part, you need to
+ * install the rc file under the directory
+ * "data" (KDEDIR/share/apps usually)+"/instancename/pluginactions/"
+ * where instancename is the name of the part's instance.
+ */
 class KPlugin : public QObject
 {
     Q_OBJECT
