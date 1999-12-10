be47799eb86f423babc744890c885b44dddbcde2
Waldo Bastian <bastian@kde.org>
1999-12-10 15:39:11 +0000

    WABA
    svn path=/trunk/kdelibs/; revision=36241

---
+++ kdecore/tests/krandomsequencetest.cpp
@@ -0,0 +1,55 @@
+/* This file is part of the KDE libraries
+    Copyright (c) 1999 Waldo Bastian <bastian@kde.org>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License version 2 as published by the Free Software Foundation.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+    Boston, MA 02111-1307, USA.
+*/
+
+#include "krandomsequence.h"
+#include "kapp.h"
+
+#include <stdio.h>
+
+int
+main(int argc, char *argv[])
+{
+   KApplication a(argc, argv, "krandomsequencetest");
+
+   long seed;
+   KRandomSequence seq;
+
+   seed = 2;
+   seq.setSeed(seed);printf("Seed = %4ld :", seed);
+   for(int i = 0; i < 20; i++)
+      printf("%3ld ", seq.getLong(100));
+   printf("\n");
+
+   seed = 0;
+   seq.setSeed(seed);printf("Seed = %4ld :", seed);
+   for(int i = 0; i < 20; i++)
+      printf("%3ld ", seq.getLong(100));
+   printf("\n");
+
+   seed = 0;
+   seq.setSeed(seed);printf("Seed = %4ld :", seed);
+   for(int i = 0; i < 20; i++)
+      printf("%3ld ", seq.getLong(100));
+   printf("\n");
+
+   seed = 2;
+   seq.setSeed(seed);printf("Seed = %4ld :", seed);
+   for(int i = 0; i < 20; i++)
+      printf("%3ld ", seq.getLong(100));
+   printf("\n");
+}
