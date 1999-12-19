dc31710753a2b4cf5fdc44bdcfca0774959094a5
Matthias Elter <m.elter@kde.org>
1999-12-19 10:04:39 +0000

    Cold Ions unthemed style. Currently havily based on Mosfet's Pillbox but this will change soon. :-)
    svn path=/trunk/kdelibs/; revision=36909

---
+++ kstyles/coldions/plugin.cpp
@@ -0,0 +1,29 @@
+#include "coldions.h"
+#include <klocale.h>
+
+extern "C" {
+    KStyle* allocate();
+    int minor_version();
+    int major_version();
+    const char *description();
+}
+
+KStyle* allocate()
+{
+    return(new ColdIonsStyle);
+}
+
+int minor_version()
+{
+    return(0);
+}
+
+int major_version()
+{
+    return(1);
+}
+
+const char *description()
+{
+    return(i18n("Cold Ions unthemed plugin"));
+}
