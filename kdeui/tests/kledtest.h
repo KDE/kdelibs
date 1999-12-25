0b5d727edbae86733e9476046f31529f58ccbe99
Mirko Boehm <mirko@kde.org>
1999-12-25 17:11:53 +0000

    Implemented a new test that shows all possible looks and shapes of the leds. Found out that circular shapes to not work at all. --Mirko.
    svn path=/trunk/kdelibs/; revision=37260

--- kdeui/tests/kledtest.h
+++ kdeui/tests/kledtest.h
@@ -7,31 +7,65 @@
 #include <stdlib.h>
 #include <kled.h>
 
-class ktmp : public QObject
+class KLedTest : public QWidget
 {
   Q_OBJECT
-public:
-  ktmp(KLed *l) : led(l), ledcolor(l->color()), ledlook(l->look()) {};
-  ~ktmp() {};
-  
-public slots:
-  void nextColor() 
-    {
-      // int tmp = (static_cast<int>(ledcolor) + 1) % 5;
-      // ledcolor=static_cast<KLed::Color>(tmp);
-      // led->setColor(ledcolor);
-      // led->repaint(); 
-    }
-  void nextLook() 
-    { 
-      int tmp = (static_cast<int>(ledlook) + 1) % 3;
-      led->setLook(static_cast<KLed::Look>(tmp));
-      led->repaint(false); 
-    }
 protected:
-  KLed *led;
-  QColor ledcolor;
-  KLed::Look  ledlook;
+  QTimer timer;
+  KLed *leds[KLed::NoOfShapes*KLed::NoOfLooks*KLed::NoOfStates];
+  const int LedWidth;
+  const int LedHeight;
+  const int Grid;
+  KLed::Shape shape;
+  KLed::Look look;
+  KLed::State state;
+  int x, y, index;
+public:
+  KLedTest(QWidget* parent=0)
+    : QWidget(parent, 0),
+      LedWidth(16),
+      LedHeight(10),
+      Grid(3)
+    {
+      y=Grid; index=0;
+      for(shape=KLed::NoShape; shape!=KLed::NoOfShapes; shape=(KLed::Shape)(shape+1))
+	{
+	  x=Grid;
+	  for(look=KLed::NoLook; look!=KLed::NoOfLooks; look=(KLed::Look)(look+1))
+	    {
+	      for(state=KLed::Off; state!=KLed::NoOfStates; state=(KLed::State)(state+1))
+		{
+		  leds[index]=new KLed(Qt::yellow, state,
+				       (KLed::Look)(look+1),
+				       (KLed::Shape)(shape+1), this);
+		  leds[index]->setGeometry(x, y, LedWidth, LedHeight);
+		  ++index;
+		  x+=Grid+LedWidth;
+		}
+	    }
+	  y+=Grid+LedHeight;
+	}
+      setFixedSize(x+Grid, y+Grid);
+      connect(&timer, SIGNAL(timeout()), SLOT(timeout()));
+      timer.start(500);
+    }
+  ~KLedTest() {};
+public slots:
+  void timeout() 
+    {
+      const int NoOfLeds=sizeof(leds)/sizeof(leds[0]);
+      int count;
+      // -----
+      for(count=0; count<NoOfLeds; ++count)
+	{
+	  if(leds[count]->state()==KLed::Off)
+	    {
+	      leds[count]->setState(KLed::On);
+	    } else {
+	      leds[count]->setState(KLed::Off);
+	    }
+	}
+    }
 };
 
 #endif
