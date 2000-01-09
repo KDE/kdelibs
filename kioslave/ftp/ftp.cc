9c776af4d33bb10d3b12cdf1495046eabdcff2b6
Bernhard Rosenkraenzer <bero@arklinux.org>
2000-01-09 20:46:19 +0000

    Fix compilation
    svn path=/trunk/kdebase/kioslave/ftp/; revision=37954

--- kioslave/ftp/ftp.cc
+++ kioslave/ftp/ftp.cc
@@ -492,9 +492,9 @@ bool Ftp::ftpOpenPASVDataConnection()
   }
 
   if ( setsockopt(sDatal, SOL_SOCKET,SO_KEEPALIVE, (char *) &on, (int) sizeof(on)) < 0 )
-    kDebugArea(7102, "Keepalive not allowed");
+    kDebugStringArea(7102, "Keepalive not allowed");
   if ( setsockopt(sDatal, SOL_SOCKET,SO_LINGER, (char *) &lng,(int) sizeof (lng)) < 0 )
-    kDebugArea(7102, "Linger mode was not allowed.");
+    kDebugStringArea(7102, "Linger mode was not allowed.");
   return 1;
 }
 
