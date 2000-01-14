0b308f20a51b2e1c5e71d8a713a29d7da6f88a13
Dawit Alemayehu <adawit@kde.org>
2000-01-14 02:34:08 +0000

    Misc changes. (Dawit A.)
    svn path=/trunk/kdelibs/; revision=38294

--- kdeui/tests/klineedittest.cpp
+++ kdeui/tests/klineedittest.cpp
@@ -1,23 +1,23 @@
 #include <klined.h>
 #include <qpushbutton.h>
 #include <kapp.h>
+#include <kcompletion.h>
 
 int main ( int argc, char **argv)
 {
-  KApplication a(argc, argv, "klineedittest");
+    KApplication a(argc, argv, "klineedittest");
+    //make a central widget to contain the other widgets
+    QWidget * w = new QWidget( );
+    KLineEdit *l = new KLineEdit( w, "mylineedit" );
+    l->enableCompletion();
+    l->resize(500,30);
+    l->setFocus();
+    QPushButton * push = new QPushButton( "Exit", w );
+    push->resize(100,30);
+    push->move(50,50);
+    QObject::connect( push, SIGNAL( clicked() ), &a, SLOT( closeAllWindows() ) );
+    a.setMainWidget(w);
+    w->show();
 
-        //make a central widget to contain the other widgets
-        QWidget * w = new QWidget( );
-        KLineEdit *l = new KLineEdit( w, "mylineedit" );
-        l->enableCompletion();
-        l->resize(500,30);
-        l->setFocus();
-        QPushButton * push = new QPushButton( "Exit", w );
-        push->resize(100,30);
-        push->move(50,50);
-        QObject::connect( push, SIGNAL( clicked() ), &a, SLOT( closeAllWindows() ) );
-        a.setMainWidget(w);
-        w->show();
-
-  return a.exec();
-}     
+    return a.exec();
+}
