8deb747a1184ac815a61106b4e2887d0d11793a2
Antonio Larrosa Jimenez <larrosa@kde.org>
2000-01-22 13:30:18 +0000

    Committed KMidSimpleAPI and some tests.
    Btw, does anybody knows if I can force libtool to generate
    libkmid with version 0.9.5 ?
    The only way I've found to do it is using -version-info 9:5:9,
    but I don't think that that's the best way.
    
    svn path=/trunk/kdelibs/; revision=38983

---
+++ libkmid/tests/apitest.cc
@@ -0,0 +1,25 @@
+#include <libkmid.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int main (int argc, char **argv)
+{
+  printf("Libkmid test2 . (C) 2000 Antonio Larrosa Jimenez . Malaga (Spain)\n");
+  printf("Using libkmid from a simple C++ application\n");
+
+  KMidSimpleAPI::kMidInit();
+  KMidSimpleAPI::kMidLoad("Layla.mid");
+  KMidSimpleAPI::kMidPlay();
+
+  for (int i=0;i<30;i++)
+  {
+    printf("%d/30 seconds\n",i+1);
+    sleep(1);
+  };    
+
+  KMidSimpleAPI::kMidStop();
+  KMidSimpleAPI::kMidDestruct();
+
+  return 0;
+};
+
---
+++ libkmid/tests/ctest.c
@@ -0,0 +1,45 @@
+#include <libkmid/libkmid.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int main (int argc, char **argv)
+{
+  int i;
+  char c;
+
+  printf("Test1 %s. Using libkmid from a simple C application\n",kMidVersion());
+  printf("%s\n",kMidCopyright());
+
+  if (kMidInit()!=0) 
+  {
+    printf("Error initializing libkmid\n");
+    return 0;
+  }
+
+  printf("Available devices :\n");
+  for (i=0;i<kMidDevices();i++)
+  {
+    printf(" %d) %s - %s\n",i,kMidName(i),kMidType(i));
+  }
+  printf("\nSelect one:");
+  c=getc(stdin);
+
+  kMidSetDevice((int)(c-'0'));
+
+  kMidLoad("Layla.mid");
+  kMidPlay();
+
+  for (i=0;i<45;i++)
+  {
+    printf("%d/45 seconds\n",i+1);
+    sleep(1);
+  }
+
+  kMidStop();
+  kMidDestruct();
+ 
+  
+  return 0;
+
+};
+
