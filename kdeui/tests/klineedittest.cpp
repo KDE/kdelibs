c1a34e7fbd760b3cd634d800d9c3075e6db90d2c
Dawit Alemayehu <adawit@kde.org>
2000-02-13 23:49:34 +0000

    Changed test code to conform to code changes in widget. (DA)
    svn path=/trunk/kdelibs/; revision=40703

--- kdeui/tests/klineedittest.cpp
+++ kdeui/tests/klineedittest.cpp
@@ -9,15 +9,17 @@ int main ( int argc, char **argv)
     KApplication a(argc, argv, "klineedittest");
     //make a central widget to contain the other widgets
     QWidget * w = new QWidget( );
+    // Create the widget with the last argument set to "true" to
+    // have KLineEdit automatically handle the completion and
+    // rotation signals.
     KLineEdit *l = new KLineEdit( w, "mylineedit", true );
-    l->setEnableContextMenu( true );
-    l->setEnableModeChanger( true );
+    l->setEnableContextMenu();
     // Shows of the value of the returnPressed signals with the QString argument.
     // We simply insert the entered items into the completion object.
     QObject::connect( l, SIGNAL( returnPressed( const QString& ) ), l->completionObject(), SLOT( addItem( const QString& ) ) );
     l->resize(500,30);
     l->setFocus();
-    QPushButton * push = new QPushButton( "Exit", w );
+    QPushButton * push = new QPushButton( "E&xit", w );
     push->resize(100,30);
     push->move(50,50);
     QObject::connect( push, SIGNAL( clicked() ), &a, SLOT( closeAllWindows() ) );
