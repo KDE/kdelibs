25c619efa57893e4408e4a372d2f87fa65cdd7fa
David Faure <faure@kde.org>
2000-02-27 02:47:59 +0000

    Added all error codes to the switch() for errorString(). Long, boring and tedious, but avoids "unknown error, send a full bugreport!" message box.
    svn path=/trunk/kdelibs/; revision=41890

--- kio/global.cpp
+++ kio/global.cpp
@@ -125,17 +125,14 @@ QString Job::errorString()
     case  KIO::ERR_UNSUPPORTED_ACTION:
       result = i18n( "Unsupported action\n%1" ).arg( m_errorText );
       break;
-    case  KIO::ERR_DOES_NOT_EXIST:
-      result = i18n( "The file or directory\n%1\ndoes not exist" ).arg( m_errorText );
-      break;
     case  KIO::ERR_IS_DIRECTORY:
       result = i18n( "%1\n is a directory,\nbut a file was expected" ).arg( m_errorText );
       break;
-    case  KIO::ERR_COULD_NOT_MKDIR:
-      result = i18n( "Could not make directory\n%1" ).arg( m_errorText );
+    case  KIO::ERR_IS_FILE:
+      result = i18n( "%1\n is a file,\nbut a directory was expected" ).arg( m_errorText );
       break;
-    case  KIO::ERR_COULD_NOT_RMDIR:
-      result = i18n( "Could not remove directory\n%1" ).arg( m_errorText );
+    case  KIO::ERR_DOES_NOT_EXIST:
+      result = i18n( "The file or directory\n%1\ndoes not exist" ).arg( m_errorText );
       break;
     case  KIO::ERR_FILE_ALREADY_EXIST:
       result = i18n( "A file named\n%1\ndoes already exist" ).arg( m_errorText );
@@ -143,8 +140,14 @@ QString Job::errorString()
     case  KIO::ERR_DIR_ALREADY_EXIST:
       result = i18n( "A directory named\n%1\ndoes already exist" ).arg( m_errorText );
       break;
+    case  KIO::ERR_UNKNOWN_HOST:
+      result = i18n( "Unknown host\n%1" ).arg( m_errorText );
+      break;
     case  KIO::ERR_ACCESS_DENIED:
       result = i18n( "Access denied to\n%1" ).arg( m_errorText );
+      break;
+    case  KIO::ERR_WRITE_ACCESS_DENIED:
+      result = i18n( "Access denied\nCould not write to\n%1" ).arg( m_errorText );
       break;
     case  KIO::ERR_CANNOT_ENTER_DIRECTORY:
       result = i18n( "Could not enter directory\n%1" ).arg( m_errorText );
@@ -154,12 +157,6 @@ QString Job::errorString()
       break;
     case  KIO::ERR_CYCLIC_LINK:
       result = i18n( "Found a cyclic link in\n%1" ).arg( m_errorText );
-      break;
-    case  KIO::ERR_CANNOT_CHMOD:
-      result = i18n( "Could not change permissions for\n%1" ).arg( m_errorText );
-      break;
-    case  KIO::ERR_WRITE_ACCESS_DENIED:
-      result = i18n( "Access denied\nCould not write to\n%1" ).arg( m_errorText );
       break;
     case  KIO::ERR_USER_CANCELED:
       result = i18n( "Action has been\ncanceled by the user\n%1" ).arg( m_errorText );
@@ -173,20 +170,11 @@ QString Job::errorString()
     case  KIO::ERR_COULD_NOT_CONNECT:
       result = i18n( "Could not connect to host\n%1" ).arg( m_errorText );
       break;
-    case  KIO::ERR_UNKNOWN_HOST:
-      result = i18n( "Unknown host\n%1" ).arg( m_errorText );
-      break;
-    case  KIO::ERR_UNKNOWN_PROXY_HOST:
-      result = i18n( "Unknown proxy host\n%1" ).arg( m_errorText );
-      break;
     case  KIO::ERR_CONNECTION_BROKEN:
       result = i18n( "Connection to host\n%1\nis broken" ).arg( m_errorText );
       break;
     case  KIO::ERR_NOT_FILTER_PROTOCOL:
       result = i18n( "The protocol %1\nis not a filter protocol" ).arg( m_errorText );
-      break;
-    case  KIO::ERR_IS_FILE:
-      result = i18n( "%1\nis a file,\nbut a directory was expected" ).arg( m_errorText );
       break;
     case  KIO::ERR_COULD_NOT_MOUNT:
       result = i18n( "Could not mount device.\nThe reported error was:\n\n%1" ).arg( m_errorText );
@@ -194,38 +182,83 @@ QString Job::errorString()
     case  KIO::ERR_COULD_NOT_UNMOUNT:
       result = i18n( "Could not unmount device.\nThe reported error was:\n\n%1" ).arg( m_errorText );
       break;
-    case  KIO::ERR_SLAVE_DIED:
-      result = i18n( "The process for the\n%1 protocol\ndied unexpectedly" ).arg( m_errorText );
+    case  KIO::ERR_COULD_NOT_READ:
+      result = i18n( "Could not read file\n%1" ).arg( m_errorText );
       break;
-    case  KIO::ERR_COULD_NOT_STAT:
-      result = i18n( "Could not access\n%1" ).arg( m_errorText );
+    case  KIO::ERR_COULD_NOT_WRITE:
+      result = i18n( "Could not write to file\n%1" ).arg( m_errorText );
       break;
-    case  KIO::ERR_CANNOT_RESUME:
-      result = i18n( "Could not resume file %1" ).arg( m_errorText );
+    case  KIO::ERR_COULD_NOT_BIND:
+      result = i18n( "Could not bind\n%1" ).arg( m_errorText );
       break;
-    case  KIO::ERR_CANNOT_DELETE:
-      result = i18n( "Could not delete file %1" ).arg( m_errorText );
+    case  KIO::ERR_COULD_NOT_LISTEN:
+      result = i18n( "Could not listen\n%1" ).arg( m_errorText );
+      break;
+    case  KIO::ERR_COULD_NOT_ACCEPT:
+      result = i18n( "Could not accept\n%1" ).arg( m_errorText );
       break;
     case  KIO::ERR_COULD_NOT_LOGIN:
       result = i18n( "Could not login into %1" ).arg ( m_errorText );
       break;
-    case  KIO::ERR_COULD_NOT_AUTHENTICATE:
-      result = i18n( "Authorization failed, %1 authentication not supported" ).arg( m_errorText );
+    case  KIO::ERR_COULD_NOT_STAT:
+      result = i18n( "Could not access\n%1" ).arg( m_errorText );
       break;
-/*
-    case  KIO::ERR_WARNING:
-      result = i18n( "Warning: %1" ).arg( m_errorText );
+    case  KIO::ERR_COULD_NOT_CLOSEDIR:
+      result = i18n( "Could not terminate listing\n%1" ).arg( m_errorText );
       break;
-*/
+    case  KIO::ERR_COULD_NOT_MKDIR:
+      result = i18n( "Could not make directory\n%1" ).arg( m_errorText );
+      break;
+    case  KIO::ERR_COULD_NOT_RMDIR:
+      result = i18n( "Could not remove directory\n%1" ).arg( m_errorText );
+      break;
+    case  KIO::ERR_CANNOT_RESUME:
+      result = i18n( "Could not resume file %1" ).arg( m_errorText );
+      break;
     case  KIO::ERR_CANNOT_RENAME:
       result = i18n( "Could not rename file %1" ).arg( m_errorText );
       break;
+    case  KIO::ERR_CANNOT_CHMOD:
+      result = i18n( "Could not change permissions for\n%1" ).arg( m_errorText );
+      break;
+    case  KIO::ERR_CANNOT_DELETE:
+      result = i18n( "Could not delete file %1" ).arg( m_errorText );
+      break;
+    case  KIO::ERR_SLAVE_DIED:
+      result = i18n( "The process for the\n%1 protocol\ndied unexpectedly" ).arg( m_errorText );
+      break;
+    case  KIO::ERR_OUT_OF_MEMORY:
+      result = i18n( "Error. Out of Memory.\n%1" ).arg( m_errorText );
+      break;
+    case  KIO::ERR_UNKNOWN_PROXY_HOST:
+      result = i18n( "Unknown proxy host\n%1" ).arg( m_errorText );
+      break;
+    case  KIO::ERR_COULD_NOT_AUTHENTICATE:
+      result = i18n( "Authorization failed, %1 authentication not supported" ).arg( m_errorText );
+      break;
+    case  KIO::ERR_ABORTED:
+      result = i18n( "User canceled action\n%1" ).arg( m_errorText );
+      break;
+    case  KIO::ERR_INTERNAL_SERVER:
+      result = i18n( "Internal error in server\n%1" ).arg( m_errorText );
+      break;
+    case  KIO::ERR_SERVER_TIMEOUT:
+      result = i18n( "Timeout on server\n%1" ).arg( m_errorText );
+      break;
+    case  KIO::ERR_UNKNOWN:
+      result = i18n( "Unknown error\n%1" ).arg( m_errorText );
+      break;
+    case  KIO::ERR_UNKNOWN_INTERRUPT:
+      result = i18n( "Unknown interrupt\n%1" ).arg( m_errorText );
+      break;
+/*
     case  KIO::ERR_CHECKSUM_MISMATCH:
       if (m_errorText)
         result = i18n( "Warning: MD5 Checksum for %1 does not match checksum returned from server" ).arg(m_errorText);
       else
         result = i18n( "Warning: MD5 Checksum for %1 does not match checksum returned from server" ).arg("document");
       break;
+*/
     case KIO::ERR_CANNOT_DELETE_ORIGINAL:
       result = i18n( "Could not delete original file %1.\nPlease check permissions." ).arg( m_errorText );
       break;
