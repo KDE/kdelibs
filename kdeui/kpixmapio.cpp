69e996dd780900f4ee90c8596e5f72acd05a7df8
Geert Jansen <jansen@kde.org>
2000-03-03 18:40:58 +0000

    Added KPixmapIO
    svn path=/trunk/kdelibs/; revision=42425

---
+++ kdeui/kpixmapio.cpp
@@ -0,0 +1,762 @@
+/* vi: ts=8 sts=4 sw=4
+ *
+ * $Id: $
+ *
+ * This file is part of the KDE project, module kdeui.
+ * Copyright (C) 2000 Geert Jansen <jansen@kde.org>.
+ * 
+ * You can Freely distribute this program under the GNU Library General 
+ * Public License. See the file "COPYING.LIB" for the exact licensing terms.
+ *
+ * kpixmapio.cpp: Fast pixmap <-> image conversion.
+ */
+
+#include <inttypes.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+#include <qimage.h>
+#include <qpixmap.h>
+#include <qcolor.h>
+#include <qglobal.h>
+
+#include <kglobal.h>
+#include <kconfig.h>
+#include <kdebug.h>
+#include "kpixmapio.h"
+
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#ifdef HAVE_MITSHM
+#include <X11/extensions/XShm.h>
+#endif
+
+#ifdef __GNUC__
+#define ID __PRETTY_FUNCTION__
+#else
+#define ID "KPixmapIO"
+#endif
+
+
+// d pointer
+
+struct KPixmapIOData {
+    int shmsize;
+    int shmpolicy;
+    int threshold;
+    int bpp;
+    int byteorder;
+    XImage *ximage;
+#ifdef HAVE_MITSHM
+    XShmSegmentInfo *shminfo;
+#endif
+};
+
+
+//  From Qt: Returns the position of the lowest set bit in val.
+
+typedef unsigned char uchar;
+typedef unsigned int uint;
+
+int lowest_bit(uint val)
+{
+    int i;
+    uint test = 1;
+    for (i=0; ((val & test) == 0) && i<32; i++, test<<=1);
+    return (i == 32) ? -1 : i;
+}
+
+
+/*** KPixmapIO ***/
+
+KPixmapIO::KPixmapIO()
+{
+    m_bShm = false;
+    d = new KPixmapIOData;
+    
+#ifdef HAVE_MITSHM
+    setShmPolicy(ShmDontKeep);
+    KConfig *config = KGlobal::config();
+    if (!config->readBoolEntry("UseMitShm", true))
+	return;
+
+    int ignore;
+    if (XQueryExtension(qt_xdisplay(), "MIT-SHM", &ignore, &ignore, &ignore)) {
+	if (XShmQueryExtension(qt_xdisplay()))
+	    m_bShm = true;
+    }
+    if (!m_bShm) {
+	kDebugInfo("%s: MIT-SHM not available!", ID);
+	return;
+    }
+
+    // Sort out bit format. Create a temporary XImage for this.
+    d->shminfo = new XShmSegmentInfo;
+    d->ximage = XShmCreateImage(qt_xdisplay(), (Visual *) QPaintDevice::x11AppVisual(),
+	    QPaintDevice::x11AppDepth(), ZPixmap, 0L, d->shminfo, 10, 10);
+    d->bpp = d->ximage->bits_per_pixel;
+    int bpp = d->bpp;
+    if (d->ximage->byte_order == LSBFirst)
+	bpp++;
+    int red_shift = lowest_bit(d->ximage->red_mask);
+    int green_shift = lowest_bit(d->ximage->green_mask);
+    int blue_shift = lowest_bit(d->ximage->blue_mask);
+    XDestroyImage(d->ximage); d->ximage = 0L;
+    d->shmsize = 0;
+
+    // Offer discrete possibilities for the bitformat. Each will have its
+    // own routine. The general algorithm using bitshifts is much too slow;
+    // this has to be done for every pixel!
+
+    if ((bpp == 32) && (red_shift == 16) && (green_shift == 8) &&
+	    (blue_shift == 0))
+	d->byteorder = bo32_ARGB;
+    else if ((bpp == 33) && (red_shift == 16) && (green_shift == 8) &&
+	    (blue_shift == 0))
+	d->byteorder = bo32_BGRA;
+    else if ((bpp == 24) && (red_shift == 16) && (green_shift == 8) &&
+	    (blue_shift == 0))
+	d->byteorder = bo24_RGB;
+    else if ((bpp == 25) && (red_shift == 16) && (green_shift == 8) &&
+	    (blue_shift == 0))
+	d->byteorder = bo24_BGR;
+    else if ((bpp == 16) && (red_shift == 11) && (green_shift == 5) &&
+	    (blue_shift == 0))
+	d->byteorder = bo16_RGB_565;
+    else if ((bpp == 16) && (red_shift == 10) && (green_shift == 5) &&
+	    (blue_shift == 0))
+	d->byteorder = bo16_RGB_555;
+    else if ((bpp == 17) && (red_shift == 11) && (green_shift == 5) &&
+	    (blue_shift == 0))
+	d->byteorder = bo16_BGR_565;
+    else if ((bpp == 16) && (red_shift == 10) && (green_shift == 5) &&
+	    (blue_shift == 0))
+	d->byteorder = bo16_BGR_555;
+    else if ((bpp == 8) || (bpp == 9))
+	d->byteorder = bo8;
+    else {
+	m_bShm = false;
+	kDebugWarning("%s: Byte order not supported!", ID);
+	kDebugWarning("%s: red = %x, green = %s, blue = %x", ID, 
+		d->ximage->red_mask, d->ximage->green_mask, 
+		d->ximage->blue_mask);
+	kDebugWarning("%s: Please report to <jansen@kde.org>", ID);
+    }
+#endif
+}
+
+
+KPixmapIO::~KPixmapIO()
+{
+    destroyXImage();
+    destroyShmSegment();
+    delete d;
+}
+
+
+QPixmap KPixmapIO::convertToPixmap(const QImage &img)
+{
+    int size = img.width() * img.height();
+    if (m_bShm && (img.depth() > 1) && (d->bpp > 8) && (size > d->threshold)) {
+	QPixmap dst(img.width(), img.height());
+	putImage(&dst, 0, 0, &img);
+	return dst;
+    } else {
+	QPixmap dst;
+	dst.convertFromImage(img);
+	return dst;
+    }
+	
+}
+
+
+QImage KPixmapIO::convertToImage(const QPixmap &pm)
+{
+    QImage image;
+    int size = pm.width() * pm.height();
+    if (m_bShm && (d->bpp >= 8) && (size > d->threshold))
+	image = getImage(&pm, 0, 0, pm.width(), pm.height());
+    else
+	image = pm.convertToImage();
+    return image;
+}
+
+
+void KPixmapIO::putImage(QPixmap *dst, const QPoint &offset, 
+    const QImage *src)
+{
+    putImage(dst, offset.x(), offset.y(), src);
+}
+
+
+void KPixmapIO::putImage(QPixmap *dst, int dx, int dy, const QImage *src)
+{
+    int size = src->width() * src->height();
+    if (m_bShm && (src->depth() > 1) && (d->bpp > 8) && (size > d->threshold)) {
+#ifdef HAVE_MITSHM
+	initXImage(src->width(), src->height());
+	convertToXImage(*src);
+	XShmPutImage(qt_xdisplay(), dst->handle(), qt_xget_temp_gc(), d->ximage,
+		dx, dy, 0, 0, src->width(), src->height(), false);
+	XSync(qt_xdisplay(), false);
+	doneXImage();
+#endif
+    } else {
+	QPixmap pix;
+	pix.convertFromImage(*src);
+	bitBlt(dst, dx, dy, &pix, 0, 0, pix.width(), pix.height());
+    }
+}
+
+
+QImage KPixmapIO::getImage(const QPixmap *src, const QRect &rect)
+{
+    return getImage(src, rect.x(), rect.y(), rect.width(), rect.height());
+}
+
+
+QImage KPixmapIO::getImage(const QPixmap *src, int sx, int sy, int sw, int sh)
+{
+    QImage image;
+    int size = src->width() * src->height();
+    if ((m_bShm) && (d->bpp >= 8) && (size > d->threshold)) {
+#ifdef HAVE_MITSHM
+	initXImage(sw, sh);
+	XShmGetImage(qt_xdisplay(), src->handle(), d->ximage, sx, sy, AllPlanes);
+	image = convertFromXImage();
+	doneXImage();
+#endif
+    } else {
+	QPixmap pix(sw, sh);
+	bitBlt(&pix, 0, 0, src, sx, sy, sw, sh);
+	image = pix.convertToImage();
+    }
+    return image;
+}
+
+
+#ifdef HAVE_MITSHM
+
+void KPixmapIO::preAllocShm(int size)
+{
+    destroyXImage();
+    createShmSegment(size);
+}
+
+
+void KPixmapIO::setShmPolicy(int policy)
+{
+    switch (policy) {
+    case ShmDontKeep:
+	d->shmpolicy = ShmDontKeep;
+	d->threshold = 5000;
+	break;
+    case ShmKeepAndGrow:
+	d->shmpolicy = ShmKeepAndGrow;
+	d->threshold = 2000;
+	break;
+    default:
+	break;
+    }
+}
+
+
+void KPixmapIO::initXImage(int w, int h)
+{
+    if (d->ximage && (w == d->ximage->width) && (h == d->ximage->height))
+	return;
+
+    createXImage(w, h);
+    int size = d->ximage->bytes_per_line * d->ximage->height;
+    if (size > d->shmsize)
+	createShmSegment(size);
+    d->ximage->data = d->shminfo->shmaddr;
+    return;
+}
+    
+
+void KPixmapIO::doneXImage()
+{
+    if (d->shmpolicy == ShmDontKeep) {
+	destroyXImage();
+	destroyShmSegment();
+    }
+}
+
+
+void KPixmapIO::destroyXImage()
+{
+    if (d->ximage) {
+	XDestroyImage(d->ximage); 
+	d->ximage = 0L;
+    }
+}
+
+
+void KPixmapIO::createXImage(int w, int h)
+{
+    destroyXImage();
+    d->ximage = XShmCreateImage(qt_xdisplay(), (Visual *) QPaintDevice::x11AppVisual(),
+	    QPaintDevice::x11AppDepth(), ZPixmap, 0L, d->shminfo, w, h);
+}
+
+
+void KPixmapIO::destroyShmSegment()
+{
+    if (d->shmsize) {
+	XShmDetach(qt_xdisplay(), d->shminfo);
+	shmdt(d->shminfo->shmaddr);
+	d->shmsize = 0;
+    }
+}
+
+
+void KPixmapIO::createShmSegment(int size)
+{
+    destroyShmSegment();
+    d->shminfo->shmid = shmget(IPC_PRIVATE, size, IPC_CREAT|0777);
+    if (d->shminfo->shmid < 0) {
+	kDebugWarning("%s: Could not get sysv shared memory segment", ID);
+	m_bShm = false;
+	return;
+    }
+
+    d->shminfo->shmaddr = (char *) shmat(d->shminfo->shmid, 0, 0);
+    if (d->shminfo->shmaddr < 0) {
+	kDebugWarning("%s: Could not attach sysv shared memory segment", ID);
+	m_bShm = false;
+	shmctl(d->shminfo->shmid, IPC_RMID, 0);
+	return;
+    }
+
+    d->shminfo->readOnly = false;
+    if (!XShmAttach(qt_xdisplay(), d->shminfo)) {
+	kDebugWarning("%s: X-Server could not attach shared memory segment", ID);
+	m_bShm = false;
+	shmdt(d->shminfo->shmaddr);
+	shmctl(d->shminfo->shmid, IPC_RMID, 0);
+	return;
+    }
+
+    d->shmsize = size;
+    XSync(qt_xdisplay(), false);
+    shmctl(d->shminfo->shmid, IPC_RMID, 0);
+}
+
+
+/*
+ * The following functions convertToXImage/convertFromXImage are a little
+ * long. This is because of speed, I want to get as much out of the inner
+ * loop as possible.
+ */
+
+QImage KPixmapIO::convertFromXImage()
+{
+    int x, y;
+    int width = d->ximage->width, height = d->ximage->height;
+    int bpl = d->ximage->bytes_per_line;
+    char *data = d->ximage->data;
+
+    QImage image;
+    if (d->bpp == 8) {
+	image.create(width, height, 8);
+
+	// Query color map. Don't remove unused entries as a speed
+	// optmization.
+	int i, ncells = 256;
+	XColor *cmap = new XColor[ncells];
+	for (i=0; i<ncells; i++)
+	    cmap[i].pixel = i;
+	XQueryColors(qt_xdisplay(), QPaintDevice::x11AppColormap(),
+		cmap, ncells);
+	image.setNumColors(ncells);
+	for (i=0; i<ncells; i++)
+	    image.setColor(i, qRgb(cmap[i].red, cmap[i].green, cmap[i].blue >> 8));
+    } else
+	image.create(width, height, 32);
+
+    switch (d->byteorder) {
+
+    case bo8:
+    {
+	for (y=0; y<height; y++)
+	    memcpy(image.scanLine(y), data + y*bpl, width);
+	break;
+    }
+
+    case bo16_RGB_565: 
+    case bo16_BGR_565:
+    {
+	uint32_t pixel, *src;
+	QRgb *dst, val;
+	for (y=0; y<height; y++) {
+	    src = (uint32_t *) (data + y*bpl);
+	    dst = (QRgb *) image.scanLine(y);
+	    for (x=0; x<width/2; x++) {
+		pixel = *src++;
+		val = ((pixel & 0xf800) << 8) | ((pixel & 0x7e0) << 4) |
+			((pixel & 0x1f) << 3);
+		*dst++ = val;
+		pixel >>= 16;
+		val = ((pixel & 0xf800) << 8) | ((pixel & 0x7e0) << 4) |
+			((pixel & 0x1f) << 3);
+		*dst++ = val;
+	    }
+	    if (width%2) {
+		pixel = *src++;
+		val = ((pixel & 0xf800) << 8) | ((pixel & 0x7e0) << 4) |
+			((pixel & 0x1f) << 3);
+		*dst++ = val;
+	    }
+	}
+	break;
+    }
+
+    case bo16_RGB_555: 
+    case bo16_BGR_555:
+    {
+	uint32_t pixel, *src;
+	QRgb *dst, val;
+	for (y=0; y<height; y++) {
+	    src = (uint32_t *) (data + y*bpl);
+	    dst = (QRgb *) image.scanLine(y);
+	    for (x=0; x<width/2; x++) {
+		pixel = *src++;
+		val = ((pixel & 0x7c00) << 9) | ((pixel & 0x3e0) << 6) |
+			((pixel & 0x1f) << 3);
+		*dst++ = val;
+		pixel >>= 16;
+		val = ((pixel & 0x7c00) << 9) | ((pixel & 0x3e0) << 6) |
+			((pixel & 0x1f) << 3);
+		*dst++ = val;
+	    }
+	    if (width%2) {
+		pixel = *src++;
+		val = ((pixel & 0x7c00) << 9) | ((pixel & 0x3e0) << 6) |
+			((pixel & 0x1f) << 3);
+		*dst++ = val;
+	    }
+	}
+	break;
+    }
+
+    case bo24_RGB:
+    {
+	char *src;
+	QRgb *dst;
+	int w1 = width/4;
+	uint32_t d1, d2, d3;
+	for (y=0; y<height; y++) {
+	    src = data + y*bpl;
+	    dst = (QRgb *) image.scanLine(y);
+	    for (x=0; x<w1; x++) {
+		d1 = *((uint32_t *)src);
+		d2 = *((uint32_t *)src + 1);
+		d3 = *((uint32_t *)src + 2);
+		src += 12;
+		*dst++ = d1;
+		*dst++ = (d1 >> 24) | (d2 << 8);
+		*dst++ = (d3 << 16) | (d2 >> 16);
+		*dst++ = d3 >> 8;
+	    }
+	    for (x=w1*4; x<width; x++) {
+		d1 = *src++ << 16;
+		d1 += *src++ << 8;
+		d1 += *src++;
+		*dst++ = d1;
+	    }
+	}
+	break;
+    }
+
+    case bo24_BGR:
+    {
+	char *src;
+	QRgb *dst;
+	int w1 = width/4;
+	uint32_t d1, d2, d3;
+	for (y=0; y<height; y++) {
+	    src = data + y*bpl;
+	    dst = (QRgb *) image.scanLine(y);
+	    for (x=0; x<w1; x++) {
+		d1 = *((uint32_t *)src);
+		d2 = *((uint32_t *)src + 1);
+		d3 = *((uint32_t *)src + 2);
+		src += 12;
+		*dst++ = d1;
+		*dst++ = (d1 >> 24) | (d2 << 8);
+		*dst++ = (d3 << 16) | (d2 >> 16);
+		*dst++ = d3 >> 8;
+	    }
+	    for (x=w1*4; x<width; x++) {
+		d1 = *src++;
+		d1 += *src++ << 8;
+		d1 += *src++ << 16;
+		*dst++ = d1;
+	    }
+	}
+	break;
+    }
+
+    case bo32_ARGB: 
+    case bo32_BGRA:
+    {
+	for (y=0; y<height; y++)
+	    memcpy(image.scanLine(y), data + y*bpl, width*4);
+	break;
+    }
+
+    }
+
+    return image;
+}
+
+
+void KPixmapIO::convertToXImage(const QImage &img)
+{
+    int x, y;
+    int width = d->ximage->width, height = d->ximage->height;
+    int bpl = d->ximage->bytes_per_line;
+    char *data = d->ximage->data;
+
+    switch (d->byteorder) {
+
+    case bo16_RGB_555: 
+    case bo16_BGR_555:
+
+	if (img.depth() == 32) {
+	    QRgb *src, pixel;
+	    uint32_t *dst, val;
+	    for (y=0; y<height; y++) {
+		src = (QRgb *) img.scanLine(y);
+		dst = (uint32_t *) (data + y*bpl);
+		for (x=0; x<width/2; x++) {
+		    pixel = *src++;
+		    val = ((pixel & 0xf80000) >> 9) | ((pixel & 0xf800) >> 5) |
+			     ((pixel & 0xff) >> 3);
+		    pixel = *src++;
+		    val |= (((pixel & 0xf80000) >> 9) | ((pixel & 0xf800) >> 5) |
+			    ((pixel & 0xff) >> 3)) << 16;
+		    *dst++ = val;
+		}
+		if (width%2) {
+		    pixel = *src++;
+		    *((uint16_t *)dst) = ((pixel & 0xf80000) >> 9) | 
+			    ((pixel & 0xf800) >> 5) | ((pixel & 0xff) >> 3);
+		}
+	    }
+	} else {
+	    uchar *src;
+	    uint32_t val, *dst;
+	    QRgb pixel, *clut = img.colorTable();
+	    for (y=0; y<height; y++) {
+		src = img.scanLine(y);
+		dst = (uint32_t *) (data + y*bpl);
+		for (x=0; x<width/2; x++) {
+		    pixel = clut[*src++];
+		    val = ((pixel & 0xf80000) >> 9) | ((pixel & 0xf800) >> 5) |
+			    ((pixel & 0xff) >> 3);
+		    pixel = clut[*src++];
+		    val |= (((pixel & 0xf80000) >> 9) | ((pixel & 0xf800) >> 5) |
+			    ((pixel & 0xff) >> 3)) << 16;
+		    *dst++ = val;
+		}
+		if (width%2) {
+		    pixel = clut[*src++];
+		    *((uint16_t *)dst) = ((pixel & 0xf80000) >> 9) | 
+			    ((pixel & 0xf800) >> 5) | ((pixel & 0xff) >> 3);
+		}
+	    }
+	}
+	break;
+
+    case bo16_RGB_565: 
+    case bo16_BGR_565:
+
+	if (img.depth() == 32) {
+	    QRgb *src, pixel;
+	    uint32_t *dst, val;
+	    for (y=0; y<height; y++) {
+		src = (QRgb *) img.scanLine(y);
+		dst = (uint32_t *) (data + y*bpl);
+		for (x=0; x<width/2; x++) {
+		    pixel = *src++;
+		    val = ((pixel & 0xf80000) >> 8) | ((pixel & 0xfc00) >> 5) |
+			     ((pixel & 0xff) >> 3);
+		    pixel = *src++;
+		    val |= (((pixel & 0xf80000) >> 8) | ((pixel & 0xfc00) >> 5) |
+			    ((pixel & 0xff) >> 3)) << 16;
+		    *dst++ = val;
+		}
+		if (width%2) {
+		    pixel = *src++;
+		    *((uint16_t *)dst) = ((pixel & 0xf80000) >> 8) | 
+			    ((pixel & 0xfc00) >> 5) | ((pixel & 0xff) >> 3);
+		}
+	    }
+	} else {
+	    uchar *src;
+	    uint32_t val, *dst;
+	    QRgb pixel, *clut = img.colorTable();
+	    for (y=0; y<height; y++) {
+		src = img.scanLine(y);
+		dst = (uint32_t *) (data + y*bpl);
+		for (x=0; x<width/2; x++) {
+		    pixel = clut[*src++];
+		    val = ((pixel & 0xf80000) >> 8) | ((pixel & 0xfc00) >> 5) |
+			    ((pixel & 0xff) >> 3);
+		    pixel = clut[*src++];
+		    val |= (((pixel & 0xf80000) >> 8) | ((pixel & 0xfc00) >> 5) |
+			    ((pixel & 0xff) >> 3)) << 16;
+		    *dst++ = val;
+		}
+		if (width%2) {
+		    pixel = clut[*src++];
+		    *((uint16_t *)dst) = ((pixel & 0xf80000) >> 8) | 
+			    ((pixel & 0xfc00) >> 5) | ((pixel & 0xff) >> 3);
+		}
+	    }
+	}
+	break;
+
+    case bo24_RGB:
+
+	if (img.depth() == 32) {
+	    char *dst;
+	    int w1 = width/4;
+	    QRgb *src, d1, d2, d3, d4;
+	    for (y=0; y<height; y++) {
+		src = (QRgb *) img.scanLine(y);
+		dst = data + y*bpl;
+		for (x=0; x<w1; x++) {
+		    d1 = (*src++ & 0xffffff); 
+		    d2 = (*src++ & 0xffffff);
+		    d3 = (*src++ & 0xffffff); 
+		    d4 = (*src++ & 0xffffff);
+		    *((uint32_t *)dst) = d1 | (d2 << 24);
+		    *((uint32_t *)dst+1) = (d2 >> 8) | (d3 << 16);
+		    *((uint32_t *)dst+2) = (d4 << 8) | (d3 >> 16);
+		    dst += 12;
+		}
+		for (x=w1*4; x<width; x++) {
+		    d1 = *src++;
+		    *dst++ = qRed(d1);
+		    *dst++ = qGreen(d1);
+		    *dst++ = qBlue(d1);
+		}
+	    }
+	} else {
+	    uchar *src, *dst;
+	    int w1 = width/4;
+	    QRgb *clut = img.colorTable(), d1, d2, d3, d4;
+	    for (y=0; y<height; y++) {
+		src = img.scanLine(y);
+		dst = (uchar *) data + y*bpl;
+		for (x=0; x<w1; x++) {
+		    d1 = (clut[*src++] & 0xffffff);
+		    d2 = (clut[*src++] & 0xffffff);
+		    d3 = (clut[*src++] & 0xffffff);
+		    d4 = (clut[*src++] & 0xffffff);
+		    *((uint32_t *)dst) = d1 | (d2 << 24);
+		    *((uint32_t *)dst+1) = (d2 >> 8) | (d3 << 16);
+		    *((uint32_t *)dst+2) = (d4 << 8) | (d3 >> 16);
+		    dst += 12;
+		}
+		for (x=w1*4; x<width; x++) {
+		    d1 = clut[*src++];
+		    *dst++ = qRed(d1);
+		    *dst++ = qGreen(d1);
+		    *dst++ = qBlue(d1);
+		}
+	    }
+	}
+	break;
+
+    case bo24_BGR:
+
+	if (img.depth() == 32) {
+	    char *dst;
+	    QRgb *src, d1, d2, d3, d4;
+	    int w1 = width/4;
+	    for (y=0; y<height; y++) {
+		src = (QRgb *) img.scanLine(y);
+		dst = data + y*bpl;
+		for (x=0; x<w1; x++) {
+		    d1 = (*src++ & 0xffffff); 
+		    d2 = (*src++ & 0xffffff);
+		    d3 = (*src++ & 0xffffff); 
+		    d4 = (*src++ & 0xffffff);
+		    *((uint32_t *)dst) = d1 | (d2 << 24);
+		    *((uint32_t *)dst+1) = (d2 >> 8) | (d3 << 16);
+		    *((uint32_t *)dst+2) = (d4 << 8) | (d3 >> 16);
+		    dst += 12;
+		}
+		for (x=w1*4; x<width; x++) {
+		    d1 = *src++;
+		    *dst++ = qBlue(d1);
+		    *dst++ = qGreen(d1);
+		    *dst++ = qRed(d1);
+		}
+	    }
+	} else {
+	    uchar *src, *dst;
+	    int w1 = width/4;
+	    QRgb *clut = img.colorTable(), d1, d2, d3, d4;
+	    for (y=0; y<height; y++) {
+		src = img.scanLine(y);
+		dst = (uchar *) data + y*bpl;
+		for (x=0; x<w1; x++) {
+		    d1 = (clut[*src++] & 0xffffff);
+		    d2 = (clut[*src++] & 0xffffff);
+		    d3 = (clut[*src++] & 0xffffff);
+		    d4 = (clut[*src++] & 0xffffff);
+		    *((uint32_t *)dst) = d1 | (d2 << 24);
+		    *((uint32_t *)dst+1) = (d2 >> 8) | (d3 << 16);
+		    *((uint32_t *)dst+2) = (d4 << 8) | (d3 >> 16);
+		    dst += 12;
+		}
+		for (x=w1*4; x<width; x++) {
+		    d1 = clut[*src++];
+		    *dst++ = qBlue(d1);
+		    *dst++ = qGreen(d1);
+		    *dst++ = qRed(d1);
+		}
+	    }
+	}
+	break;
+
+    case bo32_ARGB: 
+    case bo32_BGRA:
+
+	if (img.depth() == 32) {
+	    for (y=0; y<height; y++)
+		memcpy(data + y*bpl, img.scanLine(y), width*4);
+	} else {
+	    uchar *src;
+	    QRgb *dst, *clut = img.colorTable();
+	    for (y=0; y<height; y++) {
+		src = img.scanLine(y);
+		dst = (QRgb *) (data + y*bpl);
+		for (x=0; x<width; x++)
+		    *dst++ = clut[*src++];
+	    }
+	}
+	break;
+
+    }
+}
+
+#else
+
+void KPixmapIO::preAllocShm(int) {}
+void KPixmapIO::setShmPolicy(int) {}
+void KPixmapIO::initXImage(int, int) {}
+void KPixmapIO::doneXImage() {}
+void KPixmapIO::createXImage(int, int) {}
+void KPixmapIO::destroyXImage() {}
+void KPixmapIO::createShmSegment(int) {}
+void KPixmapIO::destroyShmSegment() {}
+QImage KPixmapIO::convertFromXImage() { return QImage(); }
+void KPixmapIO::convertToXImage(const QImage &) {}
+
+#endif // HAVE_MITSHM
---
+++ kdeui/kpixmapio.h
@@ -0,0 +1,179 @@
+/* vi: ts=8 sts=4 sw=4
+ *
+ * $Id: $
+ *
+ * This file is part of the KDE project, module kdeui.
+ * Copyright (C) 2000 Geert Jansen <jansen@kde.org>
+ * 
+ * You can Freely distribute this program under the GNU Library General 
+ * Public License. See the file "COPYING.LIB" for the exact licensing terms.
+ */
+
+#ifndef __KPixmapIO_h_Included__
+#define __KPixmapIO_h_Included__
+
+class QPixmap;
+class QImage;
+class KPixmapIOData;
+
+/**
+ * @short Fast QImage to/from QPixmap conversion.
+ * @author Geert Jansen <jansen@kde.org>
+ *
+ * KPixmapIO implements a fast path for QPixmap to/from QImage conversions.
+ * It uses the MIT-SHM shared memory extension for this. If this extension is 
+ * not available, it will fall back to standard Qt methods. 
+ *
+ * @sect Typical usage
+ *
+ * You can use KPixmapIO for load/saving pixmaps.
+ *
+ * <pre>
+ * KPixmapIO io;
+ * pixmap = io.convertToPixmap(image);
+ * image = io.convertToImage(pixmap);
+ * </pre>
+ *
+ * It also has functionality for partially updating/saving pixmaps, see 
+ * @ref #putImage and @ref #getImage.
+ *
+ * @sect KPixmapIO vs. Qt speed comparison
+ *
+ * Speed measurements that were taken show that usage of KPixmapIO for
+ * images up to a certain threshold size, offers no speed advantage over Qt. 
+ *
+ * This threshold size, amongst other causes, is determined by the shared 
+ * memory allocation policy. If the policy is @em ShmDontKeep, the 
+ * shared memory segment is discarded right after usage, and thus needs to 
+ * be allocated before each transfer. This introduces a a setup penalty not 
+ * present when the policy is @em ShmKeepAndGrow. In this case the 
+ * shared memory segment is kept and resized when necessary, until the 
+ * KPixmapIO object is destroyed.
+ *
+ * The default policy is @em ShmDontKeep. This policy makes sense when
+ * loading pixmaps once. The corresponding threshold is taken at 5.000 
+ * pixels as suggested by experiments. Below this threshold, KPixmapIO 
+ * will not use shared memory and fall back on the Qt routines.
+ *
+ * When the policy is @em ShmKeepAndGrow, the threshold is taken at
+ * 2.000 pixels. Using this policy, you might want to use @ref #preAllocShm
+ * to pre-allocate a certain amount of shared memory, in order to avoid
+ * resizes. This allocation policy makes sense in a multimedia type
+ * application where you are constantly updating the screen.
+ *
+ * Above a couple times the threshold size, KPixmapIO's and Qt's speed become 
+ * linear in the number of pixels, KPixmapIO being at least 2, and mostly around 
+ * 4 times faster than Qt, depending on the screen and image depth.
+ *
+ * Speed difference seems to be the most at 16 bpp, followed by 32 and 24
+ * bpp respectively. This can be explained by the relatively poor
+ * implementation of 16 bit RGB packing in Qt, while at 32 bpp we need to
+ * transfer more data, and thus gain more, than at 24 bpp.
+ *
+ * @sect Conclusion
+ *
+ * For large pixmaps, there's a definite speed improvement when using 
+ * KPixmapIO. On the other hand, there's no speed improvement for small 
+ * pixmaps. When you know you're only transferring small pixmaps, there's no
+ * point in using it.
+ */
+
+class KPixmapIO
+{
+public:
+    KPixmapIO();
+    ~KPixmapIO();
+
+    /**
+     * Convert an image to a pixmap.
+     * @param image The image to convert.
+     * @return The pixmap containing the image.
+     */
+    QPixmap convertToPixmap(const QImage &image);
+    
+    /**
+     * Convert a pixmap to an image.
+     * @param pixmap The pixmap to convert.
+     * @return The image.
+     */
+    QImage convertToImage(const QPixmap &pixmap);
+
+    /**
+     * Bitblt an image onto a pixmap.
+     * @param dst The destination pixmap.
+     * @param dx Destination x offset.
+     * @param dy Destination y offset.
+     * @param src The image to load.
+     */
+    void putImage(QPixmap *dst, int dx, int dy, const QImage *src);
+
+    /**
+     * This function is identical to the one above. It only differs in the
+     * arguments it accepts.
+     */
+    void putImage(QPixmap *dst, const QPoint &offset, const QImage *src);
+
+    /**
+     * Transfer (a part of) a pixmap to an image.
+     * @param src The source pixmap.
+     * @param sx Source x offset.
+     * @param sy Source y offset.
+     * @param sw Source width.
+     * @param sh Source height.
+     * @return The image.
+     */
+    QImage getImage(const QPixmap *src, int sx, int sy, int sw, int sh);
+
+    /**
+     * This function is identical to the one above. It only differs in the
+     * arguments it accepts.
+     */
+    QImage getImage(const QPixmap *src, const QRect &rect);
+
+    /**
+     * Shared memory allocation policies.
+     */
+    enum ShmPolicies {
+	ShmDontKeep,
+	ShmKeepAndGrow
+    };
+
+    /**
+     * Set the shared memory allocation policy. See the introduction for
+     * KPixmapIO for a discussion.
+     * @param policy The alloction policy.
+     */
+    void setShmPolicy(int policy);
+
+    /**
+     * Pre-allocate shared memory. KPixmapIO will be able to transfer images
+     * up to this size without resizing.
+     * @param size The size of the image in @em pixels.
+     */
+    void preAllocShm(int size);
+
+private:
+    /*
+     * Supported XImage byte orders. The notation ARGB means bytes
+     * containing A:R:G:B succeed in memory.
+     */
+    enum ByteOrders {
+	bo32_ARGB, bo32_BGRA, bo24_RGB, bo24_BGR,
+	bo16_RGB_565, bo16_BGR_565, bo16_RGB_555, 
+	bo16_BGR_555, bo8
+    };
+
+    bool m_bShm;
+    class KPixmapIOData *d;
+
+    void initXImage(int w, int h);
+    void doneXImage();
+    void createXImage(int w, int h);
+    void destroyXImage();
+    void createShmSegment(int size);
+    void destroyShmSegment();
+    void convertToXImage(const QImage &);
+    QImage convertFromXImage();
+};
+
+#endif // __KPixmapIO_h_Included__
