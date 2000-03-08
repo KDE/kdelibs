853eedbcadca43514c7932cc3c6a4326acb9944f
Dawit Alemayehu <adawit@kde.org>
2000-03-08 22:17:28 +0000

    Merged KCompletionBase's declaration.  This makes it easy to use KCompletionBase for inheriting classes. (DA)
    svn path=/trunk/kdelibs/; revision=42911

--- kdecore/kcompletion.h
+++ kdecore/kcompletion.h
@@ -387,4 +387,325 @@ private:
 };
 
 
+ /**
+ * An abstract base class for adding completion feature
+ * into widgets.
+ *
+ * This is a convienence class that tries to provide
+ * the common functions needed to add support for
+ * completion into widgets.  Refer to @ref KLineEdit
+ * or @ref KComboBox to see how to such support can be
+ * added using this base class.
+ *
+ * NOTE: Do not forget to provide an implementation for
+ * the protected pure virtual method @ref connectSignals()
+ * if you do not want the class to be abstract.
+ *
+ * @short An abstract class for using KCompletion in widgets
+ * @author Dawit Alemayehu <adawit@earthlink.net>
+ */
+class KCompletionBase
+{
+
+public:
+
+    /**
+    * Default constructor.
+    */
+    KCompletionBase();
+
+    /**
+    * Basic destructor.
+    */
+    virtual ~KCompletionBase();
+
+    /**
+    * Returns a pointer to the current completion object.
+    *
+    * If the object does not exisit, it is automatically
+    * created.  Note that the completion object created
+    * here is used by default to handle the signals
+    * internally.  It is also deleted when this object's
+    * destructor is invoked.  If you do not want these
+    * default settings, use @ref setAutoDeleteCompletionObject
+    * and @ref setHandleSignals to change the behavior.
+    * Alternatively, you can set the boolean parameter to
+    * false to disable the automatic handling of the signals
+    * by this object.  Note that the boolean argument will be
+    * ignored if there already exists a completion object since
+    * no new object needs to be created. You need to use either
+    * @ref setHandleSignals or @ref setCompletionObject for
+    * such cases depending on your requirement.
+    *
+    * @param hsig if true, handles signals internally.
+    * @return a pointer the completion object.
+    */
+    KCompletion* completionObject( bool hsig = true );
+
+    /**
+    * Sets up the completion object to be used.
+    *
+    * This method assigns the completion object and sets it
+    * up to automatically handle the completion and rotation
+    * signals internally.  You should use this function if
+    * you want to share one completion object among you widgets
+    * or need to use a customized completion object.
+    *
+    * The object assigned through this method is not deleted
+    * when this object's destructor is invoked unless you
+    * explicitly call @ref setAutoDeleteCompletionObject after
+    * calling this method.  Also if you do not want the signals
+    * to be handled by an internal implementation, be sure to
+    * set the bool argument to false.
+    *
+    * @param compObj a @ref KCompletion or a derived child object.
+    * @param hsig if true, handles signals internally.
+    */
+    virtual void setCompletionObject( KCompletion* compObj, bool hsig = true );
+
+    /**
+    * Enables this object to handle completion signals
+    * internally.
+    *
+    * This function simply assigns the boolean value that
+    * indicates whether it should handle signals or not.
+    * All child objects must provide an implementation for
+    * @ref connectSignals() which this method calls first.
+    *
+    * @param complete if true, handle completion & roation internally.
+    */
+    void setHandleSignals( bool handle );
+
+    /**
+    * Returns true if the completion object is deleted
+    * upon this widget's destruction.
+    *
+    * See @ref setCompeltionObject and @ref enableCompletion
+    * for details.
+    *
+    * @return true if the completion object
+    */
+    bool isCompletionObjectAutoDeleted() const { return m_bAutoDelCompObj; }
+
+    /**
+    * Sets the completion object for deletion upon this
+    * widget's destruction.
+    *
+    * If the argument is set to true, the completion object
+    * is deleted when this widget's destructor is called.
+    *
+    * @param autoDelete if true, delete completion object on destruction.
+    */
+    void setAutoDeleteCompletionObject( bool autoDelete ) { m_bAutoDelCompObj = autoDelete; }
+
+
+    /**
+    * Disables (temporarily) this widget's ability to emit
+    * the rotation and completion signals.
+    *
+    * Invoking this function will cause the completion &
+    * rotation signals not to be emitted.  Note that this
+    * also disbales any internal handling of these signals.
+    * However, unlike @ref setCompletionObject object,
+    * disabling the emition of the signals through this
+    * method does not delete the comlpetion object.
+    */
+    void disableSignals() { m_bEmitSignals = false; }
+
+    /**
+    * Enables the widget's ability to emit completion signals.
+    *
+    * Note that there is no need to invoke this function by
+    * default.  When a completion object is created through
+    * completionObject() or setCompletionObject(), these
+    * signals are automatically actiavted.  Only call this
+    * functions if you disbaled them manually.
+    */
+    void enableSignals()  { if( !m_bEmitSignals ) m_bEmitSignals = true; }
+
+    /**
+    * Returns true if the object handles the signals
+    *
+    * @return true if this signals are handled internally.
+    */
+    bool handleSignals() const { return m_bHandleSignals; }
+
+    /**
+    * Sets the type of completion to be used.
+    *
+    * The completion modes supported are those defined in
+    * @ref KGlobal.  See below.
+    *
+    * @param mode Completion type:
+    *   @li CompletionNone:  Disables completion feature.
+    *   @li CompletionAuto:  Attempts to find a match &
+    *                        fills-in the remaining text.
+    *   @li CompletionMan:   Acts the same as the above
+    *                        except the action has to be
+    *                        manually triggered through
+    *                        pre-defined completion key.
+    *   @li CompletionShell: Mimics the completion feature
+    *                        found in typcial *nix shell
+    *                        enviornments.
+    */
+    virtual void setCompletionMode( KGlobal::Completion mode );
+
+    /**
+    * Retrieves the current completion mode.
+    *
+    * The return values are of type @ref KGlobal::Completion.
+    * See @ref setCompletionMode for details.
+    *
+    * @return the completion mode.
+    */
+    KGlobal::Completion completionMode() const { return m_iCompletionMode; }
+
+    /**
+    * Sets the key-binding(s) to be used for rotating through
+    * a list to find the next match.
+    *
+    * When this key is activated by the user a @ref rotateDown
+    * signal will be emitted.  If no value is supplied for
+    * rDnkey or it is set to 0, then the completion key will
+    * be defaulted to the global setting.  This method returns
+    * false if rDnkey is negative or the supplied key-binding
+    * conflicts with either @ref completion or @ref rotateUp keys.
+    *
+    * @param rDnkey key-binding used to rotate up in a list.
+    * @return true if key-binding can successfully be set.
+    */
+    bool setRotateDownKey( int rDnKey = 0 );
+
+    /**
+    * Sets the key-binding to be used for rotating through a
+    * list to find the previous match.
+    *
+    * When this key is activated by the user a @ref rotateUp
+    * signal will be emitted.  If no value is supplied for
+    * rUpkey or it is set to 0, then the completion key
+    * will be defaulted to the global setting.  This method
+    * returns false if rUpkey is negative or the supplied
+    * key-binding conflicts with either @ref completion or
+    * @ref rotateDown keys.
+    *
+    * @param rUpkey key-binding used to rotate down in a list.
+    * @return  true if key-binding can successfully be set.
+    */
+    bool setRotateUpKey( int rUpKey = 0 );
+
+    /**
+    * Sets the key-binding to be used for the two manual
+    * completion types: CompletionMan and CompletionShell.
+    *
+    * This function expects the value of the modifier keys
+    * (Shift, Ctrl, Alt), if present, to be @bf summed up
+    * with actual key, ex: Qt::CTRL+Qt::Key_E.  If no value
+    * is supplied for  ckey or it is set to 0, then the
+    * completion key will be defaulted to the global setting.
+    * This function returns true if the supplied key-binding
+    * can be successfully assigned.
+    *
+    * Note that if  ckey is negative or the key-binding
+    * conflicts with either @ref completion or @ref rotateDown
+    * keys, this function will return false.  This method
+    * will also always returns false if the widget is not
+    * editable.
+    *
+    * @param ckey Key binding to use for completion
+    * @return true if key-binding is successfully set.
+    */
+    bool setCompletionKey( int ckey = 0 );
+
+    /**
+    * Returns the key-binding used for completion.
+    *
+    * If the key binding contains modifier key(s), the @bf sum
+    * of the key and the modifier will be returned. See also
+    * @ref setCompletionKey.  Note that this method is only
+    * useful when this widget is editable.  Otherwise this
+    * method has no meaning.
+    *
+    * @return the key-binding used for rotating through a list.
+    */
+    int completionKey() const { return m_iCompletionKey; }
+
+    /**
+    * Returns the key-binding used for rotating up in a list.
+    *
+    * This methods returns the key used to iterate through a
+    * list in the "UP" direction.  This is opposite to what
+    * the @ref rotateDown key does.
+    *
+    * If the key binding contains modifier key(s), the SUM of
+    * their values is returned.  See also @ref setRotateUpKey.
+    *
+    * @return the key-binding used for rotating up in a list.
+    */
+    int rotateUpKey() const { return m_iRotateUpKey; }
+
+    /**
+    * Returns the key-binding used for rotating down in a list.
+    *
+    * This methods returns the key used to iterate through a
+    * list in the "DOWN" direction.  This is opposite to what
+    * the @ref rotateDown key does.
+    *
+    * If the key binding contains modifier key(s), the SUM of
+    * their values is returned.  See also @ref setRotateDownKey.
+    *
+    * @return the key-binding used for rotating down in a list.
+    */
+    int rotateDownKey() const { return m_iRotateDnKey; }
+
+    /**
+    * Sets this object to use global values for key-bindings.
+    *
+    * This method changes the values of the key bindings for
+    * rotation and completion features to the default values
+    * provided in KGlobal.
+    *
+    * By default this object uses the global key-bindings.
+    * There is no need to call this method unless you have
+    * locally modified the key bindings and want to revert
+    * back.
+    */
+    void useGlobalSettings();
+
+
+protected:
+
+    /**
+    * Method to be implemented by inheriting objects.
+    *
+    * This function must provide an implementation for
+    * how signals are connected when they are handled
+    * internally.  Since this is implementation dependent
+    * it is left upto each inheriting object to decide.
+    */
+    virtual void connectSignals( bool handle ) const = 0;
+
+    // Stores the completion key locally
+    int m_iCompletionKey;
+    // Stores the Rotate up key locally
+    int m_iRotateUpKey;
+    // Stores the Rotate down key locally
+    int m_iRotateDnKey;
+
+    // Flag that determined whether the completion object
+    // should be deleted when this object is destroyed.
+    bool m_bAutoDelCompObj;
+    // Determines whether this widget handles completion signals
+    // internally or not
+    bool m_bHandleSignals;
+    // Determines whether this widget fires rotation signals
+    bool m_bEmitSignals;
+
+    // Stores the completion mode locally.
+    KGlobal::Completion m_iCompletionMode;
+    // Pointer to Completion object.
+    KCompletion* m_pCompObj;
+
+    KCompletionPrivate *d;
+};
+
 #endif // KCOMPLETION_H
