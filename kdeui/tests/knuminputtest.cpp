e125c3780c6064b8f87c6b2de1990b03fa7eed18
Dirk Mueller <mueller@kde.org>
2000-03-10 01:39:19 +0000

    adopt new api
    svn path=/trunk/kdelibs/; revision=43041

--- kdeui/tests/knuminputtest.cpp
+++ kdeui/tests/knuminputtest.cpp
@@ -5,7 +5,7 @@
 *
 * License: GPL, version 2
 * Version: $Id:
-* 
+*
 */
 
 #include <qlayout.h>
@@ -26,7 +26,7 @@ TopLevel::TopLevel(QWidget *parent, const char *name)
     QBoxLayout* l = new QHBoxLayout(this, 10);
 
     QGroupBox* b1 = new QVGroupBox("KIntNumInput", this);
-    
+
     i1 = new KIntNumInput("percent of usage (no slider)",
                           0, 100, 5, 42, "%", 10, false, b1, "perc_no_slider");
     i2 = new KIntNumInput("percentage of usage (with slider)",
@@ -39,23 +39,27 @@ TopLevel::TopLevel(QWidget *parent, const char *name)
     l->addWidget(b1);
 
     QGroupBox* b2 = new QVGroupBox("KDoubleNumInput", this);
-    
-    d1 = new KDoubleNumInput("percent of usage (no slider)",
-                          0, 100, 5, 42, "%", "%.1g", false, b2, "perc_no_slider");
-    d2 = new KDoubleNumInput("percentage of usage (with slider)",
-                          0, 100, 5, 42, "%", (const char*)0, true, b2, "perc_with_slider");
-#if 0    
-    d3 = new KDoubleNumInput("Hex byte (no slider)",
-                          0, 255, 1, 0xAF, "(hex)", 16, 0, false, b2, "hex_no_slider");
-    d4 = new KDoubleNumInput("Hex byte (with slider)",
-                          0, 255, 1, 0xFE, QString::null, 16, 0, true, b2, "hex_with_slider");
-#endif    
+
+    d1 = new KDoubleNumInput(0.42, b2, "perc_double_no_slider");
+    d1->setLabel("percent of usage (no slider)");
+    d1->setRange(0.0, 1.0, 0.005, false);
+
+    d2 = new KDoubleNumInput(d1, 0.422, b2, "perc_double_with_slider");
+    d2->setLabel("percentage of usage (with slider)");
+    d2->setRange(0, 1.0, 0.005);
+    d2->setSuffix("%");
+
+    d3 = new KDoubleNumInput(d2, 16.20, b2);
+    d3->setLabel("cash: ", AlignVCenter | AlignLeft);
+    d3->setRange(0.10, 100, 0.1);
+    d3->setPrefix("p");
+    d3->setSuffix("$");
 
     l->addWidget(b2);
 }
 
 
-    
+
 
 int main( int argc, char ** argv )
 {
