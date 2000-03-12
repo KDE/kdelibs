0abd9fe8bcf72fbb8614260dbfbb1a07c97e0ac5
Sven Radej <radej@kde.org>
2000-03-12 15:37:44 +0000

    sven: New KStatusBar - inherits QStatusBar
    svn path=/trunk/kdelibs/; revision=43245

--- kdeui/kstatusbar.cpp
+++ kdeui/kstatusbar.cpp
@@ -1,6 +1,6 @@
 /* This file is part of the KDE libraries
     Copyright (C) 1997 Mark Donohoe (donohoe@kde.org)
-              (C) 1997,1998 Sven Radej (sven@lisa.exp.univie.ac.at)
+              (C) 1997,1998, 2000 Sven Radej (radej@kde.org)
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Library General Public
@@ -18,15 +18,15 @@
     Boston, MA 02111-1307, USA.
 */
 
-#include <qpainter.h>
-
-#include <ktmainwindow.h>
 #include <kstatusbar.h>
-
-template class QList<KStatusBarItem>;
 
 // $Id$
 // $Log$
+// Revision 1.26  1999/12/13 22:44:37  kulow
+// put some #ifdef __GNUC__ around the #warning
+// removed the -DQT2_WORKAROUND. Sven says KMenubar is rewritten to be based
+// on QMenuBar rsn anyway, so we can live with this til then
+//
 // Revision 1.25  1999/10/31 19:44:46  bero
 // More template definitions for -frepo
 //
@@ -59,337 +59,91 @@ template class QList<KStatusBarItem>;
 //
 // Revision 1.18.2.1  1999/02/21 20:56:12  kulow
 // more porting to Qt 2.0. It compiles and links. Jucheisassa :)
-//
-// Revision 1.18  1999/01/18 10:57:04  kulow
-// .moc files are back in kdelibs. Built fine here using automake 1.3
-//
-// Revision 1.17  1999/01/15 09:31:23  kulow
-// it's official - kdelibs builds with srcdir != builddir. For this I
-// automocifized it, the generated rules are easier to maintain than
-// selfwritten rules. I have to fight with some bugs of this tool, but
-// generally it's better than keeping them updated by hand.
-//
-// Revision 1.16  1998/10/05 10:14:25  radej
-// sven: Bugfix: SEGV when 10000 messages come in the same time
-//
-// Revision 1.15  1998/09/23 14:40:22  radej
-// sven: connected timeout to clear. Thanks to Taj for hint, and no-thanks to
-//       me - alzheimer for forgeting it
-//
-// Revision 1.14  1998/09/04 22:58:09  radej
-// sven: added sizeHint. Very primitive - width is ?, but height is set
-//       according to fontMetrics
-//
-// Revision 1.13  1998/08/05 09:58:47  radej
-// sven: fixed removeItem SEGV; thanks to Frans van Dorsselaer for report.
-//
-// Revision 1.12  1998/05/08 16:25:55  radej
-// Fixed timer ftr temp messages.
-//
-// Revision 1.11  1998/05/05 10:04:40  radej
-// Not sunken in windows style
-//
-// Revision 1.10  1998/04/21 20:37:48  radej
-// Added insertWidget and some reorganisation - BINARY INCOMPATIBLE
-//
 
 
-/*************************************************************************/
-// We want a statusbar-fields to be by this amount heigher than fm.height().
-// This does NOT include the border width which we set separately for the statusbar.
 
 #define FONT_Y_DELTA 3
 #define DEFAULT_BORDERWIDTH 0
 
 KStatusBarLabel::KStatusBarLabel( const QString& text, int _id,
-                                 QWidget *parent, const char *name) :
+                                 KStatusBar *parent, const char *name) :
   QLabel( parent, name) 
 {   
   id = _id;
 
-  QFontMetrics fm = fontMetrics();
-  w = fm.width( text )+8;
-  h = fm.height() + FONT_Y_DELTA;
-  resize( w, h );
-    
-  setText( text );
-  if ( style() == MotifStyle )
-      setFrameStyle( QFrame::Panel | QFrame::Sunken );
-  setAlignment( AlignLeft | AlignVCenter );
-}
+  // Commented out - not needed? (sven)
+  //int w, h;
+  //QFontMetrics fm = fontMetrics();
+  //w = fm.width( text )+8;
+  //h = fm.height() + FONT_Y_DELTA;
+  //resize( w, h );
 
+
+  setText( text );
+  // umm... Mosfet? Can you help here?
+  //if ( style() == MotifStyle )
+  setLineWidth  (1);
+  setFrameStyle (QFrame::StyledPanel | QFrame::Plain );
+  
+  setAlignment( AlignHCenter | AlignVCenter );
+
+  connect (this, SIGNAL(itemPressed(int)), parent, SLOT(slotPressed(int)));
+  connect (this, SIGNAL(itemReleased(int)), parent, SLOT(slotReleased(int)));
+}
 
 void KStatusBarLabel::mousePressEvent (QMouseEvent *)
 {
-  emit Pressed(id);
+  emit itemPressed (id);
 }
 
 void KStatusBarLabel::mouseReleaseEvent (QMouseEvent *)
 {
-  emit Released (id);
+  emit itemReleased (id);
 }
 
-/***********************************************************************/
 
 KStatusBar::KStatusBar( QWidget *parent, const char *name )
-  : QFrame( parent, name )
+  : QStatusBar( parent, name )
 {
-  init();
+  items.setAutoDelete(true);
 }
 
 KStatusBar::~KStatusBar ()
 {
-  tmpTimer->stop();
-  for ( KStatusBarItem *b = items.first(); b; b=items.next() )
-    delete b;
-  if (tempMessage)
-    delete tempMessage;
+  // Umm... delete something;
+  items.clear();
+}
 
-  delete tmpTimer; // What do I have to notice!?
-};
-
-void KStatusBar::drawContents( QPainter * )
+void KStatusBar::insertItem( const QString& text, int id, bool permanent)
 {
-}
-
-void KStatusBar::init()
-{
-  borderwidth = DEFAULT_BORDERWIDTH;
-  fieldheight = fontMetrics().height() + FONT_Y_DELTA;
-
-  insert_order = KStatusBar::LeftToRight;
-  setFrameStyle( QFrame::NoFrame );
-  resize( width(), fieldheight + 2* borderwidth);
-  setFixedHeight(fieldheight + 2* borderwidth);
-  tempWidget=0;
-  tempMessage=0;
-
-  tmpTimer = new QTimer(this);
-  connect (tmpTimer, SIGNAL(timeout()), this, SLOT(clear()));
-
-}
-
-void KStatusBar::setHeight(int h){
-
-  fieldheight = h - 2* borderwidth;
-  resize( width(),h);
-  setFixedHeight(h);
-}
-
-
-void KStatusBar::setBorderWidth(int b){
-  
-  borderwidth = b;
-  resize( width(),height() + 2* borderwidth);
-  setFixedHeight(height() + 2* borderwidth);
-}
-
-void KStatusBar::resizeEvent( QResizeEvent * ) {
-  if (tempMessage)
-    tempMessage->setGeometry(borderwidth, borderwidth,
-                             width()-2*borderwidth, fieldheight);
-  else if (tempWidget)
-    tempWidget->setGeometry(borderwidth, borderwidth,
-                            width()-2*borderwidth, fieldheight);
-  else
-    updateRects( ); // False? You wouldn't sell that to toolbar... (sven)
-}
-
-void KStatusBar::setInsertOrder(InsertOrder order){
-
-  insert_order = order;
-
-}
-
-void KStatusBar::updateRects( bool res ){
-  
-  if( insert_order == KStatusBar::LeftToRight){
-
-    int offset= borderwidth;	
-    for ( KStatusBarItem *b = items.first(); b; b=items.next() ) {
-
-      b->setGeometry( offset, borderwidth, b->width(), fieldheight );	
-      offset+=b->width() + borderwidth;
-    }
-    
-    if ( !res ) {
-      KStatusBarItem *l = items.getLast();
-      if( l ) {
-		offset-=l->width() + borderwidth;
-		l->setGeometry( offset ,borderwidth, width() - offset - borderwidth, fieldheight );
-      }
-    }
-  }
-  else{ // KStatusBar::RightToLeft
-    int offset = width();
-
-    for ( KStatusBarItem *b = items.first(); b; b=items.next() ) {
-      offset -=b->width() + borderwidth;
-      b->setGeometry(offset,borderwidth,b->width(),fieldheight );
-
-    }
-    
-    if ( !res ) {
-      KStatusBarItem *l = items.getLast();
-      if( l != 0L ) {
-		offset+=l->width() - borderwidth;
-		l->setGeometry(borderwidth,borderwidth,offset,fieldheight);
-      }
-    }
-  }	
-}
-
-bool KStatusBar::enable( BarStatus stat )
-{
-  bool mystat = isVisible();
-  if ( (stat == Toggle && mystat) || stat == Hide )
-    hide();
-  else
-    show();
-  emit moved (); // force KTM::updateRects (David)
-  return ( isVisible() == mystat );
-}	
-
-int KStatusBar::insertItem( const QString& text, int id )
-{
-  KStatusBarLabel *label = new KStatusBarLabel( text, id, this );
-  KStatusBarItem *item = new KStatusBarItem(label, id, true);
-/*
-  // resize last item to default
-  KStatusBarItem *l = items.getLast();
-  if( l )
-  {
-    QFontMetrics fm = fontMetrics();
-    int w = fm.width(((QLabel *) l->getItem())->text() )+8;
-    int h = fm.height() + FONT_Y_DELTA;
-    l->getItem()->resize( w, h );
-  }
-*/
-  items.append( item );
-  updateRects( TRUE );
-  connect (label, SIGNAL(Pressed(int)), this, SLOT(slotPressed(int)));
-  connect (label, SIGNAL(Released(int)), this, SLOT(slotReleased(int)));
-  return items.at();
-}
-
-int KStatusBar::insertWidget(QWidget *_widget, int size, int id)
-{
-   KStatusBarItem *item = new KStatusBarItem(_widget, id, false);
-
-   items.append( item );
-   _widget->resize(size, fieldheight);
-   updateRects( TRUE );
-   return items.at();
+  KStatusBarLabel *l = new KStatusBarLabel (text, id, this);
+  items.insert(id, l);
+  addWidget (l, 0, permanent);
 }
 
 void KStatusBar::removeItem (int id)
 {
-  for ( KStatusBarItem *b = items.first(); b; b=items.next() ) 
-    if ( b->ID() == id )
-    {
-      items.remove();
-      delete b;
-      updateRects(false );
-    }
-  
-}
-
-void KStatusBar::replaceItem(int /* _id */, const QString& /* _text */ )
-{
-    debug ("Not yet implemented. Sorry.");
-}
-
-void KStatusBar::replaceItem(int /* _id */ , QWidget * /* _widget */)
-{
-    debug ("Not yet implemented. Sorry.");
+  KStatusBarLabel *l = items[id];
+  if (l)
+  {
+    removeWidget (l);
+    items.remove(id);
+    // reformat (); // needed? (sven)
+  }
+  else
+    debug ("KStatusBar::removeItem: bad id");
 }
 
 void KStatusBar::changeItem( const QString& text, int id )
 {
-  for ( KStatusBarItem *b = items.first(); b; b=items.next() ) 
-	if ( b->ID() == id )
-	  ((KStatusBarLabel *)b->getItem())->setText( text );
-}
-
-void KStatusBar::setAlignment( int id, int align)
-{
-  for ( KStatusBarItem *b = items.first(); b; b=items.next() ) 
-    if ( b->ID() == id ){
-	  ((KStatusBarLabel *)b->getItem())->setAlignment(align|AlignVCenter);
-    }
-}
-
-void KStatusBar::message(const QString& text, int time)
-{
-  if (tmpTimer->isActive())
-    tmpTimer->stop();
-  
-  if (tempMessage)
+  KStatusBarLabel *l = items[id];
+  if (l)
   {
-    delete tempMessage;
-    tempMessage = 0;
-  }
-  else if (tempWidget)
-  {
-    tempWidget->hide();
-    tempWidget=0;
+    l->setText(text);
+    reformat();
   }
   else
-    for ( KStatusBarItem *b = items.first(); b; b=items.next() )
-      b->hide();
-
-  
-  tempMessage = new KStatusBarLabel( text, -1, this );
-  tempMessage->setGeometry(borderwidth, borderwidth,
-                           width()-2*borderwidth, fieldheight);
-  tempMessage->show();
-  if (time >0)
-    tmpTimer->start(time, true);
-}
-
-void KStatusBar::message(QWidget *widget, int time)
-{
-  if (tmpTimer->isActive())
-    tmpTimer->stop();
-  
-  if (tempMessage)
-  {
-    delete tempMessage;
-    tempMessage = 0;
-  }
-  else if (tempWidget)
-  {
-    tempWidget->hide();
-    tempWidget=0;
-  }
-  else
-    for ( KStatusBarItem *b = items.first(); b; b=items.next() )
-      b->hide();
-
-  // Hi, Trolls
-  
-  tempWidget = widget;
-  tempWidget->setGeometry(borderwidth, borderwidth,
-                          width()-2*borderwidth, fieldheight);
-  tempWidget->show();
-  if (time >0)
-    tmpTimer->start(time, true);
-}
-
-
-void KStatusBar::clear()
-{
-  tmpTimer->stop();
-  if (tempMessage)
-    delete tempMessage;
-  if (tempWidget)
-    tempWidget->hide();
-  
-  for ( KStatusBarItem *b = items.first(); b; b=items.next() )
-    b->show();
-
-  tempMessage=0;
-  tempWidget=0;
+    debug ("KStatusBar::changeItem: bad id");
 }
 
 void KStatusBar::slotPressed(int _id)
@@ -402,18 +156,6 @@ void KStatusBar::slotReleased(int _id)
   emit released(_id);
 }
 
-QSize KStatusBar::sizeHint() const
-{
-  return QSize(width(), height());
-}
-
-KStatusBarItem *KStatusBar::item( int id )
-{
-  for ( KStatusBarItem *b = items.first(); b; b=items.next() ) 
-    if ( b->ID() == id )
-      return b;
-  return 0L;
-}
 
 #include "kstatusbar.moc"
 
--- kdeui/kstatusbar.h
+++ kdeui/kstatusbar.h
@@ -21,52 +21,15 @@
 #ifndef __KSTATUSBAR_H__
 #define __KSTATUSBAR_H__
 
-#include <qtimer.h>
-#include <qframe.h>
-#include <qstring.h> 
-#include <qlist.h>
+#include <qstatusbar.h>
+#include <qintdict.h>
 #include <qlabel.h> 
 
-/**
- *  Internal item class for use in KStatusBar
- *
- *  @short Internal class for use in KStatusBar
- */
-class KStatusBarItem
-{
-public:
-
-  KStatusBarItem( QWidget* w, int i, bool m = false )
-  { item = w; id = i; mine = m; };
-  ~KStatusBarItem()
-  { if( mine ) delete item; };
-  void setGeometry (int x, int y, int w, int h)
-  { item->setGeometry( x, y, w, h ); };
-  int width()
-  { return item->width(); };
-  int height()
-  { return item->height(); };
-  void show()
-  { item->show(); };
-  void hide()
-  { item->hide(); };
-  int ID()
-  {return id;};
-  QWidget *getItem()
-  { return item; };
-  
-private:
-
-  int id;
-  QWidget *item;
-  bool mine;
-};
-
+class KStatusBar;
 
 /**
  *  Internal label class for use in KStatusBar
- *
- *  @short Internal class for use in KStatusBar
+ *  @internal
  */
 class KStatusBarLabel : public QLabel
 {
@@ -74,25 +37,24 @@ class KStatusBarLabel : public QLabel
 
 public:
 
-  KStatusBarLabel( const QString& text, int ID, QWidget* parent = 0L, const char *name=0L );
-  ~KStatusBarLabel () {};
 
-  int w;
-  int h;
+  KStatusBarLabel( const QString& text, int _id, KStatusBar* parent = 0L, const char *name=0L );
+  ~KStatusBarLabel () {};
   
 protected:
 
-  void mousePressEvent( QMouseEvent* _event );
-  void mouseReleaseEvent( QMouseEvent* _event );
+  void mousePressEvent (QMouseEvent* _event);
+  void mouseReleaseEvent (QMouseEvent* _event);
 
 private:
 
+  
   int id;
 
 signals:
 
-  void Pressed( int );
-  void Released( int );
+  void itemPressed (int id);
+  void itemReleased (int id);
 };
 
 /**
@@ -105,37 +67,22 @@ signals:
  *  you don't use @ref KTMainWindow).
  *
  *  A special type of item is a message which is a temporary text-message
- *  or custom
- *  widget which is displayed on top of other items in full-width. Messages
+ *  which is displayed on top of other items in full-width. Messages
  *  are visible for specified time, or until you call the slot @ref clear().
  *
- *  @sect STILL UNIMPLEMENTED:
- *
- *  It is also possible to replace one item by another, keeping the same
- *  size and position.
+ *  Since KStatusBar inherits QStatusBar, you can freely use all QStatusBar methods.
  *
  *  @short KDE statusbar widget
- *  @author Mark Donohoe (donohoe@kde.org) Maintained by Sven Radej <sven@lisa.exp.univie.ac.at>
+ *  @author Mark Donohoe (donohoe@kde.org) Maintained by Sven Radej <radej@kde.org>
  */
-class KStatusBar : public QFrame
+class KStatusBar : public QStatusBar
 {
   Q_OBJECT
     
 public:
 
-  /**
-   * Possible states of the status bar.
-   **/
   enum BarStatus{ Toggle, Show, Hide };
-  /**
-   * Possible positions of the status bar.
-   **/
-  enum Position{ Top, Left, Bottom, Right, Floating };
-  /**
-   * Ways to inset widgets.
-   **/
-  enum InsertOrder{ LeftToRight, RightToLeft };
-
+  
   /**
    *  Construct KStatusBar object.
    */
@@ -151,196 +98,40 @@ public:
   /**
    *  Insert text label into the status bar.
    * 
-   *  When inserting the item send the
-   *  longest text you expect to go into the field as the first argument.
-   *  The field is sized to accomodate this text. However, the last field
-   *  inserted is always stretched to fit the window width.
-   *
-   *  @see insertWidget()
    */
-  int insertItem( const QString& text, int ID );
-
-  /**
-   *  Insert custom widget into the status bar.
-   *
-   *  The widget must have this statusbar
-   *  as it's parent. The size is the width of the widget.
-   *  However, the last item
-   *  inserted is always stretched to fit the window width.
-   *
-   *  @see insertItem()
-   */
-  int insertWidget( QWidget* _widget, int size, int id );
+  void insertItem( const QString& text, int ID, bool permanent=false );
 
   /**
    *  Remove an item.
-   *
-   *  If @p id corresponds to your custom widget, then it's hidden
-   *  but not deleted.
    *
    * @param id The item to remove.
    */
   void removeItem( int id );
 
   /**
-   *  NOT YET IMPLEMENTED!
-   *  Replaces item id with new label wich has text new_text. New
-   *  label will have the same position and size as old. If old item was
-   *  your custom widget it is not deleted. Note that it is rather pointless
-   *  to replace one label by another; use @ref #changeItem for that.
-   */
-  void replaceItem( int _id, const QString& new_text );
-
-  /**
-   *  NOT YET IMPLEMENTED!
-   *  Replaces item id with new widget new_widget. New widget will have the
-   *  same position and size as old item. If old item was your custom widget
-   *  it is not deleted.
-   */
-  void replaceItem( int _id, QWidget *new_widget );
-  
-  /**
    *  Change the text in a status bar field.
    *
-   *  The field is not resized!
-   *  This is useful only for labels.
+   *  The item will be resized to fit the text.
    */
   void changeItem( const QString& text, int id );
 
-  /** 
-   * Change the order of widget insertion.
-   *
-   *  If order is KStatusBar::LeftToRight the fields are inserted from left
-   *  to right, in particular the last field is streched to the right
-   *  border of the window. If order is KStatusBar::RightToLeft the fields
-   *  are inserted from the right.
-   */
-  void setInsertOrder( InsertOrder order );
-
-  /**
-   *  Sets the alignment of a field.
-   *
-   *  By default all fields are aligned left.
-   *  Useful only for labels.
-   *
-   *  Valid values for align are:
-   *
-   *  @li AlignLeft Aligns to the left border. 
-   *  @li AlignRight Aligns to the right border. 
-   *  @li AlignHCenter Aligns horizontally centered. 
-   *  @li AlignTop Aligns to the top border. 
-   *  @li AlignBottom Aligns to the bottom border. 
-   *  @li AlignVCenter Aligns vertically centered 
-   *  @li AlignCenter Same as @p AlignHCenter | @p AlignVCenter
-   */
-  void setAlignment( int id, int align );
-
-  /**
-   *  Set the height of the StatusBar.
-   *
-   *  Default height is computed from
-   *  default font height.
-   */
-  void setHeight( int );
-
-  /**
-   *  Set the border width of the status bar seperators and frame.
-   */
-  void setBorderWidth( int );
-
-  /**
-   *  Enable/ disable status bar.
-   *
-   *  You can get the same effect with @ref @QWidget::show()
-   *  or @ref QWidget::hide(), but if you do that the signal
-   *  @ref moved() won't be emitted.
-   */
-  bool enable( BarStatus stat );
-
-  /**
-   *  Hide all items and display temporary text message over entire statusbar.
-   *
-   *  The message will be removed (and old items redisplayed) after a time @p time has elapsed (in ms).
-   *  If @p time is 0 (default) the message will remain until you call
-   * @ref clear().
-   *  You can remove the message by calling @ref clear() at any time.
-   */
-  void message( const QString& text, int time = 0 );
-
-  /**
-   *  Hide all items and display temporary custom widget over entire statusbar.
-   *
-   *  The widget must have this statusbar as its parent.
-   *  The widget will be removed (and old items redisplayed) after time @p time has elapsed (in ms).
-   *  If @p time is 0 (default) widget will remain until you call @ref clear().
-   *  You can remove the message by calling @ref clear() any time. Upon
-   *  calling @ref clear() your widget will be hidden, not deleted.
-   */
-  void message( QWidget* wiiidget, int time = 0 );
-
-  /**
-   * For now returns @ref QWidget::height() and @ref QWidget::width().
-   */
-  QSize sizeHint() const;
-
-  /**
-   * Retrieve a statusbar item.
-   * @internal
-   **/
-  KStatusBarItem *item( int id );
-  
-public slots:
-
-  /**
-   *  Clear the message (if any), and return to regular state.
-   *
-   *  This method is a slot so you can connect to it. It does nothing if
-   *  @ref message() was not called before. 
-   *  If the message was your custom widget it will be hidden but not deleted, so you have to clean it up.
-   */
-  void clear();
-   
 signals:
 
   /**
    *  Emitted when mouse is pressed over statusbar item @p id.
    *
-   *  Connect to this signal
-   *  if you want to respond to mouse press events. If you want to catch this
-   *  signal for your custom widgets, the widgets must not catch mouse press
-   *  events.
+   *  Connect to this signal if you want to respond to mouse press events.
+   *  
    */
   void pressed( int );
 
   /**
    *  Emitted when mouse is released over statusbar item @p id.
    *
-   * Connect to
-   *  this signal if you want to respond to mouse clicks. If you want to catch
-   *  this signal for your custom widgets, the widgets must not catch mouse release
-   *  events.
+   * Connect to this signal if you want to respond to mouse clicks.
    */
   void released( int );
 
-  /**
-   *  Emitted when statusbar changes its position (not implemented!), or when
-   *  item is removed from statusbar.
-   *
-   *  This is normaly connected to
-   *  @ref KTMainWindow::updateRects().
-   *  If you subclass @ref KTMainWindow and reimplement
-   *  @ref KTMainWindow::resizeEvent() or
-   *  @ref KTMainWindow::updateRects(), be sure to connect to
-   *  this signal.
-   */
-  void moved();
-
-protected:
-
-  void drawContents( QPainter* );
-  void resizeEvent( QResizeEvent* );
-  void init();
-  void updateRects( bool resize = FALSE );
 
 protected slots:
 
@@ -348,18 +139,7 @@ protected slots:
   void slotReleased( int );
 
 private:
-
-  QList<KStatusBarItem> items;
-  InsertOrder insert_order;
-  int fieldheight;
-  int borderwidth;
-  KStatusBarLabel *tempMessage;
-  QWidget *tempWidget;
-  QTimer *tmpTimer; //for future bugfix
-
-  //fut:
-  bool bull;
-  int tni;
+  QIntDict<KStatusBarLabel> items;
 };
 
 #endif // __KSTATUSBAR_H__
