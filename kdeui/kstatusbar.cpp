0e4d9303d91f8948a969703161bc10e40f4b741f
Kurt Granroth <granroth@kde.org>
2000-03-13 18:04:37 +0000

    Compromise on the size grips.  They are enabled by default, but if you don't want 'em, just set
    [StatusBar style]
    SizeGripEnabled=False
    
    in kdeglobals (or in your apps config file) and they are gone
    
    svn path=/trunk/kdelibs/; revision=43354

--- kdeui/kstatusbar.cpp
+++ kdeui/kstatusbar.cpp
@@ -19,51 +19,10 @@
 */
 
 #include <kstatusbar.h>
+#include <kconfig.h>
+#include <kglobal.h>
 
 // $Id$
-// $Log$
-// Revision 1.27  2000/03/12 15:37:43  radej
-// sven: New KStatusBar - inherits QStatusBar
-//
-// Revision 1.26  1999/12/13 22:44:37  kulow
-// put some #ifdef __GNUC__ around the #warning
-// removed the -DQT2_WORKAROUND. Sven says KMenubar is rewritten to be based
-// on QMenuBar rsn anyway, so we can live with this til then
-//
-// Revision 1.25  1999/10/31 19:44:46  bero
-// More template definitions for -frepo
-//
-// Revision 1.24  1999/10/08 23:13:42  bero
-// ktopwidget.h no longer included
-//
-// Revision 1.23  1999/08/05 22:22:25  shausman
-// - implemented KStatusBarItem *KStatusBar::item( int id );
-//
-// Revision 1.22  1999/06/09 21:52:26  cschlaeg
-// serveral fixes for recently implemented layout management; removed segfault on close; removed segfault for no menubar apps; setFullWidth(false) is working again; floating a bar does not segfault any more but still does not work properly; I will look into this again.
-//
-// Revision 1.21  1999/06/06 17:29:44  cschlaeg
-// New layout management implemented for KTMainWindow. This required
-// updates for KToolBar, KMenuBar and KStatusBar. KTMainWindow::view_*
-// public variables removed. Use mainViewGeometry() instead if you really
-// have to. Added new classes in ktmlayout to handle the new layout
-// management.
-//
-// Revision 1.20  1999/05/11 23:39:45  dfaure
-// Added signal moved() to KStatusBar ; emit moved() in KStatusBar::enable ;
-//  connected it to updateRects in KTMainWindow.
-// Implemented KMenuBar::enable, for consistency with other *bars.
-//
-// Revision 1.19  1999/03/01 23:35:15  kulow
-// CVS_SILENT ported to Qt 2.0
-//
-// Revision 1.18.2.2  1999/02/22 21:14:27  kulow
-// extreme QStringing
-//
-// Revision 1.18.2.1  1999/02/21 20:56:12  kulow
-// more porting to Qt 2.0. It compiles and links. Jucheisassa :)
-
-
 
 #define FONT_Y_DELTA 3
 #define DEFAULT_BORDERWIDTH 0
@@ -109,7 +68,14 @@ KStatusBar::KStatusBar( QWidget *parent, const char *name )
   : QStatusBar( parent, name )
 {
   items.setAutoDelete(true);
-  setSizeGripEnabled(false);
+
+  // make the size grip stuff configurable
+  KConfig *config = KGlobal::config();
+  QString group(config->group());
+  config->setGroup(QString::fromLatin1("StatusBar style"));
+  bool grip_enabled = config->readBoolEntry(QString::fromLatin1("SizeGripEnabled"), true);
+  setSizeGripEnabled(grip_enabled);
+  config->setGroup(group);
 }
 
 KStatusBar::~KStatusBar ()
