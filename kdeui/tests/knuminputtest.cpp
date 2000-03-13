2ca202a0a77de6947dd589503b8bb7948c22779e
Dirk Mueller <mueller@kde.org>
2000-03-13 22:37:32 +0000

    new stuff
    svn path=/trunk/kdelibs/; revision=43376

--- kdeui/tests/knuminputtest.cpp
+++ kdeui/tests/knuminputtest.cpp
@@ -27,33 +27,47 @@ TopLevel::TopLevel(QWidget *parent, const char *name)
 
     QGroupBox* b1 = new QVGroupBox("KIntNumInput", this);
 
-    i1 = new KIntNumInput("percent of usage (no slider)",
-                          0, 100, 5, 42, "%", 10, false, b1, "perc_no_slider");
-    i2 = new KIntNumInput("percentage of usage (with slider)",
-                          0, 100, 5, 42, "%", 10, true, b1, "perc_with_slider");
-    i3 = new KIntNumInput("Hex byte (no slider)",
-                          0, 255, 1, 0xAF, "(hex)", 16, false, b1, "hex_no_slider");
-    i4 = new KIntNumInput("Hex byte (with slider)",
-                          0, 255, 1, 0xFE, QString::null, 16, true, b1, "hex_with_slider");
+    i1 = new KIntNumInput(42, b1, 10, "perc_no_slider");
+    i1->setLabel("percent of usage (no slider)");
+    i1->setRange(0, 100, 5, false);
+
+    i2 = new KIntNumInput(i1, 42, b1);
+    i2->setLabel("percentage of usage (with slider)");
+    i2->setRange(0, 100, 5);
+    i2->setSuffix(" %");
+
+    i3 = new KIntNumInput(i2, 0xAF, b1, 16);
+    i3->setLabel("Hex byte (no slider)");
+    i3->setRange(0, 255, 1, false);
+    i3->setSuffix(" (hex)");
+
+    i4 = new KIntNumInput(i3, 0xfe, b1, 16);
+    i4->setLabel("Hex byte (with slider)");
+    i4->setRange(0, 255, 1);
 
     l->addWidget(b1);
 
     QGroupBox* b2 = new QVGroupBox("KDoubleNumInput", this);
 
     d1 = new KDoubleNumInput(0.42, b2, "perc_double_no_slider");
-    d1->setLabel("percent of usage (no slider)");
+    d1->setLabel("percent of usage (no slider)", AlignTop | AlignRight);
     d1->setRange(0.0, 1.0, 0.005, false);
 
     d2 = new KDoubleNumInput(d1, 0.422, b2, "perc_double_with_slider");
-    d2->setLabel("percentage of usage (with slider)");
+    d2->setLabel("percentage of usage (with slider)", AlignBottom | AlignLeft);
     d2->setRange(0, 1.0, 0.005);
     d2->setSuffix("%");
 
     d3 = new KDoubleNumInput(d2, 16.20, b2);
-    d3->setLabel("cash: ", AlignVCenter | AlignLeft);
+    d3->setLabel("cash: ", AlignVCenter | AlignHCenter);
     d3->setRange(0.10, 100, 0.1);
     d3->setPrefix("p");
     d3->setSuffix("$");
+
+    d4 = new KDoubleNumInput(d3, 1e6, b2);
+    d4->setLabel("math test: ", AlignVCenter | AlignLeft);
+    d4->setRange(-1e10, 1e10, 1);
+    d4->setFormat("%g");
 
     l->addWidget(b2);
 }
