86a3762af7f0a6003fe11decb7ed53cbcf91e63f
Charles Samuels <charles@kde.org>
2000-03-19 08:18:44 +0000

    Making kdcop "work" :)
    svn path=/trunk/kdelibs/; revision=43993

--- kdecore/knotifyclient.h
+++ kdecore/knotifyclient.h
@@ -12,15 +12,13 @@
  * $KDEDIR/share/apps/appname/eventsrc which contains the events.
  *
  * The file looks like this:
+ * <pre>
  * [eventname]
  * sound=/path/to/sound/file
  * presentation=1
  *  ...
- *
- * WARNING! This class is a class preview.  This class has already
- * been implemented for your use.  Please do use it, and even
- * install the eventsrc file, but don't expect it to work quite 
- * yet :)
+ * </pre>
+ * This class works, but is still in testing.
  *
  * @author Charles Samuels <charles@altair.dhs.org>
  */
@@ -44,14 +42,14 @@ public:
 	/**
 	 * The Default constructor.  You should have little use
 	 * for this thanks to the @ref event method.
-	 * @arg message The event type to send, such as "Desktop1" for a virtual
-	 *              desktop change
-	 * @arg text If you need to send off a message with your alert.  This
-	 *           will happen for an error level of 2 or more.
-	 * @arg present How to present it.  If "Default" is chosen, the server
-	 *              will decide according to the config
-	 * @arg client The DCOPClient to use.  Usually it pulls the one from
-	 *             your KApplication.
+	 * @param message The event type to send, such as "Desktop1" for a virtual
+	 *                desktop change
+	 * @param text If you need to send off a message with your alert.  This
+	 *             will happen for an error level of 2 or more.
+	 * @param present How to present it.  If "Default" is chosen, the server
+	 *                will decide according to the config
+	 * @param client The DCOPClient to use.  Usually it pulls the one from
+	 *               your KApplication.
 	*/
 	KNotifyClient(const QString &message, const QString &text=0,
 	             Presentation present=Default,
@@ -74,16 +72,16 @@ public: //static methods
 	 * Call it by KNotifyClient::event("EventName");
 	 * It will use KApplication::kApplication->dcopClient() to communicate to
 	 * the server
-	 * @arg message The name of the event
-	 * @arg text The text to put in a dialog box.  This won't be shown if
-	 *           the user connected the event to sound, only.
+	 * @param message The name of the event
+	 * @param text The text to put in a dialog box.  This won't be shown if
+	 *             the user connected the event to sound, only.
 	 */
 	static bool event(const QString &message, const QString &text=0);
 	/**
 	 * Will fire an event that's not registered.
-	 * @arg text The error message text, if applicable
-	 * @arg present The error message level, one again, defaulting to "Default"
-	 * @arg soundfile The sound file to play if selected with present
+	 * @param text The error message text, if applicable
+	 * @param present The error message level, one again, defaulting to "Default"
+	 * @param soundfile The sound file to play if selected with present
 	 */
 	static bool userEvent(const QString &text=0, Presentation present=Default,
 	                      const QString &soundfile=0);
@@ -96,6 +94,11 @@ public: //static methods
 	static QString soundFileByName(const QString &eventname);
 
 private:
+	/**
+	 * Why does kdoc include this? This is an internal structure that's actually
+	 * declared under "private"
+	 * @internal
+	 */
 	struct Event
 	{
 		QString message;
