39341ab89f3041d9115d7ad62647b5001128c149
Kurt Granroth <granroth@kde.org>
2000-03-22 21:05:39 +0000

    Added mosfet's license declaration since B3 is 99.9999% mosfet's code (and 100% of all the hard stuff)
    Fixed sunken toolbar buttons while I was at it
    
    svn path=/trunk/kdelibs/; revision=44326

--- kstyles/b3plugin/b3style.cpp
+++ kstyles/b3plugin/b3style.cpp
@@ -1,3 +1,31 @@
+/*-
+ * B3Style is 99.9999% B2Style code
+ * B2Style (C)2000 Daniel M. Duley <mosfet@kde.org>
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ */
 #ifndef INCLUDE_MENUITEM_DEF
 #define INCLUDE_MENUITEM_DEF
 #endif
@@ -885,10 +913,10 @@ void B3Style::drawKToolBarButton(QPainter *p, int x, int y, int w, int h,
         p->drawLine(x2-1, y+2, x2-1, y2-3);
         p->drawLine(x+1, y2-2, x2-2, y2-2);
 
-        p->setPen(g.light());
+        p->setPen(sunken ? g.mid() : g.light());
         p->drawLine(x+1, y+2, x2-2, y+2);
         p->drawLine(x+1, y+2, x+1, y2-3);
-        p->setPen(g.mid());
+        p->setPen(sunken ? g.light() : g.mid());
         p->drawLine(x2-2, y+3, x2-2, y2-3);
         p->drawLine(x+2, y2-3, x2-2, y2-3);
     }
@@ -956,7 +984,7 @@ void B3Style::drawKToolBarButton(QPainter *p, int x, int y, int w, int h,
             if (font)
                 p->setFont(*font);
             if(raised)
-                p->setPen(Qt::blue);
+                p->setPen(KGlobalSettings::toolBarHighlightColor());
             p->drawText(x+dx, y+dy, fm.width(btext), fm.lineSpacing(), tf, btext);
         }
     }
@@ -981,7 +1009,7 @@ void B3Style::drawKToolBarButton(QPainter *p, int x, int y, int w, int h,
             if (font)
                 p->setFont(*font);
             if(raised)
-                p->setPen(Qt::blue);
+                p->setPen(KGlobalSettings::toolBarHighlightColor());
             p->drawText(x, y, w, h-3, tf, btext);
         }
     }
