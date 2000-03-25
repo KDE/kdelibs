6dd67f7230879d2ab4cf917a11d9a4517f82d592
Daniel M. Duley <daniel.duley@verizon.net>
2000-03-25 01:59:47 +0000

    Fix
    svn path=/trunk/kdelibs/; revision=44582

--- kstyles/b2plugin/b2style.h
+++ kstyles/b2plugin/b2style.h
@@ -61,62 +61,60 @@ class B2Style : public KStyle
 {
 public:
     B2Style();
-    virtual ~B2Style();
+    ~B2Style();
     virtual void polish(QWidget *w);
     virtual void unPolish(QWidget *w);
     virtual void polish(QPalette &p);
-    virtual void drawButton(QPainter *p, int x, int y, int w, int h,
-                            const QColorGroup &g, bool sunken = FALSE,
-                            const QBrush *fill = 0 );
-    virtual void drawButtonMask(QPainter *p, int x, int y, int w, int h);
-    virtual void drawComboButtonMask(QPainter *p, int x, int y, int w, int h);
-    virtual QRect buttonRect(int x, int y, int w, int h);
-    virtual void drawBevelButton(QPainter *p, int x, int y, int w, int h,
-                                 const QColorGroup &g, bool sunken = FALSE,
-                                 const QBrush *fill = 0 );
-    virtual void drawPushButton(QPushButton *btn, QPainter *p);
+    void drawButton(QPainter *p, int x, int y, int w, int h,
+                    const QColorGroup &g, bool sunken = FALSE,
+                    const QBrush *fill = 0 );
+    void drawButtonMask(QPainter *p, int x, int y, int w, int h);
+    void drawComboButtonMask(QPainter *p, int x, int y, int w, int h);
+    QRect buttonRect(int x, int y, int w, int h);
+    void drawBevelButton(QPainter *p, int x, int y, int w, int h,
+                         const QColorGroup &g, bool sunken = FALSE,
+                         const QBrush *fill = 0 );
+    void drawPushButton(QPushButton *btn, QPainter *p);
     virtual void drawPushButtonLabel (QPushButton *btn, QPainter *p);
-    virtual void drawScrollBarControls(QPainter*,  const QScrollBar*,
-                                       int sliderStart, uint controls,
-                                       uint activeControl );
-    virtual QStyle::ScrollControl scrollBarPointOver(const QScrollBar *sb,
+    void drawScrollBarControls(QPainter*,  const QScrollBar*, int sliderStart,
+                               uint controls, uint activeControl );
+    QStyle::ScrollControl scrollBarPointOver(const QScrollBar *sb,
                                              int sliderStart, const QPoint &p);
-    virtual void scrollBarMetrics(const QScrollBar *sb, int &sliderMin,
-                                  int &sliderMax, int &sliderLength,
-                                  int &buttonDim);
-    virtual QSize indicatorSize() const;
-    virtual void drawIndicator(QPainter* p, int x, int y, int w, int h,
+    void scrollBarMetrics(const QScrollBar *sb, int &sliderMin, int &sliderMax,
+                          int &sliderLength, int &buttonDim);
+    QSize indicatorSize() const;
+    void drawIndicator(QPainter* p, int x, int y, int w, int h,
                        const QColorGroup &g, int state, bool down = FALSE,
                        bool enabled = TRUE );
-    virtual QSize exclusiveIndicatorSize() const;
-    virtual void drawExclusiveIndicator(QPainter* p,  int x, int y, int w,
-                                        int h, const QColorGroup &g, bool on,
-                                        bool down = FALSE, bool enabled =  TRUE );
-    virtual void drawComboButton(QPainter *p, int x, int y, int w, int h,
+    QSize exclusiveIndicatorSize() const;
+    void drawExclusiveIndicator(QPainter* p,  int x, int y, int w, int h,
+                                const QColorGroup &g, bool on,
+                                bool down = FALSE, bool enabled =  TRUE );
+    void drawComboButton(QPainter *p, int x, int y, int w, int h,
                          const QColorGroup &g, bool sunken = FALSE,
                          bool editable = FALSE, bool enabled = TRUE,
                          const QBrush *fill = 0 );
-    virtual QRect comboButtonRect(int x, int y, int w, int h);
-    virtual QRect comboButtonFocusRect(int x, int y, int w, int h);
-    virtual int sliderLength() const;
-    virtual void drawSliderGroove(QPainter *p, int x, int y, int w, int h,
+    QRect comboButtonRect(int x, int y, int w, int h);
+    QRect comboButtonFocusRect(int x, int y, int w, int h);
+    int sliderLength() const;
+    void drawSliderGroove(QPainter *p, int x, int y, int w, int h,
                           const QColorGroup& g, QCOORD c, Orientation );
-    virtual void drawArrow(QPainter *p, Qt::ArrowType type, bool down,
+    void drawArrow(QPainter *p, Qt::ArrowType type, bool down,
                    int x, int y, int w, int h, const QColorGroup &g,
                    bool enabled=true, const QBrush *fill = 0);
-    virtual void drawSlider(QPainter *p, int x, int y, int w, int h,
+    void drawSlider(QPainter *p, int x, int y, int w, int h,
                     const QColorGroup &g, Orientation orient,
                     bool tickAbove, bool tickBelow);
-    virtual void drawSliderMask(QPainter *p, int x, int y, int w, int h,
+    void drawSliderMask(QPainter *p, int x, int y, int w, int h,
                         Orientation orient, bool, bool);
     virtual void drawKToolBar(QPainter *p, int x, int y, int w, int h,
                               const QColorGroup &g, bool floating = false);
-    virtual void drawKBarHandle(QPainter *p, int x, int y, int w, int h,
+    void drawKBarHandle(QPainter *p, int x, int y, int w, int h,
                         const QColorGroup &g, bool horizontal=false,
                         QBrush *fill=NULL);
-    virtual void drawKMenuBar(QPainter *p, int x, int y, int w, int h,
+    void drawKMenuBar(QPainter *p, int x, int y, int w, int h,
                       const QColorGroup &g, QBrush *fill=NULL);
-    virtual void drawKToolBarButton(QPainter *p, int x, int y, int w, int h,
+    void drawKToolBarButton(QPainter *p, int x, int y, int w, int h,
                             const QColorGroup &g, bool sunken=false,
                             bool raised = true, bool enabled = true,
                             bool popup = false,
@@ -124,44 +122,34 @@ public:
                             const QString& btext=QString::null,
                             const QPixmap *icon=NULL,
                             QFont *font=NULL);
-    virtual void drawKMenuItem(QPainter *p, int x, int y, int w, int h,
+    void drawKMenuItem(QPainter *p, int x, int y, int w, int h,
                        const QColorGroup &g, bool active,
                        QMenuItem *item, QBrush *fill=NULL);
-    virtual void drawPopupMenuItem(QPainter *p, bool checkable, int maxpmw,
+    void drawPopupMenuItem(QPainter *p, bool checkable, int maxpmw,
                            int tab, QMenuItem *mi, const QPalette &pal,
                            bool act, bool enabled, int x, int y, int w,
                            int h);
-    virtual void drawKProgressBlock(QPainter *p, int x, int y, int w, int h,
+    void drawKProgressBlock(QPainter *p, int x, int y, int w, int h,
                             const QColorGroup &g, QBrush *fill);
-    virtual void drawFocusRect(QPainter *p, const QRect &r, const QColorGroup &g,
+    void drawFocusRect(QPainter *p, const QRect &r, const QColorGroup &g,
                        const QColor *pen, bool atBorder);
-    virtual void drawSliderGrooveMask(QPainter *p, int x, int y, int w, int h,
+    void drawSliderGrooveMask(QPainter *p, int x, int y, int w, int h,
                               QCOORD, Orientation);
-    virtual int defaultFrameWidth() const {return(2);}
-    virtual void polishPopupMenu(QPopupMenu *mnu);
-    virtual int popupMenuItemHeight(bool c, QMenuItem *mi,
-                                    const QFontMetrics &fm);
+    int defaultFrameWidth() const {return(2);}
+    void polishPopupMenu(QPopupMenu *mnu);
     /*
-    virtual void drawTab(QPainter *p, const QTabBar *tabBar, QTab *tab,
+    void drawTab(QPainter *p, const QTabBar *tabBar, QTab *tab,
                  bool selected);
-    virtual void drawTabMask( QPainter*, const QTabBar*, QTab*, bool selected );
-    virtual void tabbarMetrics(const QTabBar *t, int &hFrame, int &vFrame,
+    void drawTabMask( QPainter*, const QTabBar*, QTab*, bool selected );
+    void tabbarMetrics(const QTabBar *t, int &hFrame, int &vFrame,
                        int &overlap);*/
 protected:
     void drawSBDeco(QPainter *p, const QRect &r, const QColorGroup &g,
                    bool horiz);
     void drawSBButton(QPainter *p, const QRect &r, const QColorGroup &g,
                       bool down=false);
-    virtual void drawSBDecoButton(QPainter *p, int x, int y, int w, int h,
-                                 const QColorGroup &g, bool down = false,
-                                 bool enabled = true);
-    virtual void drawSBArrow(QPainter *p, Qt::ArrowType type, bool down, int x,
-                             int y, int w, int h, const QColorGroup &g,
-                             bool enabled = true, const QBrush *fill = 0L);
-
 private:
     QColorGroup sliderGrp, sliderGrooveGrp, radioOnGrp;
-    bool beScrollArrow;
 };
 
 #endif
