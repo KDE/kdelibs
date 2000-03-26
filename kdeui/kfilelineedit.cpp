67bbaa0d53bb295e18b3fee372b92262bca3d767
Charles Samuels <charles@kde.org>
2000-03-26 02:16:27 +0000

    Added the K*LineEdit widgets.  A textbox with a browse button.  Just think about it :) Rikkus wrote this.  I'll add some documentation soon.  Unless, of course, he wants to :)
    svn path=/trunk/kdelibs/; revision=44723

---
+++ kdeui/kfilelineedit.cpp
@@ -0,0 +1,62 @@
+#include <qlayout.h>
+
+#include <kfiledialog.h>
+#include <kurl.h>
+
+#include "kfilelineedit.h"
+
+KPathLineEdit::KPathLineEdit
+    (const QString & initialPath, QWidget * parent)
+    :   QWidget(parent, "PathSelectWidget")
+{
+    le_path_    = new KLineEdit(initialPath, this);
+    pb_select_  = new QPushButton(this);
+    
+    pb_select_->setPixmap(BarIcon("fileopen"));
+    pb_select_->setFixedWidth(pb_select_->sizeHint().height());
+
+    QObject::connect(pb_select_, SIGNAL(clicked()), this, SLOT(s_browse()));
+
+    QHBoxLayout * layout = new QHBoxLayout(this);
+    layout->addWidget(le_path_);
+    layout->addWidget(pb_select_);
+}
+
+KPathLineEdit::~KPathLineEdit()
+{
+}
+
+    QString
+KPathLineEdit::path() const
+{
+    return le_path_->text();
+}
+
+    void
+KPathLineEdit::setPath(const QString & s)
+{
+    le_path_->setText(s);
+    emit(changed(s));
+}
+        
+    void
+KFileLineEdit::s_browse()
+{
+    KURL url =
+        KFileDialog::getOpenURL(le_path_->text(), QString::null, this);
+    
+    if (!url.isEmpty())
+        le_path_->setText(url.filename());
+}
+
+    void
+KDirLineEdit::s_browse()
+{
+    QString s = KFileDialog::getExistingDirectory(le_path_->text(), this);
+    
+    if (!s.isEmpty())
+        le_path_->setText(s);
+}
+
+
+#include "kfilelineedit.moc"
---
+++ kdeui/kfilelineedit.h
@@ -0,0 +1,78 @@
+#ifndef KFILELINEEDIT_H
+#define KFILELINEEDIT_H
+
+// Qt includes
+#include <qstring.h>
+#include <qlabel.h>
+#include <qwidget.h>
+#include <qpushbutton.h>
+#include <klineedit.h>
+
+// KDE includes
+#include <kbuttonbox.h>
+
+/**
+ * @short Browsable file/dir selector
+ * Quick hack to lay out a QLineEdit and a QPushButton to provide a megawidget
+ * that lets the user select a dir or a file path.
+ * @author Rikkus
+ */
+class KPathLineEdit : public QWidget
+{
+Q_OBJECT
+
+public:
+    KPathLineEdit(const QString & initialPath, QWidget * parent);
+    KPathLineEdit(QWidget *parent){KPathLineEdit(0,parent);}
+    virtual ~KPathLineEdit();
+    virtual QString path() const;
+    virtual void setPath(const QString &);
+        
+protected slots:
+    virtual void s_browse() = 0;
+
+signals:
+    virtual void changed(const QString &);
+
+protected:
+    QLineEdit *le_path_;
+    QPushButton *pb_select_;
+};
+
+/**
+ * @short Browsable file selector
+ * All required functions are in @ref KPathLineEdit
+ * @author Rikkus
+ */
+class KFileLineEdit : public KPathLineEdit
+{
+Q_OBJECT
+
+public:
+    KFileLineEdit(const QString & initialPath, QWidget * parent)
+        : KPathLineEdit(initialPath, parent)
+    {}
+
+protected slots:
+    virtual void s_browse();
+};
+
+/**
+ * @short Browsable directory selector
+ * All required functions are in @ref KPathLineEdit
+ * @author Rikkus
+ */
+class KDirLineEdit : public KPathLineEdit
+{
+Q_OBJECT
+public:
+    KDirLineEdit(const QString & initialPath, QWidget * parent)
+         : KPathLineEdit(initialPath, parent)
+    {}
+
+protected slots:
+     virtual void s_browse();
+};
+
+#endif
+
