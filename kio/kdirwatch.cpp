b7379f9469a03a31114a40d019a44a8f91d8476a
Kurt Granroth <granroth@kde.org>
2000-03-28 20:37:07 +0000

    Added license declarations.
    HOWEVER!  I don't know who the authors to all these files are.  I just
    put David as the copyright on all of them even though I know that coolo
    and waldo and matt koss and torben probably many others should have
    their names in addition or instead of david's.
    
    SO *please* go through the files and add your names to the copyright
    line if you think you should be there.
    
    svn path=/trunk/kdelibs/; revision=44997

--- kio/kdirwatch.cpp
+++ kio/kdirwatch.cpp
@@ -1,13 +1,20 @@
-/*
-  This file is or will be part of KDE desktop environment
+/* This file is part of the KDE libraries
+   Copyright (C) 1998 Sven Radej <sven@lisa.exp.univie.ac.at>
 
-  Copyright 1998 Sven Radej <sven@lisa.exp.univie.ac.at>
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License version 2 as published by the Free Software Foundation.
 
-  It is licensed under GPL version 2.
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
 
-  If it is part of KDE libraries than this file is licensed under
-  LGPL version 2.
- */
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
 
 #include <qtimer.h>
 #include <qstrlist.h>
