36851c6b2b01f5ce328f6ee75c88904801d600f0
Stefan Westerfeld <stefan@space.twc.de>
2000-04-01 22:50:58 +0000

    KAudioPlayer is a one-shot-play-and-forget audio playing class very similar to the old "KAudio" class. It should be usable for easy porting.
    svn path=/trunk/kdelibs/; revision=45348

---
+++ kdecore/kaudioplayer.h
@@ -0,0 +1,80 @@
+    /*
+
+    Copyright (C) 2000 Stefan Westerfeld
+                       stefan@space.twc.de
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+  
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+   
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+    Boston, MA 02111-1307, USA.
+
+    */
+
+#include <qobject.h>
+
+/**
+ * This class provides one-shot-and-forget audio playing. You will never
+ * know if what you wanted to play really got played.
+ *
+ * It doesn't require linking any special libraries, as it operates over
+ * DCOP. In the current implementation, it only indirectly communicates
+ * with the aRts soundserver, using knotify as DCOP -> MCOP bridge.
+ *
+ * Due to that fact, if you need "fast" response times, more control or
+ * feedback, use the MCOP interfaces rather than this.
+ *
+ * An example of using this class is:
+ *
+ *   KAudioPlayer::play("/var/share/foo.wav");
+ *
+ * If you want to use signals & slots, you can do something like:
+ *
+ *   KAudioPlayer player("/var/share/foo.wav");
+ *   connect(&button, SIGNAL(clicked()), &player, SLOT(play()));
+ *
+ */
+class KAudioPlayer : public QObject {
+Q_OBJECT
+private:
+	class KAudioPlayerPrivate *d;
+
+public:
+	/**
+	 * Constructor. 
+	 *
+	 * @param filename - the absolute path to the filename of the sound file
+	 *    to play.
+	 */
+	KAudioPlayer(QString filename);
+
+	/**
+	 * Destructor.
+	 */
+	~KAudioPlayer();
+
+	/**
+	 * Static play function.
+	 *
+	 * @param filename - the absolute path to the filename of the sound file
+	 *    to play.
+	 */
+	static void play(QString filename);
+
+public slots:
+	/**
+	 * Play function as slot.
+	 *
+	 * Plays the soundfile given to the constructor.
+	 */
+	void play();
+};
