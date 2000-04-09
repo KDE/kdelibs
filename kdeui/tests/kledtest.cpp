aafbbe0965ee499b01260a2001132c21cd02f808
Joerg Habenicht <j.habenicht@europemail.com>
2000-04-09 12:25:22 +0000

    can paint circular or recangular as you want to switches between the circular states and looks
    svn path=/trunk/kdelibs/; revision=46088

--- kdeui/tests/kledtest.cpp
+++ kdeui/tests/kledtest.cpp
@@ -7,11 +7,141 @@
 #include "kledtest.h"
 
 
+
+KLedTest::KLedTest(QWidget* parent)
+  : QWidget(parent, 0),
+    LedWidth(16),
+    LedHeight(10),
+    Grid(3),
+    kled_round(true) // Switch HERE between rectangle and circular leds
+{
+  if (kled_round) {
+    //KLed l(KLed::red, &qw);				// create lamp
+    //KLed l(KLed::blue, &qw);				// create lamp
+    l = new KLed(Qt::green, this);				// create lamp
+    //KLed l(KLed::yellow, &qw);				// create lamp
+    //KLed l(KLed::orange, &qw);				// create lamp
+
+
+    l->resize(16,30);
+    //l.setLook(KLed::flat);
+    l->setShape(KLed::Circular);
+    //l->setShape(KLed::Rectangular);
+
+    //l->setLook(KLed::Flat);
+    //l->setLook(KLed::Flat);
+    //l->setLook(KLed::Flat);
+
+    //l->move(5,5);
+    //    ktmp tmpobj(l);
+    
+    t_toggle.start(1000, FALSE);
+    //t_color.start(3500, FALSE);
+    t_look.start(3500, FALSE);
+    QObject::connect(&t_toggle, SIGNAL(timeout()), l, SLOT(toggle()));
+    //QObject::connect(&t_color, SIGNAL(timeout()), l, SLOT(nextColor()));
+    QObject::connect(&t_look, SIGNAL(timeout()), this, SLOT(nextLook()));
+    l->show();
+    resize(240,140);
+  }
+  else {
+    y=Grid; index=0;
+    for(shape=KLed::NoShape; shape!=KLed::NoOfShapes; shape=(KLed::Shape)(shape+1))
+      {
+      x=Grid;
+      for(look=KLed::NoLook; look!=KLed::NoOfLooks; look=(KLed::Look)(look+1))
+	{
+	  for(state=KLed::Off; state!=KLed::NoOfStates; state=(KLed::State)(state+1))
+	    {
+	      leds[index]=new KLed(Qt::yellow, state,
+				   (KLed::Look)(look+1),
+				   (KLed::Shape)(shape+1), this);
+	      leds[index]->setGeometry(x, y, LedWidth, LedHeight);
+	      ++index;
+	      x+=Grid+LedWidth;
+	    }
+	}
+      y+=Grid+LedHeight;
+      }
+    setFixedSize(x+Grid, y+Grid);
+    connect(&timer, SIGNAL(timeout()), SLOT(timeout()));
+    timer.start(500);
+  }
+}
+
+
+KLedTest::~KLedTest()
+{
+  if (kled_round) {
+    delete l;
+  }
+}
+
+/*
+void 
+KLedTest::nextColor() {
+  register int tmp = (static_cast<int>(ledcolor) + 1) % 5;
+  ledcolor=static_cast<KLed::Color>(tmp);
+  led->setColor(ledcolor);
+  //led->repaint(); 
+}
+*/
+
+void 
+KLedTest::nextLook() { 
+  register int tmp;
+  if (kled_round) {
+    tmp = (static_cast<int>(ledlook) ) % 3 + 1;
+  }
+  else {  
+    tmp = (static_cast<int>(ledlook) + 1) % 3;
+  }
+  ledlook = static_cast<KLed::Look>(tmp);
+  l->setLook(ledlook);
+  debug("painting look %i", ledlook);
+  //l->repaint(); 
+}
+
+
+void
+KLedTest::timeout() 
+{
+  const int NoOfLeds=sizeof(leds)/sizeof(leds[0]);
+  int count;
+  // -----
+  for(count=0; count<NoOfLeds; ++count)
+    {
+      if(leds[count]->state()==KLed::Off)
+	{
+	  leds[count]->setState(KLed::On);
+	} else {
+	  leds[count]->setState(KLed::Off);
+	}
+    }
+}
+
+
+/*#include <stdio.h>*/
+
 int main( int argc, char **argv )
 {
     KApplication a( argc, argv, "KLedTest" );
     KLedTest widget;
     // -----
+    /*
+    if (argc>1) { // look out for round or circular led command
+      if (strncmp(argv[1],"-c",2)) {	
+	// paint circular
+	printf("painting circular led\n");
+	widget.kled_round = true;
+      }
+      else if (strncmp(argv[1],"-r",2)) {
+	// paint rectangle
+	printf("painting rectangular led\n");
+	widget.kled_round = false;
+      }
+    }
+    */
     a.setMainWidget(&widget);
     widget.show();
     return a.exec();				// go
--- kdeui/tests/kledtest.h
+++ kdeui/tests/kledtest.h
@@ -20,52 +20,33 @@ protected:
   KLed::Look look;
   KLed::State state;
   int x, y, index;
+
+
+  QTimer t_toggle, t_color, t_look;
+  //KLed *l;				// create lamp
+  //KLed *l;				// create lamp
+  KLed *l;				// create lamp
+  //KLed *l;				// create lamp
+  //KLed *l;				// create lamp
+  //KLed::Color ledcolor;
+  KLed::Look  ledlook;
+
+
 public:
-  KLedTest(QWidget* parent=0)
-    : QWidget(parent, 0),
-      LedWidth(16),
-      LedHeight(10),
-      Grid(3)
-    {
-      y=Grid; index=0;
-      for(shape=KLed::NoShape; shape!=KLed::NoOfShapes; shape=(KLed::Shape)(shape+1))
-	{
-	  x=Grid;
-	  for(look=KLed::NoLook; look!=KLed::NoOfLooks; look=(KLed::Look)(look+1))
-	    {
-	      for(state=KLed::Off; state!=KLed::NoOfStates; state=(KLed::State)(state+1))
-		{
-		  leds[index]=new KLed(Qt::yellow, state,
-				       (KLed::Look)(look+1),
-				       (KLed::Shape)(shape+1), this);
-		  leds[index]->setGeometry(x, y, LedWidth, LedHeight);
-		  ++index;
-		  x+=Grid+LedWidth;
-		}
-	    }
-	  y+=Grid+LedHeight;
-	}
-      setFixedSize(x+Grid, y+Grid);
-      connect(&timer, SIGNAL(timeout()), SLOT(timeout()));
-      timer.start(500);
-    }
-  ~KLedTest() {};
+
+  KLedTest(QWidget* parent=0);
+  ~KLedTest();
+
+  bool kled_round;
+
+
 public slots:
-  void timeout() 
-    {
-      const int NoOfLeds=sizeof(leds)/sizeof(leds[0]);
-      int count;
-      // -----
-      for(count=0; count<NoOfLeds; ++count)
-	{
-	  if(leds[count]->state()==KLed::Off)
-	    {
-	      leds[count]->setState(KLed::On);
-	    } else {
-	      leds[count]->setState(KLed::Off);
-	    }
-	}
-    }
+  void timeout();
+
+// void nextColor();
+  void nextLook();
+
+
 };
 
 #endif
