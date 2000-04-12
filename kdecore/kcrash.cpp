ea9aafcc5649ab34da6505acc64480b0faed6bde
David Faure <faure@kde.org>
2000-04-12 21:57:08 +0000

    if handler = 0, just call signal with SIG_DFL. Not sure what calling with 0L as second arg does, but it's useless anyway. I wonder what that #warning in the header is about. setCrashHandler(myFunction) where myFunction is a static void myFunction(int) works just fine, no need for such a strange syntax.
    svn path=/trunk/kdelibs/; revision=46465

--- kdecore/kcrash.cpp
+++ kdecore/kcrash.cpp
@@ -65,8 +65,8 @@ KCrash::setCrashHandler (HandlerType handler)
 {
   if (!handler)
     signal (SIGSEGV, SIG_DFL);
-
-  signal (SIGSEGV, handler);
+  else
+    signal (SIGSEGV, handler);
 
   _crashHandler = handler;
 }
@@ -77,7 +77,7 @@ KCrash::defaultCrashHandler (int signal)
   // Handle possible recursions
   static int crashRecursionCounter = 0;
 
-  printf("KCRash: crashing.... crashRecusrionCounter = %d\n", crashRecursionCounter);
+  printf("KCrash: crashing.... crashRecusrionCounter = %d\n", crashRecursionCounter);
   printf("Appname = %p apppath = %p\n", appName, appPath);
 
   crashRecursionCounter++;
