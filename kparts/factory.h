9aa2ad2bb631c0249d2eaa513afac2fdf71bdf96
Lars Knoll <knoll@kde.org>
2000-04-15 10:39:50 +0000

    some doc about the QStringList argument of the createPart method.
    svn path=/trunk/kdelibs/; revision=46669

--- kparts/factory.h
+++ kparts/factory.h
@@ -29,6 +29,9 @@ namespace KParts
 
 class Part;
 
+/**
+ * A generic factory object to create a Part. @see_also KLibFactory.
+ */
 class Factory : public KLibFactory
 {
   Q_OBJECT
@@ -36,7 +39,29 @@ public:
   Factory( QObject *parent = 0, const char *name = 0 );
   virtual ~Factory();
 
-  virtual Part *createPart( QWidget *parentWidget = 0, const char *widgetName = 0, QObject *parent = 0, const char *name = 0, const char *classname = "KParts::Part", const QStringList &args = QStringList() ) = 0;
+    /**
+     * reimplement this method in your implementation to create the Part.
+     *
+     * The QStrinList can be used to pass additional arguments to the part.
+     * If the part needs additional arguments, it should take them as
+     * name="value" pairs. This is the way additional arguments will get passed
+     * to the part from eg. khtml. You can for example emebed the part into HTML
+     * by using the following code:
+     * <pre>
+     *    &lt;object type="my_mimetype" data="url_to_my_data"&gt;
+     *        &lt;param name="name1" value="value1"&gt;
+     *        &lt;param name="name2" value="value2"&gt;
+     *    &lt;/object&gt;
+     * </pre>
+     * This could result in a call to 
+     * <pre>
+     *     createPart( parentWidget, name, parentObject, parentName, "Kparts::Part",
+     *                 QStrinList("name1=\"value1\"", "name2=\"value2\"") );
+     * </pre>
+     * 
+     * @returns the newly created part.
+     */
+    virtual Part *createPart( QWidget *parentWidget = 0, const char *widgetName = 0, QObject *parent = 0, const char *name = 0, const char *classname = "KParts::Part", const QStringList &args = QStringList() ) = 0;
 
   virtual QObject *create( QObject *parent = 0, const char *name = 0, const char *classname = "QObject", const QStringList &args = QStringList() );
 
