c39f510c84077dd9b6c6cae353d856931ae8753a
Michael Matz <matz@kde.org>
2000-05-14 08:55:14 +0000

    openURL() : in rev. 1.76 the calling of file_copy() was changed, but some default args were ommited leading to hard to find errors with files not opened in text/plain views for data remotely loaded into temp files.  Death to all default args ;)
    svn path=/trunk/kdelibs/; revision=49409

--- kparts/part.cpp
+++ kparts/part.cpp
@@ -289,7 +289,7 @@ bool ReadOnlyPart::openURL( const KURL &url )
     // We can't use mkstemp since we don't want to create the file here
     // KIO::Job has to create it
 
-    d->m_job = KIO::file_copy( m_url, m_file, false, false, d->m_showProgressInfo );
+    d->m_job = KIO::file_copy( m_url, m_file, -1, false, false, d->m_showProgressInfo );
     emit started( d->m_job );
     connect( d->m_job, SIGNAL( result( KIO::Job * ) ), this, SLOT( slotJobFinished ( KIO::Job * ) ) );
     return true;
