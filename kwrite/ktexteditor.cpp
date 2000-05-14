85e00cca277ac9c85b8d48543ae136b91ad2b054
Simon Hausmann <hausmann@kde.org>
2000-05-14 19:36:59 +0000

    - first draft of a (more or less) abstract text editor interface, based on   the QMultiLineEdit API but as document-view model   the idea is to make 100% oof kwrite's API private (not to install any headers) and   to make kwrite implement this interface
    svn path=/trunk/kdelibs/; revision=49472

---
+++ kwrite/ktexteditor.cpp
@@ -0,0 +1,96 @@
+
+#include "ktexteditor.h"
+
+using namespace KTextEditor;
+
+class View::ViewPrivate
+{
+public:
+  ViewPrivate()
+  {
+  }
+  ~ViewPrivate()
+  {
+  }
+
+  Document *m_doc;
+};
+
+View::View( Document *doc, QWidget *parent, const char *name )
+: QWidget( parent, name )
+{
+  d = new ViewPrivate;
+  d->m_doc = doc;
+}
+
+View::~View()
+{
+  delete d;
+}
+
+Document *View::textDocument() const
+{
+  return d->m_doc;
+}
+
+class Document::DocumentPrivate
+{
+public:
+  DocumentPrivate()
+  {
+  }
+  ~DocumentPrivate()
+  {
+  }
+
+  QList<View> m_views;
+};
+
+Document::Document( QObject *parent, const char *name )
+ : KParts::ReadWritePart( parent, name )
+{
+  d = new DocumentPrivate;
+}
+
+Document::~Document()
+{
+  delete d;
+}
+
+QList<View> Document::views() const
+{
+  return d->m_views; 
+} 
+
+void Document::addView( View *view )
+{
+  if ( !view )
+    return;
+
+  if ( d->m_views.findRef( view ) != -1 )
+    return;
+
+  d->m_views.append( view );
+  connect( view, SIGNAL( destroyed() ),
+	   this, SLOT( slotViewDestroyed() ) );
+}
+
+void Document::removeView( View *view )
+{
+  if ( !view )
+    return;
+
+  disconnect( view, SIGNAL( destroyed() ),
+	      this, SLOT( slotViewDestroyed() ) );
+
+  d->m_views.removeRef( view );
+}
+
+void Document::slotViewDestroyed()
+{
+  const View *view = static_cast<const View *>( sender() );
+  d->m_views.removeRef( view );
+}
+
+
+#include "ktexteditor.moc"
---
+++ kwrite/ktexteditor.h
@@ -0,0 +1,75 @@
+#ifndef __ktexteditor_h__
+#define __ktexteditor_h__
+
+#include <kparts/part.h>
+#include <qwidget.h>
+
+namespace KTextEditor
+{
+
+class Document;
+
+class View : public QWidget, public KXMLGUIClient
+{
+public:
+  View( Document *doc, QWidget *parent, const char *name = 0 );
+  virtual ~View();
+
+  virtual Document *textDocument() const; // XXX fix when renaming KXMLGUIClient::document
+
+  virtual void setCursorPosition( int line, int col, bool mark = false ) = 0;
+  virtual void getCursorPosition( int *line, int *col ) = 0;
+
+  virtual void setSelection( int row_from, int col_from, int row_to, int col_t ) = 0;
+  virtual bool hasSelection() const = 0;
+  virtual QString selection() const = 0;
+
+private:
+  class ViewPrivate;
+  ViewPrivate *d;
+};
+
+class Document : public KParts::ReadWritePart
+{
+  Q_OBJECT
+public:
+  Document( QObject *parent = 0, const char *name = 0 );
+  virtual ~Document();
+
+  virtual View *createView( QWidget *parent, const char *name ) = 0;
+
+  virtual QList<View> views() const;
+
+  virtual QString textLine( int line ) const = 0;
+  virtual int numLines() const = 0;
+
+  virtual void insertLine( const QString &s, int line = -1 ) = 0;
+  virtual void insertAt( const QString &s, int line, int col, bool mark = FALSE ) = 0;
+  virtual void removeLine( int line ) = 0;
+
+  virtual QString text() const = 0;
+  virtual int length() const = 0;
+
+  virtual bool isOverwriteMode() const = 0;
+
+public slots:
+  virtual void setText( const QString &t ) = 0;
+  virtual void setOverwriteMode( bool b ) = 0;
+signals:
+  void textChanged();
+
+protected:
+  virtual void addView( View *view );
+  virtual void removeView( View *view );
+
+private slots:
+  void slotViewDestroyed();
+
+private:
+  class DocumentPrivate;
+  DocumentPrivate *d;
+};
+
+};
+
+#endif
