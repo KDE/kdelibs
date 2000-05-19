3e852d4115166b3387c1827ab9309ec1a77acc29
Matthias Hoelzer-Kluepfel <hoelzer@kde.org>
2000-05-19 14:32:40 +0000

    Added g3 fax reading.
    Note that you could read these files with the TIFF
    library, but this module saves you about
    13.99 MB per page.
    
    svn path=/trunk/kdelibs/; revision=49984

---
+++ kimgio/g3r.cpp
@@ -0,0 +1,52 @@
+// This library is distributed under the conditions of the GNU LGPL.
+
+#ifdef HAVE_CONFIG_H
+#include"config.h"
+#endif
+
+#ifdef HAVE_LIBTIFF
+
+#include<tiffio.h>
+#include<qimage.h>
+
+#include"g3r.h"
+
+
+void kimgio_g3_read( QImageIO *io )
+{
+  TIFF *tiff = TIFFOpen(io->fileName(), "r");  
+  if (!tiff)
+    return;
+ 
+  uint32 width, height;
+  tsize_t scanlength;
+
+  TIFFGetField( tiff, TIFFTAG_IMAGEWIDTH, &width );
+  TIFFGetField( tiff, TIFFTAG_IMAGELENGTH, &height );
+  scanlength = TIFFScanlineSize(tiff);
+
+  QImage image(width, height, 1, 0, QImage::BigEndian);
+  
+  if (scanlength != image.bytesPerLine())
+    {
+      TIFFClose(tiff);
+      return;
+    }
+
+  for (uint32 y=0; y < height; y++)
+    TIFFReadScanline(tiff, image.scanLine(y), y);
+
+  TIFFClose(tiff);
+  
+  io->setImage(image);
+  io->setStatus(0);
+}
+
+
+void kimgio_g3_write(QImageIO *)
+{
+	// TODO: stub
+}
+
+
+#endif
