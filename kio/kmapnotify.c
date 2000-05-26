60d5270b578070cc09c86e4862bc75ef9e0c462a
Rik Hemsley <rik@kde.org>
2000-05-26 19:57:27 +0000

    C version now set for compilation (uses libICE). Let's see how this compiles for people. The actual work it does is mostly disabled, because I haven't finished it yet. Anyone with good inside knowledge of DCOP want to help ?
    svn path=/trunk/kdelibs/; revision=50807

--- kio/kmapnotify.c
+++ kio/kmapnotify.c
@@ -1,6 +1,6 @@
 /*
- * Override X11 functions and communicate with kxmlrpcd, to provide
- * app-starting indication.
+ * Override X11 functions and communicate via DCOP, to provide app-starting
+ * notification.
  *
  * The original functions XMapWindow() and XMapRaised() are replaced with new
  * versions. These will call the function KDE_InterceptXMapRequest() if it
@@ -10,76 +10,34 @@
  * KDE_RealXMapRaised and KDE_RealXMapWindow to the original functions, so
  * that they may be called later.
  *
- * Next, we reads the file ~/.kxmlrpcd and extracts the port and key. We then
- * attempt to connect to localhost on the given port, authenticate ourselves
- * using the key, and send a signal out, informing whichever app wants to know
- * (currently kicker) that the application we are being preloaded before is
- * executing.
- *
- * If any part of the process of dlopen()ing libX11 and finding the functions
- * XMapRaised() and XMapWindow() fails, this is a fatal error. The dynamic
- * linker would normally do this automatically, so if we can't do it, we
- * presume it wouldn't be able to either. Hopefully this is a valid
- * presumption.
- *
- * If any part of the process of talking to kxmlrpcd fails, this is non-fatal.
- * It is not vital that we send a signal out, so we just give up.
- *
- * Note: All symbols should be prefixed with KDE_, to avoid polluting the
- * global namespace.
+ * Next we send a DCOP signal... I'll explain what happens there once I figure
+ * it out myself.
  *
  * (C) 2000 Rik Hemsley <rik@kde.org>
  */
 
 #include <sys/types.h>
-
-/* Stolen from ksock.h */
-/* we define STRICT_ANSI to get rid of some warnings in glibc */
-#ifndef __STRICT_ANSI__
-#define __STRICT_ANSI__
-#define _WE_DEFINED_IT_
-#endif
-#include <sys/socket.h>
-#ifdef _WE_DEFINED_IT_
-#undef __STRICT_ANSI__
-#undef _WE_DEFINED_IT_
-#endif
-
-#include <netinet/in.h>
-#include <netdb.h>
-
 #include <sys/stat.h>
 
-#include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <dlfcn.h>
+#include <fcntl.h>
+
 #include <string.h>
 
+#include <dlfcn.h>
+
 #include <X11/Xlib.h>
+#include <X11/X.h>
+#include <X11/Xproto.h>
+#include <X11/ICE/ICE.h>
+#include <X11/ICE/ICElib.h>
+#include <X11/ICE/ICEutil.h>
+#include <X11/ICE/ICEconn.h>
+#include <X11/ICE/ICEmsg.h>
+#include <X11/ICE/ICEproto.h>
 
-#ifdef HAVE_CONFIG_H
-#include "config.h"
-#else
-#define ksize_t size_t
-#endif
-
-#ifdef INET6
-#define KDE_SOCKADDR_IN sockaddr_in6
-#define KDE_PF_INET PF_INET6
-#define KDE_SET_SIN_FAMILY(a, b) (a).sin6_family = b
-#define KDE_SET_SIN_PORT(a, b) (a).sin6_port = b
-#define KDE_SET_SIN_ADDR(a, b) memcpy(&((a).sin6_addr), b->h_addr_list[0], b->h_length)
-#else
-#define KDE_SOCKADDR_IN sockaddr_in
-#define KDE_PF_INET PF_INET
-#define KDE_SET_SIN_FAMILY(a, b) (a).sin_family = b
-#define KDE_SET_SIN_PORT(a, b) (a).sin_port = b
-#define KDE_SET_SIN_ADDR(a, b) memcpy(&((a).sin_addr), b->h_addr_list[0], b->h_length)
-#endif
-
-// We're overriding these two functions.
 int XMapWindow(Display *, Window);
 int XMapRaised(Display *, Window);
 
@@ -90,31 +48,6 @@ KDE_XMapRequestSignature KDE_RealXMapRaised = NULL;
 
 void KDE_InterceptXMapRequest();
 
-int KDE_WriteToSocket(int sock, const char * data)
-{
-  int bytesWritten;
-  int retval = 0;
-  int stringLength = strlen(data);
-
-  bytesWritten = write(sock, data, stringLength); 
-
-  if (-1 == bytesWritten) {
-    fprintf(stderr, "kmapnotify: ");
-    perror("write");
-    close(sock);
-    retval = -1;
-  }
-
-  if (bytesWritten != stringLength) {
-    fprintf(stderr, "kmapnotify: Short write to socket !\n");
-    close(sock);
-    retval = -1;
-  }
-
-  return retval;
-}
-
-  extern
   int
 XMapWindow(Display * d, Window w)
 {
@@ -124,7 +57,6 @@ XMapWindow(Display * d, Window w)
   return KDE_RealXMapWindow(d, w);
 }
 
-  extern
   int
 XMapRaised(Display * d, Window w)
 {
@@ -137,39 +69,103 @@ XMapRaised(Display * d, Window w)
   void
 KDE_InterceptXMapRequest()
 {
-  struct KDE_SOCKADDR_IN serverAddress; 
+  /* Helper functions ****************************************************/
 
-  struct hostent * hostinfo = 0L;
+    char *
+  writeLong(char * buf, long i)
+  {
+    char * p = (char *)(&i);
+    buf[3] = *p++;
+    buf[2] = *p++;
+    buf[1] = *p++;
+    buf[0] = *p;
 
-  int     fd                = -1;
+    return buf + 4;
+  }
+
+    char *
+  writeText(char * buf, const char * text)
+  {
+    char * pos = buf;
+    pos = writeLong(buf, strlen(text));
+    memcpy(pos, text, strlen(text));
+    return pos + strlen(text);
+  }
+
+  /* Vars *****************************************************************/
+
+    struct
+  DCOPMsg {
+    CARD8 majorOpcode;
+    CARD8 minorOpcode;
+    CARD8 data[2];
+    CARD32 length B32;
+    CARD32 time;
+  };
+
+    extern
+    IcePoAuthStatus
+  _IcePoMagicCookie1Proc(
+    IceConn,
+    void **,
+    int,
+    int,
+    int,
+    void *,
+    int *,
+    void **,
+    char **
+  );
+
+  const char * receiver     = "kicker";
+  const char * object       = "AppStarting";
+  const char * function     = "clientMapped(long int)";
+
+  char  * pos               = 0L;
+  char  * vendor            = 0L;
+  char  * release           = 0L;
+  char  * appId             = 0L;
+  char  * newline           = 0L;
   void  * libX11Handle      = 0L;
   char  * homeDir           = 0L;
-  char  * xmlrpcFile        = 0L;
-  char  * xmlrpcBuf         = 0L;
-  char  * commaPosition     = 0L;
-  char  * key               = 0L;
-  int     port              = -1;
+  char  * dcopServer        = 0L;
+  char  * streamed          = 0L;
+  char  * data              = 0L;
+
+  int     setupstat         = 0; 
+  int     streamedSize      = 0;
+  int     majorOpcode       = 0;
+  int     fd                = 0;
+  int     majorVersion      = 0;
+  int     minorVersion      = 0;
   ssize_t bytesRead         = 0;
-  int     sock              = -1;
-  int     connectOK         = 0;
-  int     contentLength     = 0;
-  char  * contentLengthStr  = 0L;
-  char  * authParamStr      = 0L;
-  char  * pidParamStr       = 0L;
 
-  const char * xmlrpcFilename = "/.kxmlrpcd";
- 
-  const char * commandStart = "<?xml version=\"1.0\"?><methodCall><methodName>AppStarting.clientMapped</methodName><params>";
+  char    errBuf[1024];
 
-  const char * textParamPattern = "<param><value>%s</value></param>";
-  const char * longParamPattern = "<param><value>%ld</value></param>";
+  IceConn     iceConn;
+  IcePointer  context;
+  IcePointer  somethingToPassToProtoSetup;
 
-  const char * commandEnd = "</params></methodCall>";
+  struct DCOPMsg   pMsg;
+  struct DCOPMsg * pMsgPtr = &pMsg;
 
-  const char * header =
-    "POST /kicker HTTP/1.0\nContent-Type: text/xml\nContent-length: ";
+  CARD32  time;
+
+/*  extern int _IceLastMajorOpcode; */
+
+  static IcePoVersionRec DCOPVersions[] = {
+    { 1, 1, 0L } /* last arg is callback - none ok as 0L ? */
+  };
+
+  const char * DCOPAuthNames[] = { "MIT-MAGIC-COOKIE-1" };
+
+  IcePoAuthProc DCOPClientAuthProcs[] = { _IcePoMagicCookie1Proc };
+  
+  /* Init *****************************************************************/
 
   putenv("LD_PRELOAD=");
+  
+  /* Find symbols *********************************************************/
 
   libX11Handle = dlopen("libX11.so", RTLD_GLOBAL | RTLD_NOW);
 
@@ -181,155 +177,206 @@ KDE_InterceptXMapRequest()
     exit(1);
   }
 
-  KDE_RealXMapWindow = dlsym(libX11Handle, "XMapWindow");
+  KDE_RealXMapWindow =
+    (KDE_XMapRequestSignature)dlsym(libX11Handle, "XMapWindow");
 
   if (NULL == KDE_RealXMapWindow) {
     fprintf(stderr, "KDE: Could not find symbol XMapWindow in libX11\n");
     exit(1);
   }
  
-  KDE_RealXMapRaised = dlsym(libX11Handle, "XMapRaised");
+  KDE_RealXMapRaised =
+    (KDE_XMapRequestSignature)dlsym(libX11Handle, "XMapRaised");
 
   if (NULL == KDE_RealXMapRaised) {
     fprintf(stderr, "KDE: Could not find symbol XMapRaised in libX11\n");
     exit(1);
   }
   
+  /* Do some DCOP *********************************************************/
+  
+  /* DISABLED */
+  return;
+  
+  /* Find home dir ********************************************************/
+
   homeDir = getenv("HOME");
 
   if (NULL == homeDir) {
     fprintf(stderr, "KDE: Could not find home directory !\n");
     return;
   }
-
-  xmlrpcFile = (char *)malloc(strlen(homeDir) + strlen(xmlrpcFilename));
-
-  if (NULL == xmlrpcFile) {
-    fprintf(stderr, "kmapnotify: ");
-    perror("malloc");
-    return;
-  }
-
-  memcpy(xmlrpcFile, homeDir, strlen(homeDir));
-  memcpy(xmlrpcFile + strlen(homeDir), xmlrpcFilename, strlen(xmlrpcFilename) + 1);
-
-  fd = open(xmlrpcFile, O_RDONLY);
-
-  free(xmlrpcFile);
-
-  if (-1 == fd) {
-    fprintf(stderr, "kmapnotify: ");
-    perror("open");
-    return;
-  }
-
-  xmlrpcBuf = (char *)malloc(64);
-
-  if (NULL == xmlrpcBuf) {
-    fprintf(stderr, "kmapnotify: ");
-    perror("malloc");
-    return;
-  }
-
-  bytesRead = read(fd, (void *)xmlrpcBuf, 64);
-
-  if (-1 == bytesRead) {
-    fprintf(stderr, "kmapnotify: ");
-    perror("read");
-    return;
-  }
-
-  close(fd);
-
-  commaPosition = (char *)memchr(xmlrpcBuf, ',', bytesRead);
-
-  if (NULL == commaPosition) {
-    fprintf(stderr, "kmapnotify: .kxmlrpcd file is in wrong format !\n");
-    return;
-  }
-
-  *commaPosition = '\0';
-  port = atoi(xmlrpcBuf);
-
-  key = ++commaPosition;
-  key[16] = '\0';
-
-  sock = socket(KDE_PF_INET, SOCK_STREAM, 0);
-
-  if (-1 == sock) {
-    fprintf(stderr, "kmapnotify: ");
-    perror("socket");
-    return;
-  }
-
-  serverAddress.sin_family = KDE_PF_INET;
-
-  memset(&serverAddress, 0, sizeof(serverAddress));
-
-  hostinfo = gethostbyname("localhost");
   
-  if (NULL == hostinfo) {
-    fprintf(stderr, "kmapnotify: ");
-    perror("gethostbyname");
+  /* ICE setup ************************************************************/
+
+  majorOpcode =
+    IceRegisterForProtocolSetup(
+      (char *)("DCOP"),
+      (char *)("KDE"),
+      (char *)("1.1"),
+      1,
+      DCOPVersions,
+      1,
+      (char **)DCOPAuthNames,
+      DCOPClientAuthProcs,
+      0L
+    );
+
+  if (majorOpcode < 0) {
+    fprintf(stderr, "IceRegisterForProtocolSetup failed\n");
     return;
   }
 
-  KDE_SET_SIN_FAMILY(serverAddress, hostinfo->h_addrtype);
-  KDE_SET_SIN_PORT(serverAddress, htons(port));
-  KDE_SET_SIN_ADDR(serverAddress, hostinfo);
+  /* Find DCOP server and open connection ********************************/
 
-  connectOK = connect(sock, &serverAddress, sizeof(serverAddress));
+  dcopServer = getenv("DCOPSERVER");
 
-  if (-1 == connectOK) {
-    fprintf(stderr, "kmapnotify: ");
-    perror("connect");
-    return;
+  if (NULL == dcopServer) {
+
+    if (-1 == chdir(homeDir)) {
+      fprintf(stderr, "Could not change directory to %s\n", homeDir);
+      return;
+    }
+
+    fd = open(".DCOPserver", O_RDONLY);
+
+    if (-1 == fd) {
+      fprintf(stderr, "Cannot open ~/.DCOPserver\n");
+      return;
+    }
+
+    dcopServer = (char *)malloc(1024);
+
+    bytesRead = read(fd, dcopServer, 1024);
+
+    if (-1 == bytesRead) {
+      fprintf(stderr, "Cannot read from ~/.DCOPserver\n");
+      return;
+    }
+
+    newline = strchr(dcopServer, '\n');
+
+    if (NULL == newline) {
+      fprintf(stderr, "Invalid data in ~/.DCOPserver\n");
+      return;
+    }
+
+    *newline = '\0';
+
+    iceConn =
+      IceOpenConnection(
+        dcopServer,
+        context,
+        False,
+        majorOpcode,
+        sizeof(errBuf),
+        errBuf
+      );
+ 
+    free(dcopServer);
+
+  } else {
+
+    iceConn =
+      IceOpenConnection(
+        dcopServer,
+        context,
+        False,
+        majorOpcode,
+        sizeof(errBuf),
+        errBuf
+      );
   }
-
-  authParamStr = (char *)malloc(strlen(textParamPattern) + 14);
-
-  if (NULL == authParamStr) {
-    fprintf(stderr, "kmapnotify: ");
-    perror("malloc");
-    return;
-  }
-
-  pidParamStr = (char *)malloc(strlen(longParamPattern) + sizeof(long) * 8 - 2);
-
-  if (NULL == pidParamStr) {
-    fprintf(stderr, "kmapnotify: ");
-    perror("malloc");
-    return;
-  }
-
-  sprintf(authParamStr, textParamPattern, key);
-  sprintf(pidParamStr, longParamPattern, (long)getpid());
-
-  contentLength =
-    strlen(commandStart) +
-    strlen(authParamStr) +
-    strlen(pidParamStr) +
-    strlen(commandEnd);
-
-  contentLengthStr = (char *)malloc(sizeof(int) * 8);
-
-  if (NULL == contentLengthStr) {
-    fprintf(stderr, "kmapnotify: ");
-    perror("malloc");
-    return;
-  }
-
-  sprintf(contentLengthStr, "%d", contentLength);
   
-  if (0 != KDE_WriteToSocket(sock, header))           return;
-  if (0 != KDE_WriteToSocket(sock, contentLengthStr)) return;
-  if (0 != KDE_WriteToSocket(sock, commandStart))     return;
-  if (0 != KDE_WriteToSocket(sock, authParamStr))     return;
-  if (0 != KDE_WriteToSocket(sock, pidParamStr))      return;
-  if (0 != KDE_WriteToSocket(sock, commandEnd))       return;
+  if (iceConn == 0L) {
+    fprintf(stderr, "IceOpenConnection failed\n");
+    return;
+  }
 
-  fprintf(stderr, "kmapnotify finished OK\n");
-  close(sock);
+  IceSetShutdownNegotiation(iceConn, False);
+  
+  /* Setup protocol ******************************************************/
+
+  setupstat =
+    IceProtocolSetup(
+      iceConn,
+      majorOpcode,
+      somethingToPassToProtoSetup,
+      True, /* must authenticate */
+      &(majorVersion), &(minorVersion), &(vendor), &(release),
+      1024, errBuf
+    );
+
+  if (
+    setupstat == IceProtocolSetupFailure ||
+    setupstat == IceProtocolSetupIOError ||
+    setupstat == IceProtocolAlreadyActive)
+  {
+    fprintf(stderr, "IceProtocolSetup failed\n");
+    return;
+  }
+
+  if (IceConnectionStatus(iceConn) != IceConnectAccepted) {
+    fprintf(stderr, "DCOP server did not accept the connection\n");
+    return;
+  }
+
+  /* Register with DCOP **************************************************/
+
+/*  fprintf(stderr, "Registering with DCOP\n"); */
+
+  appId = (char *)malloc(64);
+  sprintf(appId, "%s-%d", "anonymous", getpid());
+
+  /* now do equivalent of:
+   * call("DCOPServer", "", "registerAs(QCString)", data, replyType,replyData)
+   * and check result.
+   */
+
+/*  fprintf(stderr, "Registered with DCOP\n"); */
+
+  /* Send data to DCOP ***************************************************/
+
+  IceGetHeader(
+    iceConn,
+    majorOpcode,
+    1 /* DCOPSend */,
+    sizeof(struct DCOPMsg),
+    struct DCOPMsg,
+    pMsgPtr
+  );
+
+  data = (char *)malloc(128);
+  streamed = (char *)malloc(1024);
+
+  writeLong(data, (long)getpid());
+
+  pos = streamed;
+
+  pos = writeText(pos, appId);
+  pos = writeText(pos, receiver);
+  pos = writeText(pos, object);
+  pos = writeText(pos, function);
+  pos = writeLong(pos, strlen(data));
+
+  streamedSize = pos - streamed;
+
+  pMsg.time = time = 0;
+  pMsg.length += streamedSize + strlen(data);
+
+  IceSendData(iceConn, streamedSize, streamed);
+  IceSendData(iceConn, strlen(data), data);
+  
+  free(appId);
+  free(data);
+  free(streamed);
+
+  if (IceConnectionStatus(iceConn) != IceConnectAccepted) {
+    fprintf(stderr, "IceConnectionStatus != IceConnectAccepted\n");
+    return;
+  }
+  
+  /* Done ***************************************************************/
 }
 
-// vim:ts=2:sw=2:tw=78
+/* vim: set ts=2:sw=2:tw=78: */
