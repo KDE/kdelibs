cc7c5d374cb3fdda43e70a1897fd81fe47f1943f
Rik Hemsley <rik@kde.org>
2000-05-30 18:58:42 +0000

    Beginnings of a C library that talks to DCOP via libICE. Will be used for libkmapnotify very shortly...
    svn path=/trunk/kdelibs/; revision=51228

---
+++ dcop/dcopc.c
@@ -0,0 +1,302 @@
+/*
+ * C interface to DCOP
+ *
+ * (C) 2000 Rik Hemsley <rik@kde.org>
+ * (C) 2000 Simon Hausmann <hausmann@kde.org>
+ */
+
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+#include <string.h>
+
+/* sometimes __STDC__ is defined, but to 0. The hateful X headers
+   ask for '#if __STDC__', so they become confused. */
+#ifdef __STDC__
+#if !__STDC__
+#undef __STDC__
+#define __STDC__ 1
+#endif
+#endif
+#include <X11/Xlib.h>
+#include <X11/X.h>
+#include <X11/Xproto.h>
+#include <X11/ICE/ICE.h>
+#include <X11/ICE/ICElib.h>
+#include <X11/ICE/ICEutil.h>
+#include <X11/ICE/ICEconn.h>
+#include <X11/ICE/ICEmsg.h>
+#include <X11/ICE/ICEproto.h>
+
+#include "dcopc.h"
+
+  char *
+dcop_write_int(char * buf, int i)
+{
+  char * p = (char *)(&i);
+
+  buf[3] = *p++;
+  buf[2] = *p++;
+  buf[1] = *p++;
+  buf[0] = *p;
+
+  return buf + 4;
+}
+
+  char *
+dcop_write_string(char * buf, const char * text)
+{
+  char * pos = buf;
+  int l = strlen( text ) + 1; /* we need the \0! (Simon) */
+  pos = dcop_write_int(buf, l);
+  memcpy(pos, text, l);
+  return pos + l;
+}
+
+  int
+dcop_send_signal(
+  const char * receiving_app,
+  const char * object,
+  const char * function,
+  char * data,
+  int dataLength
+)
+{
+  /* Vars *****************************************************************/
+
+    struct
+  DCOPMsg {
+    CARD8 majorOpcode;
+    CARD8 minorOpcode;
+    CARD8 data[2];
+    CARD32 length B32;
+    CARD32 time;
+  };
+
+    extern
+    IcePoAuthStatus
+  _IcePoMagicCookie1Proc(
+    IceConn,
+    void **,
+    int,
+    int,
+    int,
+    void *,
+    int *,
+    void **,
+    char **
+  );
+
+  char  * pos               = 0L;
+  char  * vendor            = 0L;
+  char  * release           = 0L;
+  char  * newline           = 0L;
+  char  * homeDir           = 0L;
+  char  * dcopServer        = 0L;
+  char  * header            = 0L;
+  unsigned int headerLength = 0;
+
+  int     setupstat         = 0;
+  int     majorOpcode       = 0;
+  int     fd                = 0;
+  int     majorVersion      = 0;
+  int     minorVersion      = 0;
+  ssize_t bytesRead         = 0;
+
+  char    errBuf[1024];
+
+  IceConn     iceConn;
+  IcePointer  context = 0;
+  IcePointer  somethingToPassToProtoSetup = 0;
+
+  struct DCOPMsg * pMsgPtr = 0;
+
+  static IcePoVersionRec DCOPVersions[] = {
+    { 1, 1, 0L } /* last arg is callback - none ok as 0L ? */
+  };
+
+  const char * DCOPAuthNames[] = { "MIT-MAGIC-COOKIE-1" };
+
+  IcePoAuthProc DCOPClientAuthProcs[] = { _IcePoMagicCookie1Proc };
+
+  /* Find home dir ********************************************************/
+
+  homeDir = getenv("HOME");
+
+  if (NULL == homeDir)
+    return DCOP_ERROR_SERVER_NOT_FOUND;
+
+  /* ICE setup ************************************************************/
+
+  majorOpcode =
+    IceRegisterForProtocolSetup(
+      (char *)("DCOP"),
+      (char *)("KDE"),
+      (char *)("1.1"),
+      1,
+      DCOPVersions,
+      1,
+      (char **)DCOPAuthNames,
+      DCOPClientAuthProcs,
+      0L
+    );
+
+  if (majorOpcode < 0)
+    return DCOP_ERROR_CANNOT_REGISTER_WITH_ICE;
+
+  /* Find DCOP server and open connection ********************************/
+
+  dcopServer = getenv("DCOPSERVER");
+
+  if (NULL == dcopServer) {
+
+    if (-1 == chdir(homeDir))
+      return DCOP_ERROR_SERVER_NOT_FOUND;
+
+    fd = open(".DCOPserver", O_RDONLY);
+
+    if (-1 == fd)
+      return DCOP_ERROR_SERVER_NOT_FOUND;
+
+    dcopServer = (char *)malloc(1024);
+
+    bytesRead = read(fd, dcopServer, 1024);
+
+    if (-1 == bytesRead)
+      return DCOP_ERROR_SERVER_NOT_FOUND;
+
+    newline = strchr(dcopServer, '\n');
+
+    if (NULL == newline)
+      return DCOP_ERROR_SERVER_NOT_FOUND;
+
+    *newline = '\0';
+
+    iceConn =
+      IceOpenConnection(
+        dcopServer,
+        context,
+        False,
+        majorOpcode,
+        sizeof(errBuf),
+        errBuf
+      );
+
+    free(dcopServer);
+
+  } else {
+
+    iceConn =
+      IceOpenConnection(
+        dcopServer,
+        context,
+        False,
+        majorOpcode,
+        sizeof(errBuf),
+        errBuf
+      );
+  }
+
+  if (iceConn == 0L)
+    return DCOP_ERROR_CANNOT_CONTACT_SERVER;
+
+  IceSetShutdownNegotiation(iceConn, False);
+
+  /* Setup protocol ******************************************************/
+
+  setupstat =
+    IceProtocolSetup(
+      iceConn,
+      majorOpcode,
+      somethingToPassToProtoSetup,
+      True, /* must authenticate */
+      &(majorVersion), &(minorVersion), &(vendor), &(release),
+      1024, errBuf
+    );
+
+  if (
+    setupstat == IceProtocolSetupFailure ||
+    setupstat == IceProtocolSetupIOError ||
+    setupstat == IceProtocolAlreadyActive)
+    return DCOP_ERROR_CANNOT_REGISTER_WITH_ICE;
+
+  if (IceConnectionStatus(iceConn) != IceConnectAccepted)
+    return DCOP_ERROR_SERVER_REFUSED_CONNECTION;
+
+  /* Send data to DCOP ***************************************************/
+
+  /*
+   * First let ICE initialize the ICE Message Header and give us a pointer
+   * to it (ICE manages that buffer internally)
+   */
+  IceGetHeader(
+    iceConn,
+    majorOpcode,
+    2 /* DCOPSend */,
+    sizeof(struct DCOPMsg),
+    struct DCOPMsg,
+    pMsgPtr
+  );
+
+  /* marshall the arguments for the DCOP message header (callerApp, destApp,
+   * destObj, destFunc.
+   * The last argument is actually part of the data part of the call, but
+   * we add it to the header. It's the size of the marshalled argument data.
+   * In Qt it would look like
+   * QDataStream str( ... )
+   * str << callerApp << destApp << destObj << destFun << argumentQByteArrayDataStuff;
+   * (where as str is the complete data stream sent do the dcopserver, excluding
+   *  the ICE header)
+   * As the QByteArray is marshalled as [size][data] and as we (below) send the
+   * data in two chunks, first the dcop msg header and the the data, we just
+   * put the [size] field as last field into the dcop msg header ;-)
+   */
+
+  header = (char *)malloc(1024);
+  pos = header;
+
+  pos = dcop_write_string(pos, "kmapnotify");
+  pos = dcop_write_string(pos, receiving_app);
+  pos = dcop_write_string(pos, object);
+  pos = dcop_write_string(pos, function);
+  pos = dcop_write_int(pos, dataLength);
+
+  headerLength = pos - header;
+
+  pMsgPtr->time = 0;
+  /* the length field tells the dcopserver how much bytes the dcop message
+   * takes up. We add that size to the already by IceGetHeader initialized
+   * length value, as it seems that under some circumstances (depending on the
+   * DCOPMsg structure size) the length field is aligned/padded. 
+   */
+  pMsgPtr->length += headerLength + dataLength;
+
+  /* first let's send the dcop message header 
+   * IceSendData automatically takes care of first sending the Ice Message
+   * Header (outbufptr > outbuf -> flush the connection buffer)
+   */
+  IceSendData(iceConn, headerLength, header);
+  /* and now the function argument data */
+  IceSendData(iceConn, dataLength, data);
+
+  /* send it all ;-) */
+  IceFlush( iceConn );
+
+  free(header);
+
+  if (IceConnectionStatus(iceConn) != IceConnectAccepted)
+    return DCOP_ERROR_SERVER_REFUSED_DATA;
+
+  IceCloseConnection( iceConn );
+
+  /* Done ***************************************************************/
+
+  return DCOP_OK;
+}
+
+/* vim: set ts=2:sw=2:tw=78: */
---
+++ dcop/dcopc.h
@@ -0,0 +1,31 @@
+/*
+ * C interface to DCOP
+ *
+ * (C) 2000 Rik Hemsley <rik@kde.org>
+ * (C) 2000 Simon Hausmann <hausmann@kde.org>
+ */
+
+#ifndef __dcopc_h__
+#define __dcopc_h__
+
+#define DCOP_OK 0
+#define DCOP_ERROR_SERVER_NOT_FOUND 1
+#define DCOP_ERROR_CANNOT_REGISTER_WITH_ICE 2
+#define DCOP_ERROR_CANNOT_CONTACT_SERVER 3
+#define DCOP_ERROR_SERVER_REFUSED_CONNECTION 4
+#define DCOP_ERROR_SERVER_REFUSED_DATA 5
+
+char * dcop_write_int(char * buf, int);
+
+char * dcop_write_string(char * buf, const char *);
+
+  int
+dcop_send_signal(
+  const char * receiving_app,
+  const char * object,
+  const char * function,
+  char * data,
+  int data_length
+);
+
+#endif // __dcopc_h__
