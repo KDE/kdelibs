f8a514732ccd8b1242ec1fb8aeb1f91ad3ff0545
Werner Trobin <trobin@kde.org>
2000-06-04 07:24:20 +0000

    const QString &foo="bar" -> const QString &foo=QString::fromLocal8Bit("bar") This fixes a warning for QT_NO_CAST_ASCII apps
    svn path=/trunk/kdelibs/; revision=51949

--- kdeui/ktmainwindow.h
+++ kdeui/ktmainwindow.h
@@ -199,7 +199,7 @@ public:
      * determined by the XML UI framework).
      */
     void setEnableToolBar( KToolBar::BarStatus stat = KToolBar::Toggle,
-                           const QString& name = "mainToolBar" );
+                           const QString& name = QString::fromLocal8Bit("mainToolBar") );
 
     /**
      * Set the width of the view frame.
