c4e1c500489be8d383c1fbfc8fc794c5e3f68bd3
Christian Gebauer <gebauer@kde.org>
2000-06-15 21:09:36 +0000

    KStdAction::aboutApp : try to use the program name provided by KGlobal::instance()->aboutData()
    svn path=/trunk/kdelibs/; revision=53352

--- kdeui/kstdaction.cpp
+++ kdeui/kstdaction.cpp
@@ -22,6 +22,7 @@
 #include <kiconloader.h>
 
 #include <kaction.h>
+#include <kaboutdata.h>
 
 #include <kapp.h>
 #include <qwhatsthis.h>
@@ -846,9 +847,11 @@ KAction *KStdAction::reportBug(const QObject *recvr, const char *slot,
 KAction *KStdAction::aboutApp(const QObject *recvr, const char *slot,
 						      QObject *parent, const char *name )
 {
-    return new KAction(i18n("&About %1...").arg(kapp->name()),
-        QIconSet(kapp->miniIcon()), 0, recvr,
-        slot, parent, name ? name : stdName(AboutApp));
+    const KAboutData *aboutData = KGlobal::instance()->aboutData();
+    QString appName = (aboutData)? aboutData->programName() : kapp->name();
+    return new KAction(i18n("&About %1...").arg(appName),
+                       QIconSet(kapp->miniIcon()), 0, recvr,
+                       slot, parent, name ? name : stdName(AboutApp));
 }
 
 KAction *KStdAction::aboutKDE(const QObject *recvr, const char *slot,
