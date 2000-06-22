dc0e4d8374f1535cc89f66115ae35ac70968b4e9
Bill Soudan <wes0472@rit.edu>
2000-06-22 01:50:33 +0000

    added setLaunchEnv("KDE_INITIAL_DESKTOP", KWin::currentDesktop()) call to startServiceInternal
    svn path=/trunk/kdelibs/; revision=53947

--- kdecore/kapp.cpp
+++ kdecore/kapp.cpp
@@ -1360,6 +1360,16 @@ startServiceInternal( const QCString &function,
    QCString replyType;
    QByteArray replyData;
    QCString _launcher = KApplication::launcher();
+   if (int desktop=KWin::currentDesktop()) {
+      QByteArray params2;
+      QDataStream stream2(params2, IO_WriteOnly);
+      QCString desktopStr;
+      desktopStr.setNum( desktop );
+      stream2 << QCString("KDE_INITIAL_DESKTOP") 
+              << desktopStr;
+      dcopClient->call(_launcher, _launcher, 
+         "setLaunchEnv(QCString,QCString)", params2, replyType, replyData);
+   }
    if (!dcopClient->call(_launcher, _launcher,
 	function, params, replyType, replyData))
    {
