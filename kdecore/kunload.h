2edf896ff6e771a47158a15ae092290129884f72
Michael Matz <matz@kde.org>
2000-06-25 23:44:22 +0000

    metaunloading
    svn path=/trunk/kdelibs/; revision=54439

---
+++ kdecore/kunload.h
@@ -0,0 +1,27 @@
+#include <qglobal.h>
+#include <qobjectdict.h>
+
+#define _UNLOAD(p) \
+static void unload_##p (const char **list) \
+{ \
+    if (!objectDict) return; \
+    qDebug("removing meta classes for %s", #p); \
+    const char **n = list; \
+    for (; *n; n++) { \
+        qDebug("  removing metaclass %s", *n); \
+	/* Because objectDict was set to AutoDelete the following does the \
+           right thing. */ \
+        while (objectDict->remove(*n)) ; \
+    } \
+    qDebug("removing done"); \
+} \
+ \
+class _KUnloadMetaClass_Helper##p { \
+public: \
+    _KUnloadMetaClass_Helper##p () {} \
+    ~_KUnloadMetaClass_Helper##p () { \
+        unload_##p (_metalist_##p); \
+    } \
+}; \
+ \
+static _KUnloadMetaClass_Helper##p _helper_object_##p;
