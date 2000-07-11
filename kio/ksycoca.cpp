f3e8b1fd7b78afa63415680795beb90c540d8709
Stephan Kulow <coolo@kde.org>
2000-07-11 18:31:48 +0000

    had to cast the first argument to munmap to char* to make Solaris happy. But this should work on void* taking plattforms too
    svn path=/trunk/kdelibs/; revision=56154

--- kio/ksycoca.cpp
+++ kio/ksycoca.cpp
@@ -160,7 +160,9 @@ void KSycoca::closeDatabase()
    {
       QBuffer *buf = (QBuffer *) device;
       buf->buffer().resetRawData(m_sycoca_mmap, m_sycoca_size);
-      munmap((void *) m_sycoca_mmap, m_sycoca_size);
+      // Solaris has munmap(char*, size_t) and everything else should
+      // be happy with a char* for munmap(void*, size_t)
+      munmap((char*) m_sycoca_mmap, m_sycoca_size);
       m_sycoca_mmap = 0;
    }
 #endif
