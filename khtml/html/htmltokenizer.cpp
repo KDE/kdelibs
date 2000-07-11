3106032d3c6ead85c386ee05eb38d8680500f794
Lars Knoll <knoll@kde.org>
2000-07-11 23:15:43 +0000

    fixed apache directory listings (once again...)
    svn path=/trunk/kdelibs/; revision=56231

--- khtml/html/htmltokenizer.cpp
+++ khtml/html/htmltokenizer.cpp
@@ -902,7 +902,7 @@ void HTMLTokenizer::parseTag(DOMStringIt &src)
 		if( beginTag && tagID != ID_IMG && tagID != ID_INPUT ) {
 		    // Ignore Space/LF's after a start tag
 		    discard = AllDiscard;
-		} else {
+		} else if (!beginTag) {
 		    // Don't ignore CR/LF's after a close tag
 		    discard = NoneDiscard;
 		    tagID -= ID_CLOSE_TAG;
@@ -920,7 +920,7 @@ void HTMLTokenizer::parseTag(DOMStringIt &src)
 		    // we have to take care to close the pre block in
 		    // case we encounter an unallowed element....
 		    if(!DOM::checkChild(ID_PRE, tagID)) {
-			//kdDebug(0) << " not allowed in <pre>" << tagID << endl;
+			kdDebug(0) << " not allowed in <pre> " << (int)tagID << endl;
 			pre = false;
 		    }
 		}
