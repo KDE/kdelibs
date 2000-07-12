ea22463bc6256e8f9b5381768ca4e3c10e9431e8
Carsten Pfeiffer <pfeiffer@kde.org>
2000-07-12 02:27:59 +0000

    auto-hide cursor stuff
    svn path=/trunk/kdelibs/; revision=56259

--- kdeui/kcursor.cpp
+++ kdeui/kcursor.cpp
@@ -19,9 +19,13 @@
 
 #include <qbitmap.h>
 #include <qcursor.h>
+#include <qevent.h>
+#include <qtimer.h>
+#include <qwidget.h>
 
 #include <kglobal.h>
 #include <kconfig.h>
+
 
 KCursor::KCursor()
 {
@@ -52,8 +56,8 @@ QCursor KCursor::handCursor()
 				0xf8, 0xff, 0x07, 0xf8, 0xff, 0x07, 0xf8, 0xff, 0x07, 0xf8, 0xff,
 				0x07, 0xf8, 0xff, 0x07, 0xf0, 0xff, 0x07, 0xf0, 0xff, 0x07, 0xe0,
 				0xff, 0x03, 0xc0, 0xff, 0x03, 0xc0, 0xff, 0x01, 0xc0, 0xff, 0x01};
-			QBitmap hand_bitmap(22, 22, HAND_BITS, true); 
-			QBitmap hand_mask(22, 22, HAND_MASK_BITS, true); 
+			QBitmap hand_bitmap(22, 22, HAND_BITS, true);
+			QBitmap hand_mask(22, 22, HAND_MASK_BITS, true);
 			hand_cursor = new QCursor(hand_bitmap, hand_mask, 7, 0);
 		}
 		else
@@ -133,3 +137,170 @@ QCursor KCursor::blankCursor()
 	return Qt::blankCursor;
 }
 
+// auto-hide cursor stuff
+
+void KCursor::setAutoHideCursor( QWidget *w, bool enable )
+{
+    if ( !w )
+	return;
+
+    KConfig *kc = KGlobal::config();
+    KConfigGroupSaver ks( kc, QString::fromLatin1("KDE") );
+    if ( !kc->readBoolEntry( QString::fromLatin1("Autohiding cursor enabled"),
+                            true ) )
+        return;
+
+    // we need an object for a slot and for the eventfilter
+    static KCursorPrivate hideHelper;
+
+    // nice Qt "feature": installing an eventfilter twice, gives us...
+    // ... two eventfilters => two events arriving here
+    // so we better remove an existing eventFilter
+    // Arnt said it will be fixed, in 2.2.
+    w->removeEventFilter( &hideHelper );
+
+    if ( enable ) {
+	hideHelper.start();
+	w->setMouseTracking( true );
+
+        // nice Qt "feature": installing an eventfilter twice, gives us...
+        // ... two eventfilters => two events arriving here
+        // so we better remove an existing eventFilter before installing one
+        // Arnt said it will be fixed, in 2.2.
+        w->removeEventFilter( &hideHelper );
+	w->installEventFilter( &hideHelper );
+    }
+
+    else {
+	w->setMouseTracking( false );
+        w->removeEventFilter( &hideHelper );
+	hideHelper.stop();
+    }
+}
+
+// **************************************************************************
+// **************************************************************************
+
+QTimer * KCursorPrivate::s_autoHideTimer = 0L;
+int KCursorPrivate::s_hideCursorDelay = 5000; // 5s default value
+int KCursorPrivate::s_count = 0;
+bool KCursorPrivate::s_isCursorHidden = false;
+bool KCursorPrivate::s_isOwnCursor    = false;
+// grr, Bug in gcc 2.95-2 (segfault when calling QCursor::operator=(QCursor))
+QCursor * KCursorPrivate::s_oldCursor = 0L;
+
+void KCursorPrivate::start()
+{
+    if ( !s_autoHideTimer ) {
+	s_autoHideTimer = new QTimer;
+	QObject::connect( s_autoHideTimer, SIGNAL( timeout() ),
+			  this, SLOT( slotHideCursor() ));
+    }
+    s_count++;
+}
+
+void KCursorPrivate::stop()
+{
+    s_count--;
+    if ( s_count <= 0 ) {
+	delete s_autoHideTimer;
+	s_autoHideTimer = 0L;
+	s_count = 0;
+    }
+}
+
+void KCursorPrivate::hideCursor( QWidget *w )
+{
+    if ( !w )
+	return;
+
+    s_isCursorHidden = true;
+    // FIXME (when Qt 2.2 is out, QWidget::hasOwnCursor())
+    s_isOwnCursor = w->testWState( WState_OwnCursor );
+    if ( s_isOwnCursor ) {
+	delete s_oldCursor;
+	s_oldCursor = new QCursor( w->cursor() );
+    }
+
+    w->setCursor( KCursor::blankCursor() );
+}
+
+void KCursorPrivate::unhideCursor( QWidget *w )
+{
+    s_isCursorHidden = false;
+    if ( w ) {
+	if ( s_isOwnCursor ) {
+	    w->setCursor( *s_oldCursor );
+	    delete s_oldCursor;
+	    s_oldCursor = 0L;
+	}
+	else
+	    w->unsetCursor();
+    }
+}
+
+
+// what a mess :-/
+bool KCursorPrivate::eventFilter( QObject *o, QEvent *e )
+{
+    if ( !o->isWidgetType() ) // should never happen, actually
+	return false;
+    qDebug("** got event %i, hidden: %i", e->type(), s_isCursorHidden);
+    int t = e->type();
+    QWidget *w = static_cast<QWidget *>( o );
+
+    if ( t == QEvent::Leave || t == QEvent::FocusOut ) {
+	qDebug("**** focusOut: %i, ishidden: %i", t, s_isCursorHidden);
+	s_autoHideTimer->stop();
+	if ( s_isCursorHidden )
+	    unhideCursor( w );
+	
+	s_isCursorHidden = false;
+	return false;
+    }
+    
+    // don't process events not coming from the focus-widget
+    if ( w != qApp->focusWidget() )
+	 return false;
+
+    else if ( t == QEvent::Enter ) {
+	if ( s_isCursorHidden )
+	    unhideCursor( w );
+	s_isCursorHidden = false;
+	s_autoHideTimer->start( s_hideCursorDelay, true );
+    }
+	
+    else { // no enter/leave/focus events
+	if ( s_isCursorHidden ) {
+	    if ( t == QEvent::MouseButtonPress ||
+		 t ==QEvent::MouseButtonRelease ||
+		 t == QEvent::MouseButtonDblClick || t == QEvent::MouseMove ||
+		 t == QEvent::Show || t == QEvent::Hide )
+		unhideCursor( w );
+	}
+	else { // cursor not hidden yet
+	    if ( t == QEvent::KeyPress ) { //t == QEvent::KeyRelease ) {
+		if ( insideWidget( QCursor::pos(), w ))
+		    hideCursor( w );
+	    }
+	    else {
+		if ( insideWidget( QCursor::pos(), w ))
+		    s_autoHideTimer->start( s_hideCursorDelay, true );
+	    }
+	}
+    }
+    return false;
+}
+
+void KCursorPrivate::slotHideCursor()
+{
+    if ( !s_isCursorHidden )
+	hideCursor( kapp->focusWidget() );
+}
+
+bool KCursorPrivate::insideWidget( const QPoint &p, QWidget *w )
+{
+    return w->geometry().contains( w->mapFromGlobal( p ) );
+}
+
+#include "kcursor.moc"
--- kdeui/kcursor.h
+++ kdeui/kcursor.h
@@ -1,5 +1,6 @@
 /* This file is part of the KDE libraries
    Copyright (C) 1998 Kurt Granroth (granroth@kde.org)
+                 2000 Carsten Pfeiffer <pfeiffer@kde.org>
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
@@ -22,7 +23,47 @@
 #ifndef _KCURSOR_H
 #define _KCURSOR_H
 
+#include <qobject.h>
+#include <qpoint.h>
+#include <kapp.h>
+
 class QCursor;
+class QWidget;
+
+
+/**
+ * I don't want the eventFilter to be in KCursor, so we have another class
+ * for that stuff
+ * @internal
+ * @author Carsten Pfeiffer <pfeiffer@kde.org>
+*/
+class KCursorPrivate : public QObject
+{
+    Q_OBJECT
+
+public:
+    virtual bool eventFilter( QObject *o, QEvent *e );
+    void start();
+    void stop();
+
+    static void hideCursor( QWidget * );
+    static void unhideCursor( QWidget * );
+
+    static int s_hideCursorDelay;
+
+private slots:
+    void slotHideCursor();
+
+private:
+    bool insideWidget( const QPoint&, QWidget * );
+
+    static bool s_isCursorHidden;
+    static bool s_isOwnCursor;
+    static QCursor *s_oldCursor;
+    static QTimer *s_autoHideTimer;
+    static int s_count;
+};
+
 
 /**
  * A wrapper around @ref QCursor that allows for "themed" cursors.
@@ -34,88 +75,128 @@ class QCursor;
  * 	setCursor(KCursor::handCursor());
  * </PRE>
  *
- * @short A QCursor wrapper allowing "themed" cursors.
+ * @short A QCursor wrapper allowing "themed" cursors and auto-hiding cursors.
  * @author Kurt Granroth <granroth@kde.org>
  */
 class KCursor
 {
 public:
-	/**
-	 * Constructor.  
-	 *
-	 * Does not do anything so far.
-	 **/
-	KCursor();
+    /**
+     * Constructor.
+     *
+     * Does not do anything so far.
+     **/
+    KCursor();
 
-	/**
-	 * Retrieve the proper hand cursor according to
-	 * the current GUI style (static function).
-	 */
-	static QCursor handCursor();
+    /**
+     * Retrieve the proper hand cursor according to
+     * the current GUI style (static function).
+     */
+    static QCursor handCursor();
 
-	/**
-	 * Retrieve the proper arrow cursor according to
-	 * the current GUI style (static function).
-	 */
-	static QCursor arrowCursor();
+    /**
+     * Retrieve the proper arrow cursor according to
+     * the current GUI style (static function).
+     */
+    static QCursor arrowCursor();
 
-	/**
-	 * Retrieve the proper up arrow cursor according to
-	 * the current GUI style (static function).
-	 */
-	static QCursor upArrowCursor();
+    /**
+     * Retrieve the proper up arrow cursor according to
+     * the current GUI style (static function).
+     */
+    static QCursor upArrowCursor();
 
-	/**
-	 * Retrieve the proper cross-hair cursor according to
-	 * the current GUI style (static function).
-	 */
-	static QCursor crossCursor();
+    /**
+     * Retrieve the proper cross-hair cursor according to
+     * the current GUI style (static function).
+     */
+    static QCursor crossCursor();
 
-	/**
-	 * Retrieve the proper hourglass cursor according to
-	 * the current GUI style (static function).
-	 */
-	static QCursor waitCursor();
+    /**
+     * Retrieve the proper hourglass cursor according to
+     * the current GUI style (static function).
+     */
+    static QCursor waitCursor();
 
-	/**
-	 * Retrieve the proper text cursor according to
-	 * the current GUI style (static function).
-	 */
-	static QCursor ibeamCursor();
+    /**
+     * Retrieve the proper text cursor according to
+     * the current GUI style (static function).
+     */
+    static QCursor ibeamCursor();
 
-	/**
-	 * Retrieve the proper vertical resize cursor
-	 * according to the current GUI style (static function).
-	 */
-	static QCursor sizeVerCursor();
+    /**
+     * Retrieve the proper vertical resize cursor
+     * according to the current GUI style (static function).
+     */
+    static QCursor sizeVerCursor();
 
-	/**
-	 * Retrieve the proper horizontal resize cursor
-	 * according to the current GUI style (static function).
-	 */
-	static QCursor sizeHorCursor();
+    /**
+     * Retrieve the proper horizontal resize cursor
+     * according to the current GUI style (static function).
+     */
+    static QCursor sizeHorCursor();
 
-	/**
-	 * Retrieve the proper diagonal resize (/) cursor
-	 * according to the current GUI style (static function).
-	 */
-	static QCursor sizeBDiagCursor();
+    /**
+     * Retrieve the proper diagonal resize (/) cursor
+     * according to the current GUI style (static function).
+     */
+    static QCursor sizeBDiagCursor();
 
-	/**
-	 * Retrieve the proper diagonal resize (\) cursor
-	 * according to the current GUI style (static function).
-	 */
-	static QCursor sizeFDiagCursor();
+    /**
+     * Retrieve the proper diagonal resize (\) cursor
+     * according to the current GUI style (static function).
+     */
+    static QCursor sizeFDiagCursor();
 
-	/**
-	 * Retrieve the proper all-directions resize cursor
-	 * according to the current GUI style (static function).
-	 */
-	static QCursor sizeAllCursor();
+    /**
+     * Retrieve the proper all-directions resize cursor
+     * according to the current GUI style (static function).
+     */
+    static QCursor sizeAllCursor();
 
-	/**
-	 * Retrieve a blank or invisible cursor (static function).
-	 */
-	static QCursor blankCursor();
+    /**
+     * Retrieve a blank or invisible cursor (static function).
+     */
+    static QCursor blankCursor();
+
+    /**
+     * Sets auto-hiding the cursor for widget @p w. Enabling it will result in
+     * the cursor being hidden when
+     * @li a key-event happens
+     * @li there are no key-events for a configured time-frame (see
+     *     @ref setHideCursorDelay())
+     *
+     * The cursor will be shown again when the focus is lost or a mouse-event
+     * happens.
+     *
+     * Side effect: when enabling auto-hide, mouseTracking is enabled for the
+     * specified widget, because it's needed to get mouse-move-events. So
+     * don't disable mouseTracking for a widget while using auto-hide for it.
+     *
+     * When disabling auto-hide, mouseTracking will be disabled, so if you need
+     * mouseTracking after disabling auto-hide, you have to reenable
+     * mouseTracking.
+     */
+    static void setAutoHideCursor( QWidget *w, bool enable );
+
+    /**
+     * Sets the delay time in milliseconds for auto-hiding. When no keyboard
+     * events arrive for that time-frame, the cursor will be hidden.
+     *
+     * Default is 5000, i.e. 5 seconds.
+     */
+    static void setHideCursorDelay( int ms ) {
+	KCursorPrivate::s_hideCursorDelay = ms;
+    }
+
+    /**
+     * @returns the current auto-hide delay time.
+     *
+     * Default is 5000, i.e. 5 seconds.
+     */
+    static int hideCursorDelay() { return KCursorPrivate::s_hideCursorDelay; }
+
 };
+
+
 #endif // _KCURSOR_H
