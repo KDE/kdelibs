279cbde493658f3f849d80c2521e3616d7ec914f
Kalle Dalheimer <kalle@kde.org>
2000-07-17 18:51:57 +0000

    fixed typo
    svn path=/trunk/kdelibs/; revision=57583

--- dcop/client/dcop.cpp
+++ dcop/client/dcop.cpp
@@ -32,11 +32,11 @@ void queryApplications()
 {
     QCStringList apps = dcop->registeredApplications();
     for ( QCStringList::Iterator it = apps.begin(); it != apps.end(); ++it )
-	if ( (*it) != dcop->appId() && (*it).left(9) != "anonymous" )
-	    fprintf( stdout, "%s\n", (*it).data() );
+    if ( (*it) != dcop->appId() && (*it).left(9) != "anonymous" )
+        fprintf( stdout, "%s\n", (*it).data() );
 
     if ( !dcop->isAttached() )
-	qFatal( "server not accessible" );
+    qFatal( "server not accessible" );
 }
 
 void queryObjects( const char* app )
@@ -44,13 +44,13 @@ void queryObjects( const char* app )
     bool ok = false;
     QCStringList objs = dcop->remoteObjects( app, &ok );
     for ( QCStringList::Iterator it = objs.begin(); it != objs.end(); ++it ) {
-	if ( (*it) == "default" && ++it != objs.end() )
-	    fprintf( stdout, "%s (default)\n", (*it).data() );
-	else
-	    fprintf( stdout, "%s\n", (*it).data() );
+    if ( (*it) == "default" && ++it != objs.end() )
+        fprintf( stdout, "%s (default)\n", (*it).data() );
+    else
+        fprintf( stdout, "%s\n", (*it).data() );
     }
     if ( !ok )
-	qFatal( "application not accessible" );
+    qFatal( "application not accessible" );
 }
 
 void queryFunctions( const char* app, const char* obj )
@@ -58,142 +58,142 @@ void queryFunctions( const char* app, const char* obj )
     bool ok = false;
     QCStringList funcs = dcop->remoteFunctions( app, obj, &ok );
     for ( QCStringList::Iterator it = funcs.begin(); it != funcs.end(); ++it ) {
-	if ( (*it) == "QCStringList functions()" )
-	    continue;
-	fprintf( stdout, "%s\n", (*it).data() );
+    if ( (*it) == "QCStringList functions()" )
+        continue;
+    fprintf( stdout, "%s\n", (*it).data() );
     }
     if ( !ok )
-	 qFatal( "object not accessible" );
+     qFatal( "object not accessible" );
 }
 
 
-bool mkBool( const QString& s ) 
+bool mkBool( const QString& s )
 {
     if ( s.lower()  == "true" )
-	return TRUE;
+    return TRUE;
     if ( s.lower()  == "yes" )
-	return TRUE;
+    return TRUE;
     if ( s.lower()  == "on" )
-	return TRUE;
+    return TRUE;
     if ( s.toInt() != 0 )
-	return TRUE;
-    
+    return TRUE;
+
     return FALSE;
 }
 
 void callFunction( const char* app, const char* obj, const char* func, int argc, char** args )
 {
-    
+
     QString f = func; // Qt is better with unicode strings, so use one.
     int left = f.find( '(' );
     int right = f.find( ')' );
-    
+
     if ( right <  left )
-	qFatal( "parantheses do not match" );
+    qFatal( "parentheses do not match" );
 
     if ( left < 0 ) {
-	// try to get the interface from the server
-	bool ok = false;
-	QCStringList funcs = dcop->remoteFunctions( app, obj, &ok );
-	QCString realfunc;
-	if ( !ok )
-	    qFatal( "object not accessible" );
-	for ( QCStringList::Iterator it = funcs.begin(); it != funcs.end(); ++it ) {
-	    int l = (*it).find( '(' );
-	    int s = (*it).find( ' ');
-	    if ( s < 0 )
-		s = 0;
-	    else
-		s++;
-	    
-	    if ( l > 0 && (*it).mid( s, l - s ) == func ) {
-		realfunc = (*it).mid( s );
-		int a = (*it).contains(',');
-		if ( a + argc == 0 || a + 1 == argc )
-		    break;
-	    }
-	}
-	if ( realfunc.isEmpty() )
-	    qFatal("no such function");
-	f = realfunc;
-	left = f.find( '(' );
-	right = f.find( ')' );
+    // try to get the interface from the server
+    bool ok = false;
+    QCStringList funcs = dcop->remoteFunctions( app, obj, &ok );
+    QCString realfunc;
+    if ( !ok )
+        qFatal( "object not accessible" );
+    for ( QCStringList::Iterator it = funcs.begin(); it != funcs.end(); ++it ) {
+        int l = (*it).find( '(' );
+        int s = (*it).find( ' ');
+        if ( s < 0 )
+        s = 0;
+        else
+        s++;
+
+        if ( l > 0 && (*it).mid( s, l - s ) == func ) {
+        realfunc = (*it).mid( s );
+        int a = (*it).contains(',');
+        if ( a + argc == 0 || a + 1 == argc )
+            break;
+        }
+    }
+    if ( realfunc.isEmpty() )
+        qFatal("no such function");
+    f = realfunc;
+    left = f.find( '(' );
+    right = f.find( ')' );
     }
 
     QStringList types;
     if ( left >0 && left + 1 < right - 1) {
-	types = QStringList::split( ',', f.mid( left + 1, right - left - 1) );
-	for ( QStringList::Iterator it = types.begin(); it != types.end(); ++it )
-	    (*it).stripWhiteSpace();
+    types = QStringList::split( ',', f.mid( left + 1, right - left - 1) );
+    for ( QStringList::Iterator it = types.begin(); it != types.end(); ++it )
+        (*it).stripWhiteSpace();
     }
 
     if ( (int) types.count() != argc ) {
-	qFatal( "arguments do not match" );
+    qFatal( "arguments do not match" );
     }
-    
+
     QByteArray data, replyData;
     QCString replyType;
     QDataStream arg(data, IO_WriteOnly);
 
     int i = 0;
     for ( QStringList::Iterator it = types.begin(); it != types.end(); ++it ) {
-	QString type = *it;
-	QString s = args[i++];
-	if ( type == "int" )
-	    arg << s.toInt();
- 	else if ( type == "long" )
- 	    arg << s.toLong();
-	else if ( type == "float" )
-	    arg << s.toFloat();
-	else if ( type == "double" )
-	    arg << s.toDouble();
- 	else if ( type == "bool" )
- 	    arg << mkBool( s );
-	else if ( type == "QString" )
-	    arg << s;
-	else if ( type == "QCString" )
-	    arg << QCString( s.latin1() );
-	else
-	    qFatal( "cannot handle datatype '%s'", type.latin1() );
+    QString type = *it;
+    QString s = args[i++];
+    if ( type == "int" )
+        arg << s.toInt();
+    else if ( type == "long" )
+        arg << s.toLong();
+    else if ( type == "float" )
+        arg << s.toFloat();
+    else if ( type == "double" )
+        arg << s.toDouble();
+    else if ( type == "bool" )
+        arg << mkBool( s );
+    else if ( type == "QString" )
+        arg << s;
+    else if ( type == "QCString" )
+        arg << QCString( s.latin1() );
+    else
+        qFatal( "cannot handle datatype '%s'", type.latin1() );
     }
-    
+
     if ( !dcop->call( app, obj, f.latin1(),  data, replyType, replyData) ) {
-	qFatal( "call failed");
+    qFatal( "call failed");
     } else {
-	QDataStream reply(replyData, IO_ReadOnly);
-	if ( replyType == "int" ) {
-	    int i;
-	    reply >> i;
-	    fprintf( stdout, "%d\n", i );
-	} else if ( replyType == "long" ) {
-	    long l;
-	    reply >> l;
-	    fprintf( stdout, "%ld\n", l );
-	} else if ( replyType == "long" ) {
-	    long l;
-	    reply >> l;
-	    fprintf( stdout, "%ld\n", l );
-	} else if ( replyType == "float" ) {
-	    float f;
-	    reply >> f;
-	    fprintf( stdout, "%f\n", (double) f );
-	} else if ( replyType == "double" ) {
-	    double d;
-	    reply >> d;
-	    fprintf( stdout, "%f\n", d );
-	} else if (replyType == "bool") {
-	    bool b;
-	    reply >> b;
-	    fprintf( stdout, "%s\n", b ? "true" : "false" );
-	} else if (replyType == "QString") {
-	    QString r;
-	    reply >> r;
-	    fprintf( stdout, "%s\n", r.latin1() );
-	} else if (replyType == "QCString") {
-	    QCString r;
-	    reply >> r;
-	    fprintf( stdout, "%s\n", r.data() );
-	}
+    QDataStream reply(replyData, IO_ReadOnly);
+    if ( replyType == "int" ) {
+        int i;
+        reply >> i;
+        fprintf( stdout, "%d\n", i );
+    } else if ( replyType == "long" ) {
+        long l;
+        reply >> l;
+        fprintf( stdout, "%ld\n", l );
+    } else if ( replyType == "long" ) {
+        long l;
+        reply >> l;
+        fprintf( stdout, "%ld\n", l );
+    } else if ( replyType == "float" ) {
+        float f;
+        reply >> f;
+        fprintf( stdout, "%f\n", (double) f );
+    } else if ( replyType == "double" ) {
+        double d;
+        reply >> d;
+        fprintf( stdout, "%f\n", d );
+    } else if (replyType == "bool") {
+        bool b;
+        reply >> b;
+        fprintf( stdout, "%s\n", b ? "true" : "false" );
+    } else if (replyType == "QString") {
+        QString r;
+        reply >> r;
+        fprintf( stdout, "%s\n", r.latin1() );
+    } else if (replyType == "QCString") {
+        QCString r;
+        reply >> r;
+        fprintf( stdout, "%s\n", r.data() );
+    }
     }
 }
 
@@ -208,19 +208,19 @@ int main( int argc, char** argv )
     switch ( argc ) {
     case 0:
     case 1:
-	queryApplications();
-	break;
+    queryApplications();
+    break;
     case 2:
-	queryObjects( argv[1] );
-	break;
+    queryObjects( argv[1] );
+    break;
     case 3:
-	queryFunctions( argv[1], argv[2] );
-	break;
+    queryFunctions( argv[1], argv[2] );
+    break;
     case 4:
     default:
-	callFunction( argv[1], argv[2], argv[3], argc - 4, &argv[4] );
-	break;
-	
+    callFunction( argv[1], argv[2], argv[3], argc - 4, &argv[4] );
+    break;
+
     }
 
     return 0;
