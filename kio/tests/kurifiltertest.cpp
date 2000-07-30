512d4f6082af2c6f13cd98a91701e4edba842a9d
David Faure <faure@kde.org>
2000-07-30 14:37:13 +0000

    Improved test suite to test the uriType().... I thought I had a bug to fix so this would be handy, but it turns out there's no bug... Well :)
    svn path=/trunk/kdelibs/; revision=59291

--- kio/tests/kurifiltertest.cpp
+++ kio/tests/kurifiltertest.cpp
@@ -8,14 +8,40 @@
 
 #include "kurifilter.h"
 
-void filter( const char* u, QStringList list = QStringList() )
+void filter( const char* u, QStringList list = QStringList(), const char * expectedResult = 0 )
 {
     QString a = QString::fromLatin1( u );
-    kdDebug() << "***********************************\nFiltering: " << a << endl;
-    if (KURIFilter::self()->filterURI(a, list))
-        kdDebug() << "After filtering: " << a << "\n***********************************\n" << endl;
+    KURIFilterData * m_filterData = new KURIFilterData;
+    m_filterData->setData( a );
+    kdDebug() << "Filtering: " << a << endl;
+    if (KURIFilter::self()->filterURI(*m_filterData, list))
+    {
+        switch( m_filterData->uriType() )
+            {
+                case KURIFilterData::LOCAL_FILE:
+                case KURIFilterData::LOCAL_DIR:
+                case KURIFilterData::NET_PROTOCOL:
+                case KURIFilterData::HELP:
+                    kdDebug() << "Here, minicli would run KRun on " << m_filterData->uri().url() << endl;
+                    break;
+                case KURIFilterData::EXECUTABLE:
+                case KURIFilterData::SHELL:
+                    kdDebug() << "Executable. cmd=" << m_filterData->uri().url() << endl;
+                    break;
+                case KURIFilterData::ERROR:
+                    kdDebug() << "Error" << endl;
+                    break;
+                default:
+                    kdDebug() << "Not found" << endl;
+        }
+    }
     else
-        kdDebug() << "No filtering required\n***********************************\n" << endl;
+        kdDebug() << "No filtering required" << endl;
+    kdDebug() << "-----" << endl;
+    if ( expectedResult )
+        if ( m_filterData->uri().url() != QString::fromLatin1( expectedResult ) )
+            kdWarning() << " Got " << m_filterData->uri().url() << " expected " << expectedResult << endl;
+    delete m_filterData;
 }
 
 int main(int argc, char **argv) {
@@ -39,27 +65,29 @@ int main(int argc, char **argv) {
     filter( "\\\\THUNDER\\", "ShortURIFilter" );
     filter( "smb://", "ShortURIFilter" );
     filter( "smb://THUNDER\\WORKGROUP", "ShortURIFilter" );
-    filter( "smb:/THUNDER/WORKGROUP", "ShortURIFilter" );   
+    filter( "smb:/THUNDER/WORKGROUP", "ShortURIFilter" );
     filter( "smb:///", "ShortURIFilter" ); // use specific filter.
     filter( "smb:", "ShortURIFilter" ); // use specific filter.
     filter( "smb:/", "ShortURIFilter" ); // use specific filter.
-   
+
     // Executable tests
-    filter( "kppp", "ShortURIFilter" );
-   
+    filter( "kppp", "ShortURIFilter", "kppp" );
+    filter( "xemacs", QStringList(), "xemacs" );
+
     // IKWS test
     filter( "KDE" );
     filter( "GNOME" );
-   
+
     // ENVIRONMENT variable
-    filter( "$KDEDIR/kdelibs/kio" );
+    filter( "$KDEDIR/kdelibs/kio" ); // note: this dir doesn't exist...
+    filter( "$KDEDIR/include" );
     filter( "$HOME/.kde/share" );
-    filter( "$HOME/$KDEDIR/kdebase/kconotrol/ebrowsing" );
+    filter( "$HOME/$KDEDIR/kdebase/kcontrol/ebrowsing" );
     filter( "$1/$2/$3" );  // can be used as bogus or valid test
     filter( "$$$$" ); // worst case scenarios.
-    filter( "$QTDIR", "ShortURIFilter" ); //use specific filter. 
+    filter( "$QTDIR", "ShortURIFilter" ); //use specific filter.
     filter( "$KDEDIR", "ShortURIFilter" ); //use specific filter.
-   
+
     return 0;
 }
 
