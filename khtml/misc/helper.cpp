1bb48586c204fec06a411ab94bfa24a795f36ed3
Simon Hausmann <hausmann@kde.org>
2000-08-15 06:52:37 +0000

    - fixed memory leak. patch posted to kde-core-devel, approx. 20 hours ago :)
    +static KStaticDeleter<HTMLColors> hcsd;
    +
     void khtml::setNamedColor(QColor &color, const QString &name)
     {
         if( !htmlColors )
    -      htmlColors = new HTMLColors;
    +        htmlColors = hcsd.setObject( new HTMLColors );
    
    svn path=/trunk/kdelibs/; revision=61055

--- khtml/misc/helper.cpp
+++ khtml/misc/helper.cpp
@@ -27,6 +27,7 @@
 #include <xml/dom_stringimpl.h>
 #include <qlist.h>
 #include <khtmllayout.h>
+#include <kstaticdeleter.h>
 //#include <kdebug.h>
 using namespace DOM;
 using namespace khtml;
@@ -36,85 +37,87 @@ using namespace khtml;
     QMap<QString,QColor> map;
     HTMLColors()
     {
-	map["black"] = "#000000";
-	map["green"] = "#008000";
-	map["silver"] = "#c0c0c0";
-	map["lime"] = "#00ff00";
-	map["gray"] = "#808080";
-	map["olive"] = "#808000";
-	map["white"] = "#ffffff";
-	map["yellow"] = "#ffff00";
-	map["maroon"] = "#800000";
-	map["navy"] = "#000080";
-	map["red"] = "#ff0000";
-	map["blue"] = "#0000ff";
-	map["purple"] = "#800080";
-	map["teal"] = "#008080";
-	map["fuchsia"] = "#ff00ff";
-	map["aqua"] = "#00ffff";
+        map["black"] = "#000000";
+        map["green"] = "#008000";
+        map["silver"] = "#c0c0c0";
+        map["lime"] = "#00ff00";
+        map["gray"] = "#808080";
+        map["olive"] = "#808000";
+        map["white"] = "#ffffff";
+        map["yellow"] = "#ffff00";
+        map["maroon"] = "#800000";
+        map["navy"] = "#000080";
+        map["red"] = "#ff0000";
+        map["blue"] = "#0000ff";
+        map["purple"] = "#800080";
+        map["teal"] = "#008080";
+        map["fuchsia"] = "#ff00ff";
+        map["aqua"] = "#00ffff";
     };
 };
 
 static HTMLColors *htmlColors = 0L;
 
+static KStaticDeleter<HTMLColors> hcsd;
+
 void khtml::setNamedColor(QColor &color, const QString &name)
 {
     if( !htmlColors )
-      htmlColors = new HTMLColors;
+        htmlColors = hcsd.setObject( new HTMLColors );
 
     if(name == "transparent" || name == "") {
-	//kdDebug( 6080 ) << "found transparent color" << endl;
-	color = QColor(); // invalid color == transparent
-	//kdDebug( 6080 ) << "colorvalid" << color.isValid() << endl;
-	return;
+        //kdDebug( 6080 ) << "found transparent color" << endl;
+        color = QColor(); // invalid color == transparent
+        //kdDebug( 6080 ) << "colorvalid" << color.isValid() << endl;
+        return;
     }
 
     // also recognize "color=ffffff"
     if (name[0] != QChar('#') && name.length() == 6)
     {
-	bool ok;
+        bool ok;
         name.toInt(&ok, 16);
-	if(ok)
-	{
-	    QString col("#");
-	    col += name;
-	    color.setNamedColor(col);
-	    return;
-	}
+        if(ok)
+        {
+            QString col("#");
+            col += name;
+            color.setNamedColor(col);
+            return;
+        }
     }
 
     if ( name.length() > 4 && name[0] == 'r'
-	      && name[1] == 'g' && name[2] == 'b'
-	      && name[3] == '(' && name[name.length()-1] == ')')
+              && name[1] == 'g' && name[2] == 'b'
+              && name[3] == '(' && name[name.length()-1] == ')')
     {
-	// CSS like rgb(r, g, b) style
-	DOMString rgb = name.mid(4, name.length()-5);
-	QList<Length> *l = rgb.implementation()->toLengthList();
-	if(l->count() != 3)
-	{
+        // CSS like rgb(r, g, b) style
+        DOMString rgb = name.mid(4, name.length()-5);
+        QList<Length> *l = rgb.implementation()->toLengthList();
+        if(l->count() != 3)
+        {
             color.setRgb(0, 0, 0);
-	    delete l;
-	    return;
-	}
-	int r = l->at(0)->isUndefined() ? 0 : l->at(0)->width(255);
-	if(r < 0) r = 0;
-	if(r > 255) r = 255;
-	int g = l->at(1)->isUndefined() ? 0 : l->at(1)->width(255);
-	if(g < 0) g = 0;
-	if(g > 255) g = 255;
-	int b = l->at(2)->isUndefined() ? 0 : l->at(2)->width(255);
-	if(b < 0) b = 0;
-	if(b > 255) b = 255;
+            delete l;
+            return;
+        }
+        int r = l->at(0)->isUndefined() ? 0 : l->at(0)->width(255);
+        if(r < 0) r = 0;
+        if(r > 255) r = 255;
+        int g = l->at(1)->isUndefined() ? 0 : l->at(1)->width(255);
+        if(g < 0) g = 0;
+        if(g > 255) g = 255;
+        int b = l->at(2)->isUndefined() ? 0 : l->at(2)->width(255);
+        if(b < 0) b = 0;
+        if(b > 255) b = 255;
 
-	color.setRgb(r, g, b);
-	delete l;
+        color.setRgb(r, g, b);
+        delete l;
     }
     else
     {
-    	const QColor& tc = htmlColors->map[name];
-	if (tc.isValid())
-    	    color = tc;	
-	else
-	    color.setNamedColor(name);
+        const QColor& tc = htmlColors->map[name];
+        if (tc.isValid())
+            color = tc;
+        else
+            color.setNamedColor(name);
     }
 }
