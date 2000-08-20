577a05edc6dbc70009b3db50fd7b114627f83a4d
Daniel M. Duley <daniel.duley@verizon.net>
2000-08-20 20:34:59 +0000

    Use Next-like radio buttons, someone changed the selected one from Light to ButtonText :P
    svn path=/trunk/kdelibs/; revision=61566

--- kstyles/stepplugin/kstep.cpp
+++ kstyles/stepplugin/kstep.cpp
@@ -423,13 +423,11 @@ void KStepStyle::drawExclusiveIndicator(QPainter *p, int x, int y, int w,
     if(on || down){
         if(down)
             p->setPen(nextGrp.mid());
-        else
-            p->setPen(nextGrp.buttonText());
         a.setPoints(QCOORDARRLEN(fill_lines), fill_lines);
         a.translate(x,y);
         p->drawLineSegments(a);
         p->fillRect(4, 4, 8, 8, (down)? g.brush(QColorGroup::Mid) :
-                    nextGrp.brush(QColorGroup::ButtonText));
+                    nextGrp.brush(QColorGroup::Light));
     }
     p->setPen(oldPen);
 }
