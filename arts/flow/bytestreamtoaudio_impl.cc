a8984257179d20000dece0e19c43ac3208cd3ce3
Stefan Westerfeld <stefan@space.twc.de>
2000-09-06 12:39:15 +0000

    Resampling patch (reviewed by Schimmi), part I: fixes ByteStreamToAudio to do appropriate conversions and resampling when getting mono/stereo, 8/16 bits, arbitary sampling rate streams.
    svn path=/trunk/kdelibs/; revision=62911

--- arts/flow/bytestreamtoaudio_impl.cc
+++ arts/flow/bytestreamtoaudio_impl.cc
@@ -22,103 +22,93 @@
 
 #include "artsflow.h"
 #include "stdsynthmodule.h"
+#include "resample.h"
 #include <iostream>
 
 using namespace std;
 using namespace Arts;
 
-#define compose_16le(first,second) \
-	(((((second)+128)&0xff) << 8)+(first))
 
-#define conv_16le_float(x) \
-	((float)((x)-32768)/32768.0)
+class PacketRefiller : public Refiller {
+public:
+	queue< DataPacket<mcopbyte>* > inqueue;
+	int pos;
+
+	PacketRefiller() : pos(0)
+	{
+	}
+	void process(DataPacket<mcopbyte>* packet)
+	{
+		inqueue.push(packet);
+	}
+	unsigned long read(unsigned char *buffer, unsigned long bytes)
+	{
+		unsigned long done = 0;
+		while(!inqueue.empty())
+		{
+			long tocopy = bytes - done;
+			if(tocopy == 0) return bytes;			/* complete? */
+
+			DataPacket<mcopbyte> *packet = inqueue.front();
+			if(tocopy > (packet->size - pos))
+				tocopy = (packet->size - pos);
+
+			memcpy(&buffer[done],&packet->contents[pos],tocopy);
+			pos += tocopy;
+			done += tocopy;
+
+			if(pos == packet->size) {
+				packet->processed();
+				pos = 0;
+				inqueue.pop();
+			}
+		}
+		return done;
+	}
+};
 
 class ByteStreamToAudio_impl : public ByteStreamToAudio_skel,
                                public StdSynthModule
 {
-	int haveBytes, pos;
-	queue< DataPacket<mcopbyte>* > inqueue;
+	PacketRefiller refiller;
+	Resampler resampler;
 	long _samplingRate, _channels, _bits;
 	bool _running;
 public:
-	ByteStreamToAudio_impl() :haveBytes(0), pos(0),
+	ByteStreamToAudio_impl() :resampler(&refiller),
 			_samplingRate(44100), _channels(2), _bits(16), _running(false)
 	{
 		//
 	}
 
 	long samplingRate() { return _samplingRate; }
-	void samplingRate(long newRate) { _samplingRate = newRate; }
+	void samplingRate(long newRate) {
+		_samplingRate = newRate;
+		resampler.setStep((float)_samplingRate / samplingRateFloat);
+	}
 
 	long channels() { return _channels; }
-	void channels(long newChannels) { _channels = newChannels; }
+	void channels(long newChannels) {
+		_channels = newChannels;
+		resampler.setChannels(_channels);
+	}
 
 	long bits() { return _bits; }
-	void bits(long newBits) { _bits = newBits; }
+	void bits(long newBits) {
+		_bits = newBits;
+		resampler.setBits(_bits);
+	}
 
 	bool running() { return _running; }
 
 	void process_indata(DataPacket<mcopbyte> *packet)
 	{
-		haveBytes += packet->size;
-		inqueue.push(packet);
+		refiller.process(packet);
 	}
 
-	inline mcopbyte getByte()
-	{
-		mcopbyte result;
-
-		assert(haveBytes);
-		DataPacket<mcopbyte> *packet = inqueue.front();
-		result = packet->contents[pos++];
-
-		if(pos == packet->size) {
-			packet->processed();
-			pos = 0;
-			inqueue.pop();
-		}
-
-		haveBytes--;
-		return result;
-	}
-
-	inline float mkSample()
-	{
-		mcopbyte first = getByte();
-		mcopbyte second = getByte();
-		return conv_16le_float(compose_16le(first,second));
-	}
 	void calculateBlock(unsigned long samples)
 	{
-		/* convert samples from buffer, as long as enough buffer space */
-		unsigned long doSamples = haveBytes/4,i;
-		if(samples < doSamples) doSamples = samples;
-
-		for(i=0;i<doSamples;i++)
-		{
-			left[i] = mkSample();
-			right[i] = mkSample();
-		}
-		
-		if(i == samples) /* did we have enough input available? */
-		{
-			_running = true;
-		}
-		else
-		{
-			if(_running)
-			{
-				cout << "ByteStreamToAudio: input underrun" << endl;
-				_running = false;
-			}
-
-			/* fill the rest with zero samples */
-			while(i != samples)
-			{
-				left[i] = right[i] = 0.0;
-				i++;
-			}
-		}
+		resampler.run(left,right,samples);
 	}
 };
 
---
+++ arts/flow/resample.h
@@ -0,0 +1,66 @@
+    /*
+
+    Copyright (C) 2000 Stefan Westerfeld
+                       stefan@space.twc.de
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+  
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+   
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+    Boston, MA 02111-1307, USA.
+
+    */
+
+
+#ifndef ARTS_FLOW_REFILL_H
+#define ARTS_FLOW_REFILL_H
+
+namespace Arts {
+
+class Refiller {
+public:
+	virtual unsigned long read(unsigned char *buffer, unsigned long len) = 0;
+	virtual ~Refiller();
+};
+
+class Resampler {
+protected:
+	static const unsigned int bufferSize = 256;		//  64 samples in buffer
+	static const unsigned int bufferWrap = 4;		// + 1 sample "wrap around"
+
+	int bufferSamples;
+	int sampleSize;
+	int dropBytes;
+
+	Refiller *refiller;
+	double pos, step;
+	int channels,bits;
+
+	unsigned char buffer[bufferSize+bufferWrap];
+	float fbuffer[bufferSize+bufferWrap];
+	long block, haveBlock;
+
+	class ResamplerPrivate *d;
+
+	void updateSampleSize();
+	void ensureRefill();
+public:
+	Resampler(Refiller *refiller);
+	void setStep(double step);
+	void setChannels(int channels);
+	void setBits(int bits);
+	void run(float *left, float *right, unsigned long samples);
+};
+
+};
+
+#endif /* ARTS_FLOW_REFILL_H */
