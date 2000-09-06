38b9dd6756d2129c754f0ede61a5880e7ed42317
Dirk Mueller <mueller@kde.org>
2000-09-06 21:44:39 +0000

    I just love commits that don't compile
    svn path=/trunk/kdelibs/; revision=62967

--- arts/soundserver/cpuusage.h
+++ arts/soundserver/cpuusage.h
@@ -1,23 +1,29 @@
+
+#ifndef _arts_cpuusage_h
+#define _arts_cpuusage_h
+
 #include "iomanager.h"
 
 namespace Arts {
 class CPUUsage
 {
 private:
-	class CPUUsagePrivate *d;
+        class CPUUsagePrivate *d;
 
 public:
-	CPUUsage();
-	~CPUUsage();
+        CPUUsage();
+        ~CPUUsage();
 
-	/**
-	 * get the current CPU usage
-	 */
-	float usage();
+        /**
+         * get the current CPU usage
+         */
+        float usage();
 
-	/**
-	 * check the current CPU usage
-	 */
-	void check();
+        /**
+         * check the current CPU usage
+         */
+        void check();
 };
 };
+
+#endif
