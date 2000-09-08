0284e512d21cc3c0fa0c75f9b8f29d3bf56c64e4
Stefan Westerfeld <stefan@space.twc.de>
2000-09-08 21:02:19 +0000

    Applied artswrapper changes (doublechecked with Olaf Kirch).
    The main modification is that all that recursive checking if artsd, and the
    parent directory of artsd and its parent directory is owned by root is gone
    now. He said: well, *IF* it these permissions are messed up, then a hacker
    would rather replace kdm or do other nasty things than causing a DOS by
    replacing artsd, so forget the checking. As it also simplifies quite some
    code, also in kcmarts, I threw it out.
    
    Also, artswrapper installs now suid root per default, which makes the
    
     kcontrol -> Sound -> Soundserver -> Run soundserver with realtime priority
    
    checkbox work without additional tweaking.
    
    svn path=/trunk/kdelibs/; revision=63198

--- arts/soundserver/artswrapper.c
+++ arts/soundserver/artswrapper.c
@@ -5,8 +5,6 @@
 #include <string.h>
 #include <malloc.h>
 
-#undef ARTSWRAPPER_DEBUG
-
 /*
  * adjust_priority
  *
@@ -81,102 +79,16 @@ void adjust_priority()
 }
 #endif
 
-/*
- * check_security
- *
- * checks that the path (file or directory) is owned by root, which makes
- * it save to assume that nobody can temper with the files in that directory,
- * and so we can execute the program with realtime priority
- *
- * result = 0  => not secure
- * result = 1  => secure (well - reasonable secure - if root installs
- *   directories as world writeable for instance, still everybody can
- *   do everything)
- */
-
-int check_security(const char *path)
-{
-	struct stat stat_buffer;
-
-#ifdef ARTSWRAPPER_DEBUG
-	printf("Checking security for %s\n",path);
-#endif
-	/*
-	 * No, we can't execute things in paths with zero length (for instance
-	 * current directory) savely. If path has length 1, it must be /, because
-	 * otherwise we will be executing stuff in a one
-	 */
-
-	if(strlen(path) == 0) return 0;
-	if(strlen(path) == 1 && path[0] != '/') return 0;
-
-	if(strlen(path) > 1)
-	{
-		char *basedir = strdup(path);
-		int end = 0;
-		int basedir_security;
-		int i;
-
-		for(i=0;i<strlen(basedir);i++)
-		{
-			if(basedir[i] == '/') end = i;
-		}
-
-		if(end == 0 && basedir[0] == '/') end = 1; /* root directory */
-		basedir[end] = 0;
-
-		basedir_security = check_security(basedir);
-		free(basedir);
-
-		if(basedir_security == 0) return 0;
-	}
-
-	stat(path, &stat_buffer); 
-
-	if(stat_buffer.st_uid != 0)
-	{
-		fprintf(stderr,"artswrapper: can't use suid, because security for\n");
-		fprintf(stderr,"    <%s> is critical, since it isn't owned by root\n",
-				path);
-		return 0;
-	}
-
-#ifdef ARTSWRAPPER_DEBUG
-	printf("%s passed security check\n",path);
-#endif
-	/*
-	 * more tests here, like "not world writeable" or "only writeable by root"?
-	 */
-	return 1;
-}
-
 int main(int argc, char **argv)
 {
-	const char *execute = EXECUTE;
-	int secure;
-
-	secure = check_security(execute);
-
 	if(argc == 2)
 	{
 		if(strcmp(argv[1],"check") == 0)
 		{
-			if(secure)
-			{
-				printf("okay\n");
-				exit(0);
-			}
-			else
-			{
-				printf("fail\n");
-				exit(1);
-			}
+			/* backward compatibility with old artswrapper */
+			printf("okay\n");
+			return 0;
 		}
-	}
-	if(!secure)
-	{
-		fprintf(stderr,"artswrapper: security check failed, leaving.\n");
-		exit(255);
 	}
 
 	adjust_priority();
@@ -185,6 +97,11 @@ int main(int argc, char **argv)
 	   (due to realtime priority stuff) */
 	if (geteuid() != getuid()) setreuid(-1, getuid());
 
-	execv(execute,argv);
-	return 0;
+	if(argc == 0)
+		return 1;
+
+	argv[0] = EXECUTE;
+	execv(EXECUTE,argv);
+	perror(EXECUTE);
+	return 1;
 }
