e67009daf839a600a7b0f3112be0ba94d468c2a2
David Faure <faure@kde.org>
2000-09-12 10:37:10 +0000

    Indentation and comments only.
    svn path=/trunk/kdelibs/; revision=63638

--- khtml/khtml_settings.h
+++ khtml/khtml_settings.h
@@ -29,50 +29,52 @@ class KConfig;
 
 /**
  * Settings for the HTML view.
- * We need this in konqueror since we need to call reparseConfiguration
- * on the static instance (even when we don't have an HTML view any longer).
  */
 class KHTMLSettings
 {
 public:
 
-  /**
-   * This enum specifies whether Java/JavaScript execution is allowed.
-   */
-  enum KJavaScriptAdvice {
-    KJavaScriptDunno=0,
-    KJavaScriptAccept,
-    KJavaScriptReject
-  };
+    /**
+     * This enum specifies whether Java/JavaScript execution is allowed.
+     */
+    enum KJavaScriptAdvice {
+	KJavaScriptDunno=0,
+	KJavaScriptAccept,
+	KJavaScriptReject
+    };
 
-  /**
-   * @internal Constructor
-   */
-  KHTMLSettings();
+    /**
+     * @internal Constructor
+     */
+    KHTMLSettings();
 
-  void init();
+    /** Called by constructor and reparseConfiguration */
+    void init();
 
-  /** Called by constructor and reparseConfiguration */
-  void init( KConfig * config, bool reset = true );
+    /** Read settings from @p config.
+     * @param reset if true, settings are always set; if false,
+     *  settings are only set if the config file has a corresponding key.
+     */
+    void init( KConfig * config, bool reset = true );
 
-  /** Destructor. Don't delete any instance by yourself. */
-  virtual ~KHTMLSettings() {};
+    /** Destructor. Don't delete any instance by yourself. */
+    virtual ~KHTMLSettings() {};
 
-  // Behaviour settings
-  bool changeCursor() { return m_bChangeCursor; }
-  bool underlineLink() { return m_underlineLink; }
+    // Behaviour settings
+    bool changeCursor() { return m_bChangeCursor; }
+    bool underlineLink() { return m_underlineLink; }
 
-  // Font settings
-  const QString& stdFontName() const { return m_strStdFontName; }
-  const QString& fixedFontName() const { return m_strFixedFontName; }
-  const QString& serifFontName() const { return m_strSerifFontName; }
+    // Font settings
+    const QString& stdFontName() const { return m_strStdFontName; }
+    const QString& fixedFontName() const { return m_strFixedFontName; }
+    const QString& serifFontName() const { return m_strSerifFontName; }
     const QString& sansSerifFontName() const { return m_strSansSerifFontName; }
     const QString& cursiveFontName() const { return m_strCursiveFontName; }
     const QString& fantasyFontName() const { return m_strFantasyFontName; }
 
     // these two can be set. Mainly for historical reasons (the method in KHTMLPart exists...)
-  void setStdFontName(const QString &n) { m_strStdFontName = n; }
-  void setFixedFontName(const QString &n) { m_strFixedFontName = n; }
+    void setStdFontName(const QString &n) { m_strStdFontName = n; }
+    void setFixedFontName(const QString &n) { m_strFixedFontName = n; }
 
     const QValueList<int> &fontSizes() const { return m_fontSizes; }
     void setFontSizes(const QValueList<int> &newFontSizes );
@@ -86,39 +88,39 @@ public:
 
     const QString &encoding() const { return m_encoding; }
 
-  // Color settings
-  const QColor& textColor() { return m_textColor; }
-  const QColor& linkColor() { return m_linkColor; }
-  const QColor& vLinkColor() { return m_vLinkColor; }
+    // Color settings
+    const QColor& textColor() { return m_textColor; }
+    const QColor& linkColor() { return m_linkColor; }
+    const QColor& vLinkColor() { return m_vLinkColor; }
 
-  // Autoload images
-  bool autoLoadImages() { return m_bAutoLoadImages; }
+    // Autoload images
+    bool autoLoadImages() { return m_bAutoLoadImages; }
 
-  // Java and JavaScript
-  bool isJavaEnabled( const QString& hostname = QString::null );
-  bool isJavaScriptEnabled( const QString& hostname = QString::null );
-  bool isCSSEnabled( const QString& hostname = QString::null );
+    // Java and JavaScript
+    bool isJavaEnabled( const QString& hostname = QString::null );
+    bool isJavaScriptEnabled( const QString& hostname = QString::null );
+    bool isCSSEnabled( const QString& hostname = QString::null );
 
-  // helpers for parsing domain-specific configuration, used in KControl module as well
-  static KJavaScriptAdvice strToAdvice(const QString& _str);
-  static void splitDomainAdvice(const QString& configStr, QString &domain,
-								KJavaScriptAdvice &javaAdvice, KJavaScriptAdvice& javaScriptAdvice);
-  static const char* adviceToStr(KJavaScriptAdvice _advice);
+    // helpers for parsing domain-specific configuration, used in KControl module as well
+    static KJavaScriptAdvice strToAdvice(const QString& _str);
+    static void splitDomainAdvice(const QString& configStr, QString &domain,
+				  KJavaScriptAdvice &javaAdvice, KJavaScriptAdvice& javaScriptAdvice);
+    static const char* adviceToStr(KJavaScriptAdvice _advice);
 
     QString settingsToCSS() const;
 
 private:
-  bool m_bChangeCursor;
-  bool m_underlineLink;
+    bool m_bChangeCursor;
+    bool m_underlineLink;
 
-  QString m_strStdFontName;
-  QString m_strFixedFontName;
-  QString m_strSerifFontName;
+    QString m_strStdFontName;
+    QString m_strFixedFontName;
+    QString m_strSerifFontName;
     QString m_strSansSerifFontName;
     QString m_strCursiveFontName;
     QString m_strFantasyFontName;
 
-	int m_fontSize;
+    int m_fontSize;
     QValueList<int>     m_fontSizes;
     int m_minFontSize;
 
@@ -126,18 +128,16 @@ private:
     bool enforceCharset;
     QString m_encoding;
 
-  QColor m_textColor;
-  QColor m_linkColor;
-  QColor m_vLinkColor;
+    QColor m_textColor;
+    QColor m_linkColor;
+    QColor m_vLinkColor;
 
-  bool m_bAutoLoadImages;
-  bool m_bEnableJava;
-  bool m_bEnableJavaScript;
+    bool m_bAutoLoadImages;
+    bool m_bEnableJava;
+    bool m_bEnableJavaScript;
     bool m_bEnableCSS;
     QMap<QString,KJavaScriptAdvice> javaDomainPolicy;
-  QMap<QString,KJavaScriptAdvice> javaScriptDomainPolicy;
-
-  //  static KonqHTMLSettings * s_HTMLSettings;
+    QMap<QString,KJavaScriptAdvice> javaScriptDomainPolicy;
 };
 
 #endif
