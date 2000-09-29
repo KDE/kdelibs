f756a46154bee9e4958958e88bdc8b9b27a2531c
Carsten Pfeiffer <pfeiffer@kde.org>
2000-09-29 20:40:01 +0000

    formatNumber() fix from bieker
    svn path=/trunk/kdelibs/; revision=65800

--- kfile/kpropsdlg.cpp
+++ kfile/kpropsdlg.cpp
@@ -686,7 +686,7 @@ KFilePropsPlugin::KFilePropsPlugin( KPropertiesDialog *_props )
 
     if (S_ISREG(item->mode())) // File (or symlink to file)
     {
-        m_sizeLabel->setText(QString::fromLatin1("%1 (%2)").arg(KIO::convertSize(item->size())).arg(item->size()));
+        m_sizeLabel->setText(QString::fromLatin1("%1 (%2)").arg(KIO::convertSize(item->size())).arg(KGlobal::locale()->formatNumber(item->size(), 0)));
         m_sizeDetermineButton = 0L;
         m_sizeStopButton = 0L;
     }
