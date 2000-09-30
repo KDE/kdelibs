7ed07997cee2034312a5b5782d33e3facac06724
Carsten Pfeiffer <pfeiffer@kde.org>
2000-09-30 11:42:15 +0000

    +#ifdef KDE_USE_FINAL +#ifdef KeyRelease +#undef KeyRelease +#endif +#endif
    svn path=/trunk/kdelibs/; revision=65850

--- kdeui/kcursor.cpp
+++ kdeui/kcursor.cpp
@@ -15,6 +15,13 @@
    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
    Boston, MA 02111-1307, USA.
 */
+
+#ifdef KDE_USE_FINAL
+#ifdef KeyRelease
+#undef KeyRelease
+#endif
+#endif
+
 #include <kcursor.h>
 
 #include <qbitmap.h>
