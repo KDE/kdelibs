1d530b057e0ec6fbc5752dbceda2f063694e60a3
Dirk Mueller <mueller@kde.org>
2000-10-02 01:34:56 +0000

    actually the REPEAT_NONE was wrong here
    svn path=/trunk/kdelibs/; revision=66116

--- khtml/css/cssparser.cpp
+++ khtml/css/cssparser.cpp
@@ -1370,7 +1370,6 @@ bool StyleBaseImpl::parseBackground(const QChar *curP, const QChar *endP, bool i
       case CSS_VAL_REPEAT:
       case CSS_VAL_REPEAT_X:
       case CSS_VAL_REPEAT_Y:
-      case CSS_VAL_REPEAT_NONE:
       case CSS_VAL_NO_REPEAT:
 	prop = CSS_PROP_BACKGROUND_REPEAT;
 	fnd[3] = true;
