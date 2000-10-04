9020a1467a9e31943dff6a25dbd7884505557b2a
Carsten Pfeiffer <pfeiffer@kde.org>
2000-10-04 10:33:27 +0000

    #include <kapp.h> #include <keditcl.h> #include <qpopupmenu.h>
    int main( int argc, char **argv )
    {
      KApplication app( argc, argv, "kedittest" );
      KEdit *edit = new KEdit( 0L );
      QPopupMenu *pop = new QPopupMenu( 0L );
      pop->insertItem( "Popupmenu item" );
      edit->installRBPopup( pop );
      edit->show();
      return app.exec();
    }
    
    svn path=/trunk/kdelibs/; revision=66351

---
+++ kdeui/tests/kedittest.cpp
@@ -0,0 +1,14 @@
+#include <kapp.h>
+#include <keditcl.h>
+#include <qpopupmenu.h>
+
+int main( int argc, char **argv )
+{
+  KApplication app( argc, argv, "kedittest" );
+  KEdit *edit = new KEdit( 0L );
+  QPopupMenu *pop = new QPopupMenu( 0L );
+  pop->insertItem( "Popupmenu item" );
+  edit->installRBPopup( pop );
+  edit->show();
+  return app.exec();
+}
