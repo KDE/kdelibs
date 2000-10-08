76b96dd233c9bf88dacdcca463429d0daf78bbb6
David Faure <faure@kde.org>
2000-10-08 21:49:12 +0000

    A little helper for testing - always useful
    svn path=/trunk/kdelibs/; revision=66833

---
+++ kio/tests/kmimemagictest.cpp
@@ -0,0 +1,25 @@
+#include <kmimemagic.h>
+#include <kinstance.h>
+#include <stdio.h>
+
+int main( int argc, char** argv )
+{
+  if (argc != 2)
+  {
+      printf("\n"
+             "Usage : ./kmimemagictest file\n");
+      return 1;
+  }
+  KInstance blah("kmimemagictest");
+
+  QString file = QString::fromLocal8Bit( argv[1] );
+
+  KMimeMagicResult * result = KMimeMagic::self()->findFileType( file );
+
+  if ( result->isValid() )
+      printf( "Found %s, encoding %s, accuracy %d\n", result->mimeType().latin1(), result->encoding().latin1(), result->accuracy() );
+  else
+      printf( "Invalid result\n");
+
+  return 0;
+}
