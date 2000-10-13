5bb28798cce66300108dde26ee5275070a5df687
Michael Matz <matz@kde.org>
2000-10-13 13:58:31 +0000

    Remove the last ';' from REGISTER_IMPLEMENTATION() macro.
         std::string interfaceName() { return impl ## ::_interfaceNameSkel(); } \
            Arts::Object_skel *createInstance() { return new impl ; }              \
    -} The_ ## impl ## _Factory;
    +} The_ ## impl ## _Factory
    +
     };
    
    svn path=/trunk/kdelibs/; revision=67474

--- arts/mcop/factory.h
+++ arts/mcop/factory.h
@@ -49,12 +49,18 @@ public:
 	virtual std::string interfaceName() = 0;
 };
 
+/*
+ * Dont's forget to place a semicolon after the use of this macro,
+ * like:
+ * REGISTER_IMPLEMENTATION(MyClass);
+ */
 #define REGISTER_IMPLEMENTATION(impl)                                      \
   class impl ## _Factory : virtual public Arts::Factory {                  \
   public:                                                                  \
     std::string interfaceName() { return impl ## ::_interfaceNameSkel(); } \
 	Arts::Object_skel *createInstance() { return new impl ; }              \
-} The_ ## impl ## _Factory;
+} The_ ## impl ## _Factory
+
 };
 
 #endif
