2ae9aefcb407b5faba150eeb250dd5a46be14698
David Faure <faure@kde.org>
2000-10-13 10:22:14 +0000

    A small script that shows tackat what he did by renaming the icons....
    svn path=/trunk/kdelibs/; revision=67456

---
+++ mimetypes/iconcheck.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Script that checks that the mimetypes point to an existing icon
+# Shows only errors, unless -verbose is used
+
+verbose=0
+grepopt="-q"
+if [ $# = 1 ]; then
+  if [ $1 = "-verbose" ]; then verbose=1; grepopt="-q"; fi
+fi
+for i in */*top ; do
+    icon=`grep Icon $i | sed -e s/Icon=//` 
+    if [ -z "$icon" ]; then
+       echo "$i : NO ICON"
+    else
+      if [ $verbose = 1 ]; then
+        echo $i
+      fi
+      if ! find ../pics | grep $grepopt "\-$icon\.png" ; then
+       echo $icon NOT FOUND 
+      fi 
+    fi 
+done  
