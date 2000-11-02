c7d9e26d898531b5119f6bf2c6c8a8a82cdd13e2
Dirk Mueller <mueller@kde.org>
2000-11-02 18:31:15 +0000

    SECURITY FIX: propagate javascript/java settings to child frames
    svn path=/trunk/kdelibs/; revision=69740

--- khtml/khtml_part.cpp
+++ khtml/khtml_part.cpp
@@ -126,7 +126,7 @@ typedef FrameList::Iterator FrameIt;
 class KHTMLPartPrivate
 {
 public:
-  KHTMLPartPrivate()
+  KHTMLPartPrivate(QObject* parent)
   {
     m_doc = 0L;
     m_decoder = 0L;
@@ -154,12 +154,26 @@ public:
     m_findDialog = 0;
     m_ssl_in_use = false;
     m_javaContext = 0;
+    m_cacheId = 0;
+    m_frameNameId = 1;
+
     m_bJScriptForce = false;
     m_bJScriptOverride = false;
     m_bJavaForce = false;
     m_bJavaOverride = false;
-    m_frameNameId = 1;
-    m_cacheId = 0;
+
+    // inherit security settings from parent
+    if(parent && parent->inherits("KHTMLPart"))
+    {
+        KHTMLPart* part = static_cast<KHTMLPart*>(parent);
+        if(part->d)
+        {
+            m_bJScriptForce = part->d->m_bJScriptForce;
+            m_bJScriptOverride = part->d->m_bJScriptOverride;
+            m_bJavaForce = part->d->m_bJavaForce;
+            m_bJavaOverride = part->d->m_bJavaOverride;
+        }
+    }
   }
   ~KHTMLPartPrivate()
   {
@@ -352,19 +366,20 @@ KHTMLPart::KHTMLPart( QWidget *parentWidget, const char *widgetname, QObject *pa
 : KParts::ReadOnlyPart( parent ? parent : parentWidget, name ? name : widgetname )
 {
     d = 0;
-  KHTMLFactory::registerPart( this );
-  setInstance( KHTMLFactory::instance() ); // doesn't work inside init() for derived classes
-                                           // Why?? :-} (Simon)
-  init( new KHTMLView( this, parentWidget, widgetname ), prof );
+    KHTMLFactory::registerPart( this );
+    setInstance( KHTMLFactory::instance() ); // doesn't work inside init() for derived classes
+    // Why?? :-} (Simon)
+    init( new KHTMLView( this, parentWidget, widgetname ), prof );
 }
 
 KHTMLPart::KHTMLPart( KHTMLView *view, QObject *parent, const char *name, GUIProfile prof )
 : KParts::ReadOnlyPart( parent, name )
 {
-  KHTMLFactory::registerPart( this );
-  setInstance( KHTMLFactory::instance() );
-  assert( view );
-  init( view, prof );
+    d = 0;
+    KHTMLFactory::registerPart( this );
+    setInstance( KHTMLFactory::instance() );
+    assert( view );
+    init( view, prof );
 }
 
 void KHTMLPart::init( KHTMLView *view, GUIProfile prof )
@@ -376,7 +391,7 @@ void KHTMLPart::init( KHTMLView *view, GUIProfile prof )
   else if ( prof == BrowserViewGUI )
     setXMLFile( "khtml_browser.rc" );
 
-  d = new KHTMLPartPrivate;
+  d = new KHTMLPartPrivate(parent());
   kdDebug() << "KHTMLPart::init this=" << this << " d=" << d << endl;
 
   d->m_view = view;
@@ -629,10 +644,15 @@ void KHTMLPart::enableJScript( bool enable )
 {
   d->m_bJScriptForce = enable;
   d->m_bJScriptOverride = true;
+  qDebug("KHTMLPart::enableJScript(%d)", enable);
+
 }
 
 bool KHTMLPart::jScriptEnabled() const
 {
+    qDebug("::jScriptEnabled: so: %d, sf: %d, se: %d",
+           d->m_bJScriptOverride, d->m_bJScriptForce, d->m_bJScriptEnabled);
+
   if ( d->m_bJScriptOverride )
       return d->m_bJScriptForce;
   return d->m_bJScriptEnabled;
