46604b7bbb9b92ceb645db3d4255378ee374f353
Antonio Larrosa Jimenez <larrosa@kde.org>
2000-11-03 18:39:50 +0000

    Added a test for KAlphaIcon, use it like this: Use kalphaicontest --help to see the options. Right now, you have to resize a bit teh window to issue a new paint event, so that you see again how much time it took to paint all the alpha blended icons. Use different methods and compare. Btw, using QPixmap doesn't seem to work even with setOptimization... I'll have to test more.
    svn path=/trunk/kdelibs/; revision=69835

---
+++ kdeui/tests/kalphaicontest.h
@@ -0,0 +1,24 @@
+#include <kdebug.h>
+#include <qwidget.h>
+#include <qimage.h>
+#include <qpixmap.h>
+#include <kimageeffect.h>
+#include <kalphaicon.h>
+
+class KAlphaTest : public QWidget
+{
+  public:
+    KAlphaTest();
+  protected:
+    void paintEvent(QPaintEvent *);
+  private:
+    QImage m_image;
+    QPixmap m_pixmap;
+    QImage m_bg;
+
+  public:
+    static bool m_useDblBuffer;
+    static bool m_usePixmap;
+    static bool m_correctOverlapping;
+};
+
