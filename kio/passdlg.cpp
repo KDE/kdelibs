a13dbc4333fbaae05ac166af4a6f9382a3f75eab
Dawit Alemayehu <adawit@kde.org>
2000-12-06 13:49:12 +0000

    - Re-designed password dialog influenced by kdesud and kmail password   dialogs. - This should be BC with the previous version. (DA)
    svn path=/trunk/kdelibs/; revision=73627

--- kio/passdlg.cpp
+++ kio/passdlg.cpp
@@ -16,114 +16,270 @@
    Boston, MA 02111-1307, USA.
 */
 
-#include "kio/passdlg.h"
-
-#include <string.h>
-
+#include <qlineedit.h>
 #include <qpushbutton.h>
+#include <qcheckbox.h>
 #include <qlabel.h>
 #include <qlayout.h>
 #include <qaccel.h>
 
-#include <kbuttonbox.h>
-#include <klocale.h>
 #include <kapp.h>
+#include <kconfig.h>
+#include <klocale.h>
+#include <kbuttonbox.h>
+#include <kstddirs.h>
+
+#include "passdlg.h"
 
 using namespace KIO;
 
-PassDlg::PassDlg( QWidget* parent, const char* name, bool modal,
-		          WFlags wflags, const QString& _head,
-		          const QString& _user, const QString& _pass )
-        : KDialog(parent, name, modal, wflags)
+struct PasswordDialog::PasswordDialogPrivate
 {
+    QLineEdit* userEdit;
+    QLineEdit* passEdit;
+    QLabel* prompt;
 
-   QVBoxLayout *layout = new QVBoxLayout( this, marginHint(), spacingHint() );
-   //
-   // Bei Bedarf einen kleinen Kommentar als Label einfuegen
-   // NB20000703: Hmm??? Something about pretty comments? Made the change
-   //             anyway, now the slave decides about the whole string.
-   QString msg;
-   if ( !_head.isEmpty() )
-   {
-        msg = "<qt><center><nobr>" + _head + "</nobr></center></qt>";
+    bool keep;
+    short unsigned int nRow;
+};
 
-   }
-   else
-   {
-        msg = i18n( "Enter your user name and password." );
-   }
-
-   QLabel *l = new QLabel(msg, this);
-   layout->addWidget( l );
-
-   QGridLayout *grid = new QGridLayout( 3, 5 );
-   layout->addLayout( grid );
-
-   l = new QLabel( i18n( "Username:" ), this );
-   l->setAlignment( Qt::AlignVCenter | Qt::AlignRight );
-   grid->addWidget( l, 0, 1 );
-   m_pUser = new QLineEdit( this );
-   grid->addWidget( m_pUser, 0, 3 );
-   l->setBuddy( m_pUser );
-
-   l = new QLabel( i18n( "Password:" ), this );
-   l->setAlignment( Qt::AlignVCenter | Qt::AlignRight );
-   grid->addWidget( l, 2, 1 );
-   m_pPass = new QLineEdit( this );
-   m_pPass->setEchoMode( QLineEdit::Password );
-   grid->addWidget( m_pPass, 2, 3 );
-   l->setBuddy( m_pPass );
-
-   if ( !_user.isEmpty() )
-     m_pUser->setText( _user );
-
-   if ( !_pass.isEmpty() )
-     m_pPass->setText( _pass );
-
-   layout->addSpacing( spacingHint() );
-
-   // Connect vom LineEdit herstellen und Accelerator
-   QAccel *ac = new QAccel(this);
-   ac->connectItem( ac->insertItem(Key_Escape), this, SLOT(reject()) );
-
-   connect( m_pPass, SIGNAL(returnPressed()), SLOT(accept()) );
-
-   // Die Buttons "OK" & "Cancel" erzeugen
-   KButtonBox *bbox = new KButtonBox(this);
-   layout->addWidget(bbox);
-
-   bbox->addStretch();
-   QPushButton *b1, *b2;
-   b1 = bbox->addButton(i18n("OK"));
-   b2 = bbox->addButton(i18n("Cancel"));
-   bbox->layout();
-       
-   // Buttons mit Funktionaliataet belegen
-   connect( b1, SIGNAL(clicked()), SLOT(accept()) );
-   connect( b2, SIGNAL(clicked()), SLOT(reject()) );
-
-   // Fenstertitel
-   setCaption( i18n("Authorization Request") );
-
-   // Focus
-   if ( _user.isEmpty() )
-     m_pUser->setFocus();
-   else
-     m_pPass->setFocus();
-
-   layout->addStretch( 10 );
-
-   resize( sizeHint() );
+PasswordDialog::PasswordDialog( QWidget* parent, const char* name,
+                                WFlags wflags, bool modal, bool enableKeep )
+               :KDialog( parent, name, modal, wflags )
+{
+    init( QString::null, QString::null, enableKeep );
 }
 
-void PassDlg::setEnableUserField( bool enable, bool gainsFocus )
+PasswordDialog::PasswordDialog( const QString& prompt, const QString& user,
+                                bool enableKeep, bool modal, QWidget* parent,
+                                const char* name, WFlags wflags )
+               :KDialog( parent, name, modal, wflags )
 {
-  m_pUser->setEnabled(enable);
-  if( enable && gainsFocus )
-    m_pUser->setFocus();
-
-  if( !enable && m_pUser->hasFocus() )
-    m_pPass->setFocus();
+    init ( prompt, user, enableKeep );
 }
+
+PasswordDialog::PasswordDialog( QWidget* parent, const char* name, bool modal,
+                                 WFlags wflags, const QString& head,
+                                 const QString& user, const QString& )
+               :KDialog( parent, name, modal, wflags )
+{
+    init( head, user, false );
+}
+
+PasswordDialog::~PasswordDialog()
+{
+    delete d;
+}
+
+void PasswordDialog::init( const QString& prompt, const QString& user,
+                           bool enableKeep  )
+{
+    d = new PasswordDialogPrivate;
+    d->nRow = 0;
+    d->keep = false;
+
+    KConfig* cfg = KGlobal::config();
+    KConfigGroupSaver saver( cfg, "Passwords" );
+
+    m_pLay = new QGridLayout( this, 12, 3, 8, 0);
+    m_pLay->addColSpacing(1, 10);
+
+    // Row 0: pixmap  prompt
+    QLabel* lbl;
+    QPixmap pix(locate("data", QString::fromLatin1("kdeui/pics/keys.png")));
+    if ( !pix.isNull() )
+    {
+        lbl = new QLabel( this );
+        lbl->setPixmap( pix );
+        lbl->setAlignment( Qt::AlignLeft|Qt::AlignVCenter );
+        lbl->setFixedSize( lbl->sizeHint() );
+        m_pLay->addWidget( lbl, 0, 0, Qt::AlignLeft );
+    }
+    d->prompt = new QLabel( this );
+    d->prompt->setAlignment( Qt::AlignLeft|Qt::AlignVCenter|Qt::WordBreak );
+    m_pLay->addWidget( d->prompt, 0, 2, Qt::AlignLeft );
+    if ( prompt.isEmpty() )
+        setPrompt( i18n( "You need to supply a username and a password" ) );
+    else
+        setPrompt( prompt );
+
+    // Row 1: Row Spacer
+    m_pLay->addRowSpacing( 1, 10 );
+
+    // Row 2-3: Reserved for an additional comment
+
+    // Row 4: Username field
+    lbl = new QLabel( i18n("&Username:"), this );
+    lbl->setAlignment( Qt::AlignVCenter | Qt::AlignLeft );
+    lbl->setFixedSize( lbl->sizeHint() );
+    m_pLay->addWidget( lbl, 4, 0 );
+    QHBoxLayout* hbl = new QHBoxLayout();
+    m_pLay->addLayout( hbl, 4, 2 );
+    d->userEdit = new QLineEdit( this );
+    lbl->setBuddy( d->userEdit );
+    QSize s = d->userEdit->sizeHint();
+    d->userEdit->setFixedHeight( s.height() );
+    d->userEdit->setMinimumWidth( s.width() );
+    lbl->setBuddy( d->userEdit );
+    hbl->addWidget( d->userEdit, 12 );
+
+    // Row 5: Row spacer
+    m_pLay->addRowSpacing( 5, 5 );
+
+    // Row 5: Password field
+    lbl = new QLabel( i18n("&Password:"), this );
+    lbl->setAlignment( Qt::AlignVCenter | Qt::AlignLeft );
+    lbl->setFixedSize( lbl->sizeHint() );
+    m_pLay->addWidget( lbl, 6, 0 );
+    hbl = new QHBoxLayout();
+    m_pLay->addLayout( hbl, 6, 2 );
+    d->passEdit = new QLineEdit( this );
+    if ( cfg->readEntry("EchoMode", "OneStar") == "NoEcho" )
+        d->passEdit->setEchoMode( QLineEdit::NoEcho );
+    else
+        d->passEdit->setEchoMode( QLineEdit::Password );
+    lbl->setBuddy( d->passEdit );
+    s = d->passEdit->sizeHint();
+    d->passEdit->setFixedHeight( s.height() );
+    d->passEdit->setMinimumWidth( s.width() );
+    lbl->setBuddy( d->passEdit );
+    hbl->addWidget( d->passEdit, 12 );
+
+    if ( enableKeep )
+    {
+        // Row 7: Add spacer
+        m_pLay->addRowSpacing( 7, 10 );
+        // Row 8: Keep Password
+        QCheckBox *cb = new QCheckBox( i18n("&Keep Password"), this );
+        cb->setFixedSize( cb->sizeHint() );
+        KConfigGroupSaver saver( cfg, "Passwords" );
+        d->keep = cfg->readBoolEntry("Keep", false );
+        cb->setChecked( d->keep );
+        connect(cb, SIGNAL(toggled( bool )), SLOT(slotKeep( bool )));
+        m_pLay->addWidget( cb, 8, 2 );
+    }
+
+    // Row 9: Add spacer
+    m_pLay->addRowSpacing( 9, 8 );
+
+    // Row 10: Add a separator
+    hbl = new QHBoxLayout();
+    m_pLay->addMultiCellLayout( hbl, 10, 10, 0, 2 );
+    lbl = new QLabel( this );
+    lbl->setFrameStyle( QFrame::HLine|QFrame::Sunken );
+    lbl->setAlignment( Qt::AlignVCenter | Qt::AlignCenter );
+    lbl->setFixedHeight( lbl->sizeHint().height() );
+    hbl->addWidget( lbl );
+
+    // Row 11: Add spacer
+    m_pLay->addRowSpacing( 11, 2 );
+
+    // Row 12: Add buttons
+    KButtonBox *bbox = new KButtonBox( this );
+    (void)bbox->addButton( i18n("&OK"), this, SLOT(accept()) );
+    (void)bbox->addButton( i18n("&Cancel"), this, SLOT(reject()) );
+    bbox->addStretch(4);
+    bbox->layout();
+    m_pLay->addWidget( bbox, 12, 2 );
+
+    // Configure necessary key-bindings and connect necessar slots and signals
+    QAccel *ac = new QAccel(this);
+    ac->connectItem( ac->insertItem(Qt::Key_Escape), this, SLOT(reject()) );
+    connect( d->userEdit, SIGNAL(returnPressed()), d->passEdit, SLOT(setFocus()) );
+    connect( d->passEdit, SIGNAL(returnPressed()), SLOT(accept()) );
+
+    if ( !user.isEmpty() )
+    {
+        d->userEdit->setText( user );
+        d->passEdit->setFocus();
+    }
+    else
+        d->userEdit->setFocus();
+
+    setFixedSize( sizeHint() );
+}
+
+QString PasswordDialog::username() const
+{
+    return d->userEdit->text();
+}
+
+QString PasswordDialog::password() const
+{
+    return d->passEdit->text();
+}
+
+bool PasswordDialog::keepPassword() const
+{
+    return d->keep;
+}
+
+void PasswordDialog::addCommentLine( const QString& label,
+                                     const QString comment )
+{
+    if (d->nRow > 0)
+        return;
+
+    QLabel* lbl = new QLabel( label, this);
+    lbl->setAlignment( Qt::AlignVCenter|Qt::AlignRight );
+    lbl->setFixedSize( lbl->sizeHint() );
+    m_pLay->addWidget( lbl, d->nRow+2, 0, Qt::AlignLeft );
+    lbl = new QLabel( comment, this);
+    lbl->setAlignment( Qt::AlignVCenter|Qt::AlignLeft|Qt::WordBreak );
+    int w = QMIN( d->prompt->sizeHint().width(), 250 );
+    lbl->setFixedSize( w, lbl->heightForWidth(w) );
+    m_pLay->addWidget( lbl, d->nRow+2, 2, Qt::AlignLeft );
+    m_pLay->addRowSpacing( 3, 10 ); // Add a spacer
+    d->nRow++;
+}
+
+void PasswordDialog::slotKeep( bool keep )
+{
+    d->keep = keep;
+}
+
+void PasswordDialog::setPrompt(const QString& prompt)
+{
+    d->prompt->setText(prompt);
+    int w = QMIN( d->prompt->sizeHint().width(), 250 );
+    d->prompt->setFixedSize( w, d->prompt->heightForWidth( w ) );
+}
+
+void PasswordDialog::setUserReadOnly( bool readOnly )
+{
+    d->userEdit->setReadOnly( readOnly );
+    if ( readOnly && d->userEdit->hasFocus() )
+        d->passEdit->setFocus();
+}
+
+int PasswordDialog::getNameAndPassword( QString& user, QString& pass, bool* keep,
+                                        const QString& prompt, bool readOnly,
+                                        const QString& caption,
+                                        const QString& comment,
+                                        const QString& label )
+{
+    PasswordDialog* dlg = new PasswordDialog( prompt, user );
+    if ( !caption.isEmpty() )
+        dlg->setPlainCaption( caption );
+    else
+        dlg->setPlainCaption( i18n("Authorization Dialog") );
+
+    if ( !comment.isEmpty() )
+        dlg->addCommentLine( label, comment );
+
+    if ( readOnly )
+        dlg->setUserReadOnly( readOnly );
+
+    int ret = dlg->exec();
+    if ( ret == Accepted )
+    {
+        user = dlg->username();
+        pass = dlg->password();
+        if ( keep ) { (*keep) = dlg->keepPassword(); }
+    }
+    delete dlg;
+    return ret;
+ }
+
 
 #include "passdlg.moc"
--- kio/passdlg.h
+++ kio/passdlg.h
@@ -1,5 +1,6 @@
 /* This file is part of the KDE libraries
    Copyright (C) 2000 David Faure <faure@kde.org>
+   Copyright (C) 2000 Dawit Alemayehu <adawit@kde.org>
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
@@ -10,30 +11,63 @@
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
    Library General Public License for more details.
 
-   You should have received a copy of the GNU Library General Public License
-   along with this library; see the file COPYING.LIB.  If not, write to
-   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-   Boston, MA 02111-1307, USA.
+   You should have received a copy of the GNU Library General Public
+   License along with this library; see the file COPYING.LIB.  If
+   not, write to the Free Software Foundation, Inc., 59 Temple
+   Place - Suite 330, Boston, MA 02111-1307, USA.
 */
+
 #ifndef __kio_pass_dlg_h__
 #define __kio_pass_dlg_h__
 
-#include <qstring.h>
-#include <qlineedit.h>
+#include <kdialog.h>
 
-#include <kdialogbase.h>
+class QGridLayout;
 
 namespace KIO {
 
-  /**
-   * A dialog for requesting a login and a password to the user.
-   * KIO-Slave authors should have a look at @ref SlaveBase::openPassDlg instead.
-   */
-  class PassDlg : public KDialog {
+/**
+ * A dialog for requesting a login and a password from the end user.
+ *
+ * KIO-Slave authors are encouraged to use @ref SlaveBase::openPassDlg
+ * instead of directly instantiating this dialog.
+ */
+class PasswordDialog : public KDialog
+{
     Q_OBJECT
-  public:
+
+public:
     /**
-     * Create a password dialog
+     * Create a password dialog.
+     *
+     * @param parent        the parent widget (default:NULL).
+     * @param name          the dialog name (default:NULL).
+     * @param wfags         window flags.
+     * @param modal         if true, the dialog will be modal(default:true).
+     * @param enableKeep    shows or hides the keep password check box.
+     */
+    PasswordDialog( QWidget* parent=0, const char* name=0, WFlags wflags=0,
+                    bool modal=true, bool enableKeep=false );
+
+    /**
+     * Create a password dialog.
+     *
+     * @param prompt        instructional text to be shown.
+     * @param user          username, if known initially.
+     * @param enableKeep    if true, makes password persistent until KDE is shutdown.
+     * @param modal         if true, the dialog will be modal (default:true).
+     * @param parent        the parent widget (default:NULL).
+     * @param name          the dialog name (default:NULL).
+     * @param wfags         window flags.
+     */
+    PasswordDialog( const QString& prompt, const QString& user,
+                    bool enableKeep = false, bool modal=true,
+                    QWidget* parent=0, const char* name=0,
+                    WFlags wflags=0 );
+
+    /**
+     * @deprecated Creates a password dialog
+     *
      * @param parent the parent widget
      * @param name the dialog name
      * @param modal if true, the dialog will be modal
@@ -41,33 +75,101 @@ namespace KIO {
      * @param head the text to show in the dialog, on top of the two fields
      * @param user user name, if known initially
      * @param pass password, if known initially
+     *
+     * IMPORTANT: This function is added for binary compatability with the
+     *            previous password dialog and as such should be avoided!
      */
-    PassDlg( QWidget* parent, const char* name, bool modal, WFlags wflags,
-	         const QString& head, const QString& user, const QString& pass );
+    PasswordDialog( QWidget* parent, const char* name, bool modal,
+                    WFlags wflags, const QString& head, const QString& user,
+                    const QString& pass );
 
     /**
-     * @return the password entered by the user
+     * Destructor
      */
-    QString password() { return m_pPass->text(); }
+    ~PasswordDialog();
 
     /**
-     * @return the login entered by the user
+     * Sets the prompt to show to the user.
      */
-    QString user() { return m_pUser->text(); }
+    void setPrompt( const QString& prompt );
 
     /**
-     * Enables/disables the username field.
+     * Adds a comment line to the dialog.
+     *
+     * This function allows you to add one additional comment
+     * line to this widget.  Calling this fucntion after a
+     * comment has already been added will not have any effect.
+     *
+     * @param label       label for comment (ex:"Command:")
+     * @param comment     the actual comment text.
      */
-    void setEnableUserField( bool, bool gainsFocus = false );
+    void addCommentLine( const QString& label, const QString comment );
 
-  private:
-    QLineEdit* m_pPass;
-    QLineEdit* m_pUser;
+    /**
+     * Returns the password entered by the user.
+     */
+    QString password() const;
 
-  };
+    /**
+     * Returns the username entered by the user.
+     */
+    QString username() const;
+
+    /**
+     * Determines whether supplied authorization should
+     * persist even after the application has been closed.
+     */
+    bool keepPassword() const;
+
+    /**
+     * Sets the username field read-only and sets the
+     * focus to the password field.
+     *
+     * @param readOnly
+     */
+    void setUserReadOnly( bool readOnly );
+
+    /**
+     * @deprecated. Use @ref setUserReadOnly(bool).
+     */
+    void setEnableUserField( bool, bool ) {};
+
+    /**
+     * A convienence static method for obtaining authorization
+     * information from the end user.
+     *
+     *
+     * @param user          username
+     * @param pass          password
+     * @param keep          pointer to flag that indicates
+     * @param prompt        text to display to user.
+     * @param readOnly      make the username field read-only.
+     * @param caption       set the title bar to given text.
+     * @param comment       extra comment to display to user.
+     * @param label         optinal label for extra comment.
+     *
+     * @return Accept/Reject based on the user choice.
+     */
+    static int getNameAndPassword( QString& user, QString& pass, bool* keep,
+                                   const QString& prompt = QString::null,
+                                   bool readOnly = false,
+                                   const QString& caption = QString::null,
+                                   const QString& comment = QString::null,
+                                   const QString& label = QString::null );
+
+private slots:
+    void slotKeep( bool );
+
+private:
+    void init( const QString&, const QString&, bool );
+
+    QGridLayout* m_pLay;
+    struct PasswordDialogPrivate;
+    PasswordDialogPrivate* d;
+};
+
+typedef PasswordDialog PassDlg;
 
 };
 
 #endif
-
-
