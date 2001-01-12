c99c1758b1e6dce42537a621ee92f884f9dac404
Lars Knoll <knoll@kde.org>
2001-01-12 15:37:58 +0000

    first version of khtmls internals. Hope this helps some people out there to understand how the lib works.
    svn path=/trunk/kdelibs/; revision=77635

--- khtml/README.java
+++
@@ -1,3 +0,0 @@
-To get the Java support working, you will need to add the Java interpreter
-(java executable) to your path. Alternatively you can specify full path 
-to Java executable in the Browser Setting dialog.
