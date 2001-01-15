1c3187f2b2c15e27ece5b50d373aec16930dcd4f
Simon Hausmann <hausmann@kde.org>
2001-01-15 16:43:27 +0000

    - make it compile ;-)
    svn path=/trunk/kdelibs/; revision=78209

--- kdeui/kwindowlistmenu.h
+++ kdeui/kwindowlistmenu.h
@@ -29,7 +29,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include <qmap.h>
 
 class KWinModule;
-class KWIndowListMenuPrivate;
+class KWindowListMenuPrivate;
 
 class KWindowListMenu : public QPopupMenu
 {
