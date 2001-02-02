6c9ff9598258c172a11a1475432eeb2610af27d4
Alex Zepeda <zipzippy@sonic.net>
2001-02-02 10:28:31 +0000

    A bunch of whitespace changes to make this thing more beautiful to my eyes.
    svn path=/trunk/kdelibs/; revision=81011

--- kdedb/plugins/postgres/connectorimpl.cpp
+++ kdedb/plugins/postgres/connectorimpl.cpp
@@ -33,22 +33,21 @@ const Q_INT32 VARHDR = 4;
 
 ConnectorImpl::ConnectorImpl()
 {
-    m_typeCache.setAutoDelete(true);
+	m_typeCache.setAutoDelete(true);
 }
 
 ConnectorImpl::ConnectorImpl(const ConnectorImpl &c)
     : Connector(c),
       conn(c.conn)
 {
-    setHost( c.host() );
-    setPort( c.port() );
-    setUser( c.user() );
-    setPassword( c.password() );
-    setConnected( c.isConnected() );
-    m_database = c.m_database; // keep always the same database
+	setHost(c.host());
+	setPort(c.port());
+	setUser(c.user());
+	setPassword (c.password());
+	setConnected(c.isConnected());
+	m_database = c.m_database; // keep always the same database
 
-    m_typeCache.setAutoDelete(true);
-
+	m_typeCache.setAutoDelete(true);
 }
 
 ConnectorImpl::~ConnectorImpl()
@@ -57,343 +56,316 @@ ConnectorImpl::~ConnectorImpl()
 
 bool ConnectorImpl::connect()
 {
-    QString connString = QString("host=%1 user=%2 password=%3");
+	QString connString = QString("host=%1 user=%2 password=%3");
     
-    connString = connString.arg(host()).arg(user()).arg(password());
+	connString = connString.arg(host()).arg(user()).arg(password());
 
-    if (port() != 0)
-        connString += QString(" port=%1").arg(port());
+	if (port() != 0)
+		connString += QString(" port=%1").arg(port());
 
-    if (m_database.isEmpty()) {
-        // we do not have a database name to connect to
-        // grab the default database from the config file, if it exists
-        KConfigBase *conf = DBENGINE->config();
-        KConfigGroupSaver(conf, "KDBPostgresPlugin");
-        conf->setGroup("KDBPostgresPlugin");
-        
-        int numHost = conf->readNumEntry("NumHosts",0);
-        
-        QString db;
-        while (numHost--) {
-            QString prefix = QString("%1_").arg(numHost);
-            QString h = conf->readEntry(prefix + "host");
-            if (h == host()) { 
-                db = conf->readEntry(prefix + "db");
-                break;
-            }
-        }
-        
-        if (!db.isEmpty())
-            connString += QString(" dbname=%1").arg(db);
-        else // try using the default 'template1', works 99%
-            connString += QString(" dbname=template1");
-    } else {
-        connString += QString(" dbname=%1").arg(m_database);
-    }
-    
-    connRep *c = new connRep;
-    c->conn = PQconnectdb(connString.ascii());                               
-    conn = c;
-    
-    if (PQstatus(connection()) == CONNECTION_BAD) {
-        DBENGINE->pushError(new KDB::InvalidLogin(this, PQerrorMessage(connection())));
-        conn = 0L;
-        setConnected(false);
-        return false;
-    } else {
-        setConnected(true);
-        return true;
-    }
+	if (m_database.isEmpty()) {
+		// we do not have a database name to connect to
+		// grab the default database from the config file, if it exists
+		KConfigBase *conf = DBENGINE->config();
+		KConfigGroupSaver(conf, "KDBPostgresPlugin");
+		conf->setGroup("KDBPostgresPlugin");
+
+		int numHost = conf->readNumEntry("NumHosts", 0);
+
+		QString db;
+		while (numHost--) {
+			QString prefix = QString("%1_").arg(numHost);
+			QString h = conf->readEntry(prefix + "host");
+			if (h == host()) { 
+				db = conf->readEntry(prefix + "db");
+				break;
+			}
+		}
+
+		if (!db.isEmpty())
+			connString += QString(" dbname=%1").arg(db);
+		else // try using the default 'template1', works 99%
+			connString += QString(" dbname=template1");
+	} else {
+		connString += QString(" dbname=%1").arg(m_database);
+	}
+
+	connRep *c = new connRep;
+	c->conn = PQconnectdb(connString.ascii());
+	conn = c;
+
+	if (PQstatus(connection()) == CONNECTION_BAD) {
+		DBENGINE->pushError(new KDB::InvalidLogin(this, PQerrorMessage(connection())));
+		conn = 0L;
+		setConnected(false);
+		return false;
+	} else {
+		setConnected(true);
+		return true;
+	}
 }
 
 void ConnectorImpl::close()
 {
-    conn = 0L;
+	conn = 0L;
 }
 
-KDB::Connector *
-ConnectorImpl::clone()
+KDB::Connector *ConnectorImpl::clone()
 {
-    return new ConnectorImpl(*this);
+	return new ConnectorImpl(*this);
 }
 
-QStringList
-ConnectorImpl::databases()
+QStringList ConnectorImpl::databases()
 {
-    KDB::RowList l = resultQuery("select datname from pg_database");
+	KDB::RowList l = resultQuery("select datname from pg_database");
 
-    QStringList lst;
-    KDB::RowList::Iterator it;
-    for( it = l.begin(); it != l.end(); ++it ) {
-        lst << (*it)[0].asString();
-    }
-    return lst;
-
+	QStringList lst;
+	KDB::RowList::Iterator it;
+	for (it = l.begin(); it != l.end(); ++it) {
+		lst << (*it)[0].asString();
+	}
+	return lst;
 }
 
-QStringList
-ConnectorImpl::tables()
+QStringList ConnectorImpl::tables()
 {
-    KDB::RowList l = resultQuery("select tablename from pg_tables where tablename !~'^pg_'");
+	KDB::RowList l = resultQuery("select tablename from pg_tables where tablename !~'^pg_'");
 
-    QStringList lst;
-    KDB::RowList::Iterator it;
-    for( it = l.begin(); it != l.end(); ++it ) {
-        lst << (*it)[0].asString();
-    }
-    return lst;
+	QStringList lst;
+	KDB::RowList::Iterator it;
+	for (it = l.begin(); it != l.end(); ++it) {
+		lst << (*it)[0].asString();
+	}
+
+	return lst;
+}
+
+KDB::RowList ConnectorImpl::fields(const QString & tableName)
+{
+	QString sql = "SELECT a.attname, ";
+	sql += "t.typname, ";
+	sql += "t.typlen, ";
+	sql += "a.atttypmod, ";
+	sql += "a.attnotnull, ";
+	sql += "a.atthasdef, ";
+	sql += "a.attnum ";
+	sql += "FROM pg_class c, pg_attribute a, pg_type t ";
+	sql += QString("WHERE c.relname = '%1'").arg(tableName);
+	sql += " AND a.attnum > 0 ";
+	sql += " AND a.attrelid = c.oid";
+	sql += " AND a.atttypid = t.oid";
+	sql += " ORDER BY a.attnum";
+
+	KDB::RowList lst = resultQuery(sql);
+
+	KDB::RowList res;
+
+	for (KDB::RowList::Iterator i = lst.begin(); i != lst.end(); i++) {
+		KDB::Row orig = *i;
+
+		KDB::Row row;
+		row << orig[0]; //name
+		row << orig[1]; //type
+
+		// decode typname e attypmod
+		if (orig[2].toInt() == -1) {
+			// calculate the length based on attypmod
+			Q_INT32 len = orig[3].toInt();
+			QString colTyp = orig[1].toString();
+			if (colTyp == "bpchar" || colTyp == "varchar") {
+				row << Value(len - VARHDR);
+				row << Value(0);
+			} else if (colTyp == "numeric") {
+				int size = ((len - VARHDR) >> 16) & 0xffff;
+				int prec = (len - VARHDR) & 0xffff;
+				row << Value(size);
+				row << Value(prec);
+			} else if (colTyp.startsWith("_")) {
+				row << Value(len + 1);
+				row << Value(0);
+			} else { // dunno! set it to 0
+				row << Value(0);
+				row << Value(0);
+			}
+		} else {
+			// we already have the size here
+			row << orig[2];
+			row << Value(0);
+		}
     
+		if (orig[4].toString() == "f")
+			row << Value(QVariant("Y"));
+		else
+			row << Value(QVariant("N"));
+
+		//TODO: these aren't mandatory, but useful
+		// load default value
+
+		// load comment
+		res << row;
+	}
+	return res;
 }
 
-KDB::RowList
-ConnectorImpl::fields(const QString & tableName)
+bool ConnectorImpl::createDatabase(const QString & name)
 {
-    QString sql = "Select a.attname, ";
-    sql += "t.typname, ";
-    sql += "t.typlen, ";
-    sql += "a.atttypmod, ";
-    sql += "a.attnotnull, ";
-    sql += "a.atthasdef, ";
-    sql += "a.attnum ";
-    sql += "FROM pg_class c, pg_attribute a, pg_type t ";
-    sql += QString("WHERE c.relname = '%1'").arg(tableName);
-    sql += " AND a.attnum > 0 ";
-    sql += " AND a.attrelid = c.oid";
-    sql += " AND a.atttypid = t.oid";
-    sql += " ORDER BY a.attnum";
+	QString sql = QString("Create database %1").arg(name);
+	execute(sql);
+	return (!DBENGINE->error());
+}
 
-    KDB::RowList lst = resultQuery(sql);
+KDB::RowList ConnectorImpl::resultQuery(const QString &sql)
+{
+	KDB::Handler *res = query(sql);
+	KDB::RowList l;
+	if (res) {
+		l = res->rows();
+		delete res;
+	}
 
-    KDB::RowList res;
+	return l;
+}
 
-    for (KDB::RowList::Iterator i = lst.begin();i != lst.end(); i++) {
-        KDB::Row orig = *i;
+bool ConnectorImpl::setCurrentDatabase(const QString &name)
+{
+	m_database = name;
+	return connect();
+}
 
-        KDB::Row row;
-        row << orig[0]; //name
-        row << orig[1]; //type
+KDB_ULONG ConnectorImpl::execute(const QString &sql)
+{
+	PGresult *res = PQexec(connection(),sql.latin1());
 
-        // decode typname e attypmod
-        if (orig[2].toInt() == -1 ) {
-            // calculate the length based on attypmod
-            Q_INT32 len = orig[3].toInt();
-            QString colTyp = orig[1].toString();
-            if (colTyp == "bpchar" || colTyp == "varchar") {
-                row << Value(len - VARHDR);
-                row << Value(0);
-            } else if (colTyp == "numeric" ) {
-                int size = ((len - VARHDR) >> 16) & 0xffff;
-                int prec = (len - VARHDR) & 0xffff;
-                row << Value(size);
-                row << Value(prec);
-            } else if (colTyp.startsWith("_") ) {
-                row << Value(len + 1);
-                row << Value(0);
-            } else { // dunno! set it to 0
-                row << Value(0);
-                row << Value(0);
-            }
-        } else {
-            // we already have the size here
-            row << orig[2];
-            row << Value(0);
-        }
+	if (!res){
+		DBENGINE->pushError(new KDB::ServerError(this, PQerrorMessage(connection())));
+		return 0L;
+	}
     
-        if (orig[4].toString() == "f")
-            row << Value(QVariant("Y"));
-        else
-            row << Value(QVariant("N"));
+	if (PQresultStatus(res) != PGRES_COMMAND_OK) {
+		if (PQresultStatus(res) == PGRES_TUPLES_OK)
+			DBENGINE->pushError(new KDB::SQLError(this, "not a command query!"));
+		else
+			DBENGINE->pushError(new KDB::SQLError(this, PQresultErrorMessage(res)));
+		return 0;
+	}
 
-        //TODO: these aren't mandatory, but useful
-        // load default value
-
-        // load comment
-        res << row;
-    }
-    return res;
+	QString nTuples = QString("%1").arg(PQcmdTuples(res));
+	PQclear(res);
+	return nTuples.toULong();
 }
 
-bool
-ConnectorImpl::createDatabase(const QString & name)
+KDB::Handler *ConnectorImpl::query(const QString &SQL)
 {
-    QString sql = QString("Create database %1").arg(name);
-    execute(sql);
-    return !DBENGINE->error();
-}
-
-KDB::RowList
-ConnectorImpl::resultQuery(const QString &sql)
-{
-    KDB::Handler *res = query(sql);
-    KDB::RowList l;
-    if (res) {
-        l = res->rows();
-        delete res;
-    }
+	PGresult *res = PQexec(connection(), SQL.latin1());
+	if (!res){
+		DBENGINE->pushError(new KDB::ServerError(this, PQerrorMessage(connection())));
+		return 0L;
+	}
     
-    return l;    
+	if (PQresultStatus(res) != PGRES_TUPLES_OK) {
+		if (PQresultStatus(res) == PGRES_COMMAND_OK)
+			DBENGINE->pushError(new KDB::SQLError(this, "not a select query!"));
+		else
+			DBENGINE->pushError(new KDB::SQLError(this, PQresultErrorMessage(res)));
+		return 0;
+	}
+
+	return new HandlerImpl(res, this);
 }
 
-bool
-ConnectorImpl::setCurrentDatabase(const QString &name)
+KDB::DataType ConnectorImpl::nativeToKDB(const QString &type)
 {
-    m_database = name;
-    return connect();
+	KDB::DataType ret = KDB::CHAR; 
+	return ret;
 }
 
-KDB_ULONG
-ConnectorImpl::execute(const QString &sql)
+QString ConnectorImpl::KDBToNative(KDB::DataType type)
 {
-    PGresult * res = PQexec(connection(),sql.latin1());
-    if (!res){
-        DBENGINE->pushError(new KDB::ServerError(this, PQerrorMessage(connection())));
-        return 0L;
-    }
+	QString ret = "CHAR";
+	return ret;
+}
+
+bool ConnectorImpl::createTable(const KDB::Table &tab)
+{
+	return false;
+}
+
+bool ConnectorImpl::dropDatabase(const QString & name)
+{
+	QString sql = QString("Drop database %1").arg(name);
+	execute(sql);
+	return (!DBENGINE->error());
+}
+
+bool ConnectorImpl::dropTable(const QString & name)
+{
+	QString sql = QString("Drop table %1").arg(name);
+	execute(sql);
+	return (!DBENGINE->error());
+}
+
+QString ConnectorImpl::oidToTypeName(Oid oid)
+{
+	//    kdDebug(20012) << k_funcinfo << oid << endl;
     
-    if (PQresultStatus(res) != PGRES_COMMAND_OK) {
-        if (PQresultStatus(res) == PGRES_TUPLES_OK)
-            DBENGINE->pushError(new KDB::SQLError(this, "not a command query!"));
-        else
-            DBENGINE->pushError(new KDB::SQLError(this, PQresultErrorMessage(res)));
-        return 0;
-    }
+	QString *s = m_typeCache.find((long int)oid);
+	if (s) {
+		//    kdDebug(20012) << "Found in cache " << *s << endl;
+		return *s;
+	} else {
+		QString sql = QString("Select typname from pg_type where oid = %1").arg((unsigned int)oid);
 
-    QString nTuples = QString("%1").arg(PQcmdTuples(res));
-    PQclear(res);
-    return nTuples.toULong();
-    
-}
-
-KDB::Handler *
-ConnectorImpl::query(const QString &SQL)
-{
-    PGresult * res = PQexec(connection(),SQL.latin1());
-    if (!res){
-        DBENGINE->pushError(new KDB::ServerError(this, PQerrorMessage(connection())));
-        return 0L;
-    }
-    
-    if (PQresultStatus(res) != PGRES_TUPLES_OK) {
-        if (PQresultStatus(res) == PGRES_COMMAND_OK)
-            DBENGINE->pushError(new KDB::SQLError(this, "not a select query!"));
-        else
-            DBENGINE->pushError(new KDB::SQLError(this, PQresultErrorMessage(res)));
-        return 0;
-    }
-
-    return new HandlerImpl(res,this);
-
-}
-
-KDB::DataType
-ConnectorImpl::nativeToKDB(const QString &type)
-{
-    KDB::DataType ret = KDB::CHAR; 
-    return ret;
-}
-
-QString
-ConnectorImpl::KDBToNative(KDB::DataType type)
-{
-    QString ret = "CHAR"; 
-    return ret;
-}
-
-bool
-ConnectorImpl::createTable(const KDB::Table &tab)
-{
-    return false;
-}
-
-bool
-ConnectorImpl::dropDatabase(const QString & name)
-{
-    QString sql = QString("Drop database %1").arg(name);
-    execute(sql);
-    return !DBENGINE->error();
-}
-
-bool
-ConnectorImpl::dropTable(const QString & name)
-{
-    QString sql = QString("Drop table %1").arg(name);
-    execute(sql);
-    return !DBENGINE->error();
-}
-
-QString
-ConnectorImpl::oidToTypeName(Oid oid)
-{
-    //    kdDebug(20012) << k_funcinfo << oid << endl;
-    
-    QString *s = m_typeCache.find((long int)oid);
-    if (s) {
-        //    kdDebug(20012) << "Found in cache " << *s << endl;
-        return *s;
-    } else {
-        QString sql = QString("Select typname from pg_type where oid = %1").arg((unsigned int)oid);
-
-        // we must do everything here to avoid recursive calls from HandlerImpl
-        PGresult * res = PQexec(connection(),sql.latin1());
-        if (!res){
-            DBENGINE->pushError(new KDB::ServerError(this, PQerrorMessage(connection())));
-            return QString::null;
-        }
+		// we must do everything here to avoid recursive calls from HandlerImpl
+		PGresult *res = PQexec(connection(), sql.latin1());
+		if (!res){
+			DBENGINE->pushError(new KDB::ServerError(this, PQerrorMessage(connection())));
+			return QString::null;
+		}
         
-        if (PQresultStatus(res) != PGRES_TUPLES_OK) {
-            DBENGINE->pushError(new KDB::SQLError(this, PQresultErrorMessage(res)));
-            return QString::null;
-        }
+		if (PQresultStatus(res) != PGRES_TUPLES_OK) {
+			DBENGINE->pushError(new KDB::SQLError(this, PQresultErrorMessage(res)));
+			return QString::null;
+		}
 
-        QString *newOid = new QString(QString::fromLocal8Bit(PQgetvalue(res, 0, 0),
-                                                             PQgetlength(res, 0, 0) + 1));
-        //kdDebug(20012) << "Loaded from pg_type " << *newOid << endl;
+		QString *newOid = new QString(QString::fromLocal8Bit(PQgetvalue(res, 0, 0),
+								     PQgetlength(res, 0, 0) + 1));
+		//kdDebug(20012) << "Loaded from pg_type " << *newOid << endl;
 
-        if (m_typeCache.insert((long int)oid, newOid)) {
-            return *newOid;
-        } else {
-            delete newOid;
-            return QString::null;
-        }
-    }
-
+		if (m_typeCache.insert((long int)oid, newOid)) {
+			return *newOid;
+		} else {
+			delete newOid;
+			return QString::null;
+		}
+	}
 }
 
-
-void 
-ConnectorImpl::beginTransaction()
+void ConnectorImpl::beginTransaction()
 {
-    execute("BEGIN TRANSACTION");
+	execute("BEGIN TRANSACTION");
 }
 
-void 
-ConnectorImpl::commit()
+void ConnectorImpl::commit()
 {
-    execute("COMMIT");
+	execute("COMMIT");
 }
 
-void 
-ConnectorImpl::rollback() 
+void ConnectorImpl::rollback() 
 {
-    execute("ROLLBACK");
+	execute("ROLLBACK");
 }
 
-bool 
-ConnectorImpl::appendField(const QString &table, KDB::Field *f)
+bool ConnectorImpl::appendField(const QString &table, KDB::Field *f)
 {
-    return false;
+	return false;
 }
 
-bool 
-ConnectorImpl::removeField(const QString &table, const QString &field)
+bool ConnectorImpl::removeField(const QString &table, const QString &field)
 {
-    return false;
+	return false;
 }
 
-bool 
-ConnectorImpl::changeField(const QString &table, KDB::Field *f)
+bool ConnectorImpl::changeField(const QString &table, KDB::Field *f)
 {
-    return false;
+	return false;
 }
-
-
