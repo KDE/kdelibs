682664f6782aac6b19a6da1dc0d23bc46790b605
Stefan Westerfeld <stefan@space.twc.de>
2001-02-05 01:25:33 +0000

    Check bits when calculating how long our hardware buffer lasts:
     	float playSpeed = AudioSubSystem::the()->channels()
    -	                * AudioSubSystem::the()->samplingRate()
    -					* 2; /* TODO: check bits */
    +	                * AudioSubSystem::the()->samplingRate();
    
    +	if(AudioSubSystem::the()->format() != 8) playSpeed *= 2.0;
    
    svn path=/trunk/kdelibs/; revision=81344

--- arts/soundserver/simplesoundserver_impl.cc
+++ arts/soundserver/simplesoundserver_impl.cc
@@ -183,9 +183,9 @@ float SimpleSoundServer_impl::serverBufferTime()
 						 * AudioSubSystem::the()->fragmentCount();
 
 	float playSpeed = AudioSubSystem::the()->channels()
-	                * AudioSubSystem::the()->samplingRate()
-					* 2; /* TODO: check bits */
+	                * AudioSubSystem::the()->samplingRate();
 
+	if(AudioSubSystem::the()->format() != 8) playSpeed *= 2.0;
 	return 1000.0 * hardwareBuffer / playSpeed;
 }
 
