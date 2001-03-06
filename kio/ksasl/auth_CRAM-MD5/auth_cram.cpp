c0c9e4e835e454705292a62a1a5c0b9f6c8ab07d
Alex Zepeda <zipzippy@sonic.net>
2001-03-06 02:36:25 +0000

    Convert the rest of this to at least compile with the current KFactory derived framework.
    svn path=/trunk/kdelibs/; revision=85550

--- kio/ksasl/auth_CRAM-MD5/auth_cram.cpp
+++ kio/ksasl/auth_CRAM-MD5/auth_cram.cpp
@@ -1,45 +1,47 @@
 // $Id$
 
-#define QSTRING_TO_MD5(x) reinterpret_cast<unsigned char *>(const_cast<char *>(x.latin1()))
-
-#include <stdlib.h>
-
-#include <iostream.h>
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
 
 #include <qstring.h>
 
 #include <kdebug.h>
 
-#include "../saslmodule.h"
+#include <kinstance.h>
+#include "../../kmdcodec.h"
 
-DECLARE_SASL_MODULE("CRAM-MD5", CramAuth);
+#include "auth_cram.h"
 
-CramAuth::CramAuth()
-	: KSASLAuthModule()
+CramAuthModule::CramAuthModule(QObject *parent, const char *name)
+	: KSASLAuthModule(parent, name)
 {
 }
 
-CramAuth::~CramAuth()
+CramAuthModule::~CramAuthModule()
 {
 }
 
-#include "local_md5.h"
+QString CramAuthModule::auth_method()
+{
+	return QString::fromLatin1("CRAM-MD5");
+}
 
-QString CramAuth::auth_response(const QString &challenge, const KURL &auth_url)
+QString CramAuthModule::auth_response(const QString &challenge, const KURL &auth_url)
 {
 	char i_key[65], o_key[65];
 	int i;
-	MD5 *imd5, *md5;
+	KMD5 *imd5, *md5;
 	QString ret;
 
 	memset(i_key, 0, 65);
 	memset(o_key, 0, 65);
 	if (auth_url.pass().length() > 64) {
-		MD5 *kmd5 = new MD5;
- 		kmd5->update(QSTRING_TO_MD5(auth_url.pass()), auth_url.pass().length());
+		KMD5 *kmd5 = new KMD5;
+ 		kmd5->update(auth_url.pass());
 		kmd5->finalize();
-		memcpy(i_key, kmd5->raw_digest(), 16);
-		memcpy(o_key, kmd5->raw_digest(), 16);
+		memcpy(i_key, kmd5->rawDigest(), 16);
+		memcpy(o_key, kmd5->rawDigest(), 16);
 		delete kmd5;
 	} else {
 		memcpy(i_key, auth_url.pass().latin1(), auth_url.pass().length());
@@ -51,19 +53,49 @@ QString CramAuth::auth_response(const QString &challenge, const KURL &auth_url)
 		o_key[i] ^= 0x5c;
 	}
 
-	imd5=new MD5;
+	imd5=new KMD5;
 	imd5->update((unsigned char *)i_key, 64);
-	imd5->update(QSTRING_TO_MD5(challenge), challenge.length());
+	imd5->update(challenge);
 	imd5->finalize();
 
-	md5= new MD5;
+	md5= new KMD5;
 	md5->update(reinterpret_cast<unsigned char *>(o_key), 64);
-	md5->update(imd5->raw_digest(), 16);
+	md5->update(imd5->rawDigest(), 16);
 	md5->finalize();
 	ret=auth_url.user();
-	ret+=QString(" ")+md5->hex_digest();
+	ret+=QString(" ")+md5->hexDigest();
 
 	delete imd5;
 	delete md5;
 	return ret;
 }
+
+// Module factory stuff.. should be automated
+CramAuthModuleFactory::CramAuthModuleFactory (QObject *parent, const char *name)
+	: KLibFactory (parent, name)
+{
+	s_instance = new KInstance( "authcrammodule" );
+}
+
+CramAuthModuleFactory::~CramAuthModuleFactory()
+{
+	delete s_instance;
+}
+
+QObject *CramAuthModuleFactory::create (QObject *parent, const char *name, const char *, const QStringList &) {
+	QObject *obj = new CramAuthModule (parent, name);
+	emit objectCreated( obj );
+	return obj;
+}
+
+KInstance *CramAuthModuleFactory::instance()
+{
+	return s_instance;
+}
+
+extern "C" {
+        void *init_ksasl_auth_cram() {
+                return new CramAuthModuleFactory;
+        }
+}
+
