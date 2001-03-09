9d9b759ca688d60538b0bc81ef781a3af12914f7
David Faure <faure@kde.org>
2001-03-09 15:52:04 +0000

    Allow to use the existing find dialog on another frame.
    svn path=/trunk/kdelibs/; revision=86121

--- khtml/khtml_part.cpp
+++ khtml/khtml_part.cpp
@@ -3843,7 +3843,7 @@ void KHTMLPart::slotFind()
                this, SLOT( slotFindDialogDestroyed() ) );
   }
 
-  d->m_findDialog->setNewSearch();
+  d->m_findDialog->setPart( part );
   d->m_findDialog->setText( part->d->m_lastFindState.text );
   d->m_findDialog->setCaseSensitive( part->d->m_lastFindState.caseSensitive );
   d->m_findDialog->setDirection( part->d->m_lastFindState.direction );
