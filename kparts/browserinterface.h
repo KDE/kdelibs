b7720cb7bf836c7c779b1e5099fb423769d6cc44
Simon Hausmann <hausmann@kde.org>
2001-03-12 18:21:38 +0000

    - documentation
    svn path=/trunk/kdelibs/; revision=86642

--- kparts/browserinterface.h
+++ kparts/browserinterface.h
@@ -7,6 +7,28 @@
 namespace KParts
 {
 
+/**
+ * The purpose of this interface is to allow a direct communication between
+ * a KPart and the hosting browser shell (for example Konqueror) . A
+ * shell implementing this interface can propagate it to embedded kpart
+ * components by using the setBrowserInterface call of the part's
+ * KParts::BrowserExtension object.
+ *
+ * This interface looks not very rich, but the main functionality is
+ * implemented using the @ref callMethod method for part->shell
+ * communication and using Qt properties for allowing a part to
+ * to explicitly query information from the shell.
+ *
+ * Konqueror in particular, as 'reference' implementation, provides
+ * the following functionality through this interface:
+ *
+ * Qt properties:
+ *   Q_PROPERTY( uint historyLength READ historyLength );
+ *
+ * Callable methods:
+ *       void goHistory( int );
+ *
+ */
 class BrowserInterface : public QObject
 {
     Q_OBJECT
@@ -14,6 +36,10 @@ public:
     BrowserInterface( QObject *parent, const char *name = 0 );
     virtual ~BrowserInterface();
 
+    /**
+     * Perform a dynamic invocation of a method in the BrowserInterface
+     * implementation. Methods are to be implemented as simple Qt slots.
+     */
     void callMethod( const char *name, const QVariant &argument );
 };
 
