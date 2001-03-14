465c97baec761cb67fe703129efab173d5b53547
Dirk Mueller <mueller@kde.org>
2001-03-14 01:23:12 +0000

    #21959: fix reset'ing of <input> types (radio in special)
    svn path=/trunk/kdelibs/; revision=86874

--- khtml/html/html_formimpl.cpp
+++ khtml/html/html_formimpl.cpp
@@ -335,7 +335,7 @@ void HTMLFormElementImpl::submit(  )
             view->addFormCompletionItem(input->name().string(), input->value().string());
         }
     }
-    
+
     QByteArray form_data = formData();
     if(m_post)
     {
@@ -686,27 +686,26 @@ ushort HTMLFieldSetElementImpl::id() const
 HTMLInputElementImpl::HTMLInputElementImpl(DocumentImpl *doc)
     : HTMLGenericFormElementImpl(doc)
 {
-    m_type = TEXT;
-    m_checked = false;
-    m_maxLen = -1;
-    m_size = 20;
-    m_clicked = false;
-    m_filename = "";
-    m_haveType = false;
-
-    view = 0;
+    init();
 }
 
 HTMLInputElementImpl::HTMLInputElementImpl(DocumentImpl *doc, HTMLFormElementImpl *f)
     : HTMLGenericFormElementImpl(doc, f)
 {
+    init();
+}
+
+void HTMLInputElementImpl::init()
+{
     m_type = TEXT;
-    m_checked = false;
     m_maxLen = -1;
     m_size = 20;
     m_clicked = false;
+    m_defaultChecked = false;
+    m_checked = false;
     m_filename = "";
     m_haveType = false;
+    m_firstAttach = true;
 
     view = 0;
 }
@@ -893,6 +892,12 @@ void HTMLInputElementImpl::attach(KHTMLView *_view)
 {
     setStyle(document->styleSelector()->styleForElement(this));
     view = _view;
+
+    if(m_firstAttach) {
+        m_defaultChecked = m_checked;
+        m_defaultValue = m_value;
+        m_firstAttach = false;
+    }
 
     khtml::RenderObject *r = _parent ? _parent->renderer() : 0;
     if(r)
@@ -1108,8 +1113,8 @@ bool HTMLInputElementImpl::encoding(const QTextCodec* codec, khtml::encodingList
 
 void HTMLInputElementImpl::reset()
 {
-    setValue(getAttribute(ATTR_VALUE));
-    setChecked(getAttribute(ATTR_CHECKED) != 0);
+    setValue(m_defaultValue);
+    setChecked(m_defaultChecked);
     if ((m_type == SUBMIT || m_type == RESET || m_type == BUTTON || m_type == IMAGE) && m_render)
         static_cast<RenderSubmitButton*>(m_render)->setClicked(false);
 }
