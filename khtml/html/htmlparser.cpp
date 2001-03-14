3b68fae3e458027570f1db0f394a4773b1324ab0
Dirk Mueller <mueller@kde.org>
2001-03-14 02:04:35 +0000

    backport fix #21972
    svn path=/branches/KDE_2_1_BRANCH/kdelibs/; revision=86890

--- khtml/html/htmlparser.cpp
+++ khtml/html/htmlparser.cpp
@@ -646,11 +646,7 @@ bool KHTMLParser::insertNode(NodeImpl *n)
                 break;
            }
            break;
-	    case ID_FONT:
-		if ( current->id() == ID_OPTION || current->id() == ID_OPTGROUP )
-		    return false;
-		break;
-	    default:
+        default:
             break;
         }
 
@@ -836,10 +832,6 @@ bool KHTMLParser::insertNode(NodeImpl *n)
 		// IE treats a nested select as </select>. Let's do the same
 		popBlock( ID_SELECT );
                 break;
-            }
-            else {
-                popBlock(ID_SELECT);
-                handled = true;
             }
             break;
             // head elements in the body should be ignored.
