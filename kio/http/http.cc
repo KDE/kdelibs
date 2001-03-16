afbd8cc85a7192dc03837a0974fc7bf5245c0c5a
Waldo Bastian <bastian@kde.org>
2001-03-16 05:55:57 +0000

    Backport of fix for BR19849
    svn path=/branches/KDE_2_1_BRANCH/kdelibs/; revision=87245

--- kio/http/http.cc
+++ kio/http/http.cc
@@ -709,7 +709,10 @@ bool HTTPProtocol::checkSSL()
         if ( result == KMessageBox::Cancel )
         {
           kdDebug(7103) << "Cancelling the loading" << endl;
-          error( ERR_USER_CANCELED, "ssl" );
+          if (m_bErrorPage)
+             errorPage();
+          else
+             error( ERR_USER_CANCELED, "ssl" );
           return false;
         }
 
@@ -1420,7 +1423,6 @@ bool HTTPProtocol::readHeader()
            error(ERR_DOES_NOT_EXIST, m_request.url.url());
            return false;
         }
-        errorPage();
         m_bCachedWrite = false; // Don't put in cache
         mayCache = false;
       }
@@ -1665,8 +1667,16 @@ bool HTTPProtocol::readHeader()
   {
     if ( m_responseCode == 401 || m_responseCode == 407 )
     {
-        http_closeConnection();  // Close the connection first
-        return false;
+        if ( getAuthorization() )
+        {
+           http_closeConnection();
+           return false; // Try again.
+        }
+
+        if (m_bError)
+           return false; // Error out
+
+        // Show error page...
     }
     m_bUnauthorized = false;
   }
@@ -3287,7 +3297,7 @@ bool HTTPProtocol::retrieveHeader( bool close_connection )
 
     if (!readHeader())
     {
-        if ( m_bError || ( m_bUnauthorized && !getAuthorization() ) )
+        if ( m_bError )
             return false;
     }
     else
@@ -3416,8 +3426,11 @@ bool HTTPProtocol::getAuthorization()
     }
     if ( prompt && !retryPrompt() )
     {
-      error(ERR_USER_CANCELED, QString::null);
-      return result;
+      if (m_bErrorPage)
+         errorPage();
+      else
+         error(ERR_USER_CANCELED, QString::null);
+      return false;
     }
   }
   else
@@ -3486,11 +3499,14 @@ bool HTTPProtocol::getAuthorization()
       default:
         break;
     }
+    return true;
   }
-  else
-    error( ERR_USER_CANCELED, QString::null );
 
-  return result;
+  if (m_bErrorPage)
+     errorPage();
+  else
+     error( ERR_USER_CANCELED, QString::null );
+  return false;
 }
 
 void HTTPProtocol::saveAuthorization()
