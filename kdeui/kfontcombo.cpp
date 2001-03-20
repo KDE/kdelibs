04c019cbc6760deb908a44887d077bf752ee8e10
Malte Starostik <malte@kde.org>
2001-03-20 10:48:56 +0000

    Added KFontCombo. Rendering of the fonts can be turned off with DisplayFontItems=false in the section [KDE] of kdeglobals
    svn path=/trunk/kdelibs/; revision=87888

---
+++ kdeui/kfontcombo.cpp
@@ -0,0 +1,263 @@
+/* This file is part of the KDE libraries
+   Copyright (c) 2001 Malte Starostik <malte.starostik@t-online.de>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License version 2 as published by the Free Software Foundation.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
+// $Id$
+
+#include <qfontdatabase.h>
+#include <qlistbox.h>
+
+#include <kapp.h>
+#include <kcharsets.h>
+#include <kconfig.h>
+#include <kglobal.h>
+
+#include "kfontcombo.h"
+#include "kfontcombo.moc"
+
+struct KFontComboPrivate
+{
+    KFontComboPrivate()
+        : bold(false),
+          italic(false),
+          underline(false),
+          strikeOut(false),
+          size(0),
+          lineSpacing(0)
+    {
+    };
+    
+    bool bold : 1;
+    bool italic : 1;
+    bool underline : 1;
+    bool strikeOut : 1;
+    bool displayFonts : 1;
+    int size;
+    int lineSpacing;
+};
+
+class KFontListItem : public QListBoxItem
+{
+public:
+    KFontListItem(const QString &fontName, KFontCombo *combo);
+    virtual ~KFontListItem();
+
+    virtual int width(const QListBox *) const;
+    virtual int height(const QListBox *) const;
+
+    void updateFont();
+    
+protected:    
+    virtual void paint(QPainter *p);
+
+private:
+    void createFont();
+
+private:
+    KFontCombo *m_combo;
+    QString m_fontName;
+    QFont *m_font;
+    bool m_canPaintName;
+};
+
+KFontListItem::KFontListItem(const QString &fontName, KFontCombo *combo)
+    : QListBoxItem(combo->listBox()),
+      m_combo(combo),
+      m_fontName(fontName),
+      m_font(0),
+      m_canPaintName(true)
+{
+    setText(fontName);
+};
+
+KFontListItem::~KFontListItem()
+{
+    delete m_font;
+}
+
+int KFontListItem::width(const QListBox *lb) const
+{
+    QFontMetrics fm(m_font ? *m_font : lb->fontMetrics());
+    return fm.width(text()) + 6;
+}
+
+int KFontListItem::height(const QListBox *lb) const
+{
+    if (m_combo->d->displayFonts)
+        return m_combo->d->lineSpacing + 2;
+    QFontMetrics fm(lb->fontMetrics());
+    return fm.lineSpacing() + 2;
+}
+
+void KFontListItem::paint(QPainter *p)
+{
+    if (m_combo->d->displayFonts)
+    {
+        if (!m_font)
+            createFont();
+
+        QString t = m_fontName;
+        if (p->device() != m_combo)
+        {
+            if (m_canPaintName)
+                p->setFont(*m_font);
+            else
+                t = QString::fromLatin1("(%1)").arg(m_fontName);
+        }
+        QFontMetrics fm(p->fontMetrics());
+        p->drawText(3, (m_combo->d->lineSpacing + fm.ascent() + fm.leading() / 2) / 2, t);
+    }
+    else
+    {
+        QFontMetrics fm(p->fontMetrics());
+        p->drawText(3, fm.ascent() + fm.leading() / 2, m_fontName);
+    }
+}
+
+void KFontListItem::updateFont()
+{
+    if (!m_font)
+        return;        
+
+    m_font->setBold(m_combo->d->bold);
+    m_font->setItalic(m_combo->d->italic);
+    m_font->setUnderline(m_combo->d->underline);
+    m_font->setStrikeOut(m_combo->d->strikeOut);
+    m_font->setPointSize(m_combo->d->size);
+}
+
+void KFontListItem::createFont()
+{
+    if (m_font)
+        return;
+        
+    m_font = new QFont(m_fontName);
+    QFontMetrics fm(*m_font);
+    for (unsigned int i = 0; i < m_fontName.length(); ++i)
+        if (!fm.inFont(m_fontName[i]))
+        {
+            m_canPaintName = false;
+            break;
+        }
+    updateFont();
+}
+
+KFontCombo::KFontCombo(QWidget *parent, const char *name)
+    : KComboBox(parent, name)
+{
+    d = new KFontComboPrivate;
+    d->displayFonts = displayFonts();
+    setInsertionPolicy(NoInsertion);
+    setSize(12);
+    QFontDatabase fontdb;
+    QStringList fonts = fontdb.families(false);
+    for (QStringList::ConstIterator it = fonts.begin(); it != fonts.end(); ++it)
+        new KFontListItem(*it, this);
+}
+
+KFontCombo::~KFontCombo()
+{
+    delete d;
+}
+
+void KFontCombo::setBold(bool bold)
+{
+    if (d->bold == bold)
+        return;
+    d->bold = bold;
+    updateFonts();
+}
+
+bool KFontCombo::bold() const
+{
+    return d->bold;
+}
+
+void KFontCombo::setItalic(bool italic)
+{
+    if (d->italic == italic)
+        return;
+    d->italic = italic;
+    updateFonts();
+}
+
+bool KFontCombo::italic() const
+{
+    return d->italic;
+}
+
+void KFontCombo::setUnderline(bool underline)
+{
+    if (d->underline == underline)
+        return;
+    d->underline = underline;
+    updateFonts();
+}
+
+bool KFontCombo::underline() const
+{
+    return d->underline;
+}
+
+void KFontCombo::setStrikeOut(bool strikeOut)
+{
+    if (d->strikeOut == strikeOut)
+        return;
+    d->strikeOut = strikeOut;
+    updateFonts();
+}
+
+bool KFontCombo::strikeOut() const
+{
+    return d->strikeOut;
+}
+
+void KFontCombo::setSize(int size)
+{
+    if (d->size == size)
+        return;
+    d->size = size;
+    QFont f;
+    f.setPointSize(size);
+    QFontMetrics fm(f);
+    d->lineSpacing = fm.lineSpacing();
+    updateFonts();
+}
+
+int KFontCombo::size() const
+{
+    return d->size;
+}
+
+void KFontCombo::updateFonts()
+{
+    if (!d->displayFonts)
+        return;
+
+    for (unsigned int i = 0; i < listBox()->count(); ++i)
+    {
+        KFontListItem *item = static_cast<KFontListItem *>(listBox()->item(i));
+        item->updateFont();
+    }
+}
+
+bool KFontCombo::displayFonts()
+{
+    KConfigGroupSaver saver(kapp->config(), "KDE");
+    return kapp->config()->readBoolEntry("DisplayFontItems", true);
+}
+
---
+++ kdeui/kfontcombo.h
@@ -0,0 +1,135 @@
+/* This file is part of the KDE libraries
+   Copyright (c) 2001 Malte Starostik <malte.starostik@t-online.de>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License version 2 as published by the Free Software Foundation.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
+// $Id$
+
+#ifndef _KFONTCOMBO_H_
+#define _KFONTCOMBO_H_
+
+#include <kcombobox.h>
+
+/**
+ * A combobox that lists the available fonts.
+ *
+ * The items are painted using the respective font itself, so the user
+ * can easily choose a font based on its look. This can be turned off
+ * globally if the user wishes so.
+ *
+ * @short A combobox to choose a font
+ * @author Malte Starostik <malte.starostik@t-online.de>
+ * @version $Id$
+ */
+class KFontCombo : public KComboBox
+{
+    Q_OBJECT
+public:
+    /**
+     * Constructor
+     *
+     * @param parent The parent widget
+     * @param name The object name for the widget
+     */
+    KFontCombo(QWidget *parent = 0, const char *name = 0);
+    /**
+     * Destructor
+     */
+    virtual ~KFontCombo();
+
+    /**
+     * Set the listed fonts to bold or normal
+     *
+     * @param bold Set to true to display fonts in bold
+     */
+    void setBold(bool bold);
+    /**
+     * Retrieve the current bold status
+     *
+     * @return True if fonts are bold
+     */
+    bool bold() const;
+    /**
+     * Set the listed fonts to italic or regular
+     *
+     * @param italic Set to true to display fonts italic
+     */
+    void setItalic(bool bold);
+    /**
+     * Retrieve the current italic status
+     *
+     * @return True if fonts are italic
+     */
+    bool italic() const;
+    /**
+     * Set the listed fonts to underlined or not underlined
+     *
+     * @param underline Set to true to display fonts underlined
+     */
+    void setUnderline(bool bold);
+    /**
+     * Retrieve the current underline status
+     *
+     * @return True if fonts are underlined
+     */
+    bool underline() const;
+    /**
+     * Set the listed fonts to striked out or not
+     *
+     * @param strikeOut Set to true to display fonts striked out
+     */
+    void setStrikeOut(bool bold);
+    /**
+     * Retrieve the current strike out status
+     *
+     * @return True if fonts are striked out
+     */
+    bool strikeOut() const;
+    /**
+     * Set the listed fonts' size
+     *
+     * @param size Set to the point size to display the fonts in
+     */
+    void setSize(int size);
+    /**
+     * Retrieve the current font size
+     *
+     * @return The point size of the fonts
+     */
+    int size() const;
+
+    /**
+     * Retrieve the user's setting of whether the items should be painted
+     * in the respective fonts or not
+     *
+     * @return True if the respective fonts are used for painting
+     */
+    static bool displayFonts();
+
+protected:
+    /**
+     * Updated the combo's @ref listBox() to reflect changes made to the
+     * fonts' attributed
+     */
+    void updateFonts();
+
+private:
+    friend class KFontListItem;
+    struct KFontComboPrivate *d;
+};
+
+#endif
+
