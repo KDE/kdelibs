c74b9181fb99c9b4a4bbf5dd46b2ec3475657d43
Dirk Mueller <mueller@kde.org>
2001-03-26 12:20:40 +0000

    thanks again for breaking compilation
    svn path=/trunk/kdelibs/; revision=88743

---
+++ kdeprint/maticparser.cpp.h
@@ -0,0 +1,17 @@
+#ifndef maticparser_cpp_h
+#define maticparser_cpp_h
+
+typedef union {
+	QString	*string;
+	MHash	*hash;
+	int	num;
+} YYSTYPE;
+#define	STRING	257
+#define	UNDEF	258
+#define	VAR	259
+#define	NUM	260
+
+
+extern YYSTYPE maticlval;
+
+#endif
