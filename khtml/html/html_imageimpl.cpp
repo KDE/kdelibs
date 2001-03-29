02c5f0de8699ad3fbc5a5649fdd24bc5c5b84bd2
Dirk Mueller <mueller@kde.org>
2001-03-29 00:14:12 +0000

    #22759, check for range first before doing map checking
    svn path=/trunk/kdelibs/; revision=89200

--- khtml/html/html_imageimpl.cpp
+++ khtml/html/html_imageimpl.cpp
@@ -76,7 +76,8 @@ bool HTMLImageElementImpl::prepareMouseEvent( int _x, int _y,
 					      MouseEvent *ev )
 {
     //kdDebug( 6030 ) << "_x=" << _x << " _tx=" << _tx << " _y=" << _y << ", _ty=" << _ty << endl;
-    if ( (usemap.length() > 0) &&
+    bool inside = HTMLElementImpl::prepareMouseEvent(_x, _y, _tx, _ty, ev);
+    if ( inside && usemap.length() > 0 &&
          ( ! (m_render && m_render->style() && m_render->style()->visiblity() == HIDDEN) ) )
     {
         RenderObject* p = m_render->parent();
@@ -102,7 +103,7 @@ bool HTMLImageElementImpl::prepareMouseEvent( int _x, int _y,
                                       renderer()->width(), renderer()->height(), ev);
 	}
     }
-    return HTMLElementImpl::prepareMouseEvent(_x, _y, _tx, _ty, ev);
+    return inside;
 }
 
 void HTMLImageElementImpl::parseAttribute(AttrImpl *attr)
