3d19f7768889e1c22b73404ab94c0868be4baf43
Dawit Alemayehu <adawit@kde.org>
2001-03-29 09:44:09 +0000

    - Backport fix for BR#23255
    svn path=/branches/KDE_2_1_BRANCH/kdelibs/; revision=89239

--- kdeui/kcombobox.cpp
+++ kdeui/kcombobox.cpp
@@ -145,7 +145,7 @@ bool KComboBox::isURLDropsEnabled() const
 
 void KComboBox::setCompletedText( const QString& text, bool marked )
 {
-    if ( marked )
+    if ( m_pEdit && marked )
 	m_pEdit->validateAndSet( text, currentText().length(),
 				 currentText().length(), text.length() );
     else
@@ -196,9 +196,7 @@ void KComboBox::makeCompletion( const QString& text )
 	    setCompletedText( match, marked );
         }
     }
-
-    // Read - only combobox
-    else if( !m_pEdit )
+    else
     {
         if( text.isNull() || !listBox() )
             return;
