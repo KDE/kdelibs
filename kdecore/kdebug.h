763800218c0739279fa51919a8de7dcd3fff29ef
Stephan Kulow <coolo@kde.org>
2001-04-07 13:48:07 +0000

    nice try Thomas -#define kdebug kndDebug  #define kdDebug kndDebug
    svn path=/trunk/kdelibs/; revision=90875

--- kdecore/kdebug.h
+++ kdecore/kdebug.h
@@ -169,7 +169,6 @@ inline kndbgstream &perror( kndbgstream & s) { return s; }
 kdbgstream kdDebug(int area = 0);
 kdbgstream kdDebug(bool cond, int area = 0);
 #else
-#define kdebug kndDebug
 #define kdDebug kndDebug
 inline kndbgstream kndDebug(int = 0) { return kndbgstream(); }
 inline kndbgstream kndDebug(bool , int  = 0) { return kndbgstream(); }
