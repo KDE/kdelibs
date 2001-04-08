86d93e8de485aee91ec5ab5a13b5088e187071aa
David Faure <faure@kde.org>
2001-04-08 21:38:35 +0000

    The session-manager file is in Latin1 too. Oh boy, KDE must be really broken for utf-16 and asian users (i.e. locales where ascii/latin1 text can't be parsed) !
    svn path=/trunk/kdelibs/; revision=91086

--- kdecore/kapp.cpp
+++ kdecore/kapp.cpp
@@ -857,6 +857,7 @@ void KApplication::propagateSessionManager()
         QFileInfo info ( f );
         smModificationTime = new QTime( info.lastModified().time() );
         QTextStream t(&f);
+        t.setEncoding( QTextStream::Latin1 );
         QString s = t.readLine();
         f.close();
         ::setenv( "SESSION_MANAGER", s.latin1(), TRUE  );
@@ -984,12 +985,14 @@ void KApplication::dcopFailure(const QString &msg)
   failureCount++;
   if (failureCount == 1)
   {
+     kdDebug() << "KApplication::dcopFailure " << getpid() << ", first failure -> trying to launch kdeinit" << endl;
      // Try to launch kdeinit.
      QString srv = KStandardDirs::findExe(QString::fromLatin1("kdeinit"));
      if (srv.isEmpty())
         srv = KStandardDirs::findExe(QString::fromLatin1("kdeinit"), KDEDIR+QString::fromLatin1("/bin"));
      if (srv.isEmpty())
         return;
+     kdDebug() << srv << endl;
      my_system(QFile::encodeName(srv));
      return;
   }
