e77bfd5e8abb5be5c3e4e351490ee747969f3bec
Carsten Pfeiffer <pfeiffer@kde.org>
2001-04-09 23:31:47 +0000

    better not crash (+ added missing copyright header)
    svn path=/trunk/kdelibs/; revision=91254

--- kio/kscan.cpp
+++ kio/kscan.cpp
@@ -1,3 +1,22 @@
+/* This file is part of the KDE libraries
+    Copyright (C) 2001 Carsten Pfeiffer <pfeiffer@kde.org>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+    Boston, MA 02111-1307, USA.
+*/
+
 #include <klocale.h>
 #include <ktrader.h>
 
@@ -13,14 +32,14 @@ KScanDialog * KScanDialog::getScanDialog( QWidget *parent, const char *name,
 	
     KService::Ptr ptr = *(offers.begin());
     KLibFactory *factory = KLibLoader::self()->factory( ptr->library().latin1() );
-    
+
     if ( !factory )
         return 0;
 
     QStringList args;
     args << QString::number( (int)modal );
 
-    QObject *res = factory->create( parent, name, "KScanDialog", args ); 
+    QObject *res = factory->create( parent, name, "KScanDialog", args );
 
     return dynamic_cast<KScanDialog *>( res );
 }
@@ -61,11 +80,11 @@ QObject *KScanDialogFactory::createObject( QObject *parent, const char *name,
     if ( strcmp( classname, "KScanDialog" ) != 0 )
         return 0;
 
-    if ( !parent->isWidgetType() )
+    if ( parent && !parent->isWidgetType() )
        return 0;
 
     bool modal = false;
-    
+
     if ( args.count() == 1 )
         modal = (bool)args[ 0 ].toInt();
 
