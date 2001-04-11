c7a2ac3204c5bbe026086a28dae1a9ae3b85a117
Carsten Pfeiffer <pfeiffer@kde.org>
2001-04-11 23:15:47 +0000

    forgot the copyright header here, too
    svn path=/trunk/kdelibs/; revision=91582

--- kio/kscan.h
+++ kio/kscan.h
@@ -1,3 +1,22 @@
+/* This file is part of the KDE libraries
+    Copyright (C) 2001 Carsten Pfeiffer <pfeiffer@kde.org>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+    Boston, MA 02111-1307, USA.
+*/
+
 #ifndef KSCAN_H
 #define KSCAN_H
 
@@ -18,7 +37,7 @@ public:
     ~KScanDialog();
 
 protected:
-    KScanDialog( int dialogFace=Tabbed, int buttonMask = Close|Help, 
+    KScanDialog( int dialogFace=Tabbed, int buttonMask = Close|Help,
 		 QWidget *parent=0L, const char *name=0, bool modal=false );
 
     int id() const { return m_currentId; }
@@ -54,8 +73,8 @@ public:
 protected:
     KScanDialogFactory( QObject *parent=0, const char *name=0 );
 
-    virtual QObject* createObject( QObject* parent = 0, const char* name = 0, 
-                                   const char* classname = "QObject", 
+    virtual QObject* createObject( QObject* parent = 0, const char* name = 0,
+                                   const char* classname = "QObject",
                                    const QStringList &args = QStringList() );
 
 
