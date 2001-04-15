b1331a390ac45b31816b63a258a9eb5b71f23673
Antti Koivisto <koivisto@iki.fi>
2001-04-15 03:10:15 +0000

    paint children of zero width positioned element
    svn path=/trunk/kdelibs/; revision=92143

--- khtml/rendering/render_flow.cpp
+++ khtml/rendering/render_flow.cpp
@@ -218,7 +218,7 @@ void RenderFlow::layout()
         kdDebug( 6040 ) << renderName() << ": containingBlock == this" << endl;
 #endif
 
-    if(m_width<=0) {
+    if(m_width<=0 && !isPositioned()) {
         if(m_y < 0) m_y = 0;
         setLayouted();
         return;
