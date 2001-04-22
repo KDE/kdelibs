8940b7e4975cb3d323464029669e3addf558262d
Michael Goffioul <kdeprint@swing.be>
2001-04-22 19:07:48 +0000

    Files added.
    svn path=/trunk/kdelibs/; revision=93476

---
+++ kdeprint/lpd/gschecker.cpp
@@ -0,0 +1,71 @@
+/*
+ *  This file is part of the KDE libraries
+ *  Copyright (c) 2001 Michael Goffioul <goffioul@imec.be>
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Library General Public
+ *  License version 2 as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Library General Public License
+ *  along with this library; see the file COPYING.LIB.  If not, write to
+ *  the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ *  Boston, MA 02111-1307, USA.
+ **/
+
+#include "gschecker.h"
+
+#include <qfile.h>
+#include <qtextstream.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+GsChecker::GsChecker(QObject *parent, const char *name)
+: QObject(parent,name)
+{
+}
+
+bool GsChecker::checkGsDriver(const QString& name)
+{
+	if (m_driverlist.count() == 0)
+		loadDriverList();
+	return m_driverlist.contains(name);
+}
+
+void GsChecker::loadDriverList()
+{
+	FILE	*proc = popen("gs -h","r");
+	if (proc)
+	{
+		QFile	f;
+		if (f.open(IO_ReadOnly,proc))
+		{
+			QTextStream	t(&f);
+			QString	buffer, line;
+			bool	ok(false);
+			while (!t.eof())
+			{
+				line = t.readLine().stripWhiteSpace();
+				if (ok)
+				{
+					if (line.find(':') != -1)
+						break;
+					else
+						buffer.append(line).append(" ");
+				}
+				else if (line.startsWith(QString::fromLatin1("Available devices:")))
+					ok = true;
+			}
+			f.close();
+			m_driverlist = QStringList::split(' ',buffer,false);
+		}
+		pclose(proc);
+	}
+}
