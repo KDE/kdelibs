9a26b52310ab3ca7c9541c8c9dbda022df7ae5e7
Dawit Alemayehu <adawit@kde.org>
2001-04-22 00:46:00 +0000

    - Credit/Documentation fixes. (DA)
    svn path=/trunk/kdelibs/; revision=93346

--- kio/authinfo.h
+++ kio/authinfo.h
@@ -1,5 +1,25 @@
-#ifndef __kio_authinfo_h__
-#define __kio_authinfo_h__
+/*
+ *  This file is part of the KDE libraries
+ *  Copyright (C) 2000-2001 Dawit Alemayehu <adawit@kde.org>
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Library General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Library General Public License
+ *  along with this library; see the file COPYING.LIB.  If not, write to
+ *  the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ *  Boston, MA 02111-1307, USA.
+ */
+
+#ifndef __KIO_AUTHINFO_H
+#define __KIO_AUTHINFO_H
 
 #include <qdatastream.h>
 #include <qstring.h>
@@ -132,7 +152,6 @@ public:
     * for the entire life of the current KDE session otherwise the
     * cached password is deleted right after the application using
     * it has been closed.
-    *
     */
     bool keepPassword;
 
