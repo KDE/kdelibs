d36b0bfb1e0a9fdb173b91fab51ea361e02e9635
nobody <nobody@localhost>
2001-04-30 21:46:58 +0000

    This commit was manufactured by cvs2svn to accommodate a server-side copy/move.
    svn path=/trunk/kdelibs/; revision=94568

---
+++ kdecore/ksycoca.cpp
@@ -0,0 +1,348 @@
+/*  This file is part of the KDE libraries
+ *  Copyright (C) 1999-2000 Waldo Bastian <bastian@kde.org>
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Library General Public
+ *  License version 2 as published by the Free Software Foundation;
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Library General Public License
+ *  along with this library; see the file COPYING.LIB.  If not, write to
+ *  the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ *  Boston, MA 02111-1307, USA.
+ **/
+
+#include "config.h"
+
+#include "ksycoca.h"
+#include "ksycocatype.h"
+#include "ksycocafactory.h"
+#include "kuserprofile.h"
+
+#include <qdatastream.h>
+#include <qfile.h>
+#include <qbuffer.h>
+
+#include <kapp.h>
+#include <dcopclient.h>
+#include <kglobal.h>
+#include <kdebug.h>
+#include <kprocess.h>
+#include <kstddirs.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#ifdef HAVE_SYS_MMAN_H
+#include <sys/mman.h>
+#endif
+
+template class QList<KSycocaFactory>;
+
+struct KSycocaPrivate {
+    KSycocaPrivate() {
+        database = 0;
+    }
+    QFile *database;
+    QStringList changeList;
+};
+
+// Read-only constructor
+KSycoca::KSycoca()
+  : DCOPObject("ksycoca"), m_lstFactories(0), m_str(0), bNoDatabase(false),
+    m_sycoca_size(0), m_sycoca_mmap(0)
+{
+   d = new KSycocaPrivate;
+   openDatabase();
+   _self = this;
+   // Register app as able to receive DCOP messages
+   if (kapp && !kapp->dcopClient()->isAttached())
+   {
+      kapp->dcopClient()->attach();
+   }
+}
+
+bool KSycoca::openDatabase( bool openDummyIfNotFound )
+{
+   m_sycoca_mmap = 0;
+   m_str = 0;
+   QString path = KGlobal::dirs()->saveLocation("tmp") + "ksycoca";
+   //kdDebug(7011) << "Trying to open ksycoca from " << path << endl;
+   delete d->database;
+   d->database = 0L;
+   QFile *database = new QFile(path);
+   if (database->open( IO_ReadOnly ))
+   {
+     m_sycoca_size = database->size();
+#ifdef HAVE_MMAP
+     m_sycoca_mmap = (const char *) mmap(0, m_sycoca_size,
+                                PROT_READ, MAP_SHARED,
+                                database->handle(), 0);
+     if (!m_sycoca_mmap)
+     {
+        kdDebug(7011) << "mmap failed. (length = " << m_sycoca_size << ")" << endl;
+#endif
+        m_str = new QDataStream(database);
+#ifdef HAVE_MMAP
+     }
+     else
+     {
+        QByteArray b_array;
+        b_array.setRawData(m_sycoca_mmap, m_sycoca_size);
+        QBuffer *buffer = new QBuffer( b_array );
+        buffer->open(IO_ReadWrite);
+        m_str = new QDataStream( buffer);
+     }
+#endif
+     bNoDatabase = false;
+   }
+   else
+   {
+     // No database file
+
+     bNoDatabase = true;
+     if (!openDummyIfNotFound)
+       return false;
+
+     // We open a dummy database instead.
+     //kdDebug(7011) << "No database, opening a dummy one." << endl;
+     QBuffer *buffer = new QBuffer( QByteArray() );
+     buffer->open(IO_ReadWrite);
+     m_str = new QDataStream( buffer);
+     (*m_str) << (Q_INT32) KSYCOCA_VERSION;
+     (*m_str) << (Q_INT32) 0;
+   }
+   m_lstFactories = new KSycocaFactoryList();
+   m_lstFactories->setAutoDelete( true );
+   d->database = database;
+   return true;
+}
+
+// Read-write constructor - only for KBuildSycoca
+KSycoca::KSycoca( bool /* dummy */ )
+  : DCOPObject("kbuildsycoca"), m_lstFactories(0), m_str(0), bNoDatabase(false),
+    m_sycoca_size(0), m_sycoca_mmap(0)
+{
+   d = new KSycocaPrivate;
+   m_lstFactories = new KSycocaFactoryList();
+   m_lstFactories->setAutoDelete( true );
+   _self = this;
+}
+
+static void delete_ksycoca_self() {
+  delete KSycoca::self();
+}
+
+KSycoca * KSycoca::self()
+{
+    if (!_self) {
+        qAddPostRoutine(delete_ksycoca_self);
+        _self = new KSycoca();
+    }
+  return _self;
+}
+
+KSycoca::~KSycoca()
+{
+   closeDatabase();
+   if (d->database != 0) delete d->database;
+   delete d;
+   _self = 0L;
+}
+
+void KSycoca::closeDatabase()
+{
+   QIODevice *device = 0;
+   if (m_str)
+      device = m_str->device();
+#ifdef HAVE_MMAP
+   if (device && m_sycoca_mmap)
+   {
+      QBuffer *buf = (QBuffer *) device;
+      buf->buffer().resetRawData(m_sycoca_mmap, m_sycoca_size);
+      // Solaris has munmap(char*, size_t) and everything else should
+      // be happy with a char* for munmap(void*, size_t)
+      munmap((char*) m_sycoca_mmap, m_sycoca_size);
+      m_sycoca_mmap = 0;
+   }
+#endif
+   if (device)
+      device->close();
+
+   delete m_str;
+   delete device;
+   m_str = 0L;
+   // It is very important to delete all factories here
+   // since they cache information about the database file
+   delete m_lstFactories;
+   m_lstFactories = 0L;
+}
+
+void KSycoca::addFactory( KSycocaFactory *factory )
+{
+   assert(m_lstFactories);
+   m_lstFactories->append(factory);
+}
+
+bool KSycoca::isChanged(const char *type)
+{
+    return self()->d->changeList.contains(type);
+}
+
+void KSycoca::notifyDatabaseChanged(const QStringList &changeList)
+{
+    d->changeList = changeList;
+    //kdDebug(7011) << "got a notifyDatabaseChanged signal !" << endl;
+    // kded tells us the database file changed
+    // Close the database and forget all about what we knew
+    // The next call to any public method will recreate
+    // everything that's needed.
+    closeDatabase();
+    // same for KUserProfile
+    KServiceTypeProfile::clear();
+
+    // Now notify applications
+    emit databaseChanged();
+}
+
+QDataStream * KSycoca::findEntry(int offset, KSycocaType &type)
+{
+   if ( !m_str )
+      openDatabase();
+   //kdDebug(7011) << QString("KSycoca::_findEntry(offset=%1)").arg(offset,8,16) << endl;
+   m_str->device()->at(offset);
+   Q_INT32 aType;
+   (*m_str) >> aType;
+   type = (KSycocaType) aType;
+   //kdDebug(7011) << QString("KSycoca::found type %1").arg(aType) << endl;
+   return m_str;
+}
+
+bool KSycoca::checkVersion(bool abortOnError)
+{
+   if ( !m_str )
+   {
+      if( !openDatabase(false /* don't open dummy db if not found */) )
+        return false; // No database found
+
+      // We should never get here... if a database was found then m_str shouldn't be 0L.
+      assert(m_str);
+   }
+   m_str->device()->at(0);
+   Q_INT32 aVersion;
+   (*m_str) >> aVersion;
+   if ( aVersion < KSYCOCA_VERSION )
+   {
+      kdWarning(7011) << "Found version " << aVersion << ", expecting version " << KSYCOCA_VERSION << " or higher." << endl;
+      if (!abortOnError) return false;
+      kdError(7011) << "Outdated database ! Stop kded and restart it !" << endl;
+      abort();
+   }
+   return true;
+}
+
+QDataStream * KSycoca::findFactory(KSycocaFactoryId id)
+{
+   // The constructor found no database, but we want one
+   if (bNoDatabase)
+   {
+      closeDatabase(); // close the dummy one
+      // Check if new database already available
+      if ( !openDatabase(false /* no dummy one*/) )
+      {
+         static bool triedLaunchingKdeinit = false;
+         if (!triedLaunchingKdeinit) // try only once
+         {
+           triedLaunchingKdeinit = true;
+           kdDebug(7011) << "findFactory: we have no database.... launching kdeinit" << endl;
+           KProcess proc;
+           proc << locate("exe","kdeinit");
+           proc.start( KProcess::Block );
+           // Ok, the new database should be here now, open it.
+         }
+         if (!openDatabase(false))
+            return 0L; // Still no database - maybe kdeinit was already running and it's too early
+      }
+   }
+   // rewind and check
+   if (!checkVersion(false))
+   {
+     kdWarning(7011) << "Outdated database found" << endl;
+     return 0L;
+   }
+   Q_INT32 aId;
+   Q_INT32 aOffset;
+   while(true)
+   {
+      (*m_str) >> aId;
+      //kdDebug(7011) << QString("KSycoca::findFactory : found factory %1").arg(aId) << endl;
+      if (aId == 0)
+      {
+         kdError(7011) << "Error, KSycocaFactory (id = " << int(id) << ") not found!" << endl;
+         break;
+      }
+      (*m_str) >> aOffset;
+      if (aId == id)
+      {
+         //kdDebug(7011) << QString("KSycoca::findFactory(%1) offset %2").arg((int)id).arg(aOffset) << endl;
+         m_str->device()->at(aOffset);
+         return m_str;
+      }
+   }
+   return 0;
+}
+
+QString KSycoca::kfsstnd_prefixes()
+{
+   if (bNoDatabase) return "";
+   if (!checkVersion(false)) return "";
+   Q_INT32 aId;
+   Q_INT32 aOffset;
+   // skip factories offsets
+   while(true)
+   {
+      (*m_str) >> aId;
+      if ( aId )
+        (*m_str) >> aOffset;
+      else
+        break; // just read 0
+   }
+   // We now point to the header
+   QString prefixes;
+   (*m_str) >> prefixes;
+   (*m_str) >> m_timeStamp;
+   return prefixes;
+}
+
+Q_UINT32 KSycoca::timeStamp()
+{
+   if (!m_timeStamp)
+      (void) kfsstnd_prefixes();
+   return m_timeStamp;
+}
+
+QString KSycoca::determineRelativePath( const QString & _fullpath, const char *_resource )
+{
+  QString sRelativeFilePath;
+  QStringList dirs = KGlobal::dirs()->resourceDirs( _resource );
+  QStringList::ConstIterator dirsit = dirs.begin();
+  for ( ; dirsit != dirs.end() && sRelativeFilePath.isEmpty(); ++dirsit ) {
+    // might need canonicalPath() ...
+    if ( _fullpath.find( *dirsit ) == 0 ) // path is dirs + relativePath
+      sRelativeFilePath = _fullpath.mid( (*dirsit).length() ); // skip appsdirs
+  }
+  if ( sRelativeFilePath.isEmpty() )
+    kdFatal(7011) << QString("Couldn't find %1 in any %2 dir !!!").arg( _fullpath ).arg( _resource) << endl;
+  //else
+    // debug code
+    //kdDebug(7011) << sRelativeFilePath << endl;
+  return sRelativeFilePath;
+}
+
+KSycoca * KSycoca::_self = 0L;
+
+#include "ksycoca.moc"
---
+++ kdecore/ksycoca.h
@@ -0,0 +1,142 @@
+/*  This file is part of the KDE libraries
+ *  Copyright (C) 1999 Waldo Bastian <bastian@kde.org>
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Library General Public
+ *  License version 2 as published by the Free Software Foundation;
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Library General Public License
+ *  along with this library; see the file COPYING.LIB.  If not, write to
+ *  the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ *  Boston, MA 02111-1307, USA.
+ **/
+
+#ifndef __ksycoca_h__
+#define __ksycoca_h__ "$Id$"
+
+#include <dcopobject.h>
+#include <qobject.h>
+#include "ksycocatype.h"
+
+class QDataStream;
+class KSycocaPrivate;
+class KSycocaFactory;
+class KSycocaFactoryList;
+
+/**
+ * Sycoca file version number.
+ * If the existing file is outdated, it will not get read
+ * but instead we'll ask kded to regenerate a new one...
+*/
+#define KSYCOCA_VERSION 34
+
+/**
+ * @internal
+ * Read-only SYstem COnfiguration CAche
+ */
+class KSycoca : public QObject, public DCOPObject
+{
+  Q_OBJECT
+  K_DCOP
+
+protected:
+   /**
+    * @internal
+    * Building database
+    */
+   KSycoca( bool /* buildDatabase */ );
+
+public:
+
+   /**
+    * Read-only database
+    */
+   KSycoca();
+
+   /**
+    * Get or create the only instance of KSycoca (read-only)
+    */
+   static KSycoca *self();
+
+   virtual ~KSycoca();
+
+   /**
+    * @internal - called by factories in read-only mode
+    * This is how factories get a stream to an entry
+    */
+   QDataStream *findEntry(int offset, KSycocaType &type);
+   /**
+    * @internal - called by factories in read-only mode
+    */
+   QDataStream *findFactory( KSycocaFactoryId id);
+   /**
+    * @internal - returns kfsstnd stored inside database
+    */
+   QString kfsstnd_prefixes();
+
+   /**
+    * @internal - returns timestamp of database
+    *
+    * The database contains all changes made _before_ this time and
+    * _might_ contain changes made after that.
+    */
+   Q_UINT32 timeStamp();
+
+   /**
+    * @internal - add a factory
+    */
+   void addFactory( KSycocaFactory * );
+
+   /**
+    * @internal
+    * @return true if building (i.e. if a KBuildSycoca);
+    */
+   virtual bool isBuilding() { return false; }
+
+   /**
+    * Determine relative path for a .desktop file from a full path and a resource name
+    */
+   static QString determineRelativePath( const QString & _fullpath, const char *_resource );
+
+   /**
+    * When you receive a "databaseChanged" signal, you can query here if
+    * a change has occured in a specific resource type.
+    * @see KStandardDirs for the various resource types.
+    */
+   static bool isChanged(const char *type);
+
+k_dcop:
+   /**
+    * internal function for receiving kded/kbuildsycoca's signal, when the sycoca file changes
+    */
+   void notifyDatabaseChanged(const QStringList &);
+
+signals:
+   /**
+        * Connect to this to get notified when the database changes
+        * (Usually apps showing icons do a 'refresh' to take into account the new mimetypes)
+        */
+   void databaseChanged();
+
+protected:
+   bool checkVersion(bool abortOnError=true);
+   bool openDatabase(bool openDummyIfNotFound=true);
+   void closeDatabase();
+   KSycocaFactoryList *m_lstFactories;
+   QDataStream *m_str;
+   static KSycoca *_self;
+   bool bNoDatabase;
+   size_t m_sycoca_size;
+   const char *m_sycoca_mmap;
+   Q_UINT32 m_timeStamp;
+
+private:
+   KSycocaPrivate *d;
+};
+
+#endif
