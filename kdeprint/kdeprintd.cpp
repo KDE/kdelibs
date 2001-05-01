29ef5d5cb2947a0a690b89182383f49e5927b50e
Michael Goffioul <kdeprint@swing.be>
2001-05-01 16:45:17 +0000

    Addition to previous commit (forgot to add new files).
    svn path=/trunk/kdelibs/; revision=94656

---
+++ kdeprint/kdeprintd.cpp
@@ -0,0 +1,110 @@
+/*
+ *  This file is part of the KDE libraries
+ *  Copyright (c) 2001 Michael Goffioul <goffioul@imec.be>
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Library General Public
+ *  License version 2 as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Library General Public License
+ *  along with this library; see the file COPYING.LIB.  If not, write to
+ *  the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ *  Boston, MA 02111-1307, USA.
+ **/
+
+#include "kdeprintd.h"
+#include "kprintprocess.h"
+
+#include <qfile.h>
+#include <klocale.h>
+#include <knotifyclient.h>
+#include <kdebug.h>
+#include <dcopclient.h>
+
+extern "C"
+{
+	KDEDModule *create_kdeprintd(const QCString& name)
+	{
+		return new KDEPrintd(name);
+	}
+}
+
+static void cleanFileList(const QStringList& files)
+{
+	for (QStringList::ConstIterator it=files.begin(); it!=files.end(); ++it)
+		QFile::remove(*it);
+}
+
+//*****************************************************************************************************
+
+KDEPrintd::KDEPrintd(const QCString& obj)
+: KDEDModule(obj)
+{
+	m_processpool.setAutoDelete(true);
+	m_tempfiles.setAutoDelete(true);
+}
+
+KDEPrintd::~KDEPrintd()
+{
+	cleanTempFiles();
+}
+
+int KDEPrintd::print(const QString& cmd, const QStringList& files, bool remflag)
+{
+	KPrintProcess	*proc = new KPrintProcess;
+	connect(proc,SIGNAL(processExited(KProcess*)),SLOT(slotProcessExited(KProcess*)));
+	*proc << cmd;
+	if (remflag)
+		m_tempfiles.insert(proc,new QStringList(files));
+	if (proc->print())
+	{
+		m_processpool.append(proc);
+		return (int)(proc->pid());
+	}
+	else
+	{
+		cleanTempFile(proc);
+		delete proc;
+		return (-1);
+	}
+}
+
+void KDEPrintd::cleanTempFiles()
+{
+	QPtrDictIterator<QStringList>	it(m_tempfiles);
+	for (;it.current();++it)
+		cleanFileList(*(it.current()));
+}
+
+void KDEPrintd::cleanTempFile(KProcess *p)
+{
+	QStringList	*l = m_tempfiles.find(p);
+	if (l)
+		cleanFileList(*l);
+}
+
+void KDEPrintd::slotProcessExited(KProcess *proc)
+{
+	KPrintProcess	*pproc = (KPrintProcess*)proc;
+	if (m_processpool.findRef(pproc) != -1)
+	{
+		m_processpool.take();
+		QString		msg;
+		if (!pproc->normalExit())
+			msg = i18n("<nobr>Abnormal process termination (<b>%1</b>).</nobr>").arg(pproc->args()->first());
+		else if (pproc->exitStatus() != 0)
+			msg = i18n("<b>%1</b>: execution failed with message:<p>%2</p>").arg(pproc->args()->first()).arg(pproc->errorMessage());
+		cleanTempFile(pproc);
+
+		delete pproc;
+		if (!msg.isEmpty())
+			KNotifyClient::event("printerror",i18n("<p><nobr>A print error occured. Error message received from system:</nobr></p><br>%1").arg(msg));
+	}
+}
+
+#include "kdeprintd.moc"
---
+++ kdeprint/kdeprintd.h
@@ -0,0 +1,55 @@
+/*
+ *  This file is part of the KDE libraries
+ *  Copyright (c) 2001 Michael Goffioul <goffioul@imec.be>
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Library General Public
+ *  License version 2 as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Library General Public License
+ *  along with this library; see the file COPYING.LIB.  If not, write to
+ *  the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ *  Boston, MA 02111-1307, USA.
+ **/
+
+#ifndef KDEPRINTD_H
+#define KDEPRINTD_H
+
+#include <kdedmodule.h>
+#include <qlist.h>
+#include <qstringlist.h>
+#include <qptrdict.h>
+
+class KPrintProcess;
+class KProcess;
+
+class KDEPrintd : public KDEDModule
+{
+	Q_OBJECT
+	K_DCOP
+
+public:
+	KDEPrintd(const QCString& obj);
+	~KDEPrintd();
+
+k_dcop:
+	int print(const QString& cmd, const QStringList& files, bool remove);
+
+protected slots:
+	void slotProcessExited(KProcess*);
+
+protected:
+	void cleanTempFiles();
+	void cleanTempFile(KProcess*);
+
+private:
+	QList<KPrintProcess>	m_processpool;
+	QPtrDict<QStringList>	m_tempfiles;
+};
+
+#endif
