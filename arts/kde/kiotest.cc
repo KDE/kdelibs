8b979eadffc3f94e20f1fe1b5db71ad253f4e1f7
Nikolas Zimmermann <wildfox@kde.org>
2001-05-03 21:26:29 +0000

    Works now! s/qapp/kapp/
    svn path=/trunk/kdelibs/; revision=95018

--- arts/kde/kiotest.cc
+++ arts/kde/kiotest.cc
@@ -1,33 +1,50 @@
 #include <stdio.h>
 #include <kmedia2.h>
+#include <kcmdlineargs.h>
 #include <connect.h>
-#include <qapplication.h>
+#include <klocale.h>
+#include <kapp.h>
+#include <kaboutdata.h>
 #include "qiomanager.h"
 #include "artskde.h"
 
 using namespace std;
 using namespace Arts;
 
+
+static KCmdLineOptions options[] =
+{
+    { "+[URL]", I18N_NOOP("URL to open"), 0 },
+    { 0, 0, 0 }
+};
+
 int main(int argc, char **argv)
 {
-	if(argc != 2)
-	{
-		fprintf(stderr, "usage: testkio <url>\n");
-		exit(1);
-	}
-
-	QApplication app(argc, argv);
+	KAboutData aboutData( "kiotest", I18N_NOOP("KIOTest"), I18N_NOOP("0.1"), "", KAboutData::License_GPL, "");
+							  
+	KCmdLineArgs::init(argc,argv,&aboutData);
+	KCmdLineArgs::addCmdLineOptions(options); 	
+	KApplication app;
 	QIOManager qiomanager;
 	Dispatcher dispatcher(&qiomanager);
 	KIOInputStream stream;
 	StdoutWriter writer;
 
-	if(!stream.openURL(argv[1]))
-	{
-		printf("can't open file %s\n",argv[1]);
-		exit(1);
-	}
+	KCmdLineArgs *args = KCmdLineArgs::parsedArgs();
 
+	if(args->count())
+	{
+	    if(!stream.openURL(args->arg(0)))
+	    {
+		printf("can't open url");
+		exit(1);
+	    }
+	}
+	else
+	    exit(1);
+	    
+	args->clear();
+	
 	connect(stream, writer);
 
 	stream.start();
