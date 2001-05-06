60cbe7316d16b66d29361df366ff0145d2463228
David Faure <faure@kde.org>
2001-05-06 20:50:19 +0000

    Improved test program, adding a real object to be able to have slots. Well, it seems the duplication bug (arkarkarkark in kopenwith) isn't related to kcombobox !??
    svn path=/trunk/kdelibs/; revision=95509

--- kdeui/tests/kcomboboxtest.cpp
+++ kdeui/tests/kcomboboxtest.cpp
@@ -1,3 +1,4 @@
+#include "kcomboboxtest.h"
 #include <qlayout.h>
 #include <qpushbutton.h>
 #include <qlabel.h>
@@ -10,35 +11,33 @@
 #include <kiconloader.h>
 #include <ksimpleconfig.h>
 
-int main ( int argc, char **argv)
-{
-    KApplication a(argc, argv, "kcomboboxtest");
 
-    // Make a central widget to contain the other widgets
-    QWidget * w = new QWidget();
+Widget::Widget()
+ : QWidget()
+{
     // Insert the widget container (parent widget) into
     // a layout manager (VERTICAL).
-    QVBoxLayout *vbox = new QVBoxLayout( w, KDialog::marginHint(), KDialog::spacingHint() );
+    QVBoxLayout *vbox = new QVBoxLayout( this, KDialog::marginHint(), KDialog::spacingHint() );
     // Resize the widget
-    w->resize( 500, 100 );
-
+    resize( 500, 100 );
+ 
     // Popuplate the select-only list box
     QStringList list;
     list << "Stone" << "Tree" << "Peables" << "Ocean" << "Sand" << "Chips" << "Computer" << "Mankind";
     list.sort();
-
+ 
     // Create and modify read-write widget
-    KComboBox *rwc = new KComboBox( true, w, "rwcombobox" );
-    QLabel* lblrw = new QLabel( rwc, "&Editable ComboBox", w, "rwcombolabel" );
+    rwc = new KComboBox( true, this, "rwcombobox" );
+    QLabel* lblrw = new QLabel( rwc, "&Editable ComboBox", this, "rwcombolabel" );
     rwc->setDuplicatesEnabled( true );
     rwc->completionObject()->setItems( list );
     rwc->setInsertionPolicy( QComboBox::NoInsertion );
     rwc->insertStringList( list );
     rwc->setEditText( "file:/home/adawit/" );
     // rwc->setCompletionMode( KGlobalSettings::CompletionAuto );
-
+ 
     // Create a read-write combobox and reproduce konqueror's code
-    KComboBox *konqc = new KComboBox( true, w, "konqc" );
+    konqc = new KComboBox( true, this, "konqc" );
     konqc->setMaxCount( 10 );
       KSimpleConfig historyConfig( "konq_history" );
       historyConfig.setGroup( "History" );
@@ -47,22 +46,23 @@ int main ( int argc, char **argv)
       s_pCompletion->setItems( historyConfig.readListEntry( "CompletionItems" ) );
       s_pCompletion->setCompletionMode( KGlobalSettings::completionMode() );
       konqc->setCompletionObject( s_pCompletion, false );
-    QLabel* lblkonq = new QLabel( konqc, "&Konqueror's ComboBox", w );
+    QLabel* lblkonq = new QLabel( konqc, "&Konqueror's ComboBox", this );
     QPixmap pix = SmallIcon("www");
     konqc->insertItem( pix, "http://www.kde.org" );
     konqc->setCurrentItem( konqc->count()-1 );
     kdDebug() << "setLocationBarURL setCurrentItem " << konqc->count()-1 << endl;
 
     // Create a read-only widget
-    KComboBox *soc = new KComboBox( w, "socombobox" );
-    QLabel* lblso = new QLabel( soc, "&Select-Only ComboBox", w, "socombolabel" );
+    soc = new KComboBox( this, "socombobox" );
+    QLabel* lblso = new QLabel( soc, "&Select-Only ComboBox", this, "socombolabel" );
     soc->setCompletionMode( KGlobalSettings::CompletionAuto );
     soc->completionObject()->setItems( list );
     soc->insertStringList( list );
 
     // Create an exit button
-    QPushButton * push = new QPushButton( "E&xit", w );
-    QObject::connect( push, SIGNAL( clicked() ), &a, SLOT( closeAllWindows() ) );
+    QPushButton * push = new QPushButton( "E&xit", this );
+    QObject::connect( push, SIGNAL( clicked() ), this, SLOT( slotDisplay() ) );
+    QObject::connect( push, SIGNAL( clicked() ), kapp, SLOT( closeAllWindows() ) );
 
     // Insert the widgets into the layout manager.
     vbox->addWidget( lblrw );
@@ -73,8 +73,25 @@ int main ( int argc, char **argv)
     vbox->addWidget( konqc );
     vbox->addWidget( push );
 
-    a.setMainWidget(w);
     rwc->setFocus();
+}
+
+void Widget::slotDisplay()
+{
+    kdDebug() << "rwc: " << rwc->currentText() << endl;
+    kdDebug() << "konqc: " << konqc->currentText() << endl;
+    kdDebug() << "soc: " << soc->currentText() << endl;
+}
+
+int main ( int argc, char **argv)
+{
+    KApplication a(argc, argv, "kcomboboxtest");
+
+    // Make a central widget to contain the other widgets
+    QWidget * w = new Widget();
+    a.setMainWidget(w);
     w->show();
     return a.exec();
 }
+
+#include "kcomboboxtest.moc"
---
+++ kdeui/tests/kcomboboxtest.h
@@ -0,0 +1,20 @@
+#ifndef somethinghere
+#define somethinghere
+
+#include <qwidget.h>
+class KComboBox;
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+public:
+    Widget();
+private slots:
+    void slotDisplay();
+private:
+    KComboBox * rwc;
+    KComboBox * konqc;
+    KComboBox * soc;
+};
+
+#endif
