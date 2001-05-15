a5a2505300b89d1058f9101725152af06ba67be5
Waldo Bastian <bastian@kde.org>
2001-05-15 00:03:13 +0000

    Force update, added KProtocolInfo::config()
    svn path=/trunk/kdelibs/; revision=97121

--- kdecore/ksycoca.h
+++ kdecore/ksycoca.h
@@ -33,7 +33,7 @@ class KSycocaFactoryList;
  * If the existing file is outdated, it will not get read
  * but instead we'll ask kded to regenerate a new one...
 */
-#define KSYCOCA_VERSION 35
+#define KSYCOCA_VERSION 36
 
 /**
  * @internal
