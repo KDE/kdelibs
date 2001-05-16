c49ae6ec4898e15c53210ea21a63e1f88c1257c0
Nikolas Zimmermann <wildfox@kde.org>
2001-05-16 19:58:18 +0000

    Adding SHOUTcast support
    svn path=/trunk/kdelibs/; revision=97450

--- arts/kde/kioinputstream_impl.cpp
+++ arts/kde/kioinputstream_impl.cpp
@@ -26,6 +26,7 @@
 #include <kio/jobclasses.h>
 #include <qtimer.h>
 #include <qdatastream.h>
+#include "artsversion.h"
 #include "kioinputstream_impl.moc"
 
 using namespace Arts;
@@ -54,6 +55,9 @@ void KIOInputStream_impl::streamStart()
 	if(m_job != 0)
 		m_job->kill();
 	m_job = KIO::get(m_url, false, false);
+	m_job->addMetaData("accept", "audio/x-mp3");
+	m_job->addMetaData("UserAgent", QString::fromLatin1("aRts/") + QString::fromLatin1(ARTS_VERSION));
+	
 	QObject::connect(m_job, SIGNAL(data(KIO::Job *, const QByteArray &)),
 			 this, SLOT(slotData(KIO::Job *, const QByteArray &)));		     
 	QObject::connect(m_job, SIGNAL(result(KIO::Job *)),
