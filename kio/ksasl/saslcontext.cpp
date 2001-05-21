45a4ea55af8e1db0f3aeb45eb3dc889070cad0ff
Alex Zepeda <zipzippy@sonic.net>
2001-05-21 12:17:32 +0000

    Add a copyright statement, kdocify a few things, and shuffle around some formatting stuff.
    svn path=/trunk/kdelibs/; revision=98048

--- kio/ksasl/saslcontext.cpp
+++ kio/ksasl/saslcontext.cpp
@@ -1,4 +1,32 @@
-// $Id$
+/*-
+ * Copyright (c) 2000,2001 Alex Zepeda <jazepeda@pacbell.net>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Redistributions of source code or in binary form must consent to
+ *    future terms and conditions as set forth by the original author.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $Id$
+ */
 
 #include <qdict.h>
 #include <qstring.h>
@@ -6,8 +34,8 @@
 
 #include <kdebug.h>
 #include <klibloader.h>
-#include <ktrader.h>
 #include <kmdcodec.h>
+#include <ktrader.h>
 
 #include "saslcontext.h"
 #include "saslmodule.h"
--- kio/ksasl/saslmodule.h
+++ kio/ksasl/saslmodule.h
@@ -6,19 +6,47 @@
 
 #include <kurl.h>
 
+/**
+  * Provides a generic client side authentication API suitable for use
+  * with any SASL compatible protocol or authentication method.
+  *
+  * @author Alex Zepeda <jazepeda@pacbell.net>
+  * @version $Revision$
+  **/
 class KSASLAuthModule
 	: public QObject {
-public:
-	KSASLAuthModule(QObject *parent, const char *name):QObject(parent,name) {;}
-	virtual ~KSASLAuthModule(){;}
-	virtual QString auth_method()=0;
 
-	// If you need to do multi pass authentication you need to set the
-	// property "more" to true in all but the last pass, and to
-	// false in the last pass.
-	// E.g. with setProperty("more", QVariant(true,0));
-	virtual QString auth_response(const QString &challenge, const KURL &auth_url)=0;
-	virtual void get_usercredentials()=0;
+public:
+
+	/**
+	  * Default ctor.  Of no use really.
+	  **/
+	KSASLAuthModule (QObject *parent, const char *name)
+		: QObject(parent, name) {;}
+
+	/**
+	  * Default destructor.
+	  **/
+	virtual ~KSASLAuthModule () {;}
+
+	/**
+	  * Returns the IANA sanctioned name for the authentication 
+	  * method as per RFC2222.
+	  **/
+	virtual QString auth_method () = 0;
+
+	/**
+	  * If you need to do multi pass authentication you need to set the
+	  * property "more" to true in all but the last pass, and to
+	  * false in the last pass.
+	  * e.g. with setProperty("more", QVariant(true,0));
+	  **/
+	virtual QString auth_response (const QString &challenge, const KURL &auth_url) = 0;
+
+	/**
+	  * Hmm.
+	  **/
+	virtual void get_usercredentials () = 0;
 };
 
 #endif
