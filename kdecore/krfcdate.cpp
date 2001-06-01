ed9784583dc03238c808054a4a998de04b792b16
Christian Gebauer <gebauer@kde.org>
2001-06-01 12:20:07 +0000

    * KRFCDate::parseDate is now RFC822/RFC2822 compliant:  - day-of-week and seconds are optional  - rfc*822 allows FWSP between the tokens    => check with isspace() instead of == ' '  - time zone parsing *  KRFCDate::parseDate now returns UTC * a new method int localUTCOffset(); which returns the   local offset to UTC in minutes * a new method QCString rfc2822DateString() which formats   a rfc2822 compliant date string
    svn path=/trunk/kdelibs/; revision=99795

--- kdecore/krfcdate.cpp
+++ kdecore/krfcdate.cpp
@@ -27,18 +27,42 @@
 
 #include <krfcdate.h>
 
-static int ymd_to_days(int year, int mon, int day)
+static unsigned int ymdhms_to_seconds(int year, int mon, int day, int hour, int minute, int second)
 {
-    return (day - 32075)
+    unsigned int ret = (day - 32075)       /* days */
             + 1461L * (year + 4800L + (mon - 14) / 12) / 4
             + 367 * (mon - 2 - (mon - 14) / 12 * 12) / 12
             - 3 * ((year + 4900L + (mon - 14) / 12) / 100) / 4
             - 2440588;
+    ret = 24*ret + hour;     /* hours   */
+    ret = 60*ret + minute;   /* minutes */
+    ret = 60*ret + second;   /* seconds */
+
+    return ret;
 }
 
 static const char haystack[37]="janfebmaraprmayjunjulaugsepoctnovdec";
 
-time_t 
+// we follow the recommendation of rfc2822 to consider all
+// obsolete time zones not listed here equivalent to "-0000"
+static const struct {
+    const char *tzName;
+    int tzOffset;
+} known_zones[] = {
+    { "UT", 0 },
+    { "GMT", 0 },
+    { "EST", -300 },
+    { "EDT", -240 },
+    { "CST", -360 },
+    { "CDT", -300 },
+    { "MST", -420 },
+    { "MDT", -360 },
+    { "PST", -480 },
+    { "PDT", -420 },
+    { 0, 0 }
+};
+
+time_t
 KRFCDate::parseDate(const QString &_date)
 {
      // This parse a date in the form:
@@ -47,43 +71,48 @@ KRFCDate::parseDate(const QString &_date)
      //     Sat, 01-Jan-2000 08:00:00 GMT
      // or
      //     Sat, 01 Jan 2000 08:00:00 GMT
+     // or
+     //     01 Jan 99 22:00 +0100    (exceptions in rfc822/rfc2822)
      //
-     // We always assume GMT, the weekday is ignored
+     // We ignore the weekday
      //
      time_t result = 0;
+     int offset = 0;
      char *newPosStr;
      const char *dateString = _date.latin1();
-     int day;
+     int day = 0;
      char monthStr[4];
-     int month;
-     int year;
-     int hour;
-     int minute;
-     int second;
+     int month = 0;
+     int year = 0;
+     int hour = 0;
+     int minute = 0;
+     int second = 0;
 
-     while(*dateString && (*dateString != ' '))
+     while(*dateString && !isdigit(*dateString))
      	dateString++;
 
      if (!*dateString)
-     	return result;  // Invalid expire date
+     	return result;  // Invalid date
 
      // ' 09-Nov-99 23:12:40 GMT'
      day = strtol(dateString, &newPosStr, 10);
      dateString = newPosStr;
 
      if ((day < 1) || (day > 31))
-     	return result; // Invalid expire date;
+     	return result; // Invalid date;
      if (!*dateString)
-     	return result;  // Invalid expire date
+     	return result;  // Invalid date
 
-     if ((*dateString != '-') && (*dateString != ' '))
-     	return result;  // Invalid expire date
-     dateString++;
+     if (*dateString == '-')
+     	dateString++;
+
+     while(*dateString && isspace(*dateString))
+     	dateString++;
 
      for(int i=0; i < 3;i++)
      {
-         if (!*dateString || (*dateString == '-') || (*dateString == ' '))
-              return result;  // Invalid expire date
+         if (!*dateString || (*dateString == '-') || isspace(*dateString))
+              return result;  // Invalid date
          monthStr[i] = tolower(*dateString++);
      }
      monthStr[3] = '\0';
@@ -91,22 +120,22 @@ KRFCDate::parseDate(const QString &_date)
      newPosStr = (char*)strstr(haystack, monthStr);
 
      if (!newPosStr)
-     	return result;  // Invalid expire date
+     	return result;  // Invalid date
 
      month = (newPosStr-haystack)/3; // Jan=00, Feb=01, Mar=02, ..
 
      if ((month < 0) || (month > 11))
-     	return result;  // Invalid expire date
+     	return result;  // Invalid date
 
-     while(*dateString && (*dateString != '-') && (*dateString != ' '))
+     while(*dateString && (*dateString != '-') && !isspace(*dateString))
      	dateString++;
 
      if (!*dateString)
-     	return result;  // Invalid expire date
+     	return result;  // Invalid date
 
      // '-99 23:12:40 GMT'
-     if ((*dateString != '-') && (*dateString != ' '))
-     	return result;  // Invalid expire date
+     if ((*dateString != '-') && !isspace(*dateString))
+     	return result;  // Invalid date
      dateString++;
 
      // '99 23:12:40 GMT'
@@ -121,62 +150,144 @@ KRFCDate::parseDate(const QString &_date)
          year += 1900;  // Y2K
 
      if ((year < 1900) || (year > 2500))
-     	return result; // Invalid expire date
+     	return result; // Invalid date
 
      if (!*dateString)
-     	return result;  // Invalid expire date
+     	return result;  // Invalid date
 
      // ' 23:12:40 GMT'
-     if (*dateString++ != ' ')
-     	return result;  // Invalid expire date
+     if (!isspace(*dateString++))
+     	return result;  // Invalid date
 
      hour = strtol(dateString, &newPosStr, 10);
      dateString = newPosStr;
 
      if ((hour < 0) || (hour > 23))
-     	return result; // Invalid expire date
+     	return result; // Invalid date
 
      if (!*dateString)
-     	return result;  // Invalid expire date
+     	return result;  // Invalid date
 
      // ':12:40 GMT'
      if (*dateString++ != ':')
-     	return result;  // Invalid expire date
+     	return result;  // Invalid date
 
      minute = strtol(dateString, &newPosStr, 10);
      dateString = newPosStr;
 
      if ((minute < 0) || (minute > 59))
-     	return result; // Invalid expire date
+     	return result; // Invalid date
 
      if (!*dateString)
-     	return result;  // Invalid expire date
+     	return result;  // Invalid date
 
      // ':40 GMT'
-     if (*dateString++ != ':')
-     	return result;  // Invalid expire date
+     if (*dateString != ':' && !isspace(*dateString))
+     	return result;  // Invalid date
 
-     second = strtol(dateString, &newPosStr, 10);
+     // seconds are optional in rfc822 + rfc2822
+     if (*dateString ==':') {
 
-     if ((second < 0) || (second > 59))
-     	return result; // Invalid expire date
+        dateString++;
 
-     result = ymd_to_days(year, month+1, day); /* days */
-     result = 24*result + hour;  /* hours   */
-     result = 60*result + minute;   /* minutes */
-     result = 60*result + second;   /* seconds */
+        second = strtol(dateString, &newPosStr, 10);
+        dateString = newPosStr;
 
-////////////////
-// Debug stuff
-//
-//   printf("time = %ld sec since epoch, ctime=%s\n", result, ctime(&result));
-//
-//   time_t now = time(0);
-//   printf("now = %ld (expire is in %s)\n", now, result > now ? "future" : "past");
-////////////////
+        if ((second < 0) || (second > 59))
+           return result; // Invalid date
+
+     } else {
+        dateString++;
+     }
+
+     while(*dateString && isspace(*dateString))
+     	dateString++;
+
+     // don't fail if the time zone is missing, some
+     // broken mail-/news-clients omit the time zone
+     if (*dateString) {
+
+        if ((*dateString == '+') || (*dateString == '-')) {
+           offset = strtol(dateString, &newPosStr, 10);
+
+           if ((offset < -9959) || (offset > 9959))
+              return result; // Invalid date
+
+           int sgn = (offset < 0)? -1:1;
+           offset = abs(offset);
+           offset = ((offset / 100)*60 + (offset % 100))*sgn;
+        } else {
+           for (int i=0; known_zones[i].tzName != 0; i++) {
+              if (0 == strncasecmp(dateString, known_zones[i].tzName, strlen(known_zones[i].tzName))) {
+                 offset = known_zones[i].tzOffset;
+                 break;
+              }
+           }
+        }
+     }
+
+     result = ymdhms_to_seconds(year, month+1, day, hour, minute, second);
+
+     // avoid negative time values
+     if ((offset > 0) && (offset > result))
+        offset = 0;
+
+     result -= offset*60;
+
      // If epoch 0 return epoch +1 which is Thu, 01-Jan-70 00:00:01 GMT
      // This is so that parse error and valid epoch 0 return values won't
-     // be the same for sensitive applications...
-     if( !result ) result++;
+     // be the same for sensitive applications...     
+     if (result < 1) result = 1;
+
      return result;
+}
+
+
+int KRFCDate::localUTCOffset()
+{
+  time_t timeNow = time((time_t*) 0);
+
+  tm *tM = gmtime(&timeNow);
+  unsigned int timeUTC = ymdhms_to_seconds(tM->tm_year+1900, tM->tm_mon+1, tM->tm_mday,
+                                           tM->tm_hour, tM->tm_min, tM->tm_sec);
+
+  tM = localtime(&timeNow);
+  unsigned int timeLocal = ymdhms_to_seconds(tM->tm_year+1900, tM->tm_mon+1, tM->tm_mday,
+                                             tM->tm_hour, tM->tm_min, tM->tm_sec);
+
+  return (timeLocal-timeUTC)/60;
+}
+
+
+static const char *day_names[] = {
+    "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"
+};
+
+static const char *month_names[] = {
+    "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+};
+
+
+QCString KRFCDate::rfc2822DateString(time_t utcTime, int utcOffset)
+{
+    utcTime += utcOffset * 60;
+    tm *tM = gmtime(&utcTime);
+    char sgn = (utcOffset < 0) ? '-' : '+';
+    int z = (utcOffset < 0) ? -utcOffset : utcOffset;
+    QCString dateStr;
+
+    dateStr.sprintf("%s, %02d %s %04d %02d:%02d:%02d %c%02d%02d",
+                    day_names[tM->tm_wday], tM->tm_mday,
+                    month_names[tM->tm_mon], tM->tm_year+1900,
+                    tM->tm_hour, tM->tm_min, tM->tm_sec,
+                    sgn, z/60%24, z%60);
+
+    return dateStr;
+}
+
+
+QCString KRFCDate::rfc2822DateString(time_t utcTime)
+{
+    return rfc2822DateString(utcTime, localUTCOffset());
 }
--- kdecore/krfcdate.h
+++ kdecore/krfcdate.h
@@ -5,7 +5,7 @@
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
    License version 2 as published by the Free Software Foundation.
-   
+
    This library is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
@@ -33,17 +33,50 @@ class KRFCDate
 public:
    /**
     * This function tries to parse a string containing a date/time in any
-    * of the formats specified by RFC822, RFC850, RFC1036 and RFC1123.
+    * of the formats specified by RFC822, RFC850, RFC1036, RFC1123 and RFC2822.
     *
     * If the date/time could not be parsed, 0 is returned.  If the
     * parsed date is epoch, then epoch+1 is returned so that a valid
     * date will not be confused with an improper date string.
     *
-    * The date/time returned is converted to the current timezone.
+    * The date/time returned is converted to UTC.
     *
-    * BUGS: Not all possible formats are covered yet.
     */
    static time_t parseDate(const QString &);
+
+
+  /**
+   * Returns the local timezone offset to UTC in seconds
+   *
+   */
+   static int localUTCOffset();
+
+
+  /**
+   * Returns a string representation of the given date and time formated
+   * in conformance to RFC2822.
+   *
+   * @param utcTime    a date and time in UTC
+   * @param utcOffset  the offset to UTC in seconds
+   *
+   */
+
+   static QCString rfc2822DateString(time_t utcTime, int utcOffset);
+
+
+  /**
+   * Returns a string representation of the given date and time formated
+   * in conformance to RFC2822.
+   *
+   * Provided for convenience, the function is equivalent to
+   * rfc2822DateString(t, localUTCOffset()).
+   *
+   * @param utcTime    a date and time in UTC
+   *
+   */
+
+   static QCString rfc2822DateString(time_t utcTime);
+
 };
 
 #endif
