9ad3256dcc0fa282a86b81c2d4ff9d4a2b371680
Lukáš Tinkl <lukas@kde.org>
2001-06-04 16:06:09 +0000

    Really reverting clipboard changes ;-)
    Hi Waldo :))))
    
    svn path=/trunk/kdelibs/; revision=100390

--- kdecore/kapp.cpp
+++ kdecore/kapp.cpp
@@ -1698,16 +1698,6 @@ void KApplication::propagateSettings(SettingsCategory arg)
     b = config->readBoolEntry("EffectFadeTooltip", false);
     QApplication::setEffectEnabled( Qt::UI_FadeTooltip, b);
 
-#ifdef QT_AUTO_COPY_TO_CLIPBOARD
-    b = config->readBoolEntry("AutoCopyToClipboard", false);
-    QApplication::setAutoCopyToClipboard(b);
-#endif
-
-#ifdef QT_AUTO_COPY_TO_CLIPBOARD
-    b = config->readBoolEntry("AutoCopyToClipboard", false);
-    QApplication::setAutoCopyToClipboard(b);
-#endif
-
     emit settingsChanged(arg);
 }
 
