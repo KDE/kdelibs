b8bddeb549c7e273be011eaf622852a64f844520
Dirk Mueller <mueller@kde.org>
2001-07-25 10:25:44 +0000

    fixing the grave compile error if TOKEN_PRINT is used
    svn path=/trunk/kdelibs/; revision=107595

--- khtml/html/htmltokenizer.cpp
+++ khtml/html/htmltokenizer.cpp
@@ -30,7 +30,6 @@
 
 //#define TOKEN_DEBUG 1
 //#define TOKEN_DEBUG 2
-//#define TOKEN_PRINT
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -300,7 +299,7 @@ void HTMLTokenizer::parseListing( DOMStringIt &src)
             if (script) {
                 if (!scriptSrc.isEmpty()) {
                     // forget what we just got; load from src url instead
-                    cachedScript = parser->doc()->docLoader()->requestScript(scriptSrc, parser->doc()->baseURL(), scriptSrcCharset);
+                    cachedScript = parser->doc()->docLoader()->requestScript(scriptSrc, scriptSrcCharset);
                     scriptSrc="";
                 }
                 else {
@@ -1534,14 +1533,6 @@ void HTMLTokenizer::processToken()
 
     dest = buffer;
 
-#ifdef TOKEN_PRINT
-    QString name = getTagName(currToken.id).string();
-    QString text = currToken.text.string();
-
-    kdDebug( 6036 ) << "Token --> " << name << "   id = " << currToken.id << endl;
-    if(currToken.text != 0)
-        kdDebug( 6036 ) << "text: \"" << text << "\"" << endl;
-#else
 #ifdef TOKEN_DEBUG
     QString name = getTagName(currToken.id).string();
     QString text;
@@ -1565,7 +1556,6 @@ void HTMLTokenizer::processToken()
         }
     }
     kdDebug( 6036 ) << endl;
-#endif
 #endif
     // pass the token over to the parser, the parser DOES NOT delete the token
     parser->parseToken(&currToken);
