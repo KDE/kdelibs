afd95e1da021bd775f8cc7ff0a02e415c5c4c790
George Staikos <staikos@kde.org>
2001-08-08 11:03:01 +0000

    Re-enable serial number output
    svn path=/trunk/kdelibs/; revision=109605

--- kcert/kcertpart.cc
+++ kcert/kcertpart.cc
@@ -120,7 +120,7 @@ QCString pass;
          QString validText("Certificate is valid from %1 to %2.");
          _validPeriod->setText(validText.arg(xc->getNotBefore()).arg(xc->getNotAfter()));
 
-  //_serialNum->setText(xc->getSerialNumber());
+  _serialNum->setText(xc->getSerialNumber());
   _certState->setText(KSSLCertificate::verifyText(p12->validate()));
 
 delete p12;
