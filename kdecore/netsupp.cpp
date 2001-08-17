b2f2fdf5665f3004bc12fded190795cb0d6664b3
Waldo Bastian <bastian@kde.org>
2001-08-17 04:30:42 +0000

    Patch by Nick Sayer / Will Andrews <will@physics.purdue.edu> -  if (serv != NULL && (*serv == '*' && serv[1] == '\0') || *serv == '\0') +  if (serv != NULL && ((*serv == '*' && serv[1] == '\0') || *serv == '\0'))
    svn path=/branches/KDE_2_2_BRANCH/kdelibs/; revision=110699

--- kdecore/netsupp.cpp
+++ kdecore/netsupp.cpp
@@ -781,7 +781,7 @@ int getaddrinfo(const char *name, const char *serv,
   if (name != NULL && ((*name == '*' && name[1] == '\0') || *name == '\0'))
     name = NULL;
   // Treat service of "*" as NULL, which I guess means no port (0)
-  if (serv != NULL && (*serv == '*' && serv[1] == '\0') || *serv == '\0')
+  if (serv != NULL && ((*serv == '*' && serv[1] == '\0') || *serv == '\0'))
     serv = NULL;
 
   if (name == NULL && serv == NULL) // what the hell do you want?
