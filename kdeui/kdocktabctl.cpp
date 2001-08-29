22e1c1a054a9b0b7bddd3f13aded6fa40af5d82d
Falk Brettschneider <falk.brettschneider@gmx.de>
2001-08-29 14:13:28 +0000

    exactly this version is also shared with other non-KDE Qt3-projects (see NO_KDE) That's why that #ifdef for Qt3 is necessary to compile with Qt3 again
    svn path=/branches/KDE_2_2_BRANCH/kdelibs/; revision=112420

--- kdeui/kdocktabctl.cpp
+++ kdeui/kdocktabctl.cpp
@@ -24,6 +24,7 @@
 #include <qapplication.h>
 #include <qwmatrix.h>
 #include <qtooltip.h>
+#include <qstyle.h>
 
 #ifndef NO_KDE2
  #include <kdebug.h>
@@ -893,7 +894,11 @@ KDockTabBar::KDockTabBar( QWidget * parent, const char * name )
   QPainter paint;
   paint.begin(left_xpm);
   paint.fillRect (0,0,16,16, QBrush (backgroundColor()));
+#if QT_VERSION < 300
   style().drawArrow(&paint, LeftArrow, false, 0 , 0 ,16, 16, colorGroup(), true);
+#else
+  style().drawPrimitive(QStyle::PE_ArrowLeft, &paint, QRect(0, 0, 16, 16), colorGroup());
+#endif
   paint.end();
 
   QWMatrix m;
