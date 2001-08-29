75a37425f0c983a2ce54fb1f95f9c473fad7e882
Matthias Hoelzer-Kluepfel <hoelzer@kde.org>
2001-08-29 12:04:03 +0000

    Fixed possible HTML form attacks by blocking post actions to all privileged ports other than http and https.
    See http://www.kb.cert.org/vuls/id/476267.
    
    svn path=/branches/KDE_2_2_BRANCH/kdelibs/; revision=112394

--- kio/job.cpp
+++ kio/job.cpp
@@ -874,9 +874,38 @@ TransferJob *KIO::get( const KURL& url, bool reload, bool showProgressInfo )
     return job;
 }
 
+class ErrorJob : public TransferJob
+{
+public:
+
+  ErrorJob(QString url, const QByteArray &packedArgs, const QByteArray &postData, bool showProgressInfo) : TransferJob("", CMD_SPECIAL, packedArgs, postData, showProgressInfo)
+  {
+    m_error = ERR_ACCESS_DENIED;
+    m_errorText = url;
+  }
+  
+};
+
 TransferJob *KIO::http_post( const KURL& url, const QByteArray &postData, bool showProgressInfo )
 {
-    assert( (url.protocol() == "http") || (url.protocol() == "https" ));
+    bool valid = true;
+    
+    // filter out non https? protocols
+    if ((url.protocol() != "http") && (url.protocol() != "https" ))
+      valid = false;
+
+    // filter out some malicious ports
+    if (!(url.port() == 80 || url.port() == 434 || url.port() >= 1024))
+      valid = false;
+    
+    // if request is not valid, return an invalid transfer job
+    if (!valid)
+    {
+      KIO_ARGS << (int)1 << url;
+      TransferJob * job = new ErrorJob(url.url(), packedArgs, postData, showProgressInfo);
+      return job;
+    }
+    
     // Send http post command (1), decoded path and encoded query
     KIO_ARGS << (int)1 << url;
     TransferJob * job = new TransferJob( url, CMD_SPECIAL,
