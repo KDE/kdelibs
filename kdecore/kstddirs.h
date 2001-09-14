918bc04abbbb808a2edab74be753ca257b1c418b
Daniel Molkentin <molkentin@kde.org>
2001-09-14 17:01:33 +0000

    Adding "#ifndef KDE_NO_COMPAT" for obsolete headers
    svn path=/trunk/kdelibs/; revision=114253

--- kdecore/kstddirs.h
+++ kdecore/kstddirs.h
@@ -1,2 +1,4 @@
 // kstddirs.h is the old name, use #include <kstandarddirs.h> from now on
+#ifndef KDE_NO_COMPAT
 #include <kstandarddirs.h>
+#endif
--- kdecore/kuniqueapp.h
+++ kdecore/kuniqueapp.h
@@ -1,2 +1,4 @@
 // kuniqueapp.h is the old name. Use #include <kuniqueapplication.h> from now on
+#ifndef KDE_NO_COMPAT
 #include <kuniqueapplication.h>
+#endif
