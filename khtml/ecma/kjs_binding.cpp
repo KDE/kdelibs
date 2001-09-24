37fbadfb07aaf2fd7e04e687b2888a5a8f7b7645
David Faure <faure@kde.org>
2001-09-24 22:39:00 +0000

    @@ -200,6 +200,8 @@ bool KJS::originCheck(const KURL &kurl1, +  if ( kurl1.isEmpty() || kurl2.isEmpty() ) +    return true; As found long ago with the "frame src='about:blank' case"
    svn path=/trunk/kdelibs/; revision=115334

--- khtml/ecma/kjs_binding.cpp
+++ khtml/ecma/kjs_binding.cpp
@@ -200,6 +200,8 @@ bool KJS::originCheck(const KURL &kurl1, const KURL &kurl2)
   if ( kurl1.protocol().find( "javascript", 0, false ) == 0 ||
        kurl2.protocol().find( "javascript", 0, false ) == 0 )
     return true;
+  if ( kurl1.isEmpty() || kurl2.isEmpty() )
+    return true;
 
   if (kurl1.protocol() == kurl2.protocol() &&
       kurl1.host() == kurl2.host() &&
