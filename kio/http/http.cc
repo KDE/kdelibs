ff107882b4bffbf0fdffb8cebf3be4f27db26901
Waldo Bastian <bastian@kde.org>
2001-09-30 22:57:15 +0000

    Don't store any text/* documents received via https in cache.
    svn path=/trunk/kdelibs/; revision=115993

--- kio/http/http.cc
+++ kio/http/http.cc
@@ -752,7 +752,7 @@ bool HTTPProtocol::http_open()
   {
      m_fcache = checkCacheEntry( );
 
-     if ((m_request.cache == CC_Reload && m_fcache) || m_bIsSSL )
+     if (m_request.cache == CC_Reload && m_fcache)
      {
         if (m_fcache)
           fclose(m_fcache);
@@ -1681,6 +1681,13 @@ bool HTTPProtocol::readHeader()
   // Do not cache pages originating from password
   // protected sites.
   if ( !hasCacheDirective && Authentication != AUTH_None )
+  {
+    m_bCachedWrite = false;
+    mayCache = false;
+  }
+
+  // Do not cache any text from SSL sites.
+  if (m_bIsSSL && m_strMimeType.startsWith("text/"))
   {
     m_bCachedWrite = false;
     mayCache = false;
