86de99fccc54e8e06c7b6525f64cb47bcc5f5c27
David Faure <faure@kde.org>
2001-10-02 11:19:06 +0000

    What was I thinking yesterday? Didn't even test the empty-ref fix :( The fix also needed a fix to encode() : -    return QString::null; +    return segment.isNull() ? QString::null : ""; // differenciate null and empty
    svn path=/trunk/kdelibs/; revision=116132

--- kdecore/kurl.cpp
+++ kdecore/kurl.cpp
@@ -68,7 +68,7 @@ static QString encode( const QString& segment, bool encode_slash, int encoding_h
   int old_length = local.length();
 
   if ( !old_length )
-    return QString::null;
+    return segment.isNull() ? QString::null : ""; // differenciate null and empty
 
   // a worst case approximation
   QChar *new_segment = new QChar[ old_length * 3 + 1 ];
