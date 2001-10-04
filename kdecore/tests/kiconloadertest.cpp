36528dbb508783d08988697933dd8204cb19ed16
David Faure <faure@kde.org>
2001-10-04 18:00:54 +0000

    Speed test
    svn path=/trunk/kdelibs/; revision=116448

--- kdecore/tests/kiconloadertest.cpp
+++ kdecore/tests/kiconloadertest.cpp
@@ -1,4 +1,5 @@
 #include <kiconloader.h>
+#include <qdatetime.h>
 #include <stdio.h>
 #include <kapp.h>
 #include <stdlib.h>
@@ -6,10 +7,13 @@
 
 int main(int argc, char *argv[])
 {
-  KApplication app(argc,argv,"kiconloadertest",false,false);
+  KApplication app(argc,argv,"kiconloadertest"/*,false,false*/);
 
   KIconLoader * mpLoader = KGlobal::iconLoader();
   int mContext = KIcon::Application;
+  QTime dt;
+  dt.start();
+  int count = 0;
   for ( int mGroup = 0; mGroup < KIcon::LastGroup ; ++mGroup )
   {
       kdDebug() << "queryIcons " << mGroup << "," << mContext << endl;
@@ -17,9 +21,14 @@ int main(int argc, char *argv[])
       kdDebug() << " -> found " << filelist.count() << " icons." << endl;
       int i=0;
       for(QStringList::Iterator it = filelist.begin();
-          it != filelist.end() && i<10;
+          it != filelist.end() /*&& i<10*/;
           ++it, ++i )
-          kdDebug() << ( i==9 ? "..." : (*it) ) << endl;
+      {
+          //kdDebug() << ( i==9 ? "..." : (*it) ) << endl;
+          mpLoader->loadIcon( (*it), mGroup );
+	  ++count;
+      }
   }
+  kdDebug() << "Loading " << count << " icons took " << (float)(dt.elapsed()) / 1000 << " seconds" << endl;
 }
 
