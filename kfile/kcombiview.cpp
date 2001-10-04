e6984fe3dbe0acf79bb480173681ca3db83cd2aa
Carsten Pfeiffer <pfeiffer@kde.org>
2001-10-04 01:26:47 +0000

    apply sorting setting in the combiview also to the directories
    svn path=/branches/KDE_2_2_BRANCH/kdelibs/; revision=116381

--- kfile/kcombiview.cpp
+++ kfile/kcombiview.cpp
@@ -127,6 +127,7 @@ void KCombiView::setSorting( QDir::SortSpec sort )
     if ( !right )
         kdFatal() << "You need to call setRight( someview ) before!" << endl;
     right->setSorting( sort );
+    left->KFileView::setSorting( sort );
     // don't call KFileView::setSorting()! It would resort all items, what
     // we don't want (the child-views do this themselves)
     mySorting = right->sorting();
@@ -137,6 +138,8 @@ void KCombiView::sortReversed()
     if ( !right )
         kdFatal() << "You need to call setRight( someview ) before!" << endl;
     right->sortReversed();
+    left->sortReversed();
+    reversed = right->isReversed();
 }
 
 void KCombiView::clearView()
