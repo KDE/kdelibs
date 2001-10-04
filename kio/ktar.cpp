d06d5489079b76067c35f0f324cdbe22719d3c3f
David Faure <faure@kde.org>
2001-10-04 20:51:28 +0000

    Tons of fixes in KLimitedIODevice (at,atEnd,correct max in read() etc.). This fixes loading of images in koffice.
    svn path=/trunk/kdelibs/; revision=116475

--- kio/ktar.cpp
+++ kio/ktar.cpp
@@ -738,7 +738,7 @@ public:
             else
                 ok = m_dev->open( m );
             if ( ok )*/
-                m_dev->at( m_start ); // No concurrent access for the moment !
+                m_dev->at( m_start ); // No concurrent access !
         }
         else
             kdWarning(7005) << "KLimitedIODevice::open only supports IO_ReadOnly!" << endl;
@@ -750,15 +750,14 @@ public:
     virtual void flush() {}
 
     virtual Offset size() const { return m_length; }
-    // at(), at(offset) and atEnd() are taken care of by QIODevice
 
     virtual Q_LONG readBlock ( char * data, Q_ULONG maxlen )
     {
-        //kdDebug(7005) << "KLimitedIODevice::readBlock maxlen=" << maxlen << endl;
-        Q_ULONG max = QMIN( at() + maxlen, m_start + m_length );
-        return m_dev->readBlock( data, max );
+        maxlen = QMIN( maxlen, m_length - at() ); // Apply upper limit
+        return m_dev->readBlock( data, maxlen );
     }
-    virtual Q_LONG writeBlock ( const char *, Q_ULONG ) { return -1; }
+    virtual Q_LONG writeBlock ( const char *, Q_ULONG ) { return -1; } // unsupported
+    virtual int putch( int ) { return -1; } // unsupported
 
     virtual int getch() {
         char c[2];
@@ -767,8 +766,14 @@ public:
         else
             return c[0];
     }
-    virtual int putch( int ) { return -1; } // unsupported
-    virtual int ungetch( int ) { return -1; } // unsupported
+    virtual int ungetch( int c ) { return m_dev->ungetch(c); } // ## apply lower limit ?
+    virtual Offset at() const { return m_dev->at() - m_start; }
+    virtual bool at( Offset pos ) {
+        Q_ASSERT( pos <= m_length );
+        pos = QMIN( pos, m_length ); // Apply upper limit
+        return m_dev->at( m_start + pos );
+    }
+    virtual bool atEnd() const { return m_dev->at() >= m_start + m_length; }
 private:
     QIODevice* m_dev;
     Q_ULONG m_start;
