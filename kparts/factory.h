41a72f86b7e48bd154bda33d09eecc2370180f0d
Simon Hausmann <hausmann@kde.org>
2001-10-10 16:53:05 +0000

    - added a KParts::GenericFactory template which implements the   createPartObject method using staticMetaObject magic and also provides   static KInstance * and KAboutData * methods
    svn path=/trunk/kdelibs/; revision=117158

--- kparts/factory.h
+++ kparts/factory.h
@@ -21,6 +21,8 @@
 #define __kparts_factory_h__
 
 #include <klibloader.h>
+#include <qmetaobject.h>
+#include <kdebug.h>
 
 class QWidget;
 
@@ -105,6 +107,106 @@ protected:
     virtual QObject *createObject( QObject *parent = 0, const char *name = 0, const char *classname = "QObject", const QStringList &args = QStringList() );
 };
 
+/**
+ * A template for a KParts::Factory implementation. It implements the pure virtual
+ * createPartObject method by instantiating the template argument when requested
+ * through the className field. In addition it is a container for a parts @ref KInstance
+ * object, by providing a static KInstance *instance() method.
+ *
+ * The template argument has to inherit from KParts::Part and has to implement two methods:
+ *  1) There needs to be a public constructor with the following signature:
+ *         MyPart( QWidget *parentWidget, const char *widgetName, QObject *parent, const char *name )
+ *
+ *  2) It needs to provide one static method to create an @ref KAboutData object per
+ *     request, holding information about the component's name, its authors, license, etc.
+ *     The signature of that static method has to be
+ *         KAboutData *createAboutData()
+ *
+ * The template will take care of memory management of the KInstance and the KAboutData object.
+ *
+ * For advanced use you can also inherit from the template and re-implement additionally the
+ * virtual KInstance *createInstance() method, for example in case you want to extend the 
+ * paths of your instance's KStandardDirs object.
+ *
+ */
+template <class T>
+class GenericFactory : public Factory
+{
+public:
+    GenericFactory() 
+    {
+        if ( s_self )
+            kdWarning() << "KParts::GenericFactory instantiated more than once!" << endl;
+        s_self = this;
+    }
+    virtual ~GenericFactory()
+    {
+        delete s_aboutData;
+        delete s_instance;
+        s_aboutData = 0;
+        s_instance = 0;
+        s_self = 0;
+    }
+
+    virtual KParts::Part *createPartObject( QWidget *parentWidget, const char *widgetName,
+	                                    QObject *parent, const char *name,
+					    const char *className,
+					    const QStringList & )
+    {
+        QMetaObject *metaObject = T::staticMetaObject();
+        while ( metaObject )
+        {
+            if ( !qstrcmp( className, metaObject->className() ) )
+                return new T( parentWidget, widgetName, parent, name );
+            metaObject = metaObject->superClass();
+        }
+        return 0;
+    }
+
+    static KInstance *instance();
+    static KAboutData *aboutData();
+
+protected:
+    virtual KInstance *createInstance()
+    {
+        return new KInstance( aboutData() );
+    }
+
+private:
+    static GenericFactory<T> *s_self;
+    static KInstance *s_instance;
+    static KAboutData *s_aboutData;
 };
+
+template <class T>
+GenericFactory<T> *GenericFactory<T>::s_self = 0;
+
+template <class T>
+KInstance *GenericFactory<T>::s_instance = 0;
+
+template <class T>
+KAboutData *GenericFactory<T>::s_aboutData = 0;
+
+template <class T>
+KInstance *GenericFactory<T>::instance()
+{
+    if ( !s_instance && s_self )
+        s_instance = s_self->createInstance();
+    return s_instance;
+}
+
+template <class T>
+KAboutData *GenericFactory<T>::aboutData()
+{
+    if ( !s_aboutData )
+        s_aboutData = T::createAboutData();
+    return s_aboutData;
+}
+
+};
+
+/*
+ * vim: et sw=4
+ */
 
 #endif
