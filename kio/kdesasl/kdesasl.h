a6f6989e27c95471fb80e566808c960836dd1a45
Michael Haeckel <haeckel@kde.org>
2001-10-13 14:54:39 +0000

    Add some information about what SASL is and what it is not to prevent people from asking me how to transfer information encrypted via SASL.
    svn path=/trunk/kdelibs/; revision=117764

--- kio/kdesasl/kdesasl.h
+++ kio/kdesasl/kdesasl.h
@@ -28,10 +28,17 @@ class QStrIList;
 /**
  * This library can create responses for SASL authentication for a given
  * challenge and a given secret. This way of authentication is common for
- * SMTP, POP3 and IMAP.
+ * SMTP, POP3, IMAP and LDAP.
  *
- * Currently PLAIN (RFC 2595), LOGIN and CRAM-MD5 (RFC 2195) authentication
- * are supported.
+ * SASL is one way strong encryption and therefore useful for authentication,
+ * but not for secret information transfer.
+ * It is possibly to prove with SASL to know a shared secret like a password.
+ * It is not possible with SASL to transfer any other information in an
+ * encrypted way. For that purpose OpenPGP or SSL are useful.
+ *
+ * Currently PLAIN (RFC 2595), LOGIN, CRAM-MD5 (RFC 2195) and
+ * DIGEST-MD5 (RFC 2831) authentication are supported.
+ * PLAIN and LOGIN do only use encoding and not encryption.
  *
  * @author Michael Häckel <Michael@Haeckel.Net>
  * @version $Id$
