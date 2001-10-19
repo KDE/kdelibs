075094679ff8b38d7d50c1b073de26c66762cc57
Roberto Teixeira <rst@kde.org>
2001-10-19 20:01:23 +0000

    Moved KArchive stuff from ktar.* to karchive.cpp and karchive.h
    svn path=/trunk/kdelibs/; revision=118678

---
+++ kio/karchive.cpp
@@ -0,0 +1,407 @@
+/* This file is part of the KDE libraries
+   Copyright (C) 2000 David Faure <faure@kde.org>
+
+   Moved from ktar.cpp by Roberto Teixeira <maragato@kde.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License version 2 as published by the Free Software Foundation.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+#include <grp.h>
+#include <pwd.h>
+#include <assert.h>
+
+#include <qcstring.h>
+#include <qdir.h>
+#include <qfile.h>
+#include <kdebug.h>
+#include <kurl.h>
+#include <kmimetype.h>
+
+#include <kfilterdev.h>
+#include <kfilterbase.h>
+
+#include "karchive.h"
+
+template class QDict<KArchiveEntry>;
+
+class KArchive::KArchivePrivate
+{
+public:
+    KArchiveDirectory* rootDir;
+};
+
+////////////////////////////////////////////////////////////////////////
+/////////////////////////// KArchive ///////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+
+KArchive::KArchive( QIODevice * dev )
+{
+    d = new KArchivePrivate;
+    d->rootDir = 0;
+    m_dev = dev;
+    m_open = false;
+}
+
+KArchive::~KArchive()
+{
+    if ( m_open )
+        close();
+    delete d->rootDir;
+    delete d;
+}
+
+bool KArchive::open( int mode )
+{
+    if(0 == m_dev)
+        return false; // Fail w/o segfaulting if the device is no good
+
+    if ( !m_dev->open( mode ) )
+        return false;
+
+    if ( m_open )
+        close();
+
+    m_mode = mode;
+    m_open = true;
+
+    Q_ASSERT( d->rootDir == 0L );
+    d->rootDir = 0L;
+
+    return openArchive( mode );
+}
+
+void KArchive::close()
+{
+    if ( !m_open )
+        return;
+
+    m_dev->close();
+    //m_dirList.clear();
+
+    delete d->rootDir;
+    d->rootDir = 0;
+    m_open = false;
+    closeArchive();
+}
+
+const KArchiveDirectory* KArchive::directory() const
+{
+    // rootDir isn't const so that parsing-on-demand is possible
+    return const_cast<KArchive *>(this)->rootDir();
+}
+
+bool KArchive::writeFile( const QString& name, const QString& user, const QString& group, uint size, const char* data )
+{
+    if ( !prepareWriting( name, user, group, size ) )
+        return false;
+
+    // Write data
+    if ( device()->writeBlock( data, size ) != (int)size )
+        return false;
+
+    return doneWriting( size );
+}
+
+KArchiveDirectory * KArchive::rootDir()
+{
+    if ( !d->rootDir )
+    {
+        //kdDebug() << "Making root dir " << endl;
+        struct passwd* pw =  getpwuid( getuid() );
+        struct group* grp = getgrgid( getgid() );
+        QString username = pw ? QFile::decodeName(pw->pw_name) : QString::number( getuid() );
+        QString groupname = grp ? QFile::decodeName(grp->gr_name) : QString::number( getgid() );
+
+        d->rootDir = new KArchiveDirectory( this, QString::fromLatin1("/"), (int)(0777 + S_IFDIR), 0, username, groupname, QString::null );
+    }
+    return d->rootDir;
+}
+
+KArchiveDirectory * KArchive::findOrCreate( const QString & path )
+{
+    //kdDebug() << "KArchive::findOrCreate " << path << endl;
+    if ( path == "" || path == "/" || path == "." ) // root dir => found
+    {
+        //kdDebug() << "KArchive::findOrCreate returning rootdir" << endl;
+        return rootDir();
+    }
+    // Important note : for tar files containing absolute paths
+    // (i.e. beginning with "/"), this means the leading "/" will
+    // be removed (no KDirectory for it), which is exactly the way
+    // the "tar" program works (though it displays a warning about it)
+    // See also KArchiveDirectory::entry().
+
+    // Already created ? => found
+    KArchiveEntry* ent = rootDir()->entry( path );
+    if ( ent && ent->isDirectory() )
+    {
+        //kdDebug() << "KArchive::findOrCreate found it" << endl;
+        return (KArchiveDirectory *) ent;
+    }
+
+    // Otherwise go up and try again
+    int pos = path.findRev( '/' );
+    KArchiveDirectory * parent;
+    QString dirname;
+    if ( pos == -1 ) // no more slash => create in root dir
+    {
+        parent =  rootDir();
+        dirname = path;
+    }
+    else
+    {
+        QString left = path.left( pos );
+        dirname = path.mid( pos + 1 );
+        parent = findOrCreate( left ); // recursive call... until we find an existing dir.
+    }
+
+    //kdDebug() << "KTar : found parent " << parent->name() << " adding " << dirname << " to ensure " << path << endl;
+    // Found -> add the missing piece
+    KArchiveDirectory * e = new KArchiveDirectory( this, dirname, d->rootDir->permissions(),
+                                                   d->rootDir->date(), d->rootDir->user(),
+                                                   d->rootDir->group(), QString::null );
+    parent->addEntry( e );
+    return e; // now a directory to <path> exists
+}
+
+void KArchive::setDevice( QIODevice * dev )
+{
+    m_dev = dev;
+}
+
+void KArchive::setRootDir( KArchiveDirectory *rootDir )
+{
+    Q_ASSERT( !d->rootDir ); // Call setRootDir only once during parsing please ;)
+    d->rootDir = rootDir;
+}
+
+////////////////////////////////////////////////////////////////////////
+/////////////////////// KArchiveEntry //////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+KArchiveEntry::KArchiveEntry( KArchive* t, const QString& name, int access, int date,
+                      const QString& user, const QString& group, const
+                      QString& symlink)
+{
+  m_name = name;
+  m_access = access;
+  m_date = date;
+  m_user = user;
+  m_group = group;
+  m_symlink = symlink;
+  m_archive = t;
+
+}
+
+QDateTime KArchiveEntry::datetime() const
+{
+  QDateTime d;
+  d.setTime_t( m_date );
+  return d;
+}
+
+////////////////////////////////////////////////////////////////////////
+/////////////////////// KArchiveFile ///////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+
+KArchiveFile::KArchiveFile( KArchive* t, const QString& name, int access, int date,
+                    const QString& user, const QString& group,
+                    const QString & symlink,
+                    int pos, int size )
+  : KArchiveEntry( t, name, access, date, user, group, symlink )
+{
+  m_pos = pos;
+  m_size = size;
+}
+
+int KArchiveFile::position() const
+{
+  return m_pos;
+}
+
+int KArchiveFile::size() const
+{
+  return m_size;
+}
+
+QByteArray KArchiveFile::data() const
+{
+  archive()->device()->at( m_pos );
+
+  // Read content
+  QByteArray arr( m_size );
+  if ( m_size )
+  {
+    assert( arr.data() );
+    int n = archive()->device()->readBlock( arr.data(), m_size );
+    if ( n != m_size )
+      arr.resize( n );
+  }
+  return arr;
+}
+
+/**
+ * A readonly device that reads from an underlying device
+ * from a given point to another (e.g. to give access to a single
+ * file inside an archive)
+ * @author David Faure <david@mandrakesoft.com>
+ */
+class KLimitedIODevice : public QIODevice
+{
+public:
+    /**
+     * @param dev the underlying device, opened or not
+     * This device itself auto-opens (in readonly mode), no need to open it.
+     */
+    KLimitedIODevice( QIODevice *dev, int start, int length )
+        : m_dev( dev ), m_start( start ), m_length( length )
+    {
+        //kdDebug(7005) << "KLimitedIODevice::KLimitedIODevice start=" << start << " length=" << length << endl;
+        setType( IO_Direct ); // we support sequential too, but then atEnd() tries getch/ungetch !
+        open( IO_ReadOnly );
+    }
+    virtual ~KLimitedIODevice() {}
+
+    virtual bool open( int m ) {
+        //kdDebug(7005) << "KLimitedIODevice::open m=" << m << endl;
+        if ( m & IO_ReadOnly ) {
+            /*bool ok = false;
+            if ( m_dev->isOpen() )
+                ok = ( m_dev->mode() == IO_ReadOnly );
+            else
+                ok = m_dev->open( m );
+            if ( ok )*/
+                m_dev->at( m_start ); // No concurrent access !
+        }
+        else
+            kdWarning(7005) << "KLimitedIODevice::open only supports IO_ReadOnly!" << endl;
+        setState( IO_Open );
+        setMode( m );
+        return true;
+    }
+    virtual void close() {}
+    virtual void flush() {}
+
+    virtual Offset size() const { return m_length; }
+
+    virtual Q_LONG readBlock ( char * data, Q_ULONG maxlen )
+    {
+        maxlen = QMIN( maxlen, m_length - at() ); // Apply upper limit
+        return m_dev->readBlock( data, maxlen );
+    }
+    virtual Q_LONG writeBlock ( const char *, Q_ULONG ) { return -1; } // unsupported
+    virtual int putch( int ) { return -1; } // unsupported
+
+    virtual int getch() {
+        char c[2];
+        if ( readBlock(c, 1) == -1)
+            return -1;
+        else
+            return c[0];
+    }
+    virtual int ungetch( int c ) { return m_dev->ungetch(c); } // ## apply lower limit ?
+    virtual Offset at() const { return m_dev->at() - m_start; }
+    virtual bool at( Offset pos ) {
+        Q_ASSERT( pos <= m_length );
+        pos = QMIN( pos, m_length ); // Apply upper limit
+        return m_dev->at( m_start + pos );
+    }
+    virtual bool atEnd() const { return m_dev->at() >= m_start + m_length; }
+private:
+    QIODevice* m_dev;
+    Q_ULONG m_start;
+    Q_ULONG m_length;
+};
+
+
+QIODevice *KArchiveFile::device() const
+{
+    return new KLimitedIODevice( archive()->device(), m_pos, m_size );
+}
+
+////////////////////////////////////////////////////////////////////////
+//////////////////////// KArchiveDirectory /////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+
+
+KArchiveDirectory::KArchiveDirectory( KArchive* t, const QString& name, int access,
+                              int date,
+                              const QString& user, const QString& group,
+                              const QString &symlink)
+  : KArchiveEntry( t, name, access, date, user, group, symlink )
+{
+  m_entries.setAutoDelete( true );
+}
+
+QStringList KArchiveDirectory::entries() const
+{
+  QStringList l;
+
+  QDictIterator<KArchiveEntry> it( m_entries );
+  for( ; it.current(); ++it )
+    l.append( it.currentKey() );
+
+  return l;
+}
+
+KArchiveEntry* KArchiveDirectory::entry( QString name )
+  // not "const QString & name" since we want a local copy
+  // (to remove leading slash if any)
+{
+  int pos = name.find( '/' );
+  if ( pos == 0 ) // ouch absolute path (see also KArchive::findOrCreate)
+  {
+    if (name.length()>1)
+    {
+      name = name.mid( 1 ); // remove leading slash
+      pos = name.find( '/' ); // look again
+    }
+    else // "/"
+      return this;
+  }
+  // trailing slash ? -> remove
+  if ( pos != -1 && pos == (int)name.length()-1 )
+  {
+    name = name.left( pos );
+    pos = name.find( '/' ); // look again
+  }
+  if ( pos != -1 )
+  {
+    QString left = name.left( pos );
+    QString right = name.mid( pos + 1 );
+
+    //kdDebug() << "KArchiveDirectory::entry left=" << left << " right=" << right << endl;
+
+    KArchiveEntry* e = m_entries[ left ];
+    if ( !e || !e->isDirectory() )
+      return 0;
+    return ((KArchiveDirectory*)e)->entry( right );
+  }
+
+  return m_entries[ name ];
+}
+
+const KArchiveEntry* KArchiveDirectory::entry( QString name ) const
+{
+  return ((KArchiveDirectory*)this)->entry( name );
+}
+
+void KArchiveDirectory::addEntry( KArchiveEntry* entry )
+{
+  m_entries.insert( entry->name(), entry );
+}
---
+++ kio/karchive.h
@@ -0,0 +1,317 @@
+/* This file is part of the KDE libraries
+   Copyright (C) 2000 David Faure <faure@kde.org>
+
+   Moved from ktar.h by Roberto Teixeira <maragato@kde.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License version 2 as published by the Free Software Foundation.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+#ifndef __karchive_h
+#define __karchive_h
+
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#include <qdatetime.h>
+#include <qstring.h>
+#include <qstringlist.h>
+#include <qdict.h>
+
+class KArchiveDirectory;
+class KArchiveFile;
+
+/**
+ * @short generic class for reading/writing archives
+ * @author David Faure <faure@kde.org>
+ */
+class KArchive
+{
+protected:
+    /**
+     * Base constructor (protected since this is a pure virtual class).
+     * @param dev the I/O device where the archive reads its data
+     * Note that this can be a file, but also a data buffer, a compression filter, etc.
+     */
+    KArchive( QIODevice * dev );
+    virtual ~KArchive();
+
+public:
+    /**
+     * Opens the archive for reading or writing.
+     * @param mode may be IO_ReadOnly or IO_WriteOnly
+     * Inherited classes might want to reimplement openArchive instead.
+     * @see #close
+     */
+    virtual bool open( int mode );
+
+    /**
+     * Closes the archive.
+     * Inherited classes might want to reimplement openArchive instead.
+     *
+     * @see #open
+     */
+    virtual void close();
+
+    /**
+     * @return true if the archive is opened
+     */
+    bool isOpened() const { return m_open; }
+
+    /**
+     * @return the mode in which the archive was opened (IO_ReadOnly or IO_WriteOnly)
+     */
+    int mode() const { return m_mode; }
+
+    /**
+     * The underlying device.
+     */
+    QIODevice * device() const { return m_dev; }
+
+    /**
+     * If an archive is opened for reading, then the contents
+     * of the archive can be accessed via this function.
+     */
+    const KArchiveDirectory* directory() const;
+
+    /**
+     * If an archive is opened for writing then you can add new directories
+     * using this function. KArchive won't write one directory twice.
+     */
+    virtual bool writeDir( const QString& name, const QString& user, const QString& group ) = 0;
+
+    /**
+     * If an archive is opened for writing then you can add a new file
+     * using this function. If the file name is for example "mydir/test1" then
+     * the directory "mydir" is automatically appended first if that did not
+     * happen yet.
+     */
+    bool writeFile( const QString& name, const QString& user, const QString& group, uint size, const char* data );
+
+    /**
+     * Here's another way of writing a file into an archive:
+     * Call @ref prepareWriting, then call write as many times as wanted,
+     * then call @ref doneWriting( totalSize )
+     * You need to know the size before hand, it is needed in the header!
+     */
+    virtual bool prepareWriting( const QString& name, const QString& user, const QString& group, uint size ) = 0;
+
+    /**
+     * Call @ref doneWriting after writing the data.
+     * @see prepareWriting
+     */
+    virtual bool doneWriting( uint size ) = 0;
+
+protected:
+    /**
+     * Opens an archive for reading or writing.
+     * Called by @ref open.
+     */
+    virtual bool openArchive( int mode ) = 0;
+
+    /**
+     * Closes the archive.
+     * Called by @ref close.
+     */
+    virtual bool closeArchive() = 0;
+
+    /**
+     * Retrieves or create the root directory.
+     * The default implementation assumes that openArchive() did the parsing,
+     * so it creates a dummy rootdir if none was set (write mode, or no '/' in the archive).
+     * Reimplement this to provide parsing/listing on demand.
+     */
+    virtual KArchiveDirectory* rootDir();
+
+    /**
+     * Ensures that @p path exists, create otherwise.
+     * This handles e.g. tar files missing directory entries, like mico-2.3.0.tar.gz :)
+     */
+    KArchiveDirectory * findOrCreate( const QString & path );
+
+    /**
+     * @internal for inherited constructors
+     */
+    void setDevice( QIODevice *dev );
+
+    /**
+     * @internal for inherited classes
+     */
+    void setRootDir( KArchiveDirectory *rootDir );
+
+private:
+    class KArchivePrivate;
+    KArchivePrivate * d;
+    QIODevice * m_dev;
+    bool m_open;
+    char m_mode;
+};
+
+/**
+ * @short Base class for the archive-file's directory structure.
+ *
+ * @see KArchiveFile
+ * @see KArchiveDirectory
+ */
+class KArchiveEntry
+{
+public:
+    KArchiveEntry( KArchive* archive, const QString& name, int access, int date,
+               const QString& user, const QString& group,
+               const QString &symlink );
+
+    virtual ~KArchiveEntry() { }
+
+    /**
+     * Creation date of the file.
+     */
+    QDateTime datetime() const;
+    int date() const { return m_date; }
+
+    /**
+     * Name of the file without path.
+     */
+    QString name() const { return m_name; }
+    /**
+     * The permissions and mode flags as returned by the stat() function
+     * in st_mode.
+     */
+    mode_t permissions() const { return m_access; }
+    /**
+     * User who created the file.
+     */
+    QString user() const { return m_user; }
+    /**
+     * Group of the user who created the file.
+     */
+    QString group() const { return m_group; }
+
+    /**
+     * Symlink if there is one
+     */
+    QString symlink() const { return m_symlink; }
+
+    /**
+     * @return true if this entry is a file
+     */
+    virtual bool isFile() const { return false; }
+    /**
+     * @return true if this entry is a directory
+     */
+    virtual bool isDirectory() const { return false; }
+
+protected:
+    KArchive* archive() const { return m_archive; }
+
+private:
+    QString m_name;
+    int m_date;
+    mode_t m_access;
+    QString m_user;
+    QString m_group;
+    QString m_symlink;
+    KArchive* m_archive;
+};
+
+/**
+ * @short A file in an archive.
+ *
+ * @see KArchive
+ * @see KArchiveDirectory
+ */
+class KArchiveFile : public KArchiveEntry
+{
+public:
+    KArchiveFile( KArchive* archive, const QString& name, int access, int date,
+              const QString& user, const QString& group, const QString &symlink,
+              int pos, int size );
+
+    virtual ~KArchiveFile() { }
+
+    /**
+     * Position of the data in the [uncompressed] archive.
+     */
+    int position() const;
+    /**
+     * Size of the data.
+     */
+    int size() const;
+
+    /**
+     * @return the content of this file.
+     * Call data() with care (only once per file), this data isn't cached.
+     */
+    QByteArray data() const;
+
+    /**
+     * This method returns QIODevice (internal class: KLimitedIODevice)
+     * on top of the underlying QIODevice. This is obviously for reading only.
+     * Note that the ownership of the device is being transferred to the caller,
+     * who will have to delete it.
+     * The returned device auto-opens (in readonly mode), no need to open it.
+     */
+    QIODevice *device() const;
+
+    /**
+     * @return true, since this entry is a file
+     */
+    virtual bool isFile() const { return true; }
+
+private:
+    int m_pos;
+    int m_size;
+};
+
+/**
+ * @short A directory in an archive.
+ *
+ * @see KArchive
+ * @see KArchiveFile
+ */
+class KArchiveDirectory : public KArchiveEntry
+{
+public:
+    KArchiveDirectory( KArchive* archive, const QString& name, int access, int date,
+                   const QString& user, const QString& group,
+                   const QString& symlink);
+
+    virtual ~KArchiveDirectory() { }
+
+    /**
+     * @return the names of all entries in this directory (filenames, no path).
+     */
+    QStringList entries() const;
+    /**
+     * @return a pointer to the entry in the directory.
+     *
+     * @param name may be "test1", "mydir/test3", "mydir/mysubdir/test3", etc.
+     */
+    KArchiveEntry* entry( QString name );
+    const KArchiveEntry* entry( QString name ) const;
+
+    /**
+     * @internal
+     * Adds a new entry to the directory.
+     */
+    void addEntry( KArchiveEntry* );
+
+    /**
+     * @return true, since this entry is a directory
+     */
+    virtual bool isDirectory() const { return true; }
+
+private:
+    QDict<KArchiveEntry> m_entries;
+};
+
+#endif
