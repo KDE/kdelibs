11d0d0376d2223a2a0329f6444d078cd184af23c
Dirk Mueller <mueller@kde.org>
2001-10-22 20:00:19 +0000

    werner won - making DOMString(int) explicit to see how many compilers break
    svn path=/trunk/kdelibs/; revision=119046

--- khtml/dom/dom_string.h
+++ khtml/dom/dom_string.h
@@ -46,7 +46,7 @@ public:
     /** default constructor. Gives an empty DOMString */
     DOMString();
     /* constructs a null DOMString. Like this assignment to 0 works */
-    DOMString(int);
+    explicit DOMString(int);
 
     DOMString(const QChar *str, uint len);
     DOMString(const QString &);
