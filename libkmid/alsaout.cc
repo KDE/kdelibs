4f998572406d86bed76d207d9cf13bef821bd71c
Ivan E. Moore II <rkrusty@debian.org>
2001-10-28 01:27:20 +0000

    [iem]  do ifdef asound2 ... endif  ifdef asound ... endif not ifdef asound2 ... else ... endif otherwise it expects that if your not using asound2 you *are* using asound1...no chance for not using asound at all.
    svn path=/trunk/kdelibs/; revision=119633

--- libkmid/alsaout.cc
+++ libkmid/alsaout.cc
@@ -50,7 +50,9 @@
 #ifdef HAVE_LIBASOUND2
 #define HAVE_ALSA_SEQ 1
 #define snd_seq_flush_output(x)	snd_seq_drain_output(x)
-#else
+#endif
+
+#ifdef HAVE_LIBASOUND
 #include <linux/asequencer.h>
 #define HAVE_ALSA_SEQ 1
 #endif
