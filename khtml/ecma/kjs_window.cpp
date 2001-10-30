757e8b7d83ff753899854531658e47b6146cf17c
David Faure <faure@kde.org>
2001-10-30 11:51:06 +0000

    Added param field in Screen's hashtable (always 0 here)
    svn path=/trunk/kdelibs/; revision=119936

--- khtml/ecma/kjs_window.cpp
+++ khtml/ecma/kjs_window.cpp
@@ -111,16 +111,16 @@ availWidth    DontEnum|ReadOnly
 */
 
 static const struct HashEntry ecmaScreenTableEntries[] = {
-   { "availLeft", Screen::availLeft, DontEnum|ReadOnly, 0 },
+   { "availLeft", Screen::availLeft, DontEnum|ReadOnly, 0, 0 },
    { 0, 0, 0, 0 },
-   { "availTop", Screen::availTop, DontEnum|ReadOnly, 0 },
-   { "height", Screen::height, DontEnum|ReadOnly, 0 },
-   { "width", Screen::width, DontEnum|ReadOnly, &ecmaScreenTableEntries[8] },
+   { "availTop", Screen::availTop, DontEnum|ReadOnly, 0, 0 },
+   { "height", Screen::height, DontEnum|ReadOnly, 0, 0 },
+   { "width", Screen::width, DontEnum|ReadOnly, 0, &ecmaScreenTableEntries[8] },
    { 0, 0, 0, 0 },
-   { "availHeight", Screen::availHeight, DontEnum|ReadOnly, 0 },
-   { "pixelDepth", Screen::pixelDepth, DontEnum|ReadOnly, &ecmaScreenTableEntries[9] },
-   { "colorDepth", Screen::colorDepth, DontEnum|ReadOnly, 0 },
-   { "availWidth", Screen::availWidth, DontEnum|ReadOnly, 0 }
+   { "availHeight", Screen::availHeight, DontEnum|ReadOnly, 0, 0 },
+   { "pixelDepth", Screen::pixelDepth, DontEnum|ReadOnly, 0, &ecmaScreenTableEntries[9] },
+   { "colorDepth", Screen::colorDepth, DontEnum|ReadOnly, 0, 0 },
+   { "availWidth", Screen::availWidth, DontEnum|ReadOnly, 0, 0 }
 };
 
 static const struct HashTable ecmaScreenTable = { 2, 10, ecmaScreenTableEntries, 10 };
