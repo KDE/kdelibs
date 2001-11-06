976a458806a9496c4631758158caebc4a34c12a5
Carsten Pfeiffer <pfeiffer@kde.org>
2001-11-06 01:40:41 +0000

    seeya
    svn path=/trunk/kdelibs/; revision=120920

--- kfile/kurlrequestertest.cpp
+++
@@ -1,9 +0,0 @@
-#include <kapplication.h>
-#include <kurlrequesterdlg.h>
-
-int main( int argc, char **argv )
-{
-    KApplication app( argc, argv, "kurlrequestertest" );
-    KURL url = KURLRequesterDlg::getURL();
-    qDebug( "Selected url: %s", url.url().latin1());
-}
