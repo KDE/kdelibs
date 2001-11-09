94d3bedae92a99ee70c762f57d3057414167d28c
Waldo Bastian <bastian@kde.org>
2001-11-09 06:43:43 +0000

    Adjust to KConfig changes.
    svn path=/trunk/kdelibs/; revision=121431

--- kio/kservice.cpp
+++ kio/kservice.cpp
@@ -316,14 +316,9 @@ public:
    KServiceReadProperty(const QString &_key, const QCString &_value)
 	: key(_key), value(_value) { }
 
-   bool hasGroup(const QString &) const { /*qDebug("hasGroup(const QString &)");*/ return false; }
-   bool hasGroup(const QCString &) const { /*qDebug("hasGroup(const QCString &)");*/ return false; }
-   bool hasGroup(const char *) const { /*qDebug("hasGroup(const char *)");*/ return false; }
+   bool internalHasGroup(const QCString &) const { /*qDebug("hasGroup(const QCString &)");*/ return false; }
 
    QStringList groupList() const { return QStringList(); }
-
-   bool hasKey(const QString &pKey) const { /*qDebug("hasKey(const QString &)");*/ return (pKey == key);}
-   bool hasKey(const char *) const { /*qDebug("hasKey(const char *)");*/ return true;}
 
    QMap<QString,QString> entryMap(const QString &) const
       { return QMap<QString,QString>(); }
