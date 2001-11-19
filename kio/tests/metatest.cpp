ac18842f96d609a19c0d088a6ee7f4631d721537
Carsten Pfeiffer <pfeiffer@kde.org>
2001-11-19 17:55:05 +0000

    better
    svn path=/trunk/kdelibs/; revision=123226

--- kio/tests/metatest.cpp
+++ kio/tests/metatest.cpp
@@ -6,26 +6,28 @@
 int main( int argc, char **argv )
 {
     KApplication app( argc, argv, "kfilemetatest" );
-    
+
     KFileMetaInfoProvider *prov = KFileMetaInfoProvider::self();
     KURL u;
     if (argv[1])
       u.setPath(argv[1]);
-    else 
+    else
       u.setPath("/tmp/metatest.txt");
-    
+
     KFileMetaInfo* info = prov->metaInfo( u );
 
     if (!info) return 1;
-    
+
     QStringList l = info->supportedKeys();
     QStringList::Iterator it;
     for (it = l.begin(); it!=l.end(); ++it)
     {
         KFileMetaInfoItem* item = info->item(*it);
-        kdDebug() << item->translatedKey() << " -> " << item->prefix()
-                  << item->value().toString() << item->postfix() << endl;
+        if ( item ) {
+            kdDebug() << item->translatedKey() << " -> " << item->prefix()
+                      << item->value().toString() << item->postfix() << endl;
+        }
     }
-    
+
     return 0;
 }
