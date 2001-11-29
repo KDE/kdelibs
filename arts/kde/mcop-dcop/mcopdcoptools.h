dc91c31c03daf1e3f4d55640cbff21a74cd5beda
Nikolas Zimmermann <wildfox@kde.org>
2001-11-29 16:23:21 +0000

    forgive me guys, i had too much todo these days
    svn path=/trunk/kdelibs/; revision=124701

---
+++ arts/kde/mcop-dcop/mcopdcoptools.h
@@ -0,0 +1,47 @@
+/*
+   Copyright (c) 2001 Nikolas Zimmermann <wildfox@kde.org>
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+*/
+
+#ifndef MCOPDCOPTOOLS_H
+#define MCOPDCOPTOOLS_H
+
+#include <string>
+
+#include <qdatastream.h>
+
+class MCOPDCOPTools
+{
+public:
+	static long getLong(QDataStream &stream)
+	{
+		long l;
+		stream << l;
+
+		return l;
+	}
+
+	static std::string getString(QDataStream &stream)
+	{
+		QCString s;
+		stream << s;
+
+		return std::string(s);
+	}
+};
+
+#endif
