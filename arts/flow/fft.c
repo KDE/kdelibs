dde4bb4ec9741b0a5f84bca0db527d76492e8ee6
Stefan Westerfeld <stefan@space.twc.de>
2001-12-14 01:10:00 +0000

    Use gsl fft (faster).
    svn path=/trunk/kdelibs/; revision=127027

--- arts/flow/fft.c
+++ arts/flow/fft.c
@@ -1,140 +1,5 @@
-/*
- * I put all fft stuff in that file, to make it more maintainable.
- * The original distribution is available at the URL.
- *
- * I make also some functions static to minimize collisions
- */
-
-/*============================================================================
-
-    fftmisc.c  -  Don Cross <dcross@intersrv.com>
-
-    http://www.intersrv.com/~dcross/fft.html
-
-    Helper routines for Fast Fourier Transform implementation.
-    Contains common code for fft_float() and fft_double().
-
-    See also:
-        fourierf.c
-        fourierd.c
-        ..\include\fourier.h
-
-    Revision history:
-
-1998 September 19 [Don Cross]
-    Improved the efficiency of IsPowerOfTwo().
-    Updated coding standards.
-
-============================================================================*/
-
-#include <stdlib.h>
-#include <stdio.h>
-#include <math.h>
-
-#define TRUE  1
-#define FALSE 0
-
-#define BITS_PER_WORD   (sizeof(unsigned) * 8)
-
-
-static int IsPowerOfTwo ( unsigned x )
-{
-    if ( x < 2 )
-        return FALSE;
-
-    if ( x & (x-1) )        /* Thanks to 'byang' for this cute trick! */
-        return FALSE;
-
-    return TRUE;
-}
-
-
-static unsigned NumberOfBitsNeeded ( unsigned PowerOfTwo )
-{
-    unsigned i;
-
-    if ( PowerOfTwo < 2 )
-    {
-        fprintf (
-            stderr,
-            ">>> Error in fftmisc.c: argument %d to NumberOfBitsNeeded is too small.\n",
-            PowerOfTwo );
-
-        exit(1);
-    }
-
-    for ( i=0; ; i++ )
-    {
-        if ( PowerOfTwo & (1 << i) )
-            return i;
-    }
-}
-
-
-
-static unsigned ReverseBits ( unsigned index, unsigned NumBits )
-{
-    unsigned i, rev;
-
-    for ( i=rev=0; i < NumBits; i++ )
-    {
-        rev = (rev << 1) | (index & 1);
-        index >>= 1;
-    }
-
-    return rev;
-}
-
-
-static double Index_to_frequency ( unsigned NumSamples, unsigned Index )
-{
-    if ( Index >= NumSamples )
-        return 0.0;
-    else if ( Index <= NumSamples/2 )
-        return (double)Index / (double)NumSamples;
-
-    return -(double)(NumSamples-Index) / (double)NumSamples;
-}
-
-#undef TRUE
-#undef FALSE
-#undef BITS_PER_WORD
-/*============================================================================
-
-    fourierf.c  -  Don Cross <dcross@intersrv.com>
-
-    http://www.intersrv.com/~dcross/fft.html
-
-    Contains definitions for doing Fourier transforms
-    and inverse Fourier transforms.
-
-    This module performs operations on arrays of 'float'.
-
-    Revision history:
-
-1998 September 19 [Don Cross]
-    Updated coding standards.
-    Improved efficiency of trig calculations.
-
-============================================================================*/
-
-#include <stdlib.h>
-#include <stdio.h>
-#include <math.h>
-
+#include "gsl/gslfft.h"
 #include "fft.h"
-
-#define CHECKPOINTER(p)  CheckPointer(p,#p)
-
-static void CheckPointer ( const void *p, const char *name )
-{
-    if ( p == NULL )
-    {
-        fprintf ( stderr, "Error in fft_float():  %s == NULL\n", name );
-        exit(1);
-    }
-}
-
 
 void fft_float (
     unsigned  NumSamples,
@@ -144,105 +9,24 @@ void fft_float (
     float    *RealOut,
     float    *ImagOut )
 {
-    unsigned NumBits;    /* Number of bits needed to store indices */
-    unsigned i, j, k, n;
-    unsigned BlockSize, BlockEnd;
+	double *ri_in = g_newa(double, NumSamples*4);
+	double *ri_out = ri_in + NumSamples*2;
+	unsigned int i;
 
-    double angle_numerator = 2.0 * DDC_PI;
-    double tr, ti;     /* temp real, temp imaginary */
+	for(i = 0; i < NumSamples; i++)
+	{
+		ri_in[2*i] = RealIn[i];
+		ri_in[2*i+1] = (ImagIn?ImagIn[i]:0.0);
+	}
 
-    if ( !IsPowerOfTwo(NumSamples) )
-    {
-        fprintf (
-            stderr,
-            "Error in fft():  NumSamples=%u is not power of two\n",
-            NumSamples );
+	if(InverseTransform == 0)
+		gsl_power2_fftac (NumSamples, ri_in, ri_out);
+	else
+		gsl_power2_fftsc (NumSamples, ri_in, ri_out);
 
-        exit(1);
-    }
-
-    if ( InverseTransform )
-        angle_numerator = -angle_numerator;
-
-    CHECKPOINTER ( RealIn );
-    CHECKPOINTER ( RealOut );
-    CHECKPOINTER ( ImagOut );
-
-    NumBits = NumberOfBitsNeeded ( NumSamples );
-
-    /*
-    **   Do simultaneous data copy and bit-reversal ordering into outputs...
-    */
-
-    for ( i=0; i < NumSamples; i++ )
-    {
-        j = ReverseBits ( i, NumBits );
-        RealOut[j] = RealIn[i];
-        ImagOut[j] = (ImagIn == NULL) ? 0.0 : ImagIn[i];
-    }
-
-    /*
-    **   Do the FFT itself...
-    */
-
-    BlockEnd = 1;
-    for ( BlockSize = 2; BlockSize <= NumSamples; BlockSize <<= 1 )
-    {
-        double delta_angle = angle_numerator / (double)BlockSize;
-        double sm2 = sin ( -2 * delta_angle );
-        double sm1 = sin ( -delta_angle );
-        double cm2 = cos ( -2 * delta_angle );
-        double cm1 = cos ( -delta_angle );
-        double w = 2 * cm1;
-        double ar[3], ai[3];
-
-        for ( i=0; i < NumSamples; i += BlockSize )
-        {
-            ar[2] = cm2;
-            ar[1] = cm1;
-
-            ai[2] = sm2;
-            ai[1] = sm1;
-
-            for ( j=i, n=0; n < BlockEnd; j++, n++ )
-            {
-                ar[0] = w*ar[1] - ar[2];
-                ar[2] = ar[1];
-                ar[1] = ar[0];
-
-                ai[0] = w*ai[1] - ai[2];
-                ai[2] = ai[1];
-                ai[1] = ai[0];
-
-                k = j + BlockEnd;
-                tr = ar[0]*RealOut[k] - ai[0]*ImagOut[k];
-                ti = ar[0]*ImagOut[k] + ai[0]*RealOut[k];
-
-                RealOut[k] = RealOut[j] - tr;
-                ImagOut[k] = ImagOut[j] - ti;
-
-                RealOut[j] += tr;
-                ImagOut[j] += ti;
-            }
-        }
-
-        BlockEnd = BlockSize;
-    }
-
-    /*
-    **   Need to normalize if inverse transform...
-    */
-
-    if ( InverseTransform )
-    {
-        double denom = (double)NumSamples;
-
-        for ( i=0; i < NumSamples; i++ )
-        {
-            RealOut[i] /= denom;
-            ImagOut[i] /= denom;
-        }
-    }
+	for(i = 0; i < NumSamples; i++)
+	{
+		RealOut[i] = ri_out[2*i];
+		ImagOut[i] = ri_out[2*i+1];
+	}
 }
-
-
