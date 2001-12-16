a8611ffb73841645b0bd7d6c22afedafd6b34285
David Faure <faure@kde.org>
2001-12-16 20:44:20 +0000

    Implemented parsing of dates passed as string. Had to duplicate almost all of krfcdate.cpp, since kjs doesn't link to kdecore :( This fixes #24176 (and any other site that uses document.lastModified for creating a Date object).
    svn path=/trunk/kdelibs/; revision=127444

--- kjs/date_object.cpp
+++ kjs/date_object.cpp
@@ -50,9 +50,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
+#include <ctype.h>
 
 #include "date_object.h"
 #include "error_object.h"
+#include "operations.h"
 
 #include "date_object.lut.h"
 
@@ -336,9 +338,12 @@ bool DateObjectImp::implementsConstruct() const
 // ECMA 15.9.3
 Object DateObjectImp::construct(ExecState *exec, const List &args)
 {
-  Value value;
-
   int numArgs = args.size();
+
+#ifdef KJS_VERBOSE
+  fprintf(stderr,"DateObjectImp::construct - %d args\n", numArgs);
+#endif
+  Value value;
 
   if (numArgs == 0) { // new Date() ECMA 15.9.3.3
 #if HAVE_SYS_TIMEB_H
@@ -359,7 +364,7 @@ Object DateObjectImp::construct(ExecState *exec, const List &args)
   } else if (numArgs == 1) {
     UString s = args[0].toString(exec);
     double d = s.toDouble();
-    if (d == NaN)
+    if (isNaN(d))
       value = parseDate(s);
     else
       value = Number(d);
@@ -394,6 +399,9 @@ bool DateObjectImp::implementsCall() const
 // ECMA 15.9.2
 Value DateObjectImp::call(ExecState */*exec*/, Object &/*thisObj*/, const List &/*args*/)
 {
+#ifdef KJS_VERBOSE
+  fprintf(stderr,"DateObjectImp::call - current time\n");
+#endif
   time_t t = time(0L);
   UString s(ctime(&t));
 
@@ -425,7 +433,7 @@ Value DateObjectFuncImp::call(ExecState *exec, Object &/*thisObj*/, const List &
     else
       return Undefined();
   }
-  else {
+  else { // UTC
     struct tm t;
     memset(&t, 0, sizeof(t));
     int n = args.size();
@@ -449,12 +457,17 @@ Value DateObjectFuncImp::call(ExecState *exec, Object &/*thisObj*/, const List &
 Value KJS::parseDate(const String &s)
 {
   UString u = s.value();
+#ifdef KJS_VERBOSE
+  fprintf(stderr,"KJS::parseDate %s\n",u.ascii());
+#endif
   int firstSlash = u.find('/');
   if ( firstSlash == -1 )
   {
-    // TODO parse dates like "December 25, 1995 23:15:00"
-    fprintf(stderr,"KJS::parseDate parsing for this format isn't implemented\n%s", u.ascii());
-    return Number(0);
+    time_t seconds = KRFCDate_parseDate( u );
+    if ( seconds == -1 )
+      return Undefined();
+    else
+      return Number(seconds * 1000.0);
   }
   else
   {
@@ -486,6 +499,249 @@ Value KJS::parseDate(const String &s)
       return Number(seconds * 1000.0);
   }
 }
+
+///// Awful duplication from krfcdate.cpp - we don't link to kdecore
+
+static unsigned int ymdhms_to_seconds(int year, int mon, int day, int hour, int minute, int second)
+{
+    unsigned int ret = (day - 32075)       /* days */
+            + 1461L * (year + 4800L + (mon - 14) / 12) / 4
+            + 367 * (mon - 2 - (mon - 14) / 12 * 12) / 12
+            - 3 * ((year + 4900L + (mon - 14) / 12) / 100) / 4
+            - 2440588;
+    ret = 24*ret + hour;     /* hours   */
+    ret = 60*ret + minute;   /* minutes */
+    ret = 60*ret + second;   /* seconds */
+
+    return ret;
+}
+
+static const char haystack[37]="janfebmaraprmayjunjulaugsepoctnovdec";
+
+// we follow the recommendation of rfc2822 to consider all
+// obsolete time zones not listed here equivalent to "-0000"
+static const struct {
+    const char *tzName;
+    int tzOffset;
+} known_zones[] = {
+    { "UT", 0 },
+    { "GMT", 0 },
+    { "EST", -300 },
+    { "EDT", -240 },
+    { "CST", -360 },
+    { "CDT", -300 },
+    { "MST", -420 },
+    { "MDT", -360 },
+    { "PST", -480 },
+    { "PDT", -420 },
+    { 0, 0 }
+};
+
+time_t KJS::KRFCDate_parseDate(const UString &_date)
+{
+     // This parse a date in the form:
+     //     Wednesday, 09-Nov-99 23:12:40 GMT
+     // or
+     //     Sat, 01-Jan-2000 08:00:00 GMT
+     // or
+     //     Sat, 01 Jan 2000 08:00:00 GMT
+     // or
+     //     01 Jan 99 22:00 +0100    (exceptions in rfc822/rfc2822)
+     //
+     // We ignore the weekday
+     //
+     time_t result = 0;
+     int offset = 0;
+     char *newPosStr;
+     const char *dateString = _date.ascii();
+     int day = 0;
+     char monthStr[4];
+     int month = 0;
+     int year = 0;
+     int hour = 0;
+     int minute = 0;
+     int second = 0;
+
+     while(*dateString && !isdigit(*dateString))
+     	dateString++;
+
+     if (!*dateString)
+     	return result;  // Invalid date
+
+     // ' 09-Nov-99 23:12:40 GMT'
+     day = strtol(dateString, &newPosStr, 10);
+     dateString = newPosStr;
+
+     if ((day < 1) || (day > 31))
+     	return result; // Invalid date;
+     if (!*dateString)
+     	return result;  // Invalid date
+
+     if (*dateString == '-')
+     	dateString++;
+
+     while(*dateString && isspace(*dateString))
+     	dateString++;
+
+     for(int i=0; i < 3;i++)
+     {
+         if (!*dateString || (*dateString == '-') || isspace(*dateString))
+              return result;  // Invalid date
+         monthStr[i] = tolower(*dateString++);
+     }
+     monthStr[3] = '\0';
+
+     newPosStr = (char*)strstr(haystack, monthStr);
+
+     if (!newPosStr)
+     	return result;  // Invalid date
+
+     month = (newPosStr-haystack)/3; // Jan=00, Feb=01, Mar=02, ..
+
+     if ((month < 0) || (month > 11))
+     	return result;  // Invalid date
+
+     while(*dateString && (*dateString != '-') && !isspace(*dateString))
+     	dateString++;
+
+     if (!*dateString)
+     	return result;  // Invalid date
+
+     // '-99 23:12:40 GMT'
+     if ((*dateString != '-') && !isspace(*dateString))
+     	return result;  // Invalid date
+     dateString++;
+
+     // '99 23:12:40 GMT'
+     year = strtol(dateString, &newPosStr, 10);
+     dateString = newPosStr;
+
+     // Y2K: Solve 2 digit years
+     if ((year >= 0) && (year < 50))
+         year += 2000;
+
+     if ((year >= 50) && (year < 100))
+         year += 1900;  // Y2K
+
+     if ((year < 1900) || (year > 2500))
+     	return result; // Invalid date
+
+     // Don't fail if the time is missing.
+     if (*dateString)
+     {
+        // ' 23:12:40 GMT'
+        if (!isspace(*dateString++))
+           return result;  // Invalid date
+
+        hour = strtol(dateString, &newPosStr, 10);
+        dateString = newPosStr;
+
+        if ((hour < 0) || (hour > 23))
+           return result; // Invalid date
+
+        if (!*dateString)
+           return result;  // Invalid date
+
+        // ':12:40 GMT'
+        if (*dateString++ != ':')
+           return result;  // Invalid date
+
+        minute = strtol(dateString, &newPosStr, 10);
+        dateString = newPosStr;
+
+        if ((minute < 0) || (minute > 59))
+           return result; // Invalid date
+
+        if (!*dateString)
+           return result;  // Invalid date
+
+        // ':40 GMT'
+        if (*dateString != ':' && !isspace(*dateString))
+           return result;  // Invalid date
+
+        // seconds are optional in rfc822 + rfc2822
+        if (*dateString ==':') {
+           dateString++;
+
+           second = strtol(dateString, &newPosStr, 10);
+           dateString = newPosStr;
+
+           if ((second < 0) || (second > 59))
+              return result; // Invalid date
+        } else {
+           dateString++;
+        }
+
+        while(*dateString && isspace(*dateString))
+           dateString++;
+     }
+
+     // don't fail if the time zone is missing, some
+     // broken mail-/news-clients omit the time zone
+     if (*dateString) {
+
+        if ((*dateString == '+') || (*dateString == '-')) {
+           offset = strtol(dateString, &newPosStr, 10);
+
+           if ((offset < -9959) || (offset > 9959))
+              return result; // Invalid date
+
+           int sgn = (offset < 0)? -1:1;
+           offset = abs(offset);
+           offset = ((offset / 100)*60 + (offset % 100))*sgn;
+        } else {
+           for (int i=0; known_zones[i].tzName != 0; i++) {
+              if (0 == strncasecmp(dateString, known_zones[i].tzName, strlen(known_zones[i].tzName))) {
+                 offset = known_zones[i].tzOffset;
+                 break;
+              }
+           }
+        }
+     }
+     if (sizeof(time_t) == 4)
+     {
+         if ((time_t)-1 < 0)
+         {
+            if (year >= 2038)
+            {
+               year = 2038;
+               month = 0;
+               day = 1;
+               hour = 0;
+               minute = 0;
+               second = 0;
+            }
+         }
+         else
+         {
+            if (year >= 2115)
+            {
+               year = 2115;
+               month = 0;
+               day = 1;
+               hour = 0;
+               minute = 0;
+               second = 0;
+            }
+         }
+     }
+
+     result = ymdhms_to_seconds(year, month+1, day, hour, minute, second);
+
+     // avoid negative time values
+     if ((offset > 0) && (offset > result))
+        offset = 0;
+
+     result -= offset*60;
+
+     // If epoch 0 return epoch +1 which is Thu, 01-Jan-70 00:00:01 GMT
+     // This is so that parse error and valid epoch 0 return values won't
+     // be the same for sensitive applications...
+     if (result < 1) result = 1;
+
+     return result;
+}
+
 
 Value KJS::timeClip(const Value &t)
 {
--- kjs/date_object.h
+++ kjs/date_object.h
@@ -119,6 +119,7 @@ namespace KJS {
 
   // helper functions
   Value parseDate(const String &s);
+  time_t KRFCDate_parseDate(const UString &_date);
   Value timeClip(const Value &t);
 
 }; // namespace
