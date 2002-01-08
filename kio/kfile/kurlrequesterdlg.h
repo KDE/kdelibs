1b292f8cdf840de0cffdc8cd841f062319f53de0
Wilco Greven <greven@kde.org>
2002-01-08 21:10:48 +0000

    Use @kde.org address.
    svn path=/trunk/kdelibs/; revision=130662

--- kio/kfile/kurlrequesterdlg.h
+++ kio/kfile/kurlrequesterdlg.h
@@ -1,5 +1,5 @@
 /* This file is part of the KDE libraries
-    Copyright (C) 2000 Wilco Greven <j.w.greven@student.utwente.nl>
+    Copyright (C) 2000 Wilco Greven <greven@kde.org>
 
     library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Library General Public
