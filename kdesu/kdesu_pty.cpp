b02f35abba0cf6feec3710fd1ca0935254b4901a
Waldo Bastian <bastian@kde.org>
2002-01-12 01:08:01 +0000

    CCMAIL: alane@geeksrus.net Patch by Alan Eldridge <alane@geeksrus.net> to fix problems on FreeBSD
    1. Array "name" is not long enough. This results in memory corruption.
     PATH_MAX or MAXPATHLEN not used because this would involve a change to
     configure.in.in to determine what symbol to use.
    
     2. Slave_fd is never closed. This results in fd and pty leakage in
     kdesu, which may repeatedly instantiate SuProcess objects in an
     attempt to get a good pasword from the user. The master side of the
     connection shouldn't be holding on to an open fd for the slave process's
     terminal, anyway.
    
    @@ -101,13 +101,13 @@
     #elif defined(HAVE_OPENPTY)
         // 2: BSD interface
         // More prefered than the linux hacks
    -    char name[10];
    +    char name[30];
         int master_fd, slave_fd;
         if (openpty(&master_fd, &slave_fd, name, 0L, 0L) != -1)  {
            ttyname = name;
            name[5]='p';
            ptyname = name;
    -        //close(slave_fd); // We don't need this yet // Yes, we do.
    +        close(slave_fd); // We don't need this yet // Yes, we do.
            ptyfd = master_fd;
            return ptyfd;
         }
    
    svn path=/trunk/kdelibs/; revision=131266

--- kdesu/kdesu_pty.cpp
+++ kdesu/kdesu_pty.cpp
@@ -101,13 +101,13 @@ int PTY::getpt()
 #elif defined(HAVE_OPENPTY)
     // 2: BSD interface
     // More prefered than the linux hacks
-    char name[10];
+    char name[30];
     int master_fd, slave_fd;
     if (openpty(&master_fd, &slave_fd, name, 0L, 0L) != -1)  {
 	ttyname = name;
 	name[5]='p';
 	ptyname = name;
-        //close(slave_fd); // We don't need this yet // Yes, we do.
+        close(slave_fd); // We don't need this yet // Yes, we do.
 	ptyfd = master_fd;
 	return ptyfd;
     }
