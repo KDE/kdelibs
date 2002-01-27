21bd9721178b6e0d662f9a3ddc8d4ae0fee57910
Dirk Mueller <mueller@kde.org>
2002-01-27 02:49:08 +0000

    raise ksycoca version number for 3.0beta2
    svn path=/trunk/kdelibs/; revision=134068

--- kdecore/ksycoca.h
+++ kdecore/ksycoca.h
@@ -34,7 +34,7 @@ class KSycocaFactoryList;
  * If the existing file is outdated, it will not get read
  * but instead we'll ask kded to regenerate a new one...
 */
-#define KSYCOCA_VERSION 43
+#define KSYCOCA_VERSION 44444
 
 /**
  * @internal
