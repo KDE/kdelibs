edb2da11c772dd6347b0421e0e2c83afac01548c
Ellis Whitehead <kde@ellisw.net>
2002-01-27 14:11:44 +0000

    wrapper for handling shortcut configuration, regardless of whether we're working on KActionCollection, KAccelActions, KStdAccel, or KActionPtrList
    svn path=/trunk/kdelibs/; revision=134148

---
+++ kdecore/kshortcutlist.cpp
@@ -0,0 +1,194 @@
+#include <qstring.h>
+#include <qvariant.h>
+
+#include <kaccel.h>
+#include <kaccelaction.h>
+#include <kconfig.h>
+#include <kdebug.h>
+#include <kglobal.h>
+#include <kglobalaccel.h>
+#include <kinstance.h>
+#include <kshortcut.h>
+#include "kshortcutlist.h"
+
+//---------------------------------------------------------------------
+// KShortcutList
+//---------------------------------------------------------------------
+
+KShortcutList::KShortcutList()
+{
+}
+
+KShortcutList::~KShortcutList()
+{
+}
+
+bool KShortcutList::isGlobal( uint ) const
+{
+	return false;
+}
+
+// FIXME: implement!
+int KShortcutList::index( const QString& /*sName*/ ) const
+{
+	return -1;
+}
+
+int KShortcutList::index( const KKeySequence& seq ) const
+{
+	if( seq.isNull() )
+		return -1;
+
+	uint nSize = count();
+	for( uint i = 0; i < nSize; i++ ) {
+		if( shortcut(i).contains( seq ) )
+			return i;
+	}
+
+	return -1;
+}
+
+const KInstance* KShortcutList::instance() const
+{
+	return 0;
+}
+
+QVariant KShortcutList::getOther( Other, uint ) const
+{
+	return QVariant();
+}
+
+bool KShortcutList::setOther( Other, uint, QVariant )
+{
+	return false;
+}
+
+bool KShortcutList::readSettings( const QString& sConfigGroup, KConfigBase* pConfig )
+{
+	kdDebug(125) << "KShortcutList::readSettings( \"" << sConfigGroup << "\", " << pConfig << " ) start" << endl;
+	if( !pConfig )
+		pConfig = KGlobal::config();
+	QString sGroup = (!sConfigGroup.isEmpty()) ? sConfigGroup : QString("Shortcuts");
+
+	// If the config file still has the old group name:
+	// FIXME: need to rename instead? -- and don't do this if hasGroup( "Shortcuts" ).
+	if( sGroup == "Shortcuts" && pConfig->hasGroup( "Keys" ) ) {
+		readSettings( "Keys", pConfig );
+	}
+
+	kdDebug(125) << "\treadSettings( \"" << sGroup << "\", " << pConfig << " )" << endl;
+	if( !pConfig->hasGroup( sGroup ) )
+		return true;
+	KConfigGroupSaver cgs( pConfig, sGroup );
+
+	uint nSize = count();
+	for( uint i = 0; i < nSize; i++ ) {
+		if( isConfigurable(i) ) {
+			QString sEntry = pConfig->readEntry( name(i) );
+			if( !sEntry.isNull() ) {
+				if( sEntry == "none" )
+					setShortcut( i, KShortcut() );
+				else
+					setShortcut( i, KShortcut(sEntry) );
+			}
+			kdDebug(125) << "\t" << name(i) << " = '" << sEntry << "'" << endl;
+		}
+	}
+
+	kdDebug(125) << "KShortcutList::readSettings done" << endl;
+	return true;
+}
+
+bool KShortcutList::writeSettings( const QString &sConfigGroup, KConfigBase* pConfig, bool bWriteAll, bool bGlobal ) const
+{
+	kdDebug(125) << "KShortcutList::writeSettings( " << sConfigGroup << ", " << pConfig << ", " << bWriteAll << ", " << bGlobal << " )" << endl;
+	if( !pConfig )
+		pConfig = KGlobal::config();
+
+	QString sGroup = (!sConfigGroup.isEmpty()) ? sConfigGroup : QString("Shortcuts");
+
+	// If it has the depricated group [Keys], remove it
+	if( pConfig->hasGroup( "Keys" ) )
+		pConfig->deleteGroup( "Keys" );
+
+	KConfigGroupSaver cs( pConfig, sGroup );
+
+	uint nSize = count();
+	for( uint i = 0; i < nSize; i++ ) {
+		if( isConfigurable(i) ) {
+			const QString& sName = name(i);
+			bool bConfigHasAction = !pConfig->readEntry( sName ).isEmpty();
+			bool bSameAsDefault = (shortcut(i) == shortcutDefault(i));
+			// If we're using a global config or this setting
+			//  differs from the default, then we want to write.
+			if( bWriteAll || !bSameAsDefault ) {
+				QString s = shortcut(i).toStringInternal();
+				if( s.isEmpty() )
+					s = "none";
+				kdDebug(125) << "\twriting " << sName << " = " << s << endl;
+				// Is passing bGlobal irrelevant, since if it's true,
+				//  then we're using the global config anyway? --ellis
+				pConfig->writeEntry( sName, s, true, bGlobal );
+			}
+			// Otherwise, this key is the same as default
+			//  but exists in config file.  Remove it.
+			else if( bConfigHasAction ) {
+				kdDebug(125) << "\tremoving " << sName << " because == default" << endl;
+				pConfig->deleteEntry( sName, bGlobal );
+			}
+		}
+	}
+
+	pConfig->sync();
+	return true;
+}
+
+//---------------------------------------------------------------------
+// KAccelShortcutList
+//---------------------------------------------------------------------
+
+KAccelShortcutList::KAccelShortcutList( KAccel* pAccel )
+: m_actions( pAccel->actions() )
+{
+	m_bGlobal = false;
+}
+
+KAccelShortcutList::KAccelShortcutList( KGlobalAccel* pAccel )
+: m_actions( pAccel->actions() )
+{
+	m_bGlobal = true;
+}
+
+KAccelShortcutList::KAccelShortcutList( KAccelActions& actions, bool bGlobal )
+: m_actions( actions )
+{
+	m_bGlobal = bGlobal;
+}
+
+
+KAccelShortcutList::~KAccelShortcutList()
+	{ }
+uint KAccelShortcutList::count() const
+	{ return m_actions.count(); }
+QString KAccelShortcutList::name( uint i ) const
+	{ return m_actions.actionPtr(i)->name(); }
+QString KAccelShortcutList::label( uint i ) const
+	{ return m_actions.actionPtr(i)->label(); }
+QString KAccelShortcutList::whatsThis( uint i ) const
+	{ return m_actions.actionPtr(i)->whatsThis(); }
+const KShortcut& KAccelShortcutList::shortcut( uint i ) const
+	{ return m_actions.actionPtr(i)->shortcut(); }
+const KShortcut& KAccelShortcutList::shortcutDefault( uint i ) const
+	{ return m_actions.actionPtr(i)->shortcutDefault(); }
+bool KAccelShortcutList::isConfigurable( uint i ) const
+	{ return m_actions.actionPtr(i)->isConfigurable(); }
+bool KAccelShortcutList::setShortcut( uint i, const KShortcut& cut )
+	{ return m_actions.actionPtr(i)->setShortcut( cut ); }
+QVariant KAccelShortcutList::getOther( Other, uint ) const
+	{ return QVariant(); }
+bool KAccelShortcutList::isGlobal( uint ) const
+	{ return m_bGlobal; }
+bool KAccelShortcutList::setOther( Other, uint, QVariant )
+	{ return false; }
+bool KAccelShortcutList::save() const
+	{ return writeSettings(); }
---
+++ kdecore/kshortcutlist.h
@@ -0,0 +1,136 @@
+/*  This file is part of the KDE libraries
+    Copyright (C) 2002 Ellis Whitehead <ellis@kde.org>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+    Boston, MA 02111-1307, USA.
+*/
+
+#ifndef __KSHORTCUTLIST_H
+#define __KSHORTCUTLIST_H
+
+class QString;
+class QVariant;
+class KInstance;
+class KShortcut;
+
+/**********************************************************************
+* This is a wrapper class which allows a function to use one interface
+* to KActionCollection, KAccelActions, and KActionPtrList.
+**********************************************************************/
+
+class KShortcutList
+{
+ public:
+	KShortcutList();
+	virtual ~KShortcutList();
+
+	virtual uint count() const = 0;
+	virtual QString name( uint ) const = 0;
+	virtual QString label( uint ) const = 0;
+	virtual QString whatsThis( uint ) const = 0;
+	virtual const KShortcut& shortcut( uint ) const = 0;
+	virtual const KShortcut& shortcutDefault( uint ) const = 0;
+	virtual bool isConfigurable( uint ) const = 0;
+	virtual bool setShortcut( uint, const KShortcut& ) = 0;
+	virtual bool isGlobal( uint ) const;
+
+	virtual int index( const QString& sName ) const;
+	virtual int index( const KKeySequence& ) const;
+	virtual const KInstance* instance() const;
+
+	// These are here in order to handle expansion.
+	enum Other { };
+	virtual QVariant getOther( Other, uint index ) const = 0;
+	virtual bool setOther( Other, uint index, QVariant ) = 0;
+
+	virtual bool save() const = 0;
+	virtual bool readSettings( const QString& sConfigGroup = QString::null, KConfigBase* pConfig = 0 );
+	virtual bool writeSettings( const QString& sConfigGroup = QString::null, KConfigBase* pConfig = 0,
+			bool bWriteAll = false, bool bGlobal = false ) const;
+
+ private:
+	class KShortcutListPrivate* d;
+};
+
+//---------------------------------------------------------------------
+// KAccelShortcutList
+//---------------------------------------------------------------------
+
+class KAccel;
+class KAccelActions;
+class KGlobalAccel;
+
+class KAccelShortcutList : public KShortcutList
+{
+ public:
+	KAccelShortcutList( KAccel* );
+	KAccelShortcutList( KGlobalAccel* );
+	KAccelShortcutList( KAccelActions&, bool bGlobal );
+	virtual ~KAccelShortcutList();
+
+	virtual uint count() const;
+	virtual QString name( uint i ) const;
+	virtual QString label( uint ) const;
+	virtual QString whatsThis( uint ) const;
+	virtual const KShortcut& shortcut( uint ) const;
+	virtual const KShortcut& shortcutDefault( uint ) const;
+	virtual bool isConfigurable( uint ) const;
+	virtual bool setShortcut( uint, const KShortcut& );
+	virtual bool isGlobal( uint ) const;
+
+	virtual QVariant getOther( Other, uint index ) const;
+	virtual bool setOther( Other, uint index, QVariant );
+
+	virtual bool save() const;
+
+ protected:
+	KAccelActions& m_actions;
+	bool m_bGlobal;
+
+ private:
+	class KAccelShortcutListPrivate* d;
+};
+
+namespace KStdAccel {
+//---------------------------------------------------------------------
+// ShortcutList
+//---------------------------------------------------------------------
+
+class ShortcutList : public KShortcutList
+{
+ public:
+	ShortcutList();
+	virtual ~ShortcutList();
+
+	virtual uint count() const;
+	virtual QString name( uint i ) const;
+	virtual QString label( uint ) const;
+	virtual QString whatsThis( uint ) const;
+	virtual const KShortcut& shortcut( uint ) const;
+	virtual const KShortcut& shortcutDefault( uint ) const;
+	virtual bool isConfigurable( uint ) const;
+	virtual bool setShortcut( uint, const KShortcut& );
+
+	virtual QVariant getOther( Other, uint index ) const;
+	virtual bool setOther( Other, uint index, QVariant );
+
+	virtual bool save() const;
+
+ private:
+	class ShortcutListPrivate* d;
+};
+};
+
+#endif // __KSHORTCUTLIST_H
