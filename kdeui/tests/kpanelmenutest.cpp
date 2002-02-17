edfb223028e4513b83544d2f0bd370f8d1d395f4
Waldo Bastian <bastian@kde.org>
2002-02-17 07:07:21 +0000

    Make it compile.
    svn path=/trunk/kdelibs/; revision=136932

--- kdeui/tests/kpanelmenutest.cpp
+++ kdeui/tests/kpanelmenutest.cpp
@@ -6,14 +6,14 @@
 TestWidget::TestWidget(QWidget *parent, const char *name)
     : QLabel(parent, name)
 {
-    testMenu = new KPanelMenu(locate("mini", "x.png"), "Client Test", this,
+    testMenu = new KPanelAppMenu(locate("mini", "x.png"), "Client Test", this,
                               "menu1");
 
     subMenu = testMenu->insertMenu(locate("mini", "x.png"), "Submenu Test");
     subMenu->insertItem(locate("mini", "bx2.png"), "First Entry", 100);
     subMenu->insertItem(locate("mini", "bx2.png"), "Second Entry", 101);
 
-    KPanelMenu *ssub = subMenu->insertMenu(locate("mini", "x.png"), "One more");
+    KPanelAppMenu *ssub = subMenu->insertMenu(locate("mini", "x.png"), "One more");
    
     connect(testMenu, SIGNAL(activated(int)), SLOT(slotMenuCalled(int)));
     connect(subMenu, SIGNAL(activated(int)), SLOT(slotSubMenuCalled(int)));
--- kdeui/tests/kpanelmenutest.h
+++ kdeui/tests/kpanelmenutest.h
@@ -1,7 +1,7 @@
 #ifndef __KPANELMENUTEST_H
 #define __KPANELMENUTEST_H
 
-#include <kpanelmenu.h>
+#include <kpanelappmenu.h>
 #include <qlabel.h>
 
 class TestWidget : public QLabel
@@ -15,7 +15,7 @@ public slots:
     void slotSubMenuCalled(int id);
 private:
     void init();
-    KPanelMenu *testMenu, *subMenu;
+    KPanelAppMenu *testMenu, *subMenu;
 };
 
 #endif
