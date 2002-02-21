1ab424cd41101bb449f28d1130dcc189165f3d29
Michael Goffioul <kdeprint@swing.be>
2002-02-21 14:47:03 +0000

    Extension to start server configuration tool
    svn path=/trunk/kdelibs/; revision=137847

--- kdeprint/management/kaddprinterwizard.cpp
+++ kdeprint/management/kaddprinterwizard.cpp
@@ -7,6 +7,7 @@
 static KCmdLineOptions options[] =
 {
 	{ "config", I18N_NOOP("Configure KDE Print"), 0 },
+	{ "serverconfig", I18N_NOOP("Configure print server"), 0 },
 	{0, 0, 0}
 };
 
@@ -20,9 +21,12 @@ int main(int argc, char *argv[])
 	KApplication app;
 	KCmdLineArgs	*args = KCmdLineArgs::parsedArgs();
 	bool	doConfig = args->isSet("config");
+	bool	doSrvConfig = args->isSet("serverconfig");
 
 	if (doConfig)
 		KMManager::self()->invokeOptionsDialog();
+	else if (doSrvConfig)
+		KMManager::self()->configureServer();
 	else if (KMManager::self()->addPrinterWizard() == -1)
 	{
 		KMessageBox::error(0, KMManager::self()->errorMsg().prepend("<qt>").append("</qt>"));
