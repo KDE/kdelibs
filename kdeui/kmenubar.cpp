5a43c2786afee15a4d09781e6ed57293cd59b60f
Kevin Puetz <kde@puetzk.org>
2002-03-01 03:24:19 +0000

    Add a hidden pref in kdeglobals to pick which Xinerama screen the menubar goes to (instead of always going to the leftmost)
    we'll add a UI in 3.1 sometime, but this makes it possible for a
    user to do something about the selection, and will be needed to support
    a UI anyway.
    
    svn path=/trunk/kdelibs/; revision=139742

--- kdeui/kmenubar.cpp
+++ kdeui/kmenubar.cpp
@@ -153,7 +153,9 @@ bool KMenuBar::eventFilter(QObject *obj, QEvent *ev)
 void KMenuBar::showEvent( QShowEvent *e )
 {
     if ( d->topLevel ) {
-        int screen = QApplication::desktop()->screenNumber(QPoint(0,0));
+        KConfigGroup xineramaConfig(KGlobal::config(),"Xinerama");
+        int screen = xineramaConfig.readNumEntry("MenubarScreen",
+            QApplication::desktop()->screenNumber(QPoint(0,0)) );
         QRect area = QApplication::desktop()->screenGeometry(screen);
         setGeometry(area.left(), area.top()-frameWidth()-2, area.width(), heightForWidth( area.width() ) );
 #ifndef Q_WS_QWS //FIXME
