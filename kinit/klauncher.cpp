c9ecf2800540bb41639e96f24ef51b8e946d78f2
Reza Arbab <arbab@austin.ibm.com>
2002-03-08 18:56:03 +0000

    Need <config.h> for FD_ZERO fix on AIX.
    svn path=/trunk/kdelibs/; revision=141843

--- kinit/klauncher.cpp
+++ kinit/klauncher.cpp
@@ -16,6 +16,9 @@
   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
   Boston, MA 02111-1307, USA.
 */
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
 
 #include <stdio.h>
 #include <unistd.h>
