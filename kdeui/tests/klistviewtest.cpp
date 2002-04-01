ed8dc5934b363d622c7892c2decc6feb751ebca0
Carsten Pfeiffer <pfeiffer@kde.org>
2002-04-01 13:24:04 +0000

    eins, zwo, drei, TEST TEST
    svn path=/trunk/kdelibs/; revision=146416

---
+++ kdeui/tests/klistviewtest.cpp
@@ -0,0 +1,25 @@
+#include <klistview.h>
+#include <kapplication.h>
+#include <kdialogbase.h>
+#include <qvbox.h>
+
+
+int main( int argc, char **argv )
+{
+	KApplication app( argc, argv, "klistviewtest" );
+	KDialogBase dialog;
+	KListView *view = new KListView( dialog.makeVBoxMainWidget() );
+	view->addColumn("Column 1");
+	view->addColumn("Column 2");
+	view->addColumn("Column 3");
+
+	new QListViewItem( view, "Item 1");
+	new QListViewItem( view, "Item 2", "Some more", "Hi Mom :)" );	
+
+
+	view->restoreLayout( KGlobal::config(), "ListView" );
+	dialog.exec();
+	view->saveLayout( KGlobal::config(), "ListView" );
+
+	return 0;
+}
