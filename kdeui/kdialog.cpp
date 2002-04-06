a2d0726f04e8fe06d902e059dddf8e15aaf5c7a4
David Faure <faure@kde.org>
2002-04-06 14:49:10 +0000

    Fixed caption in dialogs. The auto-added appname was given to QDialog::setCaption() but not to NETWinInfo.setName, and apparently the latter matters, not the former!
    OK to backport? If any app was providing its own name in the caption manually,
    then it wasn't following the spec for setCaption (see docu in kdialog.h)...
    
    svn path=/trunk/kdelibs/; revision=147445

--- kdeui/kdialog.cpp
+++ kdeui/kdialog.cpp
@@ -28,6 +28,7 @@
 
 #include <kapplication.h>
 #include <kdialog.h>
+#include <kdebug.h> 
 #include <kstaticdeleter.h>
 
 int KDialog::mMarginSize = 11;
@@ -97,10 +98,10 @@ void KDialog::polish()
 }
 
 
-void KDialog::setCaption( const QString &caption )
+void KDialog::setCaption( const QString &_caption )
 {
-  if (kapp)
-    QDialog::setCaption( kapp->makeStdCaption( caption ) );
+  QString caption = kapp ? kapp->makeStdCaption( _caption ) : _caption;
+  QDialog::setCaption( caption );
 
 #ifdef Q_WS_X11 //FIXME(E) Implement for Qt/E
   NETWinInfo info( qt_xdisplay(), winId(), qt_xrootwin(), 0 );
