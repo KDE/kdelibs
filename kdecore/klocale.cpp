123c8360c8c17a301dc857aabf64858b661deca9
John Dailey <dailey@vt.edu>
2002-05-17 15:04:59 +0000

    used correct decimal separator
    svn path=/trunk/kdelibs/; revision=155994

--- kdecore/klocale.cpp
+++ kdecore/klocale.cpp
@@ -1122,7 +1122,7 @@ double KLocale::readNumber(const QString &_str, bool * ok) const
   QString tot;
   if (neg) tot = '-';
 
-  tot += major + '.' + minor + exponentialPart;
+  tot += major + decimalSymbol() + minor + exponentialPart;
 
   return tot.toDouble(ok);
 }
