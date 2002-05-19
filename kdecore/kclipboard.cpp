cb28e74367c2d91f86e733af00c7a2db803f9933
Carsten Pfeiffer <pfeiffer@kde.org>
2002-05-19 02:32:57 +0000

    a frontend to QClipboard to make - the API easier - allow synchronization of clipboard & selection - automatically set the selection when the clipboard is set
    svn path=/trunk/kdelibs/; revision=156256

---
+++ kdecore/kclipboard.cpp
@@ -0,0 +1,97 @@
+/* This file is part of the KDE libraries
+    Copyright (C) 2002 Carsten Pfeiffer <pfeiffer@kde.org>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+    Boston, MA 02111-1307, USA.
+*/
+
+#include <qapplication.h>
+#include <qclipboard.h>
+
+#include <kconfig.h>
+#include <kglobal.h>
+
+#include "kclipboard.h"
+
+bool KClipboard::s_sync = false;
+bool KClipboard::s_implicitSelection = true;
+
+void KClipboard::setText( const QString& text, uint mode, bool honorConfiguration )
+{
+    setClipboard( text, 0L, mode, honorConfiguration );
+}
+
+void KClipboard::setData( QMimeSource *data, uint mode, bool honorConfiguration )
+{
+    setClipboard( QString::null, data, mode, honorConfiguration );
+}
+
+void KClipboard::setClipboard( const QString& text, QMimeSource *data,
+                               uint mode, bool honorConfiguration )
+{
+    init();
+
+    QClipboard *clip = QApplication::clipboard();
+    bool oldMode = clip->selectionModeEnabled();
+
+    mode = applyConfig( mode, honorConfiguration );
+
+    if ( mode & Clipboard )
+    {
+        clip->setSelectionMode( false );
+        if ( data )
+            clip->setData( data );
+        else
+            clip->setText( text );
+    }
+
+    if ( mode & Selection )
+    {
+        clip->setSelectionMode( true );
+        if ( data )
+            clip->setData( data );
+        else
+            clip->setText( text );
+    }
+
+    clip->setSelectionMode( oldMode );
+}
+
+uint KClipboard::applyConfig( uint mode, bool honorConfiguration )
+{
+    if ( honorConfiguration )
+    {
+        if ( s_sync )
+            mode = Clipboard | Selection;
+        else if ( s_implicitSelection )
+            mode |= Selection;
+    }
+
+    return mode;
+}
+
+void KClipboard::init()
+{
+    static bool initialized = false;
+    if ( !initialized )
+    {
+        initialized = true;
+        KConfigGroup config( KGlobal::config(), "General" );
+        s_sync = config.readBoolEntry( "SynchronizeClipbboardAndSelection",
+                                       s_sync );
+        s_implicitSelection = config.readBoolEntry( "ImplicitlySetSelection",
+                                                    s_implicitSelection );
+    }
+}
---
+++ kdecore/tests/kclipboardtest.cpp
@@ -0,0 +1,158 @@
+/* This file is part of the KDE libraries
+    Copyright (c) 2002 Carsten Pfeiffer <pfeiffer@kde.org>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License version 2 as published by the Free Software Foundation.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+    Boston, MA 02111-1307, USA.
+*/
+
+#include "kapplication.h"
+#include "kclipboard.h"
+
+#include <qclipboard.h>
+
+static QClipboard *clip = 0L;
+static bool allOk = true;
+
+static void printData()
+{
+    int i = 0;
+    const char *format = 0;
+    QMimeSource *data = clip->data();
+
+    qDebug("  Serialnumber: %i", data->serialNumber() );
+    
+    while ( (format = data->format( i++ ) ) ) {
+        QByteArray array = data->encodedData( format );
+        qDebug("  Format: %s: size of data: %i", format, array.size());
+    }
+}
+
+static void printContents()
+{
+    bool oldMode = clip->selectionModeEnabled();
+    
+    qDebug("::Text:");
+    
+    clip->setSelectionMode( true );
+    qDebug("Selection Text: %s", clip->text().local8Bit().data() );
+    clip->setSelectionMode( false );
+    qDebug("Clipboard Text: %s", clip->text().local8Bit().data() );
+
+    qDebug("\n::Data:");
+
+    qDebug("-> Selection:");
+    clip->setSelectionMode( true );
+    printData();
+
+    qDebug("\n-> Clipboard:");
+    clip->setSelectionMode( false );
+    printData();
+    
+    qDebug("\n\n");
+    
+    clip->setSelectionMode( oldMode );
+}
+
+static void checkResult( const QString& text )
+{
+    bool oldMode = clip->selectionModeEnabled();
+    bool selectionMode = oldMode;
+    bool clipboardMode = !oldMode;
+    
+    clip->setSelectionMode( false );
+    QString clipboard = clip->text();
+    clip->setSelectionMode( true );
+    QString selection = clip->text();
+    
+    
+    if ( KClipboard::isSynchronizing() )
+    {
+        if ( text != clipboard || text != selection )
+        {
+            qDebug("*** Synchronize mode: Bug: clipboard is != selection");
+            allOk = false;
+            printContents();
+        }
+    }
+    
+    if ( KClipboard::implicitSelection() && !KClipboard::isSynchronizing() )
+    {
+        if ( clipboardMode && clipboard != selection )
+        {
+            qDebug("*** Implicit selection: Bug: clipboard is != selection");
+            allOk = false;
+            printContents();
+        }
+        else if ( selectionMode && clipboard == selection )
+        {
+            qDebug("*** Implicit selection: Bug: clipboard is == selection");
+            allOk = false;
+            printContents();
+        }
+    }
+    else if ( !KClipboard::implicitSelection() && 
+              !KClipboard::isSynchronizing())
+    {
+        if ( clipboard == selection )
+        {
+            qDebug("*** No implicit selection: Bug: clipboard is == selection");
+            allOk = false;
+            printContents();
+        }
+    }
+    
+    clip->setSelectionMode( oldMode );
+}
+
+static void initClipboard( bool sync, bool implicit, bool selectionMode )
+{
+    qDebug("Now testing: sync: %i, implicit: %i, selectionMode: %i",
+           sync, implicit, selectionMode);
+    clip->setSelectionMode( false );
+    clip->clear();
+    clip->setSelectionMode( true );
+    clip->clear();
+    clip->setSelectionMode( selectionMode );
+
+    KClipboard::setSynchronizing( sync );
+    KClipboard::setImplicitSelection( implicit );
+}
+
+int main(int argc, char *argv[])
+{
+    KApplication a(argc, argv, "kclipboardtest");
+    clip = QApplication::clipboard();
+   
+    QString text = QString::fromLatin1("This is a test. Unbelievable, eh?");
+
+    initClipboard( true, true, false );
+    KClipboard::setText( text, KClipboard::Clipboard );
+    checkResult( text );
+   
+    initClipboard( false, false, false );
+    KClipboard::setText( text, KClipboard::Clipboard );
+    checkResult( text );
+
+    initClipboard( false, true, false );
+    KClipboard::setText( text, KClipboard::Clipboard );
+    checkResult( text );
+
+    initClipboard( false, true, true );
+    KClipboard::setText( text, KClipboard::Selection );
+    checkResult( text );
+    if ( allOk )
+        qDebug("\n\n*** All tests passed without errors.");
+    
+    return allOk ? 0 : 1;
+}
