f45e24f38ef92609934fec9fbb530c8b566be394
Carsten Pfeiffer <pfeiffer@kde.org>
2002-05-26 11:56:21 +0000

    some more e->accept();
    svn path=/trunk/kdelibs/; revision=157863

--- kdeui/ktextedit.cpp
+++ kdeui/ktextedit.cpp
@@ -1,20 +1,20 @@
 /* This file is part of the KDE libraries
-    Copyright (C) 2002 Carsten Pfeiffer <pfeiffer@kde.org>
+   Copyright (C) 2002 Carsten Pfeiffer <pfeiffer@kde.org>
 
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Library General Public
-    License as published by the Free Software Foundation; either
-    version 2 of the License, or (at your option) any later version.
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
 
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Library General Public License for more details.
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
 
-    You should have received a copy of the GNU Library General Public License
-    along with this library; see the file COPYING.LIB.  If not, write to
-    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-    Boston, MA 02111-1307, USA.
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
 */
 
 #include "ktextedit.h"
@@ -46,22 +46,27 @@ void KTextEdit::keyPressEvent( QKeyEvent *e )
 
     if ( KStdAccel::copy().contains( key ) ) {
         copy();
+        e->accept();
 	return;
     }
     else if ( KStdAccel::paste().contains( key ) ) {
         paste();
+        e->accept();
 	return;
     }
     else if ( KStdAccel::cut().contains( key ) ) {
         cut();
+        e->accept();
 	return;
     }
     else if ( KStdAccel::undo().contains( key ) ) {
         undo();
+        e->accept();
 	return;
     }
     else if ( KStdAccel::redo().contains( key ) ) {
         redo();
+        e->accept();
 	return;
     }
     else if ( KStdAccel::deleteWordBack().contains( key ) )
@@ -76,7 +81,7 @@ void KTextEdit::keyPressEvent( QKeyEvent *e )
         e->accept();
         return;
     }
-    
+
     QTextEdit::keyPressEvent( e );
 }
 
