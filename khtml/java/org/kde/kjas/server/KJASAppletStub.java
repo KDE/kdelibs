949d9e7e8b49dbfcf3f5ea13ecb4c8bf590bd940
Till Krech <till@snafu.de>
2002-06-02 02:21:08 +0000

    still not perfect but better. No more paint() Nullpointer Exceptions during applet initialization.
    svn path=/trunk/kdelibs/; revision=158959

--- khtml/java/org/kde/kjas/server/KJASAppletStub.java
+++ khtml/java/org/kde/kjas/server/KJASAppletStub.java
@@ -24,7 +24,7 @@ public class KJASAppletStub extends Frame
     String            windowName;
     String            className;
     Class             appletClass;
-
+    
     KJASAppletClassLoader loader;
     KJASAppletPanel       panel;
     Applet                app;
@@ -43,7 +43,7 @@ public class KJASAppletStub extends Frame
                            String _windowName, KJASAppletClassLoader _loader )
     {
         super( _windowName );
-
+        
         context    = _context;
         appletID   = _appletID;
         codeBase   = _codeBase;
@@ -70,8 +70,10 @@ public class KJASAppletStub extends Frame
      *************************************************************************/
     public void createApplet() {
         panel = new KJASAppletPanel( appletSize );
+        panel.setMessage("Loading...");
         add( "Center", panel );
         pack();
+        setVisible(true);
         runThread = new Thread
         (
         new Runnable() {
@@ -79,54 +81,88 @@ public class KJASAppletStub extends Frame
                 //this order is very important and took a long time
                 //to figure out- don't modify it unless there are
                 //real bug fixes
+                // This whole code is very tricky.
+                // some strange blocking occurs when loading applets.
+                // Display of the applet takes place very late, so that
+                // progress messages etc of applets can not be seen.
+                // Repaints never happen when you want them to happen
+                // during startup.  
                 
                 active = true;
-                synchronized( loader ) {
+                //synchronized( loader ) {
                     try {
                         appletClass = loader.loadClass( className );
                     } catch (Exception e) {
-                        Main.kjas_err("Class could not be loaded " + className, e);
+                        Main.kjas_err("Class could not be loaded: " + className, e);
                     }
-                }
+                //}
                 if( appletClass == null ) {
-                    panel.add( "Center", new Label( "Applet Failed", Label.CENTER ) );
+                    Main.info( "Could not load applet class " + className);
+                    panel.setMessage("Applet Failed");
                     return;
-                }
-                
+                }                
                 try {
-                    app = (Applet) appletClass.newInstance();
+                    synchronized (appletClass) {
+                        Main.debug("Applet " + appletName + " id=" + appletID + " instantiating...");
+                        app = (Applet) appletClass.newInstance();
+                        Main.debug("Applet " + appletName + " id=" + appletID + " instantiated.");
+                    }
                     app.setStub( me );
                 }
                 catch( InstantiationException e ) {
                     Main.kjas_err( "Could not instantiate applet", e );
-                    panel.add( "Center", new Label( "Applet Failed", Label.CENTER ) );
+                    panel.setMessage("Applet Failed");
                     return;
                 }
                 catch( IllegalAccessException e ) {
                     Main.kjas_err( "Could not instantiate applet", e );
-                    panel.add( "Center", new Label( "Applet Failed", Label.CENTER ) );
+                    panel.setMessage("Applet Failed");
                     return;
                 }
-        
-                //app.setVisible( false );
-                // with the preceding line, IllegalArgumentExceptions occur
-                // with certain applets (eg. Animator applet from 1.4 demos)
-                // so, don't do this!
-                remove(panel);
-                add( "Center", app );
-                invalidate();
-                setVisible(true);
-                context.showStatus("Initializing Applet: " + appletName + " ...");
-                // Main.info("Initializing Applet................");
+                
+                app.setVisible(false);
+                Main.debug("panel.add( \"Center\", app );");                
+                panel.setApplet( app );
+                //++Main.debug("app.validate();");                
+                //++app.validate();
+                Main.debug("app.setSize(appletSize);");
+                app.setSize(appletSize);
+                                
+                context.showStatus("Initializing Applet " + appletName + " ...");
+                
+                Main.debug("Applet " + appletName + " id=" + appletID + " initializing...");
                 app.init();
+                app.setVisible(true);
+                Main.debug("Applet " + appletName + " id=" + appletID + " initialized.");
+                
+                Main.debug("app.setSize(appletSize);");                
+                app.setSize(appletSize);
+                
+                app.validate();
+                
+                Main.debug("app.setVisible(true);");                
+                app.setVisible(true);
+                Main.debug("panel.setVisible(true);");                
+                panel.setVisible(true);
+                Main.debug("setVisible(true);");                
                 setVisible(true);
-                context.showStatus("Starting Applet: " + appletName + " ...");
-                // Main.info("Starting Applet................");
-                app.start();  //We're already in a thread, so don't create a new one
+                //repaint(1L);
+                //panel.repaint(1L);
+                Main.debug("app.repaint(1L);");                
+                app.repaint(1L);
+ 
+                context.showStatus("Starting Applet " + appletName + " ...");
+                // create a new thread, so we know, when the applet was started
+                
+                Main.debug("Applet " + appletName + " id=" + appletID + " starting...");
+                new KJASAppletThread(app, "KJAS-Applet-" + appletID + "-" + appletName).start(); 
+                Main.debug("Applet " + appletName + " id=" + appletID + " started.");
+                
+                context.showStatus("Applet " + appletName + " started.");
+                //setVisible(true);
             }
         }
-        , "KJAS-Applet-" + appletID + "(" + appletName + ")");
-        // Main.debug("starting runThread................");
+        , "KJAS-AppletStub-" + appletID + "-" + appletName);
         runThread.setContextClassLoader(loader);
         runThread.start();
     }
@@ -157,9 +193,6 @@ public class KJASAppletStub extends Frame
         if( runThread != null && runThread.isAlive() )
             Main.debug( "runThread is active when stub is dying" );
         
-        if (loader != null) {
-            loader.setInactive();
-        }
         active = false;
         dispose();
     }
@@ -188,7 +221,7 @@ public class KJASAppletStub extends Frame
                 Main.protocol.sendResizeAppletCmd( context.getID(), appletID, width, height );
                 appletSize = new Dimension( width, height );
                 panel.setAppletSize( appletSize );
-                pack();
+                //pack();
             }
         }
     }
@@ -244,7 +277,7 @@ public class KJASAppletStub extends Frame
     class KJASAppletPanel extends Panel
     {
         private Dimension size;
-
+        private Label msgLabel = null;
         public KJASAppletPanel( Dimension _size )
         {
             super( new BorderLayout() );
@@ -265,6 +298,38 @@ public class KJASAppletStub extends Frame
         {
             return size;
         }
+        
+        public void paint(Graphics g) {
+            Main.debug("panel paint");
+            super.paint(g);
+        }
+        
+        public void setApplet(Applet applet) {
+            if (msgLabel != null) {
+                remove(msgLabel);
+            }
+            add("Center", applet);
+            validate();  
+        }
+        public void setMessage(String msg) {
+            if (msgLabel != null) {
+                remove(msgLabel);
+            }
+            msgLabel = new Label(msg, Label.CENTER);
+            add("Center", msgLabel);
+            validate();
+        }
     }
-
-}
+    
+    class KJASAppletThread extends Thread {
+        private Applet app;
+        public KJASAppletThread(Applet app, String name) {
+            super(name);
+            this.app = app;
+        }
+        public void run() {
+            app.start();
+        }
+    }
+    
+ }
