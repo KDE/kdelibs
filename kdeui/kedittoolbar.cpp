b143bb16a173fda8f38d330acbcf091159c6f9c8
Alexander Neundorf <neundorf@kde.org>
2002-06-04 07:09:43 +0000

    wasn't this an obvious memory leak  ?
    Alex
    
    @@ -871,6 +871,7 @@ void KEditToolbarWidget::slotUpButton()
    
           // remove the old me
           m_activeList->takeItem(item);
    +      delete item;
    
           // select my clone
           m_activeList->setSelected(clone, true);
    @@ -927,6 +928,7 @@ void KEditToolbarWidget::slotDownButton(
    
           // remove the old me
           m_activeList->takeItem(item);
    +      delete item;
    
           // select my clone
           m_activeList->setSelected(clone, true);
    
    svn path=/trunk/kdelibs/; revision=159272

--- kdeui/kedittoolbar.cpp
+++ kdeui/kedittoolbar.cpp
@@ -871,6 +871,7 @@ void KEditToolbarWidget::slotUpButton()
 
       // remove the old me
       m_activeList->takeItem(item);
+      delete item;
 
       // select my clone
       m_activeList->setSelected(clone, true);
@@ -927,6 +928,7 @@ void KEditToolbarWidget::slotDownButton()
 
       // remove the old me
       m_activeList->takeItem(item);
+      delete item;
 
       // select my clone
       m_activeList->setSelected(clone, true);
