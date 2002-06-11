3fa5242be9df968af82e459ebda5901cb4c32ea5
Dirk Mueller <mueller@kde.org>
2002-06-11 23:27:24 +0000

    limit the font sizes to 0..255px to avoid crashing X on some webpages
    svn path=/trunk/kdelibs/; revision=160429

--- khtml/rendering/font.cpp
+++ khtml/rendering/font.cpp
@@ -178,8 +178,12 @@ void Font::update( QPaintDeviceMetrics* devMetrics ) const
             size = (int)((bestSize*lDpiY) / 72);
     }
 
-//      qDebug("setting font to %s, italic=%d, weight=%d, size=%d", fontDef.family.latin1(), fontDef.italic,
-//   	   fontDef.weight, size );
+    // make sure we don't bust up X11
+    size = KMAX(0, KMIN(255, size));
+
+//       qDebug("setting font to %s, italic=%d, weight=%d, size=%d", fontDef.family.latin1(), fontDef.italic,
+//    	   fontDef.weight, size );
+
 
     f.setPixelSize( size );
 
