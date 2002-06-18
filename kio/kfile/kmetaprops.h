561eb3cf506eeedfcd11633305f120fd4eed474a
Rolf Magnus <ramagnus@kde.org>
2002-06-18 08:25:21 +0000

    corrected license notice in those files (was for GPL instead of LGPL)
    svn path=/trunk/kdelibs/; revision=161789

--- kio/kfile/kmetaprops.h
+++ kio/kfile/kmetaprops.h
@@ -1,21 +1,21 @@
 /* This file is part of the KDE libraries
- * Copyright (C) 2001, 2002 Rolf Magnus <ramagnus@kde.org>
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public
- * License as published by the Free Software Foundation version 2.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; see the file COPYING.  If not, write to
- * the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
- * Boston, MA 02111-1307, USA.
- *
- *  $Id$
+    Copyright (C) 2001,2002 Rolf Magnus <ramagnus@kde.org>
+
+    library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License version 2 as published by the Free Software Foundation.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+    Boston, MA 02111-1307, USA.
+  
+    $Id$
  */
 
 #ifndef __KMETAPROPS_H__
