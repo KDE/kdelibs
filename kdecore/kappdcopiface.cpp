012c211a97965905ee330c02aa1e4e2fa9392119
Dominique Devriese <devriese@kde.org>
2002-06-18 21:58:00 +0000

    LGPL == Library GPL, not Lesser GPL
    svn path=/trunk/kdelibs/; revision=161900

--- kdecore/kappdcopiface.cpp
+++ kdecore/kappdcopiface.cpp
@@ -2,7 +2,7 @@
    Copyright (C) 2001 Ian Reinhart Geiser <geiseri@yahoo.com>
 
    This program is free software; you can redistribute it and/or
-   modify it under the terms of the Lesser GNU General Public
+   modify it under the terms of the Library GNU General Public
    License as published by the Free Software Foundation; either
    version 2 of the License, or (at your option) any later version.
 
