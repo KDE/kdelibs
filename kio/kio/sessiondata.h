8366570921c70da9de3c21157de54651ef884910
Dominique Devriese <devriese@kde.org>
2002-06-19 08:25:24 +0000

    oops: LGPL == Lesser GPL, != Library GPL...
    svn path=/trunk/kdelibs/; revision=161956

--- kio/kio/sessiondata.h
+++ kio/kio/sessiondata.h
@@ -2,7 +2,7 @@
    Copyright (C) 2000 Dawit Alemayehu <adawit@kde.org
 
    This library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Library General Public
+   modify it under the terms of the GNU Lesser General Public
    License (LGPL) as published by the Free Software Foundation;
    either version 2 of the License, or (at your option) any
    later version.
@@ -10,7 +10,7 @@
    This library is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Library General Public License for more details.
+   Lesser General Public License for more details.
 
    You should have received a copy of the GNU Lesser General Public
    License along with this library; see the file COPYING.LIB.  If not,
