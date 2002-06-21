e16cd1cc6dfb29da899e21bf3640e70a0adf1d2b
Laurence Anderson <l.d.anderson@warwick.ac.uk>
2002-06-21 10:43:51 +0000

    Added readonly support for Ar files in the KArchive framework
    svn path=/trunk/kdelibs/; revision=162289

---
+++ kio/kio/kar.cpp
@@ -0,0 +1,171 @@
+
+/* This file is part of the KDE libraries
+   Copyright (C) 2002 Laurence Anderson <l.d.anderson@warwick.ac.uk>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License version 2 as published by the Free Software Foundation.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
+#include <qfile.h>
+#include <qdir.h>
+#include <time.h>
+#include <kdebug.h>
+#include <qptrlist.h>
+#include <kmimetype.h>
+#include <qregexp.h>
+
+#include "kfilterdev.h"
+#include "kar.h"
+//#include "klimitediodevice.h"
+
+////////////////////////////////////////////////////////////////////////
+/////////////////////////// KAr ///////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+
+class KAr::KArPrivate
+{
+public:
+    KArPrivate() {}
+};
+
+KAr::KAr( const QString& filename )
+    : KArchive( 0L )
+{
+    //kdDebug(7040) << "KAr(filename) reached." << endl;
+    m_filename = filename;
+    d = new KArPrivate;
+    setDevice( new QFile( filename ) );
+}
+
+KAr::KAr( QIODevice * dev )
+    : KArchive( dev )
+{
+    //kdDebug(7040) << "KAr::KAr( QIODevice * dev) reached." << endl;
+    d = new KArPrivate;
+}
+
+KAr::~KAr()
+{
+    // mjarrett: Closes to prevent ~KArchive from aborting w/o device
+    //kdDebug(7040) << "~KAr reached." << endl;
+    if( isOpened() )
+        close();
+    if ( !m_filename.isEmpty() )
+        delete device(); // we created it ourselves
+    delete d;
+}
+
+bool KAr::openArchive( int mode )
+{
+    // Open archive
+    
+    //kdDebug(7040) << "openarchive reached." << endl;
+
+    if ( mode == IO_WriteOnly )
+        return true;
+    if ( mode != IO_ReadOnly && mode != IO_ReadWrite )
+    {
+        kdWarning(7040) << "Unsupported mode " << mode << endl;
+        return false;
+    }
+
+    QIODevice* dev = device();
+    
+    char magic[8];
+    dev->readBlock (magic, 8);
+    if (qstrncmp(magic, "!<arch>", 7) != 0) {
+        kdDebug() << "Invalid main magic" << endl;
+        return false;
+    }
+
+    char *ar_longnames = 0;
+    while (! dev->atEnd()) {
+        char ar_header_raw[61];
+        ar_header_raw[60] = '\0';
+
+        QCString name;
+        int date, uid, gid, mode, size;
+
+        dev->at( dev->at() + (2 - (dev->at() % 2)) % 2 ); // Ar headers are padded to byte boundry
+
+        if ( dev->readBlock (ar_header_raw, 60) != 60 ) { // Read ar header
+            kdDebug() << "Couldn't read header" << endl;
+            if (ar_longnames) delete[] ar_longnames;
+            //return false;
+            return true; // Probably EOF / trailing junk
+        }
+
+        QCString ar_header(ar_header_raw); // Convert header to QCString for easier handling
+
+        if (ar_header.right(2) != "`\n") { // Check header magic
+            kdDebug() << "Invalid magic" << endl;
+            if (ar_longnames) delete[] ar_longnames;
+            return false;
+        }
+
+        name = ar_header.mid( 0, 16 ); // Process header
+        date = ar_header.mid( 16, 12 ).toInt();
+        uid = ar_header.mid( 28, 6 ).toInt();
+        gid = ar_header.mid( 34, 6 ).toInt();
+        mode = ar_header.mid( 40, 8 ).toInt();
+        size = ar_header.mid( 48, 10 ).toInt();
+
+        bool skip_entry = false; // Deal with special entries
+        if (name.mid(0, 1) == "/") {
+            if (name.mid(1, 1) == "/") { // Longfilename table entry
+                if (ar_longnames) delete[] ar_longnames;
+                ar_longnames = new char[size + 1];
+                ar_longnames[size] = '\0';
+                dev->readBlock (ar_longnames, size);
+                skip_entry = true;
+                kdDebug() << "Read in longnames entry" << endl;
+            } else if (name.mid(1, 1) == " ") { // Symbol table entry
+                kdDebug() << "Skipped symbol entry" << endl;
+                dev->at( dev->at() + size );
+                skip_entry = true;
+            } else { // Longfilename
+                kdDebug() << "Longfilename #" << name.mid(1, 15).toInt() << endl;
+                if (! ar_longnames) {
+                    kdDebug() << "Invalid longfilename reference" << endl;
+                    return false;
+                }
+                name = &ar_longnames[name.mid(1, 15).toInt()];
+                name = name.left(name.find("/"));
+            }
+        }
+        if (skip_entry) continue;
+
+        name = name.stripWhiteSpace(); // Process filename
+        name.replace( QRegExp("/"), "" );
+        kdDebug() << "Filename: " << name << " Size: " << size << endl;
+
+        KArchiveEntry* entry;
+        entry = new KArchiveFile(this, name, mode, date, /*uid*/ 0, /*gid*/ 0, 0, dev->at(), size);
+        rootDir()->addEntry(entry); // Ar files don't support directorys, so everything in root    
+        
+        dev->at( dev->at() + size ); // Skip contents
+    }
+    if (ar_longnames) delete[] ar_longnames;
+
+    return true;
+}
+
+bool KAr::closeArchive()
+{
+    // Close the archive
+    return true;
+}
+
+void KAr::virtual_hook( int id, void* data )
+{ KArchive::virtual_hook( id, data ); }
---
+++ kio/kio/kar.h
@@ -0,0 +1,88 @@
+/* This file is part of the KDE libraries
+   Copyright (C) 2002 Laurence Anderson <l.d.anderson@warwick.ac.uk>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License version 2 as published by the Free Software Foundation.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+#ifndef __kar_h
+#define __kar_h
+
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#include <qdatetime.h>
+#include <qstring.h>
+#include <qstringlist.h>
+#include <qdict.h>
+
+#include <karchive.h>
+
+/**
+ * @short A class for reading ar archives.
+ * @author Holger Schroeder <holger-kde@holgis.net>
+ */
+class KAr : public KArchive
+{
+public:
+    /**
+     * Creates an instance that operates on the given filename.
+     *
+     * @param filename is a local path (e.g. "/home/holger/myfile.ar")
+     */
+    KAr( const QString& filename );
+
+    /**
+     * Creates an instance that operates on the given device.
+     * The device can be compressed (KFilterDev) or not (QFile, etc.).
+     */
+    KAr( QIODevice * dev );
+
+    /**
+     * If the ar file is still opened, then it will be
+     * closed automatically by the destructor.
+     */
+    virtual ~KAr();
+
+    /**
+     * The name of the ar file, as passed to the constructor
+     * Null if you used the QIODevice constructor.
+     */
+    QString fileName() { return m_filename; }
+
+    /*
+     * Writing not supported by this class
+     */
+    virtual bool prepareWriting( const QString&, const QString&, const QString&, uint ) { return false; }
+    virtual bool doneWriting( uint ) { return false; }
+    virtual bool writeDir( const QString&, const QString&, const QString& )  { return false; }
+
+protected:
+    /**
+     * Opens the archive for reading.
+     * Parses the directory listing of the archive
+     * and creates the KArchiveDirectory/KArchiveFile entries.
+     *
+     */
+    virtual bool openArchive( int mode );
+    virtual bool closeArchive();
+
+protected:
+    virtual void virtual_hook( int id, void* data );
+private:
+    QString m_filename;
+    class KArPrivate;
+    KArPrivate * d;
+};
+
+#endif
