bd9c6c24d11330458200b410930d126e4c2aee73
Waldo Bastian <bastian@kde.org>
2002-06-27 19:22:07 +0000

    FC nurnberg fix. (backport)
    svn path=/branches/KDE_3_0_BRANCH/kdelibs/; revision=163511

--- kdecore/kurl.cpp
+++ kdecore/kurl.cpp
@@ -232,7 +232,15 @@ static QString decode( const QString& segment, bool *keepEncoded=0, int encoding
           array.setRawData(new_segment, new_length);
           result = textCodec->toUnicode( array, new_length );
           array.resetRawData(new_segment, new_length);
+          QCString validate = textCodec->fromUnicode(result);
+          if (validate.data() == new_segment)
+          {
           bKeepEncoded = false;
+          }
+          else
+          {
+             result = QString::fromLocal8Bit(new_segment, new_length);
+          }
       }
       else
           result = QString::fromLocal8Bit(new_segment, new_length);
