76352562fae6cf741210f3410c45d134cde660fe
Koos Vriezen <koos.vriezen@gmail.com>
2002-07-05 20:33:27 +0000

    -Improved escaping of script -Better handling of script errors
    svn path=/trunk/kdelibs/; revision=165084

--- khtml/java/org/kde/javascript/JSObject.java
+++ khtml/java/org/kde/javascript/JSObject.java
@@ -15,7 +15,7 @@ public class JSObject extends netscape.javascript.JSObject {
     /* JavaScript code:
      * __lc=[[JS objects],call func, get result func, last result]
      */
-    private final String decls = "if(!__lc) __lc=[[window],function(i,v,glob){if(i>=__lc[0].length)return;__lc[3]=eval((glob?'':'__lc[0][i]')+v);},function(a){var v='E ';var t=typeof __lc[3];if(t=='undefined')v='V ';else if(t=='number')v='N '+__lc[3];else if(t=='string')v='S '+__lc[3];else{var len=__lc[0].length;__lc[0][len]=__lc[3];v=''+len+' '+__lc[3];}document[a].__lc_ret=v},0]";
+    private final String decls = "if(!__lc) __lc=[[window],function(i,v,glob){if(i>=__lc[0].length)return;__lc[3]='E ';__lc[3]=eval((glob?'':'__lc[0][i]')+v);},function(a){var t=typeof __lc[3];if(t=='undefined')v='V ';else if(t=='number')v='N '+__lc[3];else if(t=='string')if(__lc[3]=='E ')v='E ';else v='S '+__lc[3];else{var len=__lc[0].length;__lc[0][len]=__lc[3];v=''+len+' '+(__lc[3]==__lc?'[array]':__lc[3]);}document[a].__lc_ret=v},0]";
 
     public JSObject(Applet a, String name, int _id) {
         Main.info("JSObject.ctor: " + name);
@@ -33,23 +33,16 @@ public class JSObject extends netscape.javascript.JSObject {
         return id;
     }
 
-    private Object evaluate(String _script, boolean global) {
+    private Object evaluate(String _script, boolean global) throws netscape.javascript.JSException {
         Main.info("evaluate (\"" + _script + "\")");
-        int oldpos = 0;
-        int pos = _script.indexOf("\"");
-        String script = new String("");
-        while (pos > -1) {
-            script += _script.substring(oldpos, pos);
-            script += "\\\"";
-            oldpos = pos + 1;
-            pos = _script.indexOf("\"", oldpos);
-        }
-        if (oldpos < _script.length())
-            script += _script.substring(oldpos);
+        
+        String script = _script.replaceAll("\\\\", "\\\\\\\\\\\\\\\\").replaceAll("\"", "\\\\\\\\\\\\\\\"");
+
         KJASAppletContext kc = (KJASAppletContext) applet.getAppletContext();
         String appletname = kc.getAppletName(appletID);
         thread = Thread.currentThread();
-        kc.evaluateJavaScript("__lc[1](" + id + ",'" + script + (global ? "',true)" : "')"), appletID, null);
+
+        kc.evaluateJavaScript("__lc[1](" + id + ",\\\"" + script + (global ? "\\\",true)" : "\\\")"), appletID, null);
         kc.evaluateJavaScript("__lc[2]('" + appletname + "')", appletID, this);
         boolean timedout = true;
         try {
@@ -63,12 +56,14 @@ public class JSObject extends netscape.javascript.JSObject {
 
         /* lets see what we've got */
         String retval = returnvalue;
-        pos = retval.indexOf(' ');
+        int pos = retval.indexOf(' ');
         String type = retval.substring(0, pos);
+        if (type.equals("E")) // Error
+            throw new netscape.javascript.JSException("Script error");
         if (type.equals("V")) // Void
             return null;
         String value = retval.substring(pos+1);
-        Main.info("value=" + value + " type=" + type);
+        Main.info("value=" + value + " (type=" + type + ")");
         if (type.equals("N")) // Number
             return new Double(value);
         if (type.equals("S")) // String
@@ -109,7 +104,7 @@ public class JSObject extends netscape.javascript.JSObject {
             return new String("__lc[0][" + ((JSObject)o).getId() + "]");
         return new String("'" + o.toString() + "'");
     }
-    public Object call(String func, Object [] args) {
+    public Object call(String func, Object [] args) throws netscape.javascript.JSException {
         Main.info("JSObject.call: " + jsobject + "." + func);
         String script = new String("." + func + "(");
         for (int i = 0; i < args.length; i++)
@@ -117,14 +112,14 @@ public class JSObject extends netscape.javascript.JSObject {
         script += ")";
         return evaluate(script, false);
     }
-    public Object eval(String s) {
+    public Object eval(String s) throws netscape.javascript.JSException {
         return evaluate(s, true);
     }
     public boolean equals(Object obj) {
         Main.info("JSObject.equals");
         return super.equals(obj);
     }
-    public Object getMember(String name) {
+    public Object getMember(String name) throws netscape.javascript.JSException {
         Main.info("JSObject.getMember: " + jsobject + "." + name);
         return evaluate("." + name, false);
     }
@@ -132,16 +127,16 @@ public class JSObject extends netscape.javascript.JSObject {
         Main.info("JSObject.setMember: " + jsobject + "." + name);
         evaluate("." + name + "=" + convertValueJ2JS(o), false);
     }
-    public void removeMember(String name) {
+    public void removeMember(String name) throws netscape.javascript.JSException {
         Main.info("JSObject.removeMember: " + jsobject + "." + name);
         evaluate("." + name + "=null", false);
     }
     /* get array element; JS: this[index] */
-    public Object getSlot(int index){
+    public Object getSlot(int index)throws netscape.javascript.JSException {
         Main.info("JSObject.getSlot: " + jsobject + "[" + index + "]");
         return evaluate("[" + index + "]", false);
     }
-    public void setSlot(int index, Object o) {
+    public void setSlot(int index, Object o) throws netscape.javascript.JSException {
         Main.info("JSObject.setSlot: " + jsobject + "[" + index + "]");
         evaluate("[" + index + "]=" + convertValueJ2JS(o), false);
     }
