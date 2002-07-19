6c4ae309f8886f502bfbaa40a3b71704d8c8dff9
Carsten Pfeiffer <pfeiffer@kde.org>
2002-07-19 14:41:51 +0000

    test with a combo
    svn path=/trunk/kdelibs/; revision=167536

--- kdeui/tests/keditlistboxtest.cpp
+++ kdeui/tests/keditlistboxtest.cpp
@@ -1,11 +1,15 @@
 #include <kapplication.h>
 #include <keditlistbox.h>
+#include <kcombobox.h>
 
 int main( int argc, char **argv )
 {
     KApplication app( argc, argv, "keditlistboxtest" );
 
-    KEditListBox *box = new KEditListBox();
+    KEditListBox::CustomEditor editor( new KComboBox( true, 0L, "test" ) );
+    KEditListBox *box = new KEditListBox( QString::fromLatin1("KEditListBox"),
+                                          editor );
+    
     box->insertItem( QString::fromLatin1("Test") );
     box->insertItem( QString::fromLatin1("for") );
     box->insertItem( QString::fromLatin1("this") );
