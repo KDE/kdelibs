113a520b48923634161792e2eefe87fd600b406c
David Faure <faure@kde.org>
2002-07-30 18:05:20 +0000

    Fix for location='url', infinitely looping on https://start.telebank.co.il (That site still doesn't work, but they don't send us the same page mozilla gets!) +          // Check if the URL is the current one. No [infinite] redirect in that case. +          if ( !m_part->url().cmp( KURL(dstUrl), true ) )
    svn path=/trunk/kdelibs/; revision=169608

--- khtml/ecma/kjs_window.cpp
+++ khtml/ecma/kjs_window.cpp
@@ -697,13 +697,19 @@ void Window::put(ExecState* exec, const UString &propertyName, const Value &valu
       return;
     }
     case _Location: {
+      // Complete the URL using the "active part" (running interpreter)
       KHTMLPart* p = Window::retrieveActive(exec)->m_part;
       if (p) {
         QString dstUrl = p->htmlDocument().completeURL(value.toString(exec).string()).string();
+        //kdDebug() << "Window::put dstUrl=" << dstUrl << " m_part->url()=" << m_part->url().url() << endl;
         if (dstUrl.find("javascript:", 0, false) || isSafeScript(exec))
-          m_part->scheduleRedirection(0,
-                                      dstUrl,
-                                      false /*don't lock history*/);
+        {
+          // Check if the URL is the current one. No [infinite] redirect in that case.
+          if ( !m_part->url().cmp( KURL(dstUrl), true ) )
+            m_part->scheduleRedirection(0,
+                                        dstUrl,
+                                        false /*don't lock history*/);
+        }
       }
       return;
     }
