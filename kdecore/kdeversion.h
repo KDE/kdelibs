011bdc912f79c64b4a7ea670b476a71f4e22a185
Dirk Mueller <mueller@kde.org>
2002-08-05 18:12:41 +0000

    KDE 3.1 Beta1
    svn path=/trunk/kdelibs/; revision=170561

--- kdecore/kdeversion.h
+++ kdecore/kdeversion.h
@@ -22,10 +22,10 @@
 #ifndef _KDE_VERSION_H_
 #define _KDE_VERSION_H_
 
-#define KDE_VERSION 306
-#define KDE_VERSION_STRING "3.0.6 (KDE 3.1 alpha1)"
+#define KDE_VERSION 307
+#define KDE_VERSION_STRING "3.0.7 (KDE 3.1 beta1)"
 #define KDE_VERSION_MAJOR 3
 #define KDE_VERSION_MINOR 0
-#define KDE_VERSION_RELEASE 6
+#define KDE_VERSION_RELEASE 7
 
 #endif // _KDE_VERSION_H_
