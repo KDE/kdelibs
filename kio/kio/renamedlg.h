08657fd7228d92b069a891e80f68fca469c8e94b
David Faure <faure@kde.org>
2002-08-20 14:08:35 +0000

    RenameDlg and SkipDlg are back into libkio itself (instead of kio_uiserver). This fixes problems like "kmail is stuck while the file-rename dialog is opened". (as discussed with Holger Freyther long ago ;) It also helps apps which want to use those dialogs directly.
    svn path=/trunk/kdelibs/; revision=172873

--- kio/kio/renamedlg.h
+++ kio/kio/renamedlg.h
@@ -19,6 +19,129 @@
     Boston, MA 02111-1307, USA.
 */
 
-// the real file is elsewhere in the sources
-// but the real file will be installed in include/kio just like the others
-#include "../misc/renamedlg.h"
+#ifndef __kio_rename_dlg__
+#define __kio_rename_dlg__ "$Id$"
+
+#include <kurl.h>
+#include <qwidget.h>
+#include <qdialog.h>
+#include <qpushbutton.h>
+#include <qlabel.h>
+#include <qlineedit.h>
+#include <qlayout.h>
+#include <qstring.h>
+#include <sys/types.h>
+
+#include <kio/global.h>
+
+namespace KIO {
+
+enum RenameDlg_Mode { M_OVERWRITE = 1, M_OVERWRITE_ITSELF = 2, M_SKIP = 4, M_SINGLE = 8, M_MULTI = 16, M_RESUME = 32, M_NORENAME = 64 };
+
+enum RenameDlg_Result { R_RESUME = 6, R_RESUME_ALL = 7, R_OVERWRITE = 4, R_OVERWRITE_ALL = 5, R_SKIP = 2, R_AUTO_SKIP = 3, R_RENAME = 1, R_CANCEL = 0 };
+
+
+/**
+ * A dialog for the options to rename two files
+ */
+class RenameDlg : public QDialog
+{
+  Q_OBJECT
+public:
+  /**
+   * Construct a "rename" dialog
+   * @param parent parent widget (often 0)
+   * @param caption the caption for the dialog box
+   * @param src the url to the file/dir we're trying to copy, as it's part of the text message
+   * @param dest the path to destination file/dir, i.e. the one that already exists
+   * @param mode parameters for the dialog (which buttons to show...),
+   * @see RenameDlg_Mode
+   *
+   * The following parameters bring optionnal information about @p src and @p dest
+   * @param sizeSrc size of source file
+   * @param sizeDest size of destination file
+   * @param ctimeSrc creation time of source file
+   * @param ctimeDest creation time of destination file
+   * @param mtimeSrc modification time of source file
+   * @param mtimeDest modification time of destination file
+   *
+   * @param modal set to true for a modal dialog
+   */
+  RenameDlg( QWidget *parent, const QString & caption,
+             const QString & src, const QString & dest, RenameDlg_Mode mode,
+             KIO::filesize_t sizeSrc = (KIO::filesize_t) -1,
+             KIO::filesize_t sizeDest = (KIO::filesize_t) -1,
+             time_t ctimeSrc = (time_t) -1,
+             time_t ctimeDest = (time_t) -1,
+             time_t mtimeSrc = (time_t) -1,
+             time_t mtimeDest = (time_t) -1,
+             bool modal = FALSE );
+  ~RenameDlg();
+
+  /**
+   * @return the new destination
+   * valid only if RENAME was chosen
+   */
+  KURL newDestURL();
+
+
+
+public slots:
+  void b0Pressed();
+  void b1Pressed();
+  void b2Pressed();
+  void b3Pressed();
+  void b4Pressed();
+  void b5Pressed();
+  void b6Pressed();
+  void b7Pressed();
+  void b8Pressed();
+
+protected slots:
+  void enableRenameButton(const QString &);
+private:
+ class RenameDlgPrivate;
+ RenameDlgPrivate *d;
+ void pluginHandling( );
+ QString mime( const QString &string );
+};
+
+  /**
+   * \addtogroup renamedlg "RenameDlg related Functions"
+   *  @{
+   * \relate KIO::RenameDlg
+   * Construct a modal, parent-less "rename" dialog, and return
+   * a result code, as well as the new dest. Much easier to use than the
+   * class @ref RenameDlg directly.
+
+   * @param caption the caption for the dialog box
+   * @param src the file/dir we're trying to copy, as it's part of the text message
+   * @param dest the destination file/dir, i.e. the one that already exists
+   * @param mode parameters for the dialog (which buttons to show...),
+   * @see RenameDlg_Mode
+   * @param newDest the new destination path, valid if R_RENAME was returned.
+   *
+   * The following parameters bring optionnal information about @p src and @p dest
+   * @param sizeSrc size of source file
+   * @param sizeDest size of destination file
+   * @param ctimeSrc creation time of source file
+   * @param ctimeDest creation time of destination file
+   * @param mtimeSrc modification time of source file
+   * @param mtimeDest modification time of destination file
+   *
+   */
+RenameDlg_Result open_RenameDlg( const QString & caption,
+                                 const QString& src, const QString & dest,
+                                 RenameDlg_Mode mode, QString& newDestPath,
+                                 KIO::filesize_t sizeSrc = (KIO::filesize_t) -1,
+                                 KIO::filesize_t sizeDest = (KIO::filesize_t) -1,
+                                 time_t ctimeSrc = (time_t) -1,
+                                 time_t ctimeDest = (time_t) -1,
+                                 time_t mtimeSrc = (time_t) -1,
+                                 time_t mtimeDest = (time_t) -1
+                                 );
+
+/*! @} */
+
+}
+#endif
--- kio/kio/skipdlg.h
+++ kio/kio/skipdlg.h
@@ -16,6 +16,45 @@
    Boston, MA 02111-1307, USA.
 */
 
-// the real file is elsewhere in the sources
-// but the real file will be installed in include/kio just like the others
-#include "../misc/skipdlg.h"
+#ifndef __kio_skip_dlg__
+#define __kio_skip_dlg__ "$Id$"
+
+#include <kdialog.h>
+
+class QPushButton;
+class QWidget;
+
+namespace KIO {
+
+  enum SkipDlg_Result { S_SKIP = 1, S_AUTO_SKIP = 2, S_CANCEL = 0 };
+
+  SkipDlg_Result open_SkipDlg( bool _multi, const QString& _error_text = QString::null );
+
+/**
+ * @internal
+ */
+class SkipDlg : public KDialog
+{
+  Q_OBJECT
+public:
+  SkipDlg( QWidget *parent, bool _multi, const QString& _error_text, bool _modal = false );
+  ~SkipDlg();
+
+protected:
+  QPushButton *b0;
+  QPushButton *b1;
+  QPushButton *b2;
+
+  bool modal;
+
+public slots:
+  void b0Pressed();
+  void b1Pressed();
+  void b2Pressed();
+
+signals:
+  void result( SkipDlg *_this, int _button );
+};
+
+};
+#endif
