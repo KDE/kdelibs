2f4e53febd24cba8a0f0de671579ce9c6c5df0ff
Tim Jansen <tim@tjansen.de>
2002-09-07 20:39:48 +0000

    kdoc update
    svn path=/trunk/kdelibs/; revision=176308

--- kdecore/kdesktopfile.h
+++ kdecore/kdesktopfile.h
@@ -66,6 +66,7 @@ public:
    * The check is performed looking at the file extension (the file is not
    * opened).
    * Currently, valid extensions are ".kdelnk" and ".desktop".
+   * @param path the path of the file to check
    * @return @p true if the file appears to be a desktop file.
    */
   static bool isDesktopFile(const QString& path);
@@ -75,62 +76,76 @@ public:
    * By default users are authorized to run all desktop files but
    * the KIOSK framework can be used to activate certain restrictions.
    * See README.kiosk for more information.
+   * @param path the file to check
+   * @return true if the user is authorized to run the file
    */
   static bool isAuthorizedDesktopFile(const QString& path);
 
   /**
    * Returns the value of the "Type=" entry.
+   * @return the type or QString::null if not specified
    */
   QString readType() const;
 
   /**
    * Returns the value of the "Icon=" entry.
+   * @return the icon or QString::null if not specified 
    */
   QString readIcon() const;
 
   /**
    * Returns the value of the "Name=" entry.
+   * @return the name or QString::null if not specified
    */
   QString readName() const;
 
   /**
    * Returns the value of the "Comment=" entry.
+   * @return the comment or QString::null if not specified
    */
   QString readComment() const;
 
   /**
    * Returns the value of the "GenericName=" entry.
+   * @return the generic name or QString::null if not specified
    */
   QString readGenericName() const;
 
   /**
    * Returns the value of the "Path=" entry.
+   * @return the path or QString::null if not specified
    * @deprecated
    */
   QString readPath() const;
 
   /**
    * Returns the value of the "Dev=" entry.
+   * @return the device or QString::null if not specified
    */
   QString readDevice() const;
 
   /**
    * Returns the value of the "URL=" entry.
+   * @return the URL or QString::null if not specified
    */
   QString readURL() const;
 
   /**
    * Returns a list of the "Actions=" entries.
+   * @return the list of actions
    */
   QStringList readActions() const;
 
   /**
    * Sets the desktop action group.
+   * @param the new action group
    */
   void setActionGroup(const QString &group);
 
   /**
    * Returns true if the action group exists, false otherwise
+   * @param the action group to test
+   * @return true if the action group exists
    */
   bool hasActionGroup(const QString &group) const;
 
@@ -138,45 +153,57 @@ public:
    * Checks whether there is a "Type=Link" entry.
    *
    * The link points to the "URL=" entry.
+   * @return true if there is a "Type=Link" entry
    */
   bool hasLinkType() const;
 
   /**
    * Checks whether there is an entry "Type=Application".
+   * @return true if there is a "Type=Application" entry
    */
   bool hasApplicationType() const;
 
   /**
    * Checks whether there is an entry "Type=MimeType".
+   * @return true if there is a "Type=MimeType" entry
    */
   bool hasMimeTypeType() const; // funny name :)
 
   /**
    * Checks whether there is an entry "Type=FSDev".
+   * @return true if there is a "Type=FSDev" entry
    */
   bool hasDeviceType() const;
 
   /**
    * Checks whether the TryExec field contains a binary
    * which is found on the local system.
+   * @return true if TryExec contains an existing binary
    */
   bool tryExec() const;
 
   /**
+   * Returns the file name.
    * @return The filename as passed to the constructor.
    */
   QString fileName() const;
 
   /**
+   * Returns the resource.
    * @return The resource type as passed to the constructor.
    */
   QString resource() const;
 
   /**
+   * Returns the value of the "DocPath=" entry.
    * @return The value of the "DocPath=" entry.
    */
   QString readDocPath() const;
 
+  /**
+   * Returns the entry of the "SortOrder=" entry.
+   * @return the value of the "SortOrder=" entry.
+   */
   QStringList sortOrder() const;
 
 
