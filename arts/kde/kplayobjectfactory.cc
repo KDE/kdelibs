298bd205399ece9a0e3db203f5a1f1be74914a78
Matthias Welwarsky <matze@welwarsky.de>
2002-09-08 18:44:24 +0000

    reverted binary incompatible changes added to KPlayObjectFactory after the release of KDE 3.0,
    implemented new class KDE::PlayObjectFactory to implement the functionality
    that got removed by reverting those changes
    
    svn path=/trunk/kdelibs/; revision=176506

--- arts/kde/kplayobjectfactory.cc
+++ arts/kde/kplayobjectfactory.cc
@@ -23,12 +23,10 @@
 #include "kplayobject.h"
 #include "artskde.h"
 #include "kplayobjectfactory.h"
-#include "kplayobjectfactory.moc"
+#include "kplayobjectcreator.h"
 #include "kioinputstream_impl.h"
 
 #include <qfile.h>
-#include <kapplication.h>
-
 #include <kdebug.h>
 
 using namespace std;
@@ -46,75 +44,14 @@ KPlayObjectFactory::~KPlayObjectFactory()
 
 KPlayObject *KPlayObjectFactory::createPlayObject(const KURL& url, bool createBUS)
 {
-
-	// no need to go any further, and I hate deep indentation
-	if (m_server.isNull() || url.isEmpty() )
-		return new KPlayObject();
-
-	// check if streaming is allowed or the URL is a local file
-	if (m_allowStreaming && !url.isLocalFile())
-	{
-		// This is the RightWay(tm) according to stw
-		Arts::KIOInputStream_impl* instream_impl = new Arts::KIOInputStream_impl();
-		Arts::KIOInputStream instream = Arts::KIOInputStream::_from_base(instream_impl);
-
-		// signal will be called once the ioslave knows the mime-type of the stream
-		QObject::connect(instream_impl, SIGNAL(mimeTypeFound(const QString &)),
-				 this, SLOT(slotMimeType(const QString &)));
-
-		// GO!
-		instream.openURL(url.url().latin1());
-		instream.streamStart();
-
-		m_eventLoopEntered = true;
-		// FIXME: need to handle timeouts?
-		// this is UGLY, but I cannot do anything else without changing the interface
-		// the application blocks here, and restarts once slotMimeType() is called.
-		kapp->enter_loop();
-		
-		// wb :D
-		// some error occoured
-		if (m_mimeType == "application/x-zerosize")
-		    return new KPlayObject();
-
-		// ok, now we know the mimetype of the stream
-		m_stream = true;		
-		return new KPlayObject(m_server.createPlayObjectForStream(instream, 
-									  string(m_mimeType.latin1()), 
-									  createBUS), true);
-	}
-	kdDebug() << "stream is local file: " << url.url() << endl;
-
-	// usual stuff if we have a local file
 	KMimeType::Ptr mimetype = KMimeType::findByURL(url);
-	return new KPlayObject(m_server.createPlayObjectForURL(string(QFile::encodeName(url.path())), 
-							       string(mimetype->name().latin1()), 
-							       createBUS), 
-			       false);
-}
-
-void KPlayObjectFactory::slotMimeType(const QString& mimetype)
-{
-
-	kdDebug() << "slotMimeType called: " << mimetype << endl;
-
-	if ( mimetype == "application/octet-stream" )
-	    m_mimeType = "audio/x-mp3";
-	else
-	    m_mimeType = mimetype;
-
-	if (m_eventLoopEntered) {
-		m_eventLoopEntered = false;
-		kapp->exit_loop();
-	}
+	return createPlayObject(url, mimetype->name(), createBUS);
 }
 
 KPlayObject *KPlayObjectFactory::createPlayObject(const KURL& url, const QString &mimetype, bool createBUS)
 {
 	if(!m_server.isNull())
 	{
-		kdDebug() << "createPlayObject: mimetype is " << mimetype << endl;
-
 		if(mimetype == "application/octet-stream" && m_allowStreaming)
 		{
 			Arts::KIOInputStream instream;
@@ -125,15 +62,6 @@ KPlayObject *KPlayObjectFactory::createPlayObject(const KURL& url, const QString
 			// TODO: what else than hardcoding audio/x-mp3 ?
 			return new KPlayObject(m_server.createPlayObjectForStream(instream, string("audio/x-mp3"), createBUS), true);
 		}
-		else if(mimetype == "video/mpeg" && m_allowStreaming)
-		{
-			Arts::KIOInputStream instream;
-			instream.openURL(url.url().latin1());
-
-			m_stream = true;
-
-			return new KPlayObject(m_server.createPlayObjectForStream(instream, string("video/mpeg"), createBUS), true);
-		}
 		else
 			return new KPlayObject(m_server.createPlayObjectForURL(string(QFile::encodeName(url.path())), string(mimetype.latin1()), createBUS), false);
 	}
@@ -141,7 +69,47 @@ KPlayObject *KPlayObjectFactory::createPlayObject(const KURL& url, const QString
 		return new KPlayObject();
 }
 
-QStringList KPlayObjectFactory::mimeTypes(void)
+
+
+//
+
+KDE::PlayObjectFactory::PlayObjectFactory(Arts::SoundServerV2 server)
+{
+	d = new PrivateData;
+	d->server = server;
+	d->allowStreaming = true;
+	d->isStream = false;
+}
+
+KDE::PlayObjectFactory::~PlayObjectFactory()
+{
+	delete d;
+}
+
+KDE::PlayObject *KDE::PlayObjectFactory::createPlayObject(const KURL& url, bool createBUS)
+{
+	KMimeType::Ptr mimetype = KMimeType::findByURL(url);
+	return createPlayObject(url, mimetype->name(), createBUS);
+}
+
+KDE::PlayObject *KDE::PlayObjectFactory::createPlayObject(const KURL& url, const QString &mimetype, bool createBUS)
+{
+
+	// return a NULL playobject if the server is NULL
+	if ( d->server.isNull() || url.isEmpty() )
+		return new KDE::PlayObject();
+
+	// decide if it's a local file
+	if ( url.isLocalFile() || !d->allowStreaming ) {
+		// we rely on the delivered mimetype if it's a local file
+		return new KDE::PlayObject( d->server.createPlayObjectForURL( string( QFile::encodeName( url.path() ) ), string( mimetype.latin1() ), createBUS ), false );
+	}
+	// if non-local, let the KPlayObject figure out the mimetype itself
+	// this invokes asynchronous creation automatically
+	return new KDE::PlayObject( d->server, url, true, createBUS );
+}
+
+QStringList KDE::PlayObjectFactory::mimeTypes(void)
 {
 	Arts::TraderQuery query;
 	vector<Arts::TraderOffer> *offers = query.query();
--- arts/kde/kplayobjectfactory.h
+++ arts/kde/kplayobjectfactory.h
@@ -23,13 +23,11 @@
 #define KPLAYOBJECTFACTORY_H
 
 #include <kurl.h>
-#include <qobject.h>
 #include "soundserver.h"
 #include "kplayobject.h"
 
-class KPlayObjectFactory : public QObject
+class KPlayObjectFactory
 {
-Q_OBJECT
 public:
 	KPlayObjectFactory(Arts::SoundServerV2 server);
 	~KPlayObjectFactory();
@@ -41,21 +39,76 @@ public:
 	bool allowStreaming() { return m_allowStreaming; }
 
 	bool isAStream() { return m_stream; }
+	
+private:
+	Arts::SoundServerV2 m_server;
+	bool m_allowStreaming;
+	bool m_stream;
+};
+
+
+namespace KDE {
+
+	/**
+	 * This class implements a factory to create KDE::PlayObjects for
+	 * a given URL and mimetype. A detailed description about how to
+	 * use the KDE multimedia framework can be found in the documentation
+	 * for KDE::PlayObject.
+	 */
+class PlayObjectFactory
+{
+public:
+	/**
+	 * Creates a KDE::PlayObjectFactory. @p server is an 
+	 * Arts::SoundServerV2
+	 */
+	PlayObjectFactory(Arts::SoundServerV2 server);
+	~PlayObjectFactory();
+
+	/**
+	 * Creates a KDE::PlayObject to play back the file or stream
+	 * @p url points to. Set @p createBUS to "true", if you want the
+	 * PlayObject to be connected to a SYNTH_BUS_UPLINK on creation.
+	 * This is usually the case. You only need to set this to "false"
+	 * if you want to attach your own sound effects to the PlayObject.
+	 *
+	 * You don't need to know the mimetype of the file or stream, it
+	 * will be detected automatically.
+	 */
+	KDE::PlayObject *createPlayObject(const KURL& url, bool createBUS);
+	
+	/**
+	 * This method also creates a KDE::PlayObject, but does no automatic
+	 * mimetype detection. Use this method with care.
+	 */
+	KDE::PlayObject *createPlayObject(const KURL& url, const QString &mimetype, bool createBUS);
+
+	/**
+	 * setAllowStreaming( true ) allows the factory to create a
+	 * streaming PlayObject for a non-local URL. This is also the default.
+	 * Usually, you should not need to set this to false.
+	 */
+	void setAllowStreaming(bool s) { d->allowStreaming = s; }
+
+	/**
+	 * returns "true" if the factory is allowed to create streaming
+	 * PlayObjects.
+	 */
+	bool allowStreaming() { return d->allowStreaming; }
 
 	/**
 	 * Return the mimetypes that are playable
 	 */
 	static QStringList mimeTypes(void);
 
-private slots:
-	void slotMimeType(const QString &mimetype);
-
 private:
-	Arts::SoundServerV2 m_server;
-	bool m_allowStreaming;
-	bool m_stream;
-	bool m_eventLoopEntered;
-	QString m_mimeType;
+	struct PrivateData {
+		Arts::SoundServerV2 server;
+		bool allowStreaming;
+		bool isStream;
+	};
+	PrivateData* d;
 };
 
+}
 #endif
