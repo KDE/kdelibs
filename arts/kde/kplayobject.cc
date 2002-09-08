0d8fb2c155188c7cbdd69bfa96f360091bd70ae0
Matthias Welwarsky <matze@welwarsky.de>
2002-09-08 18:47:31 +0000

    implemented new class KDE::PlayObject to implement the functionality previously located in KPlayObjectFactory in a clean way.
    This fixes a number of bugs where applications crash due to reentrancy
    problems introduced by nasty event loop hackery in KPlayObjectFactory
    
    svn path=/trunk/kdelibs/; revision=176507

--- arts/kde/kplayobject.cc
+++ arts/kde/kplayobject.cc
@@ -19,18 +19,18 @@
 
 	*/
 
+#include "kplayobject.h"
 #include "kplayobject.moc"
+#include "kplayobjectcreator.h"
 #include <kdebug.h>
-
-using namespace Arts;
 
 KPlayObject::KPlayObject() : QObject()
 {
-	m_playObject = PlayObject::null();
+	m_playObject = Arts::PlayObject::null();
 	m_isStream = false;
 }
 
-KPlayObject::KPlayObject(PlayObject playobject, bool isStream) : QObject()
+KPlayObject::KPlayObject(Arts::PlayObject playobject, bool isStream) : QObject()
 {
 	m_playObject = playobject;
 	m_isStream = isStream;
@@ -93,7 +93,7 @@ Arts::poState KPlayObject::state()
 	return object().state();
 }
 
-PlayObject KPlayObject::object()
+Arts::PlayObject KPlayObject::object()
 {
 	return m_playObject;
 }
@@ -109,6 +109,185 @@ void KPlayObject::setObject(Arts::PlayObject playObject)
 }
 
 bool KPlayObject::stream()
+{
+	return m_isStream;
+}
+
+struct KDE::PlayObject::PrivateData
+{
+	PrivateData() : creator( 0 ), 
+			isProxy( false ),
+			internalState( Arts::posIdle ) {}
+	~PrivateData() {
+		delete creator;
+	}
+	Arts::SoundServerV2 server;
+	KDE::PlayObjectCreator* creator;
+	bool createBUS;
+	bool isProxy;
+	Arts::poState internalState;
+	KURL url;
+};
+
+KDE::PlayObject::PlayObject() : QObject()
+{
+	m_playObject = Arts::PlayObject::null();
+	m_isStream = false;
+	d = new PrivateData;
+}
+
+KDE::PlayObject::PlayObject(Arts::PlayObject playobject, bool isStream) : QObject()
+{
+	m_playObject = playobject;
+	m_isStream = isStream;
+	d = new PrivateData;
+	emit playObjectCreated();
+}
+
+KDE::PlayObject::PlayObject( Arts::SoundServerV2 server, const KURL& url, bool isStream, bool createBUS ) : QObject()
+{
+	kdDebug( 400 ) << "KDE::PlayObject: created as proxy for URL " << url.url()<< endl;
+
+	m_playObject = Arts::PlayObject::null();
+	m_isStream = isStream;
+	d = new PrivateData;
+	d->server = server;
+	d->url = url;
+	d->createBUS = createBUS;
+	d->isProxy = true;
+}
+
+KDE::PlayObject::~PlayObject()
+{
+	kdDebug( 400 ) << "KDE::PlayObject: destroyed" << endl;
+
+	delete d;
+}
+
+void KDE::PlayObject::play()
+{
+	kdDebug( 400 ) << "KDE::PlayObject::play()" << endl;
+
+	if ( object().isNull() ) {
+		if ( m_isStream ) {
+			if ( d->creator )
+				delete d->creator;
+			d->creator = new KDE::PlayObjectCreator( d->server );
+			d->creator->create( d->url, d->createBUS, this, SLOT( attachPlayObject( Arts::PlayObject ) ) );
+			kdDebug( 400 ) << "KDE::PlayObject::play(): creator called" << endl;
+			d->internalState = Arts::posPlaying;
+		}
+		return;
+	}
+	object().play();
+}
+
+void KDE::PlayObject::attachPlayObject( Arts::PlayObject playObject )
+{
+	kdDebug( 400 ) << "KDE::PlayObject::attachPlayObject()" << endl;
+
+	m_playObject = playObject;
+	emit playObjectCreated();
+	switch ( d->internalState ) {
+	case Arts::posIdle:
+		object().halt();
+		break;
+	case Arts::posPaused:
+		object().pause();
+		break;
+	case Arts::posPlaying:
+		object().play ();
+		break;
+	}
+}
+
+void KDE::PlayObject::seek(Arts::poTime newTime)
+{
+	if ( object().isNull() )
+		return;
+	if(!m_isStream)
+		object().seek(newTime);
+	else
+		kdDebug( 400 ) << "Seeking in a Stream? huh?" << endl;
+}
+
+void KDE::PlayObject::pause()
+{
+	if ( !object().isNull() )
+		object().pause();
+	d->internalState = Arts::posPaused;
+}
+
+void KDE::PlayObject::halt()
+{
+	kdDebug( 400 ) << "KDE::PlayObject::halt()" << endl;
+	if ( !object().isNull() )
+		object().halt();
+	else if ( d->creator ) {
+		delete d->creator;
+		d->creator = 0;
+		kdDebug( 400 ) << "KDE::PlayObject::halt(): creator destroyed" << endl;
+	}
+	d->internalState = Arts::posIdle;
+}
+
+QString KDE::PlayObject::description()
+{
+	if ( object().isNull() )
+		return QString();
+	return QString::fromLatin1(object().description().c_str());
+}
+
+Arts::poTime KDE::PlayObject::currentTime()
+{
+	if ( object().isNull() )
+		return Arts::poTime( 0, 0, -1, "" );
+	return object().currentTime();
+}
+
+Arts::poTime KDE::PlayObject::overallTime()
+{
+	if ( object().isNull() )
+		return Arts::poTime( 0, 0, -1, "" );
+	return object().overallTime();
+}
+
+Arts::poCapabilities KDE::PlayObject::capabilities()
+{
+	if ( object().isNull() )
+		return static_cast<Arts::poCapabilities>( 0 );
+	return object().capabilities();
+}
+
+QString KDE::PlayObject::mediaName()
+{
+	if ( object().isNull() )
+		return QString();
+	return QString::fromLatin1(object().mediaName().c_str());
+}
+
+Arts::poState KDE::PlayObject::state()
+{
+	if ( object().isNull() )
+		return d->internalState;
+	return object().state();
+}
+
+Arts::PlayObject KDE::PlayObject::object()
+{
+	return m_playObject;
+}
+
+bool KDE::PlayObject::isNull()
+{
+	if ( !this )
+		return true;
+	if ( d->isProxy )
+		return false;
+	return object().isNull();
+}
+
+bool KDE::PlayObject::stream()
 {
 	return m_isStream;
 }
--- arts/kde/kplayobject.h
+++ arts/kde/kplayobject.h
@@ -23,6 +23,8 @@
 #define KPLAYOBJECT_H
 
 #include "kmedia2.h"
+#include "soundserver.h"
+#include <kurl.h>
 #include <qobject.h>
 
 class KPlayObject : public QObject
@@ -113,5 +115,192 @@ private:
 	Arts::PlayObject m_playObject;
 	bool m_isStream;
 };
+
+
+
+namespace KDE {
+
+class PlayObjectFactory;
+
+/**
+  * This class acts as a general interface to the KDE multimedia framework.
+  * You basically point the Playobject to an URL and say "play", and it will
+  * automatically decode and play and / or display the file or stream.
+  * For non-local media, it will make extensive use of KIOInputStream to
+  * directly play the content from the remote location without having to
+  * download it to a temporary local file first.
+  *
+  * A KDE::PlayObject is never created directly with new, but only through
+  * a KDE::PlayObjectFactory.
+  *
+  * Basically, it is used like this:
+  * \code
+  * KArtsDispatcher dispatcher;
+  * KArtsServer server;
+  * KDE::PlayObjectFactory factory( server.server() );
+  * KDE::PlayObject* playobj = factory.createPlayObject( someURL, true );
+  * playobj->play();
+  * \endcode
+  *
+  * Internally, the KDE::PlayObject acts as a wrapper for an Arts::PlayObject.
+  *
+  * Special care needs to be taken for non-local media. In general, you
+  * cannot safely figure out the mimetype of the remote media content, by
+  * looking at the URL alone. You need to download some data to analyze
+  * the content. Since KDE::PlayObject is only a wrapper for an
+  * Arts::PlayObject, and an Arts::PlayObject needs to know the mimetype
+  * of the data it plays in order to pick the correct decoder, one cannot
+  * directly create an Arts::PlayObject and attach it to a stream. Therefore,
+  * the following approach is used.
+  *
+  * Whenever a the factory creates a KDE::PlayObject for a non-local content,
+  * it first generates a so called "Proxy" Playobject. This is a
+  * KDE::PlayObject that does not contain a real Arts::PlayObject yet.
+  * As soon as you invoke the play() method, a connection to the media
+  * source is made, and as soon as the mimetype is known, the appropriate
+  * Arts::PlayObject is created.
+  *
+  * This has some side effects that developers need to be aware of:
+  * Until the real Arts::PlayObject got created,
+  * - the capabilities() method returns "zero" capabilities,
+  * - description() and mediaName() will return a null QString,
+  * - currentTime() and overallTime() will return "zero",
+  * - despite the fact that isNull() returns "false", object().isNull()
+  *   will return "true". If you need to directly access methods of the
+  *   internal Arts::PlayObject, be sure to use object().isNull() to guard
+  *   your access.
+  *
+  * A KDE::PlayObject will emit the signal playObjectCreated()
+  * as soon as the real internal Arts::PlayObject got created. This is also
+  * true for local media files. So you can generally connect to this signal
+  * and act on it if your application needs to know about the real capabilities
+  * of the Arts::PlayObject.
+  *
+  * However, KDE::PlayObject will try to act reasonable on calls to play(),
+  * halt(), pause() and state(). If you call play() and then pause() 
+  * before the connection to the media source was established, it will 
+  * not start playing once the connection got established. Calling halt()
+  * will cancel the connection process. KDE::PlayObject will maintain
+  * an internal state variable, and calling state() will return this
+  * internal state until the real Arts::PlayObject got created, afterwards
+  * the state of the Arts::PlayObject will be returned.
+  */
+class PlayObject : public QObject
+{
+Q_OBJECT
+public:
+	~PlayObject();
+
+	/**
+	  * Returns the internal Arts::PlayObject
+	  */
+	Arts::PlayObject object();
+
+	/**
+	 * return true if this != 0.
+	 *
+	 * in essence, ((KDE::PlayObject*)0)->isNull() will not 
+	 * crash
+	 **/
+	bool isNull();
+
+	/**
+	 * returns "true" if the content to play is delivered as a stream.
+	 */
+    	bool stream();
+
+	/**
+	 * causes the PlayObject to start the play back.
+	 */
+	void play();
+	
+	/**
+	 * causes the PlayObject to skip to the time @p newTime. 
+	 * You don't need to stop or restart the play back after calling seek.
+	 */
+	void seek(Arts::poTime newTime);
+	
+	/**
+	 * causes the PlayObject to pause play back immediately. It will not
+	 * restart until you call play(). This also works on streams, the
+	 * connection to the media source will be maintained while the 
+	 * PlayObject is paused.
+	 */
+	void pause();
+	
+	/**
+	 * immediately stops the play back and resets the media to the
+	 * start of the content. If playing from a stream, halt() causes
+	 * the connection to be cancelled.
+	 */
+	 
+	void halt();
+	
+	/**
+	 * Reimplemented (Arts::PlayObject Wrapper)
+	 */
+	QString description();
+	
+	/**
+	 * Reimplemented (Arts::PlayObject Wrapper)
+	 */
+	Arts::poTime currentTime();
+	
+	/**
+	 * Reimplemented (Arts::PlayObject Wrapper)
+	 */
+	Arts::poTime overallTime();
+	
+	/**
+	 * returns the capabilities of the PlayObject. The return value is
+	 * a binary OR of Arts::capSeek and Arts::capPause, or 0.
+	 */
+	Arts::poCapabilities capabilities();
+	
+	/**
+	 * Reimplemented (Arts::PlayObject Wrapper)
+	 */
+	QString mediaName();
+	
+	/**
+	 * returns the internal state of the PlayObject. The state can be
+	 * either Arts::posIdle, Arts::posPaused or Arts::posPlaying. A
+	 * PlayObject in state Arts::posIdle is stopped. Once you call
+	 * play(), the state changes to Arts::posPlaying. pause() causes
+	 * the PlayObject to change to Arts::posPaused.
+	 */
+	Arts::poState state();
+
+signals:
+	/**
+	 * this signal is emitted as soon as the internal Arts::PlayObject
+	 * is created and ready to play. It is granted that the Arts::PlayObject
+	 * has not started playing, but KDE::PlayObject will call
+	 * object().play() immediately after emitting this signal, so you
+	 * need not do it yourself.
+	 */
+	void playObjectCreated();
+
+private slots:
+	void attachPlayObject( Arts::PlayObject );
+
+private:
+	Arts::PlayObject m_playObject;
+	bool m_isStream;
+
+	struct PrivateData;
+	PrivateData* d;
+
+	/* private constructors, to prevent instantiation and copying */
+	PlayObject();
+	PlayObject( const PlayObject& ) : QObject() {};
+	PlayObject(Arts::PlayObject playobject, bool isStream);
+	PlayObject( Arts::SoundServerV2 server, const KURL& url, bool isStream, bool createBUS );
+
+	friend class KDE::PlayObjectFactory;
+
+};
+
+}
 
 #endif
