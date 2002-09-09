6a265f58a4e8adfdb9f1ae2e679f325fd2b6ad1a
Tim Jansen <tim@tjansen.de>
2002-09-09 21:11:35 +0000

    doc update
    svn path=/trunk/kdelibs/; revision=176767

--- kdecore/ksockaddr.h
+++ kdecore/ksockaddr.h
@@ -56,6 +56,8 @@ protected:
 
   /**
    * Creates with given data
+   * @param sa a sockaddr structure
+   * @param size the size of @p sa
    */
   KSocketAddress(const sockaddr* sa, ksocklen_t size);
 
@@ -66,48 +68,60 @@ public:
   virtual ~KSocketAddress();
 
   /**
-   * Returns a string representation of this socket
+   * Returns a string representation of this socket.
+   * @return a pretty string representation
    */
   virtual QString pretty() const;
 
   /**
-   * Returns a sockaddr structure, for passing down to library functions
+   * Returns a sockaddr structure, for passing down to library functions.
+   * @return the sockaddr structure, can be 0
    */
   const sockaddr* address() const
   { return data; }
 
   /**
-   * Returns sockaddr structure size
+   * Returns sockaddr structure size.
+   * @return the size of the sockaddr structre, 0 if there is none.
    */
   virtual ksocklen_t size() const
   { return datasize; }
 
   /**
-   * Returns a sockaddr structure, for passing down to library functions
+   * Returns a sockaddr structure, for passing down to library functions.
+   * @return the sockaddr structure, can be 0.
+   * @see address()
    */
   operator const sockaddr*() const
   { return data; }
 
   /**
-   * Returns the family of this address
-   * AF_UNSPEC if it's undefined
+   * Returns the family of this address.
+   * @return the family of this address, AF_UNSPEC if it's undefined
    */
   int family() const;
 
   /**
-   * Returns the IANA family number of this address
+   * Returns the IANA family number of this address.
+   * @return the IANA family number of this address (1 for AF_INET.
+   *         2 for AF_INET6, otherwise 0)
    */
   inline int ianaFamily() const
   { return ianaFamily(family()); }
 
   /**
-   * Returns true if this equals the other socket
+   * Returns true if this equals the other socket.
    * @param other	the other socket
+   * @return true if both sockets are equal
    */
   virtual bool isEqual(const KSocketAddress& other) const;
   bool isEqual(const KSocketAddress* other) const
   { return isEqual(*other); }
 
+  /**
+   * Overloaded == operator.
+   * @see isEqual()
+   */
   bool operator==(const KSocketAddress& other) const
   { return isEqual(other); }
 
@@ -119,8 +133,20 @@ public:
    * If not possible, like the default implementation, this returns
    * the same as isEqual.
    * @param other	the other socket
+   * @return true if the code part is equal
    */
   bool isCoreEqual(const KSocketAddress& other) const;
+
+  /**
+   * Some sockets may differ in such things as services or port numbers,
+   * like Internet sockets. This function compares only the core part
+   * of that, if possible.
+   *
+   * If not possible, like the default implementation, this returns
+   * the same as isEqual.
+   * @param other	the other socket
+   * @return true if the code part is equal
+   */
   bool isCoreEqual(const KSocketAddress* other) const
   { return isCoreEqual(*other); }
 
@@ -129,6 +155,7 @@ public:
    * as the first argument.
    * In the case of Internet sockets, this is the hostname.
    * The default implementation returns QString::null.
+   * @return the node name, can be QString::null
    */
   virtual QString nodeName() const;
 
@@ -137,6 +164,7 @@ public:
    * as the service argument.
    * In the case of Internet sockets, this is the port number.
    * The default implementation returns QString::null.
+   * @return the service name, can be QString::null
    */
   virtual QString serviceName() const;
 
@@ -154,22 +182,25 @@ private:
 public:
   /**
    * Creates a new KSocketAddress or descendant class from given
-   * raw socket address
+   * raw socket address.
    * @param sa		new socket address
    * @param size	new socket address's length
+   * @return the new KSocketAddress, or 0 if the function failed
    */
   static KSocketAddress* newAddress(const struct sockaddr*, ksocklen_t size);
 
   /**
-   * Returns the IANA family number of the given address family
-   * returns 0 if there is no corresponding IANA family number
+   * Returns the IANA family number of the given address family.
+   * Returns 0 if there is no corresponding IANA family number.
    * @param af		the address family, in AF_* constants
+   * @return the IANA family number of this address (1 for AF_INET.
+   *         2 for AF_INET6, otherwise 0)
    */
   static int ianaFamily(int af);
 
   /**
-   * Returns the address family of the given IANA family number
-   * returns AF_UNSPEC for unknown IANA family numbers
+   * Returns the address family of the given IANA family number.
+   * @return the address family, AF_UNSPEC for unknown IANA family numbers
    */
   static int fromIanaFamily(int iana);
 
@@ -217,40 +248,41 @@ public:
   KInetSocketAddress(const KInetSocketAddress&);
 
   /**
-   * Creates an IPv4 socket from raw sockaddr_in
+   * Creates an IPv4 socket from raw sockaddr_in.
    * @param sin		a sockaddr_in structure to copy from
    * @param len		the socket address length
    */
   KInetSocketAddress(const sockaddr_in* sin, ksocklen_t len);
 
   /**
-   * Creates an IPv6 socket from raw sockaddr_in6
+   * Creates an IPv6 socket from raw sockaddr_in6.
    * @param sin6       	a sockaddr_in6 structure to copy from
    * @param len		the socket address length
    */
   KInetSocketAddress(const sockaddr_in6* sin6, ksocklen_t len);
 
   /**
-   * Creates a socket from information
+   * Creates a socket from information.
    * @param addr	a binary address
    * @param port	a port number
    */
   KInetSocketAddress(const in_addr& addr, unsigned short port);
 
   /**
-   * Creates a socket from information
+   * Creates a socket from information.
    * @param addr	a binary address
    * @param port	a port number
    */
   KInetSocketAddress(const in6_addr& addr, unsigned short port);
 
   /**
-   * Creates a socket from text representation
+   * Creates a socket from text representation.
    *
-   * @see setAddress
    * @param addr	a text representation of the address
    * @param port	a port number
-   * @param family	the family for this address
+   * @param family	the family for this address. Use -1 to guess the
+   *                    family type
+   * @see setAddress
    */
   KInetSocketAddress(const QString& addr, unsigned short port, int family = -1);
 
@@ -260,15 +292,17 @@ public:
   virtual ~KInetSocketAddress();
 
   /**
-   * Sets this socket to given socket
+   * Sets this socket to given socket.
    * @param ksa		the other socket
+   * @return true if successful, false otherwise
    */
   bool setAddress(const KInetSocketAddress& ksa);
 
   /**
-   * Sets this socket to given raw socket
+   * Sets this socket to given raw socket.
    * @param sin		the raw socket
    * @param len		the socket address length
+   * @return true if successful, false otherwise
    */
   bool setAddress(const sockaddr_in* sin, ksocklen_t len);
 
@@ -278,20 +312,23 @@ public:
    * Note: this function does not clear the scope ID and flow info values
    * @param sin6	the raw socket
    * @param len		the socket address length
+   * @return true if successful, false otherwise
    */
   bool setAddress(const sockaddr_in6* sin6, ksocklen_t len);
 
   /**
-   * Sets this socket to raw address and port
+   * Sets this socket to raw address and port.
    * @param addr	the address
    * @param port	the port number
+   * @return true if successful, false otherwise
    */
   bool setAddress(const in_addr& addr, unsigned short port);
 
   /**
-   * Sets this socket to raw address and port
+   * Sets this socket to raw address and port.
    * @param addr	the address
    * @param port	the port number
+   * @return true if successful, false otherwise
    */
   bool setAddress(const in6_addr& addr, unsigned short port);
 
@@ -310,67 +347,78 @@ public:
    * @param addr	the address
    * @param port	the port number
    * @param family	the address family, -1 for any
+   * @return true if successful, false otherwise
    */
   bool setAddress(const QString& addr, unsigned short port, int family = -1);
 
   /**
-   * Sets this socket's host address to given raw address
+   * Sets this socket's host address to given raw address.
    * @param addr	the address
+   * @return true if successful, false otherwise
    */
   bool setHost(const in_addr& addr);
 
   /**
-   * Sets this socket's host address to given raw address
+   * Sets this socket's host address to given raw address.
    * @param addr	the address
+   * @return true if successful, false otherwise
    */
   bool setHost(const in6_addr& addr);
 
   /**
-   * Sets this socket's host address to given text representation
+   * Sets this socket's host address to given text representation.
    * @param addr	the address
-   * @param family	the address family, -1 for any
+   * @param family	the address family, -1 to guess the family
+   * @return true if successful, false otherwise
    */
   bool setHost(const QString& addr, int family = -1);
 
   /**
-   * Sets this socket's port number to given port number
+   * Sets this socket's port number to given port number.
    * @param port	the port number
+   * @return true if successful, false otherwise
    */
   bool setPort(unsigned short port);
 
   /**
    * Turns this into an IPv4 or IPv6 address.
    *
-   * returns false if this is v6 and information was lost. That doesn't
+   * @param family the new address family
+   * @return false if this is v6 and information was lost. That doesn't
    * mean the conversion was unsuccessful.
    */
   bool setFamily(int family);
 
   /**
-   * Sets flowinfo information for this socket address if this is IPv6
+   * Sets flowinfo information for this socket address if this is IPv6.
    * @param flowinfo	flowinfo
+   * @return true if successful, false otherwise
    */
   bool setFlowinfo(Q_UINT32 flowinfo);
 
   /**
-   * Sets the scope id for this socket if this is IPv6
+   * Sets the scope id for this socket if this is IPv6.
    * @param scopeid	the scope id
+   * @return true if successful, false otherwise
    */
   bool setScopeId(int scopeid);
 
   /**
-   * Returns a pretty representation of this address
+   * Returns a pretty representation of this address.
+   * @return a pretty representation
    */
   virtual QString pretty() const;
 
   /**
-   * Returns the text representation of the host address
+   * Returns the text representation of the host address.
+   * @return a text representation of the host address
    */
   virtual QString nodeName() const;
   //  QString prettyHost() const;
 
   /**
-   * Returns the text representation of the port number
+   * Returns the text representation of the port number.
+   * @param the name of the service (a number)
    */
   virtual QString serviceName() const;
 
@@ -381,17 +429,20 @@ public:
    * This function will return an IPv4 socket if this IPv6
    * socket is a v4-mapped address. That is, if it's really
    * an IPv4 address, but in v6 disguise.
+   * @return the sockaddr_in struct, can be 0.
    */
   const sockaddr_in* addressV4() const;
 
   /**
    * Returns the socket address in IPv6
+   * @return the sockaddr_in struct, can be 0 if IPv6 is unsupported.
    */
   const sockaddr_in6* addressV6() const;
 
   /**
    * Returns the host address.
-   * Might be empty
+   * Might be empty.
+   * @return the host address
    */
   in_addr hostV4() const;
 
@@ -399,32 +450,60 @@ public:
    * Returns the host address.
    *
    * WARNING: this function is not defined if there is no IPv6 support
+   * @return the host address
    */
   in6_addr hostV6() const;
 
   /**
-   * Returns the port number
+   * Returns the port number.
+   * @return the port number
    */
   unsigned short port() const;
 
   /**
-   * Returns flowinfo for IPv6 socket
+   * Returns flowinfo for IPv6 socket.
+   * @return the flowinfo, 0 if unsupported
    */
   Q_UINT32 flowinfo() const;
 
   /**
-   * Returns the scope id for this IPv6 socket
+   * Returns the scope id for this IPv6 socket.
+   * @return the scope id
    */
   int scopeId() const;
 
   /**
-   * Returns the socket length
+   * Returns the socket length.
    * Will be either sizeof(sockaddr_in) or sizeof(sockaddr_in6)
+   * @return the length of the socket
    */
   virtual ksocklen_t size() const; // should be socklen_t
 
   /* comparation */
+  /**
+   * Compares two IPv4 addresses.
+   * @param s1 the first address to compare
+   * @param s2 the second address to compare
+   * @param coreOnly true if only core parts should be compared (only 
+   *                 the address)
+   * @return true if the given addresses are equal.
+   * @see areEqualInet6()
+   * @see KSocketAddress::isEqual()
+   * @see KSocketAddress::isCoreEqual()
+   */
   static bool areEqualInet(const KSocketAddress &s1, const KSocketAddress &s2, bool coreOnly);
+
+  /**
+   * Compares two IPv6 addresses.
+   * @param s1 the first address to compare
+   * @param s2 the second address to compare
+   * @param coreOnly true if only core parts should be compared (only 
+   *                 the address)
+   * @return true if the given addresses are equal.
+   * @see areEqualInet()
+   * @see KSocketAddress::isEqual()
+   * @see KSocketAddress::isCoreEqual()
+   */
   static bool areEqualInet6(const KSocketAddress &s1, const KSocketAddress &s2, bool coreOnly);
 
   /* operators */
@@ -432,16 +511,22 @@ public:
   /**
    * Returns the socket address.
    * This will be NULL if this is a non-convertible v6.
-   * @see addressV4
+   * @return the sockaddr_in structure, can be 0 if v6.
+   * @see addressV4()
    */
   operator const sockaddr_in*() const
   { return addressV4(); }
 
+  /**
+   * Returns the socket address.
+   * @return the sockaddr_in structure, can be 0 if v6 is unsupported.
+   * @see addressV6()
+   */
   operator const sockaddr_in6*() const
   { return addressV6(); }
 
   /**
-   * Sets this object to be the same as the other
+   * Sets this object to be the same as the other.
    */
   KInetSocketAddress& operator=(const KInetSocketAddress &other)
   { setAddress(other); return *this; }
@@ -457,6 +542,7 @@ public:
    * This function returns QString::null if the address cannot be converted.
    * @param family	the family of the address
    * @param addr	the address, in raw form
+   * @return the converted address, or QString::null if not possible.
    */
   static QString addrToString(int family, const void *addr);
 
@@ -464,10 +550,10 @@ public:
    * Converts the address given in text form into raw form.
    * The size of the destination buffer @p dest is supposed to be
    * large enough to hold the address of the given family.
-   * This function returns true if convertion was successful.
    * @param family	the family of the address
    * @param text	the text representation of the address
    * @param dest	the destination buffer of the address
+   * @return true if convertion was successful.
    */
   static bool stringToAddr(int family, const char *text, void *dest);
 
@@ -489,7 +575,7 @@ class KUnixSocketAddressPrivate;
 /**
  * A Unix socket address
  *
- * This is a Unix socket address
+ * This is a Unix socket address.
  *
  * This class expects QCString instead of QString values, which means the
  * filenames should be encoded in whatever form locale/system deems necessary
@@ -509,14 +595,14 @@ public:
   KUnixSocketAddress();
 
   /**
-   * Constructor from raw data
+   * Constructor from raw data.
    * @param raw_data	raw data
    * @param size	data length
    */
   KUnixSocketAddress(const sockaddr_un* raw_data, ksocklen_t size);
 
   /**
-   * Constructor from pathname
+   * Constructor from pathname.
    * @param path	pathname
    */
   KUnixSocketAddress(QCString pathname);
@@ -527,45 +613,65 @@ public:
   virtual ~KUnixSocketAddress();
 
   /**
-   * Sets this to given sockaddr_un
+   * Sets this to given sockaddr_un.
    * @param socket_address socket address
    * @param size	the socket length
+   * @return true if successful, false otherwise
    */
   bool setAddress(const sockaddr_un* socket_address, ksocklen_t size);
 
   /**
-   * Sets this to given pathname
+   * Sets this to given pathname.
    * @param path	pathname
+   * @return true if successful, false otherwise
    */
   bool setAddress(QCString path);
 
   /**
-   * Returns the pathname
+   * Returns the pathname.
+   * @return the pathname, can be QCString::null if uninitialized, or
+   *         "" if unknown
    */
   QCString pathname() const;
 
   /**
-   * Returns pretty representation of this socket
+   * Returns pretty representation of this socket.
+   * @return a pretty text representation of the socket.
    */
   virtual QString pretty() const;
 
   /*
    * Returns the path in the form of a QString.
-   * This can be fed into KExtendedSocket
+   * This can be fed into KExtendedSocket.
+   * @return the path (can be QString::null).
+   * @see pathname()
    */
   virtual QString serviceName() const;
 
   /**
-   * Returns raw socket address
+   * Returns raw socket address.
+   * @return the raw socket address (can be 0 if uninitialized)
    */
   const sockaddr_un* address() const;
 
   /**
-   * Returns raw socket address
+   * Returns raw socket address.
+   * @return the raw socket address (can be 0 if uninitialized)
+   * @see address()
    */
   operator const sockaddr_un*() const
   { return address(); }
 
+  /**
+   * Compares two unix socket addresses.
+   * @param s1 the first address to compare
+   * @param s2 the second address to compare
+   * @param coreOnly true if only core parts should be compared (currently 
+   *                 unused) 
+   * @return true if the given addresses are equal.
+   * @see KSocketAddress::isEqual()
+   * @see KSocketAddress::isCoreEqual()
+   */
   static bool areEqualUnix(const KSocketAddress &s1, const KSocketAddress &s2, bool coreOnly);
 
 private:
