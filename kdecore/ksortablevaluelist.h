6f642697c166a569f3841819034ebd216a9d3a0b
Tim Jansen <tim@tjansen.de>
2002-09-10 20:16:09 +0000

    doc update
    svn path=/trunk/kdelibs/; revision=176976

--- kdecore/ksortablevaluelist.h
+++ kdecore/ksortablevaluelist.h
@@ -23,15 +23,35 @@
 #include <qpair.h>
 #include <qvaluelist.h>
 
+/**
+ * KSortableItem is a @ref QPair that provides several operators
+ * for sorting.
+ * @see KSortableValueList
+ */
 template<class T, class Key = int> class KSortableItem : public QPair<Key,T>
 {
 public:
+    /**
+     * Creates a new KSortableItem with the given values.
+     * @param i the first value
+     * @param t the second value
+     */
     KSortableItem( Key i, const T& t ) : QPair<Key, T>( i, t ) {}
+    /**
+     * Creates a new KSortableItem that copies another one.
+     * @param rhs the other item to copy
+     */
     KSortableItem( const KSortableItem<T, Key> &rhs ) 
         : QPair<Key,T>( rhs.first, rhs.second ) {}
 
+    /**
+     * Creates a new KSortableItem with uninitialized values.
+     */
     KSortableItem() {}
 
+    /**
+     * Assignment operator, just copies the item.
+     */
     KSortableItem<T, Key> &operator=( const KSortableItem<T, Key>& i ) {
         first  = i.first;
         second = i.second;
@@ -39,55 +59,113 @@ public:
     }
 
     // operators for sorting
+    /**
+     * Compares the two items. This implementation only compares
+     * the first value.
+     */
     bool operator> ( const KSortableItem<T, Key>& i2 ) const {
         return (i2.first < first);
     }
+    /**
+     * Compares the two items. This implementation only compares
+     * the first value.
+     */
     bool operator< ( const KSortableItem<T, Key>& i2 ) const {
         return (first < i2.first);
     }
+    /**
+     * Compares the two items. This implementation only compares
+     * the first value.
+     */
     bool operator>= ( const KSortableItem<T, Key>& i2 ) const {
         return (first >= i2.first);
     }
+    /**
+     * Compares the two items. This implementation only compares
+     * the first value.
+     */
     bool operator<= ( const KSortableItem<T, Key>& i2 ) const {
         return !(i2.first < first);
     }
+    /**
+     * Compares the two items. This implementation only compares
+     * the first value.
+     */
     bool operator== ( const KSortableItem<T, Key>& i2 ) const {
         return (first == i2.first);
     }
+    /**
+     * Compares the two items. This implementation only compares
+     * the first value.
+     */
     bool operator!= ( const KSortableItem<T, Key>& i2 ) const {
         return (first != i2.first);
     }
 
+    /**
+     * Returns the second value.
+     * @return the second value
+     */
     T& value() {
         return second;
     }
+
+    /**
+     * Returns the second value.
+     */
     const T& value() const {
         return second;
     }
 
+    /**
+     * Returns the first value.
+     * @param the first value
+     */
     Key index() const {
         return first;
     }
 };
 
 
-// convenience
+/**
+ * KSortableValueList is a special @ref QValueList for 
+ * @ref KSortableItem. It includes convenience operators
+ * to get the first value of the KSortableItem and a method
+ * to sort all items.
+ */
 template <class T, class Key = int>
 class KSortableValueList : public QValueList<KSortableItem<T, Key> >
 {
 public:
+    /**
+     * Insert a KSortableItem with the given values.
+     * @param i the first value
+     * @param t the second value
+     */
     void insert( Key i, const T& t ) {
         QValueList<KSortableItem<T, Key> >::append( KSortableItem<T, Key>( i, t ) );
     }
     // add more as you please...
 
+    /**
+     * Returns the first value of the KSortableItem at the given position.
+     * @return the first value of the KSortableItem
+     */
     T& operator[]( Key i ) {
         return QValueList<KSortableItem<T, Key> >::operator[]( i ).value();
     }
+
+    /**
+     * Returns the first value of the KSortableItem at the given position.
+     * @return the first value of the KSortableItem
+     */
     const T& operator[]( Key i ) const {
         return QValueList<KSortableItem<T, Key> >::operator[]( i ).value();
     }
-    
+
+    /**
+     * Sorts the KSortableItems.
+     */
     void sort() {
         qHeapSort( *this );
     }
