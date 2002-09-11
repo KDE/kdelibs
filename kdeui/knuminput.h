b425963fc02d75ec34cffafb9341ec4ea2d483c9
David Faure <faure@kde.org>
2002-09-11 09:58:20 +0000

    typo fixed
    svn path=/trunk/kdelibs/; revision=177112

--- kdeui/knuminput.h
+++ kdeui/knuminput.h
@@ -870,7 +870,7 @@ public:
   **/
   virtual void setPrecision( int precision, bool force );
 
-  /** @return the cuurent value */
+  /** @return the current value */
   double value() const;
   /** @return the current lower bound */
   double minValue() const;
