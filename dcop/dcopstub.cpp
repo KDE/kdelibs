7c8de997108c956ab382d1fe0c7885b30344d5fd
Jan-Pascal van Best <janpascal@vanbest.org>
2002-09-11 14:55:21 +0000

    trivial compilation fixes
    svn path=/trunk/kdelibs/; revision=177178

--- dcop/dcopstub.cpp
+++ dcop/dcopstub.cpp
@@ -23,7 +23,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 ******************************************************************/
 
 #include "dcopstub.h"
-#include <dcopclient.h>
+#include "dcopclient.h"
 
 class DCOPStubPrivate
 {
