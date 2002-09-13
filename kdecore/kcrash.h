3ba4efe144a3cf7d33cd7c4bde52fa9d14e3fd88
Tim Jansen <tim@tjansen.de>
2002-09-13 00:31:41 +0000

    doc update
    svn path=/trunk/kdelibs/; revision=177480

--- kdecore/kcrash.h
+++ kdecore/kcrash.h
@@ -40,7 +40,16 @@ class KCrash
   static const char *appPath;
 
  public:
+  /**
+   * The default crash handler.
+   * @param signal the signal number
+   */
   static void defaultCrashHandler (int signal);
+
+  /**
+   * This function type is a pointer to a crash handler function.
+   * The function's argument is the number of the signal.
+   */
   typedef void (*HandlerType)(int);
 
   /**
@@ -51,12 +60,14 @@ class KCrash
    * @li if handler is omitted the default crash handler is installed.
    * @li an user defined function in the form:
    * static (if in a class) void myCrashHandler(int);
+   * @param handler the crash handler
    */
 
   static void setCrashHandler (HandlerType handler = defaultCrashHandler);
 
   /**
-   * Returns the installed crash handler
+   * Returns the installed crash handler.
+   * @return the crash handler
    */
   static HandlerType crashHandler() { return _crashHandler; }
 
@@ -65,27 +76,37 @@ class KCrash
    * It is the crash handler´s responsibility to call this function.
    * Therefore, if no crash handler is set, the default crash handler
    * is installed to ensure the save function is called.
+   * @param saveFunction the handler to install
    */
   static void setEmergencySaveFunction (HandlerType saveFunction = (HandlerType)0);
   /**
    * Return the currently set emergency save function.
+   * @return the emergency save function
    */
   static HandlerType emergencySaveFunction() { return _emergencySaveFunction; }
 
   /**
-   * Sets the application path @p path which should be passed to
+   * Sets the application @p path which should be passed to
    * Dr. Konqi, our nice crash display application.
+   * @param path the application path.
    */
   static void setApplicationPath (QString path) { appPath = qstrdup(path.local8Bit().data()); }
 
   /**
    * Sets the application name @p name which should be passed to
    * Dr. Konqi, our nice crash display application.
+   * @param name the name of the application, as shown in Dr. Konqi
    */
   static void setApplicationName (QString name) { appName = qstrdup(name.local8Bit().data()); }
 
  protected:
+  /**
+   * Pointer to the crash handler.
+   */
   static HandlerType _crashHandler;
+  /**
+   * Pointer to the emergency save function.
+   */
   static HandlerType _emergencySaveFunction;
 };
 
