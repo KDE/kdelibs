0d01c9972b702532724dd5589d976a467c166f87
Tim Jansen <tim@tjansen.de>
2002-09-14 15:53:37 +0000

    doc update
    svn path=/trunk/kdelibs/; revision=177786

--- kdecore/ksocks.h
+++ kdecore/ksocks.h
@@ -31,19 +31,31 @@ class KConfigBase;
 struct sockaddr;
 
 
+/**
+ * This class provides you with an interface to a 
+ * <a href="http://www.socks.nec.com/">SOCKS</a> Proxy server. A SOCKS server
+ * is able to provide full internet access behind a firewall.
+ * KSocks is a singleton, there can noly be one instance at any
+ * given time. To obtain a reference to that instance, use
+ * @ref self().
+ *
+ * @short Access to a SOCKS Proxy.
+ */
 class KSocks {
    friend class KStaticDeleter<KSocks>;
 
 public:
 
    /**
-    * Return an instance of class KSocks *
+    * Return an instance of class KSocks *.
     * You cannot delete this object.  It is a singleton class.
+    * @return the KSock instance
     */
    static KSocks *self();
 
    /**
-    * Return true if KSocks has been started (ie someone called self())
+    * Checks whether KSocks has been started (ie someone called self())
+    * @return true if activated
     */
    static bool activated();
 
@@ -58,50 +70,105 @@ public:
    void enableSocks();
 
    /**
-    * True if SOCKS is currently being used.
+    * Checks whether SOCKS is currently being used.
+    * @return true if SOCKS is currently being used.
     */
    bool usingSocks();
 
    /**
-    * True if SOCKS is available for use.
+    * Checks whether SOCKS is available for use.
+    * @return true if SOCKS is available for use.
     */
    bool hasSocks();
 
-   /**
+   /*
     **     REIMPLEMENTATIONS OF LIBC SOCKET FUNCTIONS
     **/
+   /**
+    * This is the re-implementation of libc's function of the same
+    * name. Read the appropriate man page.
+    */
     int     connect     (int sockfd, const sockaddr *serv_addr, 
                                 ksocklen_t addrlen);
+   /**
+    * This is the re-implementation of libc's function of the same
+    * name. Read the appropriate man page.
+    */
     signed long int read  (int fd, void *buf, unsigned long int count);
+   /**
+    * This is the re-implementation of libc's function of the same
+    * name. Read the appropriate man page.
+    */
     signed long int write (int fd, const void *buf, unsigned long int count);
+   /**
+    * This is the re-implementation of libc's function of the same
+    * name. Read the appropriate man page.
+    */
     int     recvfrom    (int s, void *buf, unsigned long int len, int flags, 
                                 sockaddr *from, ksocklen_t *fromlen);
+   /**
+    * This is the re-implementation of libc's function of the same
+    * name. Read the appropriate man page.
+    */
     int     sendto      (int s, const void *msg, unsigned long int len, int flags,
                              const sockaddr *to, ksocklen_t tolen);
+   /**
+    * This is the re-implementation of libc's function of the same
+    * name. Read the appropriate man page.
+    */
     int     recv        (int s, void *buf, unsigned long int len, int flags);
+   /**
+    * This is the re-implementation of libc's function of the same
+    * name. Read the appropriate man page.
+    */
     int     send        (int s, const void *msg, unsigned long int len, int flags);
+   /**
+    * This is the re-implementation of libc's function of the same
+    * name. Read the appropriate man page.
+    */
     int     getsockname (int s, sockaddr *name, ksocklen_t *namelen);
+   /**
+    * This is the re-implementation of libc's function of the same
+    * name. Read the appropriate man page.
+    */
     int     getpeername (int s, sockaddr *name, ksocklen_t *namelen);
+   /**
+    * This is the re-implementation of libc's function of the same
+    * name. Read the appropriate man page.
+    */
     int     accept      (int s, sockaddr *addr, ksocklen_t *addrlen);
+   /**
+    * This is the re-implementation of libc's function of the same
+    * name. Read the appropriate man page.
+    */
     int     select      (int n, fd_set *readfds, fd_set *writefds, 
                                 fd_set *exceptfds, struct timeval *timeout);
+   /**
+    * This is the re-implementation of libc's function of the same
+    * name. Read the appropriate man page.
+    */
     int     listen      (int s, int backlog);
+   /**
+    * This is the re-implementation of libc's function of the same
+    * name. Read the appropriate man page.
+    */
     int     bind        (int sockfd, sockaddr *my_addr, 
                                      ksocklen_t addrlen);
 
-   /*
+    /**
     *  If you're using this, you're probably doing something wrong.
     *  Please don't use it.
+    * @internal
     */
    void die();
 
-   /*
+   /**
     *  Set this before the first call to KSocks::self() and it will fail
     *  to initialize SOCKS.
     */
    static void disable();
 
-   /*
+   /**
     *  Set this before the first call to KSocks::self() and it will use
     *  @p config to read its configuration from.
     */
