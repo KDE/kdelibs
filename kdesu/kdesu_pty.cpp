4be5c335cd2650282d65c48083244f71edd74bda
Alex Zepeda <zipzippy@sonic.net>
2002-09-30 20:15:02 +0000

    Make this compile.  Unsure if fbsd-stable needs this or if this breaks it for them.  Hi Andy.
    svn path=/trunk/kdelibs/; revision=181375

--- kdesu/kdesu_pty.cpp
+++ kdesu/kdesu_pty.cpp
@@ -20,7 +20,9 @@
 #define _GNU_SOURCE   /* Needed for getpt, ptsname in glibc 2.1.x systems */
 #endif
 
-#if !defined(_XOPEN_SOURCE) && !defined(__osf__)
+// This should probably be a check for glibc, rather than a check for 
+// every system it breaks which is most everything else
+#if !defined(_XOPEN_SOURCE) && !defined(__osf__) && !defined(__FreeBSD__) && !defined(__NetBSD__)
 #define _XOPEN_SOURCE /* Needed for grantpt, unlockpt in glibc 2.1.x      */
 #endif
 
