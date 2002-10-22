7b7df21192ed9f8cb187236ef1726d521818ca1d
David Faure <faure@kde.org>
2002-10-22 15:27:21 +0000

    Based on patch by Pascal Letourneau: only look for renamedlg-plugins if M_OVERWRITE is set. This unbreaks the case of M_OVERWRITE_ITSELF for instance.
    svn path=/trunk/kdelibs/; revision=185148

--- kio/kio/renamedlg.cpp
+++ kio/kio/renamedlg.cpp
@@ -88,254 +88,264 @@ RenameDlg::RenameDlg(QWidget *parent, const QString & _caption,
                      bool _modal)
   : QDialog ( parent, "KIO::RenameDialog" , _modal )
 {
-  d = new RenameDlgPrivate( );
-  d->modal = _modal;
-  // Set "StaysOnTop", because this dialog is typically used in kio_uiserver,
-  // i.e. in a separate process.
+    d = new RenameDlgPrivate( );
+    d->modal = _modal;
+    // Set "StaysOnTop", because this dialog is typically used in kio_uiserver,
+    // i.e. in a separate process.
+    // ####### This isn't the case anymore - remove?
 #ifndef Q_WS_QWS //FIXME(E): Implement for QT Embedded
-  if (d->modal)
-    KWin::setState( winId(), NET::StaysOnTop );
+    if (d->modal)
+        KWin::setState( winId(), NET::StaysOnTop );
 #endif
 
-  d->src = _src;
-  d->dest = _dest;
-  d->plugin = false;
+    d->src = _src;
+    d->dest = _dest;
+    d->plugin = false;
 
 
-  setCaption( _caption );
+    setCaption( _caption );
 
-  d->b0 = new KPushButton( KStdGuiItem::cancel(), this );
-  connect(d->b0, SIGNAL(clicked()), this, SLOT(b0Pressed()));
+    d->b0 = new KPushButton( KStdGuiItem::cancel(), this );
+    connect(d->b0, SIGNAL(clicked()), this, SLOT(b0Pressed()));
 
-  if ( ! (_mode & M_NORENAME ) ) {
-      d->b1 = new QPushButton( i18n( "&Rename" ), this );
-      d->b1->setEnabled(false);
-      d->b8 = new QPushButton( i18n( "&Propose" ), this );
-      connect(d->b8, SIGNAL(clicked()), this, SLOT(b8Pressed()));
-      connect(d->b1, SIGNAL(clicked()), this, SLOT(b1Pressed()));
-  }
-
-  if ( ( _mode & M_MULTI ) && ( _mode & M_SKIP ) ) {
-    d->b2 = new QPushButton( i18n( "&Skip" ), this );
-    connect(d->b2, SIGNAL(clicked()), this, SLOT(b2Pressed()));
-
-    d->b3 = new QPushButton( i18n( "&Auto Skip" ), this );
-    connect(d->b3, SIGNAL(clicked()), this, SLOT(b3Pressed()));
-  }
-
-  if ( _mode & M_OVERWRITE ) {
-    d->b4 = new QPushButton( i18n( "&Overwrite" ), this );
-    connect(d->b4, SIGNAL(clicked()), this, SLOT(b4Pressed()));
-
-    if ( _mode & M_MULTI ) {
-      d->b5 = new QPushButton( i18n( "O&verwrite All" ), this );
-      connect(d->b5, SIGNAL(clicked()), this, SLOT(b5Pressed()));
-    }
-  }
-
-  if ( _mode & M_RESUME ) {
-    d->b6 = new QPushButton( i18n( "&Resume" ), this );
-    connect(d->b6, SIGNAL(clicked()), this, SLOT(b6Pressed()));
-
-    if ( _mode & M_MULTI )
-      {
-	d->b7 = new QPushButton( i18n( "R&esume All" ), this );
-	connect(d->b7, SIGNAL(clicked()), this, SLOT(b7Pressed()));
-      }
-  }
-
-  d->m_pLayout = new QVBoxLayout( this, KDialog::marginHint(),
-			       KDialog::spacingHint() );
-  d->m_pLayout->addStrut( 360 );	// makes dlg at least that wide
-  //  figure out the mimetype and load one plugin
-  pluginHandling();
-  KTrader::OfferList plugin_offers;
-  if( d->mimeSrc != KMimeType::defaultMimeType()   ){
-    plugin_offers = KTrader::self()->query(d->mimeSrc, "'RenameDlg/Plugin' in ServiceTypes");
-
-  }else if(d->mimeDest != KMimeType::defaultMimeType() ) {
-    plugin_offers = KTrader::self()->query(d->mimeDest, "'RenameDlg/Plugin' in ServiceTypes");
-  }
-  if(!plugin_offers.isEmpty() ){
-    KTrader::OfferList::ConstIterator it = plugin_offers.begin();
-    KTrader::OfferList::ConstIterator end = plugin_offers.end();
-    for( ; it != end; ++it ){
-      kdDebug(7024) << "Offers" << endl;
-      QString libName = (*it)->library();
-      if( libName.isEmpty() ){
-	kdDebug(7024) << "lib is empty" << endl;
-	continue;
-      }
-      KLibrary *lib = KLibLoader::self()->library(libName.local8Bit() );
-      if(!lib) {
-	continue;
-      }
-      KLibFactory *factory = lib->factory();
-      if(!factory){
-	lib->unload();
-	continue;
-      }
-      QObject *obj = factory->create( this, (*it)->name().latin1() );
-      if(!obj) {
-	lib->unload();
-	continue;
-      }
-      RenameDlgPlugin *plugin = static_cast<RenameDlgPlugin *>(obj);
-      if(!plugin ){
-	delete obj;
-	continue;
-      }
-      if( plugin->initialize( _mode, _src, _dest, d->mimeSrc,
-			      d->mimeDest, sizeSrc, sizeDest,
-			      ctimeSrc, ctimeDest,
-			      mtimeSrc, mtimeDest ) ) {
-      d->plugin = true;
-      d->m_pLayout->addWidget(plugin );
-      kdDebug(7024) << "RenameDlgPlugin" << endl;
-      break;
-      }else {
-	delete obj;
-      }
+    if ( ! (_mode & M_NORENAME ) ) {
+        d->b1 = new QPushButton( i18n( "&Rename" ), this );
+        d->b1->setEnabled(false);
+        d->b8 = new QPushButton( i18n( "&Propose" ), this );
+        connect(d->b8, SIGNAL(clicked()), this, SLOT(b8Pressed()));
+        connect(d->b1, SIGNAL(clicked()), this, SLOT(b1Pressed()));
     }
 
-  }
-  if( !d->plugin ){
-  // User tries to overwrite a file with itself ?
+    if ( ( _mode & M_MULTI ) && ( _mode & M_SKIP ) ) {
+        d->b2 = new QPushButton( i18n( "&Skip" ), this );
+        connect(d->b2, SIGNAL(clicked()), this, SLOT(b2Pressed()));
+
+        d->b3 = new QPushButton( i18n( "&Auto Skip" ), this );
+        connect(d->b3, SIGNAL(clicked()), this, SLOT(b3Pressed()));
+    }
+
+    if ( _mode & M_OVERWRITE ) {
+        d->b4 = new QPushButton( i18n( "&Overwrite" ), this );
+        connect(d->b4, SIGNAL(clicked()), this, SLOT(b4Pressed()));
+
+        if ( _mode & M_MULTI ) {
+            d->b5 = new QPushButton( i18n( "O&verwrite All" ), this );
+            connect(d->b5, SIGNAL(clicked()), this, SLOT(b5Pressed()));
+        }
+    }
+
+    if ( _mode & M_RESUME ) {
+        d->b6 = new QPushButton( i18n( "&Resume" ), this );
+        connect(d->b6, SIGNAL(clicked()), this, SLOT(b6Pressed()));
+
+        if ( _mode & M_MULTI )
+        {
+            d->b7 = new QPushButton( i18n( "R&esume All" ), this );
+            connect(d->b7, SIGNAL(clicked()), this, SLOT(b7Pressed()));
+        }
+    }
+
+    d->m_pLayout = new QVBoxLayout( this, KDialog::marginHint(),
+                                    KDialog::spacingHint() );
+    d->m_pLayout->addStrut( 360 );	// makes dlg at least that wide
+
+    // User tries to overwrite a file with itself ?
     if ( _mode & M_OVERWRITE_ITSELF ) {
-      QLabel *lb = new QLabel( i18n("This action would overwrite '%1' with itself.\n"
-                                    "Do you want to rename it instead?").arg(KStringHandler::csqueeze(d->src,100)), this );
-      d->m_pLayout->addWidget(lb);
-    }  else if ( _mode & M_OVERWRITE ) {
-      QGridLayout * gridLayout = new QGridLayout( 0L, 9, 2, KDialog::marginHint(),
-                                                  KDialog::spacingHint() );
-      d->m_pLayout->addLayout(gridLayout);
-      gridLayout->setColStretch(0,0);
-      gridLayout->setColStretch(1,10);
+        QLabel *lb = new QLabel( i18n( "This action would overwrite '%1' with itself.\n"
+                                       "Do you want to rename it instead?" ).arg( KStringHandler::csqueeze( d->src,100 ) ), this );
+        d->m_pLayout->addWidget( lb );
+    }
+    else if ( _mode & M_OVERWRITE ) {
 
-      QString sentence1;
-      if (mtimeDest < mtimeSrc)
-          sentence1 = i18n("An older item named '%1' already exists.");
-      else if (mtimeDest == mtimeSrc)
-          sentence1 = i18n("A similar file named '%1' already exists.");
-      else
-          sentence1 = i18n("A newer item named '%1' already exists.");
-      QLabel * lb1 = new QLabel( sentence1.arg(KStringHandler::csqueeze(d->dest,100)), this );
-      gridLayout->addMultiCellWidget( lb1, 0, 0, 0, 1 ); // takes the complete first line
+        // Figure out the mimetype and load one plugin
+        // (This is the only mode that is handled by plugins)
+        pluginHandling();
+        KTrader::OfferList plugin_offers;
+        if( d->mimeSrc != KMimeType::defaultMimeType()   ){
+            plugin_offers = KTrader::self()->query(d->mimeSrc, "'RenameDlg/Plugin' in ServiceTypes");
 
-      lb1 = new QLabel( this );
-      lb1->setPixmap( KMimeType::pixmapForURL( d->dest ) );
-      gridLayout->addMultiCellWidget( lb1, 1, 3, 0, 0 ); // takes the first column on rows 1-3
+        }else if(d->mimeDest != KMimeType::defaultMimeType() ) {
+            plugin_offers = KTrader::self()->query(d->mimeDest, "'RenameDlg/Plugin' in ServiceTypes");
+        }
+        if(!plugin_offers.isEmpty() ){
+            kdDebug(7024) << "Offers" << endl;
+            KTrader::OfferList::ConstIterator it = plugin_offers.begin();
+            KTrader::OfferList::ConstIterator end = plugin_offers.end();
+            for( ; it != end; ++it ){
+                QString libName = (*it)->library();
+                if( libName.isEmpty() ){
+                    kdDebug(7024) << "lib is empty" << endl;
+                    continue;
+                }
+                KLibrary *lib = KLibLoader::self()->library(libName.local8Bit() );
+                if(!lib) {
+                    continue;
+                }
+                KLibFactory *factory = lib->factory();
+                if(!factory){
+                    lib->unload();
+                    continue;
+                }
+                QObject *obj = factory->create( this, (*it)->name().latin1() );
+                if(!obj) {
+                    lib->unload();
+                    continue;
+                }
+                RenameDlgPlugin *plugin = static_cast<RenameDlgPlugin *>(obj);
+                if(!plugin ){
+                    delete obj;
+                    continue;
+                }
+                if( plugin->initialize( _mode, _src, _dest, d->mimeSrc,
+                                        d->mimeDest, sizeSrc, sizeDest,
+                                        ctimeSrc, ctimeDest,
+                                        mtimeSrc, mtimeDest ) ) {
+                    d->plugin = true;
+                    d->m_pLayout->addWidget(plugin );
+                    kdDebug(7024) << "RenameDlgPlugin" << endl;
+                    break;
+                } else {
+                    delete obj;
+                }
+            }
 
-      int row = 1;
-      if ( sizeDest != (KIO::filesize_t)-1 )
-      {
-          QLabel * lb = new QLabel( i18n("size %1").arg( KIO::convertSize(sizeDest) ), this );
-          gridLayout->addWidget( lb, row, 1 );
-          row++;
+        }
 
-      }
-      if ( ctimeDest != (time_t)-1 )
-      {
-          QDateTime dctime; dctime.setTime_t( ctimeDest );
-          QLabel * lb = new QLabel( i18n("created on %1").arg( KGlobal::locale()->formatDateTime(dctime) ), this );
-          gridLayout->addWidget( lb, row, 1 );
-          row++;
-      }
-      if ( mtimeDest != (time_t)-1 )
-      {
-          QDateTime dmtime; dmtime.setTime_t( mtimeDest );
-          QLabel * lb = new QLabel( i18n("modified on %1").arg( KGlobal::locale()->formatDateTime(dmtime) ), this );
-          gridLayout->addWidget( lb, row, 1 );
-          row++;
-      }
+        if( !d->plugin ){
+            // No plugin found, build default dialog
+            QGridLayout * gridLayout = new QGridLayout( 0L, 9, 2, KDialog::marginHint(),
+                                                        KDialog::spacingHint() );
+            d->m_pLayout->addLayout(gridLayout);
+            gridLayout->setColStretch(0,0);
+            gridLayout->setColStretch(1,10);
 
-      if ( !d->src.isEmpty() )
-      {
-          // rows 1 to 3 are the details (size/ctime/mtime), row 4 is empty
-          gridLayout->addRowSpacing( 4, 20 );
+            QString sentence1;
+            if (mtimeDest < mtimeSrc)
+                sentence1 = i18n("An older item named '%1' already exists.");
+            else if (mtimeDest == mtimeSrc)
+                sentence1 = i18n("A similar file named '%1' already exists.");
+            else
+                sentence1 = i18n("A newer item named '%1' already exists.");
+            QLabel * lb1 = new QLabel( sentence1.arg(KStringHandler::csqueeze(d->dest,100)), this );
+            gridLayout->addMultiCellWidget( lb1, 0, 0, 0, 1 ); // takes the complete first line
 
-          QLabel * lb2 = new QLabel( i18n("The source file is '%1'").arg(d->src), this );
-          gridLayout->addMultiCellWidget( lb2, 5, 5, 0, 1 ); // takes the complete first line
+            lb1 = new QLabel( this );
+            lb1->setPixmap( KMimeType::pixmapForURL( d->dest ) );
+            gridLayout->addMultiCellWidget( lb1, 1, 3, 0, 0 ); // takes the first column on rows 1-3
 
-          lb2 = new QLabel( this );
-          lb2->setPixmap( KMimeType::pixmapForURL( d->src ) );
-          gridLayout->addMultiCellWidget( lb2, 6, 8, 0, 0 ); // takes the first column on rows 6-8
+            int row = 1;
+            if ( sizeDest != (KIO::filesize_t)-1 )
+            {
+                QLabel * lb = new QLabel( i18n("size %1").arg( KIO::convertSize(sizeDest) ), this );
+                gridLayout->addWidget( lb, row, 1 );
+                row++;
 
-          row = 6;
+            }
+            if ( ctimeDest != (time_t)-1 )
+            {
+                QDateTime dctime; dctime.setTime_t( ctimeDest );
+                QLabel * lb = new QLabel( i18n("created on %1").arg( KGlobal::locale()->formatDateTime(dctime) ), this );
+                gridLayout->addWidget( lb, row, 1 );
+                row++;
+            }
+            if ( mtimeDest != (time_t)-1 )
+            {
+                QDateTime dmtime; dmtime.setTime_t( mtimeDest );
+                QLabel * lb = new QLabel( i18n("modified on %1").arg( KGlobal::locale()->formatDateTime(dmtime) ), this );
+                gridLayout->addWidget( lb, row, 1 );
+                row++;
+            }
 
-          if ( sizeSrc != (KIO::filesize_t)-1 )
-          {
-              QLabel * lb = new QLabel( i18n("size %1").arg( KIO::convertSize(sizeSrc) ), this );
-              gridLayout->addWidget( lb, row, 1 );
-              row++;
-          }
-          if ( ctimeSrc != (time_t)-1 )
-          {
-              QDateTime dctime; dctime.setTime_t( ctimeSrc );
-              QLabel * lb = new QLabel( i18n("created on %1").arg( KGlobal::locale()->formatDateTime(dctime) ), this );
-              gridLayout->addWidget( lb, row, 1 );
-              row++;
-          }
-          if ( mtimeSrc != (time_t)-1 )
-          {
-              QDateTime dmtime; dmtime.setTime_t( mtimeSrc );
-              QLabel * lb = new QLabel( i18n("modified on %1").arg( KGlobal::locale()->formatDateTime(dmtime) ), this );
-              gridLayout->addWidget( lb, row, 1 );
-              row++;
-          }
-      }
-  } else
-  {
-      // I wonder when this happens (David). And 'dest' isn't shown at all here...
-      QString sentence1;
-      if (mtimeDest < mtimeSrc)
- 	  sentence1 = i18n("An older item than '%1' already exists.\n").arg(d->src);
-      else if (mtimeDest == mtimeSrc)
-          sentence1 = i18n("A similar file named '%1' already exists.").arg(d->src);
-      else
-	  sentence1 = i18n("A newer item than '%1' already exists.\n").arg(d->src);
+            if ( !d->src.isEmpty() )
+            {
+                // rows 1 to 3 are the details (size/ctime/mtime), row 4 is empty
+                gridLayout->addRowSpacing( 4, 20 );
 
-      QLabel *lb = new QLabel( sentence1 + i18n("Do you want to use another file name?"), this );
-      d->m_pLayout->addWidget(lb);
-  }
-  }
-  d->m_pLineEdit = new QLineEdit( this );
-  d->m_pLayout->addWidget( d->m_pLineEdit );
-  QString fileName = KURL(d->dest).fileName();
-  d->m_pLineEdit->setText( KIO::decodeFileName( fileName ) );
-  if (d->b1)
-      connect(d->m_pLineEdit, SIGNAL(textChanged(const QString &)),
-              SLOT(enableRenameButton(const QString &)));
+                QLabel * lb2 = new QLabel( i18n("The source file is '%1'").arg(d->src), this );
+                gridLayout->addMultiCellWidget( lb2, 5, 5, 0, 1 ); // takes the complete first line
 
-  d->m_pLayout->addSpacing( 10 );
+                lb2 = new QLabel( this );
+                lb2->setPixmap( KMimeType::pixmapForURL( d->src ) );
+                gridLayout->addMultiCellWidget( lb2, 6, 8, 0, 0 ); // takes the first column on rows 6-8
 
-  QHBoxLayout* layout = new QHBoxLayout();
-  d->m_pLayout->addLayout( layout );
+                row = 6;
 
-  layout->addStretch(1);
+                if ( sizeSrc != (KIO::filesize_t)-1 )
+                {
+                    QLabel * lb = new QLabel( i18n("size %1").arg( KIO::convertSize(sizeSrc) ), this );
+                    gridLayout->addWidget( lb, row, 1 );
+                    row++;
+                }
+                if ( ctimeSrc != (time_t)-1 )
+                {
+                    QDateTime dctime; dctime.setTime_t( ctimeSrc );
+                    QLabel * lb = new QLabel( i18n("created on %1").arg( KGlobal::locale()->formatDateTime(dctime) ), this );
+                    gridLayout->addWidget( lb, row, 1 );
+                    row++;
+                }
+                if ( mtimeSrc != (time_t)-1 )
+                {
+                    QDateTime dmtime; dmtime.setTime_t( mtimeSrc );
+                    QLabel * lb = new QLabel( i18n("modified on %1").arg( KGlobal::locale()->formatDateTime(dmtime) ), this );
+                    gridLayout->addWidget( lb, row, 1 );
+                    row++;
+                }
+            }
+        }
+    }
+    else
+    {
+        // I wonder when this happens. And 'dest' isn't shown at all here...
+        // Sounds like the case where we don't want to allow overwriting, the existing
+        // file must be preserved. This doesn't happen in KIO though. (David)
+        QString sentence1;
+        if (mtimeDest < mtimeSrc)
+            sentence1 = i18n("An older item than '%1' already exists.\n").arg(d->src);
+        else if (mtimeDest == mtimeSrc)
+            sentence1 = i18n("A similar file named '%1' already exists.").arg(d->src);
+        else
+            sentence1 = i18n("A newer item than '%1' already exists.\n").arg(d->src);
 
-  if ( d->b1 )
-    layout->addWidget( d->b1 );
-  if( d->b8 )
-    layout->addWidget( d->b8 );
-  if ( d->b2 )
-    layout->addWidget( d->b2 );
-  if ( d->b3 )
-    layout->addWidget( d->b3 );
-  if ( d->b4 )
-    layout->addWidget( d->b4 );
-  if ( d->b5 )
-    layout->addWidget( d->b5 );
-  if ( d->b6 )
-    layout->addWidget( d->b6 );
-  if ( d->b7 )
-    layout->addWidget( d->b7 );
+        QLabel *lb = new QLabel( sentence1 + i18n("Do you want to use another file name?"), this );
+        d->m_pLayout->addWidget(lb);
+    }
+    d->m_pLineEdit = new QLineEdit( this );
+    d->m_pLayout->addWidget( d->m_pLineEdit );
+    QString fileName = KURL(d->dest).fileName();
+    d->m_pLineEdit->setText( KIO::decodeFileName( fileName ) );
+    if (d->b1)
+        connect(d->m_pLineEdit, SIGNAL(textChanged(const QString &)),
+                SLOT(enableRenameButton(const QString &)));
+
+    d->m_pLayout->addSpacing( 10 );
+
+    QHBoxLayout* layout = new QHBoxLayout();
+    d->m_pLayout->addLayout( layout );
+
+    layout->addStretch(1);
+
+    if ( d->b1 )
+        layout->addWidget( d->b1 );
+    if( d->b8 )
+        layout->addWidget( d->b8 );
+    if ( d->b2 )
+        layout->addWidget( d->b2 );
+    if ( d->b3 )
+        layout->addWidget( d->b3 );
+    if ( d->b4 )
+        layout->addWidget( d->b4 );
+    if ( d->b5 )
+        layout->addWidget( d->b5 );
+    if ( d->b6 )
+        layout->addWidget( d->b6 );
+    if ( d->b7 )
+        layout->addWidget( d->b7 );
 
 
-  d->b0->setDefault( true );
-  layout->addWidget( d->b0 );
+    d->b0->setDefault( true );
+    layout->addWidget( d->b0 );
 
-  resize( sizeHint() );
+    resize( sizeHint() );
 }
 
 RenameDlg::~RenameDlg()
