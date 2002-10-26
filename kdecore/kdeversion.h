72ba5c59976f06b1a2c17cd6a98ea48ca868079e
Dirk Mueller <mueller@kde.org>
2002-10-26 16:42:14 +0000

    as you all have to recompile anyway..
    svn path=/trunk/kdelibs/; revision=185752

--- kdecore/kdeversion.h
+++ kdecore/kdeversion.h
@@ -22,11 +22,11 @@
 #ifndef _KDE_VERSION_H_
 #define _KDE_VERSION_H_
 
-#define KDE_VERSION 308
-#define KDE_VERSION_STRING "3.0.8 (KDE 3.1 beta2)"
+#define KDE_VERSION 309
+#define KDE_VERSION_STRING "3.0.9 (KDE 3.1 RC1)"
 #define KDE_VERSION_MAJOR 3
 #define KDE_VERSION_MINOR 0
-#define KDE_VERSION_RELEASE 8
+#define KDE_VERSION_RELEASE 9
 
 namespace KDE
 {
