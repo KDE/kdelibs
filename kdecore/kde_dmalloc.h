fec5ff49f0ef03d4b7600271c3de8d04aed26cf6
Stephan Kulow <coolo@kde.org>
2002-12-20 08:04:31 +0000

    it's time to remove old crap
    svn path=/trunk/kdelibs/; revision=194970

--- kdecore/kde_dmalloc.h
+++ kdecore/kde_dmalloc.h
@@ -22,13 +22,7 @@
 #if !defined(KDE_DMALLOC_H) && defined(WITH_DMALLOC)
 #define KDE_DMALLOC_H
 
-#if !defined( NDEBUG) && defined (__cplusplus)
-
-void *operator new (size_t size, const char *file, const int line);
-void *operator new[] (size_t size, const char *file, const int line);
-
-#define new new(__FILE__, __LINE__)
+#warning dmalloc support has been removed from KDE. use valgrind :)
 
 #endif
 
-#endif
