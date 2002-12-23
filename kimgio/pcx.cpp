70eb6a48571d539cdb2b8ac4045c9e9ed8cc5681
Nadeem Hasan <nhasan@kde.org>
2002-12-23 03:20:37 +0000

    QImageIO plugin for PCX image format. Currently only read is supported. Write support to come later. I need some sample PCX images, especially 4bpp (16 color) images.
    svn path=/trunk/kdelibs/; revision=195485

---
+++ kimgio/pcx.cpp
@@ -0,0 +1,237 @@
+/* This file is part of the KDE project
+   Copyright (C) 2002 Nadeem Hasan <nhasan@kde.org>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License (LGPL) as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+*/
+
+#include "pcx.h"
+
+#include <qimage.h>
+
+#include <kdebug.h>
+
+QDataStream &operator>>( QDataStream &s, PALETTE &pal )
+{
+  for ( int i=0; i<16; ++i )
+    s >> pal.p[ i ].r >> pal.p[ i ].g >> pal.p[ i ].b;
+
+  return s;
+}
+
+QDataStream &operator>>( QDataStream &s, PCXHEADER &ph )
+{
+  s >> ph.Manufacturer;
+  s >> ph.Version;
+  s >> ph.Encoding;
+  s >> ph.Bpp;
+  s >> ph.XMin >> ph.YMin >> ph.XMax >> ph.YMax;
+  s >> ph.HDpi >> ph.YDpi;
+  s >> ph.Palette;
+  s >> ph.Reserved;
+  s >> ph.NPlanes;
+  s >> ph.BytesPerLine;
+  s >> ph.PaletteInfo;
+  s >> ph.HScreenSize;
+  s >> ph.VScreenSize;
+
+  return s;
+}
+
+static PCXHEADER header;
+static QImage img;
+Q_UINT16 w, h;
+
+static void readLine( QDataStream &s, QByteArray &buf )
+{
+  Q_UINT32 i=0;
+  Q_UINT8 byte, count;
+
+  if ( header.Encoding == 1 )
+  {
+    // Uncompress the image data
+    while ( i < buf.size() && !s.atEnd() )
+    {
+      count = 1;
+      s >> byte;
+      if ( byte > 0xc0 )
+      {
+        count = byte - 0xc0;
+        s >> byte;
+      }
+      while ( count-- )
+        buf[ i++ ] = byte;
+    }
+  }
+  else
+  {
+    // Image is not compressed (possible?)
+    while (  i < buf.size() && !s.atEnd() )
+    {
+      s >> byte;
+      buf[ i++ ] = byte;
+    }
+  }
+}
+
+static void readImage1( QDataStream &s )
+{
+  QByteArray buf( header.BytesPerLine );
+
+  img.create( w, h, 1, 2, QImage::BigEndian );
+
+  for ( int y=0; y<h; ++y )
+  {
+    readLine( s, buf );
+
+    for ( int x=0; x<header.BytesPerLine; ++x )
+      *( img.scanLine( y )+x ) = ~buf[ x ];
+  }
+
+  // Set the color palette
+  img.setColor( 0, qRgb( 0, 0, 0 ) );
+  img.setColor( 1, qRgb( 255, 255, 255 ) );
+}
+
+static void readImage8( QDataStream &s )
+{
+  QByteArray buf( header.BytesPerLine );
+
+  img.create( w, h, 8, 256, QImage::IgnoreEndian );
+
+  for ( int y=0; y<h; ++y )
+  {
+    readLine( s, buf );
+
+    for ( int x=0; x<header.BytesPerLine; ++x )
+      *( img.scanLine( y )+x ) = buf[ x ];
+  }
+
+  Q_UINT8 flag;
+  s >> flag;
+  kdDebug() << "Flag: " << flag << endl;
+
+  if ( flag == 12 && header.Version == 5 )
+  {
+    // Read the palette
+    Q_UINT8 r, g, b;
+    for ( int i=0; i<256; ++i )
+    {
+      s >> r >> g >> b;
+      img.setColor( i, qRgb( r, g, b ) );
+    }
+  }
+}
+
+static void readImage4( QDataStream &s )
+{
+  QByteArray buf( header.BytesPerLine );
+  QByteArray pixbuf( header.BytesPerLine );
+
+  img.create( w, h, 8, 256, QImage::IgnoreEndian );
+
+  for ( int y=0; y<h; ++y )
+  {
+    for ( int x=0; x<header.BytesPerLine; ++x )
+      pixbuf[ x ] = 0;
+
+    for ( int p=0; p<3; p++ )
+    {
+      readLine( s, buf );
+      for ( int x=0; x<header.BytesPerLine; ++x )
+        if ( buf[ x/8 ] & ( 128 >> ( x%8 ) ) )
+          pixbuf[ x ] += ( 1 << p );
+    }
+  }
+
+  // Read the palette
+  for ( int i=0; i<16; ++i )
+  {
+    struct PALETTE pal = header.Palette;
+    img.setColor( i, qRgb( pal.p[ i ].r, pal.p[ i ].g, pal.p[ i ].b ) );
+  }
+}
+
+static void readImage24( QDataStream &s )
+{
+  QByteArray r_buf( header.BytesPerLine );
+  QByteArray g_buf( header.BytesPerLine );
+  QByteArray b_buf( header.BytesPerLine );
+
+  img.create( w, h, 32 );
+
+  for ( int y=0; y<h; ++y )
+  {
+    readLine( s, r_buf );
+    readLine( s, g_buf );
+    readLine( s, b_buf );
+
+    for ( int x=0; x<header.BytesPerLine; ++x )
+    {
+      uint *p = ( uint * )img.scanLine( y )+x;
+      *p = qRgb( r_buf[ x ], g_buf[ x ], b_buf[ x ] );
+    }
+  }
+}
+
+void kimgio_pcx_read( QImageIO *io )
+{
+  QDataStream s( io->ioDevice() );
+  s.setByteOrder( QDataStream::LittleEndian );
+
+  s >> header;
+
+  w = ( header.XMax-header.XMin ) + 1;
+  h = ( header.YMax-header.YMin ) + 1;
+
+  kdDebug() << "Manufacturer: " << header.Manufacturer << endl;
+  kdDebug() << "Version: " << header.Version << endl;
+  kdDebug() << "Encoding: " << header.Encoding << endl;
+  kdDebug() << "Bpp: " << header.Bpp << endl;
+  kdDebug() << "Width: " << w << endl;
+  kdDebug() << "Height: " << h << endl;
+  kdDebug() << "Window: " << header.XMin << "," << header.XMax << "," 
+            << header.YMin << "," << header.YMax << endl;
+  kdDebug() << "BytesPerLine: " << header.BytesPerLine << endl;
+  kdDebug() << "NPlanes: " << header.NPlanes << endl;
+
+  // Skip the rest of the header
+  Q_UINT8 byte;
+  while ( s.device()->at() < 128 )
+    s >> byte;
+
+  if ( header.Bpp == 1 && header.NPlanes == 1 )
+  {
+    readImage1( s );
+  }
+  else if ( header.Bpp == 1 && header.NPlanes == 4 )
+  {
+    readImage4( s );
+  }
+  else if ( header.Bpp == 8 && header.NPlanes == 1 )
+  {
+    readImage8( s );
+  }
+  else if ( header.Bpp == 8 && header.NPlanes == 3 )
+  {
+    readImage24( s );
+  }
+
+  kdDebug() << "Image Bytes: " << img.numBytes() << endl;
+  kdDebug() << "Image Bytes Per Line: " << img.bytesPerLine() << endl;
+  kdDebug() << "Image Depth: " << img.depth() << endl;
+
+  io->setImage( img );
+  io->setStatus( 0 );
+}
+
+void kimgio_pcx_write( QImageIO *io )
+{
+  io->setStatus( -1 );
+}
+
+/* vim: et sw=2 ts=2
+*/
+
---
+++ kimgio/pcx.h
@@ -0,0 +1,74 @@
+/* This file is part of the KDE project
+   Copyright (C) 2002 Nadeem Hasan <nhasan@kde.org>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the Lesser GNU General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+*/
+
+#ifndef PCX_H
+#define PCX_H
+
+#include <qglobal.h>
+
+class QImageIO;
+
+extern "C"
+{
+  void kimgio_pcx_read( QImageIO * );
+  void kimgio_pcx_write( QImageIO * );
+}
+
+struct PALETTE
+{
+  struct
+  {
+    Q_UINT8 r;
+    Q_UINT8 g;
+    Q_UINT8 b;
+  } p[ 16 ];
+};
+
+struct PCXHEADER
+{
+  Q_UINT8  Manufacturer;    // Constant Flag, 10 = ZSoft .pcx
+  Q_UINT8  Version;         // Version information·
+                            // 0 = Version 2.5 of PC Paintbrush·
+                            // 2 = Version 2.8 w/palette information·
+                            // 3 = Version 2.8 w/o palette information·
+                            // 4 = PC Paintbrush for Windows(Plus for
+                            //     Windows uses Ver 5)·
+                            // 5 = Version 3.0 and > of PC Paintbrush
+                            //     and PC Paintbrush +, includes
+                            //     Publisher's Paintbrush . Includes
+                            //     24-bit .PCX files·
+  Q_UINT8  Encoding;        // 1 = .PCX run length encoding
+  Q_UINT8  Bpp;             // Number of bits to represent a pixel
+                            // (per Plane) - 1, 2, 4, or 8·
+  Q_UINT16 XMin;
+  Q_UINT16 YMin;
+  Q_UINT16 XMax;
+  Q_UINT16 YMax;
+  Q_UINT16 HDpi;
+  Q_UINT16 YDpi;
+  struct PALETTE Palette;
+  Q_UINT8  Reserved;        // Should be set to 0.
+  Q_UINT8  NPlanes;         // Number of color planes
+  Q_UINT16 BytesPerLine;    // Number of bytes to allocate for a scanline
+                            // plane.  MUST be an EVEN number.  Do NOT
+                            // calculate from Xmax-Xmin.·
+  Q_UINT16 PaletteInfo;     // How to interpret palette- 1 = Color/BW,
+                            // 2 = Grayscale ( ignored in PB IV/ IV + )·
+  Q_UINT16 HScreenSize;     // Horizontal screen size in pixels. New field
+                            // found only in PB IV/IV Plus
+  Q_UINT16 VScreenSize;     // Vertical screen size in pixels. New field
+                            // found only in PB IV/IV Plus
+  Q_UINT8  Filler[ 54 ];    // Blank to fill out 128 byte header.  Set all
+                            // bytes to 0
+};
+
+#endif // PCX_H
+
+/* vim: et sw=2 ts=2
+*/
