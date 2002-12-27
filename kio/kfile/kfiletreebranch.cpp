1f21303ec884a407cae6c51d18ec909a5a1bb6ef
Carsten Pfeiffer <pfeiffer@kde.org>
2002-12-27 12:21:00 +0000

    - proper deletion order - removeExtraData() is in the proper place now
    svn path=/branches/KDE_3_1_BRANCH/kdelibs/; revision=195984

--- kio/kfile/kfiletreebranch.cpp
+++ kio/kfile/kfiletreebranch.cpp
@@ -58,7 +58,7 @@ KFileTreeBranch::KFileTreeBranch( KFileTreeView *parent, const KURL& url,
 						      S_IFDIR  ),
 				       this );
    }
-   
+
    m_root->setExpandable( true );
    m_root->setPixmap( 0, pix );
    m_root->setText( 0, name );
@@ -82,10 +82,10 @@ KFileTreeBranch::KFileTreeBranch( KFileTreeView *parent, const KURL& url,
 
    connect( this, SIGNAL( clear()),
 	    this, SLOT( slotDirlisterClear()));
-   
+
    connect( this, SIGNAL( clear(const KURL&)),
 	    this, SLOT( slotDirlisterClearURL(const KURL&)));
-   
+
    connect( this, SIGNAL( redirection( const KURL& , const KURL& ) ),
 	    this, SLOT( slotRedirect( const KURL&, const KURL& )));
 
@@ -137,11 +137,11 @@ void KFileTreeBranch::addItems( const KFileItemList& list )
    KFileItem *currItem;
    KFileTreeViewItemList treeViewItList;
    KFileTreeViewItem *parentItem = 0;
-   
+
    while ( (currItem = it.current()) != 0 )
    {
       parentItem = parentKFTVItem( currItem );
-     
+
       /* Only create a new KFileTreeViewItem if it does not yet exist */
       KFileTreeViewItem *newKFTVI =
 	 static_cast<KFileTreeViewItem *>(currItem->extraData( this ));
@@ -205,7 +205,7 @@ void KFileTreeBranch::addItems( const KFileItemList& list )
 
       treeViewItList.append( newKFTVI );
    }
-      
+
    emit( newTreeViewItems( this, treeViewItList ));
 }
 
@@ -213,7 +213,7 @@ KFileTreeViewItem* KFileTreeBranch::createTreeViewItem( KFileTreeViewItem *paren
 							KFileItem *fileItem )
 {
    KFileTreeViewItem  *tvi = 0;
-   
+
    if( parent && fileItem )
    {
       tvi = new KFileTreeViewItem( parent,
@@ -255,7 +255,7 @@ void KFileTreeBranch::slotDeleteItem( KFileItem *it )
 
    if( !it ) return;
    kdDebug(250) << "Slot Delete Item hitted for " << it->url().prettyURL() << endl;
-   
+
    KFileTreeViewItem *kfti = static_cast<KFileTreeViewItem*>(it->extraData(this));
 
    if( kfti )
@@ -264,7 +264,7 @@ void KFileTreeBranch::slotDeleteItem( KFileItem *it )
       if( kfti->childCount() > 0 )
       {
 	 KFileTreeViewItem *child = static_cast<KFileTreeViewItem*>(kfti->firstChild());
-      
+
 	 while( child )
 	 {
 	    kdDebug(250) << "Calling child to be deleted !" << endl;
@@ -273,10 +273,9 @@ void KFileTreeBranch::slotDeleteItem( KFileItem *it )
 	    child = nextChild;
 	 }
       }
-      
+
       kdDebug(250) << "Found corresponding KFileTreeViewItem" << endl;
       delete( kfti );
-      it->removeExtraData( this );
    }
    else
    {
@@ -292,7 +291,7 @@ void KFileTreeBranch::slotCanceled( const KURL& url )
    m_openChildrenURLs.remove( url);
 
    // stop animations etc.
-   emit populateFinished( findTVIByURL(url));      
+   emit populateFinished( findTVIByURL(url));
 }
 
 void KFileTreeBranch::slotDirlisterClear()
@@ -303,7 +302,7 @@ void KFileTreeBranch::slotDirlisterClear()
 
    QListViewItem *child = m_root->firstChild();
    QListViewItem *next = child;
-   
+
    while( child )
    {
       next = child->nextSibling();
@@ -318,7 +317,7 @@ void KFileTreeBranch::slotRedirect( const KURL& oldUrl, const KURL&newUrl )
    {
       m_startURL = newUrl;
    }
-   
+
 }
 
 void KFileTreeBranch::slotDirlisterClearURL( const KURL& url )
@@ -377,10 +376,10 @@ void KFileTreeBranch::slCompleted( const KURL& url )
 
    kdDebug(250) << "current parent " << currParent << " is already listed: "
 		<< currParent->alreadyListed() << endl;
-   
+
    emit( populateFinished(currParent));
    emit( directoryChildCount(currParent, currParent->childCount()));
-   
+
    /* This is a walk through the children of the last populated directory.
     * Here we start the dirlister on every child of the dir and wait for its
     * finish. When it has finished, we go to the next child.
@@ -395,7 +394,7 @@ void KFileTreeBranch::slCompleted( const KURL& url )
    kdDebug(250) << "isLocalFile: " << m_startURL.isLocalFile() << endl;
    kdDebug(250) << "dirOnlyMode: " << dirOnlyMode() << endl;
 
-   
+
    if( m_recurseChildren && (!m_startURL.isLocalFile() || ! dirOnlyMode()) )
    {
       bool wantRecurseUrl = false;
@@ -413,7 +412,7 @@ void KFileTreeBranch::slCompleted( const KURL& url )
 
       if( wantRecurseUrl && currParent )
       {
-	 
+	
 	 /* now walk again through the tree and populate the children to get +-signs */
 	 /* This is the starting point. The visible folder has finished,
             processing the children has not yet started */
@@ -469,7 +468,7 @@ bool KFileTreeBranch::populate( const KURL& url,  KFileTreeViewItem *currItem )
    bool ret = false;
    if( ! currItem )
       return ret;
-   
+
    kdDebug(250) << "Populating <" << url.prettyURL() << ">" << endl;
 
    /* Add this url to the list of urls to recurse for children */
@@ -478,7 +477,7 @@ bool KFileTreeBranch::populate( const KURL& url,  KFileTreeViewItem *currItem )
       m_openChildrenURLs.append( url );
       kdDebug(250) << "Appending to list " << url.prettyURL() << endl;
    }
-   
+
    if( ! currItem->alreadyListed() )
    {
       /* start the lister */
--- kio/kfile/kfiletreeview.cpp
+++ kio/kfile/kfiletreeview.cpp
@@ -87,7 +87,12 @@ KFileTreeView::KFileTreeView( QWidget *parent, const char *name )
 
 KFileTreeView::~KFileTreeView()
 {
-   m_mapCurrentOpeningFolders.clear();
+   // we must make sure that the KFileTreeViewItems are deleted _before_ the
+   // branches are deleted. Otherwise, the KFileItems would be destroyed 
+   // and the KFileTreeViewItems had dangling pointers to them.
+   hide();
+   clear();
+   m_branches.clear(); // finally delete the branches and KFileItems
 }
 
 
