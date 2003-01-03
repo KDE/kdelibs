3920e02be4f5f9b05a4bc1ced88dd684432abda8
Dawit Alemayehu <adawit@kde.org>
2003-01-03 07:15:02 +0000

    - Cleaned up the code and make it similar to klineedittest.
    svn path=/trunk/kdelibs/; revision=197142

--- kdeui/tests/kcomboboxtest.cpp
+++ kdeui/tests/kcomboboxtest.cpp
@@ -1,8 +1,10 @@
 #include "kcomboboxtest.h"
+
+#include <qpushbutton.h>
 #include <qlayout.h>
 #include <qpixmap.h>
-#include <qpushbutton.h>
 #include <qlabel.h>
+#include <qhbox.h>
 
 #include <kapplication.h>
 #include <kdebug.h>
@@ -12,87 +14,136 @@
 #include <kiconloader.h>
 #include <ksimpleconfig.h>
 
-Widget::Widget() : QObject()
+KComboBoxTest::KComboBoxTest(QWidget* widget, const char* name )
+              :QWidget(widget, name)
+{
+  QVBoxLayout *vbox = new QVBoxLayout (this, KDialog::marginHint(), KDialog::spacingHint());
+  
+  // Read-only combobox
+  QHBox* hbox = new QHBox(this);
+  hbox->setSpacing (KDialog::spacingHint());
+  QLabel* lbl = new QLabel("&Read-Only Combo:", hbox);
+  lbl->setSizePolicy (QSizePolicy::Maximum, QSizePolicy::Preferred);
+  
+  KComboBox *ro = new KComboBox(hbox, "ReadOnlyCombo" );
+  lbl->setBuddy (ro);  
+  ro->setCompletionMode( KGlobalSettings::CompletionAuto );
+  QObject::connect (ro, SIGNAL(activated(int)), SLOT(slotActivated(int)));
+  QObject::connect (ro, SIGNAL(activated(const QString&)), SLOT (slotActivated(const QString&)));
+  vbox->addWidget (hbox);
+    
+  // Read-write combobox
+  hbox = new QHBox(this);
+  hbox->setSpacing (KDialog::spacingHint());
+  lbl = new QLabel("&Editable Combo:", hbox);
+  lbl->setSizePolicy (QSizePolicy::Maximum, QSizePolicy::Preferred);  
+  
+  KComboBox *rw = new KComboBox( true, hbox, "ReadWriteCombo" );
+  lbl->setBuddy (rw);
+  rw->setDuplicatesEnabled( true );  
+  rw->setInsertionPolicy( QComboBox::NoInsertion );
+  QObject::connect (rw, SIGNAL(activated(int)), SLOT(slotActivated(int)));
+  QObject::connect (rw, SIGNAL(activated(const QString&)), SLOT(slotActivated(const QString&)));
+  QObject::connect (rw, SIGNAL(returnPressed()), SLOT(slotReturnPressed()));
+  vbox->addWidget (hbox);
+  
+  // History combobox...
+  hbox = new QHBox(this);
+  hbox->setSpacing (KDialog::spacingHint());
+  lbl = new QLabel("&History Combo:", hbox);
+  lbl->setSizePolicy (QSizePolicy::Maximum, QSizePolicy::Preferred);  
+  
+  KComboBox *hc = new KHistoryCombo( true, hbox, "ReadWriteCombo" );
+  lbl->setBuddy (rw);
+  rw->setDuplicatesEnabled( true );  
+  rw->setInsertionPolicy( QComboBox::NoInsertion );
+  QObject::connect (hc, SIGNAL(activated(int)), SLOT(slotActivated(int)));
+  QObject::connect (hc, SIGNAL(activated(const QString&)), SLOT(slotActivated(const QString&)));
+  QObject::connect (hc, SIGNAL(returnPressed()), SLOT(slotReturnPressed()));
+  vbox->addWidget (hbox);  
+    
+  // Read-write combobox that is a replica of code in konqueror...
+  hbox = new QHBox(this);
+  hbox->setSpacing (KDialog::spacingHint());  
+  lbl = new QLabel( "&Konq's Combo:", hbox);
+  lbl->setSizePolicy (QSizePolicy::Maximum, QSizePolicy::Preferred);  
+  
+  KComboBox *konqc = new KComboBox( true, hbox, "KonqyCombo" );
+  lbl->setBuddy (konqc);
+  konqc->setMaxCount( 10 );
+  QObject::connect (konqc, SIGNAL(activated(int)), SLOT(slotActivated(int)));
+  QObject::connect (konqc, SIGNAL(activated(const QString&)), SLOT (slotActivated(const QString&)));
+  QObject::connect (konqc, SIGNAL(returnPressed()), SLOT(slotReturnPressed()));
+  vbox->addWidget (hbox);
+  
+  // Create an exit button
+  QPushButton * push = new QPushButton( "E&xit", this );
+  QObject::connect( push, SIGNAL(clicked()), SLOT(quitApp()) );
+  vbox->addWidget (push);
+  
+  // Popuplate the select-only list box
+  QStringList list;
+  list << "Stone" << "Tree" << "Peables" << "Ocean" << "Sand" << "Chips" 
+       << "Computer" << "Mankind";
+  list.sort();
+  
+  // Setup read-only combo
+  ro->insertStringList( list );
+  ro->completionObject()->setItems( list );
+  
+  // Setup read-write combo
+  rw->insertStringList( list );
+  rw->completionObject()->setItems( list );
+  
+  // Setup read-write combo
+  hc->insertStringList( list );
+  hc->completionObject()->setItems( list );
+  
+  // Setup konq's combobox
+  KSimpleConfig historyConfig( "konq_history" );
+  historyConfig.setGroup( "Location Bar" );
+  KCompletion * s_pCompletion = new KCompletion;
+  s_pCompletion->setOrder( KCompletion::Weighted );
+  s_pCompletion->setItems( historyConfig.readListEntry( "ComboContents" ) );
+  s_pCompletion->setCompletionMode( KGlobalSettings::completionMode() );
+  konqc->setCompletionObject( s_pCompletion );
+  
+  QPixmap pix = SmallIcon("www");
+  konqc->insertItem( pix, "http://www.kde.org" );
+  konqc->setCurrentItem( konqc->count()-1 );
+}
+
+KComboBoxTest::~KComboBoxTest()
 {
 }
 
-void Widget::slotActivated( int index ) {
-    kdDebug() << "slotActivated combo:" << sender()->name() << " index:" << index << endl;
+void KComboBoxTest::slotActivated( int index ) 
+{
+  kdDebug() << "Activated Combo: " << sender()->name() << ", index:" << index << endl;
+}
+
+void KComboBoxTest::slotActivated (const QString& item)
+{
+  kdDebug() << "Activated Combo: " << sender()->name() << ", item: " << item << endl;
+}
+
+void KComboBoxTest::slotReturnPressed ()
+{
+  kdDebug() << "Return Pressed: " << sender()->name() << endl;
+}
+
+void KComboBoxTest::quitApp()
+{
+  kapp->closeAllWindows();
 }
 
 int main ( int argc, char **argv)
 {
-    KApplication a(argc, argv, "kcomboboxtest");
-
-    Widget* obj = new Widget;
-
-    // Make a central widget to contain the other widgets
-    QWidget * w = new QWidget();
-    // Insert the widget container (parent widget) into
-    // a layout manager (VERTICAL).
-    QVBoxLayout *vbox = new QVBoxLayout( w, KDialog::marginHint(), KDialog::spacingHint() );
-    // Resize the widget
-    w->resize( 500, 100 );
-
-    // Popuplate the select-only list box
-    QStringList list;
-    list << "Stone" << "Tree" << "Peables" << "Ocean" << "Sand" << "Chips" << "Computer" << "Mankind";
-    list.sort();
-
-    // Create and modify read-write widget
-    KComboBox *rwc = new KComboBox( true, w, "rwcombobox" );
-    QLabel* lblrw = new QLabel( rwc, "&Editable ComboBox", w, "rwcombolabel" );
-    rwc->setDuplicatesEnabled( true );
-    rwc->completionObject()->setItems( list );
-    rwc->setInsertionPolicy( QComboBox::NoInsertion );
-    rwc->insertStringList( list );
-    rwc->setEditText( "file:/home/adawit/" );
-    // rwc->setCompletionMode( KGlobalSettings::CompletionAuto );
-    QObject::connect( rwc, SIGNAL( activated( int ) ), obj, SLOT( slotActivated( int ) ) );
-
-    // Create a read-write combobox and reproduce konqueror's code
-    KComboBox *konqc = new KComboBox( true, w, "konqc" );
-    konqc->setMaxCount( 10 );
-      KSimpleConfig historyConfig( "konq_history" );
-      historyConfig.setGroup( "Location Bar" );
-      KCompletion * s_pCompletion = new KCompletion;
-//      s_pCompletion->setOrder( KCompletion::Weighted );
-      s_pCompletion->setItems( historyConfig.readListEntry( "ComboContents" ) );
-      s_pCompletion->setCompletionMode( KGlobalSettings::completionMode() );
-      konqc->setCompletionObject( s_pCompletion );
-    QLabel* lblkonq = new QLabel( konqc, "&Konqueror's ComboBox", w );
-    QPixmap pix = SmallIcon("www");
-    konqc->insertItem( pix, "http://www.kde.org" );
-    konqc->setCurrentItem( konqc->count()-1 );
-    kdDebug() << "setLocationBarURL setCurrentItem " << konqc->count()-1 << endl;
-    QObject::connect( konqc, SIGNAL( activated( int ) ), obj, SLOT( slotActivated( int ) ) );
-
-    // Create a read-only widget
-    KComboBox *soc = new KComboBox( w, "socombobox" );
-    QLabel* lblso = new QLabel( soc, "&Select-Only ComboBox", w, "socombolabel" );
-    soc->setCompletionMode( KGlobalSettings::CompletionAuto );
-    soc->completionObject()->setItems( list );
-    soc->insertStringList( list );
-    QObject::connect( soc, SIGNAL( activated( int ) ), obj, SLOT( slotActivated( int ) ) );
-
-    // Create an exit button
-    QPushButton * push = new QPushButton( "E&xit", w );
-    QObject::connect( push, SIGNAL( clicked() ), &a, SLOT( closeAllWindows() ) );
-
-    // Insert the widgets into the layout manager.
-    vbox->addWidget( lblrw );
-    vbox->addWidget( rwc );
-    vbox->addWidget( lblso );
-    vbox->addWidget( soc );
-    vbox->addWidget( lblkonq );
-    vbox->addWidget( konqc );
-    vbox->addWidget( push );
-
-
-    a.setMainWidget(w);
-    rwc->setFocus();
-    w->show();
-    //w->setIcon( *(new QPixmap("/opt/kde2/share/icons/hicolor/16x16/apps/samba.png" )));
-    return a.exec();
+  KApplication a(argc, argv, "kcomboboxtest");
+  KComboBoxTest* t= new KComboBoxTest;    
+  a.setMainWidget (t);
+  t->show ();
+  return a.exec();
 }
+
 #include "kcomboboxtest.moc"
--- kdeui/tests/kcomboboxtest.h
+++ kdeui/tests/kcomboboxtest.h
@@ -1,16 +1,34 @@
-#ifndef somethinghere
-#define somethinghere
+#ifndef _KCOMBOBOXTEST_H
+#define _KCOMBOBOXTEST_H
 
 #include <qwidget.h>
+#include <qguardedptr.h>
+
+class QLineEdit;
+class QPushButton;
+
 class KComboBox;
 
-class Widget : public QObject
+class KComboBoxTest : public QWidget
 {
     Q_OBJECT
+    
 public:
-    Widget();
+   KComboBoxTest ( QWidget *parent=0, const char *name=0 );
+   ~KComboBoxTest();
+
 private slots:
-    void slotActivated( int );
+   void quitApp();
+   void slotActivated( int );
+   void slotReturnPressed ();
+   void slotActivated( const QString& );
+   
+protected:
+   QGuardedPtr<KComboBox> m_combobox;
+   
+   QPushButton* m_btnExit;
+   QPushButton* m_btnReadOnly;
+   QPushButton* m_btnEnable;
 };
 
 #endif
