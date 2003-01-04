bad81f062df6eabeddd3d4a2dac3e640ee430a07
Nadeem Hasan <nhasan@kde.org>
2003-01-04 00:48:25 +0000

    Update copyright.
    svn path=/trunk/kdelibs/; revision=197294

--- kimgio/pcx.h
+++ kimgio/pcx.h
@@ -1,5 +1,5 @@
 /* This file is part of the KDE project
-   Copyright (C) 2002 Nadeem Hasan <nhasan@kde.org>
+   Copyright (C) 2002-2003 Nadeem Hasan <nhasan@kde.org>
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the Lesser GNU General Public
