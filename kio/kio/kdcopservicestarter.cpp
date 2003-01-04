2ed9d9c6b583793f33f0813e1606be2bfd07c83f
David Faure <faure@kde.org>
2003-01-04 21:52:23 +0000

    * A generic DCOP service starter, using KTrader. * The default implementation starts new processes, but this interface can * also be reimplemented by specific applications to provide dlopened in-process DCOP objects.
    svn path=/trunk/kdelibs/; revision=197492

---
+++ kio/kio/kdcopservicestarter.cpp
@@ -0,0 +1,97 @@
+/* This file is part of the KDE libraries
+   Copyright (C) 2003 David Faure <faure@kde.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License version 2 as published by the Free Software Foundation.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
+#include "kdcopservicestarter.h"
+#include "ktrader.h"
+#include <kapplication.h>
+#include "kservice.h"
+#include <kstaticdeleter.h>
+#include <kdebug.h>
+#include <klocale.h>
+#include <dcopclient.h>
+
+static KStaticDeleter<KDCOPServiceStarter> dss_sd;
+KDCOPServiceStarter* KDCOPServiceStarter::s_self;
+
+KDCOPServiceStarter* KDCOPServiceStarter::self()
+{
+    if ( !s_self )
+        dss_sd.setObject( s_self, new KDCOPServiceStarter );
+    return s_self;
+}
+
+KDCOPServiceStarter::KDCOPServiceStarter()
+{
+    // Set the singleton instance - useful when a derived KDCOPServiceStarter
+    // was created (before self() was called)
+    s_self = this;
+}
+
+KDCOPServiceStarter::~KDCOPServiceStarter()
+{
+}
+
+int KDCOPServiceStarter::findServiceFor( const QString& serviceType,
+                                         const QString& _constraint,
+                                         const QString& preferences,
+                                         QString *error, QCString* pDcopService,
+                                         int flags )
+{
+    // Ask the trader which service is preferred for this servicetype
+    // We want one that provides a DCOP interface
+    QString constraint = _constraint;
+    if ( !constraint.isEmpty() )
+        constraint += " and ";
+    constraint += "exist [X-DCOP-ServiceName]";
+    KTrader::OfferList offers = KTrader::self()->query(serviceType, "Application", constraint, preferences);
+    if ( offers.isEmpty() ) {
+        if ( error )
+            *error = i18n("No service implementing %1").arg( serviceType );
+        kdWarning() << "KDCOPServiceStarter: No service implementing " << serviceType << endl;
+        return -1;
+    }
+    KService::Ptr ptr = offers.first();
+    QCString dcopService = ptr->property("X-DCOP-ServiceName").toString().latin1();
+
+    if ( !kapp->dcopClient()->isApplicationRegistered( dcopService ) )
+    {
+        QString error;
+        if ( startServiceFor( serviceType, constraint, preferences, &error, &dcopService, flags ) != 0 )
+        {
+            kdDebug() << "KDCOPServiceStarter: Couldn't start service: " << error << endl;
+            return -2;
+        }
+    }
+    kdDebug() << "KDCOPServiceStarter: DCOP service is available now, as " << dcopService << endl;
+    if ( pDcopService )
+        *pDcopService = dcopService;
+    return 0;
+}
+
+int KDCOPServiceStarter::startServiceFor( const QString& serviceType,
+                                          const QString& constraint,
+                                          const QString& preferences,
+                                          QString *error, QCString* dcopService, int /*flags*/ )
+{
+    KTrader::OfferList offers = KTrader::self()->query(serviceType, "Application", constraint, preferences);
+    if ( offers.isEmpty() )
+        return -1;
+    KService::Ptr ptr = offers.first();
+    kdDebug() << "KDCOPServiceStarter: starting " << ptr->desktopEntryPath() << endl;
+    return kapp->startServiceByDesktopPath( ptr->desktopEntryPath(), QStringList(), error, dcopService );
+}
---
+++ kio/kio/kdcopservicestarter.h
@@ -0,0 +1,103 @@
+/* This file is part of the KDE libraries
+   Copyright (C) 2003 David Faure <faure@kde.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License version 2 as published by the Free Software Foundation.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
+#ifndef KDCOPSERVICESTARTER_H
+#define KDCOPSERVICESTARTER_H
+
+#include <qstring.h>
+
+class KDCOPServiceStarter;
+template<class KDCOPServiceStarter> class KStaticDeleter;
+class QCString;
+
+/**
+ * A generic DCOP service starter, using KTrader.
+ * The default implementation starts new processes, but this interface can
+ * also be reimplemented by specific applications to provide dlopened in-process DCOP objects.
+ * @author David Faure <faure@kde.org>
+ */
+class KDCOPServiceStarter {
+    friend class KStaticDeleter<KDCOPServiceStarter>;
+public:
+
+    static KDCOPServiceStarter* self();
+
+    /**
+     * Check if a given DCOP interface is available - from the serviceType it's supposed to implement.
+     *
+     * The trader is queried to find the preferred application for this serviceType,
+     * with the constraint that its X-DCOP-ServiceName property must be defined.
+     * Then the DCOP server is checked. If the service is not available,
+     * this method will call @ref startServiceFor to start it.
+     *
+     * @param serviceType the type of service we're looking for
+     * @param constraint see @ref KTrader
+     * @param preferences see @ref KTrader
+     * @param error On failure, @p error contains a description of the error
+     *         that occurred. If the pointer is 0, the argument will be
+     *         ignored
+     * @param dcopService On success, @p dcopService contains the DCOP name
+     *         under which this service is available. If the pointer is 0 the argument
+     *         will be ignored
+     * @param flags for future extensions (currently unused)
+     *
+     * @return an error code indicating success (== 0) or failure (> 0).
+     */
+    int findServiceFor( const QString& serviceType,
+                        const QString& constraint = QString::null,
+                        const QString& preferences = QString::null,
+                        QString *error=0, QCString* dcopService=0,
+                        int flags=0 );
+
+    /**
+     * Find an implementation of the given @p serviceType,
+     * and start it, to use its DCOP interface.
+     * The default implementation uses KTrader to find the preferred Application,
+     * and then starts it using kapp->startService...
+     *
+     * However applications (like kontact) can reimplement this method, to provide
+     * an in-process way of loading the implementation for this service type.
+     *
+     * @param serviceType the type of service we're looking for
+     * @param constraint see @ref KTrader
+     * @param preferences see @ref KTrader
+     * @param error On failure, @p error contains a description of the error
+     *         that occurred. If the pointer is 0, the argument will be
+     *         ignored
+     * @param dcopService On success, @p dcopService contains the DCOP name
+     *         under which this service is available. If the pointer is 0 the argument
+     *         will be ignored
+     * @param flags for future extensions (currently unused)
+     *
+     * @return an error code indicating success (== 0) or failure (> 0).
+     */
+    virtual int startServiceFor( const QString& serviceType,
+                                 const QString& constraint = QString::null,
+                                 const QString& preferences = QString::null,
+                                 QString *error=0, QCString* dcopService=0,
+                                 int flags=0 );
+protected:
+    KDCOPServiceStarter();
+    virtual ~KDCOPServiceStarter();
+
+private:
+    static KDCOPServiceStarter* s_self;
+};
+
+#endif
+
