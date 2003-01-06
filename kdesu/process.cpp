a5519478a3130597b838c5ea59920064fd3e2763
Marc Espie <espie@nerim.net>
2003-01-06 23:14:42 +0000

    su needs a controlling terminal on OpenBSD
    svn path=/trunk/kdelibs/; revision=197927

--- kdesu/process.cpp
+++ kdesu/process.cpp
@@ -39,6 +39,10 @@
 #include <sys/stream.h>
 #endif
 
+#ifdef __OpenBSD__
+#include <sys/ioctl.h>
+#endif
+
 #ifdef HAVE_SYS_SELECT_H
 #include <sys/select.h>                // Needed on some systems.
 #endif
@@ -448,6 +452,10 @@ int PtyProcess::SetupTTY(int fd)
     ioctl(slave, I_PUSH, "ptem");
     ioctl(slave, I_PUSH, "ldterm");
 
+#endif
+
+#ifdef __OpenBSD__
+    ioctl(slave, TIOCSCTTY, (char *)NULL);
 #endif
 
     // Connect stdin, stdout and stderr
