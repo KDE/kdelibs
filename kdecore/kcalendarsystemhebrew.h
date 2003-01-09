26d624dab67e5e7ccd8c814aae0f425ffa9bf2f1
Hans Petter Bieker <bieker@kde.org>
2003-01-09 11:42:28 +0000

    The Hebrew support will be added to this file. Please feel free to help.
    svn path=/trunk/kdelibs/; revision=198468

---
+++ kdecore/kcalendarsystemhebrew.h
@@ -0,0 +1,98 @@
+/*
+   Copyright (c) 2002 Carlos Moro <cfmoro@correo.uniovi.es>
+   Copyright (c) 2002 Hans Petter Bieker <bieker@kde.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
+#ifndef KCALENDARSYSTEMHEBREW_H
+#define KCALENDARSYSTEMHEBREW_H
+
+#include <qdatetime.h>
+#include <qstring.h>
+
+#include "kcalendarsystem.h"
+
+class KCalendarSystemHebrewPrivate;
+
+/**
+ * This is the Hebrew calendar implementation.
+ *
+ * The Hebrew calendar is the traditional Islamic calendar used in the Midle
+ * East.
+ *
+ * @see KLocale,KCalendarSystem,KCalendarSystemFactory
+ *
+ * @author Carlos Moro <cfmoro@correo.uniovi.es>
+ * @licence GNU-GPL v.2
+ * @version $Id$
+ * @since 3.2
+ */
+class KCalendarSystemHebrew : public KCalendarSystem
+{
+public:
+  KCalendarSystemHebrew(const KLocale * locale = 0);
+  virtual ~KCalendarSystemHebrew();
+
+  virtual int year (const QDate & date) const;
+  virtual int month (const QDate & date) const;
+  virtual int day (const QDate & date) const;
+  virtual int dayOfWeek (const QDate & date) const;
+  virtual int dayOfYear (const QDate & date) const;
+
+  virtual bool setYMD(QDate & date, int y, int m, int d) const;
+
+  virtual QDate addYears(const QDate & date, int nyears) const;
+  virtual QDate addMonths(const QDate & date, int nmonths) const;
+  virtual QDate addDays(const QDate & date, int ndays) const;
+
+  virtual int monthsInYear (const QDate & date) const;
+  virtual int daysInYear (const QDate & date) const;
+  virtual int daysInMonth (const QDate & date) const;
+  virtual int weeksInYear(int year) const;
+  virtual int weekNumber(const QDate& date, int * yearNum = 0) const;
+
+  virtual QString monthName (int month, bool shortName = false) const;
+  virtual QString monthName (const QDate & date, bool shortName = false ) const;
+  virtual QString monthNamePossessive(int month, bool shortName = false) const;
+  virtual QString monthNamePossessive(const QDate & date, bool shortName = false ) const;
+  virtual QString weekDayName (int weekDay, bool shortName = false) const;
+  virtual QString weekDayName (const QDate & date, bool shortName = false) const;
+
+  virtual int minValidYear () const;
+  virtual int maxValidYear () const;
+  virtual int weekDayOfPray () const;
+
+  virtual QString calendarName() const;
+
+  virtual bool isLunar() const;
+  virtual bool isLunisolar() const;
+  virtual bool isSolar() const;
+
+private:
+  /**
+   * Gets the number of days in a month for a given date
+   *
+   * @param year given year
+   * @param mon month number
+   * @return number of days in month
+   */
+  int hndays(int year, int mon) const;
+
+  KCalendarSystemHebrewPrivate * d;
+};
+
+#endif
