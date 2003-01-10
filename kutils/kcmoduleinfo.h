a90fa3536b8f03933f4a23dc4a44fcd415eb6c27
nobody <nobody@localhost>
2003-01-10 21:31:03 +0000

    This commit was manufactured by cvs2svn to accommodate a server-side copy/move.
    svn path=/trunk/kdelibs/; revision=198846

---
+++ kutils/kcmoduleinfo.h
@@ -0,0 +1,112 @@
+/*
+  Copyright (c) 1999 Matthias Hoelzer-Kluepfel <hoelzer@kde.org>
+  Copyright (c) 2000 Matthias Elter <elter@kde.org>
+  Copyright (c) 2003 Daniel Molkentin <molkentin@kde.org>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+*/
+
+#ifndef KCMODULEINFO_H
+#define KCMODULEINFO_H
+
+#include <kservice.h>
+
+class QPixmap;
+class QString;
+class QStringList;
+
+/**
+ * A class that provides information about a Control Module
+ * 
+ * @internal
+ * @author Matthias Hoelzer-Kluepfel <mhk@kde.org> 
+ * @author Matthias Elter <elter@kde.org>
+ * @author Daniel Molkentin <molkentin@kde.org>
+ * @since 3.2
+ *
+ */
+class KCModuleInfo
+{
+
+public:
+
+  /**
+   * Constructs a CSModuleInfo, note that you will have to take care about the deletion
+   * yoruself!
+   **/
+  KCModuleInfo(const QString& desktopFile, const QString& baseGroup /* = QString::fromLatin1("settings")*/);
+  virtual ~KCModuleInfo();
+
+  /** @return the filename pass in the constructor **/
+  QString fileName() const { return _fileName; };
+  /** @return **/
+  const QStringList &groups() const { return _groups; };
+  /** @return **/
+  const QStringList &keywords() const { return _keywords; };
+  
+  // changed from moduleName() to avoid abiguity with QObject::moduleName() on multiple inheritance
+  /** @return the modules name **/
+  QString moduleName() const { return _name; };
+  /** @return a pointer to KService created from the modules .desktop file **/
+  KService::Ptr service() const { return _service; };
+  /** @return the comment field **/
+  QString comment() const { return _comment; };
+  /** @return the icon name **/
+  QString icon() const { return _icon; };
+  /** @return the path of the module documentation **/
+  QString docPath() const;
+  /** @return the library name **/
+  QString library() const { return _lib; };
+  /** @return a handle (usually the contents of the FactoryName field) **/
+  QString handle() const;
+  /** @return wether the module might require root permissions **/
+  bool needsRootPrivileges() const;
+  /** @return the isHiddenByDefault attribute. @deprecated **/
+  bool isHiddenByDefault() const;
+
+  QCString moduleId() const;
+
+protected:
+
+  void setGroups(const QStringList &groups) { _groups = groups; };
+  void setKeywords(const QStringList &k) { _keywords = k; };
+  void setName(const QString &name) { _name = name; };
+  void setComment(const QString &comment) { _comment = comment; };
+  void setIcon(const QString &icon) { _icon = icon; };
+  void setLibrary(const QString &lib) { _lib = lib; };
+  void setHandle(const QString &handle) { _handle = handle; };
+  void setNeedsRootPrivileges(bool needsRootPrivileges) { _needsRootPrivileges = needsRootPrivileges; };
+  void setIsHiddenByDefault(bool isHiddenByDefault) { _isHiddenByDefault = isHiddenByDefault; };
+  void setDocPath(const QString &p) { _doc = p; };
+  void loadAll();
+
+private:
+
+  QStringList _groups, _keywords;
+  QString     _name, _icon, _lib, _handle, _fileName, _doc, _comment;
+  bool        _needsRootPrivileges, _isHiddenByDefault;
+  bool        _allLoaded;
+
+  KService::Ptr _service;
+
+  class KCModuleInfoPrivate;
+  KCModuleInfoPrivate *d;
+  
+};
+
+#endif // KCMODULEINFO_H
+
+// vim: ts=2 sw=2 et
---
+++ kutils/kcmultidialog.h
@@ -0,0 +1,131 @@
+/*
+   Copyright (c) 2000 Matthias Elter <elter@kde.org>
+   Copyright (c) 2003 Daniel Molkentin <molkentin@kde.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+
+*/
+
+#ifndef KCMULTIDIALOG_H
+#define KCMULTIDIALOG_H
+
+#include <qptrlist.h>
+#include <qptrdict.h>
+
+#include <kdialogbase.h>
+#include <kcmodule.h>
+
+/**
+ * A class that offers a @ref KDialogBase containing arbitrary KControl Modules
+ *
+ * @short A method that offers a @ref KDialogBase containing arbitrary
+ *        KControl Modules.
+ *
+ * @author Matthias Elter <elter@kde.org>, Daniel Molkentin <molkentin@kde.org>
+ * @since 3.2
+ */
+class KCMultiDialog : public KDialogBase
+{
+    Q_OBJECT
+
+public:
+    /**
+     * Constructs a new KCMultiDialog
+     *
+     * @param parent The parent Widget
+     * @param baseGroup The baseGroup, if you want to call a module out of
+     *                  kcontrol, just keep "settings"
+     * @param name The widget name
+     * @param modal If you pass true here, the dialog will be modal
+     **/
+    KCMultiDialog(QWidget *parent=0,
+                  const QString& baseGroup = QString::fromLatin1("settings"),
+                  const char *name=0, bool modal=false);
+
+    /**
+     * Destructor
+     **/
+   virtual ~KCMultiDialog();
+
+    /**
+     * Add a module.
+     *
+     * @param module Specify the name of the module that is to be added
+     *               to the list of modules the dialog will show.
+     *
+     * @param withfallback Try harder to load the module. Might result
+     *                     in the module appearing outside the dialog.
+     **/
+    void addModule(const QString& module, bool withfallback=true);
+
+protected slots:
+    /**
+     * This slot is called when the user presses the "Default" Button
+     * You can reimplement it if needed.
+     *
+     * @note Make sure you call the original implementation!
+     **/
+    virtual void slotDefault();
+
+    /**
+     * This slot is called when the user presses the "Apply" Button
+     * You can reimplement it if needed
+     *
+     * @note Make sure you call the original implementation!
+     **/
+    virtual void slotApply();
+
+    /**
+     * This slot is called when the user presses the "OK" Button
+     * You can reimplement it if needed
+     *
+     * @note Make sure you call the original implementation!
+     **/
+    virtual void slotOk();
+
+    /**
+     * This slot is called when the user presses the "Help" Button
+     * You can reimplement it if needed
+     *
+     * @note Make sure you call the original implementation!
+     **/
+    virtual void slotHelp();
+
+private slots:
+
+    void slotAboutToShow(QWidget *);
+
+    void clientChanged(bool state);
+
+private:
+    struct LoadInfo {
+      LoadInfo(const QString &_path, bool _withfallback)
+         : path(_path), withfallback(_withfallback)
+         { }
+      QString path;
+      bool withfallback;
+    };
+    QPtrList<KCModule> modules;
+    QPtrDict<LoadInfo> moduleDict;
+    QString _docPath;
+    QString _baseGroup;
+
+    // For future use
+    class KCMultiDialogPrivate;
+    KCMultiDialogPrivate *d;
+};
+
+#endif //KCMULTIDIALOG_H
