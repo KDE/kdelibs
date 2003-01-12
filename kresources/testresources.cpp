839ec2945da02054f93f66466f468cbe5a821f7d
Cornelius Schumacher <schumacher@kde.org>
2003-01-12 22:21:09 +0000

    Test code.
    svn path=/trunk/kdelibs/; revision=199453

--- kresources/testresources.cpp
+++ kresources/testresources.cpp
@@ -20,7 +20,11 @@ class TestSubResource : public TestResource
   public:
     TestSubResource() : TestResource() {}
     
-    void dump() const { kdDebug() << "DUMP: TestSubResource" << endl; }
+    void dump() const
+    {
+      kdDebug() << "TestSubResource" << endl;
+      TestResource::dump();
+    }
 };
 
 int main( int argc, char **argv )
@@ -33,16 +37,45 @@ int main( int argc, char **argv )
   ResourceManager<TestResource> manager( "test" );
   
   TestResource *resource1 = new TestResource;  
+  resource1->setResourceName( "One" );
   manager.add( resource1 );
-  resource1->dump();
 
   TestResource *resource2 = new TestSubResource;  
+  resource2->setResourceName( "Two" );
   manager.add( resource2 );
-  resource2->dump();
 
-  kdDebug() << "List Resources:" << endl;
+  TestResource *resource3 = new TestSubResource;  
+  resource3->setResourceName( "Three" );
+  manager.add( resource3 );
+
+  kdDebug() << "LIST ALL:" << endl;
   ResourceManager<TestResource>::Iterator it;
   for( it = manager.begin(); it != manager.end(); ++it ) {
     (*it)->dump();
   }
+
+  resource2->setActive( false );
+  resource3->setActive( true );
+  
+  kdDebug() << "LIST ACTIVE" << endl;
+  ResourceManager<TestResource>::ActiveIterator it2;
+  for( it2 = manager.activeBegin(); it2 != manager.activeEnd(); ++it2 ) {
+    (*it2)->dump();
+  }
+
+  resource1->setActive( false );
+  resource2->setActive( true );
+  resource3->setActive( true );
+  
+  kdDebug() << "LIST ACTIVE" << endl;
+  for( it2 = manager.activeBegin(); it2 != manager.activeEnd(); ++it2 ) {
+    (*it2)->dump();
+  }
+
+  kdDebug() << "LIST ALL" << endl;
+  for( it = manager.begin(); it != manager.end(); ++it ) {
+    (*it)->dump();
+  }
+
+
 }
