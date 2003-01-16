c28ab2712f34aed39b3c4562edd2523c42edbb03
Dirk Mueller <mueller@kde.org>
2003-01-16 20:50:21 +0000

    Stop spinning wheel when cancelling a download (e.g. install.sh on http://www.adsl4linux.nl/frames.html) (backport revision 1.31)
    svn path=/branches/KDE_3_1_BRANCH/kdelibs/; revision=200417

--- khtml/khtml_run.cpp
+++ khtml/khtml_run.cpp
@@ -24,7 +24,7 @@
 #include <kio/job.h>
 #include <kdebug.h>
 #include <klocale.h>
-#include <khtml_ext.h>
+#include "khtml_ext.h"
 #include <qwidget.h>
 
 KHTMLRun::KHTMLRun( KHTMLPart *part, khtml::ChildFrame *child, const KURL &url,
@@ -59,6 +59,7 @@ void KHTMLRun::foundMimeType( const QString &_type )
     if ( m_bFinished )
     {
         m_timer.start( 0, true );
+        emit static_cast<KHTMLPart *>(m_part)->canceled(QString::null);
         return;
     }
 
