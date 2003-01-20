9ab63f07393ff9141ef8def43c2e9a6b2e1c3e3d
Zack Rusin <zack@kde.org>
2003-01-20 23:38:17 +0000

    Stop scaring people about not passing the test. Maybe it'd be a good idea to remove the uid check from Addressee comparison.
    svn path=/trunk/kdelibs/; revision=201564

--- kabc/vcardparser/testread2.cpp
+++ kabc/vcardparser/testread2.cpp
@@ -15,34 +15,28 @@ main()
 
     Addressee::List parsed = vct.parseVCards( vcards );
 
-    if ( l == parsed ) {
-        kdDebug()<<"Test - PASSED"<<endl;
+    if ( l.size() != parsed.size() ) {
+        kdDebug()<<"\tSize - FAILED : "<<l.size()<<" vs. parsed "<<parsed.size()<<endl;
     } else {
-        kdDebug()<<"Test - FAILED"<<endl;
+        kdDebug()<<"\tSize - PASSED"<<endl;
+    }
 
-        if ( l.size() != parsed.size() ) {
-            kdDebug()<<"\tSize - FAILED : "<<l.size()<<" vs. parsed "<<parsed.size()<<endl;
+    Addressee::List::iterator itr1;
+    Addressee::List::iterator itr2;
+    for ( itr1 = l.begin(), itr2 = parsed.begin();
+          itr1 != l.end(); ++itr1, ++itr2 ) {
+        if ( (*itr1).fullEmail() == (*itr2).fullEmail() &&
+             (*itr1).organization() == (*itr2).organization() &&
+             (*itr1).phoneNumbers() == (*itr2).phoneNumbers()  &&
+             (*itr1).emails() == (*itr2).emails() &&
+             (*itr1).role() == (*itr2).role()  ) {
+            kdDebug()<<"\tAddressee  - PASSED"<<endl;
+            kdDebug()<<"\t\t"<< (*itr1).fullEmail() << " VS. " << (*itr2).fullEmail()<<endl;
         } else {
-            kdDebug()<<"\tSize - PASSED"<<endl;
-        }
-
-        Addressee::List::iterator itr1;
-        Addressee::List::iterator itr2;
-        for ( itr1 = l.begin(), itr2 = parsed.begin();
-              itr1 != l.end(); ++itr1, ++itr2 ) {
-            if ( (*itr1).fullEmail() == (*itr2).fullEmail() &&
-                 (*itr1).organization() == (*itr2).organization() &&
-                 (*itr1).phoneNumbers() == (*itr2).phoneNumbers()  &&
-                 (*itr1).emails() == (*itr2).emails() &&
-                 (*itr1).role() == (*itr2).role()  ) {
-                kdDebug()<<"\tAddressee  - PASSED"<<endl;
-                kdDebug()<<"\t\t"<< (*itr1).fullEmail() << " VS. " << (*itr2).fullEmail()<<endl;
-            } else {
-                kdDebug()<<"\tAddressee  - FAILED"<<endl;
-                (*itr1).dump();
-                (*itr2).dump();
-                //kdDebug()<<"\t\t"<< (*itr1).fullEmail() << " VS. " << (*itr2).fullEmail()<<endl;
-            }
+            kdDebug()<<"\tAddressee  - FAILED"<<endl;
+            (*itr1).dump();
+            (*itr2).dump();
+            //kdDebug()<<"\t\t"<< (*itr1).fullEmail() << " VS. " << (*itr2).fullEmail()<<endl;
         }
     }
 }
