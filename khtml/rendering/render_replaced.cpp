55b7112dc5ceb9ac4c517ed6a57bc3723bc0686c
Peter Kelly <pmk@post.com>
2003-01-25 00:59:36 +0000

    hide some debug output
    svn path=/trunk/kdelibs/; revision=202643

--- khtml/rendering/render_replaced.cpp
+++ khtml/rendering/render_replaced.cpp
@@ -140,7 +140,7 @@ RenderWidget::RenderWidget(DOM::NodeImpl* node)
 
 void RenderWidget::detach(RenderArena* renderArena)
 {
-    qDebug("RenderWidget::detach( %p )",  this );
+    kdDebug( 6040 ) << "RenderWidget::detach( " << this << " )" << endl;
     remove();
 
     if ( m_widget ) {
