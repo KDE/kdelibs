aa500bdcda9ac6a0b7cfb7b85d6e672ea7703844
George Staikos <staikos@kde.org>
2003-01-29 07:32:05 +0000

    only two files left to document, and not so critical ones
    svn path=/trunk/kdelibs/; revision=203492

--- kio/kssl/ksslcertificate.h
+++ kio/kssl/ksslcertificate.h
@@ -1,6 +1,6 @@
 /* This file is part of the KDE project
  *
- * Copyright (C) 2000,2001 George Staikos <staikos@kde.org>
+ * Copyright (C) 2000-2003 George Staikos <staikos@kde.org>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -22,13 +22,9 @@
 #define _KSSLCERTIFICATE_H
 
 
-// Note: this object is VERY HEAVY TO COPY.  Please try to use reference
-//       or ptr whenever possible
-
 // UPDATE: I like the structure of this class less and less every time I look
 //         at it.  I think it needs to change.
 //
-
 //
 //  The biggest reason for making everything protected here is so that
 //  the class can have all it's methods available even if openssl is not
@@ -57,6 +53,17 @@ typedef struct x509_st X509;
 class X509;
 #endif
 
+/**
+ * KDE X.509 Certificate
+ *
+ * This class represents an X.509 (SSL) certificate.
+ * Note: this object is VERY HEAVY TO COPY.  Please try to use reference
+ *       or pointer whenever possible
+ *
+ * @author George Staikos <staikos@kde.org>
+ * @see KSSL
+ * @short KDE X.509 Certificate
+ */
 class KSSLCertificate {
 friend class KSSL;
 friend class KSSLCertificateHome;
@@ -68,81 +75,238 @@ friend class KSSLPKCS12;
 friend class KSSLD;
 
 public:
-  ~KSSLCertificate();
-  static KSSLCertificate *fromString(QCString cert);
-  // This one duplicates the X509 object for itself.
-  static KSSLCertificate *fromX509(X509 *x5);
+	/**
+	 *  Destroy this X.509 certificate.
+	 */
+	~KSSLCertificate();
 
-  // We need a static method to generate one of these, given a filename that
-  // points to a PEM encoded certificate file.  It will return NULL on failure.
-  // (that's why I don't want to use a constructor)
+	/**
+	 *  Create an X.509 certificate from a base64 encoded string.
+	 *  @param cert the certificate in base64 form
+	 *  @return the X.509 certificate, or NULL
+	 */
+	static KSSLCertificate *fromString(QCString cert);
 
-  enum KSSLValidation { Unknown, Ok, NoCARoot, InvalidPurpose,
-                        PathLengthExceeded, InvalidCA, Expired,
-                        SelfSigned, ErrorReadingRoot, NoSSL,
-                        Revoked, Untrusted, SignatureFailed,
-                        Rejected, PrivateKeyFailed };
+	/**
+	 *  Create an X.509 certificate from the internal representation.
+	 *  This one duplicates the X509 object for itself.
+	 *  @param x5 the OpenSSL representation of the certificate
+	 *  @return the X.509 certificate, or NULL
+	 *  @internal
+	 */
+	static KSSLCertificate *fromX509(X509 *x5);
 
-  enum KSSLPurpose { None=0, SSLServer=1, SSLClient=2, 
-			SMIMESign=3, SMIMEEncrypt=4, Any=5 };
+	enum KSSLValidation {   Unknown, Ok, NoCARoot, InvalidPurpose,
+				PathLengthExceeded, InvalidCA, Expired,
+				SelfSigned, ErrorReadingRoot, NoSSL,
+				Revoked, Untrusted, SignatureFailed,
+				Rejected, PrivateKeyFailed };
 
-  QString toString();
+	enum KSSLPurpose {      None=0, SSLServer=1, SSLClient=2, 
+				SMIMESign=3, SMIMEEncrypt=4, Any=5 };
 
-  QString getSubject() const;
+	/**
+	 *  Convert this certificate to a string.
+	 *  @return the certificate in base64 format
+	 */
+	QString toString();
 
-  QString getIssuer() const;
+	/**
+	 *  Get the subject of the certificate (X.509 map).
+	 *  @return the subject
+	 */
+	QString getSubject() const;
 
-  QString getNotBefore() const;
-  QString getNotAfter() const;
+	/**
+	 *  Get the issuer of the certificate (X.509 map).
+	 *  @return the issuer
+	 */
+	QString getIssuer() const;
 
-  QDateTime getQDTNotBefore() const;
-  QDateTime getQDTNotAfter() const;
+	/**
+	 *  Get the date that the certificate becomes valid on.
+	 *  @return the date as a string, localised
+	 */
+	QString getNotBefore() const;
 
-  QByteArray toDer();
-  QByteArray toPem();
-  QByteArray toNetscape();
-  QString    toText();
+	/**
+	 *  Get the date that the certificate is valid until.
+	 *  @return the date as a string, localised
+	 */
+	QString getNotAfter() const;
 
-  QString getSerialNumber() const;
-  QString getKeyType() const;
-  QString getPublicKeyText() const;
-  QString getMD5DigestText() const;
-  QString getSignatureText() const;
+	/**
+	 *  Get the date that the certificate becomes valid on.
+	 *  @return the date
+	 */
+	QDateTime getQDTNotBefore() const;
 
-  bool isValid();
-  bool isValid(KSSLPurpose p);
-  KSSLValidation validate();
-  KSSLValidation validate(KSSLPurpose p);
-  KSSLValidation revalidate();
-  KSSLValidation revalidate(KSSLPurpose p);
-  KSSLCertChain& chain();
+	/**
+	 *  Get the date that the certificate is valid until.
+	 *  @return the date
+	 */
+	QDateTime getQDTNotAfter() const;
 
-  static QString verifyText(KSSLValidation x);
+	/**
+	 *  Convert the certificate to DER (ASN.1) format.
+	 *  @return the binary data of the DER encoding
+	 */
+	QByteArray toDer();
 
-  KSSLCertificate *replicate();
-  KSSLCertificate(const KSSLCertificate& x); // copy constructor
+	/**
+	 *  Convert the certificate to PEM (base64) format.
+	 *  @return the binary data of the PEM encoding
+	 */
+	QByteArray toPem();
 
-         friend int operator==(KSSLCertificate &x, KSSLCertificate &y);
-  inline friend int operator!=(KSSLCertificate &x, KSSLCertificate &y) 
+	/**
+	 *  Convert the certificate to Netscape format.
+	 *  @return the binary data of the Netscape encoding
+	 */
+	QByteArray toNetscape();
+
+	/**
+	 *  Convert the certificate to OpenSSL plain text format.
+	 *  @return the OpenSSL text encoding
+	 */
+	QString toText();
+
+	/**
+	 *  Get the serial number of the certificate.
+	 *  @return the serial number as a string
+	 */
+	QString getSerialNumber() const;
+
+	/**
+	 *  Get the key type (RSA, DSA, etc).
+	 *  @return the key type as a string
+	 */
+	QString getKeyType() const;
+
+	/**
+	 *  Get the public key.
+	 *  @return the public key as a hexidecimal string
+	 */
+	QString getPublicKeyText() const;
+
+	/**
+	 *  Get the MD5 digest of the certificate.
+	 *  @return the MD5 digest in a hexidecimal string
+	 */
+	QString getMD5DigestText() const;
+
+	/**
+	 *  Get the signature.
+	 *  @return the signature in text format
+	 */
+	QString getSignatureText() const;
+
+	/**
+	 *  Check if this is a valid certificate.  Will use cached data.
+	 *  @return true if it is valid
+	 */
+	bool isValid();
+
+	/**
+	 *  Check if this is a valid certificate.  Will use cached data.
+	 *  @param p the purpose to validate for
+	 *  @return true if it is valid
+	 */
+	bool isValid(KSSLPurpose p);
+
+	/**
+	 *  Check if this is a valid certificate.  Will use cached data.
+	 *  @return the result of the validation
+	 */
+	KSSLValidation validate();
+
+	/**
+	 *  Check if this is a valid certificate.  Will use cached data.
+	 *  @param p the purpose to validate for
+	 *  @return the result of the validation
+	 */
+	KSSLValidation validate(KSSLPurpose p);
+
+	/**
+	 *  Check if this is a valid certificate.  Will NOT use cached data.
+	 *  @return the result of the validation
+	 */
+	KSSLValidation revalidate();
+
+	/**
+	 *  Check if this is a valid certificate.  Will NOT use cached data.
+	 *  @param p the purpose to validate for
+	 *  @return the result of the validation
+	 */
+	KSSLValidation revalidate(KSSLPurpose p);
+
+	/**
+	 *  Get a reference to the certificate chain.
+	 *  @return reference to the chain
+	 */
+	KSSLCertChain& chain();
+
+	/**
+	 *  Obtain the localized message that corresponds to a validation result.
+	 *  @param x the code to look up
+	 *  @return the message text corresponding to the validation code
+	 */
+	static QString verifyText(KSSLValidation x);
+
+	/**
+	 *  Explicitly make a copy of this certificate.
+	 *  @return a copy of the certificate
+	 */
+	KSSLCertificate *replicate();
+
+	/**
+	 *  Copy constructor.  Beware, this is very expensive.
+	 *  @param x the object to copy from
+	 */
+	KSSLCertificate(const KSSLCertificate& x); // copy constructor
+
+	/**
+	 *  Compare two certificate to determine if they are the same.
+	 */
+	friend int operator==(KSSLCertificate &x, KSSLCertificate &y);
+
+	/**
+	 *  Compare two certificate to determine if they are not the same.
+	 */
+	inline friend int operator!=(KSSLCertificate &x, KSSLCertificate &y) 
                                                        { return !(x == y); }
-  bool setCert(QString& cert);
 
-  KSSLX509V3& x509V3Extensions();
+	/**
+	 *  Re-set the certificate from a base64 string.
+	 *  @param cert the certificate to set to
+	 *  @return true on success
+	 */
+	bool setCert(QString& cert);
 
-  bool isSigner();
+	/**
+	 *  Access the X.509v3 parameters.
+	 *  @return reference to the extension object
+	 *  @see KSSLX509V3
+	 */
+	KSSLX509V3& x509V3Extensions();
+
+	/**
+	 *  Check if this is a signer certificate.
+	 *  @return true if this is a signer certificate
+	 */
+	bool isSigner();
 
 private:
-  KSSLCertificatePrivate *d;
-  int purposeToOpenSSL(KSSLPurpose p) const; 
-
+	KSSLCertificatePrivate *d;
+	int purposeToOpenSSL(KSSLPurpose p) const; 
 
 protected:
-  KSSLCertificate();
+	KSSLCertificate();
 
-  void setCert(X509 *c);
-  void setChain(void *c);
-  X509 *getCert();
-  KSSLValidation processError(int ec);
+	void setCert(X509 *c);
+	void setChain(void *c);
+	X509 *getCert();
+	KSSLValidation processError(int ec);
 };
 
 
