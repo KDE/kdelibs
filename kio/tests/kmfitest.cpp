8e4cff38889746562f38a969386c1e41d9101b44
Waldo Bastian <bastian@kde.org>
2003-02-19 15:41:38 +0000

    A test
    svn path=/trunk/kdelibs/; revision=207947

---
+++ kio/tests/kmfitest.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <qdir.h>
+#include <qfile.h>
+#include <kinstance.h>
+#include <kurl.h>
+#include <kfilemetainfo.h>
+#include <kmimetype.h>
+
+int main (int argc, char **argv)
+{
+	KInstance ins("kmfitest");
+
+	if (argc < 2) {
+		printf("usage: %s <file>\n", argv[0]);
+		return 1;
+	}
+
+	for (int i = 1; i < argc; i++) { 
+		QString file = QFile::decodeName(argv[i]);
+		qWarning("File: %s", file.local8Bit().data());
+		KMimeType::Ptr p;
+		p = KMimeType::findByPath(file);
+		qWarning("Mime type (findByPath): %s", p->name().latin1());
+		KFileMetaInfo meta(file, QString::null, KFileMetaInfo::TechnicalInfo | KFileMetaInfo::ContentInfo);
+	}
+
+	return 0;
+}
