e21493561726b49ead80dc8c5cea82c31c4a1fa6
Lars Knoll <knoll@kde.org>
2003-02-20 21:09:57 +0000

    regenerated
    svn path=/trunk/kdelibs/; revision=208196

--- khtml/css/parser.h
+++ khtml/css/parser.h
@@ -1,3 +1,7 @@
+#ifndef BISON_PARSER_TAB_H
+# define BISON_PARSER_TAB_H
+
+#ifndef YYSTYPE
 typedef union {
     CSSRuleImpl *rule;
     CSSSelector *selector;
@@ -10,52 +14,57 @@ typedef union {
     float val;
     int prop_id;
     int attribute;
-    int element;
+    unsigned int element;
     CSSSelector::Relation relation;
     bool b;
     char tok;
     Value value;
     ValueList *valueList;
-} YYSTYPE;
-#define	S	257
-#define	SGML_CD	258
-#define	INCLUDES	259
-#define	DASHMATCH	260
-#define	BEGINSWITH	261
-#define	ENDSWITH	262
-#define	CONTAINS	263
-#define	STRING	264
-#define	IDENT	265
-#define	HASH	266
-#define	IMPORT_SYM	267
-#define	PAGE_SYM	268
-#define	MEDIA_SYM	269
-#define	FONT_FACE_SYM	270
-#define	CHARSET_SYM	271
-#define	KONQ_RULE_SYM	272
-#define	KONQ_DECLS_SYM	273
-#define	KONQ_VALUE_SYM	274
-#define	IMPORTANT_SYM	275
-#define	QEMS	276
-#define	EMS	277
-#define	EXS	278
-#define	PXS	279
-#define	CMS	280
-#define	MMS	281
-#define	INS	282
-#define	PTS	283
-#define	PCS	284
-#define	DEGS	285
-#define	RADS	286
-#define	GRADS	287
-#define	MSECS	288
-#define	SECS	289
-#define	HERZ	290
-#define	KHERZ	291
-#define	DIMEN	292
-#define	PERCENTAGE	293
-#define	NUMBER	294
-#define	URI	295
-#define	FUNCTION	296
-#define	UNICODERANGE	297
+} yystype;
+# define YYSTYPE yystype
+#endif
+# define	S	257
+# define	SGML_CD	258
+# define	INCLUDES	259
+# define	DASHMATCH	260
+# define	BEGINSWITH	261
+# define	ENDSWITH	262
+# define	CONTAINS	263
+# define	STRING	264
+# define	IDENT	265
+# define	HASH	266
+# define	IMPORT_SYM	267
+# define	PAGE_SYM	268
+# define	MEDIA_SYM	269
+# define	FONT_FACE_SYM	270
+# define	CHARSET_SYM	271
+# define	NAMESPACE_SYM	272
+# define	KONQ_RULE_SYM	273
+# define	KONQ_DECLS_SYM	274
+# define	KONQ_VALUE_SYM	275
+# define	IMPORTANT_SYM	276
+# define	QEMS	277
+# define	EMS	278
+# define	EXS	279
+# define	PXS	280
+# define	CMS	281
+# define	MMS	282
+# define	INS	283
+# define	PTS	284
+# define	PCS	285
+# define	DEGS	286
+# define	RADS	287
+# define	GRADS	288
+# define	MSECS	289
+# define	SECS	290
+# define	HERZ	291
+# define	KHERZ	292
+# define	DIMEN	293
+# define	PERCENTAGE	294
+# define	NUMBER	295
+# define	URI	296
+# define	FUNCTION	297
+# define	UNICODERANGE	298
 
+
+#endif /* not BISON_PARSER_TAB_H */
