b8663642839fcf3df5b3a97ca5679b602ae00169
Koos Vriezen <koos.vriezen@gmail.com>
2003-02-24 00:22:39 +0000

    frame() can now return an object too
    svn path=/trunk/kdelibs/; revision=208904

--- khtml/khtml_part.cpp
+++ khtml/khtml_part.cpp
@@ -3784,7 +3784,7 @@ void KHTMLPart::slotChildURLRequest( const KURL &url, const KParts::URLArgs &arg
     }
   }
 
-  if ( child ) {
+  if ( child && child->m_part->inherits( "KHTMLPart" )) {
       // Inform someone that we are about to show something else.
       child->m_bNotify = true;
       requestObject( child, url, args );
