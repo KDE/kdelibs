77c232803a7b435f19aa15344399b5c100bfc8ce
Laurent Montel <montel@kde.org>
2003-02-24 17:29:08 +0000

    Fix compile
    svn path=/branches/KDE_3_1_BRANCH/kdelibs/; revision=209084

--- kio/misc/kpac/kpac_discovery.cpp
+++ kio/misc/kpac/kpac_discovery.cpp
@@ -89,7 +89,7 @@ bool KPACDiscovery::tryDiscovery()
             if (m_hostname.isEmpty())
                 return false;
             else if (m_data.isEmpty())
-                m_data = m_hostname.utf8();
+                m_data = m_hostname;
             int pos = m_data.find('.');
             if (pos == -1)
                 return false;
