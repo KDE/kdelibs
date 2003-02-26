c9d99856fa85a706320e8b6a119b368a81235a5f
Alex Zepeda <zipzippy@sonic.net>
2003-02-26 22:34:01 +0000

    alloca.h doesn't exist here (just like malloc.h doesn't).  One could be lead to believe that it does exist somewhere in KDE land by looking at config.h.
    svn path=/trunk/kdelibs/; revision=209836

--- kio/kio/kzip.cpp
+++ kio/kio/kzip.cpp
@@ -37,10 +37,18 @@
 
 */
 
+// $Id$
 
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
 
 #include <sys/time.h>
+#ifdef HAVE_ALLOCA_H
 #include <alloca.h>
+#else
+#include <stdlib.h>
+#endif
 
 #include <qasciidict.h>
 #include <qfile.h>
