1bb380b46dee62f6eb2a6ac8ee0fa3c81d991d9a
Dirk Mueller <mueller@kde.org>
2003-02-26 06:52:33 +0000

    stop crashing on hotmail.com CCMAIL: 55094-done@bugs.kde.org
    svn path=/trunk/kdelibs/; revision=209553

--- khtml/css/parser.h
+++ khtml/css/parser.h
@@ -1,3 +1,7 @@
+#ifndef BISON_PARSER_TAB_H
+# define BISON_PARSER_TAB_H
+
+#ifndef YYSTYPE
 typedef union {
     CSSRuleImpl *rule;
     CSSSelector *selector;
@@ -17,47 +21,52 @@ typedef union {
     char tok;
     Value value;
     ValueList *valueList;
-} YYSTYPE;
-#define	S	257
-#define	SGML_CD	258
-#define	INCLUDES	259
-#define	DASHMATCH	260
-#define	BEGINSWITH	261
-#define	ENDSWITH	262
-#define	CONTAINS	263
-#define	STRING	264
-#define	IDENT	265
-#define	HASH	266
-#define	IMPORT_SYM	267
-#define	PAGE_SYM	268
-#define	MEDIA_SYM	269
-#define	FONT_FACE_SYM	270
-#define	CHARSET_SYM	271
-#define	NAMESPACE_SYM	272
-#define	KONQ_RULE_SYM	273
-#define	KONQ_DECLS_SYM	274
-#define	KONQ_VALUE_SYM	275
-#define	IMPORTANT_SYM	276
-#define	QEMS	277
-#define	EMS	278
-#define	EXS	279
-#define	PXS	280
-#define	CMS	281
-#define	MMS	282
-#define	INS	283
-#define	PTS	284
-#define	PCS	285
-#define	DEGS	286
-#define	RADS	287
-#define	GRADS	288
-#define	MSECS	289
-#define	SECS	290
-#define	HERZ	291
-#define	KHERZ	292
-#define	DIMEN	293
-#define	PERCENTAGE	294
-#define	NUMBER	295
-#define	URI	296
-#define	FUNCTION	297
-#define	UNICODERANGE	298
+} yystype;
+# define YYSTYPE yystype
+# define YYSTYPE_IS_TRIVIAL 1
+#endif
+# define	S	257
+# define	SGML_CD	258
+# define	INCLUDES	259
+# define	DASHMATCH	260
+# define	BEGINSWITH	261
+# define	ENDSWITH	262
+# define	CONTAINS	263
+# define	STRING	264
+# define	IDENT	265
+# define	HASH	266
+# define	IMPORT_SYM	267
+# define	PAGE_SYM	268
+# define	MEDIA_SYM	269
+# define	FONT_FACE_SYM	270
+# define	CHARSET_SYM	271
+# define	NAMESPACE_SYM	272
+# define	KONQ_RULE_SYM	273
+# define	KONQ_DECLS_SYM	274
+# define	KONQ_VALUE_SYM	275
+# define	IMPORTANT_SYM	276
+# define	QEMS	277
+# define	EMS	278
+# define	EXS	279
+# define	PXS	280
+# define	CMS	281
+# define	MMS	282
+# define	INS	283
+# define	PTS	284
+# define	PCS	285
+# define	DEGS	286
+# define	RADS	287
+# define	GRADS	288
+# define	MSECS	289
+# define	SECS	290
+# define	HERZ	291
+# define	KHERZ	292
+# define	DIMEN	293
+# define	PERCENTAGE	294
+# define	NUMBER	295
+# define	URI	296
+# define	FUNCTION	297
+# define	UNICODERANGE	298
 
+
+#endif /* not BISON_PARSER_TAB_H */
