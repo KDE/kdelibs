f82b8ed6696856a876714e012a70dc7cac9f4bf7
Stephan Binner <binner@kde.org>
2003-02-26 09:50:35 +0000

    Backport of alternate background coloring in Konqueror list views fix (#45800).
    svn path=/branches/KDE_3_1_BRANCH/kdelibs/; revision=209576

--- kdeui/klistview.cpp
+++ kdeui/klistview.cpp
@@ -2066,6 +2066,9 @@ void KListViewItem::paintCell(QPainter *p, const QColorGroup &cg, int column, in
         p->setBrushOrigin( -listView()->contentsX(), -listView()->contentsY() );
   }
   else if (isAlternate())
+       if (listView()->viewport()->backgroundMode()==Qt::TransparentMode)
+            _cg.setColor(QColorGroup::Background, static_cast< KListView* >(listView())->alternateBackground());
+       else
         _cg.setColor(QColorGroup::Base, static_cast< KListView* >(listView())->alternateBackground());
 
   QListViewItem::paintCell(p, _cg, column, width, alignment);
