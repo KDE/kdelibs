976c3b4cdc9622b0f035ce3e6bb3ec7d1df5b848
Carsten Pfeiffer <pfeiffer@kde.org>
2003-02-27 22:00:50 +0000

    add a winId parameter so that knotify can place passive popups properly
    svn path=/trunk/kdelibs/; revision=210141

--- kdecore/knotifyclient.cpp
+++ kdecore/knotifyclient.cpp
@@ -33,8 +33,8 @@
 static const char * const daemonName="knotify";
 
 static bool sendNotifyEvent(const QString &message, const QString &text,
-                 int present, int level, const QString &sound,
-                 const QString &file)
+                            int present, int level, const QString &sound,
+                            const QString &file, int winId )
 {
   if (!kapp) return false;
 
@@ -49,15 +49,34 @@ static bool sendNotifyEvent(const QString &message, const QString &text,
   QByteArray data;
   QDataStream ds(data, IO_WriteOnly);
   QString appname = KNotifyClient::instance()->instanceName();
-  ds << message << appname << text << sound << file << present << level;
+  ds << message << appname << text << sound << file << present << level << winId;
 
   if ( !KNotifyClient::startDaemon() )
       return false;
 
-  return client->send(daemonName, "Notify", "notify(QString,QString,QString,QString,QString,int,int)", data);
+  return client->send(daemonName, "Notify", "notify(QString,QString,QString,QString,QString,int,int,int)", data);
 }
 
+#ifndef KDE_NO_COMPAT
 bool KNotifyClient::event( StandardEvent type, const QString& text )
+{
+    return event( 0, type, text );
+}
+
+bool KNotifyClient::event(const QString &message, const QString &text)
+{
+    return event(0, message, text);
+}
+
+bool KNotifyClient::userEvent(const QString &text, int present, int level,
+                              const QString &sound, const QString &file)
+{
+    return userEvent( 0, text, present, level, sound, file );
+}
+
+#endif    
+
+bool KNotifyClient::event( int winId, StandardEvent type, const QString& text )
 {
     QString message;
     switch ( type ) {
@@ -80,18 +99,20 @@ bool KNotifyClient::event( StandardEvent type, const QString& text )
     }
 
     return sendNotifyEvent( message, text, Default, Default,
-			    QString::null, QString::null);
+			    QString::null, QString::null, winId );
 }
 
-bool KNotifyClient::event(const QString &message, const QString &text)
+bool KNotifyClient::event(int winId, const QString &message, 
+                          const QString &text)
 {
-  return sendNotifyEvent(message, text, Default, Default, QString::null, QString::null);
+  return sendNotifyEvent(message, text, Default, Default, QString::null, QString::null, winId);
 }
 
-bool KNotifyClient::userEvent(const QString &text, int present, int level,
+bool KNotifyClient::userEvent(int winId, const QString &text, int present, 
+                              int level,
                               const QString &sound, const QString &file)
 {
-  return sendNotifyEvent(QString::null, text, present, level, sound, file);
+  return sendNotifyEvent(QString::null, text, present, level, sound, file, winId);
 }
 
 int KNotifyClient::getPresentation(const QString &eventname)
