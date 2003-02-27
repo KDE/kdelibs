014b192018be51dcf12549667900e95e40e008a4
Alexander Kellett <lypanov@kde.org>
2003-02-27 21:21:54 +0000

    new class structure: KNSBookmarkExporterImpl inherits from KBookmarkExporterBase api still not stable but unused in any case bc done via reimplementation of old header with new class public writeFolder func still broken,     - but its very unlikely that its used, i hope     - requires an ugly d() ptr workaround...
    svn path=/trunk/kdelibs/; revision=210115

--- kio/bookmarks/kbookmarkexporter.h
+++ kio/bookmarks/kbookmarkexporter.h
@@ -1,6 +1,7 @@
 /* This file is part of the KDE libraries
    Copyright (C) 1996-1998 Martin R. Jones <mjones@kde.org>
    Copyright (C) 2000 David Faure <faure@kde.org>
+   Copyright (C) 2003 Alexander Kellett <lypanov@kde.org>
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
@@ -23,27 +24,55 @@
 #include <qtextstream.h>
 #include <kbookmark.h>
 
+// Note - KNSBookmarkImporter can be used to get the path to the filename
+
+
 /**
  * A class that exports all the current bookmarks to Netscape/Mozilla bookmarks
  * Warning, it overwrites the existing bookmarks.html file !
+ *
+ * only for BC - remove for KDE 4.0
  */
 class KNSBookmarkExporter
 {
 public:
-    // use KNSBookmarkImporter to get the path to the filename
-    KNSBookmarkExporter( KBookmarkManager* mgr,const QString & fileName )
-        : m_fileName(fileName), m_pManager(mgr) {}
+    KNSBookmarkExporter(KBookmarkManager* mgr, const QString & fileName)
+      : m_fileName(fileName), m_pManager(mgr) { }
     ~KNSBookmarkExporter() {}
 
-    // For compat
-    void write() { write(false); }
-    // Write out. Use utf8=true for Mozilla, false for Netscape
+    void write() { write(false); } // deprecated
     void write( bool utf8 );
 
 protected:
-    void writeFolder( QTextStream &stream, KBookmarkGroup parent );
+    void writeFolder(QTextStream &stream, KBookmarkGroup parent);
     QString m_fileName;
     KBookmarkManager* m_pManager;
+};
+
+class KBookmarkExporterBase
+{
+public:
+    KBookmarkExporterBase(KBookmarkManager* mgr, const QString & fileName)
+        : m_fileName(fileName), m_pManager(mgr) 
+    { ; }
+    virtual ~KBookmarkExporterBase() {}
+    virtual void write(bool utf8 /* true for mozilla */, KBookmarkGroup) = 0;
+protected:
+    virtual const QString folderAsString(KBookmarkGroup) = 0;
+    QString m_fileName;
+    KBookmarkManager* m_pManager;
+};
+
+class KNSBookmarkExporterImpl : public KBookmarkExporterBase
+{
+public:
+    KNSBookmarkExporterImpl(KBookmarkManager* mgr, const QString & fileName)
+      : KBookmarkExporterBase(mgr, fileName) 
+    { ; }
+    virtual ~KNSBookmarkExporterImpl() {}
+    virtual void write(bool utf8, KBookmarkGroup);
+protected:
+    virtual const QString folderAsString(KBookmarkGroup);
 };
 
 #endif
