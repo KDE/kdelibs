c78f3934309948fabaa930c1aeaa24ef52d90be6
Alexander Kellett <lypanov@kde.org>
2003-03-01 00:09:04 +0000

    move nsexporter into importer_ns make copyrights follow code, hope this is okay.
    svn path=/trunk/kdelibs/; revision=210409

--- kio/bookmarks/kbookmarkimporter.cc
+++ kio/bookmarks/kbookmarkimporter.cc
@@ -1,5 +1,5 @@
 /* This file is part of the KDE libraries
-   Copyright (C) 2000 David Faure <faure@kde.org>
+   Copyright (C) 2003 Alexander Kellett <lypanov@kde.org>
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
@@ -29,7 +29,5 @@
 #include <dirent.h>
 #include <sys/stat.h>
 #include <assert.h>
-
-////
 
 // todo - put stuff in here :)
