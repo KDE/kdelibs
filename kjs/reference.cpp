a084438f868db56e89de7c6cd4fd027aa78b41bd
David Faure <faure@kde.org>
2003-03-09 00:06:59 +0000

    fix compilation with KJS_VERBOSE
    svn path=/trunk/kdelibs/; revision=212463

--- kjs/reference.cpp
+++ kjs/reference.cpp
@@ -150,7 +150,7 @@ void Reference::putValue(ExecState *exec, const Value &w)
   }
 
 #ifdef KJS_VERBOSE
-  printInfo(exec,(UString("setting property ")+getPropertyName(exec)).cstring().c_str(),w);
+  printInfo(exec,(UString("setting property ")+getPropertyName(exec).ustring()).cstring().c_str(),w);
 #endif
   Value o = getBase(exec);
   if (o.type() == NullType)
