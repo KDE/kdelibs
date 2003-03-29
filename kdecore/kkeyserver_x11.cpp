36e89667032272e610d01888557847858186df37
Ellis Whitehead <kde@ellisw.net>
2003-03-29 20:54:34 +0000

    Don't output warning when we can't translate a key -- bug 56088
    svn path=/branches/KDE_3_1_BRANCH/kdelibs/; revision=216922

--- kdecore/kkeyserver_x11.cpp
+++ kdecore/kkeyserver_x11.cpp
@@ -353,9 +353,9 @@ bool Sym::initQt( int keyQt )
 	}
 
 	m_sym = 0;
-	if( symQt != Qt::Key_Shift && symQt != Qt::Key_Control && symQt != Qt::Key_Alt && 
+	if( symQt != Qt::Key_Shift && symQt != Qt::Key_Control && symQt != Qt::Key_Alt &&
 	    symQt != Qt::Key_Meta && symQt != Qt::Key_Direction_L && symQt != Qt::Key_Direction_R )
-		kdWarning(125) << "Sym::initQt( " << QString::number(keyQt,16) << " ): failed to convert key." << endl;
+		kdDebug(125) << "Sym::initQt( " << QString::number(keyQt,16) << " ): failed to convert key." << endl;
 	return false;
 }
 
