6d2a6a5bb3b48c5c16674efeaced43953b2fa2bf
Alexander Kellett <lypanov@kde.org>
2003-04-19 08:57:26 +0000

    partially done todo list for testing all of skel.cpp's code paths
    svn path=/trunk/kdelibs/; revision=220982

--- dcop/dcopidl2cpp/dcopidl_test.h
+++ dcop/dcopidl2cpp/dcopidl_test.h
@@ -1,17 +1,49 @@
 #ifndef TEST_H
 #define TEST_H
 
-// test for k_dcop
+// skel:
+// - non "CLASS" main elements
+// - namespacing stuff in skel
+// - no functions test
+// - ASYNC test
+// - does dispatcher cope with unnamed args?
+// - DCOPParent test - superclass?
+// - qual test, const?
+// writeType test
+
+// test for k_dcop 
+// NOTE - non hashing test, therefore use < 7 function
 class DefaultTest : public QObject, virtual public DCOPObject
 {
    Q_OBJECT
    K_DCOP
 public:
-   KBookmarkNotifier();
+   DefaultTest();
 k_dcop:
-   void addedBookmark( QString filename, QString url, QString text, QString address, QString icon );
-   void createdNewFolder( QString filename, QString text, QString address );
-   void updatedAccessMetadata( QString filename, QString url );
+   void noArgsTest();
+   void argsTest( QString filename, QString url );
+   void unNamedArgsTest( QString, QString );
+
+   void function1( QString filename, QString url, QString text, QString address, QString icon );
+   void function2( QString filename, QString text, QString address );
+};
+
+// test for useHashing
+class HashingTest : public QObject, virtual public DCOPObject
+{
+   Q_OBJECT
+   K_DCOP
+public:
+   HashingTest();
+k_dcop:
+   void function1( QString );
+   void function2( QString, QString );
+   void function3( QString, QString, QString );
+   void function4( QString, QString, QString, QString );
+   void function5( QString, QString, QString, QString, QString );
+   void function6( QString, QString, QString, QString, QString, QString );
+   void function7( QString, QString, QString, QString, QString, QString, QString );
+   void function8( QString, QString, QString, QString, QString, QString, QString, QString );
 };
 
 // test for k_dcop_signals
@@ -19,12 +51,11 @@ class SignalTest : virtual public DCOPObject
 {
    K_DCOP
 public:
-   KBookmarkNotifier(QCString objId = "KBookmarkNotifier") : DCOPObject(objId) {}
-
+   SignalTest(QCString objId = "KBookmarkNotifier") : DCOPObject(objId) {}
 k_dcop_signals:
-   void addedBookmark( QString filename, QString url, QString text, QString address, QString icon );
-   void createdNewFolder( QString filename, QString text, QString address );
-   void updatedAccessMetadata( QString filename, QString url );
+   void signal1( QString filename, QString url, QString text, QString address, QString icon );
+   void signal2( QString filename, QString text, QString address );
+   void signal3( QString filename, QString url );
 };
 
 #endif // end
