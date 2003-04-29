a3a889d3322ef173b8db5847d6296c693ffa4f35
Ravikiran Rajagopal <ravi@kde.org>
2003-04-29 17:53:01 +0000

    Since we don't have a separate KSpell library any longer, remove references to it. Also, make sure doxygen doesn't complain about multiple mainpage entries in kdeui.
    svn path=/trunk/kdelibs/; revision=222782

--- kdeui/kspell.h
+++ kdeui/kspell.h
@@ -519,30 +519,4 @@ private:
   KSpellPrivate *d;
 };
 
-/**
- * @libdoc Spelling Checker Library
- *
- * @ref KSpell offers easy access to International ISpell or ASpell
- *  (at the user's option) as well as a spell-checker GUI
- *  ("Add", "Replace", etc.).
- *
- * You can use @ref KSpell to
- *  automatically spell-check an ASCII file as well as to implement
- *  online spell-checking and to spell-check proprietary format and
- *  marked up (e.g. HTML, TeX) documents.  The relevant methods for
- *  these three procedures are @ref check(), @ref checkWord(), and
- *  @ref checkList(), respectively.
- *
- * @ref KSpellConfig holds configuration information about @ref KSpell as well
- *  as acting as an options-setting dialog.
- *
- * KSpell usually works asynchronously. If you do not need that, you should
- * simply use @ref KSpell::modalCheck(). It won't return until the
- * passed string is processed or the spell checking canceled.
- * During modal spell checking your GUI is still repainted, but the user may
- * only interact with the @ref KSpell dialog.
- *
- * @see KSpell, KSpellConfig
- **/
-
 #endif
