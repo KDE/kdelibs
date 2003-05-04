a4e37f1b6018b6a61f8d6d16f6ff3ea3d2f8c73a
Georg Robbers <georg.robbers@urz.uni-hd.de>
2003-05-04 23:26:47 +0000

    merge part's GUI into khtml's when part (e.g.ark) is embedded as a frame (backport rev. 1.798)
    svn path=/branches/KDE_3_1_BRANCH/kdelibs/; revision=223927

--- khtml/khtml_part.cpp
+++ khtml/khtml_part.cpp
@@ -4938,7 +4938,14 @@ void KHTMLPart::slotPartRemoved( KParts::Part *part )
 {
 //    kdDebug(6050) << "KHTMLPart::slotPartRemoved " << part << endl;
     if ( part == d->m_activeFrame )
+    {
         d->m_activeFrame = 0L;
+        if ( !part->inherits( "KHTMLPart" ) )
+        {
+            factory()->removeClient( part );
+            removeChildClient( part );
+        }
+    }
 }
 
 void KHTMLPart::slotActiveFrameChanged( KParts::Part *part )
@@ -4960,6 +4967,18 @@ void KHTMLPart::slotActiveFrameChanged( KParts::Part *part )
            frame->repaint();
         }
     }
+
+    if( d->m_activeFrame && !d->m_activeFrame->inherits( "KHTMLPart" ) )
+    {
+        factory()->removeClient( d->m_activeFrame );
+        removeChildClient( d->m_activeFrame );
+    }
+    if( part && !part->inherits( "KHTMLPart" ) )
+    {
+        factory()->addClient( part );
+        insertChildClient( part );
+    }
+
 
     d->m_activeFrame = part;
 
