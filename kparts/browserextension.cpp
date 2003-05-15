07d57e76bc8041d21cefb2fa047c33786062525c
Malcolm Hunter <malcolm.hunter@gmx.co.uk>
2003-05-15 18:21:01 +0000

    CVS_SILENT Typographical corrections
    svn path=/trunk/kdelibs/; revision=226019

--- kparts/browserextension.cpp
+++ kparts/browserextension.cpp
@@ -454,7 +454,7 @@ void BrowserExtension::pasteRequest()
     else if ( KURIFilter::self()->filterURI( filterData, "kuriikwsfilter" ) )
     {
         if ( KMessageBox::questionYesNo( m_part->widget(),
-		    i18n( "<qt>Do you want to search the internet for <b>%1</b>" ).arg( url ),
+		    i18n( "<qt>Do you want to search the Internet for <b>%1</b>" ).arg( url ),
 		    i18n( "Internet Search" ), i18n( "&Search" ),
 		    i18n( "&Cancel" ) ) == KMessageBox::Yes)
         slotOpenURLRequest( filterData.uri(), KParts::URLArgs() );
