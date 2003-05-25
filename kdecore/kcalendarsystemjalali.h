b90fb7f4c83cc7f644486dffe7a863e2cf3115b8
Carlos Moro <cfmoro@correo.uniovi.es>
2003-05-25 14:24:52 +0000

    License updated to LGPL
    svn path=/trunk/kdelibs/; revision=227768

--- kdecore/kcalendarsystemjalali.h
+++ kdecore/kcalendarsystemjalali.h
@@ -2,26 +2,26 @@
 #define KCALENDARSYSTEMJALALI_H
 
 
-/*****************************************
- * jconv.h
- * Copyright (C) 2002 Arash Bijanzadeh  and FarsiKDE Project <www.farsikde.org>
- * Contact: Arash Bijanzadeh <a.bijanzadeh@linuxiran.org>
- *
- * This program is part of FarsiKDE
- *
- * FarsiKDE is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * FarsiKDE is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with FarsiKDE; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+/*
+   Copyright (C) 2002-2003 Arash Bijanzadeh  and FarsiKDE Project <www.farsikde.org>
+   Contact: Arash Bijanzadeh <a.bijanzadeh@linuxiran.org>
+
+   This program is part of FarsiKDE
+
+   FarsiKDE is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   FarsiKDE is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
  */
 
 #include <qdatetime.h>
