8f0f22bed826f0a000505f4d759c815334aa555c
Dirk Mueller <mueller@kde.org>
2003-05-31 12:51:56 +0000

    fix compiler warning
    svn path=/branches/KDE_3_1_BRANCH/kdelibs/; revision=228965

--- kdecore/klibloader.h
+++ kdecore/klibloader.h
@@ -36,7 +36,7 @@ class KLibLoaderPrivate;
 class KLibraryPrivate;
 
 #define K_EXPORT_COMPONENT_FACTORY( libname, factory ) \
-    extern "C" { void *init_##libname() { return new factory; } };
+    extern "C" { void *init_##libname() { return new factory; } }
 
 /**
  * @short Represents a dynamically loaded library.
