5a81851fa826b53f954b89edad25d65f5c2ab5ce
Stephan Kulow <coolo@kde.org>
2003-06-09 11:49:04 +0000

    come on! A reference for a QColor return?
    svn path=/trunk/kdelibs/; revision=230557

--- kdeui/krootpixmap.h
+++ kdeui/krootpixmap.h
@@ -101,7 +101,7 @@ public:
     /** @since 3.2
      * @return the fade color.
      */
-    const QColor &color() const { return m_FadeColor; }
+    QColor color() const { return m_FadeColor; }
 
     /** @since 3.2
      * @return the color opacity.
