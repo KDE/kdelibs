0317db5490715070e4f4b6469083aea7c545ac6e
Benjamin Meyer <ben@meyerhome.net>
2003-06-11 20:18:37 +0000

    From the fixed place; drew from my heart all love, And added to the gall.  O Lear, Lear, Lear, Lear!
    svn path=/trunk/kdelibs/; revision=230977

--- kdecore/kautoconfig.cpp
+++ kdecore/kautoconfig.cpp
@@ -286,7 +286,8 @@ void KAutoConfig::resetSettings(){
       QVariant defaultValue = d->defaultValues[groupWidget];
       if(defaultValue != propertyMap->property(groupWidget)){
         propertyMap->setProperty(groupWidget, defaultValue);
-        d->changed = true;
+        emit void widgetModified();
+	d->changed = true;
       }
     }
   }
@@ -327,6 +328,7 @@ bool KAutoConfig::parseChildren(const QWidget *widget,
 	if(setting != defaultSetting){
 	  propertyMap->setProperty(childWidget, setting);
 	  valueChanged = true;
+	  if(trackChanges) emit void widgetModified();
         }
 
 	if(trackChanges && changedMap.find(childWidget->className()) != changedMap.end())
