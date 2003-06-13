e749f9bef2fc7c1a351c69fdea4466b17561596e
Alexander Kellett <lypanov@kde.org>
2003-06-13 07:25:21 +0000

    don't use specific names in Private template
    svn path=/trunk/kdelibs/; revision=231178

--- kio/bookmarks/kbookmarkbar.cc
+++ kio/bookmarks/kbookmarkbar.cc
@@ -36,23 +36,23 @@
 
 #include <qptrdict.h>
 
-template<class KBookmarkBar, class KBookmarkBarPrivate>
+template<class Instance, class PrivateData>
 class Private {
 public:
-    static KBookmarkBarPrivate* d( const KBookmarkBar* instance )
+    static PrivateData* d( const Instance* instance )
     {
         if ( !d_ptr ) {
-            d_ptr = new QPtrDict<KBookmarkBarPrivate>;
+            d_ptr = new QPtrDict<PrivateData>;
             qAddPostRoutine( cleanup_d_ptr );
         }
-        KBookmarkBarPrivate* ret = d_ptr->find( (void*) instance );
+        PrivateData* ret = d_ptr->find( (void*) instance );
         if ( ! ret ) {
-            ret = new KBookmarkBarPrivate;
+            ret = new PrivateData;
             d_ptr->replace( (void*) instance, ret );
         }
         return ret;
     }
-    static void delete_d( const KBookmarkBar* instance )
+    static void delete_d( const Instance* instance )
     {
         if ( d_ptr )
             d_ptr->remove( (void*) instance );
@@ -62,7 +62,7 @@ private:
     {
         delete d_ptr;
     }
-    static QPtrDict<KBookmarkBarPrivate>* d_ptr;
+    static QPtrDict<PrivateData>* d_ptr;
 };
 
 #ifndef enable_final_users_suck
