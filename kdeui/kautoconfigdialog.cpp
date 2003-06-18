f01b04a387e551bc927a21952c72740bc8537d42
Benjamin Meyer <ben@meyerhome.net>
2003-06-18 16:08:09 +0000

    Added license information
    svn path=/trunk/kdelibs/; revision=232171

--- kdeui/kautoconfigdialog.cpp
+++ kdeui/kautoconfigdialog.cpp
@@ -1,3 +1,22 @@
+/*
+ *  This file is part of the KDE libraries
+ *  Copyright (C) 2003 Benjamin C Meyer (ben@meyerhome.net)
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Library General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Library General Public License
+ *  along with this library; see the file COPYING.LIB.  If not, write to
+ *  the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ *  Boston, MA 02111-1307, USA.
+ */
 #include "kautoconfigdialog.h"
 #include "kautoconfigdialog.moc"
 #include "kautoconfig.h"
@@ -10,6 +29,7 @@
 
 QAsciiDict<QObject> KAutoConfigDialog::openDialogs;
 
+// This class is here purly so we don't break binary compatibility down the road.
 class KAutoConfigDialog::KAutoConfigDialogPrivate {
 
 public:
