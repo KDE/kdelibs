1ce8913af13545e2acaef62b9534e95cf655b5f3
Carsten Pfeiffer <pfeiffer@kde.org>
2003-06-22 03:56:33 +0000

    Audio and Video preview for the file dialog! CCMAIL: 60176-close@bugs.kde.org
    svn path=/trunk/kdelibs/; revision=232890

---
+++ interfaces/kmediaplayer/kfileaudiopreview/kfileaudiopreview.cpp
@@ -0,0 +1,120 @@
+#include "kfileaudiopreview.h"
+
+#include <qcheckbox.h>
+#include <qlayout.h>
+#include <qvgroupbox.h>
+
+#include <kconfig.h>
+#include <klibloader.h>
+#include <klocale.h>
+#include <kmediaplayer/player.h>
+#include <kmimetype.h>
+#include <kparts/componentfactory.h>
+
+#include <arts/kplayobjectfactory.h>
+
+#include <config-kfile.h>
+
+class KFileAudioPreviewFactory : public KLibFactory
+{
+protected:
+    virtual QObject *createObject( QObject *parent, const char *name,
+                           const char *, const QStringList & )
+    {
+        return new KFileAudioPreview( dynamic_cast<QWidget*>( parent ), name );
+    }
+};
+
+K_EXPORT_COMPONENT_FACTORY( kfileaudiopreview, KFileAudioPreviewFactory );
+
+
+///////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////
+
+
+class KFileAudioPreview::KFileAudioPreviewPrivate
+{
+public:
+    KFileAudioPreviewPrivate( QWidget *parent )
+    {
+        player = KParts::ComponentFactory::createInstanceFromQuery<KMediaPlayer::Player>( "KMediaPlayer/Player", QString::null, parent );
+    }
+
+    ~KFileAudioPreviewPrivate()
+    {
+        delete player;
+    }
+
+    KMediaPlayer::Player *player;
+};
+
+
+KFileAudioPreview::KFileAudioPreview( QWidget *parent, const char *name )
+    : KPreviewWidgetBase( parent, name )
+{
+    QStringList formats = KDE::PlayObjectFactory::mimeTypes();
+    setSupportedMimeTypes( formats );
+    // ###
+    QStringList::ConstIterator it = formats.begin();
+    for ( ; it != formats.end(); ++it )
+        m_supportedFormats.insert( *it, (void*) 1 );
+
+    QVGroupBox *box = new QVGroupBox( i18n("Media Player"), this );
+    QVBoxLayout *layout = new QVBoxLayout( this );
+    layout->addWidget( box );
+
+    m_autoPlay = new QCheckBox( i18n("Play &automatically"), box );
+    KConfigGroup config( KGlobal::config(), ConfigGroup );
+    m_autoPlay->setChecked( config.readBoolEntry( "Autoplay sounds", true ) );
+    connect( m_autoPlay, SIGNAL(toggled(bool)), SLOT(toggleAuto(bool)) );
+
+    d = new KFileAudioPreviewPrivate( box );
+    KMediaPlayer::View *view = d->player->view();
+    view->reparent( box, QPoint(0,0) );
+    view->setEnabled( false );
+
+    (void) new QWidget( box ); // spacer
+}
+
+KFileAudioPreview::~KFileAudioPreview()
+{
+    KConfigGroup config( KGlobal::config(), ConfigGroup );
+    config.writeEntry( "Autoplay sounds", m_autoPlay->isChecked() );
+
+    delete d;
+}
+
+void KFileAudioPreview::showPreview( const KURL &url )
+{
+    if ( url.isMalformed() )
+        return;
+
+    KMimeType::Ptr mt = KMimeType::findByURL( url );
+    bool supported = m_supportedFormats.find( mt->name() );
+    d->player->view()->setEnabled( supported );
+    if ( !supported )
+        return;
+
+    static_cast<KParts::ReadOnlyPart*>(d->player)->openURL( url );
+    if ( m_autoPlay->isChecked() )
+        d->player->play();
+}
+
+void KFileAudioPreview::clearPreview()
+{
+    d->player->stop();
+    d->player->closeURL();
+}
+
+void KFileAudioPreview::toggleAuto( bool on )
+{
+    if ( on && m_currentURL.isValid() && d->player->view()->isEnabled() )
+        d->player->play();
+    else
+        d->player->stop();
+}
+
+void KFileAudioPreview::virtual_hook( int, void* )
+{}
+
+#include "kfileaudiopreview.moc"
---
+++ interfaces/kmediaplayer/kfileaudiopreview/kfileaudiopreview.h
@@ -0,0 +1,64 @@
+/* This file is part of the KDE libraries
+   Copyright (C) 2003 Carsten Pfeiffer <pfeiffer@kde.org>
+
+   library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation, version 2.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
+#ifndef KFILEAUDIOPREVIEW_H
+#define KFILEAUDIOPREVIEW_H
+
+#include <qdict.h>
+
+#include <kurl.h>
+#include <kpreviewwidgetbase.h>
+
+class QCheckBox;
+class QPushButton;
+class QLabel;
+
+class KFileDialog;
+class KFileItem;
+
+/**
+ * Audio "preview" widget for the file dialog.
+ */
+class KFileAudioPreview : public KPreviewWidgetBase
+{
+    Q_OBJECT
+
+public:
+    KFileAudioPreview(QWidget *parent = 0, const char *name = 0 );
+    ~KFileAudioPreview();
+
+public slots:
+    virtual void showPreview(const KURL &url);
+    virtual void clearPreview();
+
+private slots:
+    void toggleAuto(bool);
+
+private:
+    QDict<void> m_supportedFormats;
+    KURL m_currentURL;
+    QCheckBox *m_autoPlay;
+
+protected:
+    virtual void virtual_hook( int id, void* data );
+private:
+    class KFileAudioPreviewPrivate;
+    KFileAudioPreviewPrivate *d;
+};
+
+#endif // KFILEAUDIOPREVIEW_H
