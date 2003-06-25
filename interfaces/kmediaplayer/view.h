e2df5ff38f5a4b5bd99787f80ab94292a6a38bf6
Laurent Montel <montel@kde.org>
2003-06-25 20:56:29 +0000

    Add const
    svn path=/trunk/kdelibs/; revision=233777

--- interfaces/kmediaplayer/view.h
+++ interfaces/kmediaplayer/view.h
@@ -6,17 +6,17 @@
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
 // THE AUTHOR(S) BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
 // AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 // CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-// 
+//
 // Except as contained in this notice, the name(s) of the author(s) shall not be
 // used in advertising or otherwise to promote the sale, use or other dealings
 // in this Software without prior written authorization from the author(s).
@@ -59,7 +59,7 @@ public:
 
 	/** Return the QWidget in which video is displayed.
 		May Return 0L if there is none. */
-	QWidget *videoWidget();
+	QWidget *videoWidget() const;
 
 public slots:
 	/** Set which buttons to display. @see Button. */
@@ -80,7 +80,7 @@ signals:
 
 protected:
 	/** The implementing view should set the widget in which
-		the video will be displayed. KMediaPlayer users may 
+		the video will be displayed. KMediaPlayer users may
 		reparent() it to somewhere else, for example.
 	*/
 	void setVideoWidget(QWidget *videoWidget);
