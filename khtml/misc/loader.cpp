323cd6ee083c3cd892918c92c78c6252c0593d37
Dirk Mueller <mueller@kde.org>
2003-07-03 18:32:49 +0000

    don't send username / password in Referrer (#60479) (backport revision 1.148)
    svn path=/branches/KDE_3_1_BRANCH/kdelibs/; revision=235305

--- khtml/misc/loader.cpp
+++ khtml/misc/loader.cpp
@@ -1060,6 +1060,8 @@ void Loader::servePendingRequests()
   if ( req->m_docLoader )  {
       KURL r = req->m_docLoader->doc()->URL();
       r.setRef(QString::null);
+      r.setUser(QString::null);
+      r.setPass(QString::null);
       if ( r.protocol().startsWith( "http" ) && r.path().isEmpty() )
           r.setPath( "/" );
       job->addMetaData("referrer", r.url());
