16c4882dba9981855d42803fc15c811bf6187948
Pascal Létourneau <pascal.letourneau@gmail.com>
2003-08-05 18:23:44 +0000

    s/stripWhiteSpace/simplifyWhiteSpace/
    CCMAIL: 62145-done@bugs.kde.org
    
    svn path=/trunk/kdelibs/; revision=241131

--- khtml/xml/dom_docimpl.cpp
+++ khtml/xml/dom_docimpl.cpp
@@ -616,7 +616,7 @@ void DocumentImpl::setTitle(DOMString _title)
     for (unsigned int i = 0; i < titleStr.length(); ++i)
         if (titleStr[i] < ' ')
             titleStr[i] = ' ';
-    titleStr = titleStr.stripWhiteSpace();
+    titleStr = titleStr.simplifyWhiteSpace();
     titleStr.compose();
     if ( view() && !view()->part()->parentPart() ) {
 	if (titleStr.isNull() || titleStr.isEmpty()) {
