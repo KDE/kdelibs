04188247cfa2aab7de8229a99da879745ae4afd2
Dirk Mueller <mueller@kde.org>
2003-08-19 01:01:13 +0000

    avoid style recalculations while we're deleting the tree. CCMAIL: 61918-done@bugs.kde.org
    svn path=/branches/KDE_3_1_BRANCH/kdelibs/; revision=243502

--- khtml/xml/dom_docimpl.cpp
+++ khtml/xml/dom_docimpl.cpp
@@ -1052,10 +1052,15 @@ void DocumentImpl::open(  )
     delete m_tokenizer;
     m_tokenizer = 0;
 
-    removeChildren();
-    QPtrListIterator<RegisteredEventListener> it(m_windowEventListeners);
-    for (; it.current();)
-        m_windowEventListeners.removeRef(it.current());
+    {
+        RenderObject* render = m_render;
+        m_render = 0; // indicate destruction mode
+        removeChildren();
+        QPtrListIterator<RegisteredEventListener> it(m_windowEventListeners);
+        for (; it.current();)
+            m_windowEventListeners.removeRef(it.current());
+        m_render = render;
+    }
 
     m_tokenizer = createTokenizer();
     m_decoderMibEnum = 0;
