b6efe62258968f456324abadd04b136d0c3ad73b
Mirko Boehm <mirko@kde.org>
2003-08-25 17:42:34 +0000

    - added the license in full text
    svn path=/trunk/kdelibs/; revision=244776

--- kio/kssl/sslio.cc
+++ kio/kssl/sslio.cc
@@ -1,17 +1,31 @@
-/* -*- C++ -*-
+/* This file is part of the KDE project
+ *
+ * Copyright (C) 2001 - 2003 Mirko Boehm <mirko@kde.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
 
-   This file implements an IO device using SSL connections.
+ * This file implements an IO device using SSL connections.
 
-   $ Author: Mirko Boehm $
-   $ Copyright: (C) 2003, Mirko Boehm $
-   $ Contact: mirko@kde.org
-         http://www.kde.org
-         http://www.hackerbuero.org $
-   $ License: LGPL with the following explicit clarification:
-         This code may be linked against any version of the Qt toolkit
-         from Troll Tech, Norway. $
+ * $ Author: Mirko Boehm $
+ * $ Copyright: (C) 2003, Mirko Boehm $
+ * $ Contact: mirko@kde.org
+ *            http://www.kde.org
+ *            http://www.hackerbuero.org $
 
-   $Id$
+ * $Id$
 */
 
 #include "sslio.h"
