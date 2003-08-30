1a9ce6f0fed450157d267cef67bc6622412ba545
George Staikos <staikos@kde.org>
2003-08-30 09:32:06 +0000

    Helper class to handle multiple timeouts of multiple objects keyed by IDs. Incomplete.
    svn path=/trunk/kdelibs/; revision=246741

---
+++ kio/misc/kwalletd/ktimeout.cpp
@@ -0,0 +1,80 @@
+/*
+   This file is part of the KDE libraries
+
+   Copyright (c) 2003 George Staikos <staikos@kde.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+
+*/
+
+#include "ktimeout.h"
+
+KTimeout::KTimeout(int size)
+: QObject(), _timers(size) {
+	_timers.setAutoDelete(true);
+}
+
+
+KTimeout::~KTimeout() {
+	_timers.clear();
+}
+
+
+void KTimeout::removeTimer(int id) {
+	QTimer *t = _timers.find(id);
+	if (t != 0L) {
+		delete t;
+		_timers.remove(id);
+	}
+}
+
+
+void KTimeout::addTimer(int id, int timeout) {
+	if (_timers.find(id) != 0L) {
+		return;
+	}
+
+	QTimer *t = new QTimer;
+	connect(t, SIGNAL(timeout()), this, SLOT(timeout()));
+	t->start(timeout);
+	_timers.insert(id, t);
+}
+
+
+void KTimeout::resetTimer(int id, int timeout) {
+	QTimer *t = _timers.find(id);
+	if (t) {
+		t->changeInterval(timeout);
+	}
+}
+
+
+void KTimeout::timeout() {
+	const QTimer *t = static_cast<const QTimer*>(sender());
+	if (t) {
+		QIntDictIterator<QTimer> it(_timers);
+		for (; it.current(); ++it) {
+			if (it.current() == t) {
+				emit timedOut(it.currentKey());
+				return;
+			}
+		}
+	}
+}
+
+
+#include "ktimeout.moc"
+
