0a73234d0220fa97ed48387cefd12427b7088c9b
Simon Hausmann <hausmann@kde.org>
2003-09-08 08:09:12 +0000

    - oops, QByteArray it's not a class but a typedef. use the real type then
    svn path=/trunk/kdelibs/; revision=249329

--- dcop/dcoptypes.h
+++ dcop/dcoptypes.h
@@ -66,7 +66,7 @@ class QDateTime; inline const char* dcopTypeName( const QDateTime& ) { return "Q
 class QBitArray; inline const char* dcopTypeName( const QBitArray& ) { return "QBitArray"; }
 class QKeySequence; inline const char* dcopTypeName( const QKeySequence& ) { return "QKeySequence"; }
 class QVariant; inline const char* dcopTypeName( const QVariant& ) { return "QVariant"; }
-class QByteArray; inline const char* dcopTypeName( const QByteArray& ) { return "QByteArray"; }
+template class QMemArray<char>; inline const char* dcopTypeName( const QMemArray<char>& ) { return "QByteArray"; }
 
 // And some KDE types
 class KURL; inline const char* dcopTypeName( const KURL& ) { return "KURL"; }
