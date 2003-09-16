ad4ce642555c0a4f8c762c0bedbfd8e054fe687a
David Faure <faure@kde.org>
2003-09-16 11:03:28 +0000

    This is faster, and it might fix the 'double-statusbar-bug' in Konqueror.
    svn path=/trunk/kdelibs/; revision=251477

--- kparts/statusbarextension.cpp
+++ kparts/statusbarextension.cpp
@@ -105,10 +105,12 @@ StatusBarExtension *StatusBarExtension::childObject( QObject *obj )
 
 bool StatusBarExtension::eventFilter(QObject * watched, QEvent* ev)
 {
-  KStatusBar * sb = statusBar();
-
-  if (!sb || !GUIActivateEvent::test( ev ) ||
+  if ( !GUIActivateEvent::test( ev ) ||
       !watched->inherits("KParts::ReadOnlyPart")  )
+      return QObject::eventFilter(watched, ev);
+
+  KStatusBar * sb = statusBar();
+  if ( !sb )
       return QObject::eventFilter(watched, ev);
 
   GUIActivateEvent *gae = static_cast<GUIActivateEvent*>(ev);
