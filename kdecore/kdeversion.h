2de5a112179be1b5d0884a53f151b5e2e2c5a8da
Stephan Kulow <coolo@kde.org>
2003-09-21 12:01:30 +0000

    happy recompilation
    svn path=/trunk/kdelibs/; revision=252822

--- kdecore/kdeversion.h
+++ kdecore/kdeversion.h
@@ -22,10 +22,10 @@
 #ifndef _KDE_VERSION_H_
 #define _KDE_VERSION_H_
 
-#define KDE_VERSION_STRING "3.1.91 (CVS >= 20030907)"
+#define KDE_VERSION_STRING "3.1.92 (alpha2, CVS >= 20030921)"
 #define KDE_VERSION_MAJOR 3
 #define KDE_VERSION_MINOR 1
-#define KDE_VERSION_RELEASE 91
+#define KDE_VERSION_RELEASE 92
 #define KDE_MAKE_VERSION( a,b,c ) (((a) << 16) | ((b) << 8) | (c))
 
 #define KDE_VERSION \
