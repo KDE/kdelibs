bbd6ef3e73637f751c042bed435907c92f9d397a
Andras Mantia <amantia@kde.org>
2003-09-26 21:24:55 +0000

    CVS_SILENT Typo...
    svn path=/trunk/kdelibs/; revision=254228

--- kdeui/kactionclasses.cpp
+++ kdeui/kactionclasses.cpp
@@ -7,7 +7,7 @@
               (C) 2001 Holger Freyther <freyther@kde.org>
               (C) 2002 Ellis Whitehead <ellis@kde.org>
               (C) 2002 Joseph Wenninger <jowenn@kde.org>
-              (C) 2002 Andras Mantia <amantia@kde.org>
+              (C) 2003 Andras Mantia <amantia@kde.org>
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Library General Public
