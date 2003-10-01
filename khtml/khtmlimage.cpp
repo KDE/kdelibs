8fc8470cd00d8ba833fa305d2f858333745f32bd
Simon Hausmann <hausmann@kde.org>
2003-10-01 16:38:27 +0000

    - bring back the context menu on images
    svn path=/trunk/kdelibs/; revision=255435

--- khtml/khtmlimage.cpp
+++ khtml/khtmlimage.cpp
@@ -97,7 +97,7 @@ KHTMLImage::KHTMLImage( QWidget *parentWidget, const char *widgetName,
 
     connect( m_khtml->browserExtension(), SIGNAL( popupMenu( KXMLGUIClient *, const QPoint &, const KURL &,
              const KParts::URLArgs &, KParts::BrowserExtension::PopupFlags, mode_t) ), m_ext, SIGNAL( popupMenu( KXMLGUIClient *, const QPoint &, const KURL &,
-             const KParts::URLArgs &, mode_t) ) );
+             const KParts::URLArgs &, KParts::BrowserExtension::PopupFlags, mode_t) ) );
 
     connect( m_khtml->browserExtension(), SIGNAL( enableAction( const char *, bool ) ),
              m_ext, SIGNAL( enableAction( const char *, bool ) ) );
