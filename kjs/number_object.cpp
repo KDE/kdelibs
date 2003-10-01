d150e756f99e01f39faa47f3fb5c40a373acce3b
George Staikos <staikos@kde.org>
2003-10-01 09:52:29 +0000

    Small optimization pointed out by Luciano Montanaro
    svn path=/trunk/kdelibs/; revision=255320

--- kjs/number_object.cpp
+++ kjs/number_object.cpp
@@ -189,7 +189,7 @@ Value NumberProtoFuncImp::call(ExecState *exec, Object &thisObj, const List &arg
       x = -x;
     }
 
-    if (x >= pow(10.0,21))
+    if (x >= 10e21)
       return String(s+UString::from(x));
 
     double n = floor(x*pow(10.0,f));
