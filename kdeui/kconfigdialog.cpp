ae1d49739236860b7343ca4a080d951ffda197a8
Waldo Bastian <bastian@kde.org>
2003-10-02 11:01:22 +0000

    * KAutoConfigDialog minus the KAutoConfig part. * Cleaned up API a little.
    svn path=/trunk/kdelibs/; revision=255636

---
+++ kdeui/kconfigdialog.cpp
@@ -0,0 +1,200 @@
+/*
+ *  This file is part of the KDE libraries
+ *  Copyright (C) 2003 Benjamin C Meyer (ben+kdelibs at meyerhome dot net)
+ *  Copyright (C) 2003 Waldo Bastian <bastian@kde.org>
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Library General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Library General Public License
+ *  along with this library; see the file COPYING.LIB.  If not, write to
+ *  the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ *  Boston, MA 02111-1307, USA.
+ */
+#include "kconfigdialog.h"
+#include "kconfigdialog.moc"
+
+#include <kconfigskeleton.h>
+#include <kconfigdialogmanager.h>
+#include <klocale.h>
+#include <kiconloader.h>
+#include <kdebug.h>
+#include <qlayout.h>
+#include <qvbox.h>
+
+QAsciiDict<QObject> KConfigDialog::openDialogs;
+
+// This class is here purly so we don't break binary compatibility down the road.
+class KConfigDialog::KConfigDialogPrivate
+{
+
+public:
+  KConfigDialogPrivate(KDialogBase::DialogType t) 
+  : track(true), shown(false), type(t), mgr(0) { }
+
+  bool track;
+  bool shown;
+  KDialogBase::DialogType type;
+  KConfigDialogManager *mgr;
+};
+
+KConfigDialog::KConfigDialog( QWidget *parent, const char *name,
+		  KConfigSkeleton *config,
+		  KDialogBase::DialogType dialogType,
+		  KDialogBase::ButtonCode dialogButtons,
+		  bool modal ) :
+    QObject(parent, name), d(new KConfigDialogPrivate(dialogType)) 
+{		  
+  openDialogs.insert(name, this);
+  kdialogbase = new KDialogBase( dialogType, i18n("Configure"), parent, name,
+  modal, Qt::WStyle_DialogBorder | Qt::WDestructiveClose, dialogButtons );
+
+  connect(kdialogbase, SIGNAL(destroyed()), this, SLOT(deleteLater()));
+  connect(kdialogbase, SIGNAL(okClicked()), this, SIGNAL(okClicked()));
+  connect(kdialogbase, SIGNAL(applyClicked()), this, SIGNAL(applyClicked()));
+  connect(kdialogbase, SIGNAL(defaultClicked()), this, SIGNAL(defaultClicked()));
+
+  d->mgr = new KConfigDialogManager(kdialogbase, config);
+
+  // TODO: Emit settingsChanged signal from slot to guarantee sequence
+  connect(d->mgr, SIGNAL(settingsChanged()), this, SIGNAL(settingsChanged()));
+  connect(d->mgr, SIGNAL(settingsChanged()), this, SLOT(settingsChangedSlot()));
+  connect(d->mgr, SIGNAL(widgetModified()), this, SLOT(settingModified()));
+
+  connect(kdialogbase, SIGNAL(okClicked()), this, SLOT(updateSettings()));
+  connect(kdialogbase, SIGNAL(okClicked()), d->mgr, SLOT(updateSettings()));
+  connect(kdialogbase, SIGNAL(applyClicked()), this, SLOT(updateSettings()));
+  connect(kdialogbase, SIGNAL(applyClicked()), d->mgr, SLOT(updateSettings()));
+  connect(kdialogbase, SIGNAL(defaultClicked()), this, SLOT(updateWidgetsDefault()));
+  connect(kdialogbase, SIGNAL(defaultClicked()), d->mgr, SLOT(updateWidgetsDefault()));
+
+  connect(kdialogbase, SIGNAL(defaultClicked()), this, SLOT(settingModified()));
+
+  kdialogbase->enableButton(KDialogBase::Apply, false);
+}
+
+KConfigDialog::~KConfigDialog()
+{
+  openDialogs.remove(name());
+  delete d;
+}
+
+void KConfigDialog::addPage(QWidget *page,
+                                const QString &itemName,
+                                const QString &pixmapName,
+                                const QString &header,
+                                bool manage)
+{
+  if(d->shown)
+  {
+    kdDebug(240) << "KConfigDialog::addPage, can not a page after the dialog has been shown.";
+    return;
+  }
+  switch(d->type)
+  {
+    case KDialogBase::TreeList:
+    case KDialogBase::IconList:
+    case KDialogBase::Tabbed: {
+      QVBox *frame = kdialogbase->addVBoxPage(itemName, header, SmallIcon(pixmapName, 32));
+      frame->setSpacing( 0 );
+      frame->setMargin( 0 );
+      page->reparent(((QWidget*)frame), 0, QPoint());
+    }
+    break;
+
+    case KDialogBase::Swallow: 
+    {
+      page->reparent(((QWidget*)kdialogbase), 0, QPoint());
+      kdialogbase->setMainWidget(page);
+    }
+    break;
+
+    case KDialogBase::Plain:
+    {
+      page->reparent(((QWidget*)kdialogbase), 0, QPoint());
+      QFrame *page = kdialogbase->plainPage();
+      QVBoxLayout *topLayout = new QVBoxLayout( page, 0, 0 );
+      page->reparent(((QWidget*)page), 0, QPoint());
+      topLayout->addWidget( page );
+      kdialogbase->setMainWidget(page);
+    }
+    break;
+
+    default:
+      kdDebug(240) << "KConfigDialog::addWidget" << " unknown type.";
+  }
+  if(manage)
+    d->mgr->addWidget(page);
+}
+
+KConfigDialog* KConfigDialog::exists(const char* name)
+{
+  return ((KConfigDialog*)openDialogs.find(name));
+}
+
+bool KConfigDialog::showDialog(const char* name)
+{
+  KConfigDialog *dialog = exists(name);
+  if(dialog)
+    dialog->show();
+  return (dialog != NULL);
+}
+
+void KConfigDialog::settingModified()
+{
+  if(d->track)
+  {
+    kdialogbase->enableButton(KDialogBase::Apply, d->mgr->hasChanged() || hasChanged());
+    kdialogbase->enableButton(KDialogBase::Default, !(d->mgr->isDefault() && isDefault()));
+  }
+}
+
+void KConfigDialog::settingsChangedSlot()
+{
+  // Update the buttons
+  settingModified();
+  emit (settingsChanged(name()));
+}
+
+void KConfigDialog::setCaption(const QString &caption)
+{
+  kdialogbase->setCaption(caption);
+}
+
+void KConfigDialog::show(bool track)
+{
+  if(!d->shown)
+  {
+    updateWidgets();
+    d->mgr->updateWidgets();
+    kdialogbase->enableButton(KDialogBase::Apply, d->mgr->hasChanged() || hasChanged());
+    kdialogbase->enableButton(KDialogBase::Default, !(d->mgr->isDefault() && isDefault()));
+    d->shown = true;
+    if(!track)
+    {
+      kdialogbase->enableButton(KDialogBase::Apply, true);
+      kdialogbase->enableButton(KDialogBase::Default, true);
+    }
+  }
+  d->track = track;
+  kdialogbase->show();
+}
+
+void KConfigDialog::updateSettings()
+{
+}
+
+void KConfigDialog::updateWidgets()
+{
+}
+
+void KConfigDialog::updateWidgetsDefault()
+{
+}
---
+++ kdeui/kconfigdialog.h
@@ -0,0 +1,269 @@
+/*
+ *  This file is part of the KDE libraries
+ *  Copyright (C) 2003 Benjamin C Meyer (ben+kdelibs at meyerhome dot net)
+ *  Copyright (C) 2003 Waldo Bastian <bastian@kde.org>
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Library General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Library General Public License
+ *  along with this library; see the file COPYING.LIB.  If not, write to
+ *  the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ *  Boston, MA 02111-1307, USA.
+ */
+#ifndef KCONFIGDIALOG_H
+#define KCONFIGDIALOG_H
+
+class KConfig;
+class KConfigSkeleton;
+#include <kdialogbase.h>
+#include <qasciidict.h>
+
+/**
+ * @author Waldo Bastian <bastian@kde.org>
+ *
+ * The KConfigDialog class provides an easy and uniform means of displaying
+ * a settings dialog using KDialogBase, KConfigDialogManager and a 
+ * KConfigSkeleton derived settings class.
+ *
+ * KConfigDialog handles the enabling and disabling of buttons, creation
+ * of the dialog, and deletion of the widgets.  Because of 
+ * KConfigDialogManager, this class also manages: restoring 
+ * the settings, reseting them to the default values, and saving them.
+ *
+ * Here is an example usage of KConfigDialog:
+ *
+ * \code
+ * void KCoolApp::showSettings(){
+ *   if(KConfigDialog::showDialog("settings"))
+ *     return;
+ *   KConfigDialog *dialog = new KConfigDialog(this, "settings", KDialogBase::IconList, MySettings::self());
+ *   dialog->addPage(new General(0, "General"), i18n("General") );
+ *   dialog->addPage(new Appearance(0, "Style"), i18n("Appearance") );
+ *   connect(dialog, SIGNAL(settingsChanged()), mainWidget, SLOT(loadSettings()));
+ *   connect(dialog, SIGNAL(settingsChanged()), this, SLOT(loadSettings()));
+ *   dialog->show();
+ * }
+ * \endcode
+ *
+ * Other then the above code each class that has settings in the dialog should
+ * have a loadSettings() type slot to read settings and perform any
+ * necessary changes.
+ *
+ * @see KConfigSkeleton
+ * @since 3.2
+ */
+class KConfigDialog : public QObject {
+Q_OBJECT
+
+signals:
+  /**
+   * One or more of the settings have been permanently changed such as if
+   * the user clicked on the Apply or Ok button.
+   */
+  void settingsChanged();
+
+  /**
+   * One or more of the settings have been permanently changed such as if
+   * the user clicked on the Apply or Ok button.
+   * This signal is useful when using kautoconfigdialog to configure
+   * items in a list.  When emits the main class would then know what
+   * item in the list was actually changed.
+   * @param dialogName the name of the dialog.
+   */
+  void settingsChanged(const char *dialogName);
+
+  /**
+   * Helper signal.
+   */
+  void okClicked();
+  /**
+   * Helper signal.
+   */
+  void applyClicked();
+  /**
+   * Helper signal.
+   */
+  void defaultClicked();
+
+public:
+  /**
+   * @param parent - The parent object of this object.  Even though the class
+   * deletes itself the parent should be set so the dialog can be centered
+   * with the application on the screen.
+   *
+   * @param name - The name of this object.  The name is used in determining if
+   * there can be more then one dialog at a time.  Use names such as:
+   * "Font Settings" or "Color Settings" and not just "Settings" in
+   * applications where there are more then one dialog.
+   *
+   * @param dialogType - Type used in creating the dialog.  @see KDialogBase
+   *
+   * @param config - Config object containing settings.
+   *
+   * @param dialogButtons - Buttons that should show up on the dialog.
+   *
+   * @param modal - Whether the dialog should be modal. To prevent more than one
+   * non-modal settings dialog from showing the static function showDialog() can be
+   * used in determining if the settings dialog already exists before creating
+   * a new KConfigDialog object.
+   */
+  KConfigDialog( QWidget *parent, const char *name,
+                 KConfigSkeleton *config,
+		 KDialogBase::DialogType dialogType = KDialogBase::IconList,
+		 KDialogBase::ButtonCode dialogButtons =
+		    (KDialogBase::ButtonCode) (KDialogBase::Default | KDialogBase::Ok | KDialogBase::Apply | KDialogBase::Cancel | KDialogBase::Help),
+		 bool modal=false );
+
+  /**
+   * Deconstructor, removes name from the list of open dialogs.
+   * Deletes private class.
+   * @see exists()
+   */
+  ~KConfigDialog();
+
+  /**
+   * Adds page to the dialog and to KConfigDialogManager.  When an 
+   * application is done adding pages show() should be called to 
+   * display the dialog.
+   * Note that after you call show() you can not add any more pages
+   * to the dialog.
+   * @param page - Pointer to the page that is to be added to the dialog.
+   * This object is reparented.
+   * @param itemName - Name of the page.
+   * @param pixmapName - Name of the pixmap that should be used if needed.
+   * @param header - Header text use in the list modes. Ignored in Tabbed
+   *        mode. If empty, the itemName text is used when needed.
+   * @param manage - Whether KAutoConfig should manage the page or not.
+   */
+  void addPage( QWidget *page, const QString &itemName,
+		                  const QString &pixmapName,
+				  const QString &header=QString::null,
+				  bool manage=true);
+
+  /**
+   * See if a dialog with the name 'name' already exists.
+   * @see showDialog()
+   * @param name - Dialog name to look for.
+   * @return Pointer to widget or NULL if it does not exist.
+   */
+  static KConfigDialog* exists( const char* name );
+
+  /**
+   * Attempts to show the dialog with the name 'name'.
+   * @see exists()
+   * @param name - The name of the dialog to show.
+   * @return True if the dialog 'name' exists and was shown.
+   */
+  static bool showDialog( const char* name );
+
+  /**
+   * Shows the dialog.  This has to be called after all of the pages
+   * have been added.
+   * @param track - Track all of the widgets for any changes. If false all
+   * of the buttons are enabled.  This can be set to false as a temporary
+   * measure, but applications in general should leave it on.  This
+   * parameter only matters the first time this function is called.
+   * @see hide()
+   */
+  virtual void show( bool track=true );
+
+  /**
+   * Hides the dialog.  An application shouldn't normally need to use
+   * this function.
+   * @see show()
+   */
+  void hide() const { kdialogbase->hide(); };
+
+  /**
+   * KConfigDialog automatically gives the dialog a common KDE
+   * configuration caption. This function is provided for dialogs other
+   * then the normal application configuration such as plugin configurations.
+   * Do not include the applications name as it will automaticly be added
+   * via the KDE rules. KDialog::setCaption()
+   * @param caption - The new name for the dialog's caption such as:
+   * "Configure Audio Plugin"
+   **/
+  virtual void setCaption( const QString &caption );
+
+protected slots:
+  /**
+   * Update the settings from the dialog.
+   * Virtual function for custom additions.
+   *
+   * Example use: User clicks Ok or Apply button in a configure dialog.
+   */ 
+  virtual void updateSettings();
+
+  /**
+   * Update the dialog based on the settings.
+   * Virtual function for custom additions.
+   *
+   * Example use: Initialisation of dialog.
+   * Example use: User clicks Reset button in a configure dialog.
+   */
+  virtual void updateWidgets();
+
+  /**
+   * Update the dialog based on the default settings.
+   * Virtual function for custom additions.
+   *
+   * Example use: User clicks Defaults button in a configure dialog.
+   */
+  virtual void updateWidgetsDefault();
+
+protected:
+
+  /**
+   * Returns whether the current state of the dialog is
+   * different from the current configutation.
+   * Virtual function for custom additions.
+   */
+  virtual bool hasChanged() { return false; }
+
+  /**
+   * Returns whether the current state of the dialog is
+   * the same as the default configuration.
+   */
+  virtual bool isDefault() { return true; }
+  
+
+protected slots:
+  /**
+   * Some setting was modified, updates the Apply and Default buttons.
+   */
+  virtual void settingModified();
+
+  /**
+   * Some setting was changed. Emit the signal with the dialogs name
+   */
+  virtual void settingsChangedSlot();
+
+protected:
+  /**
+   * Pointer to the KDialogBase object.
+   */
+  KDialogBase *kdialogbase;
+
+private:
+  /**
+   * The list of existing dialogs.
+   */
+  static QAsciiDict<QObject> openDialogs;
+
+  class KConfigDialogPrivate;
+  /**
+   * Private class.
+   */
+  KConfigDialogPrivate *d;
+};
+
+#endif //KCONFIGDIALOG_H
+
