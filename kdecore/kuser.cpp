bdae0238c22c2aebbdf191df871ca154197ff01a
Laurent Montel <montel@kde.org>
2003-10-19 09:37:05 +0000

    remove $id
    svn path=/trunk/kdelibs/; revision=260085

--- kdecore/kuser.cpp
+++ kdecore/kuser.cpp
@@ -2,7 +2,6 @@
  *  KUser - represent a user/account
  *  Copyright (C) 2002 Tim Jansen <tim@tjansen.de>
  *
- *  $Id$
  *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Library General Public
