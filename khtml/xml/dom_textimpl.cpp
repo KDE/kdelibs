efe83d65270a8fa24573f5ddd05fcf6ecb8c6036
Dirk Mueller <mueller@kde.org>
2003-10-21 15:03:42 +0000

    stop gap fix for mailman interface.
    svn path=/trunk/kdelibs/; revision=260773

--- khtml/xml/dom_textimpl.cpp
+++ khtml/xml/dom_textimpl.cpp
@@ -350,7 +350,7 @@ void TextImpl::attach()
     assert(parentNode() && parentNode()->isElementNode());
 
     ElementImpl* element = static_cast<ElementImpl*>(parentNode());
-    if (!m_render && element->renderer()) {
+    if (element->renderer() && !element->renderer()->isWidget()) {
         khtml::RenderStyle* _style = element->renderer()->style();
         m_render = new (getDocument()->renderArena()) RenderText(this, str);
         m_render->setStyle(_style);
