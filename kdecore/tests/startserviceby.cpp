f94dfb857c517f6bd83681f46670bec9a1686bfc
David Faure <faure@kde.org>
2003-10-26 19:40:42 +0000

    Prove to Danimo that kapp->startServiceByDesktopName("kaddressbook") works.
    svn path=/trunk/kdelibs/; revision=262259

---
+++ kdecore/tests/startserviceby.cpp
@@ -0,0 +1,41 @@
+/* This file is part of the KDE libraries
+    Copyright (c) 1999 Waldo Bastian <bastian@kde.org>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License version 2 as published by the Free Software Foundation.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+    Boston, MA 02111-1307, USA.
+*/
+
+#include "kapplication.h"
+#include <stdio.h>
+
+#include <kcmdlineargs.h>
+#include <kaboutdata.h>
+#include <kglobalsettings.h>
+#include <kdebug.h>
+
+int
+main(int argc, char *argv[])
+{
+   KAboutData about("kapptest", "kapptest", "version");
+   KCmdLineArgs::init(argc, argv, &about);
+
+   KApplication a;
+
+   QString error;
+   QCString dcopService;
+   int pid;
+   a.startServiceByDesktopName( "kaddressbook", QString::null, &error, &dcopService, &pid );
+   kdDebug() << "Started. error=" << error << " dcopService=" << dcopService << " pid=" << pid << endl;
+   a.exec();
+}
