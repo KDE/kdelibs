7ecf52d68efbc78cefeccdba90c3cbc54e0f0b92
David Faure <faure@kde.org>
2003-11-11 23:20:29 +0000

    Extend test prog to pass url on command line
    svn path=/trunk/kdelibs/; revision=266472

--- kio/tests/kpropsdlgtest.cpp
+++ kio/tests/kpropsdlgtest.cpp
@@ -4,18 +4,30 @@
 #include <kpropertiesdialog.h>
 #include <kcmdlineargs.h>
 
+
+static KCmdLineOptions options[] =
+{
+    { "+url", "the path or url to the file/dir for which to show properties", 0 },
+    KCmdLineLastOption
+};
+
 int main ( int argc, char** argv )
 {
     KAboutData aboutData("kpropertiesdialogtest", "KIO Properties Dialog Test", "1.0");
     KCmdLineArgs::init(argc, argv, &aboutData);
+    KCmdLineArgs::addCmdLineOptions( options );
+
     KApplication app;
+
+    KCmdLineArgs *args = KCmdLineArgs::parsedArgs();
+    if ( args->count() < 1 )
+        KCmdLineArgs::usage();
+    KURL u = args->url( 0 );
 
     // This is the test for the KPropertiesDialog constructor that is now
     // documented to NOT work. Passing only a URL means a KIO::stat will happen,
     // and asking for the dialog to be modal too creates problems.
     // (A non-modal, URL-only dialog is the one kicker uses for app buttons, no problem there)
-    KURL u;
-    u.setPath( "/tmp" );
     KPropertiesDialog* dlg = new KPropertiesDialog( u, 0, 0, true /*modal*/, false /*we do exec ourselves*/ );
     dlg->exec();
 
