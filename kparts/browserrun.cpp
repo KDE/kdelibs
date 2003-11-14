d94c885e7bdb03f507ff6290b27b18dc9d65812c
David Faure <faure@kde.org>
2003-11-14 12:58:16 +0000

    I'm not fond of hardcoded mimetype lists. But I don't see another solution here.... This fixes the msgbox on the first link from http://www.w3.org/Style/CSS/Test/CSS3/Selectors/current/xhtml/full/flat/index.html It doesn't fix the XML parse error on that page though :)
    svn path=/trunk/kdelibs/; revision=267038

--- kparts/browserrun.cpp
+++ kparts/browserrun.cpp
@@ -314,6 +314,8 @@ BrowserRun::AskSaveResult BrowserRun::askEmbedOrSave( const KURL & url, const QS
     // e.g. postscript is different, because takes longer to read, so
     // it's more likely that the user might want to save it.
     if ( mime->is( "text/html" ) ||
+         mime->is( "text/xml" ) ||
+         mime->is( "application/xhtml+xml" ) ||  // this one should use X-KDE-IsAlso I guess
          mime->is( "inode/directory" ) ||
          mimeType.startsWith( "image" ) )
         return Open;
