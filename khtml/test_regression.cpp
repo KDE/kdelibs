5425dae0652819106d08432000209089480ce11f
Waldo Bastian <bastian@kde.org>
2003-11-19 11:32:03 +0000

    Use standard copyright notation.
    svn path=/trunk/kdelibs/; revision=268150

--- khtml/test_regression.cpp
+++ khtml/test_regression.cpp
@@ -1,8 +1,8 @@
 /**
  * This file is part of the KDE project
  *
- * (C) 2001,2003 Peter Kelly (pmk@post.com)
- * (C) 2003 Stephan Kulow (coolo@kde.org)
+ * Copyright (C) 2001,2003 Peter Kelly (pmk@post.com)
+ * Copyright (C) 2003 Stephan Kulow (coolo@kde.org)
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
--- khtml/test_regression_fontoverload.cpp
+++ khtml/test_regression_fontoverload.cpp
@@ -1,7 +1,7 @@
 /**
  * This file is part of the KDE project
  *
- * (C) 2003 Stephan Kulow (coolo@kde.org)
+ * Copyright (C) 2003 Stephan Kulow (coolo@kde.org)
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
