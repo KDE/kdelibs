9a73ef7c41950c455ba7c611b9ef403a47098a3e
Waldo Bastian <bastian@kde.org>
2003-11-19 11:33:54 +0000

    Use standard copyright notation
    svn path=/trunk/kdelibs/; revision=268152

--- khtml/test_regression.h
+++ khtml/test_regression.h
@@ -1,7 +1,7 @@
 /**
  * This file is part of the KDE project
  *
- * (C) 2001,2003 Peter Kelly (pmk@post.com)
+ * Copyright (C) 2001,2003 Peter Kelly (pmk@post.com)
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
