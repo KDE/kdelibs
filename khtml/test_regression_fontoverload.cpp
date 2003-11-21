59828028ee65a79d7b87de1394e4b7f55de21353
Stephan Kulow <coolo@kde.org>
2003-11-21 15:07:59 +0000

    I tried to improve the font fake engine, but failed so far.
    svn path=/trunk/kdelibs/; revision=268614

--- khtml/test_regression_fontoverload.cpp
+++ khtml/test_regression_fontoverload.cpp
@@ -27,24 +27,211 @@
 #include "khtml_settings.h"
 #include <qwidget.h>
 
+#if 0
+class QFakeFontEngine : public QFontEngineXLFD
+{
+public:
+    QFakeFontEngine( XFontStruct *fs, const char *name, int size );
+    ~QFakeFontEngine();
+
+    Error stringToCMap( const QChar *str,  int len, glyph_t *glyphs, advance_t *advances, int *nglyphs, bool mirrored ) const;
+
+    virtual glyph_metrics_t boundingBox( const glyph_t *glyphs,
+				    const advance_t *advances, const qoffset_t *offsets, int numGlyphs );
+    glyph_metrics_t boundingBox( glyph_t glyph );
+
+    int ascent() const;
+    int descent() const;
+    int leading() const;
+    int maxCharWidth() const;
+    int minLeftBearing() const { return 0; }
+    int minRightBearing() const { return 0; }
+
+#ifdef Q_WS_X11
+    int cmap() const;
+#endif
+    const char *name() const;
+
+    bool canRender( const QChar *string,  int len );
+
+    Type type() const;
+    inline int size() const { return _size; }
+
+private:
+    int _size;
+};
+
+QFakeFontEngine::QFakeFontEngine( XFontStruct *fs, const char *name, int size )
+    : QFontEngineXLFD( fs,  name,  0), _size( size )
+{
+
+}
+
+QFakeFontEngine::~QFakeFontEngine()
+{
+}
+
+QFontEngine::Error QFakeFontEngine::stringToCMap( const QChar *, int len, glyph_t *glyphs, advance_t *advances, int *nglyphs, bool ) const
+{
+    if ( *nglyphs < len ) {
+	*nglyphs = len;
+	return OutOfMemory;
+    }
+
+    memset( glyphs, 0, len * sizeof( glyph_t ) );
+    *nglyphs = len;
+
+    if ( advances ) {
+	for ( int i = 0; i < len; i++ )
+	    *(advances++) = _size;
+    }
+    return NoError;
+}
+
+glyph_metrics_t QFakeFontEngine::boundingBox( const glyph_t *, const advance_t *, const qoffset_t *, int numGlyphs )
+{
+    glyph_metrics_t overall;
+    overall.x = overall.y = 0;
+    overall.width = _size*numGlyphs;
+    overall.height = _size;
+    overall.xoff = overall.width;
+    overall.yoff = 0;
+    return overall;
+}
+
+glyph_metrics_t QFakeFontEngine::boundingBox( glyph_t )
+{
+    return glyph_metrics_t( 0, _size, _size, _size, _size, 0 );
+}
+
+
+
+int QFakeFontEngine::ascent() const
+{
+    return _size;
+}
+
+int QFakeFontEngine::descent() const
+{
+    return 0;
+}
+
+int QFakeFontEngine::leading() const
+{
+    int l = qRound( _size * 0.15 );
+    return (l > 0) ? l : 1;
+}
+
+int QFakeFontEngine::maxCharWidth() const
+{
+    return _size;
+}
+
+int QFakeFontEngine::cmap() const
+{
+    return -1;
+}
+
+const char *QFakeFontEngine::name() const
+{
+    return "null";
+}
+
+bool QFakeFontEngine::canRender( const QChar *, int )
+{
+    return TRUE;
+}
+
+QFontEngine::Type QFakeFontEngine::type() const
+{
+    return Box;
+}
+
+
 QFontEngine *
 QFontDatabase::findFont( QFont::Script script, const QFontPrivate *fp,
-			 const QFontDef &request, int force_encoding_id )
+			 const QFontDef &request, int )
 {
-    QFontEngine *fe = new QFontEngineBox( request.pixelSize );
+    QCString xlfd = "-";
+    xlfd += "*";
+    xlfd += "-";
+    xlfd += "*"; // request.family.isEmpty() ? "*" : request.family.latin1();
+
+    xlfd += "-";
+    xlfd += "*";
+    xlfd += "-";
+    xlfd += ( request.italic ? "i" : "r" );
+
+    xlfd += "-";
+    xlfd += "*";
+    // ### handle add-style
+    xlfd += "-*-";
+
+    xlfd += QString::number( request.pixelSize ).latin1();
+    xlfd += "-";
+    xlfd += "*";
+    xlfd += "-";
+    xlfd += "*";
+    xlfd += "-";
+    xlfd += "*";
+    xlfd += "-";
+
+    // ### handle cell spaced fonts
+    xlfd += "*";
+    xlfd += "-";
+    xlfd += "*";
+    xlfd += "-";
+    xlfd += "*-*";
+
+    qDebug( "findFont '%s' '%s' %d xlfd: '%s' italic=", request.addStyle.latin1(), request.family.latin1(), request.pixelSize, xlfd.data() );
+    QFontEngine *fe = 0;
+
+    XFontStruct *xfs;
+    if (! (xfs = XLoadQueryFont(QPaintDevice::x11AppDisplay(), xlfd.data() ) ) ) {
+        qDebug( "no font found" );
+    }
+
+    fe = new QFakeFontEngine( xfs, xlfd.data(),request.pixelSize );
+
+    // fe->setScale( scale );
+
     QFontCache::Key key( request, script, fp->screen );
     QFontCache::instance->insertEngine( key, fe );
     return fe;
 }
 
-bool QFontDatabase::isBitmapScalable( const QString &family,
-				      const QString &style) const
+#else
+
+QFontEngine *
+QFontDatabase::findFont( QFont::Script script, const QFontPrivate *fp,
+			 const QFontDef &request, int )
+{
+    qDebug( "findFont script=%d family='%s' pixelsize=%d italic=%d fixed=%d weight=%d stretch=%d styleHint=%d" ,
+            script,
+            request.family.latin1(),
+            request.pixelSize,
+            request.italic,
+            request.fixedPitch,
+            request.weight,
+            request.stretch,
+            request.styleHint
+            );
+
+    QFontEngine *fe = new QFontEngineBox( request.pixelSize );
+    QFontCache::Key key( request, script, fp->screen );
+    QFontCache::instance->insertEngine( key, fe );
+    return fe;
+}
+#endif
+
+bool QFontDatabase::isBitmapScalable( const QString &,
+				      const QString &) const
 {
     return true;
 }
 
-bool  QFontDatabase::isSmoothlyScalable( const QString &family,
-                                         const QString &style) const
+bool  QFontDatabase::isSmoothlyScalable( const QString &,
+                                         const QString &) const
 {
     return true;
 }
@@ -53,7 +240,7 @@ const QString &KHTMLSettings::availableFamilies()
 {
     if ( !avFamilies ) {
         avFamilies = new QString;
-        *avFamilies = ",Adobe Courier,Adobe Helvetica,Adobe New Century Schoolbook,Adobe Times,Adobe Utopia,Century Schoolbook L,Charter,Clean,Console,Courier,Courier 10 Pitch,Cursor,DEC Terminal,Dingbats,ETL Fixed,Fixed,Goha Tibeb Zemen,Gothic,Helvetica,Luxi Mono,Luxi Sans,Luxi Serif,Mincho,New Century Schoolbook,Newspaper,Nil,Proof,Schumacher Clean,Song Ti,Sony Fixed,Standard Symbols L,Symbol,Terminal,Times,Utopia,";
+        *avFamilies = ",Courier,Fixed,Helvetica,Times,Utopia,Times New Roman,Courier New,Verdana,";
     }
 
   return *avFamilies;
