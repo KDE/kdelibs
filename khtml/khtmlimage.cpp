bf8fc384eb0925b04fe5cc8bb8de52460de20e55
David Faure <faure@kde.org>
2003-12-21 17:37:50 +0000

    Provide init_khtmlimagepart in the part, not in libkhtml.so
    svn path=/trunk/kdelibs/; revision=274220

--- khtml/khtmlimage.cpp
+++ khtml/khtmlimage.cpp
@@ -34,7 +34,7 @@
 #include <kmimetype.h>
 #include <klocale.h>
 
-K_EXPORT_COMPONENT_FACTORY( khtmlimagepart, KHTMLImageFactory )
+K_EXPORT_COMPONENT_FACTORY( khtmlimagefactory /*NOT the part name, see Makefile.am*/, KHTMLImageFactory )
 
 KInstance *KHTMLImageFactory::s_instance = 0;
 
