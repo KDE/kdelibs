c91bd75a544395b675ae7da134a9be38674f55d5
David Faure <faure@kde.org>
2004-01-11 16:19:52 +0000

    Fixed out-of-bounds read. CCMAIL: bastian@kde.org
    svn path=/trunk/kdelibs/; revision=278764

--- kio/kio/kprotocolmanager.cpp
+++ kio/kio/kprotocolmanager.cpp
@@ -298,6 +298,8 @@ static bool revmatch(const char *host, const char *nplist)
     {
       if ( nptr==nplist || nptr[-1]==',' || nptr[-1]==' ')
         return true;
+      if ( hptr == host ) // e.g. revmatch("bugs.kde.org","mybugs.kde.org")
+        return false;
 
       hptr--;
       nptr--;
