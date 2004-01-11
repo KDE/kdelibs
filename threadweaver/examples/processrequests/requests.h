31f3f7c39433044cbad6acf047b7fd370167c0c6
Mirko Boehm <mirko@kde.org>
2004-01-11 22:35:37 +0000

    - added the ThreadWeaver library to kdenonbeta - ThreadWeaver eases the use of threading in event oriented programs,   providing a thread pool, job objects, automatic job scheduling and   signals and slots to notify on job completion - a couple of examples are shipped, which are the best documentation yet   available --Mirko.
    svn path=/trunk/kdenonbeta/threadweaver/; revision=278909

---
+++ threadweaver/examples/processrequests/requests.h
@@ -0,0 +1,57 @@
+#ifndef REQUESTS_H
+#define REQUESTS_H
+
+#include <qobject.h>
+#include <qpushbutton.h>
+
+#include <weaver.h>
+#include <weaverlogger.h>
+
+#include "processrequest.h"
+
+class SPRExample : public ThreadWeaver::Job
+{
+    Q_OBJECT
+public:
+    SPRExample (QObject *parent = 0, const char* name = 0);
+    ~SPRExample () {}
+protected:
+    void run ();
+};
+
+
+class APRExample : public ThreadWeaver::Job
+{
+    Q_OBJECT
+public:
+    APRExample (QObject *parent = 0, const char* name = 0);
+    ~APRExample () {}
+protected:
+    void run ();
+};
+
+class Main : public ProcessRequestBase
+{
+    Q_OBJECT
+
+public:
+    Main ();
+    ~Main () {}
+    void slotStartSPR ();
+    void slotStartAPR ();
+    void slotFinishAPR ();
+
+protected:
+    SPRExample *spr;
+    APRExample *apr;
+    ThreadWeaver::Weaver weaver;
+    ThreadWeaver::WeaverThreadLogger logger;
+
+protected slots:
+    void processSPR ();
+    void processAPR ();
+    void sprDone ();
+    void aprDone ();
+};	    
+	    
+#endif // REQUESTS.H
