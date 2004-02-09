cf7225853dc34e1aed38e4392bcab52692bcf032
Harri Porten <porten@kde.org>
2004-02-09 22:15:41 +0000

    always include the corresponding file .h first unless you want to influence the public API. Works around a conflict aCC sees between struct stat and KIO::stat().
    svn path=/branches/KDE_3_2_BRANCH/kdelibs/; revision=286624

--- kio/kio/job.cpp
+++ kio/kio/job.cpp
@@ -19,6 +19,8 @@
     Boston, MA 02111-1307, USA.
 */
 
+#include "kio/job.h"
+
 #include <config.h>
 
 #include <sys/types.h>
@@ -52,7 +54,6 @@ extern "C" {
 #include <errno.h>
 
 #include "slave.h"
-#include "kio/job.h"
 #include "scheduler.h"
 #include "kdirwatch.h"
 #include "kmimemagic.h"
