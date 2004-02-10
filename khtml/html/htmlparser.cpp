17388921bfa8e3f77fc6a48c97942e337e7dea23
Dirk Mueller <mueller@kde.org>
2004-02-10 14:04:14 +0000

    CCMAIL: 74329-done@bugs.kde.org (backport revision 1.346)
    svn path=/branches/KDE_3_2_BRANCH/kdelibs/; revision=286787

--- khtml/html/htmlparser.cpp
+++ khtml/html/htmlparser.cpp
@@ -442,8 +442,10 @@ bool KHTMLParser::insertNode(NodeImpl *n, bool flat)
                 for (unsigned long l = 0; map && l < map->length(); ++l) {
                     NodeImpl::Id attrId = map->idAt(l);
                     DOMStringImpl *attrValue = map->valueAt(l);
-                    changed = !bmap->getValue(attrId);
+                    if ( !bmap->getValue(attrId) ) {
                     bmap->setValue(attrId,attrValue);
+                        changed = true;
+                    }
                 }
                 if ( changed )
                     doc()->recalcStyle( NodeImpl::Inherit );
