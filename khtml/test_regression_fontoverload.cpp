6fd720b491fa072d693d3d772862c17d384e76c5
Stephan Kulow <coolo@kde.org>
2004-02-11 13:34:42 +0000

    it doesn't really matter right now
    svn path=/trunk/kdelibs/; revision=287109

--- khtml/test_regression_fontoverload.cpp
+++ khtml/test_regression_fontoverload.cpp
@@ -183,8 +183,9 @@ QFontDatabase::findFont( QFont::Script script, const QFontPrivate *fp,
 
     XFontStruct *xfs;
     xfs = XLoadQueryFont(QPaintDevice::x11AppDisplay(), xlfd.latin1() );
-    if (!xfs)
-	qFatal("we need some fonts. So make sure you have %s installed.", xlfd.latin1());
+    if (!xfs) // as long as you don't do screenshots, it's maybe fine
+	qWarning("we need some fonts. So make sure you have %s installed.", xlfd.latin1());
+        
 
     if ( !xfs )
         return 0;
