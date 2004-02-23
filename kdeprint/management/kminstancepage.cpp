b94bc8150d97bac91bb8dc229fc3d6f6444a3782
Michael Goffioul <kdeprint@swing.be>
2004-02-23 17:26:55 +0000

    Forbid slash in instance name (this is not CUPS-compliant)
    svn path=/trunk/kdelibs/; revision=290630

--- kdeprint/management/kminstancepage.cpp
+++ kdeprint/management/kminstancepage.cpp
@@ -138,8 +138,8 @@ void KMInstancePage::slotNew()
 			                     i18n("(Default)"),&ok,this);
 	if (ok)
 	{
-		if (name.find(QRegExp("\\s")) != -1)
-			KMessageBox::error(this, i18n("Instance name may not contain any space!"));
+		if (name.find(QRegExp("[/\\s]")) != -1)
+			KMessageBox::error(this, i18n("Instance name may not contain any space or slash!"));
 		else
 		{
 			if (name == i18n("(Default)"))
@@ -183,8 +183,8 @@ void KMInstancePage::slotCopy()
 				                     i18n("(Default)"),&ok,this);
 		if (ok)
 		{
-			if (name.find(QRegExp("\\s")) != -1)
-				KMessageBox::error(this, i18n("Instance name may not contain any space!"));
+			if (name.find(QRegExp("[/\\s]")) != -1)
+				KMessageBox::error(this, i18n("Instance name may not contain any space or slash!"));
 			else
 			{
 				if (src == i18n("(Default)"))
