096cdab3a7afca8e33f97e166f749a8c132f58d1
Nadeem Hasan <nhasan@kde.org>
2004-03-07 05:03:35 +0000

    Remove Id tags.
    svn path=/trunk/kdelibs/; revision=294121

--- kio/kfile/kicondialog.cpp
+++ kio/kfile/kicondialog.cpp
@@ -1,7 +1,5 @@
 /* vi: ts=8 sts=4 sw=4
  *
- * $Id$
- *
  * This file is part of the KDE project, module kfile.
  * Copyright (C) 2000 Geert Jansen <jansen@kde.org>
  *           (C) 2000 Kurt Granroth <granroth@kde.org>
