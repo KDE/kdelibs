1733a1bb7f6853028ff6d29c0f9857aa4ad3af2f
Melchior Franz <mfranz@kde.org>
2004-03-09 17:26:40 +0000

    CVS_SILENT fix LGPL message
    svn path=/trunk/kdelibs/; revision=294733

--- kimgio/rgb.cpp
+++ kimgio/rgb.cpp
@@ -1,11 +1,12 @@
-/* This file is part of the KDE project
-   Copyright (C) 2004 Melchior FRANZ <mfranz@kde.org>
+// kimgio module for SGI images
+//
+// Copyright (C) 2004  Melchior FRANZ  <mfranz@kde.org>
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the Lesser GNU General Public License as
+// published by the Free Software Foundation; either version 2 of the
+// License, or (at your option) any later version.
 
-   This program is free software; you can redistribute it and/or
-   modify it under the terms of the Lesser GNU General Public
-   License as published by the Free Software Foundation; either
-   version 2 of the License, or (at your option) any later version.
-*/
 
 /* this code supports:
  * reading:
--- kimgio/rgb.h
+++ kimgio/rgb.h
@@ -1,11 +1,12 @@
-/* This file is part of the KDE project
-   Copyright (C) 2004 Melchior FRANZ <mfranz@kde.org>
+// kimgio module for SGI images
+//
+// Copyright (C) 2004  Melchior FRANZ  <mfranz@kde.org>
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the Lesser GNU General Public License as
+// published by the Free Software Foundation; either version 2 of the
+// License, or (at your option) any later version.
 
-   This program is free software; you can redistribute it and/or
-   modify it under the terms of the Lesser GNU General Public
-   License as published by the Free Software Foundation; either
-   version 2 of the License, or (at your option) any later version.
-*/
 
 #ifndef KIMG_RGB_H
 #define KIMG_RGB_H
