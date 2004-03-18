a9fa7bdb849f2ff91ac667a70980ebbdbff627bd
Stephan Kulow <coolo@kde.org>
2004-03-18 16:03:51 +0000

    having a smaller number in 3.2.1 than in HEAD is asking for trouble
    svn path=/trunk/kdelibs/; revision=296884

--- kdecore/ksycoca.h
+++ kdecore/ksycoca.h
@@ -34,7 +34,7 @@ class KSycocaFactoryList;
  * If the existing file is outdated, it will not get read
  * but instead we'll ask kded to regenerate a new one...
 */
-#define KSYCOCA_VERSION 71
+#define KSYCOCA_VERSION 73
 
 /**
  * @internal
