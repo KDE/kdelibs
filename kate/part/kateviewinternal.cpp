ad0796f996eda67dbe7cd7f5d5b7993f97b0aaae
Anders Lund <anders@alweb.dk>
2004-03-22 10:20:50 +0000

    Uncommenting 3 instances of Q_ASSERT(m_view->dynWordWrap());
    They just prints a warning, unless Qt is compiled with some flag, right?
    So why are they there? If you want debugging messages, use kdDebug. And imo,
    the verbosity is too high allready!
    
    I'll backport to BRANCH immediately
    
    CCMAIL: 76860-done@bugs.kde.org
    
    svn path=/branches/KDE_3_2_BRANCH/kdelibs/; revision=297782

--- kate/part/kateviewinternal.cpp
+++ kate/part/kateviewinternal.cpp
@@ -1254,7 +1254,7 @@ LineRange KateViewInternal::range(int realLine, const LineRange* previous)
 
 LineRange KateViewInternal::currentRange()
 {
-  Q_ASSERT(m_view->dynWordWrap());
+//   Q_ASSERT(m_view->dynWordWrap());
 
   return range(cursor);
 }
@@ -1283,7 +1283,7 @@ LineRange KateViewInternal::nextRange()
 
 LineRange KateViewInternal::range(const KateTextCursor& realCursor)
 {
-  Q_ASSERT(m_view->dynWordWrap());
+//   Q_ASSERT(m_view->dynWordWrap());
 
   LineRange thisRange;
   bool first = true;
@@ -1298,7 +1298,7 @@ LineRange KateViewInternal::range(const KateTextCursor& realCursor)
 
 LineRange KateViewInternal::range(uint realLine, int viewLine)
 {
-  Q_ASSERT(m_view->dynWordWrap());
+//   Q_ASSERT(m_view->dynWordWrap());
 
   LineRange thisRange;
   bool first = true;
@@ -2298,7 +2298,7 @@ void KateViewInternal::keyPressEvent( QKeyEvent* e )
       if (m_doc->hasSelection() || (m_doc->configFlags() & KateDocumentConfig::cfTabIndentsMode))
         m_doc->indent( m_view, cursor.line(), 1 );
       else if (m_doc->configFlags() & KateDocumentConfig::cfTabInsertsTab)
-        m_doc->typeChars ( m_view, QString ("\t") );
+        m_doc->typeChars ( m_view, QString ("   t") );
       else
         m_doc->insertIndentChars ( m_view );
 
