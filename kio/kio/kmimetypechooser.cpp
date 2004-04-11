8ea947810d54e567e0cfd31fac782aaf6ca12539
Anders Lund <anders@alweb.dk>
2004-04-11 21:25:28 +0000

    Adding KMimeTypeChooser and KMimeTypeChooserDialog classes. These classes provides a easy way to select a number of mimetype in some or groups.
    CCMAIL: kde-core-devel@kde.org kwrite-devel@kde.org
    
    svn path=/trunk/kdelibs/; revision=302909

---
+++ kio/kio/kmimetypechooser.cpp
@@ -0,0 +1,289 @@
+/* This file is part of the KDE libraries
+   Copyright (C) 2001 - 2004 Anders Lund <anders@alweb.dk>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License version 2 as published by the Free Software Foundation.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
+#include "kmimetypechooser.h"
+
+#include <kconfig.h>
+#include <kiconloader.h>
+#include <klistview.h>
+#include <klocale.h>
+#include <kmimetype.h>
+#include <kprocess.h>
+#include <krun.h>
+#include <ksycoca.h>
+
+#include <qlabel.h>
+#include <qlayout.h>
+#include <qlineedit.h>
+#include <qpushbutton.h>
+#include <qwhatsthis.h>
+
+//BEGIN KMimeTypeChooserPrivate
+class KMimeTypeChooserPrivate
+{
+  public:
+    KListView *lvMimeTypes;
+    QPushButton *btnEditMimeType;
+
+    QString defaultgroup;
+    QStringList groups;
+    int visuals;
+};
+//END
+
+//BEGIN KMimeTypeChooser
+KMimeTypeChooser::KMimeTypeChooser( const QString &text,
+                              const QStringList &selMimeTypes,
+                              const QString &defaultGroup,
+                              const QStringList &groupsToShow,
+                              int visuals,
+                              QWidget *parent,
+                              const char *name )
+    : QVBox( parent, name )
+{
+  d = new KMimeTypeChooserPrivate();
+  d->lvMimeTypes = 0;
+  d->btnEditMimeType = 0;
+  d->defaultgroup = defaultGroup;
+  d->groups = groupsToShow;
+  d->visuals = visuals;
+
+  setSpacing( KDialogBase::spacingHint() );
+
+  if ( !text.isEmpty() )
+  {
+    new QLabel( text, this );
+  }
+
+  d->lvMimeTypes = new KListView( this );
+
+  d->lvMimeTypes->addColumn( i18n("Mime Type") );
+//   d->lvMimeTypes->setColumnWidthMode( 0, QListView::Manual );
+
+  if ( visuals & Comments )
+  {
+    d->lvMimeTypes->addColumn( i18n("Comment") );
+    d->lvMimeTypes->setColumnWidthMode( 1, QListView::Manual );
+  }
+  if ( visuals & Patterns )
+    d->lvMimeTypes->addColumn( i18n("Patterns") );
+
+  d->lvMimeTypes->setRootIsDecorated( true );
+
+  loadMimeTypes( selMimeTypes );
+
+  if (visuals & KMimeTypeChooser::EditButton)
+  {
+    QHBox *btns = new QHBox( this );
+    ((QBoxLayout*)btns->layout())->addStretch(1);
+    d->btnEditMimeType = new QPushButton( i18n("&Edit..."), btns );
+
+    connect( d->btnEditMimeType, SIGNAL(clicked()), this, SLOT(editMimeType()) );
+    d->btnEditMimeType->setEnabled( false );
+    connect( d->lvMimeTypes, SIGNAL( doubleClicked ( QListViewItem * )),
+             this, SLOT( editMimeType()));
+    connect( d->lvMimeTypes, SIGNAL(currentChanged(QListViewItem*)),
+             this, SLOT(slotCurrentChanged(QListViewItem*)) );
+
+    QWhatsThis::add( d->btnEditMimeType, i18n(
+        "Click this button to display the familiar KDE Mime Type Editor.") );
+  }
+}
+
+void KMimeTypeChooser::loadMimeTypes( const QStringList &_selectedMimeTypes )
+{
+  QStringList selMimeTypes;
+
+  if ( !_selectedMimeTypes.isEmpty() )
+    selMimeTypes = _selectedMimeTypes;
+  else
+    selMimeTypes = mimeTypes();
+
+  d->lvMimeTypes->clear();
+
+  QMap<QString,QListViewItem*> groups;
+  // thanks to kdebase/kcontrol/filetypes/filetypesview
+  KMimeType::List mimetypes = KMimeType::allMimeTypes();
+  QValueListIterator<KMimeType::Ptr> it(mimetypes.begin());
+
+  QListViewItem *groupItem;
+  bool agroupisopen = false;
+  QListViewItem *idefault = 0; //open this, if all other fails
+  QListViewItem *firstChecked = 0; // make this one visible after the loop
+
+  for (; it != mimetypes.end(); ++it)
+  {
+    QString mimetype = (*it)->name();
+    int index = mimetype.find("/");
+    QString maj = mimetype.left(index);
+
+    if ( d->groups.count() && !d->groups.contains( maj ) )
+      continue;
+
+    QString min = mimetype.right(mimetype.length() - (index+1));
+
+    QMapIterator<QString,QListViewItem*> mit = groups.find( maj );
+    if ( mit == groups.end() )
+    {
+      groupItem = new QListViewItem( d->lvMimeTypes, maj );
+      groups.insert( maj, groupItem );
+       if ( maj == d->defaultgroup )
+         idefault = groupItem;
+    }
+    else
+        groupItem = mit.data();
+
+    QCheckListItem *item = new QCheckListItem( groupItem, min, QCheckListItem::CheckBox );
+    item->setPixmap( 0, SmallIcon( (*it)->icon(QString::null,false) ) );
+
+    int cl = 1;
+
+    if ( d->visuals & Comments )
+    {
+      item->setText( cl, (*it)->comment(QString::null, false) );
+      cl++;
+    }
+
+    if ( d->visuals & Patterns )
+      item->setText( cl, (*it)->patterns().join("; ") );
+
+    if ( selMimeTypes.contains(mimetype) )
+    {
+      item->setOn( true );
+      groupItem->setOpen( true );
+      agroupisopen = true;
+      if ( !firstChecked )
+        firstChecked = item;
+    }
+  }
+
+  if ( firstChecked )
+    d->lvMimeTypes->ensureItemVisible( firstChecked );
+
+  if ( !agroupisopen && idefault )
+  {
+    idefault->setOpen( true );
+    d->lvMimeTypes->ensureItemVisible( idefault );
+  }
+}
+
+void KMimeTypeChooser::editMimeType()
+{
+  if ( !(d->lvMimeTypes->currentItem() && (d->lvMimeTypes->currentItem())->parent()) )
+    return;
+  QString mt = (d->lvMimeTypes->currentItem()->parent())->text( 0 ) + "/" + (d->lvMimeTypes->currentItem())->text( 0 );
+  // thanks to libkonq/konq_operations.cc
+  connect( KSycoca::self(), SIGNAL(databaseChanged()),
+           this, SLOT(slotSycocaDatabaseChanged()) );
+  QString keditfiletype = QString::fromLatin1("keditfiletype");
+  KRun::runCommand( keditfiletype + " " + KProcess::quote(mt),
+                    keditfiletype, keditfiletype /*unused*/);
+}
+
+void KMimeTypeChooser::slotCurrentChanged(QListViewItem* i)
+{
+  if ( d->btnEditMimeType )
+    d->btnEditMimeType->setEnabled( i->parent() );
+}
+
+void KMimeTypeChooser::slotSycocaDatabaseChanged()
+{
+  if ( KSycoca::self()->isChanged("mime") )
+    loadMimeTypes();
+}
+
+QStringList KMimeTypeChooser::mimeTypes() const
+{
+  QStringList l;
+  QListViewItemIterator it( d->lvMimeTypes );
+  for (; it.current(); ++it)
+  {
+    if ( it.current()->parent() && ((QCheckListItem*)it.current())->isOn() )
+      l << it.current()->parent()->text(0) + "/" + it.current()->text(0); // FIXME uncecked, should be Ok unless someone changes mimetypes during this!
+  }
+  return l;
+}
+
+QStringList KMimeTypeChooser::patterns() const
+{
+  QStringList l;
+  KMimeType::Ptr p;
+  QString defMT = KMimeType::defaultMimeType();
+  QListViewItemIterator it( d->lvMimeTypes );
+  for (; it.current(); ++it)
+  {
+    if ( it.current()->parent() && ((QCheckListItem*)it.current())->isOn() )
+    {
+      p = KMimeType::mimeType( it.current()->parent()->text(0) + "/" + it.current()->text(0) );
+      if ( p->name() != defMT )
+        l += p->patterns();
+    }
+  }
+  return l;
+}
+//END
+
+//BEGIN KMimeTypeChooserDialog
+KMimeTypeChooserDialog::KMimeTypeChooserDialog(
+                         const QString &caption,
+                         const QString& text,
+                         const QStringList &selMimeTypes,
+                         const QString &defaultGroup,
+                         const QStringList &groupsToShow,
+                         int visuals,
+                         QWidget *parent, const char *name )
+    : KDialogBase(parent, name, true, caption, Cancel|Ok, Ok)
+{
+  m_chooser = new KMimeTypeChooser( text, selMimeTypes,
+                                  defaultGroup, groupsToShow, visuals,
+                                  this, "chooser" );
+  setMainWidget(m_chooser);
+
+  KConfigGroup group( KGlobal::config(), "KMimeTypeChooserDialog");
+  resize( group.readSizeEntry("size", new QSize(400,300)) );
+}
+
+KMimeTypeChooserDialog::KMimeTypeChooserDialog(
+                         const QString &caption,
+                         const QString& text,
+                         const QStringList &selMimeTypes,
+                         const QString &defaultGroup,
+                         QWidget *parent, const char *name )
+    : KDialogBase(parent, name, true, caption, Cancel|Ok, Ok)
+{
+  m_chooser = new KMimeTypeChooser( text, selMimeTypes,
+                                  defaultGroup, QStringList(),
+                                  KMimeTypeChooser::Comments|KMimeTypeChooser::Patterns|KMimeTypeChooser::EditButton,
+                                  this, "chooser" );
+  setMainWidget(m_chooser);
+
+  KConfigGroup group( KGlobal::config(), "KMimeTypeChooserDialog");
+  resize( group.readSizeEntry("size", new QSize(400,300)) );
+}
+
+
+KMimeTypeChooserDialog::~KMimeTypeChooserDialog()
+{
+  KConfigGroup group( KGlobal::config(), "KMimeTypeChooserDialog");
+  group.writeEntry("size", size());
+}
+
+//END KMimeTypeChooserDialog
+
+// kate: space-indent on; indent-width 2; replace-tabs on;
+#include "kmimetypechooser.moc"
---
+++ kio/kio/kmimetypechooser.h
@@ -0,0 +1,176 @@
+/* This file is part of the KDE libraries
+   Copyright (C) 2001 - 2004 Anders Lund <anders@alweb.dk>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License version 2 as published by the Free Software Foundation.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
+#ifndef _KMIMETYPE_CHOOSER_H_
+#define _KMIMETYPE_CHOOSER_H_
+
+#include <qvbox.h>
+#include <kdialogbase.h>
+
+
+/**
+ * This widget provides a checkable list of all available mimetypes,
+ * and a list of selected ones, as well as a corresponding list of file
+ * extensions, an optional text and an optional edit button (not working yet).
+ * Mime types is presented in a list view, with name, comment and patterns columns.
+ *
+ * @author Anders Lund (anders at alweb dk), jan 23, 2002
+ */
+class KMimeTypeChooser : public QVBox
+{
+  Q_OBJECT
+
+  public:
+    /**
+     * Buttons and data for display.
+     */
+    enum Visuals {
+      Comments=1,   ///< Show the Mimetypes Comment field in a column ("HTML Document").
+      Patterns=2,   ///< Show the Mimetypes Patterns field in a column ("*.html;*.htm").
+      EditButton=4  ///< Show the "Edit" button, allowing to edit the selected type.
+    };
+    /**
+     * Create a new KMimeTypeChooser.
+     *
+     * @param text A Text to display above the list
+     * @param selectedMimeTypes A list of mimetype names, theese will be checked
+     *        in the list if they exist.
+     * @param visuals A OR'd Visuals enum to decide which data and buttons to display.
+     * @param defaultGroup The group to open when no groups are selected (like
+     *        "text"). If not provided, no group is opened. If @p groupsToShow
+     *        is provided and defaultGroup is not a member of that, it is ignored.
+     * @param groupsToShow a list of mimetype groups to show. If empty, all
+     *        groups are shown.
+     */
+    KMimeTypeChooser( const QString& text=QString::null,
+                      const QStringList &selectedMimeTypes=0,
+                      const QString &defaultGroup=QString::null,
+                      const QStringList &groupsToShow=QStringList(),
+                      int visuals=Comments|Patterns|EditButton,
+                      QWidget *parent=0, const char *name=0 );
+    ~KMimeTypeChooser() {};
+
+    /**
+     * @return a list of all selected selected mimetypes represented by their name.
+     */
+    QStringList mimeTypes() const;
+    /**
+     * @return a list of the fileame patterns associated with all selected mimetypes.
+     */
+    QStringList patterns() const;
+
+  public slots:
+    /**
+     * @short edit the current mimetype
+     * Uses KRun to start the KDE mimetype editor for editing the currently
+     * selected mimetype.
+     */
+    void editMimeType();
+
+  private slots:
+    /**
+     * @internal disables the "edit" button for groups
+     */
+    void slotCurrentChanged(QListViewItem* i);
+
+    /**
+     *  @internal called when the sycoca database has changed after
+     * the user edited a mimetype
+     */
+     void slotSycocaDatabaseChanged();
+
+  private:
+    /**
+     * @internal Load mime types into the list view
+     * If @p selected is empty, selectedMimeTypesStringList() is called
+     * to fill it in.
+     */
+    void loadMimeTypes( const QStringList &selected=QStringList() );
+
+    class KMimeTypeChooserPrivate *d;
+};
+
+/**
+  * @short A Dialog to choose some mimetypes.
+  * Provides a checkable tree list of mimetypes, with icons and optinally
+  * comments and patterns, and an (optional) button to display the KDE mimetype
+  * editor.
+  *
+  * Here is an example, using the dialog to set the text of two lineedits:
+  *
+  * @code
+  *    QString text = i18n("Select the MimeTypes you want for this file type.");
+  *    QStringList list = QStringList::split( QRegExp("\\s*;\\s*"), leMimetypes->text() );
+  *    KMimeTypeChooserDialog *d = new KMimeTypeChooserDialog( this, 0,
+  *                  i18n("Select Mime Types"), text, list, "text" );
+  *    if ( d->exec() == KDialogBase::Accepted ) {
+  *      leWildcards->setText( d->chooser()->patterns().join(";") );
+  *      leMimetypes->setText( d->chooser()->mimeTypes().join(";") );
+  *    }
+  * @endcode
+  *
+  * @author Anders Lund (anders at alweb dk) dec 19, 2001
+  */
+class KMimeTypeChooserDialog : public KDialogBase
+{
+  public:
+    /**
+     * Create a KMimeTypeChooser dialog.
+     *
+     * @param title The title of the dialog
+     * @param text A Text to display above the list
+     * @param selectedMimeTypes A list of mimetype names, theese will be
+     *        checked in the list if they exist.
+     *        patterns will be added to the list view.
+     * @param visuals A OR'd KMimetypeChooser::Visuals enum to decide which data
+     *        and buttons to display.
+     * @param defaultGroup The group to open when no groups are selected (like
+     *        "text"). If not provided, no group is opened. If @p groupsToShow
+     *        is provided and defaultGroup is not a member of that, it is ignored.
+     * @param groupsToShow a list of mimetype groups to show. If empty, all
+     *        groups are shown.
+     */
+    KMimeTypeChooserDialog( const QString &caption=QString::null,
+                         const QString& text=QString::null,
+                         const QStringList &selectedMimeTypes=QStringList(),
+                         const QString &defaultGroup=QString::null,
+                         const QStringList &groupsToShow=QStringList(),
+                         int visuals=KMimeTypeChooser::Comments|KMimeTypeChooser::Patterns|KMimeTypeChooser::EditButton,
+                         QWidget *parent=0, const char *name=0 );
+
+    /**
+     * @overload
+     */
+    KMimeTypeChooserDialog( const QString &caption,
+                         const QString& text,
+                         const QStringList &selectedMimeTypes,
+                         const QString &defaultGroup,
+                         QWidget *parent=0, const char *name=0 );
+
+    ~KMimeTypeChooserDialog();
+
+    /**
+     * @return a pointer to the KMimeTypeChooser widget
+     */
+     KMimeTypeChooser* chooser() { return m_chooser; }
+
+  private:
+    KMimeTypeChooser *m_chooser;
+};
+#endif // _KMIMETYPE_CHOOSER_H_
+// kate: space-indent on; indent-width 2; replace-tabs on;
