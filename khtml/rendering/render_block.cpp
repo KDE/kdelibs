80dcd9bdec18c5526bd4df786f53517ee2971a40
Dirk Mueller <mueller@kde.org>
2004-04-26 21:05:27 +0000

    avoid assertion failure on man: pages (backport revision 1.34)
    svn path=/branches/KDE_3_2_BRANCH/kdelibs/; revision=306658

--- khtml/rendering/render_block.cpp
+++ khtml/rendering/render_block.cpp
@@ -422,14 +422,16 @@ void RenderBlock::layout()
 
 void RenderBlock::layoutBlock(bool relayoutChildren)
 {
+    if (isInline() && !isReplacedBlock()) {
+        setLayouted();
+        return;
+    }
+
     //    kdDebug( 6040 ) << renderName() << " " << this << "::layoutBlock() start" << endl;
     //     QTime t;
     //     t.start();
     KHTMLAssert( !layouted() );
     KHTMLAssert( minMaxKnown() );
-
-    if (isInline()) // Inline <form>s inside various table elements can cause us to
-        return;	    // come in here.  Just bail. -dwh
 
     int oldWidth = m_width;
 
