902bce268c953b3442b2191700bd1fb3c98e7320
Jan Schäfer <j_schaef@informatik.uni-kl.de>
2004-05-10 20:21:31 +0000

    New class KNFSShare which is similar to KFileShare, but which does not use a suid root script. However, so far only the isDirectoryShared method is implemented.
    svn path=/trunk/kdelibs/; revision=310153

---
+++ kio/kio/knfsshare.cpp
@@ -0,0 +1,196 @@
+/* This file is part of the KDE project
+   Copyright (c) 2004 Jan Schaefer <j_schaef@informatik.uni-kl.de>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License version 2 as published by the Free Software Foundation.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
+#include <qdict.h>
+#include <qfile.h>
+#include <qtextstream.h>
+
+#include <kdirwatch.h>
+#include <kstaticdeleter.h>
+#include <kdebug.h>
+#include <kconfig.h>
+
+#include "knfsshare.h"
+
+class KNFSSharePrivate
+{
+public:
+  KNFSSharePrivate();
+  
+  bool readExportsFile();
+  bool findExportsFile();
+  
+  QDict<bool> sharedPaths;
+  QString exportsFile;
+};
+
+KNFSSharePrivate::KNFSSharePrivate() 
+{
+  if (findExportsFile())
+      readExportsFile();
+}  
+
+/**
+ * Try to find the samba config file path
+ * First tries the kconfig, then checks
+ * several well-known paths
+ * @return wether a smb.conf was found.
+ **/
+bool KNFSSharePrivate::findExportsFile() {
+  KConfig config("knfsshare");
+  config.setGroup("General");
+  exportsFile = config.readPathEntry("exportsFile");
+
+  if ( QFile::exists(exportsFile) )
+    return true;
+
+  if ( QFile::exists("/etc/exports") )
+    exportsFile = "/etc/exports";
+  else {
+    kdDebug(7000) << "KNFSShare: Could not found exports file!" << endl;
+    return false;
+  }
+      
+  config.writeEntry("exportsFile",exportsFile);
+  return true;
+}
+
+/**
+ * Reads all path= entries from the smb.conf file
+ * and fills the sharedPaths dict with the values
+ */
+bool KNFSSharePrivate::readExportsFile() {
+  QFile f(exportsFile);
+
+  kdDebug(7000) << "KNFSShare::readExportsFile " << exportsFile << endl;
+  
+  if (!f.open(IO_ReadOnly)) {
+    kdError() << "KNFSShare: Could not open " << exportsFile << endl;
+    return false;
+  }
+  
+ 
+  sharedPaths.clear();
+
+  QTextStream s( &f );
+  
+  bool continuedLine = false; // is true if the line before ended with a backslash
+  QString completeLine;
+  
+  while ( !s.eof() )
+  {
+    QString currentLine = s.readLine().stripWhiteSpace();
+
+    if (continuedLine) {
+      completeLine += currentLine;
+      continuedLine = false;
+    }      
+    else
+      completeLine = currentLine;
+
+    // is the line continued in the next line ?
+    if ( completeLine[completeLine.length()-1] == '\\' )
+    {
+      continuedLine = true;
+      // remove the ending backslash
+      completeLine.truncate( completeLine.length()-1 ); 
+      continue;
+    }
+    
+    // comments or empty lines
+    if (completeLine.isEmpty() ||
+        '#' == completeLine[0])
+    {
+      continue;
+    }
+
+    QString path;
+    
+    // Handle quotation marks
+    if ( completeLine[0] == '"' ) {
+      int i = completeLine.find('"',1);
+      if (i == -1) {
+        kdError() << "KNFSShare: Parse error: Missing quotation mark: " << completeLine << endl;   
+        continue;
+      }
+      path = completeLine.mid(1,i-1);
+      
+    } else { // no quotation marks
+      int i = completeLine.find(' ');
+      if (i == -1) 
+        path = completeLine;
+      else 
+        path = completeLine.left(i);
+      
+    }        
+    
+    kdDebug(7000) << "KNFSShare: Found path: " << path << endl;
+    
+    // normalize path
+    if ( path[path.length()-1] != '/' )
+             path += '/';
+    
+    bool b = true;             
+    sharedPaths.insert(path,&b);             
+  }
+
+  f.close();
+
+  return true;  
+
+}
+
+KNFSShare::KNFSShare() {
+  d = new KNFSSharePrivate();
+  if (QFile::exists(d->exportsFile)) {
+    KDirWatch::self()->addFile(d->exportsFile);
+    connect(KDirWatch::self(), SIGNAL(dirty (const QString&)),this,
+   	        SLOT(slotFileChange(const QString&)));
+  } 
+}
+
+KNFSShare::~KNFSShare() {
+  delete d;
+}
+
+
+bool KNFSShare::isDirectoryShared( const QString & path ) {
+  QString fixedPath = path;
+  if ( path[path.length()-1] != '/' )
+       fixedPath += '/';
+  
+  return d->sharedPaths.find(fixedPath) > 0;
+}
+
+void KNFSShare::slotFileChange( const QString & path ) {
+  if (path == d->exportsFile)
+     d->readExportsFile();
+}
+
+KNFSShare* KNFSShare::_instance = 0L; 
+static KStaticDeleter<KNFSShare> ksdNFSShare;
+
+KNFSShare* KNFSShare::instance() {
+  if (! _instance ) 
+      _instance = ksdNFSShare.setObject(_instance, new KNFSShare());
+      
+  return _instance;      
+}
+
+#include "knfsshare.moc"
+
---
+++ kio/kio/knfsshare.h
@@ -0,0 +1,64 @@
+/* This file is part of the KDE project
+   Copyright (c) 2004 Jan Schaefer <j_schaef@informatik.uni-kl.de>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License version 2 as published by the Free Software Foundation.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
+#ifndef knfsshare_h
+#define knfsshare_h
+
+#include <qobject.h>
+
+class KNFSSharePrivate;
+
+/**
+ * Similar functionality like KFileShare, 
+ * but works only for NFS and do not need 
+ * any suid script.
+ * Singleton class.
+ */
+class KNFSShare : public QObject 
+{
+Q_OBJECT
+public:
+  /**
+   * Returns the one and only instance of KNFSShare
+   */
+  static KNFSShare* instance();
+
+  /**
+   * Wether or not the given path is shared by NFS.
+   * @param path the path to check if it is shared by NFS.
+   * @return wether the given path is shared by NFS.
+   */
+  bool isDirectoryShared( const QString & path );
+  
+  /**
+   * KNFSShare destructor. 
+   * Do not call!
+   * The instance is destroyed automatically!
+   */ 
+  virtual ~KNFSShare();
+  
+private:
+  KNFSShare();
+  static KNFSShare* _instance;
+  KNFSSharePrivate* d;
+  
+private slots:
+  void slotFileChange(const QString&);  
+};
+
+#endif
