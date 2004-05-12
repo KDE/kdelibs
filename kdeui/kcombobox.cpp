9270e423d1c22feece4674f9d84e027ff3173008
Stephan Binner <binner@kde.org>
2004-05-12 20:31:26 +0000

    icons++
    svn path=/trunk/kdelibs/; revision=310793

--- kdeui/kcombobox.cpp
+++ kdeui/kcombobox.cpp
@@ -432,7 +432,7 @@ void KHistoryCombo::addContextMenuItems( QPopupMenu* menu )
     if ( menu )
     {
         menu->insertSeparator();
-        int id = menu->insertItem( i18n("Clear &History"), this, SLOT( slotClear()));
+        int id = menu->insertItem( SmallIcon("history_clear"), i18n("Clear &History"), this, SLOT( slotClear()));
         if (!count())
            menu->setItemEnabled(id, false);
     }
