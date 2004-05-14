6bfd06808cefacb39a4fc5f0b6094a968feed4a4
Waldo Bastian <bastian@kde.org>
2004-05-14 12:35:25 +0000

    Don't interpret e-mail address as command line option
    svn path=/branches/KDE_3_2_BRANCH/kdelibs/; revision=311364

--- kdecore/kapplication.cpp
+++ kdecore/kapplication.cpp
@@ -2177,7 +2177,7 @@ void KApplication::invokeMailer(const QString &to, const QString &cc, const QStr
 
    if (command.isEmpty() || command == QString::fromLatin1("kmail")
        || command.endsWith("/kmail"))
-     command = QString::fromLatin1("kmail --composer -s %s -c %c -b %b --body %B --attach %A %t");
+     command = QString::fromLatin1("kmail --composer -s %s -c %c -b %b --body %B --attach %A -- %t");
 
    // TODO: Take care of the preferred terminal app (instead of hardcoding
    // Konsole), this will probably require a rewrite of the configurable
