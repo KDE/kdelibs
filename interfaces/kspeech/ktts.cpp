0cf3861e489af4ff691983ed632592476736cbb6
Gary Cramblitt <garycramblitt@comcast.net>
2004-05-16 00:07:46 +0000

    1. Move KDE Speech API to libktts dir. 2. Remove GUI from kttsd.  3. New kttsmgr GUI for managing and configuring kttsd.  4.  New kcmkttsmgr kcontrol module to replace kcmkttsd (kcmkttsd remains for now).  See ChangeLog, README, and TODO for details.
    svn path=/trunk/kdelibs/; revision=311721

---
+++ interfaces/kspeech/ktts.cpp
@@ -0,0 +1,13 @@
+//
+// C++ Implementation: ktts
+//
+// Description: 
+//
+//
+// Author: Gary Cramblitt <garycramblitt@comcast.net>, (C) 2004
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+
+#include "ktts.h"
---
+++ interfaces/kspeech/ktts.h
@@ -0,0 +1,14 @@
+//
+// C++ Interface: ktts
+//
+// Description: 
+//
+//
+// Author: Gary Cramblitt <garycramblitt@comcast.net>, (C) 2004
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+
+#include "kspeech.h"
+#include "kspeechsink.h"
