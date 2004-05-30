d80784876366495ea20b80ec7da69080f9b52099
Stephan Binner <binner@kde.org>
2004-05-30 18:50:38 +0000

    Same error here, the parameter is "nowait" for result
    svn path=/branches/KDE_3_2_BRANCH/kdelibs/; revision=316194

--- kdecore/kapplication.cpp
+++ kdecore/kapplication.cpp
@@ -726,7 +726,7 @@ void KApplication::init(bool GUIenabled)
 
       atoms[n] = &kde_net_wm_user_time;
       names[n++] = (char *) "_NET_WM_USER_TIME";
-      
+
       atoms[n] = &kde_xdnd_drop;
       names[n++] = (char *) "XdndDrop";
 
@@ -1584,7 +1584,7 @@ bool KApplication::x11EventFilter( XEvent *_event )
                     && _event->xclient.data.l[ 4 ] == 0
                     && _event->xclient.data.l[ 3 ] != 0 )
                     {
-                    if( qt_x_user_time == 0 
+                    if( qt_x_user_time == 0
                         || ( _event->xclient.data.l[ 3 ] - qt_x_user_time ) < 100000U )
                         { // and the timestamp looks reasonable
                         qt_x_user_time = _event->xclient.data.l[ 3 ]; // update our qt_x_user_time from it
@@ -2244,7 +2244,7 @@ void KApplication::invokeBrowser( const QString &url, const QCString& startup_id
 {
    QString error;
 
-   if (startServiceByDesktopName("kfmclient", url, &error, 0, 0, startup_id, true))
+   if (startServiceByDesktopName("kfmclient", url, &error, 0, 0, startup_id, false))
    {
       kdWarning() << "Could not launch browser:\n" << error << endl;
       return;
@@ -2786,7 +2786,7 @@ void KApplication::allowURLAction(const QString &action, const KURL &_baseURL, c
 {
   if (authorizeURLAction(action, _baseURL, _destURL))
      return;
-     
+
   d->urlActionRestrictions.append(new KApplicationPrivate::URLActionRule
         ( action, _baseURL.protocol(), _baseURL.host(), _baseURL.path(-1),
                   _destURL.protocol(), _destURL.host(), _destURL.path(-1), true));
