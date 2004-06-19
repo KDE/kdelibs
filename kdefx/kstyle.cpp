156609c856ed573d8d3d62aace40b3181dc13612
Maks Orlovich <maksim@kde.org>
2004-06-19 15:32:49 +0000

    Do not use function-static objects, as they can cause crashes on dlclose on some versions of FreeBSD. Credit for analysis goes to Frerich. (Plastik needs corresponding change, and so does QWindowsStyle). May address 79888 CCMAIL:79888@bugs.kde.org
    svn path=/trunk/kdelibs/; revision=321967

--- kdefx/kstyle.cpp
+++ kdefx/kstyle.cpp
@@ -161,6 +161,10 @@ struct KStylePrivate
 	KStyle::KStyleScrollBarType  scrollbarType;
 	TransparencyHandler* menuHandler;
 	KStyle::KStyleFlags flags;
+	
+	//For KPE_ListViewBranch
+	QBitmap *verticalLine;
+	QBitmap *horizontalLine;
 };
 
 // -----------------------------------------------------------------------------
@@ -209,6 +213,9 @@ KStyle::KStyle( KStyleFlags flags, KStyleScrollBarType sbtype )
 													 d->menuOpacity, d->menuDropShadow);
 		}
 	}
+	
+	d->verticalLine   = 0;
+	d->horizontalLine = 0;
 
 	// Create a transparency handler if only drop shadows are enabled.
 	if (!d->menuHandler && d->menuDropShadow)
@@ -218,8 +225,10 @@ KStyle::KStyle( KStyleFlags flags, KStyleScrollBarType sbtype )
 
 KStyle::~KStyle()
 {
-	if (d->menuHandler)
-		delete d->menuHandler;
+	delete d->verticalLine;
+	delete d->horizontalLine;
+
+	delete d->menuHandler;
 
 	d->menuHandler = NULL;
 	delete d;
@@ -394,19 +403,16 @@ void KStyle::drawKStylePrimitive( KStylePrimitive kpe,
 		case KPE_ListViewBranch: {
 			// Typical Windows style listview branch element (dotted line).
 
-			static QBitmap *verticalLine = 0, *horizontalLine = 0;
-			static QCleanupHandler<QBitmap> qlv_cleanup_bitmap;
-
 			// Create the dotline pixmaps if not already created
-			if ( !verticalLine )
+			if ( !d->verticalLine )
 			{
 				// make 128*1 and 1*128 bitmaps that can be used for
 				// drawing the right sort of lines.
-				verticalLine   = new QBitmap( 1, 129, true );
-				horizontalLine = new QBitmap( 128, 1, true );
+				d->verticalLine   = new QBitmap( 1, 129, true );
+				d->horizontalLine = new QBitmap( 128, 1, true );
 				QPointArray a( 64 );
 				QPainter p2;
-				p2.begin( verticalLine );
+				p2.begin( d->verticalLine );
 
 				int i;
 				for( i=0; i < 64; i++ )
@@ -415,19 +421,16 @@ void KStyle::drawKStylePrimitive( KStylePrimitive kpe,
 				p2.drawPoints( a );
 				p2.end();
 				QApplication::flushX();
-				verticalLine->setMask( *verticalLine );
+				d->verticalLine->setMask( *d->verticalLine );
 
-				p2.begin( horizontalLine );
+				p2.begin( d->horizontalLine );
 				for( i=0; i < 64; i++ )
 					a.setPoint( i, i*2+1, 0 );
 				p2.setPen( color1 );
 				p2.drawPoints( a );
 				p2.end();
 				QApplication::flushX();
-				horizontalLine->setMask( *horizontalLine );
-
-				qlv_cleanup_bitmap.add( &verticalLine );
-				qlv_cleanup_bitmap.add( &horizontalLine );
+				d->horizontalLine->setMask( *d->horizontalLine );
 			}
 
 			p->setPen( cg.text() );		// cg.dark() is bad for dark color schemes.
@@ -444,7 +447,7 @@ void KStyle::drawKStylePrimitive( KStylePrimitive kpe,
 					int i = 128;
 					if ( i+point > end )
 						i = end-point;
-					p->drawPixmap( point, other, *horizontalLine, 0, 0, i, thickness );
+					p->drawPixmap( point, other, *d->horizontalLine, 0, 0, i, thickness );
 					point += i;
 				}
 
@@ -460,7 +463,7 @@ void KStyle::drawKStylePrimitive( KStylePrimitive kpe,
 					int i = 128;
 					if ( i+point > end )
 						i = end-point;
-					p->drawPixmap( other, point, *verticalLine, 0, pixmapoffset, thickness, i );
+					p->drawPixmap( other, point, *d->verticalLine, 0, pixmapoffset, thickness, i );
 					point += i;
 				}
 			}
