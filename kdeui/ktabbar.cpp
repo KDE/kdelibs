6c2e1d0ec95eeccec86032c2290f03af503d7203
Stephan Binner <binner@kde.org>
2004-08-25 19:33:35 +0000

    revert of bogus commit, this check prevents endless switching beneath pointer
    svn path=/trunk/kdelibs/; revision=341078

--- kdeui/ktabbar.cpp
+++ kdeui/ktabbar.cpp
@@ -149,7 +149,7 @@ void KTabBar::mouseMoveEvent( QMouseEvent *e )
             QTab *tab = selectTab( e->pos() );
             if( tab!= 0L ) {
                 int reorderStopTab = indexOf( tab->identifier() );
-                if ( mReorderStartTab!=reorderStopTab /*&& mReorderPreviousTab!=reorderStopTab JJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJj*/) {
+                if ( mReorderStartTab!=reorderStopTab && mReorderPreviousTab!=reorderStopTab ) {
                     emit( moveTab( mReorderStartTab, reorderStopTab ) );
                     mReorderPreviousTab=mReorderStartTab;
                     mReorderStartTab=reorderStopTab;
