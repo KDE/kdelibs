e34478ef6c09042f78acd2608d4ab4b062891e05
Christoph Cullmann <cullmann@kde.org>
2004-08-28 14:58:06 +0000

    not trigger show
    svn path=/trunk/kdelibs/; revision=342178

--- kutils/kmultitabbar.cpp
+++ kutils/kmultitabbar.cpp
@@ -542,7 +542,7 @@ void KMultiTabBarTab::setIcon(const QPixmap& icon)
 		        	rotateMatrix.rotate(90);
 			else
 				rotateMatrix.rotate(-90);
-			QPixmap pic=icon.xForm(rotateMatrix);
+			QPixmap pic=icon; //icon.xForm(rotateMatrix); TODO FIX THIS, THIS SHOWS WINDOW
 			d->pix=pic;
 		        setIconSet(pic);
 		} else setIconSet(icon);
@@ -889,7 +889,7 @@ void KMultiTabBar::updateSeparator() {
 			break;
 		}
 	}
-	if (hideSep) m_btnTabSep->hide(); 
+	if (hideSep) m_btnTabSep->hide();
 		else m_btnTabSep->show();
 
 }
