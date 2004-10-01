66123715bc9deef1716d48a360b7a9a23e1f3d73
Joseph Wenninger <kde@jowenn.at>
2004-10-01 18:52:03 +0000

    Fix startup notification in Kate / Bug #88876
    svn path=/trunk/kdelibs/; revision=350943

--- kdecore/kuniqueapplication.cpp
+++ kdecore/kuniqueapplication.cpp
@@ -309,6 +309,7 @@ KUniqueApplication::start()
      int exitCode;
      rs >> exitCode;
      delete dc;	// Clean up DCOP commmunication
+     KStartupInfo::appStarted();
      ::exit(exitCode);
      break;
   }
