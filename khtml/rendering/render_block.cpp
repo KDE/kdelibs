6f9bcebb7dddd2718d3cc1d81a3bb940cd0e0f80
Germain Garand <germain@ebooksfrance.org>
2004-10-02 12:28:58 +0000

    Unbreak nvidia.com menus. There seem to be some abuse of overflowWidth/Height up in the layer code.
    This, I'll have to backport.
    
    svn path=/trunk/kdelibs/; revision=351156

--- khtml/rendering/render_block.cpp
+++ khtml/rendering/render_block.cpp
@@ -1106,14 +1106,14 @@ void RenderBlock::layoutPositionedObjects(bool relayoutChildren)
         //kdDebug( 6040 ) << renderName() << " " << this << "::layoutPositionedObjects() start" << endl;
         RenderObject* r;
         QPtrListIterator<RenderObject> it(*m_positionedObjects);
-        bool fixed = (style()->position() == FIXED);
+        bool adjOverflow = !(style()->position() == FIXED) && style()->hidesOverflow();
         for ( ; (r = it.current()); ++it ) {
             //kdDebug(6040) << "   have a positioned object" << endl;
             if ( relayoutChildren )
                 r->setLayouted( false );
             if ( !r->layouted() )
                 r->layout();
-            if (!fixed && r->style()->position() == ABSOLUTE) {
+            if (adjOverflow && r->style()->position() == ABSOLUTE) {
                 if (r->xPos() + r->effectiveWidth() > m_overflowWidth)
                     m_overflowWidth = r->xPos() + r->effectiveWidth();
                 if (r->yPos() + r->effectiveHeight() > m_overflowHeight)
