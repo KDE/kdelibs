b8bf880274f1daf7a61502fd2bb3e8ba8487ec43
Brad Hards <bradh@frogmouth.net>
2004-10-10 10:38:36 +0000

    Remove Id tag
    svn path=/trunk/kdelibs/; revision=353318

--- kdeui/kactioncollection.h
+++ kdeui/kactioncollection.h
@@ -21,7 +21,6 @@
     the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
     Boston, MA 02111-1307, USA.
 */
-//$Id$
 
 #ifndef __kactioncollection_h__
 #define __kactioncollection_h__
