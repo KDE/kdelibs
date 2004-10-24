8583cea4973b26adbbfec1279ed13be51713a96f
Jason Harris <kstars@30doradus.org>
2004-10-24 07:01:29 +0000

    Eliminating flicker in KPlotWidget.  Added "WNoAutoErase" flag to QWidget ctor.  It works well!
    (note: I first tried to add "setBackgroundMode( QWidget::NoBackground )"
    to the KPlotWidget ctor, but this didn't stop the flicker.  This is
    strange, because that's how I got rid of the flicker in the SkyMap
    widget.  SkyMap doesn't use WNoAutoErase, yet it is flicker-free.  Odd.)
    
    CCMAIL: kstars-devel@kde.org
    CCMAIL: cniehaus@kde.org
    
    svn path=/trunk/kdeedu/libkdeedu/kdeeduplot/; revision=357248

--- kdeui/plotting/kplotwidget.cpp
+++ kdeui/plotting/kplotwidget.cpp
@@ -23,11 +23,13 @@
 #include "kplotwidget.h"
 
 KPlotWidget::KPlotWidget( double x1, double x2, double y1, double y2, QWidget *parent, const char* name )
- : QWidget( parent, name ),
+ : QWidget( parent, name, WNoAutoErase ),
    dXtick(0.0), dYtick(0.0),
    nmajX(0), nminX(0), nmajY(0), nminY(0),
    ShowAxes( true ), ShowTickMarks( true ), ShowTickLabels( true ), ShowGrid( false ),
    XAxisLabel(), YAxisLabel() {
+
+	setBackgroundMode( QWidget::NoBackground );
 
 	//set DataRect
 	setLimits( x1, x2, y1, y2 );
