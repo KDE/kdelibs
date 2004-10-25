c2ac8293bcfc8d34e562077ddbbeca4ada8b7e8f
David Faure <faure@kde.org>
2004-10-25 10:07:17 +0000

    Coolo: did you try compiling it??
    svn path=/trunk/kdelibs/; revision=357514

--- khtml/test_regression.cpp
+++ khtml/test_regression.cpp
@@ -690,7 +690,7 @@ bool RegressionTest::runTests(QString relPath, bool mustExist, int known_failure
 	m_currentTest = filename;
         m_known_failures = known_failure;
 	if ( filename.endsWith(".html") || filename.endsWith( ".htm" ) || filename.endsWith( ".xhtml" ) ) {
-            if ( relPath.startswith( "domts/" && !m_runJS ) )
+            if ( relPath.startsWith( "domts/" && !m_runJS ) )
                 return true;
             if ( m_runHTML )
                 testStaticFile(relPath);
