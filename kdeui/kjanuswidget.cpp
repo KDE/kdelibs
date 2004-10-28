ffa8f4e343c9f4e69151ee24c013c45b4e5713d0
Stephan Binner <binner@kde.org>
2004-10-28 17:58:15 +0000

    CVS_SILENT i18n style guide fix
    svn path=/trunk/kdelibs/; revision=358566

--- kdeui/kjanuswidget.cpp
+++ kdeui/kjanuswidget.cpp
@@ -154,7 +154,7 @@ KJanusWidget::KJanusWidget( QWidget *parent, const char *name, int face )
 
     QVBoxLayout *vbox = new QVBoxLayout( page, 0, KDialog::spacingHint() );
 
-    mTitleLabel = new QLabel( QString::fromLatin1("Empty page"), page, "KJanusWidgetTitleLabel" );
+    mTitleLabel = new QLabel( QString::fromLatin1("Empty Page"), page, "KJanusWidgetTitleLabel" );
     vbox->addWidget( mTitleLabel, 0, QApplication::reverseLayout() ? AlignRight : AlignLeft );
 
     QFont titleFont( mTitleLabel->font() );
