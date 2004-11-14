a19fd1d1486ecfb339711097601b0bad141eacbe
Szombathelyi György <gyurco@freemail.hu>
2004-11-14 01:08:41 +0000

    Clear the resource before (re)loading its contents. This fixes a problem in kaddreesbook when a ldap resource is disabled and enabled again, all contacts duplicated.
    svn path=/trunk/kdelibs/; revision=362819

--- kabc/plugins/ldapkio/resourceldapkio.cpp
+++ kabc/plugins/ldapkio/resourceldapkio.cpp
@@ -529,6 +529,8 @@ bool ResourceLDAPKIO::load()
 {
   kdDebug(7125) << "ResourceLDAPKIO::load()" << endl;
   KIO::Job *job;
+
+  clear();
   //clear the addressee
   d->mAddr = Addressee();
   d->mAd = Address( Address::Home );
@@ -564,6 +566,7 @@ bool ResourceLDAPKIO::load()
 
 bool ResourceLDAPKIO::asyncLoad()
 {
+  clear();
   //clear the addressee
   d->mAddr = Addressee();
   d->mAd = Address( Address::Home );
@@ -675,6 +678,7 @@ void ResourceLDAPKIO::data( KIO::Job *, const QByteArray &data )
       case LDIF::EndEntry: {
         d->mAddr.setResource( this );
         d->mAddr.insertAddress( d->mAd );
+ 	d->mAddr.setChanged( false );
         insertAddressee( d->mAddr );
         //clear the addressee
         d->mAddr = Addressee();
