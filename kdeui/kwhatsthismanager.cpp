2f5c4147bf0aeec20ec83259d1e0d6c24d6a5012
David Faure <faure@kde.org>
2004-11-17 11:24:49 +0000

    sshhhht
    svn path=/trunk/kdelibs/; revision=363658

--- kdeui/kwhatsthismanager.cpp
+++ kdeui/kwhatsthismanager.cpp
@@ -47,7 +47,6 @@ QString KWhatsThisUndefined::text (const QPoint &)
 {
     if (!m_widget)
         return "";
-    kdDebug () << "widget: " << m_widget << endl;
     QString txt = i18n ("<b>Not Defined</b><br>There is no \"What's This\""
             " help assigned to this widget. If you want to help us and "
             " describe the widget, you are welcome to <a href=\"submit"
