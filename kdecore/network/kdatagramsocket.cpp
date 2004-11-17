f7830c3fc5a0659ac24c0ad4524822e617cbee7f
Thiago Macieira <thiago@kde.org>
2004-11-17 01:28:02 +0000

    (Merge 1.8:1.9) Backporting the changes that went in HEAD, fixing a bug.
    svn path=/branches/KDE_3_3_BRANCH/kdelibs/; revision=363597

--- kdecore/network/kdatagramsocket.cpp
+++ kdecore/network/kdatagramsocket.cpp
@@ -1,5 +1,5 @@
 /*  -*- C++ -*-
- *  Copyright (C) 2003 Thiago Macieira <thiago.macieira@kdemail.net>
+ *  Copyright (C) 2003,2004 Thiago Macieira <thiago.macieira@kdemail.net>
  *
  *
  *  Permission is hereby granted, free of charge, to any person obtaining
@@ -42,6 +42,8 @@ KDatagramSocket::KDatagramSocket(QObject* parent, const char *name)
 
   peerResolver().setSocketType(SOCK_DGRAM);
   localResolver().setSocketType(SOCK_DGRAM);
+
+  localResolver().setFlags(KResolver::Passive);
 
   //  QObject::connect(localResolver(), SIGNAL(finished(KResolverResults)),
   //		   this, SLOT(lookupFinishedLocal()));
