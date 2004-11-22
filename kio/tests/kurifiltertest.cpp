c6aeb66283ec8055f87f648d4309002d69faac6c
David Faure <faure@kde.org>
2004-11-22 14:01:31 +0000

    Added missing license. Added test for the konq-location-bar bug with "/foo#ref" - will fix it now.
    svn path=/trunk/kdelibs/; revision=365117

--- kio/tests/kurifiltertest.cpp
+++ kio/tests/kurifiltertest.cpp
@@ -173,9 +173,11 @@ int main(int argc, char **argv)
         // URL with reference
     filter( "http://www.kde.org/index.html#q8", "http://www.kde.org/index.html#q8", KURIFilterData::NET_PROTOCOL );
         // local file with reference
-    filter( "file:/etc/passwd#q8", "file:/etc/passwd#q8", KURIFilterData::LOCAL_FILE );
+    filter( "file:/etc/passwd#q8", "file:///etc/passwd#q8", KURIFilterData::LOCAL_FILE );
+    filter( "file:///etc/passwd#q8", "file:///etc/passwd#q8", KURIFilterData::LOCAL_FILE );
+    filter( "/etc/passwd#q8", "file:///etc/passwd#q8", KURIFilterData::LOCAL_FILE );
         // local file with query (can be used by javascript)
-    filter( "file:/etc/passwd?foo=bar", "file:/etc/passwd?foo=bar", KURIFilterData::LOCAL_FILE );
+    filter( "file:/etc/passwd?foo=bar", "file:///etc/passwd?foo=bar", KURIFilterData::LOCAL_FILE );
         // local file with ? in the name (#58990)
     QFile tmpFile( "/tmp/kurifiltertest?foo" ); // Yeah, I know, security risk blah blah. This is a test prog!
 
