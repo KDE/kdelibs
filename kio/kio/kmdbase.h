9a1d2f431d2d25270088bd4ba1761e02d4d589c1
Benjamin Meyer <ben@meyerhome.net>
2004-11-22 03:01:28 +0000

    Add KDE_NO_COMPAT wrapper to behave like the rest of kdelibs header names that have been depreciated.
    svn path=/trunk/kdelibs/; revision=364982

--- kio/kio/kmdbase.h
+++ kio/kio/kmdbase.h
@@ -1,6 +1,6 @@
-/*
-    Temporary place holder until #include <kmdbase.h> entries have
-	been changed to #include <kmdcodec.h>.
-*/
+// kmdbase.h is the old name. Use #include <kmdcodec.h> from now on
+#ifdef KDE_NO_COMPAT
+#error include <kmdcodec.h> instead of <kmdbase.h>
+#else
 #include <kmdcodec.h>
-
+#endif
