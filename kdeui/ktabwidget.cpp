fe878f86ecec6a9029e5ebf81840cf99fa2f05d7
Stephan Binner <binner@kde.org>
2004-12-17 12:54:54 +0000

    Always read from "General" group. In konquerorrc these settings were in [FMSettings] though. :-|
    svn path=/trunk/kdelibs/; revision=371372

--- kdeui/ktabwidget.cpp
+++ kdeui/ktabwidget.cpp
@@ -41,6 +41,7 @@ public:
 
     KTabWidgetPrivate() {
         m_automaticResizeTabs = false;
+        KConfigGroupSaver groupsaver(KGlobal::config(), "General");
         m_maxLength = KGlobal::config()->readNumEntry("MaximumTabLength", 30);
         m_minLength = KGlobal::config()->readNumEntry("MinimumTabLength", 3);
         m_CurrentMaxLength = m_minLength;
