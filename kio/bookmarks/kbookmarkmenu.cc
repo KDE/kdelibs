657769fe19768fb18780ea0a20838a92bc95d840
Stephan Binner <binner@kde.org>
2005-01-08 17:41:22 +0000

    CVS_SILENT use KStdGuiItem
    svn path=/trunk/kdelibs/; revision=376539

--- kio/bookmarks/kbookmarkmenu.cc
+++ kio/bookmarks/kbookmarkmenu.cc
@@ -379,7 +379,7 @@ void RMB::slotRMBActionRemove( int val )
                  : i18n("Are you sure you wish to remove the bookmark\n\"%1\"?").arg(bookmark.text()),
           folder ? i18n("Bookmark Folder Deletion")
                  : i18n("Bookmark Deletion"),
-          KGuiItem( i18n("&Delete"), "editdelete"), KStdGuiItem::cancel())
+          KStdGuiItem::del(), KStdGuiItem::cancel())
         != KMessageBox::Yes
      )
     return;
