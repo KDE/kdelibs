9e4d7d8ff39e347e4fcc9f53c7d521a0301d0774
Josef Spillner <spillner@kde.org>
2005-01-14 17:16:55 +0000

    - since 3.4 (sigh)
    svn path=/trunk/kdelibs/; revision=378472

--- knewstuff/downloaddialog.h
+++ knewstuff/downloaddialog.h
@@ -101,6 +101,8 @@ class KDE_EXPORT DownloadDialog : public KDialogBase
       the application configuration.
 
       @param providerList the URL of the provider list
+
+      @since 3.4
     */
     void setProviderList(const QString& providerList);
 
