3b01f4673f32050511defe3d8c12399c0077b023
Stephan Kulow <coolo@kde.org>
2005-01-16 12:57:24 +0000

    ignore QGroupBoxes and all widgets that were given to KAccelManager::setNoAccel (this is for all submenus and titles that are abused as pure strings)
    svn path=/trunk/kdelibs/; revision=379057

--- kdecore/kaccelmanager.cpp
+++ kdecore/kaccelmanager.cpp
@@ -98,6 +98,7 @@ public:
     static QString changed_string;
     static QString added_string;
     static QString removed_string;
+    static QMap<QWidget *, int> ignored_widgets;
 
 private:
   class Item;
@@ -137,6 +138,7 @@ QString KAcceleratorManagerPrivate::added_string;
 QString KAcceleratorManagerPrivate::removed_string;
 static QStringList *kaccmp_sns = 0;
 static KStaticDeleter<QStringList> kaccmp_sns_d;
+QMap<QWidget*, int> KAcceleratorManagerPrivate::ignored_widgets;
 
 bool KAcceleratorManagerPrivate::standardName(const QString &str)
 {
@@ -264,6 +266,9 @@ void KAcceleratorManagerPrivate::traverseChildren(QWidget *widget, Item *item)
     if ( !w->isVisibleTo( widget ) || w->isTopLevel() )
         continue;
 
+    if ( KAcceleratorManagerPrivate::ignored_widgets.find( w ) != KAcceleratorManagerPrivate::ignored_widgets.end() )
+        continue;
+
     manageWidget(w, item);
   }
   delete childList;
@@ -311,7 +316,7 @@ void KAcceleratorManagerPrivate::manageWidget(QWidget *w, Item *item)
 
   if (dynamic_cast<QComboBox*>(w) || dynamic_cast<QLineEdit*>(w) ||
       dynamic_cast<QTextEdit*>(w) || dynamic_cast<QTextView*>(w) ||
-      dynamic_cast<QSpinBox*>(w))
+      dynamic_cast<QSpinBox*>(w) || dynamic_cast<QGroupBox*>(w))
       return;
 
   // now treat 'ordinary' widgets
@@ -725,7 +730,7 @@ void KPopupAccelManager::aboutToShow()
 {
   // Note: we try to be smart and avoid recalculating the accelerators
   // whenever possible. Unfortunately, there is no way to know if an
-  // item has been added or removed, so we can not do much more than
+ // item has been added or removed, so we can not do much more than
   // to compare the items each time the menu is shown :-(
 
   if (m_count != (int)m_popup->count())
@@ -844,6 +849,11 @@ void QWidgetStackAccelManager::aboutToShow(QWidget *child)
     }
 
     child->installEventFilter( this );
+}
+
+void KAcceleratorManager::setNoAccel( QWidget *widget )
+{
+    KAcceleratorManagerPrivate::ignored_widgets[widget] = 1;
 }
 
 #include "kaccelmanager_private.moc"
--- kdecore/kaccelmanager.h
+++ kdecore/kaccelmanager.h
@@ -41,41 +41,46 @@ class KDECORE_EXPORT KAcceleratorManager
 {
 public:
 
-  /**
-   * Manages the accelerators of a widget.
-   *
-   * Call this function on the top widget of the hierarchy you
-   * want to manage. It will fix the accelerators of the child widgets so
-   * there are never duplicate accelerators. It also tries to put
-   * accelerators on as many widgets as possible.
-   *
-   * The algorithm used tries to take the existing accelerators into
-   * account, as well as the class of each widget. Hopefully, the result
-   * is close to what you would assign manually.
-   *
-   * QPopupMenu's are managed dynamically, so when you add or remove entries,
-   * the accelerators are reassigned. If you add or remove widgets to your
-   * toplevel widget, you will have to call manage again to fix the
-   * accelerators.
-   *
-   * @param widget The toplevel widget you want to manage.
-   */
+    /**
+     * Manages the accelerators of a widget.
+     *
+     * Call this function on the top widget of the hierarchy you
+     * want to manage. It will fix the accelerators of the child widgets so
+     * there are never duplicate accelerators. It also tries to put
+     * accelerators on as many widgets as possible.
+     *
+     * The algorithm used tries to take the existing accelerators into
+     * account, as well as the class of each widget. Hopefully, the result
+     * is close to what you would assign manually.
+     *
+     * QPopupMenu's are managed dynamically, so when you add or remove entries,
+     * the accelerators are reassigned. If you add or remove widgets to your
+     * toplevel widget, you will have to call manage again to fix the
+     * accelerators.
+     *
+     * @param widget The toplevel widget you want to manage.
+     */
 
-  static void manage(QWidget *widget);
+    static void manage(QWidget *widget);
 
-  /**
-   * Does the same as the above function, but puts hints in the GUI so the
-   * programmer can enhance his program.
-   *
-   * TODO KDE4: merge with the above
-   * @param widget The toplevel widget you want to manage.
-   * @param programmers_mode if true, KAcceleratorManager adds (&) for removed
-   *             accels and & before added accels
-   */
-  static void manage(QWidget *widget, bool programmers_mode);
+    /**
+     * Does the same as the above function, but puts hints in the GUI so the
+     * programmer can enhance his program.
+     *
+     * TODO KDE4: merge with the above
+     * @param widget The toplevel widget you want to manage.
+     * @param programmers_mode if true, KAcceleratorManager adds (&) for removed
+     *             accels and & before added accels
+     */
+    static void manage(QWidget *widget, bool programmers_mode);
 
     /** \internal returns the result of the last manage operation. */
     static void last_manage(QString &added,  QString &changed,  QString &removed);
+
+    /**
+     * Use this method for a widget (and it's children) you want no accels to be set on.
+     */
+    static void setNoAccel( QWidget *widget );
 };
 
 
