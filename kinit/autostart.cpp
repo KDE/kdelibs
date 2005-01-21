6f13fb2da4a0ef2790854b0577b369ece1e5fb34
Waldo Bastian <bastian@kde.org>
2005-01-21 14:29:01 +0000

    Use read-only config file, don't read kdeglobals to check start condition
    svn path=/trunk/kdelibs/; revision=380800

--- kinit/autostart.cpp
+++ kinit/autostart.cpp
@@ -92,7 +92,7 @@ static bool startCondition(const QString &condition)
   if (list[0].isEmpty() || list[2].isEmpty()) 
      return true;
 
-  KConfig config(list[0]);
+  KConfig config(list[0], true, false);
   if (!list[1].isEmpty())
      config.setGroup(list[1]);
 
