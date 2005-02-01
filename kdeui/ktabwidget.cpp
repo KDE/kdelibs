03ebcf8a34ca2696bf0b98130a08a47bcca86cd5
Maks Orlovich <maksim@kde.org>
2005-02-01 04:56:41 +0000

    Fix for http://lists.kde.org/?l=kde-devel&m=110709489715924&w=2 If you pass a widget pointer to these pixelMetric primitives it /must/ be a QTabBar*, see the docs to QStyle::pixelMetric
    CCMAIL:jmolles@vodafone.es
    
    svn path=/trunk/kdelibs/; revision=384843

--- kdeui/ktabwidget.cpp
+++ kdeui/ktabwidget.cpp
@@ -160,8 +160,8 @@ bool KTabWidget::tabCloseActivatePrevious() const
 unsigned int KTabWidget::tabBarWidthForMaxChars( uint maxLength )
 {
     int hframe, overlap;
-    hframe  = tabBar()->style().pixelMetric( QStyle::PM_TabBarTabHSpace, this );
-    overlap = tabBar()->style().pixelMetric( QStyle::PM_TabBarTabOverlap, this );
+    hframe  = tabBar()->style().pixelMetric( QStyle::PM_TabBarTabHSpace, tabBar() );
+    overlap = tabBar()->style().pixelMetric( QStyle::PM_TabBarTabOverlap, tabBar() );
 
     QFontMetrics fm = tabBar()->fontMetrics();
     int x = 0;
