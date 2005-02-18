15a8766cea333a5c02af5eb214683734206c1b54
Waldo Bastian <bastian@kde.org>
2005-02-18 12:36:33 +0000

    Fix printing of QByteArray
    svn path=/trunk/kdelibs/; revision=390536

--- kdecore/kdebug.cpp
+++ kdecore/kdebug.cpp
@@ -540,7 +540,7 @@ kdbgstream& kdbgstream::operator<<( const QByteArray& data) {
     unsigned int i = 0;
     unsigned int sz = QMIN( data.size(), 64 );
     for ( ; i < sz ; ++i ) {
-        output += QString::number( data[i], 16 ).rightJustify(2, '0');
+        output += QString::number( (unsigned char) data[i], 16 ).rightJustify(2, '0');
         if ( i < sz )
             output += ' ';
     }
