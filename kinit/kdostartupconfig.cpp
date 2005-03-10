82cad107f76dc21071f3ab907875917efc6bf4c3
Luboš Luňák <l.lunak@kde.org>
2005-03-10 18:10:16 +0000

    Some setup work simply needs to be done very early in startkde, even before the splash is shown. Otherwise the splash has wrong geometry if krandr changes resolution later, or kded and ksmserver don't have the right cursor theme. On the other hand launching the very first KDE app takes some time because of the loading of all the KDE libraries, which would kinda suck especially in cases like having a nice Xlib-only splash that would otherwise launch very quickly. So here comes kstartupconfig, which is plain C++, and uses a cache of certain specified KConfig options (updated by kdostartupconfig, which is a KDE app, but oh well). The options are then sourced, and are available as normal shell variables.
    svn path=/trunk/kdelibs/; revision=396411

---
+++ kinit/kdostartupconfig.cpp
@@ -0,0 +1,118 @@
+/****************************************************************************
+
+ Copyright (C) 2005 Lubos Lunak        <l.lunak@kde.org>
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+
+****************************************************************************/
+
+#undef QT_NO_CAST_ASCII
+
+#include <qfile.h>
+#include <qtextstream.h>
+#include <kinstance.h>
+#include <kstandarddirs.h>
+#include <kconfig.h>
+#include <kdebug.h>
+
+QString get_entry( QString* ll )
+    {
+    QString& l = *ll;
+    l = l.stripWhiteSpace();
+    if( l.isEmpty())
+        return QString::null;
+    QString ret;
+    if( l[ 0 ] == '\'' )
+        {
+        unsigned int pos = 1;
+        while( pos < l.length() && l[ pos ] != '\'' )
+            ret += l[ pos++ ];
+        if( pos >= l.length())
+            {
+            *ll = QString::null;
+            return QString::null;
+            }
+        *ll = l.mid( pos + 1 );
+        return ret;
+        }
+    unsigned int pos = 0;
+    while( pos < l.length() && l[ pos ] != ' ' )
+        ret += l[ pos++ ];
+    *ll = l.mid( pos );
+    return ret;
+    }
+
+int main()
+    {
+    KInstance inst( "kdostartupconfig" );
+    kdDebug() << "Running kdostartupconfig." << endl;
+    QString keysname = locateLocal( "config", "startupconfigkeys" );
+    QFile keys( keysname );
+    if( !keys.open( IO_ReadOnly ))
+        return 3;
+    QFile f1( locateLocal( "config", "startupconfig" ));
+    if( !f1.open( IO_WriteOnly ))
+        return 4;
+    QFile f2( locateLocal( "config", "startupconfigfiles" ));
+    if( !f2.open( IO_WriteOnly ))
+        return 5;
+    QTextStream startupconfig( &f1 );
+    QTextStream startupconfigfiles( &f2 );
+    startupconfig << "#! /bin/sh\n";
+    for(;;)
+        {
+        QString line;
+        if( keys.readLine( line, 1024 ) < 0 )
+            break;
+        line = line.stripWhiteSpace();
+        if( line.isEmpty())
+            break;
+        QString tmp = line;
+        QString file, group, key, def;
+        file = get_entry( &tmp );
+        group = get_entry( &tmp );
+        key = get_entry( &tmp );
+        def = get_entry( &tmp );
+        if( file.isEmpty() || group.isEmpty() || key.isEmpty())
+            return 6;
+        KConfig cfg( file );
+        cfg.setGroup( group );
+        QString value = cfg.readEntry( key, def );
+        startupconfig << "# " << line << "\n";
+        startupconfig << file.replace( ' ', '_' ).lower()
+            << "_" << group.replace( ' ', '_' ).lower()
+            << "_" << key.replace( ' ', '_' ).lower()
+            << "=\"" << value.replace( "\"", "\\\"" ) << "\"\n";
+        startupconfigfiles << line << endl;
+        // use even currently non-existing paths in $KDEDIRS
+        QStringList dirs = QStringList::split( KPATH_SEPARATOR, KGlobal::dirs()->kfsstnd_prefixes());
+        for( QStringList::ConstIterator it = dirs.begin();
+             it != dirs.end();
+             ++it )
+            {
+            QString cfg = *it + "share/config/" + file;
+            if( KStandardDirs::exists( cfg ))
+                startupconfigfiles << cfg << "\n";
+            else
+                startupconfigfiles << "!" << cfg << "\n";
+            }
+        startupconfigfiles << "*\n";
+        }
+    return 0;
+    }
---
+++ kinit/kstartupconfig.cpp
@@ -0,0 +1,171 @@
+/****************************************************************************
+
+ Copyright (C) 2005 Lubos Lunak        <l.lunak@kde.org>
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+
+****************************************************************************/
+
+/*
+
+This utility helps to have some configuration options available in startkde
+without the need to launch anything linked to KDE libraries (which may need
+some time to load).
+
+The configuration options are written to $KDEHOME/share/config/startupconfigkeys,
+one option per line, as <file> <group> <key> <default>. It is possible to
+use ' for quoting multiword entries. Values of these options will be written
+to $KDEHOME/share/config/startupconfig as a shell script that will set
+the values to shell variables, named <file>_<group>_<key> (all spaces replaced
+by underscores, everything lowercase). So e.g. line
+"ksplashrc KSplash Theme Default" may result in "ksplashrc_ksplash_theme=Default".
+
+When a kconf_update script is used to update such option, kstartupconfig is run
+before kconf_update and therefore cannot see the change in time. To avoid this
+problem, together with the kconf_update script also the matching global config
+file should be updated (any change, kstartupconfig will see the timestamp change).
+
+Note that the kdeglobals config file is not used as a depedendency for other config
+files.
+
+Since the checking is timestamp-based, config files that are frequently updated
+should not be used.
+
+It is required to pass $KDEHOME from startkde as the argument for kstartupconfig.
+
+Kstartupconfig works by storing every line from startupconfigkeys in file startupconfigfiles
+followed by paths of all files that are relevant to the option. Non-existent files
+have '!' prepended (for the case they'll be later created), the list of files is
+terminated by line containing '*'. If the timestamps of all relevant files are older
+than the timestamp of the startupconfigfile file, there's no need to update anything.
+Otherwise kdostartupconfig is launched to create or update all the necessary files
+(which already requires loading KDE libraries, but this case should be rare).
+
+*/
+
+#include <config.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main()
+    {
+    char kdehome[ 1024 ];
+    if( getenv( "KDEHOME" ))
+        strlcpy( kdehome, getenv( "KDEHOME" ), 1024 );
+    else if( getenv( "HOME" ))
+        {
+        strlcpy( kdehome, getenv( "HOME" ), 1024 );
+        strlcat( kdehome, "/.kde", 1024  );
+        }
+    else
+        return 1;
+    char filename[ 1024 ];
+    strlcpy( filename, kdehome, 1024 );
+    strlcat( filename, "/share/config/startupconfig", 1024 );
+    if( access( filename, R_OK ) != 0 )
+        {
+        int ret = system( "kdostartupconfig" );
+        return WEXITSTATUS( ret );
+        }
+    strlcpy( filename, kdehome, 1024 );
+    strlcat( filename, "/share/config/startupconfigfiles", 1024 );
+    struct stat st;
+    if( stat( filename, &st ) != 0 )
+        {
+        int ret = system( "kdostartupconfig" );
+        return WEXITSTATUS( ret );
+        }
+    time_t config_time = st.st_mtime;
+    FILE* config = fopen( filename, "r" );
+    if( config == NULL )
+        {
+        int ret = system( "kdostartupconfig" );
+        return WEXITSTATUS( ret );
+        }
+    strlcpy( filename, kdehome, 1024 );
+    strlcat( filename, "/share/config/startupconfigkeys", 1024 );
+    FILE* keys = fopen( filename, "r" );
+    if( keys == NULL )
+        {
+        fclose( config );
+        return 2;
+        }
+    bool need_update = true;
+    for(;;)
+        {
+        char keyline[ 1024 ];
+        if( fgets( keyline, 1023, keys ) == NULL )
+            {
+            need_update = false;
+            break;
+            }
+        if( char* nl = strchr( keyline, '\n' ))
+            *nl = '\0';
+        char line[ 1024 ];
+        if( fgets( line, 1023, config ) == NULL )
+            break;
+        if( char* nl = strchr( line, '\n' ))
+            *nl = '\0';
+        if( strcmp( keyline, line ) != 0 )
+            break;
+        bool ok = false;
+        for(;;)
+            {
+            if( fgets( line, 1023, config ) == NULL )
+                break;
+            if( char* nl = strchr( line, '\n' ))
+                *nl = '\0';
+            if( *line == '\0' )
+                break;
+            if( *line == '*' )
+                {
+                ok = true;
+                break;
+                }
+            if( *line == '!' )
+                {
+                if( access( line + 1, R_OK ) == 0 )
+                    break; // file now exists -> update
+                }
+            else
+                {
+                struct stat st;
+                if( stat( line, &st ) != 0 )
+                    break;
+                if( st.st_mtime > config_time )
+                    break;
+                }
+            }
+        if( !ok )
+            break;
+        }
+    fclose( keys );
+    fclose( config );
+    if( need_update )
+        {
+        int ret = system( "kdostartupconfig" );
+        return WEXITSTATUS( ret );
+        }
+    return 0;
+    }
