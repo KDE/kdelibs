2e64da432ff0929567ef58ce5611d56fd232a372
Dirk Mueller <mueller@kde.org>
2005-04-15 00:38:47 +0000

    fix leak
    svn path=/trunk/kdelibs/; revision=405650

--- kimgio/tiffr.cpp
+++ kimgio/tiffr.cpp
@@ -85,6 +85,7 @@ KDE_EXPORT void kimgio_tiff_read( QImageIO *io )
 
 	QImage image( width, height, 32 );
 	if( image.isNull()) {
+		TIFFClose( tiff );
 		return;
 	}
 	data = (uint32 *)image.bits();
