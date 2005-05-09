00ba69f2355d63ce3d4be58e79029f82c40b0fa4
Mickael Marchand <marchand@kde.org>
2005-05-09 11:20:11 +0000

    compile and link ;) (wow, we have one app with qt4 :)
    svn path=/branches/work/kde4/kdelibs/; revision=411461

--- dcop/dcopidl2cpp/skel.cpp
+++ dcop/dcopidl2cpp/skel.cpp
@@ -25,6 +25,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include <qtextstream.h>
 #include <qstring.h>
 #include <qstringlist.h>
+#include <q3valuelist.h>
 
 #include <string.h>
 #include <stdlib.h>
