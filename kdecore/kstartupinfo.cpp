7f975e1afe28bb6b9d203ff83bb0a66c4ffe88b1
Simon Hausmann <hausmann@kde.org>
2005-05-14 11:08:25 +0000

    - use QX11Info::appUserTime()
    svn path=/branches/work/kde4/kdelibs/; revision=413687

--- kdecore/kstartupinfo.cpp
+++ kdecore/kstartupinfo.cpp
@@ -48,7 +48,7 @@ DEALINGS IN THE SOFTWARE.
 #include "kstartupinfo.h"
 
 // KDE4 TODO
-#include <QtGui/private/qt_x11_p.h>
+#include <QtGui/qx11info_x11.h>
 #define None 0
 #include <unistd.h>
 #include <sys/time.h>
@@ -936,7 +936,7 @@ QByteArray KStartupInfo::createNewStartupId()
     if (!gethostname( hostname, 255 ))
 	hostname[sizeof(hostname)-1] = '\0';
 #ifdef Q_WS_X11
-    long qt_x_user_time = X11->userTime;
+    long qt_x_user_time = QX11Info::appUserTime();
 #else
     long qt_x_user_time = 0;
 #endif
