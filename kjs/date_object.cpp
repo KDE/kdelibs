c2c2172459992dcf5764c84fd3cf31e7dd4dbb9b
Harri Porten <porten@kde.org>
2005-05-19 22:54:15 +0000

    testing svn write upon dirk's request
    svn path=/trunk/KDE/kdelibs/; revision=415912

--- kjs/date_object.cpp
+++ kjs/date_object.cpp
@@ -1,7 +1,7 @@
 // -*- c-basic-offset: 2 -*-
 /*
  *  This file is part of the KDE libraries
- *  Copyright (C) 1999-2000 Harri Porten (porten@kde.org)
+ *  Copyright (C) 1999-2005 Harri Porten (porten@kde.org)
  *  Copyright (C) 2003 Apple Computer, Inc.
  *
  *  This library is free software; you can redistribute it and/or
