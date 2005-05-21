928be2718c4179bfbf04fe5bf81ceea7636f1837
Stephan Kulow <coolo@kde.org>
2005-05-21 16:53:06 +0000

    avoid a second connect for now - fixing KIO. Thiago, you need to rethink that, you just can't connect twice to a KIO master that closes the server socket as soon as one client connects
    svn path=/branches/work/kde4/kdelibs/; revision=416470

--- kdecore/network/ksocketdevice.cpp
+++ kdecore/network/ksocketdevice.cpp
@@ -37,6 +37,7 @@
 #include <fcntl.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#include <kdebug.h>
 
 #ifdef HAVE_POLL
 # include <sys/poll.h>
@@ -57,6 +58,7 @@
 #include "ksocketbase.h"
 #include "ksocketdevice.h"
 #include "ksockssocketdevice.h"
+#include "kclientsocketbase.h"
 
 using namespace KNetwork;
 
@@ -87,6 +89,7 @@ KSocketDevice::KSocketDevice(int fd, OpenMode mode)
 {
   if (mode)
     mode |= Unbuffered;
+  kdDebug() << "KSocketDevice::KSocketDevice\n";
   KActiveSocketBase::open(mode);
   setSocketDevice(this);
 }
@@ -270,11 +273,16 @@ bool KSocketDevice::listen(int backlog)
 
 bool KSocketDevice::connect(const KResolverEntry& address, OpenMode mode)
 {
+  kdDebug() << "KSocketDevice::connect " << state() << " " << KClientSocketBase::Connected << endl;
   resetError();
+
+  if (m_sockfd >= 0)
+	return true;
 
   if (m_sockfd == -1 && !create(address))
     return false;		// failed creating!
 
+  kdDebug() << "kde_connect " << m_sockfd << endl;
   if (kde_connect(m_sockfd, address.address(), address.length()) == -1)
     {
       if (errno == EISCONN)
@@ -297,6 +305,7 @@ bool KSocketDevice::connect(const KResolverEntry& address, OpenMode mode)
       return false;
     }
 
+  kdDebug() << "KSocketDevice::connect\n";
   KActiveSocketBase::open(Unbuffered | mode);
   return true;			// all is well
 }
