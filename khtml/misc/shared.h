81933481246b7eee49b60c35331882ecb41cede5
Allan Sandfeld Jensen <kde@carewolf.com>
2005-06-08 10:55:50 +0000

    Actually previous commit needs this change as well.
    svn path=/trunk/KDE/kdelibs/; revision=423418

--- khtml/misc/shared.h
+++ khtml/misc/shared.h
@@ -10,10 +10,10 @@ public:
     ~Shared() { /*counter--;*/ }
 
     void ref() { _ref++;  }
-    void deref() { 
-	if(_ref) _ref--; 
+    void deref() {
+	if(_ref) _ref--;
 	if(!_ref)
-	    delete static_cast<type *>(this); 
+	    delete static_cast<type *>(this);
     }
     bool hasOneRef() { //kdDebug(300) << "ref=" << _ref << endl;
     	return _ref==1; }
@@ -32,10 +32,10 @@ public:
     ~TreeShared() { /*counter--;*/ }
 
     void ref() { _ref++;  }
-    void deref() { 
-	if(_ref) _ref--; 
+    void deref() {
+	if(_ref) _ref--;
 	if(!_ref && !m_parent) {
-	    delete static_cast<type *>(this); 
+	    delete static_cast<type *>(this);
 	}
     }
     bool hasOneRef() { //kdDebug(300) << "ref=" << _ref << endl;
@@ -51,6 +51,52 @@ private:
 protected:
     type *m_parent;
 };
+
+template <class T> class SharedPtr
+{
+public:
+    SharedPtr() : m_ptr(0) {}
+    explicit SharedPtr(T *ptr) : m_ptr(ptr) { if (m_ptr) m_ptr->ref(); }
+    SharedPtr(const SharedPtr &o) : m_ptr(o.m_ptr) { if (m_ptr) m_ptr->ref(); }
+    ~SharedPtr() { if (m_ptr) m_ptr->deref(); }
+
+    bool isNull() const { return m_ptr == 0; }
+
+    void reset() { if (m_ptr) m_ptr->deref(); m_ptr = 0; }
+    void reset(T *o) { if (o) o->ref(); if (m_ptr) m_ptr->deref(); m_ptr = o; }
+
+    T * get() const { return m_ptr; }
+    T &operator*() const { return *m_ptr; }
+    T *operator->() const { return m_ptr; }
+
+    bool operator!() const { return m_ptr == 0; }
+
+    inline friend bool operator==(const SharedPtr &a, const SharedPtr &b) { return a.m_ptr == b.m_ptr; }
+    inline friend bool operator==(const SharedPtr &a, const T *b) { return a.m_ptr == b; }
+    inline friend bool operator==(const T *a, const SharedPtr &b) { return a == b.m_ptr; }
+
+    SharedPtr &operator=(const SharedPtr &);
+
+private:
+    T* m_ptr;
+};
+
+template <class T> SharedPtr<T> &SharedPtr<T>::operator=(const SharedPtr<T> &o)
+{
+    if (o.m_ptr)
+        o.m_ptr->ref();
+    if (m_ptr)
+        m_ptr->deref();
+    m_ptr = o.m_ptr;
+    return *this;
+}
+
+template <class T> inline bool operator!=(const SharedPtr<T> &a, const SharedPtr<T> &b) { return !(a==b); }
+template <class T> inline bool operator!=(const SharedPtr<T> &a, const T *b) { return !(a == b); }
+template <class T> inline bool operator!=(const T *a, const SharedPtr<T> &b) { return !(a == b); }
+
+template <class T, class U> inline SharedPtr<T> static_pointer_cast(const SharedPtr<U> &p) { return SharedPtr<T>(static_cast<T *>(p.get())); }
+template <class T, class U> inline SharedPtr<T> const_pointer_cast(const SharedPtr<U> &p) { return SharedPtr<T>(const_cast<T *>(p.get())); }
 
 } // namespace
 
