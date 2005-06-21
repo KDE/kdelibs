c545565033dc19474d66e6bfe3a3f850c7ac518e
David Faure <faure@kde.org>
2005-06-21 20:06:21 +0000

    Keep the full Exec line from the existing service, when checking "remember application association for this type of file". Bugs 107860, 107335 (will close after backporting)
    svn path=/trunk/KDE/kdelibs/; revision=427779

--- kio/kfile/kopenwith.cpp
+++ kio/kfile/kopenwith.cpp
@@ -622,7 +622,8 @@ void KOpenWithDlg::setSaveNewApplications(bool b)
 
 void KOpenWithDlg::slotOK()
 {
-  QString fullExec(edit->url());
+  QString typedExec(edit->url());
+  QString fullExec(typedExec);
 
   QString serviceName;
   QString initialServiceName;
@@ -632,7 +633,7 @@ void KOpenWithDlg::slotOK()
     // No service selected - check the command line
 
     // Find out the name of the service from the command line, removing args and paths
-    serviceName = KRun::binaryName( fullExec, true );
+    serviceName = KRun::binaryName( typedExec, true );
     if (serviceName.isEmpty())
     {
       // TODO add a KMessageBox::error here after the end of the message freeze
@@ -651,6 +652,7 @@ void KOpenWithDlg::slotOK()
         if ( serv && serv->type() == "Application")
         {
             QString exec = serv->exec();
+            fullExec = exec;
             exec.replace("%u", "", false);
             exec.replace("%f", "", false);
             exec.replace("-caption %c", "");
@@ -658,7 +660,7 @@ void KOpenWithDlg::slotOK()
             exec.replace("%i", "");
             exec.replace("%m", "");
             exec = exec.simplifyWhiteSpace();
-            if (exec == fullExec)
+            if (exec == typedExec)
             {
                 ok = true;
                 m_pService = serv;
@@ -678,6 +680,7 @@ void KOpenWithDlg::slotOK()
     // Existing service selected
     serviceName = m_pService->name();
     initialServiceName = serviceName;
+    fullExec = m_pService->exec();
   }
 
   if (terminal->isChecked())
