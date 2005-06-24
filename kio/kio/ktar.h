2477a1a7bfce7e74a5b4727dc2d88dce3a192af4
Nicolas Goutte <nicolasg@snafu.de>
2005-06-24 23:08:51 +0000

    Improve Doxygen output for the KTar class (Use \warning so that the output looks better.)
    svn path=/trunk/KDE/kdelibs/; revision=428700

--- kio/kio/ktar.h
+++ kio/kio/ktar.h
@@ -55,7 +55,7 @@ public:
     /**
      * Creates an instance that operates on the given device.
      * The device can be compressed (KFilterDev) or not (QFile, etc.).
-     * WARNING: don't assume that giving a QFile here will decompress the file,
+     * @warning Do not assume that giving a QFile here will decompress the file,
      * in case it's compressed!
      * @param dev the device to read from. If the source is compressed, the
      * QIODevice must take care of decompression
