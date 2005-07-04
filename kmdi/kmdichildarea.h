eb3f52a4ca4dab97831a554e6d167922d604adc9
Allen Winter <winter@kde.org>
2005-07-04 14:25:02 +0000

    minor changes to make doxygen happy.
    svn path=/trunk/KDE/kdelibs/; revision=431498

--- kmdi/kmdichildarea.h
+++ kmdi/kmdichildarea.h
@@ -72,22 +72,22 @@ protected:
 	 * The MDI childframe window caption font
 	 */
 	QFont m_captionFont;
-	
+
 	/**
 	 * The foreground color of the active MDI childframe window caption
 	 */
 	QColor m_captionActiveBackColor;
-	
+
 	/**
 	 * The background color of the active MDI childframe window captions
 	 */
 	QColor m_captionActiveForeColor;
-	
+
 	/**
 	 * The foreground color of inactive MDI childframe window captions
 	 */
 	QColor m_captionInactiveBackColor;
-	
+
 	/**
 	 * The background color of inactive MDI childframe window captions
 	 */
@@ -97,12 +97,12 @@ protected:
 
 	// methods
 public:
-	
+
 	/**
 	 * Consruction. Note: This class needn't to know about KMdiMainFrm .
 	 */
 	KMdiChildArea( QWidget *parent );
-	
+
 	/**
 	 * Destructor : THERE should be no child windows anymore...
 	 * Howewer it simply deletes all the child widgets :)
@@ -114,76 +114,74 @@ public:
 	 * The child is shown,raised and gets focus if this window has it.
 	 */
 	void manageChild( KMdiChildFrm *lpC, bool bShow = true, bool bCascade = true );
-	
+
 	/**
 	 * Destroys a managed KMdiChildFrm
 	 * Also deletes the client attached to this child.
 	 */
 	void destroyChild( KMdiChildFrm* child, bool focusTopChild = true );
-	
+
 	/**
 	 * Destroys a managed KMdiChildFrm
 	 * Clients attached to the KMdiChildFrm are not deleted.
-	 * @param child
 	 */
 	void destroyChildButNotItsView( KMdiChildFrm *lpC, bool bFocusTopChild = true );
-	
+
 	/**
-	 * Brings the child @p child to the top of the stack
-	 * The child is focused if @p setFocus is true. If setFocus is false, the
+	 * Brings the child to the top of the stack
+	 * The child is focused if setFocus is true. If setFocus is false, the
 	 * child is just raised.
-	 * @param child 
 	 */
 	void setTopChild( KMdiChildFrm* child, bool setFocus = false );
-	
+
 	/**
 	 * Returns the topmost child (the active one) or 0 if there are no children.
 	 * Note that the topmost child may be also hidded , if ALL the windows are minimized.
 	 */
 	inline KMdiChildFrm * topChild() const { return m_pZ->last(); }
-	
+
 	/**
 	 * Returns the number of visible children
 	 */
 	int getVisibleChildCount() const;
-	
+
 	/**
 	 * Calculates the cascade point for the given index. If index is -1
 	 * the cascade point is calculated for the window following the last window
 	 * @param indexOfWindow the index of the window in relation the z-ordered window list
 	 */
 	QPoint getCascadePoint( int indexOfWindow = -1 );
-	
+
 	/**
 	 * Sets the MDI childframe window caption font
 	 * A relayout does not occur when using this function
 	 */
 	void setMdiCaptionFont( const QFont &fnt );
-	
+
 	/**
 	 * Sets the foreground color of the active MDI childframe window caption
 	 * A relayout does not occur when using this function
 	 */
 	void setMdiCaptionActiveForeColor( const QColor &clr );
-	
+
 	/**
 	 * Sets the background color of the active MDI childframe window captions
 	 * A relayout does not occur when using this function
 	 */
 	void setMdiCaptionActiveBackColor( const QColor &clr );
-	
+
 	/**
 	 * Sets the foreground color of inactive MDI childframe window captions
 	 * A relayout does not occur when using this function
 	 */
 	void setMdiCaptionInactiveForeColor( const QColor &clr );
-	
+
 	/**
 	 * Sets the background color of inactive MDI childframe window captions
 	 * A relayout does not occur when using this function
 	 */
 	void setMdiCaptionInactiveBackColor( const QColor &clr );
-	
+
 	/**
 	 * Gets all caption colors, consistent with current WM settings
 	 * (or other Desktop settings e.g. system settings for win32)
@@ -201,65 +199,65 @@ public slots:
 	 * Cascades all windows resizing them to the minimum size.
 	 */
 	void cascadeWindows();
-	
+
 	/**
 	 * Cascades all windows resizing them to the maximum available size.
 	 */
 	void cascadeMaximized();
-	
+
 	/**
 	 * Maximize all windows but only in vertical direction
 	 */
 	void expandVertical();
-	
+
 	/**
 	 * Maximize all windows but only in horizontal direction
 	 */
 	void expandHorizontal();
-	
+
 	/**
 	 * Gives focus to the topmost child if it doesn't get focus
 	 * automatically or you want to wait to give it focus
 	 */
 	void focusTopChild();
-	
+
 	/**
 	 * Tile Pragma
 	 */
 	void tilePragma();
-	
+
 	/**
 	 * Tile Anodine
 	 */
 	void tileAnodine();
-	
+
 	/**
 	 * Tile all the windows in the child area vertically
 	 */
 	void tileVertically();
-	
+
 	/**
 	 * Position and layout the minimized child frames
 	 */
 	void layoutMinimizedChildren();
 
 protected:
-	
+
 	/**
 	 * Internally used for the tile algorithm
 	 */
 	void tileAllInternal( int maxWnds );
-	
+
 	/**
 	 * Automatically resizes a maximized MDI view and layouts the positions of minimized MDI views.
 	 */
 	virtual void resizeEvent( QResizeEvent * );
-	
+
 	/**
 	 * Shows the 'Window' popup menu on right mouse button click
 	 */
 	void mousePressEvent( QMouseEvent *e );
-	
+
 	/**
 	 * Internally used. Actions that are necessary when an MDI view gets minimized
 	 */
@@ -270,12 +268,12 @@ signals:
 	 * Signals that there aren't maximized child frames any more
 	 */
 	void noMaximizedChildFrmLeft( KMdiChildFrm* );
-	
+
 	/**
 	 * Signals that the child frames are maximized now
 	 */
 	void nowMaximized( bool );
-	
+
 	/**
 	 * Signals a KMdiMainFrm that the signal/slot connections of the system buttons in the
 	 * menubar (only in Maximize mode) must be updated to another MDI view because the focused
@@ -283,13 +281,13 @@ signals:
 	 * @internal
 	 */
 	void sysButtonConnectionsMustChange( KMdiChildFrm*, KMdiChildFrm* );
-	
+
 	/**
 	 * Signals a KMdiMainFrm that the 'Window' popup menu must be shown
 	 * @internal
 	 */
 	void popupWindowMenu( QPoint );
-	
+
 	/**
 	 * Signals that the last attached (docked) MDI view has been closed.
 	 * Note: Detached MDI views can remain.
