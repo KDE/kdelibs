cee3616de4009a6cb3aa84fdbd94c324c6ce8f35
David Faure <faure@kde.org>
2005-07-05 08:25:09 +0000

    Use family names, patch by Heinrich Wendel, #98392
    svn path=/trunk/KDE/kdelibs/; revision=431782

--- khtml/khtmldefaults.h
+++ khtml/khtmldefaults.h
@@ -24,14 +24,10 @@
 #define HTML_DEFAULT_VLNK_COLOR Qt::magenta
 #define HTML_DEFAULT_BASE_COLOR Qt::white
 
-// KEEP IN SYNC WITH konqdefaults.h in kdebase/libkonq!
-// lets be modern .. -- Bernd
-#define HTML_DEFAULT_VIEW_FONT "helvetica"
-#define HTML_DEFAULT_VIEW_FIXED_FONT "courier"
-// generic CSS fonts. Since usual X distributions don't have a good set of fonts, this
-// is quite conservative...
-#define HTML_DEFAULT_VIEW_SERIF_FONT "times"
-#define HTML_DEFAULT_VIEW_SANSSERIF_FONT "helvetica"
-#define HTML_DEFAULT_VIEW_CURSIVE_FONT "helvetica"
-#define HTML_DEFAULT_VIEW_FANTASY_FONT "helvetica"
+#define HTML_DEFAULT_VIEW_FONT "Sans Serif"
+#define HTML_DEFAULT_VIEW_FIXED_FONT "Monospace"
+#define HTML_DEFAULT_VIEW_SERIF_FONT "Sans"
+#define HTML_DEFAULT_VIEW_SANSSERIF_FONT "Sans Serif"
+#define HTML_DEFAULT_VIEW_CURSIVE_FONT "Sans Serif"
+#define HTML_DEFAULT_VIEW_FANTASY_FONT "Sans Serif"
 #define HTML_DEFAULT_MIN_FONT_SIZE 7 // everything smaller is usually unreadable.
