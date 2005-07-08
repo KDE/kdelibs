bf26c2013143fc5c589df54072eb22f372e589c0
Stephan Binner <binner@kde.org>
2005-07-08 19:31:49 +0000

    Replaced several Yes/No dialog buttons
    svn path=/trunk/KDE/kdebase/kcontrol/crypto/; revision=432857

--- security/crypto/crypto.cpp
+++ security/crypto/crypto.cpp
@@ -1603,7 +1603,7 @@ TryImportPassAgain:
    cert = KSSLPKCS12::loadCertFile(certFile, QString(pass));
 
    if (!cert) {
-      rc = KMessageBox::warningYesNo(this, i18n("The certificate file could not be loaded. Try a different password?"), i18n("SSL"));
+      rc = KMessageBox::warningYesNo(this, i18n("The certificate file could not be loaded. Try a different password?"), i18n("SSL"),i18n("Try"),i18n("Do Not Try"));
       if (rc == KMessageBox::Yes) goto TryImportPassAgain;
       return;
    }
@@ -1621,8 +1621,8 @@ TryImportPassAgain:
                                                             i;
              i = static_cast<YourCertItem *>(i->nextSibling())) {
       if (i->configName() == name) {
-         rc = KMessageBox::warningYesNo(this, i18n("A certificate with that name already exists. Are you sure that you wish to replace it?"), i18n("SSL"));
-         if (rc == KMessageBox::No) {
+         rc = KMessageBox::warningContinueCancel(this, i18n("A certificate with that name already exists. Are you sure that you wish to replace it?"), i18n("SSL"), i18n("Replace"));
+         if (rc == KMessageBox::Cancel) {
             delete cert;
             return;
          }
@@ -2037,7 +2037,7 @@ void KCryptoConfig::slotCAImport() {
 
 void KCryptoConfig::offerImportToKMail( const QString& certFile )
 {
-    if ( KMessageBox::questionYesNo( this, i18n( "Do you want to make this certificate available to KMail as well?" ) ) == KMessageBox::Yes ) {
+    if ( KMessageBox::questionYesNo( this, i18n( "Do you want to make this certificate available to KMail as well?" ), QString::null, i18n("Make Available"), i18n("Do Not Make Available") ) == KMessageBox::Yes ) {
        KProcess proc;
        proc << "kleopatra";
        proc << "--import-certificate";
@@ -2061,8 +2061,8 @@ CAItem *x = static_cast<CAItem *>(caList->selectedItem());
 
 void KCryptoConfig::slotCARestore() {
 
- int rc = KMessageBox::warningYesNo(this, i18n("This will revert your certificate signers database to the KDE default.\nThis operation cannot be undone.\nAre you sure you wish to continue?"), i18n("SSL"));
-      if (rc == KMessageBox::No) {
+ int rc = KMessageBox::warningContinueCancel(this, i18n("This will revert your certificate signers database to the KDE default.\nThis operation cannot be undone.\nAre you sure you wish to continue?"), i18n("SSL"),i18n("Revert"));
+      if (rc == KMessageBox::Cancel) {
           return;
       }
 
