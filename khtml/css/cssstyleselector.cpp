5c1842140e222bcad1f53b26339b63aee5a2563b
Allan Sandfeld Jensen <kde@carewolf.com>
2005-07-11 13:08:44 +0000

    PRE should not start its own alignment, and TH should only CENTER if not inheriting. BUG: 79498 BUG: 42906
    svn path=/trunk/KDE/kdelibs/; revision=433700

--- khtml/css/cssstyleselector.cpp
+++ khtml/css/cssstyleselector.cpp
@@ -564,6 +564,10 @@ void CSSStyleSelector::adjustRenderStyle(RenderStyle* style, DOM::ElementImpl *e
 //                 style->setDisplay(style->isDisplayInlineType() ? INLINE_TABLE : TABLE);
         }
 
+        // Table headers with a text-align of auto will change the text-align to center.
+        if (e && e->id() == ID_TH && style->textAlign() == TAAUTO)
+            style->setTextAlign(CENTER);
+
         // Mutate the display to BLOCK or TABLE for certain cases, e.g., if someone attempts to
         // position or float an inline, compact, or run-in.  Cache the original display, since it
         // may be needed for positioned elements that have to compute their static normal flow
