63dd8c734890f6484ec6aefe6c55252d9fdd0a20
Maks Orlovich <maksim@kde.org>
2005-07-16 18:40:50 +0000

    Qt4 doesn't like negative length timers, so don't ask for those
    svn path=/branches/work/kde4/kdelibs/; revision=435369

--- khtml/khtml_part.cpp
+++ khtml/khtml_part.cpp
@@ -2241,7 +2241,7 @@ void KHTMLPart::checkCompleted()
     // deferred until the parent emits a completed signal.
     if ( parentPart() == 0 ) {
       //kdDebug(6050) << this << " starting redirection timer" << endl;
-      d->m_redirectionTimer.start( 1000 * d->m_delayRedirect, true );
+      d->m_redirectionTimer.start( kMax(0, 1000 * d->m_delayRedirect), true );
     } else {
       //kdDebug(6050) << this << " not toplevel -> not starting redirection timer. Waiting for slotParentCompleted." << endl;
     }
@@ -4977,7 +4977,7 @@ void KHTMLPart::slotParentCompleted()
   if ( !d->m_redirectURL.isEmpty() && !d->m_redirectionTimer.isActive() )
   {
     //kdDebug(6050) << this << ": starting timer for child redirection -> " << d->m_redirectURL << endl;
-    d->m_redirectionTimer.start( 1000 * d->m_delayRedirect, true );
+    d->m_redirectionTimer.start( kMax(0, 1000 * d->m_delayRedirect), true );
   }
 }
 
