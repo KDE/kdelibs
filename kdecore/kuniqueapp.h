31107ac849daf2102e50d1b4e1bde6faf27c970b
Caleb Tennis <caleb@gentoo.org>
2005-07-28 16:01:04 +0000

    kuniqueapp.h no longer needed
    svn path=/trunk/KDE/kdelibs/; revision=439663

--- kdecore/kuniqueapp.h
+++
@@ -1,6 +0,0 @@
-// kuniqueapp.h is the old name. Use #include <kuniqueapplication.h> from now on
-#ifdef KDE_NO_COMPAT
-#error kuniqueapp.h is the old name. Use #include <kuniqueapplication.h> from now on
-#else
-#include <kuniqueapplication.h>
-#endif
