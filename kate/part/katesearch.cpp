8be6016d4a4a8be579ab83857cec4d4aa4c68af6
Dirk Mueller <mueller@kde.org>
2005-07-29 02:35:51 +0000

    backport 413244
    svn path=/branches/KDE/3.4/kdelibs/; revision=439839

--- kate/part/katesearch.cpp
+++ kate/part/katesearch.cpp
@@ -130,6 +130,7 @@ void KateSearch::find( const QString &pattern, long flags, bool add, bool showno
   searchFlags.replace = false;
   searchFlags.finished = false;
   searchFlags.regExp = KateViewConfig::global()->searchFlags() & KFindDialog::RegularExpression;
+  searchFlags.useBackRefs = KateViewConfig::global()->searchFlags() & KReplaceDialog::BackReference;
 
   if ( searchFlags.selected )
   {
@@ -225,6 +226,7 @@ void KateSearch::findAgain( bool back )
   searchFlags.replace = false;
   searchFlags.finished = false;
   searchFlags.regExp = KateViewConfig::global()->searchFlags() & KFindDialog::RegularExpression;
+  searchFlags.useBackRefs = KateViewConfig::global()->searchFlags() & KReplaceDialog::BackReference;
 
   searchFlags.backward = searchFlags.backward != back;
   searchFlags.fromBeginning = false;
