5c3ddec5f5d1d40e3912d9097cf213a0192e2bc8
Michel Hermier <michel.hermier@gmail.com>
2005-07-29 15:06:54 +0000

    Remove some Qt < 3.2 compatibility code.
    svn path=/trunk/KDE/kdelibs/; revision=440018

--- dcop/kdatastream.h
+++ dcop/kdatastream.h
@@ -128,42 +128,5 @@ inline QDataStream & operator >> (QDataStream & str, DCOPCString& s)
     return str;
 }
 
-
-
-#if QT_VERSION < 0x030200 && !defined(Q_WS_WIN) && !defined(Q_WS_MAC)
-inline QDataStream & operator << (QDataStream & str, long long int ll)
-{
-  Q_UINT32 l1,l2;
-  l1 = ll & 0xffffffffLL;
-  l2 = ll >> 32;
-  str << l1 << l2;
-  return str;
-}
-
-inline QDataStream & operator >> (QDataStream & str, long long int&ll)
-{
-  Q_UINT32 l1,l2;
-  str >> l1 >> l2;
-  ll = ((unsigned long long int)(l2) << 32) + (long long int) l1;
-  return str;
-}
-
-inline QDataStream & operator << (QDataStream & str, unsigned long long int ll)
-{
-  Q_UINT32 l1,l2;
-  l1 = ll & 0xffffffffLL;
-  l2 = ll >> 32;
-  str << l1 << l2;
-  return str;
-}
-
-inline QDataStream & operator >> (QDataStream & str, unsigned long long int &ll)
-{
-  Q_UINT32 l1,l2;
-  str >> l1 >> l2;
-  ll = ((unsigned long long int)(l2) << 32) + (unsigned long long int) l1;
-  return str;
-}
 #endif
 
-#endif
