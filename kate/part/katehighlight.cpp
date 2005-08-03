2fc9ad8e99832de01e6a40155cc55814ae195ff3
Dominik Haumann <dhaumann@kde.org>
2005-08-03 14:55:17 +0000

    * allow "true" and "false" for "1" and "0".
    svn path=/branches/KDE/3.5/kdelibs/; revision=442669

--- kate/part/katehighlight.cpp
+++ kate/part/katehighlight.cpp
@@ -2183,7 +2183,7 @@ void KateHighlighting::readGlobalKeywordConfig()
   {
     kdDebug(13010)<<"Found global keyword config"<<endl;
 
-    if (KateHlManager::self()->syntax->groupItemData(data,QString("casesensitive"))!="0")
+    if ( IS_TRUE( KateHlManager::self()->syntax->groupItemData(data,QString("casesensitive")) ) )
       casesensitive=true;
     else
       casesensitive=false;
@@ -2287,10 +2287,10 @@ void KateHighlighting::readFoldingConfig()
   {
     kdDebug(13010)<<"Found global keyword config"<<endl;
 
-    if (KateHlManager::self()->syntax->groupItemData(data,QString("indentationsensitive"))!="1")
-      m_foldingIndentationSensitive=false;
-    else
+    if ( IS_TRUE( KateHlManager::self()->syntax->groupItemData(data,QString("indentationsensitive")) ) )
       m_foldingIndentationSensitive=true;
+    else
+      m_foldingIndentationSensitive=false;
 
     KateHlManager::self()->syntax->freeGroupInfo(data);
   }
@@ -2740,7 +2740,7 @@ int KateHighlighting::addToContextList(const QString &ident, int ctx0)
       {
         QString incCtx = KateHlManager::self()->syntax->groupItemData( data, QString("context"));
         QString incAttrib = KateHlManager::self()->syntax->groupItemData( data, QString("includeAttrib"));
-        bool includeAttrib = ( incAttrib.lower() == "true" || incAttrib.toInt() == 1 );
+        bool includeAttrib = IS_TRUE( incAttrib );
         // only context refernces of type NAME and ##Name are allowed
         if (incCtx.startsWith("##") || (!incCtx.startsWith("#")))
         {
