1d232a43d33b5a7c65eefbfba15bcd0e1b8cca31
Daniel Molkentin <molkentin@kde.org>
2005-09-05 23:48:57 +0000

    I could swear it compiled just right before my last commit..
    svn path=/trunk/KDE/kdelibs/; revision=457472

--- kdecore/kglobal.h
+++ kdecore/kglobal.h
@@ -209,12 +209,13 @@ public:
  */
 
 template<class T>
-inline T kClamp( const T& x, const T& low, const T& high ) KDE_DEPRECATED
+inline T kClamp( const T& x, const T& low, const T& high )
 {
     if ( x < low )       return low;
     else if ( high < x ) return high;
                          return x;
-}
+} KDE_DEPRECATED
+
 #endif
 
 /**
