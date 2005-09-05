48b2271c1d0575e37129b64ccbc33c737b853e88
Till Adam <adam@kde.org>
2005-09-05 12:19:11 +0000

    gpl -> lgpl
    svn path=/branches/KDE/3.5/kdelibs/; revision=457283

--- kio/kfile/kacleditwidget.cpp
+++ kio/kfile/kacleditwidget.cpp
@@ -3,9 +3,9 @@
  *                         Till Adam <adam@kde.org>                        *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
+ *   it under the terms of the GNU Library General Public License as       *
+ *   published by  the Free Software Foundation; either version 2 of the   *
+ *   License, or (at your option) any later version.                       *
  *                                                                         *
  *   This program is distributed in the hope that it will be useful,       *
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
