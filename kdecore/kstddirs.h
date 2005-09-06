8931520c0cd700dd5f3f338c4f73610da8c4b8c2
Benjamin Meyer <ben@meyerhome.net>
2005-09-06 09:09:04 +0000

    Removed depricated header from KDE2
    svn path=/trunk/KDE/kdelibs/; revision=457642

--- kdecore/kstddirs.h
+++
@@ -1,6 +0,0 @@
-// kstddirs.h is the old name, use #include <kstandarddirs.h> from now on
-#ifdef KDE_NO_COMPAT
-#error kstddirs.h is the old name, use #include <kstandarddirs.h> from now on
-#else
-#include <kstandarddirs.h>
-#endif
