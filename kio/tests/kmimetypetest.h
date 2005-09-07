cf4277ffaf5fd4e2fc5469c4bcd5b0b8de239274
David Faure <faure@kde.org>
2005-09-07 10:32:54 +0000

    forgotten file
    svn path=/trunk/KDE/kdelibs/; revision=458129

---
+++ kio/tests/kmimetypetest.h
@@ -0,0 +1,14 @@
+#ifndef KMIMETYPETEST_H
+#define KMIMETYPETEST_H
+
+#include <QObject>
+
+class KMimeTypeTest : public QObject
+{
+    Q_OBJECT
+private slots:
+    void testIcons();
+};
+
+
+#endif
