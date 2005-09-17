0098367dcf5de9b35e98f59095c01ccccfd7c17f
Dominik Haumann <dhaumann@kde.org>
2005-09-17 14:03:32 +0000

    next documentation step  * KTE::Document has real class information now.  * document KDocument::* stuff. I don't know what KDocument::Factory is good    for, it is not used at all...
    svn path=/trunk/KDE/kdelibs/; revision=461386

--- interfaces/kdocument/document.h
+++ interfaces/kdocument/document.h
@@ -3,20 +3,60 @@
 
 #include <kparts/part.h>
 
+/**
+ * Namespace for document/view architectures.
+ *
+ * This is a very basic abstraction for applications and libraries that need
+ * documents to support several views.
+ */
 namespace KDocument {
 	class View;
+
+	/**
+	 * A KParts document that provides several views.
+	 *
+	 * A document can have any number of views or no view, all representing
+	 * the same data. A view represents the document's content - usually
+	 * graphically. Every document has a unique number accessible with
+	 * documentNumber(). Create a new view with createView() and get a list
+	 * of all views with views().
+	 */
 	class KDE_EXPORT Document : public KParts::ReadWritePart {
 		Q_OBJECT
 		public:
-			Document(QObject *parent);
-			virtual ~Document() {}
-			virtual View *createView ( QWidget *parent ) = 0;
-			virtual const QList<View*> &views () = 0;
 			/**
-			 * Get the global number of this document in your application.
+			 * Constructor.
+			 *
+			 * Create a new document with @p parent.
+			 * @param parent parent object
+			 */
+			Document(QObject *parent);
+
+			/**
+			 * Virtual destructor.
+			 */
+			virtual ~Document() {}
+
+			/**
+			 * Create a new view attached to @p parent.
+			 * @param parent parent widget
+			 * @return the new view
+			 */
+			virtual View *createView ( QWidget *parent ) = 0;
+
+			/**
+			 * Get a list of all views.
+			 * @return all document views
+			 * @see createView()
+			 */
+			virtual const QList<View*> &views () = 0;
+
+			/**
+			 * Get the global unique number of this document in your application.
 			 * @return document number
 			 */
 			int documentNumber () const;
+
 		private:
 			/**
 			 * Private d-pointer
@@ -33,3 +73,4 @@ namespace KDocument {
 
 #endif
 
+// kate: space-indent off; indent-width 2; tab-width 4; replace-tabs off;
--- interfaces/kdocument/view.h
+++ interfaces/kdocument/view.h
@@ -5,17 +5,52 @@
 #include <kxmlguiclient.h>
 
 namespace KDocument {
+
+	/**
+	 * A widget with actions representing a document.
+	 *
+	 * A View represents the document's content. All views of the same
+	 * document are synchronized.
+	 */
 	class KDE_EXPORT View : public QWidget, public KXMLGUIClient {
 		Q_OBJECT
 		public:
+			/**
+			 * Constructor.
+			 *
+			 * Create a new view with parent widget @p parent.
+			 * @param parent parent widget
+			 */
 			View(QWidget *parent):QWidget(parent),KXMLGUIClient() {}
+			/**
+			 * Virtual destructor.
+			 */
 			virtual ~View() {}
 		signals:
+			/**
+			 * This signal is emitted whenever the @p view got the focus.
+			 * @param view view that emitted the signal
+			 * @see focusOut()
+			 */
 			void focusIn ( KDocument::View *view );
+
+			/**
+			 * This signal is emitted whenever the @p view lost the focus.
+			 * @param view view that emitted the signal
+			 * @see focusIn()
+			 */
 			void focusOut ( KDocument::View *view );
+
+			/**
+			 * This signal is emitted whenever the @p view sends an
+			 * information with text @p message.
+			 * @param view view that emitted the signal
+			 * @param message the information message
+			 */
 			void informationMessage ( KDocument::View *view, const QString &message );
 	};
 }
 
 #endif
 
+// kate: space-indent off; indent-width 2; tab-width 4; replace-tabs off;
