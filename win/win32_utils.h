b4b686c5cfc962c903f1cdf19caf2beba2edf69d
Jarosław Staniek <staniek@kde.org>
2005-09-18 19:12:36 +0000

    copyright
    svn path=/branches/KDE/3.5/kdelibs/; revision=461818

--- win/win32_utils.h
+++ win/win32_utils.h
@@ -1,6 +1,6 @@
 /*
    This file is part of the KDE libraries
-   Copyright (C) 2004 Jaroslaw Staniek <js@iidea.pl>
+   Copyright (C) 2004-2005 Jaroslaw Staniek <js@iidea.pl>
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
