440a499f842556f182d3d07a40a42964218c6b00
David Faure <faure@kde.org>
2005-09-27 11:45:41 +0000

    I agree that adjustPath(-1) should remove all trailing slashes if there are more than one.
    svn path=/branches/KDE/3.5/kdelibs/; revision=464447

--- kdecore/kurl.cpp
+++ kdecore/kurl.cpp
@@ -1273,8 +1273,11 @@ static QString trailingSlash( int _trailing, const QString &path )
     if ( result == "/" )
       return result;
     int len = result.length();
-    if ( (len != 0) && (result[ len - 1 ] == '/') )
-      result.truncate( len - 1 );
+    while (len > 1 && result[ len - 1 ] == '/')
+    {
+      len--;
+    }
+    result.truncate( len );
     return result;
   }
   else {
@@ -1851,9 +1854,9 @@ KURL KURL::upURL( ) const
   if (!hasSubURL())
   {
      KURL u(*this);
-     
+
      u.cd("../");
-     
+
      return u;
   }
 
