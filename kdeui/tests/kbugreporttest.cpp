b398f8671ea6795574f1b01f0bd20288663c5843
Joseph Wenninger <kde@jowenn.at>
2005-09-29 22:57:45 +0000

    test case for bug report dialog. Shows that the dialog doesn't work with kdelibs only (if bug address is not submit@bugs.kde.org) because of needed kcm_useraccout (mail settings)
    svn path=/trunk/KDE/kdelibs/; revision=465496

---
+++ kdeui/tests/kbugreporttest.cpp
@@ -0,0 +1,37 @@
+/* This file is part of the KDE libraries
+    Copyright (C) 2005 Joseph Wenninger <jowenn@kde.org>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+    Boston, MA 02111-1307, USA.
+*/
+
+#include <kaboutdata.h>
+#include <kcmdlineargs.h>
+#include <kapplication.h>
+#include <kbugreport.h>
+
+int main(int argc, char **argv) {
+   KAboutData about("kbugreporttest", "kbugreporttest", "version");
+   KCmdLineArgs::init(argc, argv, &about);
+
+   KApplication a;
+   a.setQuitOnLastWindowClosed(false);
+   KBugReport rep(0,true,&about);
+   rep.exec();
+   KAboutData about1("kbugreporttest", "kbugreporttest", "version",0,KAboutData::License_Unknown,0,0,0,"test@bugs.kde.org");
+   KBugReport rep1(0,true,&about1);
+   rep1.exec();
+   return 0;
+}
