da491dd44cb1b13f8f473aababb54ef1280e6188
David Faure <faure@kde.org>
2005-10-04 23:23:40 +0000

    Portability on != x11
    svn path=/trunk/KDE/kdelibs/; revision=467340

--- qttestlib/QtTest/qttest_kde.h
+++ qttestlib/QtTest/qttest_kde.h
@@ -11,6 +11,8 @@
 enum KDEMainFlag { NoGUI = 0, GUI = 1, AutoDcopRegistration = 2 }; // bitfield, next item is 4!
 Q_DECLARE_FLAGS(KDEMainFlags, KDEMainFlag)
 
+#ifdef Q_WS_X11
+
 // Ok this is a hack to pass the command line arguments to the test object.
 // Fix me when KApplication goes bye bye because this now only works on X11
 
@@ -25,6 +27,25 @@ int main(int argc, char *argv[]) \
     TestObject tc; \
     return QtTest::exec( &tc, argc, argv ); \
 }
+
+#else
+
+// The portable version, but command line args don't work
+
+#define QTTEST_KDEMAIN(TestObject, flags) \
+int main(int argc, char *argv[]) \
+{ \
+    KAboutData aboutData( "qttest", "qttest", "version" ); \
+    KCmdLineArgs::init( argc, argv, &aboutData );   \
+    KDEMainFlags mainFlags( flags );                 \
+    if ( (mainFlags & AutoDcopRegistration) == 0 ) \
+        KApplication::disableAutoDcopRegistration(); \
+    KApplication app( true, (mainFlags & GUI) != 0 ); \
+    TestObject tc; \
+    return QtTest::exec( &tc, argc, argv ); \
+}
+
+#endif
 
 #endif /* QTTEST_KDE_H */
 
