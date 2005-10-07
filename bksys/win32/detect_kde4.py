11f0fef6ae94e9b352ca0a9bcacc48632bc83a27
Ralf Habacker <ralf.habacker@freenet.de>
2005-10-07 20:22:12 +0000

    uses new  dcopidl source path
    svn path=/trunk/KDE/kdelibs/; revision=468360

--- bksys/win32/detect_kde4.py
+++ bksys/win32/detect_kde4.py
@@ -41,7 +41,7 @@ def bootstrap(env):
 	env['KDEMODULE'] = env.join( env['PREFIX'], 'lib/kde4')
 
 	env['KCONFIGCOMPILER']= env.join(os.getcwd(), env['_BUILDDIR_'], 'kdecore/kconfig_compiler/kconfig_compiler.exe')
-	env['DCOPIDL']        = env.join(os.getcwd(), env['_BUILDDIR_'], 'dcop/dcopidlng/dcopidl.exe --srcdir dcop/dcopidlng')
+	env['DCOPIDL']        = env.join(os.getcwd(), 'dcop/dcopidlng/dcopidl --srcdir dcop/dcopidlng')
 	env['DCOPIDL2CPP']    = env.join(os.getcwd(), env['_BUILDDIR_'], 'dcop/dcopidl2cpp/dcopidl2cpp.exe')
 
 	env['LIB_KDECORE'] = ['kdecore']
