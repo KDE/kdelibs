a6dc8d649d9f63825f38d2710b185e849a85712f
Ralf Habacker <ralf.habacker@freenet.de>
2005-10-09 00:40:06 +0000

    added some defines, still not complete
    svn path=/trunk/KDE/kdelibs/; revision=468685

--- bksys/win32/detect_lowlevel.py
+++ bksys/win32/detect_lowlevel.py
@@ -10,8 +10,15 @@ def detect(lenv):
         dest.write('/* lowlevel configuration */\n')
 
 	content="""
-#define HAVE_SYS_STAT_H
-#define HAVE_SYS_TIME_H
+#define HAVE_SYS_STAT_H 1
+#define HAVE_SYS_TIME_H 1
+#define HAVE_CTYPE_H 1 
+#define HAVE_MALLOC_H 1
+#define HAVE_STRING_H 1 
+#define LTDL_SHLIB_EXT ".dll"
+#define LTDL_OBJDIR ""
+#define HAVE_STDIO_H 1
+#define HAVE_STDLIB_H 1
 """
 
 	dest.write(content)
