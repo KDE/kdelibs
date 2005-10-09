0f932ae0b7bdc6ecd72e5bd2823dace26ffe574e
Germain Garand <germain@ebooksfrance.org>
2005-10-09 21:33:27 +0000

    positioned widgets could actually use the plural form (PaintActionChildBackgrounds), so let's opt it in too.
    CCBUG: 113995
    
    
    svn path=/branches/KDE/3.5/kdelibs/; revision=468966

--- khtml/rendering/render_replaced.cpp
+++ khtml/rendering/render_replaced.cpp
@@ -357,8 +357,9 @@ void RenderWidget::paint(PaintInfo& paintInfo, int _tx, int _ty)
 {
     _tx += m_x;
     _ty += m_y;
-
-    if (shouldPaintBackgroundOrBorder() && paintInfo.phase == PaintActionChildBackground)
+ 
+    if (shouldPaintBackgroundOrBorder() && 
+          (paintInfo.phase == PaintActionChildBackground || paintInfo.phase == PaintActionChildBackgrounds))
         paintBoxDecorations(paintInfo, _tx, _ty);
 
     if (!m_widget || !m_view || paintInfo.phase != PaintActionForeground)
