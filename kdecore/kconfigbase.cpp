71a030b0c2389d12f40a1de62f34b213def2b940
Luboš Luňák <l.lunak@kde.org>
2005-10-10 15:15:54 +0000

    Make sure QTextStream is destroyed before the matching pclose(). I get a very reproducible crash there ... and I wonder why it's only me. Also, popen() is probably not the smartest way of substituting shell variables if you ask me.
    svn path=/trunk/KDE/kdelibs/; revision=469264

--- kdecore/kconfigbase.cpp
+++ kdecore/kconfigbase.cpp
@@ -273,8 +273,10 @@ QString KConfigBase::readEntry( const char *pKey,
           FILE *fs = popen(QFile::encodeName(cmd).data(), "r");
           if (fs)
           {
+             {
              QTextStream ts(fs, QIODevice::ReadOnly);
              result = ts.readAll().trimmed();
+             }
              pclose(fs);
           }
           aValue.replace( nDollarPos, nEndPos-nDollarPos, result );
