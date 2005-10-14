7c4220f18f42243a60dd463fd7990dab87ee0635
Ralf Habacker <ralf.habacker@freenet.de>
2005-10-14 20:49:55 +0000

    updated by Christian Ehrlicher
    svn path=/trunk/KDE/kdelibs/; revision=470711

--- bksys/win32/detect_compiler.py
+++ bksys/win32/detect_compiler.py
@@ -1,24 +1,24 @@
-# Copyright David Faure <faure@kde.org> 2005
+# Copyright	David Faure	<faure@kde.org>	2005
 # BSD license (see COPYING)
 
-def detect(env):
+def	detect(env):
 	import os, re
 
-	# TODO: convert xlC and KCC code from kdelibs/configure.in.in
-
-	error_regexp = re.compile('Usage|ERROR|unrecognized option|unknown option|WARNING|missing|###')
-	if env['CC'] == 'gcc':
-		
-		for flag in ['-v', '-V', '--version', '-version']:
-			# should I use context.TryAction here?
-			syspf = os.popen(env['CXX']+' '+flag+' 2>&1 | egrep -v "Reading specs|Using built-?in specs|Configured with|Thread model"')
-			# Take the first line, and check that it doesn't look like an error message.
-			compiler_version=syspf.read().split('\n')[0]
-			if len(compiler_version) and not re.search( error_regexp, compiler_version, 'I' ):
-					return compiler_version
-	elif env['CC'] == 'cl':
-		# for now ...
-		compiler_version="1310"
-		return compiler_version
+	if env['CC'] !=	'gcc' and env['CC']	!= 'cl':
+		return False
 	
+	error_regexp = re.compile('Usage|ERROR|unrecognized	option|unknown option|WARNING|missing|###')
+	unused_regexp =	re.compile('Reading	specs|Using	built|Configured with|Thread model|Copyright|###')
+	
+	for	flag in	['-v', '-V', '--version', '-version', '/help']:
+		# should I use context.TryAction here?
+		syspf =	os.popen( env['CC']+' '+flag+' 2>&1' )
+		for	line in	syspf.read().split( '\n' ):
+			if re.search( error_regexp,	line, 'I' ):
+				continue
+			if re.search( unused_regexp, line, 'I' ):
+				continue
+			if len(line):
+				return line
+
 	return False
