6982c8d2a18bc5944676f3502d5b212de38efe7f
Ralf Habacker <ralf.habacker@freenet.de>
2005-10-19 20:16:12 +0000

    added constants for kio
    svn path=/trunk/KDE/kdelibs/; revision=472093

--- win/include/mingw/sys/stat.h
+++ win/include/mingw/sys/stat.h
@@ -14,6 +14,23 @@
 #include_next <sys/stat.h>
 
 #define _S_IFLNK	0xF000  /* Pretend */
+#define S_IFLNK	_S_IFLNK  /* kio/kio/kzip.cpp */ 
+#define _S_IFSOCK	0140000	/* socket */
+
 #define S_ISLNK(m)	(((m) & _S_IFMT) == _S_IFLNK) /* Should always be zero.*/
+#define	S_ISSOCK(m)	(((m)&_S_IFMT) == _S_IFSOCK)
+
+#define	S_IRWXG		(S_IRGRP | S_IWGRP | S_IXGRP)
+#define		S_IRGRP	0000040	/* read permission, group */
+#define		S_IWGRP	0000020	/* write permission, grougroup */
+#define		S_IXGRP 0000010/* execute/search permission, group */
+#define	S_IRWXO		(S_IROTH | S_IWOTH | S_IXOTH)
+#define		S_IROTH	0000004	/* read permission, other */
+#define		S_IWOTH	0000002	/* write permission, other */
+#define		S_IXOTH 0000001/* execute/search permission, other */
+
+#define	S_ISUID		0004000	/* set user id on execution */
+#define	S_ISGID		0002000	/* set group id on execution */
+#define	S_ISVTX		0001000	/* save swapped text even after use */
 
 #endif
