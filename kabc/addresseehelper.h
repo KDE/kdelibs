08fe300f809da34ab4e1ff024b6231eccd5f9817
Thomas Kadauke <tkadauke@gmx.de>
2005-10-30 22:12:37 +0000

    DOX: kabc documentation
    svn path=/trunk/KDE/kdelibs/; revision=475890

--- kabc/addresseehelper.h
+++ kabc/addresseehelper.h
@@ -27,25 +27,83 @@
 
 #include <set>
 
-/**
-  static data, shared by ALL addressee objects
-*/
-
 namespace KABC {
 
+/**
+ * This singleton class stores static data, which is shared
+ * by all Addressee objects. It maintains three lists of
+ * strings, which can be queried using this class:
+ *
+ * - a list of honoric prefixes, like "Mrs.", "Prof." etc,
+ *   see containsTitle()
+ * - a list of inclusions, such as "van" or "de", see
+ *   containsPrefix()
+ * - a list of honoric suffixes, such as "I" or "Jr.", see
+ *   containsSuffix()
+ *
+ * All of these lists have a hardcoded and a configurable
+ * part. The configurable part is found in @c kabcrc, group
+ * @c General, fields @c Prefixes, @c Inclusions, and
+ * @c Suffixes.
+ *
+ * In addition to the above, this class stores one conveniece
+ * setting: it stores whether or not a single name component
+ * should be interpreted as a family name (see
+ * tradeAsFamilyName()). The corresponding configuration
+ * field is @c TradeAsFamilyName.
+ */
 class KABC_EXPORT AddresseeHelper : public QObject, public DCOPObject
 {
   K_DCOP
         
   public:
+    /**
+     * Singleton interface to this class
+     *
+     * @return a pointer to the unique instance of this class.
+     */
     static AddresseeHelper *self();
 
+    /**
+     * Queries the list of honoric prefixes.
+     *
+     * @param title the honoric prefix to search for
+     * @return @c true, if @p title was found in the list,
+     *         @c false otherwise
+     */
     bool containsTitle( const QString& title ) const;
+
+    /**
+     * Queries the list of inclusions.
+     *
+     * @param prefix the inclusion to search for
+     * @return @c true, if @p prefix was found in the list,
+     *         @c false otherwise
+     */
     bool containsPrefix( const QString& prefix ) const;
+
+    /**
+     * Queries the list of honoric suffixes.
+     *
+     * @param suffix the honoric suffix to search for
+     * @return @c true, if @p suffix was found in the list,
+     *         @c false otherwise
+     */
     bool containsSuffix( const QString& suffix ) const;
+
+    /**
+     * Returns whether or not a single name component should
+     * be interpreted as a family name.
+     *
+     * @return @c true if single name component is a family name,
+     *         @c false otherwise.
+     */
     bool tradeAsFamilyName() const;
 
   k_dcop:
+    /**
+     * Recreates the static data and reparses the configuration.
+     */
     ASYNC initSettings();
 
   private:
