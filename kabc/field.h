e68482cf7eb100aa6fbbe940076247f699076f95
Thomas Kadauke <tkadauke@gmx.de>
2005-10-30 12:45:34 +0000

    DOX: fixes and extensions to field.h
    svn path=/trunk/KDE/kdelibs/; revision=475728

--- kabc/field.h
+++ kabc/field.h
@@ -30,31 +30,66 @@ class KConfigBase;
 
 namespace KABC {
 
+/**
+ * @short Represents a field in the Addressbook
+ *
+ * This class represents a field in the Addressbook database.
+ * It provides methods for accessing meta-information about
+ * the field (such as label() or category()) as well as
+ * getting or setting the field's value in an instance of
+ * Addressee (value(), setValue()).
+ *
+ * Furthermore, some static methods are provided for getting
+ * a list of all fields (allFields(), defaultFields()), for
+ * creating new fields (createCustomField()) and for saving
+ * or loading fields to/from a config file (saveFields(),
+ * restoreFields()).
+ */
 class KABC_EXPORT Field
 {
   class FieldImpl;
   friend class FieldImpl;
 
 public:
+  /**
+   * This type is used for a list of fields.
+   */
   typedef Q3ValueList<Field *> List;
 
   /**
-   * @li @p All -
-   * @li @p Frequent -
-   * @li @p Address -
-   * @li @p Email -
-   * @li @p Personal -
-   * @li @p Organization -
-   * @li @p CustomCategory -
-   */  
+   * Represents the category a field belongs to.
+   */
   enum FieldCategory
   {
+    /**
+     * All fields
+     */
     All = 0x0,
+    /**
+     * Frequently used fields
+     */
     Frequent = 0x01,
+    /**
+     * Fields which belong to the address, such as Street, City, Zip, etc.
+     */
     Address = 0x02,
+    /**
+     * Fields which store information about the e-mail contact, such as
+     * e-mail address or mail client
+     */
     Email = 0x04,
+    /**
+     * Personal fields, such as Birthday, Home Address fields, IM Address, etc.
+     */
     Personal = 0x08,
+    /**
+     * Fields about the organization, such as Business Address fields, Department,
+     * Profession, etc.
+     */
     Organization = 0x10,
+    /**
+     * Custom (user-defined) fields
+     */
     CustomCategory = 0x20
   };
 
@@ -69,20 +104,27 @@ public:
   virtual int category();
 
   /**
-   * Returns the translated label for field category.
+   * Returns the translated label for @p category.
+   *
+   * @param category the category of type FieldCategory
+   * @return the translated label
    */
   static QString categoryLabel( int category );
 
   /**
    * Returns a string representation of the value the field has in the given
-   * Addressee. Returns QString::null, if it is not possible to convert the
-   * value to a string.
+   * Addressee.
+   *
+   * @return the string representation of the value or QString::null, if it
+   * is not possible to convert the value to a string.
    */
   virtual QString value( const KABC::Addressee & );
 
   /**
-   * Sets the value of the field in the given Addressee. Returns true on success
-   * or false, if the given string couldn't be converted to a valid value.
+   * Sets the value of the field in the given Addressee.
+   *
+   * @return @c true on success or @c false, if the given string couldn't
+   * be converted to a valid value.
    */
   virtual bool setValue( KABC::Addressee &, const QString & );
 
@@ -93,11 +135,16 @@ public:
 
   /**
    * Returns, if the field is a user-defined field.
+   *
+   * @return @c true if this is a custom field, @c false otherwise
    */
   virtual bool isCustom();
 
   /**
-   * Returns, if the field is equal with @param field.
+   * Returns, if the field is equal with @p field.
+   *
+   * @param field the field to compare this field to
+   * @return @c true if the fields are equal, @c false otherwise
    */
   virtual bool equals( Field *field );
 
@@ -137,8 +184,12 @@ public:
   static void saveFields( KConfigBase *cfg, const QString &identifier,
                           const Field::List &fields );
   /**
-   * This is the same as above, with the difference, that
-   * the list is stored in KGlobal::config() in group "KABCFields".
+   * @overload
+   *
+   * Here, the list is stored in KGlobal::config() in group "KABCFields".
+   *
+   * @param identifier The unique identifier
+   * @param fields     The list of the fields
    */
   static void saveFields( const QString &identifier,
                           const Field::List &fields );
@@ -152,13 +203,22 @@ public:
   static Field::List restoreFields( KConfigBase *cfg, const QString &identifier );
 
   /**
-   * This is the same as above, with the difference, that
-   * the list is loaded from KGlobal::config() from group "KABCFields".
+   * @overload
+   *
+   * Here, the list is loaded from KGlobal::config() from group "KABCFields".
+   *
+   * @param identifier The unique identifier
    */
   static Field::List restoreFields( const QString &identifier );
 
 protected:
+  /**
+   * @internal
+   */
   static void createField( int id, int category = 0 );
+  /**
+   * @internal
+   */
   static void createDefaultField( int id, int category = 0 );
 
 private:
