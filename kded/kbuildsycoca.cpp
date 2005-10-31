61345ce8c61b8622c240bca06230e9d68ffb77e4
Helio Castro <helio@kde.org>
2005-10-31 16:14:36 +0000

    - Fix typo. Unbreak compilation
    svn path=/branches/KDE/3.5/kdelibs/; revision=476113

--- kded/kbuildsycoca.cpp
+++ kded/kbuildsycoca.cpp
@@ -936,7 +936,7 @@ extern "C" KDE_EXPORT int kdemain(int argc, char **argv)
       }
    }
 
-   if (!gGlobalDatabase)
+   if (!bGlobalDatabase)
    {
      // Recreate compatibility symlink
      QString oldPath = oldSycocaPath();
