29d0561621ebc8ac1c416260ce9eb8f6ae947018
Anders Lund <anders@alweb.dk>
2005-11-01 22:53:20 +0000

    Add explanation for this constraint
    svn path=/branches/KDE/3.5/kdelibs/; revision=476752

--- kate/part/kateview.cpp
+++ kate/part/kateview.cpp
@@ -1723,6 +1723,8 @@ void KateView::lineAsHTML (KateTextLine::Ptr line, uint startCol, uint length, Q
   for (uint curPos=startCol;curPos<(length+startCol);curPos++)
   {
     if ( curPos == 0 || line->attribute( curPos ) != line->attribute( curPos - 1 ) &&
+         // Since many highlight files contains itemdatas that have the exact
+         // same styles, join those to keep the HTML text size down
          KateAttribute(*charAttributes) != KateAttribute(*m_renderer->attribute(line->attribute(curPos))) )
     {
       (*outputStream) << textcache;
