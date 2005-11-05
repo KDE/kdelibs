48c2b3172d4dcee3de92c82ee909b222eeaacd70
Ralf Habacker <ralf.habacker@freenet.de>
2005-11-05 16:06:55 +0000

    define KICE_EXPORT when compiling libDCOP
    svn path=/trunk/KDE/kdelibs/; revision=478005

--- dcop/KDE-ICE/KICE.h
+++ dcop/KDE-ICE/KICE.h
@@ -1,6 +1,14 @@
 #ifndef KICE_H
 #define KICE_H
 
-# include <kdelibs_export.h>
+#include <kdelibs_export.h>
+
+#ifndef KICE_EXPORT
+# ifdef MAKE_DCOP_LIB
+# define KICE_EXPORT KDE_EXPORT
+# else
+# define KICE_EXPORT KDE_IMPORT
+# endif 
+#endif 
 
 #endif  // KICE_H
