5b030c005164bcf00c4b34e4682a53eb6ab18208
Pascal Létourneau <pascal.letourneau@gmail.com>
2005-11-10 00:35:46 +0000

    Escape only when needed BUG:111813
    svn path=/branches/KDE/3.5/kdelibs/; revision=479319

--- kdeui/ktabwidget.cpp
+++ kdeui/ktabwidget.cpp
@@ -281,8 +281,12 @@ void KTabWidget::updateTab( int index )
 {
     QString title = d->m_automaticResizeTabs ? d->m_tabNames[ index ] : QTabWidget::label( index );
     removeTabToolTip( page( index ) );
-    if ( title.length() > d->m_CurrentMaxLength )
-        setTabToolTip( page( index ), QStyleSheet::escape(title) );
+    if ( title.length() > d->m_CurrentMaxLength ) {
+        if ( QStyleSheet::mightBeRichText( title ) )
+            setTabToolTip( page( index ), QStyleSheet::escape(title) );
+        else
+            setTabToolTip( page( index ), title );
+    }
 
     title = KStringHandler::rsqueeze( title, d->m_CurrentMaxLength ).leftJustify( d->m_minLength, ' ' );
     title.replace( '&', "&&" );
