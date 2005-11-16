521143401b8b2ad95c370560075deb0e9cd05a4e
Adriaan de Groot <groot@kde.org>
2005-11-16 21:16:07 +0000

    toLatin1 gives us a ByteArray, which has no conversion to char * anymore, so sending them to cout is ambiguous; latin1() (isn't it deprecated yet) doesn't have that problem.
    svn path=/trunk/KDE/kdelibs/; revision=480862

--- kdeui/tests/kaccelgentest.cpp
+++ kdeui/tests/kaccelgentest.cpp
@@ -9,13 +9,13 @@ using std::endl;
 
 void check( const QString &what, const QStringList &expected, const QStringList &received )
 {
-    cout << "Testing " << what.toLatin1() << ": ";
+    cout << "Testing " << what.latin1() << ": ";
     if ( expected == received ) {
         cout << "ok" << endl;
     } else {
         cout << "ERROR!" << endl;
-        cout << "Expected: " << expected.join( "," ).toLatin1() << endl;
-        cout << "Received: " << received.join( "," ).toLatin1() << endl;
+        cout << "Expected: " << expected.join( "," ).latin1() << endl;
+        cout << "Received: " << received.join( "," ).latin1() << endl;
     }
 }
 
