4f9849abf1132bf72f896f2b01aede7320e15b01
Matt Rogers <mattr@kde.org>
2005-11-20 04:16:26 +0000

    Make the agg check work on platforms where the pkgconfig checking stuff is working and build in the svg icon rendering stuff if agg is present
    svn path=/trunk/KDE/kdelibs/; revision=481689

--- bksys/libagg.py
+++ bksys/libagg.py
@@ -52,9 +52,26 @@ def generate(env):
 	opts.Save(optionFile, env)
 
 	if not env['HELP'] and (env['_CONFIGURE_'] or not env.has_key('CACHED_' + pkgu)):
-		conf = env.Configure(custom_tests =	 { 'Check_libagg' : Check_libagg} )
+		if env['WINDOWS']:
+			conf = env.Configure(custom_tests =	 { 'Check_libagg' : Check_libagg} )
+			conf.Check_libagg()
+			env = conf.Finish()
+		else:
+			env['CACHED_' + pkgu] = 0
+			from SCons.Tool import Tool
+			pkgs = Tool('pkgconfig', ['./bksys'])
+			pkgs.generate(env)
+			have_agg  = env.pkgConfig_findPackage(pkgu, 'libagg', '2.3')
+			dest=open(env.join(env['_BUILDDIR_'], 'config-libagg.h'), 'w')
+			dest.write('/* libagg configuration created by bksys */\n')
+			if have_agg:
+				dest.write('#define HAVE_LIBAGG 1\n');
+				env['CACHED_' + pkgu] = 1
+			else:
+				env.pprint('RED', 'libagg >= 2.3 not found.')
 
-		conf.Check_libagg()
-
-		env = conf.Finish()
-		opts.Save(optionFile, env)
+			dest.close()
+			env['_CONFIG_H_'].append('libagg')
+	
+	opts.Update(env)
+	opts.Save(optionFile, env)
