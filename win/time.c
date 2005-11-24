a559fefab2b7112cad2dd1393be680614284f95c
Ralf Habacker <ralf.habacker@freenet.de>
2005-11-24 21:32:53 +0000

    - (ce) implemented gettimeofday() and settimeofday()
    svn path=/trunk/KDE/kdelibs/; revision=483032

--- win/time.c
+++ win/time.c
@@ -1,6 +1,6 @@
 /* This file is part of the KDE project
-   Copyright (C) 2003-2004 Jaroslaw Staniek <js@iidea.pl>
-   Copyright (C) 2005 Ralf Habacker <ralf.habacker@freenet.de>
+   Copyright (C) 2003-2005 Jaroslaw Staniek <js@iidea.pl>
+   Copyright (C) 2005 Christian Ehrlicher <Ch.Ehrlicher@gmx.de>
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
@@ -18,17 +18,38 @@
    Boston, MA 02110-1301, USA.
 */
 
-#include "time.h"
+#include <sys/time.h>
+#include <windows.h>
 
-KDEWIN32_EXPORT int gettimeofday(struct timeval *__p, struct timezone *__z)
+#define KDE_SECONDS_SINCE_1601	11644473600LL
+#define KDE_USEC_IN_SEC			1000000LL
+
+KDEWIN32_EXPORT int gettimeofday(struct timeval *__p, struct timezone *__t)
 {
-	/*! @todo */
-	return 1;
+	union {
+		unsigned long long ns100; /*time since 1 Jan 1601 in 100ns units */
+		FILETIME ft;
+	} now;
+	
+	GetSystemTimeAsFileTime (&now.ft);
+	__p->tv_usec = (long) ((now.ns100 / 10LL) % (KDE_USEC_IN_SEC * 10));
+	__p->tv_sec  = (long)(((now.ns100 / 10LL ) / KDE_USEC_IN_SEC) - KDE_SECONDS_SINCE_1601);
+	
+	return (0); 
 }
 
-KDEWIN32_EXPORT int settimeofday(const struct timeval *__p, const struct timezone *__z)
+KDEWIN32_EXPORT int settimeofday(const struct timeval *__p, const struct timezone *__t)
 {
-	/*! @todo */
+	union {
+		unsigned long long ns100; /*time since 1 Jan 1601 in 100ns units */
+		FILETIME ft;
+	} now;
+	SYSTEMTIME st;
+	
+	now.ns100 = ((((__p->tv_sec + KDE_SECONDS_SINCE_1601) * KDE_USEC_IN_SEC) + __p->tv_usec) * 10LL);
+	
+	FileTimeToSystemTime( &now.ft, &st );
+	SetSystemTime( &st );
 	return 1;
 }
 
