8bc2879ec5475947c097c4e76034f71f16caa87e
Ralf Habacker <ralf.habacker@freenet.de>
2005-11-26 18:33:49 +0000

    - uses CCFLAGS instead of CXXFLAGS, because CCFLAGS is also used by c++ compiler - added win32 support
    svn path=/trunk/KDE/kdelibs/; revision=483490

--- bksys/libpcre.py
+++ bksys/libpcre.py
@@ -11,7 +11,7 @@ def generate(env):
 		ret = context.TryAction('pcre-config --version')[0]
 		if ret: 
 			env['CACHED_PCRE'] = 1;
-			env['CXXFLAGS_PCRE'] = SCons.Util.CLVar( 
+			env['CCFLAGS_PCRE'] = SCons.Util.CLVar( 
 					os.popen('pcre-config --cflags').read().strip() )
 			env['LINKFLAGS_PCRE'] = SCons.Util.CLVar( 
 					os.popen('pcre-config --libs').read().strip() )
@@ -27,16 +27,25 @@ def generate(env):
 	opts = Options(optionFile)
 	opts.AddOptions(
 		('CACHED_PCRE', 'Whether libpcre is available'),
-		('CXXFLAGS_PCRE',''),
+		('CCFLAGS_PCRE',''),
 		('LINKFLAGS_PCRE',''),
+		('LIB_PCRE',''),
 		)
 	opts.Update(env)
+
 	if not env['HELP'] and (env['_CONFIGURE_'] or not env.has_key('CACHED_PCRE')):
 		conf = env.Configure(custom_tests =	 { 'Check_libpcre' : Check_libpcre } )
+		env['CACHED_PCRE'] = 0
 
-		if not conf.Check_libpcre():
+		if env['WINDOWS']:
+			if conf.CheckLib('pcre'):
+				env['CACHED_PCRE'] = 1
+				env['LIB_PCRE'] = ['pcre']
+		else:
+			conf.Check_libpcre()
+
+		if env['CACHED_PCRE'] == 0:
 			print 'libpcre not found .'
 
 		env = conf.Finish()
 		opts.Save(optionFile, env)
-
