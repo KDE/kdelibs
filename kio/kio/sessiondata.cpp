69a52997ca28f36f6cd8ce22db4634d2526c87d8
Nicolas Goutte <nicolasg@snafu.de>
2005-12-03 21:52:13 +0000

    SVN_SILENT: fix FSF address: - old address, split on two lines - old address, but newest ZIP code
    svn path=/trunk/KDE/kdelibs/; revision=485351

--- kio/kio/sessiondata.cpp
+++ kio/kio/sessiondata.cpp
@@ -14,8 +14,8 @@
 
    You should have received a copy of the GNU Lesser General Public
    License along with this library; see the file COPYING.LIB.  If not,
-   write to the Free Software Foundation, Inc., 59 Temple Place -
-   Suite 330, Boston, MA 02110-1301, USA.
+   write to the Free Software Foundation, Inc., 51 Franklin Street,
+   Fifth Floor, Boston, MA 02110-1301, USA.
 */
 
 #include <q3cstring.h>
