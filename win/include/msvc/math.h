1ae0161d3f3c0575598ae9a6173b31170f974f00
Jarosław Staniek <staniek@kde.org>
2005-12-13 23:16:13 +0000

    SVN_SILENT: 2005 even :)
    svn path=/trunk/KDE/kdelibs/; revision=488308

--- win/include/msvc/math.h
+++ win/include/msvc/math.h
@@ -1,6 +1,6 @@
 /*
    This file is part of the KDE libraries
-   Copyright (C) 205 Christian ehrlicher <ch.ehrlicher@gmx.de>
+   Copyright (C) 2005 Christian ehrlicher <ch.ehrlicher@gmx.de>
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
