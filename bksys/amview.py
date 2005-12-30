fef585091da4a76f0326a311a5d8cf735cfd775a
Ralf Habacker <ralf.habacker@freenet.de>
2005-12-30 01:08:04 +0000

    made amview as standalone application
    svn path=/trunk/KDE/kdelibs/; revision=492470

---
+++ bksys/amview.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+
+## 
+# @file 
+# bksys Makefile.am related tools
+# 
+# note: may be a future base of a Makefile.am to scons converter if someone like to write it 
+#
+# (@) 2005 Ralf Habacker  - published under the GPL license
+#
+ 
+import amtool
+
+amtool.amview()
