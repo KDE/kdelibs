e289fc0e028313a559311ee862fdb34f3207d041
Ralf Habacker <ralf.habacker@freenet.de>
2005-12-30 01:35:00 +0000

    added bksys to PATH env var
    svn path=/trunk/KDE/kdelibs/; revision=492480

--- bksys/initvars.bat
+++ bksys/initvars.bat
@@ -1,7 +1,10 @@
 ::
 :: win32 support file: allow running pythons scripts as binary 
+::
+:: run from package source root 
 :: 
 :: another solution may be http://mail.python.org/pipermail/python-list/1999-December/018772.html
+:: or http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/366355
 :: 
 @echo off
 
@@ -9,8 +12,12 @@ if not "%PYTHON%"=="" (
 	assoc .py=Python.File
 	ftype Python.File=%PYTHON%\python.exe "%%1" %%*
 	set PATHEXT=%PATHEXT%;.py
+
+	set PATH=%CD%\bksys;%PATH%
 	echo ...
 	echo now you can start any python script from bksys with bksys\scriptname
+
+
 ) else (
 	echo please set PYTHON environment variable to your python installation
 )
