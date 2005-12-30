557c622c650fabb0bf6bac9097eec606a2ab9f0e
Ralf Habacker <ralf.habacker@freenet.de>
2005-12-30 12:12:08 +0000

    added new command line option kdedir=<path> to let bksys work outside of kdelibs
    svn path=/trunk/KDE/kdelibs/; revision=492582

--- bksys/win32/detect_kde4.py
+++ bksys/win32/detect_kde4.py
@@ -11,17 +11,18 @@ def bootstrap(env):
 	env['LIBPATH_KDE4'] = env['LIBPATH_QT']
 
 	## not very portable but that's the best i have at the moment (ITA)
-	includes=['.','dcop','kio','kio\\kio','kio\\kfile','kdeui','kdecore','libltdl','kdefx']
+	if env['_IN_KDELIBS_']:
+		includes=['.','dcop','kio','kio\\kio','kio\\kfile','kdeui','kdecore','libltdl','kdefx']
 
-	env['INCLUDES_KDE4']=['#build']
-	for dir in includes:
-		env['INCLUDES_KDE4'].append('#'+dir)
-		env['INCLUDES_KDE4'].append('#build\\'+dir)
+		env['INCLUDES_KDE4']=['#build']
+		for dir in includes:
+			env['INCLUDES_KDE4'].append('#'+dir)
+			env['INCLUDES_KDE4'].append('#build\\'+dir)
 
-	# prefix/lib and prefix/lib/kde4 (ita)
-	env['RPATH_KDE4']= [
-		env['QTLIBPATH'], env.join(env['PREFIX'], 'lib'), env.join(env['PREFIX'], 'lib', 'kde4')
-	]
+		# prefix/lib and prefix/lib/kde4 (ita)
+	#	env['RPATH_KDE4']= [
+	#	env['QTLIBPATH'], env.join(env['PREFIX'], 'lib'), env.join(env['PREFIX'], 'lib', 'kde4')
+	#]
 
 	#######################################
 	## install paths
@@ -41,32 +42,54 @@ def bootstrap(env):
 	env['KDECONF']    = env.join( env['PREFIX'], 'share\\config')
 	env['KDEMODULE'] = env.join( env['PREFIX'], 'lib\\kde4')
 
-	env['KCONFIGCOMPILER']= env.join(os.getcwd(), env['_BUILDDIR_'], 'kdecore\\kconfig_compiler\\kconfig_compiler.exe')
-	env['DCOPIDL']        = env.join(os.getcwd(), 'dcop\\dcopidlng\\dcopidl --srcdir dcop\\dcopidlng')
-	env['DCOPIDL2CPP']    = env.join(os.getcwd(), env['_BUILDDIR_'], 'dcop\\dcopidl2cpp\\dcopidl2cpp.exe')
-
+	if env['_IN_KDELIBS_']:
+		env['KCONFIGCOMPILER']= env.join(os.getcwd(), env['_BUILDDIR_'], 'kdecore\\kconfig_compiler\\kconfig_compiler.exe')
+		env['DCOPIDL']        = env.join(os.getcwd(), 'dcop\\dcopidlng\\dcopidl --srcdir dcop\\dcopidlng')
+		env['DCOPIDL2CPP']    = env.join(os.getcwd(), env['_BUILDDIR_'], 'dcop\\dcopidl2cpp\\dcopidl2cpp.exe')
+	else:
+		env['KCONFIGCOMPILER']= env.join(env['_KDEDIR_'],'bin','kconfig_compiler.exe')
+		env['DCOPIDL']        = env.join(env['_KDEDIR_'],'bin','dcopidl.bat')  + ' --srcdir ' + env.join(env['_KDEDIR_'],'share','dcopidl')
+		env['DCOPIDL2CPP']    = env.join(env['_KDEDIR_'],'bin','copidl2cpp.exe')
+	
 	env['LIB_KDECORE'] = ['kdecore']
 	env['LIB_KIO']     = ['kio', 'kwalletclient']  # TODO: kdesu isn't supported yet
 	env['LIB_KDEUI']   = ['kdeui']
 
-	env['LIBPATH_KDECORE'] = [ env.join(os.getcwd(), env['_BUILDDIR_'], 'kdecore') ]
-	env['LIBPATH_KIO']     = [ env.join(os.getcwd(), env['_BUILDDIR_'], 'kio') ]
-	env['LIBPATH_KIO'].append( env.join(os.getcwd(), env['_BUILDDIR_'], 'kdesu'))
-	env['LIBPATH_KIO'].append( env.join(os.getcwd(), env['_BUILDDIR_'], 'kwallet', 'client'))
-	env['LIBPATH_KDEUI']   = [ env.join(os.getcwd(), env['_BUILDDIR_'], 'kdeui') ]
-	env['CPPPATH_KDECORE'] = [ env.join(os.getcwd(), env['_BUILDDIR_'], 'kdecore'), '#kdecore' ]
-	env['CPPPATH_KIO']     = [ env.join(os.getcwd(), env['_BUILDDIR_'], 'kio'), '#kio' ]
-	env['CPPPATH_KDEUI']   = [ env.join(os.getcwd(), env['_BUILDDIR_'], 'kdeui'), '#kdeui' ]
-
+	if env['_IN_KDELIBS_']:
+		env['LIBPATH_KDECORE'] = [ env.join(os.getcwd(), env['_BUILDDIR_'], 'kdecore') ]
+		env['LIBPATH_KIO']     = [ env.join(os.getcwd(), env['_BUILDDIR_'], 'kio') ]
+		env['LIBPATH_KIO'].append( env.join(os.getcwd(), env['_BUILDDIR_'], 'kdesu'))
+		env['LIBPATH_KIO'].append( env.join(os.getcwd(), env['_BUILDDIR_'], 'kwallet', 'client'))
+		env['LIBPATH_KDEUI']   = [ env.join(os.getcwd(), env['_BUILDDIR_'], 'kdeui') ]
+		env['CPPPATH_KDECORE'] = [ env.join(os.getcwd(), env['_BUILDDIR_'], 'kdecore'), '#kdecore' ]
+		env['CPPPATH_KIO']     = [ env.join(os.getcwd(), env['_BUILDDIR_'], 'kio'), '#kio' ]
+		env['CPPPATH_KDEUI']   = [ env.join(os.getcwd(), env['_BUILDDIR_'], 'kdeui'), '#kdeui' ]
+	else:
+		env['LIBPATH_KDE4']   += [ env.join(env['_KDEDIR_'], 'lib') ]
+		env['LIBPATH_KDECORE'] = []
+		env['LIBPATH_KIO']     = []
+		env['LIBPATH_KDEUI']   = []
+		env['INCLUDES_KDE4']   = [ env.join(env['_KDEDIR_'], 'include') ]
+		env['CPPPATH_KDECORE'] = []
+		env['CPPPATH_KIO']     = []
+		env['CPPPATH_KDEUI']   = []
+	
 ## detect kde4 configuration
 def detect(env):
-	"""
 	def getpath(varname):
 		if not env.has_key('ARGS'): return None
 		v=env['ARGS'].get(varname, None)
 		if v : v=os.path.abspath(v)
 		return v
 
+	kdedir		= getpath('kdedir')
+	if kdedir:
+		env['_KDEDIR_'] = kdedir
+		env['_IN_KDELIBS_'] = 0
+	else:
+		env['_IN_KDELIBS_'] = 1
+
+	"""
 	prefix		= getpath('prefix')
 	execprefix	= getpath('execprefix')
 	datadir		= getpath('datadir')
@@ -79,8 +102,9 @@ def detect(env):
 
 	# normal detection functions go here
 	if env['CC'] == 'cl':
-		env.AppendUnique(KDECCFLAGS = ['/FI./win/include/kdelibs_global_win.h'])
-		env.AppendUnique(KDECXXFLAGS = ['/FI./win/include/kdelibs_global_win.h'])
+		if env['_IN_KDELIBS_']:
+			env.AppendUnique(KDECCFLAGS = ['/FI./win/include/kdelibs_global_win.h'])
+			env.AppendUnique(KDECXXFLAGS = ['/FI./win/include/kdelibs_global_win.h'])
 		env.AppendUnique(KDECCFLAGS = ['-DKDE_FULL_TEMPLATE_EXPORT_INSTANTIATION'])
 		env.AppendUnique(KDECXXFLAGS = ['-DKDE_FULL_TEMPLATE_EXPORT_INSTANTIATION'])
 
