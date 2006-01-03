14f1d29c279a020d75986a909e397274499a977a
Christian Ehrlicher <ch.ehrlicher@gmx.de>
2006-01-03 18:49:45 +0000

    remove search for libs and only allow additonal lib/include paths with extralibs/extraincludes
    svn path=/trunk/KDE/kdelibs/; revision=493956

--- bksys/libjpeg.py
+++ bksys/libjpeg.py
@@ -12,39 +12,19 @@ def generate(env):
 		context.Message('Checking for libjpeg...\n')
 
 		ret = 0
-		jpeg_include_path = env['CPPPATH']
-		jpeg_lib_path = env['LIBPATH']
-		
-		if env['WINDOWS']:
-			jpeg_include_path += ['c:\\libjpeg\\include']
-			jpeg_lib_path += ['c:\\libjpeg\\lib']
-			ext='.lib'
-		else:
-			jpeg_include_path += ['/usr/include','/usr/local/include']
-			jpeg_lib_path += ['/usr/lib','/usr/local/lib']
-			ext='.so'
 
-		#we need to check for libjpeg6b then normal libjpeg to
-		#be compatible with the current autoconf check
-		header = env.find_file_ext('jpeglib.h', jpeg_include_path)
-		libjpeg = env.find_file_ext('jpeg'+ext, jpeg_lib_path)
-		libjpeg6b = env.find_file_ext('jpeg6b'+ext, jpeg_lib_path)
-
+		header = env.find_file('jpeglib.h', env['CPPPATH'])
+# CE: Doesn't work for msvc currently
+#		if conf.CheckHeader('jpeglib.h'):
 		if header != '':
-			if libjpeg != '':
-				conf.env.AppendUnique( LIBPATH = [ libjpeg ] )
-				if conf.CheckLib('jpeg'):
-					conf.env['INCLUDES_JPEG'] = [ header ]
-					conf.env['LIBPATH_JPEG']  = [ libjpeg ]
-					conf.env['LIB_JPEG']      = [ 'jpeg' ]
-					ret = 1
-			if ( ret == 0 and libjpeg6b != ''):
-				conf.env.AppendUnique( LIBPATH = [ libjpeg6b ] )
-				if conf.CheckLib('jpeg6b'):
-					conf.env['INCLUDES_JPEG '] = [ header ]
-					conf.env['LIBPATH_JPEG ']  = [ libjpeg6b ]
-					conf.env['LIB_JPEG ']      = [ 'jpeg6b' ]
-					ret = 1
+			if conf.CheckLib('jpeg'):
+				conf.env['INCLUDES_JPEG'] = [ 'jpeglib.h' ]
+				conf.env['LIB_JPEG']      = [ 'jpeg' ]
+				ret = 1
+			elif conf.CheckLib('jpeg6b'):
+				conf.env['INCLUDES_JPEG '] = [ 'jpeglib.h' ]
+				conf.env['LIB_JPEG ']      = [ 'jpeg6b' ]
+				ret = 1
 
 		context.Result(ret)
 		return ret
