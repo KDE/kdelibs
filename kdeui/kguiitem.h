0316f07ced9e5197836a5b92ab6dd0927216f01e
Matthias Kretz <kretz@kde.org>
2006-01-06 12:23:28 +0000

    link docs to KStdGuiItem
    svn path=/trunk/KDE/kdelibs/; revision=494855

--- kdeui/kguiitem.h
+++ kdeui/kguiitem.h
@@ -33,6 +33,7 @@ class QIcon;
  * @short An abstract class for GUI data such as ToolTip and Icon.
  *
  * @author Holger Freyther <freyher@yahoo.com>
+ * @see KStdGuiItem
  */
 class KDEUI_EXPORT KGuiItem
 {
