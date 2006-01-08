d731897b31148c7d3da7bfccdb399ae26bde1fc3
Maks Orlovich <maksim@kde.org>
2006-01-08 16:41:09 +0000

    Fix obvious copy-paste error. Hmm, this file refs/derefs stuff in wrong order, so crashes are possible when init* is called twice. Fixes 119734, and near certainly 107686, but I don't have the time to test that one. BUG:119734 CCBUG:107686
    svn path=/branches/KDE/3.5/kdelibs/; revision=495677

--- khtml/xml/dom2_eventsimpl.cpp
+++ khtml/xml/dom2_eventsimpl.cpp
@@ -794,8 +794,8 @@ MutationEventImpl::MutationEventImpl(EventId _id,
     if (m_newValue)
 	m_newValue->ref();
     m_attrName = attrNameArg.implementation();
-    if (m_newValue)
-	m_newValue->ref();
+    if (m_attrName)
+	m_attrName->ref();
     m_attrChange = attrChangeArg;
 }
 
