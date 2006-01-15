3a92806edb9ee317b837dae3b9d5e4a9ed4f2413
Urs Wolfer <uwolfer@kde.org>
2006-01-15 02:19:50 +0000

    improve KProgressBar test
    svn path=/trunk/KDE/kdelibs/; revision=498230

--- kdeui/tests/kprogressbartest.cpp
+++ kdeui/tests/kprogressbartest.cpp
@@ -8,52 +8,64 @@
 
 class MyWidget : public QWidget {
 public:
-	MyWidget() : QWidget()
-	{
-		setFixedSize(440, 80);
-		Progress = new KProgressBar(this);
-		Progress->resize(400, 40);
-		Progress->move(20, 20);
-		startTimer(50);
-	}
-	
+    MyWidget() : QWidget()
+    {
+        setFixedSize(440, 80);
+        Progress = new KProgressBar(this);
+        Progress->resize(400, 40);
+        Progress->move(20, 20);
+        startTimer(50);
+    }
+
 private:
-	KProgressBar *Progress;
-	
-	void timerEvent(QTimerEvent *);
+    KProgressBar *Progress;
+
+    int steps;
+
+    void timerEvent(QTimerEvent *);
 };
 
 void MyWidget::timerEvent(QTimerEvent *)
 {
-  static enum { fwd, back } direction = fwd;
-  if (direction == fwd) 
-	{
-	  if (Progress->value() == Progress->maximum())
-		direction = back;
-	  else
-		Progress->advance(1);
-	} 
-  else 
-	{
-	  if (Progress->value() == 0 /*Progress->minValue()*/) 
-		{
-				direction = fwd;
-		} 
-	  else
-		Progress->advance(-1);
-	}
+    static enum { fwd, back } direction = fwd;
+    if (direction == fwd)
+    {
+        Progress->setFormat("Forward - %p% - Value: %v - Maximum: %m");
+        if (Progress->value() == Progress->maximum())
+        {
+            steps = rand() % 200;
+            Progress->setRange(0, steps);
+            Progress->setValue(Progress->maximum());
+            direction = back;
+        }
+        else
+            Progress->advance(1);
+    }
+    else
+    {
+        Progress->setFormat("Back - %p% - Value: %v - Maximum: %m");
+        if (Progress->value() == Progress->minimum())
+        {
+            steps = rand() % 200;
+            Progress->setRange(0, steps);
+            Progress->setValue(Progress->minimum());
+            direction = fwd;
+        }
+        else
+            Progress->advance(-1);
+    }
 }
 
 int main(int argc, char *argv[])
 {
-	KCmdLineArgs::init( argc, argv, "test", "Test" ,"test app" ,"1.0" );
-	KApplication app;
-	MyWidget w;
-	
-	app.setMainWidget(&w);
+    KCmdLineArgs::init( argc, argv, "test", "Test" ,"test app" ,"1.0" );
+    KApplication app;
+    MyWidget w;
 
-	w.show();
-	
-	int ret = app.exec();
-	return ret;
+    app.setMainWidget(&w);
+
+    w.show();
+
+    int ret = app.exec();
+    return ret;
 }
