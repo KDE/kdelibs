dfe7971254cfef901a3b1c3ce590221fb9a59423
Benjamin Reed <ranger@befunk.com>
2006-01-30 02:52:48 +0000

    make it build on osx, this needs a real fix though
    svn path=/trunk/KDE/kdelibs/; revision=503723

--- knotify/daemon/notifybypopup.cpp
+++ knotify/daemon/notifybypopup.cpp
@@ -45,7 +45,7 @@ void NotifyByPopup::notify( int id, KNotifyConfig * config )
 
 	const QString &appname=config->appname;
 	
-	KPassivePopup *pop = new KPassivePopup( config->winId );
+	KPassivePopup *pop = new KPassivePopup( config->winId, 0, 0 );
 	
 	KConfigGroup globalgroup( &config->eventsfile, "Global" );
 	QString iconName = globalgroup.readEntry( "IconName", appname );
