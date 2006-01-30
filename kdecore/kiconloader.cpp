d9f50c47a28f81776ad967abca8b94050b20ebc4
Luboš Luňák <l.lunak@kde.org>
2006-01-30 14:54:05 +0000

    What's the point of building a list of all installed icon themes and checking validity of all of them when the only one needed is later checked anyway.
    svn path=/trunk/KDE/kdelibs/; revision=503834

--- kdecore/kiconloader.cpp
+++ kdecore/kiconloader.cpp
@@ -118,7 +118,6 @@ struct KIconGroup
 
 struct KIconLoaderPrivate
 {
-    QStringList mThemeList;
     QStringList mThemesInTree;
     KIconGroup *mpGroups;
     KIconThemeNode *mpThemeRoot;
@@ -197,18 +196,6 @@ void KIconLoader::init( const QString& _appname, KStandardDirs *_dirs )
     // successfully.
     d->mpThemeRoot = 0L;
 
-    // Check installed themes.
-    d->mThemeList = KIconTheme::list();
-    if (!d->mThemeList.contains(KIconTheme::defaultThemeName()))
-    {
-        kdError(264) << "Error: standard icon theme"
-                     << " \"" << KIconTheme::defaultThemeName() << "\" "
-                     << " not found!" << endl;
-        d->mpGroups=0L;
-
-        return;
-    }
-
     QString appname = _appname;
     if (appname.isEmpty())
 	appname = KGlobal::instance()->instanceName();
@@ -218,7 +205,17 @@ void KIconLoader::init( const QString& _appname, KStandardDirs *_dirs )
     if (!def->isValid())
     {
 	delete def;
+        // warn, as this is actually a small penalty hit
+        kdDebug(264) << "Couldn't find current icon theme, falling back to default." << endl;
 	def = new KIconTheme(KIconTheme::defaultThemeName(), appname);
+        if (!def->isValid())
+        {
+            kdError(264) << "Error: standard icon theme"
+                         << " \"" << KIconTheme::defaultThemeName() << "\" "
+                         << " not found!" << endl;
+            d->mpGroups=0L;
+            return;
+        }
     }
     d->mpThemeRoot = new KIconThemeNode(def);
     d->links.append(d->mpThemeRoot);
@@ -341,8 +338,7 @@ void KIconLoader::addBaseThemes(KIconThemeNode *node, const QString &appname)
 
     for (it=lst.begin(); it!=lst.end(); ++it)
     {
-	if (!d->mThemeList.contains(*it) ||
-	    ( d->mThemesInTree.contains(*it) && (*it) != "hicolor"))
+	if( d->mThemesInTree.contains(*it) && (*it) != "hicolor")
 	    continue;
 	KIconTheme *theme = new KIconTheme(*it,appname);
 	if (!theme->isValid()) {
