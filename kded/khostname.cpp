6357d9431b6cf4db2cd5cbc308b39a039f92eb38
Dirk Mueller <mueller@kde.org>
2006-02-02 17:43:52 +0000

    don't change the xauth if $XAUTHLOCALHOSTNAME patches are in effect
    svn path=/trunk/KDE/kdelibs/; revision=504971

--- kded/khostname.cpp
+++ kded/khostname.cpp
@@ -362,7 +362,9 @@ int main(int argc, char **argv)
 
    KHostName hn;
 
-   hn.changeX();
+   if(!getenv("XAUTHLOCALHOSTNAME"))
+       hn.changeX();
+
    hn.changeDcop();
    hn.changeStdDirs("socket");
    hn.changeStdDirs("tmp");
