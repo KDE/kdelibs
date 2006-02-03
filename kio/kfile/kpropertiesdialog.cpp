4f80341e65a1e0ef234b641afe5edbcc25e21dc8
Dirk Mueller <mueller@kde.org>
2006-02-03 15:46:21 +0000

    use KDesktopFile to read desktop files
    svn path=/branches/KDE/3.5/kdelibs/; revision=505307

--- kio/kfile/kpropertiesdialog.cpp
+++ kio/kfile/kpropertiesdialog.cpp
@@ -3103,12 +3103,10 @@ KDesktopPropsPlugin::KDesktopPropsPlugin( KPropertiesDialog *_props )
     return;
   f.close();
 
-  KSimpleConfig config( path );
-  config.setDollarExpansion( false );
-  config.setDesktopGroup();
-  QString nameStr = config.readEntry( "Name" );
-  QString genNameStr = config.readEntry( "GenericName" );
-  QString commentStr = config.readEntry( "Comment" );
+  KDesktopFile  config( path );
+  QString nameStr = config.readName();
+  QString genNameStr = config.readGenericName();
+  QString commentStr = config.readComment();
   QString commandStr = config.readPathEntry( "Exec" );
   if (commandStr.left(12) == "ksystraycmd ")
   {
@@ -3951,16 +3949,15 @@ KApplicationPropsPlugin::KApplicationPropsPlugin( KPropertiesDialog *_props )
     return;
   f.close();
 
-  KSimpleConfig config( path );
-  config.setDesktopGroup();
-  QString commentStr = config.readEntry( "Comment" );
-  QString genNameStr = config.readEntry( "GenericName" );
+  KDesktopFile config( path );
+  QString commentStr = config.readComment();
+  QString genNameStr = config.readGenericName();
 
   QStringList selectedTypes = config.readListEntry( "ServiceTypes" );
   // For compatibility with KDE 1.x
   selectedTypes += config.readListEntry( "MimeType", ';' );
 
-  QString nameStr = config.readEntry( QString::fromLatin1("Name") );
+  QString nameStr = config.readName();
   if ( nameStr.isEmpty() || d->m_kdesktopMode ) {
     // We'll use the file name if no name is specified
     // because we _need_ a Name for a valid file.
