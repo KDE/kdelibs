01a7637b024922038c39e3cfd630aa6f04d0711d
Luboš Luňák <l.lunak@kde.org>
2006-02-06 18:33:09 +0000

    Ok, svnrevertlast once more.
    svn path=/trunk/KDE/kdelibs/; revision=506405

--- kded/khostname.cpp
+++ kded/khostname.cpp
@@ -36,7 +36,6 @@
 #include <kstandarddirs.h>
 #include <kprocess.h>
 #include <kde_file.h>
-#include <kdebug.h>
 
 static KCmdLineOptions options[] = {
    { "+old", I18N_NOOP("Old hostname"), 0 },
@@ -156,8 +155,8 @@ void KHostName::changeX()
 
       QByteArray newNetId = newName+netId.mid(i);
 
-//      cmd = "xauth remove "+KProcess::quote(netId);
-//      system(QFile::encodeName(cmd));
+      cmd = "xauth remove "+KProcess::quote(netId);
+      system(QFile::encodeName(cmd));
       cmd = "xauth add ";
       cmd += KProcess::quote(newNetId);
       cmd += " ";
@@ -363,7 +362,9 @@ int main(int argc, char **argv)
 
    KHostName hn;
 
-   hn.changeX();
+   if(!getenv("XAUTHLOCALHOSTNAME"))
+       hn.changeX();
+
    hn.changeDcop();
    hn.changeStdDirs("socket");
    hn.changeStdDirs("tmp");
