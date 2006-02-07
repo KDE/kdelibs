2d10f10ab683549dc8beff1601d14ca81a16d724
Peter Kümmel <syntheticpp@gmx.net>
2006-02-07 09:30:40 +0000

    move kdecore/fakes.c into kdewin32.lib (only for windows)
    svn path=/trunk/KDE/kdelibs/; revision=506607

--- win/include/mingw/string.h
+++ win/include/mingw/string.h
@@ -30,13 +30,14 @@
 extern "C" {
 #endif
 
-/* implemented in fakes.c */
+KDEWIN32_EXPORT char* strndup(const char *src, size_t n);
+
+// from kdecore/fakes.c
+
 KDEWIN32_EXPORT unsigned long strlcpy(char *dst, const char *src, unsigned long siz);
 
-/* implemented in fakes.c */
 KDEWIN32_EXPORT unsigned long strlcat(char *dst, const char *src, unsigned long siz);
 
-KDEWIN32_EXPORT char* strndup(const char *src, size_t n);
 
 #ifdef __cplusplus
 }
