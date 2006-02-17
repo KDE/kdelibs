024639914c0f7b0a1cb47e709045cf0d65ed8036
Nicolas Goutte <nicolasg@snafu.de>
2006-02-17 20:48:11 +0000

    ./Messages.sh must be protected too, not only ,/Makefile.am
    svn path=/trunk/KDE/kdelibs/; revision=510719

--- Messages.sh
+++ Messages.sh
@@ -1,19 +1,17 @@
 #! /usr/bin/env bash
 ### TODO: would it not be easier to use -j for xgettext from a copy of qt-messages.pot ?
-pwd # DEBUG
 cd kabc/scripts && perl ./makeaddressee
 find . -type d | fgrep -v '.svn' | sed -e 's,$,/,' > dirs
 mfs=`find . -name Makefile.am | xargs egrep -l '^messages:'`
 msh=`find . -name Messages.sh`
 for dir in $mfs $msh; do
-  if test "$dir" != "./Makefile.am"; then
+  if test "$dir" != "./Makefile.am" -a "$dir" != "./Messages.sh"; then
     dir=`dirname $dir`
     egrep -v "^$dir" dirs > dirs.new && mv dirs.new dirs
   fi
 done
 fgrep -v "/tests" dirs > dirs.new && mv dirs.new dirs
 dirs=`sort -u dirs`
-echo "DIRS " $dirs
 find $dirs -maxdepth 1 -name "*.cpp" -print > files
 find $dirs -maxdepth 1 -name "*.cc" -print >> files
 find $dirs -maxdepth 1 -name "*.h" -print >> files
