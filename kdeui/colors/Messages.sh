230f695207161745414f6cfdc4fdfa10bc38ad47
Nicolas Goutte <nicolasg@snafu.de>
2006-02-17 14:18:34 +0000

    Create Messages.sh scripts (These two cases are midly complex ones.)
    svn path=/trunk/KDE/kdelibs/; revision=510525

---
+++ kdeui/colors/Messages.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/env bash
+: > colors.cpp
+list=`grep '^[0-9]' rgb.txt  | tr '	' ' ' | sed -e "s#^[0-9]* [0-9]* [0-9]* *##" | fgrep -v " " | fgrep -v gray | fgrep -v grey | LC_ALL=C sort -u`
+for i in $list; do
+	echo "i18n(\"color\", \"$i\");" | sed -e "s#!# #g" >> colors.cpp
+done
+$XGETTEXT colors.cpp -o $podir/kdelibs_colors.pot
+rm -f colors.cpp
