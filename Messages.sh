6e7f0bac3338502a890af93ffc8d39836754e69c
Nicolas Goutte <nicolasg@snafu.de>
2006-02-18 12:42:21 +0000

    Fix Messages.sh script (A change of directory is for more than for one line, unlike in a Makefile.)
    svn path=/trunk/KDE/kdelibs/; revision=510893

--- Messages.sh
+++ Messages.sh
@@ -1,10 +1,5 @@
 #! /usr/bin/env bash
-
-exit 1
-### FIXME: why $EXTRACTRC does not get any file? Why are qt-messages.pot and common_texts.cpp unknown
-
-### TODO: would it not be easier to use -j for xgettext from a copy of qt-messages.pot ?
-cd kabc/scripts && perl ./makeaddressee
+( cd kabc/scripts && perl ./makeaddressee )
 find . -type d | fgrep -v '.svn' | sed -e 's,$,/,' > dirs
 mfs=`find . -name Makefile.am | xargs egrep -l '^messages:'`
 msh=`find . -name Messages.sh`
@@ -22,6 +17,7 @@ find $dirs -maxdepth 1 -name "*.h" -print >> files
 echo ./kdecore/kde-config.cpp.in >> files
 $EXTRACTRC `find $dirs -maxdepth 1 \( -name \*.rc -o -name \*.ui -o -name \*.ui3 -o -name \*.ui4 -o -name \*.kcfg \) ` >> rc.cpp || exit 11
 : > kde.pot
+### TODO: would it not be easier to use -j for xgettext from a copy of qt-messages.pot ?
 $XGETTEXT -ktranslate:1,1t -ktranslate:1c,2,2t -x qt-messages.pot --files-from=files && cat messages.po qt-messages.pot > $podir/kdelibs.pot
 $XGETTEXT common_texts.cpp -o kde.pot
 rm -f dirs
