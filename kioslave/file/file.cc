17cb53f6ef42962c9687d6bbc94f7c77ce6c5620
Marc Espie <espie@nerim.net>
2006-02-19 19:15:05 +0000

    fix build on OpenBSD and other systems without ENOTSUP
    svn path=/trunk/KDE/kdelibs/; revision=511403

--- kioslave/file/file.cc
+++ kioslave/file/file.cc
@@ -187,9 +187,11 @@ void FileProtocol::chmod( const KUrl& url, int permissions )
             case EACCES:
                 error( KIO::ERR_ACCESS_DENIED, url.path() );
                 break;
+#if defined(ENOTSUP)
             case ENOTSUP: // from setACL since chmod can't return ENOTSUP
                 error( KIO::ERR_UNSUPPORTED_ACTION, i18n( "Setting ACL for %1" ).arg( url.path() ) );
                 break;
+#endif
             case ENOSPC:
                 error( KIO::ERR_DISK_FULL, url.path() );
                 break;
