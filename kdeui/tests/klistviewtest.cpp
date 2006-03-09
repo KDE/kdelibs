80fc15a85e4355842c718de764d93f3f3634a45b
David Faure <faure@kde.org>
2006-03-09 18:34:15 +0000

    actually test klistviewitem here
    svn path=/trunk/KDE/kdelibs/; revision=517037

--- kdeui/tests/klistviewtest.cpp
+++ kdeui/tests/klistviewtest.cpp
@@ -18,20 +18,20 @@ int main( int argc, char **argv )
 	view->addColumn("Column 2");
 	view->addColumn("Column 3");
 
-	new Q3ListViewItem( view, "Item 1");
-	new Q3ListViewItem( view, "Item 1");
-	new Q3ListViewItem( view, "Item 1");
-	new Q3ListViewItem( view, "Item 1");
-	new Q3ListViewItem( view, "Item 1");
-	new Q3ListViewItem( view, "Item 1");
-	new Q3ListViewItem( view, "Item 1");
-	new Q3ListViewItem( view, "Item 1");
-	new Q3ListViewItem( view, "Item 1");
-	new Q3ListViewItem( view, "Item 2", "Some more", "Hi Mom :)" );
+	new KListViewItem( view, "Item 1");
+	new KListViewItem( view, "Item 1");
+	new KListViewItem( view, "Item 1");
+	new KListViewItem( view, "Item 1");
+	new KListViewItem( view, "Item 1");
+	new KListViewItem( view, "Item 1");
+	new KListViewItem( view, "Item 1");
+	new KListViewItem( view, "Item 1");
+	new KListViewItem( view, "Item 1");
+	new KListViewItem( view, "Item 2", "Some more", "Hi Mom :)" );
 
 	view->restoreLayout( KGlobal::config(), "ListView" );
 
-	new Q3ListViewItem( view, "Item 3" );
+	new KListViewItem( view, "Item 3" );
 
 	dialog.exec();
 	view->saveLayout( KGlobal::config(), "ListView" );
