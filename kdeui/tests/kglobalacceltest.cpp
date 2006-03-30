e7c83e051e3fcd2f9147440f56152f238de52b25
Hamish Rodda <rodda@kde.org>
2006-03-30 15:00:04 +0000

    Forgot this
    svn path=/trunk/KDE/kdelibs/; revision=524516

---
+++ kdeui/tests/kglobalacceltest.cpp
@@ -0,0 +1,24 @@
+#include <kapplication.h>
+#include <kcmdlineargs.h>
+#include <kaboutdata.h>
+#include <kaction.h>
+#include <kdebug.h>
+
+int main( int argc, char **argv )
+{
+    KAboutData aboutData( "kglobalacceltest", "kglobalacceltest", "1.0" );
+    KCmdLineArgs::init(argc, argv, &aboutData);
+    KApplication app;
+
+    KActionCollection* coll = new KActionCollection( &app );
+
+    KAction* action = new KAction(coll, "test");
+    action->setGlobalShortcutAllowed(true);
+    action->setGlobalShortcut(Qt::META + Qt::Key_F5);
+
+    QObject::connect(action, SIGNAL(triggered(bool)), &app, SLOT(quit()));
+
+    kDebug() << "Press Meta+F5 to complete the test." << endl;
+
+    return app.exec();
+}
