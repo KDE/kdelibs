411b24928e831b49ebc8e59253b5fc5d292bb673
Dirk Mueller <mueller@kde.org>
2006-04-18 19:41:47 +0000

    fix crash
    svn path=/branches/KDE/3.5/kdelibs/; revision=531235

--- khtml/xml/dom2_traversalimpl.cpp
+++ khtml/xml/dom2_traversalimpl.cpp
@@ -611,7 +611,7 @@ NodeImpl *TreeWalkerImpl::getPreviousSibling(NodeImpl *n)
     // now the case if we don't have previous sibling
     else
     {
-        _tempCurrent = _tempCurrent->parentNode();
+        _tempCurrent = n->parentNode();
         if( !_tempCurrent || _tempCurrent == m_rootNode)
             return 0;
         _result = isAccepted( _tempCurrent );
