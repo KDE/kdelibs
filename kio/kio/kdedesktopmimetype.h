2ca1c20dda899c1855118ac072912971cd7bc9e3
David Faure <faure@kde.org>
2006-04-21 19:58:55 +0000

    License, header-protection.
    svn path=/trunk/KDE/kdelibs/; revision=532382

--- kio/kio/kdedesktopmimetype.h
+++ kio/kio/kdedesktopmimetype.h
@@ -1,3 +1,25 @@
+/*  This file is part of the KDE libraries
+ *  Copyright (C) 1999 Waldo Bastian <bastian@kde.org>
+ *                     David Faure   <faure@kde.org>
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Library General Public
+ *  License version 2 as published by the Free Software Foundation;
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Library General Public License
+ *  along with this library; see the file COPYING.LIB.  If not, write to
+ *  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ *  Boston, MA 02110-1301, USA.
+ **/
+
+#ifndef KDEDESKTOPMIMETYPE_H
+#define KDEDESKTOPMIMETYPE_H
+
 #include "kmimetype.h"
 
 /**
@@ -101,3 +123,5 @@ protected:
 protected:
   virtual void virtual_hook( int id, void* data );
 };
+
+#endif
