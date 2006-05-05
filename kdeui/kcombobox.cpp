KDE/kdelibs

commit 2eb7b892fca42ce6bb5c17402536144e45c3720f

qt3support QCombobox::insertItem() KComboBox::insertURL() & changeURL() -> changed parameter order to reflect Qt api changes, added addURL()
svn path=/trunk/KDE/kdelibs/; revision=515888

kdeui/kcombobox.cpp
@@ -199,24 +199,34 @@ void KComboBox::setEditURL( const KUrl& url )
     QComboBox::setEditText( url.prettyURL() );
 }
 
-void KComboBox::insertURL( const KUrl& url, int index )
+void KComboBox::addURL( const KUrl& url )
 {
-    QComboBox::insertItem( url.prettyURL(), index );
+    QComboBox::addItem( url.prettyURL() );
 }
 
-void KComboBox::insertURL( const QPixmap& pixmap, const KUrl& url, int index )
+void KComboBox::addURL( const QIcon& icon, const KUrl& url )
 {
-    QComboBox::insertItem( pixmap, url.prettyURL(), index );
+    QComboBox::addItem( icon, url.prettyURL() );
 }
 
-void KComboBox::changeURL( const KUrl& url, int index )
+void KComboBox::insertURL( int index, const KUrl& url )
+{
+    QComboBox::insertItem( index, url.prettyURL() );
+}
+
+void KComboBox::insertURL( int index, const QIcon& icon, const KUrl& url )
+{
+    QComboBox::insertItem( index, icon, url.prettyURL() );
+}
+
+void KComboBox::changeURL( int index, const KUrl& url )
 {
     QComboBox::setItemText( index, url.prettyURL() );
 }
 
-void KComboBox::changeURL( const QPixmap& pixmap, const KUrl& url, int index )
+void KComboBox::changeURL( int index, const QIcon& icon, const KUrl& url )
 {
-	QComboBox::setItemIcon( index, QIcon( pixmap ) );
+	QComboBox::setItemIcon( index, icon );
 	QComboBox::setItemText( index, url.prettyURL() );
 }
 
@@ -320,11 +330,13 @@ void KComboBox::setCurrentItem( const QString& item, bool insert, int index )
 
     if (sel == -1 && insert)
     {
-        insertItem(item, index);
-        if (index >= 0)
-            sel = index;
-        else
-            sel = count() - 1;
+		if (index >= 0) {
+            insertItem(index, item);
+			sel = index;
+		} else {
+            addItem(item);
+			sel = count() - 1;
+		}
     }
     setCurrentIndex(sel);
 }
@@ -476,9 +488,9 @@ void KHistoryCombo::addToHistory( const QString& item )
 
     // now add the item
     if ( myPixProvider )
-        insertItem( myPixProvider->pixmapFor(item, KIcon::SizeSmall), item, 0);
+        addItem( myPixProvider->pixmapFor(item, KIcon::SizeSmall), item);
     else
-        insertItem( item, 0 );
+        addItem( item );
 
     if ( wasCurrent )
         setCurrentIndex( 0 );
@@ -667,10 +679,10 @@ void KHistoryCombo::insertItems( const QStringList& items )
         const QString item = *it;
         if ( !item.isEmpty() ) { // only insert non-empty items
             if ( myPixProvider )
-                insertItem( myPixProvider->pixmapFor(item, KIcon::SizeSmall),
+                addItem( myPixProvider->pixmapFor(item, KIcon::SizeSmall),
                             item );
             else
-                insertItem( item );
+                addItem( item );
         }
         ++it;
     }

kdeui/kactionclasses.cpp
@@ -624,7 +624,7 @@ void KSelectAction::updateItems( int id )
       QStringList lst = comboItems();
       QStringList::ConstIterator it = lst.begin();
       for( ; it != lst.end(); ++it )
-        cb->insertItem( *it );
+        cb->addItem( *it );
       // qt caches and never recalculates the sizeHint()
       // qcombobox.cpp recommends calling setFont to invalidate the sizeHint
       // setFont sets own_font = True, so we're a bit mean and calll

kdeui/kcolorcombo.cpp
@@ -239,7 +239,7 @@ void KColorCombo::addColors()
 	painter.drawText( 2, QFontMetrics(painter.font()).ascent()+2, i18n("Custom...") );
 	painter.end();
 
-	insertItem( pixmap );
+	addItem( QIcon(pixmap), QString() );
 	pixmap.detach();
 
 	for ( i = 0; i < STANDARD_PAL_SIZE; i++ )
@@ -249,7 +249,7 @@ void KColorCombo::addColors()
 		painter.fillRect( rect, brush );
 		painter.end();
 
-		insertItem( pixmap );
+		addItem( QIcon(pixmap), QString() );
 		pixmap.detach();
 
 		if ( standardPalette[i] == internalcolor )

kdeui/kcombobox.h
@@ -171,41 +171,67 @@ public:
     */
     virtual ~KComboBox();
 
-    /**
+	/**
+     * Deprecated to reflect Qt api changes
+	 * @deprecated
+	 */
+	KDE_DEPRECATED void insertURL( const KUrl& url, int index = -1 )
+		{ insertURL( index < 0 ? count() : index, url ); }
+	KDE_DEPRECATED void insertURL( const QPixmap& pixmap, const KUrl& url, int index = -1 )
+		{ insertURL( index < 0 ? count() : index, QIcon(pixmap), url ); }
+	KDE_DEPRECATED void changeURL( const KUrl& url, int index )
+		{ changeURL( index, url ); }
+    KDE_DEPRECATED void changeURL( const QPixmap& pixmap, const KUrl& url, int index )
+		{ changeURL( index, QIcon(pixmap), url ); }
+
+	/**
      * Sets @p url into the edit field of the combobox. It uses
      * KUrl::prettyURL() so that the url is properly decoded for
      * displaying.
      */
     void setEditURL( const KUrl& url );
-
-    /**
-     * Inserts @p url at position @p index into the combobox. The item will
-     * be appended if @p index is negative. KUrl::prettyURL() is used
-     * so that the url is properly decoded for displaying.
-     */
-    void insertURL( const KUrl& url, int index = -1 );
-
-    /**
-     * Inserts @p url with the pixmap &p pixmap at position @p index into
-     * the combobox. The item will be appended if @p index is negative.
+   
+	/**
+     * Appends @p url to the combobox.
      * KUrl::prettyURL() is used so that the url is properly decoded
      * for displaying.
      */
-    void insertURL( const QPixmap& pixmap, const KUrl& url, int index = -1 );
+    void addURL( const KUrl& url );
+
+	/**
+     * Appends @p url with the icon &p icon to the combobox.
+     * KUrl::prettyURL() is used so that the url is properly decoded
+     * for displaying.
+     */
+    void addURL( const QIcon& icon,  const KUrl& url );
+
+	/**
+     * Inserts @p url at position @p index into the combobox.
+     * KUrl::prettyURL() is used so that the url is properly decoded
+     * for displaying.
+     */
+	void insertURL( int index, const KUrl& url );
+
+    /**
+     * Inserts @p url with the pixmap &p pixmap at position @p index into
+     * the combobox. KUrl::prettyURL() is used so that the url is
+     * properly decoded for displaying.
+     */
+    void insertURL( int index, const QIcon& icon, const KUrl& url );
 
     /**
      * Replaces the item at position @p index with @p url.
      * KUrl::prettyURL() is used so that the url is properly decoded
      * for displaying.
      */
-    void changeURL( const KUrl& url, int index );
+    void changeURL( int index, const KUrl& url );
 
     /**
-     * Replaces the item at position @p index with @p url and pixmap @p pixmap.
+     * Replaces the item at position @p index with @p url and icon @p icon.
      * KUrl::prettyURL() is used so that the url is properly decoded
      * for displaying.
      */
-    void changeURL( const QPixmap& pixmap, const KUrl& url, int index );
+    void changeURL( int index , const QIcon& icon, const KUrl& url);
 
     /**
     * Returns the current cursor position.

commit fdfe952701710f62969f5efd472ffb737d3eaf45

Insert items at top of the history list again, bug was introduced with r515888 Now, KFindDialog works again for kate.
svn path=/trunk/KDE/kdelibs/; revision=537623

kdeui/kcombobox.cpp
@@ -489,9 +489,9 @@ void KHistoryCombo::addToHistory( const QString& item )
 
     // now add the item
     if ( myPixProvider )
-        addItem( myPixProvider->pixmapFor(item, K3Icon::SizeSmall), item);
+        insertItem( 0, myPixProvider->pixmapFor(item, K3Icon::SizeSmall), item);
     else
-        addItem( item );
+        insertItem( 0, item );
 
     if ( wasCurrent )
         setCurrentIndex( 0 );