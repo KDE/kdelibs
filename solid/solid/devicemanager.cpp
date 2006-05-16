8dbe866af154af55fceb71fec3ed9e05cf5ba5d9
Kevin Ottens <ervin@kde.org>
2006-05-16 20:34:16 +0000

    Oops, remove this useless line... it raises an assert if no backend is installed.
    svn path=/branches/work/kdehw/kdehw/; revision=541635

--- solid/solid/devicemanager.cpp
+++ solid/solid/devicemanager.cpp
@@ -87,7 +87,6 @@ KDEHW::DeviceManager::DeviceManager()
     Ifaces::DeviceManager *backend = 0;
 
     KTrader::OfferList offers = KTrader::self()->query( "KdeHwDeviceManager", "(Type == 'Service')" );
-    KService::Ptr ptr = offers.first();
 
     foreach ( KService::Ptr ptr, offers )
     {
