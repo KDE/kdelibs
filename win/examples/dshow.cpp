3739c4603b6bcef8492b2b1688e882670acab677
Ralf Habacker <ralf.habacker@freenet.de>
2006-06-01 14:17:17 +0000

    added DirectShow example, avi filepath must be changed in source
    svn path=/trunk/KDE/kdelibs/; revision=547267

---
+++ win/examples/dshow.cpp
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+
+#define INITGUID
+#include <dshow.h>
+
+
+
+int main(void)
+{
+    IGraphBuilder *pGraph = NULL;
+    IMediaControl *pControl = NULL;
+    IMediaEvent   *pEvent = NULL;
+
+    // Initialize the COM library.
+    HRESULT hr = CoInitialize(NULL);
+    if (FAILED(hr))
+    {
+        printf("ERROR - Could not initialize COM library");
+        return 1;
+    }
+
+    // Create the filter graph manager and query for interfaces.
+    hr = CoCreateInstance(CLSID_FilterGraph, NULL, CLSCTX_INPROC_SERVER, 
+                        IID_IGraphBuilder, (void **)&pGraph);
+    if (FAILED(hr))
+    {
+        printf("ERROR - Could not create the Filter Graph Manager.");
+        return 1;
+    }
+
+    hr = pGraph->QueryInterface(IID_IMediaControl, (void **)&pControl);
+    hr = pGraph->QueryInterface(IID_IMediaEvent, (void **)&pEvent);
+
+    // Build the graph. IMPORTANT: Change this string to a file on your system.
+    hr = pGraph->RenderFile(L"Brodmann_brain_movie.avi", NULL);
+    if (SUCCEEDED(hr))
+    {
+        // Run the graph.
+        hr = pControl->Run();
+        if (SUCCEEDED(hr))
+        {
+            // Wait for completion.
+            long evCode;
+            pEvent->WaitForCompletion(INFINITE, &evCode);
+
+            // Note: Do not use INFINITE in a real application, because it
+            // can block indefinitely.
+        }
+    }
+    pControl->Release();
+    pEvent->Release();
+    pGraph->Release();
+    CoUninitialize();
+    
+    return 0;
+}
