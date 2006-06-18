f4426164ef9919beca8e5568a3bc221382ea0709
Helge Deller <deller@kde.org>
2006-06-18 15:04:35 +0000

    forward-port feature from KDE-3.5 branch: increased usability of KIO KSSL dialog CCMAIL: George Staikos <staikos@kde.org>
    svn path=/trunk/KDE/kdelibs/; revision=552618

--- kio/kssl/ksslcertdlg.cc
+++ kio/kssl/ksslcertdlg.cc
@@ -26,6 +26,7 @@
 #include <qradiobutton.h>
 #include <qcheckbox.h>
 #include <q3listview.h>
+#include <qframe.h>
 #include <qlabel.h>
 
 #include <kapplication.h>
@@ -41,37 +42,47 @@
 class KSSLCertDlg::KSSLCertDlgPrivate {
 private:
     friend class KSSLCertDlg;
+    QLabel *p_message;
+    QPushButton *p_pb_dontsend;
+    bool p_send_flag;
 };
 
 KSSLCertDlg::KSSLCertDlg(QWidget *parent, const char *name, bool modal)
- : KDialog(parent ), d(new KSSLCertDlgPrivate) {
-   QGridLayout *grid = new QGridLayout(this);
-   grid->setMargin(KDialog::marginHint());
-   grid->setSpacing(KDialog::spacingHint());
-
+ : KDialog(parent), d(new KSSLCertDlgPrivate) {
    setObjectName(name);
    setModal(modal);
 
-   _send = new QRadioButton(i18n("Send certificate..."), this);
-   grid->addWidget(_send, 0, 0, 1, 3);
-   connect(_send, SIGNAL(clicked()), SLOT(slotSend()));
+   QBoxLayout * grid = new QVBoxLayout( this );
+   grid->setMargin(KDialog::marginHint());
+   grid->setSpacing(KDialog::spacingHint());
 
-   _dont = new QRadioButton(i18n("Do not send a certificate"), this);
-   grid->addWidget(_dont, 1, 0, 1, 3);
-   connect(_dont, SIGNAL(clicked()), SLOT(slotDont()));
+   d->p_message = new QLabel(QString::null, this);
+   grid->addWidget(d->p_message);
+   setHost(_host);
 
    _certs = new Q3ListView(this);
-   grid->addWidget(_certs, 0, 3, 5, 3);
    _certs->addColumn(i18n("Certificate"));
+   _certs->setResizeMode(Q3ListView::LastColumn);
+   QFontMetrics fm( KGlobalSettings::generalFont() );
+   _certs->setMinimumHeight(4*fm.height());
+   grid->addWidget(_certs);
 
    _save = new QCheckBox(i18n("Save selection for this host."), this);
-   grid->addWidget(_save, 5, 0, 1, 4);
+   grid->addWidget(_save);
 
-   grid->addWidget(new KSeparator(Qt::Horizontal, this), 6, 0, 1, 6);
+   grid->addWidget(new KSeparator(Qt::Horizontal, this));
 
-   _ok = new KPushButton(KStdGuiItem::cont(), this);
-   grid->addWidget(_ok, 7, 5);
-   connect(_ok, SIGNAL(clicked()), SLOT(accept()));
+   QBoxLayout * h = new QHBoxLayout(this);
+   h->insertStretch(0);
+   grid->addLayout(h);
+
+   _ok = new KPushButton(i18n("Send certificate"), this);
+   h->addWidget(_ok);
+   connect(_ok, SIGNAL(clicked()), SLOT(slotSend()));
+
+   d->p_pb_dontsend = new KPushButton(i18n("Do not send a certificate"), this);
+   h->addWidget(d->p_pb_dontsend);
+   connect(d->p_pb_dontsend, SIGNAL(clicked()), SLOT(slotDont()));
 
 #ifndef QT_NO_WIDGET_TOPEXTRA
    setCaption(i18n("KDE SSL Certificate Dialog"));
@@ -90,9 +101,12 @@ void KSSLCertDlg::setup(QStringList certs, bool saveChecked, bool sendChecked) {
 
 void KSSLCertDlg::setupDialog(const QStringList& certs, bool saveChecked, bool sendChecked) {
   _save->setChecked(saveChecked);
-  _send->setChecked(sendChecked);
-  _dont->setChecked(!sendChecked);
-  _certs->setEnabled(sendChecked);
+  d->p_send_flag = sendChecked;
+
+  if (sendChecked)
+    _ok->setDefault(true); // "do send" is the "default action".
+  else
+    d->p_pb_dontsend->setDefault(true); // "do not send" is the "default action".
 
   for (QStringList::ConstIterator i = certs.begin(); i != certs.end(); ++i) {
     if ((*i).isEmpty())
@@ -111,35 +125,38 @@ bool KSSLCertDlg::saveChoice() {
 
 
 bool KSSLCertDlg::wantsToSend() {
-  return _send->isChecked();
+  return d->p_send_flag;
 }
 
 
 QString KSSLCertDlg::getChoice() {
-   return _certs->selectedItem()->text(0);
+   Q3ListViewItem *selected = _certs->selectedItem();
+   if (selected && d->p_send_flag)
+	return selected->text(0);
+   else
+	return QString::null;
 }
 
 
 void KSSLCertDlg::setHost(const QString& host) {
    _host = host;
-#ifndef QT_NO_WIDGET_TOPEXTRA
-   setCaption(i18n("KDE SSL Certificate Dialog")+" - "+host);
-#endif
+   d->p_message->setText(i18n("The server <b>%1</b> requests a certificate.<p>"
+			     "Select a certificate to use from the list below:")
+			 .arg(_host));
 }
 
 
 void KSSLCertDlg::slotSend() {
-   _dont->setChecked(false);
-   _send->setChecked(true);
-   _certs->setEnabled(true);
+   d->p_send_flag = true;
+   accept();
 }
 
 
 void KSSLCertDlg::slotDont() {
-   _send->setChecked(false);
-   _dont->setChecked(true);
-   _certs->setEnabled(false);
+   d->p_send_flag = false;
+   reject();
 }
+
 
 QDataStream& operator<<(QDataStream& s, const KSSLCertDlgRet& r) {
    s << qint8(r.ok?1:0) <<  r.choice << qint8(r.save?1:0) << qint8(r.send?1:0);
@@ -155,6 +172,7 @@ QDataStream& operator>>(QDataStream& s, KSSLCertDlgRet& r) {
    s >> tmp; r.send = (tmp == 1);
    return s;
 }
+
 
 #include "ksslcertdlg.moc"
 
