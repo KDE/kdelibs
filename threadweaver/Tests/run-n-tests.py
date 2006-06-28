bba911513cac7b20e34b5e4114ca95e2a8595ec2
Mirko Boehm <mirko@kde.org>
2006-06-28 01:21:58 +0000

    - try to make test run script platform independent
    svn path=/trunk/kdenonbeta/threadweaver/; revision=555619

--- threadweaver/Tests/run-n-tests.py
+++ threadweaver/Tests/run-n-tests.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python
 
+import re
 import os
 import sys
 import time
+import platform
 
 def RunTests( Number ):
+    # use Linux as the default:
+    Platform = platform.platform()
+    Command = "LD_LIBRARY_PATH=../Weaver:../Experimental ./JobTests >/dev/null"
+    if re.search( "Darwin", Platform ):
+        Command = "DYLD_LIBRARY_PATH=../Weaver:../Experimental ./JobTests.app/Contents/MacOs/JobTests >/dev/null"
     for count in range( Number ):
         print "Test run #", count + 1
-        os.system ("LD_LIBRARY_PATH=../Weaver:../Experimental ./JobTests >/dev/null")
+        os.system ( Command)
 
 Number = 10
 
@@ -25,8 +32,6 @@ elapsed = ((t2 - t1) / 60.0, t2 - t1, (t2 - t1) * 1000.0)
 
 print "RunTests: %0.3fms (%0.3fs)" % ( elapsed[2], elapsed[1] )
 print "Averages: %0.3fms per test run (%0.3fs)" % ( elapsed[2] / Number, elapsed[1] / Number )
-
-
 
 
 
