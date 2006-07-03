243569c193a7f1dbed55df4650ff88b56a915750
Hamish Rodda <rodda@kde.org>
2006-07-03 15:40:45 +0000

    Add copyright headers
    svn path=/trunk/KDE/kdelibs/; revision=557585

--- kdeui/liveui/kliveui.h
+++ kdeui/liveui/kliveui.h
@@ -1,3 +1,23 @@
+/* This file is part of the KDE libraries
+    Copyright (C) 2006 Simon Hausmann <hausmann@kde.org>
+    Copyright (C) 2006 Hamish Rodda <rodda@kde.org>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301, USA.
+*/
+
 #ifndef KLIVEUI_H
 #define KLIVEUI_H
 
@@ -23,7 +43,7 @@ class KDEUI_EXPORT KLiveUiComponent
     friend class KLiveUiBuilder;
 public:
     inline virtual ~KLiveUiComponent() {}
-    
+
     QObject *qObject() { return static_cast<QObject *>(qt_metacast("QObject")); }
 
     void activateComponentGui(QMainWindow *mw);
@@ -60,7 +80,7 @@ public:
 
     void addAction(QAction *action);
     void addActions(const QList<QAction *> actions);
-    
+
     // convenience
     QAction *addAction(const QString &text);
     QAction *addAction(const QIcon &icon, const QString &text);
@@ -69,7 +89,7 @@ public:
     QAction *addAction(const QIcon &icon, const QString &text,
                        const QObject *receiver, const char *member,
                        const QKeySequence &shortcut = QKeySequence());
-    
+
     QAction *addSeparator();
     QAction *addWidget(QWidget *widget);
 
@@ -77,7 +97,7 @@ public:
 
     void beginActionGroup(const QString &name);
     void endActionGroup();
-    
+
     void populateFromXmlGui(const QString &fileName);
 
 private:
