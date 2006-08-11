d1da205a0b61954c225d0bc7aed8b4f2fed98e1b
Matthias Kretz <kretz@kde.org>
2006-08-11 21:14:10 +0000

    SVN_SILENT: tests recreated
    svn path=/trunk/KDE/kdelibs/; revision=572191

--- phonon/tests/methods/bytestream.cpp
+++ phonon/tests/methods/bytestream.cpp
@@ -3,7 +3,6 @@ addMethod( "", "setStreamSeekable(bool)" );
 addMethod( "", "setStreamSize(qint64)" );
 addMethod( "qint64", "streamSize()" );
 addMethod( "qint64", "remainingTime()", true );
-addMethod( "qint64", "totalTime()" );
 addMethod( "bool", "streamSeekable()" );
 addMethod( "qint32", "aboutToFinishTime()" );
 addMethod( "", "setAboutToFinishTime(qint32)" );
