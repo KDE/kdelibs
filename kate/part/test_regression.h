471afca688dc0fd16e3a062da078d1f8ae9b1c8f
Leo Savernik <l.savernik@aon.at>
2006-09-07 22:39:13 +0000

    Introducing automatic regression testing for kate.
    I lifted the regression tester from khtml, so you'll still find many debris 
    from the conversion.
    
    Conceptionally, it works like this:
    - start with regression directory
    - read testcases under directory tests/
    - execute script on each test
    - compare result with template under baseline/
    - if both are equal, consider test as passed
    
    The tool works fully automatically. Additional testcases can be added easily 
    by providing an initial testcase under tests/ and an accompanying script 
    containing the statements as well as a proposed result template under 
    baseline.
    
    Shortcomings:
    - Testcase execution is *very* slow.
    - Some results are not deterministic. I. e. if you invoke
      ./testkateregression indent/csmart/openpar1.txt
      it will fail. If you run it in batch, it will pass.
    - Currently, there are only testcases for the c-style indenter.
        
        
    
    svn path=/branches/KDE/3.5/kdelibs/; revision=581920

---
+++ kate/part/test_regression.h
@@ -0,0 +1,172 @@
+/**
+ * This file is part of the KDE project
+ *
+ * Copyright (C) 2001,2003 Peter Kelly (pmk@post.com)
+ * Copyright 2006 Leo Savernik (l.savernik@aon.at)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ *
+ */
+
+#ifndef TEST_REGRESSION_H
+#define TEST_REGRESSION_H
+
+#include <katejscript.h>
+#include <kateview.h>
+#include <kurl.h>
+#include <qobject.h>
+#include <qstringlist.h>
+#include <kjs/ustring.h>
+#include <kjs/object.h>
+#include <kjs/interpreter.h>
+
+class KateDocument;
+class KateView;
+class RegressionTest;
+class QTimer;
+
+namespace KParts {
+  class URLArgs;
+}
+
+/**
+ * @internal
+ * The backbone of Kate's automatic regression tests.
+ */
+class TestJScriptEnv : public KateJScript
+{
+  public:
+    TestJScriptEnv(KateDocument *part);
+    virtual ~TestJScriptEnv();
+
+    /** returns the global scope */
+    KJS::Object global() const { return *m_global; }
+    /** returns the script interpreter */
+    KJS::Interpreter &interpreter() { return *m_interpreter; }
+    /** returns the document scope */
+    KJS::Object document() const { return *m_document; }
+    /** returns the view scope */
+    KJS::Object view() const { return *m_view; }
+};
+
+/**
+ * @internal
+ */
+class KateViewObject : public KJS::ObjectImp
+{
+  public:
+    KateViewObject(KJS::ExecState *exec, KateView *v, KJS::ObjectImp *fallback);
+    virtual ~KateViewObject();
+
+    virtual const KJS::ClassInfo *classInfo() const;
+    virtual KJS::Value get(KJS::ExecState *exec, const KJS::Identifier &propertyName) const;
+
+  private:
+    // evil hack I: class layout of katejscript/KateJSView must be duplicated
+    // here, structurally as well as functionally
+    KateView *view;
+    // end evil hack
+    KJS::ObjectImp *fallback;
+};
+
+/**
+ * @internal
+ */
+class KateViewFunction : public KJS::ObjectImp
+{
+  public:
+    KateViewFunction(KJS::ExecState *exec, KateView *v, int _id, int length);
+
+    bool implementsCall() const;
+    KJS::Value call(KJS::ExecState *exec, KJS::Object &thisObj, const KJS::List &args);
+
+    enum { KeyReturn, Type };
+  private:
+    KateView *m_view;
+    int id;
+};
+
+/**
+ * @internal
+ */
+class RegressionTest : public QObject
+{
+  Q_OBJECT
+public:
+
+    RegressionTest(KateDocument *part, const QString &baseDir,
+                   const QString &outputDir, bool _genOutput);
+    ~RegressionTest();
+
+    enum OutputType { ResultDocument };
+    void testStaticFile(const QString& filename, const QStringList &commands);
+    enum CheckResult { Failure = 0, Success = 1, Ignored = 2 };
+    CheckResult checkOutput(const QString& againstFilename);
+    enum FailureType { NoFailure = 0, AllFailure = 1, ResultFailure = 4 };
+    bool runTests(QString relPath = QString::null, bool mustExist = false, int known_failure = NoFailure, QStringList commands = QStringList());
+    bool reportResult( bool passed, const QString & description = QString::null );
+    bool reportResult(CheckResult result, const QString & description = QString::null );
+    void createMissingDirs(const QString &path);
+
+    void createLink( const QString& test, int failures );
+    void doFailureReport( const QString& test, int failures );
+
+    KateDocument *m_part;
+    KateView *m_view;
+    QString m_baseDir;
+    QString m_outputDir;
+    bool m_genOutput;
+    QString m_currentBase;
+
+    QString m_currentOutput;
+    QString m_currentCategory;
+    QString m_currentTest;
+
+    bool m_keepOutput;
+    bool m_getOutput;
+    bool m_showGui;
+    int m_passes_work;
+    int m_passes_fail;
+    int m_failures_work;
+    int m_failures_fail;
+    int m_errors;
+    bool saw_failure;
+    bool ignore_errors;
+    int m_known_failures;
+
+    static RegressionTest *curr;
+
+private:
+    void printDescription(const QString& description);
+
+    static bool svnIgnored( const QString &filename );
+
+private:
+    /**
+     * evaluate script given by \c filename within the context of \c interp.
+     * @param ignore if \c true don't evaluate if script does not exist but
+     * return true nonetheless.
+     * @return true if script was valid, false otherwise
+     */
+    bool evalJS( KJS::Interpreter &interp, const QString &filename, bool ignore = false);
+
+private slots:
+    void slotOpenURL(const KURL &url, const KParts::URLArgs &args);
+    void resizeTopLevelWidget( int, int );
+
+};
+
+#endif
