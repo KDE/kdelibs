6d2653dec44262f8cf01aed1fc9f4709d6db4b7c
Richard J. Moore <rich@kde.org>
2006-09-26 16:52:36 +0000

    Fixed the logic of the args test case. We still need to figure out why the applicationName property isn't working but that's a different issue.
    svn path=/trunk/KDE/kdelibs/; revision=588699

--- kjsembed/tests/args.js
+++ kjsembed/tests/args.js
@@ -1,3 +1,5 @@
-println("Application " + Application.applicationName + " arguments ");
-for( arg in Application.args[0] )
-	println("Application arg " + arg );
+// Why is this broken
+//println("Application " + Application.applicationName + " arguments ");
+
+for( arg in Application.args )
+	println("Application arg " + arg + ' value ' + Application.args[arg] );
