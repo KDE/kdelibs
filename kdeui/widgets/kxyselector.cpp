b21c82c9c4c900eb881510ada967a58d19245b9c
Stephan Kulow <coolo@kde.org>
2006-10-06 10:48:10 +0000

    now it compiles
    svn path=/trunk/KDE/kdelibs/; revision=592954

--- kdeui/widgets/kxyselector.cpp
+++ kdeui/widgets/kxyselector.cpp
@@ -24,6 +24,7 @@
 #include <QPainter>
 #include <QStyleOptionFrame>
 #include <QMouseEvent>
+#include <kdebug.h>
 
 //-----------------------------------------------------------------------------
 /*
