349a6eeb23f6627306a5f3ce720ac2310b5fa4f9
Jaison Lee <lee.jaison@gmail.com>
2006-10-08 14:26:26 +0000

    Add tests for KTemporaryFile.
    svn path=/trunk/KDE/kdelibs/; revision=593618

---
+++ kdecore/tests/ktemporaryfiletest.cpp
@@ -0,0 +1,80 @@
+/* kate: tab-indents off; replace-tabs on; tab-width 4; remove-trailing-space on; encoding utf-8;*/
+/*
+This file is part of the KDE libraries
+This file has been placed in the Public Domain.
+*/
+
+#include "qtest_kde.h"
+
+#include <QtCore/QDir>
+
+#include "kstandarddirs.h"
+
+#include "ktemporaryfiletest.h"
+#include "ktemporaryfile.h"
+#include "ktemporaryfiletest.moc"
+
+QTEST_KDEMAIN( KTemporaryFileTest, NoGUI )
+
+/*
+Notes on these tests:
+
+- I'm not testing anything that QTemporaryFile should handle, like the
+working of setAutoRemove(). I suggest letting Trolltech handle making sure
+those things work. These should only test KDE specific functionality.
+
+*/
+
+void KTemporaryFileTest::initTestCase()
+{
+    kdeTempDir = KStandardDirs::locateLocal("tmp", "");
+    instanceName = KGlobal::instance()->instanceName();
+
+    QDir qdir ( kdeTempDir );
+    qdir.mkdir("foo");
+}
+
+void KTemporaryFileTest::cleanupTestCase()
+{
+    QDir qdir ( kdeTempDir );
+    qdir.rmdir("foo");
+}
+
+// Test putting files in the default KDE temp directory
+void KTemporaryFileTest::testKTemporaryFile()
+{
+
+    //Test basic placement
+    {
+        KTemporaryFile file;
+        QVERIFY(file.open());
+        QVERIFY(file.fileName().startsWith(kdeTempDir + instanceName));
+        QVERIFY(file.fileName().endsWith(".tmp"));
+        QVERIFY(QFile::exists(file.fileName()));
+    }
+
+    //Test relative subdirectory
+    {
+        KTemporaryFile file;
+        file.setPrefix("foo/");
+        QVERIFY(file.open());
+        QVERIFY(file.fileName().startsWith(kdeTempDir + "foo/"));
+        QVERIFY(file.fileName().endsWith(".tmp"));
+        QVERIFY(QFile::exists(file.fileName()));
+    }
+
+    //Test relative filename
+    {
+        KTemporaryFile file;
+        file.setPrefix("spam");
+        file.setSuffix("eggs");
+        QVERIFY(file.open());
+        QVERIFY(file.fileName().startsWith(kdeTempDir + "spam"));
+        QVERIFY(file.fileName().endsWith("eggs"));
+        QVERIFY(QFile::exists(file.fileName()));
+    }
+    
+    //TODO: How to test outside of tmp when we can't be sure what
+    //directories we have write access to?
+}
+
---
+++ kdecore/tests/ktemporaryfiletest.h
@@ -0,0 +1,26 @@
+/* kate: tab-indents off; replace-tabs on; tab-width 4; remove-trailing-space on; encoding utf-8;*/
+/*
+This file is part of the KDE libraries
+This file has been placed in the Public Domain.
+*/
+
+#ifndef ktemporaryfiletest_h
+#define ktemporaryfiletest_h
+
+#include <QtCore/QObject>
+#include <QtCore/QString>
+
+class KTemporaryFileTest : public QObject
+{
+Q_OBJECT
+private slots:
+    void initTestCase();
+    void cleanupTestCase();
+    void testKTemporaryFile();
+
+private:
+    QString kdeTempDir;
+    QString instanceName;
+};
+
+#endif
