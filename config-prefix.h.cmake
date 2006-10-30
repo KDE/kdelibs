3722a2743f4f14faae18f3e5c3f63220283d8ca9
Laurent Montel <montel@kde.org>
2006-10-30 13:22:20 +0000

    Dcop is dead
    svn path=/trunk/KDE/kdelibs/; revision=600415

--- config-prefix.h.cmake
+++ config-prefix.h.cmake
@@ -7,8 +7,6 @@
 
 #define __KDE_BINDIR "${CMAKE_INSTALL_PREFIX}/bin"
 
-#define DCOP_PATH "${CMAKE_INSTALL_PREFIX}/bin"
-
 /* Compile in the exec prefix to help kstddirs in finding dynamic libs
    (This was for exec_prefix != prefix - still needed?) */
 #define __KDE_EXECPREFIX "NONE"
