d254c898315e81e0a2ef72bffe7f3bc7ae589750
Dirk Mueller <mueller@kde.org>
2006-11-23 11:03:31 +0000

    also prepend /bin and /usr/bin (binc:56479)
    svn path=/trunk/KDE/kdelibs/; revision=607143

--- kdesu/stub.cpp
+++ kdesu/stub.cpp
@@ -93,11 +93,13 @@ int StubProcess::ConverseStub(int check)
 	    writeLine(m_Command);
 	} else if (line == "path") {
 	    QByteArray path = getenv("PATH");
-	    if (m_User == "root")
+            if (!path.isEmpty() && path[0] == ':')
+                path = path.mid(1);
+	    if (m_User == "root") 
 		if (!path.isEmpty())
-		    path = "/sbin:/usr/sbin:" + path;
+		    path = "/sbin:/bin:/usr/sbin:/usr/bin:" + path;
 		else
-		    path = "/sbin:/usr/sbin";
+		    path = "/sbin:/bin:/usr/sbin:/usr/bin";
 	    writeLine(path);
 	} else if (line == "user") {
 	    writeLine(m_User);
