caa0b40e40e2672fefa851cad997d47e7769f005
Christian Ehrlicher <ch.ehrlicher@gmx.de>
2006-11-23 17:26:11 +0000

    fix msvc compile because cacheGlobalObject<class>() is not in namespace KJS, we have to use ::cacheGlobalObject<class>() here
    CCMAIL: rodda@kde.org
    
    svn path=/trunk/KDE/kdelibs/; revision=607205

--- khtml/ecma/kjs_binding.h
+++ khtml/ecma/kjs_binding.h
@@ -378,12 +378,13 @@ namespace KJS {
     } \
    const ClassInfo Class::info = { ClassName, 0, &Class##Table, 0 };
 
+// cacheGlobalObject<> is not in namepsace KJS - need to use ::cacheGlobalObject<>
 #define KJS_EMPTY_PROTOTYPE_IMP(ClassName, ClassProto, ProtoCode) \
       class ClassProto : public ObjectImp { \
-      friend ObjectImp* cacheGlobalObject<ClassProto>(ExecState *exec, const Identifier &propertyName); \
+      friend ObjectImp* ::cacheGlobalObject<ClassProto>(ExecState *exec, const Identifier &propertyName); \
       public: \
         static ObjectImp* self(ExecState *exec) { \
-          return cacheGlobalObject<ClassProto>(exec, *name()); \
+          return ::cacheGlobalObject<ClassProto>(exec, *name()); \
         } \
         virtual const ClassInfo *classInfo() const { return &info; } \
         static const ClassInfo info; \
