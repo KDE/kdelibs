a2b862efc483394c9a43b278371154c92a9fa5d0
Ralf Habacker <ralf.habacker@freenet.de>
2006-12-24 01:05:10 +0000

    revert last patch: application/octet-stream is the expected result
    svn path=/trunk/KDE/kdelibs/; revision=616182

--- kio/tests/kmimetypetest.cpp
+++ kio/tests/kmimetypetest.cpp
@@ -112,12 +112,8 @@ void KMimeTypeTest::testFindByUrl()
     mf = KMimeType::findByUrl( KUrl("http://foo/bar.png") );
     QVERIFY( mf );
 
-#ifdef Q_WS_WIN
-    // on win32 we get image/png ?? 
-    QCOMPARE( mf->name(), QString::fromLatin1( "image/png" ) ); 
-#else
     QCOMPARE( mf->name(), QString::fromLatin1( "application/octet-stream" ) ); // HTTP can't know before downloading
-#endif
+
     if ( !KProtocolInfo::isKnownProtocol(KUrl("man:/")) )
         QSKIP( "man protocol not installed", SkipSingle );
 
