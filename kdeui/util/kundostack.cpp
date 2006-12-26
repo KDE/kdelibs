41eec788d03f34d88b3ac882ac390decb1db4ee3
Peter Simonsson <peter.simonsson@gmail.com>
2006-12-26 12:49:32 +0000

    Add a KUndoStack that adds functions to QUndoStack for creating actions with kde's shortcuts and icons.
    svn path=/trunk/KDE/kdelibs/; revision=616675

---
+++ kdeui/util/kundostack.cpp
@@ -0,0 +1,78 @@
+/* This file is part of the KDE project
+   Copyright (C) 2006 Peter Simonsson <peter.simonsson@gmail.com>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "kundostack.h"
+
+#include <QAction>
+#include <QKeySequence>
+#include <QList>
+
+#include <kstandardaction.h>
+#include <kstdaccel.h>
+#include <kicon.h>
+#include <kactioncollection.h>
+
+KUndoStack::KUndoStack(QObject* parent)
+: QUndoStack(parent)
+{
+}
+
+QAction* KUndoStack::createRedoAction(KActionCollection* actionCollection, const QString& actionName)
+{
+    QAction* action = QUndoStack::createRedoAction(actionCollection);
+
+    if(actionName.isEmpty()) {
+        action->setObjectName(KStandardAction::stdName(KStandardAction::Redo));
+    } else {
+        action->setObjectName(actionName);
+    }
+
+    action->setIcon(KIcon("redo"));
+
+    QList<QKeySequence> shortcuts;
+    shortcuts << KStdAccel::redo().primary() << KStdAccel::redo().alternate();
+    action->setShortcuts(shortcuts);
+
+    actionCollection->insert(action);
+
+    return action;
+}
+
+QAction* KUndoStack::createUndoAction(KActionCollection* actionCollection, const QString& actionName)
+{
+    QAction* action = QUndoStack::createUndoAction(actionCollection);
+
+    if(actionName.isEmpty()) {
+        action->setObjectName(KStandardAction::stdName(KStandardAction::Undo));
+    } else {
+        action->setObjectName(actionName);
+    }
+
+    action->setIcon(KIcon("undo"));
+
+    QList<QKeySequence> shortcuts;
+    shortcuts << KStdAccel::undo().primary() << KStdAccel::undo().alternate();
+    action->setShortcuts(shortcuts);
+
+    actionCollection->insert(action);
+
+    return action;
+}
+
+#include "kundostack.moc"
---
+++ kdeui/util/kundostack.h
@@ -0,0 +1,59 @@
+/* This file is part of the KDE project
+   Copyright (C) 2006 Peter Simonsson <peter.simonsson@gmail.com>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#ifndef KUNDOSTACK_H
+#define KUNDOSTACK_H
+
+#include <QUndoStack>
+
+#include "kdelibs_export.h"
+
+class KActionCollection;
+
+/**
+ * Extends QUndoStack with functions that creates actions with KDE's default icons and shortcuts.
+ * See QUndoStack for more information.
+ */
+class KDEUI_EXPORT KUndoStack : public QUndoStack
+{
+    Q_OBJECT
+    public:
+        /**
+         * Constructs a KUndoStack with @p parent as parent
+         * @param parent parent of the object
+         */
+        KUndoStack(QObject* parent = 0);
+
+        /**
+         * Creates an redo action with the default shortcut and icon and adds it to @p actionCollection
+         * @param actionCollection the KActionCollection that should be the parent of the action
+         * @param actionName the created action's object name, empty string will set it to the KDE default
+         * @return the created action.
+         */
+        QAction* createRedoAction(KActionCollection* actionCollection, const QString& actionName = QString());
+        /**
+         * Creates an undo action with the default KDE shortcut and icon and adds it to @p actionCollection
+         * @param actionCollection the KActionCollection that should be the parent of the action
+         * @param actionName the created action's object name, empty string will set it to the KDE default
+         * @return the created action.
+         */
+        QAction* createUndoAction(KActionCollection* actionCollection, const QString& actionName = QString());
+};
+
+#endif
