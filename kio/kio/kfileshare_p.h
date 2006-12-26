9ea89d22c95f381bb1f0f4996051a59f83f9120a
Aaron J. Seigo <aseigo@kde.org>
2006-12-26 19:01:41 +0000

    and the new header too
    svn path=/trunk/KDE/kdelibs/; revision=616776

---
+++ kio/kio/kfileshare_p.h
@@ -0,0 +1,42 @@
+/* This file is part of the KDE project
+   Copyright (c) 2001 David Faure <faure@kde.org>
+   Copyright (c) 2001 Laurent Montel <lmontel@mandrakesoft.com>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License version 2 as published by the Free Software Foundation.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#ifndef KFILESHAREPRIVATE_H
+#define KFILESHAREPRIVATE_H
+
+/**
+ * @internal
+ * Do not use, ever.
+ */
+class KFileSharePrivate : public QObject
+{
+  Q_OBJECT
+
+public:
+  KFileSharePrivate();
+  ~KFileSharePrivate();
+  KDirWatch* m_watchFile;
+  static KFileSharePrivate *self();
+  static KFileSharePrivate *_self;
+protected Q_SLOTS: // this is why this class needs to be in the .h
+ void slotFileChange(const QString &);
+};
+
+#endif
+
