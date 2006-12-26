964de105f418fa05f63c920b5f716cf09ecb7c17
Aaron J. Seigo <aseigo@kde.org>
2006-12-26 22:52:20 +0000

    replace kstdaccel.h with an error inducing #include also make the kstdguiitem.h error make a bit more sense ;)
    svn path=/trunk/KDE/kdelibs/; revision=616814

---
+++ kde3support/kdeui/kstdaccel.h
@@ -0,0 +1,1 @@
+#error kstdaccel.h does not exist anymore. The KStdAccel class has been renamed to KStandardShortcut and the header file to kstandardshortcut.h. See the KDE4 Porting Guide for more information.
--- kde3support/kdeui/kstdguiitem.h
+++ kde3support/kdeui/kstdguiitem.h
@@ -1,1 +1,1 @@
-#error kstandardguiitem.h does not exist anymore. The KStandardGuiItem class has been renamed to KStandardGuiItem and the header file to kstandardguiitem.h. See the KDE4 Porting Guide for more information.
+#error kstdguiitem.h does not exist anymore. The KStdGuiItem class has been renamed to KStandardGuiItem and the header file to kstandardguiitem.h. See the KDE4 Porting Guide for more information.
