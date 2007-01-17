07d5defed8c45df9d7fb05d55d7d2e783b9a9efc
Jacob Rideout <kde@jacobrideout.net>
2007-01-17 12:21:15 +0000

    Changed license from GPL to LGPL for old sonnet class. This had already been changed in branches-work/sonnet
    svn path=/trunk/KDE/kdelibs/; revision=624592

--- sonnet/misc/textbreaks.h
+++ sonnet/misc/textbreaks.h
@@ -1,17 +1,21 @@
-/****************************************************************************
-**
-** Copyright (C) 2006 Jacob R Rideout <jacob@jacobrideout.net>
-** All rights reserved.
-**
-** This file may be used under the terms of the GNU General Public
-** License version 2.0 as published by the Free Software Foundation
-** and appearing in the file LICENSE.GPL included in the packaging of
-** this file.
-**
-** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
-** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
-**
-****************************************************************************/
+/*  This file is part of the KDE libraries
+    Copyright (c) 2006 Jacob R Rideout <kde@jacobrideout.net>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301, USA.
+*/
 
 #ifndef TEXTBREAKS
 #define TEXTBREAKS
