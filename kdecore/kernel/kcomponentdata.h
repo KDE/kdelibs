12ffd0357c720c4a38d879cbbc1cbba114ae16f0
Matthias Kretz <kretz@kde.org>
2007-01-30 18:17:10 +0000

    update dox
    svn path=/trunk/KDE/kdelibs/; revision=628627

--- kdecore/kernel/kcomponentdata.h
+++ kdecore/kernel/kcomponentdata.h
@@ -1,5 +1,6 @@
 /* This file is part of the KDE libraries
    Copyright (C) 1999 Torben Weis <weis@kde.org>
+   Copyright (C) 2007 Matthias Kretz <kretz@kde.org>
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
@@ -28,12 +29,19 @@ class KStandardDirs;
 class KComponentDataPrivate;
 
 /**
- * Access to KDE global objects for use in shared libraries.  In
- * practical terms, this class is used in KDE components.  This allows
- * components to store things that normally would be accessed by
- * KGlobal.
+ * \short Per component data.
  *
- * @author Torben Weis
+ * This class holds a KAboutData object or only a component name, a KStandardDirs object and a
+ * KSharedConfig object. Those objects normally are different per component but the same per
+ * instance of one component.
+ *
+ * The application component data can always be accessed using KGlobal::mainComponent() (or the
+ * convenience function KGlobal::dirs() and KGlobal::config()) while the
+ * component data of the currently active component (mainly used for KParts) can be accessed using
+ * KGlobal::activeComponent().
+ *
+ * \author Torben Weis
+ * \author Matthias Kretz <kretz@kde.org>
  */
 class KDECORE_EXPORT KComponentData
 {
@@ -41,27 +49,60 @@ class KDECORE_EXPORT KComponentData
     KSharedConfig::Ptr &privateConfig() const;
 
 public:
+    /**
+     * Creates an invalid KComponentData object.
+     *
+     * \see isValid()
+     */
     KComponentData();
+
+    /**
+     * Copy constructor.
+     *
+     * It does not copy the data. The data is shared between the old and new objects.
+     */
     KComponentData(const KComponentData&);
+
+    /**
+     * Assignment operator.
+     *
+     * It does not copy the data. The data is shared between the old and new objects.
+     *
+     * If the data of the left hand side object was only referenced from this object it is deleted
+     * if no referenced KSharedConfig object doesn't need it anymore.
+     */
     KComponentData &operator=(const KComponentData&);
+
+    /**
+     * Returns whether two KComponentData objects reference the same data.
+     */
     bool operator==(const KComponentData&) const;
+
+    /**
+     * Returns whether two KComponentData objects do not reference the same data.
+     */
     bool operator!=(const KComponentData &rhs) const { return !operator==(rhs); }
 
     /**
-     *  Constructor.
-     *  @param componentName the name of the instance
+     * Constructor.
+     *
+     * \param componentName the name of the component.
      */
     explicit KComponentData(const QByteArray &componentName);
 
     /**
      *  Constructor.
-     *  When building a KComponentData that is not your KApplication,
+     *
+     *  When instanciating a KComponentData that is not your KApplication,
      *  make sure that the KAboutData and the KComponentData have the same life time.
-     *  You have to destroy both, since the instance doesn't own the about data.
-     *  Don't build a KAboutData on the stack in this case !
+     *  You have to destroy both, since KComponentData does not own the about data.
+     *  Do not create a KAboutData on the stack in this case!
      *  Building a KAboutData on the stack is only ok for usage with
      *  KCmdLineArgs and KApplication (not destroyed until the app exits).
-     *  @param aboutData data about this instance (see KAboutData)
+     *
+     *  \param aboutData data about this component
+     *
+     *  \see KAboutData
      */
     explicit KComponentData(const KAboutData *aboutData);
 
@@ -70,6 +111,12 @@ public:
      */
     virtual ~KComponentData();
 
+    /**
+     * Returns whether this is a valid object.
+     *
+     * Don't call any functions on invalid objects, that will crash. Assignment (and of course
+     * destruction) is the only valid operation you may do.
+     */
     bool isValid() const;
 
     /**
@@ -80,22 +127,22 @@ public:
 
     /**
      * Returns the general config object ("appnamerc").
-     * @return the KConfig object for the instance.
+     * @return the KConfig object for the component.
      */
     const KSharedConfig::Ptr &config() const;
 
     /**
-     *  Returns the about data of this instance
-     *  Warning, can be 0L
-     * @return the about data of the instance, or 0 if it has
-     *         not been set yet
+     * Returns the about data of this component.
+     *
+     * \return The about data of the component. If none has been set in the constructor but a
+     * component name was set a default constructed KAboutData object is returned.
      */
     const KAboutData *aboutData() const;
 
     /**
-     * Returns the name of the instance
-     * @return the instance name, can be null if the KComponentData has been
-     *         created with a null name
+     * Returns the name of the component.
+     *
+     * \return The component name.
      */
     QByteArray componentName() const;
 
