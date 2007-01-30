328cc1d7cf56aa4b07dede5bb36717a608f6ae8a
Josef Spillner <spillner@kde.org>
2007-01-30 10:42:24 +0000

    - dia's command line options differ from day to day, switch back to dia 0.95 options - add caching to the kns2 feature guide
    svn path=/branches/work/knewstuff2/doc/; revision=628546

--- knewstuff/doc/export.sh
+++ knewstuff/doc/export.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#for i in *.dia; do echo $i; dia --export=$i.png --filter=png --nosplash $i; done
+for i in *.dia; do echo $i; dia --export=$i.png --filter=png --nosplash $i; done
 
-for i in *.dia; do echo $i; dia --export=$i.png --export-to-format=png --nosplash $i; done
+#for i in *.dia; do echo $i; dia --export=$i.png --export-to-format=png --nosplash $i; done
 
