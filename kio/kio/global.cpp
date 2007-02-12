3a356ff0869cfcc5ffec5b5959e207e36fe8a3c0
David Faure <faure@kde.org>
2007-02-12 09:08:24 +0000

    Adjust itemsSummaryString to look like 39 Items (37 Folders, 2 Files) instead of 39 Items - 2 Files - 37 Folders as discussed with Peter Penz. Note: when also showing the total size it looks like: "39 Items (37 Folders, 2 Files) (400 KB Total)" - is that ok? CCMAIL: Peter Penz <peter.penz@gmx.at>
    svn path=/trunk/KDE/kdelibs/; revision=632759

--- kio/kio/global.cpp
+++ kio/kio/global.cpp
@@ -118,17 +118,14 @@ KIO_EXPORT QTime KIO::calculateRemaining( KIO::filesize_t totalSize, KIO::filesi
 
 KIO_EXPORT QString KIO::itemsSummaryString(uint items, uint files, uint dirs, KIO::filesize_t size, bool showSize)
 {
-    QString text = items == 0 ? i18n( "No Items" ) : i18np( "One Item", "%n Items", items );
-    text += " - ";
-    text += files == 0 ? i18n( "No Files" ) : i18np( "One File", "%n Files", files );
-    if ( showSize && files > 0 )
-    {
-        text += ' ';
-        text += i18n("(%1 Total)", KIO::convertSize( size ) );
+    const QString itemsText = items == 0 ? i18n( "No Items" ) : i18np( "One Item", "%n Items", items );
+    const QString filesText = files == 0 ? i18n( "No Files" ) : i18np( "One File", "%n Files", files );
+    QString sizeText;
+    if ( showSize && files > 0 ) {
+        sizeText = i18n("(%1 Total)", KIO::convertSize( size ) );
     }
-    text += " - ";
-    text += dirs == 0 ? i18n( "No Folders" ) : i18np("One Folder", "%n Folders", dirs);
-    return text;
+    const QString foldersText = dirs == 0 ? i18n( "No Folders" ) : i18np("One Folder", "%n Folders", dirs);
+    return i18nc("Items (Folders, Files) (Size)", "%1 (%2, %3) (%4)", itemsText, foldersText, filesText, sizeText);
 }
 
 KIO_EXPORT QString KIO::encodeFileName( const QString & _str )
