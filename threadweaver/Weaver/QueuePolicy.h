65bf762fb11e133bf4968b2f3fed670586e1b69d
Mirko Boehm <mirko@kde.org>
2007-02-18 14:28:06 +0000

    - more explicit license statements
    svn path=/trunk/KDE/kdelibs/; revision=634838

--- threadweaver/Weaver/QueuePolicy.h
+++ threadweaver/Weaver/QueuePolicy.h
@@ -1,3 +1,31 @@
+/* -*- C++ -*-
+
+   This file declares the QueuePolicy class.
+
+   $ Author: Mirko Boehm $
+   $ Copyright: (C) 2004, 2005, 2006 Mirko Boehm $
+   $ Contact: mirko@kde.org
+         http://www.kde.org
+         http://www.hackerbuero.org $
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+
+   $Id: DebuggingAids.h 30 2005-08-16 16:16:04Z mirko $
+*/
+
 #ifndef QUEUEPOLICY_H
 #define QUEUEPOLICY_H
 
--- threadweaver/Weaver/WeaverInterface.cpp
+++ threadweaver/Weaver/WeaverInterface.cpp
@@ -7,9 +7,21 @@
    $ Contact: mirko@kde.org
          http://www.kde.org
          http://www.hackerbuero.org $
-   $ License: LGPL with the following explicit clarification:
-         This code may be linked against any version of the Qt toolkit
-         from Trolltech, Norway. $
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
 
    $Id: WeaverImpl.h 29 2005-08-14 19:04:30Z mirko $
 */
