53fafcd4a6a76e291cdc635000247ab45e9037ea
Mirko Boehm <mirko@kde.org>
2007-02-18 17:18:22 +0000

    - license headers
    svn path=/trunk/KDE/kdelibs/; revision=634868

--- threadweaver/WeaverGui/WeaverThreadGrid.h
+++ threadweaver/WeaverGui/WeaverThreadGrid.h
@@ -1,3 +1,31 @@
+/* -*- C++ -*-
+
+   This file declares the WeaverThreadGrid class.
+
+   $ Author: Mirko Boehm $
+   $ Copyright: (C) 2005, Mirko Boehm $
+   $ Contact: mirko@kde.org
+         http://www.kde.org
+         http://www.hackerbuero.org $
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+
+   $Id: ThreadWeaver.h 32 2005-08-17 08:38:01Z mirko $
+*/
+
 #ifndef WEAVERTHREADGRID_H
 #define WEAVERTHREADGRID_H
 
