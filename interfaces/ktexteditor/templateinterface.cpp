fad42fb0b1c03eea2ce04cb55f34f1ba678789d9
Anders Lund <anders@alweb.dk>
2007-02-18 23:03:06 +0000

    svn path=/branches/KDE/3.5/kdelibs/; revision=634977

--- interfaces/ktexteditor/templateinterface.cpp
+++ interfaces/ktexteditor/templateinterface.cpp
@@ -57,7 +57,7 @@ void TemplateInterface::setTemplateInterfaceDCOPSuffix ( const QCString &suffix 
     addrBook=KABC::StdAddressBook::self(); \
     userAddress=addrBook->whoAmI(); \
     if (userAddress.isEmpty()) { \
-      if ( KMessageBox::questionYesNo(0, \
+      if ( KMessageBox::questionYesNo(parentWindow, \
            "This template uses personal data that is stored in the KDE addressbook, but you have not selected a personal entry. You can still use the template without one, but you will have to type personal data. Would you like to select one now?", \
            "Personal data requested", \
            KStdGuiItem::yes(), KStdGuiItem::no(), "select personal data entry") == KMessageBox::Yes ) { \
