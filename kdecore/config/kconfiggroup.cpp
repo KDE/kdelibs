01cb1335f00e91b3d025a1f5c8f78bd001a190ef
Stephan Kulow <coolo@kde.org>
2007-02-20 19:33:42 +0000

    svnrevertlast
    svn path=/trunk/KDE/kdelibs/; revision=635739

--- kdecore/config/kconfiggroup.cpp
+++ kdecore/config/kconfiggroup.cpp
@@ -113,6 +113,11 @@ void KConfigGroup::deleteGroup(KConfigBase::WriteConfigFlags pFlags)
   d->mMaster->deleteGroup(d->mGroup, pFlags);
 }
 
+void KConfigGroup::changeGroup(char const* group)
+{
+   d->mGroup = group;
+}
+
 QString KConfigGroup::group() const
 {
   return d->mGroup;
