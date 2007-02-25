a8bdd6112ce9e0290b596b3f9d08ae1b6437759b
Andreas Pakulat <apaku@gmx.de>
2007-02-25 00:54:01 +0000

    Fix typo from rev. 636991 that broke build
    svn path=/trunk/KDE/kdelibs/; revision=637009

--- khtml/khtml_part.cpp
+++ khtml/khtml_part.cpp
@@ -1847,7 +1847,7 @@ void KHTMLPart::slotFinished( KJob * job )
   }
   KIO::TransferJob *tjob = ::qobject_cast<KIO::TransferJob*>(job);
   if (tjob && tjob->isErrorPage()) {
-    khtml::RenderPart *renderPart = d->frame ? d->m_frame->m_frame : 0L;
+    khtml::RenderPart *renderPart = d->m_frame ? d->m_frame->m_frame : 0L;
     if (renderPart) {
       HTMLObjectElementImpl* elt = static_cast<HTMLObjectElementImpl *>(renderPart->element());
       if (!elt)
