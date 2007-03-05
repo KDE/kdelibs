87724c681f72fb8913ccf14acba0a5e8b4ebf9d0
Harri Porten <porten@kde.org>
2007-03-05 13:15:20 +0000

    committing my fix for .contentWindow queries crashing on invalid windows
    svn path=/branches/KDE/3.5/kdelibs/; revision=639609

--- khtml/ecma/kjs_html.cpp
+++ khtml/ecma/kjs_html.cpp
@@ -1866,10 +1866,12 @@ Value KJS::HTMLElement::getValueProperty(ExecState *exec, int token) const
 				      getDOMNode(exec, frameElement.contentDocument()) : Undefined();
     case FrameContentWindow:   {
         KHTMLPart* part = static_cast<DOM::HTMLFrameElementImpl*>(frameElement.handle())->contentPart();
-        if (part)
-            return Value(Window::retrieveWindow(part));
-        else
-            return Undefined();
+        if (part) {
+          Window *w = Window::retrieveWindow(part);
+          if (w)
+            return Value(w);
+        }
+        return Undefined();
     }
     case FrameFrameBorder:     return String(frameElement.frameBorder());
     case FrameLongDesc:        return String(frameElement.longDesc());
@@ -1899,10 +1901,12 @@ Value KJS::HTMLElement::getValueProperty(ExecState *exec, int token) const
 				       getDOMNode(exec, iFrame.contentDocument()) : Undefined();
     case IFrameContentWindow:       {
         KHTMLPart* part = static_cast<DOM::HTMLIFrameElementImpl*>(iFrame.handle())->contentPart();
-        if (part)
-            return Value(Window::retrieveWindow(part));
-        else
-            return Undefined();
+        if (part) {
+          Window *w = Window::retrieveWindow(part);
+          if (w)
+            return Value(w);
+        }
+        return Undefined();
     }
     case IFrameFrameBorder:     return String(iFrame.frameBorder());
     case IFrameHeight:          return String(iFrame.height());
