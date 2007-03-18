895b604d72b96b3c2bc1411c6a0357d451db1885
Riccardo Iaconelli <riccardo@kde.org>
2007-03-18 11:49:06 +0000

    * Ported to KDElibs coding style. * Added a really cute frame with 3 buttons to choose between most recent/rated/downloaded. It looks really nice with any style I tried =)
    Note: the buttons I made from scratch are really similar to the clickable 'frames' used in kcontrol 3. Is this needed somewhere else in KDE? Actually they are made in a not really portable way, but they can be modified. Let me know if anyone needs them.
    
    svn path=/branches/work/knewstuff2/kdxs/src/knewstuff2/; revision=643775

--- knewstuff/knewstuff2/core/knewstuff2_download.cpp
+++ knewstuff/knewstuff2/core/knewstuff2_download.cpp
@@ -7,269 +7,389 @@
 #include <kapplication.h>
 #include <kcmdlineargs.h>
 #include <kdebug.h>
+#include <kicon.h>
 
 #include <qpushbutton.h>
 #include <qlayout.h>
 #include <qlistwidget.h>
 #include <qtabwidget.h>
 #include <qtablewidget.h>
+//FIXME just include as needed
+#include <QtGui>
 
 #include <unistd.h> // for exit()
 #include <stdio.h> // for stdout
 
+class KNSButton : public QAbstractButton
+{
+public:
+    KNSButton(QWidget *parent = 0);
+    void setText(const QString &text) { t = text; };
+    const QString text() { return t; };
+protected:
+    void paintEvent(QPaintEvent *event);
+private:
+    QString t;
+};
+
+KNSButton::KNSButton(QWidget *parent /*= 0*/)
+    : QAbstractButton(parent)
+{
+    t = "";
+    setCheckable(true);
+    connect(this, SIGNAL(toggled()), this, SLOT(update()));
+}
+
+void KNSButton::paintEvent(QPaintEvent *event)
+{
+    QPalette palette;
+    QPainter p(this);
+    int h = height();
+    int w = width();
+
+    //Draw the background
+    p.setPen(Qt::NoPen);
+    if (isChecked()) {
+    p.setBrush(palette.brush(QPalette::Highlight));
+    } else {
+    p.setBrush(palette.brush(QPalette::Base));
+    }
+    p.drawRect(QRect((rect().x() + 1), (rect().y() + 2), (w - 1), (h - 4)));
+
+    //Draw the text and the icon
+    p.setPen(Qt::black);
+    if (&icon()) {
+        p.drawPixmap(QRect(10, ((h/2) - 16), 32, 32),
+                     icon().pixmap(QSize(32, 32)), QRect(0, 0, 32, 32));
+    }
+
+    p.drawText(QRect((w - (w - 40)), (h/2 - 20), (w - 40), h), Qt::AlignCenter, text());
+}
+
 class FeedWidget : public QTableWidget
 {
-	public:
-		FeedWidget()
-		: QTableWidget()
-		{
-			m_entries = 0;
-			m_previewentries = 0;
+public:
+    FeedWidget()
+    : QTableWidget()
+    {
+        m_entries = 0;
+        m_previewentries = 0;
 
-			setRowCount(20);
-			setColumnCount(5);
+        setRowCount(20);
+        setColumnCount(5);
 
-			QStringList headers;
-			headers << "Name";
-			headers << "Author";
-			headers << "Downloads";
-			headers << "Rating";
-			headers << "Preview";
-			setHorizontalHeaderLabels(headers);
-		}
+        QStringList headers;
+        headers << "Name";
+        headers << "Author";
+        headers << "Downloads";
+        headers << "Rating";
+        headers << "Preview";
+        setHorizontalHeaderLabels(headers);
+    }
 
-		void addEntry(KNS::Entry *entry)
-		{
-			QTableWidgetItem *nameitem = new QTableWidgetItem();
-			nameitem->setText(entry->name().representation());
+    void addEntry(KNS::Entry *entry)
+    {
+        QTableWidgetItem *nameitem = new QTableWidgetItem();
+        nameitem->setText(entry->name().representation());
 
-			QTableWidgetItem *authoritem = new QTableWidgetItem();
-			authoritem->setText(entry->author().name());
+        QTableWidgetItem *authoritem = new QTableWidgetItem();
+        authoritem->setText(entry->author().name());
 
-			QTableWidgetItem *downloadsitem = new QTableWidgetItem();
-			downloadsitem->setText(QString::number(entry->downloads()));
+        QTableWidgetItem *downloadsitem = new QTableWidgetItem();
+        downloadsitem->setText(QString::number(entry->downloads()));
 
-			QTableWidgetItem *ratingitem = new QTableWidgetItem();
-			ratingitem->setText(QString::number(entry->rating()));
+        QTableWidgetItem *ratingitem = new QTableWidgetItem();
+        ratingitem->setText(QString::number(entry->rating()));
 
-			setItem(m_entries, 0, nameitem);
-			setItem(m_entries, 1, authoritem);
-			setItem(m_entries, 2, downloadsitem);
-			setItem(m_entries, 3, ratingitem);
+        setItem(m_entries, 0, nameitem);
+        setItem(m_entries, 1, authoritem);
+        setItem(m_entries, 2, downloadsitem);
+        setItem(m_entries, 3, ratingitem);
 
-			m_entries++;
-		}
+        m_entries++;
+    }
 
-		void addPreview(KUrl previewfile)
-		{
-			QTableWidgetItem *previewitem = new QTableWidgetItem();
-			previewitem->setSizeHint(QSize(64, 64));
-			previewitem->setIcon(QIcon(previewfile.path()));
+    void addPreview(KUrl previewfile)
+    {
+        QTableWidgetItem *previewitem = new QTableWidgetItem();
+        previewitem->setSizeHint(QSize(64, 64));
+        previewitem->setIcon(QIcon(previewfile.path()));
 
-			setItem(m_previewentries, 4, previewitem);
-			m_previewentries++;
-			// FIXME: mapping from entry to url and to vertical position(s)
-		}
+        setItem(m_previewentries, 4, previewitem);
+        m_previewentries++;
+        // FIXME: mapping from entry to url and to vertical position(s)
+    }
 
-	private:
-		int m_entries;
-		int m_previewentries;
+private:
+    int m_entries;
+    int m_previewentries;
 };
 
 KNewStuff2Download::KNewStuff2Download()
 : QWidget()
 {
-	m_engine = NULL;
-	m_activefeed = NULL;
-	m_activeentry = NULL;
+    m_engine = NULL;
+    m_activefeed = NULL;
+    m_activeentry = NULL;
 
-	resize(800, 600);
-	setWindowTitle("KNewStuff2 Download Dialog Test");
+    resize(800, 600);
+    setWindowTitle("KNewStuff2 Download Dialog Test");
 
-	QPushButton *installbutton = new QPushButton("Install");
-	connect(installbutton, SIGNAL(clicked()), SLOT(slotInstall()));
+    QPushButton *installbutton = new QPushButton("Install");
+    connect(installbutton, SIGNAL(clicked()), SLOT(slotInstall()));
 
-	QPushButton *closebutton = new QPushButton("Close");
-	connect(closebutton, SIGNAL(clicked()), SLOT(close()));
+    QPushButton *closebutton = new QPushButton("Close");
+    connect(closebutton, SIGNAL(clicked()), SLOT(close()));
 
-	m_providerlist = new QListWidget();
-	m_providerlist->setFixedWidth(200);
+    m_providerlist = new QListWidget();
+    m_providerlist->setFixedWidth(200);
 
-	m_feeds = new QTabWidget();
+    m_feeds = new QTabWidget();
 
-	QHBoxLayout *hbox = new QHBoxLayout();
-	hbox->addWidget(m_providerlist);
-	hbox->addWidget(m_feeds);
+    frame = new QFrame(this);
+    frame->setFrameStyle(QFrame::StyledPanel | QFrame::Plain);
+    QPalette palette = this->palette();
+    palette.setColor(backgroundRole(), palette.color(QPalette::Base));
+    frame->setPalette(palette);
+    frame->setLineWidth(1);
+    frame->setMidLineWidth(0);
 
-	QVBoxLayout *vbox = new QVBoxLayout();
-	setLayout(vbox);
-	vbox->addLayout(hbox);
-	vbox->addWidget(installbutton);
-	vbox->addWidget(closebutton);
+    recentButton = new KNSButton(frame);
+    recentButton->setIcon(KIcon("alarmclock"));
+    recentButton->setText("Most recent");
 
-	show();
+    estimatedButton = new KNSButton(frame);
+    estimatedButton->setIcon(KIcon("favorites"));
+    estimatedButton->setText("Most estimated");
+
+    wantedButton = new KNSButton(frame);
+    wantedButton->setIcon(KIcon("kget"));
+    wantedButton->setText("Most wanted");
+
+    connect(recentButton, SIGNAL(toggled(bool)), this, SLOT(buttonToggled(bool)));
+    connect(estimatedButton, SIGNAL(toggled(bool)), this, SLOT(buttonToggled(bool)));
+    connect(wantedButton, SIGNAL(toggled(bool)), this, SLOT(buttonToggled(bool)));
+
+    frame->setMinimumHeight(40);
+    recentButton->setChecked(true);
+
+    QHBoxLayout *hbox = new QHBoxLayout();
+    hbox->addWidget(m_providerlist);
+    hbox->addWidget(m_feeds);
+
+    QVBoxLayout *vbox = new QVBoxLayout();
+    setLayout(vbox);
+    vbox->addWidget(frame);
+    vbox->addLayout(hbox);
+    vbox->addWidget(installbutton);
+    vbox->addWidget(closebutton);
+
+    show();
+}
+
+void KNewStuff2Download::resizeEvent(QResizeEvent *event)
+{
+    int frameWidth = frame->size().width();
+    int frameHeight = frame->size().height();
+
+    recentButton->setGeometry(0, 0, frameWidth/3, frameHeight);
+    estimatedButton->setGeometry(frameWidth/3, 0, frameWidth/3, frameHeight);
+
+    //NOTE int have just integer prcision, so, they can be approximated when scaling the object.
+    //That's why those cases are possible.
+    if (((frameWidth/3)*3) > (frameWidth)) {
+        wantedButton->setGeometry((frameWidth/3)*2, 0, (frameWidth/3 - 2), frameHeight);
+    } else if (((frameWidth/3)*3) == (frameWidth)) {
+        wantedButton->setGeometry((frameWidth/3)*2, 0, frameWidth/3 - 1, frameHeight);
+    } else {
+        wantedButton->setGeometry((frameWidth/3)*2, 0, (frameWidth/3), frameHeight);
+    }
+}
+void KNewStuff2Download::buttonToggled(bool checked)
+{
+    if (checked) {
+        if (sender() == recentButton) {
+            m_checkedButton = KNewStuff2Download::Recent;
+            estimatedButton->setChecked(false);
+            wantedButton->setChecked(false);
+            switchCategory(KNewStuff2Download::Recent);
+        } else if (sender() == estimatedButton) {
+            m_checkedButton = KNewStuff2Download::Estimated;
+            recentButton->setChecked(false);
+            wantedButton->setChecked(false);
+            switchCategory(KNewStuff2Download::Estimated);
+        } else if (sender() == wantedButton) {
+            m_checkedButton = KNewStuff2Download::Wanted;
+            recentButton->setChecked(false);
+            estimatedButton->setChecked(false);
+            switchCategory(KNewStuff2Download::Wanted);
+        }
+    } else {
+        if (m_checkedButton == KNewStuff2Download::Recent && sender() == recentButton) {
+                recentButton->setChecked(true);
+        } else if (m_checkedButton == KNewStuff2Download::Estimated && sender() == estimatedButton) {
+            estimatedButton->setChecked(true);
+        } else if (m_checkedButton == KNewStuff2Download::Wanted && sender() == wantedButton) {
+            wantedButton->setChecked(true);
+        }
+    }
+}
+
+void KNewStuff2Download::switchCategory(KNewStuff2Download::Category category)
+{
+//TODO
 }
 
 void KNewStuff2Download::run()
 {
-	kDebug() << "-- test kns2 engine" << endl;
+    kDebug() << "-- test kns2 engine" << endl;
 
-	m_engine = new KNS::Engine();
-	bool success = m_engine->init("knewstuff2_test.knsrc");
+    m_engine = new KNS::Engine();
+    bool success = m_engine->init("knewstuff2_test.knsrc");
 
-	kDebug() << "-- engine test result: " << success << endl;
+    kDebug() << "-- engine test result: " << success << endl;
 
-	if(success)
-	{
-		connect(m_engine,
-			SIGNAL(signalProviderLoaded(KNS::Provider*)),
-			SLOT(slotProviderLoaded(KNS::Provider*)));
-		connect(m_engine,
-			SIGNAL(signalProvidersFailed()),
-			SLOT(slotProvidersFailed()));
-		connect(m_engine,
-			SIGNAL(signalEntryLoaded(KNS::Entry*)),
-			SLOT(slotEntryLoaded(KNS::Entry*)));
-		connect(m_engine,
-			SIGNAL(signalEntriesFailed()),
-			SLOT(slotEntriesFailed()));
-		connect(m_engine,
-			SIGNAL(signalPayloadLoaded(KUrl)),
-			SLOT(slotPayloadLoaded(KUrl)));
-		connect(m_engine,
-			SIGNAL(signalPayloadFailed()),
-			SLOT(slotPayloadFailed()));
-		connect(m_engine,
-			SIGNAL(signalPreviewLoaded(KUrl)),
-			SLOT(slotPreviewLoaded(KUrl)));
-		connect(m_engine,
-			SIGNAL(signalPreviewFailed()),
-			SLOT(slotPreviewFailed()));
+    if(success) {
+        connect(m_engine,
+            SIGNAL(signalProviderLoaded(KNS::Provider*)),
+            SLOT(slotProviderLoaded(KNS::Provider*)));
+        connect(m_engine,
+            SIGNAL(signalProvidersFailed()),
+            SLOT(slotProvidersFailed()));
+        connect(m_engine,
+            SIGNAL(signalEntryLoaded(KNS::Entry*)),
+            SLOT(slotEntryLoaded(KNS::Entry*)));
+        connect(m_engine,
+            SIGNAL(signalEntriesFailed()),
+            SLOT(slotEntriesFailed()));
+        connect(m_engine,
+            SIGNAL(signalPayloadLoaded(KUrl)),
+            SLOT(slotPayloadLoaded(KUrl)));
+        connect(m_engine,
+            SIGNAL(signalPayloadFailed()),
+            SLOT(slotPayloadFailed()));
+        connect(m_engine,
+            SIGNAL(signalPreviewLoaded(KUrl)),
+            SLOT(slotPreviewLoaded(KUrl)));
+        connect(m_engine,
+            SIGNAL(signalPreviewFailed()),
+            SLOT(slotPreviewFailed()));
 
-		m_engine->start();
-	}
-	else
-	{
-		kWarning() << "ACHTUNG: you probably need to 'make install' the knsrc file first." << endl;
-		kWarning() << "Although this is not required anymore, so something went really wrong." << endl;
-	}
+        m_engine->start();
+    } else {
+        kWarning() << "ACHTUNG: you probably need to 'make install' the knsrc file first." << endl;
+        kWarning() << "Although this is not required anymore, so something went really wrong." << endl;
+    }
 }
 
 void KNewStuff2Download::slotInstall()
 {
-	kDebug() << "Attempt to install entry..." << endl;
+    kDebug() << "Attempt to install entry..." << endl;
 
-	if(!m_activeentry)
-	{
-		kError() << "No entries loaded!" << endl;
-		return;
-	}
-	else
-	{
-		kDebug() << "Entry to install is called " << m_activeentry->name().representation() << endl;
-	}
+    if(!m_activeentry) {
+        kError() << "No entries loaded!" << endl;
+        return;
+    } else {
+        kDebug() << "Entry to install is called " << m_activeentry->name().representation() << endl;
+    }
 
-	m_engine->downloadPayload(m_activeentry);
+    m_engine->downloadPayload(m_activeentry);
 }
 
 void KNewStuff2Download::slotProviderLoaded(KNS::Provider *provider)
 {
-	kDebug() << "SLOT: slotProviderLoaded" << endl;
-	kDebug() << "-- provider: " << provider->name().representation() << endl;
+    kDebug() << "SLOT: slotProviderLoaded" << endl;
+    kDebug() << "-- provider: " << provider->name().representation() << endl;
 
-	QListWidgetItem *item = new QListWidgetItem(QIcon(), provider->name().representation());
-	m_providerlist->addItem(item);
+    QListWidgetItem *item = new QListWidgetItem(QIcon(), provider->name().representation());
+    m_providerlist->addItem(item);
 
-	QStringList feeds = provider->feeds();
-	for(QStringList::Iterator it = feeds.begin(); it != feeds.end(); it++)
-	{
-		KNS::Feed *feed = provider->downloadUrlFeed((*it));
+    QStringList feeds = provider->feeds();
+    for(QStringList::Iterator it = feeds.begin(); it != feeds.end(); it++) {
+        KNS::Feed *feed = provider->downloadUrlFeed((*it));
 
-		FeedWidget *feedtab = new FeedWidget();
-		m_feeds->addTab(feedtab, feed->name().representation());
-		m_activefeed = feedtab;
-	}
-	if(feeds.size() == 0)
-	{
-		FeedWidget *nofeedtab = new FeedWidget();
-		m_feeds->addTab(nofeedtab, "Entries");
-		m_activefeed = nofeedtab;
-	}
+        FeedWidget *feedtab = new FeedWidget();
+        m_feeds->addTab(feedtab, feed->name().representation());
+        m_activefeed = feedtab;
+    }
 
-	m_engine->loadEntries(provider);
+    if(feeds.size() == 0) {
+        FeedWidget *nofeedtab = new FeedWidget();
+        m_feeds->addTab(nofeedtab, "Entries");
+        m_activefeed = nofeedtab;
+    }
+
+    m_engine->loadEntries(provider);
 }
 
 void KNewStuff2Download::slotEntryLoaded(KNS::Entry *entry)
 {
-	kDebug() << "SLOT: slotEntryLoaded" << endl;
-	kDebug() << "-- entry: " << entry->name().representation() << endl;
+    kDebug() << "SLOT: slotEntryLoaded" << endl;
+    kDebug() << "-- entry: " << entry->name().representation() << endl;
 
-	FeedWidget *fw = dynamic_cast<FeedWidget*>(m_activefeed);
-	fw->addEntry(entry);
+    FeedWidget *fw = dynamic_cast<FeedWidget*>(m_activefeed);
+    fw->addEntry(entry);
 
-	m_engine->downloadPreview(entry);
+    m_engine->downloadPreview(entry);
 
-	m_activeentry = entry;
+    m_activeentry = entry;
 }
 
 void KNewStuff2Download::slotPreviewLoaded(KUrl preview)
 {
-	kDebug() << "-- preview downloaded successfully" << endl;
-	kDebug() << "-- downloaded to " << preview.prettyUrl() << endl;
+    kDebug() << "-- preview downloaded successfully" << endl;
+    kDebug() << "-- downloaded to " << preview.prettyUrl() << endl;
 
-	FeedWidget *fw = dynamic_cast<FeedWidget*>(m_activefeed);
-	fw->addPreview(preview);
+    FeedWidget *fw = dynamic_cast<FeedWidget*>(m_activefeed);
+    fw->addPreview(preview);
 }
 
 void KNewStuff2Download::slotPreviewFailed()
 {
-	kDebug() << "SLOT: slotPreviewFailed" << endl;
+    kDebug() << "SLOT: slotPreviewFailed" << endl;
 }
 
 void KNewStuff2Download::slotPayloadLoaded(KUrl payload)
 {
-	kDebug() << "-- entry downloaded successfully" << endl;
-	kDebug() << "-- downloaded to " << payload.prettyUrl() << endl;
+    kDebug() << "-- entry downloaded successfully" << endl;
+    kDebug() << "-- downloaded to " << payload.prettyUrl() << endl;
 
-	bool success = m_engine->install(payload.path());
-	if(success)
-	{
-		kDebug() << "-- installation succeeded" << endl;
-	}
-	else
-	{
-		kError() << "-- installation failed" << endl;
-	}
+    bool success = m_engine->install(payload.path());
+    if(success) {
+        kDebug() << "-- installation succeeded" << endl;
+    } else {
+        kError() << "-- installation failed" << endl;
+    }
 }
 
 void KNewStuff2Download::slotPayloadFailed()
 {
-	kDebug() << "SLOT: slotPayloadFailed" << endl;
+    kDebug() << "SLOT: slotPayloadFailed" << endl;
 }
 
 void KNewStuff2Download::slotProvidersFailed()
 {
-	kDebug() << "SLOT: slotProvidersFailed" << endl;
+    kDebug() << "SLOT: slotProvidersFailed" << endl;
 }
 
 void KNewStuff2Download::slotEntriesFailed()
 {
-	kDebug() << "SLOT: slotEntriesFailed" << endl;
+    kDebug() << "SLOT: slotEntriesFailed" << endl;
 }
 
 int main(int argc, char **argv)
 {
-	KCmdLineArgs::init(argc, argv, "knewstuff2_download", NULL, NULL, NULL);
-	KApplication app;
+    KCmdLineArgs::init(argc, argv, "knewstuff2_download", NULL, NULL, NULL);
+    KApplication app;
 
-	// Take source directory into account
-	kDebug() << "-- adding source directory " << KNSSRCDIR << endl;
-	KGlobal::dirs()->addResourceDir("config", KNSSRCDIR);
+    // Take source directory into account
+    kDebug() << "-- adding source directory " << KNSSRCDIR << endl;
+    KGlobal::dirs()->addResourceDir("config", KNSSRCDIR);
 
-	KNewStuff2Download *download = new KNewStuff2Download();
-	download->run();
+    KNewStuff2Download *download = new KNewStuff2Download();
+    download->run();
 
-	return app.exec();
+    return app.exec();
 }
 
 #include "knewstuff2_download.moc"
--- knewstuff/knewstuff2/core/knewstuff2_download.h
+++ knewstuff/knewstuff2/core/knewstuff2_download.h
@@ -13,6 +13,9 @@ namespace KNS
 
 class QListWidget;
 class QTabWidget;
+class KNSButton;
+class FeedWidget;
+class QFrame;
 
 class KNewStuff2Download : public QWidget
 {
@@ -30,12 +33,30 @@ public slots:
 	void slotPayloadLoaded(KUrl payload);
 	void slotPayloadFailed();
 	void slotInstall();
+protected slots:
+        void resizeEvent(QResizeEvent *event);
+        void buttonToggled(bool checked);
 private:
+        enum CategoryFlag {
+            Recent = 0x1,
+            Wanted = 0x2,
+            Estimated = 0x4
+        };
+        Q_DECLARE_FLAGS(Category, CategoryFlag);
+
+        void switchCategory(KNewStuff2Download::Category category);
+
+        FeedWidget *m_feedtab;
+        KNewStuff2Download::Category m_checkedButton;
 	KNS::Engine *m_engine;
 	QListWidget *m_providerlist;
 	QTabWidget *m_feeds;
 	QWidget *m_activefeed;
 	KNS::Entry *m_activeentry;
+        QFrame *frame;
+        KNSButton *recentButton;
+        KNSButton *estimatedButton;
+        KNSButton *wantedButton;
 };
 
 #endif
