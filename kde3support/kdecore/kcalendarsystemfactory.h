2f8a22b2e70865aa57bf333de3ebeeb38fdeb830
Aaron J. Seigo <aseigo@kde.org>
2007-03-19 21:54:14 +0000

    add a "this is gone" header for kcalendarsystemfactory
    svn path=/trunk/KDE/kdelibs/; revision=644402

---
+++ kde3support/kdecore/kcalendarsystemfactory.h
@@ -0,0 +1,5 @@
+#ifdef KDE_NO_COMPAT
+#error kcalendarsystemconfig.h does not exist anymore. The KCalendarSystemFactory class has been removed; the create() methods that were provided by KCalendarSystemFactory have been moved into KCalendarSystem. See the KDE4 Porting Guide for more information.
+#else
+#include <kcalendarsystem.h>
+#endif
