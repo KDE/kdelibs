07794d73b33f9d431348ece33d3eafd84051f094
Dirk Mueller <mueller@kde.org>
2007-03-20 09:48:55 +0000

    don't use variable size arrays when -ansi is used
    svn path=/trunk/KDE/kdelibs/; revision=644521

--- kdecore/localization/gettext.h
+++ kdecore/localization/gettext.h
@@ -165,7 +165,7 @@ npgettext_aux (const char *domain,
 #include <string.h>
 
 #define _LIBGETTEXT_HAVE_VARIABLE_SIZE_ARRAYS \
-  (__GNUC__ >= 3 || __GNUG__ >= 2 /* || __STDC_VERSION__ >= 199901L */ )
+  (__STRICT_ANSI__ - 0 == 0) && (__GNUC__ >= 3 || __GNUG__ >= 2 /* || __STDC_VERSION__ >= 199901L */ )
 
 #if !_LIBGETTEXT_HAVE_VARIABLE_SIZE_ARRAYS
 #include <stdlib.h>
