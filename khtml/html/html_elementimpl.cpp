3147c7e66b3187b84451846643adc6ce37154851
Rolf Eike Beer <kde@opensource.sf-tec.de>
2007-03-20 18:44:09 +0000

    warning--
    svn path=/trunk/KDE/kdelibs/; revision=644703

--- khtml/html/html_elementimpl.cpp
+++ khtml/html/html_elementimpl.cpp
@@ -457,7 +457,7 @@ DOMString HTMLElementImpl::innerHTML() const
     QString result; //Use QString to accumulate since DOMString is poor for appends
     for (NodeImpl *child = firstChild(); child != NULL; child = child->nextSibling()) {
         DOMString kid = child->toString();
-        result += QConstString(kid.unicode(), kid.length()).string();
+        result += QConstString(kid.unicode(), kid.length());
     }
     return result;
 }
@@ -486,7 +486,7 @@ DOMString HTMLElementImpl::innerText() const
         }
         if(n->isTextNode() ) {
             DOMStringImpl* data = static_cast<const TextImpl *>(n)->string();
-            text += QConstString(data->s, data->l).string();
+            text += QConstString(data->s, data->l);
         }
     }
  end:
