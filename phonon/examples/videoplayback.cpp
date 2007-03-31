157e7ee128ca87917ec0a9384bae7f53f99a715e
Matthias Kretz <kretz@kde.org>
2007-03-31 16:11:42 +0000

    make videoplayback a complete compileable example and add copyright + license
    svn path=/trunk/KDE/kdelibs/; revision=648528

--- phonon/examples/videoplayback.cpp
+++ phonon/examples/videoplayback.cpp
@@ -1,3 +1,39 @@
+/*  This file is part of the KDE project
+    Copyright (C) 2007 Matthias Kretz <kretz@kde.org>
+
+    Permission to use, copy, modify, and distribute this software
+    and its documentation for any purpose and without fee is hereby
+    granted, provided that the above copyright notice appear in all
+    copies and that both that the copyright notice and this
+    permission notice and warranty disclaimer appear in supporting
+    documentation, and that the name of the author not be used in
+    advertising or publicity pertaining to distribution of the
+    software without specific, written prior permission.
+
+    The author disclaim all warranties with regard to this
+    software, including all implied warranties of merchantability
+    and fitness.  In no event shall the author be liable for any
+    special, indirect or consequential damages or any damages
+    whatsoever resulting from loss of use, data or profits, whether
+    in an action of contract, negligence or other tortious action,
+    arising out of or in connection with the use or performance of
+    this software.
+
+*/
+
+#include "videoplayback.h"
+
+#include <QVBoxLayout>
+
+#include <phonon/videopath.h>
+#include <phonon/audiooutput.h>
+#include <phonon/audiopath.h>
+#include <phonon/mediaobject.h>
+#include <phonon/ui/videowidget.h>
+#include <phonon/ui/mediacontrols.h>
+
+using namespace Phonon;
+
 MediaPlayer::MediaPlayer( QWidget* parent )
 	: QWidget( parent )
 {
@@ -9,7 +45,8 @@ MediaPlayer::MediaPlayer( QWidget* parent )
 
 	m_vpath = new VideoPath( this );
 
-	m_aoutput = new AudioOutput( this );
+        // might need VideoCategory here
+        m_aoutput = new AudioOutput(Phonon::MusicCategory, this);
 	m_apath = new AudioPath( this );
 
 	m_media = new MediaObject( this );
@@ -22,10 +59,19 @@ MediaPlayer::MediaPlayer( QWidget* parent )
 	layout->addWidget( m_controls );
 	m_controls->setMediaProducer( m_media );
 	m_controls->setAudioOutput( m_aoutput );
+
+        connect(m_media, SIGNAL(hasVideoChanged(bool)), SLOT(hasVideoChanged(bool)));
 }
 
 void MediaPlayer::setUrl( const KUrl& url )
 {
 	m_media->setUrl( url );
-	m_vwidget->setVisible( m_media->hasVideo() );
+    m_vwidget->setVisible(m_media->hasVideo());
 }
+
+void MediaPlayer::hasVideoChanged(bool hasVideo)
+{
+    m_vwidget->setVisible(hasVideo);
+}
+
+#include "videoplayback.moc"
---
+++ phonon/examples/videoplayback.h
@@ -0,0 +1,62 @@
+/*  This file is part of the KDE project
+    Copyright (C) 2007 Matthias Kretz <kretz@kde.org>
+
+    Permission to use, copy, modify, and distribute this software
+    and its documentation for any purpose and without fee is hereby
+    granted, provided that the above copyright notice appear in all
+    copies and that both that the copyright notice and this
+    permission notice and warranty disclaimer appear in supporting
+    documentation, and that the name of the author not be used in
+    advertising or publicity pertaining to distribution of the
+    software without specific, written prior permission.
+
+    The author disclaim all warranties with regard to this
+    software, including all implied warranties of merchantability
+    and fitness.  In no event shall the author be liable for any
+    special, indirect or consequential damages or any damages
+    whatsoever resulting from loss of use, data or profits, whether
+    in an action of contract, negligence or other tortious action,
+    arising out of or in connection with the use or performance of
+    this software.
+
+*/
+
+#ifndef EXAMPLES_VIDEOPLAYBACK_H
+#define EXAMPLES_VIDEOPLAYBACK_H
+
+#include <QWidget>
+
+class KUrl;
+
+namespace Phonon
+{
+    class VideoWidget;
+    class VideoPath;
+    class AudioOutput;
+    class AudioPath;
+    class MediaObject;
+    class MediaControls;
+} // namespace Phonon
+
+using namespace Phonon;
+
+class MediaPlayer : public QWidget
+{
+    Q_OBJECT
+    public:
+        MediaPlayer(QWidget *parent);
+        void setUrl(const KUrl &url);
+
+    private Q_SLOTS:
+        void hasVideoChanged(bool hasVideo);
+
+    private:
+        VideoWidget *m_vwidget;
+        VideoPath *m_vpath;
+        AudioOutput *m_aoutput;
+        AudioPath *m_apath;
+        MediaObject *m_media;
+        MediaControls *m_controls;
+};
+
+#endif // EXAMPLES_VIDEOPLAYBACK_H
