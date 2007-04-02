a3352fa7cd8d236e275e18abdd22d691188b935f
Aaron J. Seigo <aseigo@kde.org>
2007-04-02 05:45:35 +0000

    * move all our color related data and classes together * classes with statics only become namespaces * some api clean ups
    svn path=/trunk/KDE/kdelibs/; revision=649116

--- kdeui/colors/kcolormimedata.h
+++ kdeui/colors/kcolormimedata.h
@@ -34,20 +34,20 @@ class QWidget;
  *
  * See the Qt drag'n'drop documentation.
  */
-class KDEUI_EXPORT KColorMimeData {
-public:
+namespace KColorMimeData
+{
     /**
      * Sets the color and text representation fields for the specified color in the mimedata object:
      * application/x-color and text/plain types are set
      */
-    static void populateMimeData(QMimeData *mimeData, const QColor& color);
+    void KDEUI_EXPORT populateMimeData(QMimeData *mimeData, const QColor& color);
 
     /**
      * Returns true if the MIME data @p mimeData contains a color object.
      * First checks for application/x-color and if that failes for a text/plain entry, which
      * represents a color in the format \#hexnumbers
      */
-    static bool canDecode(const QMimeData *mimeData);
+    bool KDEUI_EXPORT canDecode(const QMimeData *mimeData);
 
     /**
      * Decodes the MIME data @p mimeData and returns the resulting color.
@@ -55,14 +55,14 @@ public:
      * represents a color in the format \#hexnumbers. If this fails too,
      * an invalid QColor object is returned, use QColor::isValid() to test it.
      */
-    static QColor fromMimeData(const QMimeData *mimeData);
+    QColor KDEUI_EXPORT fromMimeData(const QMimeData *mimeData);
 
     /**
      * Creates a color drag object. Either you have to start this drag or delete it
      * The drag object's mime data has the application/x-color and text/plain type set and a pixmap
      * filled with the specified color, which is going to be displayed next to the mouse cursor
      */
-    static QDrag* createDrag(const QColor& color, QWidget *dragsource);
+    QDrag* KDEUI_EXPORT createDrag(const QColor& color, QWidget *dragsource);
 };
 
 
