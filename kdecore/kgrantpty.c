ad913201b78083667d6aadd6aad7aea3b6ffb797
Alex Merry <kde@randomguy3.me.uk>
2007-04-23 10:14:58 +0000

    Explicitly write out that the file is covered by the GPL in the standard way (rather than just "GPL applies").
    svn path=/trunk/KDE/kdelibs/; revision=657139

--- kdecore/kgrantpty.c
+++ kdecore/kgrantpty.c
@@ -21,7 +21,21 @@
  *
  * Contributed by Zack Weinberg <zack@rabi.phys.columbia.edu>, 1998.
  * Copyright (c) 1999 by Lars Doelle <lars.doelle@on-line.de>.
- * GPL applies.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301, USA
  */
 
 #include <config.h>
