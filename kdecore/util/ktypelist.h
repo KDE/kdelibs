d8e4518bc6b39070217aa63e3b3b3176abfb8b11
Alex Merry <kde@randomguy3.me.uk>
2007-05-01 23:38:25 +0000

    Ignore lack of emails for copyright holders.
    svn path=/trunk/KDE/kdelibs/; revision=660215

--- kdecore/util/ktypelist.h
+++ kdecore/util/ktypelist.h
@@ -2,10 +2,10 @@
  This work is derived from:
  ----
  The Loki Library
- Copyright (c) 2001 by Andrei Alexandrescu
+ Copyright (c) 2001 by Andrei Alexandrescu                                  //krazy:exclude=copyright
  This code accompanies the book:
  Alexandrescu, Andrei. "Modern C++ Design: Generic Programming and Design
-     Patterns Applied". Copyright (c) 2001. Addison-Wesley.
+     Patterns Applied". Copyright (c) 2001. Addison-Wesley.                 //krazy:exclude=copyright
  Permission to use, copy, modify, distribute and sell this software for any
      purpose is hereby granted without fee, provided that the above copyright
      notice appear in all copies and that both that copyright notice and this
