d397d568e04af0cc19900259021c8e8f55b22e68
Alex Merry <kde@randomguy3.me.uk>
2007-05-05 11:24:19 +0000

    Re-license from GPL to LGPL.  Confirmed with Sebastian Trueg.
    svn path=/trunk/KDE/kdelibs/; revision=661371

--- knepomuk/services/dummyservice.h
+++ knepomuk/services/dummyservice.h
@@ -5,11 +5,11 @@
  * This file is part of the Nepomuk KDE project.
  * Copyright (C) 2006 Sebastian Trueg <trueg@kde.org>
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * See the file "COPYING" for the exact licensing terms.
+ * See the file "COPYING.LIB" for the exact licensing terms.
  */
 
 #ifndef _NEPOMUK_DBUS_DUMMY_SERVICE_H_
