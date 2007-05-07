d82e9764c60073a92ac16e0f120e0924599f3f1b
Matthias Kretz <kretz@kde.org>
2007-05-07 17:18:30 +0000

    dox
    svn path=/trunk/KDE/kdelibs/; revision=662236

--- phonon/backendinterface.h
+++ phonon/backendinterface.h
@@ -1,5 +1,5 @@
 /*  This file is part of the KDE project
-    Copyright (C) 2006 Matthias Kretz <kretz@kde.org>
+    Copyright (C) 2006-2007 Matthias Kretz <kretz@kde.org>
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Library General Public
@@ -33,36 +33,222 @@ namespace Phonon
 /** \class BackendInterface backendinterface.h Phonon/BackendInterface
  * \short Main Backend class interface
  *
+ * This interface defines the main factory of the backend. The createObject function creates all the
+ * objects needed by the frontend.
+ *
+ * The objectDescriptionIndexes and objectDescriptionProperties functions return information about
+ * available devices, effects and codecs.
+ *
+ * An implementation could look like this:
+ * \code
+ * QObject *Backend::createObject(BackendInterface::Class c, QObject *parent, const QList<QVariant> &args)
+ * {
+ *     switch (c) {
+ *     case MediaObjectClass:
+ *         return new MediaObject(parent);
+ *     case AudioPathClass:
+ *         return new AudioPath(parent);
+ *     case VolumeFaderEffectClass:
+ *         return new VolumeFaderEffect(parent);
+ *     case AudioOutputClass:
+ *         return new AudioOutput(parent);
+ *     case AudioDataOutputClass:
+ *         return new AudioDataOutput(parent);
+ *     case VisualizationClass:
+ *         return new Visualization(parent);
+ *     case VideoPathClass:
+ *         return new VideoPath(parent);
+ *     case BrightnessControlClass:
+ *         return new BrightnessControl(parent);
+ *     case VideoDataOutputClass:
+ *         return new VideoDataOutput(parent);
+ *     case DeinterlaceFilterClass:
+ *         return new DeinterlaceFilter(parent);
+ *     case AudioEffectClass:
+ *         return new AudioEffect(args[0].toInt(), parent);
+ *     case VideoEffectClass:
+ *         return new VideoEffect(args[0].toInt(), parent);
+ *     case VideoWidgetClass:
+ *         return new VideoWidget(qobject_cast<QWidget *>(parent));
+ *     }
+ *     return 0;
+ * }
+ *
+ * QSet<int> Backend::objectDescriptionIndexes(ObjectDescriptionType type) const
+ * {
+ *     QSet<int> set;
+ *     switch(type)
+ *     {
+ *     case Phonon::AudioOutputDeviceType:
+ *         // use AudioDeviceEnumerator to list ALSA and OSS devices
+ *         set << 10000 << 10001;
+ *         break;
+ *     case Phonon::AudioCaptureDeviceType:
+ *         set << 20000 << 20001;
+ *         break;
+ *     case Phonon::VideoOutputDeviceType:
+ *         break;
+ *     case Phonon::VideoCaptureDeviceType:
+ *         set << 30000 << 30001;
+ *         break;
+ *     case Phonon::VisualizationType:
+ *     case Phonon::AudioCodecType:
+ *     case Phonon::VideoCodecType:
+ *     case Phonon::ContainerFormatType:
+ *         break;
+ *     case Phonon::AudioEffectType:
+ *         set << 0x7F000001;
+ *         break;
+ *     case Phonon::VideoEffectType:
+ *         set << 0x7E000001;
+ *         break;
+ *     }
+ *     return set;
+ * }
+ *
+ * QHash<QByteArray, QVariant> Backend::objectDescriptionProperties(ObjectDescriptionType type, int index) const
+ * {
+ *     QHash<QByteArray, QVariant> ret;
+ *     switch (type) {
+ *     case Phonon::AudioOutputDeviceType:
+ *         switch (index) {
+ *         case 10000:
+ *             ret.insert("name", QLatin1String("internal Soundcard"));
+ *             break;
+ *         case 10001:
+ *             ret.insert("name", QLatin1String("USB Headset"));
+ *             ret.insert("icon", KIcon("usb-headset"));
+ *             ret.insert("available", false);
+ *             break;
+ *         }
+ *         break;
+ *     case Phonon::AudioCaptureDeviceType:
+ *         switch (index) {
+ *         case 20000:
+ *             ret.insert("name", QLatin1String("Soundcard"));
+ *             ret.insert("description", QLatin1String("first description"));
+ *             break;
+ *         case 20001:
+ *             ret.insert("name", QLatin1String("DV"));
+ *             ret.insert("description", QLatin1String("second description"));
+ *             break;
+ *         }
+ *         break;
+ *     case Phonon::VideoOutputDeviceType:
+ *         break;
+ *     case Phonon::VideoCaptureDeviceType:
+ *         switch (index) {
+ *         case 30000:
+ *             ret.insert("name", QLatin1String("USB Webcam"));
+ *             ret.insert("description", QLatin1String("first description"));
+ *             break;
+ *         case 30001:
+ *             ret.insert("name", QLatin1String("DV"));
+ *             ret.insert("description", QLatin1String("second description"));
+ *             break;
+ *         }
+ *         break;
+ *     case Phonon::VisualizationType:
+ *         break;
+ *     case Phonon::AudioCodecType:
+ *         break;
+ *     case Phonon::VideoCodecType:
+ *         break;
+ *     case Phonon::ContainerFormatType:
+ *         break;
+ *     case Phonon::AudioEffectType:
+ *         switch (index) {
+ *         case 0x7F000001:
+ *             ret.insert("name", QLatin1String("Delay"));
+ *             ret.insert("description", QLatin1String("Simple delay effect with time, feedback and level controls."));
+ *             break;
+ *         }
+ *         break;
+ *     case Phonon::VideoEffectType:
+ *         switch (index) {
+ *         case 0x7E000001:
+ *             ret.insert("name", QLatin1String("VideoEffect1"));
+ *             ret.insert("description", QLatin1String("Description 1"));
+ *             break;
+ *         }
+ *         break;
+ *     }
+ *     return ret;
+ * }
+ * \endcode
+ *
  * \ingroup Backend
  * \author Matthias Kretz <kretz@kde.org>
  */
 class BackendInterface
 {
     public:
+        /**
+         * \internal
+         *
+         * Silence gcc's warning.
+         */
         virtual ~BackendInterface() {}
 
         /**
-         * Classes that take no additional arguments
+         * Classes that the createObject function has to handle.
          */
         enum Class {
+            /**
+             * Request to return a %MediaObject object.
+             */
             MediaObjectClass,
+            /**
+             * Request to return a %AudioPath object.
+             */
             AudioPathClass,
+            /**
+             * Request to return a %VolumeFaderEffect object.
+             */
             VolumeFaderEffectClass,
+            /**
+             * Request to return a %AudioOutput object.
+             */
             AudioOutputClass,
+            /**
+             * Request to return a %AudioDataOutput object.
+             */
             AudioDataOutputClass,
+            /**
+             * Request to return a %Visualization object.
+             */
             VisualizationClass,
+            /**
+             * Request to return a %VideoPath object.
+             */
             VideoPathClass,
+            /**
+             * Request to return a %BrightnessControl object.
+             */
             BrightnessControlClass,
+            /**
+             * Request to return a %VideoDataOutput object.
+             */
             VideoDataOutputClass,
+            /**
+             * Request to return a %DeinterlaceFilter object.
+             */
             DeinterlaceFilterClass,
             /**
+             * Request to return a %AudioEffect object.
+             *
              * Takes an additional int that specifies the effect Id.
              */
             AudioEffectClass,
             /**
+             * Request to return a %VideoEffect object.
+             *
              * Takes an additional int that specifies the effect Id.
              */
             VideoEffectClass,
+            /**
+             * Request to return a %VideoWidget object.
+             */
             VideoWidgetClass
         };
 
@@ -71,11 +257,33 @@ class BackendInterface
          *
          * \param c The requested class.
          * \param parent The parent object.
-         * \param arg1 An additional argument (documented in \ref Class1).
+         * \param args Additional arguments (documented in \ref Class).
          */
         virtual QObject *createObject(Class c, QObject *parent, const QList<QVariant> &args = QList<QVariant>()) = 0;
 
+        /**
+         * Returns the unique identifiers for the devices/effects/codecs of the given \p type.
+         *
+         * \param type see \ref ObjectDescriptionType
+         */
         virtual QSet<int> objectDescriptionIndexes(ObjectDescriptionType type) const = 0;
+
+        /**
+         * Given a unique identifier that was returned from objectDescriptionIndexes this function
+         * returns a hash mapping property names to values.
+         *
+         * The property "name" must always be present. All other properties are optional.
+         *
+         * List of possible properties:
+         * \li \c \b name: The name of the device/effect/codec/...
+         * \li \c \b description: A text explaining what this device/effect/codec/... is/can do
+         * \li \c \b icon: An icon name (using the freedesktop naming scheme) or a QIcon for this
+         * device/effect/codec/...
+         * \li \c \b available: A bool telling whether the device is present or unplugged.
+         *
+         * \param type see \ref ObjectDescriptionType
+         * \param index The unique identifier that is returned from objectDescriptionIndexes
+         */
         virtual QHash<QByteArray, QVariant> objectDescriptionProperties(ObjectDescriptionType type, int index) const = 0;
 };
 } // namespace Phonon
