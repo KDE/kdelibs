11c28c14343da80019db525a9cff1722dfa159d2
Alex Merry <kde@randomguy3.me.uk>
2007-05-07 08:50:53 +0000

    Change license to LGPL.  Confirmed with copyright holders.
    svn path=/trunk/KDE/kdelibs/; revision=662012

--- kdeui/actions/kcodecaction.h
+++ kdeui/actions/kcodecaction.h
@@ -5,14 +5,14 @@
     Copyright (c) 2003-2006 Michel Hermier    <michel.hermier@gmail.com>
     Copyright (c) 2007      Nick Shaforostoff <shafff@ukr.net>
 
-    *************************************************************************
-    *                                                                       *
-    * This program is free software; you can redistribute it and/or modify  *
-    * it under the terms of the GNU General Public License as published by  *
-    * the Free Software Foundation; either version 2 of the License, or     *
-    * (at your option) any later version.                                   *
-    *                                                                       *
-    *************************************************************************
+    ********************************************************************
+    *                                                                  *
+    * This library is free software; you can redistribute it and/or    *
+    * modify it under the terms of the GNU Lesser General Public       *
+    * License as published by the Free Software Foundation; either     *
+    * version 2 of the License, or (at your option) any later version. *
+    *                                                                  *
+    ********************************************************************
 */
 #ifndef KCODECACTION_H
 #define KCODECACTION_H
