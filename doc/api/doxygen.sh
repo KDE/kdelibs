b66859d6c5e29afed7c6cb6ecd76499cd3c1f77a
Matthias Kretz <kretz@kde.org>
2007-05-14 21:12:07 +0000

    allow the use of @topdir@ in DOXYGEN_SET_ directives in Mainpage.dox
    svn path=/trunk/KDE/kdelibs/; revision=664789

--- doc/api/doxygen.sh
+++ doc/api/doxygen.sh
@@ -608,7 +608,7 @@ apidox_subdir()
 	# Mainpage.doxs may contain overrides to our settings,
 	# so copy them in.
 	grep '^//[[:space:]]*DOXYGEN_SET_' "$srcdir/Mainpage.dox" | \
-		sed -e 's+//[[:space:]]*DOXYGEN_SET_++' >> "$subdir/Doxyfile"
+		sed -e 's+//\s*DOXYGEN_SET_++' -e "s+@topdir@+$top_srcdir+" >> "$subdir/Doxyfile"
 	apidox_specials "$srcdir/Mainpage.dox" "$subdir/Doxyfile"
 
 	excludes=`extract_line DOXYGEN_EXCLUDE`
