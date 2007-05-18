fffb4553e80b78eeead920172a9694bc0c6b7f67
Dirk Mueller <mueller@kde.org>
2007-05-18 11:54:50 +0000

    update version
    svn path=/trunk/KDE/kdelibs/; revision=665936

--- kdecore/util/kdeversion.h
+++ kdecore/util/kdeversion.h
@@ -42,7 +42,7 @@
  * or for a protocol (e.g. http) be careful that it is appropriate.
  * (Fictional) example: "4.0.90 (>=20070101)"
  */
-#define KDE_VERSION_STRING "3.90.1 (KDE 4.0-alpha1)"
+#define KDE_VERSION_STRING "3.90.1 (KDE 4.0 >= 20070518)"
 
 /// @brief Major version of KDE, at compile time
 #define KDE_VERSION_MAJOR 3
