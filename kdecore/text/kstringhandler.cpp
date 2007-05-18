48abdadba7f8b4d08ea3b947e7ef9980dff3afbc
Dirk Mueller <mueller@kde.org>
2007-05-18 12:34:15 +0000

    fix string scrambling for KDE4 as well
    svn path=/trunk/KDE/kdelibs/; revision=665959

--- kdecore/text/kstringhandler.cpp
+++ kdecore/text/kstringhandler.cpp
@@ -190,7 +190,7 @@ QString KStringHandler::obscure( const QString &str )
   QString result;
   const QChar *unicode = str.unicode();
   for ( int i = 0; i < str.length(); ++i )
-    result += ( unicode[ i ].unicode() < 0x20 ) ? unicode[ i ] :
+    result += ( unicode[ i ].unicode() < 0x21 ) ? unicode[ i ] :
         QChar( 0x1001F - unicode[ i ].unicode() );
 
   return result;
