c7c908b0b8c4170b6c35d238b1028b9bca391fbd
David Faure <faure@kde.org>
2007-05-31 17:24:47 +0000

    cupsdconf was just a test program; the cups manager dlopens the libcupsdconf library anyway  -> don't install cupsdconf anymore, and rename it to cupsconftest
    svn path=/trunk/KDE/kdelibs/; revision=670203

--- kdeprint/cups/cupsdconf2/main.cpp
+++ kdeprint/cups/cupsdconf2/main.cpp
@@ -31,7 +31,7 @@ static KCmdLineOptions options[] =
 	KCmdLineLastOption
 };
 
-extern "C" KDE_EXPORT int kdemain(int argc, char *argv[])
+extern "C" int main(int argc, char *argv[])
 {
 	KCmdLineArgs::init(argc,argv,"cupsdconf",
 			   I18N_NOOP("A CUPS configuration tool"),
