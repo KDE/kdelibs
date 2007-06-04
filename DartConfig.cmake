3b446b5846efbfb813e4dbd16d7c49468c9508ca
Alexander Neundorf <neundorf@kde.org>
2007-06-04 20:11:11 +0000

    this one is not required, CTestConfig.cmake has it all
    Alex
    
    
    svn path=/trunk/KDE/kdelibs/; revision=671452

--- DartConfig.cmake
+++
@@ -1,8 +0,0 @@
-# support for dart:  http://public.kitware.com/KDE/Testing 
-# submitted by Bill Hoffman
-include(${CMAKE_CURRENT_SOURCE_DIR}/CTestConfig.cmake)
-set(NIGHTLY_START_TIME "${CTEST_NIGHTLY_START_TIME}")
-set(DROP_METHOD "${CTEST_DROP_METHOD}")
-set(DROP_SITE "${CTEST_DROP_SITE}")
-set(DROP_LOCATION "${CTEST_DROP_LOCATION}")
-set(TRIGGER_SITE "${CTEST_TRIGGER_SITE}")
