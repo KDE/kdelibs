b74d7d65b7d45b2657098265e9483449bd2322c4
Dominik Haumann <dhaumann@kde.org>
2007-06-05 19:03:25 +0000

    api docs fixes
    svn path=/trunk/KDE/kdelibs/; revision=671903

--- kdeui/widgets/ktitlewidget.h
+++ kdeui/widgets/ktitlewidget.h
@@ -69,23 +69,23 @@ class KDEUI_EXPORT KTitleWidget : public QWidget
 public:
     /**
      * Possible title pixmap alignments.
-     */
-    enum ImageAlignment {
-        ImageLeft /*<< Display the pixmap on the left */,
-        ImageRight /*<< Display the pixmap on the right */
-    };
-
-    /**
-     * Comment message types
      *
      * @li ImageLeft: Display the pixmap left
      * @li ImageRight: Display the pixmap right (default)
      */
+    enum ImageAlignment {
+        ImageLeft, /**< Display the pixmap on the left */
+        ImageRight /**< Display the pixmap on the right */
+    };
+
+    /**
+     * Comment message types
+     */
     enum MessageType {
-        PlainMessage /*<< Normal comment */,
-        InfoMessage /*<< Information the user should be alerted to */,
-        WarningMessage /*<< A warning the user should be alerted to */,
-        ErrorMessage /*<< An error message */
+        PlainMessage, /**< Normal comment */
+        InfoMessage, /**< Information the user should be alerted to */
+        WarningMessage, /**< A warning the user should be alerted to */
+        ErrorMessage /**< An error message */
     };
 
     /**
@@ -102,16 +102,19 @@ public:
 
     /**
      * @return the text displayed in the title
+     * @see setText()
      */
     QString text() const;
 
     /**
      * @return the text displayed in the comment below the title, if any
+     * @see setComment()
      */
     QString comment() const;
 
     /**
      * @return the pixmap displayed in the title
+     * @see setPixmap()
      */
     const QPixmap *pixmap() const;
 
@@ -135,12 +138,14 @@ public Q_SLOTS:
      * @param text Text displayed on the label. It can either be plain text or rich text. If it
      * is plain text, the text is displayed as a bold title text.
      * @param alignment Alignment of the text. Default is left and vertical centered.
+     * @see text()
      */
     void setText(const QString &text, Qt::Alignment alignment = Qt::AlignLeft | Qt::AlignVCenter);
     /**
      * @param text Text displayed on the label. It can either be plain text or rich text. If it
      * is plain text, the text is displayed as a bold title text.
      * @param type The sort of message it is; will also set the icon accordingly @see MessageType
+     * @see text()
      */
     void setText(const QString &text, MessageType type);
 
@@ -148,30 +153,35 @@ public Q_SLOTS:
      * @param comment Text displayed beneath the main title as a comment.
      *                It can either be plain text or rich text.
      * @param type The sort of message it is. @see MessageType
+     * @see comment()
      */
     void setComment(const QString &comment, MessageType type = PlainMessage);
 
     /**
      * @param pixmap Pixmap displayed in the header. The pixmap is by default right, but
      * @param alignment can be used to display it also left.
+     * @see pixmap()
      */
     void setPixmap(const QPixmap &pixmap, ImageAlignment alignment = ImageRight);
 
     /**
      * @param icon name of the icon to display in the header. The pixmap is by default right, but
      * @param alignment can be used to display it also left.
+     * @see pixmap()
      */
     void setPixmap(const QString &icon, ImageAlignment alignment = ImageRight);
 
     /**
      * @param pixmap the icon to display in the header. The pixmap is by default right, but
      * @param alignment can be used to display it also left.
+     * @see pixmap()
      */
     void setPixmap(const QIcon& icon, ImageAlignment alignment = ImageRight);
 
     /**
      * @param pixmap the icon to display in the header. The pixmap is by default right, but
      * @param alignment can be used to display it also left.
+     * @see pixmap()
      */
     void setPixmap(MessageType type, ImageAlignment alignment = ImageRight);
 
