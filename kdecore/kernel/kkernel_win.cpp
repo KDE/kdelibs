b55422ce0d01f00866b400d4127b9a6d842c1da2
Christian Ehrlicher <ch.ehrlicher@gmx.de>
2007-06-06 04:43:46 +0000

    explicit use unicode versions to fix mingw compile
    svn path=/trunk/KDE/kdelibs/; revision=672111

--- kdecore/kernel/kkernel_win.cpp
+++ kdecore/kernel/kkernel_win.cpp
@@ -92,15 +92,15 @@ QString getWin32RegistryValue ( HKEY key, const QString& subKey, const QString& 
     DWORD dwType=REG_SZ;
     DWORD dwSize;
 
-    if ( ERROR_SUCCESS!=RegOpenKeyEx ( key, WIN32_CAST_CHAR subKey.utf16(), 0, KEY_READ, &hKey ) )
+    if ( ERROR_SUCCESS!=RegOpenKeyExW ( key, WIN32_CAST_CHAR subKey.utf16(), 0, KEY_READ, &hKey ) )
         FAILURE;
 
-    if ( ERROR_SUCCESS!=RegQueryValueEx ( hKey, WIN32_CAST_CHAR item.utf16(), NULL, NULL, NULL, &dwSize ) )
+    if ( ERROR_SUCCESS!=RegQueryValueExW ( hKey, WIN32_CAST_CHAR item.utf16(), NULL, NULL, NULL, &dwSize ) )
         FAILURE;
 
     lszValue = new TCHAR[dwSize];
 
-    if ( ERROR_SUCCESS!=RegQueryValueEx ( hKey, WIN32_CAST_CHAR item.utf16(), NULL, &dwType, ( LPBYTE ) lszValue, &dwSize ) ) {
+    if ( ERROR_SUCCESS!=RegQueryValueExW ( hKey, WIN32_CAST_CHAR item.utf16(), NULL, &dwType, ( LPBYTE ) lszValue, &dwSize ) ) {
         delete [] lszValue;
         FAILURE;
     }
@@ -116,14 +116,14 @@ bool showWin32FilePropertyDialog ( const QString& fileName )
 {
     QString path_ = QDir::convertSeparators ( QFileInfo ( fileName ).absoluteFilePath() );
 
-    SHELLEXECUTEINFO execInfo;
+    SHELLEXECUTEINFOW execInfo;
     memset ( &execInfo,0,sizeof ( execInfo ) );
     execInfo.cbSize = sizeof ( execInfo );
     execInfo.fMask = SEE_MASK_INVOKEIDLIST | SEE_MASK_NOCLOSEPROCESS | SEE_MASK_FLAG_NO_UI;
     const QString verb ( QLatin1String ( "properties" ) );
-    execInfo.lpVerb = ( TCHAR* ) verb.utf16();
-    execInfo.lpFile = ( TCHAR* ) path_.utf16();
-    return ShellExecuteEx ( &execInfo );
+    execInfo.lpVerb = WIN32_CAST_CHAR verb.utf16();
+    execInfo.lpFile = WIN32_CAST_CHAR path_.utf16();
+    return ShellExecuteExW ( &execInfo );
 }
 
 QByteArray getWin32LocaleName()
