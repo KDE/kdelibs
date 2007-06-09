0b9dcf001b17866582b2aad206dcdca2eacde4f4
Laurent Montel <montel@kde.org>
2007-06-09 08:41:58 +0000

    Now we don t install kssl header so we can t say use them => it not necessary to install include we said "use them"
    svn path=/trunk/KDE/kdelibs/; revision=673176

--- kde3support/kio/kssl/ksslcertdlg.h
+++
@@ -1,5 +0,0 @@
-#ifdef KDE_NO_COMPAT
-#error "ksslcertdlg.h needs to be renamed to ksslcertdialog.h"
-#else
-#include <ksslcertdialog.h>
-#endif
--- kde3support/kio/kssl/ksslinfodlg.h
+++
@@ -1,5 +0,0 @@
-#ifdef KDE_NO_COMPAT
-#error "ksslinfodlg.h needs to be renamed to ksslinfodialog.h"
-#else
-#include <ksslinfodialog.h>
-#endif
