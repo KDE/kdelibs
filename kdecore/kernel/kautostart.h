a8859e72aa0ca3a3f81ceef5132ce3f57247da55
Dominik Haumann <dhaumann@kde.org>
2007-06-16 15:22:27 +0000

    api docs: some @see references
    svn path=/trunk/KDE/kdelibs/; revision=676294

--- kdecore/kernel/kautostart.h
+++ kdecore/kernel/kautostart.h
@@ -76,33 +76,38 @@ class KDECORE_EXPORT KAutostart : public QObject
         /**
          * Flags for each of the conditions that may affect whether or not
          * a service actually autostarted on login
-         *
-         * CheckCommand: an executable that is checked for existence by name
          */
         enum Condition
         {
             NoConditions = 0,
+            /**
+             * an executable that is checked for existence by name
+             */
             CheckCommand = 1
         };
         Q_DECLARE_FLAGS(Conditions, Condition)
 
         /**
          * Enumerates the various autostart phases that occur during start-up.
-         *
-         * BaseDesktop: the essential desktop services such as panels and
-         *              window managers
-         * DesktopServices: services that should be available before most
-         *                  interactive applications start but that aren't part
-         *                  of the base desktop. This would include things such
-         *                  as clipboard managers and mouse gesture tools.
-         * Applications: everything else that doesn't belong in the above two
-         *               categories, including most system tray applications,
-         *               system monitors and interactive applications
          */
         enum StartPhase
         {
+            /**
+             * the essential desktop services such as panels and window managers
+             */
             BaseDesktop = 0,
+            /**
+             * services that should be available before most interactive
+             * applications start but that aren't part of the base desktop.
+             * This would include things such as clipboard managers and
+             * mouse gesture tools.
+             */
             DesktopServices = 1,
+            /**
+             * everything else that doesn't belong in the above two categories,
+             * including most system tray applications, system monitors and
+             * interactive applications
+             */
             Applications = 2
         };
 
@@ -110,6 +115,7 @@ class KDECORE_EXPORT KAutostart : public QObject
          * Sets the given exec to start automatically at login
          * @param autostart will register with the autostart facility when true
          *        and deregister when false
+         * @see autostarts()
          */
         void setAutostarts(bool autostart);
 
@@ -118,33 +124,36 @@ class KDECORE_EXPORT KAutostart : public QObject
          * autostart system is set to autostart at login or not
          * @param environment if provided the check will be performed as if
          *        being loaded in that environment
-         * @param check autostart conditions to check for
-         *        @see commandToCheck()
+         * @param check autostart conditions to check for (see commandToCheck())
+         * @see setAutostarts()
          */
         bool autostarts(const QString& environment = QString(),
                         Conditions check = NoConditions) const;
 
         /**
          * Returns the associated command for this autostart service
+         * @see setCommand()
          */
         QString command() const;
         /**
          * Set the associated command for this autostart service
+         * @see command()
          */
         void setCommand(const QString& command);
 
         /**
          * Returns the user-visible name this autostart service is registered as
-         * @see setEntryName
+         * @see setVisibleName(), setEntryName()
          */
         QString visibleName() const;
         /**
          * Sets the user-visible name for this autostart service.
+         * @see visibleName()
          */
         void setVisibleName(const QString& entryName);
 
         /**
-         * Checks whether or not a service by the given name is registered
+         * Checks whether or not a service by the given name @p entryName is registered
          * with the autostart system. Does not check whether or not it is
          * set to actually autostart or not.
          * @param entryName the name of the service to check for
@@ -155,11 +164,13 @@ class KDECORE_EXPORT KAutostart : public QObject
          * Returns the executable to check for when attempting to autostart
          * this service. If the executable is not found in the user's
          * environment, it will not autostart.
+         * @see setCommandToCheck()
          */
         QString commandToCheck() const;
         /**
          * Sets the executable to check for the existence of when
          * autostarting this service
+         * @see commandToCheck()
          */
         void setCommandToCheck(const QString& exec);
 
@@ -169,7 +180,7 @@ class KDECORE_EXPORT KAutostart : public QObject
          * Note that this is KDE specific and may not work in other
          * environments.
          *
-         * @see StartPhase
+         * @see StartPhase, setStartPhase()
          */
         StartPhase startPhase() const;
         /**
@@ -178,7 +189,7 @@ class KDECORE_EXPORT KAutostart : public QObject
          * Note that this is KDE specific and may not work in other
          * environments.
          *
-         * @see StartPhase
+         * @see StartPhase, startPhase()
          */
         void setStartPhase(StartPhase phase);
 
@@ -191,20 +202,24 @@ class KDECORE_EXPORT KAutostart : public QObject
          * list, then only those environments will be allowed to
          * autoload the service. If an environment is marked as both allowed
          * and excluded, it will be excluded.
+         * @see setAllowedEnvironments()
          */
         QStringList allowedEnvironments() const;
         /**
          * Sets the environments this service is allowed to start in
+         * @see allowedEnvironments(), addToAllowedEnvironments()
          */
         void setAllowedEnvironments(const QStringList& environments);
         /**
          * Adds an environment to the list of environments this service may
          * start in.
+         * @see setAllowedEnvironments(), removeFromAllowedEnvironments()
          */
         void addToAllowedEnvironments(const QString& environment);
         /**
          * Removes an environment to the list of environments this service may
          * start in.
+         * @see addToAllowedEnvironments()
          */
         void removeFromAllowedEnvironments(const QString& environment);
 
@@ -215,20 +230,24 @@ class KDECORE_EXPORT KAutostart : public QObject
          * This does not take other autostart conditions
          * such as into account. If the same environment is also marked as
          * allowed, it will still be excluded.
+         * @see setExcludedEnvironments()
          */
         QStringList excludedEnvironments() const;
         /**
          * Sets the environments this service is not allowed to start in
+         * @see excludedEnvironments(), addToExcludedEnvironments()
          */
         void setExcludedEnvironments(const QStringList& environments);
         /**
          * Adds an environment to the list of environments this service may
          * not be autostarted in
+         * @see removeFromExcludedEnvironments()
          */
         void addToExcludedEnvironments(const QString& environment);
         /**
          * Removes an environment to the list of environments this service may
          * not be autostarted in
+         * @see addToExcludedEnvironments()
          */
         void removeFromExcludedEnvironments(const QString& environment);
 
