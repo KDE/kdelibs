a7314c8d06a740ec11a032a2e3ac7a1905cca995
Luciano Montanaro <mikelima@cirulla.net>
2007-06-19 14:24:40 +0000

    Another JavaScriptCore change (Sajay Madhav) to help GC debugging The changelog says:         Add JavaScriptCore define to help with tracing of when objects are marked.
            * kjs/object.cpp:
            (KJS::JSObject::mark):
    
    svn path=/trunk/KDE/kdelibs/; revision=677612

--- kjs/object.cpp
+++ kjs/object.cpp
@@ -44,6 +44,7 @@
 #endif
 
 #define JAVASCRIPT_CALL_TRACING 0
+#define JAVASCRIPT_MARK_TRACING 0
 
 #if JAVASCRIPT_CALL_TRACING
 static bool _traceJavaScript = false;
@@ -120,11 +121,24 @@ void JSObject::mark()
 {
   JSCell::mark();
 
+#if JAVASCRIPT_MARK_TRACING
+  static int markStackDepth = 0;
+  markStackDepth++;
+  for (int i = 0; i < markStackDepth; i++)
+    putchar('-');
+  
+  printf("%s (%p)\n", className().UTF8String().c_str(), this);
+#endif
+  
   JSValue *proto = _proto;
   if (!proto->marked())
     proto->mark();
 
   _prop.mark();
+  
+#if JAVASCRIPT_MARK_TRACING
+  markStackDepth--;
+#endif
 }
 
 JSType JSObject::type() const
