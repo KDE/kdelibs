02755aef6aab265cff292c6ada1ba70845542616
Alex Merry <kde@randomguy3.me.uk>
2007-07-04 18:50:16 +0000

    Actually, ippDeleteAttribute() is available in CUPS 1.1.19. We specifically check for it in FindCups.cmake.
    svn path=/trunk/KDE/kdelibs/; revision=683458

--- kdeprint/cups/ipprequest.cpp
+++ kdeprint/cups/ipprequest.cpp
@@ -513,22 +513,6 @@ void IppRequest::setMap(const QMap<QString,QString>& opts)
 	cupsFreeOptions(n, options);
 
 	// find an remove that annoying "document-format" attribute
-#if CUPS_VERSION_MAJOR == 1 && CUPS_VERSION_MINOR >= 2
 	ipp_attribute_t *attr = ippFindAttribute(request_, "document-format", IPP_TAG_NAME);
 	ippDeleteAttribute(request_, attr);
-#else
-	// (can't use IppDeleteAttribute as older cups doesn't have that)
-	ipp_attribute_t *attr = request_->attrs;
-	while (attr)
-	{
-		if (attr->next && strcmp(attr->next->name, "document-format") == 0)
-		{
-			ipp_attribute_t	*attr2 = attr->next;
-			attr->next = attr2->next;
-			_ipp_free_attr(attr2);
-			break;
-		}
-		attr = attr->next;
-	}
-#endif
 }
