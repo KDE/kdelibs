af4a5e48ca1b67dee1df25ad7eae77cf17cfd1b8
Josef Spillner <spillner@kde.org>
2007-07-09 14:02:14 +0000

    - the "useful debugging on Loch Lomond day trip" comment by David - slightly extended by me to get around ogoffart's destructive search-and-replace tactics
    CCMAIL:faure@kde.org
    
    Hm, I guess the long-term real fix is now to fix kdeinit to report binary-incompatible KIO
    slaves instead of just reporting them to have died unexpectedly?
    
    
    svn path=/trunk/KDE/kdelibs/; revision=685690

--- kdecore/kernel/kauthorized.cpp
+++ kdecore/kernel/kauthorized.cpp
@@ -190,7 +190,9 @@ public:
     :   actionRestrictions( false ), blockEverything(false),mutex(QMutex::Recursive)
   {
     Q_ASSERT_X(QCoreApplication::instance(),"KAuthorizedPrivate()","There has to be an existing QCoreApplication::instance() pointer");
-    Q_ASSERT_X(!QCoreApplication::instance()->applicationName().isEmpty(),"KAuthorizedPrivate()","There has to be an application name set (See QCoreApplication::instance()lication::setApplicationName)");
+    // Why is this comment there? Seems unnecessary, applicationName isn't used in here
+    // It's a fallback in KGlobal, but only a fallback: if we have a KComponentData then setApplicationName isn't needed
+    Q_ASSERT_X(!QCoreApplication::instance()->applicationName().isEmpty(),"KAuthorizedPrivate()","There has to be an application name set (See QCoreApplication::instance()->setApplicationName)");
 
     KSharedConfig::Ptr config = KGlobal::config();
 
