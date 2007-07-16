78feae351736f72c65515ef811bf0f56a5fd4032
Oswald Buddenhagen <ossi@kde.org>
2007-07-16 12:07:12 +0000

    split KShell into unix/win. the win impl is a dummy.
    svn path=/trunk/KDE/kdelibs/; revision=688606

---
+++ kdecore/util/kshell_win.cpp
@@ -0,0 +1,40 @@
+/*
+    This file is part of the KDE libraries
+
+    Copyright (c) 2007 Oswald Buddenhagen <ossi@kde.org>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301, USA. 
+*/
+
+#include "kshell.h"
+
+QStringList KShell::splitArgs(const QString &args, Options flags, Errors *err)
+{
+    if (err)
+        *err = NoError;
+
+    return QStringList();
+}
+
+QString KShell::quoteArg(const QString &arg)
+{
+    if (arg.isEmpty())
+        return QString::fromLatin1("\"\"");
+
+    QString ret(arg);
+
+    return ret;
+}
