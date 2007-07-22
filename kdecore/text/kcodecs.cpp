95fbacab0607a39396c12767d620fc35a0640135
Alex Merry <kde@randomguy3.me.uk>
2007-07-22 13:00:17 +0000

    Let's have some consistency in the license statements.
    svn path=/trunk/KDE/kdelibs/; revision=690888

--- kdecore/text/kcodecs.cpp
+++ kdecore/text/kcodecs.cpp
@@ -9,9 +9,9 @@
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
+   GNU Lesser General Public License for more details.
 
-   You should have received a copy of the GNU Library General Public
+   You should have received a copy of the GNU Lesser General Public
    License along with this program; if not, write to the Free Software
    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
