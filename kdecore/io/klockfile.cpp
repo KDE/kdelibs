b048067a5dbdace1b355e5dbe5cc742673dfc173
Ralf Habacker <ralf.habacker@freenet.de>
2007-07-23 11:52:56 +0000

    - reverted 691263: don't break history
    svn path=/trunk/KDE/kdelibs/; revision=691302

--- kdecore/io/klockfile.cpp
+++ kdecore/io/klockfile.cpp
@@ -115,6 +115,7 @@ static bool operator!=( const KDE_struct_stat& st_buf1,
 
 static bool testLinkCountSupport(const QByteArray &fileName)
 {
+#ifndef Q_OS_WIN
    KDE_struct_stat st_buf;
    int result = -1;
    // Check if hardlinks raise the link count at all?
@@ -123,6 +124,9 @@ static bool testLinkCountSupport(const QByteArray &fileName)
      ::unlink( fileName+".test" );
    }
    return (result < 0 || ((result == 0) && (st_buf.st_nlink == 2)));
+#else
+   return false;
+#endif
 }
 
 static KLockFile::LockResult lockFile(const QString &lockFile, KDE_struct_stat &st_buf,
@@ -200,9 +204,11 @@ static KLockFile::LockResult deleteStaleLock(const QString &lockFile, KDE_struct
    QByteArray tmpFile = QFile::encodeName(ktmpFile->fileName());
    delete ktmpFile;
 
+#ifdef Q_OS_UNIX
    // link to lock file
    if (::link(lckFile, tmpFile) != 0)
       return KLockFile::LockFail; // Try again later
+#endif
 
    // check if link count increased with exactly one
    // and if the lock file still matches
@@ -348,7 +354,11 @@ KLockFile::LockResult KLockFile::lock(LockFlags options)
      if (n < 2000)
         n = n * 2;
 
+#ifdef Q_OS_UNIX
      select(0, 0, 0, 0, &tv);
+#else
+     //TODO for win32
+#endif
   }
   if (result == LockOK)
      d->isLocked = true;
