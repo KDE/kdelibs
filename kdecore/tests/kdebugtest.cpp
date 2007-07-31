ebf4fe22ea3ed44dd6a3778b8944b9e6d59c04aa
Thiago Macieira <thiago@kde.org>
2007-07-31 00:49:13 +0000

    Adapt the testcase. This isn't testing anything...
    svn path=/trunk/KDE/kdelibs/; revision=694552

--- kdecore/tests/kdebugtest.cpp
+++ kdecore/tests/kdebugtest.cpp
@@ -8,53 +8,52 @@
 
 void testKDebug()
 {
-    kDebug().form("mytest %s", "hello") << endl;
     QString test = "%20C this is a string";
-    kDebug(150) << test << endl;
+    kDebug(150) << test;
     QByteArray cstr = test.toLatin1();
-    kDebug(150) << test << endl;
+    kDebug(150) << test;
     QChar ch = 'a';
-    kDebug() << "QChar a: " << ch << endl;
+    kDebug() << "QChar a: " << ch;
     ch = '\r';
-    kDebug() << "QChar \\r: " << ch << endl;
-    kDebug() << k_lineinfo << "error on this line" << endl;
-    kDebug(2 == 2) << "this is right " << perror << endl;
-    kDebug() << "Before instance creation" << endl;
-    kDebug(1202) << "Before instance creation" << endl;
+    kDebug() << "QChar \\r: " << ch;
+    kDebug() << k_lineinfo << "error on this line";
+    kDebug(2 == 2) << "this is right " << perror;
+    kDebug() << "Before instance creation";
+    kDebug(1202) << "Before instance creation";
     KComponentData i("kdebugtest");
-    kDebug(1) << "kDebugInfo with inexisting area number" << endl;
-    kDebug(1202) << "This number has a value of " << 5 << endl;
-    // kDebug() << "This number should come out as appname " << 5 << " " << "test" << endl;
-    kWarning() << "1+1 = " << 1+1+1 << endl;
-    kError(1+1 != 2) << "there is something really odd!" << endl;
+    kDebug(1) << "kDebugInfo with inexisting area number";
+    kDebug(1202) << "This number has a value of " << 5;
+    // kDebug() << "This number should come out as appname " << 5 << " " << "test";
+    kWarning() << "1+1 = " << 1+1+1;
+    kError(1+1 != 2) << "there is something really odd!";
     QString s = "mystring";
-    kDebug() << s << endl;
-    kError(1202) << "Error !!!" << endl;
-    kError() << "Error with no area" << endl;
+    kDebug() << s;
+    kError(1202) << "Error !!!";
+    kError() << "Error with no area";
 
-    kDebug() << "Printing a null QWidget pointer: " << (QWidget*)0 << endl;
+    kDebug() << "Printing a null QWidget pointer: " << (QWidget*)0;
 
-    kDebug() << "char " << '^' << " " << char(26) << endl;
+    kDebug() << "char " << '^' << " " << char(26);
     QPoint p(0,9);
-    kDebug() << p << endl;
+    kDebug() << p;
 
     QRect r(9,12,58,234);
-    kDebug() << r << endl;
+    kDebug() << r;
 
     QStringList sl;
     sl << "hi" << "this" << "list" << "is" << "short";
-    kDebug() << sl << endl;
+    kDebug() << sl;
 
     QList<int> il;
-    kDebug() << "Empty QList<int>: " << il << endl;
+    kDebug() << "Empty QList<int>: " << il;
     il << 1 << 2 << 3 << 4 << 5;
-    kDebug() << "QList<int> filled: " << il << endl;
+    kDebug() << "QList<int> filled: " << il;
 
     qint64 big = 65536LL*65536*500;
-    kDebug() << big << endl;
+    kDebug() << big;
 
     QVariant v( 0.12345 );
-    kDebug() << "Variant: " << v << endl;
+    kDebug() << "Variant: " << v;
 
     QByteArray data;
     data.resize( 6 );
@@ -64,10 +63,10 @@ void testKDebug()
     data[3] = 'l';
     data[4] = 'l';
     data[5] = 'o';
-    kDebug() << data << endl;
+    kDebug() << data;
     data.resize( 80 );
     data.fill( 42 );
-    kDebug() << data << endl;
+    kDebug() << data;
 }
 
 int main(int, char** )
