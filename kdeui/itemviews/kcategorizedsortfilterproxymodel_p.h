98add06ff564f24b17283311a9d7ecd2b0566cc7
Thiago Macieira <thiago@kde.org>
2007-09-11 07:58:24 +0000

    You're not using this.
    QAtomic is off-limits now. Don't use it.
    
    svn path=/trunk/KDE/kdelibs/; revision=711029

--- kdeui/itemviews/kcategorizedsortfilterproxymodel_p.h
+++ kdeui/itemviews/kcategorizedsortfilterproxymodel_p.h
@@ -92,7 +92,6 @@ public:
     QPersistentModelIndexData() : model(0) {}
     QPersistentModelIndexData(const QModelIndex &idx) : index(idx), model(idx.model()) {}
     QModelIndex index;
-    QAtomic ref;
     const QAbstractItemModel *model;
     static QPersistentModelIndexData *create(const QModelIndex &index);
     static void destroy(QPersistentModelIndexData *data);
