4737dc2760b5045f92bab96f347b5a3f35de4cdd
Matthew Woehlke <mw_triad@users.sourceforge.net>
2007-09-12 20:20:17 +0000

    allow KStatefulBrush to be put in a QVariant
    svn path=/trunk/KDE/kdelibs/; revision=711790

--- kdeui/colors/kcolorscheme.h
+++ kdeui/colors/kcolorscheme.h
@@ -491,5 +491,7 @@ private:
     class KStatefulBrushPrivate *d;
 };
 
+Q_DECLARE_METATYPE(KStatefulBrush) /* so we can pass it in QVariant's */
+
 #endif // KCOLORSCHEME_H
 // kate: space-indent on; indent-width 4; replace-tabs on; auto-insert-doxygen on;
