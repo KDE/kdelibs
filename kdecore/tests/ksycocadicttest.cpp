f66bd66ab904b054b0a12c9089ba905fc2aca52c
David Faure <faure@kde.org>
2007-09-17 20:19:09 +0000

    ++license
    svn path=/trunk/KDE/kdelibs/; revision=713571

--- kdecore/tests/ksycocadicttest.cpp
+++ kdecore/tests/ksycocadicttest.cpp
@@ -1,3 +1,22 @@
+/* This file is part of the KDE project
+   Copyright (C) 2007 David Faure <faure@kde.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
 #include <ksycoca.h>
 #include <QBuffer>
 #include <kmimetype.h>
@@ -16,22 +35,6 @@ private:
 };
 
 QTEST_KDEMAIN_CORE(KSycocaDictTest)
-
-#if 0
-// Can't do this, KSycocaEntryPrivate isn't exported...
-// So we use KMimeType instead.
-class KSycocaFakeEntryPrivate : public KSycocaEntryPrivate
-{
-};
-
-class KSycocaFakeEntry : public KSycocaEntry
-{
-public:
-    KSycocaFakeEntry(const QString& name)
-        : KSycocaEntry(*new KSycocaFakeEntryPrivate(name))
-    {}
-};
-#endif
 
 void KSycocaDictTest::add(KSycocaDict& dict, const QString& key, const QString& mimeType)
 {
