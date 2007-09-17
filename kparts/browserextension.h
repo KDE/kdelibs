7930b2fb7ed2245eca2090b3589604f9679d20e6
Laurent Montel <montel@kde.org>
2007-09-17 06:18:36 +0000

    Change signal signature as discussed with David => KFileItemList -> QList<KFileItem>
    svn path=/trunk/KDE/kdelibs/; revision=713343

--- kparts/browserextension.h
+++ kparts/browserextension.h
@@ -646,7 +646,7 @@ public:  // yes, those signals are public; don't tell moc or doxygen :)
    * Used when a set of files/URLs is selected (with full information
    * about those URLs, including size, permissions etc.)
    */
-  void selectionInfo( const KFileItemList &items );
+  void selectionInfo( const QList<KFileItem>& items );
   /**
    * Inform the hosting application about the current selection.
    * Used when some text is selected.
