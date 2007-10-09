f943850d3823ee3bb7dc045bbdeef9cb127d104d
Thomas Zander <zander@kde.org>
2007-10-09 10:15:05 +0000

    Add the KdePrint namespace with createPrintDialog() method(s) to create the right print dialog.   Applications that use custom widget(s) to request more options from the user should use this instead of using Qt directly.
    svn path=/trunk/KDE/kdelibs/; revision=723323

---
+++ kdeui/dialogs/kdeprintdialog.cpp
@@ -0,0 +1,64 @@
+/*
+ *  This file is part of the KDE libraries
+ *  Copyright (c) 2007 Alex Merry <huntedhacker@tiscali.co.uk>
+ *  Copyright (c) 2007 Thomas Zander <zander@kde.org>
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Library General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Library General Public License
+ *  along with this library; see the file COPYING.LIB.  If not, write to
+ *  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ *  Boston, MA 02110-1301, USA.
+ **/
+
+#include "kdeprintdialog.h"
+
+#include <klocale.h>
+
+#include <QLayout>
+#include <QPrintDialog>
+#include <QStyle>
+#include <QTabWidget>
+
+QPrintDialog *KdePrint::createPrintDialog(QPrinter *printer,
+                                               const QList<QWidget*> &customTabs,
+                                               QWidget *parent)
+{
+    QPrintDialog *dialog = createPrintDialog(printer, parent);
+    if (! customTabs.isEmpty()) {
+        QWidget *generalWidget = dialog->findChild<QWidget*>("generalWidget");
+        QWidget *buttons = dialog->findChild<QWidget*>("buttonBox");
+        if (generalWidget && buttons) {
+            QTabWidget *tabs = new QTabWidget(dialog);
+            tabs->addTab(generalWidget, i18n("General"));
+            QStyle *style = dialog->style();
+            generalWidget->layout()->setContentsMargins(style->pixelMetric(QStyle::PM_LayoutLeftMargin),
+                                       style->pixelMetric(QStyle::PM_LayoutTopMargin),
+                                       style->pixelMetric(QStyle::PM_LayoutRightMargin),
+                                       style->pixelMetric(QStyle::PM_LayoutBottomMargin));
+            dialog->layout()->addWidget(tabs);
+            dialog->layout()->addWidget(buttons);
+
+            foreach(QWidget* tab, customTabs)
+                tabs->addTab(tab, tab->windowTitle());
+        }
+    }
+
+    return dialog;
+}
+
+
+QPrintDialog *KdePrint::createPrintDialog(QPrinter *printer,
+                                               QWidget *parent)
+{
+    return new QPrintDialog(printer, parent);
+}
+
---
+++ kdeui/dialogs/kdeprintdialog.h
@@ -0,0 +1,71 @@
+/*
+ *  This file is part of the KDE libraries
+ *  Copyright (c) 2007 Alex Merry <huntedhacker@tiscali.co.uk>
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Library General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Library General Public License
+ *  along with this library; see the file COPYING.LIB.  If not, write to
+ *  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ *  Boston, MA 02110-1301, USA.
+ **/
+
+#ifndef KDEPRINTDIALOG_H
+#define KDEPRINTDIALOG_H
+
+#include <kdeui_export.h>
+
+#include <QtCore/QList>
+
+class QPrintDialog;
+class QPrinter;
+class QWidget;
+
+/**
+ * Namespace for the KDE printing system
+ */
+namespace KdePrint
+{
+    /**
+     * Creates a printer dialog for a QPrinter with the given custom widgets.
+     *
+     * Note that the custom widgets will @b not be shown on Qt versions prior to
+     * 4.3.2.
+     *
+     * The caller takes ownership of the dialog and is responsible
+     * for deleting it.
+     *
+     * @param printer the QPrinter to apply settings to
+     * @param parent the parent for the dialog
+     * @param customTabs a list of custom widgets to show as tabs, the name printed on the tab will
+     *      be taken from the widgets windowTitle().
+     * @see QWidget::setWindowTitle()
+     */
+    KDEUI_EXPORT QPrintDialog *createPrintDialog(QPrinter *printer,
+                                                      const QList<QWidget*> &customTabs,
+                                                      QWidget *parent = 0);
+
+    /**
+     * Creates a printer dialog for a QPrinter
+     *
+     * The caller takes ownership of the dialog and is responsible
+     * for deleting it.
+     *
+     * @param printer the QPrinter to apply settings to
+     * @param parent the parent for the dialog
+     */
+    KDEUI_EXPORT QPrintDialog *createPrintDialog(QPrinter *printer,
+                                                      QWidget *parent = 0);
+};
+
+
+#endif // KDEPRINTDIALOG_H
+
