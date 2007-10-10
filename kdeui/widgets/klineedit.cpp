620867b7294336b9f8e3ecdb0479ac253b3f8a79
David Faure <faure@kde.org>
2007-10-10 22:14:55 +0000

    SVN_SILENT --debug
    svn path=/trunk/KDE/kdelibs/; revision=723852

--- kdeui/widgets/klineedit.cpp
+++ kdeui/widgets/klineedit.cpp
@@ -244,7 +244,7 @@ void KLineEdit::updateClearButton()
 
         // we don't need to check for RTL here as Qt does that for us
         // when it comes to style sheets
-        kDebug() << "setting padding right to " << buttonWidth;
+        //kDebug() << "setting padding right to" << buttonWidth;
         setStyleSheet( QString( "QLineEdit { padding-right: %1; }" )
                        .arg( buttonWidth ) );
     }
