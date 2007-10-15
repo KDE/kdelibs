02216d69bfdccd7ff5dc9ad1f67d280feee3ba53
Dirk Mueller <mueller@kde.org>
2007-10-15 16:27:57 +0000

    - add missing copyright/license statement - fix quoting
    svn path=/trunk/KDE/kdelibs/; revision=725526

--- kdoctools/kio_help.cpp
+++ kdoctools/kio_help.cpp
@@ -1,3 +1,25 @@
+/* This file is part of the KDE libraries
+   Copyright (C) 2000 Matthias Hoelzer-Kluepfel <hoelzer@kde.org>
+   Copyright (C) 2001 Stephan Kulow <coolo@kde.org>
+   Copyright (C) 2003 Cornelius Schumacher <schumacher@kde.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later versio
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+
 #include "kio_help.h"
 
 #include <config.h>
@@ -60,7 +82,7 @@ QString HelpProtocol::langLookup(const QString &fname)
     {
         QStringList::ConstIterator lang;
         for (lang = langs.begin(); lang != langs.end(); ++lang)
-            search.append(QString("%1%2/%3").arg(localDoc[id]).arg(*lang).arg(fname));
+            search.append(QString("%1%2/%3").arg(localDoc[id], *lang, fname));
     }
 
     // try to locate the file
