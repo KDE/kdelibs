cd905c72bd2dc3a51d267e695f6be823b8531ae4
Albert Astals Cid <tsdgeos@terra.es>
2007-10-20 20:49:01 +0000

    Revert r706570 which fixed a corner case in kaffeine and broke zoom settings on kpdf, kghostview, kview and basically anything using editable kselectations You'll have to find a fix that does not break all the other applications using the same class as you Please next time be extra careful when touching kdelibs code CCMAIL: gustavo.boiko@kdemail.net BUG: 150944
    svn path=/branches/KDE/3.5/kdelibs/; revision=727528

--- kdeui/kactionclasses.cpp
+++ kdeui/kactionclasses.cpp
@@ -667,8 +667,8 @@ int KSelectAction::plug( QWidget *widget, int index )
     KToolBar* bar = static_cast<KToolBar*>( widget );
     int id_ = KAction::getToolButtonID();
     bar->insertCombo( comboItems(), id_, isEditable(),
-                      SIGNAL( activated( int ) ), this,
-                      SLOT( slotActivated( int ) ), isEnabled(),
+                      SIGNAL( activated( const QString & ) ), this,
+                      SLOT( slotActivated( const QString & ) ), isEnabled(),
                       toolTip(), -1, index );
 
     QComboBox *cb = bar->getCombo( id_ );
