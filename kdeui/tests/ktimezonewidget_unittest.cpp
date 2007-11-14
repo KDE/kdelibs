c0485d39f519f4f70673c17e3becbaac8e7a2b24
David Faure <faure@kde.org>
2007-11-14 13:49:55 +0000

    Forgot to svn add the test for bug 152284   (part of commit 736623)
    svn path=/trunk/KDE/kdelibs/; revision=736625

---
+++ kdeui/tests/ktimezonewidget_unittest.cpp
@@ -0,0 +1,75 @@
+/* This file is part of the KDE libraries
+   Copyright (C) 2007 David Faure <faure@kde.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "qtest_kde.h"
+#include <ktimezonewidget.h>
+#include <kconfiggroup.h>
+#include <QtDBus/QtDBus>
+
+class KTimeZoneWidgetTest : public QObject
+{
+    Q_OBJECT
+
+private Q_SLOTS:
+    void testSetSelected()
+    {
+        if (!QDBusConnection::sessionBus().interface()->isServiceRegistered("org.kde.kded")) {
+            QSKIP("kded not running", SkipSingle);
+        }
+
+        KTimeZoneWidget tzw;
+        QVERIFY(tzw.topLevelItemCount() > 0);
+        QVERIFY(tzw.selectedItems().isEmpty());
+        tzw.setSelected("Europe/Paris", true);
+        QCOMPARE(tzw.selectedItems().count(), 1);
+        QCOMPARE(tzw.selection(), QStringList() << "Europe/Paris");
+        tzw.setSelected("America/New_York", true);
+        QCOMPARE(tzw.selectedItems().count(), 2);
+        QCOMPARE(tzw.selection(), QStringList() << "Europe/Paris" << "America/New_York");
+    }
+
+};
+
+// Tricky problem. The kded module writes out a config file, but unit tests have
+// a different KDEHOME so they don't see those config files.
+// Ideally unit tests should talk to their own kded instance,
+// but that means starting a new DBus session bus for all (each?) unit tests, somehow...
+//QTEST_KDEMAIN( KTimeZoneWidgetTest, GUI )
+
+int main(int argc, char *argv[])
+{
+    setenv("LC_ALL", "C", 1);
+    // The difference with QTEST_KDEMAIN is here: not setting $KDEHOME
+    unsetenv("KDE_COLOR_DEBUG");
+    KAboutData aboutData( "ktimezonewidgettest", 0, ki18n("qttest"), "version" );
+    KComponentData cData(&aboutData);
+    QApplication app( argc, argv );
+    app.setApplicationName( "ktimezonewidgettest" );
+
+#if 0
+    KTimeZoneWidget tzw;
+    tzw.show();
+    return app.exec();
+#else
+    KTimeZoneWidgetTest tc;
+    return QTest::qExec( &tc, argc, argv );
+#endif
+}
+
+#include "ktimezonewidget_unittest.moc"
