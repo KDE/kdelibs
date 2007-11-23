f94310b29c03802b8176971b3d8d4d179147f1f1
David Faure <faure@kde.org>
2007-11-23 12:41:44 +0000

    add TT task number in comment
    svn path=/trunk/KDE/kdelibs/; revision=740518

--- kdeui/dialogs/kedittoolbar_p.h
+++ kdeui/dialogs/kedittoolbar_p.h
@@ -50,7 +50,7 @@ Q_SIGNALS:
 protected:
     virtual Qt::DropActions supportedDropActions() const
     {
-        return Qt::MoveAction | Qt::CopyAction /*workaround for Qt-4.3.2 bug*/;
+        return Qt::MoveAction | Qt::CopyAction /*workaround for Qt-4.3.2 bug, TT task 188301*/;
     }
     virtual QStringList mimeTypes() const
     {
