e9e1300606ce72b5042696981b2678cd5386d216
David Faure <faure@kde.org>
2007-11-26 12:49:15 +0000

    Will, enable unit tests please
    svn path=/trunk/KDE/kdelibs/; revision=741805

--- knotify/tests/knotifytestwindow.cpp
+++ knotify/tests/knotifytestwindow.cpp
@@ -60,7 +60,7 @@ void KNotifyTestWindow::slotSendMessageEvent( )
 	m_nbNewMessage++;
 	if(!m_readNotif)
 	{
-		KNotification *n=new KNotification( "message", this, KNotification::Persistant );
+		KNotification *n=new KNotification( "message", this, KNotification::Persistent );
 		n->setText(i18n( "new message : %1" ,  view.c_text->toPlainText() ));
 		n->setActions( QStringList( i18n("Read") ) );
 		connect( n , SIGNAL(activated(unsigned int )), this , SLOT(slotMessageRead()));
