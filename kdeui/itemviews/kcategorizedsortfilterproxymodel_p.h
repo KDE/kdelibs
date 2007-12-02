a56d6adec17d51fcebee4d6b1191fe16d0dbd4eb
Rafael Fernández López <ereslibre@kde.org>
2007-12-02 23:52:02 +0000

    Ooops
    svn path=/trunk/KDE/kdelibs/; revision=744219

--- kdeui/itemviews/kcategorizedsortfilterproxymodel_p.h
+++ kdeui/itemviews/kcategorizedsortfilterproxymodel_p.h
@@ -17,9 +17,6 @@
   * along with this library; see the file COPYING.LIB.  If not, write to
   * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
   * Boston, MA 02110-1301, USA.
-  *
-  * ATTENTION: This code is based on the class QSortFilterProxyModel created
-  *            by Trolltech ASA.
   */
 
 #ifndef KCATEGORIZEDSORTFILTERPROXYMODEL_P_H
