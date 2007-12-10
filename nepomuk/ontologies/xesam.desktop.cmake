5bc455b42de65938bf22efb52138d5e88e5e5fe9
Sebastian Trueg <sebastian@trueg.de>
2007-12-10 12:35:41 +0000

    Updated ontologies. Added Xesam.
    svn path=/trunk/KDE/kdelibs/; revision=746847

---
+++ nepomuk/ontologies/xesam.desktop.cmake
@@ -0,0 +1,8 @@
+[Desktop Entry]
+Version=1.0
+Name=The Xesam meta data ontology
+Comment=The Xesam meta data ontology provides classes and properties for desktop file meta data
+URL=http://freedesktop.org/standards/xesam/1.0/core#
+Path=${DATA_INSTALL_DIR}/nepomuk/ontologies/xesam.rdf
+MimeType=application/rdf+xml;
+Type=Data
---
+++ nepomuk/ontologies/xss.desktop.cmake
@@ -0,0 +1,8 @@
+[Desktop Entry]
+Version=1.0
+Name=The Xesam Schema ontology
+Comment=
+URL=http://freedesktop.org/standards/xesam/1.0/schema#
+Path=${DATA_INSTALL_DIR}/nepomuk/ontologies/xss.rdf
+MimeType=application/rdf+xml;
+Type=Data
