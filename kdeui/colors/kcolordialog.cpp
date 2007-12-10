05a700be27b80d83f32dd320e414138d91032d13
Urs Wolfer <uwolfer@kde.org>
2007-12-10 18:29:12 +0000

    improve icon size (fix my commit from yesterday; thanks Matthew for the comment) CCMAIL: mwoehlke@tibco.com
    svn path=/trunk/KDE/kdelibs/; revision=746960

--- kdeui/colors/kcolordialog.cpp
+++ kdeui/colors/kcolordialog.cpp
@@ -1155,8 +1155,6 @@ KColorDialog::KColorDialog( QWidget *parent, bool modal )
   button->setIcon( KIcon("color-picker"));
   int commonHeight = addButton->sizeHint().height();
   button->setFixedSize( commonHeight, commonHeight );
-  //kDebug() << commonHeight;
-  button->setIconSize(QSize(commonHeight, commonHeight));
   l_hbox->addWidget(button, 0, Qt::AlignHCenter );
   connect( button, SIGNAL( clicked()), SLOT( slotColorPicker()));
 
