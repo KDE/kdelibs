fc683220d05021c177e36f894c4a0dd0e1697be4
Germain Garand <germain@ebooksfrance.org>
2007-12-20 23:53:17 +0000

    .restrict post-painting update of the cursor position to situations where  scrolling really occured or we might get a feedback loop. .make it safer by going through the event loop (as suggested  by Maksim ages ago)
    BUG: 154216
    
    svn path=/trunk/KDE/kdelibs/; revision=751054

--- khtml/khtmlview.cpp
+++ khtml/khtmlview.cpp
@@ -852,11 +852,10 @@ void KHTMLView::paintEvent( QPaintEvent *e )
     khtml::DrawContentsEvent event( &p, ex, ey, ew, eh );
     QApplication::sendEvent( m_part, &event );
 
-    if (d->scrollingSelf || d->contentsMoving || r.contains(widget()->mapFromGlobal(QCursor::pos()))) {
+    if (d->contentsMoving && widget()->underMouse()) {
         QMouseEvent *tempEvent = new QMouseEvent( QEvent::MouseMove, widget()->mapFromGlobal( QCursor::pos() ),
                                               Qt::NoButton, Qt::NoButton, Qt::NoModifier );
-        mouseMoveEvent( tempEvent );
-        delete tempEvent;
+        QApplication::postEvent(widget(), tempEvent);
     }
 
     d->painting = false;
