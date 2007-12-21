806ce4521a15f558349232be51e121e84e332212
Tom Albers <toma@kde.org>
2007-12-21 19:19:26 +0000

    RELICENSE: GPL2->GPL2+3
    svn path=/trunk/KDE/kdelibs/; revision=751388

--- kioslave/http/kcookiejar/kcookiejar.h
+++ kioslave/http/kcookiejar/kcookiejar.h
@@ -3,9 +3,10 @@
 
     Copyright (C) 1998 Waldo Bastian (bastian@kde.org)
 
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU General Public License
-    version 2 as published by the Free Software Foundation.
+    This library is free software; you can redistribute it and/or 
+    modify it under the terms of the GNU Library General Public License 
+    as published by the Free Software Foundation; either 
+    version 2, or (at your option) version 3.
 
     This software is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
--- kioslave/http/kcookiejar/tests/kcookiejartest.cpp
+++ kioslave/http/kcookiejar/tests/kcookiejartest.cpp
@@ -4,8 +4,9 @@
     Copyright (C) 2004 Waldo Bastian (bastian@kde.org)
 
     This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU General Public License
-    version 2 as published by the Free Software Foundation.
+    modify it under the terms of the GNU Library General Public License
+    as published by the Free Software Foundation; either
+    version 2, or (at your option) version 3.
 
     This software is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
