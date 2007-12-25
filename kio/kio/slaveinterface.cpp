12034c715563908ca7fdc43b582362e6d6354ace
Maks Orlovich <maksim@kde.org>
2007-12-25 23:01:02 +0000

    Use a debug area, same as rest of file. I want to see my own, wrongly-not-using-debug-area-either debug output, not KIO stuff... :-)
    svn path=/trunk/KDE/kdelibs/; revision=752839

--- kio/kio/slaveinterface.cpp
+++ kio/kio/slaveinterface.cpp
@@ -314,7 +314,7 @@ bool SlaveInterface::dispatch( int _cmd, const QByteArray &rawdata )
         MetaData meta_data;
         stream >> meta_data;
         d->m_incomingMetaData += meta_data;
-        kDebug() << "incoming metadata now" << d->m_incomingMetaData
+        kDebug(7007) << "incoming metadata now" << d->m_incomingMetaData
                  << "\n newly arrived metadata is" << meta_data;
         emit metaData(meta_data);
         break;
