cecef0aaa24633a8cf812e804247a1a5764e2c67
Sebastian Trueg <sebastian@trueg.de>
2008-02-02 14:06:42 +0000

    * Updated NFO (we use Xesam but as nfo is in KDE 4.0 I leave it in) * Added NIE as we need it for Akonadi.
    svn path=/trunk/KDE/kdelibs/; revision=769967

---
+++ nepomuk/ontologies/nie.desktop.cmake
@@ -0,0 +1,8 @@
+[Desktop Entry]
+Version=1.0
+Name=Nepomuk Information Element Core Ontology
+Comment=Core ontology for the Nepomuk Information Element framework which provides a unified vocabulary for describing native resources available on the desktop.
+URL=http://www.semanticdesktop.org/ontologies/2007/01/19/nie#
+Path=${DATA_INSTALL_DIR}/nepomuk/ontologies/nie.trig
+MimeType=application/x-trig
+Type=Data
