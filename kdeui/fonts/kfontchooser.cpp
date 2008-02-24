06e3caca5aa600783c706b4267fef5a7851ff325
Thomas McGuire <mcguire@kde.org>
2008-02-24 13:29:26 +0000

    Additionally, don't dereference the pointer if it is null.
    svn path=/trunk/KDE/kdelibs/; revision=778731

--- kdeui/fonts/kfontchooser.cpp
+++ kdeui/fonts/kfontchooser.cpp
@@ -598,7 +598,9 @@ void KFontChooser::Private::_k_family_chosen_slot(const QString& family)
     QString currentFamily;
     if (family.isEmpty()) {
         Q_ASSERT( familyListBox->currentItem() );
-        currentFamily = qtFamilies[familyListBox->currentItem()->text()];
+        if (familyListBox->currentItem()) {
+          currentFamily = qtFamilies[familyListBox->currentItem()->text()];
+        }
     }
     else {
         currentFamily = qtFamilies[family];
