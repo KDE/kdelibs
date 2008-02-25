e458f006ae42d6f6f7dcf92a99760aa780cf1e65
Thierry Bastian <thierry.bastian@trolltech.com>
2008-02-25 12:30:14 +0000

    added QT_BEGIN_NAMESPACE/QT_END_NAMESPACE for Qt 4.4 compatibility
    svn path=/trunk/KDE/kdelibs/; revision=779063

--- phonon/phonondefs_p.h
+++ phonon/phonondefs_p.h
@@ -131,6 +131,8 @@ QMetaObject::invokeMethod(m_backendObject, methodName, Qt::DirectConnection, Q_A
 #define pBACKEND_CALL2(methodName, varType1, var1, varType2, var2) \
 QMetaObject::invokeMethod(m_backendObject, methodName, Qt::DirectConnection, Q_ARG(varType1, var1), Q_ARG(varType2, var2))
 
+QT_BEGIN_NAMESPACE
+
 namespace Phonon
 {
     namespace
@@ -222,6 +224,8 @@ namespace Phonon
         const Type *const iface;
     };
 } // namespace Phonon
+
+QT_END_NAMESPACE
 
 #define INTERFACE_CALL(function) \
 Iface<PHONON_INTERFACENAME >::cast(d)->function
