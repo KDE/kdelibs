5733a2d2493500708c31fb36c585784a6e7e4d85
Matthias Kretz <kretz@kde.org>
2008-03-01 00:29:35 +0000

    some dox work
    svn path=/trunk/KDE/kdelibs/; revision=780792

--- phonon/audiooutputinterface.h
+++ phonon/audiooutputinterface.h
@@ -1,5 +1,5 @@
 /*  This file is part of the KDE project
-    Copyright (C) 2007 Matthias Kretz <kretz@kde.org>
+    Copyright (C) 2007-2008 Matthias Kretz <kretz@kde.org>
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Library General Public
@@ -31,7 +31,6 @@ namespace Phonon
 /** \class AudioOutputInterface audiooutputinterface.h Phonon/AudioOutputInterface
  * \short Interface for AudioOutput objects
  *
- * \ingroup Backend
  * \author Matthias Kretz <kretz@kde.org>
  */
 class AudioOutputInterface
@@ -39,10 +38,35 @@ class AudioOutputInterface
     public:
         virtual ~AudioOutputInterface() {}
 
+        /**
+         * Returns the current software volume.
+         *
+         * A value of 0.0 means muted, 1.0 means unchanged, 2.0 means double voltage (i.e. all
+         * samples are multiplied by 2).
+         */
         virtual qreal volume() const = 0;
+        /**
+         * Sets the new current software volume.
+         *
+         * A value of 0.0 means muted, 1.0 means unchanged, 2.0 means double voltage (i.e. all
+         * samples are multiplied by 2).
+         */
         virtual void setVolume(qreal) = 0;
 
+        /**
+         * Returns the index of the device that is used. The index is the number returned from
+         * BackendInterface::objectDescriptionIndexes(AudioOutputDeviceType).
+         */
         virtual int outputDevice() const = 0;
+        /**
+         * Requests to change the current output device to the one identified by the passed index.
+         *
+         * The index is the number returned from
+         * BackendInterface::objectDescriptionIndexes(AudioOutputDeviceType).
+         *
+         * \returns \c true if the requested device works and is used after this call.
+         * \returns \c false if something failed and the device is not used after this call.
+         */
         virtual bool setOutputDevice(int) = 0;
 };
 } // namespace Phonon
--- phonon/effectinterface.h
+++ phonon/effectinterface.h
@@ -1,5 +1,5 @@
 /*  This file is part of the KDE project
-    Copyright (C) 2006 Matthias Kretz <kretz@kde.org>
+    Copyright (C) 2006-2008 Matthias Kretz <kretz@kde.org>
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Library General Public
@@ -29,18 +29,29 @@ QT_BEGIN_NAMESPACE
 namespace Phonon
 {
     class EffectParameter;
-    /**
+    /** \class EffectInterface effectinterface.h Phonon/EffectInterface
+     * \short Interface for Effect objects
+     *
      * \author Matthias Kretz <kretz@kde.org>
      */
     class EffectInterface
     {
         public:
             virtual ~EffectInterface() {}
+            /**
+             * Returns the EffectParameter objects to describe the parameters of this effect.
+             */
             virtual QList<EffectParameter> parameters() const = 0;
+            /**
+             * Returns the value for the selected parameter.
+             */
             virtual QVariant parameterValue(const EffectParameter &) const = 0;
+            /**
+             * Sets the value for the selected parameter.
+             */
             virtual void setParameterValue(const EffectParameter &, const QVariant &newValue) = 0;
     };
-} //namespace Phonon::Fake
+} //namespace Phonon
 
 Q_DECLARE_INTERFACE(Phonon::EffectInterface, "EffectInterface0.phonon.kde.org")
 
