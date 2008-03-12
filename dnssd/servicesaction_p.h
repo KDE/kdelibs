7a652df96ac4fb74d792ec70494dd24905b73c61
Jakub Stachowski <qbast@go2.pl>
2008-03-12 21:28:31 +0000

    Upload forgotten file
    svn path=/trunk/KDE/kdelibs/; revision=784957

---
+++ dnssd/servicesaction_p.h
@@ -0,0 +1,48 @@
+/* This file is part of the KDE project
+ *
+ * Copyright (C) 2008 Jakub Stachowski <qbast@go2.pl>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifndef DNSSDSERVICESACTION_P_H
+#define DNSSDSERVICESACTION_P_H
+
+#include <QtCore/QObject>
+
+namespace DNSSD
+{
+
+class ServicesActionPrivate : public QObject
+{
+Q_OBJECT
+
+public:
+	ServicesActionPrivate(ServiceBrowser* browser, ServicesAction* parent=0);
+	~ServicesActionPrivate();
+
+        ServiceBrowser* m_browser;
+	QMenu* m_menu;
+	ServicesAction* m_parent;
+public slots:
+	void serviceSelected();
+	void serviceAdded(DNSSD::RemoteService::Ptr srv);
+	void serviceRemoved(DNSSD::RemoteService::Ptr srv);
+};
+
+}
+
+#endif
