3ec7c4e3dc5c701aac94bd7ad7936f8fb5bad8e3
Jakob Petsovits <jpetso@gmx.at>
2008-03-23 15:31:18 +0000

    Spelling fixes.
    svn path=/trunk/KDE/kdelibs/; revision=789201

--- kdeui/util/ksystemtrayicon.h
+++ kdeui/util/ksystemtrayicon.h
@@ -44,9 +44,9 @@ class QAction;
  * commands, including "Minimize/Restore" and "Quit".
  *
  * Please note that QSystemTrayIcon::showMessage(..) should not be
- * used for KDE appliaction because the popup message has no KDE standard
+ * used for KDE application because the popup message has no KDE standard
  * look & feel and cannot be controlled by KDE configurations. 
- * Use Knotify or KPassivePopup instead.
+ * Use KNotify or KPassivePopup instead.
  *
  * @author Matthias Ettrich <ettrich@kde.org>
  **/
