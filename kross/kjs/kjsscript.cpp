c738fea9b8799cda0180bfc552ad1b70859ec76c
Sebastian Sauer <mail@dipe.org>
2008-03-26 22:57:26 +0000

    * publish ourself and the manager too like at the other backends. * fix another assert by handling the err.
    svn path=/trunk/KDE/kdelibs/; revision=790559

--- kross/kjs/kjsscript.cpp
+++ kross/kjs/kjsscript.cpp
@@ -191,6 +191,10 @@ bool KjsScript::initialize()
     kjsinterpreter->setShouldPrintExceptions(true);
     KJS::ExecState* exec = kjsinterpreter->globalExec();
 
+    // publish our own action and the manager
+    d->publishObject(exec, "self", action());
+    d->publishObject(exec, "Kross", &Manager::self());
+
     d->m_defaultFunctionNames = functionNames();
     d->m_defaultFunctionNames << "Kross";
 
@@ -289,7 +293,13 @@ void KjsScript::execute()
     }
 
     KJS::JSObject* kjsglobal = kjsinterpreter->globalObject();
-    Q_ASSERT( ! exec->hadException() );
+    if( exec->hadException() ) {
+        ErrorInterface error = extractError(d->m_engine->completion(), exec);
+        exec->clearException();
+        krossdebug(QString("KjsScript::execute() failed: %1").arg(error.errorMessage()));
+        setError(&error);
+        return;
+    }
 
     foreach(QObject* object, d->m_autoconnect) {
         const QMetaObject* metaobject = object->metaObject();
@@ -416,7 +426,7 @@ QVariant KjsScript::callFunction(const QString& name, const QVariantList& args)
         return QVariant();
     }
 
-    QVariant result = KJSEmbed::convertToVariant(exec, retValue);
+    QVariant result = retValue ? KJSEmbed::convertToVariant(exec, retValue) : QVariant();
     Q_ASSERT( ! exec->hadException() );
     return result;
 }
