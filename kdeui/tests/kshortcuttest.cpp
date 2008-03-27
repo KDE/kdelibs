7e03359893b541b5d9a66197a7c893e96b8575cb
David Faure <faure@kde.org>
2008-03-27 23:16:57 +0000

    KDE eats the 'e' key because Qt4 can't parse "Win+E" anymore, indeed. But the fault lies in khotkeys kconf_update scripts.
    svn path=/trunk/KDE/kdelibs/; revision=790998

--- kdeui/tests/kshortcuttest.cpp
+++ kdeui/tests/kshortcuttest.cpp
@@ -1,29 +1,67 @@
-#include "kshortcuttest.h"
+/* This file is part of the KDE libraries
+    Copyright 2006 Simon Hausmann <hausmann@kde.org>
+    Copyright 2008 David Faure <faure@kde.org>
 
-void tst_KShortcut::isEmpty()
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301, USA.
+*/
+
+#include "qtest_kde.h"
+#include "kshortcut.h"
+
+class tst_KShortcut : public QObject
 {
-    KShortcut cut;
-    QVERIFY(cut.isEmpty());
+    Q_OBJECT
+private Q_SLOTS:
+    void isEmpty()
+    {
+        KShortcut cut;
+        QVERIFY(cut.isEmpty());
 
-    cut = KShortcut(0, 0);
-    QVERIFY(cut.isEmpty());
+        cut = KShortcut(0, 0);
+        QVERIFY(cut.isEmpty());
 
-    cut = KShortcut(QKeySequence());
-    QVERIFY(cut.isEmpty());
+        cut = KShortcut(QKeySequence());
+        QVERIFY(cut.isEmpty());
 
-    cut = KShortcut(QKeySequence(), QKeySequence());
-    QVERIFY(cut.isEmpty());
+        cut = KShortcut(QKeySequence(), QKeySequence());
+        QVERIFY(cut.isEmpty());
 
-    cut = KShortcut(QList<QKeySequence>());
-    QVERIFY(cut.isEmpty());
+        cut = KShortcut(QList<QKeySequence>());
+        QVERIFY(cut.isEmpty());
 
-    cut = KShortcut(Qt::Key_A);
-    QVERIFY(!cut.isEmpty());
+        cut = KShortcut(Qt::Key_A);
+        QVERIFY(!cut.isEmpty());
+    }
 
-    cut = KShortcut(";, Alt+;; ;, Alt+A, ;");
-    QVERIFY(cut.primary() == QKeySequence::fromString(";, Alt+;"));
-    QVERIFY(cut.alternate() == QKeySequence::fromString(";, Alt+A, ;"));
-}
+    void parsing()
+    {
+        KShortcut cut;
+        cut = KShortcut(";, Alt+;; ;, Alt+A, ;");
+        QVERIFY(cut.primary() == QKeySequence::fromString(";, Alt+;"));
+        QVERIFY(cut.alternate() == QKeySequence::fromString(";, Alt+A, ;"));
+
+        // The famous "KDE4 eats my E key" bug: Win+E isn't parsed anymore.
+        cut = KShortcut("Win+E");
+        QVERIFY(cut.primary()[0] == Qt::Key_E);
+
+        cut = KShortcut("Meta+E");
+        QVERIFY(cut.primary()[0] == Qt::META | Qt::Key_E);
+
+    }
+};
 
 QTEST_KDEMAIN(tst_KShortcut, NoGUI)
 
