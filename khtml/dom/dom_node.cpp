17f129be840010b70caf52979dab2a3c21cdc45b
Maks Orlovich <maksim@kde.org>
2008-04-01 20:41:43 +0000

    automatically merged revision 792683: Properly close DOM-created nodes. Fixes CNN.com election tracker, and 3 Acid3 tests (more like 3.5, actually --- gets one further). It also makes Acid3 look uglier, since it exposes a bug in rendering of alternate content (it didn't show up before since the object was plain not  getting loaded). I know what's wrong just need to <strike>Bug dfaure for a solution</strike>figure out how to fix it.
    CCBUG:156947
    
    svn path=/trunk/KDE/kdelibs/; revision=792687

--- khtml/dom/dom_node.cpp
+++ khtml/dom/dom_node.cpp
@@ -252,7 +252,6 @@ Node Node::insertBefore( const Node &newChild, const Node &refChild )
     NodeImpl *r = impl->insertBefore( newChild.impl, refChild.impl, exceptioncode );
     if (exceptioncode)
 	throw DOMException(exceptioncode);
-    if (!newChild.impl->closed()) newChild.impl->close();
     return r;
 }
 
@@ -262,9 +261,7 @@ Node Node::replaceChild( const Node &newChild, const Node &oldChild )
     int exceptioncode = 0;
     impl->replaceChild( newChild.impl, oldChild.impl, exceptioncode );
     if (exceptioncode)
-	throw DOMException(exceptioncode);
-    if (newChild.impl && !newChild.impl->closed()) newChild.impl->close();
-    
+	throw DOMException(exceptioncode);    
     return oldChild;
 }
 
@@ -286,7 +283,6 @@ Node Node::appendChild( const Node &newChild )
     NodeImpl *r = impl->appendChild( newChild.impl, exceptioncode );
     if (exceptioncode)
 	throw DOMException(exceptioncode);
-    if (!newChild.impl->closed()) newChild.impl->close();
     return r;
 }
 
