f4095497dfaf7d9d36eb334dfeee68ae4b6dc287
Matthias Kretz <kretz@kde.org>
2008-04-03 15:29:43 +0000

    make room for branches/work/phonon-threadsafe-mediastream
    svn path=/trunk/KDE/kdelibs/; revision=793306

--- phonon/experimental/tests/methods/audiodataoutput.cpp
+++
@@ -1,8 +0,0 @@
-addSignal("dataReady(const QMap<Phonon::Experimental::AudioDataOutput::Channel, QVector<qint16>>&)");
-addMethod("int", "sampleRate()");
-addMethod("", "setFormat(Phonon::Experimental::AudioDataOutput::Format)");
-addMethod("Phonon::Experimental::AudioDataOutput::Format", "format()");
-addSignal("endOfMedia(int)");
-addMethod("int", "dataSize()");
-addSignal("dataReady(const QMap<Phonon::Experimental::AudioDataOutput::Channel, QVector<float>>&)");
-addMethod("", "setDataSize(int)");
--- phonon/experimental/tests/methods/visualization.cpp
+++
@@ -1,4 +0,0 @@
-addMethod("", "setVisualization(int)");
-addMethod("", "setVideoOutput(QObject*)");
-addMethod("", "setAudioPath(QObject*)");
-addMethod("int", "visualization()");
