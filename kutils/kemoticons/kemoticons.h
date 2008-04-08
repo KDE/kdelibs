9d2830415413e0374cde4d15e1cae499899ea2a1
Carlo Segato <brandon.ml@gmail.com>
2008-04-08 10:10:43 +0000

    add some basics prototype
    svn path=/trunk/playground/libs/emoticons/; revision=794676

---
+++ kutils/kemoticons/kemoticons.h
@@ -0,0 +1,45 @@
+/***************************************************************************
+ *   Copyright (C) 2008 by Carlo Segato <brandon.ml@gmail.com>             *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA            *
+ ***************************************************************************/
+
+#ifndef KEMOTICONS_H
+#define KEMOTICONS_H
+
+#include "kemoticons_export.h"
+#include <QtCore/QObject>
+
+class KEMOTICONS_EXPORT KEmoticons : public QObject
+{
+    Q_OBJECT
+    public:
+        KEmoticons();
+        ~KEmoticons();
+        
+        KEmoticonsTheme getTheme();
+        KEmoticonsTheme getTheme(QString name);
+        
+        void setTheme(const KEmoticonsTheme &theme);
+        void setTheme(const QString &theme);
+        
+        KEmoticonsTheme newTheme(const QString &name);
+        
+};
+
+#endif /* KEMOTICONS_H */
+
+// kate: space-indent on; indent-width 4; replace-tabs on;
---
+++ kutils/kemoticons/kemoticonstheme.h
@@ -0,0 +1,47 @@
+/***************************************************************************
+ *   Copyright (C) 2008 by Carlo Segato <brandon.ml@gmail.com>             *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA            *
+ ***************************************************************************/
+
+#ifndef KEMOTICONS_THEME_H
+#define KEMOTICONS_THEME_H
+
+#include "kemoticons_export.h"
+#include "kemoticonstheme_p.h"
+
+class KEMOTICONS_EXPORT KEmoticonsTheme
+{   
+    public:
+        KEmoticonsTheme(const QString &theme);
+        virtual ~KEmoticonsTheme();
+        virtual bool removeEmoticon(const QString &emo);
+        virtual bool addEmoticon(const QString &emo, const QString &text, bool copy);
+        virtual void save();
+        
+        QString parseEmoticons(const QString &text);
+        
+    protected:
+        virtual void loadTheme();
+    
+    private:
+        KEmoticonsThemePrivate const *d;
+        
+};
+
+#endif /* KEMOTICONS_THEME_H */
+
+// kate: space-indent on; indent-width 4; replace-tabs on;
