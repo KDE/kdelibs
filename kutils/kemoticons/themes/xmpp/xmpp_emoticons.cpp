487ae8f4e936538712677139bcc200ed12bba6b0
Carlo Segato <brandon.ml@gmail.com>
2008-04-11 16:22:03 +0000

    added xmpp emoticons theme
    svn path=/trunk/playground/libs/emoticons/; revision=795854

---
+++ kutils/kemoticons/themes/xmpp/xmpp_emoticons.cpp
@@ -0,0 +1,173 @@
+/***************************************************************************
+ *   Copyright (C) 2008 by Carlo Segato <brandon.ml@gmail.com>             *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA            *
+ ***************************************************************************/
+
+#include "xmpp_emoticons.h"
+
+#include <QtCore/QFile>
+#include <QtCore/QFileInfo>
+#include <QtGui/QImageReader>
+
+#include <KPluginFactory>
+#include <KDebug>
+#include <KStandardDirs>
+#include <KMimeType>
+
+K_PLUGIN_FACTORY(XmppEmoticonsFactory, registerPlugin<XmppEmoticons>();)
+K_EXPORT_PLUGIN(XmppEmoticonsFactory("XmppEmoticons"))
+
+XmppEmoticons::XmppEmoticons(QObject *parent, const QVariantList &args)
+    : KEmoticonsTheme(parent, args)
+{
+}
+
+XmppEmoticons::~XmppEmoticons()
+{
+}
+
+bool XmppEmoticons::removeEmoticon(const QString &emo)
+{
+    QString emoticon = QFileInfo(d->m_themeMap.key(emo.split(" "))).fileName();
+    QDomElement fce = m_themeXml.firstChildElement("icondef");
+
+    if (fce.isNull())
+        return false;
+
+    QDomNodeList nl = fce.childNodes();
+    for (uint i = 0; i < nl.length(); i++) {
+        QDomElement de = nl.item(i).toElement();
+        if (!de.isNull() && de.tagName() == "icon") {
+            QDomNodeList snl = de.childNodes();
+            QStringList sl;
+            QStringList mime;
+
+            for (uint k = 0; k < snl.length(); k++) {
+                QDomElement sde = snl.item(k).toElement();
+
+                if (!sde.isNull() && sde.tagName() == "object" && sde.text() == emoticon) {
+                    fce.removeChild(de);
+                    d->m_themeMap.remove(d->m_themeMap.key(emo.split(" ")));
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
+
+bool XmppEmoticons::addEmoticon(const QString &emo, const QString &text, bool copy)
+{
+    KEmoticonsTheme::addEmoticon(emo, text, copy);
+
+    QStringList splitted = text.split(" ");
+    QDomElement fce = m_themeXml.firstChildElement("icondef");
+
+    if (fce.isNull()) {
+        return false;
+    }
+
+    QDomElement emoticon = m_themeXml.createElement("icon");
+    fce.appendChild(emoticon);
+    QStringList::const_iterator constIterator;
+
+    for (constIterator = splitted.begin(); constIterator != splitted.end(); constIterator++) {
+        QDomElement emotext = m_themeXml.createElement("text");
+        QDomText txt = m_themeXml.createTextNode((*constIterator).trimmed());
+        emotext.appendChild(txt);
+        emoticon.appendChild(emotext);
+    }
+
+    QDomElement emoElement = m_themeXml.createElement("object");
+    KMimeType::Ptr mimePtr = KMimeType::findByPath(emo, 0, true);
+    emoElement.setAttribute("mime", mimePtr->name());
+    QDomText txt = m_themeXml.createTextNode(QFileInfo(emo).fileName());
+
+    emoElement.appendChild(txt);
+    emoticon.appendChild(emoElement);
+
+    d->m_themeMap[emo] = splitted;
+    return true;
+}
+
+void XmppEmoticons::save()
+{
+    QFile fp(d->m_themePath + '/' + d->m_fileName);
+
+    if (!fp.exists()) {
+        kWarning() << fp.fileName() << "doesn't exist!";
+        return;
+    }
+
+    if (!fp.open( QIODevice::WriteOnly)) {
+        kWarning() << fp.fileName() << "can't open WriteOnly!";
+        return;
+    }
+
+    QTextStream emoStream(&fp);
+    emoStream << m_themeXml.toString(4);
+    fp.close();
+}
+
+bool XmppEmoticons::loadTheme(const QString &path)
+{
+    KEmoticonsTheme::loadTheme(path);
+
+    QDomElement fce = m_themeXml.firstChildElement("icondef");
+
+    if (fce.isNull()) {
+        return false;
+    }
+
+    QDomNodeList nl = fce.childNodes();
+
+    d->m_themeMap.clear();
+
+    for (uint i = 0; i < nl.length(); i++) {
+        QDomElement de = nl.item(i).toElement();
+
+        if(!de.isNull() && de.tagName() == "icon") {
+            QDomNodeList snl = de.childNodes();
+            QStringList sl;
+            QString emo;
+            QStringList mime;
+            mime << "image/png" << "image/gif" << "image/bmp" << "image/jpeg";
+
+            for (uint k = 0; k < snl.length(); k++) {
+                QDomElement sde = snl.item(k).toElement();
+
+                if(!sde.isNull() && sde.tagName() == "text") {
+                    sl << sde.text();
+                } else if(!sde.isNull() && sde.tagName() == "object" && mime.contains(sde.attribute("mime"))) {
+                    emo = sde.text();
+                }
+            }
+
+            emo = KGlobal::dirs()->findResource("emoticons", d->m_themeName + '/' + emo);
+
+            if (emo.isNull()) {
+                continue;
+            }
+
+            d->m_themeMap[emo] = sl;
+        }
+    }
+
+    return true;
+}
+
+// kate: space-indent on; indent-width 4; replace-tabs on;
---
+++ kutils/kemoticons/themes/xmpp/xmpp_emoticons.h
@@ -0,0 +1,44 @@
+/***************************************************************************
+ *   Copyright (C) 2008 by Carlo Segato <brandon.ml@gmail.com>             *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA            *
+ ***************************************************************************/
+
+#ifndef XMPP_EMOTICONS_H
+#define XMPP_EMOTICONS_H
+
+#include <kemoticonstheme.h>
+
+class XmppEmoticons : public KEmoticonsTheme
+{
+    Q_OBJECT
+    public:
+        XmppEmoticons(QObject *parent, const QVariantList &args);
+        ~XmppEmoticons();
+        
+        bool loadTheme(const QString &path);
+        
+        bool removeEmoticon(const QString &emo);
+        bool addEmoticon(const QString &emo, const QString &text, bool copy);
+        void save();
+    
+    private:
+        QDomDocument m_themeXml;
+};
+
+#endif /* XMPP_EMOTICONS_H */
+
+// kate: space-indent on; indent-width 4; replace-tabs on;
