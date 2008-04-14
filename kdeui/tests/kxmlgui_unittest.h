06da158b5edb6966b88092619cc91d23eecdbc95
David Faure <faure@kde.org>
2008-04-14 09:26:34 +0000

    Don't remove separator after action list, testcase by Joris Guisson, patch by Albert Astals Cid, unit test by me.
    svn path=/trunk/KDE/kdelibs/; revision=796756

--- kdeui/tests/kxmlgui_unittest.h
+++ kdeui/tests/kxmlgui_unittest.h
@@ -31,6 +31,7 @@ private Q_SLOTS:
     void testVersionHandlerNewVersionNothingKept();
     void testVersionHandlerNewVersionUserChanges();
     void testMergingSeparators();
+    void testActionListAndSeparator();
 };
 
 #endif
