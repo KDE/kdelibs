3d1308ae0d31be58e9b8742bc7f04d5d4a5c87f0
Maks Orlovich <maksim@kde.org>
2008-05-30 17:34:49 +0000

    Backport fix for #162837 CCBUG:162837
    svn path=/branches/KDE/4.0/kdelibs/; revision=814564

--- khtml/rendering/render_replaced.h
+++ khtml/rendering/render_replaced.h
@@ -27,6 +27,7 @@
 #include <QtCore/QObject>
 #include <QAbstractScrollArea>
 #include <QScrollBar>
+#include <QPointer>
 
 class KHTMLView;
 class QWidget;
@@ -147,7 +148,7 @@ protected:
 
     QWidget *m_widget;
     KHTMLView* m_view;
-    QWidget *m_underMouse;
+    QPointer<QWidget> m_underMouse;
 
     //Because we mess with normal detach due to ref/deref,
     //we need to keep track of the arena ourselves
