e71d39d60039267d6258077f1791bb3af40c2600
Maks Orlovich <maksim@kde.org>
2008-05-31 16:51:45 +0000

    Fix crash on asahi.com CCBUG:162797
    svn path=/trunk/KDE/kdelibs/; revision=814893

--- khtml/xml/dom_nodeimpl.cpp
+++ khtml/xml/dom_nodeimpl.cpp
@@ -2312,6 +2312,8 @@ NodeImpl::Id GenericRONamedNodeMapImpl::mapId(DOMStringImpl* namespaceURI,
 
 void RegisteredListenerList::addEventListener(EventName id, EventListener *listener, const bool useCapture)
 {
+    if (!listener)
+	return;
     RegisteredEventListener rl(id,listener,useCapture);
     if (!listeners)
         listeners = new QList<RegisteredEventListener>;
