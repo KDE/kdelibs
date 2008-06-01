db21f96f009b8610af11cabc47afbaf77e3c6693
Kevin Krammer <kevin.krammer@gmx.at>
2008-06-01 14:53:08 +0000

    APIDOX: fixed enum value in example code
    svn path=/trunk/KDE/kdelibs/; revision=815338

--- kdeui/dialogs/kconfigdialog.h
+++ kdeui/dialogs/kconfigdialog.h
@@ -50,7 +50,7 @@ class KConfigDialogManager;
  *   if(KConfigDialog::showDialog("settings"))
  *     return;
  *   KConfigDialog *dialog = new KConfigDialog(this, "settings", MySettings::self());
- *   dialog->setFaceType(KPageDialog::IconList);
+ *   dialog->setFaceType(KPageDialog::List);
  *   dialog->addPage(new General(0, "General"), i18n("General") );
  *   dialog->addPage(new Appearance(0, "Style"), i18n("Appearance") );
  *   connect(dialog, SIGNAL(settingsChanged(const QString&)), mainWidget, SLOT(loadSettings()));
