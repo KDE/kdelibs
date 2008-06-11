15c8755ff7ead0315096876ec39989c87afe6a07
Aurélien Gâteau <agateau@kde.org>
2008-06-11 21:00:17 +0000

    Always init mDefaultIconSize.
    Avoids returning crazy values in the (rare) case where kdelibs is installed
    but kdebase is not (Oxygen icon theme is in kdebase).
    CCMAIL: rivolaks@hot.ee
    
    svn path=/trunk/KDE/kdelibs/; revision=819720

--- kdeui/icons/kiconcache.cpp
+++ kdeui/icons/kiconcache.cpp
@@ -237,8 +237,12 @@ int KIconCache::defaultIconSize(KIconLoader::Group group) const
 
 void KIconCache::setThemeInfo(const QList<KIconTheme*>& themes)
 {
-    if (themes.isEmpty())
+    if (themes.isEmpty()) {
+        for (KIconLoader::Group i = KIconLoader::FirstGroup; i < KIconLoader::LastGroup; i++) {
+            d->mDefaultIconSize[i] = 0;
+        }
         return;
+    }
     // This as to be done always, even if the cache itself is disabled
     for (KIconLoader::Group i = KIconLoader::FirstGroup; i < KIconLoader::LastGroup; i++) {
         d->mDefaultIconSize[i] = themes.first()->defaultSize(i);
