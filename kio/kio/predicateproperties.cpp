4e64642ec064d6ee98f60bd7a8495d4bf00d0468
Jos van den Oever <jos@vandenoever.info>
2008-06-16 20:06:11 +0000

    Additional fix that limits the amount of data being read while doing a quick analysis of a file. As discussed on bugs.kde.org.
    CCBUG:163050 
    
    
    svn path=/trunk/KDE/kdelibs/; revision=821198

--- kio/kio/predicateproperties.cpp
+++ kio/kio/predicateproperties.cpp
@@ -20,6 +20,7 @@
 #include "kfilemetainfo_p.h"
 #include "kglobal.h"
 #include <strigi/streamanalyzer.h>
+#include <strigi/analysisresult.h>
 #include <strigi/analyzerconfiguration.h>
 #include <QDebug>
 using namespace Strigi;
@@ -112,9 +113,21 @@ PredicatePropertyProvider::getPredicateProperties(const QString& key) {
     return p;
 }
 
+class ShallowAnalysisConfiguration : public Strigi::AnalyzerConfiguration { 
+     /** Limit the amount of data we will read from a stream. 
+       * This is a suggestion to analyzers which they should follow. 
+       * Only index real files. We do not look at subfiles. 
+       * This setting is needed because by default all subfiles are examined. 
+       **/ 
+     int64_t maximalStreamReadLength(const Strigi::AnalysisResult& ar) { 
+         // 64k should be enough 
+         return (ar.depth() == 0) ?65536 :0; 
+     } 
+}; 
+
 class PredicatePropertyProvider::Private {
 public:
-    AnalyzerConfiguration config;
+    ShallowAnalysisConfiguration config;
     StreamAnalyzer indexer;
     Private() :indexer(config) {}
 };
