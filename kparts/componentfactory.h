16de8ffbc161b6f60d01928d9741f853d4fe56a7
Allen Winter <winter@kde.org>
2008-07-04 11:11:57 +0000

    Add the standard LGPLv2+ license header, approved by Simon. Give Simon copyright 2001.
    David, Alex, you also made substantial commits to this file
    so please add your copyright, if you want one.
    CCMAIL: hausmann@kde.org,faure@kde.org,huntedhacker@tiscali.co.uk
    
    svn path=/trunk/KDE/kdelibs/; revision=827997

--- kparts/componentfactory.h
+++ kparts/componentfactory.h
@@ -1,5 +1,23 @@
-#ifndef __kparts_componentfactory_h__
-#define __kparts_componentfactory_h__
+/* This file is part of the KDE project
+   Copyright (C) 2001 Simon Hausmann <hausmann@kde.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to the
+   Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+#ifndef KPARTS_COMPONENTFACTORY_H
+#define KPARTS_COMPONENTFACTORY_H
 
 #include <kparts/factory.h>
 #include <kparts/part.h>
@@ -12,8 +30,8 @@ namespace KParts
     namespace ComponentFactory
     {
         /**
-         * This template function allows to ask the given kparts factory to create an
-         * instance of the given template type.
+         * This template function allows to ask the given kparts factory to
+         * create an instance of the given template type.
          *
          * Example of usage:
          * \code
