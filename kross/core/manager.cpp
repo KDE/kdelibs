KDE/kdelibs

commit c5652a4f178de611359afea79dd0038c98208d69

lua is a supported backend now too :)
svn path=/trunk/KDE/kdelibs/; revision=834544

kross/core/manager.cpp
@@ -198,6 +198,18 @@ Manager::Manager()
     }
 #endif
 
+#ifdef KROSS_LUA_LIBRARY
+    if( void* funcPtr = loadLibrary(KROSS_LUA_LIBRARY, "krossinterpreter") ) {
+        d->interpreterinfos.insert("lua",
+            new InterpreterInfo("lua",
+                funcPtr, // library
+                "*.lua .luac", // file filter-wildcard
+                QStringList() << "application/x-lua" // mimetypes
+            )
+        );
+    }
+#endif
+
     // fill the list of supported interpreternames.
     QHash<QString, InterpreterInfo*>::Iterator it( d->interpreterinfos.begin() );
     for(; it != d->interpreterinfos.end(); ++it)

kross/core/krossconfig.h
@@ -83,6 +83,7 @@ namespace Kross {
     #define KROSS_JAVA_LIBRARY "libkrossjava"
     #define KROSS_FALCON_LIBRARY "krossfalcon"
     #define KROSS_QTSCRIPT_LIBRARY "krossqts"
+    #define KROSS_LUA_LIBRARY "kloss"
 
 }
 

commit 4a0406d17d13200436d75b8432270277ee846869

fix for a stupid typo introduced with r834544 :-/
svn path=/trunk/KDE/kdelibs/; revision=834713

kross/core/manager.cpp
@@ -203,7 +203,7 @@ Manager::Manager()
         d->interpreterinfos.insert("lua",
             new InterpreterInfo("lua",
                 funcPtr, // library
-                "*.lua .luac", // file filter-wildcard
+                "*.lua *.luac", // file filter-wildcard
                 QStringList() << "application/x-lua" // mimetypes
             )
         );