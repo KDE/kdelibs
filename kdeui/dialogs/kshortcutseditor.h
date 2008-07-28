53b15088eec8bacda57f2d1a27d5e8028ea7aeae
Urs Wolfer <uwolfer@kde.org>
2008-07-28 17:51:34 +0000

    SVN_SILENT revert unwanted change... Thanks to Michael Jansen for reviewing. CCMAIL:kde@michael-jansen.biz
    svn path=/trunk/KDE/kdelibs/; revision=838779

--- kdeui/dialogs/kshortcutseditor.h
+++ kdeui/dialogs/kshortcutseditor.h
@@ -6,7 +6,7 @@
     Copyright (C) 2007 Andreas Hartmetz <ahartmetz@gmail.com>
     Copyright (C) 2008 Michael Jansen <kde@michael-jansen.biz>
 
-    This library is free software; you can redistribute it and/orffffff
+    This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Library General Public
     License as published by the Free Software Foundation; either
     version 2 of the License, or (at your option) any later version.
