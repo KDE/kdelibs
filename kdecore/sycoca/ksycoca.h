8a1316454b8bf3336929e111e9ba6dd2470b7c98
Ralf Habacker <ralf.habacker@freenet.de>
2008-08-01 12:23:32 +0000

    reverted last change, it requires patching ksycoca.cpp first
    svn path=/trunk/KDE/kdelibs/; revision=840651

--- kdecore/sycoca/ksycoca.h
+++ kdecore/sycoca/ksycoca.h
@@ -38,6 +38,10 @@ class KSycocaPrivate;
 #define KSYCOCA_VERSION 116
 
 /**
+ * Sycoca file name, used internally (by kbuildsycoca)
+ */
+#define KSYCOCA_FILENAME "ksycoca4"
+/**
  * Executable name of the kbuildsycoca program
  */
 #define KBUILDSYCOCA_EXENAME "kbuildsycoca4"
