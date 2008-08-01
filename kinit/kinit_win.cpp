8add9dcde2edbce5ab8fc2d11db51c69976d4056
Ralf Habacker <ralf.habacker@freenet.de>
2008-08-01 08:29:42 +0000

    merged win32 fixes from trunk
    svn path=/branches/KDE/4.1/kdelibs/; revision=840463

--- kinit/kinit_win.cpp
+++ kinit/kinit_win.cpp
@@ -41,9 +41,10 @@
 #include <kapplication.h>
 #include <kdeversion.h>
 
+//#define ENABLE_SUICIDE 
+//#define ENABLE_EXIT
 
 #define KDED_EXENAME "kded4"
-#define KNOTIFY_EXENAME "knotify4"
 
 static KComponentData *s_instance = 0;
 
@@ -52,50 +53,6 @@ int verbose=0;
 
 /// holds process list for suicide mode
 QList<QProcess*> startedProcesses;
-
-// internal launch function
-int launch(const QString &cmd)
-{
-    QProcess *proc = new QProcess();
-    proc->start(cmd);
-    proc->waitForStarted();
-    startedProcesses << proc;
-    _PROCESS_INFORMATION* _pid = proc->pid();
-    int pid = _pid ? _pid->dwProcessId : 0;
-    if (verbose) {
-        fprintf(stderr,"%s",proc->readAllStandardError().constData());
-        fprintf(stderr,"%s",proc->readAllStandardOutput().constData());
-    }
-    if (pid) {
-       if (verbose)
-           fprintf(stderr, "kdeinit4: Launched %s, pid = %ld\n", qPrintable(cmd),(long) pid);
-    }
-    else {
-       if (verbose)
-           fprintf(stderr, "kdeinit4: could not launch %s, exiting",qPrintable(cmd));
-    }
-    return pid;
-}
-
-/// check dbus registration
-bool checkIfRegisteredInDBus(const QString &name, int _timeout=10)
-{
-    int timeout = _timeout * 5;
-    while(timeout) {
-    	if ( QDBusConnection::sessionBus().interface()->isServiceRegistered( name ) )
-    	    break;
-    	Sleep(200);
-    	timeout--;
-    }
-		if (!timeout) {
-			if (verbose)
-			    fprintf(stderr,"not registered %s in dbus after %d secs\n",qPrintable(name),_timeout);
-			return false;
-		}
-		if (verbose)
-		    fprintf(stderr,"%s is registered in dbus\n",qPrintable(name));
-    return true;
-}
 
 class ProcessListEntry {
     public:
@@ -111,7 +68,19 @@ class ProcessListEntry {
        QString path;
        int pid;
        HANDLE handle;
+       friend QDebug operator <<(QDebug out, const ProcessListEntry &c);
 };
+
+QDebug operator <<(QDebug out, const ProcessListEntry &c)
+{
+    out << "(ProcessListEntry" 
+        << "name" << c.name
+        << "path" << c.path
+        << "pid" << c.pid
+        << "handle" << c.handle
+        << ")";
+    return out;
+}    
 
 /**
  holds system process list
@@ -122,7 +91,7 @@ class ProcessList {
        ~ProcessList();
        ProcessListEntry *hasProcessInList(const QString &name);
        bool terminateProcess(const QString &name);
-       void dumpList();
+       QList<ProcessListEntry *> &list() { return processList; }
     private:
        void initProcessList();
        void getProcessNameAndID( DWORD processID );
@@ -141,6 +110,7 @@ void ProcessList::getProcessNameAndID( DWORD processID )
                                    false, processID );
 
     // Get the process name.
+    int ret;
 
     if (NULL != hProcess )
     {
@@ -150,15 +120,14 @@ void ProcessList::getProcessNameAndID( DWORD processID )
        if ( EnumProcessModules( hProcess, &hMod, sizeof(hMod),
               &cbNeeded) )
        {
-             GetModuleFileNameExA( hProcess, hMod, szProcessName,
+            ret = GetModuleFileNameExA( hProcess, hMod, szProcessName,
                                            sizeof(szProcessName)/sizeof(TCHAR) );
        }
     }
-
-    processList << new ProcessListEntry(hProcess,szProcessName,processID );
-
-    if (verbose)
-       fprintf(stderr,"%s    (PID: %u)\n", szProcessName, processID );
+    if (ret > 0)
+    {
+        processList << new ProcessListEntry(hProcess,szProcessName,processID );
+    }
 }
 
 
@@ -196,15 +165,6 @@ ProcessList::~ProcessList()
     }
 }
 
-
-void ProcessList::dumpList()
-{
-    ProcessListEntry *ple;
-    foreach(ple,processList) {
-        fprintf(stdout,"%s    (PID: %u)\n", ple->name.toLatin1().data(), ple->pid);
-    }
-}
-
 /**
  return process list entry of given name
 */
@@ -213,30 +173,137 @@ ProcessListEntry *ProcessList::hasProcessInList(const QString &name)
     ProcessListEntry *ple;
     foreach(ple,processList) {
         if (ple->name == name || ple->name == name + ".exe") {
-            if (verbose)
-                fprintf(stderr,"found %s with pid=%d\n",qPrintable(ple->name),ple->pid);
             return ple;
         }
     }
     return NULL;
 }
 
+/**
+ terminate process from process list
+*/
 bool ProcessList::terminateProcess(const QString &name)
 {
     ProcessListEntry *p = hasProcessInList(name);
-    if (p) {
-        int ret = TerminateProcess(p->handle,0) ? true : false;
-        if (verbose)
-            fprintf(stderr,"process %s terminated %d %d\n",qPrintable(name),ret,GetLastError());
-        return ret;
+    if (!p)
+        return false;
+    return TerminateProcess(p->handle,0) ? true : false;
+}
+
+// internal launch function
+int launch(const QString &cmd)
+{
+    QProcess *proc = new QProcess();
+    proc->start(cmd);
+    proc->waitForStarted();
+    startedProcesses << proc;
+    _PROCESS_INFORMATION* _pid = proc->pid();
+    int pid = _pid ? _pid->dwProcessId : 0;
+    if (verbose) {
+        fprintf(stderr,"%s",proc->readAllStandardError().constData());
+        fprintf(stderr,"%s",proc->readAllStandardOutput().constData());
+    }
+    if (pid) {
+       if (verbose)
+           fprintf(stderr, "kdeinit4: Launched %s, pid = %ld\n", qPrintable(cmd),(long) pid);
     }
     else {
        if (verbose)
-           fprintf(stderr,"process %s not found\n",qPrintable(name));
-       return false;
+           fprintf(stderr, "kdeinit4: could not launch %s, exiting",qPrintable(cmd));
+    }
+    return pid;
+}
+
+/// check dbus registration
+bool checkIfRegisteredInDBus(const QString &name, int _timeout=10)
+{
+    int timeout = _timeout * 5;
+    while(timeout) {
+        if ( QDBusConnection::sessionBus().interface()->isServiceRegistered( name ) )
+            break;
+        Sleep(200);
+        timeout--;
+    }
+        if (!timeout) {
+            if (verbose)
+                fprintf(stderr,"not registered %s in dbus after %d secs\n",qPrintable(name),_timeout);
+            return false;
+        }
+        if (verbose)
+            fprintf(stderr,"%s is registered in dbus\n",qPrintable(name));
+    return true;
+}
+
+void listAllRunningKDEProcesses(ProcessList &processList)
+{
+    ProcessListEntry *ple;
+    QString installPrefix = KStandardDirs::installPath("kdedir");
+
+    foreach(ple,processList.list()) 
+    {
+        if (ple->path.toLower().startsWith(installPrefix.toLower()))
+            fprintf(stderr,"path: %s name: %s pid: %u\n", ple->path.toLatin1().data(), ple->name.toLatin1().data(), ple->pid);
     }
 }
 
+void terminateAllRunningKDEProcesses(ProcessList &processList)
+{
+    ProcessListEntry *ple;
+    QString installPrefix = KStandardDirs::installPath("kdedir");
+
+    foreach(ple,processList.list()) 
+    {
+        if (ple->path.toLower().startsWith(installPrefix.toLower())) 
+        {
+            if (verbose)
+                fprintf(stderr,"terminating path: %s name: %s pid: %u\n", ple->path.toLatin1().data(), ple->name.toLatin1().data(), ple->pid);
+            processList.terminateProcess(ple->name);
+        }
+    }
+}
+
+void listAllNamedAppsInDBus()
+{
+    QDBusConnection connection = QDBusConnection::sessionBus();
+    QDBusConnectionInterface *bus = connection.interface();
+    const QStringList services = bus->registeredServiceNames();
+    foreach(const QString &service, services) {
+        if (service.startsWith("org.freedesktop.DBus") || service.startsWith(':'))
+            continue;
+        fprintf(stderr, "%s \n", service.toLatin1().data());
+    }
+}
+
+void quitApplicationsOverDBus()
+{
+    QDBusConnection connection = QDBusConnection::sessionBus();
+    QDBusConnectionInterface *bus = connection.interface();
+    const QStringList services = bus->registeredServiceNames();
+    foreach(const QString &service, services) {
+        if (service.startsWith("org.freedesktop.DBus") || service.startsWith(':'))
+            continue;
+        QDBusInterface *iface = new QDBusInterface(service,
+                               QLatin1String("/MainApplication"),
+//  see http://lists.kde.org/?l=kde-core-devel&m=121641642911291&w=2
+#if QT_VERSION < 0x040402
+                               QLatin1String(""),
+#else                             
+                               QLatin1String("org.kde.KApplication"),
+#endif
+                               connection);
+        if (!iface->isValid()) {
+            if (verbose)
+                fprintf(stderr, "invalid interface of service %s\n", service.toLatin1().data());
+            continue;
+        }
+        iface->call("quit");
+        if (iface->lastError().isValid()) {
+            if (verbose)
+                fprintf(stderr,"killing %s with result\n", iface->lastError().message().toLatin1().data());
+        }
+        delete iface;
+    }
+}
 
 int main(int argc, char **argv, char **envp)
 {
@@ -245,9 +312,11 @@ int main(int argc, char **argv, char **envp)
     bool launch_klauncher = true;
     bool launch_kded = true;
     bool suicide = false;
-//    bool terminate_kde = false;       // unused atm
-
-    ProcessList processList;
+    bool listProcesses = false;
+    bool killProcesses = false;
+    bool listAppsInDBus = false;
+    bool quitAppsOverDBus = false;
+    bool shutdown = false;
 
     /** Save arguments first... **/
     char **safe_argv = (char **) malloc( sizeof(char *) * argc);
@@ -262,40 +331,70 @@ int main(int argc, char **argv, char **envp)
             launch_kded = false;
         if (strcmp(safe_argv[i], "--suicide") == 0)
             suicide = true;
-//        if (strcmp(safe_argv[i], "--exit") == 0)
-//            keep_running = 0;
+#ifdef ENABLE_EXIT
+        if (strcmp(safe_argv[i], "--exit") == 0)
+            keep_running = 0;
+#endif            
         if (strcmp(safe_argv[i], "--verbose") == 0)
             verbose = 1;
         if (strcmp(safe_argv[i], "--help") == 0)
         {
            printf("Usage: kdeinit4 [options]\n");
-           printf("       --no-dbus              Do not start dcopserver\n");
-           printf("       --no-klauncher         Do not start klauncher\n");
-           printf("       --no-kded              Do not start kded\n");
-           printf("       --suicide              Terminate when no KDE applications are left running\n");
-           printf("       --terminate            Terminate all running kde processes\n");
-           printf("       --verbose              print verbose messages\n");
-           printf("       --list                 list system process for which the user has the right to terminate\n");
-      // printf("       --exit                    Terminate when kded has run\n");
+#ifdef ENABLE_EXIT
+           printf("   --exit                     Terminate when kded has run\n");
+#endif
+           printf("   --help                     this help page\n");
+           printf("   --list                     list kde processes\n");
+           printf("   --list-dbus-apps           list all applications registered in dbus\n");
+           printf("   --quit-over-dbus           quit all application registered in dbus\n");
+           printf("   --no-dbus                  do not start dbus-daemon\n");
+           printf("   --no-klauncher             do not start klauncher\n");
+           printf("   --no-kded                  do not start kded\n");
+           printf("   --shutdown                 safe shutdown of all running kde processes\n");
+           printf("                              first over dbus, then using hard kill\n");
+#ifdef ENABLE_SUICIDE
+           printf("    --suicide                 terminate when no KDE applications are left running\n");
+#endif
+           printf("   --terminate                hard kill of *all* running kde processes\n");
+           printf("   --verbose                  print verbose messages\n");
            exit(0);
         }
-        if (strcmp(safe_argv[i], "--list") == 0) {
-            processList.dumpList();
-            exit(0);
-        }
-        if (strcmp(safe_argv[i], "--terminate") == 0) {
-            QStringList appList;
-            appList << KDED_EXENAME << KNOTIFY_EXENAME << "nepomukdaemon" 
-                      << "kuiserver"  << "kioslave" << "klauncher" << "dbus-daemon";
-
-            foreach(QString app, appList)
-                processList.terminateProcess(app);
-            exit(0);
-        }
+        if (strcmp(safe_argv[i], "--list") == 0)
+            listProcesses = true;
+        if (strcmp(safe_argv[i], "--shutdown") == 0)
+            shutdown = true;
+        if (strcmp(safe_argv[i], "--terminate") == 0 || strcmp(safe_argv[i], "--kill") == 0)
+            killProcesses = true;
+        if (strcmp(safe_argv[i], "--list-dbus-apps") == 0)
+            listAppsInDBus = true;
+        if (strcmp(safe_argv[i], "--quit-over-dbus") == 0)
+            quitAppsOverDBus = true;
     }
 
-    /** Make process group leader (for shutting down children later) **/
+    ProcessList processList;
 
+    if (listProcesses) {
+        listAllRunningKDEProcesses(processList);
+        return 0;
+    }
+    else if (killProcesses) {
+        terminateAllRunningKDEProcesses(processList);
+        return 0;
+    }
+    else if (listAppsInDBus) {
+        listAllNamedAppsInDBus();
+        return 0;
+    }
+    else if (quitAppsOverDBus) {
+        quitApplicationsOverDBus();
+        return 0;
+    }
+    else if (shutdown) {
+        quitApplicationsOverDBus();
+        Sleep(2000);
+        terminateAllRunningKDEProcesses(processList);
+    }
+    
     /** Create our instance **/
     s_instance = new KComponentData("kdeinit4", QByteArray(), KComponentData::SkipMainComponentRegistration);
 
@@ -347,6 +446,7 @@ int main(int argc, char **argv, char **envp)
     free (safe_argv);
 
     /** wait for termination of all (core) processes */
+#ifdef ENABLE_SUICIDE
     if (suicide) {
         QProcess *proc;
         int can_exit=1;
@@ -360,5 +460,6 @@ int main(int argc, char **argv, char **envp)
         } while(!can_exit);
         return 0;
     }
+#endif    
     return 0;
 }
