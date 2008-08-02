ff9fd6813d2dc5466cc0c6517081bb5dfbae0351
Oswald Buddenhagen <ossi@kde.org>
2008-08-02 08:14:47 +0000

    don't break on trivial case :}
    svn path=/trunk/KDE/kdelibs/; revision=840913

--- kdecore/util/kshell_unix.cpp
+++ kdecore/util/kshell_unix.cpp
@@ -122,9 +122,14 @@ QStringList KShell::splitArgs( const QString &args, Options flags, Errors *err)
             {
                 int pos2 = pos;
                 QChar cc;
-                do
+                do {
+                    if (pos2 >= args.length()) {
+                        // Exactly one word
+                        ret += args.mid(pos - 1);
+                        goto okret;
+                    }
                     cc = args[pos2++];
-                while (cc == QLatin1Char('_') ||
+                } while (cc == QLatin1Char('_') ||
                        (cc >= QLatin1Char('A') && cc <= QLatin1Char('Z')) ||
                        (cc >= QLatin1Char('a') && cc <= QLatin1Char('z')) ||
                        (cc >= QLatin1Char('0') && cc <= QLatin1Char('9')));
