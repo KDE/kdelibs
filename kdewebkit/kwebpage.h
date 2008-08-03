c394bec8a5d5c3b57bed93e7844ea10ac33f0d0f
Urs Wolfer <uwolfer@kde.org>
2008-08-03 21:34:16 +0000

    Separation of KDE and WebKit KPart stuff: introduction of KWebView and KWebPage. These classes can also be used in other places of KDE where the KPart would not be useful. Based on a patch by Michael Howell, thanks! CCMAIL:mhowell123@gmail.com
    svn path=/trunk/playground/libs/webkitkde/kwebpage.h; revision=841695

---
+++ kdewebkit/kwebpage.h
@@ -0,0 +1,58 @@
+/*
+ * This file is part of the KDE project.
+ *
+ * Copyright (C) 2008 Dirk Mueller <mueller@kde.org>
+ * Copyright (C) 2008 Urs Wolfer <uwolfer @ kde.org>
+ * Copyright (C) 2008 Michael Howell <mhowell123@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ *
+ */
+#ifndef KWEBPAGE_H
+#define KWEBPAGE_H
+
+#include <kdemacros.h>
+
+#include <qwebpage.h>
+
+class QWebFrame;
+
+class KDE_EXPORT KWebPage : public QWebPage
+{
+    Q_OBJECT
+public:
+    KWebPage(QObject *parent);
+    virtual ~KWebPage();
+
+protected:
+    virtual QString chooseFile(QWebFrame *frame, const QString &suggestedFile);
+    virtual void javaScriptAlert(QWebFrame *frame, const QString &msg);
+    virtual bool javaScriptConfirm(QWebFrame *frame, const QString &msg);
+    virtual bool javaScriptPrompt(QWebFrame *frame, const QString &msg, const QString &defaultValue, QString *result);
+    virtual QString userAgentForUrl(const QUrl& url) const;
+
+    virtual QObject *createPlugin(const QString &classid, const QUrl &url, const QStringList &paramNames, const QStringList &paramValues);
+
+protected Q_SLOTS:
+    void slotHandleUnsupportedContent(QNetworkReply *reply);
+    void slotDownloadRequested(const QNetworkRequest &request);
+
+private:
+    class KWebPagePrivate;
+    KWebPagePrivate* const d;
+};
+
+#endif // KWEBPAGE_H
