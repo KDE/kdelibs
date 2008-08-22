e216f0720d6682fc2edae44ef169e4614dce7fff
Maks Orlovich <maksim@kde.org>
2008-08-22 17:37:56 +0000

    Backport bugfixes from trunk: r846699 by Matthias Grimrath --- fixes extra - in parsed comments r842096 by Vir --- fixes assert-fail on parsing of some SSL-related commands
    svn path=/branches/KDE/4.1/kdelibs/; revision=850972

--- khtml/html/htmltokenizer.cpp
+++ khtml/html/htmltokenizer.cpp
@@ -513,7 +513,10 @@ void HTMLTokenizer::parseComment(TokenizerString &src)
             {
                 if ( scriptCodeSize > 2 && scriptCode[scriptCodeSize-3] == '-' &&
                      scriptCode[scriptCodeSize-2] == '-' )
+                {
+                    scriptCodeSize -= 1;
                     scriptEnd=true;
+                }
             }
 
             if (canClose || handleBrokenComments || scriptEnd ){
@@ -1444,7 +1447,7 @@ void HTMLTokenizer::parseTag(TokenizerString &src)
             else if ( !brokenScript && tagID == ID_SCRIPT ) {
                 DOMStringImpl* a = 0;
                 bool foundTypeAttribute = false;
-                scriptSrc = scriptSrcCharset = QString();
+                scriptSrc.clear(); scriptSrcCharset.clear();
                 if ( currToken.attrs && /* potentially have a ATTR_SRC ? */
                      view &&  /* are we a regular tokenizer or just for innerHTML ? */
                      parser->doc()->view()->part()->jScriptEnabled() /* jscript allowed at all? */
