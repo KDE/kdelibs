023bd186946322d686bd78a1bad5ecac33a22426
Rafael Fernández López <ereslibre@kde.org>
2008-08-28 14:18:48 +0000

    Use a different curve shape. Use the same we do use on plasma
    svn path=/trunk/KDE/kdelibs/; revision=853959

--- kdeui/widgets/kfadewidgeteffect.cpp
+++ kdeui/widgets/kfadewidgeteffect.cpp
@@ -169,6 +169,7 @@ KFadeWidgetEffect::KFadeWidgetEffect(QWidget *destWidget)
     setGeometry(QRect(destWidget->mapTo(parentWidget(), QPoint(0, 0)), destWidget->size()));
     d->oldPixmap = QPixmap::grabWidget(destWidget);
     d->timeLine.setFrameRange(0, 255);
+    d->timeLine.setCurveShape(QTimeLine::EaseOutCurve);
     connect(&d->timeLine, SIGNAL(finished()), SLOT(finished()));
     connect(&d->timeLine, SIGNAL(frameChanged(int)), SLOT(repaint()));
     show();
