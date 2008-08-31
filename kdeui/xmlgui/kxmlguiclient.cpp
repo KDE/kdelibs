0e35e49834058cb67e9f72a25bd37b060e3f9ac1
Rafael Fernández López <ereslibre@kde.org>
2008-08-31 19:38:31 +0000

    Be strict with invalid XML files. They cause unexpected visual outputs on applications, and a message on the console is probably not the best way to give such a warning. Make the application assert.
    CCMAIL: kde-core-devel@kde.org
    
    svn path=/trunk/KDE/kdelibs/; revision=855438

--- kdeui/xmlgui/kxmlguiclient.cpp
+++ kdeui/xmlgui/kxmlguiclient.cpp
@@ -229,7 +229,9 @@ void KXMLGUIClient::setXML( const QString &document, bool merge )
     QDomDocument doc;
     QString errorMsg;
     int errorLine, errorColumn;
-    if (doc.setContent(document, &errorMsg, &errorLine, &errorColumn)) {
+    bool result = doc.setContent(document, &errorMsg, &errorLine, &errorColumn);
+    Q_ASSERT_X( result, "setXML", "The XML file that was set to KXMLGUIClient is invalid. Please check its correctness with xmllint" );
+    if ( result ) { // kept for release mode
         setDOMDocument( doc, merge );
     } else {
         kError() << "Error parsing XML document:" << errorMsg << "at line" << errorLine << "column" << errorColumn;
