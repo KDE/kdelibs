468e310cbfc7fc5add5041268180989efbcbf547
Maks Orlovich <maksim@kde.org>
2008-09-12 00:47:36 +0000

    Tighten up the response parsing to not get confused and out-of-sync quite so easily. Fixes #169556 for 4.1 branch, trunk has a better fix, with a new header parser..
    svn path=/branches/KDE/4.1/kdelibs/; revision=860101

--- kioslave/http/http.cpp
+++ kioslave/http/http.cpp
@@ -2753,7 +2753,7 @@ try_again:
     // calling application later
     m_responseHeaders << QString::fromLatin1(buf);
 
-    if ((strncasecmp(buf, "HTTP", 4) == 0) ||
+    if ((strncasecmp(buf, "HTTP/", 5) == 0) ||
         (strncasecmp(buf, "ICY ", 4) == 0)) // Shoutcast support
     {
       if (strncasecmp(buf, "ICY ", 4) == 0)
