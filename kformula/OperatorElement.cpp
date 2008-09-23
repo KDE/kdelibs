324f5e4a8fc279fb94f6f2c8f2ba3628566a759d
John Tapsell <john.tapsell@kdemail.net>
2008-09-23 21:06:49 +0000

    Remove debug line - opps
    svn path=/trunk/KDE/kdelibs/; revision=864072

--- kformula/OperatorElement.cpp
+++ kformula/OperatorElement.cpp
@@ -40,7 +40,6 @@ QRectF OperatorElement::renderToPath( const QString& raw, QPainterPath& path, co
     qreal lSpace = am->parseMathSpace(dict.lSpace(), this);
     path.moveTo( path.currentPosition() + QPointF( lSpace, 0.0 ) );
     QFont font = am->font(this);
-    font.setStretch(500);
     path.addText( path.currentPosition(), font, raw );
     QFontMetricsF fm(font);
     return path.boundingRect().adjusted(0,0,lSpace+rSpace,0);
