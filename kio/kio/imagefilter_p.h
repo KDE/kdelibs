0e572afe5d559ebaa11fef6a16e79a19522475ae
Fredrik Höglund <fredrik@kde.org>
2008-10-03 15:15:31 +0000

    Make this function exported so we can use it in libkfile.
    svn path=/trunk/KDE/kdelibs/; revision=867468

--- kio/kio/imagefilter_p.h
+++ kio/kio/imagefilter_p.h
@@ -35,11 +35,13 @@
 #ifndef KIO_IMAGEFILTER_P_H
 #define KIO_IMAGEFILTER_P_H
 
+#include <kio/global.h>
+
 class QImage;
 class QColor;
 namespace KIO {
 
-class ImageFilter
+class KIO_EXPORT ImageFilter
 {
 public:
     // Blurs the alpha channel of the image and recolors it to the specified color.
