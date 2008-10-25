d328cd3503fdbd83642e9cd8dc0e6477472c94ab
Alexander Neundorf <neundorf@kde.org>
2008-10-25 16:24:44 +0000

    the kdelibs dashboard is now at my.cdash.org
    Alex
    
    
    
    svn path=/trunk/KDE/kdelibs/; revision=875795

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,7 +1,13 @@
+## This file should be placed in the root directory of your project.
+## Then modify the CMakeLists.txt file in the root directory of your
+## project to incorporate the testing dashboard.
+## # The following are required to uses Dart and the Cdash dashboard
+##   ENABLE_TESTING()
+##   INCLUDE(Dart)
 set(CTEST_PROJECT_NAME "kdelibs")
 set(CTEST_NIGHTLY_START_TIME "00:00:00 EST")
 
 set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "www.cdash.org")
-set(CTEST_DROP_LOCATION "/CDash/submit.php?project=kdelibs")
+set(CTEST_DROP_SITE "my.cdash.org")
+set(CTEST_DROP_LOCATION "/submit.php?project=kdelibs")
 set(CTEST_DROP_SITE_CDASH TRUE)
