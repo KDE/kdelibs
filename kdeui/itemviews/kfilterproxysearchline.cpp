495be0b6d43cbc144958e089ee74a6c3bac922a3
Tom Albers <toma@kde.org>
2008-11-01 22:26:52 +0000

    GPL->LGPL and remove debug.
    svn path=/trunk/KDE/kdelibs/; revision=878851

--- kdeui/itemviews/kfilterproxysearchline.cpp
+++ kdeui/itemviews/kfilterproxysearchline.cpp
@@ -1,20 +1,21 @@
-/* This file is part of the KDE project
-   Copyright (C) 2007 Omat Holding B.V. <info@omat.nl>
-
-   This program is free software; you can redistribute it and/or
-   modify it under the terms of the GNU General Public
-   License as published by the Free Software Foundation; either
-   version 2 of the License, or (at your option) any later version.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
-*/
+/*
+ * Copyright (C) 2007-2008 Omat Holding B.V. <info@omat.nl>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
 
 #include "kfilterproxysearchline.h"
 
@@ -25,7 +26,6 @@
 
 #include <klineedit.h>
 #include <klocale.h>
-#include <kdebug.h>
 
 /**
  * Private class that helps to provide binary compatibility between releases.
@@ -64,7 +64,6 @@ void KFilterProxySearchLine::Private::slotSearchLineActivate()
     proxy->setFilterKeyColumn( -1 );
     proxy->setFilterCaseSensitivity( Qt::CaseInsensitive );
     proxy->setFilterRegExp( searchLine->text() );
-    kDebug() << "Setting filter to " << searchLine->text();
 }
 //@endcond
 
