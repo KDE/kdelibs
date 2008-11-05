1da22f1fa73a97ef0e17f7f1099d375c6c0bdbeb
Michael Jansen <kde@michael-jansen.biz>
2008-11-05 00:24:16 +0000

    When deleting actions unregister session shortcuts with kdedglobalaccel instead of setting them inactive.
    svn path=/trunk/KDE/kdelibs/; revision=880246

--- kdeui/actions/kaction.cpp
+++ kdeui/actions/kaction.cpp
@@ -119,7 +119,11 @@ KAction::~KAction()
     if (d->globalShortcutEnabled) {
         // - remove the action from KGlobalAccel
         d->globalShortcutEnabled = false;
-        KGlobalAccel::self()->d->remove(this, KGlobalAccelPrivate::SetInactive);
+        if (this->objectName().startsWith("_k_session:")) {
+            KGlobalAccel::self()->d->remove(this, KGlobalAccelPrivate::UnRegister);
+        } else {
+            KGlobalAccel::self()->d->remove(this, KGlobalAccelPrivate::SetInactive);
+        }
     }
 
     KGestureMap::self()->removeGesture(d->shapeGesture, this);
