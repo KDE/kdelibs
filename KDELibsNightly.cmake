b455b2398f9ed1c903079af85f7167350c6de01d
Alexander Neundorf <neundorf@kde.org>
2008-11-09 20:36:33 +0000

    -add ctest script files to make running nightly builds easier
    Alex
    
    
    svn path=/trunk/KDE/kdelibs/; revision=882117

---
+++ KDELibsNightly.cmake
@@ -0,0 +1,31 @@
+#############################
+# for cmake 2.6.[0..2]:
+if(NOT CMAKE_SYSTEM)
+   include(CMakeDetermineSystem)
+   if(CMAKE_HOST_UNIX)
+      include(Platform/UnixPaths)
+   endif(CMAKE_HOST_UNIX)
+   if(CMAKE_HOST_WIN32)
+      include(Platform/WindowsPaths)
+   endif(CMAKE_HOST_WIN32)
+endif(NOT_CMAKE_SYSTEM)
+
+#############################
+
+set(CTEST_UPDATE_TYPE svn)
+set(SVN_REPOSITORY https://neundorf@svn.kde.org/home/kde/trunk/KDE/kdelibs )
+set(CTEST_CMAKE_GENERATOR "Unix Makefiles" )
+
+# this is default, so it is not necessary
+# set(CTEST_AVOID_SPACES FALSE)
+
+
+include (${CTEST_SCRIPT_DIRECTORY}/cmake/modules/KDE4CTestNightlySetup.cmake)
+
+ctest_empty_binary_directory("${CTEST_BINARY_DIRECTORY}")
+ctest_start(Experimental)
+ctest_update(SOURCE "${CTEST_SOURCE_DIRECTORY}" )
+ctest_configure(BUILD "${CTEST_BINARY_DIRECTORY}" )
+ctest_build(BUILD "${CTEST_BINARY_DIRECTORY}" )
+# ctest_test(BUILD "${CTEST_BINARY_DIRECTORY}" )
+ctest_submit()
