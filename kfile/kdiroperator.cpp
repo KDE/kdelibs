481544431e5c4189b6f297f67683a0340d6b0e60
David Faure <faure@kde.org>
2008-11-28 11:19:53 +0000

    'Toggle hidden files' takes a good 3 seconds here (too many zsh history files in my home), so better show a busy cursor during the emitChanges call. I'll also optimize it so that it doesn't take that long anymore, this commit is just for better user feedback.
    svn path=/trunk/KDE/kdelibs/; revision=890032

--- kfile/kdiroperator.cpp
+++ kfile/kdiroperator.cpp
@@ -1081,7 +1081,9 @@ void KDirOperator::setUrl(const KUrl& _newurl, bool clearforward)
 
 void KDirOperator::updateDir()
 {
+    QApplication::setOverrideCursor(Qt::WaitCursor);
     d->dirLister->emitChanges();
+    QApplication::restoreOverrideCursor();
 }
 
 void KDirOperator::rereadDir()
