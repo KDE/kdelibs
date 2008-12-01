b6d473e4ec3535c48aaaf37193f458409cf6fb01
Alexis Ménard <menard@kde.org>
2008-12-01 10:19:22 +0000

    Allow developpers that start using 4.5 to run applications with the new option added in Qt. Usefull for Qt developers to test their stuff with KDE.
    Plasma is for instance faster with raster engine.
    
    Ok by dfaure
    
    svn path=/trunk/KDE/kdelibs/; revision=891126

--- kdecore/kernel/kcmdlineargs.cpp
+++ kdecore/kernel/kcmdlineargs.cpp
@@ -301,7 +301,9 @@ KCmdLineArgsStatic::KCmdLineArgsStatic () {
 #endif
     qt_options.add("reverse", ki18n("mirrors the whole layout of widgets"));
     qt_options.add("stylesheet <file.qss>", ki18n("applies the Qt stylesheet to the application widgets"));
-
+#if QT_VERSION >= 0x040500
+    qt_options.add("graphicssystem <system>", ki18n("use a different graphics system instead of the default one, options are raster and opengl (experimental)"));
+#endif
     // KDE options
     kde_options.add("caption <caption>",   ki18n("Use 'caption' as name in the titlebar"));
     kde_options.add("icon <icon>",         ki18n("Use 'icon' as the application icon"));
