18aca9bd5f37a4366ff55d977c273c21f6753a04
Josef Spillner <spillner@kde.org>
2008-12-30 13:20:48 +0000

    - convert all licence header sections to LGPLv2.1+ template from techbase - other related consistency improvements so all KNS2 licence headers look the same, except for the authors part
    svn path=/trunk/KDE/kdelibs/; revision=903327

--- knewstuff/knewstuff2/core/coreengine.h
+++ knewstuff/knewstuff2/core/coreengine.h
@@ -1,22 +1,20 @@
 /*
     This file is part of KNewStuff2.
     Copyright (c) 2007 Josef Spillner <spillner@kde.org>
-    Copyright 2007 Frederik Gladhorn <frederik.gladhorn@kdemail.net>
+    Copyright (c) 2007 Frederik Gladhorn <frederik.gladhorn@kdemail.net>
 
     This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Library General Public
+    modify it under the terms of the GNU Lesser General Public
     License as published by the Free Software Foundation; either
-    version 2 of the License, or (at your option) any later version.
+    version 2.1 of the License, or (at your option) any later version.
 
     This library is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Library General Public License for more details.
+    Lesser General Public License for more details.
 
-    You should have received a copy of the GNU Library General Public License
-    along with this library; see the file COPYING.LIB.  If not, write to
-    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-    Boston, MA 02110-1301, USA.
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library.  If not, see <http://www.gnu.org/licenses/>.
 */
 #ifndef KNEWSTUFF2_COREENGINE_H
 #define KNEWSTUFF2_COREENGINE_H
--- knewstuff/knewstuff2/ui/itemsview.cpp
+++ knewstuff/knewstuff2/ui/itemsview.cpp
@@ -1,23 +1,22 @@
-/***************************************************************************
- *   Copyright (C) 2005 by Enrico Ros <eros.kde@email.it>                  *
- *   Copyright (C) 2005 - 2007 Josef Spillner <spillner@kde.org>           *
- *             (C) 2007 Dirk Mueller <mueller@kde.org>                     *
- *                                                                         *
- *   This library is free software; you can redistribute it and/or         *
- *   modify it under the terms of the GNU Lesser General Public            *
- *   License as published by the Free Software Foundation; either          *
- *   version 2 of the License, or (at your option) any later version.      *
- *                                                                         *
- *   This library is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
- *   Lesser General Public License for more details.                       *
- *                                                                         *
- *   You should have received a copy of the GNU Lesser General Public      *
- *   License along with this library; if not, write to the                 *
- *   Free Software Foundation, Inc.,                                       *
- *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA          *
- ***************************************************************************/
+/*
+    This file is part of KNewStuff2.
+    Copyright (C) 2005 by Enrico Ros <eros.kde@email.it>
+    Copyright (C) 2005 - 2007 Josef Spillner <spillner@kde.org>
+    Copyright (C) 2007 Dirk Mueller <mueller@kde.org>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 // own include
 #include "itemsview.h"
