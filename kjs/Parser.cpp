88e337b50fbd59562ee86fb4f1514605bffb68d8
Sebastian Sauer <mail@dipe.org>
2009-01-11 22:31:34 +0000

    compile also with KJS_VERBOSE defined
    svn path=/trunk/KDE/kdelibs/; revision=909625

--- kjs/Parser.cpp
+++ kjs/Parser.cpp
@@ -123,7 +123,7 @@ void Parser::parse(const UString& sourceURL, int startingLineNumber,
     }
 
 #ifdef KJS_VERBOSE
-  fprintf( stderr, "%s\n", prog->toString().ascii() );
+  fprintf( stderr, "%s\n", m_progNode->toString().ascii() );
 #endif
 }
 
