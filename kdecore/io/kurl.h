9dd6502eb4bfaa722845a02deadcaa64635403d3
David Faure <faure@kde.org>
2009-02-10 21:09:34 +0000

    Ooops, I meant it this way round, thanks for noticing, Gof.
    svn path=/trunk/KDE/kdelibs/; revision=924471

--- kdecore/io/kurl.h
+++ kdecore/io/kurl.h
@@ -1012,8 +1012,8 @@ public:
    *
    * If you are looking for the notion of "relative path" (foo) vs "absolute path" (/foo),
    * use QUrl::isRelative() instead.
-   * Indeed, isRelativeUrl() returns false for the string "/foo" since it doesn't contain a protocol,
-   * while KUrl("/foo").isRelative() is true since the KUrl constructor turns it into file:///foo.
+   * Indeed, isRelativeUrl() returns true for the string "/foo" since it doesn't contain a protocol,
+   * while KUrl("/foo").isRelative() is false since the KUrl constructor turns it into file:///foo.
    * The two methods basically test the same thing, but this one takes a string (which is faster)
    * while the class method requires a QUrl/KUrl which gives a more expected result, given
    * the "magic" in the KUrl constructor.
