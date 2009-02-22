2fb2b3ccafb59a86d5a1e43943d700de090ce8c4
Till Adam <adam@kde.org>
2009-02-22 12:57:31 +0000

    Re-add the timezone reworking, now that the drop is out, so I can fix it on Windows.
    svn path=/branches/kdepim/enterprise4/kdelibs-4.1-branch/; revision=929946

--- kdecore/date/ksystemtimezone.cpp
+++ kdecore/date/ksystemtimezone.cpp
@@ -325,6 +325,45 @@ void KSystemTimeZonesPrivate::cleanup()
 
 #ifdef Q_OS_WIN
 
+
+static const TCHAR timeZonesKey[] = TEXT("SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Time Zones");
+static const int MAX_KEY_LENGTH = 255;
+
+// TCHAR can be either uchar, or wchar_t:
+static inline QString tchar_to_qstring( TCHAR * ustr ) {
+    const char * str = reinterpret_cast<const char*>( ustr );
+    return QString::fromLocal8Bit( str );
+}
+static inline QString tchar_to_qstring( const wchar_t * str ) {
+    return QString::fromWCharArray( str );
+}
+
+static QStringList list_key( HKEY key ) {
+
+    DWORD numSubKeys = 0;
+    QStringList result;
+
+    if ( RegQueryInfoKey( key, 0, 0, 0, &numSubKeys, 0, 0, 0, 0, 0, 0, 0 ) == ERROR_SUCCESS )
+        for ( DWORD i = 0 ; i < numSubKeys ; ++i ) {
+            TCHAR name[MAX_KEY_LENGTH+1];
+            DWORD nameLen = MAX_KEY_LENGTH;
+            if ( RegEnumKeyEx( key, i, name, &nameLen, 0, 0, 0, 0 ) == ERROR_SUCCESS )
+                result.push_back( tchar_to_qstring( name ) );
+        }
+
+    return result;
+}
+
+static QStringList list_time_zones() {
+
+    HKEY timeZones;
+    if ( RegOpenKeyEx( HKEY_LOCAL_MACHINE, timeZonesKey, 0, KEY_READ, &timeZones ) != ERROR_SUCCESS )
+        return QStringList();
+    const HKeyCloser closer( timeZones );
+    return list_key( timeZones );
+}
+
+
 void KSystemTimeZonesPrivate::updateTimezoneInformation(bool update)
 {
     if (!m_source)
