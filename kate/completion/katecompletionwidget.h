b461e29f0092a55ce1e3452c6b2a19b4ee3c35af
David Nolden <david.nolden.kde@art-master.de>
2009-02-24 22:35:40 +0000

    Backport r931189: Fix a potential crash that could happen when a completion-model was destroyed during running code-completion.
    svn path=/branches/KDE/4.2/kdelibs/; revision=931190

--- kate/completion/katecompletionwidget.h
+++ kate/completion/katecompletionwidget.h
@@ -141,6 +141,7 @@ class KateCompletionWidget : public QFrame
 //    virtual void focusInEvent ( QFocusEvent * event );
 
   private Q_SLOTS:
+    void modelDestroyed(QObject* model);
     void modelContentChanged();
     void cursorPositionChanged();
     void editDone(KateEditInfo* edit);
