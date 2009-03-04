af948559eb4facc6b19b7ca32b4a95a46d41283f
David Faure <faure@kde.org>
2009-03-04 20:42:14 +0000

    SVN_SILENT license header
    svn path=/trunk/KDE/kdelibs/; revision=935328

--- kdecore/sycoca/ksycoca_p.h
+++ kdecore/sycoca/ksycoca_p.h
@@ -1,3 +1,23 @@
+/*  This file is part of the KDE libraries
+ *  Copyright (C) 1999-2000 Waldo Bastian <bastian@kde.org>
+ *  Copyright (C) 2005-2009 David Faure <faure@kde.org>
+ *  Copyright (C) 2008 Hamish Rodda <rodda@kde.org>
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Library General Public
+ *  License version 2 as published by the Free Software Foundation;
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Library General Public License
+ *  along with this library; see the file COPYING.LIB.  If not, write to
+ *  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ *  Boston, MA 02110-1301, USA.
+ **/
+
 #ifndef KSYCOCA_P_H
 #define KSYCOCA_P_H
 
