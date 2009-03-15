b945702414c7ccfedad987f42b917a9e22574d41
Andrew Coles <andrew.i.coles@googlemail.com>
2009-03-15 01:39:40 +0000

    In lieu of a BIC breaking change, ignore the consequential warnings on gcc for something entirely harmless.
    svn path=/trunk/KDE/kdelibs/; revision=939518

--- threadweaver/Weaver/JobCollection.h
+++ threadweaver/Weaver/JobCollection.h
@@ -81,6 +81,11 @@ namespace ThreadWeaver {
         /** Return a reference to the job in the job list at position i. */
         Job* jobAt( int i );
 
+#ifdef __GNUC__
+#if __GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ > 1)
+#pragma GCC diagnostic ignored "-Wignored-qualifiers"
+#endif
+#endif
         /** Return the number of jobs in the joblist. */
         const int jobListLength(); // const qualifier is possibly BiC?
 
