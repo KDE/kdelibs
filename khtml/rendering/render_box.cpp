KDE/kdelibs

commit bb49335e041121be0421ffb7fb0b00e770406721

Consider the real root background box when computing some background properties - not the one that is extended on the whole canvas.
Patch based on work by Anatoli Papirovski <apapirovski mac dot com>

BUG: 169608

svn path=/trunk/KDE/kdelibs/; revision=855521

khtml/rendering/render_box.cpp
@@ -575,8 +575,21 @@ void RenderBox::paintBackgroundExtended(QPainter *p, const QColor &c, const Back
                 }
             }
 
-            int pw = w - hpab;
-            int ph = h - vpab;
+            int pw, ph;
+            if (isRoot()) {
+                // the root's background box 'spills out' to cover the whole canvas, so we have to
+                // go back to its true edge for the purpose of computing background-size
+                // and honouring background-origin
+                pw = width() - hpab;
+                ph = height() - vpab; 
+                left += marginLeft();
+                hpab += marginLeft() + marginRight();
+                vpab += marginTop() + marginBottom();
+                top += marginTop();
+            } else {
+                pw = w - hpab;
+                ph = h - vpab;
+            }
             scaledImageWidth = pw;
             scaledImageHeight = ph;
             calculateBackgroundSize(bgLayer, scaledImageWidth, scaledImageHeight);

commit 0618001a85c7ef6b956e69c36f5d9259a2942a51

automatically merged revision 944739: fix regression in sizing of the background image for the root element.
this regressed in r855521, as I didn't quite understood all the subtleties
of the background size for the root case.

BUG: 187285

svn path=/branches/KDE/4.2/kdelibs/; revision=944745

khtml/rendering/render_box.cpp
@@ -579,6 +579,8 @@ void RenderBox::paintBackgroundExtended(QPainter *p, const QColor &c, const Back
     if (shouldPaintBackgroundImage) {
         int sx = 0;
         int sy = 0;
+        int rw = 0;
+        int rh = 0;
         int cw,ch;
         int cx,cy;
         int scaledImageWidth, scaledImageHeight;
@@ -602,28 +604,34 @@ void RenderBox::paintBackgroundExtended(QPainter *p, const QColor &c, const Back
             }
 
             int pw, ph;
+            pw = w - hpab;
+            ph = h - vpab;
             if (isRoot()) {
                 // the root's background box 'spills out' to cover the whole canvas, so we have to
                 // go back to its true edge for the purpose of computing background-size
                 // and honouring background-origin
-                pw = width() - hpab;
-                ph = height() - vpab; 
+                rw = width() - hpab;
+                rh = height() - vpab; 
                 left += marginLeft();
                 hpab += marginLeft() + marginRight();
                 vpab += marginTop() + marginBottom();
                 top += marginTop();
+                scaledImageWidth = rw;
+                scaledImageHeight = rh;
             } else {
-                pw = w - hpab;
-                ph = h - vpab;
+                scaledImageWidth = pw;
+                scaledImageHeight = ph;
             }
-            scaledImageWidth = pw;
-            scaledImageHeight = ph;
             calculateBackgroundSize(bgLayer, scaledImageWidth, scaledImageHeight);
 
             EBackgroundRepeat bgr = bgLayer->backgroundRepeat();
             if (bgr == NO_REPEAT || bgr == REPEAT_Y) {
                 cw = scaledImageWidth;
-                int xPosition = bgLayer->backgroundXPosition().minWidth(pw-scaledImageWidth);
+                int xPosition;
+                if (isRoot())
+                    xPosition = bgLayer->backgroundXPosition().minWidth(rw-scaledImageWidth);
+                else
+                    xPosition = bgLayer->backgroundXPosition().minWidth(pw-scaledImageWidth);
                 if ( xPosition >= 0 ) {
                     cx = _tx + xPosition;
                     cw = qMin(scaledImageWidth, pw - xPosition);
@@ -641,14 +649,22 @@ void RenderBox::paintBackgroundExtended(QPainter *p, const QColor &c, const Back
                 cw = w;
                 cx = _tx;
                 if (scaledImageWidth > 0) {
-                    int xPosition = bgLayer->backgroundXPosition().minWidth(pw-scaledImageWidth);
+                    int xPosition;
+                    if (isRoot())
+                        xPosition = bgLayer->backgroundXPosition().minWidth(rw-scaledImageWidth);
+                    else
+                        xPosition = bgLayer->backgroundXPosition().minWidth(pw-scaledImageWidth);
                     sx = scaledImageWidth - (xPosition % scaledImageWidth);
                     sx -= left % scaledImageWidth;
                 }
             }
             if (bgr == NO_REPEAT || bgr == REPEAT_X) {
                 ch = scaledImageHeight;
-                int yPosition = bgLayer->backgroundYPosition().minWidth(ph - scaledImageHeight);
+                int yPosition;
+                if (isRoot())
+                    yPosition = bgLayer->backgroundYPosition().minWidth(rh - scaledImageHeight);
+                else
+                    yPosition = bgLayer->backgroundYPosition().minWidth(ph - scaledImageHeight);
                 if ( yPosition >= 0 ) {
                     cy = _ty + yPosition;
                     ch = qMin(ch, ph - yPosition);
@@ -667,7 +683,11 @@ void RenderBox::paintBackgroundExtended(QPainter *p, const QColor &c, const Back
                 ch = h;
                 cy = _ty;
                 if (scaledImageHeight > 0) {
-                    int yPosition = bgLayer->backgroundYPosition().minWidth(ph - scaledImageHeight);
+                    int yPosition;
+                    if (isRoot())
+                        yPosition = bgLayer->backgroundYPosition().minWidth(rh - scaledImageHeight);
+                    else
+                        yPosition = bgLayer->backgroundYPosition().minWidth(ph - scaledImageHeight);
                     sy = scaledImageHeight - (yPosition % scaledImageHeight);
                     sy -= top % scaledImageHeight;
                 }