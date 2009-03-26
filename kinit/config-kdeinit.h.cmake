0abff7120c93d5e80d82c63cfe923277ecc33f61
Benjamin Reed <ranger@befunk.com>
2009-03-26 12:43:54 +0000

    forward-port the progname reversion/fix
    svn path=/trunk/KDE/kdelibs/; revision=944890

--- kinit/config-kdeinit.h.cmake
+++ kinit/config-kdeinit.h.cmake
@@ -1,6 +1,7 @@
 
 // These are for proctitle.cpp:
 #cmakedefine HAVE___PROGNAME 1
+#cmakedefine HAVE___PROGNAME_FULL 1
 #cmakedefine HAVE_SYS_PSTAT_H 1
 #cmakedefine HAVE_PSTAT 1
 #cmakedefine HAVE_SETPROCTITLE 1
