46f2d7605b41444d85b44bc667fdd301ea82ad95
David Faure <faure@kde.org>
2009-03-27 14:53:51 +0000

    Yeah, license. Maciej, ok with LGPL v2+3+e.V for these 6 lines of code? ;-)
    svn path=/trunk/KDE/kdelibs/; revision=945501

--- kdeui/tests/fixx11h_test.cpp
+++ kdeui/tests/fixx11h_test.cpp
@@ -1,3 +1,24 @@
+/* This file is part of the KDE libraries
+ *  Copyright 2009 David Faure <faure@kde.org>
+ *  Copyright 2009 Maciej Mrozowski <reavertm@poczta.fm>
+ *
+ *  This library is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License or ( at
+ *  your option ) version 3 or, at the discretion of KDE e.V. ( which shall
+ *  act as a proxy as in section 14 of the GPLv3 ), any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this library; see the file COPYING.LIB.  If not, write to
+ *  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ *  Boston, MA 02110-1301, USA.
+ */
+
 // From https://bugs.gentoo.org/show_bug.cgi?id=263823#c8
 #include <X11/Xlib.h>
 #include <fixx11h.h>
