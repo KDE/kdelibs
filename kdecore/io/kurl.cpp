ec6247406386ac64989013ca55c57644318813e7
Ralf Habacker <ralf.habacker@freenet.de>
2009-04-11 21:17:49 +0000

    win32 fix: display some debug messages only when debugging KURL
    svn path=/trunk/KDE/kdelibs/; revision=952432

--- kdecore/io/kurl.cpp
+++ kdecore/io/kurl.cpp
@@ -369,7 +369,9 @@ KUrl::KUrl( const QString &str )
 {
   if ( !str.isEmpty() ) {
 #ifdef Q_WS_WIN
+#ifdef DEBUG_KURL
     kDebug(126) << "KUrl::KUrl ( const QString &str = " << str.toAscii().data() << " )";
+#endif    
     QString pathToSet;
     // when it starts with file:// it's a url and must be valid. we don't care if the
     // path exist/ is valid or not
@@ -1680,7 +1682,7 @@ QString KUrl::relativeUrl(const KUrl &base_url, const KUrl &url)
 
 void KUrl::setPath( const QString& _path )
 {
-#ifdef Q_WS_WIN
+#if defined(Q_WS_WIN) && defined(DEBUG_KURL)
     kDebug(126) << "KUrl::setPath " << " " << _path.toAscii().data();
 #endif
     if ( scheme().isEmpty() )
