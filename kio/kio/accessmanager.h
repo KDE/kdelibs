a47ba20db83b1c9bb040b64e9f55bef3df4e6af0
Urs Wolfer <uwolfer@kde.org>
2009-04-12 18:26:00 +0000

    Add KIO::AccessManager, a KDE implementation of QNetworkAccessManager. Any KDE code using QNetworkAccessManager should switch to it now.  KIO::AccessManager uses KIO for all network operations.
    Code has been developed and tested with WebKitKDE. As discussed with Thiago.
    
    svn path=/trunk/KDE/kdelibs/; revision=952866

---
+++ kio/kio/accessmanager.h
@@ -0,0 +1,55 @@
+/*
+ * This file is part of the KDE project.
+ *
+ * Copyright (C) 2008 - 2009 Urs Wolfer <uwolfer @ kde.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ *
+ */
+
+#ifndef KIO_ACCESSMANAGER_H
+#define KIO_ACCESSMANAGER_H
+
+#include <QtNetwork/QNetworkAccessManager>
+
+#include <kio/global.h>
+
+/**
+ * KDE implementation of QNetworkAccessManager. Use this class instead of QNetworkAccessManager
+ * for any KDE application since it uses KIO for network operations.
+ * @since 4.3
+ */
+
+namespace KIO {
+
+class KIO_EXPORT AccessManager : public QNetworkAccessManager
+{
+    Q_OBJECT
+public:
+    AccessManager(QObject *parent);
+    virtual ~AccessManager();
+
+protected:
+    virtual QNetworkReply *createRequest(Operation op, const QNetworkRequest &req, QIODevice *outgoingData = 0);
+
+private:
+    class AccessManagerPrivate;
+    AccessManagerPrivate* const d;
+};
+
+}
+
+#endif // KIO_ACCESSMANAGER_H
---
+++ kio/kio/accessmanagerreply_p.h
@@ -0,0 +1,70 @@
+/*
+ * This file is part of the KDE project.
+ *
+ * Copyright (C) 2008 - 2009 Urs Wolfer <uwolfer @ kde.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ *
+ */
+
+#ifndef KIO_ACCESSMANAGERREPLY_P_H
+#define KIO_ACCESSMANAGERREPLY_P_H
+
+#include <QtNetwork/QNetworkReply>
+
+namespace KIO
+{
+    class Job;
+}
+class KJob;
+class KUrl;
+
+namespace KDEPrivate {
+
+/**
+ * Used for KIO::AccessManager; KDE implementation of QNetworkReply.
+ * @since 4.3
+ */
+
+class AccessManagerReply : public QNetworkReply
+{
+    Q_OBJECT
+public:
+    AccessManagerReply(const QNetworkAccessManager::Operation &op, const QNetworkRequest &request, KIO::Job *kioJob, QObject *parent);
+    virtual ~AccessManagerReply();
+    virtual qint64 bytesAvailable() const;
+    virtual void abort();
+
+public Q_SLOTS:
+    void appendData(KIO::Job *kioJob, const QByteArray &data);
+    void setMimeType(KIO::Job *kioJob, const QString &mimeType);
+    void jobDone(KJob *kJob);
+
+protected:
+    virtual qint64 readData(char *data, qint64 maxSize);
+
+private:
+    class AccessManagerReplyPrivate;
+    AccessManagerReplyPrivate* const d;
+
+    Q_PRIVATE_SLOT(d, void _k_redirection(KIO::Job *job, const KUrl &url))
+    Q_PRIVATE_SLOT(d, void _k_percent(KJob *job, unsigned long percent))
+    Q_PRIVATE_SLOT(d, void _k_permanentRedirection(KIO::Job *job, const KUrl &fromUrl, const KUrl &toUrl))
+};
+
+}
+
+#endif // KIO_ACCESSMANAGERREPLY_P_H
