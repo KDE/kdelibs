a0b1dd55d433d4a403f64f561e3d6c80d1bcec83
David Faure <faure@kde.org>
2009-04-15 17:09:30 +0000

    Relicense from LGPLv2 to LGPLv2+. Couldn't do it on kbzip2filter.* because of garbanzo's contribution.
    svn path=/trunk/KDE/kdelibs/; revision=954336

--- kdecore/compression/kgzipfilter.h
+++ kdecore/compression/kgzipfilter.h
@@ -1,9 +1,10 @@
 /* This file is part of the KDE libraries
-   Copyright (C) 2000 David Faure <faure@kde.org>
+   Copyright (C) 2000, 2009 David Faure <faure@kde.org>
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
-   License version 2 as published by the Free Software Foundation.
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
 
    This library is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
