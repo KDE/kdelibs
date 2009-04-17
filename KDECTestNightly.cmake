161cbe475d3fd368f7adac8f608074b0e76ee378
Alexander Neundorf <neundorf@kde.org>
2009-04-17 18:45:46 +0000

    -2nd try at helping with setting up nightly builds for KDE
    Alex
    
    
    
    svn path=/trunk/KDE/kdelibs/; revision=955475

---
+++ KDECTestNightly.cmake
@@ -0,0 +1,84 @@
+###########################################################
+# generic code
+###########################################################
+
+cmake_minimum_required(VERSION 2.6)
+
+include(CMakeDetermineSystem)
+if(CMAKE_HOST_UNIX)
+   include(Platform/UnixPaths)
+endif(CMAKE_HOST_UNIX)
+
+if(CMAKE_HOST_WIN32)
+   include(Platform/WindowsPaths)
+endif(CMAKE_HOST_WIN32)
+
+get_filename_component(currentDirectory "${CMAKE_CURRENT_LIST_FILE}" PATH)
+
+if(NOT EXISTS "${currentDirectory}/CMakeLists.txt")
+   message(FATAL_ERROR "This script must be in the source tree of your project.")
+endif(NOT EXISTS "${currentDirectory}/CMakeLists.txt")
+
+include("${currentDirectory}/CTestConfig.cmake")
+include("${currentDirectory}/CTestCustom.cmake" OPTIONAL)
+
+set(DASHBOARD_DIR "$ENV{HOME}/Dashboards" )
+
+if(NOT DEFINED CTEST_SOURCE_DIRECTORY)
+   if(CTEST_AVOID_SPACES)
+      set(CTEST_SOURCE_DIRECTORY "${DASHBOARD_DIR}/${CTEST_PROJECT_NAME}/srcdir" )
+   else(CTEST_AVOID_SPACES)
+      set(CTEST_SOURCE_DIRECTORY "${DASHBOARD_DIR}/${CTEST_PROJECT_NAME}/src dir" )
+   endif(CTEST_AVOID_SPACES)
+endif(NOT DEFINED CTEST_SOURCE_DIRECTORY)
+
+if(NOT DEFINED CTEST_BINARY_DIRECTORY)
+   if(CTEST_AVOID_SPACES)
+      set(CTEST_BINARY_DIRECTORY "${DASHBOARD_DIR}/${CTEST_PROJECT_NAME}/builddir" )
+   else(CTEST_AVOID_SPACES)
+      set(CTEST_BINARY_DIRECTORY "${DASHBOARD_DIR}/${CTEST_PROJECT_NAME}/build dir" )
+   endif(CTEST_AVOID_SPACES)
+endif(NOT DEFINED CTEST_BINARY_DIRECTORY)
+
+
+
+site_name(CTEST_SITE)
+set(CTEST_BUILD_NAME ${CMAKE_SYSTEM_NAME})
+
+if("${CTEST_CMAKE_GENERATOR}" MATCHES Makefile)
+   find_program(MAKE_EXECUTABLE make gmake)
+   set(CTEST_BUILD_COMMAND    "${MAKE_EXECUTABLE}" )
+else("${CTEST_CMAKE_GENERATOR}" MATCHES Makefile)
+   if(NOT DEFINED CTEST_BUILD_COMMAND)
+      message(FATAL_ERROR "CTEST_CMAKE_GENERATOR is set to \"${CTEST_CMAKE_GENERATOR}\", but CTEST_BUILD_COMMAND has not been set")
+   endif(NOT DEFINED CTEST_BUILD_COMMAND)
+endif("${CTEST_CMAKE_GENERATOR}" MATCHES Makefile)
+
+string(TOLOWER ${CTEST_VCS} _ctest_vcs)
+set(_have_vcs FALSE)
+# only set this if there is no checkout yet
+set(CTEST_CHECKOUT_COMMAND)
+
+if ("${_ctest_vcs}" STREQUAL svn)
+   find_program(SVN_EXECUTABLE svn)
+   if (NOT SVN_EXECUTABLE)
+      message(FATAL_ERROR "Error: CTEST_VCS is svn, but could not find svn executable")
+   endif (NOT SVN_EXECUTABLE)
+   set(CTEST_UPDATE_COMMAND ${SVN_EXECUTABLE})
+   if(NOT EXISTS "${CTEST_SOURCE_DIRECTORY}/.svn/entries")
+      set(CTEST_CHECKOUT_COMMAND "${SVN_EXECUTABLE} co ${CTEST_VCS_REPOSITORY}/${CTEST_VCS_PATH} \"${CTEST_SOURCE_DIRECTORY}\"")
+   endif(NOT EXISTS "${CTEST_SOURCE_DIRECTORY}/.svn/entries")
+   set(_have_vcs TRUE)
+endif ("${_ctest_vcs}" STREQUAL svn)
+
+if ("${_ctest_vcs}" STREQUAL cvs)
+   find_program(CVS_EXECUTABLE cvs cvsnt)
+   if (NOT CVS_EXECUTABLE)
+      message(FATAL_ERROR "Error: CTEST_VCS is cvs, but could not find cvs or cvsnt executable")
+   endif (NOT CVS_EXECUTABLE)
+   set(CTEST_UPDATE_COMMAND ${CVS_EXECUTABLE})
+   if(NOT EXISTS "${CTEST_SOURCE_DIRECTORY}/CVS/Entries")
+      set(CTEST_CHECKOUT_COMMAND "${CVS_EXECUTABLE} -d ${CTEST_VCS_REPOSITORY} co  -d \"${CTEST_SOURCE_DIRECTORY}\" ${CTEST_VCS_PATH}")
+   endif(NOT EXISTS "${CTEST_SOURCE_DIRECTORY}/CVS/Entries")
+   set(_have_vcs TRUE)
+endif ("${_ctest_vcs}" STREQUAL cvs)
--- KDELibsNightly.cmake
+++ KDELibsNightly.cmake
@@ -1,36 +1,17 @@
-# This is a script for ctest for running a nightly build.
-# Still pre-alpha, don't run it if you not working on it.
-# Alex
-
-#############################
-# for cmake 2.6.[0..2]:
-if(NOT CMAKE_SYSTEM)
-   include(CMakeDetermineSystem)
-   if(CMAKE_HOST_UNIX)
-      include(Platform/UnixPaths)
-   endif(CMAKE_HOST_UNIX)
-   if(CMAKE_HOST_WIN32)
-      include(Platform/WindowsPaths)
-   endif(CMAKE_HOST_WIN32)
-endif(NOT CMAKE_SYSTEM)
-
-#############################
-
-set(CTEST_UPDATE_TYPE svn)
-set(SVN_REPOSITORY https://svn.kde.org/home/kde/trunk/KDE/kdelibs )
+set(CTEST_VCS svn)
+set(CTEST_VCS_REPOSITORY https://neundorf@svn.kde.org/home/kde/trunk/KDE/kdelibs)
 set(CTEST_CMAKE_GENERATOR "Unix Makefiles" )
 
-# this is default, so it is not necessary
 # set(CTEST_AVOID_SPACES FALSE)
 
-
-include (${CTEST_SCRIPT_DIRECTORY}/cmake/modules/KDE4CTestNightlySetup.cmake)
-
+# generic support code, sets up everything required:
+include(KDECTestNightly.cmake)
 
 ctest_empty_binary_directory("${CTEST_BINARY_DIRECTORY}")
-ctest_start(Experimental)
+ctest_start(Nightly)
 ctest_update(SOURCE "${CTEST_SOURCE_DIRECTORY}" )
 ctest_configure(BUILD "${CTEST_BINARY_DIRECTORY}" )
 ctest_build(BUILD "${CTEST_BINARY_DIRECTORY}" )
-# ctest_test(BUILD "${CTEST_BINARY_DIRECTORY}" )
+ctest_test(BUILD "${CTEST_BINARY_DIRECTORY}" )
 ctest_submit()
+
