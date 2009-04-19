0fe33f30ddc81f192cbaaee6ebd089cff2bad170
Alexander Neundorf <neundorf@kde.org>
2009-04-19 18:59:13 +0000

    -don't put my login into the URL -add a comment what this actually is
    Alex
    
    
    
    svn path=/trunk/KDE/kdelibs/; revision=956285

--- KDELibsNightly.cmake
+++ KDELibsNightly.cmake
@@ -1,12 +1,18 @@
+# This is a script for running a Nightly build of kdelibs.
+# It's still work in progress.
+#
+# Alex <neundorf AT kde.org>
+
 set(CTEST_VCS svn)
-set(CTEST_VCS_REPOSITORY https://neundorf@svn.kde.org/home/kde/trunk/KDE/kdelibs)
+set(CTEST_VCS_REPOSITORY https://svn.kde.org/home/kde/trunk/KDE/kdelibs)
 set(CTEST_CMAKE_GENERATOR "Unix Makefiles" )
+
+#ctest_read_custom_files()
 
 # set(CTEST_AVOID_SPACES FALSE)
 
-get_filename_component(_currentDir "${CMAKE_CURRENT_LIST_FILE}" PATH)
-
 # generic support code, sets up everything required:
+get_filename_component(_currentDir "${CMAKE_CURRENT_LIST_FILE}" PATH)
 include( "${_currentDir}/KDECTestNightly.cmake" )
 
 ctest_empty_binary_directory("${CTEST_BINARY_DIRECTORY}")
