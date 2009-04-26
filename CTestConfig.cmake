f82e7f469c93c9ad9904196c4fdd91950ddc5a07
Alexander Neundorf <neundorf@kde.org>
2009-04-26 21:36:27 +0000

    ...even more warnings
    Alex
    
    
    
    svn path=/trunk/KDE/kdelibs/; revision=959717

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -5,13 +5,13 @@
 ##   ENABLE_TESTING()
 ##   INCLUDE(Dart)
 set(CTEST_PROJECT_NAME "kdelibs")
-set(CTEST_NIGHTLY_START_TIME "22:00:00 CET")
+set(CTEST_NIGHTLY_START_TIME "20:00:00 CET")
 
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "my.cdash.org")
 set(CTEST_DROP_LOCATION "/submit.php?project=kdelibs")
 set(CTEST_DROP_SITE_CDASH TRUE)
 
-set(CTEST_CUSTOM_MAXIMUM_NUMBER_OF_WARNINGS 1500)
+set(CTEST_CUSTOM_MAXIMUM_NUMBER_OF_WARNINGS 3000)
 set(CTEST_CUSTOM_WARNING_EXCEPTION "kdecore/network/k3socket[a-z]+\\.h"
                                    "kdecore/network/k3clientsocketbase\\.h" )
