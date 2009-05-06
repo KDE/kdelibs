2dad1d440afb68369c8570666f61e3e331023bc6
Maks Orlovich <maksim@kde.org>
2009-05-06 05:00:59 +0000

    Don't needlessly clear selection when an another app grabs it not the expected behavior in KDE.
    svn path=/branches/KDE/4.2/kdelibs/; revision=964131

--- khtml/khtml_part.cpp
+++ khtml/khtml_part.cpp
@@ -1621,9 +1621,6 @@ void KHTMLPart::clear()
   if (d->editor_context.m_caretBlinkTimer >= 0)
       killTimer(d->editor_context.m_caretBlinkTimer);
   d->editor_context.reset();
-#ifndef QT_NO_CLIPBOARD
-  connect( qApp->clipboard(), SIGNAL( selectionChanged()), SLOT( slotClearSelection()));
-#endif
 
   d->m_jobPercent = 0;
 
@@ -6407,9 +6404,7 @@ void KHTMLPart::khtmlMouseReleaseEvent( khtml::MouseReleaseEvent *event )
     QString text = selectedText();
     text.replace(QChar(0xa0), ' ');
     if (!text.isEmpty()) {
-        disconnect( qApp->clipboard(), SIGNAL( selectionChanged()), this, SLOT( slotClearSelection()));
         qApp->clipboard()->setText(text,QClipboard::Selection);
-        connect( qApp->clipboard(), SIGNAL( selectionChanged()), SLOT( slotClearSelection()));
     }
 #endif
     //kDebug( 6000 ) << "selectedText = " << text;
