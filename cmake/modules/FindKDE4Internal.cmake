f1a2141d591dc55cf79f9c4a61d59a34bce36843
Jarosław Staniek <staniek@kde.org>
2009-06-18 17:06:41 +0000

    Revert the addition of ms-bitfields to the global build flags until we have pushed that across the whole stack. For now we are solving it in gpgme++ locally.
    svn path=/branches/kdepim/enterprise4/kdelibs-4.2-branch/; revision=983638

--- cmake/modules/FindKDE4Internal.cmake
+++ cmake/modules/FindKDE4Internal.cmake
@@ -997,11 +997,6 @@ if (CMAKE_COMPILER_IS_GNUCXX)
       ###Enterprise4: do not define. we build our own and we want debug messages.
       ###add_definitions(-DQT_NO_DEBUG)
 
-      # to make our mingw builds runtime compatible with the way most third
-      # party libs (including gtk, gpgme, etc.) are built, add msvc-style
-      # memory layout flags
-      set(CMAKE_C_FLAGS   "${CMAKE_C_FLAGS}   -mms-bitfields")
-      set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mms-bitfields")
    endif (MINGW)
 
    check_cxx_compiler_flag(-fPIE HAVE_FPIE_SUPPORT)
