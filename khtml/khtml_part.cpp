8d8575fac3a0add1130a46c6fa988782787276f1
Maks Orlovich <maksim@kde.org>
2009-06-20 21:56:39 +0000

    Revert 964132 which caused problems with disappearing selections.
    svn path=/trunk/KDE/kdelibs/; revision=984512

--- khtml/khtml_part.cpp
+++ khtml/khtml_part.cpp
@@ -1630,6 +1630,9 @@ void KHTMLPart::clear()
   if (d->editor_context.m_caretBlinkTimer >= 0)
       killTimer(d->editor_context.m_caretBlinkTimer);
   d->editor_context.reset();
+#ifndef QT_NO_CLIPBOARD
+  connect( qApp->clipboard(), SIGNAL( selectionChanged()), SLOT( slotClearSelection()));
+#endif
 
   d->m_jobPercent = 0;
 
@@ -6443,7 +6446,9 @@ void KHTMLPart::khtmlMouseReleaseEvent( khtml::MouseReleaseEvent *event )
     QString text = selectedText();
     text.replace(QChar(0xa0), ' ');
     if (!text.isEmpty()) {
+        disconnect( qApp->clipboard(), SIGNAL( selectionChanged()), this, SLOT( slotClearSelection()));
         qApp->clipboard()->setText(text,QClipboard::Selection);
+        connect( qApp->clipboard(), SIGNAL( selectionChanged()), SLOT( slotClearSelection()));
     }
 #endif
     //kDebug( 6000 ) << "selectedText = " << text;
