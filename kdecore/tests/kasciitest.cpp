727c87041b8b2676d3d5e20d40e31b8785f41bd5
Volker Krause <vkrause@kde.org>
2009-06-20 17:13:26 +0000

    SVN_SILENT replace no longer valid email address
    svn path=/trunk/KDE/kdelibs/; revision=984393

--- kdecore/tests/kasciitest.cpp
+++ kdecore/tests/kasciitest.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright (c) 2006 Volker Krause <volker.krause@rwth-aachen.de>
+    Copyright (c) 2006 Volker Krause <vkrause@kde.org>
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Library General Public
