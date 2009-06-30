2698bbac7dfa5ab318ad7bc65d0d49174fc1971f
Christoph Feck <christoph@maxiom.de>
2009-06-30 19:04:00 +0000

    Remove gzip handling (Qt now does it)
    svn path=/trunk/KDE/kdelibs/; revision=989715

--- kdeui/util/ksvgrenderer.cpp
+++ kdeui/util/ksvgrenderer.cpp
@@ -20,53 +20,29 @@
 
 #include "ksvgrenderer.h"
 
-#include <kfilterdev.h>
-
-#include <QtCore/QBuffer>
-#include <QtCore/QFile>
-
 KSvgRenderer::KSvgRenderer(QObject *parent) : QSvgRenderer(parent)
 {
 }
 
 KSvgRenderer::KSvgRenderer(const QString &filename, QObject *parent) :
-    QSvgRenderer(parent)
+    QSvgRenderer(filename, parent)
 {
-    load(filename);
 }
 
 KSvgRenderer::KSvgRenderer(const QByteArray &contents, QObject *parent) :
-    QSvgRenderer(parent)
+    QSvgRenderer(contents, parent)
 {
-    load(contents);
 }
 
 bool
 KSvgRenderer::load(const QString &filename)
 {
-    QFile fi(filename);
-    if (!fi.open(QIODevice::ReadOnly))
-        return false;
-    return load(fi.readAll());
+    return QSvgRenderer::load(filename);
 }
 
 bool
 KSvgRenderer::load(const QByteArray &contents)
 {
-    if (!contents.startsWith("<?xml")) {
-        QBuffer buf(const_cast<QByteArray *>(&contents));
-        QIODevice *flt = KFilterDev::device(
-            &buf, QString::fromLatin1("application/x-gzip"), false);
-        if (!flt)
-            return false;
-        if (!flt->open(QIODevice::ReadOnly)) {
-            delete flt;
-            return false;
-        }
-        QByteArray ar = flt->readAll();
-        delete flt;
-        return QSvgRenderer::load(ar);
-    }
     return QSvgRenderer::load(contents);
 }
 
