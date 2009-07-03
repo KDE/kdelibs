d6dfe2443e5c1278345af5ae2a849f0443480c44
Michael Leupold <lemma@confuego.org>
2009-07-03 23:51:52 +0000

    Fix my previous commit: NULL pointer may not be passed to XFree. This version is also more readable.
    svn path=/trunk/KDE/kdelibs/; revision=991064

--- kdeui/util/kmodifierkeyinfoprovider_x11.cpp
+++ kdeui/util/kmodifierkeyinfoprovider_x11.cpp
@@ -51,14 +51,17 @@ unsigned int xkbVirtualModifier(XkbDescPtr xkb, const char *name)
     Q_ASSERT(xkb != 0);
 
     unsigned int mask = 0;
+    bool nameEqual;
     for (int i = 0; i < XkbNumVirtualMods; ++i) {
         char *modStr = XGetAtomName(xkb->dpy, xkb->names->vmods[i]);
-        if (modStr != 0 && strcmp(name, modStr) == 0) {
-	    XFree(modStr);
-            XkbVirtualModsToReal(xkb, 1 << i, &mask);
-            break;
+        if (modStr != 0) {
+            nameEqual = (strcmp(name, modStr) == 0);
+            XFree(modStr);
+            if (nameEqual) {
+                XkbVirtualModsToReal(xkb, 1 << i, &mask);
+                break;
+            }
         }
-	XFree(modStr);
     }
     return mask;
 }
