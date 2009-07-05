5abebfaf92e44df8d9f71d8df6a34d505e4f9fcf
Dario Freddi <drf@kde.org>
2009-07-05 15:22:23 +0000

    Docs
    svn path=/trunk/playground/libs/kidle/; revision=991755

--- kutils/kidletime/kidle.h
+++ kutils/kidletime/kidle.h
@@ -29,8 +29,6 @@ class KIdleTimePrivate;
  * only for finding out about the current idle time of the PC, but also for getting
  * notified upon idle time events, such as custom timeouts, or user activity.
  *
- * It also includes a number of 
- *
  * @author Dario Freddi
  */
 class KIdleTime : public QObject
@@ -40,20 +38,79 @@ class KIdleTime : public QObject
     Q_DISABLE_COPY(KIdleTime)
 
 public:
+    /**
+     * @returns the instance of KIdleTime
+     *
+     */
     static KIdleTime *instance();
   
+    /**
+     * The destructor
+     */
     virtual ~KIdleTime();
     
+    /**
+     * Gets the idle time of the system, in milliseconds
+     *
+     * @returns the idle time of the system
+     */
     int getIdleTime();
+    
+    /**
+     * Attempts to simulate user activity. This implies that after calling this
+     * function, the idle time of the system will become 0 and eventually resumingFromIdle
+     * will be triggered
+     *
+     * @see resumingFromIdle
+     */
     void simulateUserActivity();
     
-public slots:
+public Q_SLOTS:
+    /**
+     * Sets a new timeout to catch. When calling this function, after the system will be idle for
+     * msec milliseconds, the signal timeoutReached will be triggered. Please note that until you will
+     * call stopCatchingIdleTimeout, or you supply a different timeout, the signal will be triggered every
+     * time the system will be idle for msec milliseconds
+     *
+     * @param msec the time, in milliseconds, after which the signal will be triggered
+     *
+     * @see stopCatchingIdleTimeout
+     * @see timeoutReached
+     *
+     */
     void catchIdleTimeout(int msec);
+    /**
+     * Stops catching the previous set timeout (if any). This means that after calling this function, the signal
+     * timeoutReached won't be called again until catchIdleTimeout is called again.
+     *
+     * @see timeoutReached
+     * @see catchIdleTimeout
+     */
     void stopCatchingIdleTimeout();
+    /**
+     * Catches the next resume from idle event. This means that whenever user activity will be registered, or
+     * simulateUserActivity is called, the signal resumingFromIdle will be triggered. Please note that this function
+     * will trigger the signal just for the very first resume event: so you need to call it whenever you need to track
+     * a single resume event.
+     *
+     * @see resumingFromIdle
+     * @see simulateUserActivity
+     *
+     */
     void catchNextResumeEvent();
     
-signals:
+Q_SIGNALS:
+    /**
+     * Triggered if KIdle is catching resume events and when the system resumes from an idle state
+     *
+     * @see catchNextResumeEvent
+     */
     void resumingFromIdle();
+    /**
+     * Triggered if a timeout is being catched and when the system is idle for the chosen amount of milliseconds
+     *
+     * @param msec the time, in milliseconds, the system has been idle for
+     */
     void timeoutReached(int msec);
 
 private:
