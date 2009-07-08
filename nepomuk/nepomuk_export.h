6a5f46024844481621ef99d33db35c7a3906e756
Romain Pokrzywka <romain@kdab.net>
2009-07-08 14:42:23 +0000

    the middleware library doesn't exist anymore, remove the corresponding export macro
    svn path=/trunk/KDE/kdelibs/; revision=993354

--- nepomuk/nepomuk_export.h
+++ nepomuk/nepomuk_export.h
@@ -33,14 +33,4 @@
 # endif
 #endif
 
-#ifndef NEPOMUK_MIDDLEWARE_EXPORT
-# if defined(MAKE_NEPOMUK_MIDDLEWARE_LIB)
-   /* We are building this library */
-#  define NEPOMUK_MIDDLEWARE_EXPORT KDE_EXPORT
-# else
-   /* We are using this library */
-#  define NEPOMUK_MIDDLEWARE_EXPORT KDE_IMPORT
-# endif
-#endif
-
 #endif
