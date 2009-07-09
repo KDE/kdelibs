0ac9e2d08e0817ea6ae751fa4ded16ff4455d06d
Nicola Gigante <nicola.gigante@gmail.com>
2009-07-09 10:18:35 +0000

    Added the alpha macosx backend. The code is trivial and should be OK, but I can't test it yet because I have configuration problems with dbus and cmake.
    svn path=/trunk/playground/base/libkauth/; revision=993676

---
+++ kdecore/auth/backends/mac/AuthServicesBackend.h
@@ -0,0 +1,40 @@
+/*
+ *   Copyright (C) 2008 Nicola Gigante <nicola.gigante@gmail.com>
+ *                                                               
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or   
+ *   (at your option) any later version.                                 
+ *                                                                       
+ *   This program is distributed in the hope that it will be useful,     
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of      
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the       
+ *   GNU General Public License for more details.                        
+ *                                                                       
+ *   You should have received a copy of the GNU General Public License   
+ *   along with this program; if not, write to the                       
+ *   Free Software Foundation, Inc.,                                     
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA .      
+ */
+
+#ifndef AUTHSERVICES_BACKEND_H
+#define AUTHSERVICES_BACKEND_H
+
+#include "AuthBackend.h"
+
+class AuthServicesBackend : public QObject, public AuthBackend
+{
+	Q_OBJECT
+	Q_INTERFACES(AuthBackend)
+	
+public:
+	AuthServicesBackend();
+	virtual void setupAction(const QString&);
+	virtual bool authorizeAction(const QString&);
+	virtual Action::AuthStatus actionStatus(const QString&);
+	virtual QByteArray callerID();
+	virtual bool isCallerAuthorized(const QString &action, QByteArray callerID);
+};
+
+#endif
+
