KDE/kdelibs

commit e861a039bd0f8eb4092c409d7f6426982a08c89b

Performace improvement: before doing anything check if actually a new value has been set.
svn path=/trunk/KDE/kdelibs/; revision=995245

kdeui/widgets/ktabwidget.cpp
@@ -287,6 +287,7 @@ void KTabWidget::insertTab( QWidget *child, QTab *tab, int index )
 
 void KTabWidget::setTabBarHidden( bool hide )
 {
+  if (hide == isTabBarHidden()) return;
   QWidget *rightcorner = cornerWidget( Qt::TopRightCorner );
   QWidget *leftcorner = cornerWidget( Qt::TopLeftCorner );
 

commit 052b47f52efd568c9fdb68628578ceb217189e79

Unbreak hidding the tab bar, which did not work anymore. That caused KMail to always show the tab bar, even when the option to only show it with > 1 tab was enabled.
Problem was that isTabBarHidden() checked if the tabbar widget was visible, and not that it was explicitly
hidden by the user.

Bug introduced in r995245.

Add unit test for this.

CCMAIL: uwolfer@kde.org

svn path=/trunk/KDE/kdelibs/; revision=997708

kdeui/widgets/ktabwidget.cpp
@@ -52,6 +52,7 @@ class KTabWidget::Private
     Private( KTabWidget *parent )
       : m_parent( parent ),
         m_automaticResizeTabs( false ),
+        m_tabBarHidden( false ),
         m_resizeSuspend(ResizeEnabled)
     {
 
@@ -64,6 +65,7 @@ class KTabWidget::Private
 
     KTabWidget *m_parent;
     bool m_automaticResizeTabs;
+    bool m_tabBarHidden;
     int m_maxLength;
     int m_minLength;
     int m_currentTabLength;
@@ -287,10 +289,13 @@ void KTabWidget::insertTab( QWidget *child, QTab *tab, int index )
 
 void KTabWidget::setTabBarHidden( bool hide )
 {
-  if (hide == isTabBarHidden()) return;
+  if (hide == isTabBarHidden())
+    return;
+
   QWidget *rightcorner = cornerWidget( Qt::TopRightCorner );
   QWidget *leftcorner = cornerWidget( Qt::TopLeftCorner );
 
+  d->m_tabBarHidden = hide;
   if ( hide ) {
     if ( leftcorner ) leftcorner->hide();
     if ( rightcorner ) rightcorner->hide();
@@ -304,7 +309,7 @@ void KTabWidget::setTabBarHidden( bool hide )
 
 bool KTabWidget::isTabBarHidden() const
 {
-  return !( tabBar()->isVisible() );
+  return d->m_tabBarHidden;
 }
 
 void KTabWidget::setTabTextColor( int index, const QColor& color )

kdeui/widgets/ktabwidget.h
@@ -130,7 +130,9 @@ class KDEUI_EXPORT KTabWidget : public QTabWidget //krazy:exclude=qclasses
     void setTabBarHidden( bool hide );
 
     /**
-     * Returns true if the tabbar is hidden.
+     * Returns true if the tabbar was hidden by a call to setTabBarHidden( true ).
+     * Returns false if the widget itself is hidden, but no call to setTabBarHidden( true )
+     * has been made.
      */
     bool isTabBarHidden() const;
 

kdeui/tests/ktabwidget_unittest.cpp
@@ -94,6 +94,17 @@ private Q_SLOTS:
         }
      }
 
+     void testSetHidden()
+     {
+         KTabWidget w;
+         w.insertTab(0, new QWidget, "a tab");
+         QVERIFY(!w.isTabBarHidden());
+         w.show();
+         QVERIFY(!w.isTabBarHidden());
+         w.setTabBarHidden(true);
+         QVERIFY(w.isTabBarHidden());
+     }
+
 private Q_SLOTS:
     void slotCurrentChanged(int index)
     {