8629482e2cbaa8d80fc1181304b4da6c92bc9165
Laurent Montel <montel@kde.org>
2009-07-23 06:03:55 +0000

    Fix iterator
    svn path=/trunk/KDE/kdelibs/; revision=1001384

--- kfile/kfilepreviewgenerator.cpp
+++ kfile/kfilepreviewgenerator.cpp
@@ -839,8 +839,8 @@ void KFilePreviewGenerator::Private::applyCutItemEffect(const KFileItemList& ite
                 const QSize actualSize = icon.actualSize(m_viewAdapter->iconSize());
                 QPixmap pixmap = icon.pixmap(actualSize);
                 
-                const QHash<KUrl, QPixmap>::const_iterator cacheIt = m_cutItemsCache.find(item.url());
-                if ((cacheIt == m_cutItemsCache.end()) || (cacheIt->cacheKey() != pixmap.cacheKey())) {
+                const QHash<KUrl, QPixmap>::const_iterator cacheIt = m_cutItemsCache.constFind(item.url());
+                if ((cacheIt == m_cutItemsCache.constEnd()) || (cacheIt->cacheKey() != pixmap.cacheKey())) {
                     pixmap = iconEffect.apply(pixmap, KIconLoader::Desktop, KIconLoader::DisabledState);
                     m_dirModel->setData(index, QIcon(pixmap), Qt::DecorationRole);
                     
