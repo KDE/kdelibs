faa34a4f9a85735685a33acd5508c2debe32af2c
Lamarque Vieira Souza <lamarque@gmail.com>
2009-08-11 16:37:06 +0000

    Backport r1010110 by lvsouza from trunk to the 4.3 branch:
    Implements support to tablet switch in solid. The switch informs if a tablet laptop has its screen in normal rotation, like normal laptops, or if it is rotated like a tabletPC. Patch from Daniel O. Nascimento.
    
    svn path=/branches/KDE/4.3/kdelibs/; revision=1010112

--- solid/solid/button.h
+++ solid/solid/button.h
@@ -55,9 +55,10 @@ namespace Solid
          * - LidButton : The switch on a laptop that senses whether the lid is open or closed.
          * - PowerButton : The main power button on the computer.
          * - SleepButton : The sleep button on a computer capable of putting the computer into a suspend state.
+	 * - TabletButton : The tablet switch on a laptop that senses if the monitor is rotated or not.
          * - UnknownButtonType : The type of the button is unknow.
          */
-         enum ButtonType{ LidButton, PowerButton, SleepButton, UnknownButtonType };
+         enum ButtonType{ LidButton, PowerButton, SleepButton, UnknownButtonType, TabletButton };
 
 
     private:
