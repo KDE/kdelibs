KDE/kdelibs

commit e15000d146a08748f40070147857c0ba7745e641

Set error code when the library cannot be found. Based on patch by Michael Jansen (but without the lookup into "qtplugins", there is no way to load QPluginFactory-based plugins with KLibrary (which expects KPluginFactory, which is not a subclass of QPluginFactory). But that's fine, the phonon kcm actually tries both ways anyway.
svn path=/trunk/KDE/kdelibs/; revision=994634

kdecore/util/kpluginloader.cpp
@@ -18,6 +18,7 @@
 
 #include "kpluginloader.h"
 
+#include "kaboutdata.h"
 #include <kcomponentdata.h>
 #include <kstandarddirs.h>
 #include <klocale.h>
@@ -80,8 +81,8 @@ extern QString fixLibPrefix(const QString& libname);
 
 QString findLibraryInternal(const QString &name, const KComponentData &cData)
 {
+    // Convert name to a valid platform libname
     QString libname = makeLibName(name);
-
     QFileInfo fileinfo(name);
     bool hasPrefix = fileinfo.fileName().startsWith("lib");
     bool kdeinit = fileinfo.fileName().startsWith("libkdeinit4_");
@@ -89,25 +90,36 @@ QString findLibraryInternal(const QString &name, const KComponentData &cData)
     if (hasPrefix && !kdeinit)
         kDebug(kLibraryDebugArea()) << "plugins should not have a 'lib' prefix:" << libname;
 
+    // If it is a absolute path just return it
+    if (!QDir::isRelativePath(libname))
+        return libname;
+
+    // Start looking
     QString libfile;
-    if (QDir::isRelativePath(libname)) {
-        libfile = cData.dirs()->findResource("module", libname);
-        if (libfile.isEmpty()) {
+
+    // Check for kde modules/plugins?
+    libfile = cData.dirs()->findResource("module", libname);
+    if (!libfile.isEmpty())
+        return libfile;
+
+    // Now look where they don't belong but sometimes are
 #ifdef Q_OS_WIN
-            libname = fixLibPrefix(libname);
+    libname = fixLibPrefix(libname);
 #else
-            if (!hasPrefix)
-                libname = fileinfo.path() + QLatin1String("/lib") + fileinfo.fileName();
+    if (!hasPrefix)
+        libname = fileinfo.path() + QLatin1String("/lib") + fileinfo.fileName();
 #endif
-            libfile = cData.dirs()->findResource("lib", libname);
-            if (!libfile.isEmpty() && !kdeinit)
-                kDebug(kLibraryDebugArea()) << "library" << libname << "not found under 'module' but under 'lib'";
+
+    libfile = cData.dirs()->findResource("lib", libname);
+    if (!libfile.isEmpty()) {
+        if (!kdeinit) {
+            kDebug(kLibraryDebugArea()) << "library" << libname << "not found under 'module' but under 'lib'";
         }
+        return libfile;
     }
-    else {
-        libfile = libname;
-    }
-    return libfile;
+
+    // Nothing found
+    return QString();
 }
 
 bool KPluginLoader::isLoaded() const
@@ -119,20 +131,55 @@ KPluginLoader::KPluginLoader(const QString &plugin, const KComponentData &compon
     : QPluginLoader(findLibraryInternal(plugin, componentdata), parent), d_ptr(new KPluginLoaderPrivate(plugin))
 {
     d_ptr->q_ptr = this;
+    Q_D(KPluginLoader);
+
+    // No lib, no fun.
+    if (fileName().isEmpty()) {
+        kWarning(kLibraryDebugArea()) << "Could not find plugin" << plugin;
+        d->errorString = i18n(
+                "Could not find plugin '%1' for application '%2'",
+                plugin,
+                componentdata.aboutData()->appName());
+        return;
+    }
+
     load();
 }
+
 
 KPluginLoader::KPluginLoader(const KService &service, const KComponentData &componentdata, QObject *parent)
 : QPluginLoader(findLibraryInternal(service.library(), componentdata), parent), d_ptr(new KPluginLoaderPrivate(service.library()))
 {
     d_ptr->q_ptr = this;
     Q_D(KPluginLoader);
-    Q_ASSERT(service.isValid());
 
-    if (service.library().isEmpty()) {
-        d->errorString = i18n("The service '%1' provides no library or the Library key is missing in the .desktop file.", service.name());
+    // It's probably to late to check this because service.library() is used
+    // above.
+    if (!service.isValid()) {
+        d->errorString = i18n("The provided service is not valid", service.entryPath());
+        kWarning(kLibraryDebugArea()) << "Invalid service provided for KPluginLoader::KPluginLoader()";
         return;
     }
+
+    // service.library() is used to find the lib. So first check if it is empty.
+    if (service.library().isEmpty()) {
+        d->errorString = i18n("The service '%1' provides no library or the Library key is missing in ", service.entryPath());
+        kWarning(kLibraryDebugArea()) << "The service" << service.entryPath() <<"provided no library or the Library key is missing";
+        return;
+    }
+
+    // No lib, no fun. service.library() was set but we were still unable to
+    // find the lib.
+    if (fileName().isEmpty()) {
+        kWarning(kLibraryDebugArea()) << "Could not find plugin/library for" << service.entryPath()
+                      << "library name:" << service.library();
+        d->errorString = i18n(
+                "Could not find plugin '%1' for application '%2'",
+                service.name(),
+                componentdata.aboutData()->appName());
+        return;
+    }
+
     load();
 }
 
@@ -161,6 +208,7 @@ KPluginFactory *KPluginLoader::factory()
     KPluginFactory *factory = qobject_cast<KPluginFactory *>(obj);
 
     if (factory == 0) {
+        kDebug(kLibraryDebugArea()) << "Expected a KPluginFactory, got a" << obj->metaObject()->className();
         delete obj;
         d->errorString = i18n("The library %1 does not offer a KDE 4 compatible factory." , d->name);
     }

kdecore/util/klibrary.cpp
@@ -158,6 +158,8 @@ static KPluginFactory *kde4Factory(KLibrary *lib)
 
     if( !factory )
     {
+        if (instance)
+            kDebug(kLibraryDebugArea()) << "Expected a KPluginFactory, got a" << instance->metaObject()->className();
         kDebug(kLibraryDebugArea()) << "The library" << lib->fileName() << "does not offer a KDE 4 compatible factory.";
         return 0;
     }

kdecore/util/kpluginloader.h
@@ -34,7 +34,7 @@ class KPluginLoaderPrivate;
 
 /**
  * \class KPluginLoader kpluginloader.h <KPluginLoader>
- * 
+ *
  * This class can be used to dynamically load a plugin library at runtime.
  *
  * This class makes sure that the Qt and KDE versions used to compile this library aren't newer then
@@ -85,6 +85,8 @@ public:
     /**
      * Used this constructor to load a plugin with a given library name. Plugin libraries shouldn't have a 'lib' prefix.
      *
+     * errorString() will be set if problems are encountered.
+     *
      * \param plugin The name of the plugin library.
      * \param componentdata The KStandardDirs object from componentdata is used to search the library.
      * \param parent A parent object.
@@ -93,6 +95,8 @@ public:
 
     /**
      * Used this constructor to load a plugin from a service. The service must contain a library.
+     *
+     * errorString() will be set if problems are encountered.
      *
      * \param service The service for which the library should be loaded.
      * \param componentdata The KStandardDirs object from componentdata is used to search the library.

commit 879ca65fa92379dc023259e88c1b65ed7fa997f4

fix plugin loading on msvc as well as on mingw which broke in r994634
svn path=/branches/KDE/4.3/kdelibs/; revision=1011114

kdecore/util/kpluginloader.cpp
@@ -89,6 +89,10 @@ QString findLibraryInternal(const QString &name, const KComponentData &cData)
 
     if (hasPrefix && !kdeinit)
         kDebug(150) << "plugins should not have a 'lib' prefix:" << libname;
+#ifdef Q_OS_WIN
+    // first remove the 'lib' prefix in front of windows plugins
+    libname = fixLibPrefix(libname);
+#endif
 
     // If it is a absolute path just return it
     if (!QDir::isRelativePath(libname))
@@ -103,9 +107,7 @@ QString findLibraryInternal(const QString &name, const KComponentData &cData)
         return libfile;
 
     // Now look where they don't belong but sometimes are
-#ifdef Q_OS_WIN
-    libname = fixLibPrefix(libname);
-#else
+#ifndef Q_OS_WIN
     if (!hasPrefix)
         libname = fileinfo.path() + QLatin1String("/lib") + fileinfo.fileName();
 #endif