357fe5621d9b47d5f7d5fd0ce5d609cd1d6321b2
Dario Freddi <drf@kde.org>
2009-08-31 15:35:03 +0000

    Pushing a fake backend
    svn path=/trunk/KDE/kdelibs/; revision=1017809

---
+++ kdecore/auth/backends/fake/FakeBackend.cpp
@@ -0,0 +1,60 @@
+/*
+*   Copyright (C) 2008 Nicola Gigante <nicola.gigante@gmail.com>
+*
+*   This program is free software; you can redistribute it and/or modify
+*   it under the terms of the GNU Lesser General Public License as published by
+*   the Free Software Foundation; either version 2.1 of the License, or
+*   (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU Lesser General Public License
+*   along with this program; if not, write to the
+*   Free Software Foundation, Inc.,
+*   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA .
+*/
+
+#include "FakeBackend.h"
+
+#include <QDebug>
+#include <syslog.h>
+
+namespace KAuth
+{
+
+FakeBackend::FakeBackend()
+{
+    // Nothing to do here...
+}
+
+Action::AuthStatus FakeBackend::authorizeAction(const QString &action)
+{    
+    return Action::Denied;
+}
+
+void FakeBackend::setupAction(const QString &action)
+{
+    Q_UNUSED(action)
+}
+
+Action::AuthStatus FakeBackend::actionStatus(const QString &action)
+{
+    Q_UNUSED(action)
+}
+
+QByteArray FakeBackend::callerID() const
+{
+    return QByteArray();
+}
+
+bool FakeBackend::isCallerAuthorized(const QString &action, QByteArray callerID)
+{
+    return false;
+}
+
+} // namespace Auth
+
+Q_EXPORT_PLUGIN2(auth_backend, KAuth::FakeBackend)
---
+++ kdecore/auth/backends/fake/FakeBackend.h
@@ -0,0 +1,50 @@
+/*
+*   Copyright (C) 2008 Nicola Gigante <nicola.gigante@gmail.com>
+*
+*   This program is free software; you can redistribute it and/or modify
+*   it under the terms of the GNU Lesser General Public License as published by
+*   the Free Software Foundation; either version 2.1 of the License, or
+*   (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU Lesser General Public License
+*   along with this program; if not, write to the
+*   Free Software Foundation, Inc.,
+*   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA .
+*/
+
+#ifndef POLICYKIT_BACKEND_H
+#define POLICYKIT_BACKEND_H
+
+#include "AuthBackend.h"
+#include <QHash>
+
+class QByteArray;
+
+namespace KAuth
+{
+
+class FakeBackend : public AuthBackend
+{
+    Q_OBJECT
+    Q_INTERFACES(KAuth::AuthBackend)
+
+public:
+    FakeBackend();
+    virtual void setupAction(const QString&);
+    virtual Action::AuthStatus authorizeAction(const QString&);
+    virtual Action::AuthStatus actionStatus(const QString&);
+    virtual QByteArray callerID() const;
+    virtual bool isCallerAuthorized(const QString &action, QByteArray callerID);
+
+Q_SIGNALS:
+    void actionStatusChanged(const QString &action, Action::AuthStatus status);
+};
+
+} // namespace Auth
+
+#endif
