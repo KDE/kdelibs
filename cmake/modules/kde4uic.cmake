2fbc31b8c98d48b3749dffd00509cb0af1668b1c
David Faure <faure@kde.org>
2009-09-01 18:15:17 +0000

    Don't generate a file with just two includes in them, if uic failed. This way typing "make" again, shows the real error from uic, rather than the "class not found"...
    svn path=/branches/KDE/4.3/kdelibs/; revision=1018467

--- cmake/modules/kde4uic.cmake
+++ cmake/modules/kde4uic.cmake
@@ -41,14 +41,16 @@ if (KDE4_HEADER)
 endif (KDE4_HEADER)
 
 
-#replace tr218n("") with QString::null to avoid warning from KLocale
-STRING(REGEX REPLACE "tr2i18n\\(\"\"\\)" "QString::null" _uic_CONTENTS "${_uic_CONTENTS}" )
-STRING(REGEX REPLACE "tr2i18n\\(\"\", \"\"\\)" "QString::null" _uic_CONTENTS "${_uic_CONTENTS}" )
-#replace image15_data with img15_filename to make enable_final work
-STRING(REGEX REPLACE "image([0-9]+)_data" "img\\1_${KDE_UIC_BASENAME}" _uic_CONTENTS "${_uic_CONTENTS}")
-#fixup include guard
-STRING(REGEX REPLACE "#ifndef " "#ifndef UI_" _uic_CONTENTS "${_uic_CONTENTS}")
-STRING(REGEX REPLACE "#define " "#define UI_" _uic_CONTENTS "${_uic_CONTENTS}")
+IF (${_uic_CONTENTS})
+  #replace tr218n("") with QString::null to avoid warning from KLocale
+  STRING(REGEX REPLACE "tr2i18n\\(\"\"\\)" "QString::null" _uic_CONTENTS "${_uic_CONTENTS}" )
+  STRING(REGEX REPLACE "tr2i18n\\(\"\", \"\"\\)" "QString::null" _uic_CONTENTS "${_uic_CONTENTS}" )
+  #replace image15_data with img15_filename to make enable_final work
+  STRING(REGEX REPLACE "image([0-9]+)_data" "img\\1_${KDE_UIC_BASENAME}" _uic_CONTENTS "${_uic_CONTENTS}")
+  #fixup include guard
+  STRING(REGEX REPLACE "#ifndef " "#ifndef UI_" _uic_CONTENTS "${_uic_CONTENTS}")
+  STRING(REGEX REPLACE "#define " "#define UI_" _uic_CONTENTS "${_uic_CONTENTS}")
 
-FILE(WRITE ${KDE_UIC_CPP_FILE} "#include <kdialog.h>\n#include <klocale.h>\n\n${_uic_CONTENTS}\n")
+  FILE(WRITE ${KDE_UIC_CPP_FILE} "#include <kdialog.h>\n#include <klocale.h>\n\n${_uic_CONTENTS}\n")
+ENDIF()
 
