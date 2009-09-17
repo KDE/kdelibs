70220c973c4a833799c1c52de7d00857f8f19853
Sebastian Trueg <sebastian@trueg.de>
2009-09-17 16:40:52 +0000

    for files we now make the pimo thing and the file resource use the same URI - it make everything much simpler.
    svn path=/trunk/KDE/kdelibs/; revision=1024937

--- nepomuk/core/resourcedata.cpp
+++ nepomuk/core/resourcedata.cpp
@@ -105,6 +105,19 @@ QString Nepomuk::ResourceData::kickoffUriOrId() const
 }
 
 
+bool Nepomuk::ResourceData::isFile()
+{
+    return (
+        ( m_uri.scheme() == "file" ||
+          constHasType( Soprano::Vocabulary::Xesam::File() ) ||
+          constHasType( Nepomuk::Vocabulary::NFO::FileDataObject() ) )
+        &&
+        ( QFile::exists( m_uri.toLocalFile()) ||
+          QFile::exists( property(Nepomuk::Vocabulary::NIE::url()).toUrl().toLocalFile() ) )
+        );
+}
+
+
 QUrl Nepomuk::ResourceData::uri() const
 {
     if( m_proxyData )
@@ -285,12 +298,9 @@ bool Nepomuk::ResourceData::store()
             statements.append( Statement( m_uri, Soprano::Vocabulary::NAO::identifier(), LiteralValue(m_kickoffIdentifier) ) );
         }
 
-        // HACK: make sure that files have proper url properties so long as we do not have a File class for
+        // make sure that files have proper url properties so long as we do not have a File class for
         // Dolphin and co.
-        if ( ( m_uri.scheme() == "file" ||
-               constHasType( Soprano::Vocabulary::Xesam::File() ) ||
-               constHasType( Nepomuk::Vocabulary::NFO::FileDataObject() ) ) &&
-               QFile::exists( m_uri.toLocalFile()) ) {
+        if ( isFile() ) {
             statements.append( Statement( m_uri, Nepomuk::Vocabulary::NIE::url(), m_uri ) );
         }
 
@@ -694,13 +704,19 @@ Nepomuk::Thing Nepomuk::ResourceData::pimoThing()
         if( hasType( Vocabulary::PIMO::Thing() ) ) {
             kDebug() << "we are already a thing. Creating link to ourself" << m_uri << this;
             // we are out own thing
-            m_pimoThing = new Thing( this );
+            m_pimoThing = new Thing(this);
+        }
+        else if( isFile() ) {
+            // files are a special case in every aspect. this includes pimo things.
+            // files are their own grounding occurrence. This makes a lot of things
+            // much simpler.
+            m_pimoThing = new Thing(this);
         }
         else {
             kDebug() << "creating new thing for" << m_uri << m_types << this;
             m_pimoThing = new Thing();
-            m_pimoThing->m_data->m_groundingOccurence = this;
         }
+        m_pimoThing->m_data->m_groundingOccurence = this;
         kDebug() << "created thing" << m_pimoThing->m_data << "with grounding occurence" << m_pimoThing->m_data->m_groundingOccurence;
     }
     return *m_pimoThing;
