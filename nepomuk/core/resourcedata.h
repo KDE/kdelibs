bda2f7de569de0261f008de45be167e9a77c503d
Sebastian Trueg <sebastian@trueg.de>
2009-09-18 10:00:31 +0000

    forgot to commit this one - sorry
    svn path=/trunk/KDE/kdelibs/; revision=1025293

--- nepomuk/core/resourcedata.h
+++ nepomuk/core/resourcedata.h
@@ -1,4 +1,4 @@
-/* 
+/*
  * This file is part of the Nepomuk KDE project.
  * Copyright (C) 2006-2009 Sebastian Trueg <trueg@kde.org>
  *
@@ -6,12 +6,12 @@
  * modify it under the terms of the GNU Library General Public
  * License as published by the Free Software Foundation; either
  * version 2 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Library General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Library General Public License
  * along with this library; see the file COPYING.LIB.  If not, write to
  * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
@@ -64,6 +64,11 @@ namespace Nepomuk {
             return m_ref;
         }
 
+        bool isFile();
+
+        /**
+         * Tries to determine if this resource represents a file by examining the type and the uri.
+         */
         bool isFile();
 
         /**
