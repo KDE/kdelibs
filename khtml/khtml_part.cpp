da8da8cb73babce2e11cb9900665e4a5a7d2d924
Maks Orlovich <maksim@kde.org>
2009-09-19 18:24:38 +0000

    automatically merged revision 1025800: Fix show()/hide()
    svn path=/trunk/KDE/kdelibs/; revision=1025801

--- khtml/khtml_part.cpp
+++ khtml/khtml_part.cpp
@@ -5528,14 +5528,14 @@ void KHTMLPart::restoreState( QDataStream &stream )
 
 void KHTMLPart::show()
 {
-  if ( d->m_view )
-    d->m_view->show();
+  if ( widget() )
+    widget()->show();
 }
 
 void KHTMLPart::hide()
 {
-  if ( d->m_view )
-    d->m_view->hide();
+  if ( widget() )
+    widget()->hide();
 }
 
 DOM::Node KHTMLPart::nodeUnderMouse() const
