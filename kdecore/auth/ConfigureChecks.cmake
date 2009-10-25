6fee84c21fe977cb02f687eaf3e052abb22f4df8
Till Adam <adam@kde.org>
2009-10-25 09:15:50 +0000

    Rename the variable used to identify the Mac auth backend from APPLE to OSX, as the former is a reserved string, which results in my cmake failing to do perform string equality on it properly.
    svn path=/trunk/KDE/kdelibs/; revision=1039987

--- kdecore/auth/ConfigureChecks.cmake
+++ kdecore/auth/ConfigureChecks.cmake
@@ -8,7 +8,7 @@ set(KAUTH_BACKEND "" CACHE STRING "Specifies the KAuth backend to build. Current
 ## 4.4: We leave polkit-0.9 having more priority over polkit-1. This will change from 4.5 on
 if(NOT KAUTH_BACKEND)
     if (APPLE)
-        set (KAUTH_BACKEND "Apple")
+        set (KAUTH_BACKEND "OSX")
     elseif (UNIX)
         macro_optional_find_package(PolkitQt)
         macro_log_feature(POLKITQT_FOUND "PolkitQt" "Qt Wrapper around Policykit" "http://api.kde.org/polkit-qt"
@@ -30,16 +30,16 @@ else(NOT KAUTH_BACKEND)
     string(TOUPPER ${KAUTH_BACKEND} KAUTH_BACKEND)
     
     # Check if the specified backend is valid. If it is not, we fall back to the Fake one
-    if (NOT KAUTH_BACKEND STREQUAL "APPLE" AND NOT KAUTH_BACKEND STREQUAL "POLKITQT" AND NOT KAUTH_BACKEND STREQUAL "FAKE")
+    if (NOT KAUTH_BACKEND STREQUAL "OSX" AND NOT KAUTH_BACKEND STREQUAL "POLKITQT" AND NOT KAUTH_BACKEND STREQUAL "FAKE")
         message ("WARNING: The KAuth Backend ${KAUTH_BACKEND} you specified does not exist. Falling back to Fake backend")
         set (KAUTH_BACKEND "Fake")
-    endif (NOT KAUTH_BACKEND STREQUAL "APPLE" AND NOT KAUTH_BACKEND STREQUAL "POLKITQT" AND NOT KAUTH_BACKEND STREQUAL "FAKE")
+    endif (NOT KAUTH_BACKEND STREQUAL "OSX" AND NOT KAUTH_BACKEND STREQUAL "POLKITQT" AND NOT KAUTH_BACKEND STREQUAL "FAKE")
 
     # Check requirements for each backend. If not, fall back to the fake one
-    if (KAUTH_BACKEND STREQUAL "APPLE" AND NOT APPLE)
+    if (KAUTH_BACKEND STREQUAL "OSX" AND NOT APPLE)
         message ("WARNING: You chose the Apple KAuth backend but your system does not support it. Falling back to Fake backend")
         set (KAUTH_BACKEND "FAKE")
-    endif (KAUTH_BACKEND STREQUAL "APPLE" AND NOT APPLE)
+    endif (KAUTH_BACKEND STREQUAL "OSX" AND NOT APPLE)
     if (KAUTH_BACKEND STREQUAL "POLKITQT")
         macro_optional_find_package(PolkitQt)
         macro_log_feature(POLKITQT_FOUND "PolkitQt" "Qt Wrapper around Policykit" "http://api.kde.org/polkit-qt"
@@ -54,7 +54,7 @@ else(NOT KAUTH_BACKEND)
 endif(NOT KAUTH_BACKEND)
 
 # Add the correct libraries depending on the backend
-if(KAUTH_BACKEND STREQUAL "APPLE")
+if(KAUTH_BACKEND STREQUAL "OSX")
     find_library(CORE_FOUNDATION_LIBRARY CoreFoundation)
     find_library(SECURITY_LIBRARY Security)
 elseif(KAUTH_BACKEND STREQUAL "POLKITQT")
