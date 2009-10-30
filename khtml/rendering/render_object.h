d0e6d81013e5d92d51b83a3c1ec195dacc0bbcad
Maks Orlovich <maksim@kde.org>
2009-10-30 15:21:50 +0000

    Implement custom scrollHeight for RenderTextArea --- it needs to be able to look inside its scrollbar usage, so normal RenderObject/RenderLayer stuff is insufficient.
    Fixes growing suggestion box on reviewboard.
    
    (Though, I really do wonder why the test scrollHeight and height for inequality --- 
    it'd make perfect sense for scrollHeight to be smaller than element height when one 
    has enough room, and at least some Opera versions behave that way, too; so testing 
    for scrollHeight > height would be better. Not that it'd help us before... but, well,
    brittle code...)
    
    BUG:199986
    
    svn path=/branches/KDE/4.3/kdelibs/; revision=1042752

--- khtml/rendering/render_object.h
+++ khtml/rendering/render_object.h
@@ -156,8 +156,8 @@ public:
     virtual RenderObject *firstChild() const { return 0; }
     virtual RenderObject *lastChild() const { return 0; }
 
-    RenderObject *nextRenderer() const; 
-    RenderObject *previousRenderer() const; 
+    RenderObject *nextRenderer() const;
+    RenderObject *previousRenderer() const;
 
     RenderObject *nextEditable() const;
     RenderObject *previousEditable() const;
@@ -346,7 +346,7 @@ public:
     DOM::DocumentImpl* document() const;
     DOM::NodeImpl* element() const { return isAnonymous() ? 0L : m_node; }
     DOM::NodeImpl* node() const { return m_node; }
-    
+
     virtual bool handleEvent(const DOM::EventImpl&) { return false; }
 
    /**
@@ -644,12 +644,12 @@ public:
     // how much goes over the left hand side (0 or a negative number)
     virtual int overflowTop() const { return 0; }
     virtual int overflowLeft() const { return 0; }
-    
+
     /**
      * Returns the height that is effectively considered when contemplating the
      * object as a whole -- usually the overflow height, or the height if clipped.
      */
-    int effectiveHeight() const { return hasOverflowClip() ? height() + borderTopExtra() + borderBottomExtra() : 
+    int effectiveHeight() const { return hasOverflowClip() ? height() + borderTopExtra() + borderBottomExtra() :
                                          qMax(overflowHeight() - overflowTop(),  height() + borderTopExtra() + borderBottomExtra()); }
     /**
      * Returns the width that is effectively considered when contemplating the
@@ -667,8 +667,8 @@ public:
     int clientTop() const;
     short clientWidth() const;
     int clientHeight() const;
-    short scrollWidth() const;
-    int scrollHeight() const;
+    virtual short scrollWidth() const;
+    virtual int scrollHeight() const;
 
     virtual bool isSelfCollapsingBlock() const { return false; }
     short collapsedMarginTop() const { return maxTopMargin(true)-maxTopMargin(false);  }
@@ -807,7 +807,7 @@ public:
     virtual void detach( );
 
     bool documentBeingDestroyed() const { return !document()->renderer(); }
-    
+
     void setDoNotDelete(bool b) { m_doNotDelete = b; }
     bool doNotDelete() const { return m_doNotDelete; }
 
@@ -892,7 +892,7 @@ private:
 
     bool m_needsPageClear            : 1;
     bool m_containsPageBreak         : 1;
-    
+
     bool m_hasOverflowClip           : 1;
     bool m_inPosObjectList           : 1;
 
