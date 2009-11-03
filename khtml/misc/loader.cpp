80f4bef510a7d0b4db1c2bc30dd1107385c4adb7
Guy Maurel <guy-kde@maurel.de>
2009-11-03 18:52:36 +0000

    add parentheses around && at line 1212
    svn path=/trunk/KDE/kdelibs/; revision=1044434

--- khtml/misc/loader.cpp
+++ khtml/misc/loader.cpp
@@ -1212,7 +1212,7 @@ void DocLoader::printPreloadStats()
     KUrl fullURL (m_doc->completeURL( url.string() )); \
     if ( !fullURL.isValid() || \
          ( m_part && m_part->onlyLocalReferences() && fullURL.protocol() != "file" && fullURL.protocol() != "data") || \
-         doRedirectCheck && (m_doc && !KAuthorized::authorizeUrlAction("redirect", m_doc->URL(), fullURL))) \
+         ( doRedirectCheck && (m_doc && !KAuthorized::authorizeUrlAction("redirect", m_doc->URL(), fullURL)))) \
          return 0L;
 
 CachedImage *DocLoader::requestImage( const DOM::DOMString &url)
