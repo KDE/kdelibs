0f03a78c918ba2661c5c43bf80aa5aa880523990
Volker Krause <vkrause@kde.org>
2009-11-18 09:23:55 +0000

    Exlude files generated by moc and uic from the coverage analysis and ignore icecream errors.
    svn path=/trunk/KDE/kdelibs/; revision=1050789

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -18,3 +18,5 @@ set(CTEST_CUSTOM_WARNING_EXCEPTION "kdecore/network/k3socket[a-z]+\\.h"
                                    "Warning: No relevant classes found. No output generated."  # moc didn't find anything to do
                                    "yy" "YY"                                                   # ignore the warnings in generated code
    )
+set(CTEST_CUSTOM_ERROR_EXCEPTION "ICECC")
+set(CTEST_CUSTOM_COVERAGE_EXCLUDE ".moc$" "moc_" "ui_")
