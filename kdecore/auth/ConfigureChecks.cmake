ff3e1063bbbc58794f2b26e392bd5ad8d4367ed1
Dario Freddi <drf@kde.org>
2009-11-27 14:36:29 +0000

    CCMAIL: neundorf@kde.org
    Fixing naming mess in polkitqt-1 cmake files. Renaming all the according variables to
    POLKITQT-1_* for consistency with the so name of the library (polkit-qt-1). Again for
    consistency, rename the KAUTH_BACKEND of polkitqt-1 to PolkitQt-1 instead of PolkitQt1.
    
    This time it works :)
    
    
    svn path=/trunk/KDE/kdelibs/; revision=1055185

--- kdecore/auth/ConfigureChecks.cmake
+++ kdecore/auth/ConfigureChecks.cmake
@@ -1,7 +1,7 @@
 ####### checks for kdecore/kauth ###############
 
 set(KAUTH_BACKEND "" CACHE STRING "Specifies the KAuth backend to build. Current available options are 
-                                   PolkitQt, Fake, Apple. Not setting this variable will build the most 
+                                   PolkitQt, PolkitQt-1, Fake, Apple. Not setting this variable will build the most 
                                    appropriate backend for your system")
 ## Check if the user did not specify a backend to be built. If that is the case,
 ## we check what is the best backend to build on this system.
@@ -11,18 +11,24 @@ if(NOT KAUTH_BACKEND)
         set (KAUTH_BACKEND "OSX")
     elseif (UNIX)
         macro_optional_find_package(PolkitQt)
-        macro_log_feature(POLKITQT_FOUND "PolkitQt" "Qt Wrapper around Policykit" "http://api.kde.org/polkit-qt"
-                          FALSE "" "STRONGLY RECOMMENDED: Needed to make KAuth work, hence to enable some workspace functionalities")
 
         if (POLKITQT_FOUND)
             set (KAUTH_BACKEND "PolkitQt")
+            macro_log_feature(POLKITQT_FOUND "PolkitQt" "Qt Wrapper around Policykit" "http://api.kde.org/polkit-qt"
+                              FALSE "" "STRONGLY RECOMMENDED: Needed to make KAuth work (using PolicyKit < 0.90), hence to enable some workspace functionalities")
         else (POLKITQT_FOUND)
             macro_optional_find_package(PolkitQt-1)
-            macro_log_feature(POLKITQT-1_FOUND "PolkitQt1" "Qt Wrapper around polkit-1" "http://techbase.kde.org/Polkit-Qt-1"
-                          FALSE "" "Using PolkitQt1")# TODO: some comment
+            
             if (POLKITQT-1_FOUND)
                 set (KAUTH_BACKEND "PolkitQt1")
+                macro_log_feature(POLKITQT-1_FOUND "PolkitQt-1" "Qt Wrapper around polkit-1" "http://techbase.kde.org/Polkit-Qt-1"
+                                  FALSE "" "STRONGLY RECOMMENDED: Needed to make KAuth work (using polkit-1), hence to enable some workspace functionalities")
             else (POLKITQT-1_FOUND)
+                # Nothing was found: notify and log the missing features
+                macro_log_feature(POLKITQT_FOUND "PolkitQt" "Qt Wrapper around Policykit" "http://api.kde.org/polkit-qt"
+                                  FALSE "" "STRONGLY RECOMMENDED: Needed to make KAuth work (using PolicyKit < 0.90), hence to enable some workspace functionalities")
+                macro_log_feature(POLKITQT-1_FOUND "PolkitQt-1" "Qt Wrapper around polkit-1" "http://techbase.kde.org/Polkit-Qt-1"
+                                  FALSE "" "STRONGLY RECOMMENDED: Needed to make KAuth work (using polkit-1), hence to enable some workspace functionalities")
                 set (KAUTH_BACKEND "Fake")
             endif (POLKITQT-1_FOUND)
         endif (POLKITQT_FOUND)
@@ -37,10 +43,10 @@ else(NOT KAUTH_BACKEND)
     string(TOUPPER ${KAUTH_BACKEND} KAUTH_BACKEND)
     
     # Check if the specified backend is valid. If it is not, we fall back to the Fake one
-    if (NOT KAUTH_BACKEND STREQUAL "OSX" AND NOT KAUTH_BACKEND STREQUAL "POLKITQT" AND NOT KAUTH_BACKEND STREQUAL "POLKITQT1" AND NOT KAUTH_BACKEND STREQUAL "FAKE")
+    if (NOT KAUTH_BACKEND STREQUAL "OSX" AND NOT KAUTH_BACKEND STREQUAL "POLKITQT" AND NOT KAUTH_BACKEND STREQUAL "POLKITQT-1" AND NOT KAUTH_BACKEND STREQUAL "FAKE")
         message ("WARNING: The KAuth Backend ${KAUTH_BACKEND} you specified does not exist. Falling back to Fake backend")
         set (KAUTH_BACKEND "Fake")
-    endif (NOT KAUTH_BACKEND STREQUAL "OSX" AND NOT KAUTH_BACKEND STREQUAL "POLKITQT" AND NOT KAUTH_BACKEND STREQUAL "POLKITQT1" AND NOT KAUTH_BACKEND STREQUAL "FAKE")
+    endif (NOT KAUTH_BACKEND STREQUAL "OSX" AND NOT KAUTH_BACKEND STREQUAL "POLKITQT" AND NOT KAUTH_BACKEND STREQUAL "POLKITQT-1" AND NOT KAUTH_BACKEND STREQUAL "FAKE")
 
     # Check requirements for each backend. If not, fall back to the fake one
     if (KAUTH_BACKEND STREQUAL "OSX" AND NOT APPLE)
@@ -58,17 +64,17 @@ else(NOT KAUTH_BACKEND)
             set (KAUTH_BACKEND "FAKE")
         endif (NOT POLKITQT_FOUND)
     endif (KAUTH_BACKEND STREQUAL "POLKITQT")
-    if (KAUTH_BACKEND STREQUAL "POLKITQT1")
+    if (KAUTH_BACKEND STREQUAL "POLKITQT-1")
         macro_optional_find_package(PolkitQt-1)
         macro_log_feature(POLKITQT-1_FOUND "PolkitQt-1" "Qt Wrapper around polkit-1" "http://techbase.kde.org/Polkit-Qt-1"
-                          FALSE "" "Using PolkitQt1")# TODO: some comment
+                          FALSE "" "STRONGLY RECOMMENDED: Needed to make KAuth work (using polkit-1), hence to enable some workspace functionalities")
 
         if (NOT POLKITQT-1_FOUND)
             message ("WARNING: You chose the PolkitQt-1 KAuth backend but you don't have PolkitQt-1 installed.
                       Falling back to Fake backend")
             set (KAUTH_BACKEND "FAKE")
         endif (NOT POLKITQT-1_FOUND)
-    endif (KAUTH_BACKEND STREQUAL "POLKITQT1")
+    endif (KAUTH_BACKEND STREQUAL "POLKITQT-1")
 endif(NOT KAUTH_BACKEND)
 
 # Add the correct libraries depending on the backend
@@ -77,6 +83,6 @@ if(KAUTH_BACKEND STREQUAL "OSX")
     find_library(SECURITY_LIBRARY Security)
 elseif(KAUTH_BACKEND STREQUAL "POLKITQT")
     include_directories(${POLKITQT_INCLUDE_DIR})
-elseif(KAUTH_BACKEND STREQUAL "POLKITQT1")
-    include_directories(${POLKITQT1_INCLUDE_DIR})
+elseif(KAUTH_BACKEND STREQUAL "POLKITQT-1")
+    include_directories(${POLKITQT-1_INCLUDE_DIR})
 endif()
