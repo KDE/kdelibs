70479bff657ffe2c374f4a1cc9b338ad6b9c7959
David Faure <faure@kde.org>
2009-12-04 11:00:16 +0000

    Fix compilation on Solaris with Sun Studio 12. Fixed for: 4.4.0-beta2 BUG: 216331
    svn path=/trunk/KDE/kdelibs/; revision=1058367

--- kdecore/tests/kglobaltest.cpp
+++ kdecore/tests/kglobaltest.cpp
@@ -42,8 +42,8 @@ private Q_SLOTS:
     {
         QTimer child(this);
         QCOMPARE(KGlobal::findDirectChild<QTimer *>(this), &child);
-        QCOMPARE(KGlobal::findDirectChild<QTimer *>(&child), (QObject*)0);
-        QCOMPARE(KGlobal::findDirectChild<QEventLoop *>(this), (QObject*)0);
+        QCOMPARE(KGlobal::findDirectChild<QTimer *>(&child), (QTimer*)0);
+        QCOMPARE(KGlobal::findDirectChild<QEventLoop *>(this), (QEventLoop*)0);
     }
 
     // The former implementation of QTest::kWaitForSignal would return
