1f4dac4a48bcd8fad7897ab37fa47bf0eae1eca6
John Layt <john@layt.net>
2009-12-28 23:39:55 +0000

    krazy fixes
    svn path=/trunk/KDE/kdelibs/; revision=1067123

--- kdeui/kernel/kuniqueapplication_p.h
+++ kdeui/kernel/kuniqueapplication_p.h
@@ -1,5 +1,5 @@
 /* This file is part of the KDE libraries
-    Copyright (c) 2006 Thiago Macieira <thiagokde.org>
+    Copyright (c) 2006 Thiago Macieira <thiago@kde.org>
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Library General Public
