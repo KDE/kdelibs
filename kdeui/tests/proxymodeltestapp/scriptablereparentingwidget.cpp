8f6d653e533b72a4dd4f521c7bf103a03d92e7cd
Stephen Kelly <steveire@gmail.com>
2010-01-06 12:06:51 +0000

    Add a scriptable reparenting proxy model and widget.
    svn path=/trunk/KDE/kdelibs/; revision=1070663

---
+++ kdeui/tests/proxymodeltestapp/scriptablereparentingwidget.cpp
@@ -0,0 +1,98 @@
+/*
+    Copyright (c) 2009 Stephen Kelly <steveire@gmail.com>
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Library General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+    License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to the
+    Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+    02110-1301, USA.
+*/
+
+#include "scriptablereparentingwidget.h"
+
+#include <QPlainTextEdit>
+#include <QTreeView>
+#include <QVBoxLayout>
+#include <QScriptEngine>
+
+#include "kdebug.h"
+#include <QLabel>
+#include <QSplitter>
+
+ScriptableReparentingProxyModel::ScriptableReparentingProxyModel(QObject* parent)
+  : KReparentingProxyModel(parent),
+    m_scriptEngine(new QScriptEngine(this))
+{
+
+}
+
+
+bool ScriptableReparentingProxyModel::isDescendantOf(const QModelIndex& ancestor, const QModelIndex& descendant) const
+{
+  if (!m_implementationFunction.isValid())
+    return KReparentingProxyModel::isDescendantOf(ancestor, descendant);
+
+  QScriptValueList arguments = QScriptValueList() << ancestor.data().toInt() << descendant.data().toInt();
+  QScriptValue returnValue = m_implementationFunction.call(QScriptValue(), arguments );
+
+  if (!returnValue.isBool())
+    return KReparentingProxyModel::isDescendantOf(ancestor, descendant);
+
+  return returnValue.toBool();
+}
+
+void ScriptableReparentingProxyModel::setImplementation(const QString& implementation)
+{
+  beginChangeRule();
+  m_implementationFunction = m_scriptEngine->evaluate(implementation);
+  m_implementationFunction = m_scriptEngine->globalObject().property("isDescendantOf");
+  endChangeRule();
+}
+
+ScriptableReparentingWidget::ScriptableReparentingWidget(QAbstractItemModel *rootModel, QWidget* parent, Qt::WindowFlags f)
+  : QWidget(parent, f),
+    m_reparentingProxyModel(new ScriptableReparentingProxyModel(this))
+{
+  QVBoxLayout *mainLayout = new QVBoxLayout(this);
+  QSplitter *splitter = new QSplitter(Qt::Vertical, this);
+  mainLayout->addWidget(splitter);
+
+  m_treeView = new QTreeView(splitter);
+  QWidget *container = new QWidget(splitter);
+  QVBoxLayout *layout = new QVBoxLayout(container);
+  m_textEdit = new QPlainTextEdit(container);
+
+  layout->addWidget(new QLabel("function isDescendantOf (ancestor, descendant) {", container));
+  QHBoxLayout *indentedLayout = new QHBoxLayout(container);
+  indentedLayout->addSpacing(30);
+  indentedLayout->addWidget(m_textEdit);
+  layout->addLayout(indentedLayout);
+  layout->addWidget(new QLabel("}", container));
+
+  m_reparentingProxyModel->setSourceModel(rootModel);
+  m_treeView->setModel(m_reparentingProxyModel);
+
+  splitter->setStretchFactor(0, 100);
+
+  connect(m_textEdit, SIGNAL(textChanged()), SLOT(textChanged()));
+  textChanged();
+}
+
+
+void ScriptableReparentingWidget::textChanged()
+{
+  m_reparentingProxyModel->setImplementation("function isDescendantOf (ancestor, descendant) { " + m_textEdit->toPlainText()  + " }");
+  m_treeView->expandAll();
+}
+
+
+#include "scriptablereparentingwidget.moc"
---
+++ kdeui/tests/proxymodeltestapp/scriptablereparentingwidget.h
@@ -0,0 +1,64 @@
+/*
+    Copyright (c) 2009 Stephen Kelly <steveire@gmail.com>
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Library General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+    License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to the
+    Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+    02110-1301, USA.
+*/
+
+#ifndef SCRIPTABLEREPARENTINGWIDGET_H
+#define SCRIPTABLEREPARENTINGWIDGET_H
+
+#include <QWidget>
+#include <QScriptValue>
+
+#include "kreparentingproxymodel.h"
+
+
+class QTreeView;
+class QPlainTextEdit;
+class QScriptEngine;
+
+class ScriptableReparentingProxyModel : public KReparentingProxyModel
+{
+  Q_OBJECT
+public:
+  ScriptableReparentingProxyModel(QObject* parent = 0);
+
+  /* reimp */ bool isDescendantOf(const QModelIndex& ancestor, const QModelIndex& descendant) const;
+
+  void setImplementation(const QString &implementation);
+
+private:
+  QScriptEngine *m_scriptEngine;
+  mutable QScriptValue m_implementationFunction;
+};
+
+class ScriptableReparentingWidget : public QWidget
+{
+  Q_OBJECT
+public:
+  ScriptableReparentingWidget(QAbstractItemModel *rootModel, QWidget* parent = 0, Qt::WindowFlags f = 0);
+
+private slots:
+  void textChanged();
+
+private:
+  ScriptableReparentingProxyModel *m_reparentingProxyModel;
+  QTreeView *m_treeView;
+  QPlainTextEdit *m_textEdit;
+};
+
+
+#endif // SCRIPTABLEREPARENTINGWIDGET_H
