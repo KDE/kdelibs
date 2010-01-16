26373580a03a576c04281003a3cce5a08c31e7d5
Thomas McGuire <mcguire@kde.org>
2010-01-16 18:47:06 +0000

    Add big warning about exec()
    svn path=/trunk/KDE/kdelibs/; revision=1075779

--- kdecore/jobs/kjob.h
+++ kdecore/jobs/kjob.h
@@ -226,6 +226,19 @@ public:
     /**
      * Executes the job synchronously.
      *
+     * This will start a nested QEventLoop internally. Nested event loop can be dangerous and
+     * can have unintended side effects, you should avoid calling exec() whenever you can and use the 
+     * asyncronous interface of KJob instead.
+     *
+     * Should you indeed call this method, you need to make sure that all callers are reentrant,
+     * so that events delivered by the inner event loop don't cause non-reentrant functions to be
+     * called, which usually wreaks havoc.
+     *
+     * Note that the event loop started by this method does not process user input events, which means
+     * your user interface will effectivly be blocked. Other events like paint or network events are 
+     * still being processed. The advantage of not processing user input events is that the chance of
+     * accidental reentrancy is greatly reduced. Still you should avoid calling this function.
+     *
      * @return true if the job has been executed without error, false otherwise
      */
     bool exec();
