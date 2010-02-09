bc86eb1e42494f2105ea4de7f811d4e660efc813
David Faure <faure@kde.org>
2010-02-09 16:04:25 +0000

    Backport: Fix erroneous values due to overflow, patch by Bruce Cran.
    svn path=/branches/KDE/4.4/kdelibs/; revision=1087810

--- kio/kfile/kdiskfreespaceinfo.cpp
+++ kio/kfile/kdiskfreespaceinfo.cpp
@@ -133,8 +133,9 @@ KDiskFreeSpaceInfo KDiskFreeSpaceInfo::freeSpaceInfo( const QString& path )
     // Prefer mountPoint if available, so that it even works with non-existing files.
     const QString pathArg = info.d->mountPoint.isEmpty() ? path : info.d->mountPoint;
     if (!statvfs(QFile::encodeName(pathArg).constData(), &statvfs_buf)) {
-        info.d->available = statvfs_buf.f_bavail * statvfs_buf.f_bsize;
-        info.d->size = statvfs_buf.f_blocks * statvfs_buf.f_bsize;
+        const quint64 blksize = quint64(statvfs_buf.f_bsize); // cast to avoid overflow
+        info.d->available = statvfs_buf.f_bavail * blksize;
+        info.d->size = statvfs_buf.f_blocks * blksize;
         info.d->valid = true;
     }
 #endif
