edbbefd4a3f7daff699232237c1073471f70b128
David Faure <faure@kde.org>
2010-02-11 08:19:05 +0000

    Fix crash in KDirWatch when using a messagebox in a slot connected to dirty(), and in that nested event loop, doing things like getting another notification and removing/adding watches. BUG 220153.
    svn path=/trunk/KDE/kdelibs/; revision=1088544

--- kio/tests/kdirwatch_unittest.cpp
+++ kio/tests/kdirwatch_unittest.cpp
@@ -35,14 +35,21 @@ public:
         m_path = m_tempDir.name();
     }
 
-private Q_SLOTS:
+private Q_SLOTS: // test methods
     void touchOneFile();
+    void watchAndModifyOneFile();
     void touch1000Files();
     void removeAndReAdd();
+    void nestedEventLoop();
 
+protected Q_SLOTS: // internal slots
+    void nestedEventLoopSlot();
+    
 private:
     QList<QVariantList> waitForDirtySignal(KDirWatch& dw, int expected);
-    void createFile(int num);
+    QString createFile(int num);
+    void appendToFile(int num);
+    
     KTempDir m_tempDir;
     QString m_path;
 };
@@ -53,12 +60,23 @@ QTEST_KDEMAIN(KDirWatch_UnitTest, GUI)
 static const char s_filePrefix[] = "This_is_a_test_file_";
 
 // helper method
-void KDirWatch_UnitTest::createFile(int num)
+QString KDirWatch_UnitTest::createFile(int num)
 {
     const QString fileName = s_filePrefix + QString::number(num);
     QFile file(m_path + fileName);
-    QVERIFY(file.open(QIODevice::WriteOnly));
+    bool ok = file.open(QIODevice::WriteOnly);
+    Q_ASSERT(ok);
     file.write(QByteArray("foo"));
+    file.close();
+    return m_path + fileName;
+}
+
+void KDirWatch_UnitTest::appendToFile(int num)
+{
+    const QString fileName = s_filePrefix + QString::number(num);
+    QFile file(m_path + fileName);
+    QVERIFY(file.open(QIODevice::Append | QIODevice::WriteOnly));
+    file.write(QByteArray("foobar"));
     file.close();
 }
 
@@ -78,16 +96,33 @@ QList<QVariantList> KDirWatch_UnitTest::waitForDirtySignal(KDirWatch& watch, int
     return spyDirty;
 }
 
-void KDirWatch_UnitTest::touchOneFile()
+void KDirWatch_UnitTest::touchOneFile() // watch a dir, create a file in it
 {
     KDirWatch watch;
     watch.addDir(m_path);
     watch.startScan();
 
-    createFile(0);
+    const QString file0 = createFile(0);
     QList<QVariantList> spy = waitForDirtySignal(watch, 1);
     QVERIFY(spy.count() >= 1);
-    QCOMPARE(spy[0][0].toString(), m_path + s_filePrefix + "0");
+    QCOMPARE(spy[0][0].toString(), file0);
+}
+
+void KDirWatch_UnitTest::watchAndModifyOneFile() // watch a specific file, and modify it
+{
+    KDirWatch watch;
+    const QString file = createFile(0);
+    watch.addFile(file);
+    watch.startScan();
+
+    // Make sure KDirWatch treats the modification as one
+    QTest::qWait(1000);
+
+    appendToFile(0);
+
+    QList<QVariantList> spy = waitForDirtySignal(watch, 1);
+    QVERIFY(spy.count() >= 1);
+    QCOMPARE(spy[0][0].toString(), file);
 }
 
 void KDirWatch_UnitTest::touch1000Files()
@@ -118,10 +153,56 @@ void KDirWatch_UnitTest::removeAndReAdd()
     // Just like KDirLister does: remove the watch, then re-add it.
     watch.removeDir(m_path);
     watch.addDir(m_path);
-    createFile(1);
+    const QString file1 = createFile(1);
     spy = waitForDirtySignal(watch, 1);
     QVERIFY(spy.count() >= 1);
-    QCOMPARE(spy[0][0].toString(), m_path + s_filePrefix + "1");
+    QCOMPARE(spy[0][0].toString(), file1);
+}
+
+void KDirWatch_UnitTest::nestedEventLoop() // #220153: watch two files, and modify 2nd while in slot for 1st
+{
+    KDirWatch watch;
+
+    const QString file0 = createFile(0);
+    watch.addFile(file0);
+    const QString file1 = createFile(1);
+    watch.addFile(file1);
+    watch.startScan();
+
+    // Make sure KDirWatch treats the modification as one
+    QTest::qWait(1000);
+
+    appendToFile(0);
+
+    // use own spy, to connect it before nestedEventLoopSlot, otherwise it reverses order
+    QSignalSpy spyDirty(&watch, SIGNAL(dirty(QString)));
+    connect(&watch, SIGNAL(dirty(QString)), this, SLOT(nestedEventLoopSlot()));
+    waitForDirtySignal(watch, 1);
+    QVERIFY(spyDirty.count() >= 2);
+    QCOMPARE(spyDirty[0][0].toString(), file0);
+    QCOMPARE(spyDirty[spyDirty.count()-1][0].toString(), file1);
+}
+
+void KDirWatch_UnitTest::nestedEventLoopSlot()
+{
+    const KDirWatch* const_watch = qobject_cast<const KDirWatch *>(sender());
+    KDirWatch* watch = const_cast<KDirWatch *>(const_watch);
+    // let's not come in this slot again
+    disconnect(watch, SIGNAL(dirty(QString)), this, SLOT(nestedEventLoopSlot()));
+
+    appendToFile(1);
+    kDebug() << "now waiting for signal";
+    // The nested event processing here was from a messagebox in #220153
+    QList<QVariantList> spy = waitForDirtySignal(*watch, 1);
+    QVERIFY(spy.count() >= 1);
+    const QString file1 = m_path + s_filePrefix + "1";
+    QCOMPARE(spy[spy.count()-1][0].toString(), file1);
+    kDebug() << "done";
+
+    // Now the user pressed reload...
+    const QString file0 = m_path + s_filePrefix + "0";
+    watch->removeFile(file0);
+    watch->addFile(file0);
 }
 
 #include "kdirwatch_unittest.moc"
