42b135b6e77717e0c47fa5abee8bf730d64ddec9
Jonathan Schmidt-Dominé <devel@the-user.org>
2010-02-19 00:56:47 +0000

    Relicensed as LGPL3+
    svn path=/trunk/playground/devtools/kte_iconinserter/; revision=1092466

--- kate/plugins/kte_iconinserter/iconinserterplugin.h
+++ kate/plugins/kte_iconinserter/iconinserterplugin.h
@@ -1,20 +1,23 @@
-/***************************************************************************
-*   This file is part of the KTextEditor-Icon-Inserter-Plugin             *
-*   Copyright 2009 Jonathan Schmidt-Dominé <devel@the-user.org>           *
-*                                                                         *
-*   This program is free software; you can redistribute it and/or         *
-*   modify it under the terms of the GNU General Public License as        *
-*   published by the Free Software Foundation; either version 3 of        *
-*   the License or (at your option) any later version accepted.           *
-*                                                                         *
-*   This program is distributed in the hope that it will be useful,       *
-*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
-*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
-*   GNU General Public License for more details.                          *
-*                                                                         *
-*   You should have received a copy of the GNU General Public License     *
-*   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
-***************************************************************************/
+/****************************************************************************
+ *   This file is part of the KTextEditor-Icon-Inserter-Plugin              *
+ *   Copyright 2009-2010 Jonathan Schmidt-Dominé <devel@the-user.org>       *
+ *                                                                          *
+ *   This program is free software; you can redistribute it and/or modify   *
+ *   it under the terms of the GNU Library General Public License version   *
+ *   3, or (at your option) any later version, as published by the Free     *
+ *   Software Foundation.                                                   *
+ *                                                                          *
+ *   This library is distributed in the hope that it will be useful, but    *
+ *   WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU      *
+ *   Library General Public License for more details.                       *
+ *                                                                          *
+ *   You should have received a copy of the GNU Library General Public      *
+ *   License along with the kdelibs library; see the file COPYING.LIB. If   *
+ *   not, write to the Free Software Foundation, Inc., 51 Franklin Street,  *
+ *   Fifth Floor, Boston, MA 02110-1301, USA. or see                        *
+ *   <http://www.gnu.org/licenses/>.                                        *
+ ****************************************************************************/
 
 #ifndef KDEVICONINSERTERPLUGIN_H
 #define KDEVICONINSERTERPLUGIN_H
