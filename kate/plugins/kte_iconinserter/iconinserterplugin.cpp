f1d27d4f0c2e7b18acb4359f1ad84d39a94a892f
Jonathan Schmidt-Dominé <devel@the-user.org>
2010-02-19 00:58:07 +0000

    Indentation was wrong.
    svn path=/trunk/playground/devtools/kte_iconinserter/; revision=1092467

--- kate/plugins/kte_iconinserter/iconinserterplugin.cpp
+++ kate/plugins/kte_iconinserter/iconinserterplugin.cpp
@@ -1,23 +1,23 @@
 /****************************************************************************
-*   This file is part of the KTextEditor-Icon-Inserter-Plugin              *
-*   Copyright 2009-2010 Jonathan Schmidt-Dominé <devel@the-user.org>       *
-*                                                                          *
-*   This program is free software; you can redistribute it and/or modify   *
-*   it under the terms of the GNU Library General Public License version   *
-*   3, or (at your option) any later version, as published by the Free     *
-*   Software Foundation.                                                   *
-*                                                                          *
-*   This library is distributed in the hope that it will be useful, but    *
-*   WITHOUT ANY WARRANTY; without even the implied warranty of             *
-*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU      *
-*   Library General Public License for more details.                       *
-*                                                                          *
-*   You should have received a copy of the GNU Library General Public      *
-*   License along with the kdelibs library; see the file COPYING.LIB. If   *
-*   not, write to the Free Software Foundation, Inc., 51 Franklin Street,  *
-*   Fifth Floor, Boston, MA 02110-1301, USA. or see                        *
-*   <http://www.gnu.org/licenses/>.                                        *
-****************************************************************************/
+ *   This file is part of the KTextEditor-Icon-Inserter-Plugin              *
+ *   Copyright 2009-2010 Jonathan Schmidt-Dominé <devel@the-user.org>       *
+ *                                                                          *
+ *   This program is free software; you can redistribute it and/or modify   *
+ *   it under the terms of the GNU Library General Public License version   *
+ *   3, or (at your option) any later version, as published by the Free     *
+ *   Software Foundation.                                                   *
+ *                                                                          *
+ *   This library is distributed in the hope that it will be useful, but    *
+ *   WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU      *
+ *   Library General Public License for more details.                       *
+ *                                                                          *
+ *   You should have received a copy of the GNU Library General Public      *
+ *   License along with the kdelibs library; see the file COPYING.LIB. If   *
+ *   not, write to the Free Software Foundation, Inc., 51 Franklin Street,  *
+ *   Fifth Floor, Boston, MA 02110-1301, USA. or see                        *
+ *   <http://www.gnu.org/licenses/>.                                        *
+ ****************************************************************************/
 
 #include "iconinserterplugin.h"
 
