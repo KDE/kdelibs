1f930fd2df24c3e6f23de9a180210ec1756618fc
Stephen Kelly <steveire@gmail.com>
2010-03-22 13:30:27 +0000

    Update the proxy model test interfaces.
    svn path=/trunk/KDE/kdelibs/; revision=1106302

--- kdeui/tests/proxymodeltestsuite/proxymodeltest.h
+++ kdeui/tests/proxymodeltestsuite/proxymodeltest.h
@@ -49,7 +49,36 @@ enum SourceModel
   IntermediateProxy
 };
 
-class ProxyModelTest : public QObject
+class BuiltinTestDataInterface
+{
+public:
+  virtual ~BuiltinTestDataInterface() { }
+private:
+  virtual void testInsertWhenEmptyData() = 0;
+  virtual void testInsertInRootData() = 0;
+  virtual void testInsertInTopLevelData() = 0;
+  virtual void testInsertInSecondLevelData() = 0;
+};
+
+class BuiltinTestInterface : BuiltinTestDataInterface
+{
+public:
+  virtual ~BuiltinTestInterface() { }
+private:
+  virtual void testInsertWhenEmpty_data() = 0;
+  virtual void testInsertWhenEmpty() = 0;
+
+  virtual void testInsertInRoot_data() = 0;
+  virtual void testInsertInRoot() = 0;
+
+  virtual void testInsertInTopLevel_data() = 0;
+  virtual void testInsertInTopLevel() = 0;
+
+  virtual void testInsertInSecondLevel_data() = 0;
+  virtual void testInsertInSecondLevel() = 0;
+};
+
+class ProxyModelTest : public QObject, protected BuiltinTestInterface
 {
   Q_OBJECT
 public:
@@ -74,10 +103,11 @@ protected:
   void doCleanupTestCase() { cleanupTestCase(); }
   void doCleanup() { cleanup(); }
 
-  void testEmptyModel();
-  void doTestMappings(const QModelIndex &parent);
-  void testSourceReset();
-  void testDestroyModel();
+signals:
+  void testInsertWhenEmptyData();
+  void testInsertInRootData();
+  void testInsertInTopLevelData();
+  void testInsertInSecondLevelData();
 
 protected slots:
   void testMappings();
@@ -90,16 +120,36 @@ private slots:
   void cleanup();
   void cleanupTestCase();
 
+  void testEmptyModel();
+  void testSourceReset();
+  void testDestroyModel();
+
+  void testInsertWhenEmpty_data() { clearDataTags(); testInsertWhenEmptyData(); }
+  void testInsertWhenEmpty() { doTest(); }
+
+  void testInsertInRoot_data() { clearDataTags(); testInsertInRootData(); }
+  void testInsertInRoot() { doTest(); }
+
+  void testInsertInTopLevel_data() { clearDataTags(); testInsertInTopLevelData(); }
+  void testInsertInTopLevel() { doTest(); }
+
+  void testInsertInSecondLevel_data() { clearDataTags(); testInsertInSecondLevelData(); }
+  void testInsertInSecondLevel() { doTest(); }
+
 protected:
   void connectTestSignals(QObject *reciever);
   void disconnectTestSignals(QObject *reciever);
 
   void connectProxy(QAbstractProxyModel *proxyModel);
+  void doTestMappings(const QModelIndex &parent);
+  void initRootModel(DynamicTreeModel *rootModel, const QString &currentTest, const QString &currentTag);
 
   void doTest();
   void handleSignal(QVariantList expected);
   QVariantList getResultSignal();
   int getChange(bool sameParent, int start, int end, int currentPosition, int destinationStart);
+  void clearDataTags() { m_dataTags.clear(); }
+  QStringList dataTags() const { return m_dataTags; }
 
 private:
   DynamicTreeModel *m_rootModel;
@@ -108,7 +158,131 @@ private:
   QAbstractProxyModel *m_intermediateProxyModel;
   ModelSpy *m_modelSpy;
   ModelCommander *m_modelCommander;
-
+  QStringList m_dataTags;
 };
+
+class ProxyModelTestData : public QObject, BuiltinTestDataInterface
+{
+  Q_OBJECT
+public:
+  ProxyModelTestData(ProxyModelTest *parent = 0)
+    : QObject(parent), m_proxyModelTest(parent)
+  {
+  }
+
+protected:
+  void dummyTestData()
+  {
+    QTest::addColumn<SignalList>("signalList");
+    QTest::addColumn<PersistentChangeList>("changeList");
+
+    QTest::newRow("fail01") << SignalList() << PersistentChangeList();
+    QSKIP("Test not implemented", SkipSingle);
+  }
+
+  void processTestName(const QString &name)
+  {
+    if (m_currentTestFunction != QTest::currentTestFunction())
+    {
+      m_testNames.clear();
+      m_currentTestFunction = QTest::currentTestFunction();
+    }
+    m_testNames.insert(name);
+  }
+
+  QStringList namedTests()
+  {
+    return m_testNames.toList();
+  }
+
+  void noopTest(const QString &name)
+  {
+    processTestName(name);
+
+    QTest::newRow(name.toAscii()) << SignalList() << PersistentChangeList();
+  }
+
+  void testForwardingInsertData(const IndexFinder &indexFinder)
+  {
+    QTest::addColumn<SignalList>("signalList");
+    QTest::addColumn<PersistentChangeList>("changeList");
+
+    newInsertTest("insert01", indexFinder, 0, 0, 10);
+    newInsertTest("insert02", indexFinder, 0, 9, 10);
+    newInsertTest("insert03", indexFinder, 10, 10, 10);
+    newInsertTest("insert04", indexFinder, 10, 19, 10);
+    newInsertTest("insert05", indexFinder, 4, 4, 10);
+    newInsertTest("insert06", indexFinder, 4, 13, 10);
+    newInsertTest("insert07", indexFinder, 0, 0, 10);
+    newInsertTest("insert08", indexFinder, 10, 10, 10);
+    newInsertTest("insert09", indexFinder, 4, 4, 10);
+    newInsertTest("insert10", indexFinder, 0, 4, 10);
+    newInsertTest("insert11", indexFinder, 10, 14, 10);
+    newInsertTest("insert12", indexFinder, 4, 8, 10);
+  }
+
+  void newInsertTest(const QString &name, const IndexFinder &indexFinder, int start, int end, int rowCount)
+  {
+    processTestName(name);
+
+    SignalList signalList;
+    PersistentChangeList persistentList;
+
+    signalList << m_proxyModelTest->getSignal(RowsAboutToBeInserted, indexFinder, start, end);
+    signalList << m_proxyModelTest->getSignal(RowsInserted, indexFinder, start, end);
+
+    if (rowCount - 1 + ( end - start + 1 ) > end)
+      persistentList << m_proxyModelTest->getChange( indexFinder, start, rowCount - 1, end - start + 1 );
+
+    QTest::newRow(name.toAscii()) << signalList << persistentList;
+  }
+
+  void noop_testInsertWhenEmptyData()
+  {
+    QTest::addColumn<SignalList>("signalList");
+    QTest::addColumn<PersistentChangeList>("changeList");
+
+    noopTest("insert01");
+    noopTest("insert02");
+    noopTest("insert03");
+  }
+
+  void noop_testInsertInRootData()
+  {
+    QTest::addColumn<SignalList>("signalList");
+    QTest::addColumn<PersistentChangeList>("changeList");
+
+    // These commands have no effect because this model shows children of selection.
+
+    noopTest("insert01");
+    noopTest("insert02");
+    noopTest("insert03");
+    noopTest("insert04");
+    noopTest("insert05");
+    noopTest("insert06");
+    noopTest("insert07");
+    noopTest("insert08");
+    noopTest("insert09");
+    noopTest("insert10");
+    noopTest("insert11");
+    noopTest("insert12");
+  }
+
+  void noop_testInsertInTopLevelData()
+  {
+    // Same test names etc.
+    noop_testInsertInRootData();
+  }
+
+  void noop_testInsertInSecondLevelData()
+  {
+    noop_testInsertInRootData();
+  }
+
+  ProxyModelTest *m_proxyModelTest;
+  QString m_currentTestFunction;
+  QSet<QString> m_testNames;
+};
+
 
 #endif
