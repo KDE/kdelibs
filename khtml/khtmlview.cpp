c7ab0de0bd4efcd37fda4b79df45b77ae26938a8
Pino Toscano <pino@kde.org>
2010-04-17 19:41:05 +0000

    explicit get the pointer out of the smart pointer when adding it to the non-smart-pointer list (fixed building on solaris, for example)
    svn path=/trunk/KDE/kdelibs/; revision=1115870

--- khtml/khtmlview.cpp
+++ khtml/khtmlview.cpp
@@ -3180,7 +3180,7 @@ void KHTMLView::print(bool quick)
     QPointer<KHTMLPrintSettings> printSettings(new KHTMLPrintSettings); //XXX: doesn't save settings between prints like this
     const QPointerDeleter settingsDeleter(printSettings); //the printdialog takes ownership of the settings widget, thus this workaround to avoid double deletion
     QPrinter printer;
-    QPointer<QPrintDialog> dialog = KdePrint::createPrintDialog(&printer, KdePrint::SystemSelectsPages, QList<QWidget*>() << printSettings, this);
+    QPointer<QPrintDialog> dialog = KdePrint::createPrintDialog(&printer, KdePrint::SystemSelectsPages, QList<QWidget*>() << printSettings.data(), this);
 
     const QPointerDeleter dialogDeleter(dialog);
 
