7334ce88a92cdb1cf6ae21e05e5d92d38e6ac5e2
Aurélien Gâteau <agateau@kde.org>
2010-05-21 21:29:07 +0000

    Place donation and knowledgebase links on their own lines
    svn path=/trunk/KDE/kdelibs/; revision=1129301

--- knewstuff/knewstuff3/ui/entrydetailsdialog.cpp
+++ knewstuff/knewstuff3/ui/entrydetailsdialog.cpp
@@ -114,10 +114,10 @@ void EntryDetails::entryChanged(const KNS3::EntryInternal& entry)
                               i18nc("A link to the description of this Get Hot New Stuff item", "Homepage") + "</a>");
 
     if (!m_entry.donationLink().isEmpty()) {
-        homepageText += "  <a href=\"" + m_entry.donationLink() + "\">" + i18nc("A link to make a donation for a Get Hot New Stuff item (opens a web browser)", "Make a donation") + "</a>";
+        homepageText += "<br><a href=\"" + m_entry.donationLink() + "\">" + i18nc("A link to make a donation for a Get Hot New Stuff item (opens a web browser)", "Make a donation") + "</a>";
     }
     if (!m_entry.knowledgebaseLink().isEmpty()) {
-        homepageText += "  <a href=\"" + m_entry.knowledgebaseLink() + "\">" 
+        homepageText += "<br><a href=\"" + m_entry.knowledgebaseLink() + "\">" 
             + i18ncp("A link to the knowledgebase (like a forum) (opens a web browser)", "Knowledgebase (no entries)", "Knowledgebase (%1 entries)", m_entry.numberKnowledgebaseEntries()) + "</a>";
     }
     ui->homepageLabel->setText(homepageText);
