e48611fde3e3c73e60a1e80e5ea7b154c9401bdf
Ben Cooksley <bcooksley@kde.org>
2010-05-22 23:55:00 +0000

    Update KAboutDialog for new website. CCMAIL: andresbajotierra@gmail.com REVIEW: 3411
    svn path=/trunk/KDE/kdelibs/; revision=1129618

--- kdeui/dialogs/kaboutkdedialog_p.cpp
+++ kdeui/dialogs/kaboutkdedialog_p.cpp
@@ -55,14 +55,16 @@ KAboutKdeDialog::KAboutKdeDialog(QWidget *parent)
     about->setTextInteractionFlags(Qt::TextBrowserInteraction);
     about->setText(i18n("<html>"
         "<b>KDE</b> is a world-wide network of software engineers, artists, writers, translators and facilitators "
-        "who are committed to <a href=\"http://www.gnu.org/philosophy/free-sw.html\">Free Software</a> development. "
+        "who are committed to <a href=\"%1\">Free Software</a> development. "
         "This community has created hundreds of Free Software applications as part of the KDE "
         "Development Platform and KDE Software Distribution.<br /><br />"
         "KDE is a cooperative enterprise in which no single entity controls the "
         "efforts or products of KDE to the exclusion of others. Everyone is welcome to join and "
         "contribute to KDE, including you.<br /><br />"
-        "Visit <a href=\"http://www.kde.org/\">http://www.kde.org</a> for "
-        "more information about the KDE community and the software we produce.</html>"));
+        "Visit <a href=\"%2\">%2</a> for "
+        "more information about the KDE community and the software we produce.</html>",
+        QLatin1String("http://www.gnu.org/philosophy/free-sw.html"),
+        QLatin1String("http://www.kde.org/")));
 
     QLabel *report = new QLabel;
     report->setMargin(10);
@@ -75,11 +77,12 @@ KAboutKdeDialog::KAboutKdeDialog(QWidget *parent)
         "do so. However, you - the user - must tell us when "
         "something does not work as expected or could be done better.<br /><br />"
         "KDE has a bug tracking system. Visit "
-        "<a href=\"http://bugs.kde.org/\">http://bugs.kde.org</a> or "
+        "<a href=\"%1\">%1</a> or "
         "use the \"Report Bug...\" dialog from the \"Help\" menu to report bugs.<br /><br />"
         "If you have a suggestion for improvement then you are welcome to use "
         "the bug tracking system to register your wish. Make sure you use the "
-        "severity called \"Wishlist\".</html>" ));
+        "severity called \"Wishlist\".</html>",
+        QLatin1String("https://bugs.kde.org/")));
 
     QLabel *join = new QLabel;
     join->setMargin(10);
@@ -94,12 +97,14 @@ KAboutKdeDialog::KAboutKdeDialog(QWidget *parent)
         "improved documentation. You decide!"
         "<br /><br />"
         "Visit "
-        "<a href=\"http://www.kde.org/jobs/\">http://www.kde.org/jobs/</a> "
+        "<a href=\"%1\">%1</a> "
         "for information on some projects in which you can participate."
         "<br /><br />"
         "If you need more information or documentation, then a visit to "
-        "<a href=\"http://techbase.kde.org/\">http://techbase.kde.org</a> "
-        "will provide you with what you need.</html>"));
+        "<a href=\"%2\">%2</a> "
+        "will provide you with what you need.</html>",
+        QLatin1String("http://www.kde.org/community/getinvolved/"),
+        QLatin1String("http://techbase.kde.org/")));
 
     QLabel *support = new QLabel;
     support->setMargin(10);
@@ -111,7 +116,7 @@ KAboutKdeDialog::KAboutKdeDialog(QWidget *parent)
         "KDE software is and will always be available free of charge, however creating it is not free.<br /><br />"
         "To support development the KDE community has formed the KDE e.V., a non-profit organization "
         "legally founded in Germany. KDE e.V. represents the KDE community in legal and financial matters. "
-        "See <a href=\"http://www.kde-ev.org/\">http://www.kde-ev.org</a>"
+        "See <a href=\"%1\">%1</a>"
         " for information on KDE e.V.<br /><br />"
         "KDE benefits from many kinds of contributions, including financial. "
         "We use the funds to reimburse members and others for expenses "
@@ -119,8 +124,10 @@ KAboutKdeDialog::KAboutKdeDialog(QWidget *parent)
         "support and organizing conferences and meetings. <br /> <br />"
         "We would like to encourage you to support our efforts with a "
         "financial donation, using one of the ways described at "
-        "<a href=\"http://www.kde.org/support/\">http://www.kde.org/support/</a>."
-        "<br /><br />Thank you very much in advance for your support.</html>") + "<br /><br />"); // FIXME: ugly <br /> at the end...
+        "<a href=\"%2\">%2</a>."
+        "<br /><br />Thank you very much in advance for your support.</html>",
+        QLatin1String("http://ev.kde.org/"),
+        QLatin1String("http://www.kde.org/community/donations/")) + "<br /><br />"); // FIXME: ugly <br /> at the end...
 
     QTabWidget *tabWidget = new QTabWidget;
     tabWidget->setUsesScrollButtons(false);
