KDE/kdelibs

commit 228e83f176fd31414a888e4fdab3ca136d4592dc

BUG: 98790 Correctly handle CHARSET attribute in vcard parsing and handle given string as plain C-byte array
svn path=/branches/KDE/3.5/kdelibs/; revision=758100

kabc/vcardparser/vcardparser.cpp
@@ -19,6 +19,7 @@
 */
 
 #include <qregexp.h>
+#include <qtextcodec.h>
 
 #include <kmdcodec.h>
 
@@ -129,31 +130,44 @@ VCard::List VCardParser::parseVCards( const QString& text )
 
         removeEscapes( value );
 
+        QByteArray output;
+        bool wasBase64Encoded = false;
+
         params = vCardLine.parameterList();
         if ( params.findIndex( "encoding" ) != -1 ) { // have to decode the data
-          QByteArray input, output;
-          input = value.local8Bit();
+          QByteArray input;
+          input = QCString(value.latin1());
           if ( vCardLine.parameter( "encoding" ).lower() == "b" ||
-               vCardLine.parameter( "encoding" ).lower() == "base64" )
+               vCardLine.parameter( "encoding" ).lower() == "base64" ) {
             KCodecs::base64Decode( input, output );
+            wasBase64Encoded = true;
+          }
           else if ( vCardLine.parameter( "encoding" ).lower() == "quoted-printable" ) {
             // join any qp-folded lines
             while ( value.at( value.length() - 1 ) == '=' && it != linesEnd ) {
               value = value.remove( value.length() - 1, 1 ) + (*it);
               ++it;
             }
-            input = value.local8Bit();
+            input = QCString(value.latin1());
             KCodecs::quotedPrintableDecode( input, output );
           }
-          if ( vCardLine.parameter( "charset" ).lower() == "utf-8" ) {
-            vCardLine.setValue( QString::fromUtf8( output.data(), output.size() ) );
+        } else {
+          output = QCString(value.latin1());
+        }
+
+        if ( params.findIndex( "charset" ) != -1 ) { // have to convert the data
+          QTextCodec *codec =
+            QTextCodec::codecForName( vCardLine.parameter( "charset" ).latin1() );
+          if ( codec ) {
+            vCardLine.setValue( codec->toUnicode( output ) );
           } else {
-            vCardLine.setValue( output );
+            vCardLine.setValue( QString::fromUtf8( output ) );
           }
-        } else if ( vCardLine.parameter( "charset" ).lower() == "utf-8" ) {
-          vCardLine.setValue( QString::fromUtf8( value.ascii() ) );
-        } else
-          vCardLine.setValue( value );
+        } else if ( wasBase64Encoded ) {
+            vCardLine.setValue( output );
+        } else {  // if charset not given, assume it's in UTF-8 (as used in previous KDE versions)
+            vCardLine.setValue( QString::fromUtf8( output ) );
+        }
 
         currentVCard.addLine( vCardLine );
       }

kabc/vcardformatplugin.cpp
@@ -41,7 +41,7 @@ bool VCardFormatPlugin::load( Addressee &addressee, QFile *file )
   QString data;
 
   QTextStream t( file );
-  t.setEncoding( QTextStream::UnicodeUTF8 );
+  t.setEncoding( QTextStream::Latin1 );
   data = t.read();
 
   VCardConverter converter;
@@ -60,7 +60,7 @@ bool VCardFormatPlugin::loadAll( AddressBook*, Resource *resource, QFile *file )
   QString data;
 
   QTextStream t( file );
-  t.setEncoding( QTextStream::UnicodeUTF8 );
+  t.setEncoding( QTextStream::Latin1 );
   data = t.read();
 
   VCardConverter converter;

kabc/vcardparser/testread.cpp
@@ -70,7 +70,7 @@ int main( int argc, char **argv )
   QString text;
 
   QTextStream s( &file );
-  s.setEncoding( QTextStream::UnicodeUTF8 );
+  s.setEncoding( QTextStream::Latin1 );
   text = s.read();
   file.close();
 

commit b47c845ccdebcfd6f0fb6117a4c6545d3172820d

merge SVN commit 1097892 by tmcguire from 3.5 branch:
Fix regression: Use fromLatin1() instead of fromUtf8(), since we use latin1() above.
This worked previously, because local8bit() was used, which happened to be utf-8, but
got broken in r758100.

Of course the whole thing is horrible broken anyway, since it is limited to latin1 characters
and converts a QString to QCString and back, but at least this improves things a bit.

kolab/issue4054


svn path=/branches/kdepim/enterprise/kdelibs/; revision=1132145

kabc/vcardparser/vcardparser.cpp
@@ -161,12 +161,12 @@ VCard::List VCardParser::parseVCards( const QString& text )
           if ( codec ) {
             vCardLine.setValue( codec->toUnicode( output ) );
           } else {
-            vCardLine.setValue( QString::fromUtf8( output ) );
+            vCardLine.setValue( QString::fromLatin1( output ) );
           }
         } else if ( wasBase64Encoded ) {
             vCardLine.setValue( output );
-        } else {  // if charset not given, assume it's in UTF-8 (as used in previous KDE versions)
-            vCardLine.setValue( QString::fromUtf8( output ) );
+        } else {
+            vCardLine.setValue( QString::fromLatin1( output ) );
         }
 
         currentVCard.addLine( vCardLine );