f37a7d3ae2867e0470f97ce1ad7f68aead3c8f06
Allen Winter <winter@kde.org>
2010-05-29 21:33:37 +0000

    merge SVN commit 987597 by tmcguire from 3.5 branch:
    Fix passwords with '!' in them here as well.
    
    
    svn path=/branches/kdepim/enterprise/kdelibs/; revision=1132140

--- kdecore/kstringhandler.cpp
+++ kdecore/kstringhandler.cpp
@@ -558,7 +558,7 @@ QString KStringHandler::obscure( const QString &str )
   QString result;
   const QChar *unicode = str.unicode();
   for ( uint i = 0; i < str.length(); ++i )
-    result += ( unicode[ i ].unicode() < 0x21 ) ? unicode[ i ] :
+    result += ( unicode[ i ].unicode() <= 0x21 ) ? unicode[ i ] :
         QChar( 0x1001F - unicode[ i ].unicode() );
 
   return result;
