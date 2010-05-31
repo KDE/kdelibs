2322491628cc75b8210abbde2d8c8baf0745a6b0
Kevin Ottens <ervin@kde.org>
2010-05-31 10:46:01 +0000

    Be much more aggressive about using the symbols coming from the other libraries split out from the old libkutils.
    Otherwise for some reason some new linker manage to avoid depending on
    libkcmutils and friends. You just gotta love when linker behavior change
    behind your back. ;-)
    
    BUG: 240120
    
    
    svn path=/trunk/KDE/kdelibs/; revision=1132757

--- kutils/dummy.cpp
+++ kutils/dummy.cpp
@@ -8,6 +8,15 @@ Q_GLOBAL_STATIC_WITH_ARGS( KPrintPreview, bar, ( 0, 0 ) )
 Q_GLOBAL_STATIC( KEmoticons, foobar )
 Q_GLOBAL_STATIC( KIdleTime*, baz )
 
+void _k__get_dummy_used()
+{
+    foo();
+    bar();
+    foobar();
+    baz();
+    KIdleTime::instance();
+}
+
 #ifdef Q_WS_WIN
 __declspec(dllexport) int __dummy_function() { return 5; }
 #endif
