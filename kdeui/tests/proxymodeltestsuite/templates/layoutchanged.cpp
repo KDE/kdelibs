3ea5526655e070951c5820f29a8ad7b226abab57
Stephen Kelly <steveire@gmail.com>
2010-07-02 09:34:43 +0000

    Fix the generated code for layout changes somewhat.
    svn path=/trunk/KDE/kdelibs/; revision=1145230

--- kdeui/tests/proxymodeltestsuite/templates/layoutchanged.cpp
+++ kdeui/tests/proxymodeltestsuite/templates/layoutchanged.cpp
@@ -1,1 +1,10 @@
-1
+
+  {
+    ModelLayoutChangeCommand layoutChangeCommand(&rootModel);
+    layoutChangeCommand.setInitialTree({{ event.interpretString }}
+    );
+    QList<ModelLayoutChangeCommand::PersistentChange> changes;
+    // TODO: generate code for changes.
+    layoutChangeCommand.setChanges(changes);
+    layoutChangeCommand.doCommand();
+  }
