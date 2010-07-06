913081d62adfcb0b8482a724211e296512a4f6ce
Sebastian Trueg <sebastian@trueg.de>
2010-07-06 17:20:12 +0000

    Converted KEditListBox into KEditListWidget which is derived from QWidget instead of QGroupBox. It makes for a cleaner API and allows to use the list widget in any layout necessary.
    svn path=/trunk/KDE/kdelibs/; revision=1146793

--- kdeui/widgets/keditlistwidget.h
+++ kdeui/widgets/keditlistwidget.h
@@ -1,5 +1,6 @@
 /* This file is part of the KDE libraries
     Copyright (C) 2000 David Faure <faure@kde.org>, Alexander Neundorf <neundorf@kde.org>
+              (C) 2010 Sebastian Trueg <trueg@kde.org>
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Library General Public
@@ -17,12 +18,12 @@
     Boston, MA 02110-1301, USA.
 */
 
-#ifndef KEDITLISTBOX_H
-#define KEDITLISTBOX_H
+#ifndef KEDITLISTWIDGET_H
+#define KEDITLISTWIDGET_H
 
 #include <kdeui_export.h>
 
-#include <QtGui/QGroupBox>
+#include <QtGui/QWidget>
 #include <QtGui/QStringListModel>
 
 class KLineEdit;
@@ -30,13 +31,21 @@ class KComboBox;
 class QListView;
 class QPushButton;
 
-class KEditListBoxPrivate;
+class KEditListWidgetPrivate;
 /**
- * @brief An editable listbox
+ * An editable listbox
  *
- * @deprecated in favor of KEditListWidget embedded in a QGroupBox.
+ * This class provides an editable listbox, this means
+ * a listbox which is accompanied by a line edit to enter new
+ * items into the listbox and pushbuttons to add and remove
+ * items from the listbox and two buttons to move items up and down.
+ *
+ * \image html keditlistbox.png "KDE Edit List Box Widget"
+ *
+ *
+ * @since 4.6
  */
-class KDEUI_EXPORT KEditListBox : public QGroupBox
+class KDEUI_EXPORT KEditListWidget : public QWidget
 {
    Q_OBJECT
 
@@ -89,48 +98,10 @@ public:
       /**
        * Create an editable listbox.
        */
-      explicit KEditListBox(QWidget *parent = 0);
+      explicit KEditListWidget(QWidget *parent = 0);
 
       /**
-       * Create an editable listbox.
-       *
-       * The same as the other constructor, additionally it takes
-       * @p title, which will be the title of the groupbox around the listbox.
-       */
-      explicit KEditListBox(const QString& title, QWidget *parent = 0);
-
-// ### KDE5: remove name arguments and simplify (merge?!) constructors
-
-      /**
-       * Create an editable listbox.
-       *
-       * \deprecated
-       *
-       * If @p checkAtEntering is true, after every character you type
-       * in the line edit KEditListBox will enable or disable
-       * the Add-button, depending whether the current content of the
-       * line edit is already in the listbox. Maybe this can become a
-       * performance hit with large lists on slow machines.
-       * If @p checkAtEntering is false,
-       * it will be checked if you press the Add-button. It is not
-       * possible to enter items twice into the listbox.
-       */
-      explicit KDE_DEPRECATED KEditListBox(QWidget *parent, const char *name,
-                            bool checkAtEntering = false, Buttons buttons = All );
-      /**
-       * Create an editable listbox.
-       *
-       * \deprecated
-       *
-       * The same as the other constructor, additionally it takes
-       * @p title, which will be the title of the frame around the listbox.
-       */
-      explicit KDE_DEPRECATED KEditListBox(const QString& title, QWidget *parent,
-                            const char *name, bool checkAtEntering = false,
-                            Buttons buttons = All );
-
-      /**
-       * Another constructor, which allows to use a custom editing widget
+       * Constructor which allows to use a custom editing widget
        * instead of the standard KLineEdit widget. E.g. you can use a
        * KUrlRequester or a KComboBox as input widget. The custom
        * editor must consist of a lineedit and optionally another widget that
@@ -140,12 +111,12 @@ public:
        *
        * @see KUrlRequester::customEditor(), setCustomEditor
        */
-      KEditListBox( const QString& title,
-                    const CustomEditor &customEditor,
-                    QWidget *parent = 0, const char *name = 0,
-                    bool checkAtEntering = false, Buttons buttons = All );
+      KEditListWidget( const CustomEditor &customEditor,
+                       QWidget *parent = 0,
+                       bool checkAtEntering = false,
+                       Buttons buttons = All );
 
-      virtual ~KEditListBox();
+      virtual ~KEditListWidget();
 
       /**
        * Return a pointer to the embedded QListView.
@@ -223,7 +194,7 @@ public:
 
       /**
        * If @p check is true, after every character you type
-       * in the line edit KEditListBox will enable or disable
+       * in the line edit KEditListWidget will enable or disable
        * the Add-button, depending whether the current content of the
        * line edit is already in the listbox. Maybe this can become a
        * performance hit with large lists on slow machines.
@@ -247,8 +218,6 @@ public:
        * is used as representation. A KComboBox or a KUrlRequester have a
        * KLineEdit as child-widget for example, so the KComboBox is used as
        * the representation widget.
-       *
-       * @since 4.1
        */
       void setCustomEditor( const CustomEditor& editor );
 
@@ -272,24 +241,22 @@ public:
        */
       void removed( const QString & text );
 
-   protected Q_SLOTS: // KDE5: make private?
+   private Q_SLOTS:
       void moveItemUp();
       void moveItemDown();
       void addItem();
       void removeItem();
       void enableMoveButtons(const QModelIndex&, const QModelIndex&);
       void typedSomething(const QString& text);
-
-   private Q_SLOTS:
       void slotSelectionChanged( const QItemSelection& selected, const QItemSelection& deselected );
 
    private:
-      friend class KEditListBoxPrivate;
-      KEditListBoxPrivate* const d;
+      friend class KEditListWidgetPrivate;
+      KEditListWidgetPrivate* const d;
 
-      Q_DISABLE_COPY(KEditListBox)
+      Q_DISABLE_COPY(KEditListWidget)
 };
 
-Q_DECLARE_OPERATORS_FOR_FLAGS(KEditListBox::Buttons)
+Q_DECLARE_OPERATORS_FOR_FLAGS(KEditListWidget::Buttons)
 
 #endif
