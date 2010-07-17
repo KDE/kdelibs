eb0672c9e326ca7ad08c90f3adb817af501b75cf
Max Brazhnikov <makc@freebsd.org>
2010-07-17 09:33:05 +0000

    Backport 1150972 from trunk:
    Use frsize to calculate free space, thus fix reported value for filesystems where frsize != bsize
    
    Review request:	http://reviewboard.kde.org/r/4551/
    Approved by:	Sebastian Trueg
    
    svn path=/branches/KDE/4.5/kdelibs/; revision=1150973

--- kio/kfile/kdiskfreespaceinfo.cpp
+++ kio/kfile/kdiskfreespaceinfo.cpp
@@ -133,7 +133,7 @@ KDiskFreeSpaceInfo KDiskFreeSpaceInfo::freeSpaceInfo( const QString& path )
     // Prefer mountPoint if available, so that it even works with non-existing files.
     const QString pathArg = info.d->mountPoint.isEmpty() ? path : info.d->mountPoint;
     if (!statvfs(QFile::encodeName(pathArg).constData(), &statvfs_buf)) {
-        const quint64 blksize = quint64(statvfs_buf.f_bsize); // cast to avoid overflow
+        const quint64 blksize = quint64(statvfs_buf.f_frsize); // cast to avoid overflow
         info.d->available = statvfs_buf.f_bavail * blksize;
         info.d->size = statvfs_buf.f_blocks * blksize;
         info.d->valid = true;
