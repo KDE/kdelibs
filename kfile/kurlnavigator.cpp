KDE/kdelibs

commit 6b3b91388f1aea623f473cd83c724110274489c9

Do not switch the old-style URL combo box back to breadcrumb navigation mode if the Escape key is pressed while the combo box is open.
Review at http://reviewboard.kde.org/r/3613/

BUG:200842

svn path=/trunk/KDE/kdelibs/; revision=1115714

kfile/kurlnavigator.cpp
@@ -1152,11 +1152,12 @@ void KUrlNavigator::savePosition(int x, int y)
     d->m_history[d->m_historyIndex].pos = QPoint(x, y);
 }
 
-void KUrlNavigator::keyReleaseEvent(QKeyEvent* event)
+void KUrlNavigator::keyPressEvent(QKeyEvent* event)
 {
-    QWidget::keyReleaseEvent(event);
     if (isUrlEditable() && (event->key() == Qt::Key_Escape)) {
         setUrlEditable(false);
+    } else {
+        QWidget::keyPressEvent(event);
     }
 }
 

kfile/kurlnavigator.h
@@ -424,9 +424,9 @@ protected:
     /*
      * If the Escape key is pressed, the navigation bar should switch
      * to the breadcrumb view.
-     * @see QWidget::keyReleaseEvent()
+     * @see QWidget::keyPressEvent()
      */
-    virtual void keyReleaseEvent(QKeyEvent* event);
+    virtual void keyPressEvent(QKeyEvent* event);
 
     /*
      * Paste the clipboard content as URL, if the middle mouse

commit 5e2d2becb4a0634aaaaf06af1ec7dce33a3b17c4

Restore binary compatibility, which got broken in r1115714 (reviewboard 3613). /usr/lib/python2.6/site-packages/PyKDE4/kio.so: undefined symbol: _ZN13KUrlNavigator15keyReleaseEventEP9QKeyEvent CCMAIL: jjm@keelhaul.me.uk, peter.penz@gmx.at
svn path=/trunk/KDE/kdelibs/; revision=1155117

kfile/kurlnavigator.cpp
@@ -1095,6 +1095,11 @@ void KUrlNavigator::keyPressEvent(QKeyEvent* event)
     }
 }
 
+void KUrlNavigator::keyReleaseEvent(QKeyEvent* event)
+{
+    QWidget::keyReleaseEvent(event);
+}
+
 void KUrlNavigator::mouseReleaseEvent(QMouseEvent* event)
 {
     if (event->button() == Qt::MidButton) {

kfile/kurlnavigator.h
@@ -428,6 +428,11 @@ protected:
      */
     virtual void keyPressEvent(QKeyEvent* event);
 
+    /**
+     * Reimplemented for internal purposes
+     */
+    virtual void keyReleaseEvent(QKeyEvent* event);
+
     /*
      * Paste the clipboard content as URL, if the middle mouse
      * button has been clicked.