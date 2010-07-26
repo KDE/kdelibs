0d253314f3a0f144fd92d23f740e1d3895a78f5d
Jonathan Schmidt-Dominé <devel@the-user.org>
2010-07-26 14:10:47 +0000

    Changed ktypelistutils license header
    svn path=/trunk/KDE/kdelibs/; revision=1154914

--- kdecore/util/ktypelistutils.h
+++ kdecore/util/ktypelistutils.h
@@ -3,7 +3,8 @@
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
-   License version 2 as published by the Free Software Foundation.
+   License version 2 as published by the Free Software Foundation,
+   or - at your option - any later version.
 
    This library is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
