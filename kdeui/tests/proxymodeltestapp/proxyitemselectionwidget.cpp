fee4fa52468bd79d115c1ca4f46ac11b7930da8d
Stephen Kelly <steveire@gmail.com>
2010-08-05 12:49:38 +0000

    Fix a nasty off-by-one in the index mapper.
    svn path=/trunk/KDE/kdelibs/; revision=1159470

--- kdeui/tests/proxymodeltestapp/proxyitemselectionwidget.cpp
+++ kdeui/tests/proxymodeltestapp/proxyitemselectionwidget.cpp
@@ -43,6 +43,20 @@ ProxyItemSelectionWidget::ProxyItemSelectionWidget(QWidget* parent, Qt::WindowFl
 
   DynamicTreeWidget *dynamicTreeWidget = new DynamicTreeWidget(rootModel, splitter);
 
+  dynamicTreeWidget->setInitialTree(
+    "- 1"
+    "- 2"
+    "- - 3"
+    "- - - 4"
+    "- 5"
+    "- 6"
+    "- 7"
+  );
+
+  QSplitter *vSplitter = new QSplitter(Qt::Vertical, splitter);
+  QSplitter *hSplitter1 = new QSplitter(vSplitter);
+  QSplitter *hSplitter2 = new QSplitter(vSplitter);
+
   QSortFilterProxyModel *proxy1 = new QSortFilterProxyModel(this);
   SON(proxy1);
   QSortFilterProxyModel *proxy2 = new QSortFilterProxyModel(this);
@@ -54,8 +68,10 @@ ProxyItemSelectionWidget::ProxyItemSelectionWidget(QWidget* parent, Qt::WindowFl
   QSortFilterProxyModel *proxy5 = new QSortFilterProxyModel(this);
   SON(proxy5);
 
-  QTreeView *view1 = new QTreeView(splitter);
-  QTreeView *view2 = new QTreeView(splitter);
+  QTreeView *view1 = new QTreeView(hSplitter1);
+  QTreeView *view2 = new QTreeView(hSplitter1);
+  QTreeView *view3 = new QTreeView(hSplitter2);
+  QTreeView *view4 = new QTreeView(hSplitter2);
 
   proxy1->setSourceModel( rootModel );
   proxy2->setSourceModel( proxy1 );
@@ -66,6 +82,8 @@ ProxyItemSelectionWidget::ProxyItemSelectionWidget(QWidget* parent, Qt::WindowFl
 
   view1->setModel( proxy3 );
   view2->setModel( proxy5 );
+  view3->setModel( proxy2 );
+  view4->setModel( proxy1 );
 
   QItemSelectionModel *rootSelectionModel = dynamicTreeWidget->treeView()->selectionModel();
 
@@ -75,7 +93,12 @@ ProxyItemSelectionWidget::ProxyItemSelectionWidget(QWidget* parent, Qt::WindowFl
   KLinkItemSelectionModel *view2SelectionModel = new KLinkItemSelectionModel(view2->model(), view1->selectionModel(), this );
   view2->setSelectionModel( view2SelectionModel );
 
+  KLinkItemSelectionModel *view3SelectionModel = new KLinkItemSelectionModel(view3->model(), view4->selectionModel(), this );
+  view3->setSelectionModel( view3SelectionModel );
+
   view1->expandAll();
   view2->expandAll();
+  view3->expandAll();
+  view4->expandAll();
 }
 
