KDE/kdelibs

commit 61e15d880f4cd17ff1f3543f6711c59bdc8b729a

Remove qt3-time QListBox workarounds, and use QListWidget's setCurrentRow(-1) instead. Simplify the code in up(). Remove the wrong code in KLineEdit which assumed the old completionbox "first item but unselected" hack instead of -1.  (this broke "key down" in some cases on the first item, as reported by Fredrikh) -> with more unittests
svn path=/trunk/KDE/kdelibs/; revision=1121183

kdeui/widgets/klineedit.cpp
@@ -1510,32 +1510,20 @@ void KLineEdit::setCompletedItems( const QStringList& items, bool autoSuggest )
         {
             QListWidgetItem* currentItem = d->completionBox->currentItem();
 
-            bool wasSelected = false;
             QString currentSelection;
-
             if ( currentItem != 0 ) {
-                wasSelected = currentItem->isSelected();
                 currentSelection = currentItem->text();
             }
 
             d->completionBox->setItems( items );
 
-            const QList<QListWidgetItem*> matchedItems = d->completionBox->findItems( currentSelection , Qt::MatchExactly);
+            const QList<QListWidgetItem*> matchedItems = d->completionBox->findItems(currentSelection, Qt::MatchExactly);
             QListWidgetItem* matchedItem = matchedItems.isEmpty() ? 0 : matchedItems.first();
 
-            // If no item is selected, that means the listbox hasn't been manipulated by the user yet,
-            // because it's not possible otherwise to have no selected item. In such case make
-            // always the first item current and unselected, so that the current item doesn't jump.
-            if( !matchedItem || !wasSelected )
-            {
-                wasSelected = false;
-                matchedItem = d->completionBox->item( 0 );
-            }
             if ( matchedItem )
             {
                 const bool blocked = d->completionBox->blockSignals( true );
                 d->completionBox->setCurrentItem( matchedItem );
-                matchedItem->setSelected(wasSelected);
                 d->completionBox->blockSignals( blocked );
             }
         }
@@ -1551,7 +1539,7 @@ void KLineEdit::setCompletedItems( const QStringList& items, bool autoSuggest )
         {
             const int index = items.first().indexOf( txt );
             const QString newText = items.first().mid( index );
-            setUserSelection(false);
+            setUserSelection(false); // can be removed? setCompletedText sets it anyway
             setCompletedText(newText,true);
         }
     }

kdeui/tests/klineedit_unittest.cpp
@@ -92,7 +92,7 @@ private Q_SLOTS:
         // the suggestion from auto completion emits textChanged but not userTextChanged nor textEdited
         w.setCompletionMode(KGlobalSettings::CompletionAuto);
         KCompletion completion;
-	completion.setSoundsEnabled(false);
+        completion.setSoundsEnabled(false);
         QStringList items;
         items << "KDE is cool" << "KDE is really cool";
         completion.setItems(items);
@@ -169,6 +169,35 @@ private Q_SLOTS:
         QCOMPARE(activatedSpy.count(), 1);
         QCOMPARE(w.text(), items.at(1));
         QVERIFY(w.isModified());
+    }
+
+    void testCompletionBox()
+    {
+        KLineEdit w;
+        w.setText("K");
+        w.setCompletionMode(KGlobalSettings::CompletionPopup);
+        KCompletion completion;
+        completion.setSoundsEnabled(false);
+        w.setCompletionObject(&completion);
+        QStringList items;
+        items << "KDE is cool" << "KDE is really cool";
+        completion.setItems(items);
+        QTest::keyClick(&w, 'D', Qt::ShiftModifier);
+        QCOMPARE(w.text(), QString::fromLatin1("KD"));
+        QCOMPARE(w.completionBox()->currentRow(), -1);
+        QTest::keyClick(&w, 'E', Qt::ShiftModifier);
+        QCOMPARE(w.text(), QString::fromLatin1("KDE"));
+        QCOMPARE(w.completionBox()->currentRow(), -1);
+        w.completionBox()->up(); // no-op
+        QCOMPARE(w.text(), QString::fromLatin1("KDE"));
+        w.completionBox()->down(); // select 1st item
+        QCOMPARE(w.text(), items.at(0));
+        w.completionBox()->down(); // select 2nd item
+        QCOMPARE(w.text(), items.at(1));
+        w.completionBox()->up();
+        QCOMPARE(w.text(), items.at(0));
+        w.completionBox()->up(); // no-op
+        QCOMPARE(w.text(), items.at(0));
     }
 
     void testPaste()

kdeui/widgets/kcompletionbox.cpp
@@ -41,7 +41,6 @@ public:
     QWidget *m_parent; // necessary to set the focus back
     QString cancelText;
     bool tabHandling : 1;
-    bool down_workaround : 1; // next call to down() selects the first item
     bool upwardBox : 1;
     bool emitSelected : 1;
 };
@@ -51,7 +50,6 @@ KCompletionBox::KCompletionBox( QWidget *parent )
 {
     d->m_parent        = parent;
     d->tabHandling     = true;
-    d->down_workaround = false;
     d->upwardBox       = false;
     d->emitSelected    = true;
 
@@ -65,8 +63,6 @@ KCompletionBox::KCompletionBox( QWidget *parent )
 
     connect( this, SIGNAL( itemDoubleClicked( QListWidgetItem * )),
              SLOT( slotActivated( QListWidgetItem * )) );
-    connect( this, SIGNAL( currentItemChanged( QListWidgetItem * , QListWidgetItem * )),
-             SLOT( slotCurrentChanged() ));
     connect( this, SIGNAL( itemClicked( QListWidgetItem * )),
              SLOT( slotItemClicked( QListWidgetItem * )) );
 }
@@ -267,10 +263,9 @@ void KCompletionBox::popup()
     if ( count() == 0 )
         hide();
     else {
-        //TODO KDE 4 - Port: ensureCurrentVisible();
         bool block = signalsBlocked();
         blockSignals( true );
-        setCurrentItem( 0 );
+        setCurrentRow( -1 );
         blockSignals( block );
         clearSelection();
         if ( !isVisible() )
@@ -408,22 +403,18 @@ QSize KCompletionBox::sizeHint() const
 
 void KCompletionBox::down()
 {
-    int i = currentRow();
-
-    if ( i == 0 && d->down_workaround ) {
-        d->down_workaround = false;
-        setCurrentRow( 0 );
-        item(0)->setSelected(true);
-    }
-    else if ( i < (int) count() - 1 ) {
-        setCurrentRow( i + 1 );
+    const int i = currentRow();
+    if (i < count() - 1) {
+        setCurrentRow(i + 1);
     }
 }
 
 void KCompletionBox::up()
 {
-    if ( currentItem() && row(currentItem()) > 0 )
-        setCurrentItem( item(row(currentItem()) - 1) );
+    const int i = currentRow();
+    if (i > 0) {
+        setCurrentRow(i - 1);
+    }
 }
 
 void KCompletionBox::pageDown()
@@ -445,7 +436,7 @@ void KCompletionBox::pageUp()
 
 void KCompletionBox::home()
 {
-    setCurrentItem( 0 );
+    setCurrentRow( 0 );
 }
 
 void KCompletionBox::end()
@@ -501,7 +492,7 @@ void KCompletionBox::insertItems( const QStringList& items, int index )
     blockSignals( true );
     KListWidget::insertItems( index, items );
     blockSignals( block );
-    d->down_workaround = true;
+    setCurrentRow(-1);
 }
 
 void KCompletionBox::setItems( const QStringList& items )
@@ -555,19 +546,12 @@ void KCompletionBox::setItems( const QStringList& items )
         sizeAndPosition();
 
     blockSignals(block);
-    d->down_workaround = true;
-}
-
-void KCompletionBox::slotCurrentChanged()
-{
-    d->down_workaround = false;
 }
 
 void KCompletionBox::slotItemClicked( QListWidgetItem *item )
 {
     if ( item )
     {
-        d->down_workaround = false;
         hide();
         emit currentTextChanged( item->text() );
         emit activated( item->text() );

commit 9ef599d153331ea568973a5a763f2daff0e246b4

Unselect the current item when setting new completion items, if it is nowhere in the new items. Regression introduced in r1121183. Unittested now, won't happen again ;) FIXED-IN: 4.5.1 Thanks for the precise bug report. BUG: 247552
svn path=/trunk/KDE/kdelibs/; revision=1164472

kdeui/widgets/klineedit.cpp
@@ -1364,7 +1364,7 @@ bool KLineEdit::event( QEvent* ev )
 
             kleStyle->m_subStyle = style();
             // this guards against "wrap around" where another style, e.g. QStyleSheetStyle,
-            // is setting the style on QEvent::StyleChange 
+            // is setting the style on QEvent::StyleChange
             d->lastStyleClass = QLatin1String(style()->metaObject()->className());
             setStyle(kleStyle);
             d->lastStyleClass.clear();
@@ -1551,11 +1551,12 @@ void KLineEdit::setCompletedItems( const QStringList& items, bool autoSuggest )
             const QList<QListWidgetItem*> matchedItems = d->completionBox->findItems(currentSelection, Qt::MatchExactly);
             QListWidgetItem* matchedItem = matchedItems.isEmpty() ? 0 : matchedItems.first();
 
-            if ( matchedItem )
-            {
+            if (matchedItem) {
                 const bool blocked = d->completionBox->blockSignals( true );
                 d->completionBox->setCurrentItem( matchedItem );
                 d->completionBox->blockSignals( blocked );
+            } else {
+                d->completionBox->setCurrentRow(-1);
             }
         }
         else // completion box not visible yet -> show it

kdeui/tests/klineedit_unittest.cpp
@@ -174,30 +174,41 @@ private Q_SLOTS:
     void testCompletionBox()
     {
         KLineEdit w;
-        w.setText("K");
+        w.setText("/");
         w.setCompletionMode(KGlobalSettings::CompletionPopup);
         KCompletion completion;
         completion.setSoundsEnabled(false);
         w.setCompletionObject(&completion);
         QStringList items;
-        items << "KDE is cool" << "KDE is really cool";
+        items << "/home/" << "/hold/";
         completion.setItems(items);
-        QTest::keyClick(&w, 'D', Qt::ShiftModifier);
-        QCOMPARE(w.text(), QString::fromLatin1("KD"));
+        QTest::keyClick(&w, 'h');
+        QCOMPARE(w.text(), QString::fromLatin1("/h"));
         QCOMPARE(w.completionBox()->currentRow(), -1);
-        QTest::keyClick(&w, 'E', Qt::ShiftModifier);
-        QCOMPARE(w.text(), QString::fromLatin1("KDE"));
+        QCOMPARE(w.completionBox()->items(), items);
+        QTest::keyClick(&w, 'o');
+        QCOMPARE(w.text(), QString::fromLatin1("/ho"));
         QCOMPARE(w.completionBox()->currentRow(), -1);
         w.completionBox()->up(); // no-op
-        QCOMPARE(w.text(), QString::fromLatin1("KDE"));
+        QCOMPARE(w.text(), QString::fromLatin1("/ho"));
         w.completionBox()->down(); // select 1st item
         QCOMPARE(w.text(), items.at(0));
         w.completionBox()->down(); // select 2nd item
         QCOMPARE(w.text(), items.at(1));
-        w.completionBox()->up();
+        w.completionBox()->up();   // select 1st item again
         QCOMPARE(w.text(), items.at(0));
         w.completionBox()->up(); // no-op
         QCOMPARE(w.text(), items.at(0));
+
+        QStringList newItems;
+        newItems << "/home/kde";
+        completion.setItems(newItems);
+        QTest::keyClick(&w, 'k');
+        QCOMPARE(w.text(), QString("/home/k"));
+        //QCOMPARE(w.completionBox()->currentRow(), -1); // #247552
+        w.completionBox()->down(); // select the item
+        QCOMPARE(w.completionBox()->items(), newItems);
+        QCOMPARE(w.text(), newItems.at(0));
     }
 
     void testPaste()