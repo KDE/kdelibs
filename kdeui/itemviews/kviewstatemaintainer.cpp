a2dbee67f87c10c520ffeab39d991285e3834d36
Stephen Kelly <steveire@gmail.com>
2010-08-17 12:44:40 +0000

    Put the KViewStateMaintainer into kdelibs.
    svn path=/trunk/KDE/kdelibs/; revision=1164671

---
+++ kdeui/itemviews/kviewstatemaintainer.cpp
@@ -0,0 +1,114 @@
+/*
+    Copyright (C) 2010 Klarälvdalens Datakonsult AB,
+        a KDAB Group company, info@kdab.net,
+        author Stephen Kelly <stephen@kdab.com>
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Library General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+    License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to the
+    Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+    02110-1301, USA.
+*/
+
+#include "kviewstatemaintainer.h"
+
+class KViewStateMaintainerBasePrivate
+{
+  KViewStateMaintainerBasePrivate(KSharedConfigPtr configPtr, const QString &name, KViewStateMaintainerBase *qq)
+    : q_ptr(qq), m_view( 0 ), m_selectionModel( 0 ), m_configPtr(configPtr), m_name(name)
+  {
+
+  }
+
+  Q_DECLARE_PUBLIC(KViewStateMaintainerBase)
+  KViewStateMaintainerBase * const q_ptr;
+
+  void _k_modelAboutToBeReset();
+  void _k_modelReset();
+
+  QAbstractItemView *m_view;
+  QItemSelectionModel *m_selectionModel;
+
+  const KSharedConfigPtr m_configPtr;
+  const QString m_name;
+};
+
+void KViewStateMaintainerBasePrivate::_k_modelAboutToBeReset()
+{
+  Q_Q(KViewStateMaintainerBase);
+  q->saveState();
+}
+
+void KViewStateMaintainerBasePrivate::_k_modelReset()
+{
+  Q_Q(KViewStateMaintainerBase);
+  q->restoreState();
+}
+
+KViewStateMaintainerBase::KViewStateMaintainerBase(KSharedConfigPtr configPtr, const QString &name, QObject* parent)
+  : QObject(parent), d_ptr(new KViewStateMaintainerBasePrivate(configPtr, name, this))
+{
+
+}
+
+KViewStateMaintainerBase::~KViewStateMaintainerBase()
+{
+  delete d_ptr;
+}
+
+KConfigGroup KViewStateMaintainerBase::configGroup() const
+{
+  Q_D(const KViewStateMaintainerBase);
+  return KConfigGroup(d->m_configPtr, d->m_name);
+}
+
+QItemSelectionModel* KViewStateMaintainerBase::selectionModel() const
+{
+  Q_D(const KViewStateMaintainerBase);
+  return d->m_selectionModel;
+}
+
+void KViewStateMaintainerBase::setSelectionModel(QItemSelectionModel* selectionModel)
+{
+  Q_D(KViewStateMaintainerBase);
+  d->m_selectionModel = selectionModel;
+
+  if (d->m_view && d->m_view->model()) {
+    disconnect(d->m_view->model(), SIGNAL(modelAboutToBeReset()), this, SLOT(_k_modelAboutToBeReset()));
+    disconnect(d->m_view->model(), SIGNAL(modelReset()), this, SLOT(_k_modelReset()));
+  }
+  connect(d->m_selectionModel->model(), SIGNAL(modelAboutToBeReset()), SLOT(_k_modelAboutToBeReset()), Qt::UniqueConnection);
+  connect(d->m_selectionModel->model(), SIGNAL(modelReset()), SLOT(_k_modelReset()), Qt::UniqueConnection);
+}
+
+QAbstractItemView* KViewStateMaintainerBase::view() const
+{
+  Q_D(const KViewStateMaintainerBase);
+  return d->m_view;
+}
+
+void KViewStateMaintainerBase::setView(QAbstractItemView* view)
+{
+  Q_D(KViewStateMaintainerBase);
+  d->m_view = view;
+
+  if (d->m_selectionModel && d->m_selectionModel->model()) {
+    disconnect(d->m_selectionModel->model(), SIGNAL(modelAboutToBeReset()), this, SLOT(_k_modelAboutToBeReset()));
+    disconnect(d->m_selectionModel->model(), SIGNAL(modelReset()), this, SLOT(_k_modelReset()));
+  }
+  if (d->m_view && d->m_view->model()) {
+    connect(d->m_view->model(), SIGNAL(modelAboutToBeReset()), SLOT(_k_modelAboutToBeReset()), Qt::UniqueConnection);
+    connect(d->m_view->model(), SIGNAL(modelReset()), SLOT(_k_modelReset()), Qt::UniqueConnection);
+  }
+}
+
+#include "kviewstatemaintainer.moc"
---
+++ kdeui/itemviews/kviewstatemaintainer.h
@@ -0,0 +1,107 @@
+/*
+    Copyright (C) 2010 Klarälvdalens Datakonsult AB,
+        a KDAB Group company, info@kdab.net,
+        author Stephen Kelly <stephen@kdab.com>
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Library General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+    License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to the
+    Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+    02110-1301, USA.
+*/
+
+#ifndef KVIEWSTATEMAINTAINER_H
+#define KVIEWSTATEMAINTAINER_H
+
+#include "kdeui_export.h"
+
+#include <QtGui/QItemSelectionModel>
+#include <QtGui/QAbstractItemView>
+
+#include <KConfigGroup>
+#include <KSharedConfig>
+#include <KSharedConfigPtr>
+
+class KViewStateMaintainerBasePrivate;
+
+class KDEUI_EXPORT KViewStateMaintainerBase : public QObject
+{
+  Q_OBJECT
+public:
+  KViewStateMaintainerBase(KSharedConfigPtr configPtr, const QString &name, QObject* parent = 0);
+  ~KViewStateMaintainerBase();
+
+  void setSelectionModel(QItemSelectionModel *selectionModel);
+  QItemSelectionModel *selectionModel() const;
+
+  void setView(QAbstractItemView *view);
+  QAbstractItemView *view() const;
+
+  virtual void saveState() = 0;
+  virtual void restoreState() = 0;
+
+protected:
+  KConfigGroup configGroup() const;
+
+private:
+  Q_DECLARE_PRIVATE(KViewStateMaintainerBase)
+  KViewStateMaintainerBasePrivate * const d_ptr;
+
+  Q_PRIVATE_SLOT( d_func(), void _k_modelAboutToBeReset() )
+  Q_PRIVATE_SLOT( d_func(), void _k_modelReset() )
+};
+
+/**
+ * @brief Encapsulates the maintenance of state between resets of QAbstractItemModel
+ *
+ * @code
+ *   m_collectionViewStateMaintainer = new KViewStateMaintainer<Akonadi::ETMViewStateSaver>(KGlobal::config(), "collectionView");
+ *   m_collectionViewStateMaintainer->setView(m_collectionView);
+ *
+ *   m_collectionCheckStateMaintainer = new KViewStateMaintainer<Akonadi::ETMViewStateSaver>(KGlobal::config(), "collectionCheckState");
+ *   m_collectionCheckStateMaintainer->setSelectionModel(m_checkableProxy->selectionModel());
+ * @endcode
+ *
+ * @see KViewStateSaver
+ */
+template<typename StateSaver>
+class KDEUI_EXPORT KViewStateMaintainer : public KViewStateMaintainerBase
+{
+  typedef StateSaver StateRestorer;
+public:
+  KViewStateMaintainer(KSharedConfigPtr configPtr, const QString &name, QObject* parent = 0)
+    : KViewStateMaintainerBase(configPtr, name, parent)
+  {
+
+  }
+
+  /* reimp */ void saveState()
+  {
+    StateSaver saver;
+    saver.setView(view());
+    saver.setSelectionModel(selectionModel());
+    KConfigGroup cfg = configGroup();
+    saver.saveState(cfg);
+    cfg.sync();
+  }
+
+  /* reimp */ void restoreState()
+  {
+    StateRestorer *restorer = new StateRestorer;
+    restorer->setView(view());
+    restorer->setSelectionModel(selectionModel());
+    restorer->restoreState(configGroup());
+  }
+
+};
+
+#endif
