04d93efe9ef417e09e50df23cbe2cd7918ac41f4
Sebastian Trueg <sebastian@trueg.de>
2010-08-23 08:16:10 +0000

    Added methods to construct a set of standard queries that are commonly used in several applications. This makes it even simpler for developers to query certain resources/files.
    svn path=/trunk/KDE/kdelibs/; revision=1166990

---
+++ nepomuk/query/standardqueries.cpp
@@ -0,0 +1,124 @@
+/*
+   This file is part of the Nepomuk KDE project.
+   Copyright (C) 2010 Sebastian Trueg <trueg@kde.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) version 3, or any
+   later version accepted by the membership of KDE e.V. (or its
+   successor approved by the membership of KDE e.V.), which shall
+   act as a proxy defined in Section 6 of version 3 of the license.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "standardqueries.h"
+#include "filequery.h"
+#include "comparisonterm.h"
+#include "literalterm.h"
+#include "andterm.h"
+#include "orterm.h"
+#include "negationterm.h"
+
+#include "nie.h"
+#include "nfo.h"
+#include "nie.h"
+#include "nuao.h"
+
+#include <Soprano/Vocabulary/NAO>
+
+#include <QtCore/QDate>
+
+
+Nepomuk::Query::Query Nepomuk::Query::standardQuery( StandardQuery query, const Term& subterm )
+{
+    switch( query ) {
+    case LastModifiedFilesQuery: {
+        ComparisonTerm lastModifiedTerm( Nepomuk::Vocabulary::NIE::lastModified(), Term() );
+        lastModifiedTerm.setSortWeight( 1, Qt::DescendingOrder );
+        FileQuery lastModifiedQuery( lastModifiedTerm );
+        return lastModifiedQuery;
+    }
+
+    case MostImportantResourcesQuery: {
+        ComparisonTerm fancyTerm( Soprano::Vocabulary::NAO::score(), Term() );
+        fancyTerm.setSortWeight( 1, Qt::DescendingOrder );
+        Query fancyQuery( fancyTerm );
+        return fancyQuery;
+    }
+
+    case NeverOpenedFilesQuery: {
+        // there are two ways a usage count of 0 can be expressed:
+        // 1. property with value 0
+        // 2. no property at all
+        OrTerm usageCntTerm(
+            ComparisonTerm(
+                Nepomuk::Vocabulary::NUAO::usageCount(),
+                LiteralTerm( 0 ),
+                ComparisonTerm::Equal ),
+            NegationTerm::negateTerm(
+                ComparisonTerm(
+                    Nepomuk::Vocabulary::NUAO::usageCount(),
+                    Term() ) ) );
+
+        // Before we had the data management service there was no usage count
+        // tracking. Thus, in order not to return all files we should filter
+        // out all files that were created before we started tracking usage.
+        // However, by default we only show the top 10 results. Thus, in the
+        // worst case this query will return the same as lastModifiedFilesQuery().
+        ComparisonTerm modDateTerm(
+            Nepomuk::Vocabulary::NIE::lastModified(),
+            Term() );
+        modDateTerm.setSortWeight( 1, Qt::DescendingOrder );
+
+        FileQuery query( usageCntTerm && modDateTerm );
+        return query;
+    }
+
+    case ResourcesForActivityQuery: {
+        // FIXME
+    }
+    }
+}
+
+
+Nepomuk::Query::Query Nepomuk::Query::dateRangeQuery( const QDate& start, const QDate& end, DateRangeFlags dateFlags )
+{
+    // create our range
+    const LiteralTerm dateFrom( QDateTime( start, QTime( 0,0,0 ) ) );
+    const LiteralTerm dateTo( QDateTime( end, QTime( 23, 59, 59, 999 ) ) );
+
+    Query query;
+
+    if( dateFlags & ModificationDate ) {
+        // include files modified in our date range
+        ComparisonTerm lastModifiedStart = Nepomuk::Vocabulary::NIE::lastModified() > dateFrom;
+        ComparisonTerm lastModifiedEnd = Nepomuk::Vocabulary::NIE::lastModified() < dateTo;
+        query = query || ( lastModifiedStart && lastModifiedEnd );
+    }
+
+    if( dateFlags & ContentDate ) {
+        // include files created (as in photos taken) in our data range
+        ComparisonTerm contentCreatedStart = Nepomuk::Vocabulary::NIE::contentCreated() > dateFrom;
+        ComparisonTerm contentCreatedEnd = Nepomuk::Vocabulary::NIE::contentCreated() < dateTo;
+        query = query || ( contentCreatedStart && contentCreatedEnd );
+    }
+
+    if( dateFlags & UsageDate ) {
+        // include files opened (and optionally modified) in our date range
+        // TODO: also take the end of the event into account
+        ComparisonTerm accessEventStart = Nepomuk::Vocabulary::NUAO::start() > dateFrom;
+        ComparisonTerm accessEventEnd = Nepomuk::Vocabulary::NUAO::start() < dateTo;
+        ComparisonTerm accessEventCondition = Nepomuk::Vocabulary::NUAO::involves() == ( accessEventStart && accessEventEnd );
+        query = query || accessEventCondition.inverted();
+    }
+
+    return query;
+}
---
+++ nepomuk/query/standardqueries.h
@@ -0,0 +1,138 @@
+/*
+   This file is part of the Nepomuk KDE project.
+   Copyright (C) 2010 Sebastian Trueg <trueg@kde.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) version 3, or any
+   later version accepted by the membership of KDE e.V. (or its
+   successor approved by the membership of KDE e.V.), which shall
+   act as a proxy defined in Section 6 of version 3 of the license.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _NEPOMUK_STANDARD_QUERIES_H_
+#define _NEPOMUK_STANDARD_QUERIES_H_
+
+#include "nepomukquery_export.h"
+#include "term.h"
+
+class QDate;
+
+namespace Nepomuk {
+    namespace Query {
+        class Query;
+
+        /**
+         * A set of predefined queries that can be created via standardQuery().
+         *
+         * \since 4.6
+         */
+        enum StandardQuery {
+            /**
+             * Creates a query that returns all files sorted by descending modification date.
+             *
+             * The subterm parameter can be used to specify an application restricting the results
+             * to files created/opened with that application.
+             */
+            LastModifiedFilesQuery,
+
+            /**
+             * Creates a query that returns all resources sorted by descending score (as calculated
+             * by the DataMaintenanceService)
+             *
+             * The subterm parameter can be used to specify an application restricting the results
+             * to files created/opened with that application.
+             */
+            MostImportantResourcesQuery,
+
+            /**
+             * Creates a query that returns all files with a usage count of 0
+             * sorted by descending modification date.
+             */
+            NeverOpenedFilesQuery,
+
+            /**
+             * Get the resources related to a specific activity. Use a ResourceTerm referring to
+             * the activity as parameter in standardQuery.
+             */
+            ResourcesForActivityQuery
+        };
+
+
+        /**
+         * Modificators to influence the behaviour of dateRangeQuery().
+         *
+         * \since 4.6
+         */
+        enum DateRangeFlag {
+            /**
+             * Query for the modification date (nie:lastModified)
+             */
+            ModificationDate = 0x1,
+
+            /**
+             * Query for the content creation date (nie:contentCreated)
+             */
+            ContentDate = 0x2,
+
+            /**
+             * Query for usage events referring to the resource.
+             */
+            UsageDate = 0x4,
+
+            /**
+             * Query for all possible dates.
+             */
+            AllDates = ModificationDate|ContentDate|UsageDate
+        };
+        Q_DECLARE_FLAGS( DateRangeFlags, DateRangeFlag )
+
+        /**
+         * Create a standard query as defined by \p query.
+         *
+         * \param query The query to be generated. See StandardQuery.
+         * \param subterm An optional subterm used for specific types of standard queries that need
+         * a parameter like ResourcesForActivityQuery.
+         *
+         * To get a query that only returns files (this is already true for some of the predefined queries)
+         * use something like the following:
+         *
+         * \code
+         * Query::FileQuery query = Query::standardQuery( Query::LastModifiedFilesQuery );
+         * \endcode
+         *
+         * Be aware that queries can be combined. One can for example get the most important files related
+         * to an activity as follows:
+         *
+         * \code
+         * Query query = Query::standardQuery( Query::ResourcesForActivityQuery, myActivity )
+         *     && Query::standardQuery( Query::MostImportantResourcesQuery );
+         * \endcode
+         *
+         * \since 4.6
+         */
+        NEPOMUKQUERY_EXPORT Query standardQuery( StandardQuery query, const Term& subterm = Term() );
+
+        /**
+         * Create a query that returns resources/files that have been modified/accessed in the range
+         * from \p start to \p end (including both full days). The flags specified in \p dateFlags can be used to influence the
+         * type of dates that are queried.
+         *
+         * \since 4.6
+         */
+        NEPOMUKQUERY_EXPORT Query dateRangeQuery( const QDate& start, const QDate& end, DateRangeFlags dateFlags = AllDates );
+    }
+}
+
+Q_DECLARE_OPERATORS_FOR_FLAGS( Nepomuk::Query::DateRangeFlags )
+
+#endif
