d9f31b58980ead5ea4daacff592744d7a1e0ac3a
Michel Ludwig <michel.ludwig@gmail.com>
2010-08-29 09:25:27 +0000

    - Add a 'restore' method to 'Sonnet::BackgroundChecker', which allows it to read a given spell check configuration and then to pass it on to the filter object that is used internally. The implementation is similar to what is done inside 'Sonnet::Highlighter'. - Use the restore method inside KatePart to finally fix bug 244907 for KatePart as well.
    Back-port of commit 1169440.
    
    
    svn path=/branches/KDE/4.5/kdelibs/; revision=1169445

--- kate/spellcheck/ontheflycheck.cpp
+++ kate/spellcheck/ontheflycheck.cpp
@@ -60,6 +60,10 @@ KateOnTheFlyChecker::KateOnTheFlyChecker(KateDocument *document)
           this, SLOT(updateConfig()));
   connect(document, SIGNAL(respellCheckBlock(KateDocument*, int, int)),
           this, SLOT(handleRespellCheckBlock(KateDocument*, int, int)));
+
+  // load the settings for the speller
+  updateConfig();
+
   foreach(KTextEditor::View* view, document->views()) {
     addView(document, view);
   }
@@ -407,6 +411,7 @@ void KateOnTheFlyChecker::performSpellCheck()
             this,
             SLOT(misspelling(const QString&,int)));
     connect(m_backgroundChecker, SIGNAL(done()), this, SLOT(spellCheckDone()));
+    m_backgroundChecker->restore(KGlobal::config().data());
   }
   m_backgroundChecker->setSpeller(m_speller);
   m_backgroundChecker->setText(text); // don't call 'start()' after this!
@@ -665,6 +670,9 @@ void KateOnTheFlyChecker::updateConfig()
 {
   ON_THE_FLY_DEBUG;
   m_speller.restore(KGlobal::config().data());
+  if(m_backgroundChecker) {
+    m_backgroundChecker->restore(KGlobal::config().data());
+  }
 }
 
 void KateOnTheFlyChecker::refreshSpellCheck(const KTextEditor::Range &range)
