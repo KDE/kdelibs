1e6095353b2730985d9e4eaf7013f1f2d8c66720
David Faure <faure@kde.org>
2010-09-02 21:25:31 +0000

    Prefix with KDE4_ as requested by Alexander. Second full recompile, everyone :)
    svn path=/trunk/KDE/kdelibs/; revision=1171144

--- kdecore/io/kdebug.h
+++ kdecore/io/kdebug.h
@@ -243,8 +243,8 @@ public:
     explicit inline KDebug(QtMsgType type, const char *f = 0, int l = -1, const char *info = 0)
         : file(f), funcinfo(info), line(l), level(type)
         {
-#ifdef CMAKE_TOPLEVEL_DIR_LENGTH // set by FindKDE4Internal.cmake
-            file = file + CMAKE_TOPLEVEL_DIR_LENGTH + 1;
+#ifdef KDE4_CMAKE_TOPLEVEL_DIR_LENGTH // set by FindKDE4Internal.cmake
+            file = file + KDE4_CMAKE_TOPLEVEL_DIR_LENGTH + 1;
 #endif
         }
 
