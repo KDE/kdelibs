ecb4b1848da6e8ea43525e6dcff749995a69c6ac
Andreas Hartmetz <ahartmetz@gmail.com>
2010-09-29 23:52:33 +0000

    update a few copyright years
    svn path=/trunk/KDE/kdelibs/; revision=1181085

--- kdecore/network/kssld/kssld.h
+++ kdecore/network/kssld/kssld.h
@@ -1,7 +1,7 @@
 /*
    This file is part of the KDE libraries
 
-   Copyright (c) 2007, 2008 Andreas Hartmetz <ahartmetz@gmail.com>
+   Copyright (c) 2007, 2008, 2010 Andreas Hartmetz <ahartmetz@gmail.com>
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
