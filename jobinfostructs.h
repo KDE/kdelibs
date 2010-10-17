3abcf4a982b70ed7e7e7263979a4d0f04ee9cb95
Valentin Rusu <kde@rusu.info>
2010-10-17 22:59:53 +0000

    ACL preparing refactoring ended
    svn path=/trunk/playground/base/ksecretservice/; revision=1186927

--- jobinfostructs.h
+++ jobinfostructs.h
@@ -20,6 +20,23 @@
 
 #ifndef JOBINFOSTRUCTS_H
 #define JOBINFOSTRUCTS_H
+/**
+ * @file jobinfostructs.h
+ *
+ * This file contain a collection of data objects exchanged by the various
+ * asynchronous jobs used by the frontend and backend layer to interact.
+ * We opted for structs and not classes as these data objects are not 
+ * real full-fledget objets. They only need to transport the various contextual
+ * parameters needed for the jobs to perform.
+ *
+ * All these data objects inherit from a base JobBaseInfo that mainly holds
+ * information about the d-bus peer that originated the various operations
+ * the jobs are requested to do.
+ *
+ * Some of the structures do not have any members. That is on purpose, as 
+ * defining ans using them would allow easy addition of new data pieces to
+ * exchange on the job chain path.
+ */
 
 #include <QtCore/QString>
 #include <QtCore/QMap>
@@ -27,13 +44,25 @@
 
 #include "peer.h"
 
+/**
+ * This is the base class of all the jobs value objects
+ * It takes ownership of the Peer pointer and deletes it
+ * when it's destructor is called
+ */
 struct JobBaseInfo
 {
    /**
      * The peer that requested the delete operation
+     * Beware when storing the pointer to the peer outside
+     * this class as it's deleting it when destructed
      */
    const Peer* m_peer;
    JobBaseInfo( const Peer* peer ) : m_peer( peer ) {}
+   virtual ~JobBaseInfo()
+   {
+      if (m_peer != 0)
+         delete m_peer, m_peer = 0;
+   }
 };
 
 class BackendCollection;
@@ -45,20 +74,22 @@ struct CollectionCreateInfo : public JobBaseInfo
    
    CollectionCreateInfo( const QString& label, const Peer* peer ) : 
       JobBaseInfo(peer), m_label( label ), m_locked(false)
-   {
-   }
+   {}
 };
 
 struct CollectionDeleteInfo : public JobBaseInfo
 {
-   CollectionDeleteInfo( const Peer* peer ) : JobBaseInfo( peer )
+   mutable BackendCollection *m_collection;
+   CollectionDeleteInfo( const Peer* peer, BackendCollection *collection =0) : JobBaseInfo( peer ),
+      m_collection(collection)
    {}
 };
 
 struct CollectionUnlockInfo : public JobBaseInfo
 {
-   BackendCollection *m_collection;
-   CollectionUnlockInfo( const Peer* peer ) : JobBaseInfo( peer )
+   mutable BackendCollection *m_collection; // collection may be changed on the job path
+   CollectionUnlockInfo( const Peer* peer, BackendCollection *collection =0) : JobBaseInfo( peer ),
+      m_collection(collection)
    {}
 };
 
@@ -71,6 +102,14 @@ struct ItemCreateInfo : public JobBaseInfo
    const QCA::SecureArray &m_secret;
    bool m_replace;
    bool m_locked;
+   /**
+    * @param label human-readable label for the new item
+    * @param attributes attributes to set for the new item
+    * @param secret the secret to store
+    * @param locked true if the item should be locked after creation, false else
+    * @param replace if true, replace an item with the same attributes if it already exists
+    * @param peer the d-bus peer that initiated this operation
+    */
    ItemCreateInfo( const QString &label, 
                    const QMap<QString, QString> &attributes,
                    const QCA::SecureArray &secret,
@@ -85,7 +124,8 @@ struct ItemCreateInfo : public JobBaseInfo
 
 struct ItemDeleteInfo : public JobBaseInfo
 {
-   ItemDeleteInfo( const Peer* peer ) : JobBaseInfo( peer )
+   mutable BackendItem* m_item; // item information may be added on the job path so let it be mutable
+   ItemDeleteInfo( const Peer* peer, BackendItem *item =0) : JobBaseInfo( peer ), m_item( item )
    {}
 };
 
