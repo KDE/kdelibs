39cbf653fa549cbe9e6f2d35722a79b150890223
Laurent Montel <montel@kde.org>
2010-10-26 11:55:30 +0000

    There is not ui3 or ui4 files
    svn path=/trunk/KDE/kdelibs/; revision=1189918

--- Messages.sh
+++ Messages.sh
@@ -17,6 +17,6 @@ find $dirs -maxdepth 1 -name "*.cc" -print >> files
 find $dirs -maxdepth 1 -name "*.h" -print >> files
 # Add the kdecore/date files not added in kdecalendarsystems.pot
 find ./kdecore/date -name "*.cpp" -o -name "*.cc" -o -name "*.h" | grep -v -E kcalendarsystem.* >> files
-$EXTRACTRC `find $dirs -maxdepth 1 \( -name \*.rc -o -name \*.ui -o -name \*.ui3 -o -name \*.ui4 -o -name \*.kcfg \) ` >> rc.cpp || exit 11
+$EXTRACTRC `find $dirs -maxdepth 1 \( -name \*.rc -o -name \*.ui -o -name \*.kcfg \) ` >> rc.cpp || exit 11
 $XGETTEXT -ktranslate:1,1t -ktranslate:1c,2,2t --files-from=files -o $podir/kdelibs4.pot
 rm -f dirs files
