f303284e0ce3738ce8a1d8eddbfe510a2db10d43
Peter Penz <peter.penz19@gmail.com>
2010-11-07 13:15:17 +0000

    Provide a unit-test for KFileMetaInfo.
    svn path=/trunk/KDE/kdelibs/; revision=1193877

---
+++ kio/tests/kfilemetainfotest.h
@@ -0,0 +1,45 @@
+/* This file is part of the KDE project
+   Copyright (C) 2010 Peter Penz <peter.penz19@gmail.com>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#ifndef KFILEMETAINFOTEST_H
+#define KFILEMETAINFOTEST_H
+
+#include <ktempdir.h>
+#include <QtCore/QEventLoop>
+#include <QtCore/QObject>
+
+class KFileMetaInfoTest : public QObject
+{
+    Q_OBJECT
+
+private Q_SLOTS:
+    void initTestCase();
+    void testMetaInfo();
+    void testReentrancy();
+
+protected Q_SLOTS:
+    void exitLoop();
+
+private:
+    int m_exitCount;
+    QEventLoop m_eventLoop;
+    KTempDir m_tempDir;
+};
+
+#endif
