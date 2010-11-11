6ea35152f4b8d8d8a62fe7ae2dc08674088a59fe
Lukáš Tinkl <lukas@kde.org>
2010-11-11 11:58:04 +0000

    attribute the correct license
    svn path=/trunk/KDE/kdelibs/; revision=1195517

--- solid/solid/backends/udisks/linux_dvd_rw_utils.c
+++ solid/solid/backends/udisks/linux_dvd_rw_utils.c
@@ -1,11 +1,23 @@
-/*
- *
- * This is part of dvd+rw-tools by Andy Polyakov <appro@fy.chalmers.se>
- *
- * Use-it-on-your-own-risk, GPL bless...
- *
- * For further details see http://fy.chalmers.se/~appro/linux/DVD+RW/
-*/
+/***************************************************************************
+ *   This file is part of dvd+rw-tools                                     *
+ *   Copyright (C) 2002-2010 Andy Polyakov <appro@fy.chalmers.se>          *
+ *                                                                         *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA          *
+ ***************************************************************************/
 
 #define CREAM_ON_ERRNO(s)	do {				\
     switch ((s)[2]&0x0F)					\
