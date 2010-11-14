92732caed7312d8e51f19eb6d55223b2af29b323
Luigi Toscano <luigi.toscano@tiscali.it>
2010-11-14 16:19:54 +0000

    Remove the dependency from the embedded DocBook XSLT, use our translations
    This commit introduces the last changes needed to get rid of the old embedded
    copy of DocBook XSLT:
    - make the common includes (kde-common-*xsl.cmake) dependent on the
      system-provided DocBook XSLTs (i.e. it generates them on the fly during
      the compilation).
    - change the macro used to generate manpages to use kde-include-man.xsl
      instead of the embedded copy of manpages generator (kde-man.xsl has never
      been used in KDE Platform 4.x as far as I know).
      It is worth noting that, on the contrary, kde-include-common.xsl is not
      used directly (because it is really common to multiple files, here
      kde-include-man.xsl really replaces the kde-man.xsl).
    - introduce a small helper program as a dependency for meinproc which will
      generate, during compile time, two xml files needed in order to make
      the system working:
      * all-l10n.xml contains the references to all the system-provided language
        files for XSLT, plus the ones not included upstream but translated in our
        repository; it is used in place of the system-provided common/l10n.xml
      * kde-custom-l10n.xml contains the references to the language files
        containing customizations for system-provided languages (included into 
        the official docbook-xsl).
    - do not install anymore the files under docbook/xsl (which will be removed)
    
    See also: http://kde.markmail.org/thread/as4ctj7kt3raws4q
    
    And now let's hope that the whole thing won't explode...
    
    CCMAIL: kde-i18n-doc@kde.org
    
    svn path=/trunk/KDE/kdelibs/; revision=1196999

---
+++ kdoctools/customization/kde-include-common.xsl.cmake
@@ -0,0 +1,10 @@
+<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+		version="1.0">
+
+   <xsl:import href="@DOCBOOKXSL_DIR@/html/autoidx.xsl"/>
+   <xsl:import href="@DOCBOOKXSL_DIR@/html/chunk.xsl"/>
+
+   <xsl:param name="l10n.xml" select="document('xsl/all-l10n.xml')"/>
+   <xsl:param name="local.l10n.xml" select="document('xsl/kde-custom-l10n.xml')"/>
+        
+</xsl:stylesheet>
---
+++ kdoctools/docbookl10nhelper.cpp
@@ -0,0 +1,195 @@
+/*  This file is part of the KDE project
+    Copyright (C) 2010 Luigi Toscano <luigi.toscano@tiscali.it>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) version 3, or any
+    later version accepted by the membership of KDE e.V. (or its
+    successor approved by the membership of KDE e.V.), which shall
+    act as a proxy defined in Section 6 of version 3 of the license.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <QCoreApplication>
+#include <QDebug>
+#include <QDir>
+#include <QFile>
+#include <QIODevice>
+#include <QList>
+#include <QPair>
+#include <QRegExp>
+#include <QStringList>
+#include <QTextStream>
+
+
+class LangListType: public QList<QPair<QString,QString> >
+{
+public:
+   int searchLang( QString el ) {
+
+      for ( int i = 0; i < size(); ++i ) {
+         if ( at( i ).first == el )
+            return i;
+      }
+      return -1;
+   }
+};
+
+int writeLangFile( const QString &fname, const QString &dtdPath,
+                   const LangListType &langMap ) {
+
+   QFile outFile( fname );
+   if ( ! outFile.open( QIODevice::WriteOnly ) ) {
+      qCritical() << QString( "Could not write %1" )
+                     .arg( outFile.fileName() );
+      return( 1 );
+   }
+
+   QTextStream outStream( &outFile );
+   outStream << "<?xml version='1.0'?>" << endl;
+   outStream << QString( "<!DOCTYPE l:i18n SYSTEM \"%1\" [" )
+               .arg( dtdPath ) << endl;
+
+   LangListType::const_iterator i = langMap.constBegin();
+   while ( i != langMap.constEnd() ) {
+      //qDebug() << (*i).first << ": " << (*i).second;
+      outStream << QString( "<!ENTITY %1 SYSTEM \"%2\">" )
+                   .arg( (*i).first ).arg( (*i).second ) << endl;
+      ++i;
+   }
+   outStream << "]>" << endl;
+
+   if ( langMap.size() > 0 ) {
+      outStream
+         << "<l:i18n xmlns:l=\"http://docbook.sourceforge.net/xmlns/l10n/1.0\">"
+         << endl;
+      i = langMap.constBegin();
+      while ( i != langMap.constEnd() ) {
+         outStream << QString( "&%1;" )
+                      .arg( (*i).first ) << endl;
+         ++i;
+      }
+      outStream << "</l:i18n>" << endl;
+   }
+
+   outFile.close();
+
+   return( 0 );
+}
+
+inline const QString addTrailingSlash( const QString &p ) {
+   return p.endsWith( "/" ) ? p : p + "/";
+}
+
+int main( int argc, char **argv ) {
+   QCoreApplication app( argc, argv );
+
+   const QStringList arguments = app.arguments();
+   if ( arguments.count() != 4 ) {
+      qCritical() << "wrong argument count";
+      return ( 1 );
+   }
+
+   const QString l10nDir = addTrailingSlash( arguments[1] );
+   const QString l10nCustomDir = addTrailingSlash( arguments[2] );
+   const QString destDir = addTrailingSlash( arguments[3] );
+
+   QFile i18nFile( l10nDir + "common/l10n.xml" );
+
+   if ( ! i18nFile.open( QIODevice::ReadOnly ) ) {
+      qCritical() << i18nFile.fileName() << " not found";
+      return( 1 );
+   }
+
+   const QString all10nFName = destDir + "all-l10n.xml";
+   const QString customl10nFName = destDir + "kde-custom-l10n.xml";
+
+   /*
+    * for each language defined in the original l10n.xml, copy
+    * it into all-l10n.xml and store it in a list;
+    **/
+   QRegExp rxEntity, rxDocType;
+   rxDocType.setPattern("^\\s*<!DOCTYPE\\s+l:i18n\\s+SYSTEM\\s+\"l10n\\.dtd\"\\s+\\[\\s*$");
+   rxEntity.setPattern("^\\s*<!ENTITY\\s+([^\\s]+)\\s+SYSTEM\\s+\"([^\\s]+)\">\\s*$");
+   QTextStream inStream( &i18nFile );
+   int parsingState = 0;
+
+   LangListType allLangs, customLangs;
+
+   while ( ! inStream.atEnd() ) {
+      QString line = inStream.readLine();
+
+      switch ( parsingState ) {
+       case 0:
+         if ( rxDocType.indexIn( line ) != -1 ) {
+            parsingState = 1;
+            //qDebug() << "DTD found";
+         }
+         break;
+       case 1:
+         QString langCode, langFile;
+         if ( rxEntity.indexIn( line ) != -1 ) {
+            langCode = rxEntity.cap( 1 );
+            langFile = l10nDir + "common/" + rxEntity.cap( 2 );
+            allLangs += qMakePair( langCode, langFile );
+            //qDebug() << langCode << " - " << langFile;
+         }
+         break;
+      }
+
+   }
+   i18nFile.close();
+
+   /* read the list of locally-available custom languages */
+   QDir outDir( l10nCustomDir );
+
+   QStringList dirFileFilters;
+   dirFileFilters << "*.xml";
+   QStringList customLangFiles = outDir.entryList( dirFileFilters,
+                           QDir::Files|QDir::NoSymLinks, QDir::Name );
+   /* the following two calls to removeOne should not be needed, as
+    * the customization directory from the sources should not contain
+    * those files
+    */
+   customLangFiles.removeOne( "all-l10n.xml" );
+   customLangFiles.removeOne( "kde-custom-l10n.xml" );
+   //qDebug() << "customLangFiles:" << customLangFiles;
+
+   /*
+    * for each custom language (from directory listing), if it is not
+    * in the list of upstream languages, add it to all-l10n.xml,
+    * otherwise add it to kde-custom-l10n.xml
+    */
+   QStringList::const_iterator i = customLangFiles.constBegin();
+   while ( i != customLangFiles.constEnd() ) {
+      QString langFile = (*i);
+      /* remove trailing .xml */
+      QString langCode = langFile.left( langFile.length() - 4 );
+
+      QPair<QString,QString> cl = qMakePair( langCode, langFile );
+      if ( ( allLangs.searchLang( langCode ) ) > 0 ) {
+      /* custom language found in upstream list */
+         customLangs += cl;
+      } else {
+      /* custom language not found in upstream list */
+         allLangs += cl;
+      }
+      ++i;
+   }
+
+
+   int res = writeLangFile( all10nFName, l10nDir + "common/l10n.dtd",
+                            allLangs );
+   res += writeLangFile( customl10nFName, l10nDir + "common/l10n.dtd",
+                         customLangs );
+
+   return( res );
+}
