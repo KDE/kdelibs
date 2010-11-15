KDE/kdelibs

commit 9eeedfde933056deb4b9e4f29cd299845f6e7717

Internal cleanups: Don't work with QString when dealing with URLs, use the KUrl interface instead
svn path=/trunk/KDE/kdelibs/; revision=1168855

kfile/kurlnavigator.cpp
@@ -971,22 +971,19 @@ KUrl KUrlNavigator::uncommittedUrl() const
 
 void KUrlNavigator::setLocationUrl(const KUrl& newUrl)
 {
-    const KUrl currentUrl = locationUrl();
-    if (newUrl == currentUrl) {
+    if (newUrl == locationUrl()) {
         return;
     }
 
     KUrl url = newUrl;
     url.cleanPath();
     
-    QString urlStr;
     KUriFilterData urlData(url);
     urlData.setCheckForExecutables(false);
     if (KUriFilter::self()->filterUri(urlData, QStringList() << "kshorturifilter" << "kurisearchfilter")) {
         url = urlData.uri();
-        urlStr = urlData.uri().url();
     } else {
-        urlStr = KUrlCompletion::replacedPath(url.url(), true, true);
+        url = KUrlCompletion::replacedPath(url.url(), true, true);
     }
     
     if ((url.protocol() == QLatin1String("tar")) || (url.protocol() == QLatin1String("zip"))) {
@@ -1009,22 +1006,20 @@ void KUrlNavigator::setLocationUrl(const KUrl& newUrl)
         if (!insideCompressedPath) {
             // drop the tar: or zip: protocol since we are not
             // inside the compressed path anymore
-            urlStr = url.path();
+            url.setProtocol(QString());
         }
     }
-
-    const KUrl transformedUrl(urlStr);
 
     // Check whether current history element has the same URL.
     // If this is the case, just ignore setting the URL.
     const LocationData& data = d->m_history[d->m_historyIndex];
-    const bool isUrlEqual = transformedUrl.equals(locationUrl(), KUrl::CompareWithoutTrailingSlash) ||
-                            (!transformedUrl.isValid() && (urlStr == data.url.url()));
+    const bool isUrlEqual = url.equals(locationUrl(), KUrl::CompareWithoutTrailingSlash) ||
+                            (!url.isValid() && url.equals(data.url, KUrl::CompareWithoutTrailingSlash));
     if (isUrlEqual) {
         return;
     }
 
-    emit urlAboutToBeChanged(transformedUrl);
+    emit urlAboutToBeChanged(url);
 
     if (d->m_historyIndex > 0) {
         // If an URL is set when the history index is not at the end (= 0),
@@ -1038,7 +1033,7 @@ void KUrlNavigator::setLocationUrl(const KUrl& newUrl)
 
     Q_ASSERT(d->m_historyIndex == 0);
     LocationData newData;
-    newData.url = transformedUrl;
+    newData.url = url;
     d->m_history.insert(0, newData);
 
     // Prevent an endless growing of the history: remembering
@@ -1051,7 +1046,7 @@ void KUrlNavigator::setLocationUrl(const KUrl& newUrl)
     }
 
     emit historyChanged();
-    emit urlChanged(transformedUrl);
+    emit urlChanged(url);
 
     d->updateContent();
 

commit a9558869ece3bfdc50592dd5786212cc5298f759

When going "up" out of an archive, set the protocol to "file". Fixes the problem that Dolphin reports an "invalid protocol" and is unable to view the folder containing the archive.
This is a fix for a regression caused by r1168855. Unit test included.

svn path=/trunk/KDE/kdelibs/; revision=1197418

kfile/kurlnavigator.cpp
@@ -1005,8 +1005,8 @@ void KUrlNavigator::setLocationUrl(const KUrl& newUrl)
         }
         if (!insideCompressedPath) {
             // drop the tar: or zip: protocol since we are not
-            // inside the compressed path anymore
-            url.setProtocol(QString());
+            // inside the compressed path
+            url.setProtocol("file");
         }
     }
 

kfile/tests/kurlnavigatortest.cpp
@@ -159,4 +159,34 @@ void KUrlNavigatorTest::testHistoryInsert()
     QCOMPARE(m_navigator->historySize(), 4);
 }
 
+/**
+ * When the current URL is inside an archive and the user goes "up", it is expected
+ * that the new URL is that of the folder containing the archive (unless the URL was
+ * in a subfolder inside the archive). Furthermore, the protocol should be "file".
+ * An empty protocol would lead to problems in Dolphin, see
+ *
+ * https://bugs.kde.org/show_bug.cgi?id=251553
+ */
+
+void KUrlNavigatorTest::bug251553_goUpFromArchive()
+{
+    m_navigator->setLocationUrl(KUrl("zip:/test/archive.zip"));
+    QCOMPARE(m_navigator->locationUrl().path(), QLatin1String("/test/archive.zip"));
+    QCOMPARE(m_navigator->locationUrl().protocol(), QLatin1String("zip"));
+
+    bool ok = m_navigator->goUp();
+    QVERIFY(ok);
+    QCOMPARE(m_navigator->locationUrl().path(KUrl::AddTrailingSlash), QLatin1String("/test/"));
+    QCOMPARE(m_navigator->locationUrl().protocol(), QLatin1String("file"));
+
+    m_navigator->setLocationUrl(KUrl("tar:/test/archive.tar.gz"));
+    QCOMPARE(m_navigator->locationUrl().path(), QLatin1String("/test/archive.tar.gz"));
+    QCOMPARE(m_navigator->locationUrl().protocol(), QLatin1String("tar"));
+
+    ok = m_navigator->goUp();
+    QVERIFY(ok);
+    QCOMPARE(m_navigator->locationUrl().path(KUrl::AddTrailingSlash), QLatin1String("/test/"));
+    QCOMPARE(m_navigator->locationUrl().protocol(), QLatin1String("file"));
+}
+
 #include "kurlnavigatortest.moc"

kfile/tests/kurlnavigatortest.h
@@ -36,6 +36,8 @@ private slots:
     void testGoForward();
     void testHistoryInsert();
 
+    void bug251553_goUpFromArchive();
+
 private:
     KUrlNavigator* m_navigator;
 };