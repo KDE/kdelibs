ba2542e76bd51449e9b50b5565c488f1c3b91d7e
David Faure <faure@kde.org>
2010-12-08 15:04:37 +0000

    KConfig returns the resolved path now, adjust this test.
    svn path=/trunk/KDE/kdelibs/; revision=1204663

--- kdecore/tests/kdesktopfiletest.cpp
+++ kdecore/tests/kdesktopfiletest.cpp
@@ -39,7 +39,7 @@ void KDesktopFileTest::testRead()
     QCOMPARE(df.hasLinkType(), false);
     QCOMPARE(df.hasMimeTypeType(), false);
     QCOMPARE(df.hasApplicationType(), false);
-    QCOMPARE(df.fileName(), fileName);
+    QCOMPARE(df.fileName(), KStandardDirs::realFilePath(fileName));
     QCOMPARE(df.resource(), "apps"); // default for .desktop files
 }
 
