63dfd8247155a1514d6666d9f554cc0cfa7c7ae0
Dawit Alemayehu <adawit@kde.org>
2010-12-26 22:44:59 +0000

    Backported the fix for BR# 260769
    svn path=/branches/KDE/4.6/kdelibs/; revision=1209482

--- kio/kio/tcpslavebase.cpp
+++ kio/kio/tcpslavebase.cpp
@@ -267,18 +267,19 @@ ssize_t TCPSlaveBase::read(char* data, ssize_t len)
     }
 
     if (!d->socket.bytesAvailable()) {
-        if (d->isBlocking) {
-            d->socket.waitForReadyRead(-1);
-        } else {
-            d->socket.waitForReadyRead(readTimeout());
-        }
-    } else if (d->socket.encryptionMode() != KTcpSocket::SslClientMode ||
+        const int timeout = d->isBlocking ? -1 : readTimeout();
+        d->socket.waitForReadyRead(timeout);
+    }
+#if 0
+    // Do not do this because its only benefit is to cause a nasty side effect
+    // upstream in Qt. See BR# 260769.
+    else if (d->socket.encryptionMode() != KTcpSocket::SslClientMode ||
                QNetworkProxy::applicationProxy().type() == QNetworkProxy::NoProxy) {
         // we only do this when it doesn't trigger Qt socket bugs. When it doesn't break anything
         // it seems to help performance.
         d->socket.waitForReadyRead(0);
     }
-
+#endif
     return d->socket.read(data, len);
 }
 
