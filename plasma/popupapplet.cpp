7a184d8abab4eff38b1bb1649b8c37c44ca070fe
Aaron Seigo <aseigo@kde.org>
2011-02-04 16:18:03 -0800

    toggle on activation

--- plasma/popupapplet.cpp
+++ plasma/popupapplet.cpp
@@ -436,8 +436,7 @@ void PopupAppletPrivate::popupConstraintsEvent(Plasma::Constraints constraints)
 
 void PopupAppletPrivate::appletActivated()
 {
-    q->setStatus(Plasma::NeedsAttentionStatus);
-    q->showPopup();
+    internalTogglePopup();
 }
 
 QSizeF PopupApplet::sizeHint(Qt::SizeHint which, const QSizeF & constraint) const
@@ -802,10 +801,7 @@ void PopupAppletPrivate::dialogSizeChanged()
 
 void PopupAppletPrivate::dialogStatusChanged(bool status)
 {
-    if (!status) {
-        q->setStatus(PassiveStatus);
-    }
-
+    q->setStatus(status ? NeedsAttentionStatus : PassiveStatus);
     q->popupEvent(status);
 }
 
