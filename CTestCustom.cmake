b370d95806542972077aa76d255b0d13f0409013
Christophe Giboudeaux <cgiboudeaux@gmail.com>
2011-02-16 20:52:08 +0100

    Add a CTestCustom.cmake file and increase the maximum warnings that will appear on cdash

---
+++ CTestCustom.cmake
@@ -0,0 +1,7 @@
+# This file contains all the specific settings that will be used
+# when running 'make Experimental' or 'make ExperimentalBuild'
+
+# Change the maximum warnings that will be displayed
+# on the report page (default 50)
+set(CTEST_CUSTOM_MAXIMUM_NUMBER_OF_WARNINGS 3000)
+
