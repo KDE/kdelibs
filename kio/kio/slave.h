b0827c7ba3921815845d374427ea374d82887eae
Dawit Alemayehu <adawit@kde.org>
2011-03-05 01:06:41 -0500

    Fixed code indentation consistency.

--- kio/kio/slave.h
+++ kio/kio/slave.h
@@ -47,14 +47,13 @@ class SimpleJob;
 // TODO: KDE5: Separate public API and private stuff for this better
 class KIO_SLAVE_EXPORT Slave : public KIO::SlaveInterface
 {
-	Q_OBJECT
+  Q_OBJECT
 public:
-	explicit Slave(const QString &protocol, QObject *parent = 0);
+  explicit Slave(const QString &protocol, QObject *parent = 0);
 
   virtual ~Slave();
 
-	void setPID(pid_t);
-
+  void setPID(pid_t);
   int slave_pid();
 
   void setJob(KIO::SimpleJob *job);
@@ -91,23 +90,24 @@ public:
   virtual void setConfig(const MetaData &config);
 
   /**
-	 * The protocol this slave handles.
-	 *
+   * The protocol this slave handles.
+   *
    * @return name of protocol handled by this slave, as seen by the user
    */
   QString protocol();
 
   void setProtocol(const QString & protocol);
+
   /**
-	 * The actual protocol used to handle the request.
-	 *
-	 * This method will return a different protocol than
-	 * the one obtained by using protocol() if a
-	 * proxy-server is used for the given protocol.  This
-	 * usually means that this method will return "http"
-	 * when the actuall request was to retrieve a resource
-	 * from an "ftp" server by going through a proxy server.
-	 *
+   * The actual protocol used to handle the request.
+   *
+   * This method will return a different protocol than
+   * the one obtained by using protocol() if a
+   * proxy-server is used for the given protocol.  This
+   * usually means that this method will return "http"
+   * when the actuall request was to retrieve a resource
+   * from an "ftp" server by going through a proxy server.
+   *
    * @return the actual protocol (io-slave) that handled the request
    */
   QString slaveProtocol();
@@ -132,67 +132,70 @@ public:
    */
   QString passwd();
 
-	/**
-	 * Creates a new slave.
-	 *
-	 * @param protocol the protocol
-	 * @param url is the url
-	 * @param error is the error code on failure and undefined else.
-	 * @param error_text is the error text on failure and undefined else.
-	 *
-	 * @return 0 on failure, or a pointer to a slave otherwise.
-	 */
-	static Slave* createSlave( const QString &protocol, const KUrl& url, int& error, QString& error_text );
+  /**
+   * Creates a new slave.
+   *
+   * @param protocol the protocol
+   * @param url is the url
+   * @param error is the error code on failure and undefined else.
+   * @param error_text is the error text on failure and undefined else.
+   *
+   * @return 0 on failure, or a pointer to a slave otherwise.
+   */
+  static Slave* createSlave( const QString &protocol, const KUrl& url, int& error, QString& error_text );
 
-	/**
-	 * Requests a slave on hold for ths url, from klauncher, if there is such a job.
-	 * See hold()
-	 */
-	static Slave* holdSlave( const QString &protocol, const KUrl& url );
+  /**
+   * Requests a slave on hold for ths url, from klauncher, if there is such a job.
+   * See hold()
+   */
+  static Slave* holdSlave( const QString &protocol, const KUrl& url );
 
-	// == communication with connected kioslave ==
-	// whenever possible prefer these methods over the respective
-	// methods in connection()
-	/**
-	 * Suspends the operation of the attached kioslave.
-	 */
-        virtual void suspend();
-	/**
-	 * Resumes the operation of the attached kioslave.
-	 */
-        virtual void resume();
-	/**
-	 * Tells whether the kioslave is suspended.
-	 * @return true if the kioslave is suspended.
-	 */
-        virtual bool suspended();
-	/**
-	 * Sends the given command to the kioslave.
-	 * @param cmd command id
-	 * @param arr byte array containing data
-	 */
-        virtual void send(int cmd, const QByteArray &arr = QByteArray());
+  // == communication with connected kioslave ==
+  // whenever possible prefer these methods over the respective
+  // methods in connection()
+  /**
+   * Suspends the operation of the attached kioslave.
+   */
+  virtual void suspend();
 
-	// == end communication with connected kioslave ==
-	/**
-	 * Puts the kioslave associated with @p url at halt, and return it to klauncher, in order
-	 * to let another application connect to it and finish the job.
-	 * This is for the krunner case: type a URL in krunner, it will start downloading
-	 * to find the mimetype (KRun), and then hold the slave, publish the held slave using,
-	 * this method, and the final application can continue the same download by requesting
-	 * the same URL.
-	 */
-	virtual void hold(const KUrl &url); // TODO KDE5: no reason to be virtual
+  /**
+   * Resumes the operation of the attached kioslave.
+   */
+  virtual void resume();
 
-	/**
-	 * @return The time this slave has been idle.
-	 */
-	time_t idleTime();
+  /**
+   * Tells whether the kioslave is suspended.
+   * @return true if the kioslave is suspended.
+   */
+  virtual bool suspended();
 
-	/**
-	 * Marks this slave as idle.
-	 */
-	void setIdle();
+  /**
+   * Sends the given command to the kioslave.
+   * @param cmd command id
+   * @param arr byte array containing data
+   */
+  virtual void send(int cmd, const QByteArray &arr = QByteArray());
+
+  // == end communication with connected kioslave ==
+  /**
+   * Puts the kioslave associated with @p url at halt, and return it to klauncher, in order
+   * to let another application connect to it and finish the job.
+   * This is for the krunner case: type a URL in krunner, it will start downloading
+   * to find the mimetype (KRun), and then hold the slave, publish the held slave using,
+   * this method, and the final application can continue the same download by requesting
+   * the same URL.
+   */
+  virtual void hold(const KUrl &url); // TODO KDE5: no reason to be virtual
+
+  /**
+   * @return The time this slave has been idle.
+   */
+  time_t idleTime();
+
+  /**
+   * Marks this slave as idle.
+   */
+  void setIdle();
 
   /*
    * @returns Whether the slave is connected
@@ -206,8 +209,8 @@ public:
 
 public Q_SLOTS:
   void accept();
-	void gotInput();
-	void timeout();
+  void gotInput();
+  void timeout();
 
 Q_SIGNALS:
   void slaveDied(KIO::Slave *slave);
