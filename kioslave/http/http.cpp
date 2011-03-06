0b952035a61707fb870ea7239160a57fdeac631d
Maks Orlovich <maksim@kde.org>
2011-03-06 10:02:53 -0500

    Fix regression in charset parsing.
    Implicit LWS is permitted between tokens in key/value pairs.
    
    BUG: 260885

--- kioslave/http/http.cpp
+++ kioslave/http/http.cpp
@@ -3034,6 +3034,8 @@ endParsing:
                     mediaAttribute = toQString(statement.mid(0, index));
                     mediaValue = toQString(statement.mid(index+1));
                 }
+                mediaAttribute = mediaAttribute.trimmed();
+                mediaValue     = mediaValue.trimmed();
 
                 if (mediaValue.startsWith(QLatin1Char('"'))) {
                     mediaValue.remove(QLatin1Char('"'));
