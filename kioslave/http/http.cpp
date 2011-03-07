KDE/kdelibs

commit 79d7273aff384fa6d1a6a209182fd0ad42ac691a

Retry the request if server disconnected before the content body could be sent to it.
This is the same protection as the one in ::sendQuery and protects against crashes like the one that prints out the following:

kio_http(3980)/kio (kioslave): Got cmd  77  while waiting for an answer!
kioslave: ####### CRASH ###### protocol = https pid = 3980 signal = 6

Conflicts:

	kioslave/http/http.cpp

kioslave/http/http.cpp
@@ -3831,11 +3831,17 @@ bool HTTPProtocol::sendBody()
   kDebug( 7113 ) << cLength.trimmed();
 
   // Send the content length...
-  bool sendOk = (write(cLength.toLatin1(), cLength.length()) == (ssize_t) cLength.length());
-  if (!sendOk)
-  {
-    kDebug( 7113 ) << "Connection broken when sending "
-                    << "content length: (" << m_request.url.host() << ")";
+  bool sendOk = (write(cLength.data(), cLength.size()) == (ssize_t) cLength.size());
+  if (!sendOk) {
+    // The server might have closed the connection due to a timeout, or maybe
+    // some transport problem arose while the connection was idle.
+    if (m_request.isKeepAlive)
+    {
+      httpCloseConnection();
+      return true; // Try again
+    }
+
+    kDebug(7113) << "Connection broken while sending POST content size to" << m_request.url.host();
     error( ERR_CONNECTION_BROKEN, m_request.url.host() );
     return false;
   }

commit 7bb4e5237f9cdcc454d02270479abc2dc4447f0c

Fix the writing out of the Content-Length header.
Regression introduced by 79d7273aff384fa6d1a6a209182fd0ad42ac691a's use
of .data() instead of .toLatin1().

kioslave/http/http.cpp
@@ -3827,11 +3827,11 @@ bool HTTPProtocol::sendBody()
 
   infoMessage(i18n("Sending data to %1" ,  m_request.url.host()));
 
-  const QString cLength = QLatin1String("Content-Length: ") + QString::number(m_POSTbuf.size()) + QLatin1String("\r\n\r\n");
-  kDebug( 7113 ) << cLength.trimmed();
+  const QByteArray cLength = QByteArray("Content-Length: ") + QByteArray::number(m_POSTbuf.size()) + "\r\n\r\n";
+  kDebug(7113) << cLength.trimmed();
 
   // Send the content length...
-  bool sendOk = (write(cLength.data(), cLength.size()) == (ssize_t) cLength.size());
+  bool sendOk = (write(cLength.constData(), cLength.size()) == (ssize_t) cLength.size());
   if (!sendOk) {
     // The server might have closed the connection due to a timeout, or maybe
     // some transport problem arose while the connection was idle.
@@ -3847,7 +3847,7 @@ bool HTTPProtocol::sendBody()
   }
 
   // Send the data...
-  // kDebug( 7113 ) << "POST DATA:" << QCString(m_POSTbuf);
+  //kDebug(7113) << "POST DATA:" << QString::fromLocal8Bit(m_POSTbuf);
   sendOk = (write(m_POSTbuf.data(), m_POSTbuf.size()) == (ssize_t) m_POSTbuf.size());
   if (!sendOk)
   {