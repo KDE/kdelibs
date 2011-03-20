bca14a638b1fe1b993f1408a0d33da863bcfd6f6
Dawit Alemayehu <adawit@kde.org>
2011-03-20 17:02:13 -0400

    assert -> Q_ASSERT

--- kioslave/ftp/ftp.cpp
+++ kioslave/ftp/ftp.cpp
@@ -43,7 +43,6 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-#include <assert.h>
 #include <ctype.h>
 #include <errno.h>
 #include <fcntl.h>
@@ -244,7 +243,7 @@ void Ftp::ftpCloseControlConnection()
  */
 const char* Ftp::ftpResponse(int iOffset)
 {
-  assert(m_control != NULL);    // must have control connection socket
+  Q_ASSERT(m_control != NULL);    // must have control connection socket
   const char *pTxt = m_lastControlLine.data();
 
   // read the next line ...
@@ -343,7 +342,7 @@ bool Ftp::ftpOpenConnection (LoginMode loginMode)
   // check for implicit login if we are already logged on ...
   if(loginMode == loginImplicit && m_bLoggedOn)
   {
-    assert(m_control != NULL);    // must have control connection socket
+    Q_ASSERT(m_control != NULL);    // must have control connection socket
     return true;
   }
 
@@ -358,7 +357,7 @@ bool Ftp::ftpOpenConnection (LoginMode loginMode)
     return false;
   }
 
-  assert( !m_bLoggedOn );
+  Q_ASSERT( !m_bLoggedOn );
 
   m_initialPath.clear();
   m_currentPath.clear();
@@ -442,7 +441,7 @@ bool Ftp::ftpLogin()
 {
   infoMessage( i18n("Sending login information") );
 
-  assert( !m_bLoggedOn );
+  Q_ASSERT( !m_bLoggedOn );
 
   QString user = m_user;
   QString pass = m_pass;
@@ -675,7 +674,7 @@ void Ftp::ftpAutoLoginMacro ()
  */
 bool Ftp::ftpSendCmd( const QByteArray& cmd, int maxretries )
 {
-  assert(m_control != NULL);    // must have control connection socket
+  Q_ASSERT(m_control != NULL);    // must have control connection socket
 
   if ( cmd.indexOf( '\r' ) != -1 || cmd.indexOf( '\n' ) != -1)
   {
@@ -774,8 +773,8 @@ bool Ftp::ftpSendCmd( const QByteArray& cmd, int maxretries )
  */
 int Ftp::ftpOpenPASVDataConnection()
 {
-  assert(m_control != NULL);    // must have control connection socket
-  assert(m_data == NULL);       // ... but no data connection
+  Q_ASSERT(m_control != NULL);    // must have control connection socket
+  Q_ASSERT(m_data == NULL);       // ... but no data connection
 
   // Check that we can do PASV
   QHostAddress addr = m_control->peerAddress();
@@ -832,8 +831,8 @@ int Ftp::ftpOpenPASVDataConnection()
  */
 int Ftp::ftpOpenEPSVDataConnection()
 {
-  assert(m_control != NULL);    // must have control connection socket
-  assert(m_data == NULL);       // ... but no data connection
+  Q_ASSERT(m_control != NULL);    // must have control connection socket
+  Q_ASSERT(m_data == NULL);       // ... but no data connection
 
   QHostAddress address = m_control->peerAddress();
   int portnum;
@@ -877,7 +876,7 @@ int Ftp::ftpOpenEPSVDataConnection()
 int Ftp::ftpOpenDataConnection()
 {
   // make sure that we are logged on and have no data connection...
-  assert( m_bLoggedOn );
+  Q_ASSERT( m_bLoggedOn );
   ftpCloseDataConnection();
 
   int  iErrCode = 0;
@@ -924,8 +923,8 @@ int Ftp::ftpOpenDataConnection()
  */
 int Ftp::ftpOpenPortDataConnection()
 {
-  assert(m_control != NULL);    // must have control connection socket
-  assert(m_data == NULL);       // ... but no data connection
+  Q_ASSERT(m_control != NULL);    // must have control connection socket
+  Q_ASSERT(m_data == NULL);       // ... but no data connection
 
   m_bPasv = false;
   if (m_extControl & eprtUnknown)
@@ -1117,7 +1116,7 @@ void Ftp::rename( const KUrl& src, const KUrl& dst, KIO::JobFlags flags )
 
 bool Ftp::ftpRename(const QString & src, const QString & dst, KIO::JobFlags jobFlags)
 {
-    assert(m_bLoggedOn);
+    Q_ASSERT(m_bLoggedOn);
 
     // Must check if dst already exists, RNFR+RNTO overwrites by default (#127793).
     if (!(jobFlags & KIO::Overwrite)) {
@@ -1172,7 +1171,7 @@ void Ftp::del( const KUrl& url, bool isfile )
 
 bool Ftp::ftpChmod( const QString & path, int permissions )
 {
-  assert( m_bLoggedOn );
+  Q_ASSERT( m_bLoggedOn );
 
   if(m_extControl & chmodUnknown)      // previous errors?
     return false;
@@ -1207,7 +1206,7 @@ void Ftp::chmod( const KUrl & url, int permissions )
 
 void Ftp::ftpCreateUDSEntry( const QString & filename, FtpEntry& ftpEnt, UDSEntry& entry, bool isDir )
 {
-  assert(entry.count() == 0); // by contract :-)
+  Q_ASSERT(entry.count() == 0); // by contract :-)
 
   entry.insert( KIO::UDSEntry::UDS_NAME, filename );
   entry.insert( KIO::UDSEntry::UDS_SIZE, ftpEnt.size );
@@ -1512,7 +1511,7 @@ bool Ftp::ftpOpenDir( const QString & path )
 
 bool Ftp::ftpReadDir(FtpEntry& de)
 {
-  assert(m_data != NULL);
+  Q_ASSERT(m_data != NULL);
 
   // get a line from the data connecetion ...
   while( true )
