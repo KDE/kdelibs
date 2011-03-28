9941ebff54bd9d4349c0384dfa0cca2ace9549c4
Marco Martin <notmart@gmail.com>
2011-03-28 17:00:48 +0200

    relicense to lgpl upon permission of authors

--- experimental/libkdeclarative/bindings/i18n.cpp
+++ experimental/libkdeclarative/bindings/i18n.cpp
@@ -2,9 +2,8 @@
  *   Copyright 2009 Aaron Seigo <aseigo@kde.org>
  *
  *   This program is free software; you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as
- *   published by the Free Software Foundation; either version 2, or
- *   (at your option) any later version.
+ *   it under the terms of the GNU Library General Public License version 2 as
+ *   published by the Free Software Foundation
  *
  *   This program is distributed in the hope that it will be useful,
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of
