ba22a1d02382503ca95832a149155e83632c36f6
Dawit Alemayehu <adawit@kde.org>
2011-04-05 17:46:58 -0400

    Do not trim whitespaces from filenames.
    BUG:88575
    REVIEW:100943
    FIXED-IN:4.6.2

--- kioslave/ftp/ftp.cpp
+++ kioslave/ftp/ftp.cpp
@@ -655,7 +655,7 @@ void Ftp::ftpAutoLoginMacro ()
         // TODO: Add support for arbitrary commands
         // besides simply changing directory!!
         if ( (*it).startsWith( QLatin1String("cwd") ) )
-          ftpFolder( (*it).mid(4).trimmed(), false );
+          ftpFolder( (*it).mid(4), false );
       }
 
       break;
@@ -1601,7 +1601,7 @@ bool Ftp::ftpReadDir(FtpEntry& de)
           continue; // Don't trick us!
         // Some sites put more than one space between the date and the name
         // e.g. ftp://ftp.uni-marburg.de/mirror/
-        de.name     = remoteEncoding()->decode(tmp.trimmed());
+        de.name     = remoteEncoding()->decode(tmp);
       }
 
       de.type = S_IFREG;
