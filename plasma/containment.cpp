4441b6a9961482c91f6a67620a32120c99778291
Alex Fiestas <afiestas@kde.org>
2011-04-11 15:06:27 +0200

    Add KAuthorize containment_context_menu into createToolBox
    Having the posibility of avoid the context_menu while we're
    allowing the cashew has no sense. So let's use the same action
    to lockdown both.

--- plasma/containment.cpp
+++ plasma/containment.cpp
@@ -2166,7 +2166,7 @@ void Containment::destroy(bool confirm)
 
 void ContainmentPrivate::createToolBox()
 {
-    if (!toolBox) {
+    if (!toolBox && KAuthorized::authorizeKAction("plasma/containment_context_menu")) {
         toolBox = Plasma::AbstractToolBox::load(q->corona()->preferredToolBoxPlugin(type), QVariantList(), q);
 
         if (toolBox) {
