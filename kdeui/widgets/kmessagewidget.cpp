0a95e0af02b5dda40a49d1deb30f13dcf7846e5e
Aurélien Gâteau <agateau@kde.org>
2011-04-23 22:20:23 +0200

    Typo in copyright year

--- kdeui/widgets/kmessagewidget.cpp
+++ kdeui/widgets/kmessagewidget.cpp
@@ -1,6 +1,6 @@
 /* This file is part of the KDE libraries
  *
- * Copyright (c) 2010 Aurélien Gâteau <agateau@kde.org>
+ * Copyright (c) 2011 Aurélien Gâteau <agateau@kde.org>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
