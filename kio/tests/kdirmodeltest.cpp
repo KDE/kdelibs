KDE/kdelibs

commit c8939409eed00420fb43ff22cfc6c9092e4da7e5

Fixed triggering of an assertion if the parent directory of the model has been deleted from outside (reviewed by David Faure). This fixes a crash in Dolphin and Konqueror if the currently shown directory gets deleted from outside.
CCMAIL: faure@kde.org
CCMAIL: riccardo@kde.org

svn path=/trunk/KDE/kdelibs/; revision=729674

kio/kio/kdirmodel.cpp
@@ -355,7 +355,8 @@ void KDirModelPrivate::_k_slotDeleteItem(const KFileItem& item)
         return;
 
     KDirModelDirNode* dirNode = node->parent();
-    Q_ASSERT(dirNode);
+    if (!dirNode)
+        return;
 
     QModelIndex parentIndex = indexForNode(dirNode); // O(n)
     q->beginRemoveRows( parentIndex, rowNumber, rowNumber );

commit 83538b4339a65c90764975f01a4b9bafbabd9595

Fix possible crash in KDirModel if the root item is deleted or moved
If the root item of the dir model is deleted, but it is not the first
item in the list that KDirModelPrivate::_k_slotDeleteItems(...) gets
from the dir lister, a crash may result because
KDirModelPrivate::removeFromNodeHash(...) calls isDir() for a null
KFileItem. This commit extends the protection agains this kind of crash
that has been introduced in c8939409eed00420fb43ff22cfc6c9092e4da7e5 for
the first item to the rest of the list.
CCBUG: 196695

kio/kio/kdirmodel.cpp
@@ -489,6 +489,11 @@ void KDirModelPrivate::_k_slotDeleteItems(const KFileItemList& items)
                 kWarning(7008) << "No node found for item that was just removed:" << url;
                 continue;
             }
+            if (!node->parent()) {
+                // The root node has been deleted, but it was not first in the list 'items'.
+                // see https://bugs.kde.org/show_bug.cgi?id=196695
+                return;
+            }
         }
         rowNumbers.setBit(node->rowNumber(), 1); // O(n)
         removeFromNodeHash(node, url);

kio/tests/kdirmodeltest.cpp
@@ -1066,6 +1066,27 @@ void KDirModelTest::testSmb()
     }
 }
 
+class MyDirLister : public KDirLister
+{
+public:
+    void emitItemsDeleted(const KFileItemList& items) { emit itemsDeleted(items); }
+};
+
+void KDirModelTest::testBug196695()
+{
+    KFileItem rootItem(KUrl(m_tempDir->name()), QString(), KFileItem::Unknown);
+    KFileItem childItem(KUrl(QString(m_tempDir->name() + "toplevelfile_1")), QString(), KFileItem::Unknown);
+
+    KFileItemList list;
+    // Important: the root item must not be first in the list to trigger bug 196695
+    list << childItem << rootItem;
+
+    MyDirLister* dirLister = static_cast<MyDirLister*>(m_dirModel->dirLister());
+    dirLister->emitItemsDeleted(list);
+
+    fillModel(true);
+}
+
 void KDirModelTest::testDeleteFile()
 {
     fillModel(false);

kio/tests/kdirmodeltest.h
@@ -60,6 +60,7 @@ private Q_SLOTS:
     void testRemoteUrlWithHost();
     void testZipFile();
     void testSmb();
+    void testBug196695();
 
     // These tests must be done last
     void testDeleteFile();