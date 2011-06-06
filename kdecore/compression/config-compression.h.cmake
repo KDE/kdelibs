5fa283b0e68eb5596b05260c562dea73cc793431
David Faure <faure@kde.org>
2011-06-06 03:21:21 +0200

    Use #cmakedefine01, shorter syntax.

--- kdecore/compression/config-compression.h.cmake
+++ kdecore/compression/config-compression.h.cmake
@@ -1,8 +1,8 @@
-#define HAVE_BZIP2_SUPPORT @HAVE_BZIP2_SUPPORT@
+#cmakedefine01 HAVE_BZIP2_SUPPORT
 
 /* Set to 1 if the libbz2 functions need the BZ2_ prefix */
-#define NEED_BZ2_PREFIX @NEED_BZ2_PREFIX@
+#cmakedefine01 NEED_BZ2_PREFIX
 
 /* Set to 1 if you have xz */
-#define HAVE_XZ_SUPPORT @HAVE_XZ_SUPPORT@
+#cmakedefine01 HAVE_XZ_SUPPORT
 
