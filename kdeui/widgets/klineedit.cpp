868d1919c2317a50da7aa118ceef0b2ab3215c88
Valentin Rusu <kde@rusu.info>
2011-06-11 18:21:59 +0200

    Clear selected text when middle clicking to prepare for paste
    This would prevent crashes in e.g. Kate

--- kdeui/widgets/klineedit.cpp
+++ kdeui/widgets/klineedit.cpp
@@ -1107,6 +1107,16 @@ void KLineEdit::mousePressEvent( QMouseEvent* e )
         e->accept();
         return;
     }
+    
+    // if middle clicking and if text is present in the clipboard then clear the selection
+    // to prepare paste operation
+    if ( e->button() == Qt::MidButton ) {
+      if ( QApplication::clipboard()->text( QClipboard::Selection ).length() >0 ) { 
+	if ( hasSelectedText() ) {
+	  backspace();
+	}
+      }
+    }
 
     QLineEdit::mousePressEvent( e );
 }
