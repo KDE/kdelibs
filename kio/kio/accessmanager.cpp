KDE/kdelibs

commit 1d2f85c8c94712ac2201a7b2efa875f3f9e9f322

rename isLocalRequest to AccessManager_isLocalRequest
move it into KDEPrivate namespace and make it nonstatic
so that it can be used from both classes

kio/kio/accessmanager.cpp
@@ -204,7 +204,7 @@ QNetworkReply *AccessManager::createRequest(Operation op, const QNetworkRequest 
     KIO::SimpleJob *kioJob = 0;
     const KUrl reqUrl (req.url());
 
-    if (!d->externalContentAllowed && !isLocalRequest(reqUrl)) {
+    if (!d->externalContentAllowed && !KDEPrivate::AccessManager_isLocalRequest(reqUrl)) {
         kDebug( 7044 ) << "Blocked: " << reqUrl;
         KDEPrivate::AccessManagerReply* reply = new KDEPrivate::AccessManagerReply(op, req, kioJob, this);
         reply->setStatus(i18n("Blocked request."),QNetworkReply::ContentAccessDenied);

kio/kio/accessmanagerreply_p.cpp
@@ -37,6 +37,13 @@
 
 namespace KDEPrivate {
 
+bool AccessManager_isLocalRequest(const KUrl& url)
+{
+    const QString scheme (url.protocol());
+    return (KProtocolInfo::isKnownProtocol(scheme) &&
+            KProtocolInfo::protocolClass(scheme).compare(QL1S(":local"), Qt::CaseInsensitive) == 0);
+}
+
 AccessManagerReply::AccessManagerReply(const QNetworkAccessManager::Operation &op,
                                        const QNetworkRequest &request,
                                        KIO::SimpleJob *kioJob,
@@ -152,7 +159,7 @@ void AccessManagerReply::readHttpResponseHeaders(KIO::Job *job)
     const KIO::MetaData& metaData = job->metaData();
     if (metaData.isEmpty()) {
         // Allow handling of local resources such as man pages and file url...
-        if (isLocalRequest(url())) {
+        if (AccessManager_isLocalRequest(url())) {
             setHeader(QNetworkRequest::ContentLengthHeader, job->totalAmount(KJob::Bytes));
             setAttribute(QNetworkRequest::HttpStatusCodeAttribute, "200");
             emit metaDataChanged();

kio/kio/accessmanagerreply_p.h
@@ -85,13 +85,8 @@ private:
 };
 
 
-static bool isLocalRequest(const KUrl& url)
-{
-    const QString scheme (url.protocol());
-    return (KProtocolInfo::isKnownProtocol(scheme) &&
-            KProtocolInfo::protocolClass(scheme).compare(QL1S(":local"), Qt::CaseInsensitive) == 0);
-}
 
+bool AccessManager_isLocalRequest(const KUrl&);
 
 }
 

commit 588c3abc075af0690928a8c6f4a1227bd5379f20

Fix logic broken by commit 1d2f85c8.

kio/kio/accessmanager.cpp
@@ -204,7 +204,7 @@ QNetworkReply *AccessManager::createRequest(Operation op, const QNetworkRequest 
     KIO::SimpleJob *kioJob = 0;
     const KUrl reqUrl (req.url());
 
-    if (!d->externalContentAllowed && !KDEPrivate::AccessManager_isLocalRequest(reqUrl)) {
+    if (!d->externalContentAllowed && !KDEPrivate::AccessManager_isLocalRequest(reqUrl) && reqUrl.scheme() != QL1S("data")) {
         kDebug( 7044 ) << "Blocked: " << reqUrl;
         KDEPrivate::AccessManagerReply* reply = new KDEPrivate::AccessManagerReply(op, req, kioJob, this);
         reply->setStatus(i18n("Blocked request."),QNetworkReply::ContentAccessDenied);