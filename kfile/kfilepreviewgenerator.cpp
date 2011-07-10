b078346038bb7bfdec2d73936fefb58d6af151eb
Frank Reininghaus <frank78ac@googlemail.com>
2011-07-10 15:25:40 +0200

    KFilePreviewGenerator: update JPG preview plugin settings if needed
    In KDE <= 4.6, the 'jpegrotatedthumbnail' plugin was used to provide
    thumbnails for JPG images which are rotated correctly. In KDE 4.7, this
    is done by the usual 'jpegthumbnail' plugin if it is configured
    accordingly. This patch adds a check to KFilePreviewGenerator's
    constructor that replaces the obsolete plugin by the correct one.
    
    CCBUG:277251
    (cherry picked from commit e3c0ddfa5b4c0085687884a4e385241b2d86f7c4)

--- kfile/kfilepreviewgenerator.cpp
+++ kfile/kfilepreviewgenerator.cpp
@@ -480,11 +480,20 @@ KFilePreviewGenerator::Private::Private(KFilePreviewGenerator* parent,
     connect(m_changedItemsTimer, SIGNAL(timeout()),
             q, SLOT(delayedIconUpdate()));
 
-    const KConfigGroup globalConfig(KGlobal::config(), "PreviewSettings");
+    KConfigGroup globalConfig(KGlobal::config(), "PreviewSettings");
     m_enabledPlugins = globalConfig.readEntry("Plugins", QStringList()
                                                          << "directorythumbnail"
                                                          << "imagethumbnail"
                                                          << "jpegthumbnail");
+
+    // If the user is upgrading from KDE <= 4.6, we must check if he had the 'jpegrotatedthumbnail' plugin enabled.
+    // This plugin does not exist any more in KDE >= 4.7, so we have to replace it with the 'jpegthumbnail' plugin.
+    if(m_enabledPlugins.contains(QLatin1String("jpegrotatedthumbnail"))) {
+        m_enabledPlugins.removeAll(QLatin1String("jpegrotatedthumbnail"));
+        m_enabledPlugins.append(QLatin1String("jpegthumbnail"));
+        globalConfig.writeEntry("Plugins", m_enabledPlugins);
+        globalConfig.sync();
+    }
 }
 
 KFilePreviewGenerator::Private::~Private()
