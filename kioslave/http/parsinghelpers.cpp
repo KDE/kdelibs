54e8eded22c4af61f609b6184cc0293df407a2f9
Rolf Eike Beer <eike-kernel@sf-tec.de>
2011-07-16 16:19:18 +0200

    fix filename security check being omitted on parse error in HTTP header
    A header like this:
    
    Content-Disposition: attachment; filename="/home/eike/.gnupg/gpg.conf";
      foo="bar; foo="baz"
    
    would not have the path from the filename stripped because of the later parse
    error.
    
    This adds a unit test for this and some other cornercases.
    
    CCBUG:278643

--- kioslave/http/parsinghelpers.cpp
+++ kioslave/http/parsinghelpers.cpp
@@ -23,6 +23,7 @@
 #include <QUrl>
 
 #include <kcodecs.h>
+#include <kdebug.h>
 
 // Advance *pos beyond spaces / tabs
 static void skipSpace(const char input[], int *pos, int end)
@@ -400,7 +401,7 @@ static QString extractMaybeQuotedUntil(const QString &str, int &pos)
     }
 }
 
-static QMap<QString, QString> contentDispositionParser(const QString &disposition)
+static QMap<QString, QString> contentDispositionParserInternal(const QString &disposition)
 {
     kDebug(7113) << "disposition: " << disposition;
     int pos = 0;
@@ -423,7 +424,7 @@ static QMap<QString, QString> contentDispositionParser(const QString &dispositio
         if (key.isEmpty()) {
             // parse error in this key: do not parse more, but add up
             // everything we already got
-            kDebug(7113) << "parse error, abort parsing";
+            kDebug(7113) << "parse error in key, abort parsing";
             break;
         }
 
@@ -436,7 +437,7 @@ static QMap<QString, QString> contentDispositionParser(const QString &dispositio
 
         if (val.isEmpty()) {
             if (pos == -1) {
-                kDebug(7113) << "parse error, abort parsing";
+                kDebug(7113) << "parse error in value, abort parsing";
                 break;
             }
             continue;
@@ -550,6 +551,13 @@ static QMap<QString, QString> contentDispositionParser(const QString &dispositio
             parameters.insert(i.key(), val);
         }
     }
+
+    return parameters;
+}
+
+static QMap<QString, QString> contentDispositionParser(const QString &disposition)
+{
+    QMap<QString, QString> parameters = contentDispositionParserInternal(disposition);
 
     const QLatin1String fn("filename");
     if (parameters.contains(fn)) {
