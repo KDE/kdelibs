ebf830c5929b8946c87eb3133633a2e65ebae908
Dawit Alemayehu <adawit@kde.org>
2011-08-04 13:03:47 -0400

    Changed the column name of the unit tests in 'testAuthentication_data' to reflect their origin. (cherry picked from commit f2b6b86313b56e5ccc642c7733ac7deca1d52355)

--- kioslave/http/tests/httpauthenticationtest.cpp
+++ kioslave/http/tests/httpauthenticationtest.cpp
@@ -161,18 +161,20 @@ void HTTPAuthenticationTest::testAuthentication_data()
     QTest::addColumn<QByteArray>("cnonce");
 
     // Test cases from  RFC 2617...
-    QTest::newRow("RfC2617-alladin") << QByteArray("Basic realm=\"WallyWorld\"") <<
-                            QByteArray("Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ==") <<
-                            QByteArray("Aladdin") <<
-                            QByteArray("open sesame") <<
-                            QByteArray() <<
-                            QByteArray();
-    QTest::newRow("RfC2617-mufasa") << QByteArray("Digest realm=\"testrealm@host.com\", qop=\"auth,auth-int\", nonce=\"dcd98b7102dd2f0e8b11d0f600bfb0c093\", opaque=\"5ccc069c403ebaf9f0171e9517f40e41\"") <<
-                            QByteArray("Digest username=\"Mufasa\", realm=\"testrealm@host.com\", nonce=\"dcd98b7102dd2f0e8b11d0f600bfb0c093\", uri=\"/dir/index.html\", algorithm=MD5, qop=auth, cnonce=\"0a4f113b\", nc=00000001, response=\"6629fae49393a05397450978507c4ef1\", opaque=\"5ccc069c403ebaf9f0171e9517f40e41\"") <<
-                            QByteArray("Mufasa") <<
-                            QByteArray("Circle Of Life") <<
-                            QByteArray("http://www.nowhere.org/dir/index.html") <<
-                            QByteArray("0a4f113b");
+    QTest::newRow("rfc-2617-basic-example")
+        << QByteArray("Basic realm=\"WallyWorld\"")
+        << QByteArray("Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ==")
+        << QByteArray("Aladdin")
+        << QByteArray("open sesame")
+        << QByteArray()
+        << QByteArray();
+    QTest::newRow("rfc-2617-digest-example")
+        << QByteArray("Digest realm=\"testrealm@host.com\", qop=\"auth,auth-int\", nonce=\"dcd98b7102dd2f0e8b11d0f600bfb0c093\", opaque=\"5ccc069c403ebaf9f0171e9517f40e41\"")
+        << QByteArray("Digest username=\"Mufasa\", realm=\"testrealm@host.com\", nonce=\"dcd98b7102dd2f0e8b11d0f600bfb0c093\", uri=\"/dir/index.html\", algorithm=MD5, qop=auth, cnonce=\"0a4f113b\", nc=00000001, response=\"6629fae49393a05397450978507c4ef1\", opaque=\"5ccc069c403ebaf9f0171e9517f40e41\"")
+        << QByteArray("Mufasa")
+        << QByteArray("Circle Of Life")
+        << QByteArray("http://www.nowhere.org/dir/index.html")
+        << QByteArray("0a4f113b");
 }
 
 void HTTPAuthenticationTest::testAuthentication()
