5ab0e0b1d1438191e2f29d934c885bb35ba497e7
Valentin Rusu <kde@rusu.info>
2011-08-08 18:44:28 +0200

    Added versioning information
    Took into account Alin's version information macros

---
+++ version.h
@@ -0,0 +1,28 @@
+/* This file is part of the KDE project
+ *
+ * Copyright (C) 2011 Valentin Rusu <kde@rusu.info>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifndef KSECRETSSERVICECLIENT_VERSION_H
+#define KSECRETSSERVICECLIENT_VERSION_H
+
+static const char ksecretsserviceclient_version[] = "0.1.0";
+
+#define KSECRETSSERVICECLIENT_VERSION 0x000100
+
+#endif // KSECRETSSERVICECLIENT_VERSION_H
