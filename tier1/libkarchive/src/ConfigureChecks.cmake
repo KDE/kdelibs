db587412087bfc226e7fd9775a25e82097fa27b5
Stephen Kelly <steveire@gmail.com>
2011-08-26 12:01:15 +0200

    We already use cmakedefine01, so no need for the macro in addition.

--- tier1/libkarchive/src/ConfigureChecks.cmake
+++ tier1/libkarchive/src/ConfigureChecks.cmake
@@ -4,9 +4,9 @@ macro_log_feature(BZIP2_FOUND "BZip2" "Support for BZip2 compressed files and da
 macro_optional_find_package(LibLZMA)
 macro_log_feature(LIBLZMA_FOUND "LZMA/XZ" "Support for xz compressed files and data streams" "http://tukaani.org/xz/" FALSE "" "")
 
-macro_bool_to_01(BZIP2_FOUND HAVE_BZIP2_SUPPORT)
+set(HAVE_BZIP2_SUPPORT ${BZIP2_FOUND})
 if(BZIP2_FOUND AND BZIP2_NEED_PREFIX)
     set(NEED_BZ2_PREFIX 1)
 endif(BZIP2_FOUND AND BZIP2_NEED_PREFIX)
 
-macro_bool_to_01(LIBLZMA_FOUND HAVE_XZ_SUPPORT)
+set(HAVE_XZ_SUPPORT ${LIBLZMA_FOUND})
