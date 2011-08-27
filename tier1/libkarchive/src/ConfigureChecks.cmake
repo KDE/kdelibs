9422f315cb2a3e27417d897b0756cd21c1f466cf
Stephen Kelly <steveire@gmail.com>
2011-08-27 18:19:37 +0200

    Remove usage of macro_optional_find_package.
    It is obsolete with CMake 2.8.6 which provides options to disable finding packages.

--- tier1/libkarchive/src/ConfigureChecks.cmake
+++ tier1/libkarchive/src/ConfigureChecks.cmake
@@ -1,4 +1,4 @@
-macro_optional_find_package(BZip2)
+find_package(BZip2)
 set_package_properties(BZip2 PROPERTIES
   URL "http://www.bzip.org"
   DESCRIPTION "Support for BZip2 compressed files and data streams"
@@ -6,7 +6,7 @@ set_package_properties(BZip2 PROPERTIES
   PURPOSE "Support for BZip2 compressed files and data streams"
 )
 
-macro_optional_find_package(LibLZMA)
+find_package(LibLZMA)
 set_package_properties(LibLZMA PROPERTIES
   URL "http://tukaani.org/xz/"
   DESCRIPTION "Support for xz compressed files and data streams"
