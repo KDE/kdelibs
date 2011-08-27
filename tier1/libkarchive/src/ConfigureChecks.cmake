3a16229be9d1bb72187d515ba2ee0da65c0e35e9
Stephen Kelly <steveire@gmail.com>
2011-08-27 09:28:13 +0200

    Start porting to feature_summary.

--- tier1/libkarchive/src/ConfigureChecks.cmake
+++ tier1/libkarchive/src/ConfigureChecks.cmake
@@ -1,8 +1,19 @@
 macro_optional_find_package(BZip2)
-macro_log_feature(BZIP2_FOUND "BZip2" "Support for BZip2 compressed files and data streams" "http://www.bzip.org" FALSE "" "STRONGLY RECOMMENDED")
+find_package(BZip2)
+set_package_properties(BZip2 PROPERTIES
+  URL "http://www.bzip.org"
+  DESCRIPTION "Support for BZip2 compressed files and data streams"
+  TYPE RECOMMENDED
+  PURPOSE "Support for BZip2 compressed files and data streams"
+)
 
 macro_optional_find_package(LibLZMA)
-macro_log_feature(LIBLZMA_FOUND "LZMA/XZ" "Support for xz compressed files and data streams" "http://tukaani.org/xz/" FALSE "" "")
+find_package(LibLZMA)
+set_package_properties(LibLZMA PROPERTIES
+  URL "http://tukaani.org/xz/"
+  DESCRIPTION "Support for xz compressed files and data streams"
+  PURPOSE "Support for xz compressed files and data streams"
+)
 
 set(HAVE_BZIP2_SUPPORT ${BZIP2_FOUND})
 if(BZIP2_FOUND AND BZIP2_NEED_PREFIX)
