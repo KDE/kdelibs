d13a3ad5bb0ffb77705e609516434a07c6dc11b2
Romain Perier <bambi@ubuntu.com>
2011-11-01 15:06:27 +0100

    Port KConfigInit to the new KLockFile from kcoreaddons (don't use KLockFile::Ptr anymore)

--- kdecore/config/kconfigini.cpp
+++ kdecore/config/kconfigini.cpp
@@ -578,7 +578,8 @@ bool KConfigIniBackend::lock(const KComponentData& componentData)
 void KConfigIniBackend::unlock()
 {
     lockFile->unlock();
-    lockFile.clear();
+    delete lockFile;
+    lockFile = NULL;
 }
 
 bool KConfigIniBackend::isLocked() const
