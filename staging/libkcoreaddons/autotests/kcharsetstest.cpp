f00ffccd1f2badb81de9b9bf461ee73c88afa3de
Romain Perier <bambi@ubuntu.com>
2011-12-10 14:56:19 +0100

    Move kcharsetstest from kdecore to kcoreaddons (everything is ported to ECMQtFrameworks)

--- staging/libkcoreaddons/autotests/kcharsetstest.cpp
+++ staging/libkcoreaddons/autotests/kcharsetstest.cpp
@@ -1,7 +1,7 @@
 
 #include <kcharsets.h>
-#include <kdebug.h>
 #include <QtCore/QString>
+#include <QtCore/QDebug>
 
 #include <assert.h>
 
@@ -16,15 +16,16 @@ int main()
     // Test that toLocal8Bit works even without a QCoreApplication,
     // thanks to the static initializer in KCatalog.
     // Do NOT move this code to a QTestLib unit test ;-)
+
     QString one = QString::fromUtf8("é");
     QByteArray one8bit = one.toLocal8Bit();
     if (qgetenv("LANG").endsWith("UTF-8")) { // krazy:exclude=strings
-        kDebug() << one << one8bit;
+        qDebug() << one << one8bit;
         Q_ASSERT(one8bit.length() == 2);
     }
 
-    QString input( "&lt;Hello &amp;World&gt;" );
+    QString input = QString::fromLatin1( "&lt;Hello &amp;World&gt;" );
     QString output = KCharsets::resolveEntities( input );
-    assert( output == "<Hello &World>" );
+    assert( output == QString::fromLatin1("<Hello &World>") );
     return 0;
 }
