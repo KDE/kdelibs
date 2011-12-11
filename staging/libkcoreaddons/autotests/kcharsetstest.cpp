c28722b20f8262be9c58c17cd1134b72d13ca342
Romain Perier <bambi@ubuntu.com>
2011-12-11 11:37:22 +0100

    Write a real unittest for KCharsets (not perfect yet)

--- staging/libkcoreaddons/autotests/kcharsetstest.cpp
+++ staging/libkcoreaddons/autotests/kcharsetstest.cpp
@@ -1,31 +1,109 @@
+/*
+ * Copyright 2011  Romain Perier <bambi@kubuntu.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "kcharsetstest.h"
 
 #include <kcharsets.h>
 #include <QtCore/QString>
 #include <QtCore/QDebug>
+#include <QtTest/QtTest>
 
-#include <assert.h>
-
-int main()
+static bool encodingNameHasADescription(const QString &encodingName, const QStringList &descriptions)
 {
-    // Note: ctest sets LANG=C, while running the test directly uses your real $LANG.
-    // And since the static initializer runs before main, we cannot setenv("LANG") here,
-    // it's too late.
-    //kDebug() << "LANG=" << getenv("LANG");
-    //kDebug() << "LC_ALL=" << getenv("LC_ALL");
-
-    // Test that toLocal8Bit works even without a QCoreApplication,
-    // thanks to the static initializer in KCatalog.
-    // Do NOT move this code to a QTestLib unit test ;-)
-
-    QString one = QString::fromUtf8("é");
-    QByteArray one8bit = one.toLocal8Bit();
-    if (qgetenv("LANG").endsWith("UTF-8")) { // krazy:exclude=strings
-        qDebug() << one << one8bit;
-        Q_ASSERT(one8bit.length() == 2);
-    }
-
-    QString input = QString::fromLatin1( "&lt;Hello &amp;World&gt;" );
-    QString output = KCharsets::resolveEntities( input );
-    assert( output == QString::fromLatin1("<Hello &World>") );
-    return 0;
+    Q_FOREACH(QString description, descriptions)
+        if (description.contains(encodingName))
+            return true;
+    return false;
 }
+
+void KCharsetsTest::testSingleton()
+{
+    QVERIFY(KCharsets::charsets() != NULL);
+    QCOMPARE(KCharsets::charsets(), KCharsets::charsets());
+}
+
+void KCharsetsTest::testCodecForName()
+{ 
+    KCharsets *singleton = KCharsets::charsets();
+
+    QCOMPARE(singleton->codecForName(QString::fromLatin1("ISO-8859-1"))->name(), QTextCodec::codecForName("ISO 8859-1")->name());
+    QCOMPARE(singleton->codecForName(QString::fromLatin1("ISO 8859-1"))->name(), QTextCodec::codecForName("iso-8859-1")->name());
+    QCOMPARE(singleton->codecForName(QString::fromLatin1("iso 8859-1"))->name(), QTextCodec::codecForName("iso-8859-1")->name());
+    QCOMPARE(singleton->codecForName(QString::fromLatin1("utf-8"))->name(), QTextCodec::codecForName("UTF-8")->name());
+    QCOMPARE(singleton->codecForName(QString::fromLatin1("utf8"))->name(), QTextCodec::codecForName("UTF-8")->name());
+    QCOMPARE(singleton->codecForName(QString::fromLatin1("UTF8"))->name(), QTextCodec::codecForName("UTF-8")->name());
+
+    QCOMPARE(singleton->codecForName(QString::fromLatin1(""))->name(), QTextCodec::codecForLocale()->name());
+    QCOMPARE(singleton->codecForName(QString::fromLatin1("fake encoding"))->name(), QTextCodec::codecForName("iso-8859-1")->name());
+}
+
+void KCharsetsTest::testFromEntity()
+{
+    KCharsets *singleton = KCharsets::charsets();
+
+    QCOMPARE(singleton->fromEntity(QString::fromLatin1("&#1234")), QChar(1234));
+    QCOMPARE(singleton->fromEntity(QString::fromLatin1("&#x1234")), QChar(0x1234));
+    QCOMPARE(singleton->fromEntity(QString::fromLatin1("lt")), QChar::fromLatin1('<'));
+    QCOMPARE(singleton->fromEntity(QString::fromLatin1("gt")), QChar::fromLatin1('>'));
+    QCOMPARE(singleton->fromEntity(QString::fromLatin1("quot")), QChar::fromLatin1('"'));
+    QCOMPARE(singleton->fromEntity(QString::fromLatin1("amp")), QChar::fromLatin1('&'));
+    QCOMPARE(singleton->fromEntity(QString::fromLatin1("apos")), QChar::fromLatin1('\''));
+}
+
+void KCharsetsTest::testToEntity()
+{
+    KCharsets *singleton = KCharsets::charsets();
+
+    // Fix KCharsets::toEntity() first
+}
+
+void KCharsetsTest::testResolveEntities()
+{
+    KCharsets *singleton = KCharsets::charsets();
+
+    QCOMPARE(singleton->resolveEntities(QString::fromLatin1("&quot;&apos;&lt;Hello &amp;World&gt;&apos;&quot;")),
+        QString::fromLatin1("\"\'<Hello &World>\'\"")); 
+}
+
+void KCharsetsTest::testEncodingNames()
+{
+    KCharsets *singleton = KCharsets::charsets();
+
+    QCOMPARE(singleton->availableEncodingNames().count(), singleton->descriptiveEncodingNames().count());
+
+    Q_FOREACH(QString encodingName, singleton->availableEncodingNames()) {
+        bool ok = false;
+
+        if (encodingName == QString::fromLatin1("ucs2") || encodingName == QString::fromLatin1("ISO 10646-UCS-2"))
+            singleton->codecForName(QString::fromLatin1("UTF-16"), ok);
+        else if (encodingName == QString::fromLatin1("utf7"))
+            continue;
+        else
+            singleton->codecForName(encodingName, ok);
+
+        if (!ok) {
+            qDebug() << "Error:" << encodingName << "not found";
+            QVERIFY(NULL);
+	}
+        QVERIFY( encodingNameHasADescription(encodingName, singleton->descriptiveEncodingNames()) );
+        QVERIFY( !singleton->descriptionForEncoding(encodingName).isEmpty() );
+        QCOMPARE( singleton->encodingForName( singleton->descriptionForEncoding(encodingName) ), encodingName );
+    }
+}
+
+
+QTEST_MAIN(KCharsetsTest)
---
+++ staging/libkcoreaddons/autotests/kcharsetstest.h
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2011  Romain Perier <bambi@kubuntu.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef KCHARSETSTEST_H
+#define KCHARSETSTEST_H
+
+#include <QtCore/QObject>
+
+class KCharsetsTest: public QObject
+{
+    Q_OBJECT
+private Q_SLOTS:
+    void testSingleton();
+    void testCodecForName();
+    void testFromEntity();
+    void testToEntity();
+    void testResolveEntities();
+    void testEncodingNames();
+};
+
+#endif /* KCHARSETSTEST_H */
