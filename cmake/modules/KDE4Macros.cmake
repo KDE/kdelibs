a41687ff79d763128502e4a0704b7e5f06a722aa
Stephen Kelly <steveire@gmail.com>
2012-02-08 23:44:46 +0100

    Split the DocTools macros into the doctools directory.

--- cmake/modules/KDE4Macros.cmake
+++ cmake/modules/KDE4Macros.cmake
@@ -42,137 +42,6 @@ macro (_KDE4_GET_CUSTOM_TARGET_PROPERTY _var _target_name _property_name)
    set(${_var} "${_kde4_${_dir}_${_target_name}_${_property_name}}")
 endmacro (_KDE4_GET_CUSTOM_TARGET_PROPERTY)
 
-macro (KDE4_CREATE_HANDBOOK _docbook)
-   get_filename_component(_input ${_docbook} ABSOLUTE)
-   set(_doc ${CMAKE_CURRENT_BINARY_DIR}/index.cache.bz2)
-
-   #Bootstrap
-   if (_kdeBootStrapping)
-      set(_ssheet "${CMAKE_BINARY_DIR}/kdoctools/customization/kde-chunk.xsl")
-      set(_bootstrapOption "--srcdir=${CMAKE_BINARY_DIR}/kdoctools/")
-   else (_kdeBootStrapping)
-      set(_ssheet "${KDE4_DATA_INSTALL_DIR}/ksgmltools2/customization/kde-chunk.xsl")
-      set(_bootstrapOption)
-   endif (_kdeBootStrapping)
-
-   file(GLOB _docs *.docbook)
-
-#   if (CMAKE_CROSSCOMPILING)
-#      set(IMPORT_MEINPROC4_EXECUTABLE "${KDE_HOST_TOOLS_PATH}/ImportMeinProc4Executable.cmake" CACHE FILEPATH "Point it to the export file of meinproc4 from a native build")
-#      include(${IMPORT_MEINPROC4_EXECUTABLE})
-#      set(KDE4_MEINPROC_EXECUTABLE meinproc4)
-#   endif (CMAKE_CROSSCOMPILING)
-
-   add_custom_command(OUTPUT ${_doc}
-      COMMAND ${KDE4_MEINPROC_EXECUTABLE} --check ${_bootstrapOption} --cache ${_doc} ${_input}
-      DEPENDS ${_docs} ${_KDE4_MEINPROC_EXECUTABLE_DEP} ${_ssheet}
-      WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-   )
-   get_filename_component(_targ ${CMAKE_CURRENT_SOURCE_DIR} NAME)
-   set(_targ "${_targ}-handbook")
-   add_custom_target(${_targ} ALL DEPENDS ${_doc})
-
-   if(KDE4_ENABLE_HTMLHANDBOOK)
-      set(_htmlDoc ${CMAKE_CURRENT_SOURCE_DIR}/index.html)
-      add_custom_command(OUTPUT ${_htmlDoc}
-         COMMAND ${KDE4_MEINPROC_EXECUTABLE} --check ${_bootstrapOption} -o ${_htmlDoc} ${_input}
-         DEPENDS ${_input} ${_KDE4_MEINPROC_EXECUTABLE_DEP} ${_ssheet}
-         WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-      )
-      add_custom_target(htmlhandbook DEPENDS ${_htmlDoc})
-   endif(KDE4_ENABLE_HTMLHANDBOOK)
-
-   set(_args ${ARGN})
-
-   set(_installDest)
-   if(_args)
-      list(GET _args 0 _tmp)
-      if("${_tmp}" STREQUAL "INSTALL_DESTINATION")
-         list(GET _args 1 _installDest )
-         list(REMOVE_AT _args 0 1)
-      endif("${_tmp}" STREQUAL "INSTALL_DESTINATION")
-   endif(_args)
-
-   get_filename_component(dirname ${CMAKE_CURRENT_SOURCE_DIR} NAME_WE)
-   if(_args)
-      list(GET _args 0 _tmp)
-      if("${_tmp}" STREQUAL "SUBDIR")
-         list(GET _args 1 dirname )
-         list(REMOVE_AT _args 0 1)
-      endif("${_tmp}" STREQUAL "SUBDIR")
-   endif(_args)
-
-   if(_installDest)
-      file(GLOB _images *.png)
-      install(FILES ${_doc} ${_docs} ${_images} DESTINATION ${_installDest}/${dirname})
-      # TODO symlinks on non-unix platforms
-      if (UNIX)
-         # execute some cmake code on make install which creates the symlink
-         install(CODE "execute_process(COMMAND ${CMAKE_COMMAND} -E create_symlink \"${_installDest}/common\"  \"\$ENV{DESTDIR}${_installDest}/${dirname}/common\" )" )
-      endif (UNIX)
-   endif(_installDest)
-
-endmacro (KDE4_CREATE_HANDBOOK)
-
-
-macro (KDE4_CREATE_MANPAGE _docbook _section)
-   get_filename_component(_input ${_docbook} ABSOLUTE)
-   get_filename_component(_base ${_input} NAME)
-
-   string(REGEX REPLACE "\\.${_section}\\.docbook$" "" _base ${_base})
-
-   set(_doc ${CMAKE_CURRENT_BINARY_DIR}/${_base}.${_section})
-   # sometimes we have "man-" prepended
-   string(REGEX REPLACE "/man-" "/" _outdoc ${_doc})
-
-   #Bootstrap
-   if (_kdeBootStrapping)
-      set(_ssheet "${CMAKE_BINARY_DIR}/kdoctools/customization/kde-include-man.xsl")
-      set(_bootstrapOption "--srcdir=${CMAKE_BINARY_DIR}/kdoctools/")
-   else (_kdeBootStrapping)
-      set(_ssheet "${KDE4_DATA_INSTALL_DIR}/ksgmltools2/customization/kde-include-man.xsl")
-      set(_bootstrapOption)
-   endif (_kdeBootStrapping)
-
-#   if (CMAKE_CROSSCOMPILING)
-#      set(IMPORT_MEINPROC4_EXECUTABLE "${KDE_HOST_TOOLS_PATH}/ImportMeinProc4Executable.cmake" CACHE FILEPATH "Point it to the export file of meinproc4 from a native build")
-#      include(${IMPORT_MEINPROC4_EXECUTABLE})
-#      set(KDE4_MEINPROC_EXECUTABLE meinproc4)
-#   endif (CMAKE_CROSSCOMPILING)
-
-   add_custom_command(OUTPUT ${_outdoc}
-      COMMAND ${KDE4_MEINPROC_EXECUTABLE} --stylesheet ${_ssheet} --check ${_bootstrapOption} ${_input}
-      DEPENDS ${_input} ${_KDE4_MEINPROC_EXECUTABLE_DEP} ${_ssheet}
-   )
-   get_filename_component(_targ ${CMAKE_CURRENT_SOURCE_DIR} NAME)
-   set(_targ "${_targ}-manpage-${_base}")
-   add_custom_target(${_targ} ALL DEPENDS "${_outdoc}")
-
-   set(_args ${ARGN})
-
-   set(_installDest)
-   if(_args)
-      list(GET _args 0 _tmp)
-      if("${_tmp}" STREQUAL "INSTALL_DESTINATION")
-         list(GET _args 1 _installDest )
-         list(REMOVE_AT _args 0 1)
-      endif("${_tmp}" STREQUAL "INSTALL_DESTINATION")
-   endif(_args)
-
-   get_filename_component(dirname ${CMAKE_CURRENT_SOURCE_DIR} NAME_WE)
-   if(_args)
-      list(GET _args 0 _tmp)
-      if("${_tmp}" STREQUAL "SUBDIR")
-         list(GET _args 1 dirname )
-         list(REMOVE_AT _args 0 1)
-      endif("${_tmp}" STREQUAL "SUBDIR")
-   endif(_args)
-
-   if(_installDest)
-      install(FILES ${_outdoc} DESTINATION ${_installDest}/man${_section})
-   endif(_installDest)
-endmacro (KDE4_CREATE_MANPAGE)
-
 
 # This macro doesn't set up the RPATH related options for executables anymore,
 # since now (wioth cmake 2.6) just the full RPATH is used always for everything.
@@ -645,12 +514,6 @@ macro(KDE4_CREATE_EXPORTS_HEADER _outputFile _libName)
    configure_file(${KDE4_MODULE_DIR}/kde4exportsheader.h.in ${_outputFile})
 endmacro(KDE4_CREATE_EXPORTS_HEADER _outputFile _libName)
 
-
-macro (KDE4_CREATE_HTML_HANDBOOK _docbook)
-   message(STATUS "KDE4_CREATE_HTML_HANDBOOK() is deprecated. Enable the option KDE4_ENABLE_HTMLHANDBOOK instead, this will give you targets htmlhandbook for creating the html help.")
-endmacro (KDE4_CREATE_HTML_HANDBOOK)
-
-
 # This macro is only kept around for compatibility, it is not needed/used anymore
 # since CMake 2.6.0. With CMake 2.6.0 it is not necessary anymore link libraries again
 # ("relink") to change their RPATH. Since this is fast now, they are now always built with
@@ -725,10 +588,12 @@ endfunction(KDE4_INSTALL_AUTH_ACTIONS)
 if (_kdeBootStrapping)
   include("${CMAKE_SOURCE_DIR}/kdecore/KDECoreMacros.cmake")
   include("${CMAKE_SOURCE_DIR}/kdeui/KDEUIMacros.cmake")
+  include("${CMAKE_SOURCE_DIR}/kdoctools/KDocToolsMacros.cmake")
   include("${CMAKE_SOURCE_DIR}/kde3support/KDE3SupportMacros.cmake")
 else()
   include("${KDE4_MODULE_DIR}/KDECoreMacros.cmake")
   include("${KDE4_MODULE_DIR}/KDEUIMacros.cmake")
+  include("${KDE4_MODULE_DIR}/KDocToolsMacros.cmake")
   include("${KDE4_MODULE_DIR}/KDE3SupportMacros.cmake")
 endif()
 
---
+++ kdoctools/KDocToolsMacros.cmake
@@ -0,0 +1,142 @@
+# Copyright (c) 2006-2009 Alexander Neundorf, <neundorf@kde.org>
+# Copyright (c) 2006, 2007, Laurent Montel, <montel@kde.org>
+# Copyright (c) 2007 Matthias Kretz <kretz@kde.org>
+#
+# Redistribution and use is allowed according to the terms of the BSD license.
+# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+
+
+macro (KDE4_CREATE_HANDBOOK _docbook)
+   get_filename_component(_input ${_docbook} ABSOLUTE)
+   set(_doc ${CMAKE_CURRENT_BINARY_DIR}/index.cache.bz2)
+
+   #Bootstrap
+   if (_kdeBootStrapping)
+      set(_ssheet "${CMAKE_BINARY_DIR}/kdoctools/customization/kde-chunk.xsl")
+      set(_bootstrapOption "--srcdir=${CMAKE_BINARY_DIR}/kdoctools/")
+   else (_kdeBootStrapping)
+      set(_ssheet "${KDE4_DATA_INSTALL_DIR}/ksgmltools2/customization/kde-chunk.xsl")
+      set(_bootstrapOption)
+   endif (_kdeBootStrapping)
+
+   file(GLOB _docs *.docbook)
+
+#   if (CMAKE_CROSSCOMPILING)
+#      set(IMPORT_MEINPROC4_EXECUTABLE "${KDE_HOST_TOOLS_PATH}/ImportMeinProc4Executable.cmake" CACHE FILEPATH "Point it to the export file of meinproc4 from a native build")
+#      include(${IMPORT_MEINPROC4_EXECUTABLE})
+#      set(KDE4_MEINPROC_EXECUTABLE meinproc4)
+#   endif (CMAKE_CROSSCOMPILING)
+
+   add_custom_command(OUTPUT ${_doc}
+      COMMAND ${KDE4_MEINPROC_EXECUTABLE} --check ${_bootstrapOption} --cache ${_doc} ${_input}
+      DEPENDS ${_docs} ${_KDE4_MEINPROC_EXECUTABLE_DEP} ${_ssheet}
+      WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+   )
+   get_filename_component(_targ ${CMAKE_CURRENT_SOURCE_DIR} NAME)
+   set(_targ "${_targ}-handbook")
+   add_custom_target(${_targ} ALL DEPENDS ${_doc})
+
+   if(KDE4_ENABLE_HTMLHANDBOOK)
+      set(_htmlDoc ${CMAKE_CURRENT_SOURCE_DIR}/index.html)
+      add_custom_command(OUTPUT ${_htmlDoc}
+         COMMAND ${KDE4_MEINPROC_EXECUTABLE} --check ${_bootstrapOption} -o ${_htmlDoc} ${_input}
+         DEPENDS ${_input} ${_KDE4_MEINPROC_EXECUTABLE_DEP} ${_ssheet}
+         WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+      )
+      add_custom_target(htmlhandbook DEPENDS ${_htmlDoc})
+   endif(KDE4_ENABLE_HTMLHANDBOOK)
+
+   set(_args ${ARGN})
+
+   set(_installDest)
+   if(_args)
+      list(GET _args 0 _tmp)
+      if("${_tmp}" STREQUAL "INSTALL_DESTINATION")
+         list(GET _args 1 _installDest )
+         list(REMOVE_AT _args 0 1)
+      endif("${_tmp}" STREQUAL "INSTALL_DESTINATION")
+   endif(_args)
+
+   get_filename_component(dirname ${CMAKE_CURRENT_SOURCE_DIR} NAME_WE)
+   if(_args)
+      list(GET _args 0 _tmp)
+      if("${_tmp}" STREQUAL "SUBDIR")
+         list(GET _args 1 dirname )
+         list(REMOVE_AT _args 0 1)
+      endif("${_tmp}" STREQUAL "SUBDIR")
+   endif(_args)
+
+   if(_installDest)
+      file(GLOB _images *.png)
+      install(FILES ${_doc} ${_docs} ${_images} DESTINATION ${_installDest}/${dirname})
+      # TODO symlinks on non-unix platforms
+      if (UNIX)
+         # execute some cmake code on make install which creates the symlink
+         install(CODE "execute_process(COMMAND ${CMAKE_COMMAND} -E create_symlink \"${_installDest}/common\"  \"\$ENV{DESTDIR}${_installDest}/${dirname}/common\" )" )
+      endif (UNIX)
+   endif(_installDest)
+
+endmacro (KDE4_CREATE_HANDBOOK)
+
+
+macro (KDE4_CREATE_MANPAGE _docbook _section)
+   get_filename_component(_input ${_docbook} ABSOLUTE)
+   get_filename_component(_base ${_input} NAME)
+
+   string(REGEX REPLACE "\\.${_section}\\.docbook$" "" _base ${_base})
+
+   set(_doc ${CMAKE_CURRENT_BINARY_DIR}/${_base}.${_section})
+   # sometimes we have "man-" prepended
+   string(REGEX REPLACE "/man-" "/" _outdoc ${_doc})
+
+   #Bootstrap
+   if (_kdeBootStrapping)
+      set(_ssheet "${CMAKE_BINARY_DIR}/kdoctools/customization/kde-include-man.xsl")
+      set(_bootstrapOption "--srcdir=${CMAKE_BINARY_DIR}/kdoctools/")
+   else (_kdeBootStrapping)
+      set(_ssheet "${KDE4_DATA_INSTALL_DIR}/ksgmltools2/customization/kde-include-man.xsl")
+      set(_bootstrapOption)
+   endif (_kdeBootStrapping)
+
+#   if (CMAKE_CROSSCOMPILING)
+#      set(IMPORT_MEINPROC4_EXECUTABLE "${KDE_HOST_TOOLS_PATH}/ImportMeinProc4Executable.cmake" CACHE FILEPATH "Point it to the export file of meinproc4 from a native build")
+#      include(${IMPORT_MEINPROC4_EXECUTABLE})
+#      set(KDE4_MEINPROC_EXECUTABLE meinproc4)
+#   endif (CMAKE_CROSSCOMPILING)
+
+   add_custom_command(OUTPUT ${_outdoc}
+      COMMAND ${KDE4_MEINPROC_EXECUTABLE} --stylesheet ${_ssheet} --check ${_bootstrapOption} ${_input}
+      DEPENDS ${_input} ${_KDE4_MEINPROC_EXECUTABLE_DEP} ${_ssheet}
+   )
+   get_filename_component(_targ ${CMAKE_CURRENT_SOURCE_DIR} NAME)
+   set(_targ "${_targ}-manpage-${_base}")
+   add_custom_target(${_targ} ALL DEPENDS "${_outdoc}")
+
+   set(_args ${ARGN})
+
+   set(_installDest)
+   if(_args)
+      list(GET _args 0 _tmp)
+      if("${_tmp}" STREQUAL "INSTALL_DESTINATION")
+         list(GET _args 1 _installDest )
+         list(REMOVE_AT _args 0 1)
+      endif("${_tmp}" STREQUAL "INSTALL_DESTINATION")
+   endif(_args)
+
+   get_filename_component(dirname ${CMAKE_CURRENT_SOURCE_DIR} NAME_WE)
+   if(_args)
+      list(GET _args 0 _tmp)
+      if("${_tmp}" STREQUAL "SUBDIR")
+         list(GET _args 1 dirname )
+         list(REMOVE_AT _args 0 1)
+      endif("${_tmp}" STREQUAL "SUBDIR")
+   endif(_args)
+
+   if(_installDest)
+      install(FILES ${_outdoc} DESTINATION ${_installDest}/man${_section})
+   endif(_installDest)
+endmacro (KDE4_CREATE_MANPAGE)
+
+macro (KDE4_CREATE_HTML_HANDBOOK _docbook)
+   message(STATUS "KDE4_CREATE_HTML_HANDBOOK() is deprecated. Enable the option KDE4_ENABLE_HTMLHANDBOOK instead, this will give you targets htmlhandbook for creating the html help.")
+endmacro (KDE4_CREATE_HTML_HANDBOOK)
