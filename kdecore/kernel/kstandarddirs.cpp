e0f27012b06d5bd19ff0eba309571b23ef56072a
David Faure <faure@kde.org>
2012-02-14 13:17:21 +0100

    Merge unix and windows code paths for installPath(),
    now that we use relative paths on both platforms.

--- kdecore/kernel/kstandarddirs.cpp
+++ kdecore/kernel/kstandarddirs.cpp
@@ -240,6 +240,97 @@ static const int types_indices[] = {
 static void tokenize(QStringList& token, const QString& str,
                      const QString& delim);
 
+#ifdef Q_OS_WIN
+QString getKde4Prefix();
+#endif
+
+QString KStandardDirs::installPath(const char *type)
+{
+    Q_ASSERT(type != NULL);
+
+    switch (type[0]) {
+        case 'a':
+            if (strcmp("apps", type) == 0)
+                return QFile::decodeName(APPLNK_INSTALL_DIR "/");
+            break;
+        case 'c':
+            if (strcmp("config", type) == 0)
+                return QFile::decodeName(CONFIG_INSTALL_DIR "/");
+            break;
+        case 'k':
+            if (strcmp("kcfg", type) == 0)
+                return QFile::decodeName(KCFG_INSTALL_DIR "/");
+            if (strcmp("kdedir", type) == 0)
+#ifdef Q_OS_WIN
+                return getKde4Prefix();
+#else
+                return QFile::decodeName(KDEDIR "/");
+#endif
+            break;
+        case 'd':
+            if (strcmp("data", type) == 0)
+                return QFile::decodeName(DATA_INSTALL_DIR "/");
+            break;
+        case 'e':
+            if (strcmp("exe", type) == 0)
+                return QFile::decodeName(BIN_INSTALL_DIR "/");
+            break;
+        case 'h':
+            if (strcmp("html", type) == 0)
+                return QFile::decodeName(HTML_INSTALL_DIR "/");
+            break;
+        case 'i':
+            if (strcmp("icon", type) == 0)
+                return QFile::decodeName(ICON_INSTALL_DIR "/");
+            if (strcmp("include", type) == 0)
+                return QFile::decodeName(INCLUDE_INSTALL_DIR "/");
+            break;
+        case 'l':
+            if (strcmp("lib", type) == 0)
+                return QFile::decodeName(LIB_INSTALL_DIR "/");
+            if (strcmp("libexec", type) == 0)
+                return QFile::decodeName(KDEDIR "/lib" KDELIBSUFF "/kde4/libexec/");
+            if (strcmp("locale", type) == 0)
+                return QFile::decodeName(LOCALE_INSTALL_DIR "/");
+            break;
+        case 'm':
+            if (strcmp("module", type) == 0)
+                return QFile::decodeName(PLUGIN_INSTALL_DIR "/");
+            if (strcmp("mime", type) == 0)
+                return QFile::decodeName(MIME_INSTALL_DIR "/");
+            break;
+        case 'q':
+            if (strcmp("qtplugins", type) == 0)
+                return QFile::decodeName(PLUGIN_INSTALL_DIR "/plugins/");
+            break;
+        case 's':
+            if (strcmp("services", type) == 0)
+                return QFile::decodeName(SERVICES_INSTALL_DIR "/");
+            if (strcmp("servicetypes", type) == 0)
+                return QFile::decodeName(SERVICETYPES_INSTALL_DIR "/");
+            if (strcmp("sound", type) == 0)
+                return QFile::decodeName(SOUND_INSTALL_DIR "/");
+            break;
+        case 't':
+            if (strcmp("templates", type) == 0)
+                return QFile::decodeName(TEMPLATES_INSTALL_DIR "/");
+            break;
+        case 'w':
+            if (strcmp("wallpaper", type) == 0)
+                return QFile::decodeName(WALLPAPER_INSTALL_DIR "/");
+            break;
+        case 'x':
+            if (strcmp("xdgconf-menu", type) == 0)
+                return QFile::decodeName(SYSCONF_INSTALL_DIR "/xdg/menus/");
+            if (strcmp("xdgdata-apps", type) == 0)
+                return QFile::decodeName(XDG_APPS_INSTALL_DIR "/");
+            if (strcmp("xdgdata-dirs", type) == 0)
+                return QFile::decodeName(XDG_DIRECTORY_INSTALL_DIR "/");
+            break;
+    }
+    return QString();
+}
+
 KStandardDirs::KStandardDirs()
     : d(new KStandardDirsPrivate(this))
 {
