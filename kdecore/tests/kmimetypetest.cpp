1231722d97e03a299883c22758a581ca765a5a88
David Faure <faure@kde.org>
2012-02-16 11:20:23 +0100

    Adapt to recent change in KMail2.desktop
    (making it work with the older version too)

--- kdecore/tests/kmimetypetest.cpp
+++ kdecore/tests/kmimetypetest.cpp
@@ -916,7 +916,8 @@ void KMimeTypeTest::testHelperProtocols()
     QVERIFY(KProtocolInfo::isKnownProtocol("mailto"));
     QVERIFY(KProtocolInfo::isHelperProtocol("mailto"));
     QVERIFY(KProtocolInfo::isHelperProtocol(KUrl("mailto:faure@kde.org")));
-    QCOMPARE(KProtocolInfo::exec("mailto"), QString::fromLatin1("kmail -caption \"%c\"")); // comes from KMail2.desktop
+    QVERIFY2(KProtocolInfo::exec("mailto").contains(QLatin1String("kmail -caption \"%c\"")), // comes from KMail2.desktop
+                qPrintable(KProtocolInfo::exec("mailto")));
     QVERIFY(!KProtocolInfo::isHelperProtocol("http"));
     QVERIFY(!KProtocolInfo::isHelperProtocol("ftp"));
     QVERIFY(!KProtocolInfo::isHelperProtocol("file"));
