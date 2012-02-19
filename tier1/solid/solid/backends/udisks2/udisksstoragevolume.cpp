aef03f63de422990ffe14f72b9994c8aba3141b6
Lukas Tinkl <lukas@kde.org>
2012-02-19 17:00:00 +0100

    udisks2 Solid backend
    preliminary version, WIP, just compiles now
    
    I haven't done any testing, not even enabled for building

---
+++ tier1/solid/solid/backends/udisks2/udisksstoragevolume.cpp
@@ -0,0 +1,102 @@
+/*
+    Copyright 2010 Michael Zanetti <mzanetti@kde.org>
+    Copyright 2010-2012 Lukáš Tinkl <ltinkl@redhat.com>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) version 3, or any
+    later version accepted by the membership of KDE e.V. (or its
+    successor approved by the membership of KDE e.V.), which shall
+    act as a proxy defined in Section 6 of version 3 of the license.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "udisksstoragevolume.h"
+
+using namespace Solid::Backends::UDisks2;
+
+StorageVolume::StorageVolume(Device *device)
+    : Block(device)
+{
+}
+
+StorageVolume::~StorageVolume()
+{
+}
+
+QString StorageVolume::encryptedContainerUdi() const
+{
+    const QString path = m_device->prop("CryptoBackingDevice").value<QDBusObjectPath>().path();
+    if ( path.isEmpty() || path == "/")
+        return QString();
+    else
+        return path;
+}
+
+qulonglong StorageVolume::size() const
+{
+    return m_device->prop("Size").toULongLong();
+}
+
+QString StorageVolume::uuid() const
+{
+    return QString::fromUtf8(m_device->prop("IdUUID").toByteArray());
+}
+
+QString StorageVolume::label() const
+{
+    QString label = m_device->prop("HintName").toString();
+    if (label.isEmpty())
+        label = QString::fromUtf8(m_device->prop("IdLabel").toByteArray());
+    if (label.isEmpty())
+        label = QString::fromUtf8(m_device->prop("Name").toByteArray());
+    return label;
+}
+
+QString StorageVolume::fsType() const
+{
+    return m_device->prop("IdType").toString();
+}
+
+Solid::StorageVolume::UsageType StorageVolume::usage() const
+{
+    const QString usage = m_device->prop("IdUsage").toString();
+
+    if (usage == "filesystem")
+    {
+        return Solid::StorageVolume::FileSystem;
+    }
+    else if (usage == "partitiontable")  // FIXME udisks2
+    {
+        return Solid::StorageVolume::PartitionTable;
+    }
+    else if (usage == "raid")
+    {
+        return Solid::StorageVolume::Raid;
+    }
+    else if (usage == "crypto")
+    {
+        return Solid::StorageVolume::Encrypted;
+    }
+    else if (usage == "unused")         // FIXME udisks2
+    {
+        return Solid::StorageVolume::Unused;
+    }
+    else
+    {
+        return Solid::StorageVolume::Other;
+    }
+}
+
+bool StorageVolume::isIgnored() const
+{
+    return m_device->prop("HintIgnore").toBool(); // FIXME tune
+}
