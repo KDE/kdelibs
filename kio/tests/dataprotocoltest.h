6929fc4f3c55c6da01b5674475a79aac225c58e6
Rolf Eike Beer <kde@opensource.sf-tec.de>
2012-04-16 19:19:05 +0200

    test: use QTest in dataprotocol test

---
+++ kio/tests/dataprotocoltest.h
@@ -0,0 +1,21 @@
+// testing the data kioslave
+// (C) 2002, 2003 Leo Savernik
+// (C) 2012 Rolf Eike Beer <kde@opensource.sf-tec.de>
+
+#ifndef DATAPROTOCOLTEST_H
+#define DATAPROTOCOLTEST_H
+
+#include <kio/global.h>
+#include <qtest_kde.h>
+
+#include <QtCore/QObject>
+#include <QtCore/QString>
+
+class DataProtocolTest : public QObject {
+  Q_OBJECT
+private Q_SLOTS:
+  void runAllTests();
+  void runAllTests_data();
+};
+
+#endif /* DATAPROTOCOLTEST_H */
