c49aa50299bf7221d7477b198bffb209db84ee94
David Faure <faure@kde.org>
2012-05-14 01:02:07 +0200

    Increase version number in runtime services, to avoid mixups.
    Executable names:
    kdeinit4 -> kdeinit5
    kded4 -> kded
    
    DBus service names:
    org.kde.kded -> org.kde.kded5
    org.kde.klauncher -> org.kde.klauncher5
    
    This fixes jobtest with Qt5 (difference in QDateTime serialization), but
    more generally it will allow BIC changes in SlaveBase, as well as
    not crashing due to kdeinit4 or kded4 loading KF5/Qt5 modules, etc.
    
    (commit extracted from 2b3f291194cadc2f456d326c53d7d1e98e08ef86)

--- solid/solid/backends/udisks2/udisksstorageaccess.cpp
+++ solid/solid/backends/udisks2/udisksstorageaccess.cpp
@@ -304,7 +304,7 @@ bool StorageAccess::requestPassphrase()
 
     QString appId = QCoreApplication::applicationName();
 
-    QDBusInterface soliduiserver("org.kde.kded", "/modules/soliduiserver", "org.kde.SolidUiServer");
+    QDBusInterface soliduiserver("org.kde.kded5", "/modules/soliduiserver", "org.kde.SolidUiServer");
     QDBusReply<void> reply = soliduiserver.call("showPassphraseDialog", udi, returnService,
                                                 m_lastReturnObject, wId, appId);
     m_passphraseRequested = reply.isValid();
