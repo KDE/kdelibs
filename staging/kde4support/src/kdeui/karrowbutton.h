071ce1841ca8f0c49bd1628243ff6569f0f96078
Stephen Kelly <steveire@gmail.com>
2012-05-16 02:10:21 +0200

    KArrowButton is deprecated. Move to kde4support.

--- staging/kde4support/src/kdeui/karrowbutton.h
+++ staging/kde4support/src/kdeui/karrowbutton.h
@@ -19,7 +19,7 @@
 #ifndef karrowbutton_h
 #define karrowbutton_h
 
-#include <kdeui_export.h>
+#include <kde4support_export.h>
 #include <QPushButton>
 
 class KArrowButtonPrivate;
@@ -38,7 +38,7 @@ class KArrowButtonPrivate;
  *
  * @author Frerich Raabe
  */
-class KDEUI_DEPRECATED_EXPORT KArrowButton : public QPushButton
+class KDE4SUPPORT_DEPRECATED_EXPORT KArrowButton : public QPushButton
 {
 	Q_OBJECT
 	/**
