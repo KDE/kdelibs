2c56ed9cd2a806acf1af70735f37ffb7f9f1fff7
Stephen Kelly <steveire@gmail.com>
2012-05-18 00:38:38 +0200

    Fix build of test.

--- tier1/kwidgetsaddons/tests/khboxtest.cpp
+++ tier1/kwidgetsaddons/tests/khboxtest.cpp
@@ -1,13 +1,12 @@
 #include "khboxtest.h"
 #include <QPushButton>
-#include <kaboutdata.h>
-#include <kcmdlineargs.h>
-#include <kapplication.h>
+#include <QApplication>
+
 
 KHBoxTest::KHBoxTest( QWidget* parentWidget )
     : KHBox( parentWidget )
 {
-    pbAdd = new QPushButton( "Add a button", this );
+    pbAdd = new QPushButton( QLatin1String("Add a button"), this );
     connect( pbAdd, SIGNAL(clicked()), this, SLOT(slotAdd()) );
     pbRemove = 0;
 }
@@ -15,7 +14,7 @@ KHBoxTest::KHBoxTest( QWidget* parentWidget )
 void KHBoxTest::slotAdd()
 {
     if ( !pbRemove ) {
-        pbRemove = new QPushButton( "Remove me", this );
+        pbRemove = new QPushButton( QLatin1String("Remove me"), this );
         connect( pbRemove, SIGNAL(clicked()), this, SLOT(slotRemove()) );
         pbAdd->setEnabled( false );
     }
@@ -30,10 +29,7 @@ void KHBoxTest::slotRemove()
 
 int main( int argc, char ** argv )
 {
-    KAboutData about("KHBoxTest", 0, qi18n("KHBoxTest"), "version");
-    KCmdLineArgs::init(argc, argv, &about);
-
-    KApplication app;
+    QApplication app(argc, argv);
 
     KHBoxTest *toplevel = new KHBoxTest(0);
     toplevel->show();
