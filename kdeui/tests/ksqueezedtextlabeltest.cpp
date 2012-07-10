56fead3268cb5e06da8ac7f0b6cc768164cbd451
Dominik Haumann <dhaumann@kde.org>
2012-07-10 22:57:39 +0200

    port away from KVBox

--- kdeui/tests/ksqueezedtextlabeltest.cpp
+++ kdeui/tests/ksqueezedtextlabeltest.cpp
@@ -1,24 +1,33 @@
 #include "ksqueezedtextlabel.h"
 #include <kapplication.h>
 #include <kcmdlineargs.h>
-#include <kvbox.h>
+#include <QWidget>
+#include <QVBoxLayout>
 
 int main( int argc, char **argv )
 {
   KCmdLineArgs::init(argc, argv, "test", 0, qi18n("Test"), "1.0", qi18n("test app"));
   KApplication app;
 
-  KVBox* box = new KVBox();
-  KSqueezedTextLabel *l = new KSqueezedTextLabel( "This is a rather long string", box);
-  l = new KSqueezedTextLabel( "This is another long string, selectable by mouse", box );
-  l->setTextElideMode( Qt::ElideRight );
-  l->setTextInteractionFlags(Qt::TextSelectableByMouse);
+  QWidget* box = new QWidget();
+  QVBoxLayout* layout = new QVBoxLayout(box);
+
+  KSqueezedTextLabel *l1 = new KSqueezedTextLabel( "This is a rather long string", box);
+  KSqueezedTextLabel *l2 = new KSqueezedTextLabel( "This is another long string, selectable by mouse", box );
+  l2->setTextElideMode( Qt::ElideRight );
+  l2->setTextInteractionFlags(Qt::TextSelectableByMouse);
   KSqueezedTextLabel* urlLabel = new KSqueezedTextLabel("http://www.example.com/this/url/is/selectable/by/mouse", box);
   urlLabel->setTextInteractionFlags(Qt::TextSelectableByMouse);
 
-  new QLabel("This is a normal QLabel", box);
+  layout->addWidget(l1);
+  layout->addWidget(l2);
+  layout->addWidget(urlLabel);
+  layout->addWidget(new QLabel("This is a normal QLabel", box));
+
   QLabel* selectableLabel = new QLabel("This is a normal QLabel, selectable by mouse", box);
   selectableLabel->setTextInteractionFlags(Qt::TextSelectableByMouse);
+
+  layout->addWidget(selectableLabel);
 
   box->show();
 
