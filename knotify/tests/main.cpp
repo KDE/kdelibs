61d00a1683e445eb4c436e763a231c4d6fb202de
Jeremy Whiting <jpwhiting@kde.org>
2012-08-13 11:28:56 -0600

    frameworks: port knotify/tests/main.cpp from KApplication to QApplication

--- knotify/tests/main.cpp
+++ knotify/tests/main.cpp
@@ -1,25 +1,22 @@
 /*
  * Copyright (C) 2005-2006 by Olivier Goffart <ogoffart at kde.org>
  */
-#include <kapplication.h>
+#include <QApplication>
 #include <klocalizedstring.h>
 #include <kaboutdata.h>
-#include <kcmdlineargs.h>
 
 #include "knotifytestwindow.h"
 
 int main( int argc, char ** argv )
 {
-  KCmdLineOptions options;
   KAboutData aboutData( "knotifytest", 0, qi18n("KNotifyTest"), "0.1",
 			qi18n( "A test program for KDE Notifications" ),
 			KAboutData::License_GPL,
 			qi18n("(c) 2006, Olivier Goffart"));
   aboutData.addAuthor(qi18n("Olivier Goffart"), QLocalizedString(), "ogoffart @ kde.org");
-  KCmdLineArgs::init( argc, argv, &aboutData );
-  KCmdLineArgs::addCmdLineOptions( options );
+  QApplication::setApplicationName(aboutData.appName());
 
-  KApplication a;
+  QApplication a(argc, argv);
   KNotifyTestWindow* knotifytestwindow = new KNotifyTestWindow;
   knotifytestwindow->show();
 
