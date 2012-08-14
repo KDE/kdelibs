e76b8d9777b0cb6d960c65cd2774eb8a52b47f02
Stephen Kelly <steveire@gmail.com>
2012-08-14 10:24:36 +0200

    Require CMake 2.8.9.

--- kjsembed/qtonly/FindQJSInternal.cmake
+++ kjsembed/qtonly/FindQJSInternal.cmake
@@ -19,7 +19,7 @@
 
 INCLUDE (MacroEnsureVersion)
 
-cmake_minimum_required(VERSION 2.8.8 FATAL_ERROR)
+cmake_minimum_required(VERSION 2.8.9 FATAL_ERROR)
 
 
 option(KDE4_BUILD_TESTS  "Build the tests")
