4f38fa85c3e1b4a6a71b0c239ca6cd0f1e51f8c7
David Faure <faure@kde.org>
2012-08-18 22:34:10 +0200

    prepend our libexec to the path, don't append it.
    Starting /opt/kde3/bin/klauncher from kdeinit5 isn't so interesting :-)

--- kinit/kinit.cpp
+++ kinit/kinit.cpp
@@ -411,7 +411,7 @@ QByteArray execpath_avoid_loops( const QByteArray& exec, int envc, const char* e
              paths = QFile::decodeName(path).split(pathSepRegExp);
      } else {
          paths = QString::fromLocal8Bit(qgetenv("PATH")).split(pathSepRegExp, QString::KeepEmptyParts);
-         paths.append(QFile::decodeName(CMAKE_INSTALL_PREFIX "/" LIBEXEC_INSTALL_DIR));
+         paths.prepend(QFile::decodeName(CMAKE_INSTALL_PREFIX "/" LIBEXEC_INSTALL_DIR));
      }
      QString execpath = QStandardPaths::findExecutable(QFile::decodeName(exec), paths);
      if (avoid_loops && !execpath.isEmpty()) {
