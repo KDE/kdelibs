a5d78c4d24e79c2a6c4727c167d0d1263f45b53c
Nicolas Lécureuil <neoclust.kde@free.fr>
2012-08-21 02:35:56 +0200

    Convert TIME_WITH_SYS_TIME to cmakedefine01

--- kioslave/ftp/ConfigureChecks.cmake
+++ kioslave/ftp/ConfigureChecks.cmake
@@ -4,5 +4,6 @@ include(CheckSymbolExists)
 include(CheckCXXSymbolExists)
 
 check_include_files(sys/time.h    HAVE_SYS_TIME_H)
+check_include_files(sys/time.h    TIME_WITH_SYS_TIME)
 
 check_symbol_exists(strtoll         "stdlib.h"                 HAVE_STRTOLL)
