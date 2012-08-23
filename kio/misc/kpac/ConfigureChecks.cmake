a13bc2325c42b648fa5bba055ac1e2fa8f114b7a
Nicolas Lécureuil <neoclust.kde@free.fr>
2012-08-23 12:20:24 +0200

    Fix use of ConfigureChecks.cmake and make link

--- kio/misc/kpac/ConfigureChecks.cmake
+++ kio/misc/kpac/ConfigureChecks.cmake
@@ -2,6 +2,8 @@ include(CheckIncludeFiles)
 include(CheckLibraryExists)
 include(CheckPrototypeExists)
 
+check_include_files(sys/param.h   HAVE_SYS_PARAM_H)
+check_include_files(sys/types.h   HAVE_SYS_TYPES_H)
 check_include_files(arpa/nameser_compat.h HAVE_ARPA_NAMESER_COMPAT_H)
 check_include_files(arpa/nameser8_compat.h HAVE_ARPA_NAMESER8_COMPAT_H)
 check_include_files("sys/types.h;netinet/in.h"  HAVE_NETINET_IN_H) 
@@ -9,7 +11,7 @@ check_prototype_exists(res_init "sys/types.h;netinet/in.h;arpa/nameser.h;resolv.
 
 # Check for libresolv
 # e.g. on slackware 9.1 res_init() is only a define for __res_init, so we check both, Alex
-set(HAVE_RESOLV_LIBRARY FALSE)                                        # kdecore, kdecore/network, kpac
+set(HAVE_RESOLV_LIBRARY FALSE)                                        
 check_library_exists(resolv res_init "" HAVE_RES_INIT_IN_RESOLV_LIBRARY)
 check_library_exists(resolv __res_init "" HAVE___RES_INIT_IN_RESOLV_LIBRARY)
 if (HAVE___RES_INIT_IN_RESOLV_LIBRARY OR HAVE_RES_INIT_IN_RESOLV_LIBRARY)
