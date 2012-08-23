d4e720b86259c2d8737b9064aca651b3fecb266f
Nicolas Lécureuil <neoclust.kde@free.fr>
2012-08-23 04:13:38 +0200

    Add new files

---
+++ kde3support/config-kde3support.h.cmake
@@ -0,0 +1,7 @@
+#define KDELIBSUFF "${KDELIBSUFF}"
+
+#cmakedefine01 HAVE_STRINGS_H
+#cmakedefine01 HAVE_SYS_SELECT_H
+#cmakedefine01 HAVE_INITGROUPS
+#cmakedefine01 HAVE_PATHS_H
+#cmakedefine01 HAVE_SYS_STAT_H
