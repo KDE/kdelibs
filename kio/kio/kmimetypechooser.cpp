fa151df3e3ae39290d348917e64244d287ffe8ad
David Faure <faure@kde.org>
2012-08-25 11:43:06 +0200

    Improve usability of KMimeTypeChooser (column widths, dialog size)

--- kio/kio/kmimetypechooser.cpp
+++ kio/kio/kmimetypechooser.cpp
@@ -80,19 +80,17 @@ KMimeTypeChooser::KMimeTypeChooser( const QString &text,
   d->mimeTypeTree = new QTreeWidget( this );
   QStringList headerLabels;
   headerLabels.append( i18n("Mime Type") );
-//   d->mimeTypeTree->setColumnWidthMode( 0, QListView::Manual );
 
   if ( visuals & Comments ) {
       headerLabels.append( i18n("Comment") );
-      //d->mimeTypeTree->setColumnWidthMode( 1, Q3ListView::Manual );
   }
   if ( visuals & Patterns ) {
       headerLabels.append( i18n("Patterns") );
   }
 
-//  d->mimeTypeTree->setRootIsDecorated( true );
   d->mimeTypeTree->setColumnCount(headerLabels.count());
   d->mimeTypeTree->setHeaderLabels(headerLabels);
+  d->mimeTypeTree->setColumnWidth(0, 200); // big enough for most names, but not for the insanely long ones
 
   d->loadMimeTypes( selMimeTypes );
 
@@ -192,6 +190,7 @@ void KMimeTypeChooserPrivate::loadMimeTypes( const QStringList &_selectedMimeTyp
     idefault->setExpanded( true );
     mimeTypeTree->scrollToItem( idefault );
   }
+  mimeTypeTree->resizeColumnToContents(1);
 }
 
 void KMimeTypeChooserPrivate::_k_editMimeType()
@@ -338,7 +337,7 @@ void KMimeTypeChooserDialog::Private::init()
   q->setDefaultButton( Ok );
 
   KConfigGroup group( KGlobal::config(), "KMimeTypeChooserDialog");
-  q->resize( group.readEntry("size", QSize(500,400)));
+  q->resize( group.readEntry("size", QSize(600,500)));
 }
 
 KMimeTypeChooserDialog::~KMimeTypeChooserDialog()
