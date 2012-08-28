466d93189cb55968a2509ceef34fc10e002f77b1
Nicolas Lécureuil <neoclust.kde@free.fr>
2012-08-29 01:21:19 +0200

    Add missing setter in kjs/ConfigureChecks.cmake that prevented HAVE_FUNC_ISNAN to be set OK

--- kjs/ConfigureChecks.cmake
+++ kjs/ConfigureChecks.cmake
@@ -8,6 +8,10 @@ include(CheckCXXSymbolExists)
 include(CheckFunctionExists)
 include(CheckStructMember)
 
+# The FindKDE4.cmake module sets _KDE4_PLATFORM_DEFINITIONS with
+# definitions like _GNU_SOURCE that are needed on each platform.
+set(CMAKE_REQUIRED_DEFINITIONS ${_KDE4_PLATFORM_DEFINITIONS})
+
 check_library_exists(pthread pthread_attr_get_np "" HAVE_PTHREAD_ATTR_GET_NP)
 check_library_exists(pthread pthread_getattr_np "" HAVE_PTHREAD_GETATTR_NP)
 
@@ -24,11 +28,9 @@ check_include_files("strings.h"     HAVE_STRINGS_H)
 check_include_files("crtdbg.h"      HAVE_CRTDBG_H)
 check_struct_member(tm tm_gmtoff time.h HAVE_TM_GMTOFF)
 
-#macro_push_required_vars()
 check_function_exists(_finite    HAVE_FUNC__FINITE)
 check_function_exists(finite     HAVE_FUNC_FINITE)
 check_function_exists(posix_memalign     HAVE_FUNC_POSIX_MEMALIGN)
 check_function_exists(gettimeofday    HAVE_GETTIMEOFDAY)
 check_symbol_exists(isnan   "math.h" HAVE_FUNC_ISNAN)
 check_symbol_exists(isinf   "math.h" HAVE_FUNC_ISINF)
-#macro_pop_required_vars()
