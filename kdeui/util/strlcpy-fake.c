3398105907d7dda210934a4f185ecfe20492c9b0
Nicolas Lécureuil <neoclust.kde@free.fr>
2012-09-06 20:26:48 +0200

    Fix author mail, and add proper licence

--- kdeui/util/strlcpy-fake.c
+++ kdeui/util/strlcpy-fake.c
@@ -1,8 +1,27 @@
 /* This file is part of the KDE libraries
-   Copyright (c) Todd.Miller@cs.colorado.edu
+   Copyright (c) Todd C. Miller <Todd.Miller@courtesan.com>
    Copyright (c) 2002 Dirk Mueller <mueller@kde.org>
-   The source code for strlcpy() and strlcat() is available free of charge and 
-   under a BSD-style license as part of the OpenBSD operating system.
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions
+   are met:
+
+   1. Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+   2. Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+
+   THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+   IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+   OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+   IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+   INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+   NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+   THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
 #include <config-strlcpy.h>
