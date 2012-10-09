18f8ac8469272770367a02e89220f14086fea7c3
Albert Astals Cid <aacid@kde.org>
2012-10-09 18:40:42 +0200

    Enable test building by default

--- cmake/modules/FindKDE4Internal.cmake
+++ cmake/modules/FindKDE4Internal.cmake
@@ -716,7 +716,7 @@ endif(NOT PHONON_FOUND)
 #####################  provide some options   ##########################################
 
 option(KDE4_ENABLE_FINAL "Enable final all-in-one compilation")
-option(KDE4_BUILD_TESTS  "Build the tests")
+option(KDE4_BUILD_TESTS  "Build the tests" ON)
 option(KDE4_ENABLE_HTMLHANDBOOK  "Create targets htmlhandbook for creating the html versions of the docbook docs")
 set(KDE4_SERIALIZE_TOOL "" CACHE STRING "Tool to serialize resource-intensive commands in parallel builds")
 
