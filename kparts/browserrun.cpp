23ebe75385d43da21560172c675395a5949c1f37
Montel Laurent <montel@kde.org>
2012-10-19 13:25:28 +0200

    Revert "Fix Bug 306814 - Open with dialog appears when clicking open with"
    This reverts commit 6df4713cbf33fce9ff8c47c1afb424165f321577.

--- kparts/browserrun.cpp
+++ kparts/browserrun.cpp
@@ -305,18 +305,8 @@ BrowserRun::NonEmbeddableResult BrowserRun::handleNonEmbeddable(const QString& _
                          this, SLOT(slotCopyToTempFileResult(KJob*)) );
                 return Delayed; // We'll continue after the job has finished
             }
-            if (selectedService) {
+            if (selectedService)
                 *selectedService = question.selectedService();
-                if (selectedService) {
-                    KUrl::List lst;
-                    lst.append(KRun::url());
-
-                    if (*selectedService && KRun::run( *(*selectedService), lst, 0 ) ) {
-                        setFinished( true );
-                        return Handled;
-                    }
-                }
-            }
         }
     }
 
@@ -327,6 +317,7 @@ BrowserRun::NonEmbeddableResult BrowserRun::handleNonEmbeddable(const QString& _
         setFinished( true );
         return Handled;
     }
+
     KIO::Scheduler::publishSlaveOnHold(); // publish any slave on hold so it can be reused.
     return NotHandled;
 }
@@ -336,6 +327,7 @@ bool BrowserRun::allowExecution( const QString &mimeType, const KUrl &url )
 {
     if ( !KRun::isExecutable( mimeType ) )
       return true;
+
     if ( !url.isLocalFile() ) // Don't permit to execute remote files
         return false;
 
