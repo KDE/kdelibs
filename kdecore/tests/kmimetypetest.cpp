3f213e111b79e0bde61db82e98a3b9fc48be54dd
David Faure <faure@kde.org>
2012-10-22 11:46:45 +0200

    Don't skip looking for duplicate glob->mimetype associations.
    Skipping doesn't simply create a performance problem as I thought,
    it actually creates a fake "ambiguity" ("more than one mimetype found for
    this glob"), which leads to determination-from-contents.
    
    Testcase:
    kmimetypefinder kdeplasma-addons/applets/nowplaying/package/contents/ui/PositionSlider.qml
    (any QML file that starts with a C-like comment)
    with two lines "50:text/x-qml:*.qml" in /usr/share/mime/globs2.
    
    I'll add a unit test in Qt 5 and fix the bug there as well.
    Then I'll look into fixing the same bug in xdg-mime itself...

--- kdecore/tests/kmimetypetest.cpp
+++ kdecore/tests/kmimetypetest.cpp
@@ -326,7 +326,8 @@ void KMimeTypeTest::testFindByPathWithContent()
     }
 
     // Now the case where extension differs from contents, but contents has >80 magic rule
-    // XDG spec says: contents wins. But we can't sniff all files...
+    // XDG spec used to say: contents wins. But we can't sniff all files...
+    // XDG spec has now been amended, extensions always win.
     {
         KTemporaryFile txtTempFile;
         txtTempFile.setSuffix(".txt");
