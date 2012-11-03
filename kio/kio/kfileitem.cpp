KDE/kdelibs

commit b8f64ca3f4b6311519c21046031d66d9d0a570c6

Consider two KFileItems 'equal' if they share the same URL
Up to now, KFileItem::operator==(const KFileItem&) only returned true if
both items shared the same d pointer. However, the implicitly shared
d pointer is an implementation detail of the class, and making the
comparison depend on it might not be expected by the developer using the
class. Fixes a problem in Dolphin, where the unexpected outcome of the
comparison caused permanent high CPU usage.

CCBUG: 304986

kio/kio/kfileitem.cpp
@@ -1307,13 +1307,12 @@ bool KFileItem::cmp( const KFileItem & item ) const
 
 bool KFileItem::operator==(const KFileItem& other) const
 {
-    // is this enough?
-    return d == other.d;
+    return d == other.d || d->m_url == other.d->m_url;
 }
 
 bool KFileItem::operator!=(const KFileItem& other) const
 {
-    return d != other.d;
+    return !operator==(other);
 }
 
 #ifndef KDE_NO_DEPRECATED

kio/kio/kfileitem.h
@@ -490,16 +490,22 @@ public:
 
     /**
      * Somewhat like a comparison operator, but more explicit,
-     * and it can detect that two kfileitems are equal even when they do
-     * not share the same internal pointer - e.g. when KDirLister compares
-     * fileitems after listing a directory again, to detect changes.
+     * and it can detect that two fileitems differ if any property of the file item
+     * has changed (file size, modification date, etc.). Two items are equal if
+     * all properties are equal. In contrast, operator== only compares URLs.
      * @param item the item to compare
      * @return true if all values are equal
      */
     bool cmp( const KFileItem & item ) const;
 
+    /**
+     * Returns true if both items share the same URL.
+     */
     bool operator==(const KFileItem& other) const;
 
+    /**
+     * Returns true if both items do not share the same URL.
+     */
     bool operator!=(const KFileItem& other) const;
 
 
@@ -646,6 +652,8 @@ private:
 private:
     KIO_EXPORT friend QDataStream & operator<< ( QDataStream & s, const KFileItem & a );
     KIO_EXPORT friend QDataStream & operator>> ( QDataStream & s, KFileItem & a );
+
+    friend class KFileItemTest;
 };
 
 Q_DECLARE_METATYPE(KFileItem)

kio/tests/kfileitemtest.cpp
@@ -105,14 +105,18 @@ void KFileItemTest::testDetach()
     QCOMPARE(fileItem2.extraData(this), (const void*)this);
 #endif
     QVERIFY(fileItem == fileItem2);
+    QVERIFY(fileItem.d == fileItem2.d);
     fileItem2.mark();
     QVERIFY(fileItem2.isMarked());
     QVERIFY(!fileItem.isMarked());
-    QVERIFY(fileItem != fileItem2);
+    QVERIFY(fileItem == fileItem2);
+    QVERIFY(fileItem.d != fileItem2.d);
 
     fileItem = fileItem2;
     QVERIFY(fileItem2.isMarked());
     QVERIFY(fileItem == fileItem2);
+    QVERIFY(fileItem.d == fileItem2.d);
+    QVERIFY(!(fileItem != fileItem2));
 }
 
 void KFileItemTest::testBasic()
@@ -246,7 +250,9 @@ void KFileItemTest::testCmp()
 
     KFileItem fileItem(KFileItem::Unknown, KFileItem::Unknown, KUrl(file.fileName()), true /*on demand*/);
     KFileItem fileItem2(KFileItem::Unknown, KFileItem::Unknown, KUrl(file.fileName()), false);
-    QVERIFY(fileItem != fileItem2); // created independently so not 'equal'
+    QVERIFY(fileItem == fileItem2); // created independently, but still 'equal'
+    QVERIFY(fileItem.d != fileItem2.d);
+    QVERIFY(!(fileItem != fileItem2));
     QVERIFY(fileItem.cmp(fileItem2));
 }
 

commit 535de2bcc3e39e10843cc1374d831e974a610c96

fix regression introduced in b8f64ca3
Commit b8f64ca3 breaks commit 0b894212 in that it dereferences d->url without
actually checking d for validity, thus introducing a potential crash. This fixes
the regression by first comparing the d-pointers similar to KFileItem::cmp(KFileItem&).

See commit 0b894212.

BUG: 309489
FIXED-IN: 4.10

kio/kio/kfileitem.cpp
@@ -1307,7 +1307,13 @@ bool KFileItem::cmp( const KFileItem & item ) const
 
 bool KFileItem::operator==(const KFileItem& other) const
 {
-    return d == other.d || d->m_url == other.d->m_url;
+    if (!d && !other.d)
+        return true;
+
+    if (!d || !other.d)
+        return false;
+
+    return d->m_url == other.d->m_url;
 }
 
 bool KFileItem::operator!=(const KFileItem& other) const