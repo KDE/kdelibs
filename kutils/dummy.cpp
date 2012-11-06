fe8bf466c15acdb73cab008ecea2dc0e3d1ca583
Valentin Rusu <kde@rusu.info>
2012-11-07 00:00:33 +0100

    detach kemoticons from kutils

--- kutils/dummy.cpp
+++ kutils/dummy.cpp
@@ -1,16 +1,13 @@
 #include <kcmoduleproxy.h>
 #include <kprintpreview.h>
-#include <kemoticons/kemoticons.h>
 
 Q_GLOBAL_STATIC_WITH_ARGS( KCModuleProxy, foo, ( (const char *)0 ) )
 Q_GLOBAL_STATIC_WITH_ARGS( KPrintPreview, bar, ( (QPrinter*)0, (QWidget*)0 ) )
-Q_GLOBAL_STATIC( KEmoticons, foobar )
 
 void _k__get_dummy_used()
 {
     foo();
     bar();
-    foobar();
 }
 
 #ifdef Q_OS_WIN
