b9df2b0dd915a87ae2b48ad8a2ad93abdeb985db
Mirko Boehm (AWS) <mirko@agile-workers.com>
2013-02-14 17:24:00 +0100

    Add Threadweaver QueueBenchmarks.

---
+++ tier1/threadweaver/autotests/QueueBenchmarks.cpp
@@ -0,0 +1,155 @@
+#include <numeric>
+
+#include <QString>
+#include <QtTest>
+#include <QCoreApplication>
+#include <QList>
+
+#include <Job.h>
+#include <ThreadWeaver.h>
+
+//FIXME Wouldn't it be nice to be able to execute jobs in the local thread?
+class AccumulateJob : public ThreadWeaver::Job {
+public:
+    explicit AccumulateJob()
+        : m_count(0)
+        , m_result(0)
+    {
+    }
+
+    void setCount(quint64 count) {
+        m_count = count;
+    }
+
+    quint64 result() const {
+        return m_result;
+    }
+
+    void executeLocal() {
+        run();
+    }
+
+protected:
+    void run() {
+        quint64 numbers[m_count];
+        for(quint64 i = 0; i < m_count; ++i) {
+            numbers[i] = i;
+        }
+        m_result = std::accumulate(numbers, numbers + m_count, 0);
+    }
+
+private:
+    quint64 m_count;
+    quint64 m_result;
+};
+
+class QueueBenchmarksTest : public QObject
+{
+    Q_OBJECT
+    
+public:
+    QueueBenchmarksTest();
+    
+private Q_SLOTS:
+    void initTestCase();
+    void cleanupTestCase();
+    void BaselineBenchmark();
+    void BaselineBenchmark_data();
+    void IndividualJobsBenchmark();
+    void IndividualJobsBenchmark_data();
+
+private:
+    void defaultBenchmarkData(bool singleThreaded);
+};
+
+QueueBenchmarksTest::QueueBenchmarksTest()
+{
+}
+
+void QueueBenchmarksTest::initTestCase()
+{
+}
+
+void QueueBenchmarksTest::cleanupTestCase()
+{
+}
+
+/** @brief BaselineBenchmark simply performs the same operations in a loop.
+ *
+ * The result amounts to what time the jobs used in the benchmark need to execute without queueing or thread
+ * synchronization overhead. */
+void QueueBenchmarksTest::BaselineBenchmark()
+{
+    QFETCH(int, m);
+    QFETCH(int, c);
+    QFETCH(int, b);
+    QFETCH(int, t);
+    const int n = c*b;
+    Q_UNUSED(t); // in this case
+
+    AccumulateJob jobs[n];
+    for(int i = 0; i < n; ++i) {
+        jobs[i].setCount(m);
+    }
+
+    QBENCHMARK {
+        for(int i = 0; i < n; ++i) {
+            jobs[i].executeLocal();
+        }
+    }
+}
+
+void QueueBenchmarksTest::BaselineBenchmark_data()
+{
+    defaultBenchmarkData(true);
+}
+
+void QueueBenchmarksTest::IndividualJobsBenchmark()
+{
+    QFETCH(int, m);
+    QFETCH(int, c);
+    QFETCH(int, b);
+    QFETCH(int, t);
+    const int n = c*b;
+
+    ThreadWeaver::Weaver weaver;
+    weaver.setMaximumNumberOfThreads(t);
+    weaver.suspend();
+    AccumulateJob jobs[n];
+    for(int i = 0; i < n; ++i) {
+        jobs[i].setCount(m);
+        weaver.enqueue(&jobs[i]);
+    }
+
+    QBENCHMARK {
+        weaver.resume();
+        weaver.finish();
+    }
+}
+
+void QueueBenchmarksTest::IndividualJobsBenchmark_data()
+{
+    defaultBenchmarkData(false);
+}
+
+void QueueBenchmarksTest::defaultBenchmarkData(bool singleThreaded)
+{
+    QTest::addColumn<int>("m"); // number of quint64's to accumulate
+    QTest::addColumn<int>("c"); // operations per block
+    QTest::addColumn<int>("b"); // number of blocks, number of jobs is b*c
+    QTest::addColumn<int>("t"); // number of worker threads
+
+    const QList<int> threads = singleThreaded ? QList<int>() << 1 : QList<int>() << 1 << 2 << 4 << 8 << 16 << 32 << 64 << 128;
+    const QList<int> ms = QList<int>() << 1 << 10 << 100 << 1000 << 10000;
+    Q_FOREACH(int m, ms) {
+        Q_FOREACH(int t, threads) {
+            const QString name = tr("%1 threads, %2 values").arg(t).arg(m);
+            // newRow expects const char*, but then qstrdup's it in the QTestData constructor. Eeeew.
+            QTest::newRow(qPrintable(name)) << m << 256 << 256 << t;
+        }
+    }
+}
+
+QTEST_MAIN(QueueBenchmarksTest)
+
+#include "QueueBenchmarks.moc"
