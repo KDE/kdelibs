66efa8ea9094d02e4f1eeb09c8bbd58d319d5ab0
Alex Neundorf <neundorf@kde.org>
2013-02-26 20:32:24 +0100

    CamelCase the karchive library
    Alex

--- cmake/modules/FindKF5Transitional.cmake
+++ cmake/modules/FindKF5Transitional.cmake
@@ -8,12 +8,12 @@ find_package(KDE4Internal REQUIRED)
 set(KDE4_KDECORE_LIBRARY ${KDE4_KDECORE_LIBS} ${KDE4_KDE4SUPPORT_LIBS})
 
 
-list(APPEND KDE4_KDEUI_LIBS ${KDE4_KDE4SUPPORT_LIBS}  KDE4__kwidgetsaddons KDE4__ItemModels KDE4__sonnetcore KDE4__kguiaddons KDE4__kwidgets KDE4__KWindowSystem KDE4__karchive)
+list(APPEND KDE4_KDEUI_LIBS ${KDE4_KDE4SUPPORT_LIBS}  KDE4__kwidgetsaddons KDE4__ItemModels KDE4__sonnetcore KDE4__kguiaddons KDE4__kwidgets KDE4__KWindowSystem KDE4__KArchive)
 
 list(APPEND KDE4_KIO_LIBS ${KDE4_KDEUI_LIBS} ${KDE4_KDE4SUPPORT_LIBS} KDE4__kiocore KDE4__kwidgetsaddons)
-list(APPEND KDE4_KFILE_LIBS ${KDE4_KDE4SUPPORT_LIBS} KDE4__kguiaddons KDE4__karchive  KDE4__kwidgetsaddons)
-list(APPEND KDE4_KPARTS_LIBS ${KDE4_KDE4SUPPORT_LIBS} KDE4__kguiaddons KDE4__karchive ${KDE4_KDEUI_LIBS}  KDE4__kwidgetsaddons)
-list(APPEND KDE4_KHTML_LIBS KDE4__kguiaddons KDE4__karchive ${KDE4_KDEUI_LIBS}  KDE4__kwidgetsaddons)
+list(APPEND KDE4_KFILE_LIBS ${KDE4_KDE4SUPPORT_LIBS} KDE4__kguiaddons KDE4__KArchive  KDE4__kwidgetsaddons)
+list(APPEND KDE4_KPARTS_LIBS ${KDE4_KDE4SUPPORT_LIBS} KDE4__kguiaddons KDE4__KArchive ${KDE4_KDEUI_LIBS}  KDE4__kwidgetsaddons)
+list(APPEND KDE4_KHTML_LIBS KDE4__kguiaddons KDE4__KArchive ${KDE4_KDEUI_LIBS}  KDE4__kwidgetsaddons)
 
 set(KDE4_KIO_LIBRARY ${KDE4_KIO_LIBS})
 set(KDE4_KDEUI_LIBRARY ${KDE4_KDEUI_LIBS})
