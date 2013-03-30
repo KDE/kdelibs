11a13a1e6d74f00b1329f2db9a2de3efddfc65d0
Bernd Buschinski <b.buschinski@googlemail.com>
2013-03-30 14:13:08 +0100

    khtml/canvas: Ignore invalid values for BezierCurveTo instead of throwing an exception
    According to:
    http://philip.html5.org/tests/canvas/suite/tests/spec.html#testrefs.2d.nonfinite
    and test:
    http://philip.html5.org/tests/canvas/suite/tests/2d.path.bezierCurveTo.nonfinite.html
    
    REVIEW:108744

--- khtml/ecma/kjs_context2d.cpp
+++ khtml/ecma/kjs_context2d.cpp
@@ -382,7 +382,7 @@ JSValue *KJS::Context2DFunction::callAsFunction(ExecState *exec, JSObject *thisO
 
     case Context2D::BezierCurveTo: {
         KJS_REQUIRE_ARGS(6);
-        KJS_CHECK_FLOAT_ARGS(0, 5);
+        KJS_CHECK_FLOAT_ARGUMENTS_IGNORE_INVALID(0, 5);
 
         ctx->bezierCurveTo(args[0]->toFloat(exec), args[1]->toFloat(exec),
                            args[2]->toFloat(exec), args[3]->toFloat(exec),
