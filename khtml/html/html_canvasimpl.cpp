49bceccabfb8a491c1779340bcade7e492aec875
Bernd Buschinski <b.buschinski@googlemail.com>
2013-03-30 14:44:04 +0100

    khtml/canvas: Do not throw an exception if width/height values are negative for fillRect, but do nothing if width/height are zero
    According to:
    http://philip.html5.org/tests/canvas/suite/tests/spec.html#testrefs.2d.fillRect
    and test:
    http://philip.html5.org/tests/canvas/suite/tests/2d.fillRect.negative.html
    
    REVIEW:108751

--- khtml/html/html_canvasimpl.cpp
+++ khtml/html/html_canvasimpl.cpp
@@ -984,8 +984,7 @@ void CanvasContext2DImpl::clearRect (float x, float y, float w, float h, int& ex
 void CanvasContext2DImpl::fillRect (float x, float y, float w, float h, int& exceptionCode)
 {
     exceptionCode = 0;
-    if (w < 0.0f || h < 0.0f) {
-        exceptionCode = DOMException::INDEX_SIZE_ERR;
+    if (w == 0.0f || h == 0.0f) {
         return;
     }
 
