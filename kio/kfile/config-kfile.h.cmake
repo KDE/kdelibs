f5469542b92350bf5638a78a258a63d6c650c640
Aleix Pol <aleixpol@kde.org>
2013-06-12 16:18:58 +0200

    Revert "Fix compilation"
    This reverts commit 417b86bb4d674bdb9934a12a45170e7e64b66385.

--- kio/kfile/config-kfile.h.cmake
+++ kio/kfile/config-kfile.h.cmake
@@ -16,9 +16,6 @@
    Boston, MA 02110-1301, USA.
 */
 
-#ifndef CONFIG_KFILE_H
-#define CONFIG_KFILE_H
-
 #cmakedefine01 HAVE_GETGROUPLIST
 #if ! HAVE_GETGROUPLIST
 #include <sys/types.h> /* for gid_t */
