5eea81fc23ac360ae1052fd3f7c48cdc4c876f35
David Faure <faure+bluesystems@kde.org>
2013-06-20 17:58:35 +0200

    Put this back in, for people who haven't updated ECM yet.

--- tier1/kwindowsystem/src/kstartupinfo.cpp
+++ tier1/kwindowsystem/src/kstartupinfo.cpp
@@ -30,6 +30,11 @@ DEALINGS IN THE SOFTWARE.
 #endif
 #endif
 
+// We use emit
+#ifdef QT_NO_KEYWORDS
+#undef QT_NO_KEYWORDS
+#endif
+
 #ifdef QT_NO_CAST_FROM_ASCII
 #undef QT_NO_CAST_FROM_ASCII
 #endif
