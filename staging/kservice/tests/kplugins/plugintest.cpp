e8187d78af1d520d37d0a62385e2f1a12f53cf3e
Sebastian Kügler <sebas@kde.org>
2013-06-26 05:01:28 +0200

    Small test program which loads our kpluginfactory plugin

---
+++ staging/kservice/tests/kplugins/plugintest.cpp
@@ -0,0 +1,87 @@
+/******************************************************************************
+*   Copyright 2013 Sebastian Kügler <sebas@kde.org>                           *
+*                                                                             *
+*   This library is free software; you can redistribute it and/or             *
+*   modify it under the terms of the GNU Library General Public               *
+*   License as published by the Free Software Foundation; either              *
+*   version 2 of the License, or (at your option) any later version.          *
+*                                                                             *
+*   This library is distributed in the hope that it will be useful,           *
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU          *
+*   Library General Public License for more details.                          *
+*                                                                             *
+*   You should have received a copy of the GNU Library General Public License *
+*   along with this library; see the file COPYING.LIB.  If not, write to      *
+*   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,      *
+*   Boston, MA 02110-1301, USA.                                               *
+*******************************************************************************/
+
+#include "plugintest.h"
+
+#include <kqpluginfactory.h>
+#include <klocalizedstring.h>
+
+
+#include <qcommandlineparser.h>
+#include <QDir>
+#include <QJsonObject>
+#include <QStandardPaths>
+#include <QTimer>
+#include <QPluginLoader>
+
+#include <QDebug>
+
+#include <iostream>
+#include <iomanip>
+
+
+class PluginTestPrivate {
+public:
+    QString pluginName;
+    QCommandLineParser *parser;
+};
+
+PluginTest::PluginTest(int& argc, char** argv, QCommandLineParser *parser) :
+    QCoreApplication(argc, argv)
+{
+    d = new PluginTestPrivate;
+    d->parser = parser;
+    QTimer::singleShot(0, this, SLOT(runMain()));
+}
+
+PluginTest::~PluginTest()
+{
+    delete d;
+}
+
+void PluginTest::runMain()
+{
+    qDebug() << "plugin test runs: ";
+    //loadDataEngine();
+    qDebug() << " - - - -- - - - - ------------------------------------\n";
+    loadKQPlugin();
+    exit(0);
+    return;
+}
+
+void PluginTest::loadKQPlugin()
+{
+    qDebug() << "Load KQPlugin";
+    QString pluginPath = "/home/sebas/kf5/install/lib/x86_64-linux-gnu/kplugins/";
+    QCoreApplication::addLibraryPath(pluginPath);
+    QPluginLoader loader("/home/sebas/kf5/install/lib/x86_64-linux-gnu/kplugins/libkqpluginfactory.so", this);
+    //KQPluginFactory *factory = qobject_cast<KQPluginFactory*>(loader.instance());
+    QObject *factory = loader.instance();
+    if (factory) {
+        qDebug() << "loaded successfully and cast";
+        qDebug() << "metadata: " << loader.metaData();
+    } else {
+        qDebug() << "loading failed somehow";
+    }
+    //KQPluginFactory* factory = new KQPluginFactory(KPluginInfo(), this);
+
+}
+
+#include "moc_plugintest.cpp"
+
---
+++ staging/kservice/tests/kplugins/plugintest.h
@@ -0,0 +1,44 @@
+/******************************************************************************
+*   Copyright 2012 Sebastian Kügler <sebas@kde.org>                           *
+*                                                                             *
+*   This library is free software; you can redistribute it and/or             *
+*   modify it under the terms of the GNU Library General Public               *
+*   License as published by the Free Software Foundation; either              *
+*   version 2 of the License, or (at your option) any later version.          *
+*                                                                             *
+*   This library is distributed in the hope that it will be useful,           *
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU          *
+*   Library General Public License for more details.                          *
+*                                                                             *
+*   You should have received a copy of the GNU Library General Public License *
+*   along with this library; see the file COPYING.LIB.  If not, write to      *
+*   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,      *
+*   Boston, MA 02110-1301, USA.                                               *
+*******************************************************************************/
+
+#ifndef PLUGINTEST_H
+#define PLUGINTEST_H
+
+#include <QCoreApplication>
+
+class QCommandLineParser;
+class PluginTestPrivate;
+
+class PluginTest : public QCoreApplication
+{
+    Q_OBJECT
+
+    public:
+        PluginTest(int& argc, char** argv, QCommandLineParser *parser);
+        virtual ~PluginTest();
+
+    public Q_SLOTS:
+        void runMain();
+
+    private:
+        void loadKQPlugin();
+        PluginTestPrivate* d;
+};
+
+#endif
