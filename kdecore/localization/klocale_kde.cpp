f4269ef3498581964e8a1a13cd0d6d7f19c88762
Szókovács Róbert <szo@szo.hu>
2013-07-01 11:28:15 +0100

    Fix/workaround for legacy encoded filename handling
    This patch works around the problem of filenames that are not valid
    UTF8 strings: in KLocalePrivate::initFileNameEncoding() KDE sets the
    QFile's encoding/decoding function, to to/fromUTF8() in QString, which
    in turn calls QUtf8's converter function. I replaced this with the said
    functions' copy/pasted version and changed it, so when it encounters an
    invalid UTF8 string, it will encode it byte by byte, mapping the lower
    128 their normal unicode place and the upper 128 to U+10FE00-U+10FE7F,
    and the decoder reverses it.
    
    BUG: 159241
    REVIEW: 110043

--- kdecore/localization/klocale_kde.cpp
+++ kdecore/localization/klocale_kde.cpp
@@ -27,6 +27,7 @@
 #include "config-localization.h"
 
 #include <math.h>
+#include <locale.h>
 
 #ifdef HAVE_SYS_TIME_H
 #include <sys/time.h>
@@ -2672,19 +2673,196 @@ void KLocalePrivate::initFileNameEncoding()
     if (m_utf8FileEncoding) {
         QFile::setEncodingFunction(KLocalePrivate::encodeFileNameUTF8);
         QFile::setDecodingFunction(KLocalePrivate::decodeFileNameUTF8);
+    } else {
+        const QByteArray ctype = setlocale(LC_CTYPE, 0);
+        int indexOfDot = ctype.indexOf('.');
+        if (indexOfDot != -1) {
+            if (!qstrnicmp(ctype.data() + indexOfDot + 1, "UTF-8", 5)) {
+                QFile::setEncodingFunction(KLocalePrivate::encodeFileNameUTF8);
+                QFile::setDecodingFunction(KLocalePrivate::decodeFileNameUTF8);
+                m_utf8FileEncoding = true;
+            }
+            return;
+        }
+        QByteArray lang = qgetenv("LC_ALL");
+        if (lang.isEmpty() || lang == "C") {
+            lang = qgetenv("LC_CTYPE");
+        }
+        if (lang.isEmpty() || lang == "C") {
+            lang = qgetenv("LANG");
+        }
+        indexOfDot = lang.indexOf('.');
+        if (indexOfDot != -1) {
+            if (!qstrnicmp(lang.data() + indexOfDot + 1, "UTF-8", 5)) {
+                QFile::setEncodingFunction(KLocalePrivate::encodeFileNameUTF8);
+                QFile::setDecodingFunction(KLocalePrivate::decodeFileNameUTF8);
+                m_utf8FileEncoding = true;
+            }
+        }
     }
     // Otherwise, stay with QFile's default filename encoding functions
     // which, on Unix platforms, use the locale's codec.
 }
 
+static inline bool isUnicodeNonCharacter(uint ucs4)
+{
+    return (ucs4 & 0xfffe) == 0xfffe || (ucs4 - 0xfdd0U) < 16;
+}
+
 QByteArray KLocalePrivate::encodeFileNameUTF8(const QString & fileName)
 {
-    return fileName.toUtf8();
+    if (fileName.isNull()) return QByteArray();
+    int len = fileName.length();
+    const QChar *uc = fileName.constData();
+
+    uchar replacement = '?';
+    int rlen = 3*len;
+    int surrogate_high = -1;
+
+    QByteArray rstr;
+    rstr.resize(rlen);
+    uchar* cursor = (uchar*)rstr.data();
+    const QChar *ch = uc;
+    int invalid = 0;
+
+    const QChar *end = ch + len;
+    while (ch < end) {
+        uint u = ch->unicode();
+        if (surrogate_high >= 0) {
+            if (ch->isLowSurrogate()) {
+                u = QChar::surrogateToUcs4(surrogate_high, u);
+                surrogate_high = -1;
+            } else {
+                // high surrogate without low
+                *cursor = replacement;
+                ++ch;
+                ++invalid;
+                surrogate_high = -1;
+                continue;
+            }
+        } else if (ch->isLowSurrogate()) {
+            // low surrogate without high
+            *cursor = replacement;
+            ++ch;
+            ++invalid;
+            continue;
+        } else if (ch->isHighSurrogate()) {
+            surrogate_high = u;
+            ++ch;
+            continue;
+        }
+
+        if (u >= 0x10FE00 && u <= 0x10FE7F) {
+            *cursor++ = (uchar)(u - 0x10FE00 + 128) ;
+        }
+        else if (u < 0x80) {
+            *cursor++ = (uchar)u;
+        } else {
+            if (u < 0x0800) {
+                *cursor++ = 0xc0 | ((uchar) (u >> 6));
+            } else {
+                // is it one of the Unicode non-characters?
+                if (isUnicodeNonCharacter(u)) {
+                    *cursor++ = replacement;
+                    ++ch;
+                    ++invalid;
+                    continue;
+                }
+
+                if (u > 0xffff) {
+                    *cursor++ = 0xf0 | ((uchar) (u >> 18));
+                    *cursor++ = 0x80 | (((uchar) (u >> 12)) & 0x3f);
+                } else {
+                    *cursor++ = 0xe0 | (((uchar) (u >> 12)) & 0x3f);
+                }
+                *cursor++ = 0x80 | (((uchar) (u >> 6)) & 0x3f);
+            }
+            *cursor++ = 0x80 | ((uchar) (u&0x3f));
+        }
+        ++ch;
+    }
+
+    rstr.resize(cursor - (const uchar*)rstr.constData());
+    return rstr;
 }
 
 QString KLocalePrivate::decodeFileNameUTF8(const QByteArray &localFileName)
 {
-    return QString::fromUtf8(localFileName);
+    const char *chars = localFileName;
+    int len = qstrlen(chars);
+    int need = 0;
+    uint uc = 0;
+    uint min_uc = 0;
+
+    QString result(need + len + 1, Qt::Uninitialized); // worst case
+    ushort *qch = (ushort *)result.unicode();
+    uchar ch;
+
+    for (int i = 0; i < len; ++i) {
+        ch = chars[i];
+        if (need) {
+            if ((ch&0xc0) == 0x80) {
+                uc = (uc << 6) | (ch & 0x3f);
+                --need;
+                if (!need) {
+                    bool nonCharacter;
+                    if (!(nonCharacter = isUnicodeNonCharacter(uc)) && uc > 0xffff && uc < 0x110000) {
+                        // surrogate pair
+                        Q_ASSERT((qch - (ushort*)result.unicode()) + 2 < result.length());
+                        *qch++ = QChar::highSurrogate(uc);
+                        *qch++ = QChar::lowSurrogate(uc);
+                    } else if ((uc < min_uc) || (uc >= 0xd800 && uc <= 0xdfff) || nonCharacter || uc >= 0x110000) {
+                        // error: overlong sequence, UTF16 surrogate or non-character
+                        goto error;
+                    } else {
+                        *qch++ = uc;
+                    }
+                }
+            } else {
+                goto error;
+            }
+        } else {
+            if (ch < 128) {
+                *qch++ = ushort(ch);
+            } else if ((ch & 0xe0) == 0xc0) {
+                uc = ch & 0x1f;
+                need = 1;
+                min_uc = 0x80;
+            } else if ((ch & 0xf0) == 0xe0) {
+                uc = ch & 0x0f;
+                need = 2;
+                min_uc = 0x800;
+            } else if ((ch&0xf8) == 0xf0) {
+                uc = ch & 0x07;
+                need = 3;
+                min_uc = 0x10000;
+            } else {
+                goto error;
+            }
+        }
+    }
+    if (need > 0) {
+        // unterminated UTF sequence
+        goto error;
+    }
+    result.truncate(qch - (ushort *)result.unicode());
+    return result;
+
+error: 
+
+    qch = (ushort *)result.unicode();
+    for (int i = 0; i < len; ++i) {
+        ch = chars[i];
+        if (ch < 128) {
+            *qch++ = ushort(ch);
+        } else {
+            uint uc = ch - 128 + 0x10FE00; //U+10FE00-U+10FE7F
+            *qch++ = QChar::highSurrogate(uc);
+            *qch++ = QChar::lowSurrogate(uc);
+        }
+    }
+    result.truncate(qch - (ushort *)result.unicode());
+    return result;
 }
 
 void KLocalePrivate::setDateFormat(const QString &format)
