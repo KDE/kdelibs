d2c2d09190241b9979ed52057df7709d226def76
Mirko Boehm (AWS) <mirko@agile-workers.com>
2013-07-24 21:46:44 +0200

    Add QJobPointer typedef. Fix DeleteTest.

--- tier1/threadweaver/autotests/DeleteTest.cpp
+++ tier1/threadweaver/autotests/DeleteTest.cpp
@@ -15,22 +15,22 @@ public:
     explicit InstanceCountingJobSequence()
         : JobSequence()
     {
-        ++instances_;
+        instances_.fetchAndAddAcquire(1);
     }
 
     ~InstanceCountingJobSequence() {
-        --instances_;
+        instances_.fetchAndAddAcquire(-1);
     }
 
     static int instances() {
-        return instances_;
+        return instances_.loadAcquire();
     }
 
 private:
-    static int instances_;
+    static QAtomicInt instances_;
 };
 
-int InstanceCountingJobSequence::instances_;
+QAtomicInt InstanceCountingJobSequence::instances_;
 
 DeleteTest::DeleteTest()
 {
@@ -43,23 +43,25 @@ void DeleteTest::DeleteSequenceTest()
     const int NumberOfSequences = 100;
     ThreadWeaver::Weaver::instance()->suspend();
     for (int i = 0; i < NumberOfSequences; ++i) {
-        InstanceCountingJobSequence* seq = new InstanceCountingJobSequence;
-        seq->addJob(JobPointer(new BusyJob));
-        seq->addJob(JobPointer(new BusyJob));
-        QObjectJobDecorator *decorated = new QObjectJobDecorator(seq);
-        QVERIFY(connect(decorated, SIGNAL(done(ThreadWeaver::JobPointer)), SLOT(deleteSequence(ThreadWeaver::JobPointer))));
-        JobPointer jobSeq(decorated);
+        QJobPointer seq(new QObjectJobDecorator(new InstanceCountingJobSequence));
+        seq->sequence()->addJob(JobPointer(new BusyJob));
+        seq->sequence()->addJob(JobPointer(new BusyJob));
+        QVERIFY(connect(seq.data(), SIGNAL(done(ThreadWeaver::JobPointer)), SLOT(deleteSequence(ThreadWeaver::JobPointer))));
 
-        ThreadWeaver::Weaver::instance()->enqueue(jobSeq);
+        ThreadWeaver::Weaver::instance()->enqueue(seq);
         m_finishCount.fetchAndAddRelease(1);
     }
-    QCOMPARE(m_finishCount.fetchAndAddRelease(0), NumberOfSequences);
+    QCOMPARE(m_finishCount.loadAcquire(), NumberOfSequences);
     QEventLoop loop;
-    QVERIFY(connect(this, SIGNAL(deleteSequenceTestCompleted()), &loop, SLOT(quit())));
+    QVERIFY(connect(this, SIGNAL(deleteSequenceTestCompleted()), &loop, SLOT(quit()), Qt::QueuedConnection));
     ThreadWeaver::Weaver::instance()->resume();
     ThreadWeaver::Weaver::instance()->finish();
     loop.exec();
-    QCOMPARE(m_finishCount.fetchAndStoreRelease(0), 0);
+    QCOMPARE(m_finishCount.loadAcquire(), 0);
+    // The used Weaver instance needs to be shut down. The threads may still hold a reference to the previous job while
+    // waiting for the next one or blocking because the queue is empty. If all threads have exited, not references to any jobs are
+    // held anymore.
+    ThreadWeaver::Weaver::instance()->shutDown();
     QCOMPARE(InstanceCountingJobSequence::instances(), 0);
 }
 
