e84396d3ae07f4213276398e4d2b51fa456d08d8
Kevin Ottens <ervin+bluesystems@kde.org>
2013-07-25 11:59:25 +0200

    Fix not found sonnet(core|ui)_export.h
    0d5e8a432b7b63357696dbf2fbed5633919bff6b adjusted the include path for
    the export file to be sonnet/sonnetfoo_export.h in the public header
    files. This broke the build as those files cannot be found in the
    current build directory.
    
    To solve this problem the export files are now generated in a
    sub-directory sonnet, so that the path matches. In addition the includes
    in the private headers are adjusted to also look for sonnet/ so that
    they can find it.
    
    Patch by Martin Gräßlin
    
    REVIEW: 111678
    CCMAIL: arichardson.kde@googlemail.com

--- tier1/sonnet/src/core/client_p.h
+++ tier1/sonnet/src/core/client_p.h
@@ -25,7 +25,7 @@
 #include <QtCore/QStringList>
 #include <QtCore/QString>
 
-#include <sonnetcore_export.h>
+#include <sonnet/sonnetcore_export.h>
 /*
  * The fact that this class inherits from QObject makes me
  * hugely unhappy. The reason for as of this writing is that
