0db14b4e98dd1e4c5dc2f5147c011957fd01f924
Mirko Boehm (AWS) <mirko@agile-workers.com>
2013-08-07 13:58:51 +0200

    Introduce IdDecorator.
    It will greatly simplify the implementation of actual decorators.
    Simplify QObjectJobDecorator accordingly.

---
+++ tier1/threadweaver/src/Weaver/IdDecorator.h
@@ -0,0 +1,71 @@
+#ifndef THREADWEAVER_IDDECORATOR_H
+#define THREADWEAVER_IDDECORATOR_H
+
+#include <QObject>
+
+#include <threadweaver_export.h>
+#include <JobInterface.h>
+
+namespace ThreadWeaver {
+
+class JobCollection;
+class JobSequence;
+
+/** @brief IdDecorator decorates a job without changing it's behaviour.
+ *
+ *  It is supposed to be used as the base class for actual decorators that do change the behaviour of jobs. */
+class THREADWEAVER_EXPORT IdDecorator : public JobInterface
+{
+public:
+    explicit IdDecorator(JobInterface* job);
+    ~IdDecorator() Q_DECL_OVERRIDE;
+    /** Retrieve the decorated job. */
+    const JobInterface* job() const;
+    /** Retrieve the decorated job. */
+    JobInterface* job();
+    /** Retrieve the decorated job as a JobCollection.
+     *  If the decorated Job is not a JobCollection, 0 is returned. */
+    const JobCollection* collection() const;
+    /** Retrieve the decorated job as a JobCollection.
+     *  If the decorated Job is not a JobCollection, 0 is returned. */
+    JobCollection* collection();
+    /** Retrieve the decorated job as a JobSequence.
+     *  If the decorated Job is not a JobSequence, 0 is returned. */
+    const JobSequence* sequence() const;
+    /** Retrieve the decorated job as a JobSequence.
+     *  If the decorated Job is not a JobSequence, 0 is returned. */
+    JobSequence* sequence();
+
+    void execute(Thread*, JobPointer job) Q_DECL_OVERRIDE;
+    Executor* setExecutor(Executor* executor) Q_DECL_OVERRIDE;
+    Executor* executor() const Q_DECL_OVERRIDE;
+    int priority() const Q_DECL_OVERRIDE;
+    bool success () const Q_DECL_OVERRIDE;
+    void requestAbort() Q_DECL_OVERRIDE;
+    void aboutToBeQueued(QueueAPI *api) Q_DECL_OVERRIDE;
+    void aboutToBeQueued_locked(QueueAPI *api) Q_DECL_OVERRIDE;
+    void aboutToBeDequeued(QueueAPI *api) Q_DECL_OVERRIDE;
+    void aboutToBeDequeued_locked(QueueAPI *api) Q_DECL_OVERRIDE;
+    bool canBeExecuted(JobPointer job) Q_DECL_OVERRIDE;
+    bool isFinished() const Q_DECL_OVERRIDE;
+    void assignQueuePolicy(QueuePolicy*) Q_DECL_OVERRIDE;
+    void removeQueuePolicy(QueuePolicy*) Q_DECL_OVERRIDE;
+
+protected:
+    void freeQueuePolicyResources() Q_DECL_OVERRIDE;
+    void run() Q_DECL_OVERRIDE;
+    void defaultBegin(JobPointer job, Thread* thread) Q_DECL_OVERRIDE;
+    void defaultEnd(JobPointer job, Thread* thread) Q_DECL_OVERRIDE;
+
+    Thread* thread() Q_DECL_OVERRIDE;
+    void setFinished(bool status) Q_DECL_OVERRIDE;
+    QMutex* mutex() const Q_DECL_OVERRIDE;
+
+private:
+    class Private;
+    Private* const d;
+};
+
+}
+
+#endif // THREADWEAVER_IDDECORATOR_H
--- tier1/threadweaver/src/Weaver/QObjectJobDecorator.cpp
+++ tier1/threadweaver/src/Weaver/QObjectJobDecorator.cpp
@@ -5,53 +5,22 @@
 
 namespace ThreadWeaver {
 
-//Pssst: QObjectJobDecorator uses the d pointer to hold decoratee. It has no other member variables.
 QObjectJobDecorator::QObjectJobDecorator(JobInterface *decoratee, QObject *parent)
     : QObject(parent)
-    , d(reinterpret_cast<Private*>(decoratee))
+    , IdDecorator(decoratee)
 {
-}
-
-QObjectJobDecorator::~QObjectJobDecorator()
-{
-    Q_ASSERT(d);
-    delete job();
-}
-
-QMutex *QObjectJobDecorator::mutex() const
-{
-    Q_ASSERT(d);
-    return job()->mutex();
-}
-
-void QObjectJobDecorator::setFinished(bool status)
-{
-    Q_ASSERT(d);
-    job()->setFinished(status);
-}
-
-Thread *QObjectJobDecorator::thread()
-{
-    Q_ASSERT(d);
-    return job()->thread();
-}
-
-void QObjectJobDecorator::run()
-{
-    Q_ASSERT(d);
-    job()->run();
 }
 
 void QObjectJobDecorator::defaultBegin(JobPointer self, Thread *thread)
 {
-    Q_ASSERT(d);
+    Q_ASSERT(job());
     Q_EMIT started(self);
     job()->defaultBegin(self, thread);
 }
 
 void QObjectJobDecorator::defaultEnd(JobPointer self, Thread *thread)
 {
-    Q_ASSERT(d);
+    Q_ASSERT(job());
     job()->defaultEnd(self, thread);
     if (!self->success()) {
         Q_EMIT failed(self);
@@ -59,124 +28,6 @@ void QObjectJobDecorator::defaultEnd(JobPointer self, Thread *thread)
     Q_EMIT done(self);
 }
 
-void QObjectJobDecorator::freeQueuePolicyResources()
-{
-    Q_ASSERT(d);
-    job()->freeQueuePolicyResources();
 }
 
-void QObjectJobDecorator::removeQueuePolicy(QueuePolicy* policy)
-{
-    Q_ASSERT(d);
-    job()->removeQueuePolicy(policy);
-}
-
-void QObjectJobDecorator::assignQueuePolicy(QueuePolicy* policy)
-{
-    Q_ASSERT(d);
-    job()->assignQueuePolicy(policy);
-}
-
-bool QObjectJobDecorator::isFinished() const
-{
-    Q_ASSERT(d);
-    return job()->isFinished();
-}
-
-bool QObjectJobDecorator::canBeExecuted(JobPointer self)
-{
-    Q_ASSERT(d);
-    return job()->canBeExecuted(self);
-}
-
-void QObjectJobDecorator::aboutToBeQueued(QueueAPI *api)
-{
-    Q_ASSERT(d);
-    job()->aboutToBeQueued(api);
-}
-
-void QObjectJobDecorator::aboutToBeQueued_locked(QueueAPI *api)
-{
-    Q_ASSERT(d);
-    job()->aboutToBeQueued_locked(api);
-}
-
-void QObjectJobDecorator::aboutToBeDequeued(QueueAPI *api)
-{
-    Q_ASSERT(d);
-    job()->aboutToBeDequeued(api);
-}
-
-void QObjectJobDecorator::aboutToBeDequeued_locked(QueueAPI* api)
-{
-    Q_ASSERT(d);
-    job()->aboutToBeDequeued_locked(api);
-}
-
-void QObjectJobDecorator::requestAbort()
-{
-    Q_ASSERT(d);
-    job()->requestAbort();
-}
-
-bool QObjectJobDecorator::success() const
-{
-    Q_ASSERT(d);
-    return job()->success();
-}
-
-int QObjectJobDecorator::priority() const
-{
-    Q_ASSERT(d);
-    return job()->priority();
-}
-
-Executor *QObjectJobDecorator::executor() const
-{
-    Q_ASSERT(d);
-    return job()->executor();
-}
-
-Executor *QObjectJobDecorator::setExecutor(Executor *executor)
-{
-    Q_ASSERT(d);
-    return job()->setExecutor(executor);
-}
-
-void QObjectJobDecorator::execute(ThreadWeaver::Thread* thread, ThreadWeaver::JobPointer self)
-{
-    Q_ASSERT(d);
-    job()->execute(thread, self);
-}
-
-const ThreadWeaver::JobInterface* QObjectJobDecorator::job() const
-{
-    return reinterpret_cast<JobInterface*>(d);
-}
-
-JobInterface *QObjectJobDecorator::job()
-{
-    return reinterpret_cast<JobInterface*>(d);
-}
-
-const ThreadWeaver::JobCollection *ThreadWeaver::QObjectJobDecorator::collection() const
-{
-   return dynamic_cast<const JobCollection*>(job());
-}
-
-JobCollection *QObjectJobDecorator::collection()
-{
-    return dynamic_cast<JobCollection*>(job());
-}
-
-const JobSequence *QObjectJobDecorator::sequence() const
-{
-    return dynamic_cast<const JobSequence*>(job());
-}
-
-JobSequence *QObjectJobDecorator::sequence()
-{
-    return dynamic_cast<JobSequence*>(job());
-}
-
-}
+#include "moc_QObjectJobDecorator.cpp"
