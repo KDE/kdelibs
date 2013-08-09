d9a79c3091d44a00ac21e47f08a30e356b681832
Wojciech Kapuscinski <wojtask9.kde@gmail.com>
2013-08-09 22:45:10 +0200

    Use LINK_PRIVATE LINK_PUBLIC in CMake (part 2)
    REVIEW: 111985

--- tier2/kauth/src/ConfigureChecks.cmake
+++ tier2/kauth/src/ConfigureChecks.cmake
@@ -150,12 +150,6 @@ elseif(KDE4_AUTH_BACKEND_NAME STREQUAL "POLKITQT-1")
 
     set(KAUTH_BACKEND_LIBS ${POLKITQT-1_CORE_LIBRARY} Qt5::DBus Qt5::Widgets)
 
-    find_package(X11)
-    if (X11_FOUND)
-        # QtGui as well
-        set(KAUTH_BACKEND_LIBS ${KAUTH_BACKEND_LIBS} Qt5::Widgets)
-     endif (X11_FOUND)
-
     # POLKITQT-1_POLICY_FILES_INSTALL_DIR has an absolute pathname, fix that.
     string(REPLACE ${POLKITQT-1_INSTALL_DIR}
         ${CMAKE_INSTALL_PREFIX} _KDE4_AUTH_POLICY_FILES_INSTALL_DIR
