KDE/kdelibs

commit bf9a7092566622404768bb7ac98a7a52530edc10

Port calls of S_IS(x) macros to their qplatformdefs equivalent
- S_ISLNK(x) => (x & QT_STAT_LNK)
- S_ISREG(x) => (x & QT_STAT_REG)
- S_ISDIR(x) => (x & QT_STAT_DIR)

In kfileitem.cpp: only use S_ISSOCK, S_ISCHR, S_ISBLK and S_ISFIFO when building
on Unix platforms (Q_OS_UNIX) because those macros have no qplatformdefs
equivalents.

REVIEW: 112064

kio/kfile/kpropertiesdialog.cpp
@@ -77,6 +77,7 @@ extern "C" {
 #include <QFileInfo>
 #include <qmimedatabase.h>
 #include <QUrl>
+#include <qplatformdefs.h>
 
 #if HAVE_POSIX_ACL
 extern "C" {
@@ -895,7 +896,7 @@ KFilePropsPlugin::KFilePropsPlugin( KPropertiesDialog *_props )
         directory += ')';
     }
 
-    if (!isTrash && (bDesktopFile || S_ISDIR(mode))
+    if (!isTrash && (bDesktopFile || (mode & QT_STAT_DIR))
         && !d->bMultiple // not implemented for multiple
         && enableIconButton()) // #56857
     {
@@ -1486,7 +1487,7 @@ void KFilePropsPlugin::applyIconChanges()
     if ( url.isLocalFile()) {
         QString path;
 
-        if (S_ISDIR(properties->item().mode()))
+        if (properties->item().mode() & QT_STAT_DIR)
         {
             path = url.toLocalFile() + QString::fromLatin1("/.directory");
             // don't call updateUrl because the other tabs (i.e. permissions)

kio/kio/krun.cpp
@@ -84,6 +84,7 @@
 #elif defined(Q_WS_WIN)
 #include <QDesktopServices>
 #endif
+#include <qplatformdefs.h>
 #include <qstandardpaths.h>
 
 KRun::KRunPrivate::KRunPrivate(KRun *parent)
@@ -1246,7 +1247,7 @@ void KRun::init()
 
 #if 0 // removed for KF5 (for portability). Reintroduce a bool or flag if useful.
     // Did we already get the information that it is a directory ?
-    if (S_ISDIR(d->m_mode)) {
+    if (d->m_mode & QT_STAT_DIR) {
         mimeTypeDetermined("inode/directory");
         return;
     }
@@ -1415,7 +1416,7 @@ void KRun::slotStatResult(KJob * job)
 
         const KIO::UDSEntry entry = statJob->statResult();
         const mode_t mode = entry.numberValue(KIO::UDSEntry::UDS_FILE_TYPE);
-        if (S_ISDIR(mode)) {
+        if (mode & QT_STAT_DIR) {
             d->m_bIsDirectory = true; // it's a dir
         }
         else {

kioslave/file/file.cpp
@@ -266,7 +266,7 @@ void FileProtocol::mkdir( const QUrl& url, int permissions )
         }
     }
 
-    if ( S_ISDIR( buff.st_mode ) ) {
+    if ( buff.st_mode & QT_STAT_DIR ) {
         // qDebug() << "ERR_DIR_ALREADY_EXIST";
         error(KIO::ERR_DIR_ALREADY_EXIST, path);
         return;
@@ -295,11 +295,11 @@ void FileProtocol::get( const QUrl& url )
         return;
     }
 
-    if ( S_ISDIR( buff.st_mode ) ) {
+    if ( buff.st_mode & QT_STAT_DIR ) {
         error(KIO::ERR_IS_DIRECTORY, path);
         return;
     }
-    if ( !S_ISREG( buff.st_mode ) ) {
+    if ( !( buff.st_mode & QT_STAT_REG ) ) {
         error(KIO::ERR_CANNOT_OPEN_FOR_READING, path);
         return;
     }
@@ -392,11 +392,11 @@ void FileProtocol::open(const QUrl &url, QIODevice::OpenMode mode)
         return;
     }
 
-    if ( S_ISDIR( buff.st_mode ) ) {
+    if ( buff.st_mode & QT_STAT_DIR ) {
         error(KIO::ERR_IS_DIRECTORY, openPath);
         return;
     }
-    if ( !S_ISREG( buff.st_mode ) ) {
+    if ( !( buff.st_mode & QT_STAT_REG ) ) {
         error(KIO::ERR_CANNOT_OPEN_FOR_READING, openPath);
         return;
     }
@@ -513,7 +513,7 @@ void FileProtocol::put( const QUrl& url, int _mode, KIO::JobFlags _flags )
         QT_STATBUF buff_part;
         bPartExists = (QT_LSTAT(QFile::encodeName(dest_part).constData(), &buff_part) != -1);
 
-        if (bPartExists && !(_flags & KIO::Resume) && !(_flags & KIO::Overwrite) && buff_part.st_size > 0 && S_ISREG(buff_part.st_mode))
+        if (bPartExists && !(_flags & KIO::Resume) && !(_flags & KIO::Overwrite) && buff_part.st_size > 0 && (buff_part.st_mode & QT_STAT_REG))
         {
             // qDebug() << "calling canResume with" << KIO::number(buff_part.st_size);
 
@@ -528,7 +528,7 @@ void FileProtocol::put( const QUrl& url, int _mode, KIO::JobFlags _flags )
 
     if ( bOrigExists && !(_flags & KIO::Overwrite) && !(_flags & KIO::Resume))
     {
-        if (S_ISDIR(buff_orig.st_mode))
+        if (buff_orig.st_mode & QT_STAT_DIR)
             error( KIO::ERR_DIR_ALREADY_EXIST, dest_orig );
         else
             error( KIO::ERR_FILE_ALREADY_EXIST, dest_orig );
@@ -751,7 +751,7 @@ bool FileProtocol::createUDSEntry( const QString & filename, const QByteArray & 
             entry.insert( KIO::UDSEntry::UDS_INODE, buff.st_ino );
         }
 
-        if (S_ISLNK(buff.st_mode)) {
+        if (buff.st_mode & QT_STAT_LNK) {
 
             char buffer2[ 1000 ];
             int n = readlink( path.data(), buffer2, 999 );
@@ -1211,7 +1211,7 @@ static void appendACLAtoms( const QByteArray & path, UDSEntry& entry, mode_t typ
 
     acl_t acl = 0;
     acl_t defaultAcl = 0;
-    bool isDir = S_ISDIR( type );
+    bool isDir = type & QT_STAT_DIR;
     // do we have an acl for the file, and/or a default acl for the dir, if it is one?
     acl = acl_get_file( path.data(), ACL_TYPE_ACCESS );
     /* Sadly libacl does not provided a means of checking for extended ACL and default

kioslave/file/file_unix.cpp
@@ -85,7 +85,7 @@ void FileProtocol::copy( const QUrl &srcUrl, const QUrl &destUrl,
         return;
     }
 
-    if ( S_ISDIR( buff_src.st_mode ) ) {
+    if ( buff_src.st_mode & QT_STAT_DIR ) {
 	error(KIO::ERR_IS_DIRECTORY, src);
 	return;
     }
@@ -98,7 +98,7 @@ void FileProtocol::copy( const QUrl &srcUrl, const QUrl &destUrl,
     bool dest_exists = (QT_LSTAT(_dest.data(), &buff_dest) != -1);
     if ( dest_exists )
     {
-        if (S_ISDIR(buff_dest.st_mode))
+        if (buff_dest.st_mode & QT_STAT_DIR)
         {
            error(KIO::ERR_DIR_ALREADY_EXIST, dest);
            return;
@@ -119,7 +119,7 @@ void FileProtocol::copy( const QUrl &srcUrl, const QUrl &destUrl,
         // If the destination is a symlink and overwrite is TRUE,
         // remove the symlink first to prevent the scenario where
         // the symlink actually points to current source!
-        if ((_flags & KIO::Overwrite) && S_ISLNK(buff_dest.st_mode))
+        if ((_flags & KIO::Overwrite) && (buff_dest.st_mode & QT_STAT_LNK))
         {
             //qDebug() << "copy(): LINK DESTINATION";
             QFile::remove(dest);
@@ -388,8 +388,8 @@ void FileProtocol::listDir( const QUrl& url)
                 continue; // how can stat fail?
             }
             entry.insert(KIO::UDSEntry::UDS_FILE_TYPE,
-                          (S_ISDIR(st.st_mode)) ? S_IFDIR : S_IFREG );
-            const bool isSymLink = S_ISLNK(st.st_mode);
+                          (st.st_mode & QT_STAT_DIR) ? S_IFDIR : S_IFREG );
+            const bool isSymLink = (st.st_mode & QT_STAT_LNK);
 #endif
             if (isSymLink) {
                 // for symlinks obey the UDSEntry contract and provide UDS_LINK_DEST
@@ -437,7 +437,7 @@ void FileProtocol::rename( const QUrl &srcUrl, const QUrl &destUrl,
     bool dest_exists = (QT_LSTAT(_dest.data(), &buff_dest) != -1);
     if ( dest_exists )
     {
-        if (S_ISDIR(buff_dest.st_mode))
+        if (buff_dest.st_mode & QT_STAT_DIR)
         {
            error(KIO::ERR_DIR_ALREADY_EXIST, dest);
            return;
@@ -498,7 +498,7 @@ void FileProtocol::symlink( const QString &target, const QUrl &destUrl, KIO::Job
             else
             {
                 QT_STATBUF buff_dest;
-                if (QT_LSTAT(QFile::encodeName(dest), &buff_dest) == 0 && S_ISDIR(buff_dest.st_mode))
+                if (QT_LSTAT(QFile::encodeName(dest), &buff_dest) == 0 && (buff_dest.st_mode & QT_STAT_DIR))
                     error(KIO::ERR_DIR_ALREADY_EXIST, dest);
                 else
                     error(KIO::ERR_FILE_ALREADY_EXIST, dest);

staging/kio/src/core/kfileitem.cpp
@@ -34,6 +34,7 @@
 #include <QtCore/QMap>
 #include <QMimeDatabase>
 #include <QDebug>
+#include <qplatformdefs.h>
 
 #include <klocalizedstring.h>
 #include <kdesktopfile.h>
@@ -44,10 +45,6 @@
 #include <ksambashare.h>
 #endif
 #include <kfilesystemtype_p.h>
-
-#ifndef S_ISSOCK
-#define S_ISSOCK(x) false
-#endif
 
 class KFileItemPrivate : public QSharedData
 {
@@ -471,8 +468,9 @@ QString KFileItemPrivate::parsePermissions(mode_t perm) const
     if (m_bLink)
         buffer[0] = 'l';
     else if (m_fileMode != KFileItem::Unknown) {
-        if (S_ISDIR(m_fileMode))
+        if (m_fileMode & QT_STAT_DIR)
             buffer[0] = 'd';
+#ifdef Q_OS_UNIX
         else if (S_ISSOCK(m_fileMode))
             buffer[0] = 's';
         else if (S_ISCHR(m_fileMode))
@@ -481,6 +479,7 @@ QString KFileItemPrivate::parsePermissions(mode_t perm) const
             buffer[0] = 'b';
         else if (S_ISFIFO(m_fileMode))
             buffer[0] = 'p';
+#endif // Q_OS_UNIX
         else
             buffer[0] = '-';
     } else {
@@ -1023,7 +1022,7 @@ QStringList KFileItem::overlays() const
         names.append("emblem-symbolic-link");
     }
 
-    if ( !S_ISDIR( d->m_fileMode ) // Locked dirs have a special icon, use the overlay for files only
+    if ( !(d->m_fileMode & QT_STAT_DIR) // Locked dirs have a special icon, use the overlay for files only
          && !isReadable()) {
         names.append("object-locked");
     }
@@ -1053,7 +1052,7 @@ QStringList KFileItem::overlays() const
     }
 
 #ifndef Q_OS_WIN
-    if( S_ISDIR( d->m_fileMode ) && d->m_bIsLocalUrl)
+    if( (d->m_fileMode & QT_STAT_DIR) && d->m_bIsLocalUrl)
     {
         if (KSambaShare::instance()->isDirectoryShared( d->m_url.toLocalFile() ) ||
             KNFSShare::instance()->isDirectoryShared( d->m_url.toLocalFile() ))
@@ -1166,7 +1165,7 @@ bool KFileItem::isDir() const
         //qDebug() << d << url() << "can't say -> false";
         return false; // can't say for sure, so no
     }
-    return (S_ISDIR(d->m_fileMode));
+    return d->m_fileMode & QT_STAT_DIR;
 }
 
 bool KFileItem::isFile() const
@@ -1220,7 +1219,7 @@ QString KFileItem::getStatusBarInfo() const
     {
         text += i18n(" (Points to %1)", targetUrl().toDisplayString());
     }
-    else if ( S_ISREG( d->m_fileMode ) )
+    else if ( d->m_fileMode & QT_STAT_REG )
     {
         text += QString(" (%1, %2)").arg( comment, KIO::convertSize( size() ) );
     }
@@ -1655,7 +1654,7 @@ bool KFileItem::isRegularFile() const
     if (!d)
         return false;
 
-    return S_ISREG(d->m_fileMode);
+    return d->m_fileMode & QT_STAT_REG;
 }
 
 QDebug operator<<(QDebug stream, const KFileItem& item)

staging/kio/src/core/udsentry.cpp
@@ -21,12 +21,12 @@
 
 #include "udsentry.h"
 
-#include <sys/stat.h> // S_ISDIR
-
 #include <QtCore/QString>
 #include <QtCore/QHash>
 #include <QtCore/QList>
 #include <QtCore/QDataStream>
+
+#include <qplatformdefs.h>
 
 using namespace KIO;
 
@@ -82,7 +82,7 @@ long long UDSEntry::numberValue(uint field, long long defaultValue) const
 
 bool UDSEntry::isDir() const
 {
-    return S_ISDIR(numberValue(UDS_FILE_TYPE));
+    return numberValue(UDS_FILE_TYPE) & QT_STAT_DIR;
 }
 
 bool UDSEntry::isLink() const

staging/kio/src/widgets/kurlcompletion.cpp
@@ -1316,7 +1316,7 @@ void KUrlCompletion::postProcessMatch(QString* pMatch) const
 
             QT_STATBUF sbuff;
             if (QT_STAT(file.constData(), &sbuff) == 0) {
-                if (S_ISDIR(sbuff.st_mode))
+                if (sbuff.st_mode & QT_STAT_DIR)
                     pMatch->append(QLatin1Char('/'));
             } else {
                 //qDebug() << "Could not stat file" << copy;

tier1/karchive/src/k7zip.cpp
@@ -23,6 +23,7 @@
 #include <QtCore/QDir>
 #include <QtCore/QBuffer>
 #include <QtCore/QFile>
+#include <qplatformdefs.h>
 
 #include "kcompressiondevice.h"
 #include <kfilterbase.h>
@@ -2635,7 +2636,7 @@ bool K7Zip::openArchive( QIODevice::OpenMode mode )
         bool symlink = false;
         if (fileInfo->attributes & FILE_ATTRIBUTE_UNIX_EXTENSION) {
             access = fileInfo->attributes >> 16;
-            if (S_ISLNK(access)) {
+            if (access & QT_STAT_LNK) {
                 symlink = true;
             }
         } else {

commit b1d903e15b9c62558d67b0ceba0ca725921c79e1

Fix porting errors introduced by bf9a709
S_ISLNK is *not* equivalent to "& QT_STAT_LNK" (it's not even equivalent
to S_IFLNK in fact). This porting error was caught by our unit tests...

CCMAIL: agateau@kde.org

kio/kfile/kpropertiesdialog.cpp
@@ -896,7 +896,7 @@ KFilePropsPlugin::KFilePropsPlugin( KPropertiesDialog *_props )
         directory += ')';
     }
 
-    if (!isTrash && (bDesktopFile || (mode & QT_STAT_DIR))
+    if (!isTrash && (bDesktopFile || ((mode & QT_STAT_MASK) == QT_STAT_DIR))
         && !d->bMultiple // not implemented for multiple
         && enableIconButton()) // #56857
     {
@@ -1487,7 +1487,7 @@ void KFilePropsPlugin::applyIconChanges()
     if ( url.isLocalFile()) {
         QString path;
 
-        if (properties->item().mode() & QT_STAT_DIR)
+        if ((properties->item().mode() & QT_STAT_MASK) == QT_STAT_DIR)
         {
             path = url.toLocalFile() + QString::fromLatin1("/.directory");
             // don't call updateUrl because the other tabs (i.e. permissions)

kio/kio/krun.cpp
@@ -1247,7 +1247,7 @@ void KRun::init()
 
 #if 0 // removed for KF5 (for portability). Reintroduce a bool or flag if useful.
     // Did we already get the information that it is a directory ?
-    if (d->m_mode & QT_STAT_DIR) {
+    if ((d->m_mode & QT_STAT_MASK) == QT_STAT_DIR) {
         mimeTypeDetermined("inode/directory");
         return;
     }
@@ -1416,7 +1416,7 @@ void KRun::slotStatResult(KJob * job)
 
         const KIO::UDSEntry entry = statJob->statResult();
         const mode_t mode = entry.numberValue(KIO::UDSEntry::UDS_FILE_TYPE);
-        if (mode & QT_STAT_DIR) {
+        if ((mode & QT_STAT_MASK) == QT_STAT_DIR) {
             d->m_bIsDirectory = true; // it's a dir
         }
         else {

kioslave/file/file.cpp
@@ -266,7 +266,7 @@ void FileProtocol::mkdir( const QUrl& url, int permissions )
         }
     }
 
-    if ( buff.st_mode & QT_STAT_DIR ) {
+    if ( (buff.st_mode & QT_STAT_MASK) == QT_STAT_DIR ) {
         // qDebug() << "ERR_DIR_ALREADY_EXIST";
         error(KIO::ERR_DIR_ALREADY_EXIST, path);
         return;
@@ -295,11 +295,11 @@ void FileProtocol::get( const QUrl& url )
         return;
     }
 
-    if ( buff.st_mode & QT_STAT_DIR ) {
+    if ( (buff.st_mode & QT_STAT_MASK) == QT_STAT_DIR ) {
         error(KIO::ERR_IS_DIRECTORY, path);
         return;
     }
-    if ( !( buff.st_mode & QT_STAT_REG ) ) {
+    if ( ( buff.st_mode & QT_STAT_MASK ) != QT_STAT_REG ) {
         error(KIO::ERR_CANNOT_OPEN_FOR_READING, path);
         return;
     }
@@ -392,11 +392,11 @@ void FileProtocol::open(const QUrl &url, QIODevice::OpenMode mode)
         return;
     }
 
-    if ( buff.st_mode & QT_STAT_DIR ) {
+    if ( (buff.st_mode & QT_STAT_MASK) == QT_STAT_DIR ) {
         error(KIO::ERR_IS_DIRECTORY, openPath);
         return;
     }
-    if ( !( buff.st_mode & QT_STAT_REG ) ) {
+    if ( ( buff.st_mode & QT_STAT_MASK ) != QT_STAT_REG ) {
         error(KIO::ERR_CANNOT_OPEN_FOR_READING, openPath);
         return;
     }
@@ -513,7 +513,7 @@ void FileProtocol::put( const QUrl& url, int _mode, KIO::JobFlags _flags )
         QT_STATBUF buff_part;
         bPartExists = (QT_LSTAT(QFile::encodeName(dest_part).constData(), &buff_part) != -1);
 
-        if (bPartExists && !(_flags & KIO::Resume) && !(_flags & KIO::Overwrite) && buff_part.st_size > 0 && (buff_part.st_mode & QT_STAT_REG))
+        if (bPartExists && !(_flags & KIO::Resume) && !(_flags & KIO::Overwrite) && buff_part.st_size > 0 && ((buff_part.st_mode & QT_STAT_MASK) == QT_STAT_REG))
         {
             // qDebug() << "calling canResume with" << KIO::number(buff_part.st_size);
 
@@ -528,7 +528,7 @@ void FileProtocol::put( const QUrl& url, int _mode, KIO::JobFlags _flags )
 
     if ( bOrigExists && !(_flags & KIO::Overwrite) && !(_flags & KIO::Resume))
     {
-        if (buff_orig.st_mode & QT_STAT_DIR)
+        if ((buff_orig.st_mode & QT_STAT_MASK) == QT_STAT_DIR)
             error( KIO::ERR_DIR_ALREADY_EXIST, dest_orig );
         else
             error( KIO::ERR_FILE_ALREADY_EXIST, dest_orig );
@@ -751,7 +751,7 @@ bool FileProtocol::createUDSEntry( const QString & filename, const QByteArray & 
             entry.insert( KIO::UDSEntry::UDS_INODE, buff.st_ino );
         }
 
-        if (buff.st_mode & QT_STAT_LNK) {
+        if ((buff.st_mode & QT_STAT_MASK) == QT_STAT_LNK) {
 
             char buffer2[ 1000 ];
             int n = readlink( path.data(), buffer2, 999 );
@@ -1211,7 +1211,7 @@ static void appendACLAtoms( const QByteArray & path, UDSEntry& entry, mode_t typ
 
     acl_t acl = 0;
     acl_t defaultAcl = 0;
-    bool isDir = type & QT_STAT_DIR;
+    bool isDir = (type & QT_STAT_MASK) == QT_STAT_DIR;
     // do we have an acl for the file, and/or a default acl for the dir, if it is one?
     acl = acl_get_file( path.data(), ACL_TYPE_ACCESS );
     /* Sadly libacl does not provided a means of checking for extended ACL and default

kioslave/file/file_unix.cpp
@@ -85,7 +85,7 @@ void FileProtocol::copy( const QUrl &srcUrl, const QUrl &destUrl,
         return;
     }
 
-    if ( buff_src.st_mode & QT_STAT_DIR ) {
+    if ( (buff_src.st_mode & QT_STAT_MASK) == QT_STAT_DIR ) {
 	error(KIO::ERR_IS_DIRECTORY, src);
 	return;
     }
@@ -98,7 +98,7 @@ void FileProtocol::copy( const QUrl &srcUrl, const QUrl &destUrl,
     bool dest_exists = (QT_LSTAT(_dest.data(), &buff_dest) != -1);
     if ( dest_exists )
     {
-        if (buff_dest.st_mode & QT_STAT_DIR)
+        if ((buff_dest.st_mode & QT_STAT_MASK) == QT_STAT_DIR)
         {
            error(KIO::ERR_DIR_ALREADY_EXIST, dest);
            return;
@@ -119,7 +119,7 @@ void FileProtocol::copy( const QUrl &srcUrl, const QUrl &destUrl,
         // If the destination is a symlink and overwrite is TRUE,
         // remove the symlink first to prevent the scenario where
         // the symlink actually points to current source!
-        if ((_flags & KIO::Overwrite) && (buff_dest.st_mode & QT_STAT_LNK))
+        if ((_flags & KIO::Overwrite) && ((buff_dest.st_mode & QT_STAT_MASK) == QT_STAT_LNK))
         {
             //qDebug() << "copy(): LINK DESTINATION";
             QFile::remove(dest);
@@ -388,8 +388,8 @@ void FileProtocol::listDir( const QUrl& url)
                 continue; // how can stat fail?
             }
             entry.insert(KIO::UDSEntry::UDS_FILE_TYPE,
-                          (st.st_mode & QT_STAT_DIR) ? S_IFDIR : S_IFREG );
-            const bool isSymLink = (st.st_mode & QT_STAT_LNK);
+                          ((st.st_mode & QT_STAT_MASK) == QT_STAT_DIR) ? S_IFDIR : S_IFREG );
+            const bool isSymLink = ((st.st_mode & QT_STAT_MASK) == QT_STAT_LNK);
 #endif
             if (isSymLink) {
                 // for symlinks obey the UDSEntry contract and provide UDS_LINK_DEST
@@ -437,7 +437,7 @@ void FileProtocol::rename( const QUrl &srcUrl, const QUrl &destUrl,
     bool dest_exists = (QT_LSTAT(_dest.data(), &buff_dest) != -1);
     if ( dest_exists )
     {
-        if (buff_dest.st_mode & QT_STAT_DIR)
+        if ((buff_dest.st_mode & QT_STAT_MASK) == QT_STAT_DIR)
         {
            error(KIO::ERR_DIR_ALREADY_EXIST, dest);
            return;
@@ -498,7 +498,7 @@ void FileProtocol::symlink( const QString &target, const QUrl &destUrl, KIO::Job
             else
             {
                 QT_STATBUF buff_dest;
-                if (QT_LSTAT(QFile::encodeName(dest), &buff_dest) == 0 && (buff_dest.st_mode & QT_STAT_DIR))
+                if (QT_LSTAT(QFile::encodeName(dest), &buff_dest) == 0 && ((buff_dest.st_mode & QT_STAT_MASK) == QT_STAT_DIR))
                     error(KIO::ERR_DIR_ALREADY_EXIST, dest);
                 else
                     error(KIO::ERR_FILE_ALREADY_EXIST, dest);

staging/kio/src/core/kfileitem.cpp
@@ -468,7 +468,7 @@ QString KFileItemPrivate::parsePermissions(mode_t perm) const
     if (m_bLink)
         buffer[0] = 'l';
     else if (m_fileMode != KFileItem::Unknown) {
-        if (m_fileMode & QT_STAT_DIR)
+        if ((m_fileMode & QT_STAT_MASK) == QT_STAT_DIR)
             buffer[0] = 'd';
 #ifdef Q_OS_UNIX
         else if (S_ISSOCK(m_fileMode))
@@ -1022,7 +1022,7 @@ QStringList KFileItem::overlays() const
         names.append("emblem-symbolic-link");
     }
 
-    if ( !(d->m_fileMode & QT_STAT_DIR) // Locked dirs have a special icon, use the overlay for files only
+    if ( ((d->m_fileMode & QT_STAT_MASK) != QT_STAT_DIR) // Locked dirs have a special icon, use the overlay for files only
          && !isReadable()) {
         names.append("object-locked");
     }
@@ -1052,7 +1052,7 @@ QStringList KFileItem::overlays() const
     }
 
 #ifndef Q_OS_WIN
-    if( (d->m_fileMode & QT_STAT_DIR) && d->m_bIsLocalUrl)
+    if( ((d->m_fileMode & QT_STAT_MASK) == QT_STAT_DIR) && d->m_bIsLocalUrl)
     {
         if (KSambaShare::instance()->isDirectoryShared( d->m_url.toLocalFile() ) ||
             KNFSShare::instance()->isDirectoryShared( d->m_url.toLocalFile() ))
@@ -1165,7 +1165,7 @@ bool KFileItem::isDir() const
         //qDebug() << d << url() << "can't say -> false";
         return false; // can't say for sure, so no
     }
-    return d->m_fileMode & QT_STAT_DIR;
+    return (d->m_fileMode & QT_STAT_MASK) == QT_STAT_DIR;
 }
 
 bool KFileItem::isFile() const
@@ -1219,7 +1219,7 @@ QString KFileItem::getStatusBarInfo() const
     {
         text += i18n(" (Points to %1)", targetUrl().toDisplayString());
     }
-    else if ( d->m_fileMode & QT_STAT_REG )
+    else if ( (d->m_fileMode & QT_STAT_MASK) == QT_STAT_REG )
     {
         text += QString(" (%1, %2)").arg( comment, KIO::convertSize( size() ) );
     }
@@ -1654,7 +1654,7 @@ bool KFileItem::isRegularFile() const
     if (!d)
         return false;
 
-    return d->m_fileMode & QT_STAT_REG;
+    return (d->m_fileMode & QT_STAT_MASK) == QT_STAT_REG;
 }
 
 QDebug operator<<(QDebug stream, const KFileItem& item)

staging/kio/src/core/udsentry.cpp
@@ -82,7 +82,7 @@ long long UDSEntry::numberValue(uint field, long long defaultValue) const
 
 bool UDSEntry::isDir() const
 {
-    return numberValue(UDS_FILE_TYPE) & QT_STAT_DIR;
+    return (numberValue(UDS_FILE_TYPE) & QT_STAT_MASK) == QT_STAT_DIR;
 }
 
 bool UDSEntry::isLink() const

staging/kio/src/widgets/kurlcompletion.cpp
@@ -1316,7 +1316,7 @@ void KUrlCompletion::postProcessMatch(QString* pMatch) const
 
             QT_STATBUF sbuff;
             if (QT_STAT(file.constData(), &sbuff) == 0) {
-                if (sbuff.st_mode & QT_STAT_DIR)
+                if ((sbuff.st_mode & QT_STAT_MASK) == QT_STAT_DIR)
                     pMatch->append(QLatin1Char('/'));
             } else {
                 //qDebug() << "Could not stat file" << copy;

tier1/karchive/src/k7zip.cpp
@@ -2636,7 +2636,7 @@ bool K7Zip::openArchive( QIODevice::OpenMode mode )
         bool symlink = false;
         if (fileInfo->attributes & FILE_ATTRIBUTE_UNIX_EXTENSION) {
             access = fileInfo->attributes >> 16;
-            if (access & QT_STAT_LNK) {
+            if ((access & QT_STAT_MASK) == QT_STAT_LNK) {
                 symlink = true;
             }
         } else {

tier1/karchive/src/kzip.cpp
@@ -699,7 +699,7 @@ bool KZip::openArchive( QIODevice::OpenMode mode )
             else
             {
             QString symlink;
-        if (access & QT_STAT_LNK) {
+        if ((access & QT_STAT_MASK) == QT_STAT_LNK) {
             symlink = QFile::decodeName(pfi.guessed_symlink);
         }
                 QDateTime mtime = KArchivePrivate::time_tToDateTime(pfi.mtime);

tier1/kcoreaddons/src/lib/io/kdirwatch.cpp
@@ -397,7 +397,7 @@ void KDirWatchPrivate::inotifyEventReceived()
               // so in that case we'll just take both kinds of clients and emit Deleted.
               KDirWatch::WatchModes flag = KDirWatch::WatchSubDirs | KDirWatch::WatchFiles;
               if (QT_STAT(QFile::encodeName(tpath).constData(), &stat_buf) == 0) {
-                bool isDir = stat_buf.st_mode & QT_STAT_DIR;
+                bool isDir = (stat_buf.st_mode & QT_STAT_MASK) == QT_STAT_DIR;
                 flag = isDir ? KDirWatch::WatchSubDirs : KDirWatch::WatchFiles;
               }
               int counter = 0;
@@ -815,11 +815,11 @@ void KDirWatchPrivate::addEntry(KDirWatch* instance, const QString& _path,
   Entry* e = &(*newIt);
 
   if (exists) {
-    e->isDir = stat_buf.st_mode & QT_STAT_DIR;
+    e->isDir = (stat_buf.st_mode & QT_STAT_MASK) == QT_STAT_DIR;
 
     if (e->isDir && !isDir) {
       if (QT_LSTAT(QFile::encodeName(path).constData(), &stat_buf) == 0) {
-        if (stat_buf.st_mode & QT_STAT_LNK)
+        if ((stat_buf.st_mode & QT_STAT_MASK) == QT_STAT_LNK)
           // if it's a symlink, don't follow it
           e->isDir = false;
         else

kio/tests/fileundomanagertest.cpp
@@ -96,9 +96,9 @@ static void createTestSymlink( const QString& path )
         if ( !ok )
             qFatal("couldn't create symlink: %s", strerror(errno));
         QVERIFY( QT_LSTAT( QFile::encodeName( path ), &buf ) == 0 );
-        QVERIFY( buf.st_mode & QT_STAT_LNK );
+        QVERIFY( (buf.st_mode & QT_STAT_MASK) == QT_STAT_LNK );
     } else {
-        QVERIFY( buf.st_mode & QT_STAT_LNK );
+        QVERIFY( (buf.st_mode & QT_STAT_MASK) == QT_STAT_LNK );
     }
     qDebug( "symlink %s created", qPrintable( path ) );
     QVERIFY( QFileInfo( path ).isSymLink() );

kio/tests/kioslavetest.cpp
@@ -362,7 +362,7 @@ void KioslaveTest::printUDSEntry( const KIO::UDSEntry & entry )
                 {
                     mode_t mode = (mode_t)entry.numberValue(*it);
                     qDebug() << "File Type : " << mode;
-                    if ( mode & QT_STAT_DIR )
+                    if ( (mode & QT_STAT_MASK) == QT_STAT_DIR )
                     {
                         qDebug() << "is a dir";
                     }

kio/tests/kiotesthelper.h
@@ -81,7 +81,7 @@ static void createTestSymlink( const QString& path, const QByteArray& target = "
         qFatal("couldn't create symlink: %s", strerror(errno));
     QT_STATBUF buf;
     QVERIFY( QT_LSTAT( QFile::encodeName( path ), &buf ) == 0 );
-    QVERIFY( buf.st_mode & QT_STAT_LNK );
+    QVERIFY( (buf.st_mode & QT_STAT_MASK) == QT_STAT_LNK );
     //qDebug( "symlink %s created", qPrintable( path ) );
     QVERIFY( QFileInfo( path ).isSymLink() );
 }