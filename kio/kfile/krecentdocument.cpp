KDE/kdelibs

commit 48b78ce3e16d99be8d8ff9070eab1b145c574306

Ensure desktop files created by KRecentDocuments have a proper filename, not just ".desktop".
Example: https://codereview.qt-project.org/#q,60491,n,z has no filename.

kio/kfile/krecentdocument.cpp
@@ -107,9 +107,10 @@ void KRecentDocument::add(const KUrl& url, const QString& desktopEntryName)
     if(!useRecent || maxEntries <= 0)
         return;
 
-    QString path = recentDocumentDirectory();
-
-    QString dStr = path + url.fileName();
+    const QString path = recentDocumentDirectory();
+    const QString fileName = url.fileName();
+    // don't create a file called ".desktop", it will lead to an empty name in kio_recentdocuments
+    const QString dStr = fileName.isEmpty() ? QString("unnamed") : fileName;
 
     QString ddesktop = dStr + QLatin1String(".desktop");
 

commit 13f0e65f8b0bbee8fa32d7634743026a518903c1

krecentdocument: Ensure full path is saved with entries.
This was accidentally broken in commit 48b78ce3e, which resulted in only
the filename (not the full path) being saved with KRecentDocument
entries.

The suggested fix in the reported bug is not adequate as 'dStr' can be
used later in the function, so it needs to retain the semantics it had
prior to commit 48b78ce3e. We do appreciate the prompt report though!.

BUG:323747
FIXED-IN:4.11.1

kio/kfile/krecentdocument.cpp
@@ -110,7 +110,7 @@ void KRecentDocument::add(const KUrl& url, const QString& desktopEntryName)
     const QString path = recentDocumentDirectory();
     const QString fileName = url.fileName();
     // don't create a file called ".desktop", it will lead to an empty name in kio_recentdocuments
-    const QString dStr = fileName.isEmpty() ? QString("unnamed") : fileName;
+    const QString dStr = path + (fileName.isEmpty() ? QString("unnamed") : fileName);
 
     QString ddesktop = dStr + QLatin1String(".desktop");
 