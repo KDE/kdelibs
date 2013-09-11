f3700f5e16973d2be3cbb6562c3159cd6f30de83
Sebastian Kügler <sebas@kde.org>
2013-09-11 02:17:10 +0200

    Fix date

--- staging/kservice/autotests/pluginlocatortest.h
+++ staging/kservice/autotests/pluginlocatortest.h
@@ -1,5 +1,5 @@
 /******************************************************************************
-*   Copyright 2012 Sebastian Kügler <sebas@kde.org>                           *
+*   Copyright 2013 Sebastian Kügler <sebas@kde.org>                           *
 *                                                                             *
 *   This library is free software; you can redistribute it and/or             *
 *   modify it under the terms of the GNU Library General Public               *
