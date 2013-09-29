20c7cb92f484cf74ed9b7535a7dd74bf2c25876b
David Faure <faure@kde.org>
2013-09-29 18:52:07 +0200

    Delete libkdeqt5staging. This commit can be reverted if we ever need it again.

--- libkdeqt5staging/src/update_qcommandline.sh
+++
@@ -1,4 +0,0 @@
-cp -f /d/qt/5/qtbase-cli_parser_rebased/src/corelib/tools/qcommand* .
-perl -pi -e 's/Q_CORE_EXPORT/KDEQT5STAGING_EXPORT/g' qc*.h
-perl -pi -e 's/QtCore\///' qc*.h
-perl -pi -e '$_ = "#include \"kdeqt5staging_export.h\"\n" . $_ if (/qstringlist.h/)' qcommandlineoption.h
