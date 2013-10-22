a01d537858bc964b4500d852407b97801bb93287
David Faure <faure@kde.org>
2013-10-22 11:55:27 +0200

    Move KFileDialog-based tests to kde4support

--- staging/kde4support/tests/kfstest.cpp
+++ staging/kde4support/tests/kfstest.cpp
@@ -38,8 +38,39 @@
 #include <QDebug>
 #include <kicondialog.h>
 
-#include "kfdtest.h"
+#include "kfstest.h"
 #include <kconfiggroup.h>
+
+#include <QtCore/QMutableStringListIterator>
+#include <kfiledialog.h>
+#include <kmessagebox.h>
+#include <QtCore/QTimer>
+
+KFDTest::KFDTest( const QUrl& startDir, QObject *parent, const char *name )
+    : QObject( parent ),
+      m_startDir( startDir )
+{
+    setObjectName( name );
+    QTimer::singleShot( 1000, this, SLOT(doit()));
+}
+
+void KFDTest::doit()
+{
+    KFileDialog *dlg = new KFileDialog( m_startDir, QString(), 0L);
+    dlg->setMode( KFile::File);
+    dlg->setOperationMode( KFileDialog::Saving );
+    QStringList filter;
+    filter << "all/allfiles" << "text/plain";
+    dlg->setMimeFilter( filter, "all/allfiles" );
+
+    if ( dlg->exec() == QDialog::Accepted )
+    {
+        KMessageBox::information(0, QString::fromLatin1("You selected the file: %1").arg(dlg->selectedUrl().toString()));
+    }
+
+//     qApp->quit();
+}
+
 
 int main(int argc, char **argv)
 {
