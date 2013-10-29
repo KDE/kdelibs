KDE/kdelibs

commit 139a09daf188129c470b6b3f49d7720e6a2b71b9

Trivial warning fixes
Fixes for warnings (almost entirely unused variables or parameters)
where the correct fix was obvious.

REVIEW: 113378

khtml/src/rendering/render_canvasimage.cpp
@@ -72,12 +72,12 @@ void RenderCanvasImage::paint(PaintInfo& i, int _tx, int _ty)
         return;
 
     bool isPrinting = (i.p->device()->devType() == QInternal::Printer);
-    bool drawSelectionTint = (selectionState() != SelectionNone) && !isPrinting;
+    //bool drawSelectionTint = (selectionState() != SelectionNone) && !isPrinting;
     if (i.phase == PaintActionSelection) {
         if (selectionState() == SelectionNone) {
             return;
         }
-        drawSelectionTint = false;
+        //drawSelectionTint = false;
     }
 
     int cWidth = contentWidth();

tier1/itemmodels/src/kselectionproxymodel.cpp
@@ -1271,6 +1271,7 @@ QModelIndex KSelectionProxyModelPrivate::mapParentFromSource(const QModelIndex &
 
 static bool indexIsValid(bool startWithChildTrees, int row, const QList<QPersistentModelIndex> &rootIndexList, const SourceIndexProxyRowMapping &mappedFirstChildren)
 {
+#ifndef QT_NO_DEBUG
     if (!startWithChildTrees) {
         Q_ASSERT(rootIndexList.size() > row);
         Q_UNUSED(rootIndexList);
@@ -1288,6 +1289,7 @@ static bool indexIsValid(bool startWithChildTrees, int row, const QList<QPersist
         Q_ASSERT(sourceFirstChild.parent().isValid());
         Q_ASSERT(row <= proxyFirstRow + sourceFirstChild.model()->rowCount(sourceFirstChild.parent()));
     }
+#endif
     return true;
 }
 
@@ -1499,6 +1501,7 @@ void KSelectionProxyModelPrivate::updateInternalIndexes(const QModelIndex &paren
 bool KSelectionProxyModelPrivate::parentAlreadyMapped(const QModelIndex &parent) const
 {
     Q_Q(const KSelectionProxyModel);
+    Q_UNUSED(q) // except in Q_ASSERT
     Q_ASSERT(parent.model() == q->sourceModel());
     return m_mappedParents.leftContains(parent);
 }
@@ -1506,6 +1509,7 @@ bool KSelectionProxyModelPrivate::parentAlreadyMapped(const QModelIndex &parent)
 bool KSelectionProxyModelPrivate::firstChildAlreadyMapped(const QModelIndex &firstChild) const
 {
     Q_Q(const KSelectionProxyModel);
+    Q_UNUSED(q) // except in Q_ASSERT
     Q_ASSERT(firstChild.model() == q->sourceModel());
     return m_mappedFirstChildren.leftContains(firstChild);
 }

interfaces/kimproxy/library/kimproxy.cpp
@@ -575,7 +575,7 @@ void KIMProxy::pollAll( const QString &uid )
 void KIMProxy::pollApp( const QString & appId )
 {
 	//qDebug() ;
-	OrgKdeKIMInterface * appStub = m_im_client_stubs.value( appId );
+	//OrgKdeKIMInterface * appStub = m_im_client_stubs.value( appId );
 	QStringList contacts = m_im_client_stubs.value( appId )->allContacts();
 	QStringList::iterator it = contacts.begin();
 	QStringList::iterator end = contacts.end();

khtml/src/css/css_ruleimpl.cpp
@@ -76,7 +76,8 @@ CSSFontFaceRuleImpl::~CSSFontFaceRuleImpl()
 void CSSFontFaceRuleImpl::setDeclaration( CSSStyleDeclarationImpl* decl)
 {
     assert(!m_style);
-    if (m_style = decl)
+    m_style = decl;
+    if (m_style)
         m_style->ref();
 }
 

khtml/src/css/css_webfont.cpp
@@ -495,8 +495,6 @@ void CSSFontSelector::addFontFaceRule(const CSSFontFaceRuleImpl* fontFaceRule)
 
     int srcLength = srcList->length();
 
-    bool foundLocal = false;
-
 #if 0
     // ENABLE(SVG_FONTS)
     bool foundSVGFont = false;
@@ -522,7 +520,6 @@ void CSSFontSelector::addFontFaceRule(const CSSFontFaceRuleImpl* fontFaceRule)
             }
         } else {
             source = new CSSFontFaceSource(item->resource());
-            foundLocal = true;
         }
 
         if (!fontFace)

khtml/src/css/cssstyleselector.cpp
@@ -3696,6 +3696,7 @@ void CSSStyleSelector::applyRule( int id, DOM::CSSValueImpl *value )
                         break;
                     default:
                         assert(false);
+                        quote = NO_QUOTE;
                 }
                 style->addContent(quote);
             } else {

khtml/src/ecma/kjs_clientrect.cpp
@@ -43,7 +43,7 @@ ClientRect::ClientRect(ExecState* /*exec*/, float left, float top, float width, 
 {
 }
 
-ClientRect::ClientRect(ExecState* exec, const QRectF& rect)
+ClientRect::ClientRect(ExecState* /*exec*/, const QRectF& rect)
     : JSObject(),
       m_rect(rect)
 {

khtml/src/ecma/kjs_events.cpp
@@ -1134,7 +1134,7 @@ bool DOMHashChangeEvent::getOwnPropertySlot(ExecState *exec, const Identifier& p
   return getStaticValueSlot<DOMHashChangeEvent, DOMEvent>(exec,&DOMHashChangeEventTable,this,propertyName,slot);
 }
 
-JSValue *DOMHashChangeEvent::getValueProperty(ExecState *exec, int token) const
+JSValue *DOMHashChangeEvent::getValueProperty(ExecState * /*exec*/, int token) const
 {
   DOM::HashChangeEventImpl& event = *impl();
   switch (token) {

commit 73048bfeb8df80146975561c9100d40ccd13cd1d

Fix a couple of warnings inadvertentently caused by 139a09da
REVIEW: 113430

khtml/src/rendering/render_canvasimage.cpp
@@ -71,7 +71,7 @@ void RenderCanvasImage::paint(PaintInfo& i, int _tx, int _ty)
     if (i.phase != PaintActionForeground && i.phase != PaintActionSelection)
         return;
 
-    bool isPrinting = (i.p->device()->devType() == QInternal::Printer);
+    //bool isPrinting = (i.p->device()->devType() == QInternal::Printer);
     //bool drawSelectionTint = (selectionState() != SelectionNone) && !isPrinting;
     if (i.phase == PaintActionSelection) {
         if (selectionState() == SelectionNone) {

tier1/itemmodels/src/kselectionproxymodel.cpp
@@ -1274,7 +1274,6 @@ static bool indexIsValid(bool startWithChildTrees, int row, const QList<QPersist
 #ifndef QT_NO_DEBUG
     if (!startWithChildTrees) {
         Q_ASSERT(rootIndexList.size() > row);
-        Q_UNUSED(rootIndexList);
     } else {
 
         Q_ASSERT(!mappedFirstChildren.isEmpty());
@@ -1289,6 +1288,11 @@ static bool indexIsValid(bool startWithChildTrees, int row, const QList<QPersist
         Q_ASSERT(sourceFirstChild.parent().isValid());
         Q_ASSERT(row <= proxyFirstRow + sourceFirstChild.model()->rowCount(sourceFirstChild.parent()));
     }
+#else
+    Q_UNUSED(startWithChildTrees)
+    Q_UNUSED(row)
+    Q_UNUSED(rootIndexList)
+    Q_UNUSED(mappedFirstChildren)
 #endif
     return true;
 }