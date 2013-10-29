55b8d2ced8fc30d7775af5e11fc198b12d3f13d0
Kevin Ottens <ervin@kde.org>
2013-10-29 08:57:01 +0100

    Move kdewidgets to Tier 3 + Tier 4
    The parts linking to KDE4Support moved there, the rest is in Tier 3.

--- tier3/kdewidgets/KDEWidgetsMacros.cmake
+++ tier3/kdewidgets/KDEWidgetsMacros.cmake
@@ -16,8 +16,23 @@
 #    for Qt Designer plugins.
 #
 
+if (WIN32)
+    # CMAKE_CFG_INTDIR is the output subdirectory created e.g. by XCode and MSVC
+    if (NOT WINCE)
+        set(KDEWIDGETS_MAKEKDEWIDGETS_EXECUTABLE          ${EXECUTABLE_OUTPUT_PATH}/${CMAKE_CFG_INTDIR}/makekdewidgets )
+    else (NOT WINCE)
+        set(KDEWIDGETS_MAKEKDEWIDGETS_EXECUTABLE          ${HOST_BINDIR}/${CMAKE_CFG_INTDIR}/makekdewidgets )
+    endif(NOT WINCE)
+
+    set(KDEWIDGETS_MAKEKDEWIDGETS_EXECUTABLE          ${EXECUTABLE_OUTPUT_PATH}/${CMAKE_CFG_INTDIR}/makekdewidgets )
+else (WIN32)
+    set(KDEWIDGETS_MAKEKDEWIDGETS_EXECUTABLE          ${CMAKE_BINARY_DIR}/tier3/kdewidgets/src/makekdewidgets${CMAKE_EXECUTABLE_SUFFIX} )
+endif (WIN32)
+
+set( _KDEWIDGETS_MAKEKDEWIDGETS_EXECUTABLE_DEP makekdewidgets)
+
 macro (KDEWIDGETS_ADD_WIDGET_FILES _sources)
-   foreach (_current_FILE ${ARGN})
+   foreach (_current_FILE ${_sources})
 
       get_filename_component(_input ${_current_FILE} ABSOLUTE)
       get_filename_component(_basename ${_input} NAME_WE)
@@ -28,7 +43,7 @@ macro (KDEWIDGETS_ADD_WIDGET_FILES _sources)
       add_custom_command(OUTPUT ${_source}
         COMMAND ${KDEWIDGETS_MAKEKDEWIDGETS_EXECUTABLE}
         ARGS -o ${_source} ${_input}
-        MAIN_DEPENDENCY ${_input} DEPENDS ${_KDEWIDGETS_MAKEKDEWIDGETS_DEP})
+        MAIN_DEPENDENCY ${_input} DEPENDS ${_KDEWIDGETS_MAKEKDEWIDGETS_EXECUTABLE_DEP})
 
       # create moc file
       qt5_generate_moc(${_source} ${_moc} )
