f5c87f8fc93df462551a8702f372095935a25770
Alex Merry <kde@randomguy3.me.uk>
2013-11-02 15:34:48 +0000

    UDisks2 backend: Ignore propertiesChanged signal for generic interfaces
    We only store properties for UDisks2-specific interfaces, so we should
    only listen to updates to properties on those interfaces.
    
    REVIEW: 113713

--- tier1/solid/src/solid/backends/udisks2/udisksdevicebackend.cpp
+++ tier1/solid/src/solid/backends/udisks2/udisksdevicebackend.cpp
@@ -195,6 +195,9 @@ void DeviceBackend::checkCache(const QString& key) const
 
 void DeviceBackend::slotPropertiesChanged(const QString& ifaceName, const QVariantMap& changedProps, const QStringList& invalidatedProps)
 {
+    if (!ifaceName.startsWith(UD2_DBUS_SERVICE)) {
+        return;
+    }
     //qDebug() << m_udi << "'s interface" << ifaceName << "changed props:";
 
     QMap<QString, int> changeMap;
