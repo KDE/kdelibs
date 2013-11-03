a8c1501636f2239b0cea9ff89a5c8082625c7bc5
David Faure <faure@kde.org>
2013-11-03 10:25:47 +0100

    Move kpac to KIO (the kded module is called by kprotocolmanager)

--- staging/kio/src/kpac/ConfigureChecks.cmake
+++ staging/kio/src/kpac/ConfigureChecks.cmake
@@ -1,6 +1,5 @@
 include(CheckIncludeFiles)
 include(CheckLibraryExists)
-include(CheckPrototypeExists)
 include(CheckLibraryExists)
 
 check_include_files(sys/param.h   HAVE_SYS_PARAM_H)
@@ -10,11 +9,9 @@ check_include_files(arpa/nameser8_compat.h HAVE_ARPA_NAMESER8_COMPAT_H)
 check_include_files("sys/types.h;netinet/in.h"  HAVE_NETINET_IN_H)
 check_include_files(stdint.h      HAVE_STDINT_H) 
 
-check_prototype_exists(res_init "sys/types.h;netinet/in.h;arpa/nameser.h;resolv.h" HAVE_RES_INIT_PROTO)
-
 # Check for libresolv
 # e.g. on slackware 9.1 res_init() is only a define for __res_init, so we check both, Alex
-set(HAVE_RESOLV_LIBRARY FALSE)                                        
+set(HAVE_RESOLV_LIBRARY FALSE)
 check_library_exists(resolv res_init "" HAVE_RES_INIT_IN_RESOLV_LIBRARY)
 check_library_exists(resolv __res_init "" HAVE___RES_INIT_IN_RESOLV_LIBRARY)
 if (HAVE___RES_INIT_IN_RESOLV_LIBRARY OR HAVE_RES_INIT_IN_RESOLV_LIBRARY)
