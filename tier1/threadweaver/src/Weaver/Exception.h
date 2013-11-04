725089be52cd5e8fdf98796fef8476816d3fdf8b
Mirko Boehm (Endocode) <mirko@endocode.com>
2013-11-04 18:24:41 +0100

    Avoid QString::null.

--- tier1/threadweaver/src/Weaver/Exception.h
+++ tier1/threadweaver/src/Weaver/Exception.h
@@ -12,7 +12,7 @@ namespace ThreadWeaver {
 class THREADWEAVER_EXPORT Exception : public std::runtime_error
 {
 public:
-    explicit Exception(const QString& message = QString::null);
+    explicit Exception(const QString& message = QString());
     ~Exception() throw();
     QString message() const;
 
@@ -23,13 +23,13 @@ private:
 class THREADWEAVER_EXPORT JobAborted : public Exception
 {
 public:
-    explicit JobAborted(const QString& message = QString::null);
+    explicit JobAborted(const QString& message = QString());
 };
 
 class THREADWEAVER_EXPORT JobFailed : public Exception
 {
 public:
-    explicit JobFailed(const QString& message = QString::null);
+    explicit JobFailed(const QString& message = QString());
 };
 
 }
