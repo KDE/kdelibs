ea7bf8a06b35048b0bd0164c6a28fa24ec4553d2
David Faure <faure@kde.org>
2013-11-05 09:39:31 +0100

    Move kio's Messages.sh and adapt it (we probably have a lot more of these to do...)
    Remove kio subdir.
    kio is dead, long live KIO :)

--- kio/Messages.sh
+++
@@ -1,3 +0,0 @@
-#! /usr/bin/env bash
-$EXTRACTRC `find . ../kioslave ../kfile -name \*.rc -o -name \*.ui -o -name \*.ui3 -o -name \*.ui4` >> rc.cpp || exit 11
-$XGETTEXT `find . ../kioslave ../kfile -name "*.cpp" -o -name "*.cc" -o -name "*.h" | grep -v "/tests"` -o $podir/kio4.pot
