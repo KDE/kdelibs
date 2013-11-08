7272f67d562a2d59fea6af50e7e2ce8ce8642bc3
Martin Gräßlin <mgraesslin@kde.org>
2013-11-08 10:22:22 +0100

    Unbreak build of kauth with latest Qt stable
    Tracing got removed again in stable branch as of
    git commit 79b975756a100cc46182c2949e71a683a656bd12 with
    Change-Id If97340c37b8b3363f597683336a8390d5ff386f1

--- tier2/kauth/src/backends/dbus/DBusHelperProxy.cpp
+++ tier2/kauth/src/backends/dbus/DBusHelperProxy.cpp
@@ -319,9 +319,6 @@ void debugMessageReceived(int t, const QString &message)
 {
     QtMsgType type = (QtMsgType)t;
     switch (type) {
-    case QtTraceMsg:
-        qDebug("Trace message from helper: %s", message.toLatin1().data());
-        break;
     case QtDebugMsg:
         qDebug("Debug message from helper: %s", message.toLatin1().data());
         break;
