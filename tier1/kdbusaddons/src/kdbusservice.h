1c25b415863adeacf8fda5d817ead73e09005bab
Alex Merry <kde@randomguy3.me.uk>
2013-11-11 16:27:32 +0000

    Improve KDBusAddons API docs
    REVIEW: 113816

--- tier1/kdbusaddons/src/kdbusservice.h
+++ tier1/kdbusaddons/src/kdbusservice.h
@@ -31,27 +31,39 @@
 class KDBusServicePrivate;
 
 /**
- * KDBusService takes care of registering the current process with DBus.
+ * KDBusService takes care of registering the current process with D-Bus.
  *
- * This is typically done in the main() function.
- * An application can work in one of two modes:
- * <ul>
- *   <li>Multiple: the application can be launched many times. To allow this,
- *    the service name in the DBus registration contains the PID. Example:
- *      org.kde.konqueror-12345.
- *   <li>Unique: only one instance of this application can ever run. The application
- *   registers to DBus without PID, and any attempt to run the application again will
- *   call a DBus method called "Activate" in the running instance, and then quit.
- *   To implement this method, inherit from KDBusService and reimplement
- *       virtual int Activate();
- *   This slot usually raises the main window of the application.
- * </ul>
+ * This registers the application at a predictable location on D-Bus, registers
+ * the QCoreApplication (or subclass) object at /MainApplication, and
+ * assists in implementing the application side of D-Bus activation from
+ * the <a
+ * href="http://standards.freedesktop.org/desktop-entry-spec/desktop-entry-spec-latest.html">Desktop
+ * Entry Specification</a>.
  *
- * Important: in order to avoid a race, the application should try to export its
- * objects to DBus before instanciating KDBusService.
- * Otherwise the application appears on the bus before its objects are accessible
- * via DBus, which could be a problem for other apps or scripts which start the
- * application in order to talk DBus to it immediately.
+ * An application can either work in Multiple mode or Unique mode.
+ *
+ * In Multiple mode, the application can be launched many times.  The service
+ * name in the D-Bus registration will contain the PID to distinguish the
+ * various instances; for example: <tt>org.kde.konqueror-12345</tt>.
+ *
+ * In Unique mode, only one instance of this application can ever run.
+ * The first instance of the application registers with D-Bus without the PID,
+ * and any attempt to run the application again will call the D-Bus method
+ * "Activate" in the running instance, and then quit.
+ *
+ * In order to allow for activation over D-Bus, applications should connect to
+ * the signals emitted by this class.  In particular, the activateRequested()
+ * signal should normally be connected to a slot that raises the main window of
+ * the application.  This is particularly important for Unique-mode
+ * applications, but the activation interface will also be exported to D-Bus for
+ * Multiple-mode applications.
+ *
+ * @note In order to avoid a race, the application should export its objects to
+ * D-Bus before allowing the event loop to run (for example, by calling
+ * QCoreApplication::exec()).  Otherwise, the application will appear on the bus
+ * before its objects are accessible via D-Bus, which could be a problem for
+ * other applications or scripts which start the application in order to talk
+ * D-Bus to it immediately.
  *
  * Example usage:
  *
@@ -72,46 +84,71 @@ class KDBUSADDONS_EXPORT KDBusService : public QObject
     Q_OBJECT
     Q_ENUMS(StartupOption)
     Q_FLAGS(StartupOptions)
-    //Q_CLASSINFO("D-Bus Interface", "org.freedesktop.Application")
 
 public:
+    /** Options to control the behaviour of KDBusService */
     enum StartupOption {
+        /** Indicates that only one instance of this application should ever
+         * exist.
+         *
+         * Cannot be combined with @c Multiple.
+         */
         Unique = 1,
+        /** Indicates that multiple instances of the application may exist.
+         *
+         * Cannot be combined with @c Unique.  This is the default.
+         */
         Multiple = 2,
+        /** Indicates that the application should not exit if it failed to
+         * register with D-Bus.
+         *
+         * By default, KDBusService will quit the application if it failed to
+         * register the service with D-Bus (with error code @c 1).
+         */
         NoExitOnFailure = 4
     };
 
     Q_DECLARE_FLAGS(StartupOptions, StartupOption)
 
     /**
-     * Constructor. Registers the current process to DBus, using the given options.
-     * The DBus service name is determined from the organization domain (reversed)
-     * and the application name of the QApplication instance. For instance:
+     * Registers the current process to D-Bus at an address based on the
+     * application name and organization domain.
+     *
+     * The DBus service name is the reversed organization domain, followed by
+     * the application name.  If @p options includes the @c Multiple flag, the
+     * application PID will be appended.  For example,
+     * @code
      * app.setApplicationName("kuiserver");
      * app.setOrganizationDomain("kde.org");
-     * will make KDBusService register as org.kde.kuiserver.
+     * @endcode
+     * will make KDBusService register as @c org.kde.kuiserver in @c Unique
+     * mode, and @c org.kde.kuiserver-1234 (if the process has PID @c 1234) in
+     * @c Multiple mode.
      */
     explicit KDBusService(StartupOptions options = Multiple, QObject *parent = 0);
 
     /**
-     * Destructor. Does not de-register from DBus. Could be added, though.
+     * Destroys this object (but does not unregister the application).
      *
-     * KDBusService de-registers when the application is about to quit, though, to ensure
-     * it doesn't receive calls anymore after that point.
+     * Deleting this object before unregister() is called (either manually or
+     * because QCoreApplication::aboutToQuit() was emitted) could confuse
+     * clients, who will see the service on the bus but will be unable to use
+     * the activation methods.
      */
     ~KDBusService();
 
     /**
-     * Returns true if the DBus registration succeeded.
+     * Returns true if the D-Bus registration succeeded.
      *
      * Note that this is only useful when specifying the option NoExitOnFailure.
-     * Otherwise the simple fact that this process is still running, indicates
-     * that the registration succeeded, since KDBusService quits on failure, by default.
+     * Otherwise, the simple fact that this process is still running indicates
+     * that the registration succeeded.
      */
     bool isRegistered() const;
 
     /**
-     * Returns the error message from the DBus registration, in case it failed.
+     * Returns the error message from the D-Bus registration if it failed.
+     *
      * Note that this is only useful when specifying the option NoExitOnFailure.
      * Otherwise the process has quit by the time you can get a chance to call this.
      */
@@ -119,32 +156,38 @@ public:
 
 Q_SIGNALS:
     /**
-     * This method is called when a Unique application is launched while it is
-     * already running. In single-windows applications, this method would typically
+     * Signals that the application is to be activated.
+     *
+     * In a @c Unique application, this signal will typically be emitted in the
+     * first instance of the application to be run when a second instance is
+     * run.
+     *
+     * In single-window applications, the connected signal should typically
      * raise the window.
-     * The default implementation does nothing.
      */
     void activateRequested();
 
     /**
-     * Opens one or more files in the application.
-     * @param uris The URLs of the files to open.
+     * Signals that one or more files should be opened in the application.
+     *
+     * @param uris  The URLs of the files to open.
      */
     void openRequested(const QList<QUrl> &uris);
 
     /**
-     * This method is called when a Unique application is launched while it is
-     * already running, and the user selected a specific application action to trigger.
-     * The default implementation does nothing.
+     * Signals that an application action should be triggered.
+     *
+     * See the desktop entry specification for more information.
      */
     void activateActionRequested(const QString &actionName, const QVariant& parameter);
 
 public Q_SLOTS:
     /**
-     * Unregister from DBus.
-     * This is called automatically when the application is about to quit,
-     * to make sure it doesn't keep receiving calls to Activate while it's
-     * doing final cleanups.
+     * Unregister from D-Bus.
+     *
+     * This is called automatically when the application is about to quit, to
+     * make sure it doesn't keep receiving calls to its D-Bus interface while it
+     * is doing final cleanups.
      */
     void unregister();
 
