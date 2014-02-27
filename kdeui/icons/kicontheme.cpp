d3d3ee9b4a50a0738edcbf6abcfac07f99e45d61
David Faure <faure@kde.org>
2014-02-27 22:38:34 +0100

    No point in parsing kdeglobals while parsing e.g. oxygen/index.theme
    Backport from http://commits.kde.org/kiconthemes/a890e9b05982e600f7b449820daa45825943d8ed

--- kdeui/icons/kicontheme.cpp
+++ kdeui/icons/kicontheme.cpp
@@ -184,7 +184,7 @@ KIconTheme::KIconTheme(const QString& name, const QString& appName)
 
     // Use KSharedConfig to avoid parsing the file many times, from each kinstance.
     // Need to keep a ref to it to make this useful
-    d->sharedConfig = KSharedConfig::openConfig(fileName);
+    d->sharedConfig = KSharedConfig::openConfig(fileName, KConfig::NoGlobals);
 
     KConfigGroup cfg(d->sharedConfig, mainSection);
     d->mName = cfg.readEntry("Name");
