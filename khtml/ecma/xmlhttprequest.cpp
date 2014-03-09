fa2eb64702b17d328689d88fa8b82b50580ab435
Andrea Iacovitti <aiacovitti@libero.it>
2014-03-09 20:18:02 +0100

    Remove this code. It makes no sense to call open if setting custom header name to "get" or "post". Setting custom "Referer" header is forbidden and anyhow overridden in XMLHttpRequest::send.

--- khtml/ecma/xmlhttprequest.cpp
+++ khtml/ecma/xmlhttprequest.cpp
@@ -622,28 +622,9 @@ void XMLHttpRequest::setRequestHeader(const QString& _name, const QString& _valu
     return;
   }
 
-  // Sanitize the referrer header to protect against spoofing...
-  if(name == "referer") {
-    KUrl referrerURL(value);
-    if (urlMatchesDocumentDomain(referrerURL))
-      m_requestHeaders[name] = referrerURL.url();
-    return;
-  }
-
-  // Sanitize the request headers below and handle them as if they are
-  // calls to open. Otherwise, we will end up ignoring them all together!
-  // TODO: Do something about "put" which kio_http sort of supports and
-  // the webDAV headers such as PROPFIND etc...
-  if (name == "get"  || name == "post") {
-    KUrl reqURL(doc->URL(), value);
-    open(name, reqURL, async, ec);
-    return;
-  }
-
   // Reject all banned headers.
   if (!canSetRequestHeader(_name)) {
-      kWarning(6070) << "Refusing to set unsafe XMLHttpRequest header "
-                     << name << endl;
+      kWarning(6070) << "Refusing to set unsafe XMLHttpRequest header" << _name;
       return;
   }
 
