KDE/kdelibs

commit 52ee764212613012f1f6bc5924dd7d7b4c5a531a

fix icon generation and installation on osx REVIEW: 107752

cmake/modules/KDE4Macros.cmake
@@ -818,7 +818,13 @@ macro (KDE4_ADD_KDEINIT_EXECUTABLE _target_NAME )
 
       set_target_properties(kdeinit_${_target_NAME} PROPERTIES OUTPUT_NAME kdeinit4_${_target_NAME})
 
-      kde4_add_executable(${_target_NAME} "${_nogui}" ${CMAKE_CURRENT_BINARY_DIR}/${_target_NAME}_dummy.cpp)
+      if (Q_WS_MAC)
+	      list(FIND _SRCS *.icns _icon_position)
+	      if(NOT _res_position EQUAL -1)
+		      list(GET _SRCS ${_icon_position} _resourcefile)
+	      endif(NOT _res_position EQUAL -1)
+      endif (Q_WS_MAC)
+      kde4_add_executable(${_target_NAME} "${_nogui}" ${CMAKE_CURRENT_BINARY_DIR}/${_target_NAME}_dummy.cpp ${_resourcefile})
       target_link_libraries(${_target_NAME} kdeinit_${_target_NAME})
    endif(WIN32)
 
@@ -1201,28 +1207,47 @@ macro (KDE4_ADD_APP_ICON appsources pattern)
         endif(PNG2ICO_EXECUTABLE)
     endif(WIN32)
     if (Q_WS_MAC)
-        # first convert image to a tiff using the Mac OS X "sips" utility,
-        # then use tiff2icns to convert to an icon
-        find_program(SIPS_EXECUTABLE NAMES sips)
-        find_program(TIFF2ICNS_EXECUTABLE NAMES tiff2icns)
-        if (SIPS_EXECUTABLE AND TIFF2ICNS_EXECUTABLE)
-            file(GLOB_RECURSE files  "${pattern}")
-            # we can only test for the 128-icon like that - we don't use patterns anymore
-            foreach (it ${files})
-                if (it MATCHES ".*128.*" )
-                    set (_icon ${it})
-                endif (it MATCHES ".*128.*")
-            endforeach (it)
+        # first generate .iconset directory structure, then convert to .icns format using the Mac OS X "iconutil" utility,
+        # to create retina compatible icon, you need png source files in pixel resolution 16x16, 32x32, 64x64, 128x128,
+	# 256x256, 512x512, 1024x1024
+	find_program(ICONUTIL_EXECUTABLE NAMES iconutil)
+	if (ICONUTIL_EXECUTABLE)
+	    file(GLOB_RECURSE files  "${pattern}")
+	    add_custom_command(OUTPUT ${_outfilename}.iconset
+	                       COMMAND ${CMAKE_COMMAND} -E make_directory ${_outfilename}.iconset
+		               DEPENDS ${files}
+                              )
+	    set(_icons)
+	    macro(copy_icons _pattern output)
+	        foreach(it ${files})
+		    if(it MATCHES ${_pattern})
+		        add_custom_command(OUTPUT ${_outfilename}.iconset/icon_${output}.png
+				           COMMAND ${CMAKE_COMMAND} -E copy ${it} icon_${output}.png
+				           WORKING_DIRECTORY ${_outfilename}.iconset
+				           DEPENDS ${_outfilename}.iconset
+				          )
+	                list(APPEND _icons ${_outfilename}.iconset/icon_${output}.png)
+	            endif(it MATCHES ${_pattern})
+		endforeach(it ${files})
+            endmacro(copy_icons)
 
-            if (_icon)
-                
-                # first, get the basename of our app icon
-                add_custom_command(OUTPUT ${_outfilename}.icns ${outfilename}.tiff
-                                   COMMAND ${SIPS_EXECUTABLE} -s format tiff ${_icon} --out ${outfilename}.tiff
-                                   COMMAND ${TIFF2ICNS_EXECUTABLE} ${outfilename}.tiff ${_outfilename}.icns
-                                   DEPENDS ${_icon}
-                                   )
-
+            copy_icons(".*16.*" "16x16")
+            copy_icons(".*32.*" "16x16@2x")
+            copy_icons(".*32.*" "32x32")
+            copy_icons(".*64.*" "32x32@2x")
+            copy_icons(".*128.*" "128x128")
+            copy_icons(".*256.*" "128x128@2x")
+            copy_icons(".*256.*" "256x256")
+            copy_icons(".*512.*" "256x256@2x")
+            copy_icons(".*512.*" "512x512")
+            copy_icons(".*1024.*" "512x512@2x")
+	    if (_icons)
+                # generate .icns icon file
+                add_custom_command(OUTPUT ${_outfilename}.icns
+			           COMMAND ${ICONUTIL_EXECUTABLE} --convert icns
+				           --output ${_outfilename}.icns ${_outfilename}.iconset
+                                   DEPENDS ${_icons}
+                                  )
                 # This will register the icon into the bundle
                 set(MACOSX_BUNDLE_ICON_FILE ${appsources}.icns)
 
@@ -1232,15 +1257,15 @@ macro (KDE4_ADD_APP_ICON appsources pattern)
 
                 # Install the icon into the Resources dir in the bundle
                 set_source_files_properties(${_outfilename}.icns PROPERTIES MACOSX_PACKAGE_LOCATION Resources)
-
-            else(_icon)
-                # TODO - try to scale a non-128 icon...? Try to convert an SVG on the fly?
-                message(STATUS "Unable to find an 128x128 icon that matches pattern ${pattern} for variable ${appsources} - application will not have an application icon!")
-            endif(_icon)
-
-        else(SIPS_EXECUTABLE AND TIFF2ICNS_EXECUTABLE)
-            message(STATUS "Unable to find the sips and tiff2icns utilities - application will not have an application icon!")
-        endif(SIPS_EXECUTABLE AND TIFF2ICNS_EXECUTABLE)
+            else(_icons)
+                message(STATUS "Unable to find an icon that matches pattern ${pattern}
+		        for variable ${appsources} - application will not have an
+			application icon!
+		    ")
+            endif(_icons)
+        else(ICONUTIL_EXECUTABLE)
+            message(STATUS "Unable to find the iconutil utility - application will not have an application icon!")
+        endif(ICONUTIL_EXECUTABLE)
     endif(Q_WS_MAC)
 endmacro (KDE4_ADD_APP_ICON)
 

commit bdb672c5af44f5715a5bdf81193b9021ecd9f07a

KDE4_ADD_KDEINIT_EXECUTABLE: Check the right variable for Mac.
Fix a regression introduced by 52ee7642, which copy-pasted some code
from the Windows block. It caused the executable created with this macro
to unconditionally build and depend on the last source file passed to
it, even though it's only supposed to be part of the kdeinit_$TARGET
target.

BUG:		334702
FIXED-IN:	4.13.2

cmake/modules/KDE4Macros.cmake
@@ -829,9 +829,9 @@ macro (KDE4_ADD_KDEINIT_EXECUTABLE _target_NAME )
 
       if (Q_WS_MAC)
 	      list(FIND _SRCS *.icns _icon_position)
-	      if(NOT _res_position EQUAL -1)
+	      if(NOT _icon_position EQUAL -1)
 		      list(GET _SRCS ${_icon_position} _resourcefile)
-	      endif(NOT _res_position EQUAL -1)
+	      endif(NOT _icon_position EQUAL -1)
       endif (Q_WS_MAC)
       kde4_add_executable(${_target_NAME} "${_nogui}" ${CMAKE_CURRENT_BINARY_DIR}/${_target_NAME}_dummy.cpp ${_resourcefile})
       target_link_libraries(${_target_NAME} kdeinit_${_target_NAME})