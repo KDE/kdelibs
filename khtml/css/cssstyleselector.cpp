d79560a6118268799cd7aa8c9b73e56aaf900cb1
Andrea Iacovitti <aiacovitti@libero.it>
2014-07-01 21:11:52 +0200

    Initialize family to standard font name in FontDef() and simply/optimize the code.

--- khtml/css/cssstyleselector.cpp
+++ khtml/css/cssstyleselector.cpp
@@ -506,11 +506,7 @@ void CSSStyleSelector::computeFontSizesFor(int logicalDpiY, int zoomFactor, QVec
     const float toPix = 1.0;
 #else
     Q_UNUSED( isFixed );
-
-    // ### get rid of float / double
-    float toPix = logicalDpiY/72.0f;
-    if (toPix  < 96.0f/72.0f)
-         toPix = 96.0f/72.0f;
+    const float toPix = qMax(logicalDpiY, 96) / 72.0f;
 #endif // ######### fix isFixed code again.
 
     fontSizes.resize( MAXFONTSIZES );
@@ -3765,7 +3761,7 @@ void CSSStyleSelector::applyRule( int id, DOM::CSSValueImpl *value )
 #ifdef APPLE_CHANGES
             fontDef.family = initialDef.firstFamily();
 #else
-            fontDef.family.clear();
+            fontDef.family = initialDef.family;
 #endif
             if (style->setFontDef(fontDef))
                 fontDirty = true;
